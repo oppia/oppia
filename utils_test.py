df2744e4f9ab839f7aa639232dbfeb7cc228843b
Sean Lip <sll@google.com>
2013-03-15 15:47:54 -0700

    Add more tests for utils.

--- utils_test.py
+++ utils_test.py
@@ -38,3 +38,67 @@ class UtilsTests(test_utils.AppEngineTestBase):
         self.assertIn('Base', js_file)
         self.assertIn('function YamlEditor(', js_file)
         self.assertNotIn('function EditorExploration(', js_file)
+
+        # Try the case where no controllers are needed.
+        js_file = utils.get_js_controllers([])
+        self.assertEqual(js_file, '')
+
+    def test_parse_with_jinja(self):
+        """Test parse_with_jinja method."""
+        parsed_str = utils.parse_with_jinja('{{test}}', {'test': 'hi'})
+        self.assertEqual(parsed_str, 'hi')
+
+        # Some parameters are missing.
+        parsed_str = utils.parse_with_jinja(
+            '{{test}} and {{test2}}', {'test2': 'hi'})
+        self.assertEqual(parsed_str, ' and hi')
+
+        # All parameters are missing.
+        parsed_str = utils.parse_with_jinja('{{test}} and {{test2}}', {})
+        self.assertEqual(parsed_str, ' and ')
+
+        # Default parameters are used.
+        parsed_str = utils.parse_with_jinja('{{test}} and {{test2}}', {}, 'def')
+        self.assertEqual(parsed_str, 'def and def')
+
+        # The string has no parameters.
+        parsed_str = utils.parse_with_jinja('no params', {'param': 'hi'})
+        self.assertEqual(parsed_str, 'no params')
+
+        # Integer parameters are used.
+        parsed_str = utils.parse_with_jinja('int {{i}}', {'i': 2})
+        self.assertEqual(parsed_str, 'int 2')
+
+    def test_get_comma_sep_string_from_list(self):
+        """Test get_comma_sep_string_from_list method."""
+        alist = ['a', 'b', 'c', 'd']
+        results = ['', 'a', 'a and b', 'a, b and c', 'a, b, c and d']
+
+        for i in range(len(alist) + 1):
+            comma_sep_string = utils.get_comma_sep_string_from_list(alist[:i])
+            self.assertEqual(comma_sep_string, results[i])
+
+    def test_encode_strings_as_ascii(self):
+        """Test encode_strings_as_ascii method."""
+        # Integers.
+        o = utils.encode_strings_as_ascii(2)
+        self.assertEqual(o, 2)
+        self.assertNotEqual(o, '2')
+
+        # Floats.
+        o = utils.encode_strings_as_ascii(2.0)
+        self.assertEqual(o, 2.0)
+        self.assertNotEqual(o, '2')
+        self.assertNotEqual(o, '2.0')
+
+        # Strings.
+        o = utils.encode_strings_as_ascii('a')
+        self.assertEqual(o, 'a')
+        self.assertIsInstance(o, str)
+
+        # Unicode strings.
+        o = utils.encode_strings_as_ascii(u'a')
+        self.assertEqual(o, 'a')
+        self.assertIsInstance(o, str)
+
+        # TODO(sll): Add more tests here.
