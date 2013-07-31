8c223fcaf5f523d39fd741367d96ee1b1cf7f809
sunu <sunu0000@gmail.com>
2013-07-31 23:41:01 +0530

    Install django dependencies before running tests.

--- scripts/django_testrunner.sh
+++ scripts/django_testrunner.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+virtualenv venv;
+source venv/bin/activate;
+pip install -r requirements.txt;	
+
 python manage.py test oppia.storage.base_model.test_django \
 oppia.storage.image.test_django oppia.storage.parameter.test_django oppia.storage.state.test_django \
 oppia.storage.exploration.test_django ;
