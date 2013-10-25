16f1b5d1e28ba1a66d9806ae5d2e733fa32ba87e
Sean Lip <sll@google.com>
2013-10-25 16:19:28 -0700

    Do not install coverage directly on the system; run it locally. This should be less invasive.

--- scripts/setup_gae.sh
+++ scripts/setup_gae.sh
@@ -24,9 +24,10 @@ fi
 export SETUP_GAE_DONE=true
 
 export GOOGLE_APP_ENGINE_HOME=$TOOLS_DIR/google_appengine_1.7.7/google_appengine
+export COVERAGE_HOME=$TOOLS_DIR/coverage-3.6
 
 # Note that if the following line is changed so that it uses webob_1_1_1, PUT requests from the frontend fail.
-export PYTHONPATH=.:$GOOGLE_APP_ENGINE_HOME:$GOOGLE_APP_ENGINE_HOME/lib/webob_0_9:$TOOLS_DIR/webtest-1.4.2
+export PYTHONPATH=.:$COVERAGE_HOME:$GOOGLE_APP_ENGINE_HOME:$GOOGLE_APP_ENGINE_HOME/lib/webob_0_9:$TOOLS_DIR/webtest-1.4.2
 
 echo Deleting old *.pyc files
 find . -iname "*.pyc" -exec rm -f {} \;
--- scripts/test.sh
+++ scripts/test.sh
@@ -38,41 +38,16 @@ source $(dirname $0)/setup.sh || exit 1
 source $(dirname $0)/setup_gae.sh || exit 1
 
 
-# Note: you can safely delete all of the following code (up to the end of the
-# file) if it leads to errors on your system. It runs checks to see how well
-# the tests cover the code.
-
-echo Checking if coverage is installed on the system
-IS_COVERAGE_INSTALLED=$(python - << EOF
-import sys
-try:
-    import coverage as coverage_module
-except:
-    coverage_module = None
-if coverage_module:
-    sys.stderr.write('Coverage is installed in %s\n' % coverage_module.__path__)
-    print 1
-else:
-    sys.stderr.write('Coverage is NOT installed\n')
-    print 0
-EOF
-)
-
-if [ $IS_COVERAGE_INSTALLED = 0 ]; then
+echo Checking whether coverage is installed in $TOOLS_DIR
+if [ ! -d "$TOOLS_DIR/coverage-3.6" ]; then
   echo Installing coverage
   rm -rf $TOOLS_DIR/coverage || sudo rm -rf $TOOLS_DIR/coverage
   wget http://pypi.python.org/packages/source/c/coverage/coverage-3.6.tar.gz#md5=67d4e393f4c6a5ffc18605409d2aa1ac -O coverage.tar.gz
   tar xvzf coverage.tar.gz -C $TOOLS_DIR
   rm coverage.tar.gz
-  mv $TOOLS_DIR/coverage-3.6 $TOOLS_DIR/coverage
-
-  cd $TOOLS_DIR/coverage
-  python setup.py install || sudo python setup.py install
-  cd $OPPIA_DIR
-  rm -rf $TOOLS_DIR/coverage || sudo rm -rf $TOOLS_DIR/coverage
 fi
 
-coverage run ./core/tests/gae_suite.py $@
-coverage report --omit="$TOOLS_DIR/*","$THIRD_PARTY_DIR/*","/usr/share/pyshared/*" --show-missing
+python $COVERAGE_HOME/coverage run ./core/tests/gae_suite.py $@
+python $COVERAGE_HOME/coverage report --omit="$TOOLS_DIR/*","$THIRD_PARTY_DIR/*","/usr/share/pyshared/*" --show-missing
 
 echo Done!
