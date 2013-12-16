13a14e2fb760d80269cea49296cd172746e9697e
Sean Lip <sean@seanlip.org>
2013-12-15 17:31:30 -0800

    Separate the select2 and RTE directives.

--- core/templates/dev/head/components/richTextEditor.js
+++ core/templates/dev/head/components/richTextEditor.js
@@ -1,4 +1,4 @@
-// Copyright 2012 Google Inc. All Rights Reserved.
+// Copyright 2013 Google Inc. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,75 +13,10 @@
 // limitations under the License.
 
 /**
- * @fileoverview Directives for reusable simple editor components.
+ * @fileoverview Directive for the rich text editor component.
  *
  * @author sll@google.com (Sean Lip)
  */
-
-oppia.directive('select2Dropdown', function() {
-  // Directive for incorporating select2 dropdowns.
-  return {
-    restrict: 'E',
-    scope: {
-      choices: '=',
-      item: '=',
-      newChoiceRegex: '@',
-      placeholder: '@',
-      width: '@'
-    },
-    template: '<input type="hidden">',
-    controller: function($scope, $element, $attrs) {
-      $scope.newChoiceValidator = new RegExp($scope.newChoiceRegex);
-
-      $scope.makeUnique = function(arr) {
-        var hashMap = {};
-        var result = [];
-        for (var i = 0; i < arr.length; i++) {
-          if (!hashMap.hasOwnProperty(arr[i])) {
-            result.push(arr[i]);
-            hashMap[arr[i]] = 1;
-          }
-        }
-        return result;
-      };
-
-      $scope.uniqueChoices = $scope.makeUnique($scope.choices);
-      $scope.select2Choices = [];
-      for (var i = 0; i < $scope.uniqueChoices.length; i++) {
-        $scope.select2Choices.push({
-          id: $scope.uniqueChoices[i],
-          text: $scope.uniqueChoices[i]
-        });
-      }
-
-      var select2Node = $element[0].firstChild;
-      $(select2Node).select2({
-        data: $scope.select2Choices,
-        placeholder: $scope.placeholder,
-        width: $scope.width || '250px',
-        createSearchChoice: function(term, data) {
-          if ($(data).filter(function() {
-            return this.text.localeCompare(term) === 0;
-          }).length === 0) {
-            return (
-              term.match($scope.newChoiceValidator) ?
-                  {id: term, text: term} : null
-            );
-          }
-        }
-      });
-
-      // Initialize the dropdown.
-      $(select2Node).select2('val', $scope.item);
-
-      // Update $scope.item when the selection changes.
-      $(select2Node).on('change', function(e) {
-        $scope.item = e.val;
-        $scope.$apply();
-      });
-    }
-  };
-});
 
 oppia.directive('richTextEditor', function($q, $sce, $modal, $http, warningsData, oppiaHtmlEscaper, requestCreator) {
   return {
