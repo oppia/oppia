e30e0cbcca5b2d7c4e6368f24c8a8509bbd82f50
Sean Lip <sean@seanlip.org>
2013-12-15 17:17:51 -0800

    Monkey-patch appstats to log at level DEBUG instead of INFO.

--- appengine_config.py
+++ appengine_config.py
@@ -16,14 +16,49 @@
 
 __author__ = 'sll@google.com (Sean Lip)'
 
+import logging
 import os
 import sys
+import time
 
 appstats_CALC_RPC_COSTS = True
 
 
 def webapp_add_wsgi_middleware(app):
+    """Add AppStats recording.
+
+    This also sets the level of appstats log messages to 'debug' by
+    monkey-patching. For details, see:
+
+        http://stackoverflow.com/questions/4305243/disable-appstats-logging
+    """
     from google.appengine.ext.appstats import recording
+
+    def save(self):
+        t0 = time.time()
+        with self._lock:
+            num_pending = len(self.pending)
+        if num_pending:
+            logging.warn('Found %d RPC request(s) without matching response '
+                         '(presumably due to timeouts or other errors)',
+                         num_pending)
+        self.dump()
+        try:
+            key, len_part, len_full = self._save()
+        except Exception:
+            logging.exception('Recorder.save() failed')
+            return
+        t1 = time.time()
+        link = 'http://%s%s/details?time=%s' % (
+            self.env.get('HTTP_HOST', ''),
+            '/_ah/stats',
+            int(self.start_timestamp * 1000))
+        logging.debug('Saved; key: %s, part: %s bytes, full: %s bytes, '
+                      'overhead: %.3f + %.3f; link: %s',
+                      key, len_part, len_full, self.overhead, t1-t0, link)
+
+    recording.Recorder.save = save
+
     app = recording.appstats_wsgi_middleware(app)
     return app
 
