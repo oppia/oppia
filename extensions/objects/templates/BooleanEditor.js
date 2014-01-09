3d68da3e2720ae450989a33a1886e97db6ad4b16
Sean Lip <sean@seanlip.org>
2014-01-08 21:11:12 -0800

    Add templates for a boolean editor.

---
+++ extensions/objects/templates/BooleanEditor.js
@@ -0,0 +1,41 @@
+// Copyright 2012 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+// The value for this editor is always editable.
+
+oppia.directive('booleanEditor', function($compile, warningsData) {
+  return {
+    link: function(scope, element, attrs) {
+      scope.getTemplateUrl = function() {
+        return OBJECT_EDITOR_TEMPLATES_URL + scope.$parent.objType;
+      };
+      $compile(element.contents())(scope);
+    },
+    restrict: 'E',
+    scope: true,
+    template: '<span ng-include="getTemplateUrl()"></span>',
+    controller: function ($scope, $attrs) {
+      // Reset the component each time the value changes (e.g. if this is part
+      // of an editable list).
+      $scope.$watch('$parent.value', function(newValue, oldValue) {
+        $scope.localValue = {label: newValue || false};
+      }, true);
+
+      $scope.$watch('localValue.label', function(newValue, oldValue) {
+        $scope.$parent.value = newValue;
+      });
+    }
+  };
+});
