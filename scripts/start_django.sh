63f047754a3065b217a4d4631950945245a94a9a
Sean Lip <sll@google.com>
2014-01-31 17:43:45 -0800

    Add AUTHORS and CONTRIBUTORS files; update copyright notices.

--- scripts/start_django.sh
+++ scripts/start_django.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright 2013 Google Inc. All Rights Reserved.
+# Copyright 2014 The Oppia Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
