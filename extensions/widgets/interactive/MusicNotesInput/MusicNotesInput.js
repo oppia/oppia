006d7bb362d8cd97bf6748da6082f1ef7ab2c91c
Michael Wagner <wagnerdmike@gmail.com>
2014-03-16 22:48:42 -0700

    Update directive title

--- extensions/widgets/interactive/MusicNotesInput/MusicNotesInput.js
+++ extensions/widgets/interactive/MusicNotesInput/MusicNotesInput.js
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 /**
- * Directive for the CodeRepl interactive widget.
+ * Directive for the MusicNotesInput interactive widget.
  *
  * IMPORTANT NOTE: The naming convention for customization args that are passed
  * into the directive is: the name of the parameter, followed by 'With',
