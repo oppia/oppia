oppia/oppia

commit 5b4a64d16cc99028105e31fe1e1a81f41c78f222

Merge of missing answer changes, fixing conflicts

core/storage/base_model/gae_models.py
@@ -16,10 +16,7 @@
 
 __author__ = 'Sean Lip'
 
-import base64
 import feconf
-import hashlib
-
 import utils
 
 from core.platform import models
@@ -136,9 +133,9 @@ class BaseModel(ndb.Model):
         ID_LENGTH = 12
         # TODO(sll): Pull this logic out into utils.
         for i in range(MAX_RETRIES):
-            new_id = base64.urlsafe_b64encode(hashlib.sha1(
-                '%s%s' % (entity_name, utils.get_random_int(RAND_RANGE))
-            ).digest())[:ID_LENGTH]
+            new_id = utils.convert_to_hash(
+                '%s%s' % (entity_name, utils.get_random_int(RAND_RANGE)),
+                ID_LENGTH)
             if not cls.get_by_id(new_id):
                 return new_id
 

core/storage/statistics/gae_models.py
@@ -18,14 +18,22 @@
 
 __author__ = 'Sean Lip'
 
+import base64
 import logging
+import hashlib
 
 from core.platform import models
 (base_models,) = models.Registry.import_models([models.NAMES.base_model])
+import utils
 
 from google.appengine.ext import ndb
 
 QUERY_LIMIT = 100
+MAX_ANSWER_HASH_LEN = 100
+
+
+def hash_answer(answer):
+    return utils.convert_to_hash(answer, MAX_ANSWER_HASH_LEN)
 
 
 class StateCounterModel(base_models.BaseModel):
@@ -173,21 +181,157 @@ class FeedbackItemModel(base_models.BaseModel):
         ).filter(cls.status == 'new').fetch(QUERY_LIMIT)
 
 
+class UnresolvedAnswerHistoryModel(base_models.BaseModel):
+    """A record of an unresolved answer and the history that led up to it.
+
+    Entities here cannot be modified after they are created.
+    """
+    # A string containing the full (unhashed) answer.
+    answer = ndb.TextProperty()
+    # A record of the previous interactions by this reader.
+    history = ndb.JsonProperty()
+
+    @classmethod
+    def _construct_id(
+            cls, exploration_id, exploration_version, state_name, rule, answer):
+        return ':'.join([
+            exploration_id, str(exploration_version), state_name, rule,
+            hash_answer(answer)
+        ])
+
+    @classmethod
+    def get_or_create(
+            cls, exploration_id, exploration_version, state_name, rule, answer,
+            history):
+        """Gets or creates a new unresolved answers entity."""
+        entity_id = cls._construct_id(
+            exploration_id, exploration_version, state_name, rule, answer)
+        unresolved_answer_entity = cls(
+            id=entity_id, answer=answer, history=history)
+        # TODO(sfederwisch): figure out a way to store this so it doesn't go over
+        # maximum length of a property. NOTE: this only works for now since we are
+        # not retrieving UnresolvedAnswerHistoryModel for anything.
+        try:
+            unresolved_answer_entity.put()
+        except Exception as e:
+            logging.error(e)
+            pass
+
+    # TODO(sfederwisch): Implement the following methods (all of these have a
+    # given exploration id, exploration version, state name and rule):
+    #     get_entities_for_answer(..., answer)
+    #     get_an_entity_with_answer(..., answer)  (optional)
+    #     delete_entity(..., answer)
+
+
+class StateAnnotationsModel(base_models.BaseModel):
+    """Data related to a state.
+
+    This includes unresolved answers, reader-submitted feedback and any
+    calculated scores for the annotations (like confusion scores, etc.)
+    """
+    # Reader answers that have not been addressed. This is a dict keyed by a
+    # hash of the answer, and whose value is a dict with the following keys:
+    # 'answer', 'rule', 'count', 'exploration_version'.
+    unresolved_answers = ndb.JsonProperty(indexed=False)
+    # Save key values to be used later.
+    exp_id = ndb.StringProperty(indexed=False)
+    state_name = ndb.StringProperty(indexed=False) 
+
+    @classmethod
+    def get_or_create(cls, exp_id, state_name):
+        """Returns a state annotations object, possibly creating it."""
+        entity_id = '%s:%s' % (exp_id, state_name)
+
+        state_annotations = cls.get(entity_id, strict=False)
+        if not state_annotations:
+            state_annotations = cls(id=entity_id, unresolved_answers={},
+                                    exp_id=exp_id, state_name=state_name)
+        return state_annotations
+
+    def increment_unresolved_answer_count(
+            self, exploration_version, rule, answer):
+        """Increments the corresponding count for this unresolved answer."""
+        hashed_answer = hash_answer(answer)
+        if hashed_answer in self.unresolved_answers:
+            self.unresolved_answers[hashed_answer]['count'] += 1
+            score = self.unresolved_answers[hashed_answer]['count']
+        else:
+            self.unresolved_answers[hashed_answer] = {
+                'answer': answer,
+                'rule': str(rule),
+                'count': 1,
+                'exploration_version': exploration_version
+            }
+            score = 1
+        exp_model = ExplorationAnnotationsModel.get_or_create(
+            self.exp_id)
+        exp_model.update_state_data(self.state_name, 'missing',
+            score, answer, rule.is_generic)
+        exp_model.put()
+
+    # TODO(sfederwisch): Add additional methods and properties.
+
+
+class ExplorationAnnotationsModel(base_models.BaseModel):
+    """Data related to an exploration"""
+    # Threshold for an answer to qualify as 'missing'
+    MISSING_ANSWER_THRESHOLD = 7
+    # Data from states that would be retrieved for display with 
+    # exploration as a whole, namely which states should be flagged.
+    # This is a dict keyed by state_name, and whose value is a dict
+    # with the following keys: 'flag_type', 'score', 'data'.
+    summarized_state_data = ndb.JsonProperty(indexed=False)
+
+    @classmethod
+    def get_or_create(cls, exploration_id):
+        """Returns ExplorationAnnotationsModel, possibly creating it."""
+        exploration_annotations = cls.get(exploration_id, strict=False)
+        if not exploration_annotations:
+            exploration_annotations = cls(id=exploration_id,
+                                          summarized_state_data={})
+        return exploration_annotations
+
+    def update_state_data(self, state_name, flag_type, score, data, is_generic):
+        """Update state specific data if needed based off of new flag values."""
+        flaggable = False
+        if flag_type == "missing":
+            flaggable = (score > self.MISSING_ANSWER_THRESHOLD) and is_generic
+        if flaggable:
+            if state_name in self.summarized_state_data:
+                saved_score = self.summarized_state_data[state_name]['score']
+                if (score > saved_score):
+                    self.summarized_state_data[state_name] = {
+                        'flag_type': flag_type,
+                        'score': score,
+                        'data': data
+                    }
+            else:
+                self.summarized_state_data[state_name] = {
+                    'flag_type': flag_type,
+                    'score': score,
+                    'data': data
+                }
+        
+    # TODO(sfederwisch): add functions for resolved answers, or similar changes
+
+
 def process_submitted_answer(
-        exploration_id, state_name, handler_name, rule_str, answer):
+        exploration_id, exploration_version, state_name, handler_name,
+        rule, answer):
     """Adds an answer to the answer log for the rule it hits.
 
     Args:
         exploration_id: the exploration id
         state_name: the state name
         handler_name: a string representing the handler name (e.g., 'submit')
-        rule_str: a string representation of the rule
+        rule: the rule
         answer: an HTML string representation of the answer
     """
     # TODO(sll): Run these two updates in a transaction.
 
     answer_log = StateRuleAnswerLogModel.get_or_create(
-        exploration_id, state_name, handler_name, rule_str)
+        exploration_id, state_name, handler_name, str(rule))
     if answer in answer_log.answers:
         answer_log.answers[answer] += 1
     else:
@@ -204,8 +348,27 @@ def process_submitted_answer(
     counter.active_answer_count += 1
     counter.put()
 
+    state_annotations = StateAnnotationsModel.get_or_create(
+        exploration_id, state_name)
+    state_annotations.increment_unresolved_answer_count(
+        exploration_version, rule, answer)
+    # TODO(sfederwisch): figure out a way to store this so it doesn't go over
+    # maximum length of a property. NOTE: this only works for now since we are
+    # not retrieving StateAnnotations for anything.
+    try:
+        state_annotations.put()
+    except Exception as e:
+        logging.error(e)
+        pass
 
-def resolve_answers(exploration_id, state_name, handler_name, rule_str, answers):
+    # TODO(sfederwisch): Replace None with the actual reader history.
+    UnresolvedAnswerHistoryModel.get_or_create(
+        exploration_id, exploration_version, state_name, str(rule), answer,
+        None)
+
+
+def resolve_answers(
+        exploration_id, state_name, handler_name, rule_str, answers):
     """Resolves selected answers for the given rule.
 
     Args:
@@ -227,7 +390,8 @@ def resolve_answers(exploration_id, state_name, handler_name, rule_str, answers)
             logging.error(
                 'Answer %s not found in answer log for rule %s of exploration '
                 '%s, state %s, handler %s' % (
-                    answer, rule_str, exploration_id, state_name, handler_name))
+                    answer, rule_str, exploration_id, state_name,
+                    handler_name))
         else:
             resolved_count += answer_log.answers[answer]
             del answer_log.answers[answer]

utils.py
@@ -16,6 +16,8 @@
 
 __author__ = 'sll@google.com (Sean Lip)'
 
+import base64
+import hashlib
 import json
 import os
 import random
@@ -298,3 +300,19 @@ class JSONEncoderForHTML(json.JSONEncoder):
         for chunk in chunks:
             yield chunk.replace('&', '\\u0026').replace(
                 '<', '\\u003c').replace('>', '\\u003e')
+
+
+def convert_to_hash(string, max_length=None):
+    """Convert a string to a SHA1 hash."""
+    if not isinstance(string, basestring):
+        raise Exception(
+            'Expected string, received %s of type %s' %
+            (string, type(string)))
+
+    encoded_string = base64.urlsafe_b64encode(
+        hashlib.sha1(string).digest())
+
+    if max_length is None:
+        return encoded_string[:max_length]
+    else:
+        return encoded_string

core/tests/gae_suite.py
@@ -65,7 +65,7 @@ sys.path.append(root_dir)
 
 import feconf
 
-EXPECTED_TEST_COUNT = 233
+EXPECTED_TEST_COUNT = 235
 
 
 _PARSER = argparse.ArgumentParser()

core/controllers/editor.py
@@ -511,6 +511,7 @@ class ExplorationStatisticsHandler(EditorHandler):
         self.render_json({
             'num_visits': stats_services.get_exploration_visit_count(
                 exploration_id),
+            'exp_info': stats_services.get_exploration_info(exploration_id),
             'num_completions': stats_services.get_exploration_completed_count(
                 exploration_id),
             'state_stats': stats_services.get_state_stats_for_exploration(

core/controllers/reader.py
@@ -133,17 +133,16 @@ class FeedbackHandler(base.BaseHandler):
     REQUIRE_PAYLOAD_CSRF_CHECK = False
 
     def _append_answer_to_stats_log(
-            self, old_state, answer, exploration_id, old_state_name,
-            old_params, handler, rule):
+            self, old_state, answer, exploration_id, exploration_version,
+            old_state_name, old_params, handler, rule):
         """Append the reader's answer to the statistics log."""
         widget = widget_registry.Registry.get_widget_by_id(
             feconf.INTERACTIVE_PREFIX, old_state.widget.widget_id)
 
         recorded_answer = widget.get_stats_log_html(
             old_state.widget.customization_args, old_params, answer)
-
         stats_services.EventHandler.record_answer_submitted(
-            exploration_id, old_state_name, handler, str(rule),
+            exploration_id, 1, old_state_name, handler, rule,
             recorded_answer)
 
     def _append_content(self, exploration, sticky, finished, old_params,
@@ -226,8 +225,8 @@ class FeedbackHandler(base.BaseHandler):
         )
 
         self._append_answer_to_stats_log(
-            old_state, answer, exploration_id, old_state_name, old_params,
-            handler, rule)
+            old_state, answer, exploration_id, exploration.version,
+            old_state_name, old_params, handler, rule)
 
         # Append the reader's answer to the response HTML.
         reader_response_html = old_widget.get_reader_response_html(

core/domain/exp_domain.py
@@ -27,6 +27,7 @@ import logging
 import re
 import string
 
+
 from core.domain import fs_domain
 from core.domain import html_cleaner
 from core.domain import param_domain
@@ -150,7 +151,7 @@ class RuleSpec(object):
         }
 
     @classmethod
-    def from_dict(cls, rulespec_dict):
+    def from_dict_and_obj_type(cls, rulespec_dict, obj_type):
         return cls(
             rulespec_dict['definition'],
             rulespec_dict['dest'],
@@ -159,9 +160,10 @@ class RuleSpec(object):
                 param_change['name'], param_change['generator_id'],
                 param_change['customization_args'])
              for param_change in rulespec_dict['param_changes']],
+            obj_type,
         )
 
-    def __init__(self, definition, dest, feedback, param_changes):
+    def __init__(self, definition, dest, feedback, param_changes, obj_type):
         # A dict specifying the rule definition. E.g.
         #
         #   {'rule_type': 'default'}
@@ -184,11 +186,19 @@ class RuleSpec(object):
         # Exploration-level parameter changes to make if this rule is
         # triggered.
         self.param_changes = param_changes or []
+        self.obj_type = obj_type
 
     @property
     def is_default(self):
         """Returns True if this spec corresponds to the default rule."""
         return self.definition['rule_type'] == 'default'
+
+    @property
+    def is_generic(self):
+        """Returns whether this rule is generic."""
+        if self.is_default:
+            return True
+        return rule_domain.is_generic(self.obj_type, self.definition['name'])
 
     def get_feedback_string(self):
         """Returns a (possibly empty) string with feedback for this rule."""
@@ -205,8 +215,8 @@ class RuleSpec(object):
             return '%s(%s)' % (self.definition['name'], ','.join(param_list))
 
     @classmethod
-    def get_default_rule_spec(cls, state_name):
-        return RuleSpec({'rule_type': 'default'}, state_name, [], [])
+    def get_default_rule_spec(cls, state_name, obj_type):
+        return RuleSpec({'rule_type': 'default'}, state_name, [], [], obj_type)
 
     def validate(self):
         if not isinstance(self.definition, dict):
@@ -280,8 +290,7 @@ class RuleSpec(object):
                 cls.validate_rule_definition(child_rule, exp_param_specs)
 
 
-DEFAULT_RULESPEC = RuleSpec.get_default_rule_spec(feconf.END_DEST)
-DEFAULT_RULESPEC_STR = str(DEFAULT_RULESPEC)
+DEFAULT_RULESPEC_STR = 'Default'
 
 
 class AnswerHandlerInstance(object):
@@ -295,10 +304,10 @@ class AnswerHandlerInstance(object):
         }
 
     @classmethod
-    def from_dict(cls, handler_dict):
+    def from_dict_and_obj_type(cls, handler_dict, obj_type):
         return cls(
             handler_dict['name'],
-            [RuleSpec.from_dict(rs) for rs in handler_dict['rule_specs']],
+            [RuleSpec.from_dict_and_obj_type(rs, obj_type) for rs in handler_dict['rule_specs']],
         )
 
     def __init__(self, name, rule_specs=None):
@@ -308,7 +317,7 @@ class AnswerHandlerInstance(object):
         self.name = name
         self.rule_specs = [RuleSpec(
             rule_spec.definition, rule_spec.dest, rule_spec.feedback,
-            rule_spec.param_changes
+            rule_spec.param_changes, rule_spec.obj_type
         ) for rule_spec in rule_specs]
 
     @property
@@ -318,8 +327,8 @@ class AnswerHandlerInstance(object):
         return self.rule_specs[-1]
 
     @classmethod
-    def get_default_handler(cls, state_name):
-        return cls('submit', [RuleSpec.get_default_rule_spec(state_name)])
+    def get_default_handler(cls, state_name, obj_type):
+        return cls('submit', [RuleSpec.get_default_rule_spec(state_name, obj_type)])
 
     def validate(self):
         if self.name != 'submit':
@@ -350,11 +359,16 @@ class WidgetInstance(object):
         }
 
     @classmethod
+    def _get_obj_type(cls, widget_class_name):
+	return widget_registry.Registry.get_widget_by_id(feconf.INTERACTIVE_PREFIX, widget_class_name)._handlers[0]['obj_type']
+
+    @classmethod
     def from_dict(cls, widget_dict):
-        return cls(
+        obj_type = cls._get_obj_type(widget_dict['widget_id'])
+        return cls(    
             widget_dict['widget_id'],
             widget_dict['customization_args'],
-            [AnswerHandlerInstance.from_dict(h)
+            [AnswerHandlerInstance.from_dict_and_obj_type(h, obj_type)
              for h in widget_dict['handlers']],
             widget_dict['sticky'],
         )
@@ -426,11 +440,12 @@ class WidgetInstance(object):
 
     @classmethod
     def create_default_widget(cls, default_dest_state_name):
+        default_obj_type = WidgetInstance._get_obj_type(feconf.DEFAULT_WIDGET_ID)
         return cls(
             feconf.DEFAULT_WIDGET_ID,
             {},
             [AnswerHandlerInstance.get_default_handler(
-                default_dest_state_name)]
+                default_dest_state_name, default_obj_type)]
         )
 
 
@@ -513,7 +528,8 @@ class State(object):
                                      for feedback in rule_dict['feedback']]
             if 'param_changes' not in rule_dict:
                 rule_dict['param_changes'] = []
-            rule_spec = RuleSpec.from_dict(rule_dict)
+            obj_type = WidgetInstance._get_obj_type(self.widget.widget_id)
+            rule_spec = RuleSpec.from_dict_and_obj_type(rule_dict, obj_type)
             rule_type = rule_spec.definition['rule_type']
 
             if rule_ind == len(ruleset) - 1:
@@ -1137,7 +1153,7 @@ class Exploration(object):
                                     % pc.name)
 
             wdict = sdict['widget']
-            widget_handlers = [AnswerHandlerInstance.from_dict({
+            widget_handlers = [AnswerHandlerInstance.from_dict_and_obj_type({
                 'name': handler['name'],
                 'rule_specs': [{
                     'definition': rule_spec['definition'],
@@ -1146,7 +1162,7 @@ class Exploration(object):
                                  for feedback in rule_spec['feedback']],
                     'param_changes': rule_spec.get('param_changes', []),
                 } for rule_spec in handler['rule_specs']],
-            }) for handler in wdict['handlers']]
+            }, WidgetInstance._get_obj_type(wdict['widget_id'])) for handler in wdict['handlers']]
 
             state.widget = WidgetInstance(
                 wdict['widget_id'], wdict['customization_args'],

core/domain/rule_domain.py
@@ -70,6 +70,14 @@ def get_rules_for_obj_type(obj_type):
     return results
 
 
+def is_generic(obj_type, rule_name):
+    """Checks whether this rule is labelled generic"""
+    rule_list = get_rules_for_obj_type(obj_type)
+    for rule_type in rule_list:
+        if rule_type.__name__ == rule_name:
+            return rule_type.is_generic
+
+
 def get_param_list(description):
     """Get a parameter list from the rule description."""
     param_list = []

commit 93bbf5e217a3806cb1841a462b2a046d32864274

Fix a bug introduced in revision 5b4a64d16cc involving misgeneration of new exploration ids.

core/storage/base_model/gae_models.py
@@ -131,7 +131,6 @@ class BaseModel(ndb.Model):
         MAX_RETRIES = 10
         RAND_RANGE = 127 * 127
         ID_LENGTH = 12
-        # TODO(sll): Pull this logic out into utils.
         for i in range(MAX_RETRIES):
             new_id = utils.convert_to_hash(
                 '%s%s' % (entity_name, utils.get_random_int(RAND_RANGE)),

core/storage/statistics/gae_models.py
@@ -18,9 +18,7 @@
 
 __author__ = 'Sean Lip'
 
-import base64
 import logging
-import hashlib
 
 from core.platform import models
 (base_models,) = models.Registry.import_models([models.NAMES.base_model])
@@ -236,7 +234,7 @@ class StateAnnotationsModel(base_models.BaseModel):
     unresolved_answers = ndb.JsonProperty(indexed=False)
     # Save key values to be used later.
     exp_id = ndb.StringProperty(indexed=False)
-    state_name = ndb.StringProperty(indexed=False) 
+    state_name = ndb.StringProperty(indexed=False)
 
     @classmethod
     def get_or_create(cls, exp_id, state_name):
@@ -277,7 +275,7 @@ class ExplorationAnnotationsModel(base_models.BaseModel):
     """Data related to an exploration"""
     # Threshold for an answer to qualify as 'missing'
     MISSING_ANSWER_THRESHOLD = 7
-    # Data from states that would be retrieved for display with 
+    # Data from states that would be retrieved for display with
     # exploration as a whole, namely which states should be flagged.
     # This is a dict keyed by state_name, and whose value is a dict
     # with the following keys: 'flag_type', 'score', 'data'.
@@ -312,7 +310,7 @@ class ExplorationAnnotationsModel(base_models.BaseModel):
                     'score': score,
                     'data': data
                 }
-        
+
     # TODO(sfederwisch): add functions for resolved answers, or similar changes
 
 

utils.py
@@ -302,7 +302,7 @@ class JSONEncoderForHTML(json.JSONEncoder):
                 '<', '\\u003c').replace('>', '\\u003e')
 
 
-def convert_to_hash(string, max_length=None):
+def convert_to_hash(string, max_length):
     """Convert a string to a SHA1 hash."""
     if not isinstance(string, basestring):
         raise Exception(
@@ -312,7 +312,4 @@ def convert_to_hash(string, max_length=None):
     encoded_string = base64.urlsafe_b64encode(
         hashlib.sha1(string).digest())
 
-    if max_length is None:
-        return encoded_string[:max_length]
-    else:
-        return encoded_string
+    return encoded_string[:max_length]

core/controllers/galleries_test.py
@@ -85,6 +85,21 @@ class LearnGalleryTest(test_utils.GenericTestBase):
 
         self.logout()
 
+    def test_new_exploration_ids(self):
+        """Test generation of exploration ids."""
+        EDITOR_EMAIL = 'editor@example.com'
+        self.register_editor(EDITOR_EMAIL)
+        self.login(EDITOR_EMAIL)
+
+        response = self.testapp.get(feconf.CONTRIBUTE_GALLERY_URL)
+        self.assertEqual(response.status_int, 200)
+        csrf_token = self.get_csrf_token_from_response(response)
+        EXP_A_DICT = {'title': 'A', 'category': 'Test Explorations'}
+        exp_a_id = self.post_json(
+            feconf.NEW_EXPLORATION_URL, EXP_A_DICT, csrf_token
+        )[galleries.EXPLORATION_ID_KEY]
+        self.assertEqual(len(exp_a_id), 12)
+
     def test_can_see_explorations_to_playtest(self):
         """Test viewability of playtestable explorations."""
         EDITOR_EMAIL = 'editor@example.com'

core/tests/gae_suite.py
@@ -65,7 +65,7 @@ sys.path.append(root_dir)
 
 import feconf
 
-EXPECTED_TEST_COUNT = 235
+EXPECTED_TEST_COUNT = 237
 
 
 _PARSER = argparse.ArgumentParser()

utils_test.py
@@ -126,3 +126,13 @@ class UtilsTests(test_utils.GenericTestBase):
         with self.assertRaisesRegexp(
                 Exception, 'URL query parameter name must be a string'):
             utils.set_url_query_parameter('http://test.com?a=b', None, 'value')
+
+    def test_convert_to_hash(self):
+        """Test convert_to_hash() method."""
+        orig_string = 'name_to_convert'
+        full_hash = utils.convert_to_hash(orig_string, 28)
+        abbreviated_hash = utils.convert_to_hash(orig_string, 5)
+
+        self.assertEqual(len(full_hash), 28)
+        self.assertEqual(len(abbreviated_hash), 5)
+        self.assertEqual(full_hash[:5], abbreviated_hash)