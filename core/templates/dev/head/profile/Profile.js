fa2e79f017cd8a541161b59c61ef55d8441e045c
Koji Ashida <kojiashida@gmail.com>
2014-04-30 23:19:24 -0700

    Profile page expl stats coding fixes.

--- core/templates/dev/head/profile/Profile.js
+++ core/templates/dev/head/profile/Profile.js
@@ -19,17 +19,18 @@
  */
 
 function Profile($scope, $http, $rootScope, warningsData, oppiaRequestCreator) {
+  var EXPLORATION_STATUS_PRIVATE = 'private';
   $scope.profileDataUrl = '/profilehandler/data/';
   $rootScope.loadingMessage = 'Loading';
 
-  var findExpStats = function(data) {
+  var computeExplorationStats = function(data) {
     var result = {};
     var count = function(exps, totalProperty, privateProperty) {
       var totalCount = 0;
       var privateCount = 0;
-      for (id in exps) {
+      for (var id in exps) {
         totalCount++;
-        if (exps[id].rights.status == 'private') {
+        if (exps[id].rights.status == EXPLORATION_STATUS_PRIVATE) {
           privateCount++;
         }
       }
@@ -44,7 +45,7 @@ function Profile($scope, $http, $rootScope, warningsData, oppiaRequestCreator) {
 
   // Retrieves profile data from the server.
   $http.get($scope.profileDataUrl).success(function(data) {
-    $scope.explorationStats = findExpStats(data);
+    $scope.explorationStats = computeExplorationStats(data);
     $scope.ownedExplorations = data.owned;
     $scope.editableExplorations = data.editable;
     $scope.viewableExplorations = data.viewable;
