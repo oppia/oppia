e27b0f0062fb55b684fd57496c467fb907953417
Chin Zhan Xiong <czxcjx1234@gmail.com>
2014-08-02 00:23:00 +0800

    Redid graph visualization in a more angular-ish way

--- extensions/widgets/interactive/GraphInput/GraphInput.js
+++ extensions/widgets/interactive/GraphInput/GraphInput.js
@@ -28,49 +28,8 @@ oppia.directive('oppiaInteractiveGraphInput',[
 			templateUrl: 'interactiveWidget/GraphInput',
 			controller: ['$scope', '$element', '$attrs', function($scope, $element, $attrs) {
 				$scope.errorMessage = '';
-				var graph = {'vertices':[],'edges':[],'isDirected':[],'isWeighted':[],'isLabeled':[]};
-
-				var VIS_WIDTH = '100%';
-				var VIS_HEIGHT = 250;
-				var VERTEX_RADIUS = 5;
-				var EDGE_STYLE = "stroke:black;stroke-width:2;";
-
-				$scope.updateGraphViz = function() {
-					//Clear all SVG elements
-					d3.select($element[0]).selectAll('svg').remove();
-
-					//Create main SVG element
-					var graphVis = d3.select($element[0]).select('.oppia-graph-input-viz-container').append('svg:svg')
-						.attr({
-							'class': 'oppia-graph-input-viz',
-							'width': VIS_WIDTH,
-							'height': VIS_HEIGHT
-						});
-
-					//Create vertices
-					for (var i = 0; i < graph.vertices.length; i++) {
-						var vertex = graph.vertices[i];
-						graphVis.append('svg:circle').attr({
-							'cx': vertex.x,
-							'cy': vertex.y,
-							'r': VERTEX_RADIUS,
-							'stroke-width': 0,
-							'fill': 'black'
-						});
-					}
-
-					//Create edges
-					for (var i = 0; i < graph.edges.length; i++) {
-						var edge = graph.edges[i];
-						graphVis.append('svg:line').attr({
-							'x1': graph.vertices[edge.src].x,
-							'y1': graph.vertices[edge.src].y,
-							'x2': graph.vertices[edge.dst].x,
-							'y2': graph.vertices[edge.dst].y,
-							'style': EDGE_STYLE
-						});
-					}
-				}
+				$scope.graph = {'vertices':[],'edges':[],'isDirected':[],'isWeighted':[],'isLabeled':[]};
+				//$scope.updateGraphViz = function() {}
 
 				//TODO: Actually write this function?
 				$scope.checkValidGraph = function(g) {
@@ -81,8 +40,8 @@ oppia.directive('oppiaInteractiveGraphInput',[
 				$scope.updateGraphFromJSON = function(jsonGraph) {
 					var newGraph = JSON.parse(jsonGraph);
 					if ($scope.checkValidGraph(newGraph)) {
-						graph = newGraph;
-						$scope.updateGraphViz();
+						$scope.graph = newGraph;
+						//$scope.updateGraphViz();
 					} else {
 						errorMessage = "Invalid graph!";
 					}
@@ -92,7 +51,7 @@ oppia.directive('oppiaInteractiveGraphInput',[
 				}
 				
 				$scope.submitGraph = function() {
-					var strGraph = JSON.stringify(graph);
+					var strGraph = JSON.stringify($scope.graph);
 					$scope.$parent.$parent.submitAnswer(strGraph, 'submit');
 				};
 				
