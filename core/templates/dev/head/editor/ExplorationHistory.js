b1ef0e1aeb74111a665499ed144b445ce536482b
Sean Lip <sean@seanlip.org>
2014-08-02 15:06:03 -0700

    Move exploration history functions out into a separate controller.

---
+++ core/templates/dev/head/editor/ExplorationHistory.js
@@ -0,0 +1,54 @@
+// Copyright 2014 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Controllers for the exploration history tab.
+ *
+ * @author sll@google.com (Sean Lip)
+ */
+
+function ExplorationHistory($scope, $http, $modal, explorationData, warningsData) {
+  $scope.explorationId = explorationData.explorationId;
+  $scope.explorationSnapshotsUrl = '/createhandler/snapshots/' + $scope.$parent.explorationId;
+  $scope.explorationSnapshots = null;
+
+  $scope.$on('refreshVersionHistory', function(evt, data) {
+    if (data.forceRefresh || $scope.explorationSnapshots === null) {
+      $scope.refreshVersionHistory();
+    }
+  });
+
+  // Refreshes the displayed version history log.
+  $scope.refreshVersionHistory = function() {
+    $http.get($scope.explorationSnapshotsUrl).then(function(response) {
+      var data = response.data;
+
+      $scope.explorationSnapshots = [];
+      for (var i = 0; i < data.snapshots.length; i++) {
+        $scope.explorationSnapshots.push({
+          'committerId': data.snapshots[i].committer_id,
+          'createdOn': data.snapshots[i].created_on,
+          'commitMessage': data.snapshots[i].commit_message,
+          'versionNumber': data.snapshots[i].version_number,
+          'autoSummary': data.snapshots[i].auto_summary
+        });
+      }
+    });
+  };
+}
+
+/**
+ * Injects dependencies in a way that is preserved by minification.
+ */
+ExplorationHistory.$inject = ['$scope', '$http', '$modal', 'explorationData', 'warningsData'];
