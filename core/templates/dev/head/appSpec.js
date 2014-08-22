d23c8109020f957b461d035685cdaf523b5430c3
Sean Lip <sll@google.com>
2014-08-22 11:39:56 -0700

    Add a mock to give a specific date in tests.

--- core/templates/dev/head/appSpec.js
+++ core/templates/dev/head/appSpec.js
@@ -1,13 +1,13 @@
 // Copyright 2014 The Oppia Authors. All Rights Reserved.
 //
-// Licensed under the Apache License, Version 2.0 (the 'License');
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an 'AS-IS' BASIS,
+// distributed under the License is distributed on an "AS-IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -58,42 +58,44 @@ describe('Datetime Formatter', function() {
   beforeEach(module('oppia'));
 
   describe('datetimeformatter', function() {
+    // This corresponds to Fri, 21 Nov 2014 9:45:00 GMT.
+    var NOW_MILLIS = 1416563100000;
+    var YESTERDAY_MILLIS = NOW_MILLIS - 24 * 60 * 60 * 1000;
     var df = null;
+    var oldDate = Date;
 
     beforeEach(inject(function($injector) {
-	  df = $injector.get('oppiaDatetimeFormatter');
-	}));
+      df = $injector.get('oppiaDatetimeFormatter');
+
+      // Mock Date() to give a time of NOW_MILLIS in GMT. (Unfortunately, there
+      // doesn't seem to be a good way to set the timezone locale directly.)
+      spyOn(window, 'Date').andCallFake(function(optionalMillisSinceEpoch) {
+        if (optionalMillisSinceEpoch) {
+          return new oldDate(optionalMillisSinceEpoch);
+        } else {
+          return new oldDate(NOW_MILLIS);
+        }
+      });
+    }));
 
     it('should correctly return date time string', function() {
-	  
-      expect(df.getHumanReadableDatetime(1408124298786)).toBe('Fri, 15 Aug 2014 17:38:18 GMT');
+      expect(df.getHumanReadableDatetime(NOW_MILLIS)).toBe(
+        'Fri, 21 Nov 2014 09:45:00 GMT');
+      expect(df.getHumanReadableDatetime(YESTERDAY_MILLIS)).toBe(
+        'Thu, 20 Nov 2014 09:45:00 GMT');
     });
 
-    it('should correctly return local date time string', function() {
-
-      expect(df.getLocaleString(1408124298786)).toBe('8/15/2014 11:08:18 PM');
-    });
-    /**     
-     * it('should show only the time for a datetime occurring today', function() {
-     *
-     *  var today = new Date();
-     *  var millis = today.getTime();
-     *  expect(df.getLocaleAbbreviatedDatetimeString(millis)).toBe('03:00 PM'); 
-     * });
-     */	
-    it('should show only date for old date', function() {
-
-      var oldMillis = 966946483;
-      expect(df.getLocaleAbbreviatedDatetimeString(oldMillis).toBe('8/22/2000'));
+    it('should show only the time for a datetime occurring today', function() {
+      // In any timezone, 10 minutes before xx:45:00 should still fall within the
+      // same date as xx:45:00.
+      expect(df.getLocaleAbbreviatedDatetimeString(
+        NOW_MILLIS - 10 * 60 * 1000)).not.toBe('11/21/2014');
     });
 
-    it('should show only date for yesterday even when it's less thatn 24 hours ago', function() {
-      
-      var yesterday = new Date();
-      yesterday.setDate(yesterday.getDate() - 1);
-      // Make it less than 24 hours ago.
-      yesterday.setHour(yesterday.getHour() + 1);
-      expect(df.getLocaleAbbreviatedDatetimeString(yesterday.getTime()).toBe(yesterday.toLocaleDateString()));
-    });  
+    it('should show only the date for a datetime occurring before today', function() {
+      // 100 hours ago.
+      expect(df.getLocaleAbbreviatedDatetimeString(
+        NOW_MILLIS - 100 * 60 * 60 * 1000)).toBe('11/16/2014');
+    });
   });
 });
