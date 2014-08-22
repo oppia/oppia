25c2056bd6682175c6b34cae8830193ef8e427a4
Shntanu Bhowmik <sbhowmik@google.com>
2014-08-22 19:17:01 +0530

    Resolving comments.

--- core/templates/dev/head/appSpec.js
+++ core/templates/dev/head/appSpec.js
@@ -1,13 +1,13 @@
 // Copyright 2014 The Oppia Authors. All Rights Reserved.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the 'License');
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS-IS" BASIS,
+// distributed under the License is distributed on an 'AS-IS' BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -21,7 +21,7 @@
 describe('Validators service', function() {
   beforeEach(module('oppia'));
 
-  describe('validatorservice', function() {
+  describe('validators service', function() {
     var vs = null;
 
     beforeEach(inject(function($injector) {
@@ -58,35 +58,42 @@ describe('Datetime Formatter', function() {
   beforeEach(module('oppia'));
 
   describe('datetimeformatter', function() {
-    var vs = null;
+    var df = null;
 
     beforeEach(inject(function($injector) {
-      vs = $injector.get('oppiaDatetimeFormatter');
-    }));
+	  df = $injector.get('oppiaDatetimeFormatter');
+	}));
 
-      it('should correctly return date time string', function() {
-
-	expect(vs.getHumanReadableDatetime(1408124298786)).toBe('Fri, 15 Aug 2014 17:38:18 GMT');
-      });
-
-      it('should correctly return local date time string', function() {
-
-        expect(vs.getLocaleString(1408124298786).toBe('8/15/2014 11:08:18 PM'));
-      });
-	
-      it('should correctly return local date time string', function() {
-		
-        var d = new Date();
-	var millis = d.getTime();
-        var currentTime = new Date(millis).toLocaleTimeString().replace(/:\d\d /, ' ');
-        // Get yesterday's date.
-	d.setDate(date.getDate() - 1);
-        var yesterdayMillis = d.getTime();
-	var yesterdayDate =  new Date(millis).toLocaleDateString();
-		
-        expect(vs.getLocaleString(millis).toBe(currentTime));
-	
-	expect(vs.getLocaleString(yesterdayMillis).toBe(yesterdayDate));
-      });	
+    it('should correctly return date time string', function() {
+	  
+      expect(df.getHumanReadableDatetime(1408124298786)).toBe('Fri, 15 Aug 2014 17:38:18 GMT');
     });
+
+    it('should correctly return local date time string', function() {
+
+      expect(df.getLocaleString(1408124298786)).toBe('8/15/2014 11:08:18 PM');
+    });
+    /**     
+     * it('should show only the time for a datetime occurring today', function() {
+     *
+     *  var today = new Date();
+     *  var millis = today.getTime();
+     *  expect(df.getLocaleAbbreviatedDatetimeString(millis)).toBe('03:00 PM'); 
+     * });
+     */	
+    it('should show only date for old date', function() {
+
+      var oldMillis = 966946483;
+      expect(df.getLocaleAbbreviatedDatetimeString(oldMillis).toBe('8/22/2000'));
+    });
+
+    it('should show only date for yesterday even when it's less thatn 24 hours ago', function() {
+      
+      var yesterday = new Date();
+      yesterday.setDate(yesterday.getDate() - 1);
+      // Make it less than 24 hours ago.
+      yesterday.setHour(yesterday.getHour() + 1);
+      expect(df.getLocaleAbbreviatedDatetimeString(yesterday.getTime()).toBe(yesterday.toLocaleDateString()));
+    });  
+  });
 });
