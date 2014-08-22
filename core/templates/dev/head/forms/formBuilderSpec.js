0ca47686933a520d57a826ed9b24d1e21e6077c7
Sean Lip <sean@seanlip.org>
2014-08-21 20:42:15 -0700

    Move HTML editor code to the schema-based editor files.

--- core/templates/dev/head/forms/formBuilderSpec.js
+++ core/templates/dev/head/forms/formBuilderSpec.js
@@ -136,3 +136,58 @@ describe('Normalizer tests', function() {
     expect(filter('')).toBe(false);
   }));
 });
+
+describe('RTE directive', function() {
+  var elm, scope, $httpBackend;
+
+  beforeEach(module('oppia'));
+  beforeEach(inject(function($rootScope, $compile, _$httpBackend_) {
+    $httpBackend = _$httpBackend_;
+    $httpBackend.expectGET('/widgetrepository/data/noninteractive').respond({
+      data: {
+        widgetRepository: {
+          'Image': [{
+            frontend_name: 'image',
+            name: 'Image',
+            tooltip: 'Insert image',
+            icon_data_url: 'data:123'
+          }]
+        }
+      }
+    });
+
+    elm = $compile('<rich-text-editor></rich-text-editor>')($rootScope);
+    scope = $rootScope;
+    scope.$digest();
+  }));
+
+  it('should convert correctly between HTML and RTE', inject(function($rootScope, $compile) {
+    var testData = [
+      ['<div></div>', '<div></div>'],
+      ['<div>abc</div>', '<div>abc</div>'],
+      ['<div>abc</div><br>', '<div>abc</div><br>'],
+      ['<div>abc<span>def</span></div><b>ghi</b>', '<div>abc<span>def</span></div><b>ghi</b>'],
+      ['<oppia-noninteractive-image></oppia-noninteractive-image>',
+       '<img src="data:123" class="oppia-noninteractive-image">'],
+      ['<oppia-noninteractive-image image_id-with-value="&amp;quot;T&amp;quot;"></oppia-noninteractive-image>',
+       '<img src="data:123" class="oppia-noninteractive-image" image_id-with-value="&amp;quot;T&amp;quot;">']
+    ];
+
+    var rteControllerScope = elm.scope();
+
+    // TODO(sll): Why isn't this being auto-populated?
+    rteControllerScope._NONINTERACTIVE_WIDGETS = [{
+      name: 'image',
+      backendName: 'Image',
+      tooltip: 'Insert image',
+      iconDataUrl: 'data:123'
+    }];
+
+    for (var i = 0; i < testData.length; i++) {
+      expect(rteControllerScope._convertHtmlToRte(testData[i][0]))
+        .toEqual(testData[i][1]);
+      expect(rteControllerScope._convertRteToHtml(testData[i][1]))
+        .toEqual(testData[i][0]);
+    }
+  }));
+});
