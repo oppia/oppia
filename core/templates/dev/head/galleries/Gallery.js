3a90191a8e699819f6a84594efd1ac9d354ac3df
Jasper Deng <jasper.deng@gmail.com>
2014-11-16 17:16:58 -0800

    Cause the search icon to change to a different symbol while results are being fetched

--- core/templates/dev/head/galleries/Gallery.js
+++ core/templates/dev/head/galleries/Gallery.js
@@ -105,6 +105,7 @@ oppia.controller('Gallery', [
              oppiaDatetimeFormatter, oppiaDebouncer, urlService) {
   $scope.galleryDataUrl = '/galleryhandler/data';
   $scope.currentUserIsModerator = false;
+  $scope.isLoading = false;
 
   $scope.getCategoryList = function() {
     return Object.keys($scope.selectedCategories);
@@ -141,6 +142,7 @@ oppia.controller('Gallery', [
     if (!$scope.searchQuery) {
       $http.get($scope.galleryDataUrl).success($scope.refreshGalleryData);
     } else {
+      $scope.isLoading = true;
       $http.get($scope.galleryDataUrl + '?q=' + $scope.searchQuery).success(
         $scope.refreshGalleryData);
     }
@@ -150,6 +152,7 @@ oppia.controller('Gallery', [
     $scope.onSearchQueryChangeExec, 400);
 
   $scope.refreshGalleryData = function(data) {
+    $scope.isLoading = false;
     $scope.featuredExplorations = data.featured;
     $scope.publicExplorations = data['public'];
 
