28fc72696539cc52d9192e52aaacc47bb683fcd6
Jacob Davis <jacobdavis11@gmail.com>
2014-11-30 01:58:10 -0500

    Protractor tests: fix capitalisation

--- extensions/objects/protractor.js
+++ extensions/objects/protractor.js
@@ -39,7 +39,7 @@ var BooleanEditor = function(elem) {
 
 var CoordTwoDim = function(elem) {
   return {
-    // Coordinates is a two-element list whose elements represent latitude and
+    // coordinates is a two-element list whose elements represent latitude and
     // longitude respectively.
     setValue: function(coordinates) {
       elem.all(by.tagName('input')).first().clear();
