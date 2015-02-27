c463479ce1e990345e9d7db2794192cef85c3254
Shantanu Bhowmik <sbhowmik@google.com>
2015-02-28 01:47:37 +0530

    Redesigning the dashboard to include tiles similar to gallery. Issue 632.

--- core/templates/dev/head/dashboard/Dashboard.js
+++ core/templates/dev/head/dashboard/Dashboard.js
@@ -36,9 +36,62 @@ oppia.controller('Dashboard', [
   var EXPLORATION_STATUS_PUBLIC = 'public';
   var EXPLORATION_STATUS_FEATURED = 'publicized';
 
+  // Default color.
+  var _COLOR_TEAL = 'teal';
+  // Social sciences.
+  var _COLOR_SALMON = 'salmon';
+  // Art.
+  var _COLOR_SUNNYSIDE = 'sunnyside';
+  // Mathematics and computing.
+  var _COLOR_SHARKFIN = 'sharkfin';
+  // Science.
+  var _COLOR_GUNMETAL = 'gunmetal';
+
+  var CATEGORY_TO_DEFAULT_COLOR = {
+    'Architecture': _COLOR_SUNNYSIDE,
+    'Art': _COLOR_SUNNYSIDE,
+    'Biology': _COLOR_GUNMETAL,
+    'Business': _COLOR_SALMON,
+    'Chemistry': _COLOR_GUNMETAL,
+    'Computing': _COLOR_SHARKFIN,
+    'Economics': _COLOR_SALMON,
+    'Education': _COLOR_TEAL,
+    'Engineering': _COLOR_GUNMETAL,
+    'Environment': _COLOR_GUNMETAL,
+    'Geography': _COLOR_SALMON,
+    'Government': _COLOR_SALMON,
+    'Hobbies': _COLOR_TEAL,
+    'Languages': _COLOR_SUNNYSIDE,
+    'Law': _COLOR_SALMON,
+    'Life Skills': _COLOR_TEAL,
+    'Mathematics': _COLOR_SHARKFIN,
+    'Medicine': _COLOR_GUNMETAL,
+    'Music': _COLOR_SUNNYSIDE,
+    'Philosophy': _COLOR_SALMON,
+    'Physics': _COLOR_GUNMETAL,
+    'Programming': _COLOR_SHARKFIN,
+    'Psychology': _COLOR_SALMON,
+    'Puzzles': _COLOR_TEAL,
+    'Reading': _COLOR_TEAL,
+    'Religion': _COLOR_SALMON,
+    'Sport': _COLOR_SUNNYSIDE,
+    'Statistics': _COLOR_SHARKFIN,
+    'Welcome': _COLOR_TEAL
+  };
+
   $scope.navigateToItem = function(activityId, updateType) {
     window.location.href = (
       '/create/' + activityId + (updateType === 'feedback_thread' ? '#/feedback': ''));
+  };
+
+  $scope.getImageSrcUrl = function(exploration) {
+    return '/images/gallery/default.png';
+  };
+
+  $scope.getImageContainerClass = function(exploration) {
+    var color = CATEGORY_TO_DEFAULT_COLOR.hasOwnProperty(exploration.category) ?
+      CATEGORY_TO_DEFAULT_COLOR[exploration.category] : _COLOR_TEAL;
+    return 'oppia-gallery-tile-image-translucent oppia-gallery-tile-image-' + color;
   };
 
   $scope.getLocaleAbbreviatedDatetimeString = function(millisSinceEpoch) {
@@ -60,19 +113,19 @@ oppia.controller('Dashboard', [
     $scope.lastSeenMsec = data.last_seen_msec || 0.0;
     $scope.currentUsername = data.username;
 
-    $scope.privateExplorationIds = [];
-    $scope.publicExplorationIds = [];
-    $scope.featuredExplorationIds = [];
+    $scope.privateExplorations = [];
+    $scope.publicExplorations = [];
+    $scope.featuredExplorations = [];
     $scope.explorations = data.explorations;
 
     for (var expId in $scope.explorations) {
       var status = $scope.explorations[expId].status;
       if (status == EXPLORATION_STATUS_PRIVATE) {
-        $scope.privateExplorationIds.push(expId);
+        $scope.privateExplorations.push($scope.explorations[expId]);
       } else if (status == EXPLORATION_STATUS_PUBLIC) {
-        $scope.publicExplorationIds.push(expId);
+        $scope.publicExplorations.push($scope.explorations[expId]);
       } else if (status == EXPLORATION_STATUS_FEATURED) {
-        $scope.featuredExplorationIds.push(expId);
+        $scope.featuredExplorations.push($scope.explorations[expId]);
       } else {
         throw ('Error: Invalid exploration status ' + status);
       }
