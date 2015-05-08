2beb9f9aa94403ce33cd7c04d2f76b614a158626
Sean Lip <sean@seanlip.org>
2015-05-07 22:40:04 -0700

    Fix broken test.

--- core/controllers/home_test.py
+++ core/controllers/home_test.py
@@ -31,9 +31,7 @@ class HomePageTest(test_utils.GenericTestBase):
         self.assertEqual(response.status_int, 200)
         response.mustcontain(
             'Your personal tutor',
-            'Oppia - Gallery', 'About', 'Login',
-            # No navbar tabs should be highlighted.
-            no=['class="active"', 'Logout'])
+            'Oppia - Gallery', 'About', 'Login', no=['Logout'])
 
     def test_notifications_dashboard_redirects_for_logged_out_users(self):
         """Test the logged-out view of the notifications dashboard."""
