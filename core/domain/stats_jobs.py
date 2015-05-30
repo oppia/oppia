37d4bd1025341998c20dc90924937487ef8690e4
Sean Lip <sll@google.com>
2015-05-29 18:05:55 -0700

    Tidy up some code, and fix lint issues.

--- core/domain/stats_jobs.py
+++ core/domain/stats_jobs.py
@@ -392,71 +392,98 @@ class NullStateHitEventsMigrator(jobs.BaseMapReduceJobManager):
 
 class StatisticsAudit(jobs.BaseMapReduceJobManager):
 
+    _STATE_COUNTER_ERROR_KEY = 'State Counter ERROR'
+
     @classmethod
     def entity_classes_to_map_over(cls):
-        return [stats_models.ExplorationAnnotationsModel,
-                stats_models.StateCounterModel]
+        return [
+            stats_models.ExplorationAnnotationsModel,
+            stats_models.StateCounterModel]
 
     @staticmethod
     def map(item):
-       if isinstance(item, stats_models.StateCounterModel):
-           if item.first_entry_count < 0:
-               yield ('State Counter ERROR',
-                   'Less than 0: %s %d' % (item.key, item.first_entry_count))
-           return
-       if item.exploration_id is None:
-           return
-       yield (item.exploration_id, { 'version': item.version,
-                                     'starts': item.num_starts,
-                                     'comp': item.num_completions,
-                                     'state_hit': item.state_hit_counts})
+        if isinstance(item, stats_models.StateCounterModel):
+            if item.first_entry_count < 0:
+                yield (
+                    StatisticsAudit._STATE_COUNTER_ERROR_KEY,
+                    'Less than 0: %s %d' % (item.key, item.first_entry_count))
+        elif item.exploration_id is None:
+            return
+
+        yield (item.exploration_id, {
+            'version': item.version,
+            'starts': item.num_starts,
+            'completions': item.num_completions,
+            'state_hit': item.state_hit_counts
+        })
 
     @staticmethod
     def reduce(key, stringified_values):
-        if key == 'State Counter ERROR':
+        if key == StatisticsAudit._STATE_COUNTER_ERROR_KEY:
             for value_str in stringified_values:
                 yield (value_str,)
             return
+
+        # If the code reaches this point, we are looking at values that
+        # correspond to each version of a particular exploration.
+
+        # These variables correspond to the _ALL_VERSIONS_STRING version.
         all_starts = 0
-        all_comps = 0
-        all_state_hit = {}
+        all_completions = 0
+        all_state_hit = collections.defaultdict(int)
+
+        # These variables correspond to the sum of counts for all other
+        # versions besides _ALL_VERSIONS_STRING.
         sum_starts = 0
-        sum_comps = 0
-        sum_state_hit = {}
+        sum_completions = 0
+        sum_state_hit = collections.defaultdict(int)
+
         for value_str in stringified_values:
             value = ast.literal_eval(value_str)
             if value['starts'] < 0:
-                yield ('starts is less than 0 exp_id:%s version:%s starts:%s' % (key, value['version'], value['starts']),)
-            if value['comp'] < 0:
-                yield ('comps is less than 0 exp_id:%s version:%s starts:%s' % (key, value['version'], value['comp']),)
-            if value['comp'] > value['starts']:
-                yield ('comps > starts exp_id:%s version:%s %s>%s' % (key, value['version'], value['comp'], value['starts']),)
-            if value['version'] == 'all':
+                yield (
+                    'Negative start count: exp_id:%s version:%s starts:%s' %
+                    (key, value['version'], value['starts']),)
+
+            if value['completions'] < 0:
+                yield (
+                    'Negative completion count: exp_id:%s version:%s '
+                    'completions:%s' %
+                    (key, value['version'], value['completions']),)
+
+            if value['completions'] > value['starts']:
+                yield ('Completions > starts: exp_id:%s version:%s %s>%s' % (
+                    key, value['version'], value['completions'],
+                    value['starts']),)
+
+            if value['version'] == _ALL_VERSIONS_STRING:
                 all_starts = value['starts']
-                all_comps = value['comp']
-                for state_hit in value['state_hit']:
-                    count = value['state_hit'][state_hit]['first_entry_count']
-                    all_state_hit[state_hit] = count
+                all_completions = value['completions']
+                for (state_name, counts) in value['state_hit'].iteritems():
+                    all_state_hit[state_name] = counts['first_entry_count']
             else:
                 sum_starts += value['starts']
-                sum_comps += value['comp']
-                for state_hit in value['state_hit']:
-                    count = value['state_hit'][state_hit]['first_entry_count']
-                    if state_hit in sum_state_hit:
-                        sum_state_hit[state_hit] += count
-                    else:
-                        sum_state_hit[state_hit] = count
+                sum_completions += value['completions']
+                for (state_name, counts) in value['state_hit'].iteritems():
+                    sum_state_hit[state_name] += counts['first_entry_count']
 
         if sum_starts != all_starts:
-            yield ('sum of starts != all exp_id:%s sum: %s all: %s'
+            yield ('Non-all != all for starts: exp_id:%s sum: %s all: %s'
                 % (key, sum_starts, all_starts),)
-        if sum_comps != all_comps:
-            yield ('sum of comps != all exp_id:%s sum: %s all: %s'
-                % (key, sum_comps, all_comps),)
-        if all_state_hit != sum_state_hit:
-            for state_hit in all_state_hit:
-                if state_hit not in sum_state_hit:
-                    if all_state_hit[state_hit] != 0:
-                        yield ('state in all that is not in sum exp_id:%s state_name:%s, all:%s' % (key, state_hit, all_state_hit[state_hit]),)
-                elif all_state_hit[state_hit] != sum_state_hit[state_hit]:
-                    yield ('state hit count not same exp_id: %s state: %s all: %s sum:%s' % (key, state_hit, all_state_hit[state_hit], sum_state_hit[state_hit]),)
+        if sum_completions != all_completions:
+            yield ('Non-all != all for completions: exp_id:%s sum: %s all: %s'
+                % (key, sum_completions, all_completions),)
+
+        for state_name in all_state_hit:
+            if (state_name not in sum_state_hit and
+                    all_state_hit[state_name] != 0):
+                yield (
+                    'state hit count not same exp_id:%s state:%s, '
+                    'all:%s sum: null' % (
+                        key, state_name, all_state_hit[state_name]),)
+            elif all_state_hit[state_name] != sum_state_hit[state_name]:
+                yield (
+                    'state hit count not same exp_id: %s state: %s '
+                    'all: %s sum:%s' % (
+                        key, state_name, all_state_hit[state_name],
+                        sum_state_hit[state_name]),)
