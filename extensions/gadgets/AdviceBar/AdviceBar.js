144dbebe64b05a0115193c6c50694d460f87ddcc
Michael Anuzis <anuzis@google.com>
2015-06-23 12:02:38 -0400

    Minor refinements to AdviceBar.js adviceBar.css ScoreBar.js and scoreBar.css per front-end review feedback. Awaiting decision on which refactoring route to take to bring gadget size constants to the frontend. (options detailed inline in prior code review on AdviceBar.js line 59).

--- extensions/gadgets/AdviceBar/AdviceBar.js
+++ extensions/gadgets/AdviceBar/AdviceBar.js
@@ -27,13 +27,6 @@ oppia.directive('oppiaGadgetAdviceBar', [
     var _MAX_TIP_COUNT = 3;
     var _MIN_TIP_COUNT = 1;
 
-    // Constants for calculation of height and width.
-    // TODO(anuzis): Update these values to reflect actual size
-    // as UX polish is finalized.
-    var _WIDTH = 100;
-    var _TITLE_HEIGHT = 50;
-    var _HEIGHT_PER_ADVICE_RESOURCE = 100;
-
     return {
       restrict: 'E',
       templateUrl: 'gadget/AdviceBar',
@@ -41,58 +34,22 @@ oppia.directive('oppiaGadgetAdviceBar', [
         $scope.adviceBarTitle = oppiaHtmlEscaper.escapedJsonToObj($attrs.titleWithValue);
         $scope.adviceBarResources = oppiaHtmlEscaper.escapedJsonToObj($attrs.adviceObjectsWithValue);
 
-        $scope.getWarnings = function() {
+        // TODO(anuzis): Update this method to suit the front-end API Sean and
+        // Vishal determine is best when it's decided.
+        $scope.validate = function() {
           var tipCount = $scope.adviceBarResources.length;
           if (tipCount > _MAX_TIP_COUNT) {
             var validationError = 'AdviceBars are limited to ' +
-              _MAX_TIP_COUNT + ' tip' + (_MAX_TIP_COUNT>1?'s':'') + '.';
+              _MAX_TIP_COUNT + ' tip' + (_MAX_TIP_COUNT>1 ? 's' : '') + '.';
             return validationError;
           } else if (tipCount < _MIN_TIP_COUNT) {
             var validationError = 'AdviceBars need at least ' +
-              _MIN_TIP_COUNT + ' tip' + (_MIN_TIP_COUNT>1?'s':'') + '.';
+              _MIN_TIP_COUNT + ' tip' + (_MIN_TIP_COUNT>1 ? 's' : '') + '.';
             return validationError;
           } else {
             return '';
           }
-        }
-
-        // @sll: Per our VC, we agreed to have each gadget provide static
-        // constants for WIDTH and HEIGHT rather than a dynamic value that
-        // depends on its customization args. After the VC, Vishal and I
-        // discussed ways of implementing this and again the only simple
-        // solution appeared to be having gadget developers hard-code these
-        // values in a shared Service.
-        //
-        // We considered something akin to a front-end gadget Registry that
-        // would have a record of approved gadgets (similar to feconf.py)
-        // and which directories to retrieve their constants from based
-        // on gadgetType, but that didn't seem trivial to implement this
-        // afternoon. Any suggestions on how to validate gadget width/height
-        // keeping files cleanly decoupled in each gadget's directory?
-        //
-        // Even if height/width values aren't dynamic based on customization
-        // args, we want to be able to enforce reasonable defaults that
-        // prevent vertical AdviceBars from being added to the bottom, and
-        // the horizontal ScoreBar from being added on the left/right.
-
-        $scope.getHeight = function() {
-          var height = 0;
-          var tip_count = $scope.adviceBarResources.length;
-
-          // If the AdviceBar has a title to display, factor it into
-          // the gadget's height.
-          if ($scope.adviceBarTitle != '') {
-            height = height + _TITLE_HEIGHT;
-          }
-          height += _HEIGHT_PER_ADVICE_RESOURCE * tip_count;
-          return height;
-        }
-
-        $scope.getWidth = function () {
-          return _WIDTH;
-        }
-
-
+        };
 
         $scope.overlayAdviceModal = function(adviceResourceIndex) {
           $modal.open({
