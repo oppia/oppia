9c83fea4da724a29d9b53a04c298281d7e679134
MAKOSCAFEE <barnabasmakonda@gmail.com>
2015-09-17 11:04:07 +0300

    clean the code more

--- core/templates/dev/head/editor/ExplorationEditor.js
+++ core/templates/dev/head/editor/ExplorationEditor.js
@@ -454,13 +454,9 @@ oppia.controller('ExplorationSaveAndPublishButtons', [
   };
 
   $scope.saveChanges = function() {
-<<<<<<< HEAD
-    
-    routerService.savePendingChanges();
-=======
+
     routerService.savePendingChanges();
 
->>>>>>> parent of 04aa127... prevent multiple modal from opening Fixes #762
     $scope.changeListSummaryUrl = (
       '/createhandler/change_list_summary/' + explorationData.explorationId);
 
