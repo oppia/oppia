f59b6edae8e39df8f9739825115b2a0357971b65
Michael Anuzis <anuzis@google.com>
2015-09-22 14:48:06 +0900

    Adding an object editor for the new ParameterName object Sean created. Updating protractor tests to use it. Correcting positioning of bottom gadget panel HTML. Updating backend test count. Some tests are failing with the left/right panels removed. Non-trivial to refactor them out completely. Meeting Amit to discuss.

--- extensions/gadgets/ScoreBar/ScoreBar.py
+++ extensions/gadgets/ScoreBar/ScoreBar.py
@@ -44,8 +44,6 @@ class ScoreBar(base.BaseGadget):
         },
         'default_value': 100
     }, {
-        # TODO(anuzis): Validate that the paramName corresponds to a valid
-        # parameter name in the exploration.
         'name': 'paramName',
         'description': 'The parameter name this score bar follows.',
         'schema': {
