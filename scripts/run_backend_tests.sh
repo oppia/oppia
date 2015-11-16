7a023eaaf69c4ee69ff4229090fcd0cd753cf320
Isaac Friedman <isaac@isaac-friedman.com>
2015-11-15 21:49:12 -0500

    latest iteration on adding numpy

--- scripts/run_backend_tests.sh
+++ scripts/run_backend_tests.sh
@@ -67,7 +67,12 @@ set -e
 source $(dirname $0)/setup.sh || exit 1
 source $(dirname $0)/setup_gae.sh || exit 1
 
-pip install numpy==1.6.1 --target="$THIRD_PARTY_DIR"
+# Install numpy.
+echo Checking if numpy is installed in third_party/pip_packages
+if [ ! -d "$THIRD_PARTY_DIR/pip_packages/numpy" ]; then
+  echo Installing numpy
+  pip install numpy==$NUMPY_REV --target="$THIRD_PARTY_DIR/pip_packages"
+fi
 
 # Install webtest.
 echo Checking if webtest is installed in third_party
