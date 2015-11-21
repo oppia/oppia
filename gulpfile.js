94848a40e048d4ea648585aef547cdc7fe9d4ccf
MAKOSCAFEE <barnabasmakonda@gmail.com>
2015-11-21 05:09:32 +0300

    addressed all review comments

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@
 
 var argv = require('yargs')
     .usage(
-      'Usage: $0 --gae_devserver_path [path to app engine]' +
+      'Usage: $0 --gaeDevserverPath [path to app engine]' +
       '--clear_datastore [whether to clear all data storage]' +
       '--enable_sendmail [whether to send emails]' +
       '--minify [whether to minify third-party dependencies]').argv;
@@ -32,7 +32,7 @@ var manifest = require('./manifest.json');
 var minifyCss = require('gulp-minify-css');
 var path = require('path');
 
-var gae_devserver_path = argv.gae_devserver_path;
+var gaeDevserverPath = argv.gaeDevserverPath;
 var params = {
   admin_host: '0.0.0.0',
   admin_port: 8000,
@@ -53,28 +53,27 @@ var cssFilesPath = [];
 var fontFolderPath = [];
 var cssBackgroundPath = [];
 var generatedCssTargetDir = path.join(
-  'third_party', 'generated', isMinificationNeeded ? 'prod' : 'dev', 'css');
+  'third_party', 'generated',
+  isMinificationNeeded ? 'prod' : 'dev', 'css');
 
 for (var dependencyId in frontendDependencies) {
   var dependency = frontendDependencies[dependencyId];
+  var dependencyDir = dependency.targetDirPrefix + dependency.version;
   if (dependency.hasOwnProperty('cssFiles')) {
     dependency.cssFiles.forEach(function(cssFiles) {
       cssFilesPath.push(path.join(
-        'third_party', 'static', dependency.targetDirPrefix +
-        dependency.version, cssFiles));
-      });
+        'third_party', 'static', dependencyDir, cssFiles));
+    });
   }
   if (dependency.hasOwnProperty('fontsPath')) {
     var fontPrefix = '*.{eot,woff2,ttf,woff,eof,svg}';
-    fontFolderPath.push(path.join('third_party', 'static',
-      dependency.targetDirPrefix + dependency.version,
+    fontFolderPath.push(path.join('third_party', 'static', dependencyDir,
       dependency.fontsPath, fontPrefix));
   }
   if (dependency.hasOwnProperty('cssBackgroundImage')) {
     dependency.cssBackgroundImage.forEach(function(imagePath) {
       cssBackgroundPath.push(path.join(
-        'third_party', 'static', dependency.targetDirPrefix +
-        dependency.version, imagePath));
+        'third_party', 'static', dependencyDir, imagePath));
     });
   }
 }
@@ -85,23 +84,26 @@ gulp.task('css', function() {
     .pipe(gulp.dest(generatedCssTargetDir));
 });
 
+// This task is used to copy all fonts which are used by
+// Bootstrap and font-Awesome to one folder
 gulp.task('copyFonts', function() {
   gulp.src(fontFolderPath)
-  .pipe(gulp.dest(path.join(
-    'third_party', 'generated',
-    isMinificationNeeded ? 'prod' : 'dev', 'fonts')));
+    .pipe(gulp.dest(path.join(
+      'third_party', 'generated',
+      isMinificationNeeded ? 'prod' : 'dev', 'fonts')));
 });
 
+// This is a task which copies backgroun image used by select2.css
 // TODO(Barnabas) find a way of removing this task.
-// It is a bit of a hacky method and does not seem to scale with a program
+// It is a bit of a hacky method and does not seems to scale with a program
 gulp.task('copyCssBackgroundImages', function() {
   gulp.src(cssBackgroundPath)
-  .pipe(gulp.dest(generatedCssTargetDir));
+    .pipe(gulp.dest(generatedCssTargetDir));
 });
 
 gulp.task('gulpStartGae', function() {
   gulp.src('app.yaml')
-    .pipe(gulpStartGae(gae_devserver_path, [], params));
+    .pipe(gulpStartGae(gaeDevserverPath, [], params));
 });
 
 // This takes all functions  that are required for the build
