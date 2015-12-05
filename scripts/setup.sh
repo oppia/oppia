2e87f921c2664021b827ebed45a38eb2b9dfc4c5
Madiyar Aitbayev <madiyar92@gmail.com>
2015-12-05 01:55:17 +0000

    Fix bug in script/setup.sh
    When trying to run scripts/start.sh got an error in script/setup.sh,
    "No such file or directory"

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -152,7 +152,11 @@ export PYTHON_CMD
 
 # List all node modules that are currently installed. The "npm list" command is
 # slow, so we precompute this here and refer to it as needed.
-NPM_INSTALLED_MODULES="$($NPM_CMD list)"
+NPM_INSTALLED_MODULES=""
+if [ -d "$NODE_PATH" ]; then
+  NPM_INSTALLED_MODULES="$($NPM_CMD list)"
+fi
+
 export NPM_INSTALLED_MODULES
 
 install_node_module() {
