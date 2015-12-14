0505eb2fa32303ad3bb79282cf309bbc04f22d13
Shouvik Roy <shouvik@techie.com>
2015-12-15 02:53:51 +0530

    Address review comments

--- core/tests/protractor/explorationRating.js
+++ core/tests/protractor/explorationRating.js
@@ -30,29 +30,32 @@ describe('Gallery view', function() {
   var CATEGORY_BUSINESS = 'Business';
   var LANGUAGE_ENGLISH = 'English';
   var MINIMUM_ACCEPTABLE_NUMBER_OF_RATINGS = 1;
+  var addRating = function(userEmail, userName, explorationName, ratingValue) {
+      users.createUser(userEmail, userName);
+      users.login(userEmail);
+      browser.get(general.GALLERY_URL_SUFFIX);
+      gallery.playExploration(EXPLORATION_RATINGTEST);
+      player.expectExplorationNameToBe(explorationName);
+      player.submitAnswer('Continue');
+      player.rateExploration(ratingValue);
 
+      users.logout();
+    };
+    
   it('should display ratings on exploration when minimum ratings have been submitted', function() {
     users.createUser('rating@example.com', 'Rating');
     // Create an test exploration
     users.login('rating@example.com');
     workflow.createAndPublishExploration(
       EXPLORATION_RATINGTEST, CATEGORY_BUSINESS,
-      'hold the light of the two trees', LANGUAGE_ENGLISH);
+      'an objective', LANGUAGE_ENGLISH);
     users.logout();
 
     // Create test users, play exploration and review them after completion
     for (var i = 0; i < MINIMUM_ACCEPTABLE_NUMBER_OF_RATINGS - 1; i++) {
       var userEmail = 'NoDisplay' + i + '@example.com';
       var username = 'NoDisplay' + i;
-      users.createUser(userEmail, username);
-      users.login(userEmail);
-      browser.get(general.GALLERY_URL_SUFFIX);
-      gallery.playExploration(EXPLORATION_RATINGTEST);
-      player.expectExplorationNameToBe('RatingTest');
-      player.submitAnswer('Continue');
-      player.rateExploration(4);
-
-      users.logout();
+      addRating(userEmail, username, EXPLORATION_RATINGTEST, 4);
     }
 
     browser.get(general.GALLERY_URL_SUFFIX);
@@ -60,21 +63,14 @@ describe('Gallery view', function() {
 
     var userEmail = 'Display@example.com';
     var username = 'Display';
-    users.createUser(userEmail, username);
-    users.login(userEmail);
-    browser.get(general.GALLERY_URL_SUFFIX);
-    gallery.playExploration(EXPLORATION_RATINGTEST);
-    player.expectExplorationNameToBe('RatingTest');
-    player.submitAnswer('Continue');
-    player.rateExploration(4);
-
-    users.logout();
+    addRating(userEmail, username, EXPLORATION_RATINGTEST, 4);
 
     browser.get(general.GALLERY_URL_SUFFIX);
     gallery.expectExplorationRatingToEqual(EXPLORATION_RATINGTEST, '4.0');
 
     gallery.playExploration(EXPLORATION_RATINGTEST);
     player.expectExplorationRatingOnInformationCardToEqual('4');
+
   });
   afterEach(function() {
     general.checkForConsoleErrors([]);
