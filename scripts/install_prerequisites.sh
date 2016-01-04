6f61bcfe70eb9691d184a4d0b25d439a8e185af8
philoniare <philoniare@gmail.com>
2016-01-04 09:33:15 -0500

    fix windows installation #1209

---
+++ scripts/install_prerequisites.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash 
+
+
+sudo apt-get update
+ 
+sudo apt-get install git 
+
+sudo apt-get install unzip
+ sudo apt-get install default-jre
+
+sudo apt-get install curl python-setuptools python-dev python-pip 
+sudo pip install --upgrade pip
