2cb647f9549cbb420b1bb4b9314cc6eed5ed2473
Reto Brunner <brunnre8@gmail.com>
2016-01-07 23:29:49 +0100

    addressed review issues

--- scripts/pre_push_hook.py
+++ scripts/pre_push_hook.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python2.7
-"""Pre-push hook that executes the Python/JS linters on all files that
+# coding: utf-8
+#
+# Copyright 2014 The Oppia Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS-IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+'''Pre-push hook that executes the Python/JS linters on all files that
 deviate from develop.
 (By providing the list of files to `scripts/pre_commit_linter.py`)
-To install the hook manually simply execute this script with the `--install`
-flag.
-"""
+To install the hook manually simply execute this script from the oppia root dir
+with the `--install` flag.
+To bypass the validation upon `git push` use the following command:
+`git push REMOTE BRANCH --no-verify`
+'''
 
 import os
 import sys
@@ -14,42 +32,35 @@ import pprint
 import argparse
 import shutil
 
-_PARSER = argparse.ArgumentParser()
-_PARSER.add_argument("remote", nargs="?", help="provided by git before push")
-_PARSER.add_argument("url", nargs="?", help="provided by git before push")
-_PARSER.add_argument("--install", action="store_true", default=False,
-                     help="Install pre_push_hook to the .git/hooks directory")
-_ARGS = _PARSER.parse_args()
-REMOTE = _ARGS.remote
 
-GitRef = collections.namedtuple("GitRef", ["local_ref", "local_sha1",
-                                           "remote_ref", "remote_sha1"])
+GitRef = collections.namedtuple('GitRef', ['local_ref', 'local_sha1',
+                                           'remote_ref', 'remote_sha1'])
 
-# git hash of /dev/null, refers to an "empty" commit
-GIT_NULL_COMMIT = "4b825dc642cb6eb9a060e54bf8d69288fbee4904"
+# git hash of /dev/null, refers to an 'empty' commit
+GIT_NULL_COMMIT = '4b825dc642cb6eb9a060e54bf8d69288fbee4904'
 
 
 # caution, __file__ is here *OPPiA/.git/hooks* and not in *OPPIA/scripts*
 FILE_DIR = os.path.abspath(os.path.dirname(__file__))
 OPPIA_DIR = os.path.join(FILE_DIR, os.pardir, os.pardir)
-SCRIPTS_DIR = os.path.join(OPPIA_DIR, "scripts")
-LINTER_SCRIPT = "pre_commit_linter.py"
-LINTER_FILE_FLAG = "--files"
-PYTHON_CMD = "python"
-BACKEND_TEST_SCRIPT = "run_backend_tests.sh"
-FRONTEND_TEST_SCRIPT = "run_frontend_tests.sh"
+SCRIPTS_DIR = os.path.join(OPPIA_DIR, 'scripts')
+LINTER_SCRIPT = 'pre_commit_linter.py'
+LINTER_FILE_FLAG = '--files'
+PYTHON_CMD = 'python'
+BACKEND_TEST_SCRIPT = 'run_backend_tests.sh'
+FRONTEND_TEST_SCRIPT = 'run_frontend_tests.sh'
 
 
 def _start_subprocess_for_result(cmd):
-    """Starts subprocess and returns (stdout, stderr)"""
+    '''Starts subprocess and returns (stdout, stderr)'''
     task = subprocess.Popen(cmd, stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE)
     out, err = task.communicate()
     return out, err
 
 
-def _git_diff_names_only(left, right, diff_filter=""):
-    """Compare two branches/commits etc with git.
+def _git_diff_names_only(left, right, diff_filter=''):
+    '''Compare two branches/commits etc with git.
     Parameter:
         left: the lefthand comperator
         right: the righthand comperator
@@ -58,10 +69,10 @@ def _git_diff_names_only(left, right, diff_filter=""):
         List of file names
     Raises:
         ValueError if git command fails
-    """
-    git_cmd = ["git", "diff", "--name-only"]
+    '''
+    git_cmd = ['git', 'diff', '--name-only']
     if diff_filter:
-        git_cmd.append("--diff-filter={}".format(diff_filter))
+        git_cmd.append('--diff-filter={}'.format(diff_filter))
     git_cmd.extend([left, right])
     out, err = _start_subprocess_for_result(git_cmd)
     if not err:
@@ -71,7 +82,7 @@ def _git_diff_names_only(left, right, diff_filter=""):
 
 
 def _compare_to_remote(remote, local_branch, remote_branch=None):
-    """Compare local with remote branch with git diff.
+    '''Compare local with remote branch with git diff.
     Parameter:
         remote: Git remote being pushed to
         local_branch: Git branch being pushed to
@@ -82,40 +93,45 @@ def _compare_to_remote(remote, local_branch, remote_branch=None):
         but not deleted
     Raises:
         ValueError if git command fails
-    """
+    '''
     remote_branch = remote_branch if remote_branch else local_branch
-    git_remote = "{}/{}".format(remote, remote_branch)
+    git_remote = '%s/%s' % (remote, remote_branch)
     return _git_diff_names_only(git_remote, local_branch,
-                                diff_filter="ACMRT")
+                                diff_filter='ACMRT')
 
 
-def _collect_files_being_pushed(ref_list):
-    """Get all files that are modified and are being pushed"""
+def _collect_files_being_pushed(ref_list, remote):
+    '''Get all files that are modified and are being pushed'''
     # get branch name from e.g. local_ref='refs/heads/lint_hook'
-    branches = [ref.local_ref.split("/")[-1] for ref in ref_list]
+    branches = [ref.local_ref.split('/')[-1] for ref in ref_list]
     hashes = [ref.local_sha1 for ref in ref_list]
     remote_hashes = [ref.remote_sha1 for ref in ref_list]
     changed_files = set()
     for branch, sha1, remote_sha1 in zip(branches, hashes, remote_hashes):
-        # if remote doesn't have the branch yet, git reports the following
-        #  remote_sha1: '0000000000000000000000000000000000000000'
-        if set(remote_sha1) != {"0"}:
+        # git reports the following for an empty / non existing branch
+        # sha1: '0000000000000000000000000000000000000000'
+        if set(sha1) == {'0'}:
+            # We are deleting a branch, nothing to do
+            continue
+        elif set(remote_sha1) != {'0'}:
             try:
-                files = _compare_to_remote(REMOTE, branch)
+                files = _compare_to_remote(remote, branch)
             except ValueError as e:
-                sys.exit(e.message)
+                print e.message
+                sys.exit(1)
         else:
             # Get the difference to origin/develop instead
             try:
-                files = _compare_to_remote(REMOTE, branch,
-                                           remote_branch="develop")
+                files = _compare_to_remote(remote, branch,
+                                           remote_branch='develop')
             except ValueError:
                 # give up, return all files in repo
                 try:
                     files = _git_diff_names_only(GIT_NULL_COMMIT, sha1,
-                                                 diff_filter="ACMRT")
+                                                 diff_filter='ACMRT')
                 except ValueError as e:
-                    sys.exit(e.message)
+                    print e.message
+                    sys.exit(1)
 
         changed_files.update(files)
 
@@ -125,7 +141,7 @@ def _collect_files_being_pushed(ref_list):
 def _get_refs():
     # Git provides refs in STDIN
     ref_list = [GitRef(*ref_str.split()) for ref_str in sys.stdin]
-    print "ref_list:"
+    print 'ref_list:'
     pprint.pprint(ref_list)
     return ref_list
 
@@ -138,7 +154,7 @@ def _start_linter(files):
 
 
 def _start_sh_script(scriptname):
-    cmd = ["bash", os.path.join(SCRIPTS_DIR, scriptname)]
+    cmd = ['bash', os.path.join(SCRIPTS_DIR, scriptname)]
     task = subprocess.Popen(cmd)
     task.communicate()
     return task.returncode
@@ -147,38 +163,49 @@ def _start_sh_script(scriptname):
 def _install_hook():
     # install script ensures that oppia is root
     oppia_dir = os.getcwd()
-    hooks_dir = os.path.join(oppia_dir, ".git", "hooks")
-    pre_push_file = os.path.join(hooks_dir, "pre-push")
+    hooks_dir = os.path.join(oppia_dir, '.git', 'hooks')
+    pre_push_file = os.path.join(hooks_dir, 'pre-push')
     if os.path.islink(pre_push_file):
-        print "Symlink already exists"
+        print 'Symlink already exists'
         return
     try:
         os.symlink(os.path.abspath(__file__), pre_push_file)
-        print "Created symlink in .git/hooks directory"
+        print 'Created symlink in .git/hooks directory'
     # raises AttributeError on windows, OSError added as failsafe
     except (OSError, AttributeError):
         shutil.copy(__file__, pre_push_file)
-        print "Copied file to .git/hooks directory"
+        print 'Copied file to .git/hooks directory'
 
 
 def main():
-    if _ARGS.install:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('remote', nargs='?', help='provided by git before push')
+    parser.add_argument('url', nargs='?', help='provided by git before push')
+    parser.add_argument('--install', action='store_true', default=False,
+                        help='Install pre_push_hook to the .git/hooks directory')
+    args = parser.parse_args()
+    remote = args.remote
+    if args.install:
         _install_hook()
         sys.exit(0)
     refs = _get_refs()
-    modified_files = _collect_files_being_pushed(refs)
-    print "Files being pushed:"
-    pprint.pprint(modified_files)
-    lint_status = _start_linter(modified_files)
-    if lint_status != 0:
-        sys.exit("Push failed, please correct the linting issues above")
+    modified_files = _collect_files_being_pushed(refs, remote)
+    if modified_files:
+        print 'Files being pushed:'
+        pprint.pprint(modified_files)
+        lint_status = _start_linter(modified_files)
+        if lint_status != 0:
+            print 'Push failed, please correct the linting issues above'
+            sys.exit(1)
     frontend_status = _start_sh_script(FRONTEND_TEST_SCRIPT)
     if frontend_status != 0:
-        sys.exit("Push aborted due to failing frontend tests.")
+        print 'Push aborted due to failing frontend tests.'
+        sys.exit(1)
     backend_status = _start_sh_script(BACKEND_TEST_SCRIPT)
     if backend_status != 0:
-        sys.exit("Push aborted due to failing backend tests.")
+        print 'Push aborted due to failing backend tests.'
+        sys.exit(1)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
