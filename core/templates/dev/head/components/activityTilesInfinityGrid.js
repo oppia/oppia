b36974bdf3fd9fa47803018caebbce788d5d6de2
Giritheja <s.giritheja@gmail.com>
2016-03-13 23:40:50 +0530

    Indentation changes

--- core/templates/dev/head/components/activityTilesInfinityGrid.js
+++ core/templates/dev/head/components/activityTilesInfinityGrid.js
@@ -4,20 +4,20 @@ oppia.directive('activityTilesInfinityGrid', [function() {
     templateUrl: 'components/activityTilesInfinityGrid',
     controller: [
       '$scope', '$rootScope', '$modal', '$window',
-      function(
-          $scope, $rootScope, $modal, $window, searchService) {
-            $scope.showMoreExplorations = function() {
-              if (!$rootScope.loadingMessage) {
-                $scope.pageLoaderIsBusy = true;
-
-                searchService.loadMoreData(function(data, hasPageFinishedLoading) {
-                  $scope.allExplorationsInOrder = $scope.allExplorationsInOrder.concat(
-                    data.explorations_list);
-                  $scope.finishedLoadingPage = hasPageFinishedLoading;
-                  $scope.pageLoaderIsBusy = false;
-                });
-              }
-            };
+      function($scope, $rootScope, $modal, $window, searchService) {
+        $scope.showMoreExplorations = function() {
+          if (!$rootScope.loadingMessage) {
+            $scope.pageLoaderIsBusy = true;
+            searchService.loadMoreData(function(data,
+              hasPageFinishedLoading) {
+              $scope.allExplorationsInOrder =
+              $scope.allExplorationsInOrder.concat(
+                data.explorations_list);
+              $scope.finishedLoadingPage = hasPageFinishedLoading;
+              $scope.pageLoaderIsBusy = false;
+            });
+          }
+        };
       }
     ]
   };
