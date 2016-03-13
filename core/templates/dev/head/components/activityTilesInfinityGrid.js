2d361437bcb4c115c3de0fd999acbd49dc5f5ca4
Giritheja <s.giritheja@gmail.com>
2016-03-13 11:05:59 +0530

    Created activity-tiles-infinity-grid directive

---
+++ core/templates/dev/head/components/activityTilesInfinityGrid.js
@@ -0,0 +1,24 @@
+oppia.directive('activityTilesInfinityGrid', [function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'components/activityTilesInfinityGrid',
+    controller: [
+      '$scope', '$rootScope', '$modal', '$window',
+      function(
+          $scope, $rootScope, $modal, $window, searchService) {
+            $scope.showMoreExplorations = function() {
+              if (!$rootScope.loadingMessage) {
+                $scope.pageLoaderIsBusy = true;
+
+                searchService.loadMoreData(function(data, hasPageFinishedLoading) {
+                  $scope.allExplorationsInOrder = $scope.allExplorationsInOrder.concat(
+                    data.explorations_list);
+                  $scope.finishedLoadingPage = hasPageFinishedLoading;
+                  $scope.pageLoaderIsBusy = false;
+                });
+              }
+            };
+      }
+    ]
+  };
+}]);
