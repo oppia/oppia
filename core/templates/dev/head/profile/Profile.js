6e8ddd0df1e3f2b3640cd79ccbc9edfd11b12205
kerryxwang <kerryxwang@gmail.com>
2016-03-23 08:23:14 -0700

    New pull request with updated code for profile redesign from previous pull request

--- core/templates/dev/head/profile/Profile.js
+++ core/templates/dev/head/profile/Profile.js
@@ -14,11 +14,13 @@
 
 /**
  * @fileoverview Data and controllers for the Oppia profile page.
+ *
+ * @author sean@seanlip.org (Sean Lip)
  */
 
 oppia.controller('Profile', [
-  '$scope', '$http', '$rootScope', 'oppiaDatetimeFormatter',
-  function($scope, $http, $rootScope, oppiaDatetimeFormatter) {
+  '$scope', '$filter', '$http', '$rootScope', 'oppiaDatetimeFormatter',
+  function($scope, $filter, $http, $rootScope, oppiaDatetimeFormatter) {
     var profileDataUrl = '/profilehandler/data/' + GLOBALS.PROFILE_USERNAME;
     var DEFAULT_PROFILE_PICTURE_URL = '/images/general/no_profile_picture.png';
 
@@ -29,22 +31,94 @@ oppia.controller('Profile', [
     $rootScope.loadingMessage = 'Loading';
     $http.get(profileDataUrl).success(function(data) {
       $rootScope.loadingMessage = '';
+      $scope.username = {
+        title: 'Username',
+        value: data.username,
+        helpText: (data.username)
+      };
       $scope.userBio = data.user_bio;
       $scope.userDisplayedStatistics = [{
-        title: 'User Impact Score',
+        title: 'Impact',
         value: data.user_impact_score,
         helpText: (
           'A rough measure of the impact of explorations created by this ' +
           'user. Better ratings and more playthroughs improve this score.')
       }, {
-        title: 'Created Explorations',
+        title: 'Created',
         value: data.created_exp_summary_dicts.length
       }, {
-        title: 'Edited Explorations',
+        title: 'Edited',
         value: data.edited_exp_summary_dicts.length
       }];
-      $scope.userCreatedExplorations = data.created_exp_summary_dicts;
-      $scope.userEditedExplorations = data.edited_exp_summary_dicts;
+
+      $scope.userEditedExplorations = data.edited_exp_summary_dicts.sort(
+          function(exploration1, exploration2) {
+        if (exploration1.ratings > exploration2.ratings) {
+          return 1;
+        } else if (exploration1.ratings === exploration2.ratings) {
+          if (exploration1.playthroughs > exploration2.playthroughs) {
+            return 1;
+          } else if (exploration1.playthroughs > exploration2.playthroughs) {
+              return 0;
+          } else {
+            return -1;
+          }
+        } else {
+          return -1;
+        }
+      });
+
+      $scope.currentPageNumber = 0;
+      $scope.pageSize = 6;
+      $scope.startingExplorationNumber = 1;
+      $scope.endingExplorationNumber = 6;
+      $scope.updateDisplayedExplorationNumbers = function() {
+        var startingNumber = $scope.currentPageNumber * $scope.pageSize + 1;
+        $scope.startingExplorationNumber = startingNumber;
+        if ($scope.userEditedExplorations.length > (
+            $scope.currentPageNumber + 1) * $scope.pageSize) {
+          var endingNumber = $scope.currentPageNumber * $scope.pageSize + 6;
+          $scope.endingExplorationNumber = endingNumber;
+        } else {
+          var endingNumber = $scope.userEditedExplorations.length;
+          $scope.endingExplorationNumber = endingNumber;
+        }
+      };
+
+      $scope.goToPreviousPage = function() {
+        if ($scope.currentPageNumber === 0) {
+          console.log("Error: cannot decrement page");
+        } else {
+          $scope.currentPageNumber--;
+          $scope.updateDisplayedExplorationNumbers();
+        }
+      };
+      $scope.goToNextPage = function() {
+        if ($scope.currentPageNumber * $scope.pageSize >= data.edited_exp_summary_dicts.length) {
+          console.log("Error: Cannot increment page")
+        } else {
+          $scope.currentPageNumber++;
+          $scope.updateDisplayedExplorationNumbers();
+        }
+      };
+
+      $scope.getExplorationsToDisplay = function() {
+        $scope.explorationsOnPage = [];
+        $scope.explorationIndexStart = $scope.currentPageNumber * $scope.pageSize;
+        $scope.explorationIndexEnd = (
+          $scope.explorationIndexStart + $scope.pageSize - 1);
+        for (var ind = $scope.explorationIndexStart;
+            ind <= $scope.explorationIndexEnd; ind++) {
+          $scope.explorationsOnPage.push($scope.userEditedExplorations[ind]);
+          if (ind === $scope.userEditedExplorations.length - 1) {
+            break;
+          }
+        }
+        return $scope.explorationsOnPage;
+      };
+
+      $scope.numUserPortfolioExplorations = (
+        data.edited_exp_summary_dicts.length);
       $scope.subjectInterests = data.subject_interests;
       $scope.firstContributionMsec = data.first_contribution_msec;
       $scope.profilePictureDataUrl = (
