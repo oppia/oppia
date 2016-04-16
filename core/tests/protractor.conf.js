be9a0dd016742c81c0ee9331acdcf4052c70919b
Barnabas Makonda <barnabasmakonda@gmail.com>
2016-04-16 03:36:10 +0300

    Fix #995: add Protractor e2e tests to Travis-CI

--- core/tests/protractor.conf.js
+++ core/tests/protractor.conf.js
@@ -78,7 +78,7 @@ exports.config = {
   //
   // A base URL for your application under test. Calls to protractor.get()
   // with relative paths will be prepended with this.
-  baseUrl: 'http://localhost:4445',
+  baseUrl: 'http://localhost:9001',
 
   // Selector for the element housing the angular app - this defaults to
   // body, but is necessary if ng-app is on a descendant of <body>
