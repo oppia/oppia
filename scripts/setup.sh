8a781cdef25837b6de3c6851bb394aef35d7e87d
Ashish <ashishpahwa7@gmail.com>
2016-05-04 15:59:53 +0530

    added google- chrome after line 203

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -204,6 +204,11 @@ elif [ "$VAGRANT" = true ]; then
 elif [ -f "/usr/bin/chromium-browser" ]; then
   # Unix.
   export CHROME_BIN="/usr/bin/chromium-browser"
+
+elif [ -f "/usr/bin/chromium-browser" ]; then
+  # Unix.
+  export CHROME_BIN="/usr/bin/google-chrome"
+
 elif [ -f "/c/Program Files (x86)/Google/Chrome/Application/chrome.exe" ]; then
   # Windows.
   export CHROME_BIN="/c/Program Files (x86)/Google/Chrome/Application/chrome.exe"
