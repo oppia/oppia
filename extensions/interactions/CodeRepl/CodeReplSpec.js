99f7c93f38b15c6106f09070a713e0baf0cc8b48
Ashish <ashishpahwa7@gmail.com>
2016-05-04 17:46:21 +0530

    Replace runCode with runAndSubmitCode

--- extensions/interactions/CodeRepl/CodeReplSpec.js
+++ extensions/interactions/CodeRepl/CodeReplSpec.js
@@ -50,7 +50,7 @@ describe('CodeRepl interaction', function() {
 
     it('loads the code template', function() {
       expect(elt.html()).toContain('code-repl-input-box');
-      expect(elt.html()).toContain('runAndSubmitCode(code, "")');
+      expect(elt.html()).toContain('runAndSubmitCode(code,SUBMIT)');
     });
   });
 });
