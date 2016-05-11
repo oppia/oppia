11bea2dc7c1cfd97a7653a23738587433878c84b
Sourav Badami <souravbadami@gmail.com>
2016-05-12 04:52:38 +0530

    Fixed linting issues.

--- scripts/run_pattern_check.py
+++ scripts/run_pattern_check.py
@@ -17,11 +17,7 @@
 # Pre-submission script for Oppia.
 # This script checks for unaccepted text patterns in the commit.
 
-import fnmatch
-import os
 import subprocess
-
-_PARENT_DIR = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
 
 
 def _get_changed_filenames():
@@ -38,37 +34,40 @@ def _get_changed_filenames():
     return unstaged_files + staged_files
 
 
-def check_for_bad_patterns(dir):
+def check_for_bad_patterns():
     total_files_checked = 0
     total_error_count = 0
     files = _get_changed_filenames()
     if len(files) != 0:
-        for file in files:
-            with open(file) as f:
+        for filename in files:
+            with open(filename) as f:
                 content = f.read()
                 total_files_checked += 1
                 if "__author__" in content:
-                	print file + " --> " + "Please remove author tags from this file."
-                	total_error_count += 1
+                    print filename + " --> " + "Please remove author tags from\
+                    this file."
+                    total_error_count += 1
                 if "datetime.datetime.now()" in content:
-                	print file + " --> " + "Please use datetime.datetime.utcnow() instead of datetime.datetime.now()."
-                	total_error_count += 1
+                    print filename + " --> " + "Please use datetime.datetime.u\
+                    tcnow() instead of datetime.datetime.now()."
+                    total_error_count += 1
 
     return total_files_checked, total_error_count
 
 
 def main():
-    total_files_checked, total_error_count = check_for_bad_patterns(_PARENT_DIR)
+    total_files_checked, total_error_count = check_for_bad_patterns()
     print ''
     print '+------------------+'
     print '| SUMMARY OF TESTS |'
     print '+------------------+'
     print ''
     if total_files_checked == 0:
-        print ('WARNING: No files were checked.')
+        print "WARNING: No files were checked."
     else:
         if total_files_checked and total_error_count == 0:
-            print '(%s FILES CHECKED, %s ERRORS FOUND)' % (total_files_checked, total_error_count)
+            print '(%s FILES CHECKED, %s ERRORS FOUND)' % (total_files_checked\
+            , total_error_count)
         else:
             exit(1)
 
