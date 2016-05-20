89f1331766224e536846cae73deabfcae2cafed8
Rajendra kadam <raju249@users.noreply.github.com>
2016-05-20 11:04:44 +0530

    Fix #1848

--- scripts/install_third_party.py
+++ scripts/install_third_party.py
@@ -27,7 +27,8 @@ import zipfile
 
 import common
 
-# These two lines prevent a "IOError: [Errno socket error] [Errno -2] Name or service not known" error
+#These two lines prevent a "IOError: [Errno socket error] 
+#[Errno -2] Name or service not known" error
 # in urllib.urlretrieve, if the user is behind a proxy.
 if 'VAGRANT' in os.environ:
     os.environ['http_proxy'] = ''
