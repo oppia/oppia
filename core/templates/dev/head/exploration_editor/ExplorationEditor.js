74742a1f9fb54d4c5ded6ee1bde2219d1699e0f6
Sean Lip <sean@seanlip.org>
2016-06-05 22:20:23 -0700

    Discard draft changes before reverting. Fix typo.

--- core/templates/dev/head/exploration_editor/ExplorationEditor.js
+++ core/templates/dev/head/exploration_editor/ExplorationEditor.js
@@ -152,7 +152,7 @@ oppia.controller('ExplorationEditor', [
 
         if (data.is_version_of_draft_valid === false &&
             data.draft_changes !== null &&
-            data.draft.changes.length > 0) {
+            data.draft_changes.length > 0) {
           // Show modal displaying lost changes if the version of draft
           // changes is invalid, and draft_changes is not `null`.
           autosaveInfoModalsService.showVersionMismatchModal(
