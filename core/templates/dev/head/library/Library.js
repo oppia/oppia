3f3d441997629221f806eb1f88b995b89cf2d81a
Mark Halpin <markhalpin3@gmail.com>
2016-06-13 05:00:41 -0400

    Remove js from library.js and library.html in favor of var.

--- core/templates/dev/head/library/Library.js
+++ core/templates/dev/head/library/Library.js
@@ -23,6 +23,9 @@ oppia.controller('Library', [
       $scope, $http, $rootScope, $window, $timeout, i18nIdService,
       urlService, CATEGORY_LIST, searchService, windowDimensionsService) {
     $rootScope.loadingMessage = 'I18N_LIBRARY_LOADING';
+      var possibleBanners = ['banner1.svg', 'banner2.svg', 'banner3.svg', 'banner4.svg'];
+  $scope.bannerImageFilename = possibleBanners[Math.floor(Math.random() *
+    possibleBanners.length)]
 
     // Below is the width of each tile (width + margins), which can be found
     // in core/templates/dev/head/components/
