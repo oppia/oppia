61f95c91d7eec311deb3c6ef9a76cedf5e03182b
Mark Halpin <markhalpin3@gmail.com>
2016-06-13 05:30:33 -0400

    Fix lint indenting.

--- core/templates/dev/head/library/Library.js
+++ core/templates/dev/head/library/Library.js
@@ -23,9 +23,9 @@ oppia.controller('Library', [
       $scope, $http, $rootScope, $window, $timeout, i18nIdService,
       urlService, CATEGORY_LIST, searchService, windowDimensionsService) {
     $rootScope.loadingMessage = 'I18N_LIBRARY_LOADING';
-      var possibleBannerFilenames = [
+    var possibleBannerFilenames = [
         'banner1.svg', 'banner2.svg', 'banner3.svg', 'banner4.svg'];
-      $scope.bannerImageFilename = possibleBannerFilenames[
+    $scope.bannerImageFilename = possibleBannerFilenames[
          Math.floor(Math.random() * possibleBannerFilenames.length)];
 
     // Below is the width of each tile (width + margins), which can be found
