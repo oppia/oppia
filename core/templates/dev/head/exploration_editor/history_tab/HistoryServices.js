6a969180ed94503d2d7c76b717027c3f715970d5
Sean Lip <sean@seanlip.org>
2016-06-19 17:14:04 -0700

    Pull exploration diff computation into a separate service.

--- core/templates/dev/head/exploration_editor/history_tab/HistoryServices.js
+++ core/templates/dev/head/exploration_editor/history_tab/HistoryServices.js
@@ -126,98 +126,11 @@ oppia.factory('versionsTreeService', [function() {
 }]);
 
 oppia.factory('compareVersionsService', [
-  '$http', '$q', 'versionsTreeService', 'explorationData', 'INTERACTION_SPECS',
-  function($http, $q, versionsTreeService, explorationData, INTERACTION_SPECS) {
-    var STATE_PROPERTY_ADDED = 'added';
-    var STATE_PROPERTY_DELETED = 'deleted';
-    var STATE_PROPERTY_CHANGED = 'changed';
-    var STATE_PROPERTY_UNCHANGED = 'unchanged';
-    var _maxId = 0;
-
-    // Functions to assign ids to states
-    var _resetMaxId = function() {
-      _maxId = 0;
-    };
-    var _generateNewId = function() {
-      _maxId++;
-      return _maxId;
-    };
-
-    /**
-     * A helper function that takes a list of changes and uses them to modify
-     * stateIds and stateData accordingly.
-     *
-     * stateIds is an object whose keys are the newest state name and whose
-     * values are the assigned state id.
-     * stateData an object whose keys are state IDs and whose value is an object
-     * with these keys:
-     *  - 'newestStateName': the latest name of the state
-     *  - 'originalStateName': the first encountered name for the state
-     *  - 'stateProperty': 'changed', 'unchanged', 'added' or 'deleted'
-     * changeList is the change list to apply.
-     * directionForwards is true if changes are compared in increasing version
-     * number, and false if changes are compared in decreasing version number.
-     */
-    var _trackChanges = function(
-        stateIds, stateData, changeList, directionForwards) {
-      changeList.forEach(function(change) {
-        if ((directionForwards && change.cmd === 'add_state') ||
-            (!directionForwards && change.cmd === 'delete_state')) {
-          if (!stateIds.hasOwnProperty(change.state_name)) {
-            var newId = _generateNewId();
-            stateIds[change.state_name] = newId;
-          }
-          var currentStateId = stateIds[change.state_name];
-          if (stateData.hasOwnProperty(currentStateId) &&
-              stateData[currentStateId].stateProperty ==
-                STATE_PROPERTY_DELETED) {
-            stateData[currentStateId].stateProperty = STATE_PROPERTY_CHANGED;
-            stateData[currentStateId].newestStateName = change.state_name;
-          } else {
-            stateData[currentStateId] = {
-              newestStateName: change.state_name,
-              originalStateName: change.state_name,
-              stateProperty: STATE_PROPERTY_ADDED
-            };
-          }
-        } else if ((directionForwards && change.cmd === 'delete_state') ||
-            (!directionForwards && change.cmd === 'add_state')) {
-          if (stateData[stateIds[change.state_name]].stateProperty ==
-              STATE_PROPERTY_ADDED) {
-            stateData[stateIds[change.state_name]].stateProperty = (
-              STATE_PROPERTY_CHANGED);
-          } else {
-            stateData[stateIds[change.state_name]].stateProperty = (
-              STATE_PROPERTY_DELETED);
-          }
-        } else if (change.cmd === 'rename_state') {
-          var newStateName = null;
-          var oldStateName = null;
-          if (directionForwards) {
-            newStateName = change.new_state_name;
-            oldStateName = change.old_state_name;
-          } else {
-            newStateName = change.old_state_name;
-            oldStateName = change.new_state_name;
-          }
-          stateIds[newStateName] = stateIds[oldStateName];
-          delete stateIds[oldStateName];
-          stateData[stateIds[newStateName]].newestStateName = newStateName;
-        } else if (change.cmd === 'edit_state_property') {
-          if (stateData[stateIds[change.state_name]].stateProperty ==
-              STATE_PROPERTY_UNCHANGED) {
-            stateData[stateIds[change.state_name]].stateProperty = (
-              STATE_PROPERTY_CHANGED);
-          }
-        } else if (
-            change.cmd !== 'migrate_states_schema_to_latest_version' &&
-            change.cmd !== 'AUTO_revert_version_number' &&
-            change.cmd !== 'edit_exploration_property') {
-          throw new Error('Invalid change command: ' + change.cmd);
-        }
-      });
-    };
-
+  '$http', '$q', 'versionsTreeService', 'explorationData',
+  'ExplorationDiffService',
+  function(
+      $http, $q, versionsTreeService, explorationData,
+      ExplorationDiffService) {
     /**
      * Constructs the combined list of changes needed to get from v1 to v2.
      *
@@ -251,184 +164,7 @@ oppia.factory('compareVersionsService', [
       return combinedChangeList;
     };
 
-    /**
-     * Returns an adjacency matrix, adjMatrix, of links between states
-     * (represented by ids). adjMatrix[state1Id][state2Id] is true if there
-     * is a link from state 1 to state 2 and false otherwise.
-     *
-     * Args:
-     * - states: an object whose keys are state names and values are objects
-     *     representing the state.
-     * - stateIds: an object whose keys are state names and values are state
-     *     ids.
-     * - maxId: the maximum id in states and stateIds.
-     */
-    var _getAdjMatrix = function(states, stateIds, maxId) {
-      adjMatrix = {};
-      for (var stateId = 1; stateId <= maxId; stateId++) {
-        adjMatrix[stateId] = {};
-      }
-      for (var state1Id = 1; state1Id <= maxId; state1Id++) {
-        for (var state2Id = 1; state2Id <= maxId; state2Id++) {
-          adjMatrix[state1Id][state2Id] = false;
-        }
-      }
-      for (var stateName in states) {
-        var interaction = states[stateName].interaction;
-        var groups = interaction.answer_groups;
-        for (var h = 0; h < groups.length; h++) {
-          var dest = groups[h].outcome.dest;
-          adjMatrix[stateIds[stateName]][stateIds[dest]] = true;
-        }
-        if (interaction.default_outcome) {
-          var defaultDest = interaction.default_outcome.dest;
-          adjMatrix[stateIds[stateName]][stateIds[defaultDest]] = true;
-        }
-      }
-      return adjMatrix;
-    };
-
-    /**
-     * Returns a list of objects representing links in the diff graph.
-     * Each object represents one link, and has keys:
-     *  - 'source': source state of link
-     *  - 'target': target state of link
-     *  - 'linkProperty': 'added', 'deleted' or 'unchanged'
-     */
-    var _compareLinks = function(
-        v1States, originalStateIds, v2States, newestStateIds) {
-      links = [];
-      var adjMatrixV1 = _getAdjMatrix(v1States, originalStateIds, _maxId);
-      var adjMatrixV2 = _getAdjMatrix(v2States, newestStateIds, _maxId);
-
-      for (var i = 1; i <= _maxId; i++) {
-        for (var j = 1; j <= _maxId; j++) {
-          if (i === j) {
-            continue;
-          }
-          if (adjMatrixV1[i][j] && adjMatrixV2[i][j]) {
-            links.push({
-              source: i,
-              target: j,
-              linkProperty: 'unchanged'
-            });
-          } else if (!adjMatrixV1[i][j] && adjMatrixV2[i][j]) {
-            links.push({
-              source: i,
-              target: j,
-              linkProperty: 'added'
-            });
-          } else if (adjMatrixV1[i][j] && !adjMatrixV2[i][j]) {
-            links.push({
-              source: i,
-              target: j,
-              linkProperty: 'deleted'
-            });
-          }
-        }
-      }
-
-      return links;
-    };
-
-    var _generateInitialStateIdsAndData = function(statesDict) {
-      var result = {
-        stateIds: {},
-        stateData: {}
-      };
-
-      _resetMaxId();
-
-      for (var stateName in statesDict) {
-        var stateId = _generateNewId();
-        result.stateData[stateId] = {
-          newestStateName: stateName,
-          originalStateName: stateName,
-          stateProperty: STATE_PROPERTY_UNCHANGED
-        };
-        result.stateIds[stateName] = stateId;
-      }
-      return result;
-    };
-
-    var _postprocessStateIdsAndData = function(
-        originalStateIds, stateIds, stateData, v1States, v2States,
-        v1InitStateName, v2InitStateName) {
-      // Ignore changes that were canceled out by later changes
-      for (var stateId in stateData) {
-        if (stateData[stateId].stateProperty === STATE_PROPERTY_CHANGED &&
-            v1States.hasOwnProperty(
-              stateData[stateId].originalStateName) &&
-            v2States.hasOwnProperty(stateData[stateId].newestStateName) &&
-            angular.equals(v1States[stateData[stateId].originalStateName],
-              v2States[stateData[stateId].newestStateName])) {
-          stateData[stateId].stateProperty = STATE_PROPERTY_UNCHANGED;
-        }
-      }
-
-      // Delete states not present in both v1 and v2
-      for (var stateId in stateData) {
-        if (!v1States.hasOwnProperty(
-              stateData[stateId].originalStateName) &&
-            !v2States.hasOwnProperty(stateData[stateId].newestStateName)) {
-          delete stateData[stateId];
-        }
-      }
-
-      // Track whether terminal nodes in v1 or v2
-      // TODO(bhenning): could show changes to terminal nodes in diff
-      var finalStateIds = [];
-      for (var stateId in stateData) {
-        var oldState = v1States[stateData[stateId].originalStateName];
-        var newState = v2States[stateData[stateId].newestStateName];
-        var oldStateIsTerminal = false;
-        var newStateIsTerminal = false;
-        if (oldState) {
-          oldStateIsTerminal = (
-            oldState.interaction.id &&
-            INTERACTION_SPECS[oldState.interaction.id].is_terminal);
-        }
-        if (newState) {
-          newStateIsTerminal = (
-            newState.interaction.id &&
-            INTERACTION_SPECS[newState.interaction.id].is_terminal);
-        }
-        if (oldStateIsTerminal || newStateIsTerminal) {
-          finalStateIds.push(stateId);
-        }
-      }
-
-      var links = _compareLinks(
-        v1States, originalStateIds, v2States, stateIds);
-
-      return {
-        nodes: stateData,
-        links: links,
-        v1InitStateId: originalStateIds[v1InitStateName],
-        v2InitStateId: stateIds[v2InitStateName],
-        finalStateIds: finalStateIds
-      };
-    };
-
     return {
-      /**
-       * Summarizes the diff information for a single change that moves an
-       * exploration from one version to the next.
-       */
-      getDiffData: function(
-          oldStates, newStates, changeList, oldInitStateName,
-          newInitStateName) {
-        var oldInfo = _generateInitialStateIdsAndData(oldStates);
-        var stateData = oldInfo.stateData;
-        var stateIds = oldInfo.stateIds;
-
-        var originalStateIds = angular.copy(stateIds);
-        _trackChanges(stateIds, stateData, changeList, true);
-
-        return _postprocessStateIdsAndData(
-          originalStateIds, stateIds, stateData, oldStates, newStates,
-          oldInitStateName, newInitStateName);
-      },
       /**
        * Summarize changes to states and rules between v1 and v2.
        * Returns a promise for an object whose keys are 'initStateName',
@@ -465,25 +201,28 @@ oppia.factory('compareVersionsService', [
           var v1States = response.v1Data.data.states;
           var v2States = response.v2Data.data.states;
 
-          var v1Info = _generateInitialStateIdsAndData(v1States);
-          var stateData = v1Info.stateData;
-          var stateIds = v1Info.stateIds;
-
-          var originalStateIds = angular.copy(stateIds);
-
-          // Populate stateData with changes from v1 to LCA and from LCA to v2
+          // Track changes from v1 to LCA, and then from LCA to v2.
           var lca = versionsTreeService.findLCA(v1, v2);
-          _trackChanges(
-            stateIds, stateData, _getCombinedChangeList(lca, v1, false),
-            false);
-          _trackChanges(
-            stateIds, stateData, _getCombinedChangeList(lca, v2, true),
-            true);
 
-          return _postprocessStateIdsAndData(
-            originalStateIds, stateIds, stateData, v1States, v2States,
-            response.v1Data.data.init_state_name,
-            response.v2Data.data.init_state_name);
+          var diffGraphData = ExplorationDiffService.getDiffGraphData(
+            v1States, v2States, [{
+              changeList: _getCombinedChangeList(lca, v1, false),
+              directionForwards: false
+            }, {
+              changeList: _getCombinedChangeList(lca, v2, true),
+              directionForwards: true
+            }]
+          );
+
+          return {
+            nodes: diffGraphData.nodes,
+            links: diffGraphData.links,
+            finalStateIds: diffGraphData.finalStateIds,
+            v1InitStateId: diffGraphData.originalStateIds[
+              response.v1Data.data.init_state_name],
+            v2InitStateId: diffGraphData.stateIds[
+              response.v2Data.data.init_state_name]
+          };
         });
       }
     };
