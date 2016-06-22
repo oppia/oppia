9ead391a8a47e4f06fea9b5e103ef9ca0ab7a4c2
Avijit Gupta <526avijit@gmail.com>
2016-06-23 01:54:12 +0530

    Fixes to code

--- core/templates/dev/head/dashboard/Dashboard.js
+++ core/templates/dev/head/dashboard/Dashboard.js
@@ -41,7 +41,7 @@ oppia.controller('Dashboard', [
     $http.get('/dashboardhandler/data').then(function(response) {
       var data = response.data;
       $scope.explorationsList = data.explorations_list.sort(function(a, b) {
-        if (a.title === '' || b.title === '' || a.title > b.title) {
+        if (a.title === '' || a.title > b.title) {
           return 1;
         }
         if (a.title < b.title) {
