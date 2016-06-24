81a47c8ba62cb9ab7490f700a672270fb02367df
Vishal Gupta <vishal.vgu+github@gmail.com>
2016-06-24 17:20:12 +0530

    Implemented helper methods for tests, restructured the tests

--- core/tests/performance_tests/test_config.py
+++ core/tests/performance_tests/test_config.py
@@ -12,16 +12,37 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Performance test config."""
+"""Config file for threshold metrics of Performance Tests.
 
-TEST_DATA = [{
-    'url': '/splash',
-    'size_limits_mb': {
-        'uncached': 10.0,
-        'cached': 1.0,
-    },
-    'load_time_limits_secs': {
-        'uncached': 10.0,
-        'cached': 3.0,
-    },
-}]
+This file contains, for every Oppia page, the corresponding thresholds for
+the different performance metrics.
+
+Each page entry includes:
+    url: relative path to the page.
+    size_limits_mb: threshold for the total data transferred to load the page,
+                    which includes size of all resources requested to display
+                    the page for:
+        uncached: a new session i.e, a first time user.
+        cached: a cached session i.e, a return user.
+    load_time_limits_secs: threshold for the total time to load the page,
+                            which includes time taken to load all the resources
+                            required by the page.
+        uncached: see above.
+        cached: see above.
+"""
+
+SPLASH_PAGE_KEY = 'splash'
+
+TEST_DATA = {
+    SPLASH_PAGE_KEY: {
+        'url': '/splash',
+        'size_limits_mb': {
+            'uncached': 10.0,
+            'cached': 1.0,
+        },
+        'load_time_limits_secs': {
+            'uncached': 10.0,
+            'cached': 3.0,
+        },
+    }
+}
