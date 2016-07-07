79dc42a5dc21b1c3a1d6be1850ad08348fe5d08f
Vishal Gupta <gvishal@users.noreply.github.com>
2016-07-07 22:20:11 +0530

    #2048 add performance tests for various Oppia pages. (#2167)
    * Added tests for library page
    
    * Implemented browser interactions and tests for 5 other Oppia pages.
    
    * Remove xvfb-wrapper from performance tests, install xvfb for all users
    
    * Addressed review comments
    
    * Moved xvfb installation to performance tests
    
    * Minor fix
    
    * Addressed review comments
    
    * Fix: #2203. Modified run_performance_tests.sh to run test for a single page
    
    * Added name in todo
    
    * Removed callback pattern, updated run_performance_tests to run tests individually.
    
    * Fixed cookie issue
    
    * Modified timings
    
    * Addressed review comments.
    
    * Minor changes
    
    * Addressed review comments, modified travis to run only the splash test.
    
    * Addressed review comments
    
    * Addressed review comments.
    
    * Added docstring for check_login
    
    * More review comments fixed.
    
    * Modified run_performance_tests.sh
    
    * minor change
    
    * Updated travis.yml and size limit

--- feconf.py
+++ feconf.py
@@ -40,7 +40,6 @@ if PLATFORM == 'gae':
 else:
     raise Exception('Invalid platform: expected one of [\'gae\']')
 
-
 TESTS_DATA_DIR = os.path.join('core', 'tests', 'data')
 SAMPLE_EXPLORATIONS_DIR = os.path.join('data', 'explorations')
 SAMPLE_COLLECTIONS_DIR = os.path.join('data', 'collections')
@@ -396,6 +395,7 @@ EMBEDDED_GOOGLE_GROUP_URL = (
 ALLOW_YAML_FILE_UPLOAD = False
 
 # TODO(sll): Add all other URLs here.
+ADMIN_URL = '/admin'
 COLLECTION_DATA_URL_PREFIX = '/collection_handler/data'
 COLLECTION_WRITABLE_DATA_URL_PREFIX = '/collection_editor_handler/data'
 COLLECTION_RIGHTS_PREFIX = '/collection_editor_handler/rights'
@@ -424,6 +424,7 @@ NEW_COLLECTION_URL = '/collection_editor_handler/create_new'
 NEW_EXPLORATION_URL = '/contributehandler/create_new'
 RECENT_COMMITS_DATA_URL = '/recentcommitshandler/recent_commits'
 RECENT_FEEDBACK_MESSAGES_DATA_URL = '/recent_feedback_messages'
+ROBOTS_TXT_URL = '/robots.txt'
 SITE_LANGUAGE_DATA_URL = '/save_site_language'
 SIGNUP_DATA_URL = '/signuphandler/data'
 SIGNUP_URL = '/signup'
