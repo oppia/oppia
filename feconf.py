e26ee9a363509ddd43f077905d7971f4664b68b6
Avijit Gupta <526avijit@gmail.com>
2016-07-07 23:19:20 +0530

    Fix part of #1366: Cron job to keep history of dashboard statistics (#2173)
    * Cron job for recording creator stats
    
    * Fixes
    
    * Write getter and tests for weekly dasboard stats
    
    * Minor changes to model
    
    * Fix initial round of review comments
    
    * Fix call to one-off job through cron
    
    * Removed extra whiteline
    
    * Fix accidental line deletion
    
    * Partially addressed review comments
    
    * Add sample test for multiple weeks
    
    * Add test to check format of datetime string
    
    * Add schema version to user dashboard stats
    
    * Lint fix
    
    * Fix brackets around conditional
    
    * Fix continuous job and modify tests accordingly
    
    * Address review comments
    
    * Create new stats model if not already present
    
    * Address review comments
    
    * Use get_or_create for model
    
    * Reused get_or_create in method

--- feconf.py
+++ feconf.py
@@ -72,6 +72,11 @@ NUMBER_OF_TOP_RATED_EXPLORATIONS = 8
 # for recently published explorations.
 RECENTLY_PUBLISHED_QUERY_LIMIT = 8
 
+# The current version of the dashboard stats blob schema. If any backward-
+# incompatible changes are made to the stats blob schema in the data store,
+# this version number must be changed.
+CURRENT_DASHBOARD_STATS_SCHEMA_VERSION = 1
+
 # The current version of the exploration states blob schema. If any backward-
 # incompatible changes are made to the states blob schema in the data store,
 # this version number must be changed and the exploration migration job
@@ -261,6 +266,11 @@ PANELS_PROPERTIES = {
 
 # When the site terms were last updated, in UTC.
 REGISTRATION_PAGE_LAST_UPDATED_UTC = datetime.datetime(2015, 10, 14, 2, 40, 0)
+
+# Format of string for dashboard statistics logs.
+# NOTE TO DEVELOPERS: This format should not be changed, since it is used in
+# the existing storage models for UserStatsModel.
+DASHBOARD_STATS_DATETIME_STRING_FORMAT = '%Y-%m-%d'
 
 # The maximum size of an uploaded file, in bytes.
 MAX_FILE_SIZE_BYTES = 1048576
