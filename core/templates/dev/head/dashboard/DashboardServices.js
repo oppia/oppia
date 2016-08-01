ae678c2c82fff1ddcc5e0853496ee229ae8cd92f
Avijit Gupta <526avijit@gmail.com>
2016-08-01 19:17:11 +0530

    Initial commit for dashboard services

---
+++ core/templates/dev/head/dashboard/DashboardServices.js
@@ -0,0 +1,56 @@
+// Copyright 2014 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Standalone services for the creator dashboard page.
+ */
+
+// Service for sorting the explorations based on different parameters.
+oppia.factory('sortExplorationsService', [
+  function() {
+    var explorationsList = [];
+    var SORT_BY_KEYS = [
+      'title',
+      'last_updated_msec',
+      'num_views',
+      'num_open_threads',
+      'num_unresolved_answers'
+    ];
+    var sortByKey = function(key, reverse) {
+      var result = explorationsList;
+      result.sort(function(prev, next) {
+        if (prev[key] < next[key]) {
+          return true;
+        }
+        return false;
+      });
+      if (reverse) {
+        return result.reverse();
+      }
+      return result;
+    };
+    return {
+      setExplorationsList: function(explorationsList) {
+        explorationsList = explorationsList;
+      },
+      sortBy: function(param, reverse) {
+        if (SORT_BY_KEYS.indexOf(param) !== -1) {
+          return sortByKey(param, reverse);
+        } else {
+          return explorationsList;
+        }
+      }
+    };
+  }
+]);
