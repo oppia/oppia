3d2a6ad0ad68f134da6721c8a6911e61c26f96f6
Sean Lip <sean@seanlip.org>
2016-08-13 21:51:22 -0700

    Modify code REPL rules to do less normalization for snippets.

--- extensions/interactions/CodeRepl/CodeRepl.js
+++ extensions/interactions/CodeRepl/CodeRepl.js
@@ -275,15 +275,13 @@ oppia.factory('codeReplRulesService', [
     CodeContains: function(answer, inputs) {
       var normalizedCode =
         codeNormalizationService.getNormalizedCode(answer.code);
-      var normalizedSnippet =
-        codeNormalizationService.getNormalizedCode(inputs.x);
+      var normalizedSnippet = $filter('normalizeWhitespace')(inputs.x);
       return normalizedCode.indexOf(normalizedSnippet) !== -1;
     },
     CodeDoesNotContain: function(answer, inputs) {
       var normalizedCode =
         codeNormalizationService.getNormalizedCode(answer.code);
-      var normalizedSnippet =
-        codeNormalizationService.getNormalizedCode(inputs.x);
+      var normalizedSnippet = $filter('normalizeWhitespace')(inputs.x);
       return normalizedCode.indexOf(normalizedSnippet) === -1;
     },
     OutputContains: function(answer, inputs) {
