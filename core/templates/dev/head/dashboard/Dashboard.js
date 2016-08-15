0624fc6ba182f20e61dde5f006d02ced960dac2f
Avijit Gupta <526avijit@gmail.com>
2016-08-15 06:59:43 +0530

    Address review comments

--- core/templates/dev/head/dashboard/Dashboard.js
+++ core/templates/dev/head/dashboard/Dashboard.js
@@ -100,7 +100,9 @@ oppia.controller('Dashboard', [
       }
     };
 
-    $scope.sortExplorations = function(entity) {
+    $scope.sortByFunction = function(entity) {
+      // This function is passed as a custom comparator function to `orderBy`,
+      // so that special cases can be handled while sorting explorations.
       var value = entity[$scope.currentSortType];
       var DEFAULT_TEXT_EMPTY_TITLE = 'Untitled';
       if ($scope.currentSortType === EXPLORATIONS_SORT_BY_KEYS.TITLE) {
