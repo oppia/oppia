4c4d6f3f6c8915f1f597d7ece21ff6132fef273e
Jacob Davis <jacobdavis11@gmail.com>
2016-08-26 02:58:48 +0200

    Metrics: add analytics events for donation and the teach with Oppia program. (#2435)

--- core/templates/dev/head/pages/Donate.js
+++ core/templates/dev/head/pages/Donate.js
@@ -17,10 +17,30 @@
  */
 
 oppia.controller('Donate', [
-  '$scope', 'UrlInterpolationService',
+  '$http', '$scope', '$timeout', '$window', 'UrlInterpolationService',
+  'siteAnalyticsService',
   function(
-      $scope, UrlInterpolationService) {
+      $http, $scope, $timeout, $window, UrlInterpolationService,
+      siteAnalyticsService) {
     $scope.donateImgUrl = UrlInterpolationService.getStaticImageUrl(
       '/general/opp_donate_text.svg');
+
+    $scope.onDonateThroughAmazon = function() {
+      siteAnalyticsService.registerGoToDonationSiteEvent('Amazon');
+      $timeout(function() {
+        $window.location = 'https://smile.amazon.com/ch/81-1740068';
+      }, 150);
+      return false;
+    };
+
+    $scope.onDonateThroughPayPal = function() {
+      // Redirection to PayPal will be initiated at the same time as this
+      // function is run, but should be slow enough to allow this function time
+      // to complete. It is not possible to do $http.post() in javascript after
+      // a delay because cross-site POSTing is not permitted in scripts; see
+      // https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS
+      // for more information.
+      siteAnalyticsService.registerGoToDonationSiteEvent('PayPal');
+    };
   }
 ]);
