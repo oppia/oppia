3eeb58144734bc9dd055f5a6d782e7ad1d94304f
Arunabh <arunabhghosh98@gmail.com>
2016-08-29 14:54:46 +0530

    Fix #2354: put languages in alphabetical order (#2368)
    * Fix #2354: languages appear in alphabetical order in splash page
    
    * Add comment to ensure the languages are in alphabetical order
    
    * Convert dict of languages to list - starting implementation
    
    * languages as list are now displayed in footer
    
    * fixed linting issues
    
    * fix a few bugs to pass end to end tests
    
    * partial modifications based on feedback
    
    * address review comments
    
    * update karma global variable for supported site languages
    
    * modify comment for adding languages alphabetically

--- core/tests/karma-globals.js
+++ core/tests/karma-globals.js
@@ -20,10 +20,13 @@ var GLOBALS = {
   INTERACTION_SPECS: {},
   GADGET_SPECS: {},
   PANEL_SPECS: {},
-  SUPPORTED_SITE_LANGUAGES: {
-    en: 'English',
-    es: 'Español'
-  }
+  SUPPORTED_SITE_LANGUAGES: [{
+    id: 'id',
+    text: 'Bahasa Indonesia'
+  }, {
+    id: 'en',
+    text: 'English'
+  }]
 };
 
 /* This function overwrites the translationProvider for a dummy function
