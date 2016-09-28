be310cc22fa925ec6df40c4d27c989cc387870f7
andimeo <andimeo@foxmail.com>
2016-09-28 11:29:30 +0800

    pylint: wrong continued indentation

--- core/controllers/base.py
+++ core/controllers/base.py
@@ -408,7 +408,7 @@ class BaseHandler(webapp2.RequestHandler):
         # GET_HANDLER_ERROR_RETURN_TYPE.
         # Otherwise, we check whether self.payload exists.
         if (self.payload is not None or
-            self.GET_HANDLER_ERROR_RETURN_TYPE == feconf.HANDLER_TYPE_JSON):
+                self.GET_HANDLER_ERROR_RETURN_TYPE == feconf.HANDLER_TYPE_JSON):
             self.render_json(values)
         else:
             self.values.update(values)
