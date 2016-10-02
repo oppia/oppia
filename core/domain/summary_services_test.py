d37dafc35b1b2345fc74691f19cbf29a12f1eea0
Sourav Badami <souravbadami@gmail.com>
2016-10-02 11:55:32 +0530

    Minor Changes.

--- core/domain/summary_services_test.py
+++ core/domain/summary_services_test.py
@@ -934,24 +934,20 @@ class CollectionNodeMetadataDictsTest(
         summaries = (summary_services.get_exploration_metadata_dicts(
             [self.EXP_ID1, self.EXP_ID2, self.EXP_ID3], self.albert_id))
 
-        expected_summary1 = {
+        expected_summaries = [{
             'id': self.EXP_ID1,
             'objective': u'An objective 1',
             'title': u'Exploration 1 Albert title',
-        }
-        expected_summary2 = {
+        }, {
             'id': self.EXP_ID2,
             'objective': u'An objective 2',
             'title': u'Exploration 2 Albert title',
-        }
-        expected_summary3 = {
+        }, {
             'id': self.EXP_ID3,
             'objective': u'An objective 3',
             'title': u'Exploration 3 Albert title',
-        }
-        self.assertEqual(expected_summary1, summaries)
-        self.assertEqual(expected_summary2, summaries)
-        self.assertEqual(expected_summary3, summaries)
+        }]
+        self.assertEqual(expected_summaries, summaries)
 
     def test_private_exps_of_another_user_are_not_returned(self):
         summaries = (summary_services.get_exploration_metadata_dicts(
@@ -976,7 +972,7 @@ class CollectionNodeMetadataDictsTest(
         self.assertEqual(expected_summary, summaries)
 
     def test_deleted_exps_are_not_returned(self):
-        exp_services.delete_exploration(self.albert_id, self.EXP_ID_2)
+        exp_services.delete_exploration(self.albert_id, self.EXP_ID2)
 
         summaries = (summary_services.get_exploration_metadata_dicts(
             [self.EXP_ID2, self.EXP_ID3, self.EXP_ID4], self.bob_id))
@@ -986,23 +982,20 @@ class CollectionNodeMetadataDictsTest(
             'objective': u'An objective 3',
             'title': u'Exploration 3 Albert title',
         }
-        self.assertEqual(expected_summary, summaries[0])
+        self.assertEqual(expected_summary, summaries)
 
     def test_exp_metadata_dicts_matching_query(self):
         summaries, _ = (
             summary_services.get_exp_metadata_dicts_matching_query(
-                'Exploration 1 Albert title', None, self.albert_id))
+                'Exploration 1', None, self.albert_id))
 
-        expected_summary1 = {
+        expected_summaries = [{
             'id': self.EXP_ID1,
             'objective': u'An objective 1',
             'title': u'Exploration 1 Albert title',
-        }
-        expected_summary2 = {
+        }, {
             'id': self.EXP_ID2,
             'objective': u'An objective 2',
             'title': u'Exploration 2 Albert title',
-        }
-
-        self.assertEqual(expected_summary1, summaries)
-        self.assertEqual(expected_summary2, summaries)
+        }]
+        self.assertEqual(expected_summaries, summaries)
