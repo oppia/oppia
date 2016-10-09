83b7484004578d00eadf5655b80b5f7886389bb1
Arunabh98 <arunabhghosh98@gmail.com>
2016-10-09 06:32:27 +0530

    Fix #2531: show multiple rules for an answer group

--- core/templates/dev/head/components/ResponseHeaderDirective.js
+++ core/templates/dev/head/components/ResponseHeaderDirective.js
@@ -25,7 +25,8 @@ oppia.directive('responseHeader', [function() {
       getSummary: '&summary',
       getShortSummary: '&shortSummary',
       isActive: '&isActive',
-      getOnDeleteFn: '&onDeleteFn'
+      getOnDeleteFn: '&onDeleteFn',
+      extraRules: '&extraRules'
     },
     templateUrl: 'components/responseHeader',
     controller: [
