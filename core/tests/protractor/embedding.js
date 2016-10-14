c92350dc77cb57db558c59852535ac42d9ad51e0
Sean Lip <sll@google.com>
2016-10-13 19:13:26 -0700

    Fix mismatch in embedding test.

--- core/tests/protractor/embedding.js
+++ core/tests/protractor/embedding.js
@@ -140,8 +140,9 @@ describe('Embedding', function() {
           var EMBEDDING_PREFIX = 'Embedding test: ';
           if (message.indexOf(EMBEDDING_PREFIX) !== -1) {
             var index = message.indexOf(EMBEDDING_PREFIX);
-            embeddingLogs.push(
-              message.substring(index + EMBEDDING_PREFIX.length));
+            // The "-1" in substring() removes the trailing quotation mark.
+            embeddingLogs.push(message.substring(
+              index + EMBEDDING_PREFIX.length, message.length - 1));
           }
         } catch (err) {}
       }
