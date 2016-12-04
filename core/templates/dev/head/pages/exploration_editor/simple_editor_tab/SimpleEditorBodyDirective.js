87f641b48c1ba25d363c480c6fb516307dd0b800
Keksike <cihan.m.bebek@gmail.com>
2016-12-04 12:18:25 +0900

    rename finishExploration to publishChanges

--- core/templates/dev/head/pages/exploration_editor/simple_editor_tab/SimpleEditorBodyDirective.js
+++ core/templates/dev/head/pages/exploration_editor/simple_editor_tab/SimpleEditorBodyDirective.js
@@ -57,7 +57,7 @@ oppia.directive('simpleEditorBody', [function() {
           return false;
         };
 
-        $scope.finishExploration = function() {
+        $scope.publishChanges = function() {
           // If exploration is not yet published
           // and doesn't have unsaved changes,
           // we can just open publishing modal straight away.
