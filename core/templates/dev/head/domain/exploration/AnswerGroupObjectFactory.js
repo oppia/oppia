fb571ed9aeba6062c5ed4c67af58383973db59df
Tony <tjiang11@jhu.edu>
2017-01-01 21:00:21 -0600

    Testing

--- core/templates/dev/head/domain/exploration/AnswerGroupObjectFactory.js
+++ core/templates/dev/head/domain/exploration/AnswerGroupObjectFactory.js
@@ -12,8 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-oppia.factory('AnswerGroupObjectFactory', [
-  function() {
+oppia.factory('AnswerGroupObjectFactory', [function() {
     var AnswerGroup = function(ruleSpecs, outcome) {
       this.ruleSpecs = ruleSpecs;
       this.outcome = outcome;
