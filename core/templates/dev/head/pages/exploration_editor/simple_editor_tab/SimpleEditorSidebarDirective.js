74d59ba1ec3910c7ae9d43b7e26a999c838f8b71
andromfins <zeiurfhc@gmail.com>
2017-01-10 14:22:22 +0000

    Update the sidebar active element when the user scrolls the main content.

--- core/templates/dev/head/pages/exploration_editor/simple_editor_tab/SimpleEditorSidebarDirective.js
+++ core/templates/dev/head/pages/exploration_editor/simple_editor_tab/SimpleEditorSidebarDirective.js
@@ -16,13 +16,116 @@
  * @fileoverview Directive for the sidebar of the simple editor.
  */
 
-oppia.factory('ScrollSyncService', ['$anchorScroll', function($anchorScroll) {
-    $anchorScroll.yOffset = 80;
-    return {
-      scrollTo: function(hash) {
-        $anchorScroll(hash);
+oppia.factory('ScrollSyncService', ['$anchorScroll', '$document', '$rootScope',
+  function($anchorScroll, $document, $rootScope) {
+  scrollSyncService = {
+    _targets: [],
+    _questionList: null,
+    addTarget: function(target) {
+      this._targets.push(target);
+    },
+    _clear: function() {
+      angular.element('.highlighted').removeClass('highlighted');
+    },
+    _collapse: function(hash) {
+      var question;
+      var questionIndex;
+      var questions = this._questionList.getQuestions();
+      for (var i = 0, questionsLength = questions.length;
+            i < questionsLength; i++) {
+        if (questions[i].getHash() === hash) {
+          questionIndex = i;
+          break;
+        }
+      };
+      if (!angular.isDefined(questionIndex)) {
+        return;
+      };
+      question = questions[questionIndex];
+      $rootScope.$broadcast('SimpleEditorSidebarLinkCollapse', question);
+    },
+    // Get Question id given a child id.
+    getQuestionHash: function(childHash) {
+      var hash = childHash.split('-');
+      if (hash.length > 2) {
+        return hash.slice(0, 2).join('-');
+      } else {
+        return hash[0];
       }
+    },
+    // Update the active nav link
+    updateActive: function(hash) {
+      if (!angular.isDefined(hash)) {
+        return;
+      } else {
+        this._clear();
+        var elm = angular.element('#si-' + hash);
+
+        if (!(hash === 'intro' || hash === 'title')) {
+          elm.addClass('highlighted');
+        };
+
+        // Uncollapse the nav link in case hidden
+        if (elm.is(':hidden')) {
+          this._collapse(this.getQuestionHash(hash));
+        };
+        // TODO: scroll to the nav link in case not displayed
+      }
+    },
+    scrollTo: function(hash) {
+      $anchorScroll.yOffset = 80;
+      $anchorScroll(hash);
     }
+  };
+
+  // Onscroll event handler
+  $document.on('scroll', function() {
+    var documentY = $document.scrollTop();
+    var positiveTargets = {};
+    var positiveTargetsDy = [];
+    // Find the closest positive target
+    for (var i = 0, targetsLength = scrollSyncService._targets.length;
+          i < targetsLength; i++) {
+      var target = angular.element(scrollSyncService._targets[i]);
+      var targetOffset = target.offset().top;
+      var dy = targetOffset - documentY;
+      if (dy > 0) {
+        positiveTargets[dy] = target;
+        positiveTargetsDy.push(dy);
+      }
+    };
+    for (var i = 0, targetsLength = positiveTargetsDy.length,
+          minPositive = null; i < targetsLength; i++) {
+      if (i === 0) {
+        minPositive = positiveTargetsDy[i];
+        continue;
+      } else {
+        var dy = positiveTargetsDy[i];
+        if (minPositive > dy) {
+          minPositive = dy;
+        }
+      }
+    };
+    if (minPositive === null) {
+      return;
+    } else {
+      // Update the sidebar active element
+      var closestTarget = positiveTargets[minPositive];
+      scrollSyncService.updateActive(closestTarget.attr('id'));
+    }
+  });
+
+  return scrollSyncService;
+}]);
+
+// Used to mark parts of the editor that have a sidebar item associated to it.
+oppia.directive('scrollSync', ['ScrollSyncService', function(ScrollSyncService) {
+  return {
+    restrict: 'A',
+    link: function(scope, element) {
+      ScrollSyncService.addTarget(element);
+    }
+  };
 }]);
 
 oppia.directive('simpleEditorSidebar', [function() {
@@ -30,7 +133,7 @@ oppia.directive('simpleEditorSidebar', [function() {
     restrict: 'E',
     templateUrl: 'simpleEditor/sidebar',
     controller: [
-        '$scope', 'EditorModeService', 'SimpleEditorManagerService',
+       '$scope', 'EditorModeService', 'SimpleEditorManagerService',
         'ScrollSyncService',
         function($scope, EditorModeService, SimpleEditorManagerService,
                  ScrollSyncService) {
@@ -41,14 +144,26 @@ oppia.directive('simpleEditorSidebar', [function() {
         {label: 'Bridge text', abbr: 'bt'}
       ];
       $scope.setEditorModeToFull = EditorModeService.setModeToFull;
-      $scope.questionList = SimpleEditorManagerService.getQuestionList();
-      $scope.hightlighted_element_hash = 'title';
+      $scope.questionList = ScrollSyncService._questionList = SimpleEditorManagerService.getQuestionList();
       $scope.scrollTo = function(hash) {
         ScrollSyncService.scrollTo(hash);
-        $scope.highlightedElementHash = hash;
       };
-      $scope.isHighlighted = function(hash) {
-        return hash === $scope.highlightedElementHash;
+      $scope.collapse = function(question) {
+        if (!angular.isDefined(question.collapsed)) {
+          question.collapsed = false;
+        } else {
+          question.collapsed = !question.collapsed;
+        }
+      };
+      $scope.$on('SimpleEditorSidebarLinkCollapse', function(event, question) {
+        $scope.collapse(question);
+        $scope.$apply();
+      });
+      $scope.isCollapsed = function(question) {
+        if (!angular.isDefined(question.collapsed)) {
+          question.collapsed = false;
+        };
+        return question.collapsed;
       };
     }]
   };
