54953deb9a1fb276888f51d1e8b30e7b88335a6f
Himanshu Dixit <hudixt@gmail.com>
2017-01-17 23:34:04 +0530

    Final Review Changes

--- core/templates/dev/head/components/social_buttons/SocialButtonsDirective.js
+++ core/templates/dev/head/components/social_buttons/SocialButtonsDirective.js
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 /**
- * @fileoverview Directive for the Social Buttons. Used for Social Buttons displayed in the footer.
+ * @fileoverview Directive for the social buttons displayed in footer
  */
 
 oppia.directive('socialButtons', [function() {
