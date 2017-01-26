3f722ec82133efc1144d3edfb6c2b81247673c03
Kevin Thomas <kevintab@tutanota.com>
2017-01-27 05:25:17 +0530

    path version

--- core/templates/dev/head/pages/collection_player/CollectionPlayer.js
+++ core/templates/dev/head/pages/collection_player/CollectionPlayer.js
@@ -33,15 +33,25 @@ oppia.animation('.oppia-collection-animate-slide', function() {
 oppia.controller('CollectionPlayer', [
   '$scope', '$http', 'ReadOnlyCollectionBackendApiService',
   'CollectionObjectFactory', 'CollectionPlaythroughObjectFactory',
-  'alertsService',
+  'alertsService', 'UrlInterpolationService',
   function($scope, $http, ReadOnlyCollectionBackendApiService,
     CollectionObjectFactory, CollectionPlaythroughObjectFactory,
-    alertsService) {
+    alertsService, UrlInterpolationService) {
     $scope.collection = null;
     $scope.collectionPlaythrough = null;
     $scope.collectionId = GLOBALS.collectionId;
     $scope.showingAllExplorations = !GLOBALS.isLoggedIn;
     $scope.previewCardIsShown = true;
+    $scope.getStaticImageUrl = UrlInterpolationService.getStaticImageUrl;
+    $scope.pathIconParameters = [];
+
+    $scope.setIconHighlight = function(index) {
+      document.getElementById('highlight' + index).setAttribute('class','ng-show');
+    };
+
+    $scope.unsetIconHighlight = function(index) {
+      document.getElementById('highlight' + index).setAttribute('class','ng-hide');
+    };
 
     $scope.togglePreviewCard = function() {
       $scope.previewCardIsShown = !$scope.previewCardIsShown;
@@ -106,6 +116,83 @@ oppia.controller('CollectionPlayer', [
       $scope.currentExplorationId = explorationId;
       $scope.summaryToPreview = $scope.getCollectionNodeForExplorationId(
         explorationId).getExplorationSummaryObject();
+    };
+
+    $scope.generatePathParameters = function() {
+      var pathSvgParameters = "M250 80  C 500 100, 500 280, 250 300"
+      var collectionNodeCount = $scope.collection.getCollectionNodeCount()
+      var sParameterExtension = ""
+      $scope.svgHeight = 220
+      if (collectionNodeCount === 1) {
+        $scope.pathIconParameters = $scope.generatePathIconParameters();
+        return ""
+      }
+      else if(collectionNodeCount === 2) {
+        $scope.pathIconParameters = $scope.generatePathIconParameters();
+        return pathSvgParameters
+      }
+      else {
+        var y = 500;
+        for(var i = 1; i < Math.floor(collectionNodeCount / 2) ; i++) {
+          if(i % 2 === 0) {
+            x = 500
+          }
+          else {
+            x = 0
+          }
+          sParameterExtension += x + " " + y + ", "
+          y += 20
+          sParameterExtension += 250 + " " + y + ", "
+          y += 200
+        }
+        pathSvgParameters += " S " + sParameterExtension
+      }
+        if(collectionNodeCount % 2 === 0) {
+          $scope.svgHeight = y - 280;
+        }
+        else {
+          $scope.svgHeight = y - 150;
+        }
+        $scope.pathIconParameters = $scope.generatePathIconParameters();
+        return pathSvgParameters
+    };
+
+    $scope.generatePathIconParameters = function() {
+      var collectionNodes = $scope.collection.getCollectionNodes();
+      var arr = [];
+      arr.push({
+          thumbnailIconUrl: collectionNodes[0].getExplorationSummaryObject().thumbnail_icon_url,
+          left: '225px',
+          top: '60px',
+          thumbnailBgColor: collectionNodes[0].getExplorationSummaryObject().thumbnail_bg_color
+        });
+      var x = 225;
+      var y = 60;
+      var count = 1;
+
+      for (var i=1; i<$scope.collection.getCollectionNodeCount(); i++) {
+        if (count === 0 && x === 225) {
+          x = 30;
+          y += 110;
+          count = 1;
+        }
+        else if (count === 1 && x === 225) {
+          x = 420;
+          y += 110;
+          count = 0;
+        }
+        else {
+          x = 225;
+          y += 110;
+        }
+        arr.push({
+          thumbnailIconUrl: collectionNodes[i].getExplorationSummaryObject().thumbnail_icon_url,
+          left: x + 'px',
+          top: y + 'px',
+          thumbnailBgColor: collectionNodes[i].getExplorationSummaryObject().thumbnail_bg_color
+        });
+      }
+      return arr;
     };
 
     $http.get('/collectionsummarieshandler/data', {
