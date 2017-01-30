acaa4e8a07754a84f592cd8e499aaf425d52b262
kirankonduru <kiranckonduru@gmail.com>
2017-01-30 09:55:53 +0530

    Updates alt for image upload in explorations as a required field

--- schema_utils.py
+++ schema_utils.py
@@ -141,7 +141,8 @@ def normalize_against_schema(obj, schema, apply_custom_validators=True):
             for validator in schema[SCHEMA_KEY_VALIDATORS]:
                 kwargs = dict(validator)
                 del kwargs['id']
-                assert _Validators.get(validator['id'])(normalized_obj, **kwargs), (
+                assert _Validators.get(validator['id'])(normalized_obj,
+                    **kwargs), (
                     'Validation failed: %s (%s) for object %s' % (
                         validator['id'], kwargs, normalized_obj))
 
