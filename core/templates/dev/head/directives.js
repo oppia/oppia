8c9907501b60ffc165f9505d4b4d9d551e70c3db
SudhanvaMG <mgsudhanva@gmail.com>
2017-01-31 13:20:00 +0530

    #2954 lang attribute

--- core/templates/dev/head/directives.js
+++ core/templates/dev/head/directives.js
@@ -156,3 +156,19 @@ oppia.directive('mobileFriendlyTooltip', ['$timeout', function($timeout) {
     }
   };
 }]);
+
+oppia.directive( 'updateLanguage', updateLanguage );
+function updateLanguage( $rootScope ) {
+    return {
+      link: function( scope, element ) {
+        var listener = function( event, translationResp ) {
+          var defaultLang = "en",
+              currentlang = translationResp.language;
+
+          element.attr("lang", currentlang || defaultLang );
+        };
+
+        $rootScope.$on('$translateChangeSuccess', listener);
+      }
+   };
+}
