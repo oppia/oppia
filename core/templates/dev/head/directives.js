d6d9b51f14a3290f2d213d123f6d98c86381c63d
Sudhanva MG <mgsudhanva@gmail.com>
2017-01-31 15:56:35 +0530

    Styles changes

--- core/templates/dev/head/directives.js
+++ core/templates/dev/head/directives.js
@@ -157,13 +157,13 @@ oppia.directive('mobileFriendlyTooltip', ['$timeout', function($timeout) {
   };
 }]);
 
-oppia.directive('updateLanguage', [function($rootScope) {
+oppia.directive('updateLanguage', ['$rootScope',function($rootScope) {
   return {
     link: function( scope, element ) {
-      var listener = function( event, translationResponse ) {
-        var defaultLang = "en",
-            currentlang = translationResponse.language;
-            element.attr("lang", currentlang || defaultLang );
+      var listener = function( evt, translationResponse ) {
+        var DEFAULT_LANG = 'en',
+        var CURRENT_LANG = translationResponse.language;
+        element.attr('lang', CURRENT_LANG || DEFAULT_LANG);
       };
       $rootScope.$on('$translateChangeSuccess', listener);
     }
