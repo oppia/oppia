3c2462ec97287790ec300506bb5b719af73af85f
Prasanna Patil <prasannapatil08@gmail.com>
2017-02-10 01:25:29 +0530

    FIx #2768: Implement email receiving functionality for feedback messages. (#3029)
    * Implement email receiving functionality for feedback messages.
    
    * Fix typing mistake.
    
    * Remove unused imports.
    
    * Addressed review comments
    
    * Fix backend tests.
    
    * Addressed review comments.

--- feconf.py
+++ feconf.py
@@ -198,6 +198,8 @@ MAILGUN_API_KEY = None
 # If the Mailgun email API is used, the "None" below should be replaced
 # with the Mailgun domain name (ending with mailgun.org).
 MAILGUN_DOMAIN_NAME = None
+# Domain name for email address.
+INCOMING_EMAILS_DOMAIN_NAME = 'example.com'
 # Committer id for system actions.
 SYSTEM_COMMITTER_ID = 'admin'
 SYSTEM_EMAIL_ADDRESS = 'system@example.com'
