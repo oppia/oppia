b326f0bde390afc9710a155bfb0a65b66d6d77e2
1995YogeshSharma <yogesh.sharma@research.iiit.ac.in>
2017-02-17 13:58:55 +0530

    Fixes #3064: linting issues solved

--- scripts/getOppiaClassesPerFile.py
+++ scripts/getOppiaClassesPerFile.py
@@ -16,25 +16,25 @@ my_soup = bs4.BeautifulSoup(f.read(), 'html.parser').find('script')
 inside_script_content = str(my_soup.contents)
 
 soup_from_text_inside_script = bs4.BeautifulSoup(
-	inside_script_content, 'html.parser')
+   inside_script_content, 'html.parser')
 
 #extracting list of all the classes per tag
 all_classes = [
-	tag.attrs.get('class') for tag in soup_from_text_inside_script.findAll()]
+   tag.attrs.get('class') for tag in soup_from_text_inside_script.findAll()]
 
 list_oppia_classes = []
 
 #iterating through the list and output the result if there is a match
 for i in all_classes:
-	if i == None:
-		continue
-	else:
-		for j in i:
-			if find_match.match(j):
-				list_oppia_classes.append(j)
+    if i is None:
+        continue
+    else:
+        for j in i:
+            if find_match.match(j):
+            list_oppia_classes.append(j)
 
 for i in set(list_oppia_classes):
-	print i
+    print i
 
 
 
