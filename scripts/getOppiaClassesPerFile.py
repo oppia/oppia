15d4042a46a9f257f40671e90ebf958da916a296
1995YogeshSharma <yogesh.sharma@research.iiit.ac.in>
2017-02-17 13:55:00 +0530

    Fixes #3064: linting issues solved

--- scripts/getOppiaClassesPerFile.py
+++ scripts/getOppiaClassesPerFile.py
@@ -1,37 +1,39 @@
-import bs4
 import re
+import bs4
 
 #get directive filename from user
-fname = raw_input('enter file name  ')
+file_name = raw_input('enter file name  ')
 
-f = open(fname, 'r')
+f = open(file_name, 'r')
 
 #making regular expression to match all oppia classes
-findMatch = re.compile('.*oppia.*')
+find_match = re.compile('.*oppia.*')
 
-#making BeautifulSoup object of file then extracting the script tag and 
+#making BeautifulSoup object of file then extracting the script tag and
 # making BeautifulSoup object of text inside script tag
-mySoup = bs4.BeautifulSoup(f.read(), 'html.parser').find('script')
+my_soup = bs4.BeautifulSoup(f.read(), 'html.parser').find('script')
 
-insideScriptContent = str(mySoup.contents)
+inside_script_content = str(my_soup.contents)
 
-soupFromTextInsideScript = bs4.BeautifulSoup(insideScriptContent, 'html.parser')
+soup_from_text_inside_script = bs4.BeautifulSoup(
+	inside_script_content, 'html.parser')
 
 #extracting list of all the classes per tag
-all_classes = [tag.attrs.get('class') for tag in soupFromTextInsideScript.findAll()]
+all_classes = [
+	tag.attrs.get('class') for tag in soup_from_text_inside_script.findAll()]
 
-listOppiaClasses = []
+list_oppia_classes = []
 
 #iterating through the list and output the result if there is a match
-for i in all_classes :
-	if i == None :
+for i in all_classes:
+	if i == None:
 		continue
 	else:
-		for j in i :
-			if findMatch.match(j):
-				listOppiaClasses.append(j)
+		for j in i:
+			if find_match.match(j):
+				list_oppia_classes.append(j)
 
-for i in set(listOppiaClasses):
+for i in set(list_oppia_classes):
 	print i
 
 
