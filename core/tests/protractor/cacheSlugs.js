6d4b71a7ae4df4d3c88fa97da79ead46478c5806
Vojtěch Jelínek <vojtin.j@gmail.com>
2017-02-23 20:17:52 +0100

    Extended comment

--- core/tests/protractor/cacheSlugs.js
+++ core/tests/protractor/cacheSlugs.js
@@ -57,7 +57,9 @@ describe('Cache Slugs', function() {
     browser.get(ERROR_PAGE_URL_SUFFIX);
     var expectedErrors = [
       'http://localhost:9001/build/fail/logo/288x128_logo_white.png',
-      // Warning fired by synchronous AJAX request for constatns
+      // Warning fired by synchronous AJAX request in
+      // core/templates/dev/head/pages/header_js_libs.html this warning is
+      // expected and it's needed for constants loading.
       'Synchronous XMLHttpRequest on the main thread'
     ];
     checkConsoleErrorsExist(expectedErrors);
