5ce50ec1e2311f9f97bb526f2d7469f9141d930a
Sourav Badami <souravbadami@gmail.com>
2017-04-05 00:03:28 +0530

    Added test for SearchExplorationBackendApiService.js.

---
+++ core/templates/dev/head/domain/collection/SearchExplorationsBackendApiServiceSpec.js
@@ -0,0 +1,55 @@
+// Copyright 2015 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Unit tests for SearchExplorationsBackendApiService.
+ */
+
+describe('Exploration search backend API service', function() {
+  var SearchExplorationsBackendApiService = null;
+  var $rootScope = null;
+  var $scope = null;
+  var $httpBackend = null;
+
+  beforeEach(module('oppia'));
+  beforeEach(module('oppia', GLOBALS.TRANSLATOR_PROVIDER_FOR_TESTS));
+
+  beforeEach(inject(function($injector) {
+    SearchExplorationsBackendApiService = $injector.get(
+      'SearchExplorationsBackendApiService');
+    $rootScope = $injector.get('$rootScope');
+    $scope = $rootScope.$new();
+    $httpBackend = $injector.get('$httpBackend');
+  }));
+
+  afterEach(function() {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
+
+  it('should search for an exploration from the backend', function() {
+    var successHandler = jasmine.createSpy('success');
+    var failHandler = jasmine.createSpy('fail');
+
+    $httpBackend.expect(
+      'GET', '/exploration/metadata_search?q=three').respond(200);
+    SearchExplorationsBackendApiService.getExplorations('three').then(
+      successHandler, failHandler);
+    $httpBackend.flush();
+    $rootScope.$digest();
+
+    expect(successHandler).toHaveBeenCalled();
+    expect(failHandler).not.toHaveBeenCalled();
+  });
+});
