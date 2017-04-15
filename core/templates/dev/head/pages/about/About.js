739bd11920fe714a3e2f8e2c9db241f7e6d76ee9
Rohan Gulati <rohangulati@gmail.com>
2017-04-15 15:22:27 +0530

    Converted the list of names from a string to a list; Removed changes to the License text

--- core/templates/dev/head/pages/about/About.js
+++ core/templates/dev/head/pages/about/About.js
@@ -7,7 +7,7 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an 'AS-IS' BASIS,
+// distributed under the License is distributed on an "AS-IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -19,19 +19,23 @@
 oppia.controller('About', [
   '$scope', 'UrlInterpolationService',
   function($scope, UrlInterpolationService) {
-    $scope.listOfNames = ' Alex Kauffmann,' + ' Allison Barros,' +
-      ' Amy Latten,' + ' Brett Barros,' + ' Crystal Kwok,' +
-      ' Daniel Hernandez,' + ' Divya Siddarth,' +
-      ' Ilwon Yoon,' + '  Jennifer Chen,' + 
-      ' John Cox' + ' John Orr,' + 
-      ' Katie Berlent,' + ' Michael Wawszczak,' +
-      'Mike Gainer,' + ' Neil Fraser,' +
-      ' Noah Falstein,' + ' Nupur Jain,' +
-      ' Peter Norvig,' + ' Philip Guo,' +
-      ' Piotr Mitros,' + ' Rachel Chen,' + 
-      ' Rahim Nathwani,' + ' Robyn Choo,' +
-      ' Tricia Ngoon, ' + ' Vikrant Nanda,' + 
-      ' Vinamrata Singal,' + ' &' + ' Yarin Feigenbaum';
+    var listOfNamesToThank = ['Alex Kauffmann', 'Allison Barros',
+                              'Amy Latten', 'Brett Barros',
+                              'Crystal Kwok', 'Daniel Hernandez',
+                              'Divya Siddarth', 'Ilwon Yoon',
+                              'Jennifer Chen', 'John Cox',
+                              'John Orr', 'Katie Berlent',
+                              'Michael Wawszczak', 'Mike Gainer',
+                              'Neil Fraser', 'Noah Falstein',
+                              'Nupur Jain', 'Peter Norvig',
+                              'Philip Guo', 'Piotr Mitros',
+                              'Rachel Chen', 'Rahim Nathwani',
+                              'Robyn Choo', 'Tricia Ngoon',
+                              'Vikrant Nanda', 'Vinamrata Singal',
+                              'Yarin Feigenbaum'];
+    $scope.listOfNames = listOfNamesToThank
+      .slice(0, listOfNamesToThank.length - 1).join(', ') +
+      ' & ' + listOfNamesToThank[listOfNamesToThank.length - 1];
     $scope.getStaticImageUrl = UrlInterpolationService.getStaticImageUrl;
     $scope.aboutPageMascotImgUrl = UrlInterpolationService.getStaticImageUrl(
       '/general/about_page_mascot.png');
