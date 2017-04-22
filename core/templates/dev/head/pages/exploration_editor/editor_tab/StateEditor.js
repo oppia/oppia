02f6920e9e49f478724828974b6b1b1fd5363e3d
Shaz <shahebaz13@gmail.com>
2017-04-22 06:44:38 +0530

    Rename SVG and other class specific changes

--- core/templates/dev/head/pages/exploration_editor/editor_tab/StateEditor.js
+++ core/templates/dev/head/pages/exploration_editor/editor_tab/StateEditor.js
@@ -40,7 +40,7 @@ oppia.controller('StateEditor', [
     $scope.isInteractionShown = false;
 
     $scope.oppiaBlackImgUrl = UrlInterpolationService.getStaticImageUrl(
-      '/avatar/oppia_black_100px.svg');
+      '/avatar/oppia_avatar_100px.svg');
 
     $scope.isCurrentStateInitialState = function() {
       return (
