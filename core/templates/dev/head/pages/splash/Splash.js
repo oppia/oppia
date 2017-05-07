c5850ce9c99b31f95de441567aeb2108700686a6
Jared Silver <jared@jaredsilver.name>
2017-05-07 16:26:15 -0400

    Implement display of featured splash page explorations on new splash pages; modify LibraryGroupIndexHandler endpoint to hackily serve up featured splash page exploration summaries

--- core/templates/dev/head/pages/splash/Splash.js
+++ core/templates/dev/head/pages/splash/Splash.js
@@ -17,15 +17,13 @@
  */
 
 oppia.controller('Splash', [
-  '$scope', '$timeout', '$window', 'siteAnalyticsService',
+  '$http', '$scope', '$timeout', '$window', 'siteAnalyticsService',
   'UrlInterpolationService',
-  function($scope, $timeout, $window, siteAnalyticsService,
+  function($http, $scope, $timeout, $window, siteAnalyticsService,
     UrlInterpolationService) {
     $scope.getStaticImageUrl = UrlInterpolationService.getStaticImageUrl;
-    $scope.getStaticSubjectImageUrl = function(subjectName) {
-      return UrlInterpolationService.getStaticImageUrl('/subjects/' +
-        subjectName + '.svg');
-    };
+    $scope.aboutPageMascotImgUrl = UrlInterpolationService.getStaticImageUrl(
+      '/general/about_page_mascot.png');
 
     $scope.onRedirectToLogin = function(destinationUrl) {
       siteAnalyticsService.registerStartLoginEvent(
@@ -52,7 +50,13 @@ oppia.controller('Splash', [
       return false;
     };
 
-    $scope.aboutPageMascotImgUrl = UrlInterpolationService.getStaticImageUrl(
-      '/general/about_page_mascot.png');
+    $http.get('/librarygrouphandler', {
+      params: {
+        group_name: 'splash_page_featured'
+      }
+    }).success(
+    function(data) {
+      $scope.activityList = data.activity_list;
+    });
   }
 ]);
