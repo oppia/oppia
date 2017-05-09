f45ea587362b39107b64d2ec230ced778a5177d9
Jared Silver <jared@jaredsilver.name>
2017-05-09 00:13:05 -0400

    Testing New Splash Page Variants (#3396)
    * Modify pages controller to randomly redirect to one of five splash page variants; add skeletons for four new splash page variants
    
    * Interim commit for new splash nv1
    
    * Interim commit for new splash nv4
    
    * Interim commit for new splash nv2
    
    * Interim commit for new splash nv3
    
    * Finalize new splash page tests except for exploration cards
    
    * Implement display of featured splash page explorations on new splash pages; modify LibraryGroupIndexHandler endpoint to hackily serve up featured splash page exploration summaries
    
    * Make the linters happy again
    
    * Move 'splash_page_featured' to feconf, change some formatting, fix broken test to account for additional redirect on visiting root
    
    * Improve naming of constant for Splash.js GET request group_name param
    
    * Fixed indentation of button text, alphabetized css attributes
    
    * Reverting a change that may have resulted in SVGs not displaying on main splash page in production

--- feconf.py
+++ feconf.py
@@ -717,6 +717,8 @@ LIBRARY_CATEGORY_TOP_RATED_EXPLORATIONS = (
 # The i18n id for the header of the "Recently Published" category in the
 # library index page.
 LIBRARY_CATEGORY_RECENTLY_PUBLISHED = 'I18N_LIBRARY_GROUPS_RECENTLY_PUBLISHED'
+# group_name param for GET request in Splash.js
+LIBRARY_CATEGORY_SPLASH_PAGE_FEATURED = 'splash_page_featured'
 
 # The group name that appears at the end of the url for the recently published
 # page.
