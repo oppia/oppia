003e4f7c73274a4f4de8f73063924c233fb99058
Arunabh98 <arunabhghosh98@gmail.com>
2017-05-16 16:05:49 +0530

    Add tests for the new models.

--- core/storage/user/gae_models_test.py
+++ core/storage/user/gae_models_test.py
@@ -21,6 +21,58 @@ from core.tests import test_utils
 (user_models,) = models.Registry.import_models([models.NAMES.user])
 
 
+class IncompleteExplorationUserModelTest(test_utils.GenericTestBase):
+    """Tests for IncompleteExplorationUserModel class."""
+
+    USER_ID = 'user_id'
+    EXP_ID_0 = 'exp_id_0'
+    EXP_ID_1 = 'exp_id_1'
+
+    def setUp(self):
+        super(IncompleteExplorationUserModelTest, self).setUp()
+        user_models.IncompleteExplorationUserModel(
+            id='%s.%s' % (self.USER_ID, self.EXP_ID_0), user_id=self.USER_ID,
+            exploration_id=self.EXP_ID_0, version_last_played=1,
+            time_last_played_msec=1000, last_state_played='state_name').put()
+
+    def test_create_success(self):
+        user_models.IncompleteExplorationUserModel.create(
+            self.USER_ID, self.EXP_ID_1).put()
+        retrieved_object = user_models.IncompleteExplorationUserModel.get_by_id(
+            '%s.%s' % (self.USER_ID, self.EXP_ID_1))
+
+        self.assertEqual(retrieved_object.user_id, self.USER_ID)
+        self.assertEqual(retrieved_object.exploration_id, self.EXP_ID_1)
+
+    def test_get_success(self):
+        retrieved_object = user_models.IncompleteExplorationUserModel.get(
+            self.USER_ID, self.EXP_ID_0)
+
+        self.assertEqual(retrieved_object.user_id, self.USER_ID)
+        self.assertEqual(retrieved_object.exploration_id, self.EXP_ID_0)
+        self.assertEqual(retrieved_object.version_last_played, 1)
+        self.assertEqual(retrieved_object.time_last_played_msec, 1000)
+        self.assertEqual(retrieved_object.last_state_played, 'state_name')
+
+    def test_get_failure(self):
+        retrieved_object = user_models.IncompleteExplorationUserModel.get(
+            self.USER_ID, 'unknown_exp_id')
+
+        self.assertEqual(retrieved_object, None)
+
+    def test_update_last_played_information(self):
+        retrieved_object = user_models.IncompleteExplorationUserModel.get_by_id(
+            '%s.%s' % (self.USER_ID, self.EXP_ID_0))
+        retrieved_object.update_last_played_information(
+            2000, 2, 'new_state_name')
+
+        self.assertEqual(retrieved_object.user_id, self.USER_ID)
+        self.assertEqual(retrieved_object.exploration_id, self.EXP_ID_0)
+        self.assertEqual(retrieved_object.version_last_played, 2)
+        self.assertEqual(retrieved_object.time_last_played_msec, 2000)
+        self.assertEqual(retrieved_object.last_state_played, 'new_state_name')
+
+
 class ExplorationUserDataModelTest(test_utils.GenericTestBase):
     """Tests for the ExplorationUserDataModel class."""
 
