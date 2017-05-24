a2178317f95ffffa82514ec3171bb268a6893b65
Zachary Puller <zach@Zacharys-MacBook-Air.local>
2017-05-24 12:50:18 -0500

    verify distinct numeric ranges

--- extensions/interactions/NumericInput/validator.js
+++ extensions/interactions/NumericInput/validator.js
@@ -17,18 +17,97 @@
  */
 
 oppia.factory('NumericInputValidationService', [
-  'baseInteractionValidationService',
-  function(baseInteractionValidationService) {
+  'WARNING_TYPES', 'baseInteractionValidationService',
+  function(WARNING_TYPES, baseInteractionValidationService) {
     return {
       getCustomizationArgsWarnings: function(customizationArgs) {
-        // TODO(juansaba): Implement customization args validations.
         return [];
       },
       getAllWarnings: function(
           stateName, customizationArgs, answerGroups, defaultOutcome) {
-        return this.getCustomizationArgsWarnings(customizationArgs).concat(
+        var warningsList = [];
+
+        warningsList = warningsList.concat(
+          this.getCustomizationArgsWarnings(customizationArgs));
+
+        /*
+        Store an answer range for every rule, then check for overlapping
+        ranges. A range is an object of the form:
+        {
+          lb: float, lower bound
+          ub: float, upper bound
+          lbi: bool, is lower bound inclusive
+          ubi: bool, is upper bound inclusive
+        }
+        */
+        var overlaps = function(ra, rb) {
+          var a_extends_b = false;
+          if ((ra.ub > rb.lb) || (ra.ub >= rb.lb && ra.ubi && rb.lbi)) {
+            a_extends_b = true;
+          }
+          var b_extends_a = false;
+          if ((rb.ub > ra.lb) || (rb.ub >= ra.lb && rb.ubi && ra.lbi)) {
+            b_extends_a = true;
+          }
+          return a_extends_b && b_extends_a;
+        };
+
+        var ranges = [];
+        for (var i = 0; i < answerGroups.length; i++) {
+          var rules = answerGroups[i].rules;
+          for (var j = 0; j < rules.length; j++) {
+            var rule = rules[j];
+            var range = {};
+            switch (rule.type) {
+              case "Equals":
+                var x = rule.inputs.x;
+                range = { lb: x, ub: x, lbi: true, ubi: true };
+                break;
+              case "IsInclusivelyBetween":
+                var a = rule.inputs.a;
+                var b = rule.inputs.b;
+                range = { lb: a, ub: b, lbi: true, ubi: true };
+                break;
+              case "IsGreaterThan":
+                var x = rule.inputs.x;
+                range = { lb: x, ub: Infinity, lbi: false, ubi: false };
+                break;
+              case "IsGreaterThanOrEqualTo":
+                var x = rule.inputs.x;
+                range = { lb: x, ub: Infinity, lbi: true, ubi: false };
+                break;
+              case "IsLessThanOrEqualTo":
+                var x = rule.inputs.x;
+                range = { lb: -Infinity, ub: x, lbi: false, ubi: false };
+                break;
+              case "IsLessThanOrEqualTo":
+                var x = rule.inputs.x;
+                range = { lb: -Infinity, ub: x, lbi: false, ubi: true };
+                break;
+              case "IsWithinTolerance":
+                var x = rule.inputs.x;
+                var tol = rule.inputs.tol;
+                range = { lb: x-tol, ub: x+tol, lbi: true, ubi: true };
+                break;
+              default:
+            }
+            for (var k = 0; k < ranges.length; k++) {
+              if (overlaps(ranges[k], range)) {
+                warningsList.push({
+                  type: WARNING_TYPES.CRITICAL,
+                  message: 'Please ensure all numeric ranges are distinct.'
+                });
+              }
+            }
+            ranges.push(range);
+          }
+        }
+
+        warningsList = warningsList.concat(
           baseInteractionValidationService.getAllOutcomeWarnings(
             answerGroups, defaultOutcome, stateName));
+
+        return warningsList;
       }
     };
   }
