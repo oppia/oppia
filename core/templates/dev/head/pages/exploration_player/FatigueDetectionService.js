1e7ff650845460435c829f7fa568da6fb9b64980
Tony <tjiang11@jhu.edu>
2017-05-25 16:03:13 -0500

    Lint

--- core/templates/dev/head/pages/exploration_player/FatigueDetectionService.js
+++ core/templates/dev/head/pages/exploration_player/FatigueDetectionService.js
@@ -17,35 +17,37 @@
  */
 
 oppia.factory('fatigueDetectionService', 
-  ['$modal',
-  function($modal) {
-  var submissionTimes = [];
+  ['$modal', function($modal) {
+    var submissionTimes = [];
 
-  return {
-    addSubmission: function() {
-      submissionTimes.push(+new Date());
-      // 4 submissions in under 10 seconds
-      if (submissionTimes.length > 3) {
-        var timeThreeSubmissionsAgo = submissionTimes.shift();
-        var timeLastSubmission = submissionTimes[submissionTimes.length - 1];
-        if (timeLastSubmission - timeThreeSubmissionsAgo < 10000) {
-          $modal.open({
-            templateUrl: 'modals/takeBreak',
-            backdrop: 'static',
-            resolve: {},
-            controller: ['$scope', '$modalInstance', function($scope, $modalInstance) {
-              $scope.okay = function() {
-                $modalInstance.close('okay');
-              };
-            }]
-          });
-          return true;
+    return {
+      addSubmission: function() {
+        submissionTimes.push(+new Date());
+        // 4 submissions in under 10 seconds
+        if (submissionTimes.length > 3) {
+          var timeThreeSubmissionsAgo = submissionTimes.shift();
+          var timeLastSubmission = submissionTimes[submissionTimes.length - 1];
+          if (timeLastSubmission - timeThreeSubmissionsAgo < 10000) {
+            $modal.open({
+              templateUrl: 'modals/takeBreak',
+              backdrop: 'static',
+              resolve: {},
+              controller: [
+                '$scope', '$modalInstance',
+                function($scope, $modalInstance) {
+                $scope.okay = function() {
+                  $modalInstance.close('okay');
+                };
+              }]
+            });
+            return true;
+          }
         }
+        return false;
+      },
+      reset: function() {
+        submissionTimes = [];
       }
-      return false;
-    },
-    reset: function() {
-      submissionTimes = [];
     }
   }
-}]);
+]);
