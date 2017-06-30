23fd7862875ad1c7b273b56401db6a477823760f
Zachary Puller <zach.puller@gmail.com>
2017-06-30 12:54:33 -0500

    Fix #3446: formatting

--- extensions/interactions/TextInput/TextInputValidationService.js
+++ extensions/interactions/TextInput/TextInputValidationService.js
@@ -41,8 +41,8 @@ oppia.factory('TextInputValidationService', [
 
         var rows = customizationArgs.rows.value;
         if (isInt(rows)) {
-          var customArgSpecs =
-              INTERACTION_SPECS.TextInput.customization_arg_specs;
+          var textSpecs = INTERACTION_SPECS.TextInput;
+          var customArgSpecs = textSpecs.customization_arg_specs;
           var rowsSpecs = customArgSpecs[1];
           var MIN_ROWS = rowsSpecs.schema.validators[0].min_value;
           var MAX_ROWS = rowsSpecs.schema.validators[1].max_value;
