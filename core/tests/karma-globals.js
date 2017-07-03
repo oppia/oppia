30544f9b6e56185e36fc6b2d3b843610c30b1713
Zachary Puller <zach.puller@gmail.com>
2017-07-02 21:25:13 -0500

    Fix #3446: Formatting

--- core/tests/karma-globals.js
+++ core/tests/karma-globals.js
@@ -1,13 +1,13 @@
 // Copyright 2016 The Oppia Authors. All Rights Reserved.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the 'License');
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS-IS" BASIS,
+// distributed under the License is distributed on an 'AS-IS' BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -20,48 +20,48 @@ var GLOBALS = {
   INTERACTION_SPECS: {
     TextInput: {
       is_terminal: false,
-      description: "Allows learners to enter arbitrary text strings.",
-      display_mode: "inline",
-      name: "Text Input",
+      description: 'Allows learners to enter arbitrary text strings.',
+      display_mode: 'inline',
+      name: 'Text Input',
       is_linear: false,
       customization_arg_specs:  [
         {
-          default_value: "",
-          name: "placeholder",
+          default_value: '',
+          name: 'placeholder',
           schema: {
-            type: "unicode"
+            type: 'unicode'
           },
-          description: "Placeholder text (optional)"
+          description: 'Placeholder text (optional)'
         },
         {
           default_value: 1,
-          name: "rows",
+          name: 'rows',
           schema: {
             validators: [
               {
-                id: "is_at_least",
+                id: 'is_at_least',
                 min_value: 1
               },
               {
-                id: "is_at_most",
+                id: 'is_at_most',
                 max_value: 200
               }
             ],
-            type: "int"
+            type: 'int'
           },
-          description: "Height (in rows)"
+          description: 'Height (in rows)'
         }
       ],
-      id: "TextInput",
+      id: 'TextInput',
       default_outcome_heading: null,
       instructions: null,
       needs_summary: false,
       rule_descriptions: {
-        StartsWith: "starts with {{x|NormalizedString}}",
-        FuzzyMatches: "is similar to {{training_data|SetOfNormalizedString}}",
-        FuzzyEquals: "is equal to {{x|NormalizedString}}, misspelled by at most one character",
-        Contains: "contains {{x|NormalizedString}}",
-        CaseSensitiveEquals: "is equal to {{x|NormalizedString}}, taking case into account","Equals":"is equal to {{x|NormalizedString}}"
+        StartsWith: 'starts with {{x|NormalizedString}}',
+        FuzzyMatches: 'is similar to {{training_data|SetOfNormalizedString}}',
+        FuzzyEquals: 'is equal to {{x|NormalizedString}}, misspelled by at most one character',
+        Contains: 'contains {{x|NormalizedString}}',
+        CaseSensitiveEquals: 'is equal to {{x|NormalizedString}}, taking case into account','Equals':'is equal to {{x|NormalizedString}}'
       },
       is_string_classifier_trainable: true,
       is_trainable: true,
@@ -82,11 +82,11 @@ var GLOBALS = {
 
 /* This function overwrites the translationProvider for a dummy function
  * (customLoader). This is necessary to prevent the js test warnings about an
- * "unexpected GET request" when the translationProvider tries to load the
+ * 'unexpected GET request' when the translationProvider tries to load the
  * translation files.
  * More info in the angular-translate documentation:
  *   http://angular-translate.github.io/docs/#/guide
- * (see the "Unit Testing" section).
+ * (see the 'Unit Testing' section).
  */
 GLOBALS.TRANSLATOR_PROVIDER_FOR_TESTS = function($provide, $translateProvider) {
   $provide.factory('customLoader', function($q) {
