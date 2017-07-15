92b725d9b59fd94bc515bb41f333ba4dfbd1a98e
Tony <tjiang11@jhu.edu>
2017-07-15 17:42:25 -0400

    Remove test files

--- core/templates/dev/head/services/AssetsBackendApiServiceSpec.js
+++
@@ -1,8 +0,0 @@
-describe('Assets backend API service', function() {
-  var AssetsBackendApiService = null;
-
-  beforeEach(module('oppia'));
-  beforeEach(inject(function($injector) {
-    AssetsBackendApiService = $injector.get('AssetsBackendApiService');
-  }));
-})
