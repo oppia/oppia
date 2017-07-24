2c750bf45694c16731363f8a17a620b64912c871
1995YogeshSharma <yogesh.sharma@research.iiit.ac.in>
2017-07-24 15:22:31 +0530

    changed time in feedback_services backend test from 1 to 1.5

--- core/domain/feedback_services_test.py
+++ core/domain/feedback_services_test.py
@@ -426,7 +426,9 @@ class FeedbackThreadUnitTests(test_utils.GenericTestBase):
         feedback_services.get_thread_summaries(self.user_id, thread_ids)
         elapsed_time = time.time() - start
         print "Time for fetching all the thread summaries -", elapsed_time
-        self.assertLessEqual(elapsed_time, 1)
+        # Note (1995YogeshSharma): changing the below check from 1 to 1.5 as
+        #   this backend test fails on travis sometimes with 1 second.
+        self.assertLessEqual(elapsed_time, 1.5)
 
     def test_update_messages_read_by_the_user(self):
         feedback_services.create_thread(
