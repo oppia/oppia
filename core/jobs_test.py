e0836f202d19cf1ddaa54a0ad0c8184695b661e6
Brian Rodriguez <thatbrod@gmail.com>
2017-07-30 11:29:51 -0400

    Make compress_output_tests comprehensive of possibilities.

--- core/jobs_test.py
+++ core/jobs_test.py
@@ -17,6 +17,7 @@
 """Tests for long running jobs and continuous computations."""
 
 import ast
+from itertools import combinations
 
 from core import jobs
 from core import jobs_registry
@@ -363,53 +364,63 @@ class JobManagerUnitTests(test_utils.GenericTestBase):
         self.assertIsNone(DummyJobManager.get_error(job2_id))
 
     def test_compress_output_list_with_single_char_outputs(self):
-        sample_output_list = [1, 2, 3, 4, 5]
-        expected = ['1', '2', '3', '<TRUNCATED>']
+        input_list = [1, 2, 3, 4, 5]
+        expected_possibilities = [
+            ['1', '2', '3', '<TRUNCATED>'], ['1', '2', '4', '<TRUNCATED>'],
+            ['1', '2', '5', '<TRUNCATED>'], ['1', '3', '4', '<TRUNCATED>'],
+            ['1', '3', '5', '<TRUNCATED>'], ['1', '4', '5', '<TRUNCATED>'],
+            ['2', '3', '4', '<TRUNCATED>'], ['2', '3', '5', '<TRUNCATED>'],
+            ['2', '4', '5', '<TRUNCATED>'], ['3', '4', '5', '<TRUNCATED>'],
+        ]
         actual = jobs.BaseJobManager._compress_output_list(  # pylint: disable=protected-access
-            sample_output_list, test_only_max_output_len=3)
-        self.assertEquals(expected, sorted(actual))
+            input_list, test_only_max_output_len=3)
+        self.assertIn(sorted(actual), expected_possibilities)
 
     def test_compress_output_list_with_multi_char_outputs(self):
-        sample_output_list = ['abcd', 'efgh', 'ijkl']
-        expected = ['abcd', 'efgh', 'ij <TRUNCATED>']
+        input_list = ['abcd', 'efgh', 'ijkl']
+        expected_possibilities = [
+            ['abcd', 'efgh', 'ij <TRUNCATED>'],
+            ['abcd', 'ef <TRUNCATED>', 'ijkl'],
+            ['ab <TRUNCATED>', 'efgh', 'ijkl'],
+        ]
         actual = jobs.BaseJobManager._compress_output_list(  # pylint: disable=protected-access
-            sample_output_list, test_only_max_output_len=10)
-        self.assertEquals(expected, sorted(actual))
+            input_list, test_only_max_output_len=10)
+        self.assertIn(sorted(actual), expected_possibilities)
 
     def test_compress_output_list_with_zero_max_output_len(self):
-        sample_output_list = [1, 2, 3]
+        input_list = [1, 2, 3]
         expected = ['<TRUNCATED>']
         actual = jobs.BaseJobManager._compress_output_list(  # pylint: disable=protected-access
-            sample_output_list, test_only_max_output_len=0)
+            input_list, test_only_max_output_len=0)
         self.assertEquals(expected, sorted(actual))
 
     def test_compress_output_list_with_exact_max_output_len(self):
-        sample_output_list = ['abc']
+        input_list = ['abc']
         expected = ['abc']
         actual = jobs.BaseJobManager._compress_output_list(  # pylint: disable=protected-access
-            sample_output_list, test_only_max_output_len=3)
+            input_list, test_only_max_output_len=3)
         self.assertEquals(expected, sorted(actual))
 
     def test_compress_output_list_with_empty_outputs(self):
-        sample_output_list = []
+        input_list = []
         expected = []
-        actual = jobs.BaseJobManager._compress_output_list(sample_output_list)  # pylint: disable=protected-access
+        actual = jobs.BaseJobManager._compress_output_list(input_list)  # pylint: disable=protected-access
         self.assertEquals(expected, sorted(actual))
 
     def test_compress_output_list_with_duplicate_outputs(self):
-        sample_output_list = ['bar', 'foo'] * 3
+        input_list = ['bar', 'foo'] * 3
         expected = ['(3x) bar', '(3x) foo']
         actual = jobs.BaseJobManager._compress_output_list(  # pylint: disable=protected-access
-            sample_output_list,
+            input_list,
             # Ensure truncation doesn't happen in this test.
             test_only_max_output_len=sum(len(s) for s in expected))
         self.assertEquals(expected, sorted(actual))
 
     def test_compress_output_list_with_truncated_duplicate_outputs(self):
-        sample_output_list = ['supercalifragilisticexpialidocious'] * 3
+        input_list = ['supercalifragilisticexpialidocious'] * 3
         expected = ['(3x) super <TRUNCATED>']
         actual = jobs.BaseJobManager._compress_output_list(  # pylint: disable=protected-access
-            sample_output_list, test_only_max_output_len=10)
+            input_list, test_only_max_output_len=10)
         self.assertEquals(expected, sorted(actual))
 
 
