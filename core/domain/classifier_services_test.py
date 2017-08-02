c7b8424f15cdd0a6f7325809f7d9ed4122039eef
Pranav Siddharth <pranavsid98@gmail.com>
2017-08-02 15:31:02 +0530

    fixes

--- core/domain/classifier_services_test.py
+++ core/domain/classifier_services_test.py
@@ -75,21 +75,21 @@ class ClassifierServicesTests(test_utils.GenericTestBase):
         return (answer_group.get_classifier_rule_index() == rule_spec_index and
                 predict_counter.times_called == 1)
 
-    # def test_string_classifier_classification(self):
-    #     """All these responses trigger the string classifier."""
-    #
-    #     with self.swap(feconf, 'ENABLE_STRING_CLASSIFIER', True):
-    #         self.assertTrue(
-    #             self._is_string_classifier_called(
-    #                 'it\'s a permutation of 3 elements'))
-    #         self.assertTrue(
-    #             self._is_string_classifier_called(
-    #                 'There are 3 options for the first ball, and 2 for the '
-    #                 'remaining two. So 3*2=6.'))
-    #         self.assertTrue(
-    #             self._is_string_classifier_called('abc acb bac bca cbb cba'))
-    #         self.assertTrue(
-    #             self._is_string_classifier_called('dunno, just guessed'))
+    def test_string_classifier_classification(self):
+        """All these responses trigger the string classifier."""
+
+        with self.swap(feconf, 'ENABLE_STRING_CLASSIFIER', True):
+            self.assertTrue(
+                self._is_string_classifier_called(
+                    'it\'s a permutation of 3 elements'))
+            self.assertTrue(
+                self._is_string_classifier_called(
+                    'There are 3 options for the first ball, and 2 for the '
+                    'remaining two. So 3*2=6.'))
+            self.assertTrue(
+                self._is_string_classifier_called('abc acb bac bca cbb cba'))
+            self.assertTrue(
+                self._is_string_classifier_called('dunno, just guessed'))
 
     def test_creation_of_jobs_and_mappings(self):
         """Test the create_classifier_training_jobs method and
