26e7b107b3e7c68f582c0b10438e4cb27c2a11a0
Ray T <thisraymond@gmail.com>
2017-08-03 10:44:20 +0000

    Updated comment as suggested

--- core/templates/dev/head/i18n.js
+++ core/templates/dev/head/i18n.js
@@ -91,10 +91,12 @@ oppia.controller('I18nFooter', [
           site_language_code: $scope.currentLanguageCode
         });
       } else {
-        // This is a workaround for the library page as the base url
-        // changes the default cookie path.
-        // This will always set the cookie value manually for all non-logged in
-        // users in case we change the base url on other pages of the site.
+        // $translate.use() sets a cookie for the translation language, but does
+        // so using the page's base URL as the cookie path. However, the base
+        // URL is modified in pages like /library, thus causing the cookie path
+        // to change; in such cases, the user's preferences are not picked up by
+        // other pages. To avoid this, we manually set the cookie using the '/'
+        // path each time a non-logged-in user changes their site language.
         $cookies.put(
           'NG_TRANSLATE_LANG_KEY',
           '"' + $scope.currentLanguageCode + '"', {path: '/'});
