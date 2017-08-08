27f65833097a31507d03a569a9f388ac41728cd1
Kevin Thomas <kevintab@tutanota.com>
2017-08-09 00:32:14 +0530

    karma tests

--- core/templates/dev/head/pages/exploration_editor/editor_tab/SolutionValidityServiceSpec.js
+++ core/templates/dev/head/pages/exploration_editor/editor_tab/SolutionValidityServiceSpec.js
@@ -13,186 +13,35 @@
 // limitations under the License.
 
 /**
- * @fileoverview Unit tests for State Validity Service.
+ * @fileoverview Unit tests for Solution Validity Service.
  */
 
-describe('State Validity Service', function() {
-  describe('StateValidityService', function() {
+describe('Solution Validity Service', function() {
+  describe('SolutionValidityService', function() {
     beforeEach(function() {
       module('oppia');
-      // Set a global value for INTERACTION_SPECS that will be used by all the
-      // descendant dependencies.
-      module(function($provide) {
-        $provide.constant('INTERACTION_SPECS', {
-          TextInput: {
-            display_mode: 'inline',
-            is_terminal: false
-          },
-          TerminalInteraction: {
-            display_mode: 'inline',
-            is_terminal: true
-          }
-        });
-      });
     });
 
-    var scope, ecs, cls, ess, siis, scas, idc, sof, svs, IS;
-    var $httpBackend;
-    var mockExplorationData;
-
-    beforeEach(function() {
-      mockExplorationData = {
-        explorationId: 0,
-        autosaveChangeList: function() {}
-      };
-      module(function($provide) {
-        $provide.value('explorationData', mockExplorationData);
-      });
-      spyOn(mockExplorationData, 'autosaveChangeList');
-    });
+    var scope, svs;
 
     beforeEach(inject(function($rootScope, $controller, $injector) {
       scope = $rootScope.$new();
-      ecs = $injector.get('editorContextService');
-      cls = $injector.get('changeListService');
-      ess = $injector.get('explorationStatesService');
-      siis = $injector.get('stateInteractionIdService');
-      scas = $injector.get('stateCustomizationArgsService');
-      idc = $injector.get('interactionDetailsCache');
-      sof = $injector.get('SolutionObjectFactory');
-      svs = $injector.get('SolutionVerificationService');
-      IS = $injector.get('INTERACTION_SPECS');
-      $httpBackend = $injector.get('$httpBackend');
-      scope.stateInteractionIdService = siis;
-      scope.stateCustomizationArgsService = scas;
-      scope.interactionDetailsCache = idc;
+      svs = $injector.get('SolutionValidityService');
 
-      ess.init({
-        'First State': {
-          content: {
-            html: 'First State Content',
-            audio_translations: {}
-          },
-          interaction: {
-            id: 'TextInput',
-            answer_groups: [{
-              correct: false,
-              outcome: {
-                dest: 'End State',
-                feedback: [],
-                param_changes: []
-              },
-              rule_specs: [{
-                inputs: {x: 'abc'},
-                rule_type: 'Contains'
-              }]
-            }],
-            default_outcome: {
-              dest: 'default',
-              feedback: [],
-              param_changes: []
-            },
-            fallbacks: [],
-            hints: [{
-              hint_text: 'one'
-            }, {
-              hint_text: 'two'
-            }]
-          },
-          param_changes: []
-        },
-        'End State': {
-          content: {
-            html: '',
-            audio_translations: {}
-          },
-          interaction: {
-            id: 'TextInput',
-            answer_groups: [{
-              rule_specs: [],
-              outcome: {},
-              correct: false
-            }],
-            default_outcome: {
-              dest: 'default',
-              feedback: [],
-              param_changes: []
-            },
-            fallbacks: [],
-            hints: []
-          },
-          param_changes: []
-        }
-      });
+      it('should store validity of the solution correctly',
+        function () {
+          // Initialize SolutionValidityService.
+          svs.init();
 
-      var stateEditorCtrl = $controller('StateEditor', {
-        $scope: scope,
-        editorContextService: ecs,
-        changeListService: cls,
-        explorationStatesService: ess,
-        editabilityService: {
-          isEditable: function() {
-            return true;
-          }
-        },
-        INTERACTION_SPECS: IS
-      });
+          svs.updateValidity('State 1', true);
+          expect(svs.isSolutionValid('State 1')).toBe(true);
 
-      var interactionCtrl = $controller('StateInteraction', {
-        $scope: scope,
-        editorContextService: ecs,
-        changeListService: cls,
-        explorationStatesService: ess,
-        editabilityService: {
-          isEditable: function() {
-            return true;
-          }
-        },
-        stateInteractionIdService: siis,
-        stateCustomizationArgsService: scas,
-        interactionDetailsCache: idc,
-        INTERACTION_SPECS: IS
-      });
-    }));
+          svs.deleteSolutionValidity('State 1');
+          expect(Object.keys(svs.getAllValidities()).length === 0).toBe(true);
 
-    it('should store the correct validity of a solution',
-      function() {
-        ecs.setActiveStateName('First State');
-        scope.initStateEditor();
-
-        var state = ess.getState('First State');
-        siis.init(
-          'First State', state.interaction.id, state.interaction, 'widget_id');
-        scas.init(
-          'First State', state.interaction.customizationArgs,
-          state.interaction, 'widget_customization_args');
-
-        siis.displayed = 'TextInput';
-        scope.onCustomizationModalSavePostHook();
-
-        ess.saveSolution('First State', sof.createNew(false, 'abc', 'nothing'));
-
-        svs.verifySolution(0, state,
-          ess.getState('First State').interaction.solution.correctAnswer,
-          function() {
-            ess.updateSolutionValidity('First State', true);
-            expect(ess.isSolutionValid('First State')).toEqual(true);
-          },
-          function() {
-            ess.updateSolutionValidity('First State', false);
-          });
-
-
-        svs.verifySolution(0, state,
-          ess.getState('First State').interaction.solution.correctAnswer,
-          function() {
-            ess.updateSolutionValidity('First State', true);
-          },
-          function() {
-            ess.updateSolutionValidity('First State', false);
-            expect(ess.isSolutionValid('First State'), false);
-          });
-      }
-    );
-  });
+          svs.updateValidity('State 1', false);
+          expect(svs.isSolutionValid('State 1')).toBe(false);
+        });
+    }))
+  })
 });
