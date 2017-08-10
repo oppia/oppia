61e4eef7bcedef6f317f800b122af21c3bf5dfac
MAKOSCAFEE <barnabasmakonda@gmail.com>
2017-08-10 11:30:38 +0300

    address Sean review comments

--- core/templates/dev/head/pages/library/SearchResultsDirective.js
+++ core/templates/dev/head/pages/library/SearchResultsDirective.js
@@ -26,7 +26,7 @@ oppia.directive('searchResults', [
         function($scope, $rootScope, $timeout, $window, siteAnalyticsService) {
           $rootScope.loadingMessage = 'Loading';
           $scope.someResultsExist = true;
-          $scope.hasFullyRegistered = GLOBALS.hasFullyRegistered;
+          $scope.hasFullyRegistered = Boolean(GLOBALS.username);
 
           // Called when the first batch of search results is retrieved from the
           // server.
