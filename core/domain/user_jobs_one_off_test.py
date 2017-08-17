6efaab3c6aea9fa45368db916abd1a0c28402701
Arunabh <arunabhghosh98@gmail.com>
2017-08-18 05:24:12 +0530

    Assign the default dashboard to all the users of the site. (#3760)
    * Define dashboard.
    
    * Fix the test.
    
    * Test working.
    
    * address review comments.

--- core/domain/user_jobs_one_off_test.py
+++ core/domain/user_jobs_one_off_test.py
@@ -20,6 +20,7 @@ import ast
 import datetime
 import re
 
+from constants import constants
 from core.domain import collection_domain
 from core.domain import collection_services
 from core.domain import config_services
@@ -146,6 +147,54 @@ class UserContributionsOneOffJobTests(test_utils.GenericTestBase):
         self.assertEqual(
             user_d_contributions_model.created_exploration_ids,
             [self.EXP_ID_2])
+
+
+class UserDefaultDashboardOneOffJobTests(test_utils.GenericTestBase):
+    """Tests for the one-off username length distribution job."""
+
+    CREATOR_USER_EMAIL = 'creator@example.com'
+    CREATOR_USER_USERNAME = 'creator'
+    LEARNER_USER_EMAIL = 'learner@example.com'
+    LEARNER_USER_USERNAME = 'learner'
+
+    EXP_ID_1 = 'exp_id_1'
+    EXP_ID_2 = 'exp_id_2'
+
+    def _run_one_off_job(self):
+        """Runs the one-off MapReduce job."""
+        job_id = (
+            user_jobs_one_off.UserDefaultDashboardOneOffJob.create_new())
+        user_jobs_one_off.UserDefaultDashboardOneOffJob.enqueue(job_id)
+        self.assertEqual(
+            self.count_jobs_in_taskqueue(
+                taskqueue_services.QUEUE_NAME_ONE_OFF_JOBS), 1)
+        self.process_and_flush_pending_tasks()
+
+    def test_default_dashboard(self):
+        """Tests whether the one off jobs assigns the correct dashboard
+        to the user.
+        """
+        self.signup(self.CREATOR_USER_EMAIL, self.CREATOR_USER_USERNAME)
+        creator_user_id = self.get_user_id_from_email(
+            self.CREATOR_USER_EMAIL)
+        self.signup(self.LEARNER_USER_EMAIL, self.LEARNER_USER_USERNAME)
+        learner_user_id = self.get_user_id_from_email(
+            self.LEARNER_USER_EMAIL)
+
+        self.save_new_valid_exploration(
+            self.EXP_ID_1, creator_user_id, end_state_name='End')
+
+        self._run_one_off_job()
+
+        creator_settings = user_services.get_user_settings(creator_user_id)
+        learner_settings = user_services.get_user_settings(learner_user_id)
+
+        self.assertEqual(
+            creator_settings.default_dashboard,
+            constants.DASHBOARD_TYPE_CREATOR)
+        self.assertEqual(
+            learner_settings.default_dashboard,
+            constants.DASHBOARD_TYPE_LEARNER)
 
 
 class UsernameLengthDistributionOneOffJobTests(test_utils.GenericTestBase):
