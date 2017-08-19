3532dd15406328ea19d8fc36d989b3d1d3dc11f4
Shubha Gupta <shubha1593@gmail.com>
2017-08-19 12:40:22 +0530

    Make time users locale instead of specific locale

--- core/templates/dev/head/app.js
+++ core/templates/dev/head/app.js
@@ -471,7 +471,7 @@ oppia.factory('oppiaDatetimeFormatter', ['$filter', function($filter) {
       var date = new Date(millisSinceEpoch);
       if (date.toLocaleDateString() === new Date().toLocaleDateString()) {
         // The replace function removes 'seconds' from the time returned.
-        return date.toLocaleTimeString('en-US',
+        return date.toLocaleTimeString([],
           {hour: 'numeric', minute: 'numeric', hour12: true});
       } else if (date.getFullYear() === new Date().getFullYear()) {
         return $filter('date')(date, 'MMM d');
