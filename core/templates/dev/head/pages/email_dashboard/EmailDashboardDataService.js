bf42d0b09d6d7972107f4085e39bbc5f01b98abe
Ajo John <ajojohn555@gmail.com>
2017-08-21 14:23:25 +0530

    Change the file name

--- core/templates/dev/head/pages/email_dashboard/EmailDashboardDataService.js
+++ core/templates/dev/head/pages/email_dashboard/EmailDashboardDataService.js
@@ -11,10 +11,10 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 /**
  * @fileoverview Services for oppia email dashboard page.
- */
+*/
+ 
 oppia.factory('EmailDashboardDataService', [
   '$http', '$q', function($http, $q) {
     var QUERY_DATA_URL = '/emaildashboarddatahandler';
