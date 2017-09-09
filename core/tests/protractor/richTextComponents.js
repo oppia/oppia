5a68dfef8bf7922e903d52ce6f38d2d76ead15cc
Pranav Siddharth <pranavsid98@gmail.com>
2017-09-09 19:51:17 +0530

    add todo doc strings

--- core/tests/protractor/richTextComponents.js
+++ core/tests/protractor/richTextComponents.js
@@ -84,7 +84,8 @@ describe('rich-text components', function() {
       // https://code.google.com/p/google-cast-sdk/issues/detail?id=309 is fixed
       'cast_sender.js - Failed to load resource: net::ERR_FAILED',
       'Uncaught ReferenceError: ytcfg is not defined',
-      // TODO (@pranavsid98) Remove when we resolve the reason for this error.
+      // TODO (@pranavsid98) This error is caused by the upgrade from Chrome 60
+      // to Chrome 61. Chrome version at time of recording this is 61.0.3163.
       'chrome-extension://invalid/ - Failed to load resource: net::ERR_FAILED',
     ]);
   });
--- core/tests/protractor/staticPagesTour.js
+++ core/tests/protractor/staticPagesTour.js
@@ -62,7 +62,8 @@ describe('Oppia static pages tour', function() {
       // https://code.google.com/p/google-cast-sdk/issues/detail?id=309 is fixed
       'cast_sender.js - Failed to load resource: net::ERR_FAILED',
       'Uncaught ReferenceError: ytcfg is not defined',
-      // TODO (@pranavsid98) Remove when we resolve the reason for this error.
+      // TODO (@pranavsid98) This error is caused by the upgrade from Chrome 60
+      // to Chrome 61. Chrome version at time of recording this is 61.0.3163.
       'chrome-extension://invalid/ - Failed to load resource: net::ERR_FAILED',
     ]);
   });
