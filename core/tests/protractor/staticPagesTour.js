f0305b38ba66c93d2f0a5792654e617973771737
Pranav Siddharth <pranavsid98@gmail.com>
2017-09-09 05:45:21 +0530

    another fix

--- core/tests/protractor/staticPagesTour.js
+++ core/tests/protractor/staticPagesTour.js
@@ -61,6 +61,7 @@ describe('Oppia static pages tour', function() {
       // TODO (Jacob) Remove when
       // https://code.google.com/p/google-cast-sdk/issues/detail?id=309 is fixed
       'cast_sender.js - Failed to load resource: net::ERR_FAILED',
+      'chrome-extension://invalid/ - Failed to load resource: net::ERR_FAILED',
       'Uncaught ReferenceError: ytcfg is not defined'
     ]);
   });
