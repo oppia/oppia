8d8962f8ad607170d28c8efc1f606032636fbf00
giritheja <s.giritheja@gmail.com>
2017-09-17 08:19:15 -0700

    addressed review comments

--- core/domain/exp_domain.py
+++ core/domain/exp_domain.py
@@ -1240,10 +1240,10 @@ class State(object):
         """Validates various properties of the State.
 
         Args:
-            exp_param_specs_dict: dict or none. A dict of specified parameters
+            exp_param_specs_dict: dict or None. A dict of specified parameters
                 used in this exploration. Keys are parameter names and values
                 are ParamSpec value objects with an object type
-                property(obj_type). It is none if the state belongs to a
+                property(obj_type). It is None if the state belongs to a
                 question.
             allow_null_interaction. bool. Whether this state's interaction is
                 allowed to be unspecified.
