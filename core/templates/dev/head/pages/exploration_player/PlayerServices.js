c02e63e3770042ae031a302a9dc27729b7940a1e
Brian Rodriguez <thatbrod@gmail.com>
2017-09-18 12:52:31 -0400

    Use forEach instead of for in, fixing bug which looped through array indicies instead of values.

--- core/templates/dev/head/pages/exploration_player/PlayerServices.js
+++ core/templates/dev/head/pages/exploration_player/PlayerServices.js
@@ -131,12 +131,12 @@ oppia.factory('oppiaPlayerService', [
     // manual parameter changes (in editor preview mode).
     var initParams = function(manualParamChanges) {
       var baseParams = {};
-      for (var paramName in exploration.paramSpecs.getParamNames()) {
+      exploration.paramSpecs.getParamNames().forEach(function(paramName) {
         // TODO(sll): This assumes all parameters are of type
         // UnicodeString. We should generalize this to other default values
         // for different types of parameters.
         baseParams[paramName] = '';
-      }
+      });
 
       var startingParams = makeParams(
         baseParams,
