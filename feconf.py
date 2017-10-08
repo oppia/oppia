48608125fc616b7851fd2275b05c7f859e4de52f
Sean Lip <sean@seanlip.org>
2017-10-08 01:48:57 -0700

    Update the Oppia blog link; redo text on the Teach with Oppia page.

--- feconf.py
+++ feconf.py
@@ -585,7 +585,6 @@ USERNAME_CHECK_DATA_URL = '/usernamehandler/data'
 
 NAV_MODE_ABOUT = 'about'
 NAV_MODE_GET_STARTED = 'get_started'
-NAV_MODE_BLOG = 'blog'
 NAV_MODE_COLLECTION = 'collection'
 NAV_MODE_CONTACT = 'contact'
 NAV_MODE_CREATE = 'create'
@@ -727,8 +726,6 @@ ABOUT_PAGE_DESCRIPTION = (
     'scenarios for others.')
 GET_STARTED_PAGE_DESCRIPTION = (
     'Learn how to get started using Oppia.')
-BLOG_PAGE_DESCRIPTION = (
-    'Keep up to date with Oppia news and updates via our blog.')
 CONTACT_PAGE_DESCRIPTION = (
     'Contact the Oppia team, submit feedback, and learn how to get involved '
     'with the Oppia project.')
