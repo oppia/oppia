a346f4dc0357afd90da1f94350dce11e013360aa
Sean Lip <sean@seanlip.org>
2017-10-09 00:17:53 -0700

    Clean up deploy scripts and move common functions to common.py.

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -12,15 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Deployment script for Oppia.
+"""This is a deployment script for Oppia that should only be used by release
+coordinators.
 
-USE THIS SCRIPT AT YOUR OWN RISK! A safe option is to modify app.yaml manually
-and run the 'appcfg.py update' command.
-
-This script performs a deployment of Oppia to a Google App Engine appspot
-instance. It creates a build with unnecessary files removed, and saves a copy
-of the uploaded files to a deployment folder in the parent directory of the
-oppia/ folder. It then pushes this build to the production server.
+The script creates a build with unnecessary files removed, and saves a copy of
+the uploaded files to a deployment folder in the parent directory of the oppia/
+folder. It then pushes this build to the production server.
 
 IMPORTANT NOTES:
 1.  You will need to first create a folder called ../deploy_data/[APP_NAME],
@@ -67,9 +64,14 @@ _PARSER = argparse.ArgumentParser()
 _PARSER.add_argument(
     '--app_name', help='name of the app to deploy to', type=str)
 
+APP_NAME_OPPIASERVER = 'oppiaserver'
+APP_NAME_OPPIATESTSERVER = 'oppiatestserver'
+
 PARSED_ARGS = _PARSER.parse_args()
 if PARSED_ARGS.app_name:
     APP_NAME = PARSED_ARGS.app_name
+    if APP_NAME not in [APP_NAME_OPPIASERVER, APP_NAME_OPPIATESTSERVER]:
+        raise Exception('Invalid app name: %s' % APP_NAME)
 else:
     raise Exception('No app name specified.')
 
@@ -94,6 +96,27 @@ IMAGE_DIRS = ['avatar', 'general', 'sidebar', 'logo']
 # Denotes length for cache slug used in production mode. It consists of
 # lowercase alphanumeric characters.
 CACHE_SLUG_PROD_LENGTH = 6
+
+
+def _ensure_release_scripts_folder_exists_and_is_up_to_date():
+    """Checks that the release-scripts folder exists and is up-to-date."""
+    parent_dirpath = os.path.join(os.getcwd(), os.pardir)
+    release_scripts_dirpath = os.path.join(parent_dirpath, 'release-scripts')
+
+    # If the release-scripts folder does not exist, set it up.
+    if not os.path.isdir(release_scripts_dirpath):
+        with common.CD(parent_dirpath):
+            subprocess.call([
+                'git', 'clone', 'https://github.com/oppia/release-scripts'])
+
+    with common.CD(release_scripts_dirpath):
+        common.verify_local_repo_is_clean()
+        common.verify_current_branch_name('master')
+
+        # Update the local repo.
+        remote_alias = common.get_remote_alias(
+            'https://github.com/oppia/release-scripts')
+        subprocess.call(['git', 'pull', remote_alias])
 
 
 def preprocess_release():
@@ -149,8 +172,9 @@ def preprocess_release():
 
 
 def _execute_deployment():
-    # Check that the current directory is correct.
+    # Do prerequisite checks.
     common.require_cwd_to_be_oppia()
+    _ensure_release_scripts_folder_exists_and_is_up_to_date()
 
     current_git_revision = subprocess.check_output(
         ['git', 'rev-parse', 'HEAD']).strip()
@@ -200,6 +224,16 @@ def _execute_deployment():
                     current_git_revision))
 
         print 'Returning to oppia/ root directory.'
+
+    # If this is a test server deployment, open the library page (for sanity
+    # checking) and the GAE error logs.
+    if APP_NAME == APP_NAME_OPPIATESTSERVER:
+        common.open_new_tab_in_browser(
+            'https://console.cloud.google.com/logs/viewer?'
+            'project=%s&key1=default&minLogLevel=500'
+            % APP_NAME_OPPIATESTSERVER)
+        common.open_new_tab_in_browser(
+            'https://%s.appspot.com/library' % APP_NAME_OPPIATESTSERVER)
 
     print 'Done!'
 
