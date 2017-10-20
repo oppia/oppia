69d2f4c4a6b4e86d78bc2c60ac83793a64519e11
Pranav Siddharth <pranavsid98@gmail.com>
2017-10-20 08:36:10 +0530

    quick fix

--- core/templates/dev/head/app.js
+++ core/templates/dev/head/app.js
@@ -44,7 +44,7 @@ oppia.constant('OBJECT_EDITOR_URL_PREFIX', '/object_editor_template/');
 // NOTE TO DEVELOPERS: This should be synchronized with the value in feconf.
 oppia.constant('ENABLE_ML_CLASSIFIERS', false);
 // NOTE TO DEVELOPERS: This should be synchronized with the value in feconf.
-oppia.constant('ENABLE_NEW_STATS_FRAMEWORK', true);
+oppia.constant('ENABLE_NEW_STATS_FRAMEWORK', false);
 // Feature still in development.
 oppia.constant('INFO_MESSAGE_SOLUTION_IS_INVALID',
   'The current solution does not lead to another card.');
