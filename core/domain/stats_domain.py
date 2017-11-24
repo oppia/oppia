0964e66003236a975305a423598780353e8358da
Brian Rodriguez <thatbrod@gmail.com>
2017-11-24 13:29:49 -0500

    Change docstring to highlight intent as an output.

--- core/domain/stats_domain.py
+++ core/domain/stats_domain.py
@@ -559,7 +559,7 @@ class AnswerCalculationOutput(object):
 
 
 class AnswerFrequencyList(AnswerCalculationOutput):
-    """Domain object that represents a list of AnswerOccurrences."""
+    """Domain object that represents an output list of AnswerOccurrences."""
     def __init__(self, answer_occurrences=None):
         """Initialize domain object for answer frequency list for a given list
         of AnswerOccurrence objects (default is empty list).
