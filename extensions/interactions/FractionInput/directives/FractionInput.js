2de0f12d869103424bad4250a6b953b7c752cad4
Anthony-Alridge <anthonyalridge270@gmail.com>
2017-11-29 14:58:17 +0000

    Refactor files to match new file layout for extensions

--- extensions/interactions/FractionInput/directives/FractionInput.js
+++ extensions/interactions/FractionInput/directives/FractionInput.js
@@ -12,105 +12,131 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-oppia.directive('oppiaInteractiveFractionInput', [function() {
-  return {
-    restrict: 'E',
-    scope: {
-      onSubmit: '&'
-    },
-    templateUrl: 'interaction/FractionInput',
-    controller: [
-      '$scope', '$attrs', 'FocusManagerService',
-      'fractionInputRulesService',
-      'FractionObjectFactory', 'FRACTION_PARSING_ERRORS',
-      function($scope, $attrs, FocusManagerService,
-        fractionInputRulesService,
-        FractionObjectFactory, FRACTION_PARSING_ERRORS) {
-        $scope.answer = '';
-        $scope.labelForFocusTarget = $attrs.labelForFocusTarget || null;
-        var requireSimplestForm =
-          $attrs.requireSimplestFormWithValue === 'true';
-        var errorMessage = '';
-        var FORM_ERROR_TYPE = 'FRACTION_FORMAT_ERROR';
-        $scope.FRACTION_INPUT_FORM_SCHEMA = {
-          type: 'unicode',
-          ui_config: {}
-        };
+oppia.directive('oppiaInteractiveFractionInput', [
+  'UrlInterpolationService',
+  function(UrlInterpolationService) {
+    return {
+      restrict: 'E',
+      scope: {
+        onSubmit: '&'
+      },
+      templateUrl: UrlInterpolationService.getExtensionResourceUrl(
+        '/interactions/FractionInput/directives/' +
+        'fraction_input_interaction_directive.html'),
+      controller: [
+        '$scope', '$attrs', 'FocusManagerService',
+        'fractionInputRulesService',
+        'FractionObjectFactory', 'FRACTION_PARSING_ERRORS',
+        function($scope, $attrs, FocusManagerService,
+          fractionInputRulesService,
+          FractionObjectFactory, FRACTION_PARSING_ERRORS) {
+          $scope.answer = '';
+          $scope.labelForFocusTarget = $attrs.labelForFocusTarget || null;
+          var requireSimplestForm =
+            $attrs.requireSimplestFormWithValue === 'true';
+          var errorMessage = '';
+          var FORM_ERROR_TYPE = 'FRACTION_FORMAT_ERROR';
+          $scope.FRACTION_INPUT_FORM_SCHEMA = {
+            type: 'unicode',
+            ui_config: {}
+          };
 
-        $scope.getWarningText = function() {
-          return errorMessage;
-        };
+          $scope.getWarningText = function() {
+            return errorMessage;
+          };
 
-        /**
-         * Disables the input box if the data entered is not a valid prefix
-         * for a fraction.
-         * Examples of valid prefixes:
-         * -- 1
-         * -- 1 2
-         * -- 1 2/
-         * -- 2/
-         * -- 1 2/3
-         */
-        $scope.$watch('answer', function(newValue) {
-          var INVALID_CHARS_REGEX = /[^\d\s\/-]/g;
-          // Accepts incomplete fraction inputs (see examples above except last).
-          var PARTIAL_FRACTION_REGEX =
-            /^\s*(-?\s*((\d*\s*\d+\s*\/?\s*)|\d+)\s*)?$/;
-          // Accepts complete fraction inputs.
-          var FRACTION_REGEX =
-            /^\s*-?\s*((\d*\s*\d+\s*\/\s*\d+)|\d+)\s*$/;
-          if (INVALID_CHARS_REGEX.test(newValue)) {
-            errorMessage = FRACTION_PARSING_ERRORS.INVALID_CHARS;
-            $scope.FractionInputForm.answer.$setValidity(
-              FORM_ERROR_TYPE, false);
-          } else if (!(FRACTION_REGEX.test(newValue) ||
-              PARTIAL_FRACTION_REGEX.test(newValue))) {
-            errorMessage = FRACTION_PARSING_ERRORS.INVALID_FORMAT;
-            $scope.FractionInputForm.answer.$setValidity(
-              FORM_ERROR_TYPE, false);
-          } else {
-            errorMessage = '';
-            $scope.FractionInputForm.answer.$setValidity(
-              FORM_ERROR_TYPE, true);
-          }
-        });
-
-        $scope.submitAnswer = function(answer) {
-          try {
-            var fraction = FractionObjectFactory.fromRawInputString(
-              answer);
-            if (requireSimplestForm &&
-              !angular.equals(fraction, fraction.convertToSimplestForm())
-            ) {
-              errorMessage = (
-                'Please enter an answer in simplest form ' +
-                '(e.g., 1/3 instead of 2/6).');
+          /**
+           * Disables the input box if the data entered is not a valid prefix
+           * for a fraction.
+           * Examples of valid prefixes:
+           * -- 1
+           * -- 1 2
+           * -- 1 2/
+           * -- 2/
+           * -- 1 2/3
+           */
+          $scope.$watch('answer', function(newValue) {
+            var INVALID_CHARS_REGEX = /[^\d\s\/-]/g;
+            // Accepts incomplete fraction inputs (see examples above except last).
+            var PARTIAL_FRACTION_REGEX =
+              /^\s*(-?\s*((\d*\s*\d+\s*\/?\s*)|\d+)\s*)?$/;
+            // Accepts complete fraction inputs.
+            var FRACTION_REGEX =
+              /^\s*-?\s*((\d*\s*\d+\s*\/\s*\d+)|\d+)\s*$/;
+            if (INVALID_CHARS_REGEX.test(newValue)) {
+              errorMessage = FRACTION_PARSING_ERRORS.INVALID_CHARS;
+              $scope.FractionInputForm.answer.$setValidity(
+                FORM_ERROR_TYPE, false);
+            } else if (!(FRACTION_REGEX.test(newValue) ||
+                PARTIAL_FRACTION_REGEX.test(newValue))) {
+              errorMessage = FRACTION_PARSING_ERRORS.INVALID_FORMAT;
               $scope.FractionInputForm.answer.$setValidity(
                 FORM_ERROR_TYPE, false);
             } else {
-              $scope.onSubmit({
-                answer: fraction,
-                rulesService: fractionInputRulesService
-              });
+              errorMessage = '';
+              $scope.FractionInputForm.answer.$setValidity(
+                FORM_ERROR_TYPE, true);
             }
-          } catch (parsingError) {
-            errorMessage = parsingError.message;
-            $scope.FractionInputForm.answer.$setValidity(
-              FORM_ERROR_TYPE, false);
-          }
-        };
-      }
-    ]
-  };
-}]);
+          });
+
+          $scope.submitAnswer = function(answer) {
+            try {
+              var fraction = FractionObjectFactory.fromRawInputString(
+                answer);
+              if (requireSimplestForm &&
+                !angular.equals(fraction, fraction.convertToSimplestForm())
+              ) {
+                errorMessage = (
+                  'Please enter an answer in simplest form ' +
+                  '(e.g., 1/3 instead of 2/6).');
+                $scope.FractionInputForm.answer.$setValidity(
+                  FORM_ERROR_TYPE, false);
+              } else {
+                $scope.onSubmit({
+                  answer: fraction,
+                  rulesService: fractionInputRulesService
+                });
+              }
+            } catch (parsingError) {
+              errorMessage = parsingError.message;
+              $scope.FractionInputForm.answer.$setValidity(
+                FORM_ERROR_TYPE, false);
+            }
+          };
+        }
+      ]
+    };
+  }
+]);
 
 oppia.directive('oppiaResponseFractionInput', [
-  'FractionObjectFactory', 'HtmlEscaperService',
-  function(FractionObjectFactory, HtmlEscaperService) {
+  'FractionObjectFactory', 'HtmlEscaperService', 'UrlInterpolationService',
+  function(FractionObjectFactory, HtmlEscaperService,
+    UrlInterpolationService) {
     return {
       restrict: 'E',
       scope: {},
-      templateUrl: 'response/FractionInput',
+      templateUrl: UrlInterpolationService.getExtensionResourceUrl(
+        '/interactions/FractionInput/directives/' +
+        'fraction_input_response_directive.html'),
+      controller: ['$scope', '$attrs', function($scope, $attrs) {
+        var answer = HtmlEscaperService.escapedJsonToObj($attrs.answer);
+        $scope.answer = FractionObjectFactory.fromDict(answer).toString();
+      }]
+    };
+  }
+]);
+
+oppia.directive('oppiaShortResponseFractionInput', [
+  'FractionObjectFactory', 'HtmlEscaperService', 'UrlInterpolationService',
+  function(FractionObjectFactory, HtmlEscaperService,
+    UrlInterpolationService) {
+    return {
+      restrict: 'E',
+      scope: {},
+      templateUrl: UrlInterpolationService.getExtensionResourceUrl(
+        '/interactions/FractionInput/directives/' +
+        'fraction_input_short_response_directive.html'),
       controller: ['$scope', '$attrs', function($scope, $attrs) {
         var answer = HtmlEscaperService.escapedJsonToObj($attrs.answer);
         $scope.answer = FractionObjectFactory.fromDict(answer).toString();
