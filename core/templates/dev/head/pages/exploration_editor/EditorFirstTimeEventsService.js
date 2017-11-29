147b14cf8a33d495d5a46c8f5f1df1d44f2a5655
aks681 <akshayanand681@gmail.com>
2017-11-29 14:14:05 +0530

    reduced spacing in comments

--- core/templates/dev/head/pages/exploration_editor/EditorFirstTimeEventsService.js
+++ core/templates/dev/head/pages/exploration_editor/EditorFirstTimeEventsService.js
@@ -13,9 +13,9 @@
 // limitations under the License.
 
 /**
- *  @fileoverview A Service registering analytics events for the editor
- *  for events which are  only logged when they happen after the editor
- *  is opened for the first time for an exploration.
+ * @fileoverview A Service registering analytics events for the editor
+ * for events which are  only logged when they happen after the editor
+ * is opened for the first time for an exploration.
  */
 
 oppia.factory('EditorFirstTimeEventsService', [
