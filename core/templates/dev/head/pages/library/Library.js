29200edc77121ec90a6ab4aa53353e11e403e51c
jervis446 <kumar.adarshluv99@gmail.com>
2017-11-29 06:48:31 +0530

    update changes

--- core/templates/dev/head/pages/library/Library.js
+++ core/templates/dev/head/pages/library/Library.js
@@ -30,13 +30,13 @@ oppia.controller('Library', [
   '$scope', '$http', '$modal', '$rootScope', '$window', '$timeout',
   'ConstructTranslationIdsService', 'UrlService', 'ALL_CATEGORIES',
   'SearchService','WindowDimensionsService', 'UrlInterpolationService', 
-  'LIBRARY_PAGE_MODES','LIBRARY_TILE_WIDTH_PX', 'AlertsService',
+  'LIBRARY_PAGE_MODES', 'LIBRARY_TILE_WIDTH_PX', 'AlertsService',
   'LearnerDashboardIdsBackendApiService',
   'LearnerDashboardActivityIdsObjectFactory', 'LearnerPlaylistService',
   function(
       $scope, $http, $modal, $rootScope, $window, $timeout,
-      ConstructTranslationIdsService, UrlService,ALL_CATEGORIES,
-      SearchService,WindowDimensionsService, UrlInterpolationService,
+      ConstructTranslationIdsService, UrlService, ALL_CATEGORIES,
+      SearchService, WindowDimensionsService, UrlInterpolationService,
       LIBRARY_PAGE_MODES, LIBRARY_TILE_WIDTH_PX, AlertsService,
       LearnerDashboardIdsBackendApiService,
       LearnerDashboardActivityIdsObjectFactory, LearnerPlaylistService) {
