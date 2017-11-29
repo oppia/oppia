74395235dab3a85b643e3f3035638b049fbf4f86
Prasanna Patil <prasannapatil08@gmail.com>
2017-11-29 15:15:56 +0530

    Addressed review comments.

--- core/templates/dev/head/pages/exploration_player/InputResponsePairDirective.js
+++ core/templates/dev/head/pages/exploration_player/InputResponsePairDirective.js
@@ -24,7 +24,7 @@ oppia.directive('inputResponsePair', [
         data: '=',
         oppiaAvatarImageUrl: '&',
         profilePicture: '&',
-        inputResponsePairId: '=',
+        getInputResponsePairId: '&inputResponsePairId',
       },
       templateUrl: UrlInterpolationService.getDirectiveTemplateUrl(
         '/pages/exploration_player/' +
