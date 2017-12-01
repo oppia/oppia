71a4df801077baa0715be86b76ccbf980f014b38
Boyd Ching <chingboyd@gmail.com>
2017-12-01 19:31:14 +0900

    add parentheses

--- core/templates/dev/head/pages/exploration_player/ProgressDotsDirective.js
+++ core/templates/dev/head/pages/exploration_player/ProgressDotsDirective.js
@@ -32,8 +32,9 @@ oppia.directive('progressDots', [
         function($scope, $rootScope, PlayerPositionService, UrlService,
             WindowDimensionsService) {
           var isIframed = UrlService.isIframed();
-          $scope.MAX_DOTS = WindowDimensionsService.isWindowNarrow() ||
-            isIframed ? 6 : 18;
+          $scope.MAX_DOTS = (
+            WindowDimensionsService.isWindowNarrow() || isIframed ?
+            6 : 18);
           $scope.dots = [];
           $scope.currentDotIndex = PlayerPositionService.getActiveCardIndex();
           var initialDotCount = $scope.getNumDots();
