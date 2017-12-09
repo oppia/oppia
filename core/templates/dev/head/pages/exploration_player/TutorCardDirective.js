fb42122249550b75a424d70093f43d7c116efbab
Kevin Thomas <kevintab@tutanota.com>
2017-12-09 23:47:19 +0530

    Fixes recordSolutionHit error in editor preview

--- core/templates/dev/head/pages/exploration_player/TutorCardDirective.js
+++ core/templates/dev/head/pages/exploration_player/TutorCardDirective.js
@@ -127,8 +127,10 @@ oppia.directive('tutorCard', [
             var responseHtml = solution.getOppiaResponseHtml(interaction);
             PlayerTranscriptService.addNewResponse(responseHtml);
             $scope.helpCardHtml = responseHtml;
-            ExplorationPlayerService.recordSolutionHit(
-              PlayerPositionService.getCurrentStateName());
+            if(!ExplorationPlayerService.isInPreviewMode()) {
+              ExplorationPlayerService.recordSolutionHit(
+                PlayerPositionService.getCurrentStateName());
+            }
           };
 
           $scope.isHintAvailable = function() {
