fb1cbfa01c038999d922c26f8d99a7da80342217
Brian Rodriguez <thatbrod@gmail.com>
2017-12-10 20:26:44 -0500

    Don't show addressed column on graphs.

--- extensions/interactions/MultipleChoiceInput/MultipleChoiceInput.py
+++ extensions/interactions/MultipleChoiceInput/MultipleChoiceInput.py
@@ -62,7 +62,7 @@ class MultipleChoiceInput(base.BaseInteraction):
         'options': {
             'x_axis_label': 'Answer',
             'y_axis_label': 'Count',
-            'show_addressed_column': True,
+            'show_addressed_column': False,
         },
         'calculation_id': 'AnswerFrequencies',
     }]
