561df4dff1ad5d4e313cbba7937a61761ea60ab8
Sagar Manohar <sagarmanohar24@gmail.com>
2017-12-17 14:20:17 -0500

    Add newlines and lexicographical order

--- core/templates/dev/head/pages/exploration_editor/settings_tab/SettingsTab.js
+++ core/templates/dev/head/pages/exploration_editor/settings_tab/SettingsTab.js
@@ -42,6 +42,11 @@ oppia.controller('SettingsTab', [
       EXPLORATION_TITLE_INPUT_FOCUS_LABEL, UserEmailPreferencesService,
       EditableExplorationBackendApiService, UrlInterpolationService,
       explorationAutomaticTextToSpeechService) {
+    $scope.canDelete = GLOBALS.canDelete;
+    $scope.canModifyRoles = GLOBALS.canModifyRoles;
+    $scope.canReleaseOwnership = GLOBALS.canReleaseOwnership;
+    $scope.canUnpublish = GLOBALS.canUnpublish;
+
     $scope.EXPLORATION_TITLE_INPUT_FOCUS_LABEL = (
       EXPLORATION_TITLE_INPUT_FOCUS_LABEL);
 
@@ -56,11 +61,6 @@ oppia.controller('SettingsTab', [
     $scope.isRolesFormOpen = false;
 
     $scope.TAG_REGEX = GLOBALS.TAG_REGEX;
-
-    $scope.canModifyRoles = GLOBALS.canModifyRoles;
-    $scope.canReleaseOwnership = GLOBALS.canReleaseOwnership;
-    $scope.canDelete = GLOBALS.canDelete;
-    $scope.canUnpublish = GLOBALS.canUnpublish;
 
     var CREATOR_DASHBOARD_PAGE_URL = '/creator_dashboard';
     var EXPLORE_PAGE_PREFIX = '/explore/';
