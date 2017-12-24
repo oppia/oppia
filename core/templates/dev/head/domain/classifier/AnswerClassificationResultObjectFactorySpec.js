68f9f580ad8edd655cba88c3c52db92d52ad801f
jervis446 <kumar.adarshluv99@gmail.com>
2017-12-24 20:52:30 +0530

    update

---
+++ core/templates/dev/head/domain/classifier/AnswerClassificationResultObjectFactorySpec.js
@@ -0,0 +1,41 @@
+// Copyright 2016 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Unit tests for the AnswerClassificationResultObjectFactory.
+ */
+ 
+describe('Answer Classification Object Factory', function() {
+  var AnswerClassificationResultFactory;
+  var EXPLICIT_CLASSIFICATION;
+
+  beforeEach(module('oppia'));
+
+  beforeEach(inject(function($injector) {
+    AnswerClassificationResultFactory = $injector.get(
+        'AnswerClassificationResultFactory');
+  }));
+
+  it('should create a new result', function() {
+    var answerClassificationResult = (
+      AnswerClassificationResultFactory.createNew(
+        'outcome 2', [''], []), 1, 0, EXPLICIT_CLASSIFICATION);
+
+    expect(answerClassificationResult.outcome).toEqual(0);
+    expect(answerClassificationResult.answerGroupIndex).toEqual(1);
+    expect(answerClassificationResult.ruleIndex).toEqual(2);
+    expect(answerClassificationResult.classificationCategorization).toEqual(
+      'classificationCategorization');
+  });
+});
