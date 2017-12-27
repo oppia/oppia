51e365a1fc858b6a8b5ee296a0d7f4a4df979763
nix07 <nikhil07prakash@gmail.com>
2017-12-27 11:06:15 +0530

    Fix part of #3712: Corrected indentation issues

--- core/templates/dev/head/pages/exploration_player/ContinueButtonDirective.js
+++ core/templates/dev/head/pages/exploration_player/ContinueButtonDirective.js
@@ -1,23 +1,23 @@
-// Copyright 2017 The Oppia Authors. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS-IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+  // Copyright 2017 The Oppia Authors. All Rights Reserved.
+  //
+  // Licensed under the Apache License, Version 2.0 (the "License");
+  // you may not use this file except in compliance with the License.
+  // You may obtain a copy of the License at
+  //
+  //      http://www.apache.org/licenses/LICENSE-2.0
+  //
+  // Unless required by applicable law or agreed to in writing, software
+  // distributed under the License is distributed on an "AS-IS" BASIS,
+  // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  // See the License for the specific language governing permissions and
+  // limitations under the License.
 
 /**
- * @fileoverview Directive for the Continue button in exploration player and
- * editor.
- */
+* @fileoverview Directive for the Continue button in exploration player and
+* editor.
+*/
 
- oppia.directive('continueButton', [
+oppia.directive('continueButton', [
   'UrlInterpolationService', function(UrlInterpolationService) {
     return {
       restrict: 'E',
@@ -30,4 +30,4 @@
         'continue_button_directive.html')
     };
   }
- ]);
+]);
