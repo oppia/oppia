71f7676a7118f93105c2ac17cd30516348aef1b9
Shubha Gupta <shubha1593@gmail.com>
2017-12-29 14:56:52 +0530

    Remove linting error.

--- core/templates/dev/head/pages/exploration_editor/history_tab/HistoryTabSpec.js
+++ core/templates/dev/head/pages/exploration_editor/history_tab/HistoryTabSpec.js
@@ -65,9 +65,9 @@ describe('HistoryTab controller', function() {
         {vnum: 1, selected: false}
       ];
       $scope.computeVersionsToDisplay();
-      expect($scope.versionNumbersToDisplay).toEqual([32, 31, 30, 29, 28, 27, 26, 25,
-        24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7,
-        6, 5, 4, 3]);
+      expect($scope.versionNumbersToDisplay).toEqual([
+        32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16,
+        15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3]);
       $scope.currentPageDisplay = 2;
       $scope.computeVersionsToDisplay();
       expect($scope.versionNumbersToDisplay).toEqual([2, 1]);
