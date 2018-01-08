ed612957f9ccf926b2b22cd12b39c9d83d5d3ca2
WickedBrat <harshsrivastav123@gmail.com>
2018-01-08 18:21:16 +0530

    Fix for issue #4057

--- core/templates/dev/head/domain/sidebar/SidebarStatusServiceSpec.js
+++ core/templates/dev/head/domain/sidebar/SidebarStatusServiceSpec.js
@@ -7,18 +7,47 @@ describe('SidebarStatusService', function() {
     SidebarStatusService = $injector.get('SidebarStatusService');
   }));
 
-  it('should open the sidebar', function() {
+  it('should open the sidebar if it is not open', function() {
     SidebarStatusService.openSidebar();
     expect(SidebarStatusService.isSidebarShown()).toBe(true);
+  });
+
+  it('should not open the sidebar if it is open', function() {
+    SidebarStatusService.openSidebar();
+    SidebarStatusService.openSidebar();
+    expect(SidebarStatusService.isSidebarShown()).toBe(true);
+  });
+
+  it('should close the sidebar if it is open', function() {
+    SidebarStatusService.openSidebar();
+    SidebarStatusService.closeSidebar();
+    expect(SidebarStatusService.isSidebarShown()).toBe(false);
+  });
+
+  it('should not close the sidebar if it is not open', function() {
+    SidebarStatusService.closeSidebar();
+    expect(SidebarStatusService.isSidebarShown()).toBe(false);
+  });
+
+  it('should toggle sidebar to open', function(){
+    SidebarStatusService.toggleSidebar();
+    expect(SidebarStatusService.isSidebarShown()).toBe(true);
+  });
+
+  it('should toggle sidebar to close', function(){
+    SidebarStatusService.openSidebar();
     SidebarStatusService.toggleSidebar();
     expect(SidebarStatusService.isSidebarShown()).toBe(false);
   });
 
-  it('should close the sidebar', function() {
-    SidebarStatusService.openSidebar();
-    SidebarStatusService.closeSidebar();
+  it('should close sidebar if pendingSidebarClick is false', function(){
+    SidebarStatusService.onDocumentClick();
     expect(SidebarStatusService.isSidebarShown()).toBe(false);
-    SidebarStatusService.toggleSidebar();
+  });
+
+  it('should toggle endingSidebarClick', function(){
+    SidebarStatusService.openSidebar();
+    SidebarStatusService.onDocumentClick();
     expect(SidebarStatusService.isSidebarShown()).toBe(true);
   });
 });
