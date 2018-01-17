57cc5eb6019bea64d312ad280a640ab9f2e39561
Tony <tjiang11@jhu.edu>
2018-01-17 14:52:08 -0600

    Add css prefixes.

--- core/templates/dev/head/pages/exploration_player/AudioBarDirective.js
+++ core/templates/dev/head/pages/exploration_player/AudioBarDirective.js
@@ -121,14 +121,14 @@ oppia.directive('audioBar', [
             var scrollTop = $(this).scrollTop();
             var audioHeader = angular.element($('.audio-header:first'));
             if (scrollTop > lastScrollTop) {
-              audioHeader.addClass('nav-up');
+              audioHeader.addClass('audio-bar-nav-up');
               if (!$scope.audioBarIsExpanded) {
-                audioHeader.addClass('nav-hidden');
+                audioHeader.addClass('audio-bar-nav-hidden');
               }
             } else if (scrollTop === 0 ||
                        scrollTop + $(window).height() < $(document).height()) {
-              audioHeader.removeClass('nav-up');
-              audioHeader.removeClass('nav-hidden');
+              audioHeader.removeClass('audio-bar-nav-up');
+              audioHeader.removeClass('audio-bar-nav-hidden');
             }
             lastScrollTop = scrollTop;
           };
