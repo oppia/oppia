1e4c930027765476f9412516f4adf4fbb0721b6a
vibhor98 <agarwalvibhor84@gmail.com>
2018-01-20 18:34:45 +0530

    Remove empty first line in docstrings.

--- scripts/release_info.py
+++ scripts/release_info.py
@@ -13,7 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """
 Script that simplifies releases by collecting various information.
 Should be run from the oppia root dir.
