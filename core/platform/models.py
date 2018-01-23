7998dba232c9c779ecffbb7d3709e44beb1d3195
Joydeep Mukherjee <joydeep1701@gmail.com>
2018-01-23 18:03:41 +0530

    Documented core.platform.models

--- core/platform/models.py
+++ core/platform/models.py
@@ -27,8 +27,17 @@ NAMES = utils.create_enum(
 
 
 class _Platform(object):
+    """Acts as a base class for platform-specific imports related to GAE
+    """
     @classmethod
     def import_models(cls):
+        """Acts as an abstract method that should be implemented on inherited
+            classes.
+
+        Raises:
+            NotImplementedError: If import_models method is not overwritten in
+                                    derived classes.
+        """
         raise NotImplementedError
 
 
