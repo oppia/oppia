d74558157bbf41c68b607b8c1c1ef71a10228ac6
Sandeep Dubey <dubeysandeep.in@gmail.com>
2018-01-25 08:25:41 +0530

    More changes

--- core/tests/protractor/richTextComponents.js
+++ core/tests/protractor/richTextComponents.js
@@ -95,7 +95,7 @@ describe('rich-text components', function() {
       // to Chrome 61. Chrome version at time of recording this is 61.0.3163.
       'chrome-extension://invalid/ - Failed to load resource: net::ERR_FAILED',
       'Error parsing header X-XSS-Protection: 1; mode=block; ' +
-      'report=https:\/\/www.google.com\/appserve\/security-bugs\/log\/youtube:'
+      'report=https:\/\/www.google.com\/appserve\/security-bugs\/log\/youtube:',
     ]);
   });
 });
