4809c7ee803ac47994b374f53a24638e9b1e4de8
srikant <srksriks123@gmail.com>
2018-02-01 01:40:00 +0530

    Solves issue #4600

--- core/controllers/editor.py
+++ core/controllers/editor.py
@@ -904,7 +904,7 @@ class AudioUploadHandler(EditorHandler):
                 audio = mutagen.File(tempbuffer)
         except mutagen.mp3.HeaderNotFoundError:
             raise self.InvalidInputException(
-                '%s file cannot sync to MPEG Frame' %extension )
+                '%s file cannot sync to MPEG Frame' %extension)
         except mutagen.MutagenError:
             # The calls to mp3.MP3() versus mutagen.File() seem to behave
             # differently upon not being able to interpret the audio.
