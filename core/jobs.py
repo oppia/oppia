7f18113c09f96c14a8902da18c28d97e77065607
Anookeen <anookeen@gmail.com>
2018-02-09 20:18:58 +0000

    Sorted imports

--- core/jobs.py
+++ core/jobs.py
@@ -19,16 +19,11 @@
 import ast
 import collections
 import copy
-import datetime
-import json
-import logging
 from core.platform import models
-import traceback
-
-import utils
-
+import datetime
 from google.appengine.api import app_identity
 from google.appengine.ext import ndb
+import json
 from mapreduce import base_handler
 from mapreduce import context
 from mapreduce import input_readers
@@ -36,7 +31,15 @@ from mapreduce import mapreduce_pipeline
 from mapreduce import output_writers
 from mapreduce import model as mapreduce_model
 from mapreduce import util as mapreduce_util
+import logging
 from pipeline import pipeline
+import traceback
+
+import utils
+
+
+
+
 
 (base_models, job_models,) = models.Registry.import_models([
     models.NAMES.base_model, models.NAMES.job])
