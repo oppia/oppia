a6240346081081eee5c6ad165adf8e2869153c26
Divyadeep Singh <divyadeep.singh962118@gmail.com>
2018-02-16 23:41:40 +0530

    Added missing docstrings in file: core/storage/job/gae_models.py and corrected formatting errors.

--- core/storage/job/gae_models.py
+++ core/storage/job/gae_models.py
@@ -89,12 +89,12 @@ class JobModel(base_models.BaseModel):
 
     @property
     def is_cancelable(self):
-    	"""Checks if the job is cancelable.
-    	
-    	Returns:
-    		bool. True if the status_code is queued or started, otherwise 
-    		false.
-    	"""
+        """Checks if the job is cancelable.
+
+        Returns:
+            bool. True if the status_code is queued or started, otherwise 
+            false.
+        """
         # Whether the job is currently in 'queued' or 'started' status.
         return self.status_code in [STATUS_CODE_QUEUED, STATUS_CODE_STARTED]
 
