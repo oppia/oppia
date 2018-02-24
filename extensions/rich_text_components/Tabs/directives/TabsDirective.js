afcb901b41e9e17c5bb135c138f82590cf4b65d1
srikar0896 <srikar.0896@gmail.com>
2018-02-25 01:08:15 +0530

    Fix #1494:Can't see videos in Tabs.

--- extensions/rich_text_components/Tabs/directives/TabsDirective.js
+++ extensions/rich_text_components/Tabs/directives/TabsDirective.js
@@ -34,3 +34,17 @@ oppia.directive('oppiaNoninteractiveTabs', [
     };
   }
 ]);
+
+oppia.directive('bindHtmlCompile', ['$compile', function ($compile) {
+  return {
+    restrict: 'A',
+    link: function (scope, element, attrs) {
+      scope.$watch(function () {
+        return scope.$eval(attrs.bindHtmlCompile);
+      }, function (value) {
+        element.html(value);
+        $compile(element.contents())(scope);
+      });
+    }
+  };
+}]);
