46deb0b22552863d9b40603543e870d4ef239540
jervis446 <kumar.adarshluv99@gmail.com>
2018-02-24 14:23:50 +0530

    add changes

--- core/templates/dev/head/components/forms/FormBuilder.js
+++ core/templates/dev/head/components/forms/FormBuilder.js
@@ -352,7 +352,7 @@ oppia.directive('applyValidation', ['$filter', function($filter) {
 oppia.directive('requireIsFloat', ['$filter', function($filter) {
   return {
     require: 'ngModel',
-    restrict: 'A',
+    restrict: 'E',
     link: function(scope, elm, attrs, ctrl) {
       var floatValidator = function(viewValue) {
         var filteredValue = $filter('isFloat')(viewValue);
