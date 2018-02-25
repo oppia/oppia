9bcf90ed3f04944347d2c2c26e8ecf075bbe73b1
srikar0896 <srikar.0896@gmail.com>
2018-02-26 01:42:56 +0530

    removed directive.

--- extensions/rich_text_components/Tabs/directives/TabsDirective.js
+++ extensions/rich_text_components/Tabs/directives/TabsDirective.js
@@ -34,17 +34,3 @@ oppia.directive('oppiaNoninteractiveTabs', [
     };
   }
 ]);
-
-oppia.directive('bindHtmlCompile', ['$compile', function ($compile) {
-  return {
-    restrict: 'A',
-    link: function (scope, element, attrs) {
-      scope.$watch(function () {
-        return scope.$eval(attrs.bindHtmlCompile);
-      }, function (value) {
-        element.html(value);
-        $compile(element.contents())(scope);
-      });
-    }
-  };
-}]);
