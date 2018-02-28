e591a28adc0707406db0a714b38f33f20d2181c2
Apurv-Bajaj <apurvabajaj007@gmail.com>
2018-02-28 19:58:14 +0530

    Add tests

---
+++ scripts/explicit_kwargs_checker_tests.py
@@ -0,0 +1,57 @@
+# coding: utf-8
+#
+# Copyright 2018 The Oppia Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import astroid
+
+import pylint.testutils
+
+import explicit_kwargs_checker
+
+
+class ExplicitKwargsCheckerTests(pylint.testutils.CheckerTestCase):
+    CHECKER_CLASS = explicit_kwargs_checker.ExplicitKwargsChecker
+
+    def finds_non_explicit_kwargs_test(self):
+        func_node = astroid.extract_node("""
+        def test(test_var_one, test_var_two=4, test_var_three=5, test_var_four="test_checker"): #@
+            test_var_five = test_var_two + test_var_three
+            return test_var_five
+        """)
+        self.checker.visit_functiondef(func_node)
+        func_args = func_node.args
+        self.checker.visit_arguments(func_args)
+        func_call_node_one, func_call_node_two, func_call_node_three = (
+            astroid.extract_node("""
+        test(2, 5, 6) #@
+        test(2) #@
+        test(2, 5, 6, test_var_four="test_string") #@
+        """))
+        with self.assertAddsMessages(
+            pylint.testutils.Message(
+                msg_id='non-explicit-kwargs',
+                node=func_call_node_one,
+            ),
+        ):
+            self.checker.visit_call(func_call_node_one)
+        with self.assertNoMessages():
+            self.checker.visit_call(func_call_node_two)
+        with self.assertAddsMessages(
+            pylint.testutils.Message(
+                msg_id='non-explicit-kwargs',
+                node=func_call_node_three,
+            ),
+        ):
+            self.checker.visit_call(func_call_node_three)
