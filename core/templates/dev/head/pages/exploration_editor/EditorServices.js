56ed327513fe6654f285200518b6514c1fd94eca
mittrayash <mittrayash@gmail.com>
2018-03-03 16:53:32 +0530

    fixed auto-indent 4 spacing back to 2 spacing

--- core/templates/dev/head/pages/exploration_editor/EditorServices.js
+++ core/templates/dev/head/pages/exploration_editor/EditorServices.js
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/**
+ * @fileoverview Standalone services for the exploration editor page.
+ */
 
 // A data service that stores the current state content.
 // TODO(sll): Add validation.
