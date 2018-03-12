11bf21f927576bb7ab98987e9b60e0bbc38526b9
Brian Rodriguez <thatbrod@gmail.com>
2018-03-11 22:30:07 -0400

    Address issues.

--- core/templates/dev/head/pages/exploration_editor/editor_tab/UnresolvedAnswersOverviewDirective.js
+++ core/templates/dev/head/pages/exploration_editor/editor_tab/UnresolvedAnswersOverviewDirective.js
@@ -16,57 +16,14 @@
  * @fileoverview Directive for the state graph visualization.
  */
 
-oppia.directive('unresolvedAnswersOverviewViz', [
+oppia.directive('unresolvedAnswersOverview', [
   'UrlInterpolationService', function(UrlInterpolationService) {
     return {
       // Note: This directive is used as attribute because pannability does not
       //    work when directive is used as element. (Convention in the codebase
       //    is to use directive as element.)
-      restrict: 'A',
-      scope: {
-        allowPanning: '@',
-        centerAtCurrentState: '@',
-        currentStateId: '&',
-        // A function returning an object with these keys:
-        //  - 'nodes': An object whose keys are node ids and whose values are
-        //             node labels
-        //  - 'links': A list of objects with keys:
-        //            'source': id of source node
-        //            'target': id of target node
-        //            'linkProperty': property of link which determines how
-        //              it is styled (styles in linkPropertyMapping). If
-        //              linkProperty or corresponding linkPropertyMatching
-        //              is undefined, link style defaults to the gray arrow.
-        //  - 'initStateId': The initial state id
-        //  - 'finalStateIds': The list of ids corresponding to terminal states
-        //             (i.e., those whose interactions are terminal).
-        graphData: '&',
-        // Object whose keys are ids of nodes to display a warning tooltip over
-        highlightStates: '=',
-        // Id of a second initial state, which will be styled as an initial
-        // state
-        initStateId2: '=',
-        isEditable: '=',
-        // Object which maps linkProperty to a style
-        linkPropertyMapping: '=',
-        // Object whose keys are node ids and whose values are node colors
-        nodeColors: '=',
-        // A value which is the color of all nodes
-        nodeFill: '@',
-        // Object whose keys are node ids with secondary labels and whose
-        // values are secondary labels. If this is undefined, it means no nodes
-        // have secondary labels.
-        nodeSecondaryLabels: '=',
-        // Function called when node is clicked. Should take a parameter
-        // node.id.
-        onClickFunction: '=',
-        onDeleteFunction: '=',
-        onMaximizeFunction: '=',
-        // Object whose keys are ids of nodes, and whose values are the
-        // corresponding node opacities.
-        opacityMap: '=',
-        showWarningSign: '@'
-      },
+      restrict: 'E',
+      scope: {},
       templateUrl: UrlInterpolationService.getDirectiveTemplateUrl(
         '/pages/exploration_editor/editor_tab/' +
         'unresolved_answers_overview_directive.html'),
