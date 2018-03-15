685745b9c4861dba8ec8bbdca6cade2d70aeeb61
Apurv-Bajaj <apurvabajaj007@gmail.com>
2018-03-15 13:41:40 +0530

    Made Review Changes

--- scripts/custom_lint_checks_test.py
+++ scripts/custom_lint_checks_test.py
@@ -15,7 +15,7 @@
 # limitations under the License.
 #
 # For details on how to write such tests, please refer to
-# https://github.com/oppia/oppia/wiki/Writing-Tests
+# https://github.com/oppia/oppia/wiki/Writing-Tests-For-Pylint
 
 import os
 import sys
@@ -25,53 +25,56 @@ _PARENT_DIR = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
 _PYLINT_PATH = os.path.join(_PARENT_DIR, 'oppia_tools', 'pylint-1.7.1')
 sys.path.insert(0, _PYLINT_PATH)
 
-# Since this module needs to be imported after adding Isort path,
-# we need to disable isort for this line.
+# Since these module needs to be imported after adding Pylint path,
+# we need to disable isort for the below lines to prevent import
+# order errors.
 # pylint: disable=wrong-import-position
 # pylint: disable=relative-import
-import astroid  #isort:skip
-import explicit_kwargs_checker  #isort:skip
+import astroid  # isort:skip
+import custom_lint_checks  # isort:skip
 import pylint  # isort:skip
 from pylint.testutils import CheckerTestCase  # isort:skip
+# pylint: enable=wrong-import-position
+# pylint: enable=relative-import
 
 
 class ExplicitKwargsCheckerTest(unittest.TestCase):
 
     def test_finds_non_explicit_kwargs(self):
-        explicit_kwargs_checker_test_object = CheckerTestCase()
-        explicit_kwargs_checker_test_object.CHECKER_CLASS = (
-            explicit_kwargs_checker.ExplicitKwargsChecker)
-        explicit_kwargs_checker_test_object.setup_method()
+        checker_test_object = CheckerTestCase()
+        checker_test_object.CHECKER_CLASS = (
+            custom_lint_checks.ExplicitKwargsChecker)
+        checker_test_object.setup_method()
         func_node = astroid.extract_node("""
         def test(test_var_one, test_var_two=4, test_var_three=5, test_var_four="test_checker"): #@
             test_var_five = test_var_two + test_var_three
             return test_var_five
         """)
-        explicit_kwargs_checker_test_object.checker.visit_functiondef(func_node)
+        checker_test_object.checker.visit_functiondef(func_node)
         func_args = func_node.args
-        explicit_kwargs_checker_test_object.checker.visit_arguments(func_args)
+        checker_test_object.checker.visit_arguments(func_args)
         func_call_node_one, func_call_node_two, func_call_node_three = (
             astroid.extract_node("""
         test(2, 5, 6) #@
         test(2) #@
         test(2, 5, 6, test_var_four="test_string") #@
         """))
-        with explicit_kwargs_checker_test_object.assertAddsMessages(
+        with checker_test_object.assertAddsMessages(
             pylint.testutils.Message(
                 msg_id='non-explicit-kwargs',
                 node=func_call_node_one,
             ),
         ):
-            explicit_kwargs_checker_test_object.checker.visit_call(
+            checker_test_object.checker.visit_call(
                 func_call_node_one)
-        with explicit_kwargs_checker_test_object.assertNoMessages():
-            explicit_kwargs_checker_test_object.checker.visit_call(
+        with checker_test_object.assertNoMessages():
+            checker_test_object.checker.visit_call(
                 func_call_node_two)
-        with explicit_kwargs_checker_test_object.assertAddsMessages(
+        with checker_test_object.assertAddsMessages(
             pylint.testutils.Message(
                 msg_id='non-explicit-kwargs',
                 node=func_call_node_three,
             ),
         ):
-            explicit_kwargs_checker_test_object.checker.visit_call(
+            checker_test_object.checker.visit_call(
                 func_call_node_three)
