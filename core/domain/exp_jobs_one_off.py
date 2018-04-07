52fdd80a951c6752dc186bb7eb153dc424eb7eac
Prasanna Patil <prasannapatil08@gmail.com>
2018-04-07 17:01:13 +0530

    Allow models regeneration from MR job

--- core/domain/exp_jobs_one_off.py
+++ core/domain/exp_jobs_one_off.py
@@ -361,7 +361,6 @@ class ExplorationStateIdMappingJob(jobs.BaseMapReduceOneOffJobManager):
     def map(item):
         if item.deleted:
             return
-
         try:
             exploration = exp_services.get_exploration_from_model(item)
         except Exception as e:
@@ -413,12 +412,30 @@ class ExplorationStateIdMappingJob(jobs.BaseMapReduceOneOffJobManager):
                         'revert_version_number'):
                     reverted_version = change_list[0]['version_number']
                     # pylint: disable=line-too-long
-                    exp_services.create_and_save_state_id_mapping_model_for_reverted_exploration(
-                        exploration.id, exploration.version - 1, reverted_version)
+                    state_id_mapping = exp_services.generate_state_id_mapping_model_for_reverted_exploration(
+                        exploration.id, exploration.version - 1,
+                        reverted_version)
                     # pylint: enable=line-too-long
                 else:
-                    exp_services.create_and_save_state_id_mapping_model(
-                        exploration, change_list)
+                    state_id_mapping = (
+                        exp_services.generate_state_id_mapping_model(
+                            exploration, change_list))
+
+                # pylint: disable=protected-access
+                instance_id = (
+                    exp_models.StateIdMappingModel._generate_instance_id(
+                        exploration.id, exploration.version))
+                # pylint: enable=protected-access
+
+                state_id_mapping_model = exp_models.StateIdMappingModel(
+                    id=instance_id,
+                    exploration_id=state_id_mapping.exploration_id,
+                    exploration_version=state_id_mapping.exploration_version,
+                    state_names_to_ids=state_id_mapping.state_names_to_ids,
+                    largest_state_id_used=(
+                        state_id_mapping.largest_state_id_used))
+                state_id_mapping_model.put()
+
             except Exception as e:
                 yield ('ERROR with exp_id %s' % item.id, str(e))
                 return
