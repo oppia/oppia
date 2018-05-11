3de9713e83d402ce872e7b32b0507e8bb91f9673
Pranav Siddharth <pranavsid98@gmail.com>
2018-05-11 11:27:27 +0530

    remove unused function

--- extensions/issues/base_test.py
+++ extensions/issues/base_test.py
@@ -28,31 +28,6 @@ EARLY_QUIT_ID = 'EarlyQuit'
 class IssueUnitTests(test_utils.GenericTestBase):
     """Test that the default issues are valid."""
 
-    def _validate_customization_arg_specs(self, customization_args):
-        for ca_spec in customization_args:
-            self.assertEqual(set(ca_spec.keys()), set([
-                'name', 'description', 'schema', 'default_value']))
-
-            self.assertTrue(isinstance(ca_spec['name'], basestring))
-            self.assertTrue(self._is_alphanumeric_string(ca_spec['name']))
-            self.assertTrue(isinstance(ca_spec['description'], basestring))
-            self.assertGreater(len(ca_spec['description']), 0)
-
-            schema_utils_test.validate_schema(ca_spec['schema'])
-            self.assertEqual(
-                ca_spec['default_value'],
-                schema_utils.normalize_against_schema(
-                    ca_spec['default_value'], ca_spec['schema']))
-
-            if ca_spec['schema']['type'] == 'custom':
-                obj_class = obj_services.Registry.get_object_class_by_type(
-                    ca_spec['schema']['obj_type'])
-                self.assertIsNotNone(obj_class.edit_html_filename)
-                self.assertIsNotNone(obj_class.edit_js_filename)
-                self.assertEqual(
-                    ca_spec['default_value'],
-                    obj_class.normalize(ca_spec['default_value']))
-
     def test_issue_properties(self):
         """Test the standard properties of issues."""
 
