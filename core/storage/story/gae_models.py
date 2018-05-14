8c297413f6b0cfb1a5eb8f7368c59512fe9e67ea
aks681 <akshayanand681@gmail.com>
2018-05-14 22:22:06 +0530

    added story and topic models

---
+++ core/storage/story/gae_models.py
@@ -0,0 +1,161 @@
+# Copyright 2018 The Oppia Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Models for storing the story data models."""
+
+from core.platform import models
+import utils
+
+from google.appengine.ext import ndb
+
+(base_models,) = models.Registry.import_models([models.NAMES.base_model])
+
+
+class StorySnapshotMetadataModel(base_models.BaseSnapshotMetadataModel):
+    """Storage model for the metadata for a story snapshot."""
+    pass
+
+
+class StorySnapshotContentModel(base_models.BaseSnapshotContentModel):
+    """Storage model for the content of a story snapshot."""
+    pass
+
+
+class StoryModel(base_models.VersionedModel):
+    """Model for storing stories.
+
+    This class should only be imported by the story domain file, the
+    story services file, and the story model test file.
+    """
+    SNAPSHOT_METADATA_CLASS = StorySnapshotMetadataModel
+    SNAPSHOT_CONTENT_CLASS = StorySnapshotContentModel
+    ALLOW_REVERT = True
+
+    # The title of the story.
+    title = ndb.StringProperty(required=True, indexed=True)
+    # A high-level description of the story.
+    description = ndb.StringProperty(indexed=False)
+    # The topic id the story corresponds to.
+    topic = ndb.StringProperty(indexed=False)
+    # A set of notes, that describe the characters, main storyline, and setting.
+    notes = ndb.StringProperty(repeated=True, indexed=False)
+    # The schema version for the story.
+    schema_version = (
+        ndb.IntegerProperty(required=True, indexed=True))
+    # The ISO 639-1 code for the language this question is written in.
+    language_code = ndb.StringProperty(required=True, indexed=True)
+    # The story graph specifying the connection between nodes.
+    story_contents = ndb.JsonProperty(default={}, indexed=False)
+
+
+class StoryCommitLogEntryModel(base_models.BaseModel):
+    """Log of commits to stories.
+
+    A new instance of this model is created and saved every time a commit to
+    StoryModel occurs.
+
+    The id for this model is of the form
+    'story-{{STORY_ID}}-{{STORY_VERSION}}'.
+    """
+    # Update superclass model to make these properties indexed.
+    created_on = ndb.DateTimeProperty(auto_now_add=True, indexed=True)
+    last_updated = ndb.DateTimeProperty(auto_now=True, indexed=True)
+
+    # The id of the user.
+    user_id = ndb.StringProperty(indexed=True, required=True)
+    # The username of the user, at the time of the edit.
+    username = ndb.StringProperty(indexed=True, required=True)
+    # The id of the story being edited.
+    story_id = ndb.StringProperty(indexed=True, required=True)
+    # The type of the commit: 'create', 'revert', 'edit', 'delete'.
+    commit_type = ndb.StringProperty(indexed=True, required=True)
+    # The commit message.
+    commit_message = ndb.TextProperty(indexed=False)
+    # The commit_cmds dict for this commit.
+    commit_cmds = ndb.JsonProperty(indexed=False, required=True)
+    # The version number of the story after this commit.
+    version = ndb.IntegerProperty()
+
+    @classmethod
+    def get_commit(cls, story_id, version):
+        """Returns the commit corresponding to the given story id and
+        version number.
+
+        Args:
+            story_id: str. The id of the story being edited.
+            version: int. The version number of the story after the commit.
+
+        Returns:
+            The commit with the given story id and version number.
+        """
+        return cls.get_by_id('story-%s-%s' % (story_id, version))
+
+    @classmethod
+    def get_all_commits(cls, page_size, urlsafe_start_cursor):
+        """Fetches a list of all the commits sorted by their last updated
+        attribute.
+
+        Args:
+            page_size: int. The maximum number of entities to be returned.
+            urlsafe_start_cursor: str or None. If provided, the list of
+                returned entities starts from this datastore cursor.
+                Otherwise, the returned entities start from the beginning
+                of the full list of entities.
+
+        Returns:
+            3-tuple of (results, cursor, more) as described in fetch_page() at:
+            https://developers.google.com/appengine/docs/python/ndb/queryclass,
+            where:
+                results: List of query results.
+                cursor: str or None. A query cursor pointing to the next
+                    batch of results. If there are no more results, this might
+                    be None.
+                more: bool. If True, there are (probably) more results after
+                    this batch. If False, there are no further results after
+                    this batch.
+        """
+        return cls._fetch_page_sorted_by_last_updated(
+            cls.query(), page_size, urlsafe_start_cursor)
+
+
+class StorySummaryModel(base_models.BaseModel):
+    """Summary model for an Oppia Story.
+
+    This should be used whenever the content blob of the story is not
+    needed (e.g. search results, etc).
+
+    A StorySummaryModel instance stores the following information:
+
+        id, description, language_code, last_updated, created_on, version.
+
+    The key of each instance is the story id.
+    """
+
+    # The title of the story.
+    title = ndb.StringProperty(required=True, indexed=True)
+    # The topic id the story corresponds to.
+    topic = ndb.StringProperty(indexed=False)
+    # The ISO 639-1 code for the language this question is written in.
+    language_code = ndb.StringProperty(required=True, indexed=True)
+    # Time when the story model was last updated (not to be
+    # confused with last_updated, which is the time when the
+    # story *summary* model was last updated).
+    story_model_last_updated = ndb.DateTimeProperty(indexed=True)
+    # Time when the story model was created (not to be confused
+    # with created_on, which is the time when the story *summary*
+    # model was created).
+    story_model_created_on = ndb.DateTimeProperty(indexed=True)
+    # The number of nodes that are part of this story.
+    node_count = ndb.IntegerProperty()
+    version = ndb.IntegerProperty()
