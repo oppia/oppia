a7f01b3528e5f2414e67c3173fd4b6fb5dd3ae3d
Aashishgaba097 <aashishgaba097@gmail.com>
2018-05-22 07:22:34 +0530

    Created ExtractImageFilenamesFromStateService and its Spec file

--- core/templates/dev/head/pages/exploration_player/ExtractImageFilenamesFromStateService.js
+++ core/templates/dev/head/pages/exploration_player/ExtractImageFilenamesFromStateService.js
@@ -16,55 +16,58 @@
  * @fileoverview Service to extract image filenames in a State.
  */
 
-oppia.factory('ExtractImageFilesInStateService', ['HtmlEscaperService',
-function(HtmlEscaperService) {
-  var filenamesInState = [];
-  var _getAllHtmlOfState = function(state) {    
+oppia.factory('ExtractImageFilenamesFromStateService', ['HtmlEscaperService',
+  function(HtmlEscaperService) {
+    var INTERACTION_TYPE_MULTIPLE_CHOICE = 'MultipleChoiceInput';
+    var INTERACTION_TYPE_ITEM_SELECTION = 'ItemSelectionInput';
+    var INTERACTION_TYPE_IMAGE_CLICK_INPUT = 'ImageClickInput';
 
-    var _allHtmlInTheState = []
-// The order of the images in a state is also maintained. The images should be
-// preloaded in the following order --- content, customizationArgs of 
-// interactions, feedback of outcomes(including feedback of default outcome 
-// if any), hints, solution if any.
-    var stateContentHtml = state.content.getHtml()
-    _allHtmlInTheState.push(stateContentHtml);
+    var filenamesInState = [];
+    var _getAllHtmlOfState = function(state) {
+      var _allHtmlInTheState = [];
+      // The order of the imagenames extracted is same as they appea in a state
+      // . The images should be preloaded in the following order --- content,
+      // customizationArgs of interactions, feedback of outcomes(including
+      // feedback of default outcome if any), hints, solution if any.
+      var stateContentHtml = state.content.getHtml();
+      _allHtmlInTheState.push(stateContentHtml);
 
-    if (['ItemSelectionInput','MultipleChoiceInput'].indexOf(state.
-      interaction.id)) {
-      var customizationArgsHtml = '';
-      state.interaction.customizationArgs.choices.value.forEach(function(
-        value) {
-        customizationArgsHtml = customizationArgsHtml.concat(value);
-      });
-      _allHtmlInTheState.push(customizationArgsHtml);
-    }
-
-    state.interaction.answerGroups.forEach(function(answerGroup) {
-      var answerGroupHtml = answerGroup.outcome.feedback.getHtml();
-      _allHtmlInTheState.push(answerGroupHtml);
-    });
-
-    if (state.interaction.defaultOutcome !== null) {
-      var defaultOutcomeHtml = state.interaction.defaultOutcome.feedback
-        .getHtml();
-      if(defaultOutcomeHtml !== '') {
-        _allHtmlInTheState.push(defaultOutcomeHtml);
+      if (state.interaction.id === INTERACTION_TYPE_MULTIPLE_CHOICE ||
+        state.interaction.id === INTERACTION_TYPE_ITEM_SELECTION ) {
+        var customizationArgsHtml = '';
+        state.interaction.customizationArgs.choices.value.forEach(
+          function(value) {
+            customizationArgsHtml = customizationArgsHtml.concat(value);
+          });
+        _allHtmlInTheState.push(customizationArgsHtml);
       }
-    }
 
-    state.interaction.hints.forEach(function(hint) {
-      var hintHtml = hint.hintContent.getHtml();
-      _allHtmlInTheState.push(hintHtml);
-    });
-    if (state.interaction.solution !== null) {
-      var solutionHtml = state.interaction.solution.explanation.getHtml();
-      _allHtmlInTheState.push(solutionHtml);
-    }
+      state.interaction.answerGroups.forEach(function(answerGroup) {
+        var answerGroupHtml = answerGroup.outcome.feedback.getHtml();
+        _allHtmlInTheState.push(answerGroupHtml);
+      });
+
+      if (state.interaction.defaultOutcome !== null) {
+        var defaultOutcomeHtml = state.interaction.defaultOutcome.feedback
+          .getHtml();
+        if (defaultOutcomeHtml !== '') {
+          _allHtmlInTheState.push(defaultOutcomeHtml);
+        }
+      }
+
+      state.interaction.hints.forEach(function(hint) {
+        var hintHtml = hint.hintContent.getHtml();
+        _allHtmlInTheState.push(hintHtml);
+      });
+      if (state.interaction.solution !== null) {
+        var solutionHtml = state.interaction.solution.explanation.getHtml();
+        _allHtmlInTheState.push(solutionHtml);
+      }
       return _allHtmlInTheState;
-  }
+    };
 
-  var _extractFilepathValueFromOppiaNonInteractiveImageTag = function(
-    strHtml) {
+    var _extractFilepathValueFromOppiaNonInteractiveImageTag = function(
+        strHtml) {
       var filenames = [];
       var dummyElement = document.createElement('div');
       dummyElement.innerHTML = HtmlEscaperService.escapedStrToUnescapedStr(
@@ -75,29 +78,35 @@ function(HtmlEscaperService) {
       for (i = 0; i < imageTagList.length; i++) {
         filenames.push(imageTagList[i].getAttribute('filepath-with-value'));
       }
+      // The name in the array is stored as '"image.png"'. We need to remove
+      // the inverted commas. We remove the first and the last character from
+      // the string (name).
+      for (var x in filenames) {
+        filenames[x] = filenames[x].slice(1, filenames[x].length - 1);
+      }
       return filenames;
-  };
+    };
 
-  var _getImageFilenamesInState = function(state) {
-    var filenamesInState = [];
-    /* The Image And Region interaction has an image whose filename is directly
-    /  stored in the customizationArgs.ImageAndRegion.ImagePath 
-    */
-    if (state.interaction.id === 'ImageClickInput') {
-      filenamesInState.push(
-        state.interaction.customizationArgs.ImageAndRegion.ImagePath);
-    }
-    allHtmlOfState = _getAllHtmlOfState(state);
-    allHtmlOfState.forEach(function(htmlStr) {
-      filenamesInState = filenamesInState.concat(
-        _extractFilepathValueFromOppiaNonInteractiveImageTag(htmlStr));
-    });
-    return filenamesInState;
-  }
+    var _getImageFilenamesInState = function(state) {
+      var filenamesInState = [];
+      // The Image Click Input interaction has an image whose filename is
+      // directly stored in the customizationArgs.imageAndRegion.value
+      // .imagePath
+      if (state.interaction.id === 'INTERACTION_TYPE_IMAGE_CLICK_INPUT') {
+        filenamesInState.push(
+          state.interaction.customizationArgs.imageAndRegions.value.imagePath);
+      }
+      allHtmlOfState = _getAllHtmlOfState(state);
+      allHtmlOfState.forEach(function(htmlStr) {
+        filenamesInState = filenamesInState.concat(
+          _extractFilepathValueFromOppiaNonInteractiveImageTag(htmlStr));
+      });
+      return filenamesInState;
+    };
 
-  return {
-    getImageFilenamesInState: function(state) {
-      return _getImageFilenamesInState(state);
-    }
-  }
-}])
+    return {
+      getImageFilenamesInState: function(state) {
+        return _getImageFilenamesInState(state);
+      }
+    };
+  }]);
--- core/templates/dev/head/pages/exploration_player/ExtractImageFilenamesFromStateServiceSpec.js
+++ core/templates/dev/head/pages/exploration_player/ExtractImageFilenamesFromStateServiceSpec.js
@@ -27,10 +27,10 @@ describe('Extracting Image file names in the state service', function() {
           is_terminal: false
         },
         ItemSelectionInput: {
-            is_terminal: false
+          is_terminal: false
         },
         MultipleChoiceInput: {
-            is_terminal: false
+          is_terminal: false
         },
         Continue: {
           is_terminal: false
@@ -49,7 +49,7 @@ describe('Extracting Image file names in the state service', function() {
   beforeEach(inject(function($injector) {
     eof = $injector.get('ExplorationObjectFactory');
     ecs = $injector.get('ExplorationContextService');
-    eifss = $injector.get('ExtractImageFilesInStateService');
+    eifss = $injector.get('ExtractImageFilenamesFromStateService');
     spyOn(ecs, 'getExplorationId').and.returnValue('1');
     $rootScope = $injector.get('$rootScope');
     explorationDict = {
@@ -62,7 +62,7 @@ describe('Extracting Image file names in the state service', function() {
       author_notes: '',
       states_schema_version: 15,
       init_state_name: 'Introduction',
-      states: { 
+      states: {
         'State 1': {
           param_changes: [],
           content: {
@@ -121,26 +121,34 @@ describe('Extracting Image file names in the state service', function() {
             default_outcome: {
               dest: 'Introduction',
               feedback: {
-                audio_tranlsation: {},
+                audio_translation: {},
                 html: 'Try Again!'
               }
             },
             confirmed_unclassified_answers: [],
             customization_args: {
               choices: {
-                values: ['<p> Go to ItemSelection <oppia-noninteractive-image filepath-with-value="&amp;quot;sIMultipleChoice1.png&amp;quot;"></oppia-noninteractive-image></p>',
-                '<p> Go to ImageAndRegion<oppia-noninteractive-image filepath-with-value="&amp;quot;sIMultipleChoice2.png&amp;quot;"></oppia-noninteractive-image></p>'
-              ]
+                value: [
+                  '<p> Go to ItemSelection <oppia-noninteractive-image' +
+                  ' filepath-with-value="&amp;quot;sIMultipleChoice1.png&amp;' +
+                  'quot;"></oppia-noninteractive-image></p>',
+                  '<p> Go to ImageAndRegion<oppia-noninteractive-image' +
+                  ' filepath-with-value="&amp;quot;sIMultipleChoice2.png&amp;' +
+                  'quot;"></oppia-noninteractive-image></p>'
+                ]
               }
             },
             answer_groups: [
               {
                 labelled_as_correct: false,
                 outcome: {
-                  dest: 'State 1',
+                  dest: 'State 4',
                   feedback: {
                     audio_translations: {},
-                    html: '<p>We are going to ItemSelection <oppia-noninteractive-image filepath-with-value="&amp;quot;sIOutcomeFeedback.png&amp;quot;"></oppia-noninteractive-image></p>'
+                    html: '<p>We are going to ItemSelection' +
+                          '<oppia-noninteractive-image filepath-with-value=' +
+                          '"&amp;quot;sIOutcomeFeedback.png&amp;quot;">' +
+                          '</oppia-noninteractive-image></p>'
                   },
                   param_changes: [],
                   refresher_exploration_id: null
@@ -157,10 +165,10 @@ describe('Extracting Image file names in the state service', function() {
               {
                 labelled_as_correct: false,
                 outcome: {
-                  dest: 'State 1',
+                  dest: 'State 5',
                   feedback: {
                     audio_translations: {},
-                    html: 'Lets go to state 5 ImageAndRegion'
+                    html: "Let's go to state 5 ImageAndRegion"
                   },
                   param_changes: [],
                   refresher_exploration_id: null
@@ -176,6 +184,275 @@ describe('Extracting Image file names in the state service', function() {
               }
 
             ],
+            hints: [],
+            solution: null
+          }
+        },
+        'State 4': {
+          param_changes: [],
+          content: {
+            html: '<p><oppia-noninteractive-image filepath-with-value="&amp;' +
+                  'quot;s4Content.png&amp;quot;">' +
+                  '</oppia-noninteractive-image></p>',
+            audio_translations: {}
+          },
+          interaction: {
+            id: 'ItemSelectionInput',
+            default_outcome: {
+              feedback: {
+                html: '<p>Try Again! <oppia-noninteractive-image' +
+                      'filepath-with-value="&amp;quot;' +
+                      's4DefaultOutcomeFeedback.png&amp;quot;">' +
+                      '</oppia-noninteractive-image></p>',
+                audio_translation: {}
+              },
+              dest: 'State 4',
+              param_changes: []
+            },
+            confirmed_unclassifies_answers: [],
+            customization_args: {
+              minAllowableSelectionCount: {
+                value: 1
+              },
+              maxAllowableSelectionCount: {
+                value: 2
+              },
+              choices: {
+                value: [
+                  '<p><oppia-noninteractive-image filepath-with-value="&amp;' +
+                  'quot;s4Choice1.png&amp;quot;">' +
+                  '</oppia-noninteractive-image></p>',
+                  '<p><oppia-noninteractive-image filepath-with-value="&amp;' +
+                  'quot;s4Choice2.png&amp;quot;">' +
+                  '</oppia-noninteractive-image></p>']
+              }
+            },
+            hints: [],
+            solution: null,
+            answer_groups: [
+              {
+                labelled_as_correct: false,
+                outcome: {
+                  dest: 'State 6',
+                  feedback: {
+                    audio_translations: {},
+                    html: "It is choice number 1. Let's go to the Text Input"
+                  },
+                  param_changes: [],
+                  refresher_exploration_id: null
+                },
+                rule_specs: [
+                  {
+                    inputs: {
+                      x: [
+                        '<p><oppia-noninteractive-image filepath-with-value=' +
+                        '"&amp;quot;s4Choice1.png&amp;quot;">' +
+                        '</oppia-noninteractive-image></p>'
+                      ]
+                    },
+                    rule_type: 'Equals'
+                  }
+                ]
+              },
+              {
+                labelled_as_correct: true,
+                outcome: {
+                  dest: 'State 1',
+                  feedback: {
+                    audio_translations: {},
+                    html: 'It is choice number 2'
+                  },
+                  param_changes: [],
+                  refresher_exploration_id: null
+                },
+                rule_specs: [
+                  {
+                    inputs: {
+                      x: [
+                        '<p><oppia-noninteractive-image filepath-with-value=' +
+                        '"&amp;quot;s4Choice2.png&amp;quot;">' +
+                        '</oppia-noninteractive-image></p>'
+                      ]
+                    },
+                    rule_type: 'Equals'
+                  }
+                ]
+              }
+            ]
+          }
+        },
+        'State 5': {
+          classifier_model_id: null,
+          param_changes: [],
+          content: {
+            html: '',
+            audio_translation: {}
+          },
+          interaction: {
+            id: 'ImageClickInput',
+            confirmed_unclassified_answers: [],
+            default_outcome: {
+              dest: 'State 5',
+              feedback: {
+                html: 'Try Again!',
+                audio_translation: {}
+              }
+            },
+            answer_groups: [
+              {
+                labelled_as_correct: false,
+                outcome: {
+                  dest: 'State 5',
+                  feedback: {
+                    audio_translations: {},
+                    html: '<p>That is the class definition. Try again.</p>'
+                  },
+                  param_changes: [],
+                  refresher_exploration_id: null
+                },
+                rule_specs: [{
+                  inputs: {
+                    x: 'classdef'
+                  },
+                  rule_type: 'IsInRegion'
+                }]
+              },
+              {
+                labelled_as_correct: false,
+                outcome: {
+                  dest: 'State 5',
+                  feedback: {
+                    audio_translations: {},
+                    html: '<p>That is a function, which is close to what you' +
+                          'are looking for. Try again!</p>'
+                  },
+                  param_changes: [],
+                  refresher_exploration_id: null
+                },
+                rule_specs: [{
+                  inputs: {
+                    x: 'instancefunc'
+                  },
+                  rule_type: 'IsInRegion'
+                }]
+              },
+              {
+                labelled_as_correct: false,
+                outcome: {
+                  dest: 'State 5',
+                  feedback: {
+                    audio_translations: {},
+                    html: '<p>That is the class docstring. Try again.</p>'
+                  },
+                  param_changes: [],
+                  refresher_exploration_id: null
+                },
+                rule_specs: [{
+                  inputs: {
+                    x: 'docstring'
+                  },
+                  rule_type: 'IsInRegion'
+                }]
+              },
+              {
+                labelled_as_correct: false,
+                outcome: {
+                  dest: 'State 5',
+                  feedback: {
+                    audio_translations: {},
+                    html: "<p>That's a classmethod. It does execute code," +
+                          "but it doesn't construct anything. Try again!</p>"
+                  },
+                  param_changes: [],
+                  refresher_exploration_id: null
+                },
+                rule_specs: [{
+                  inputs: {
+                    x: 'classfunc'
+                  },
+                  rule_type: 'IsInRegion'
+                }]
+              },
+              {
+                labelled_as_correct: false,
+                outcome: {
+                  dest: 'State 1',
+                  feedback: {
+                    audio_translations: {},
+                    html: '<p>You found it! This is the code responsible for' +
+                          'constructing a new class object.</p>'
+                  },
+                  param_changes: [],
+                  refresher_exploration_id: null
+                },
+                rule_specs: [{
+                  inputs: {
+                    x: 'ctor'
+                  },
+                  rule_type: 'IsInRegion'
+                }]
+              }
+            ],
+            customization_args: {
+              highlightRegionsOnHover: {
+                value: true
+              },
+              imageAndRegions: {
+                value: {
+                  imagePath: 's5ImagePath.png',
+                  labeledRegions: [{
+                    label: 'classdef',
+                    region: {
+                      area: [
+                        [0.004291845493562232, 0.004692192192192192],
+                        [0.40987124463519314, 0.05874624624624625]
+                      ],
+                      regionType: 'Rectangle'
+                    }
+                  },
+                  {
+                    label: 'docstring',
+                    region: {
+                      area: [
+                        [0.07296137339055794, 0.06475225225225226],
+                        [0.9892703862660944, 0.1218093093093093]
+                      ],
+                      regionType: 'Rectangle'
+                    }
+                  },
+                  {
+                    label: 'instancefunc',
+                    region: {
+                      area: [
+                        [0.07296137339055794, 0.15183933933933935],
+                        [0.6995708154506438, 0.44012762762762764]
+                      ],
+                      regionType: 'Rectangle'
+                    }
+                  },
+                  {
+                    label: 'classfunc',
+                    region: {
+                      area: [
+                        [0.06866952789699571, 0.46114864864864863],
+                        [0.6931330472103004, 0.776463963963964]
+                      ],
+                      regionType: 'Rectangle'
+                    }
+                  },
+                  {
+                    label: 'ctor',
+                    region: {
+                      area: [
+                        [0.06437768240343347, 0.821509009009009],
+                        [0.740343347639485, 0.9926801801801802]
+                      ],
+                      regionType: 'Rectangle'
+                    }
+                  }]
+                }
+              }
+            },
             hints: [],
             solution: null
           }
@@ -239,19 +516,22 @@ describe('Extracting Image file names in the state service', function() {
                 refresher_exploration_id: null
               }
             }],
-            hints: [
-              {
-                hint_content: {
-                  html: '<p><oppia-noninteractive-image filepath-with-value="&amp;quot;s6Hint1.png&amp;quot;"></oppia-noninteractive-image></p>',
-                  audio_translations: {}
-                }
-              }],
+            hints: [{
+              hint_content: {
+                html: '<p><oppia-noninteractive-image filepath-with-value="' +
+                      '&amp;quot;s6Hint1.png&amp;quot;">' +
+                      '</oppia-noninteractive-image></p>',
+                audio_translations: {}
+              }
+            }],
             solution: {
               answer_is_exclusive: false,
               correct_answer: 'cat',
               explanation: {
                 audio_translations: {},
-                html: '<p><oppia-noninteractive-image filepath-with-value="&amp;quot;s6SolutionExplanation.png&amp;quot;"></oppia-noninteractive-image></p>'
+                html: '<p><oppia-noninteractive-image filepath-with-value="' +
+                      '&amp;quot;s6SolutionExplanation.png&amp;quot;">' +
+                      '</oppia-noninteractive-image></p>'
               }
             },
           },
@@ -263,12 +543,16 @@ describe('Extracting Image file names in the state service', function() {
       version: 1
     };
 
-    
+
     filenamesInExploration = {
       'State 1': [],
       'State 3': [],
-      'State 6': ['s6Hint1.png','s6SolutionExplanation.png'],
-      'Introduction': ['sIMultipleChoice1.png','sIMultipleChoice2.png','sIOutcomeFeedback.png']
+      'State 4': ['s4Content.png', 's4Choice1.png', 's4Choice2.png',
+        's4DefaultOutcomeFeedback.png'],
+      'State 5': ['s5ImagePath.png'],
+      'State 6': ['s6Hint1.png', 's6SolutionExplanation.png'],
+      Introduction: ['sIMultipleChoice1.png', 'sIMultipleChoice2.png',
+        'sIOutcomeFeedback.png']
     };
   }));
 
@@ -276,9 +560,14 @@ describe('Extracting Image file names in the state service', function() {
     function() {
       var exploration = eof.createFromBackendDict(explorationDict);
       var states = exploration.getStates();
-      for (statename in states) {
-        expect(eifss.getImageFilenamesInState(states[statename])).toBe(filenamesInExploration[states[state]]);
-      } 
+      var stateNames = states.getStateNames();
+      stateNames.forEach(function(statename) {
+        var filenamesInState = eifss.getImageFilenamesInState(states
+          .getState(statename));
+        for (x in filenamesInState) {
+          expect(filenamesInState[x]).toBe(
+            (filenamesInExploration[statename])[x]);
+        }
+      });
     });
 });
-  
