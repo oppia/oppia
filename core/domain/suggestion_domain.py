345bd1e844607ebcf478259c6631e23fd21e6935
Nithesh <nithusha21@yahoo.co.in>
2018-05-22 12:53:47 +0530

    added suggestion domain and tests

---
+++ core/domain/suggestion_domain.py
@@ -0,0 +1,103 @@
+# Copyright 2018 The Oppia Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Domain object for Oppia suggestions."""
+
+from core.domain import user_services
+
+
+class Suggestion(object):
+    """Domain object for a suggestion.
+
+    Attributes:
+        suggestion_id: str. The ID of the suggestion.
+        suggestion_type: str. The type of the suggestion.
+        target_type: str. The type of target entity being edited.
+        target_id: str, The ID of the target entity being edited.
+        target_version_at_submission: int. The version number of the target
+            entity at the time of creation of the suggestion.
+        status: str. The status of the suggestion.
+        author_id: str. The ID of the user who submitted the suggestion.
+        assigned_reviewer_id: str. The ID of the user assigned to
+            review the suggestion.
+        final_reviewer_id: str. The ID of the reviewer who has accepted the
+            suggestion.
+        change_cmd: dict. The actual content of the suggestion.
+        score_category: str. The scoring category for the suggestion.
+    """
+
+    def __init__(
+            self, suggestion_id, suggestion_type, target_type, target_id,
+            target_version_at_submission, status, author_id,
+            assigned_reviewer_id, final_reviewer_id, change_cmd,
+            score_category):
+        """Initializes a suggestion object."""
+        self.suggestion_id = suggestion_id
+        self.suggestion_type = suggestion_type
+        self.target_type = target_type
+        self.target_id = target_id
+        self.target_version_at_submission = target_version_at_submission
+        self.status = status
+        self.author_id = author_id
+        self.assigned_reviewer_id = assigned_reviewer_id
+        self.final_reviewer_id = final_reviewer_id
+        self.change_cmd = change_cmd
+        self.score_category = score_category
+
+    def get_author_name(self):
+        """Returns the author's username.
+
+        Returns:
+            str. The username of the author of the suggestion.
+        """
+        return user_services.get_username(self.author_id)
+
+
+    def get_assigned_reviewer_name(self):
+        """Returns the assigned reviewer's username.
+
+        Returns:
+            str. The username of the assigned reviewer of the suggestion.
+        """
+        return user_services.get_username(self.assigned_reviewer_id)
+
+    def get_final_reviewer_name(self):
+        """Returns the final reviewer's username.
+
+        Returns:
+            str. The username of the final reviewer of the suggestion.
+        """
+        if self.final_reviewer_id:
+            return user_services.get_username(self.final_reviewer_id)
+        return ''
+
+    def to_dict(self):
+        """Returns a dict representation of a suggestion object.
+
+        Returns:
+            dict. A dict representation of a suggestion object.
+        """
+        return {
+            'suggestion_id': self.suggestion_id,
+            'suggestion_type': self.suggestion_type,
+            'target_type': self.target_type,
+            'target_id': self.target_id,
+            'target_version_at_submission': self.target_version_at_submission,
+            'status': self.status,
+            'author_name': self.get_author_name(),
+            'final_reviewer_name': self.get_final_reviewer_name(),
+            'assigned_reviewer_name': self.get_assigned_reviewer_name(),
+            'change_cmd': self.change_cmd,
+            'score_category': self.score_category
+        }
---
+++ core/domain/suggestion_domain_test.py
@@ -0,0 +1,69 @@
+# Copyright 2018 The Oppia Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for suggestion domain objects."""
+
+from core.domain import suggestion_domain
+from core.platform import models
+from core.tests import test_utils
+
+(suggestion_models,) = models.Registry.import_models([models.NAMES.suggestion])
+
+
+class SuggestionDomainUnitTests(test_utils.GenericTestBase):
+    """Tests for the suggestion class."""
+
+    AUTHOR_EMAIL = 'author@example.com'
+    REVIEWER_EMAIL = 'reviewer@example.com'
+    ASSIGNED_REVIEWER_EMAIL = 'assigned_reviewer@example.com'
+
+    def setUp(self):
+        super(SuggestionDomainUnitTests, self).setUp()
+
+        self.signup(self.AUTHOR_EMAIL, 'author')
+        self.author_id = self.get_user_id_from_email(self.AUTHOR_EMAIL)
+        self.signup(self.REVIEWER_EMAIL, 'reviewer')
+        self.reviewer_id = self.get_user_id_from_email(self.REVIEWER_EMAIL)
+        self.signup(self.ASSIGNED_REVIEWER_EMAIL, 'assignedReviewer')
+        self.assigned_reviewer_id = self.get_user_id_from_email(
+            self.ASSIGNED_REVIEWER_EMAIL)
+
+    def test_to_dict(self):
+        expected_suggestion_dict = {
+            'suggestion_id': 'exploration.exp1.thread1',
+            'suggestion_type': suggestion_models.SUGGESTION_EDIT_STATE_CONTENT,
+            'target_type': suggestion_models.TARGET_TYPE_EXPLORATION,
+            'target_id': 'exp1',
+            'target_version_at_submission': 1,
+            'status': suggestion_models.STATUS_ACCEPTED,
+            'author_name': 'author',
+            'final_reviewer_name': 'reviewer',
+            'assigned_reviewer_name': 'assignedReviewer',
+            'change_cmd': {},
+            'score_category': 'translation.English'
+        }
+
+        observed_suggestion = suggestion_domain.Suggestion(
+            expected_suggestion_dict['suggestion_id'],
+            expected_suggestion_dict['suggestion_type'],
+            expected_suggestion_dict['target_type'],
+            expected_suggestion_dict['target_id'],
+            expected_suggestion_dict['target_version_at_submission'],
+            expected_suggestion_dict['status'], self.author_id,
+            self.assigned_reviewer_id, self.reviewer_id,
+            expected_suggestion_dict['change_cmd'],
+            expected_suggestion_dict['score_category'])
+
+        self.assertDictEqual(
+            observed_suggestion.to_dict(), expected_suggestion_dict)
