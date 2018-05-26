ec47119e8666ccd3726f8939cc33300fec3ffc55
Apurv-Bajaj <apurvabajaj007@gmail.com>
2018-05-27 02:41:28 +0530

    isolate CoordTwoDimEditor

--- extensions/objects/templates/CoordTwoDimEditor.js
+++ extensions/objects/templates/CoordTwoDimEditor.js
@@ -101,7 +101,9 @@ oppia.directive('coordTwoDimEditor', [
         $compile(element.contents())(scope);
       },
       restrict: 'E',
-      scope: true,
+      scope: {
+        value: '='
+      },
       template: '<span ng-include="getTemplateUrl()"></span>'
     };
   }]);
