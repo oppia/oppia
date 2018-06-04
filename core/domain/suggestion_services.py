46cf7a5419e31961e0f534f5a9da2b08c5c02b15
Nithesh <nithusha21@yahoo.co.in>
2018-06-04 22:07:11 +0530

    more linting fixes

--- core/domain/suggestion_services.py
+++ core/domain/suggestion_services.py
@@ -21,7 +21,6 @@ from core.domain import suggestion_registry
 from core.domain import user_services
 from core.platform import models
 import feconf
-import utils
 
 (feedback_models, suggestion_models) = models.Registry.import_models([
     models.NAMES.feedback, models.NAMES.suggestion])
