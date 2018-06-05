e9275548f7c36ab6dad550e8d16fe0c668fecb1d
Pranav Siddharth S <pranavsid98@gmail.com>
2018-06-05 19:13:07 +0530

    Playthrough Visualisation: Milestone 2.1 (#5015)
    * added exp issue object factory
    
    * learner action object factory
    
    * playthrough object factory
    
    * playthrough object factory tests
    
    * fix lint failure
    
    * addressed set of review comments
    
    * addressed review comments
    
    * addressed another set of changes
    
    * addressed review changes

---
+++ core/templates/dev/head/domain/statistics/LearnerActionObjectFactorySpec.js
@@ -0,0 +1,45 @@
+// Copyright 2018 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Unit tests for the LearnerActionObjectFactory.
+ */
+
+describe('Learner Action Object Factory', function() {
+  beforeEach(module('oppia'));
+
+  beforeEach(inject(function($injector) {
+    this.laof = $injector.get('LearnerActionObjectFactory');
+  }));
+
+  it('should create a new learner action', function() {
+    var learnerActionObject = new this.laof('AnswerSubmit', {}, 1);
+
+    expect(learnerActionObject.actionType).toEqual('AnswerSubmit');
+    expect(learnerActionObject.actionCustomizationArgs).toEqual({});
+    expect(learnerActionObject.schemaVersion).toEqual(1);
+  });
+
+  it('should create a new learner action from a backend dict', function() {
+    var learnerActionObject = this.laof.createFromBackendDict({
+      actionType: 'AnswerSubmit',
+      actionCustomizationArgs: {},
+      schemaVersion: 1
+    });
+
+    expect(learnerActionObject.actionType).toEqual('AnswerSubmit');
+    expect(learnerActionObject.actionCustomizationArgs).toEqual({});
+    expect(learnerActionObject.schemaVersion).toEqual(1);
+  });
+});
