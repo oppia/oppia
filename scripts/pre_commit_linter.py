0759aaf389662e07bb3421920497cdabc8e590d5
Apurv Bajaj <apurvabajaj007@gmail.com>
2018-06-05 08:16:06 +0530

    Improving the dev workflow: Setup lint check for CSS and fix corresponding errors (#4643)
    * Added rules and fixed errors
    
    * Milestone 1 and 2
    
    * Minor Fix
    
    * Review changes
    
    * Revert: oppia.css
    
    * Fix pre-commit script
    
    * lint fix
    
    * fix liny py function
    
    * lint fix
    
    * Modify config
    
    * Modify linter
    
    * Basic Setup
    
    * Setup .stylelintrc for oppia.css and make fixes for css within html pages
    
    * Fix unmet peer dependency(1)
    
    * Run all pre-commit checks
    
    * Modify pre-commit script
    
    * Reduce timeout
    
    * Partial fix of oppia.css
    
    * Partial fix of oppia.css (2)
    
    * Fix errors in oppia.css
    
    * Add newline in .stylelintrc

--- scripts/pre_commit_linter.py
+++ scripts/pre_commit_linter.py
@@ -300,6 +300,55 @@ def _get_all_files_in_directory(dir_path, excluded_glob_patterns):
     return files_in_directory
 
 
+def _lint_css_files(stylelint_path, config_path, files_to_lint, stdout, result):
+    """Prints a list of lint errors in the given list of CSS files.
+
+    Args:
+        stylelint_path: str. Path to the Stylelint binary.
+        config_path: str. Path to the configuration file.
+        files_to_lint: list(str). A list of filepaths to lint.
+        stdout:  multiprocessing.Queue. A queue to store Stylelint outputs.
+        result: multiprocessing.Queue. A queue to put results of test.
+    """
+    start_time = time.time()
+    num_files_with_errors = 0
+
+    num_css_files = len(files_to_lint)
+    if not files_to_lint:
+        result.put('')
+        print 'There are no CSS files to lint.'
+        return
+
+    print 'Total css files: ', num_css_files
+    stylelint_cmd_args = [
+        stylelint_path, '--config=' + config_path]
+    for _, filename in enumerate(files_to_lint):
+        print 'Linting: ', filename
+        proc_args = stylelint_cmd_args + [filename]
+        proc = subprocess.Popen(
+            proc_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        linter_stdout, linter_stderr = proc.communicate()
+        if linter_stderr:
+            print 'LINTER FAILED'
+            print linter_stderr
+            sys.exit(1)
+
+        if linter_stdout:
+            num_files_with_errors += 1
+            print linter_stdout
+            stdout.put(linter_stdout)
+
+    if num_files_with_errors:
+        result.put('%s    %s CSS file' % (
+            _MESSAGE_TYPE_FAILED, num_files_with_errors))
+    else:
+        result.put('%s   %s CSS file linted (%.1f secs)' % (
+            _MESSAGE_TYPE_SUCCESS, num_css_files, time.time() - start_time))
+
+    print 'CSS linting finished.'
+
+
 def _lint_js_files(
         node_path, eslint_path, files_to_lint, stdout, result):
     """Prints a list of lint errors in the given list of JavaScript files.
@@ -310,9 +359,6 @@ def _lint_js_files(
         files_to_lint: list(str). A list of filepaths to lint.
         stdout:  multiprocessing.Queue. A queue to store ESLint outputs.
         result: multiprocessing.Queue. A queue to put results of test.
-
-    Returns:
-        None
     """
     start_time = time.time()
     num_files_with_errors = 0
@@ -359,9 +405,6 @@ def _lint_py_files(config_pylint, config_pycodestyle, files_to_lint, result):
         config_pycodestyle: str. Path to the tox.ini file.
         files_to_lint: list(str). A list of filepaths to lint.
         result: multiprocessing.Queue. A queue to put results of test.
-
-    Returns:
-        None
     """
     start_time = time.time()
     are_there_errors = False
@@ -526,39 +569,74 @@ def _pre_commit_linter(all_files):
         parent_dir, 'oppia_tools', 'node-6.9.1', 'bin', 'node')
     eslint_path = os.path.join(
         parent_dir, 'node_modules', 'eslint', 'bin', 'eslint.js')
-
-    if not os.path.exists(eslint_path):
+    stylelint_path = os.path.join(
+        parent_dir, 'node_modules', 'stylelint', 'bin', 'stylelint.js')
+    config_path_for_css_in_html = os.path.join(
+        parent_dir, 'oppia', '.stylelintrc')
+    config_path_for_oppia_css = os.path.join(
+        parent_dir, 'oppia', 'core', 'templates', 'dev', 'head',
+        'css', '.stylelintrc')
+    if not (os.path.exists(eslint_path) and os.path.exists(stylelint_path)):
         print ''
         print 'ERROR    Please run start.sh first to install node-eslint '
-        print '         and its dependencies.'
+        print '         or node-stylelint and its dependencies.'
         sys.exit(1)
 
     js_files_to_lint = [
         filename for filename in all_files if filename.endswith('.js')]
     py_files_to_lint = [
         filename for filename in all_files if filename.endswith('.py')]
+    html_files_to_lint_for_css = [
+        filename for filename in all_files if filename.endswith('.html')]
+    css_files_to_lint = [
+        filename for filename in all_files if filename.endswith('oppia.css')]
+
+    css_in_html_result = multiprocessing.Queue()
+    css_in_html_stdout = multiprocessing.Queue()
+
+    linting_processes = []
+    linting_processes.append(multiprocessing.Process(
+        target=_lint_css_files, args=(
+            stylelint_path,
+            config_path_for_css_in_html,
+            html_files_to_lint_for_css, css_in_html_stdout,
+            css_in_html_result)))
+
+    css_result = multiprocessing.Queue()
+    css_stdout = multiprocessing.Queue()
+
+    linting_processes.append(multiprocessing.Process(
+        target=_lint_css_files, args=(
+            stylelint_path,
+            config_path_for_oppia_css,
+            css_files_to_lint, css_stdout,
+            css_result)))
 
     js_result = multiprocessing.Queue()
-    linting_processes = []
     js_stdout = multiprocessing.Queue()
+
     linting_processes.append(multiprocessing.Process(
         target=_lint_js_files, args=(
             node_path, eslint_path, js_files_to_lint,
             js_stdout, js_result)))
 
     py_result = multiprocessing.Queue()
+
     linting_processes.append(multiprocessing.Process(
         target=_lint_py_files,
         args=(config_pylint, config_pycodestyle, py_files_to_lint, py_result)))
-    print 'Starting Javascript and Python Linting'
+
+    print 'Starting CSS, Javascript and Python Linting'
     print '----------------------------------------'
+
     for process in linting_processes:
+        process.daemon = True
         process.start()
 
     for process in linting_processes:
         # Require timeout parameter to prevent against endless waiting for the
         # linting function to return.
-        process.join(timeout=600)
+        process.join(timeout=200)
 
     js_messages = []
     while not js_stdout.empty():
@@ -568,6 +646,8 @@ def _pre_commit_linter(all_files):
     print '\n'.join(js_messages)
     print '----------------------------------------'
     summary_messages = []
+    summary_messages.append(css_in_html_result.get())
+    summary_messages.append(css_result.get())
     summary_messages.append(js_result.get())
     summary_messages.append(py_result.get())
     print '\n'.join(summary_messages)
