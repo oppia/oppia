ba2833f7f790bf9d18d9e7458208e8187b3e2683
Nitish Bansal <nitishbansal2297@gmail.com>
2018-06-07 04:25:25 +0530

    added one off job for pre migration testing

--- core/domain/exp_jobs_one_off.py
+++ core/domain/exp_jobs_one_off.py
@@ -25,6 +25,7 @@ from constants import constants
 from core import jobs
 from core.domain import exp_domain
 from core.domain import exp_services
+from core.domain import html_cleaner
 from core.domain import rights_manager
 from core.platform import models
 import feconf
@@ -506,6 +507,78 @@ class ExplorationContentValidationJob(jobs.BaseMapReduceOneOffJobManager):
 
         for html_data in html_list:
             soup = bs4.BeautifulSoup(html_data, 'html.parser')
+
+            # Text with no parent tag is also invalid.
+            for content in soup.contents:
+                if not content.name:
+                    err_dict['strings'].append(html_data)
+                    break
+
+            for tag in soup.findAll():
+                # Checking for tags not allowed in RTE.
+                if tag.name not in allowed_tag_list:
+                    if 'invalidTags' in err_dict:
+                        err_dict['invalidTags'].append(tag.name)
+                    else:
+                        err_dict['invalidTags'] = [tag.name]
+                    err_dict['strings'].append(html_data)
+                # Checking for parent-child relation that are not
+                # allowed in RTE.
+                parent = tag.parent.name
+                if (tag.name in allowed_tag_list) and (
+                        parent not in allowed_parent_list[tag.name]):
+                    if tag.name in err_dict:
+                        err_dict[tag.name].append(parent)
+                    else:
+                        err_dict[tag.name] = [parent]
+                    err_dict['strings'].append(html_data)
+
+        for key in err_dict:
+            if err_dict[key]:
+                yield(key, list(set(err_dict[key])))
+
+    @staticmethod
+    def reduce(key, values):
+        final_values = [ast.literal_eval(value) for value in values]
+        # Combine all values from multiple lists into a single list
+        # for that error type.
+        yield (key, list(set().union(*final_values)))
+
+
+class ExplorationMigrationValidationJob(jobs.BaseMapReduceOneOffJobManager):
+    """Job that migrates the html content of an exploration into the valid
+    Textangular format and validates it.
+    """
+
+    @classmethod
+    def entity_classes_to_map_over(cls):
+        return [exp_models.ExplorationModel]
+
+    @staticmethod
+    def map(item):
+        if item.deleted:
+            return
+
+        exploration = exp_services.get_exploration_from_model(item)
+        # err_dict is a dictionary to store the invalid tags and the
+        # invalid parent-child relations that we find.
+        err_dict = {}
+
+        # All the invalid html strings will be stored in this.
+        err_dict['strings'] = []
+
+        allowed_parent_list = (
+            feconf.RTE_CONTENT_SPEC['RTE_TYPE_TEXTANGULAR']
+            ['ALLOWED_PARENT_LIST'])
+        allowed_tag_list = (
+            feconf.RTE_CONTENT_SPEC['RTE_TYPE_TEXTANGULAR']
+            ['ALLOWED_TAG_LIST'])
+
+        html_list = exploration.get_all_html_content_strings()
+
+        for html_data in html_list:
+            migrated_data = html_cleaner.convert_to_text_angular(html_data)
+            soup = bs4.BeautifulSoup(migrated_data, 'html.parser')
 
             # Text with no parent tag is also invalid.
             for content in soup.contents:
