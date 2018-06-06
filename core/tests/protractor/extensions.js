7157e7d321550e3806495950470962c114603c36
Sandeep Dubey <dubeysandeep.in@gmail.com>
2018-06-06 15:28:37 +0530

    Added error logs as expected error

--- core/tests/protractor/extensions.js
+++ core/tests/protractor/extensions.js
@@ -98,6 +98,10 @@ describe('rich-text components', function() {
       'chrome-extension://invalid/ - Failed to load resource: net::ERR_FAILED',
       'Error parsing header X-XSS-Protection: 1; mode=block; ' +
       'report=https:\/\/www.google.com\/appserve\/security-bugs\/log\/youtube:',
+
+      'https://www.youtube.com/youtubei/v1/log_interaction?alt=json&' +
+      'key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8 - Failed to load resource:' +
+      'the server responded with a status of 401 ()',
     ]);
   });
 });
