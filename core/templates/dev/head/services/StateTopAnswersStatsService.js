9e1f3f8a62b33928c60a020b300043667f5a80ec
Brian Rodriguez <brian@brianrodri.com>
2018-06-08 13:06:35 -0400

    Initial commit for hooks.

--- core/templates/dev/head/services/StateTopAnswersStatsService.js
+++ core/templates/dev/head/services/StateTopAnswersStatsService.js
@@ -47,18 +47,41 @@ oppia.factory('StateTopAnswersStatsService', [
       });
     };
 
+    var deleteState = function(stateName) {
+      // No action needed, it's helpful to keep the old state data around just
+      // in case a user decides to revert their changes.
+    };
+
+    var addState = function(stateName) {
+      if (stateTopAnswerStatsCache.hasOwnProperty(stateName)) {
+        // No action needed, we already have data available for this state.
+      } else {
+        // Prepare an empty set of answers, after all, a brand new state
+        // couldn't possibly have any answers yet.
+        stateTopAnswerStatsCache[stateName] = [];
+      }
+    };
+
     return {
       /**
        * Calls the backend asynchronously to setup the answer statistics of each
        * state this exploration contains.
+       *
+       * @param {?string[]} stateNames - Optional collection of states already
+       *    known to exist. When they are not found in the backend, they will be
+       *    given an empty set of top answers.
        */
-      init: function() {
+      init: function(stateNames) {
+        stateNames = stateNames || [];
         $http.get(
           UrlInterpolationService.interpolateUrl(
             '/createhandler/state_answer_stats/<exploration_id>',
             {exploration_id: ExplorationContextService.getExplorationId()})
         ).then(function(response) {
           stateTopAnswerStatsCache = {};
+          stateNames.forEach(function(stateName) {
+            stateTopAnswerStatsCache[stateName] = [];
+          });
           Object.keys(response.data.answers).forEach(function(stateName) {
             var answerStatsBackendDicts = response.data.answers[stateName];
             stateTopAnswerStatsCache[stateName] = answerStatsBackendDicts.map(
@@ -84,6 +107,25 @@ oppia.factory('StateTopAnswersStatsService', [
        */
       refreshStateStats: function(stateName) {
         refreshAddressedInfo(stateName);
+      },
+
+      onStateDelete: function(stateName) {
+        deleteState(stateName);
+      },
+
+      onStateAdd: function(stateName) {
+        addState(stateName);
+      },
+
+      onStateChangeAnswerGroups: function(stateName) {
+        refreshAddressedInfo(stateName);
+      },
+
+      onStateChangeName: function(oldStateName, newStateName) {
+        var oldStateAnswerStats = stateTopAnswerStatsCache[oldStateName];
+        deleteState(oldStateName);
+        addState(newStateName);
+        stateTopAnswerStatsCache[newStateName] = oldStateAnswerStats;
       },
     };
   }
