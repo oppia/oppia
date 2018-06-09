1a5c1257f5aef4bb607613c41f2f315963bbc9d2
Brian Rodriguez <brian@brianrodri.com>
2018-06-09 13:23:58 -0400

    Add createSampleBackendDict test for StatesObjectFactory.

--- core/templates/dev/head/domain/exploration/StatesObjectFactorySpec.js
+++ core/templates/dev/head/domain/exploration/StatesObjectFactorySpec.js
@@ -288,5 +288,15 @@ describe('States object factory', function() {
           })]
         });
     });
+
+    describe('.createSampleBackendDict', function() {
+      it('is accepted by .createFromBackendDict', function() {
+        var sampleBackendDict = ssof.createSampleBackendDict();
+
+        expect(function() {
+          ssof.createFromBackendDict(sampleBackendDict);
+        }).not.toThrow();
+      });
+    })
   });
 });
