95cc39b9442f21dc4e2641d88216c8d3154f91f1
Siddhant Srivastav <harshsrivastav123@gmail.com>
2018-06-10 08:27:28 +0530

    Update gae_models_test.py

--- core/storage/user/gae_models_test.py
+++ core/storage/user/gae_models_test.py
@@ -216,7 +216,7 @@ class UserSkillMasteryModelTests(test_utils.GenericTestBase):
 
     def test_get_failure(self):
         retrieved_object = user_models.UserSkillMasteryModel.get(
-            'unknown_model_id', False)
+            'unknown_model_id', strict=False)
 
         self.assertEqual(retrieved_object, None)
 
