a5c68da8077725d5c69708b87ffc15599241591f
Brian Rodriguez <brian@brianrodri.com>
2018-06-17 13:02:22 -0400

    Newline for readability.

--- core/templates/dev/head/pages/exploration_editor/ExplorationStatesServiceSpec.js
+++ core/templates/dev/head/pages/exploration_editor/ExplorationStatesServiceSpec.js
@@ -21,6 +21,7 @@ describe('ExplorationStatesService', function() {
 
   beforeEach(inject(function($injector) {
     this.ess = $injector.get('ExplorationStatesService');
+
     spyOn($injector.get('ExplorationContextService'), 'getExplorationId')
       .and.returnValue('7');
   }));
