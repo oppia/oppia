c107ce852855f37eb721bcb2a9410055d37dbc7e
Vojtěch Jelínek <vojtin.j@gmail.com>
2018-06-22 11:29:07 +0200

    fix tests

--- core/templates/dev/head/pages/collection_editor/editor_tab/CollectionNodeCreatorDirective.js
+++ core/templates/dev/head/pages/collection_editor/editor_tab/CollectionNodeCreatorDirective.js
@@ -75,8 +75,10 @@ oppia.directive('collectionNodeCreator', [
 
           var isValidSearchQuery = function(searchQuery) {
             // Allow underscores because they are allowed in exploration IDs.
-            var INVALID_SEARCH_CHARS = INVALID_NAME_CHARS.replace(
-              '_', '');
+            var INVALID_SEARCH_CHARS = (
+              INVALID_NAME_CHARS.filter(function(item) {
+                return item !== '_';
+              }));
             for (var i = 0; i < INVALID_SEARCH_CHARS.length; i++) {
               if (searchQuery.indexOf(INVALID_SEARCH_CHARS[i]) !== -1) {
                 return false;
