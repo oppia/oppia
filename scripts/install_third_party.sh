3b32eb0c41ef055c6153eeb3fa5eadd8c6d6c019
Apurv-Bajaj <apurvabajaj007@gmail.com>
2018-06-23 23:02:48 +0530

    Remove all options

--- scripts/install_third_party.sh
+++ scripts/install_third_party.sh
@@ -111,11 +111,7 @@ echo Checking if pylint is installed in $TOOLS_DIR/pip_packages
 if [ ! -d "$TOOLS_DIR/pylint-1.8.4" ]; then
   echo Installing Pylint
 
-  # See https://stackoverflow.com/a/4495175 for the rationale behind the "user"
-  # and "prefix" flags. --system is used for Debian systems (cf. `pip install --help`).
-  # The "user" tag does not work for virtualenv since user site-packages are not visible
-  # in the virtualenv created by Travis.
-  pip install pylint==1.8.4 --target="$TOOLS_DIR/pylint-1.8.4" --prefix=--system
+  pip install pylint==1.8.4 --target="$TOOLS_DIR/pylint-1.8.4"
   # Add __init__.py file so that pylint dependency backports are resolved
   # correctly.
   touch $TOOLS_DIR/pylint-1.8.4/backports/__init__.py
@@ -166,22 +162,14 @@ echo Checking if browsermob-proxy is installed in $TOOLS_DIR/pip_packages
 if [ ! -d "$TOOLS_DIR/browsermob-proxy-0.7.1" ]; then
   echo Installing browsermob-proxy
 
-  # See https://stackoverflow.com/a/4495175 for the rationale behind the "user"
-  # and "prefix" flags. --system is used for Debian systems (cf. `pip install --help`).
-  # The "user" tag does not work for virtualenv since user site-packages are not visible
-  # in the virtualenv created by Travis.
-  pip install browsermob-proxy==0.7.1 --target="$TOOLS_DIR/browsermob-proxy-0.7.1" --prefix=--system
+  pip install browsermob-proxy==0.7.1 --target="$TOOLS_DIR/browsermob-proxy-0.7.1"
 fi
 
 echo Checking if selenium is installed in $TOOLS_DIR/pip_packages
 if [ ! -d "$TOOLS_DIR/selenium-2.53.2" ]; then
   echo Installing selenium
 
-  # See https://stackoverflow.com/a/4495175 for the rationale behind the "user"
-  # and "prefix" flags. --system is used for Debian systems (cf. `pip install --help`).
-  # The "user" tag does not work for virtualenv since user site-packages are not visible
-  # in the virtualenv created by Travis.
-  pip install selenium==2.53.2 --target="$TOOLS_DIR/selenium-2.53.2" --prefix=--system
+  pip install selenium==2.53.2 --target="$TOOLS_DIR/selenium-2.53.2"
 fi
 
 # install pre-push script
