70e13e458a1fec11eaad9b798e18b7d85bdffa46
Pranav Siddharth S <pranavsid98@gmail.com>
2018-06-25 19:37:39 +0530

    Added unit test for exploration reverts effect on exploration issues model (#5147)
    * added test to check reverts
    
    * fix backend test
    
    * Addressed all review comments
    
    * Addressed comment

--- core/domain/stats_services.py
+++ core/domain/stats_services.py
@@ -291,8 +291,8 @@ def update_exp_issues_for_new_exp_version(
 
     Args:
         exploration: Exploration. Domain object for the exploration.
-        exp_versions_diff: ExplorationVersionsDiff. The domain object for the
-            exploration versions difference.
+        exp_versions_diff: ExplorationVersionsDiff|None. The domain object for
+            the exploration versions difference, None if it is a revert.
         revert_to_version: int|None. If the change is a revert, the version.
             Otherwise, None.
     """
@@ -305,8 +305,12 @@ def update_exp_issues_for_new_exp_version(
     # Handling reverts.
     if revert_to_version:
         old_exp_issues = get_exp_issues(exploration.id, revert_to_version)
-        exp_issues.unresolved_issues = old_exp_issues.unresolved_issues
-        exp_issues.exp_version += 1
+        # If the old exploration issues model doesn't exist, the current model
+        # is carried over (this is a fallback case for some tests, and can
+        # never happen in production.)
+        if old_exp_issues:
+            exp_issues.unresolved_issues = old_exp_issues.unresolved_issues
+        exp_issues.exp_version = exploration.version + 1
         create_exp_issues_model(exp_issues)
         return
 
