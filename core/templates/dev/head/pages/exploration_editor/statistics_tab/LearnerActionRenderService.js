1ece773b4a4b7fce1a599eb7ade639c03847ecd7
Pranav Siddharth <pranavsid98@gmail.com>
2018-06-29 16:24:02 +0530

    Added LARS service and specs

---
+++ core/templates/dev/head/pages/exploration_editor/statistics_tab/LearnerActionRenderService.js
@@ -0,0 +1,158 @@
+// Copyright 2018 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Service for rendering learner action HTML strings.
+ */
+
+oppia.factory('LearnerActionRenderService', [
+  'ACTION_TYPE_ANSWER_SUBMIT', 'ACTION_TYPE_EXPLORATION_START',
+  'ACTION_TYPE_EXPLORATION_QUIT', 'ISSUE_TYPE_MULTIPLE_INCORRECT_SUBMISSIONS',
+  function(
+      ACTION_TYPE_ANSWER_SUBMIT, ACTION_TYPE_EXPLORATION_START,
+      ACTION_TYPE_EXPLORATION_QUIT, ISSUE_TYPE_MULTIPLE_INCORRECT_SUBMISSIONS) {
+    var renderExplorationStartActionHTML = function(stateName) {
+      var htmlString =
+        'Started exploration at card "' + stateName + '".<br><br>';
+      return htmlString;
+    };
+
+    var renderExplorationQuitActionHTML = function(
+        stateName, timeSpentInStateSecs) {
+      var htmlString =
+        'Left the exploration after spending a total of ' +
+        timeSpentInStateSecs + ' seconds on card "' + stateName +
+        '".<br><br>';
+      return htmlString;
+    };
+
+    var renderContinueButtonSubmitActionHTML = function(
+        stateName, timeSpentInStateSecs) {
+      var htmlString =
+        'Pressed "Continue" to move to card "' + stateName + '" after ' +
+        timeSpentInStateSecs + ' seconds.<br><br>';
+      return htmlString;
+    };
+
+    var renderAnswerSubmitActionHTML = function(
+        answer, destStateName, timeSpentInStateSecs, currentStateName) {
+      var htmlString;
+      if (currentStateName === destStateName) {
+        htmlString =
+          'Submitted answer "' + answer + '" in card "' + currentStateName +
+          '".<br><br>';
+      } else {
+        htmlString =
+          'Submitted answer "' + answer + '" and moved to card "' +
+          destStateName + '" after spending ' + timeSpentInStateSecs +
+          ' seconds on card "' + currentStateName + '".<br><br>';
+      }
+      return htmlString;
+    };
+
+    var renderLearnerActionsTableForMultipleIncorrectIssue = function(
+        finalBlock) {
+      var index = finalBlock.length - 1;
+      var stateName =
+        finalBlock[index].actionCustomizationArgs.state_name.value;
+
+      var tableHTML = '<table><tr><th>Answer</th><th>Feedback</th></tr>';
+      for (var i = 0; i < index; i++) {
+        if (finalBlock[i].actionType !== ACTION_TYPE_ANSWER_SUBMIT) {
+          continue;
+        }
+        var answer = finalBlock[i].actionCustomizationArgs.answer.value;
+        var feedback = finalBlock[i].actionCustomizationArgs.feedback.value;
+        tableHTML +=
+          '<tr><td>' + answer + '</td><td>' + feedback + '</td></tr>';
+      }
+      tableHTML += '</table>';
+      return tableHTML;
+    };
+
+    var renderLearnerActionHTML = function(learnerAction) {
+      var actionType = learnerAction.actionType;
+      var custArgs = learnerAction.actionCustomizationArgs;
+      if (actionType === ACTION_TYPE_EXPLORATION_START) {
+        return renderExplorationStartActionHTML(custArgs.state_name.value);
+      } else if (actionType === ACTION_TYPE_EXPLORATION_QUIT) {
+        return renderExplorationQuitActionHTML(
+          custArgs.state_name.value, custArgs.time_spent_in_state_secs.value);
+      } else if (actionType === ACTION_TYPE_ANSWER_SUBMIT) {
+        interactionId =
+          custArgs.interaction_id.value;
+        if (interactionId === 'Continue') {
+          return renderContinueButtonSubmitActionHTML(
+            custArgs.dest_state_name.value,
+            custArgs.time_spent_in_state_secs.value);
+        }
+        if (custArgs.answer.value) {
+          return renderAnswerSubmitActionHTML(
+            custArgs.answer.value, custArgs.dest_state_name.value,
+            custArgs.time_spent_in_state_secs.value, custArgs.state_name.value
+          );
+        }
+      }
+    };
+
+    return {
+      renderFinalDisplayBlockForMISIssueHTML: function(block) {
+        var index = block.length - 1;
+        var stateName = block[index].actionCustomizationArgs.state_name.value;
+        var htmlString = '';
+        for (
+          var i = 0; block[i].actionType !== ACTION_TYPE_ANSWER_SUBMIT; i++) {
+          htmlString += renderLearnerActionHTML(block[i]);
+        }
+        htmlString +=
+          'Submitted the following answers in card "' + stateName + '"<br><br>';
+        htmlString += renderLearnerActionsTableForMultipleIncorrectIssue(block);
+        htmlString += '<br><br>';
+        htmlString += renderLearnerActionHTML(block[index]);
+        return htmlString;
+      },
+      renderDisplayBlockHTML: function(block) {
+        var htmlString = '';
+        for (var i = 0; i < block.length; i++) {
+          htmlString += renderLearnerActionHTML(block[i]);
+        }
+        return htmlString;
+      },
+      getDisplayBlocks: function(learnerActions) {
+        var displayBlocks = [];
+        var lastIndex = learnerActions.length - 1;
+        var localBlock = [learnerActions[lastIndex]];
+        var stateName =
+          learnerActions[lastIndex].actionCustomizationArgs.state_name.value;
+        for (var i = lastIndex - 1; i >= 0; i--) {
+          var action = learnerActions[i];
+          if (action.actionCustomizationArgs.state_name.value !== stateName) {
+            stateName = action.actionCustomizationArgs.state_name.value;
+            if (localBlock.length < 4) {
+              localBlock.unshift(action);
+              continue;
+            }
+            displayBlocks.push(localBlock);
+            localBlock = [action];
+          } else {
+            localBlock.unshift(action);
+          }
+        }
+        if (localBlock) {
+          displayBlocks.push(localBlock);
+        }
+        return displayBlocks;
+      }
+    };
+  }]);
---
+++ core/templates/dev/head/pages/exploration_editor/statistics_tab/LearnerActionRenderServiceSpec.js
@@ -0,0 +1,310 @@
+// Copyright 2018 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Unit tests for the learner action render service.
+ */
+
+describe('Learner Action Render Service', function() {
+  beforeEach(module('oppia'));
+
+  describe('Test learner action render service functions', function() {
+    beforeEach(inject(function($injector) {
+      this.laof = $injector.get('LearnerActionObjectFactory');
+      this.ps = $injector.get('PlaythroughService');
+      spyOn(this.ps, 'isPlayerExcludedFromSamplePopulation').and.returnValue(
+        false);
+      this.lars = $injector.get('LearnerActionRenderService');
+      this.ps.initSession('expId1', 1);
+    }));
+
+    it('should split up EarlyQuit learner actions into display blocks.',
+      function() {
+        this.ps.recordExplorationStartAction('stateName1');
+        this.ps.recordAnswerSubmitAction(
+          'stateName1', 'stateName2', 'Continue', '', 'Welcome', 30);
+        this.ps.recordAnswerSubmitAction(
+          'stateName2', 'stateName2', 'TextInput', 'Hello', 'Try again', 30);
+        this.ps.recordExplorationQuitAction('stateName2', 120);
+
+        var learnerActions = this.ps.getPlaythrough().actions;
+
+        var displayBlocks = this.lars.getDisplayBlocks(learnerActions);
+        expect(displayBlocks.length).toEqual(1);
+        expect(displayBlocks[0].length).toEqual(4);
+        expect(displayBlocks[0][0]).toEqual(this.laof.createNew(
+          'ExplorationStart', {
+            state_name: {
+              value: 'stateName1'
+            }
+          }, 1
+        ));
+        expect(displayBlocks[0][3]).toEqual(this.laof.createNew(
+          'ExplorationQuit', {
+            state_name: {
+              value: 'stateName2'
+            },
+            time_spent_in_state_secs: {
+              value: 120
+            }
+          }, 1
+        ));
+      });
+
+    it('should split up many learner actions into different display blocks.',
+      function() {
+        this.ps.recordExplorationStartAction('stateName1');
+        this.ps.recordAnswerSubmitAction(
+          'stateName1', 'stateName2', 'TextInput', 'Hello', 'Try again', 30);
+        this.ps.recordAnswerSubmitAction(
+          'stateName2', 'stateName3', 'TextInput', 'Hello', 'Try again', 30);
+        this.ps.recordAnswerSubmitAction(
+          'stateName3', 'stateName1', 'TextInput', 'Hello', 'Try again', 30);
+        this.ps.recordAnswerSubmitAction(
+          'stateName1', 'stateName2', 'TextInput', 'Hello', 'Try again', 30);
+        this.ps.recordAnswerSubmitAction(
+          'stateName2', 'stateName3', 'TextInput', 'Hello', 'Try again', 30);
+        this.ps.recordAnswerSubmitAction(
+          'stateName3', 'stateName1', 'TextInput', 'Hello', 'Try again', 30);
+        this.ps.recordAnswerSubmitAction(
+          'stateName1', 'stateName2', 'TextInput', 'Hello', 'Try again', 30);
+        this.ps.recordAnswerSubmitAction(
+          'stateName2', 'stateName3', 'TextInput', 'Hello', 'Try again', 30);
+        this.ps.recordAnswerSubmitAction(
+          'stateName3', 'stateName1', 'TextInput', 'Hello', 'Try again', 30);
+        this.ps.recordExplorationQuitAction('stateName1', 120);
+
+        var learnerActions = this.ps.getPlaythrough().actions;
+
+        var displayBlocks = this.lars.getDisplayBlocks(learnerActions);
+        expect(displayBlocks.length).toEqual(3);
+        expect(displayBlocks[0].length).toEqual(4);
+        expect(displayBlocks[1].length).toEqual(4);
+        expect(displayBlocks[2].length).toEqual(3);
+
+        expect(displayBlocks[2][0]).toEqual(this.laof.createNew(
+          'ExplorationStart', {
+            state_name: {
+              value: 'stateName1'
+            }
+          }, 1
+        ));
+        expect(displayBlocks[2][2]).toEqual(this.laof.createNew(
+          'AnswerSubmit', {
+            state_name: {
+              value: 'stateName2'
+            },
+            dest_state_name: {
+              value: 'stateName3'
+            },
+            interaction_id: {
+              value: 'TextInput'
+            },
+            answer: {
+              value: 'Hello'
+            },
+            feedback: {
+              value: 'Try again'
+            },
+            time_spent_in_state_secs: {
+              value: 30
+            }
+          }, 1
+        ));
+
+        expect(displayBlocks[1][0]).toEqual(this.laof.createNew(
+          'AnswerSubmit', {
+            state_name: {
+              value: 'stateName3'
+            },
+            dest_state_name: {
+              value: 'stateName1'
+            },
+            interaction_id: {
+              value: 'TextInput'
+            },
+            answer: {
+              value: 'Hello'
+            },
+            feedback: {
+              value: 'Try again'
+            },
+            time_spent_in_state_secs: {
+              value: 30
+            }
+          }, 1
+        ));
+        expect(displayBlocks[1][3]).toEqual(this.laof.createNew(
+          'AnswerSubmit', {
+            state_name: {
+              value: 'stateName3'
+            },
+            dest_state_name: {
+              value: 'stateName1'
+            },
+            interaction_id: {
+              value: 'TextInput'
+            },
+            answer: {
+              value: 'Hello'
+            },
+            feedback: {
+              value: 'Try again'
+            },
+            time_spent_in_state_secs: {
+              value: 30
+            }
+          }, 1
+        ));
+
+        expect(displayBlocks[0][0]).toEqual(this.laof.createNew(
+          'AnswerSubmit', {
+            state_name: {
+              value: 'stateName1'
+            },
+            dest_state_name: {
+              value: 'stateName2'
+            },
+            interaction_id: {
+              value: 'TextInput'
+            },
+            answer: {
+              value: 'Hello'
+            },
+            feedback: {
+              value: 'Try again'
+            },
+            time_spent_in_state_secs: {
+              value: 30
+            }
+          }, 1
+        ));
+        expect(displayBlocks[0][3]).toEqual(this.laof.createNew(
+          'ExplorationQuit', {
+            state_name: {
+              value: 'stateName1'
+            },
+            time_spent_in_state_secs: {
+              value: 120
+            }
+          }, 1
+        ));
+      });
+
+    it('should assign multiple learner actions at same state to same block.',
+      function() {
+        this.ps.recordExplorationStartAction('stateName1');
+        this.ps.recordAnswerSubmitAction(
+          'stateName1', 'stateName1', 'TextInput', 'Hello', 'Try again', 30);
+        this.ps.recordAnswerSubmitAction(
+          'stateName1', 'stateName1', 'TextInput', 'Hello', 'Try again', 30);
+        this.ps.recordAnswerSubmitAction(
+          'stateName1', 'stateName1', 'TextInput', 'Hello', 'Try again', 30);
+        this.ps.recordAnswerSubmitAction(
+          'stateName1', 'stateName1', 'TextInput', 'Hello', 'Try again', 30);
+        this.ps.recordAnswerSubmitAction(
+          'stateName1', 'stateName1', 'TextInput', 'Hello', 'Try again', 30);
+        this.ps.recordAnswerSubmitAction(
+          'stateName1', 'stateName1', 'TextInput', 'Hello', 'Try again', 30);
+        this.ps.recordExplorationQuitAction('stateName1', 120);
+
+        var learnerActions = this.ps.getPlaythrough().actions;
+
+        var displayBlocks = this.lars.getDisplayBlocks(learnerActions);
+        expect(displayBlocks.length).toEqual(1);
+        expect(displayBlocks[0].length).toEqual(8);
+        expect(displayBlocks[0][0]).toEqual(this.laof.createNew(
+          'ExplorationStart', {
+            state_name: {
+              value: 'stateName1'
+            }
+          }, 1
+        ));
+        expect(displayBlocks[0][7]).toEqual(this.laof.createNew(
+          'ExplorationQuit', {
+            state_name: {
+              value: 'stateName1'
+            },
+            time_spent_in_state_secs: {
+              value: 120
+            }
+          }, 1
+        ));
+      });
+
+    it('should render tables for MultipleIncorrectSubmissions issue block.',
+      function() {
+        this.ps.recordExplorationStartAction('stateName1');
+        this.ps.recordAnswerSubmitAction(
+          'stateName1', 'stateName1', 'TextInput', 'Hello', 'Try again', 30);
+        this.ps.recordAnswerSubmitAction(
+          'stateName1', 'stateName1', 'TextInput', 'Hello', 'Try again', 30);
+        this.ps.recordAnswerSubmitAction(
+          'stateName1', 'stateName1', 'TextInput', 'Hello', 'Try again', 30);
+        this.ps.recordAnswerSubmitAction(
+          'stateName1', 'stateName1', 'TextInput', 'Hello', 'Try again', 30);
+        this.ps.recordAnswerSubmitAction(
+          'stateName1', 'stateName1', 'TextInput', 'Hello', 'Try again', 30);
+        this.ps.recordExplorationQuitAction('stateName1', 120);
+
+        var learnerActions = this.ps.getPlaythrough().actions;
+
+        var displayBlocks = this.lars.getDisplayBlocks(learnerActions);
+
+        expect(displayBlocks.length).toEqual(1);
+
+        var finalBlockHTML = this.lars.renderFinalDisplayBlockForMISIssueHTML(
+          displayBlocks[0]);
+
+        expect(finalBlockHTML).toEqual(
+          'Started exploration at card "stateName1".<br><br>' +
+          'Submitted the following answers in card "stateName1"<br><br>' +
+          '<table><tr><th>Answer</th><th>Feedback</th></tr>' +
+          '<tr><td>Hello</td><td>Try again</td></tr>' +
+          '<tr><td>Hello</td><td>Try again</td></tr>' +
+          '<tr><td>Hello</td><td>Try again</td></tr>' +
+          '<tr><td>Hello</td><td>Try again</td></tr>' +
+          '<tr><td>Hello</td><td>Try again</td></tr></table><br><br>' +
+          'Left the exploration after spending a total of 120 seconds on card' +
+          ' "stateName1".<br><br>'
+        );
+      });
+
+    it('should render HTML for learner action display blocks.', function() {
+      this.ps.recordExplorationStartAction('stateName1');
+      this.ps.recordAnswerSubmitAction(
+        'stateName1', 'stateName2', 'Continue', '', 'Welcome', 30);
+      this.ps.recordAnswerSubmitAction(
+        'stateName2', 'stateName2', 'TextInput', 'Hello', 'Try again', 30);
+      this.ps.recordExplorationQuitAction('stateName2', 120);
+
+      var learnerActions = this.ps.getPlaythrough().actions;
+
+      var displayBlocks = this.lars.getDisplayBlocks(learnerActions);
+
+      expect(displayBlocks.length).toEqual(1);
+
+      var blockHTML = this.lars.renderDisplayBlockHTML(displayBlocks[0]);
+
+      expect(blockHTML).toEqual(
+        'Started exploration at card "stateName1".<br><br>' +
+        'Pressed "Continue" to move to card "stateName2" after 30 seconds.' +
+        '<br><br>' +
+        'Submitted answer "Hello" in card "stateName2".<br><br>' +
+        'Left the exploration after spending a total of 120 seconds on card ' +
+        '"stateName2".<br><br>'
+      );
+    });
+  });
+});
