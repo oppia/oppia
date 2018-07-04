472cbea0e7ad878807488a11819604b56ddd5b70
aks681 <akshayanand681@gmail.com>
2018-07-04 20:53:34 +0530

    Added initial validations and tests

--- core/templates/dev/head/domain/story/StoryValidationService.js
+++ core/templates/dev/head/domain/story/StoryValidationService.js
@@ -20,9 +20,155 @@
  */
 
 oppia.factory('StoryValidationService', [
-  function() {
+  'NODE_ID_PREFIX', 'StoryContentsObjectFactory', 'StoryNodeObjectFactory',
+  'StoryObjectFactory',
+  function(
+      NODE_ID_PREFIX, StoryContentsObjectFactory, StoryNodeObjectFactory,
+      StoryObjectFactory) {
+    var _checkValidNodeId = function(nodeId) {
+      if (typeof nodeId !== 'string') {
+        return false;
+      }
+      var nodeIdPattern = new RegExp(NODE_ID_PREFIX + "[0-9]+", 'g');
+      if (!nodeId.match(nodeIdPattern)) {
+        return false;
+      }
+      return true;
+    };
+
+    var _validateNode = function(node) {
+      var issues = [];
+
+      if (!(node instanceof StoryNodeObjectFactory)) {
+        issues.push('All nodes should be a StoryNode instance');
+        return issues;
+      }
+      if (!_checkValidNodeId(node.getId())) {
+        issues.push('Invalid node id');
+      }
+      if (typeof node.getOutline() !== 'string') {
+        issues.push('Node outline should be a string');
+      }
+      if ((typeof node.getExplorationId() !== 'string') && (node.getExplorationId() !== null)) {
+        issues.push('Exploration id should be a string or null');
+      }
+      if (typeof node.getOutlineStatus() !== 'boolean') {
+        issues.push('Node outline status should be true or false.');
+      }
+      var prerequisiteSkillIds = node.getPrerequisiteSkillIds();
+      var acquiredSkillIds = node.getAcquiredSkillIds();
+      var destinationNodeIds = node.getDestinationNodeIds();
+      if (prerequisiteSkillIds.constructor !== Array) {
+        issues.push('Prerequisite skill ids should be an array');
+      }
+      if (prerequisiteSkillIds.some(function(skillId) {
+          return typeof skillId !== 'string';
+        })) {
+        issues.push('Each prerequisite skill id should be a string');
+      }
+      if ((new Set(prerequisiteSkillIds)).size !==
+          prerequisiteSkillIds.length) {
+        issues.push('All prerequisite skill ids should be distinct');
+      }
+
+      if (acquiredSkillIds.constructor !== Array) {
+        issues.push('Acquired skill ids should be an array');
+      }
+      if (acquiredSkillIds.some(function(skillId) {
+          return typeof skillId !== 'string';
+        })) {
+        issues.push('Each acquired skill id should be a string');
+      }
+      if ((new Set(acquiredSkillIds)).size !==
+          acquiredSkillIds.length) {
+        issues.push('All acquired skill ids should be distinct');
+      }
+
+      for (var i = 0; i < prerequisiteSkillIds.length; i++) {
+        if (acquiredSkillIds.indexOf(prerequisiteSkillIds[i]) !== -1) {
+          issues.push(
+            'Acquired and prerequisite skills for a node should not have any ' +
+            'skill in common');
+        }
+      }
+
+      if (destinationNodeIds.constructor !== Array) {
+        issues.push('Destination node ids should be an array');
+      }
+      if (destinationNodeIds.some(function(nodeId) {
+          return !_checkValidNodeId(nodeId);
+        })) {
+        issues.push('Each destination node id should be valid');
+      }
+      if (destinationNodeIds.some(function(nodeId) {
+          return nodeId === node.getId();
+        })) {
+        issues.push(
+          'A destination node id of a node should not point to the same node.');
+      }
+      if ((new Set(destinationNodeIds)).size !==
+          destinationNodeIds.length) {
+        issues.push('All destination node ids should be distinct');
+      }
+      return issues;
+    };
+
+    var _validateStoryContents = function(storyContents) {
+      var issues = [];
+      if (!(storyContents instanceof StoryContentsObjectFactory)) {
+        issues.push('Story contents should be StoryContents object');
+        return issues;
+      }
+      if (!_checkValidNodeId(storyContents.getInitialNodeId())) {
+        issues.push('Invalid initial node id');
+      }
+      if (!_checkValidNodeId(storyContents.getNextNodeId())) {
+        issues.push('Invalid next node id');
+      }
+      if (storyContents.getNodes().constructor !== Array) {
+        issues.push('Story nodes should be an array');
+        return issues;
+      }
+      var nodes = storyContents.getNodes();
+      var allNodesValid = true;
+      for (var i = 0; i < nodes.length; i++) {
+        var nodeIssues = _validateNode(nodes[i]);
+        if (nodeIssues.length > 0) {
+          allNodesValid = false;
+        }
+        issues = issues.concat(nodeIssues);
+      }
+      if (!allNodesValid) {
+        return issues;
+      }
+      // Provided the nodes list is valid and each node in it is valid, the
+      // following check is done similar to the one in
+      // story_domain.StoryContents to check if each node can be visited with
+      // given parameters.
+
+
+      return issues;
+    };
+
     var _validateStory = function(story) {
       var issues = [];
+      if (!(story instanceof StoryObjectFactory)) {
+        issues.push('The story should be Story object');
+        return issues;
+      }
+      if (typeof story.getTitle() !== 'string' || story.getTitle() === '') {
+        issues.push('Story title should be a non-empty string');
+      }
+      if (typeof story.getDescription() !== 'string') {
+        issues.push('Story description should be a string');
+      }
+      if (typeof story.getNotes() !== 'string') {
+        issues.push('Story notes should be a string');
+      }
+      if (typeof story.getLanguageCode() !== 'string') {
+        issues.push('Story language code should be a string');
+      }
+      issues = issues.concat(_validateStoryContents(story.getStoryContents()));
 
       return issues;
     };
@@ -34,7 +180,7 @@ oppia.factory('StoryValidationService', [
        * match the validations performed in the backend. This function is
        * expensive, so it should be called sparingly.
        */
-      findValidationIssuesStory: function(story) {
+      findValidationIssuesInStory: function(story) {
         return _validateStory(story);
       }
     };
--- core/templates/dev/head/domain/story/StoryValidationServiceSpec.js
+++ core/templates/dev/head/domain/story/StoryValidationServiceSpec.js
@@ -62,4 +62,66 @@ describe('Story validation service', function() {
       sampleStoryBackendObject);
   }));
 
+  it('should correctly validate story', function() {
+    _sampleStory.setTitle('');
+    _sampleStory.setDescription(123);
+    _sampleStory.setNotes(123);
+    _sampleStory.setLanguageCode(123);
+    expect(
+      StoryValidationService.findValidationIssuesInStory(_sampleStory)
+    ).toEqual([
+      'Story title should be a non-empty string',
+      'Story description should be a string',
+      'Story notes should be a string',
+      'Story language code should be a string'
+    ]);
+  });
+
+  it('should correctly validate node ids', function() {
+    _sampleStory.getStoryContents().getNodes()[0].addDestinationNodeId('node1');
+    expect(
+      StoryValidationService.findValidationIssuesInStory(_sampleStory)
+    ).toEqual([
+      'Each destination node id should be valid'
+    ]);
+
+    _sampleStory.getStoryContents().getNodes()[0].removeDestinationNodeId(
+      'node1');
+    _sampleStory.getStoryContents().getNodes()[0].addDestinationNodeId(
+      'node_a');
+    expect(
+      StoryValidationService.findValidationIssuesInStory(_sampleStory)
+    ).toEqual([
+      'Each destination node id should be valid'
+    ]);
+
+    _sampleStory.getStoryContents().getNodes()[0].removeDestinationNodeId(
+      'node_a');
+    _sampleStory.getStoryContents().getNodes()[0].addDestinationNodeId(
+      'node_111');
+    expect(
+      StoryValidationService.findValidationIssuesInStory(_sampleStory)
+    ).toEqual([]);
+  });
+
+  it('should correctly validate story nodes', function() {
+    _sampleStory.getStoryContents().setNodeExplorationId('node_1', 123);
+    _sampleStory.getStoryContents().setNodeOutline('node_1', 123);
+    _sampleStory.getStoryContents().addPrerequisiteSkillIdToNode('node_1', 123);
+    _sampleStory.getStoryContents().addAcquiredSkillIdToNode('node_1', 123);
+    _sampleStory.getStoryContents().addDestinationNodeIdToNode(
+      'node_1', 'node_1');
+
+    expect(
+      StoryValidationService.findValidationIssuesInStory(_sampleStory)
+    ).toEqual([
+      'Node outline should be a string',
+      'Exploration id should be a string or null',
+      'Each prerequisite skill id should be a string',
+      'Each acquired skill id should be a string',
+      'Acquired and prerequisite skills for a node should not have any ' +
+      'skill in common',
+      'A destination node id of a node should not point to the same node.'
+    ]);
+  });
 });
