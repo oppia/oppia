3196bae82623407cd082a0b334988e719855e6c1
Viet Tran Quoc Hoang <hoangviet1993@gmail.com>
2018-07-08 21:14:45 -0400

    Adding GraphInput to interactions's entries

--- extensions/interactions/protractor.js
+++ extensions/interactions/protractor.js
@@ -45,6 +45,7 @@
 var INTERACTIONS = {
   Continue: require('./Continue/protractor.js'),
   EndExploration: require('./EndExploration/protractor.js'),
+  GraphInput: require('./GraphInput/protractor.js'),
   FractionInput: require('./FractionInput/protractor.js'),
   MultipleChoiceInput: require('./MultipleChoiceInput/protractor.js'),
   NumericInput: require('./NumericInput/protractor.js'),
