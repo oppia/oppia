797cda93f5373917d199e7dbe7429af5210ca3d6
Nitish Bansal <nitishbansal2297@gmail.com>
2018-07-11 00:07:43 +0530

    Added comment to explain why the path of pre is different

--- core/templates/dev/head/components/CkEditorRteDirective.js
+++ core/templates/dev/head/components/CkEditorRteDirective.js
@@ -92,6 +92,8 @@ oppia.directive('ckEditorRte', [
         CKEDITOR.plugins.addExternal(
           'sharedspace',
           '/third_party/static/ckeditor-sharedspace-4.9.2/', 'plugin.js');
+        // Pre plugin is not available for 4.9.2 version of CKEditor. This is
+        // a self created plugin (other plugins are provided by CKEditor).
         CKEDITOR.plugins.addExternal(
           'pre', '/extensions/ckeditor_plugins/pre/', 'plugin.js');
         CKEDITOR.plugins.addExternal(
