676e37676fddd89a3252d7c10c36610eb81ef81a
Prasanna Patil <prasannapatil08@gmail.com>
2018-07-11 06:52:05 +0530

    Add backend tests

--- core/domain/stats_domain_test.py
+++ core/domain/stats_domain_test.py
@@ -1085,6 +1085,10 @@ class SubmittedAnswerValidationTests(test_utils.GenericTestBase):
             self.submitted_answer,
             'Expected answer_group_index to be non-negative')
 
+    def test_rule_spec_index_can_be_none(self):
+        self.submitted_answer.rule_spec_index = None
+        self.submitted_answer.validate()
+
     def test_rule_spec_index_must_be_integer(self):
         self.submitted_answer.rule_spec_index = '0'
         self._assert_validation_error(
