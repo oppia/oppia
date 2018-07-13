30acc98631f746883ea471f81e9205a8fa1b1595
hoangviet1993 <hoangviet1993@gmail.com>
2018-07-13 13:32:53 -0400

    Removing unconsumed console error from stats tab test

--- core/tests/protractor/stateEditor.js
+++ core/tests/protractor/stateEditor.js
@@ -523,10 +523,6 @@ describe('State editor', function() {
   });
 
   afterEach(function() {
-    // TODO(pranavsid98): Remove this checked error once we figure out and fix
-    // the cause.
-    general.checkForConsoleErrors([
-      'TypeError: google.visualization.PieChart is not a constructor'
-    ]);
+    general.checkForConsoleErrors([]);
   });
 });
