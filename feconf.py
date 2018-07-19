32b4610a10372206f5538dcd16373b04f717bbf0
Prasanna Patil <prasannapatil08@gmail.com>
2018-07-20 03:09:35 +0530

    Convert all floating point numbers back to float from strings in received classifier data (#5291)
    * Convert all floating point numbers back to float from strings in
    classifier data
    
    * Fix backend tests
    
    * Add more partially deterministic approach.
    
    * Update decoding algorithm
    
    * Address review comments
    
    * Update decoding algorithm
    
    * update decoding algorithm
    
    * Addressed review comments
    
    * change classifie_data to classifier_data_with_stringified_floats

--- feconf.py
+++ feconf.py
@@ -741,6 +741,15 @@ SHOW_COLLECTION_NAVIGATION_TAB_STATS = False
 # is created or updated.
 ENABLE_STATE_ID_MAPPING = False
 
+# The regular expression used to identify whether a string contains float value.
+# The regex must match with regex that is stored in vmconf.py file of Oppia-ml.
+# If this regex needs to be modified then first of all shutdown Oppia-ml VM.
+# Then update the regex constant in here and Oppia both.
+# Run any migration job that is required to migrate existing trained models
+# before starting Oppia-ml again.
+FLOAT_VERIFIER_REGEX = (
+    '^([-+]?\\d*\\.\\d+)$|^([-+]?(\\d*\\.?\\d+|\\d+\\.?\\d*)e[-+]?\\d*)$')
+
 # Current event models schema version. All event models with an
 # event_schema_version of 1 are the events collected before the rework of the
 # statistics framework which brought about the recording of new event models;
