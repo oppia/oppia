dc7d7ec5e67744e767f1c13e230987420e533d6c
vibhor98 <agarwalvibhor84@gmail.com>
2018-07-21 01:55:01 +0530

    Add constants for currency units.

--- core/templates/dev/head/domain/objects/NumberWithUnitsObjectFactory.js
+++ core/templates/dev/head/domain/objects/NumberWithUnitsObjectFactory.js
@@ -27,10 +27,17 @@ oppia.constant('NUMBER_WITH_UNITS_PARSING_ERRORS', {
     'Please ensure that unit only contains numbers, alphabets, (, ), *, ^, /, -'
 });
 
+oppia.constant('CURRENCY_UNITS', {
+  dollar: ['$', 'dollars', 'Dollars', 'Dollar', 'USD'],
+  rupee: ['Rs', 'rupees', '₹', 'Rupees', 'Rupee'],
+  cent: ['cents', 'Cent', 'Cents'],
+  paise: ['paisa', 'Paise', 'Paisa']
+});
+
 oppia.factory('NumberWithUnitsObjectFactory', [
-  'UnitsObjectFactory', 'FractionObjectFactory',
+  'UnitsObjectFactory', 'FractionObjectFactory', 'CURRENCY_UNITS',
   'NUMBER_WITH_UNITS_PARSING_ERRORS', function(
-      UnitsObjectFactory, FractionObjectFactory,
+      UnitsObjectFactory, FractionObjectFactory, CURRENCY_UNITS,
       NUMBER_WITH_UNITS_PARSING_ERRORS) {
     var NumberWithUnits = function(type, real, fractionObj, unitsObj) {
       this.type = type;
@@ -186,189 +193,188 @@ oppia.factory('NumberWithUnitsObjectFactory', [
   }
 ]);
 
-oppia.factory('UnitsObjectFactory', [function() {
-  var Units = function(unitsList) {
-    this.units = unitsList;
-  };
-
-  var isunit = function(unit) {
-    return !('/*() '.includes(unit));
-  };
-
-  Units.stringToLexical = function(units) {
-    units += '#';
-    var unitList = [];
-    var unit = '';
-    for (var i = 0; i < units.length; i++) {
-      if ('*/()# '.includes(units[i]) && unit !== 'per') {
-        if (unit.length > 0) {
-          if ((unitList.length > 0) && isunit(unitList.slice(-1).pop())) {
-            unitList.push('*');
-          }
-          unitList.push(unit);
-          unit = '';
-        }
-        if (!('# '.includes(units[i]))) {
-          unitList.push(units[i]);
-        }
-      } else if (units[i] === ' ' && unit === 'per') {
-        unitList.push('/');
-        unit = '';
-      } else {
-        unit += units[i];
-      }
-    }
-    return unitList;
-  };
-
-  var unitWithMultiplier = function(unitList) {
-    var multiplier = 1;
-    var unitsWithMultiplier = [];
-    var parenthesisStack = [];
-
-    for (var ind = 0; ind < unitList.length; ind++) {
-      if (unitList[ind] === '/') {
-        multiplier = -multiplier;
-      } else if (unitList[ind] === '(') {
-        if (unitList[ind - 1] === '/') {
-          // If previous element was division then we need to inverse
-          // multiplier when we find its corresponsing closing parenthesis.
-          // Second element of pushed element is used for this purpose.
-          parenthesisStack.push(['(', -1]);
-        } else {
-          // If previous element was not division then we don't need to
-          // invert the multiplier.
-          parenthesisStack.push(['(', 1]);
-        }
-      } else if (unitList[ind] === ')') {
-        var elem = parenthesisStack.pop();
-        multiplier = parseInt(elem[1]) * multiplier;
-      } else if (isunit(unitList[ind])) {
-        unitsWithMultiplier.push([unitList[ind], multiplier]);
-        // If previous element was division then we need to invert
-        // multiplier.
-        if (unitList[ind - 1] === '/') {
-          multiplier = -multiplier;
-        }
-      }
-    }
-    return unitsWithMultiplier;
-  };
-
-  var convertUnitDictToList = function(unitDict) {
-    var unitList = [];
-    for (var key in unitDict) {
-      unitList.push({unit: key, exponent: unitDict[key]});
-    }
-    return unitList;
-  };
-
-  var unitToList = function(unitsWithMultiplier) {
-    var unitDict = {};
-    for (var i = 0; i < unitsWithMultiplier.length; i++) {
-      var unit = unitsWithMultiplier[i][0];
-      var multiplier = unitsWithMultiplier[i][1];
-      var ind = unit.indexOf('^');
-      if (ind > -1) {
-        var s = unit.substr(0, ind);
-        var power = parseInt(unit.substr(ind + 1));
-      } else {
-        var s = unit;
-        var power = 1;
-      }
-      if (!(s in unitDict)) {
-        unitDict[s] = 0;
-      }
-      unitDict[s] += multiplier * power;
-    }
-    return convertUnitDictToList(unitDict);
-  };
-
-  Units.prototype.toDict = function() {
-    return {
-      units: this.units
+oppia.factory('UnitsObjectFactory', ['CURRENCY_UNITS',
+  function(CURRENCY_UNITS) {
+    var Units = function(unitsList) {
+      this.units = unitsList;
     };
-  };
 
-  Units.fromList = function(unitsList) {
-    return new Units(unitsList);
-  };
+    var isunit = function(unit) {
+      return !('/*() '.includes(unit));
+    };
 
-  Units.fromStringToList = function(unitsString) {
-    return unitToList(unitWithMultiplier(Units.stringToLexical(unitsString)));
-  };
-
-  Units.prototype.toString = function() {
-    var unit = '';
-    for (var i = 0; i < this.units.length; i++) {
-      var d = this.units[i];
-      if (d.exponent === 1) {
-        unit += d.unit + ' ';
-      } else {
-        unit += d.unit + '^' + d.exponent.toString() + ' ';
+    Units.stringToLexical = function(units) {
+      units += '#';
+      var unitList = [];
+      var unit = '';
+      for (var i = 0; i < units.length; i++) {
+        if ('*/()# '.includes(units[i]) && unit !== 'per') {
+          if (unit.length > 0) {
+            if ((unitList.length > 0) && isunit(unitList.slice(-1).pop())) {
+              unitList.push('*');
+            }
+            unitList.push(unit);
+            unit = '';
+          }
+          if (!('# '.includes(units[i]))) {
+            unitList.push(units[i]);
+          }
+        } else if (units[i] === ' ' && unit === 'per') {
+          unitList.push('/');
+          unit = '';
+        } else {
+          unit += units[i];
+        }
       }
-    }
-    return unit.trim();
-  };
+      return unitList;
+    };
 
-  Units.createCurrencyUnits = function() {
-    // Creates user-defined currency (base + sub) units.
-    math.createUnit('dollar', {aliases: [
-      'dollars', 'Dollar', 'USD', 'Dollars']});
-    math.createUnit('cent', {definition: '0.01 dollar', aliases: [
-      'Cent', 'cents', 'Cents']});
-    math.createUnit('rupees', {aliases: [
-      'Rupees', 'Rs', 'rupee', 'Rupee']});
-    math.createUnit('paise', {definition: '0.01 rupees', aliases: ['paisa']});
-  };
+    var unitWithMultiplier = function(unitList) {
+      var multiplier = 1;
+      var unitsWithMultiplier = [];
+      var parenthesisStack = [];
 
-  Units.toMathjsCompatibleString = function(units) {
-    // Makes the units compatible with the math.js allowed format.
-    units = units.replace(/per/g, '/');
+      for (var ind = 0; ind < unitList.length; ind++) {
+        if (unitList[ind] === '/') {
+          multiplier = -multiplier;
+        } else if (unitList[ind] === '(') {
+          if (unitList[ind - 1] === '/') {
+            // If previous element was division then we need to inverse
+            // multiplier when we find its corresponsing closing parenthesis.
+            // Second element of pushed element is used for this purpose.
+            parenthesisStack.push(['(', -1]);
+          } else {
+            // If previous element was not division then we don't need to
+            // invert the multiplier.
+            parenthesisStack.push(['(', 1]);
+          }
+        } else if (unitList[ind] === ')') {
+          var elem = parenthesisStack.pop();
+          multiplier = parseInt(elem[1]) * multiplier;
+        } else if (isunit(unitList[ind])) {
+          unitsWithMultiplier.push([unitList[ind], multiplier]);
+          // If previous element was division then we need to invert
+          // multiplier.
+          if (unitList[ind - 1] === '/') {
+            multiplier = -multiplier;
+          }
+        }
+      }
+      return unitsWithMultiplier;
+    };
 
-    if (units.includes('Dollars') || units.includes('dollars') ||
-      units.includes('Dollar')) {
-      units = units.replace('dollars', 'dollar');
-      units = units.replace('Dollars', 'dollar');
-      units = units.replace('Dollar', 'dollar');
-    }
-    if (units.includes('Rupees') || units.includes('rupees') ||
-      units.includes('Rupee')) {
-      units = units.replace('rupees', 'rupee');
-      units = units.replace('Rupees', 'rupee');
-      units = units.replace('Rupee', 'rupee');
-    }
+    var convertUnitDictToList = function(unitDict) {
+      var unitList = [];
+      for (var key in unitDict) {
+        unitList.push({unit: key, exponent: unitDict[key]});
+      }
+      return unitList;
+    };
 
-    // Special symbols need to be replaced as math.js doesn't support custom
-    // units starting with special symbols. Also, it doesn't allow units
-    // followed by a number as in the case of currency units.
-    if (units.includes('$')) {
-      units = units.replace('$', '');
-      units = 'dollar ' + units;
-    }
-    if (units.includes('Rs') || units.includes('₹')) {
-      units = units.replace('Rs', '');
-      units = units.replace('₹', '');
-      units = 'rupee ' + units;
-    }
-    return units.trim();
-  };
+    var unitToList = function(unitsWithMultiplier) {
+      var unitDict = {};
+      for (var i = 0; i < unitsWithMultiplier.length; i++) {
+        var unit = unitsWithMultiplier[i][0];
+        var multiplier = unitsWithMultiplier[i][1];
+        var ind = unit.indexOf('^');
+        if (ind > -1) {
+          var s = unit.substr(0, ind);
+          var power = parseInt(unit.substr(ind + 1));
+        } else {
+          var s = unit;
+          var power = 1;
+        }
+        if (!(s in unitDict)) {
+          unitDict[s] = 0;
+        }
+        unitDict[s] += multiplier * power;
+      }
+      return convertUnitDictToList(unitDict);
+    };
 
-  Units.fromRawInputString = function(units) {
-    try {
-      Units.createCurrencyUnits();
-    } catch (parsingError) {}
+    Units.prototype.toDict = function() {
+      return {
+        units: this.units
+      };
+    };
 
-    compatibleUnits = Units.toMathjsCompatibleString(units);
-    if (compatibleUnits !== '') {
+    Units.fromList = function(unitsList) {
+      return new Units(unitsList);
+    };
+
+    Units.fromStringToList = function(unitsString) {
+      return unitToList(unitWithMultiplier(Units.stringToLexical(unitsString)));
+    };
+
+    Units.prototype.toString = function() {
+      var unit = '';
+      for (var i = 0; i < this.units.length; i++) {
+        var d = this.units[i];
+        if (d.exponent === 1) {
+          unit += d.unit + ' ';
+        } else {
+          unit += d.unit + '^' + d.exponent.toString() + ' ';
+        }
+      }
+      return unit.trim();
+    };
+
+    Units.createCurrencyUnits = function() {
+      // Creates user-defined currency (base + sub) units.
+      math.createUnit('dollar', {aliases: CURRENCY_UNITS.dollar});
+      math.createUnit('cent', {
+        definition: '0.01 dollar', aliases: CURRENCY_UNITS.cent});
+      math.createUnit('rupee', {aliases: CURRENCY_UNITS.rupee});
+      math.createUnit('paise', {
+        definition: '0.01 rupee', aliases: CURRENCY_UNITS.paise});
+    };
+
+    Units.toMathjsCompatibleString = function(units) {
+      // Makes the units compatible with the math.js allowed format.
+      units = units.replace(/per/g, '/');
+
+      // Special symbols need to be replaced as math.js doesn't support custom
+      // units starting with special symbols. Also, it doesn't allow units
+      // followed by a number as in the case of currency units.
+      if (units.includes('$')) {
+        units = units.replace('$', '');
+        units = 'dollar ' + units;
+      }
+      if (units.includes('Rs') || units.includes('₹')) {
+        units = units.replace('Rs', '');
+        units = units.replace('₹', '');
+        units = 'rupee ' + units;
+      }
+
+      for (var i = 0; i < CURRENCY_UNITS.dollar.length; i++) {
+        if (units.includes(CURRENCY_UNITS.dollar[i])) {
+          units = units.replace(CURRENCY_UNITS.dollar[i], 'dollar');
+        }
+      }
+
+      for (var i = 0; i < CURRENCY_UNITS.rupee.length; i++) {
+        if (units.includes(CURRENCY_UNITS.rupee[i])) {
+          units = units.replace(CURRENCY_UNITS.rupee[i], 'rupee');
+        }
+      }
+      return units.trim();
+    };
+
+    Units.fromRawInputString = function(units) {
       try {
-        math.unit(compatibleUnits);
-      } catch (err) {
-        throw new Error(err);
-      }
-    }
-    return new Units(Units.fromStringToList(units));
-  };
+        Units.createCurrencyUnits();
+      } catch (parsingError) {}
 
-  return Units;
-}]);
+      compatibleUnits = Units.toMathjsCompatibleString(units);
+      if (compatibleUnits !== '') {
+        try {
+          math.unit(compatibleUnits);
+        } catch (err) {
+          throw new Error(err);
+        }
+      }
+      return new Units(Units.fromStringToList(units));
+    };
+
+    return Units;
+  }]);
