07d24d4448a43906ecac5adee6a9cd3d2068f77b
Pranav Siddharth S <pranavsid98@gmail.com>
2018-07-23 01:33:17 +0530

    Fix variable declaration error (#5351)

--- core/templates/dev/head/pages/exploration_player/PlayerServices.js
+++ core/templates/dev/head/pages/exploration_player/PlayerServices.js
@@ -370,6 +370,12 @@ oppia.factory('ExplorationPlayerService', [
             interactionRulesService));
         var answerIsCorrect = classificationResult.outcome.labelledAsCorrect;
 
+        // Use angular.copy() to clone the object
+        // since classificationResult.outcome points
+        // at oldState.interaction.default_outcome
+        var outcome = angular.copy(classificationResult.outcome);
+        var newStateName = outcome.dest;
+
         if (!_editorPreviewMode) {
           var feedbackIsUseful = (
             AnswerClassificationService.isClassifiedExplicitlyOrGoesToNewState(
@@ -391,11 +397,6 @@ oppia.factory('ExplorationPlayerService', [
           }
         }
 
-        // Use angular.copy() to clone the object
-        // since classificationResult.outcome points
-        // at oldState.interaction.default_outcome
-        var outcome = angular.copy(classificationResult.outcome);
-        var newStateName = outcome.dest;
         var refresherExplorationId = outcome.refresherExplorationId;
         var newState = exploration.getState(newStateName);
 
