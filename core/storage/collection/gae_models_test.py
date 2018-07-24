d6397283dd11a968248d07f1d6403ff80053a811
Joshua Lan <josh.lan9@gmail.com>
2018-07-23 21:13:48 -0700

    Address review comments, add test for CollectionRightsModel

--- core/storage/collection/gae_models_test.py
+++ core/storage/collection/gae_models_test.py
@@ -18,6 +18,7 @@
 from constants import constants
 from core.domain import collection_domain
 from core.domain import collection_services
+from core.domain import rights_manager
 from core.platform import models
 from core.tests import test_utils
 
@@ -39,15 +40,37 @@ class CollectionModelUnitTest(test_utils.GenericTestBase):
         self.assertEquals(num_collections, 1)
 
 
+class CollectionRightsModelUnitTest(test_utils.GenericTestBase):
+    """Test the CollectionRightsModel class."""
+
+    def test_save(self):
+        collection_models.CollectionRightsModel(
+            id='id',
+            owner_ids=['owner_ids'],
+            editor_ids=['editor_ids'],
+            translator_ids=['translator_ids'],
+            viewer_ids=['viewer_ids'],
+            community_owned=False,
+            status=constants.ACTIVITY_STATUS_PUBLIC,
+            viewable_if_private=False,
+            first_published_msec=0.0
+            ).save('cid', 'Created new collection',
+                   [{'cmd': rights_manager.CMD_CREATE_NEW}])
+        collection_model = collection_models.CollectionRightsModel.get('id')
+        self.assertEqual('id', collection_model.id)
+
+
 class CollectionCommitLogEntryModelUnitTest(test_utils.GenericTestBase):
     """Test the CollectionCommitLogEntryModel class."""
 
     def test_get_all_non_private_commits(self):
         private_commit = collection_models.CollectionCommitLogEntryModel.create(
-            'id', None, 'commiter_id', 'username', 'msg', 'create', {},
+            '-a-', 0, 'commiter_id', 'username', 'msg',
+            'create', [{}],
             constants.ACTIVITY_STATUS_PRIVATE, False)
         public_commit = collection_models.CollectionCommitLogEntryModel.create(
-            'id', None, 'commiter_id', 'username', 'msg', 'create', {},
+            '-b-', 0, 'commiter_id', 'username', 'msg',
+            'create', [{}],
             constants.ACTIVITY_STATUS_PUBLIC, False)
         private_commit.collection_id = 'a'
         public_commit.collection_id = 'b'
@@ -56,13 +79,14 @@ class CollectionCommitLogEntryModelUnitTest(test_utils.GenericTestBase):
         commits = (
             collection_models.CollectionCommitLogEntryModel
             .get_all_non_private_commits(2, None, None))
-        self.assertEquals(1, len(commits[0]))
+        self.assertEquals(False, commits[2])
+        self.assertEquals('collection--b--0', commits[0][0].id)
 
 
-class CollectionSummaryModel(test_utils.GenericTestBase):
+class CollectionSummaryModelUnitTest(test_utils.GenericTestBase):
     """Tests for the CollectionSummaryModel."""
 
-    def test_get_all_non_private(self):
+    def test_get_non_private(self):
         public_collection_summary_model = (
             collection_models.CollectionSummaryModel(
                 id='id0',
@@ -106,8 +130,9 @@ class CollectionSummaryModel(test_utils.GenericTestBase):
                 collection_model_created_on=None,
             ))
         private_collection_summary_model.put()
-        commits = collection_models.CollectionSummaryModel.get_non_private()
-        self.assertEqual(1, len(commits))
+        collection_summary_models = (
+            collection_models.CollectionSummaryModel.get_non_private())
+        self.assertEqual(1, len(collection_summary_models))
 
     def test_get_private_at_least_viewable(self):
         viewable_collection_summary_model = (
@@ -153,10 +178,11 @@ class CollectionSummaryModel(test_utils.GenericTestBase):
                 collection_model_created_on=None,
             ))
         unviewable_collection_summary_model.put()
-        commits = (
+        collection_summary_models = (
             collection_models.CollectionSummaryModel
             .get_private_at_least_viewable('a'))
-        self.assertEqual(1, len(commits))
+        self.assertEqual(1, len(collection_summary_models))
+        self.assertEqual('id0', collection_summary_models[0].id)
 
     def test_get_at_least_editable(self):
         editable_collection_summary_model = (
@@ -202,7 +228,12 @@ class CollectionSummaryModel(test_utils.GenericTestBase):
                 collection_model_created_on=None,
             ))
         uneditable_collection_summary_model.put()
-        commits = (
+        collection_summary_models = (
             collection_models.CollectionSummaryModel
             .get_at_least_editable('a'))
-        self.assertEqual(1, len(commits))
+        self.assertEqual(1, len(collection_summary_models))
+        self.assertEqual('id0', collection_summary_models[0].id)
+        collection_summary_models = (
+            collection_models.CollectionSummaryModel
+            .get_at_least_editable('viewer_ids'))
+        self.assertEqual(0, len(collection_summary_models))
