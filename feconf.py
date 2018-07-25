09a2950d819068d474765aa1f2b97df9f3f8eff4
Apurv-Bajaj <apurvabajaj007@gmail.com>
2018-07-25 11:00:01 +0530

    Add test exploration for rating mechanism

--- feconf.py
+++ feconf.py
@@ -544,6 +544,7 @@ DEMO_EXPLORATIONS = {
     u'20': 'example_exploration_in_collection2.yaml',
     u'21': 'example_exploration_in_collection3.yaml',
     u'22': 'protractor_mobile_test_exploration.yaml',
+    u'23': 'rating_test.yaml',
 }
 
 DEMO_COLLECTIONS = {
