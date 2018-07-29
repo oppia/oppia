0e80d53585a78a005e3170ffbe703af3867d511f
hoangviet1993 <hoangviet1993@gmail.com>
2018-07-29 08:21:22 -0400

    Using spaces instead of tabs
    Re-write comment to remove tab
    
    Minor changes

--- scripts/install_chrome_on_travis.sh
+++ scripts/install_chrome_on_travis.sh
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS-IS" BASIS,
@@ -13,13 +13,12 @@
 # limitations under the License.
 
 # This script should only be ran by Travis to install and provide a constant 
-# version of Chrome for Protractor.
-# The CHROME_SOURCE_URL is an environment variable set in the repository settings	
-# of Travis. It can be found under 'Environment Variables' header here:	
-# https://travis-ci.org/oppia/oppia/settings
+# version of Chrome.
+# CHROME_SOURCE_URL is an environment variable set in Oppia's Travis repo settings
+# It can be found under 'Environment Variables' header here: https://travis-ci.org/oppia/oppia/settings
 
 if [ ! -f $HOME/.cache/TravisChrome/$(basename $CHROME_SOURCE_URL) ]; then
-  # Caching Chrome Debian package after download to prevent connection problem.
+  # Caching Chrome's Debian package after download to prevent connection problem.
   mkdir -p $HOME/.cache/TravisChrome/
   cd $HOME/.cache/TravisChrome/
   # --remote-name : Write output to a file named as the remote file.
