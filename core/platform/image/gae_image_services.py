66e9b63d67c6245476aaf6aa5b1793f131a4fb5b
Aashishgaba097 <aashishgaba097@gmail.com>
2018-08-05 00:15:22 +0530

    resolved some comments

--- core/platform/image/gae_image_services.py
+++ core/platform/image/gae_image_services.py
@@ -33,19 +33,19 @@ def get_image_dimensions(file_content):
         file_content: str. The content of the file.
 
     Returns:
-        tuple. Returns height and width of the image.
+        tuple(int). Returns height and width of the image.
     """
     img = Image.open(io.BytesIO(file_content))
     width, height = img.size
     return height, width
 
 
-def compress_image(image_content, scaling_factor=1):
+def compress_image(image_content, scaling_factor):
     """Compresses the image by resizing the image with the scaling factor.
 
     Args:
         image_content: str. Content of the file to be compressed.
-        scaling_factor: int. The number by which the image will be scaled.
+        scaling_factor: float. The number by which the image will be scaled.
 
     Returns:
         str. Returns the content of the compressed image.
