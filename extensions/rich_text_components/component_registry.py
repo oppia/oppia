5e703068dd4beb1aceca5309515208cb1327cfb8
Nitish Bansal <nitishbansal2297@gmail.com>
2018-08-05 00:11:19 +0530

    minor nit

--- extensions/rich_text_components/component_registry.py
+++ extensions/rich_text_components/component_registry.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 #
-# Copyright 2014 The Oppia Authors. All Rights Reserved.
+# Copyright 2018 The Oppia Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
