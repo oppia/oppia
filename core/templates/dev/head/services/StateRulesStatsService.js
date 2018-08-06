c82cb30dbd5e59764e38a94341b9a5f2775b6092
Brian Rodriguez <brian@brianrodri.com>
2018-08-06 10:05:46 -0400

    Fix attempt 1.

--- core/templates/dev/head/services/StateRulesStatsService.js
+++ core/templates/dev/head/services/StateRulesStatsService.js
@@ -58,22 +58,17 @@ oppia.factory('StateRulesStatsService', [
             exploration_id: explorationId,
             visualizations_info: response.data.visualizations_info.map(
               function(vizInfo) {
-                var vizInfoDataWithAddressedInfo = {};
                 if (vizInfo.addressed_info_is_supported) {
-                  vizInfoDataWithAddressedInfo = {
-                    data: vizInfo.data.map(function(vizInfoDatum) {
-                      return Object.assign({
-                        is_addressed: (
-                          AnswerClassificationService
-                            .isClassifiedExplicitlyOrGoesToNewState(
-                              state.name, state,
-                              vizInfoDatum.answer, interactionRulesService))
-                      }, vizInfoDatum);
-                    })
-                  };
+                  vizInfo = {data: vizInfo};
+                  angular.forEach(vizInfo, function(vizInfoDatum) {
+                    vizInfoDatum.is_addressed =
+                      AnswerClassificationService
+                        .isClassifiedExplicitlyOrGoesToNewState(
+                          state.name, state, vizInfoDatum.answer,
+                          interactionRulesService)
+                  });
                 }
-
-                return Object.assign({}, vizInfo, vizInfoDataWithAddressedInfo);
+                return vizInfo;
               })
           };
         });
