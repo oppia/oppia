fb4c698ed289f3942732b173c460061773a92705
Sandeep Dubey <dubeysandeep.in@gmail.com>
2018-08-12 21:01:25 +0530

    Added e2e tests

--- core/tests/protractor_utils/ExplorationEditorTranslationTab.js
+++ core/tests/protractor_utils/ExplorationEditorTranslationTab.js
@@ -19,31 +19,75 @@
 var waitFor = require('../protractor_utils/waitFor.js');
 
 var ExplorationEditorTranslationTab = function() {
-  var contentTabButton = element(by.css('protractor-test-content-tab'));
-  var feedbackTabButton = element(by.css('protractor-test-feedback-tab'));
-  var hintsTabButton = element(by.css('protractor-test-hints-tab'));
-  var solutionTabButton = element(by.css('protractor-test-solution-tab'));
-  var contentTabText = element(by.css('protractor-test-content-text'));
+  var contentTabButton = element(
+    by.css('.protractor-test-translation-content-tab'));
+  var feedbackTabButton = element(
+    by.css('.protractor-test-translation-feedback-tab'));
+  var hintsTabButton = element(
+    by.css('.protractor-test-translation-hints-tab'));
+  var solutionTabButton = element(
+    by.css('.protractor-test-translation-solution-tab'));
+
+  var contentTabText = element(by.css('.protractor-test-content-text'));
+
+  var feedbackList = element.all(
+    by.css('li.protractor-test-translation-feedback'));
+  var translationFeedback = function(index) {
+    return element(by.css('.protractor-test-feedback-' + index));
+  };
+  var translationFeedbackText = function(index) {
+    return element(by.css('.protractor-test-feedback-' + index + '-text'));
+  };
+
+  var hintsList = element.all(
+    by.css('li.protractor-test-translation-hint'));
+  var translationHint = function(index) {
+    return element(by.css('.protractor-test-hint-' + index));
+  };
+  var translationHintText = function(index) {
+    return element(by.css('.protractor-test-hint-' + index + '-text'));
+  };
+
+  var solutionTabText = element(by.css('.protractor-test-solution-text'));
+
   var _selectLanguage = function(language) {
     element(by.css('.protractor-test-translation-language-selector')).
       element(by.cssContainingText('option', language)).click();
   };
 
   this.expectContentTabContentToMatch = function(content) {
-    waitFor.pageToFullyLoad();
-    expect(contentTabText.getAttribute('textContent')).toMatch(content);
+    waitFor.elementToBeClickable(
+      contentTabButton, 'Content Tab button is not clickable');
+    contentTabButton.click();
+    expect(contentTabText.getText()).toMatch(content);
   };
 
-  this.expectFeedbackTabContentsToMatch = function() {
+  this.expectFeedbackTabContentsToMatch = function(contents) {
+    waitFor.elementToBeClickable(
+      feedbackTabButton, 'Feedback Tab button is not clickable');
     feedbackTabButton.click();
+    expect(feedbackList.count()).toEqual(contents.length);
+    for (index in contents) {
+      translationFeedback(index).click();
+      expect(translationFeedbackText(index).getText()).toMatch(contents[index]);
+    }
   };
 
-  this.expectHintsTabContentsToMatch = function() {
+  this.expectHintsTabContentsToMatch = function(contents) {
+    waitFor.elementToBeClickable(
+      hintsTabButton, 'Hints Tab button is not clickable');
     hintsTabButton.click();
+    for (index in contents) {
+      translationHint(index).click();
+      expect(translationHintText(index).getText()).toMatch(contents[index]);
+    }
   };
 
-  this.expectSolutionTabContentToMatch = function() {
+  this.expectSolutionTabContentToMatch = function(content) {
+    waitFor.elementToBeClickable(
+      solutionTabButton, 'Solution Tab button is not clickable');
     solutionTabButton.click();
+    expect(solutionTabText.getText()).toMatch(content);
   };
 
   this.changeTranslationLanguage = function(language) {
