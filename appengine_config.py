51216a30951b1d8eb97d3cadb6ed056a3b84bc0b
Aashishgaba097 <aashishgaba097@gmail.com>
2018-08-12 11:29:11 +0530

    dev mode to is oppia_tools there

--- appengine_config.py
+++ appengine_config.py
@@ -70,11 +70,11 @@ def webapp_add_wsgi_middleware(app):
 
 # Root path of the app.
 ROOT_PATH = os.path.dirname(__file__)
+_PARENT_DIR = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+oppia_tools_path = os.path.join(_PARENT_DIR, 'oppia_tools')
 
-if feconf.DEV_MODE:
-    _PARENT_DIR = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
-    pil_path = os.path.join(_PARENT_DIR, 'oppia_tools', 'PIL-1.1.7')
-
+if os.path.isdir(oppia_tools_path):
+    pil_path = os.path.join(oppia_tools_path, 'PIL-1.1.7')
     if not os.path.isdir(pil_path):
         raise Exception('Invalid path for oppia_tools library: %s' % pil_path)
     sys.path.insert(0, pil_path)
