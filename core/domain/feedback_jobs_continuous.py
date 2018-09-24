d066c83f0a6bb4d83647e658f7e81c54697225ac
netajik <kancharapunetaji@gmail.com>
2018-09-24 19:51:58 +0530

    Fix part of #4374: update docstrings in oppia.core.controllers.translator and oppia.core.domain.feedback_jobs_continuous.

--- core/domain/feedback_jobs_continuous.py
+++ core/domain/feedback_jobs_continuous.py
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""Feedback jobs for continuous-computation."""
+
 from core import jobs
 from core.domain import feedback_domain
 from core.platform import models
