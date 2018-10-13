f8ce7cfc195f77874d9abde6f9d27db2712e70fa
son-vyas <soniyavyas29@gmail.com>
2018-10-13 07:15:38 +0000

    Modified check for both single line and multiline docstring

--- scripts/pre_commit_linter.py
+++ scripts/pre_commit_linter.py
@@ -1011,13 +1011,13 @@ def _check_docstrings(all_files):
                     prev_line = file_content[line_num - 1].lstrip().rstrip()
 
                 # Check for space after """ in the single line docstring.
-                if line.startswith('""" ') and line.endswith('"""'):
+                if line.startswith('""" ') or line.endswith(' """'):
                     failed = True
                     print'%s --> Line %s: %s' % (
                         filename, line_num + 1, extra_space_message)
 
                 # Check for space before  """ in the multi-line docstring.
-                elif line.startswith('""" ') and line.endswith(' """'):
+                elif line.startswith('""" ') or line.endswith(' """'):
                     failed = True
                     print'%s --> Line %s: %s' % (
                         filename, line_num + 1, extra_space_message)
