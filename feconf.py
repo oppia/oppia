c4f8875b227dfe05a413a7882dd60b56dc7ce495
Sean Lip <sean@seanlip.org>
2018-10-15 00:54:40 -0700

    Fix backend tests.

--- feconf.py
+++ feconf.py
@@ -27,8 +27,8 @@ DEBUG = False
 
 # When DEV_MODE is true check that we are running in development environment.
 # The SERVER_SOFTWARE environment variable does not exist in Travis, hence the
-# need for an explicit default.
-if (constants.DEV_MODE and
+# need for an explicit check.
+if (constants.DEV_MODE and os.getenv('SERVER_SOFTWARE') and
         not os.getenv('SERVER_SOFTWARE', default='').startswith('Development')):
     raise Exception('DEV_MODE can\'t be true on production.')
 
