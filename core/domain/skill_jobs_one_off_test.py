d0616e8b06884f8e6905b408b84b6f0d7a5d0d39
Nalin Bhardwaj <nalinbhardwaj@nibnalin.me>
2018-10-19 13:16:36 +0530

    Fix #5052: Create one-off schema migration jobs for topics, skills, questions and stories (#5660)
    * collection_jobs_one_off_test : fix nits
    
    - Remove extraneous definition
    - Fix comment typo
    
    * skill_jobs_one_off : Create schema migration jobs for skill
    
    Adds basic tests as well.
    
    * story_jobs_one_off : Create schema migration jobs for story
    
    Adds basic tests as well.
    
    * topic_jobs_one_off : Create schema migration jobs for topic
    
    Adds basic tests as well.
    
    * question_jobs_one_off : Create schema migration jobs for question
    
    Adds basic tests as well.
    
    * fixup! skill_jobs_one_off : Create schema migration jobs for skill
    
    * fixup! story_jobs_one_off : Create schema migration jobs for story
    
    * fixup! topic_jobs_one_off : Create schema migration jobs for topic
    
    * fixup! question_jobs_one_off : Create schema migration jobs for question
    
    * *_services: remove run_conversion from get_*_from_model
    
    * job_registry: Add jobs to jobs_registry.
    
    Guards jobs using feconf.ENABLE_NEW_STRUCTURES as well.
    Modifies tests accordingly.
    
    * commit_cmd from_version and to_version to int
    
    Changes from_version and to_version to be int, not str, in existing one off jobs.
    
    * Revert "commit_cmd from_version and to_version to int"
    
    This reverts commit ad8e497473443bd386e1431b70be7f4e86f248e4.
    
    * commit_cmd: add note for version fields
    
    * fixup! job_registry: Add jobs to jobs_registry.
    
    * Use constants.ENABLE_NEW_STRUCTURES
    
    Changes due to rebase over `8f3d11c`.
    
    * Change how deleted items are handled
    
    https://github.com/oppia/oppia/pull/5660/files#r220786659
    
    * rename jobs as *OneOffJob
    
    * create test_utils functions and tests for updating old schemas
    
    * Use ast.literal_eval in reduce and change how migrated items are handled.
    
    * fix migrated item handling for collections
    
    * update tests to test job outputs and update jobs to remove disabled key
    
    * break lines

---
+++ core/domain/skill_jobs_one_off_test.py
@@ -0,0 +1,160 @@
+# coding: utf-8
+#
+# Copyright 2018 The Oppia Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for Skill-related one-off jobs."""
+import ast
+
+from constants import constants
+from core.domain import skill_domain
+from core.domain import skill_jobs_one_off
+from core.domain import skill_services
+from core.platform import models
+from core.tests import test_utils
+import feconf
+
+(skill_models,) = models.Registry.import_models([models.NAMES.skill])
+
+
+class SkillMigrationOneOffJobTest(test_utils.GenericTestBase):
+
+    ALBERT_EMAIL = 'albert@example.com'
+    ALBERT_NAME = 'albert'
+
+    SKILL_ID = 'skill_id'
+
+    def setUp(self):
+        super(SkillMigrationOneOffJobTest, self).setUp()
+
+        # Setup user who will own the test skills.
+        self.albert_id = self.get_user_id_from_email(self.ALBERT_EMAIL)
+        self.signup(self.ALBERT_EMAIL, self.ALBERT_NAME)
+        self.process_and_flush_pending_tasks()
+
+    def test_migration_job_does_not_convert_up_to_date_skill(self):
+        """Tests that the skill migration job does not convert a
+        skill that is already the latest schema version.
+        """
+        # Create a new skill that should not be affected by the
+        # job.
+        skill = skill_domain.Skill.create_default_skill(
+            self.SKILL_ID, description='A description')
+        skill_services.save_new_skill(self.albert_id, skill)
+        self.assertEqual(
+            skill.skill_contents_schema_version,
+            feconf.CURRENT_SKILL_CONTENTS_SCHEMA_VERSION)
+        self.assertEqual(
+            skill.misconceptions_schema_version,
+            feconf.CURRENT_MISCONCEPTIONS_SCHEMA_VERSION)
+
+        # Start migration job.
+        with self.swap(constants, 'ENABLE_NEW_STRUCTURES', True):
+            job_id = (
+                skill_jobs_one_off.SkillMigrationOneOffJob.create_new())
+            skill_jobs_one_off.SkillMigrationOneOffJob.enqueue(job_id)
+            self.process_and_flush_pending_tasks()
+
+        # Verify the skill is exactly the same after migration.
+        updated_skill = (
+            skill_services.get_skill_by_id(self.SKILL_ID))
+        self.assertEqual(
+            updated_skill.skill_contents_schema_version,
+            feconf.CURRENT_SKILL_CONTENTS_SCHEMA_VERSION)
+        self.assertEqual(
+            updated_skill.misconceptions_schema_version,
+            feconf.CURRENT_MISCONCEPTIONS_SCHEMA_VERSION)
+
+        output = skill_jobs_one_off.SkillMigrationOneOffJob.get_output(job_id) # pylint: disable=line-too-long
+        expected = [[u'skill_migrated',
+                     [u'1 skills successfully migrated.']]]
+        self.assertEqual(expected, [ast.literal_eval(x) for x in output])
+
+    def test_migration_job_skips_deleted_skill(self):
+        """Tests that the skill migration job skips deleted skill
+        and does not attempt to migrate.
+        """
+        skill = skill_domain.Skill.create_default_skill(
+            self.SKILL_ID, description='A description')
+        skill_services.save_new_skill(self.albert_id, skill)
+
+        # Delete the skill before migration occurs.
+        skill_services.delete_skill(
+            self.albert_id, self.SKILL_ID)
+
+        # Ensure the skill is deleted.
+        with self.assertRaisesRegexp(Exception, 'Entity .* not found'):
+            skill_services.get_skill_by_id(self.SKILL_ID)
+
+        # Start migration job on sample skill.
+        with self.swap(constants, 'ENABLE_NEW_STRUCTURES', True):
+            job_id = (
+                skill_jobs_one_off.SkillMigrationOneOffJob.create_new())
+            skill_jobs_one_off.SkillMigrationOneOffJob.enqueue(job_id)
+
+            # This running without errors indicates the deleted skill is
+            # being ignored.
+            self.process_and_flush_pending_tasks()
+
+        # Ensure the skill is still deleted.
+        with self.assertRaisesRegexp(Exception, 'Entity .* not found'):
+            skill_services.get_skill_by_id(self.SKILL_ID)
+
+        output = skill_jobs_one_off.SkillMigrationOneOffJob.get_output(job_id) # pylint: disable=line-too-long
+        expected = [[u'skill_deleted',
+                     [u'Encountered 1 deleted skills.']]]
+        self.assertEqual(expected, [ast.literal_eval(x) for x in output])
+
+    def test_migration_job_converts_old_skill(self):
+        """Tests that the schema conversion functions work
+        correctly and an old skill is converted to new
+        version.
+        """
+
+        # Generate skill with old(v1) misconceptions schema
+        # version and old(v1) skill contents schema version.
+        skill_contents = {
+            'worked_examples': [],
+            'explanation': ''
+        }
+        self.save_new_skill_with_story_and_skill_contents_schema_version(
+            self.SKILL_ID, self.albert_id, 'A description', 0,
+            misconceptions=[], skill_contents=skill_contents)
+        skill = (
+            skill_services.get_skill_by_id(self.SKILL_ID))
+        self.assertEqual(skill.misconceptions_schema_version, 1)
+        self.assertEqual(skill.skill_contents_schema_version, 1)
+
+        # Start migration job.
+        with self.swap(constants, 'ENABLE_NEW_STRUCTURES', True):
+            job_id = (
+                skill_jobs_one_off.SkillMigrationOneOffJob.create_new())
+            skill_jobs_one_off.SkillMigrationOneOffJob.enqueue(job_id)
+            self.process_and_flush_pending_tasks()
+
+            # Verify that the skill migrates correctly.
+            updated_skill = (
+                skill_services.get_skill_by_id(self.SKILL_ID))
+
+        self.assertEqual(
+            updated_skill.skill_contents_schema_version,
+            feconf.CURRENT_SKILL_CONTENTS_SCHEMA_VERSION)
+        self.assertEqual(
+            updated_skill.misconceptions_schema_version,
+            feconf.CURRENT_MISCONCEPTIONS_SCHEMA_VERSION)
+
+        output = skill_jobs_one_off.SkillMigrationOneOffJob.get_output(job_id) # pylint: disable=line-too-long
+        expected = [[u'skill_migrated',
+                     [u'1 skills successfully migrated.']]]
+        self.assertEqual(expected, [ast.literal_eval(x) for x in output])
