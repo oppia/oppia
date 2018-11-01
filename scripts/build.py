c5f181c73d7c9835f34df525fab1dd841acdb244
Keshav Bathla <34139754+import-keshav@users.noreply.github.com>
2018-11-01 17:01:11 -0400

    Update build.py

--- scripts/build.py
+++ scripts/build.py
@@ -1098,7 +1098,7 @@ def generate_build_directory():
 
 def build():
     """Creates a third party directory where all the dependencies of JS and
-    CSS files minifies and stored.
+    CSS files minified and stored.
     """
     parser = optparse.OptionParser()
     parser.add_option(
