dd8b686bef3500f79897b1b38d20374dc83c1018
Chris Tao <christao@MainTank.local>
2018-11-05 00:43:18 -0800

    initial try, cannot invoke UnsentFeedbackEmailHandler

--- core/controllers/tasks_test.py
+++ core/controllers/tasks_test.py
@@ -60,7 +60,7 @@ class UnsentFeedbackEmailHandlerTests(test_utils.GenericTestBase):
             #create another message.
             feedback_services.create_message(
                 thread_id, self.user_id_b, None, None, 'user b message')
-             #check that there are two messages in thread
+            #check that there are two messages in thread
             messages = feedback_services.get_messages(thread_id)
             self.assertEqual(len(messages), 2)
             #create feedback message 
@@ -68,5 +68,6 @@ class UnsentFeedbackEmailHandlerTests(test_utils.GenericTestBase):
                 thread_id, self.user_id_a, None, None, 'testing feedback')
             #telling tasks.py to send email to User 'A'
             #Using UnsentFeedbackEmailHandler
+
             feedback_services.enqueue_feedback_message_batch_email_task(
                 'A')            
