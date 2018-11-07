9d731fcf60a0a4aee5256cb27b56a924fd84db46
Chris Tao <christao@MainTank.local>
2018-11-06 23:44:40 -0800

    Finished UnsentFeedbackEmailHandler and SuggestionEmailHandler

--- core/controllers/tasks_test.py
+++ core/controllers/tasks_test.py
@@ -13,27 +13,30 @@
 # limitations under the License.
 
 """Tests for Tasks Email Handler."""
-
+from core.domain import exp_domain
 from core.domain import feedback_services
+from core.domain import suggestion_services
+from core.domain import exp_services
 from core.platform import models
 from core.tests import test_utils
+from core.domain import rights_manager
 import feconf
 
 (job_models, email_models) = models.Registry.import_models(
     [models.NAMES.job, models.NAMES.email])
-(feedback_models, email_models) = models.Registry.import_models(
-    [models.NAMES.feedback, models.NAMES.email])
+(feedback_models, suggestion_models) = models.Registry.import_models(
+    [models.NAMES.feedback, models.NAMES.suggestion])
 transaction_services = models.Registry.import_transaction_services()
 taskqueue_services = models.Registry.import_taskqueue_services()
 
 
-class UnsentFeedbackEmailHandlerTests(test_utils.GenericTestBase):
+class TasksTests(test_utils.GenericTestBase):
 
     USER_A_EMAIL = 'a@example.com'
     USER_B_EMAIL = 'b@example.com'
 
     def setUp(self):
-        super(UnsentFeedbackEmailHandlerTests, self).setUp()
+        super(TasksTests, self).setUp()
         self.signup(self.USER_A_EMAIL, 'userA')
         self.user_id_a = self.get_user_id_from_email(self.USER_A_EMAIL)
         self.signup(self.USER_B_EMAIL, 'userB')
@@ -46,7 +49,7 @@ class UnsentFeedbackEmailHandlerTests(test_utils.GenericTestBase):
             feconf, 'CAN_SEND_EMAILS', True)
         self.can_send_feedback_email_ctx = self.swap(
             feconf, 'CAN_SEND_FEEDBACK_MESSAGE_EMAILS', True)
-
+        self.THREAD_ID = 'exploration.exp1.thread_1'
 
     def test_UnsentFeedbackEmailHandler(self):
         #create feedback thread.
@@ -83,10 +86,73 @@ class UnsentFeedbackEmailHandlerTests(test_utils.GenericTestBase):
             expected_message = (
                 'Hi userA,\n\nNew update to thread "a subject"'
                 ' on Title:\n- userB: user b message\n(You received'
-                'this message because you are a participant in this thread.)'
+                ' this message because you are a participant in this thread.)'
                 '\n\nBest wishes,\nThe Oppia team\n\nYou can change your email'
                 ' preferences via the Preferences page.')
 
             #assert that the message is correct.
             self.assertEqual(len(messages), 1)
             self.assertEqual(messages[0].body.decode(), expected_message)
+    def test_SuggestionEmailHandler(self):
+        """Tests SuggestionEmailHandler functionality"""
+        class FakeActivityRights:
+            def __init__(
+                self, exploration_id, owner_ids, editor_ids, translator_ids,
+                viewer_ids, community_owned=False, cloned_from=None,
+                status=True, viewable_if_private=False,
+                first_published_msec=None):
+                #user B ID hardcoded into owner_ids to get email_manager.
+                #to send email to user B to test functionality.
+                self.owner_ids = ['121121523518511814218']
+        self.email_user_b=self.swap(rights_manager, 'ActivityRights', FakeActivityRights)
+        with self.email_user_b, self.can_send_feedback_email_ctx, self.can_send_emails_ctx:
+            change = {
+                'cmd': exp_domain.CMD_EDIT_STATE_PROPERTY,
+                'property_name': exp_domain.STATE_PROPERTY_CONTENT,
+                'state_name': 'state_1',
+                'new_value': 'new suggestion content'}
+            #create suggestion from user A to user B.
+            suggestion_services.create_suggestion(
+                suggestion_models.SUGGESTION_TYPE_EDIT_STATE_CONTENT,
+                suggestion_models.TARGET_TYPE_EXPLORATION,
+                self.exploration.id, 1,
+                self.user_id_a, change, 'test description',
+                None)
+            threadlist = feedback_services.get_all_threads(
+                    suggestion_models.TARGET_TYPE_EXPLORATION, self.exploration.id, True)
+            thread_id = threadlist[0].id
+            #enqueue and send suggestion email task.
+            payload = {
+                'exploration_id': self.exploration.id,
+                'thread_id': thread_id}
+            messages = self.mail_stub.get_sent_messages()
+            self.assertEqual(len(messages), 0)
+            taskqueue_services.enqueue_email_task(
+                feconf.TASK_URL_SUGGESTION_EMAILS, payload, 0)
+            self.process_and_flush_pending_tasks()
+            #check that user B recieved message.
+            messages = self.mail_stub.get_sent_messages(to= self.USER_B_EMAIL)
+            self.assertEqual(len(messages), 1)
+    def test_InstantFeedbackMessageEmailHandler(self):
+        """Tests Instant feedback message handler"""
+        #WORK IN PROGRESS.
+        with self.can_send_feedback_email_ctx, self.can_send_emails_ctx:
+            feedback_services.create_thread(
+                feconf.ENTITY_TYPE_EXPLORATION, self.exploration.id,
+                self.user_id_a, 'a subject', 'some text')
+            threadlist = feedback_services.get_all_threads(
+                feconf.ENTITY_TYPE_EXPLORATION, self.exploration.id, False)
+            thread_id = threadlist[0].id
+            #create another message.
+            feedback_services.create_message(
+                thread_id, self.user_id_b, None, None, 'user b message')
+            #send instant feedback message email.
+            reference_dict= {
+                'entity_id': self.exploration.id,
+                'thread_id': thread_id}
+
+
+
+
+
+
