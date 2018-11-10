93ec1ab4a337a9de0935a717dc28bf29f68f10a0
Chris Tao <christao@calvisitor-10-105-155-74.calvisitor.1918.berkeley.edu>
2018-11-10 13:50:29 -0800

    Added special cases for comments that don't start with capital letter

--- scripts/pre_commit_linter.py
+++ scripts/pre_commit_linter.py
@@ -982,7 +982,7 @@ def _check_comments(all_files):
                         print '%s --> Line %s: %s' % (
                             filename, line_num + 1, message)
 
-                #Check that comment starts with a space.
+                # Check that comment starts with a space.
                 if line.startswith('#') and len(line) > 1 and line[1] != ' ':
                     message = (
                         'There should be a space at the beginning '
@@ -992,10 +992,10 @@ def _check_comments(all_files):
                         filename, line_num + 1, message)
                 if not line.startswith('# coding:') and not (
                     line.startswith('# pylint:')):
-                    # check that comment starts with a capital letter. 
+                    # Check that comment starts with a capital letter.
                     if line.startswith('#') and len(line) > 2 and not (
-                            previous_line.startswith('#')) and (
-                            line[2] != line[2].upper()):
+                        previous_line.startswith('#')) and (
+                        line[2] != line[2].upper()):
                         message = (
                             'There should be a capital letter'
                             ' to begin the content of the comment.')
