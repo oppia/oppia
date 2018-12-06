703fedbdd7b95995614f020d7c279d3d66ce242a
Brian Rodriguez <brian@brianrodri.com>
2018-12-05 21:02:23 -0500

    Describe exploration ids

--- core/templates/dev/head/pages/exploration_player/PlaythroughService.js
+++ core/templates/dev/head/pages/exploration_player/PlaythroughService.js
@@ -21,6 +21,7 @@ oppia.constant(
   '/explorehandler/store_playthrough/<exploration_id>');
 
 oppia.constant('PLAYTHROUGH_ENABLED_EXPLORATION_WHITELIST', [
+  // NOTE: These are the exploration ids of the Fractions lessons.
   "umPkwp0L1M0-", "MjZzEVOG47_1", "9trAQhj6uUC2", "rfX8jNkPnA-1",
   "0FBWxCE5egOw", "670bU6d9JGBh", "aHikhPlxYgOH", "-tMgcP1i_4au",
   "zW39GLG_BdN2", "Xa3B_io-2WI5", "6Q6IyIDkjpYC", "osw1m5Q3jK41"
