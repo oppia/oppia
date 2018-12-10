4d615cf707ac5ce9b3adea99562a577f7bfe5754
Tony Jiang <tjiang11@jhu.edu>
2018-12-10 09:59:48 -0500

    Implement editing of questions (#5614)
    * Save.
    
    * Save.
    
    * Save. UrlInterpolationError topic null.
    
    * Save.
    
    * Modify backend to return associated skills when fetching a question.
    
    * save.
    
    * Saving content works.
    
    * Copy interaction.
    
    * Apply QuestionUpdateService for all changes.
    
    * Circumvent race condition on initialization.
    
    * Lint.
    
    * Clone UndoRedoService for questions.
    
    * Fetch question summaries after edit question.
    
    * Create reusable function in QuestionEditorDirective.
    
    * Lint.
    
    * Add test.
    
    * Fix test.
    
    * Lint.
    
    * Disable flag.
    
    * Review changes.
    
    * Review changes.
    
    * Review comments.
    
    * Lint.
    
    * Review changes.
    
    * Deduplicate UndoRedoService.
    
    * Move updateFunction into QuestionUpdateService.
    
    * Fix test.
    
    * Lint.
    
    * Update documentation.
    
    * Add tests for get_models_by_question_id
    
    * Update setQuestionStateData.
    
    * Fix test.
    
    * Fix test.
    
    * Lint.
    
    * Add test.
    
    * Lint.
    
    * Edit questions in modal.
    
    * Add blank commit message.
    
    * Remove duplicate function; Fix issue with not being able to edit a question's interaction; Fix issue with not validating when no interaction is specified.
    
    * Lint.
    
    * Lint.
    
    * Fix.
    
    * Flip flag.

--- core/controllers/question_editor.py
+++ core/controllers/question_editor.py
@@ -112,8 +112,17 @@ class EditableQuestionDataHandler(base.BaseHandler):
             raise self.PageNotFoundException(
                 'The question with the given id doesn\'t exist.')
 
+        associated_skill_ids = [link.skill_id for link in (
+            question_services.get_question_skill_links_of_question(
+                question_id))]
+        associated_skills = skill_services.get_multi_skills(
+            associated_skill_ids)
+        associated_skill_dicts = [
+            skill.to_dict() for skill in associated_skills]
+
         self.values.update({
-            'question_dict': question.to_dict()
+            'question_dict': question.to_dict(),
+            'associated_skill_dicts': associated_skill_dicts
         })
         self.render_json(self.values)
 
@@ -154,7 +163,7 @@ class EditableQuestionDataHandler(base.BaseHandler):
 
         question_dict = question_services.get_question_by_id(
             question_id).to_dict()
-        return self.render_json({
+        self.render_json({
             'question_dict': question_dict
         })
 
