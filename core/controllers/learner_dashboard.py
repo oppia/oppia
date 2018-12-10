1b3f5a0b93f9f5e532c727815802e59dbe6cf3d7
Nithesh N. Hariharan <nithusha21@yahoo.co.in>
2018-12-10 20:23:47 +0530

    Fix error while displaying suggestion threads on learner dashboard page (#5965)
    * fix error on learner dashboard page
    
    * fix style

--- core/controllers/learner_dashboard.py
+++ core/controllers/learner_dashboard.py
@@ -162,6 +162,7 @@ class LearnerDashboardFeedbackThreadHandler(base.BaseHandler):
 
         message_summary_list = []
         suggestion = suggestion_services.get_suggestion_by_id(thread_id)
+        suggestion_thread = feedback_services.get_thread(thread_id)
 
         exploration_id = feedback_services.get_exp_id_from_thread_id(thread_id)
         if suggestion:
@@ -172,7 +173,7 @@ class LearnerDashboardFeedbackThreadHandler(base.BaseHandler):
             suggestion_summary = {
                 'suggestion_html': suggestion.change.new_value['html'],
                 'current_content_html': current_content_html,
-                'description': suggestion.description,
+                'description': suggestion_thread.subject,
                 'author_username': authors_settings[0].username,
                 'author_picture_data_url': (
                     authors_settings[0].profile_picture_data_url)
