95cea276259bb8a8a1fc1cdf37223ac582c931d7
Ankita Saxena <ankitasonu24@gmail.com>
2018-12-11 01:05:49 +0530

    Improve naming, remove extra lines

--- core/storage/file/gae_models_test.py
+++ core/storage/file/gae_models_test.py
@@ -26,11 +26,11 @@ import feconf
 class FileMetadataModelTest(test_utils.GenericTestBase):
     """Tests the FileMetadataModel class."""
 
-    def test_get_new_id(self):
+    def test_get_new_id_raises_not_implemented_error(self):
         with self.assertRaises(NotImplementedError):
             file_models.FileMetadataModel.get_new_id('entity1')
 
-    def test_get_undeleted(self):
+    def test_get_undeleted_returns_undeleted_instances(self):
         file_metadata_model1 = file_models.FileMetadataModel.create(
             'exp_id1', 'path/to/file1.png')
         file_metadata_model1.commit(feconf.SYSTEM_COMMITTER_ID, [])
@@ -53,7 +53,7 @@ class FileMetadataModelTest(test_utils.GenericTestBase):
         self.assertEqual(
             file_metadata_model1, file_metadata_model_list[0])
 
-    def test_construct_id(self):
+    def test_construct_id_constructs_correct_model_id(self):
         file_metadata_model_id1 = (
             file_models.FileMetadataModel._construct_id( # pylint: disable=protected-access
                 'exp_id1', 'path/to/file.png'))
@@ -66,14 +66,14 @@ class FileMetadataModelTest(test_utils.GenericTestBase):
         self.assertEqual(
             file_metadata_model_id2, '/exp_id1/path/to/file.png')
 
-    def test_create(self):
+    def test_create_creates_model_with_correct_id(self):
         file_metadata_model = file_models.FileMetadataModel.create(
             'exp_id1', 'path/to/file.png')
         self.assertEqual(file_metadata_model.deleted, False)
         self.assertEqual(
             file_metadata_model.id, '/exp_id1/path/to/file.png')
 
-    def test_get_model(self):
+    def test_get_model_returns_the_instance_with_desired_id(self):
         file_metadata_model = file_models.FileMetadataModel.create(
             'exp_id1', 'path/to/file1.png')
         file_metadata_model.commit(feconf.SYSTEM_COMMITTER_ID, [])
@@ -92,7 +92,7 @@ class FileMetadataModelTest(test_utils.GenericTestBase):
             actual_model = file_models.FileMetadataModel.get_model(
                 'exp_id1', 'path/to/file2.png', True)
 
-    def test_get_version(self):
+    def test_get_version_returns_instance_with_desired_version(self):
         file_metadata_model = file_models.FileMetadataModel.create(
             'exp_id1', 'path/to/file1.png')
         file_metadata_model.commit(feconf.SYSTEM_COMMITTER_ID, [])
@@ -106,7 +106,7 @@ class FileMetadataModelTest(test_utils.GenericTestBase):
             'exp_id1', 'path/to/file1.png', 2)
         self.assertEqual(file_metadata_model.key, actual_model.key)
 
-    def test_commit(self):
+    def test_commit_updates_version_of_instance(self):
         file_metadata_model = file_models.FileMetadataModel.create(
             'exp_id1', 'path/to/file1.png')
         self.assertEqual(file_metadata_model.version, 0)
@@ -118,29 +118,25 @@ class FileMetadataModelTest(test_utils.GenericTestBase):
 class FileModelTest(test_utils.GenericTestBase):
     """Tests the FileModel class."""
 
-    def test_reconstitute(self):
+    def test_reconstitute_changes_the_content_snapshot(self):
         file_model = file_models.FileModel.create(
             'exp_id1', 'path/to/file.png')
         self.assertEqual(file_model.content, None)
-        file_model._reconstitute( # pylint: disable=protected-access
-            'file_content')
+        file_model._reconstitute('file_content') # pylint: disable=protected-access
         self.assertEqual(file_model.content, 'file_content')
 
-    def test_compute_snapshot(self):
+    def test_compute_snapshot_returns_file_content_snapshot(self):
         file_model = file_models.FileModel.create(
             'exp_id1', 'path/to/file.png')
-        self.assertEqual(
-            file_model._compute_snapshot(), None) # pylint: disable=protected-access
-        file_model._reconstitute( # pylint: disable=protected-access
-            'file_content')
-        self.assertEqual(
-            file_model._compute_snapshot(), 'file_content') # pylint: disable=protected-access
+        self.assertEqual(file_model._compute_snapshot(), None) # pylint: disable=protected-access
+        file_model._reconstitute('file_content') # pylint: disable=protected-access
+        self.assertEqual(file_model._compute_snapshot(), 'file_content') # pylint: disable=protected-access
 
-    def test_get_new_id(self):
+    def test_get_new_id_raises_not_implemented_error(self):
         with self.assertRaises(NotImplementedError):
             file_models.FileModel.get_new_id('entity1')
 
-    def test_construct_id(self):
+    def test_construct_id_constructs_correct_model_id(self):
         file_model_id1 = file_models.FileModel._construct_id( # pylint: disable=protected-access
             'exp_id1', 'path/to/file.png')
         file_model_id2 = file_models.FileModel._construct_id( # pylint: disable=protected-access
@@ -149,13 +145,13 @@ class FileModelTest(test_utils.GenericTestBase):
         self.assertEqual(file_model_id1, '/exp_id1/path/to/file.png')
         self.assertEqual(file_model_id2, '/exp_id1/path/to/file.png')
 
-    def test_create(self):
+    def test_create_creates_model_with_correct_id(self):
         file_model = file_models.FileModel.create(
             'exp_id1', 'path/to/file.png')
         self.assertEqual(file_model.deleted, False)
         self.assertEqual(file_model.id, '/exp_id1/path/to/file.png')
 
-    def test_get_model(self):
+    def test_get_model_returns_the_instance_with_desired_id(self):
         file_model = file_models.FileModel.create(
             'exp_id1', 'path/to/file1.png')
         file_model.commit(feconf.SYSTEM_COMMITTER_ID, [])
@@ -174,7 +170,7 @@ class FileModelTest(test_utils.GenericTestBase):
             actual_model = file_models.FileModel.get_model(
                 'exp_id1', 'path/to/file2.png', True)
 
-    def test_commit(self):
+    def test_commit_updates_version_of_instance(self):
         file_model = file_models.FileModel.create(
             'exp_id1', 'path/to/file1.png')
         self.assertEqual(file_model.version, 0)
@@ -182,7 +178,7 @@ class FileModelTest(test_utils.GenericTestBase):
         file_model.commit(feconf.SYSTEM_COMMITTER_ID, [])
         self.assertEqual(file_model.version, 1)
 
-    def test_get_version(self):
+    def test_get_version_returns_instance_with_desired_version(self):
         file_model = file_models.FileModel.create(
             'exp_id1', 'path/to/file1.png')
         file_model.commit(feconf.SYSTEM_COMMITTER_ID, [])
