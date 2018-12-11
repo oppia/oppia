b0077ead0d85c3e43db7eae25c92edac3a570353
Brian Rodriguez <brian@brianrodri.com>
2018-12-10 20:09:04 -0500

    better accessor

--- core/controllers/editor.py
+++ core/controllers/editor.py
@@ -863,7 +863,7 @@ class TopUnresolvedAnswersHandler(EditorHandler):
 
     GET_HANDLER_ERROR_RETURN_TYPE = feconf.HANDLER_TYPE_JSON
 
-    @acl_decorators.can_edit_exploration
+    @acl_decorators.can_play_exploration
     def get(self, exploration_id):
         """Handles GET requests for unresolved answers."""
         try:
