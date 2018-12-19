420777ccd6b0b38e83c9e2ef13f3fcb9b810e526
Vojtěch Jelínek <vojtin.j@gmail.com>
2018-12-19 23:43:41 +0530

    Update core/templates/dev/head/components/side_navigation_bar/SideNavigationBarDirective.js
    Co-Authored-By: rakshitkumarcse <32777813+rakshitkumarcse@users.noreply.github.com>

--- core/templates/dev/head/components/side_navigation_bar/SideNavigationBarDirective.js
+++ core/templates/dev/head/components/side_navigation_bar/SideNavigationBarDirective.js
@@ -26,7 +26,7 @@ oppia.directive('sideNavigationBar', [
         'side_navigation_bar_directive.html'),
       controller: ['$scope', '$timeout', function(
           $scope, $timeout) {
-        $scope.CURRENT_URL = window.location.pathname;
+        $scope.currentUrl = $location.path();
         $scope.getStaticImageUrl = UrlInterpolationService.getStaticImageUrl;
       }]
     };
