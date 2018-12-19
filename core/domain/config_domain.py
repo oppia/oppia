cf73427369b7a69a834c68669c03cb0319e88f1c
Brian Rodriguez <brian@brianrodri.com>
2018-12-19 00:41:01 -0500

    fix i18n

--- core/domain/config_domain.py
+++ core/domain/config_domain.py
@@ -275,6 +275,6 @@ RECORD_PLAYTHROUGH_PROBABILITY = ConfigProperty(
     'record_playthrough_probability', FLOAT_SCHEMA,
     'The probability of recording playthroughs', 0.2)
 
-IS_ISSUES_TAB_ENABLED = ConfigProperty(
-    'is_issues_tab_enabled', BOOL_SCHEMA,
-    'Exposes the Issues Tab for creators in the exploration editor.', False)
+IS_IMPROVEMENTS_TAB_ENABLED = ConfigProperty(
+    'is_improvements_tab_enabled', BOOL_SCHEMA,
+    'Exposes the Improvements Tab for creators in the exploration editor.', False)
