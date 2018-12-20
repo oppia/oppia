1288bd5941f4ab6c8d621bfcfdfdca9c8dc1324a
Rishav Chakraborty <annonymousxyz@outlook.com>
2018-12-21 01:45:47 +0530

    Lint checks

--- core/controllers/custom_landing_pages_test.py
+++ core/controllers/custom_landing_pages_test.py
@@ -42,19 +42,23 @@ class StewardsLandingPageTest(test_utils.GenericTestBase):
     volunteers, or NGOs).
     """
     def test_nonprofits_landing_page(self):
-        response = self.get_html_response(feconf.CUSTOM_NONPROFITS_LANDING_PAGE_URL)
+        response = self.get_html_response(
+            feconf.CUSTOM_NONPROFITS_LANDING_PAGE_URL)
         response.mustcontain(
             'Let\'s work together to make compelling educational')
 
     def test_parents_landing_page(self):
-        response = self.get_html_response(feconf.CUSTOM_PARENTS_LANDING_PAGE_URL)
+        response = self.get_html_response(
+            feconf.CUSTOM_PARENTS_LANDING_PAGE_URL)
         response.mustcontain(
             'Help your child learn with our free, engaging lessons')
 
     def test_teachers_landing_page(self):
-        response = self.get_html_response(feconf.CUSTOM_TEACHERS_LANDING_PAGE_URL)
+        response = self.get_html_response(
+            feconf.CUSTOM_TEACHERS_LANDING_PAGE_URL)
         response.mustcontain('Oppia\'s free, personalized lessons are a great')
 
     def test_volunteers_landing_page(self):
-        response = self.get_html_response(feconf.CUSTOM_VOLUNTEERS_LANDING_PAGE_URL)
+        response = self.get_html_response(
+            feconf.CUSTOM_VOLUNTEERS_LANDING_PAGE_URL)
         response.mustcontain('Help improve access to high-quality education')
