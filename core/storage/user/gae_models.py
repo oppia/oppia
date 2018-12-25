7022eae3736c65a0d47d10a3ff7fce68a47dafd2
Yash Jipkate <yashjipkate@gmail.com>
2018-12-25 22:12:10 +0530

    Review checks #5
    Minor fixes
    
    Merge branch 'docstrings-backend' of https://github.com/YashJipkate/oppia into docstrings-backend

--- core/storage/user/gae_models.py
+++ core/storage/user/gae_models.py
@@ -182,6 +182,7 @@ class ExpUserLastPlaythroughModel(base_models.BaseModel):
         Args:
             user_id: str. The id of the user.
             exploration_id: str. The id of the exploration.
+
         Returns:
             str. The generated key using user_id and exploration_id
                 of the form [user_id].[exploration_id].
@@ -420,9 +421,11 @@ class ExplorationUserDataModel(base_models.BaseModel):
     def _generate_id(cls, user_id, exploration_id):
         """Generates key for the instance of ExplorationUserDataModel class in
         the required format with the arguments provided.
+        
         Args:
             user_id: str. The id of the user.
             exploration_id: str. The id of the exploration.
+
         Returns:
             str. The generated key using user_id and exploration_id
                 of the form [user_id].[exploration_id].
