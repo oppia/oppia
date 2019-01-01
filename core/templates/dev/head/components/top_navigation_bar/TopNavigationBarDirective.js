59260be91622f286575a658556a0a912458949d7
Ankita Saxena <ankitasonu24@gmail.com>
2019-01-02 00:27:29 +0530

    Fix #6040: Fix issues in develop (#6038)
    * Fix issues in develop
    
    * revert changes
    
    * Increase value of timeout_multiplier
    
    * Linter catches the issues, redo the changes
    
    * Revert changes in exp services test
    
    * Revert changes in top navigation bar directive
    
    * Update method to obtain messages to fix linter
    
    * Address review comments
    
    * Fix issues
    
    * Increase timeout
    
    * Increase timeout
    
    * Revert changes
    
    * Revert changes
    
    * Increase timeout further
    
    * Fix pylint error
    
    * Fix js errors

--- core/templates/dev/head/components/top_navigation_bar/TopNavigationBarDirective.js
+++ core/templates/dev/head/components/top_navigation_bar/TopNavigationBarDirective.js
@@ -80,7 +80,7 @@ oppia.directive('topNavigationBar', [
           var NAV_MODES_WITH_CUSTOM_LOCAL_NAV = [
             'create', 'explore', 'collection', 'topics_and_skills_dashboard',
             'topic_editor', 'story_editor'];
-          $scope.currentUrl = window.location.pathname.split("/")[1];
+          $scope.currentUrl = window.location.pathname.split('/')[1];
           $scope.LABEL_FOR_CLEARING_FOCUS = LABEL_FOR_CLEARING_FOCUS;
           $scope.newStructuresEnabled = constants.ENABLE_NEW_STRUCTURE_EDITORS;
           $scope.getStaticImageUrl = UrlInterpolationService.getStaticImageUrl;
