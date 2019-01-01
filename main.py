4094f183fad8f2f375b756ce4eff7b2aad847c9f
Brian Rodriguez <brian@brianrodri.com>
2019-01-01 15:20:23 -0500

    remove playthrough whitelist handler

--- main.py
+++ main.py
@@ -631,9 +631,6 @@ URLS = MAPREDUCE_HANDLERS + [
         r'/ml/nextjobhandler', classifier.NextJobHandler),
 
     get_redirect_route(
-        r'/playthroughdatahandler/whitelist',
-        editor.FetchPlaythroughWhitelistHandler),
-    get_redirect_route(
         r'/playthroughdatahandler/<exploration_id>/<playthrough_id>',
         editor.FetchPlaythroughHandler),
 
