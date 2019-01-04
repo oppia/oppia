c16ef599ad13dc7d678c097b0035e9a1f1564636
Rakshit Kumar <rakshit.kumar.cse17@itbhu.ac.in>
2019-01-04 10:13:55 +0530

    Fix #5771: Uses cookies to fix the issue.

--- core/templates/dev/head/pages/exploration_editor/translation_tab/StateTranslationDirective.js
+++ core/templates/dev/head/pages/exploration_editor/translation_tab/StateTranslationDirective.js
@@ -290,7 +290,8 @@ oppia.directive('stateTranslation', [
             $scope.answerChoices = StateEditorService.getAnswerChoices(
               $scope.stateInteractionId, currentCustomizationArgs);
             if (sessionStorage.getItem('activatedTabId' !== null)) {
-              $scope.onTabClick(sessionStorage.getItem('activatedTabId')); }
+              $scope.onTabClick(sessionStorage.getItem('activatedTabId'));
+            }
             else {
               $scope.activatedTabId = $scope.TAB_ID_CONTENT;
               sessionStorage.setItem('activatedTabId', $scope.activatedTabId);
