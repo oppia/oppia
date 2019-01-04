c827a9cadba78f87bb1f47fbd8ac00ac85f1c084
Brian Rodriguez <brian@brianrodri.com>
2019-01-03 22:59:31 -0500

    remove extra whitespaces

--- core/controllers/features_test.py
+++ core/controllers/features_test.py
@@ -33,29 +33,24 @@ class ExplorationPlaythroughRecordingFeatureTest(test_utils.GenericTestBase):
 
     def setUp(self):
         super(ExplorationPlaythroughRecordingFeatureTest, self).setUp()
-
         # An editor signs up.
         self.signup(self.EDITOR_EMAIL, self.EDITOR_USERNAME)
         self.editor_id = self.get_user_id_from_email(self.EDITOR_EMAIL)
         self.editor = user_services.UserActionsInfo(self.editor_id)
-
         # The editor creates an exploration which will be whitelisted.
         self.save_new_valid_exploration(
             self.WHITELISTED_EXP_ID, self.editor_id,
             title='My First Exploration', end_state_name='END')
         rights_manager.publish_exploration(self.editor, self.WHITELISTED_EXP_ID)
-
         # The editor creates an exploration which will not be whitelisted.
         self.save_new_valid_exploration(
             self.NON_WHITELISTED_EXP_ID, self.editor_id,
             title='My Second Exploration', end_state_name='END')
         rights_manager.publish_exploration(
             self.editor, self.NON_WHITELISTED_EXP_ID)
-
         # An admin signs up.
         self.signup(self.ADMIN_EMAIL, self.ADMIN_USERNAME)
         self.admin_id = self.get_user_id_from_email(self.ADMIN_EMAIL)
-
         # The admin whitelists one of the editor's new explorations.
         config_services.set_property(
             self.admin_id, 'whitelisted_exploration_ids_for_playthroughs',
@@ -64,7 +59,6 @@ class ExplorationPlaythroughRecordingFeatureTest(test_utils.GenericTestBase):
     def tearDown(self):
         config_services.revert_property(
             self.admin_id, 'whitelisted_exploration_ids_for_playthroughs')
-
         super(ExplorationPlaythroughRecordingFeatureTest, self).tearDown()
 
     def test_can_record_playthroughs_in_whitelisted_explorations(self):
