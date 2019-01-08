6c53d6b14c529b0808becc6e990dd2190a55f8b9
Yash Jipkate <yashjipkate@gmail.com>
2019-01-08 08:29:43 +0530

    Merge conditional statements as requested in review

--- core/controllers/profile.py
+++ core/controllers/profile.py
@@ -405,21 +405,15 @@ class UrlHandler(base.BaseHandler):
             login_url = None
             self.render_json({'login_url': login_url})
         else:
-            if self.request:
-                if self.request.get('current_url'):
-                    target_url = (
-                        '/' if self.request.get('current_url').endswith(
-                            feconf.SPLASH_URL)
-                        else self.request.get('current_url'))
-                    login_url = (
-                        current_user_services.create_login_url(target_url))
-                    self.render_json({'login_url': login_url})
-                else:
-                    raise self.InvalidInputException(
-                        'Empty GET parameters passed'
-                    )
+            if self.request and self.request.get('current_url'):
+                target_url = (
+                    '/' if self.request.get('current_url').endswith(
+                        feconf.SPLASH_URL)
+                    else self.request.get('current_url'))
+                login_url = (
+                    current_user_services.create_login_url(target_url))
+                self.render_json({'login_url': login_url})
             else:
                 raise self.InvalidInputException(
-                    'Expected URL in the GET request parameters, None '
-                    'passed'
+                    'Incomplete or empty GET parameters passed'
                 )
