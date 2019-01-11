95d66da8e995b0e26f1ceea677cc721bac4b2041
Tony Jiang <tjiang11@jhu.edu>
2019-01-11 07:30:06 -0500

    Add audio translation schema for subtopic pages (#5823)
    * Add audio translation schema for subtopic pages.
    
    * Rename html_data to page_contents.
    
    * Lint.
    
    * Lint.
    
    * Lint.
    
    * Turn off flag.
    
    * Split updates of html and audio.
    
    * Lint.
    
    * Lint.
    
    * Remove irrelevant files.
    
    * Address review comments.
    
    * Use pipe operator.
    
    * Address review comments.
    
    * Fix tests.
    
    * Lint.
    
    * Fix test.
    
    * Fix test.
    
    * Address review comments.
    
    * Lint.
    
    * Lint.

--- core/domain/topic_services.py
+++ core/domain/topic_services.py
@@ -414,9 +414,18 @@ def apply_change_list(topic_id, change_list):
                         'The subtopic with id %s doesn\'t exist' % change.id)
 
                 if (change.property_name ==
-                        subtopic_page_domain.SUBTOPIC_PAGE_PROPERTY_HTML_DATA):
-                    modified_subtopic_pages[subtopic_page_id].update_html_data(
-                        change.new_value)
+                        subtopic_page_domain.
+                        SUBTOPIC_PAGE_PROPERTY_PAGE_CONTENTS_HTML):
+                    modified_subtopic_pages[
+                        subtopic_page_id].update_page_contents_html(
+                            change.new_value)
+
+                elif (change.property_name ==
+                      subtopic_page_domain.
+                      SUBTOPIC_PAGE_PROPERTY_PAGE_CONTENTS_AUDIO):
+                    modified_subtopic_pages[
+                        subtopic_page_id].update_page_contents_audio(
+                            change.new_value)
                 else:
                     raise Exception('Invalid change dict.')
             elif change.cmd == topic_domain.CMD_UPDATE_SUBTOPIC_PROPERTY:
