478020b68f92a7b1252d851f180f940c6241c8ba
netajik <kancharapunetaji@gmail.com>
2019-01-15 13:35:10 +0530

    Fix part of #5134: Added backend tests for core.controllers.pages

--- core/controllers/pages_test.py
+++ core/controllers/pages_test.py
@@ -19,9 +19,9 @@ import feconf
 
 
 class AboutPageTest(test_utils.GenericTestBase):
+    """Test for About page."""
 
     def test_about_page(self):
-        """Test for About page."""
         response = self.testapp.get('/about')
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.content_type, 'text/html')
@@ -30,9 +30,9 @@ class AboutPageTest(test_utils.GenericTestBase):
 
 
 class SplashPageTest(test_utils.GenericTestBase):
+    """Test for splash page."""
 
     def test_splash_page(self):
-        """Test for splash page."""
         response = self.testapp.get('/splash')
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.content_type, 'text/html')
@@ -47,9 +47,9 @@ class SplashPageTest(test_utils.GenericTestBase):
 
 
 class GetStartedPageTest(test_utils.GenericTestBase):
+    """Test for get started page."""
 
     def test_get_started_page(self):
-        """Test for get started page."""
         response = self.testapp.get('/get_started')
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.content_type, 'text/html')
@@ -57,9 +57,9 @@ class GetStartedPageTest(test_utils.GenericTestBase):
 
 
 class TeachPageTest(test_utils.GenericTestBase):
+    """Test for teach page."""
 
     def test_teach_page(self):
-        """Test for teach page."""
         response = self.testapp.get('/teach')
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.content_type, 'text/html')
@@ -67,9 +67,9 @@ class TeachPageTest(test_utils.GenericTestBase):
 
 
 class ContactPageTest(test_utils.GenericTestBase):
+    """Test for contact page."""
 
     def test_contact_page(self):
-        """Test for contact page."""
         response = self.testapp.get('/contact')
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.content_type, 'text/html')
@@ -77,36 +77,36 @@ class ContactPageTest(test_utils.GenericTestBase):
 
 
 class DonatePageTest(test_utils.GenericTestBase):
+    """Test for donate page."""
 
     def test_donate_page(self):
-        """Test for donate page."""
         response = self.testapp.get('/donate')
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.content_type, 'text/html')
 
 
 class ThanksPageTest(test_utils.GenericTestBase):
+    """Test for thanks page."""
 
     def test_thanks_page(self):
-        """Test for thanks page."""
         response = self.testapp.get('/thanks')
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.content_type, 'text/html')
 
 
 class ForumPageTest(test_utils.GenericTestBase):
+    """Test for forum page."""
 
     def test_forum_page(self):
-        """Test for forum page."""
         response = self.testapp.get('/forum')
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.content_type, 'text/html')
 
 
 class TermsPageTest(test_utils.GenericTestBase):
+    """Test for terms page."""
 
     def test_terms_page(self):
-        """Test for terms page."""
         response = self.testapp.get('/terms')
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.content_type, 'text/html')
@@ -114,50 +114,50 @@ class TermsPageTest(test_utils.GenericTestBase):
 
 
 class PrivacyPageTest(test_utils.GenericTestBase):
+    """Test for privacy page."""
 
     def test_privacy_page(self):
-        """Test for privacy page."""
         response = self.testapp.get('/privacy')
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.content_type, 'text/html')
 
 
 class AboutRedirectPageTest(test_utils.GenericTestBase):
+    """Test for about redirect page."""
 
     def test_about_redirect_page(self):
-        """Test for about redirect page."""
         response = self.testapp.get('/credits')
         self.assertEqual(response.status_int, 302)
 
 
 class FoundationRedirectPageTest(test_utils.GenericTestBase):
+    """Test for foundation redirect page."""
 
     def test_foundation_redirect_page(self):
-        """Test for foundation redirect page."""
         response = self.testapp.get('/foundation')
         self.assertEqual(response.status_int, 302)
 
 
 class TeachRedirectPageTest(test_utils.GenericTestBase):
+    """Test for teach redirect page."""
 
     def test_teach_redirect_page(self):
-        """Test for teach redirect page."""
         response = self.testapp.get('/participate')
         self.assertEqual(response.status_int, 302)
 
 
 class ConsoleErrorPageTest(test_utils.GenericTestBase):
+    """Test for console error page."""
 
     def test_console_error_page(self):
-        """Test for console error page."""
         response = self.testapp.get('/console_errors')
         self.assertEqual(response.status_int, 200)
 
 
 class MaintenancePageTest(test_utils.GenericTestBase):
+    """Test for maintenance page."""
 
     def test_maintenance_page(self):
-        """Test for maintenance page."""
         with self.swap(feconf, 'ENABLE_MAINTENANCE_MODE', True):
             response = self.testapp.get('/admin')
             self.assertEqual(response.status_int, 302)
