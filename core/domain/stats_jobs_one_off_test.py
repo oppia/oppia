1504ba5de4b5f5014c5e4a1db84f2a8add26983d
Brian Rodriguez <brian@brianrodri.com>
2019-01-20 02:45:36 -0500

    better spacing

--- core/domain/stats_jobs_one_off_test.py
+++ core/domain/stats_jobs_one_off_test.py
@@ -233,8 +233,9 @@ class RemoveInvalidPlaythroughsOneOffJobTests(OneOffJobTestBase):
         self.assertEqual(
             issues_model.unresolved_issues[0]['playthrough_ids'],
             [new_playthrough_model_ids[0], new_playthrough_model_ids[1]])
-        # The second issue element should be the one with an old and a new
-        # playthrough. Only the new playthrough should remain.
+        # The second issue element should be the one with an old playthrough and
+        # a new playthrough.
+        # Only the new playthrough should remain.
         self.assertEqual(
             issues_model.unresolved_issues[1]['playthrough_ids'],
             [new_playthrough_model_ids[2]])
