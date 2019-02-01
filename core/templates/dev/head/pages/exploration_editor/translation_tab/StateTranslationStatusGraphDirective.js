fab453f9805ae61c7e042b258819b8f2e46811bc
Rakshit Kumar <rakshit.kumar.cse17@itbhu.ac.in>
2019-02-01 14:20:53 +0530

    Final changes.

--- core/templates/dev/head/pages/exploration_editor/translation_tab/StateTranslationStatusGraphDirective.js
+++ core/templates/dev/head/pages/exploration_editor/translation_tab/StateTranslationStatusGraphDirective.js
@@ -51,7 +51,9 @@ oppia.directive('stateTranslationStatusGraph', [
                 StateEditorService.getActiveStateName(),
                 stateData.contentIdsToAudioTranslations);
               $rootScope.$broadcast('refreshStateTranslation');
+              setTimeout(function() {
               $rootScope.$broadcast('refreshAudioTranslationBar');
+            }, 0);
             }
           };
         }
