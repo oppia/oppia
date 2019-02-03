fc38a04e92017dd9158ff3322d164509777ae8fb
aks681 <akshayanand681@gmail.com>
2019-02-03 23:40:13 +0530

    added topics and skills dashboard e2e tests

---
+++ core/tests/protractor_desktop/topicsAndSkillsDashboard.js
@@ -0,0 +1,71 @@
+// Copyright 2018 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview End-to-end tests for the topics and skills dashboard page.
+ */
+
+var forms = require('../protractor_utils/forms.js');
+var general = require('../protractor_utils/general.js');
+var users = require('../protractor_utils/users.js');
+var waitFor = require('../protractor_utils/waitFor.js');
+var workflow = require('../protractor_utils/workflow.js');
+
+var AdminPage = require('../protractor_utils/AdminPage.js');
+var TopicsAndSkillsDashboardPage =
+  require('../protractor_utils/TopicsAndSkillsDashboardPage.js');
+
+describe('Learner dashboard functionality', function() {
+  var topicsAndSkillsDashboardPage = null;
+
+  beforeAll(function() {
+    topicsAndSkillsDashboardPage =
+      new TopicsAndSkillsDashboardPage.TopicsAndSkillsDashboardPage();
+    users.createAdmin('creator@topicsAndSkillsDashboard.com',
+      'creatorTopicsAndSkillsDashboard');
+  });
+
+  beforeEach(function() {
+    users.login('creator@topicsAndSkillsDashboard.com');
+  });
+
+  it('should add a new topic to list', function() {
+    topicsAndSkillsDashboardPage.get();
+    topicsAndSkillsDashboardPage.expectNumberOfTopicsToBe(0);
+    topicsAndSkillsDashboardPage.createTopicWithTitle('Topic 1');
+
+    topicsAndSkillsDashboardPage.get();
+    topicsAndSkillsDashboardPage.expectNumberOfTopicsToBe(1);
+  });
+
+  it('should add a new unpublished skill to list', function() {
+    topicsAndSkillsDashboardPage.get();
+    topicsAndSkillsDashboardPage.navigateToUnusedSkillsTab();
+
+    topicsAndSkillsDashboardPage.expectNumberOfSkillsToBe(0);
+    topicsAndSkillsDashboardPage.createSkillWithDescription('Skill 1');
+
+    topicsAndSkillsDashboardPage.get();
+    topicsAndSkillsDashboardPage.navigateToUnpublishedSkillsTab();
+    topicsAndSkillsDashboardPage.expectNumberOfSkillsToBe(1);
+  });
+
+  // TODO(aks681): Once skill editor tests are done by Nalin, add tests here
+  // regarding published skills.
+
+  afterEach(function() {
+    general.checkForConsoleErrors([]);
+    users.logout();
+  });
+});
