4a1046a3eca28b042679bca39333c9a38ee7455e
aks681 <akshayanand681@gmail.com>
2019-02-12 23:38:12 +0530

    fix lint

--- core/domain/config_domain.py
+++ core/domain/config_domain.py
@@ -269,7 +269,7 @@ WHITELISTED_EXPLORATION_IDS_FOR_PLAYTHROUGHS = ConfigProperty(
     'The set of exploration IDs for recording playthrough issues', [
         'umPkwp0L1M0-', 'MjZzEVOG47_1', '9trAQhj6uUC2', 'rfX8jNkPnA-1',
         '0FBWxCE5egOw', '670bU6d9JGBh', 'aHikhPlxYgOH', '-tMgcP1i_4au',
-        'zW39GLG_BdN2', 'Xa3B_io-2WI5', '6Q6IyIDkjpYC', 'osw1m5Q3jK41', '3bIacy28xisB'])
+        'zW39GLG_BdN2', 'Xa3B_io-2WI5', '6Q6IyIDkjpYC', 'osw1m5Q3jK41'])
 
 RECORD_PLAYTHROUGH_PROBABILITY = ConfigProperty(
     'record_playthrough_probability', FLOAT_SCHEMA,
