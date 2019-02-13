4bca15411dd055ad27d4a81be6f7cb16aa2cc5c8
Rakshit Kumar <rakshit.kumar.cse17@itbhu.ac.in>
2019-02-13 14:40:18 +0530

    Update

--- core/templates/dev/head/pages/exploration_editor/translation_tab/TranslationStatusServiceSpec.js
+++ core/templates/dev/head/pages/exploration_editor/translation_tab/TranslationStatusServiceSpec.js
@@ -16,56 +16,65 @@
  * @fileoverview Unit test for the Translation status service.
  */
 
-describe('Translation status service', function() {
+fdescribe('Translation status service', function() {
   beforeEach(module('oppia', function($provide) {
     $provide.value('TranslationLanguageService', {
       getActiveLanguageCode: function() {
         return 'en';
       },
-      availableTranslationLanguageCodes: ['en'],
-      allContentId: ['content', 'default_outcome', 'feedback_1']
-    });
-    $provide.value('StateContentIdsToAudioTranslationsService', {
-      displayed: {
-        getAllContentId: function() {
-          return ['content', 'default_outcome', 'feedback_1'];
-        },
-        getAudioTranslation: function(contentId, langCode) {
-          if (contentId === 'content') {
-            return {filename: 'test_audio_1_en.mp3',
-              fileSizeBytes: 95426, needsUpdate: true};
-          } else {
-            return {};
-          }
-        }
-      }
+      availableTranslationLanguageCodes: ['en']
     });
     $provide.value('ExplorationStatesService', {
       isInitialized: function() {
         return true;
       },
       getStateNames: function() {
-        return ['First'];
+        return ['First', 'Second', 'Third'];
       },
       getContentIdsToAudioTranslationsMemento: function(stateName) {
         if (stateName === 'First') {
-          return ContentIdsToAudioTranslationsObjectFactory.
+          var audio = ContentIdsToAudioTranslationsObjectFactory.
             createFromBackendDict({
-              content: {en: {filename: 'test_audio_1_en.mp3',
-                fileSizeBytes: 95426, needsUpdate: true}},
+              content: {},
               default_outcome: {},
               feedback_1: {}
             });
+            audio.addAudioTranslation('content', 'en', 'test_audio_1_en.mp3',
+              96426);
+            audio.toggleNeedsUpdateAttribute('content', 'en');
+            return audio;
+        } else if (stateName === 'Second') {
+          var audio = ContentIdsToAudioTranslationsObjectFactory.
+            createFromBackendDict({
+              content: {},
+              default_outcome: {},
+              feedback_1: {}
+            });
+          return audio;
+        } else if (stateName === 'Third') {
+          var audio = ContentIdsToAudioTranslationsObjectFactory.
+            createFromBackendDict({
+              content: {}
+            });
+            audio.addAudioTranslation('content', 'en', 'test_audio_2_en.mp3',
+              90000);
+            return audio;
         }
       },
       getInteractionIdMemento: function(stateName) {
-        if (stateName === 'First') {
+        if (stateName === 'First' || stateName === 'Second') {
           return 'MultipleChoiceInput';
+        } else if (stateName === 'Third') {
+          return 'EndExploration';
         }
       }
     });
     $provide.constant('INTERACTION_SPECS', {
       MultipleChoiceInput: {
+        is_linear: false,
+        is_terminal: false
+      },
+      EndExploration: {
         is_linear: false,
         is_terminal: false
       }
@@ -80,11 +89,42 @@ describe('Translation status service', function() {
         'ContentIdsToAudioTranslationsObjectFactory');
     }));
 
-    it('should get state names that need audio update', function() {
+    it('should get state names that need audio update correctly', function() {
       // To call _computeAllStatesStatus() function of TranslationStatusService.
       tss.getAllStateStatusColors();
 
       var statesNeedingAudioUpdate = tss.getAllStatesNeedUpdatewarning();
+      expect(statesNeedingAudioUpdate['First'].
+        indexOf('Audio needs update!')).toBe(0);
+      expect(statesNeedingAudioUpdate['Second']).toBe(undefined);
+      expect(statesNeedingAudioUpdate['Third']).toBe(undefined);
+    });
+
+    it('should get count of required audio translations correctly',
+      function() {
+      // To call _computeAllStatesStatus() function of TranslationStatusService.
+      tss.getAllStateStatusColors();
+
+      var explorationAudioRequiredCount = tss.
+      getExplorationAudioRequiredCount();
+      expect(explorationAudioRequiredCount).toBe(7);
+    });
+
+    it('should get count of audio translations not available correctly',
+      function() {
+      // To call _computeAllStatesStatus() function of TranslationStatusService.
+      tss.getAllStateStatusColors();
+
+      var explorationAudioNotAvailableCount = tss.
+      getExplorationAudioNotAvailableCount();
+      expect(explorationAudioNotAvailableCount).toBe(5);
+    });
+
+    it('should get all state status colors correctly', function() {
+      var stateWiseStatusColor = tss.getAllStateStatusColors();
+      expect(stateWiseStatusColor['First']).toBe('#E9B330');
+      expect(stateWiseStatusColor['Second']).toBe('#D14836');
+      expect(stateWiseStatusColor['Third']).toBe('#16A765');
     });
   });
 });
