d8e8415799efdabbfc5016e3fcd88f8ac7dcb56b
mzaman07 <9009585+mzaman07@users.noreply.github.com>
2019-02-21 16:06:55 -0700

    lint fix

--- core/templates/dev/head/pages/exploration_editor/translation_tab/AudioTranslationBarDirective.js
+++ core/templates/dev/head/pages/exploration_editor/translation_tab/AudioTranslationBarDirective.js
@@ -48,7 +48,7 @@ oppia.directive('audioTranslationBar', [
 
         $('.oppia-translation-tab').on('drop', function(evt) {
           evt.preventDefault();
-          if (evt.target.classList.contains('oppia-drop-area-message') && 
+          if (evt.target.classList.contains('oppia-drop-area-message') &&
             GLOBALS.can_translate) {
             files = evt.originalEvent.dataTransfer.files;
             scope.openAddAudioTranslationModal(files);
