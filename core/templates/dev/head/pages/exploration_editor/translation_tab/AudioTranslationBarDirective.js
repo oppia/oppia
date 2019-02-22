97d06d9f124248fd0b3f3a518ee6b1761895bb59
mzaman07 <9009585+mzaman07@users.noreply.github.com>
2019-02-22 02:11:27 -0700

    added a check on drop to check for login status.

--- core/templates/dev/head/pages/exploration_editor/translation_tab/AudioTranslationBarDirective.js
+++ core/templates/dev/head/pages/exploration_editor/translation_tab/AudioTranslationBarDirective.js
@@ -49,7 +49,7 @@ oppia.directive('audioTranslationBar', [
         $('.oppia-translation-tab').on('drop', function(evt) {
           evt.preventDefault();
           if (evt.target.classList.contains('oppia-drop-area-message') &&
-            GLOBALS.can_translate) {
+            GLOBALS.can_translate && GLOBALS.userIsLoggedIn) {
             files = evt.originalEvent.dataTransfer.files;
             scope.openAddAudioTranslationModal(files);
           }
