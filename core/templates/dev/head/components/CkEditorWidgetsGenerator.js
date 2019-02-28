b84d1b1d75bf64c427d6bde9e55f4a29ce67b28e
Keshav Bathla <keshavbathla2017@gmail.com>
2019-02-28 17:50:24 +0530

    fixes docs issue

--- core/templates/dev/head/components/CkEditorWidgetsGenerator.js
+++ core/templates/dev/head/components/CkEditorWidgetsGenerator.js
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 /**
- * @fileoverview Dynamically generate CKEditor widgets for the rich text
+ * @fileoverview dynamically generate CKEditor widgets for the rich text
  components.
  */
 
