8f16ccc289a1aa0fd67e97e083beaeb59b1ab144
Akshay Anand <akshayanand681@gmail.com>
2019-03-03 12:08:47 +0530

    New Structures e2e tests (#6315)
    * added topics and skills dashboard e2e tests
    
    * fix lint
    
    * fixed test
    
    * added topic and skill deletion tests
    
    * Created test files
    
    * Modified conf files
    
    * added topic name, description and create story tests
    
    * added story editor tests
    
    * Add E2E tests for Skill Editor
    
    * changes
    
    * Added skill publishing tests
    
    * Lint
    
    * added subtopic tests
    
    * Fix #5692 and done with all e2e tests

--- core/tests/protractor_utils/ExplorationEditorMainTab.js
+++ core/tests/protractor_utils/ExplorationEditorMainTab.js
@@ -139,6 +139,8 @@ var ExplorationEditorMainTab = function() {
     by.css('.protractor-test-save-state-content'));
   var stateNameSubmitButton = stateNameContainer.element(
     by.css('.protractor-test-state-name-submit'));
+  var answerCorrectnessToggle = element(
+    by.css('.protractor-test-editor-correctness-toggle'));
 
   /*
    * Actions
@@ -273,8 +275,10 @@ var ExplorationEditorMainTab = function() {
     // If the destination is being changed, open the corresponding editor.
     if (destStateName || destStateName !== '(try again)') {
     // Set destination contents.
-      _setOutcomeDest(
-        destStateName, createNewState, null);
+      if (destStateName !== null) {
+        _setOutcomeDest(
+          destStateName, createNewState, null);
+      }
     }
 
     // Close new response modal.
@@ -359,6 +363,9 @@ var ExplorationEditorMainTab = function() {
         // Save destination.
         expect(saveOutcomeDestButton.isDisplayed()).toBe(true);
         saveOutcomeDestButton.click();
+      },
+      markAsCorrect: function() {
+        answerCorrectnessToggle.click();
       },
       // The current state name must be at the front of the list.
       expectAvailableDestinationsToBe: function(stateNames) {
