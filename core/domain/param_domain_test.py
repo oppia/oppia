156ef38caddb928a76082e0ace2359427ca6cd3d
nikhilmudholkar <nikhilrmudholkar29@gmail.com>
2019-03-04 11:56:24 +0530

    97% coverage for core.domain.param_domain

--- core/domain/param_domain_test.py
+++ core/domain/param_domain_test.py
@@ -21,9 +21,31 @@ from core.tests import test_utils
 import utils
 
 
-class ParameterDomainUnitTests(test_utils.GenericTestBase):
+class ParamSpecUnitTests(test_utils.GenericTestBase):
     """Tests for parameter domain objects."""
+    # Unittest to test to_dict function.
+    def test_to_dict(self):
+        initial_vals = {
+            'obj_type': 'UnicodeString',
+        }
+        param_spec_obj = param_domain.ParamSpec('UnicodeString')
+        # ParamSpec.from_dict(initial_vals)
+        self.assertDictEqual(
+            initial_vals,
+            param_spec_obj.to_dict())
 
+    # Unittest to test from_dict function.
+    def test_from_dict(self):
+        initial_vals = {
+            'obj_type': 'UnicodeString',
+        }
+        param_spec_obj = param_domain.ParamSpec('UnicodeString')
+        obj_from_dict = param_spec_obj.from_dict(initial_vals)
+        self.assertDictEqual(
+            param_spec_obj.to_dict(),
+            obj_from_dict.to_dict())
+
+    # UnitTest to test validate function in ParamSpec class.
     def test_param_spec_validation(self):
         """Test validation of param specs."""
         param_spec = param_domain.ParamSpec('FakeType')
@@ -40,11 +62,50 @@ class ParameterDomainUnitTests(test_utils.GenericTestBase):
         param_spec.obj_type = 'UnicodeString'
         param_spec.validate()
 
+
+class ParamChangeUnitTests(test_utils.GenericTestBase):
+
+    # Unittest to test to_dict function.
+    def test_to_dict(self):
+        initial_vals = {
+            'name': 'abc',
+            'generator_id': 'Copier',
+            'customization_args': {'value': '3'}
+        }
+        param_change_obj = param_domain.ParamChange(
+            'abc', 'Copier', {'value': '3'})
+        # ParamSpec.from_dict(initial_vals)
+        self.assertDictEqual(
+            initial_vals,
+            param_change_obj.to_dict())
+
+    # Unittest to test from_dict function.
+    def test_from_dict(self):
+        initial_vals = {
+            'name': 'abc',
+            'generator_id': 'Copier',
+            'customization_args': {'value': '3'}
+        }
+        param_change_obj = param_domain.ParamChange(
+            'abc', 'Copier', {'value': '3'})
+        obj_from_dict = param_change_obj.from_dict(initial_vals)
+        self.assertDictEqual(
+            param_change_obj.to_dict(),
+            obj_from_dict.to_dict())
+
+
+    # UnitTest to test validate function in ParamChange class.
     def test_param_change_validation(self):
         """Test validation of parameter changes."""
+        # Raise an error because the name is not a string.
+        with self.assertRaisesRegexp(
+            utils.ValidationError, 'Expected param_change name to be a string'
+            ):
+            param_domain.ParamChange(1, 'Copier', {}).validate()
+
         # Raise an error because the name is invalid.
         with self.assertRaisesRegexp(
-            utils.ValidationError, 'Only parameter names'
+            utils.ValidationError, 'Only parameter names with characters'
             ):
             param_domain.ParamChange('¡hola', 'Copier', {}).validate()
 
@@ -59,6 +120,13 @@ class ParameterDomainUnitTests(test_utils.GenericTestBase):
             utils.ValidationError, 'Expected a dict'
             ):
             param_domain.ParamChange('abc', 'Copier', ['a', 'b']).validate()
+
+        # Raise an error because the customization_arg name is not a string.
+        with self.assertRaisesRegexp(
+            Exception, 'Invalid parameter change customization_arg name'
+            ):
+            param_domain.ParamChange('abc', 'Copier', {True: '3'}).validate()
+
 
     def test_param_change_class(self):
         """Test the ParamChange class."""
