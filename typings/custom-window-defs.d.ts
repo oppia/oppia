06cd1886475a7012062fd4f52d1b240b99919afc
Ankita Saxena <ankitasonu24@gmail.com>
2019-03-04 21:51:59 +0530

    Add custom typings for window

---
+++ typings/custom-window-defs.d.ts
@@ -0,0 +1,16 @@
+interface Window {
+	GLOBALS?: any;
+	CodeMirror?: any;
+	HTMLElement?: any;
+	safari?: any;
+	opera?: any;
+	MathJax?: any;
+	Math?: any;
+	decodeURIComponent?: any;
+	encodeURIComponent?: any;
+	BlobBuilder?: any;
+	WebKitBlobBuilder?: any;
+	MozBlobBuilder?: any;
+	MSBlobBuilder?: any;
+	__fixtures__?: any;
+}
