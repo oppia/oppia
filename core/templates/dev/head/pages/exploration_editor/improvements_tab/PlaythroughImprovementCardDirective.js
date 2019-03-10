af6660a84a50cbbc0431a8da6fd04f3ad3db6d99
Brian Rodriguez <brian@brianrodri.com>
2019-03-10 15:17:18 -0400

    use bidirectional binding

--- core/templates/dev/head/pages/exploration_editor/improvements_tab/PlaythroughImprovementCardDirective.js
+++ core/templates/dev/head/pages/exploration_editor/improvements_tab/PlaythroughImprovementCardDirective.js
@@ -17,7 +17,7 @@ oppia.directive('playthroughImprovementCard', [
     return {
       restrict: 'E',
       scope: {
-        data: '&',
+        data: '=',
       },
       templateUrl: UrlInterpolationService.getDirectiveTemplateUrl(
         '/pages/exploration_editor/improvements_tab/' +
