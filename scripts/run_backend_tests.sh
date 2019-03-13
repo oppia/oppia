c57a40f2993ad56ce5ed03c6003cf4164d309ae8
Ankita Saxena <ankitasonu24@gmail.com>
2019-03-13 15:02:41 +0530

    Revert fix

--- scripts/run_backend_tests.sh
+++ scripts/run_backend_tests.sh
@@ -87,7 +87,7 @@ done
 
 # Compile typescript files
 echo "Compiling typescript..."
-../node_modules/typescript/bin/tsc
+$NODE_MODULE_DIR/typescript/bin/tsc
 
 $PYTHON_CMD scripts/backend_tests.py $@
 
