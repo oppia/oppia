a4bf97ae99969d4eff39e019f6a3d4de9ea0ecff
ameykudari <ameykudari05@gmail.com>
2019-03-14 17:57:24 +0530

    fixed typo

--- core/tests/protractor_utils/workflow.js
+++ core/tests/protractor_utils/workflow.js
@@ -122,7 +122,7 @@ var createAndPublishExploration = function(
 // Here, 'roleName' is the user-visible form of the role name (e.g. 'Manager').
 var _addExplorationRole = function(roleName, username) {
   element(
-    by.model('ExplorationTitleService.displayed')).sendKeys('Chuck Norris');
+    by.model('explorationTitleService.displayed')).sendKeys('Chuck Norris');
   element(by.css('.protractor-test-edit-roles')).click();
   element(by.css('.protractor-test-role-username')).sendKeys(username);
   element(by.css('.protractor-test-role-select')).
