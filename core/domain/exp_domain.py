89f813260b8ebf195af4c5a31829a4d93b2e82e0
Sandeep Dubey <dubeysandeep.in@gmail.com>
2019-03-18 18:57:03 +0530

    Added exploration migration.

--- core/domain/exp_domain.py
+++ core/domain/exp_domain.py
@@ -2177,6 +2177,40 @@ class Exploration(object):
         return states_dict
 
     @classmethod
+    def _convert_states_v27_dict_to_v28_dict(cls, states_dict):
+        """Converts from version 27 to 28. Version 28 adds content_id to
+        customization_arg for interactions which can have translations.
+
+        Args:
+            states_dict: dict. A dict where each key-value pair represents,
+                respectively, a state name and a dict used to initialize a
+                State domain object.
+
+        Returns:
+            dict. The converted states_dict.
+        """
+        for state_dict in states_dict.values():
+            if state_dict['interaction']['id'] == 'DragAndDropSortInput' or (
+                state_dict['interaction']['id'] == 'ItemSelectionInput') or (
+                state_dict['interaction']['id'] == 'MultipleChoiceInput'):
+                customization_args = state_dict[
+                    'interaction']['customization_args']
+                new_values = []
+                for value in customization_args['choices']['value']:
+                    new_values.append({
+                        'html': value,
+                        'content_id': 'interaction_' + (
+                            utils.generate_random_string(5))
+                    })
+                customization_args.update({
+                    'choices': {
+                        'value': new_values
+                    }
+                })
+
+        return states_dict
+
+    @classmethod
     def update_states_from_model(
             cls, versioned_exploration_states, current_states_schema_version,
             exploration_id):
@@ -2211,7 +2245,7 @@ class Exploration(object):
     # incompatible changes are made to the exploration schema in the YAML
     # definitions, this version number must be changed and a migration process
     # put in place.
-    CURRENT_EXP_SCHEMA_VERSION = 32
+    CURRENT_EXP_SCHEMA_VERSION = 33
     LAST_UNTITLED_SCHEMA_VERSION = 9
 
     @classmethod
@@ -2789,6 +2823,21 @@ class Exploration(object):
         return exploration_dict
 
     @classmethod
+    def _convert_v32_dict_to_v33_dict(cls, exploration_dict):
+        """Converts a v32 exploration dict into a v33 exploration dict.
+
+        Adds content_id to customization_args for interaction which can have
+        translations.
+        """
+        exploration_dict['schema_version'] = 33
+
+        exploration_dict['states'] = cls._convert_states_v27_dict_to_v28_dict(
+            exploration_dict['states'])
+        exploration_dict['states_schema_version'] = 28
+
+        return exploration_dict
+
+    @classmethod
     def _migrate_to_latest_yaml_version(
             cls, yaml_content, exp_id, title=None, category=None):
         """Return the YAML content of the exploration in the latest schema
@@ -2980,6 +3029,11 @@ class Exploration(object):
             exploration_dict = cls._convert_v31_dict_to_v32_dict(
                 exploration_dict)
             exploration_schema_version = 32
+
+        if exploration_schema_version == 32:
+            exploration_dict = cls._convert_v32_dict_to_v33_dict(
+                exploration_dict)
+            exploration_schema_version = 33
 
         return (exploration_dict, initial_schema_version)
 
--- core/domain/exp_domain_test.py
+++ core/domain/exp_domain_test.py
@@ -3661,7 +3661,128 @@ states_schema_version: 27
 tags: []
 title: Title
 """)
-    _LATEST_YAML_CONTENT = YAML_CONTENT_V32
+
+    YAML_CONTENT_V33 = ("""author_notes: ''
+auto_tts_enabled: true
+blurb: ''
+category: Category
+correctness_feedback_enabled: false
+init_state_name: (untitled state)
+language_code: en
+objective: ''
+param_changes: []
+param_specs: {}
+schema_version: 33
+states:
+  (untitled state):
+    classifier_model_id: null
+    content:
+      content_id: content
+      html: ''
+    content_ids_to_audio_translations:
+      content: {}
+      default_outcome: {}
+      feedback_1: {}
+    interaction:
+      answer_groups:
+      - outcome:
+          dest: END
+          feedback:
+            content_id: feedback_1
+            html: <p>Correct!</p>
+          labelled_as_correct: false
+          missing_prerequisite_skill_id: null
+          param_changes: []
+          refresher_exploration_id: null
+        rule_specs:
+        - inputs:
+            x: InputString
+          rule_type: Equals
+        tagged_misconception_id: null
+        training_data: []
+      confirmed_unclassified_answers: []
+      customization_args:
+        placeholder:
+          value: ''
+        rows:
+          value: 1
+      default_outcome:
+        dest: (untitled state)
+        feedback:
+          content_id: default_outcome
+          html: ''
+        labelled_as_correct: false
+        missing_prerequisite_skill_id: null
+        param_changes: []
+        refresher_exploration_id: null
+      hints: []
+      id: TextInput
+      solution: null
+    param_changes: []
+    written_translations:
+      translations_mapping:
+        content: {}
+        default_outcome: {}
+        feedback_1: {}
+  END:
+    classifier_model_id: null
+    content:
+      content_id: content
+      html: <p>Congratulations, you have finished!</p>
+    content_ids_to_audio_translations:
+      content: {}
+    interaction:
+      answer_groups: []
+      confirmed_unclassified_answers: []
+      customization_args:
+        recommendedExplorationIds:
+          value: []
+      default_outcome: null
+      hints: []
+      id: EndExploration
+      solution: null
+    param_changes: []
+    written_translations:
+      translations_mapping:
+        content: {}
+  New state:
+    classifier_model_id: null
+    content:
+      content_id: content
+      html: ''
+    content_ids_to_audio_translations:
+      content: {}
+      default_outcome: {}
+    interaction:
+      answer_groups: []
+      confirmed_unclassified_answers: []
+      customization_args:
+        placeholder:
+          value: ''
+        rows:
+          value: 1
+      default_outcome:
+        dest: END
+        feedback:
+          content_id: default_outcome
+          html: ''
+        labelled_as_correct: false
+        missing_prerequisite_skill_id: null
+        param_changes: []
+        refresher_exploration_id: null
+      hints: []
+      id: TextInput
+      solution: null
+    param_changes: []
+    written_translations:
+      translations_mapping:
+        content: {}
+        default_outcome: {}
+states_schema_version: 28
+tags: []
+title: Title
+""")
+    _LATEST_YAML_CONTENT = YAML_CONTENT_V33
 
     def test_load_from_v1(self):
         """Test direct loading from a v1 yaml file."""
@@ -4066,7 +4187,7 @@ title: title
 """)
 
 # pylint: disable=line-too-long
-    YAML_CONTENT_V32_IMAGE_DIMENSIONS = ("""author_notes: ''
+    YAML_CONTENT_V33_IMAGE_DIMENSIONS = ("""author_notes: ''
 auto_tts_enabled: true
 blurb: ''
 category: category
@@ -4076,7 +4197,7 @@ language_code: en
 objective: ''
 param_changes: []
 param_specs: {}
-schema_version: 32
+schema_version: 33
 states:
   Introduction:
     classifier_model_id: null
@@ -4293,7 +4414,7 @@ states:
         content: {}
         default_outcome: {}
         feedback_1: {}
-states_schema_version: 27
+states_schema_version: 28
 tags: []
 title: title
 """)
@@ -4408,7 +4529,7 @@ tags: []
 title: Title
 """)
 
-    YAML_CONTENT_V32_WITH_IMAGE_CAPTION = ("""author_notes: ''
+    YAML_CONTENT_V33_WITH_IMAGE_CAPTION = ("""author_notes: ''
 auto_tts_enabled: true
 blurb: ''
 category: Category
@@ -4418,7 +4539,7 @@ language_code: en
 objective: ''
 param_changes: []
 param_specs: {}
-schema_version: 32
+schema_version: 33
 states:
   (untitled state):
     classifier_model_id: null
@@ -4526,7 +4647,7 @@ states:
       translations_mapping:
         content: {}
         default_outcome: {}
-states_schema_version: 27
+states_schema_version: 28
 tags: []
 title: Title
 """)
@@ -4543,7 +4664,7 @@ title: Title
             exploration = exp_domain.Exploration.from_yaml(
                 'eid', self.YAML_CONTENT_V26_TEXTANGULAR)
         self.assertEqual(
-            exploration.to_yaml(), self.YAML_CONTENT_V32_IMAGE_DIMENSIONS)
+            exploration.to_yaml(), self.YAML_CONTENT_V33_IMAGE_DIMENSIONS)
 
     def test_load_from_v27_without_image_caption(self):
         """Test direct loading from a v27 yaml file."""
@@ -4554,7 +4675,7 @@ title: Title
             exploration = exp_domain.Exploration.from_yaml(
                 'eid', self.YAML_CONTENT_V27_WITHOUT_IMAGE_CAPTION)
         self.assertEqual(
-            exploration.to_yaml(), self.YAML_CONTENT_V32_WITH_IMAGE_CAPTION)
+            exploration.to_yaml(), self.YAML_CONTENT_V33_WITH_IMAGE_CAPTION)
 
 
 class ConversionUnitTests(test_utils.GenericTestBase):
