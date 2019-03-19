74425b20d96d0ab603cc38b0006a6489a45bfc9a
Ankita <ankitasonu24@gmail.com>
2019-03-19 22:48:01 +0530

    Check ajv update for test

--- scripts/install_third_party.sh
+++ scripts/install_third_party.sh
@@ -22,7 +22,7 @@ echo Installing third-party JS libraries and zip files.
 $PYTHON_CMD scripts/install_third_party.py
 
 # Install third-party node modules needed for the build process.
-install_node_module ajv 5.0.0
+install_node_module ajv 6.9.1
 install_node_module babel-eslint 10.0.1
 install_node_module browserstack-local 1.3.3
 install_node_module dotenv 6.0.0
