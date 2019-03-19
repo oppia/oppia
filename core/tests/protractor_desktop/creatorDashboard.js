857c823e3059bb207e36183663a1cfdc2174740e
apoorv1997 <apoorv.b15@iiits.in>
2019-03-19 18:31:29 +0530

    create dashboard frontend test completed

--- core/tests/protractor_desktop/creatorDashboard.js
+++ core/tests/protractor_desktop/creatorDashboard.js
@@ -32,95 +32,95 @@ var ExplorationPlayerPage = require('../protractor_utils/ExplorationPlayerPage.j
 var LibraryPage = require('../protractor_utils/LibraryPage.js');
 
 describe('Creator dashboard functionality', function() {
-    var creatorDashboardPage = null;
-    var explorationEditorPage = null;
-    var explorationPlayerPage = null;
-    var libraryPage = null;
-    var learnerDashboardPage = null;
+  var creatorDashboardPage = null;
+  var explorationEditorPage = null;
+  var explorationPlayerPage = null;
+  var libraryPage = null;
+  var learnerDashboardPage = null;
   
-    beforeAll(function() {
-      libraryPage = new LibraryPage.LibraryPage();
-      collectionEditorPage = new CollectionEditorPage.CollectionEditorPage();
-      creatorDashboardPage = new CreatorDashboardPage.CreatorDashboardPage();
-      explorationEditorPage = new ExplorationEditorPage.ExplorationEditorPage();
-      explorationEditorMainTab = explorationEditorPage.getMainTab();
-      explorationEditorSettingsTab = explorationEditorPage.getSettingsTab();
-      explorationPlayerPage = new ExplorationPlayerPage.ExplorationPlayerPage();
-    });
+  beforeAll(function() {
+    libraryPage = new LibraryPage.LibraryPage();
+    collectionEditorPage = new CollectionEditorPage.CollectionEditorPage();
+    creatorDashboardPage = new CreatorDashboardPage.CreatorDashboardPage();
+    explorationEditorPage = new ExplorationEditorPage.ExplorationEditorPage();
+    explorationEditorMainTab = explorationEditorPage.getMainTab();
+    explorationEditorSettingsTab = explorationEditorPage.getSettingsTab();
+    explorationPlayerPage = new ExplorationPlayerPage.ExplorationPlayerPage();
+  });
   
-    it('displays creators subscribers', function() {
-      //created a creator ID and 2 learner ID.
-      var creator1Id = 'creatorName';
-      users.createUser('learner1@learnerDashboard.com', 'learner1learnerDashboard');
-      users.createUser('learner2@learnerDashboard.com', 'learner2learnerDashboard');
-      users.createUser(creator1Id + '@creatorDashboard.com', creator1Id);
+  it('displays creators subscribers', function() {
+    //created a creator ID and 2 learner ID.
+    var creator1Id = 'creatorName';
+    users.createUser('learner1@learnerDashboard.com', 'learner1learnerDashboard');
+    users.createUser('learner2@learnerDashboard.com', 'learner2learnerDashboard');
+    users.createUser(creator1Id + '@creatorDashboard.com', creator1Id);
 
       //created a new exploration for the creator.
-      users.login(creator1Id + '@creatorDashboard.com');
-      creatorDashboardPage.clickCreateExplorationButton();    
-      workflow.createAndPublishExploration(
-        'Activations',
-        'Chemistry',
-        'Learn about different types of chemistry activations.',
-        'English'
-      );
-      users.logout();
+    users.login(creator1Id + '@creatorDashboard.com');
+    creatorDashboardPage.clickCreateExplorationButton();    
+    workflow.createAndPublishExploration(
+      'Activations',
+      'Chemistry',
+      'Learn about different types of chemistry activations.',
+      'English'
+    );
+    users.logout();
      
-      //both the learners will subscribe to the creator.
-      users.login('learner1@learnerDashboard.com');
-      subscriptionDashboardPage.navigateToUserSubscriptionPage(creator1Id);
-      subscriptionDashboardPage.navigateToSubscriptionButton();
-      users.logout();
+    //both the learners will subscribe to the creator.
+    users.login('learner1@learnerDashboard.com');
+    subscriptionDashboardPage.navigateToUserSubscriptionPage(creator1Id);
+    subscriptionDashboardPage.navigateToSubscriptionButton();
+    users.logout();
 
-      users.login('learner2@learnerDashboard.com');
-      subscriptionDashboardPage.navigateToUserSubscriptionPage(creator1Id);
-      subscriptionDashboardPage.navigateToSubscriptionButton();
-      users.logout();
+    users.login('learner2@learnerDashboard.com');
+    subscriptionDashboardPage.navigateToUserSubscriptionPage(creator1Id);
+    subscriptionDashboardPage.navigateToSubscriptionButton();
+    users.logout();
      
       //exploration created by the creator.
-      users.login(creator1Id + '@creatorDashboard.com');
-      workflow.createAndPublishExploration(
-        'Activations',
-        'Chemistry',
-        'Learn about different types of chemistry activations.',
-        'English'
-      );
+    users.login(creator1Id + '@creatorDashboard.com');
+    workflow.createAndPublishExploration(
+      'Activations',
+      'Chemistry',
+      'Learn about different types of chemistry activations.',
+      'English'
+    );
     
       //navigating to creator's subscribed dashboard.
-      creatorDashboardPage.navigateToSubscriptionDashboard();
-      users.logout();
-    });
+    creatorDashboardPage.navigateToSubscriptionDashboard();
+    users.logout();
+  });
 
-    it('get all feedback messages count & edit exploration', function() {
-      var creator1Id = 'creatorName';
-      users.createUser(creator1Id + '@learnerDashboard.com', creator1Id);
-      users.createUser('learner1@learnerDashboard.com', 'learner1learnerDashboard');
-      users.login(creator1Id + '@creatorDashboard.com');
-      creatorDashboardPage.clickCreateExplorationButton();    
-      workflow.createAndPublishExploration(
-        'Activations',
-        'Chemistry',
-        'Learn about different types of chemistry activations.',
-        'English'
-      );
-      users.logout();
+  it('get all feedback messages count & edit exploration', function() {
+    var creator1Id = 'creatorName';
+    users.createUser(creator1Id + '@learnerDashboard.com', creator1Id);
+    users.createUser('learner1@learnerDashboard.com', 'learner1learnerDashboard');
+    users.login(creator1Id + '@creatorDashboard.com');
+    creatorDashboardPage.clickCreateExplorationButton();    
+    workflow.createAndPublishExploration(
+      'Activations',
+      'Chemistry',
+      'Learn about different types of chemistry activations.',
+      'English'
+    );
+    users.logout();
 
       //learner gives a feedback to creator exploration.
-      users.login('learner2@learnerDashboard.com');
-      var feedback = 'A good exploration. Would love to see a few more questions';
-      libraryPage.get();
-      libraryPage.findExploration('Activations');
-      explorationPlayerPage.submitFeedback(feedback);
-      users.logout();
+    users.login('learner2@learnerDashboard.com');
+    var feedback = 'A good exploration. Would love to see a few more questions';
+    libraryPage.get();
+    libraryPage.findExploration('Activations');
+    explorationPlayerPage.submitFeedback(feedback);
+    users.logout();
 
-      //creator checks all his feedback messages.
-      users.login(creator1Id + '@creatorDashboard.com');
-      creatorDashboardPage.getNumberOfFeedbackMessages();
-      creatorDashboardPage.editExploration('Activations');
-      users.logout();
-    });
+    //creator checks all his feedback messages.
+    users.login(creator1Id + '@creatorDashboard.com');
+    creatorDashboardPage.getNumberOfFeedbackMessages();
+    creatorDashboardPage.editExploration('Activations');
+    users.logout();
+  });
       
-    afterEach(function() {
-        general.checkForConsoleErrors([]);
-    });
+  afterEach(function() {
+    general.checkForConsoleErrors([]);
+  });
 });
