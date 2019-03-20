75e189c163bc1b0092b46456f5ee692fe603a1b8
sophiewu6 <wushiqi1998@gmail.com>
2019-03-20 00:01:08 -0700

    Minor change as directed

--- core/templates/dev/head/pages/state_editor/StateInteractionEditorDirective.js
+++ core/templates/dev/head/pages/state_editor/StateInteractionEditorDirective.js
@@ -219,12 +219,12 @@ oppia.directive('stateInteractionEditor', [
                     // Checks that the modal is invoked by exploration_editor or
                     // skill_editor, topic_editor. 
                     var currentUrl = $window.location.pathname.split('/')[1];
-                    if (currentUrl.match('create') || 
-                        currentUrl.match('creator_dashboard')) {
+                    if (currentUrl === 'create' || 
+                        currentUrl === 'creator_dashboard') {
                       $scope.ALLOWED_INTERACTION_CATEGORIES = (
                         ALLOWED_INTERACTION_CATEGORIES);
-                    } else if (currentUrl.match('skill_editor') ||
-                               currentUrl.match('topic_editor')) {
+                    } else if (currentUrl === 'skill_editor' ||
+                               currentUrl === 'topic_editor') {
                       $scope.ALLOWED_INTERACTION_CATEGORIES = (
                         ALLOWED_QUESTION_INTERACTION_CATEGORIES);
                     } else {
