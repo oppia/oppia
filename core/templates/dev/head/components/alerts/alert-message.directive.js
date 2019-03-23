91fc23d59445050cc22aabfc138b4e7c5f0ae2b6
Yash Jipkate <yashjipkate@gmail.com>
2019-03-23 17:25:50 +0530

    Moularize alert message component

--- core/templates/dev/head/components/alerts/alert-message.directive.js
+++ core/templates/dev/head/components/alerts/alert-message.directive.js
@@ -1,4 +1,4 @@
-// Copyright 2016 The Oppia Authors. All Rights Reserved.
+// Copyright 2019 The Oppia Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,10 +13,10 @@
 // limitations under the License.
 
 /**
- * @fileoverview Directive for Alert Messages
+ * @fileoverview Directive for the alert message component module.
  */
 
-oppia.directive('alertMessage', [function() {
+angular.module('alertMessageModule').directive('alertMessage', [function() {
   return {
     restrict: 'E',
     scope: {
@@ -51,3 +51,4 @@ oppia.directive('alertMessage', [function() {
     }
   };
 }]);
+    
