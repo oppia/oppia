f139177975c64026a521077867f07d43a4c2625a
Yash Jipkate <yashjipkate@gmail.com>
2019-03-26 11:49:33 +0530

    Added one missed file overview

--- core/templates/dev/head/pages/library-page/activity-tiles-infinity-grid/activity-tiles-infinity-grid.module.js
+++ core/templates/dev/head/pages/library-page/activity-tiles-infinity-grid/activity-tiles-infinity-grid.module.js
@@ -12,4 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/**
+ * @fileoverview Directive for an infinitely-scrollable view of activity tiles.
+ */
+
 angular.module('activityTilesInfinityGridModule', []);
