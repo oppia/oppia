dcfb1ae62136fef3e30e677c68130e5c5f2845c3
Ankita Saxena <ankitasonu24@gmail.com>
2019-03-30 04:35:13 +0530

    Add ajv-keywords to avoid conflict

--- scripts/install_third_party.sh
+++ scripts/install_third_party.sh
@@ -23,6 +23,7 @@ $PYTHON_CMD scripts/install_third_party.py
 
 # Install third-party node modules needed for the build process.
 install_node_module ajv 6.10.0
+install_node_module ajv-keywords 3.4.0
 install_node_module browserstack-local 1.3.3
 install_node_module dotenv 6.0.0
 install_node_module eslint 5.0.0
