19e2b3d79451dbc1bee949ae29b149227019d110
apoorv1997 <apoorv.b15@iiits.in>
2019-04-04 09:49:12 +0530

    added profile features functionalities

--- core/tests/protractor_desktop/profileFeatures.js
+++ core/tests/protractor_desktop/profileFeatures.js
@@ -28,95 +28,92 @@ var SubscriptionDashboardPage =
   require('../protractor_utils/SubscriptionDashboardPage.js');
 
 describe('Profile Features functionality', function() {
-    var libraryPage = null;
-    var learnerDashboardPage = null;
-    var subscriptionDashboardPage = null;
+  var libraryPage = null;
+  var learnerDashboardPage = null;
+  var subscriptionDashboardPage = null;
 
-    var creator1Id = 'FirstCreator';
-    var creator2Id = 'SecondCreator';
-    users.createUser('FirstUser@profileFeatures.com',
-      'FirstUserprofileFeatures');
-    users.createUser('SecondUser@profileFeatures.com',
-      'SecondUserprofileFeatures');
-    users.createUser(creator1Id + '@profileFeatures.com',
-      creator1Id);
-    users.createUser(creator2Id + '@profileFeatures.com',
-      creator2Id);
-  
-    beforeAll(function() {
-      libraryPage = new LibraryPage.LibraryPage();
-      learnerDashboardPage = new LearnerDashboardPage.LearnerDashboardPage();
-      subscriptionDashboardPage =
-        new SubscriptionDashboardPage.SubscriptionDashboardPage();
-    });
+  var creator1Id = 'FirstCreator';
+  var creator2Id = 'SecondCreator';
+  users.createUser('FirstUser@profileFeatures.com',
+    'FirstUserprofileFeatures');
+  users.createUser('SecondUser@profileFeatures.com',
+    'SecondUserprofileFeatures');
+  users.createUser(creator1Id + '@profileFeatures.com',
+    creator1Id);
+  users.createUser(creator2Id + '@profileFeatures.com',
+    creator2Id);
 
-    it('View a particular exploration created by another User', function() {
-      users.login(creator1Id + '@profileFeatures.com');
-      workflow.createAndPublishExploration(
-        'Activation',
-        'Chemistry',
-        'English'
-      );
-      users.logout();
+  beforeAll(function() {
+    libraryPage = new LibraryPage.LibraryPage();
+    learnerDashboardPage = new LearnerDashboardPage.LearnerDashboardPage();
+    subscriptionDashboardPage =
+      new SubscriptionDashboardPage.SubscriptionDashboardPage();
+  });
 
-      users.login('FirstUser@profileFeatures.com');
-      // Subscribing to a User to get all his created explorations.
-      subscriptionDashboardPage.navigateToUserSubscriptionPage(creator1Id);
-      subscriptionDashboardPage.navigateToSubscriptionButton();
+  it('View a particular exploration created by another User', function() {
+    users.login(creator1Id + '@profileFeatures.com');
+    workflow.createAndPublishExploration(
+      'Activation',
+      'Chemistry',
+      'English'
+    );
+    users.logout();
 
-      // finding a particular exploration on user's library and playing it.
-      libraryPage.get();
-      libraryPage.findExploration('Activations');
-      libraryPage.playExploration('Activations');
-      users.logout();
-    });
+    users.login('FirstUser@profileFeatures.com');
+    // Subscribing to a User to get all his created explorations.
+    subscriptionDashboardPage.navigateToUserSubscriptionPage(creator1Id);
+    subscriptionDashboardPage.navigateToSubscriptionButton();
 
-    it('Subscribe to a particular User', function() {
-      users.login('FirstUser@profileFeatures.com');
-      // Subscribe to both the creators.
-      subscriptionDashboardPage.navigateToUserSubscriptionPage(creator1Id);
-      subscriptionDashboardPage.navigateToSubscriptionButton();
-      subscriptionDashboardPage.navigateToUserSubscriptionPage(creator2Id);
-      subscriptionDashboardPage.navigateToSubscriptionButton();
+    // finding a particular exploration on user's library and playing it.
+    libraryPage.get();
+    libraryPage.findExploration('Activations');
+    libraryPage.playExploration('Activations');
+    users.logout();
+  });
 
-      // Checking all the users subscribed present in user's dashboard.
-      learnerDashboardPage.get();
-      learnerDashboardPage.navigateToSubscriptionsSection();
-      users.logout();
+  it('Subscribe to a particular User', function() {
+    users.login('FirstUser@profileFeatures.com');
+    
+    // Subscribe to both the creators.
+    subscriptionDashboardPage.navigateToUserSubscriptionPage(creator1Id);
+    subscriptionDashboardPage.navigateToSubscriptionButton();
+    subscriptionDashboardPage.navigateToUserSubscriptionPage(creator2Id);
+    subscriptionDashboardPage.navigateToSubscriptionButton();
 
-      users.login('SecondUser@profileFeatures.com');
-      // Subscribe to both the creators.
-      subscriptionDashboardPage.navigateToUserSubscriptionPage(creator1Id);
-      subscriptionDashboardPage.navigateToSubscriptionButton();
-      subscriptionDashboardPage.navigateToUserSubscriptionPage(creator2Id);
-      subscriptionDashboardPage.navigateToSubscriptionButton();
+    // Checking all the users subscribed present in user's dashboard.
+    // Both creators should be present in the subscriptions section of the
+    // dashboard.
+    learnerDashboardPage.get();
+    learnerDashboardPage.navigateToSubscriptionsSection();
+    // The last user (SecondCreator) that learner subsribes to is placed first
+    // in the list.
+    learnerDashboardPage.expectSubscriptionFirstNameToMatch('Second...');
+    // The first user (FirstCreator) that learner subscribes to is placed
+    // last in the list.
+    learnerDashboardPage.expectSubscriptionLastNameToMatch('First...');
+    users.logout();
+  });
 
-      // Checking all the users subscribed present in user's dashboard.
-      learnerDashboardPage.get();
-      learnerDashboardPage.navigateToSubscriptionsSection();
-      users.logout();
-    });
+  it('View number of explorations created by a User', function() {
+    users.login('FirstUser@profileFeatures.com');
 
-    it('View number of explorations created by a User', function() {
-      users.login('FirstUser@profileFeatures.com');
+    // Creating some explorations.
+    workflow.createAndPublishExploration(
+      'Activation',
+      'Chemistry',
+      'Introduction',
+      'English',
+      'BUS101',
+      'Business'
+    );
 
-      // Creating some explorations.
-      workflow.createAndPublishExploration(
-        'Activation',
-        'Chemistry',
-        'Introduction',
-        'English',
-        'BUS101',
-        'Business'
-      );
+    // Getting Number of explorations created by a User.
+    LibraryPage.get();
+    LibraryPage.getAllExplorationElements();
+    users.logout();
+  });
 
-      // Getting Number of explorations created by a User.
-      LibraryPage.get();
-      LibraryPage.getAllExplorationElements();
-      users.logout();
-    });
-
-    afterEach(function() {
-      general.checkForConsoleErrors([]);
-    });
+  afterEach(function() {
+    general.checkForConsoleErrors([]);
+  });
 });
