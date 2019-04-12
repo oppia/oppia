1e72de750e2376f99878ce130d57b6ad3c6d8cbe
Shuta Suzuki <shutas@umich.edu>
2019-04-12 16:19:12 -0400

    FIX: Import order

--- core/storage/question/gae_models_test.py
+++ core/storage/question/gae_models_test.py
@@ -15,11 +15,11 @@
 """Tests for core.storage.question.gae_models."""
 
 import datetime
+import types
 
 from core.domain import state_domain
 from core.platform import models
 from core.tests import test_utils
-import types
 
 (question_models,) = models.Registry.import_models([models.NAMES.question])
 
