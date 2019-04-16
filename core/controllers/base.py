f36c96a7b44851947aa14afd969cf444ed335cf1
Vrajesh Mehta <vmehta342@gmail.com>
2019-04-16 18:08:39 -0400

    removing user_is_logged_in from base.py

--- core/controllers/base.py
+++ core/controllers/base.py
@@ -326,8 +326,6 @@ class BaseHandler(webapp2.RequestHandler):
                 app_identity_services.get_gcs_resource_bucket_name()),
             # The 'path' variable starts with a forward slash.
             'FULL_URL': '%s://%s%s' % (scheme, netloc, path),
-            'user_is_logged_in': user_services.has_fully_registered(
-                self.user_id)
         })
 
         if 'status_code' not in values:
