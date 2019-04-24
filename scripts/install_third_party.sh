03ed0e994fb7ce5ca4864abc1cbabcb4561ef30a
Ankita Saxena <ankitasonu24@gmail.com>
2019-04-24 21:36:26 +0530

    Try fixing dependency issue

--- scripts/install_third_party.sh
+++ scripts/install_third_party.sh
@@ -22,11 +22,10 @@ echo Installing third-party JS libraries and zip files.
 $PYTHON_CMD scripts/install_third_party.py
 
 # Install third-party node modules needed for the build process.
-install_node_module ajv-keywords 3.4.0
+install_node_module eslint 5.0.0
 install_node_module ajv 6.10.0
 install_node_module browserstack-local 1.3.3
 install_node_module dotenv 6.0.0
-install_node_module eslint 5.0.0
 install_node_module eslint-plugin-angular 0.12.0
 install_node_module eslint-plugin-html 4.0.1
 install_node_module gulp 3.9.0
