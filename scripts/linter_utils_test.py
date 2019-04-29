ede8a56a522c2b04a927d314f92890bd21ea6d2a
Brian Rodriguez <brian@brianrodri.com>
2019-04-29 17:04:30 -0400

    Introduce `linter_utils` module (#6650)
    * introduce utils.memoize
    
    * add missing docstrings
    
    * improve docstrings
    
    * add test for kwargs
    
    * stronger key
    
    * explicit kwarg test
    
    * explicit kwargs test
    
    * remove debug prints
    
    * add test for demonstrating differences when using kwargs with default values
    
    * fix comment
    
    * add extra checks
    
    * better test names
    
    * move comment
    
    * support kwargs with default values
    
    * add spacing
    
    * better function name
    
    * better var name
    
    * consistent test names
    
    * improve docstrings
    
    * add tests for classmethod decorator
    
    * fix indent-error
    
    * add missing docstrings
    
    * improve comments
    
    * disable unused-variable
    
    * use double-checked locking
    
    * pluralization
    
    * Update utils_test.py
    
    remove unreachable code
    
    * introduce linter_utils
    
    * introduce linter_utils
    
    * revert utils imports
    
    * more documentation
    
    * test mixed args and kwargs
    
    * add scope of functions
    
    * spacing
    
    * better variable names
    
    * remove newline
    
    * fix docstrings
    
    * fix import order
    
    * Fix copyright date

---
+++ scripts/linter_utils_test.py
@@ -0,0 +1,169 @@
+# coding: utf-8
+#
+# Copyright 2019 The Oppia Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Unit tests for linter_utils.py."""
+
+import collections
+
+# pylint: disable=relative-import
+from core.tests import test_utils
+import linter_utils
+# pylint: enable=relative-import
+
+
+class MemoizeTest(test_utils.GenericTestBase):
+    """Tests for the memoize function."""
+
+    def test_memoize_with_args(self):
+        call_counter = collections.Counter()
+
+        @linter_utils.memoize
+        def count_calls(arg):
+            """Counts calls made with given arg."""
+            call_counter[arg] += 1
+
+        unique_objs = (object(), object())
+        self.assertEqual(call_counter[unique_objs[0]], 0)
+        self.assertEqual(call_counter[unique_objs[1]], 0)
+
+        count_calls(unique_objs[0])
+        self.assertEqual(call_counter[unique_objs[0]], 1)
+        self.assertEqual(call_counter[unique_objs[1]], 0)
+
+        count_calls(unique_objs[0])
+        count_calls(unique_objs[1])
+        self.assertEqual(call_counter[unique_objs[0]], 1)
+        self.assertEqual(call_counter[unique_objs[1]], 1)
+
+    def test_memoize_with_kwargs(self):
+        call_counter = collections.Counter()
+
+        @linter_utils.memoize
+        def count_calls(**kwargs):
+            """Counts calls made with given kwargs."""
+            hashable_kwargs = tuple(sorted(kwargs.iteritems()))
+            call_counter[hashable_kwargs] += 1
+
+        empty_kwargs = ()
+        nonempty_kwargs = (('kwarg', 0),)
+        self.assertEqual(call_counter[empty_kwargs], 0)
+        self.assertEqual(call_counter[nonempty_kwargs], 0)
+
+        count_calls()
+        self.assertEqual(call_counter[empty_kwargs], 1)
+        self.assertEqual(call_counter[nonempty_kwargs], 0)
+
+        count_calls()
+        count_calls(kwarg=0)
+        self.assertEqual(call_counter[empty_kwargs], 1)
+        self.assertEqual(call_counter[nonempty_kwargs], 1)
+
+    def test_memoize_with_kwargs_using_default_values(self):
+        call_counter = collections.Counter()
+
+        @linter_utils.memoize
+        def count_calls(kwarg=0):
+            """Counts calls made with given kwargs."""
+            call_counter[kwarg] += 1
+
+        self.assertEqual(call_counter[0], 0)
+        count_calls()
+        self.assertEqual(call_counter[0], 1)
+        count_calls(kwarg=0)
+        self.assertEqual(call_counter[0], 1)
+
+    def test_memoize_with_methods(self):
+        class CallCounter(object):
+            """Counts calls made to an instance."""
+            def __init__(self):
+                self.count = 0
+
+            @linter_utils.memoize
+            def __call__(self):
+                self.count += 1
+
+        call_counter_a, call_counter_b = CallCounter(), CallCounter()
+        self.assertEqual(call_counter_a.count, 0)
+        self.assertEqual(call_counter_b.count, 0)
+
+        call_counter_a()
+        self.assertEqual(call_counter_a.count, 1)
+        self.assertEqual(call_counter_b.count, 0)
+
+        call_counter_a()
+        call_counter_b()
+        self.assertEqual(call_counter_a.count, 1)
+        self.assertEqual(call_counter_b.count, 1)
+
+    def test_memoize_with_classmethods(self):
+        class GoodCallCounter(object):
+            """Counts calls made to the class."""
+            count = 0
+
+            @classmethod
+            @linter_utils.memoize
+            def method_decorated_by_memoize_before_classmethod(cls):
+                """memoize is called first so this def will work properly."""
+                cls.count += 1
+
+        call_counter_a, call_counter_b = GoodCallCounter(), GoodCallCounter()
+        self.assertEqual(GoodCallCounter.count, 0)
+
+        call_counter_a.method_decorated_by_memoize_before_classmethod()
+        self.assertEqual(GoodCallCounter.count, 1)
+
+        call_counter_a.method_decorated_by_memoize_before_classmethod()
+        call_counter_b.method_decorated_by_memoize_before_classmethod()
+        self.assertEqual(GoodCallCounter.count, 1)
+
+        with self.assertRaisesRegexp(TypeError, 'not a Python function'):
+            class BadCallCounter(object):  # pylint: disable=unused-variable
+                """Counts calls made to the class."""
+                count = 0
+
+                @linter_utils.memoize
+                @classmethod
+                def method_decorated_by_classmethod_before_memoize(cls):
+                    """classmethods are not real functions so trying to memoize
+                    them will raise a TypeError.
+                    """
+
+    def test_memoize_with_argument_values_in_different_orders(self):
+        call_counter = collections.Counter()
+
+        @linter_utils.memoize
+        def count_calls(a, b, c=0, d=1):
+            """Counts calls made with the given arguments."""
+            key = (a, b, c, d)
+            call_counter[key] += 1
+
+        self.assertEqual(call_counter[(5, 6, 0, 1)], 0)
+        self.assertEqual(call_counter[(6, 5, 0, 1)], 0)
+        count_calls(5, 6)
+        count_calls(6, 5)
+        self.assertEqual(call_counter[(5, 6, 0, 1)], 1)
+        self.assertEqual(call_counter[(6, 5, 0, 1)], 1)
+        count_calls(5, 6, c=0, d=1)
+        count_calls(6, 5, c=0, d=1)
+        self.assertEqual(call_counter[(5, 6, 0, 1)], 1)
+        self.assertEqual(call_counter[(6, 5, 0, 1)], 1)
+
+        self.assertEqual(call_counter[(5, 6, 2, 3)], 0)
+        count_calls(5, 6, c=2, d=3)
+        self.assertEqual(call_counter[(5, 6, 2, 3)], 1)
+        count_calls(5, 6, d=3, c=2)
+        self.assertEqual(call_counter[(5, 6, 3, 2)], 0)
+        self.assertEqual(call_counter[(5, 6, 2, 3)], 1)
