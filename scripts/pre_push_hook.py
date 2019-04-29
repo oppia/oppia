2ce997c98198c4c387bbc2068bef845ca99342ee
Sandeep Dubey <dubeysandeep.in@gmail.com>
2019-04-30 00:23:47 +0530

    Fix pre_push_hook issue with fronted test.

--- scripts/pre_push_hook.py
+++ scripts/pre_push_hook.py
@@ -295,7 +295,7 @@ def _install_hook():
         print 'Copied file to .git/hooks directory'
 
 
-def does_diff_include_js_files(files_to_lint):
+def does_diff_include_ts_files(files_to_lint):
     """Returns true if diff includes JS files.
 
     Args:
@@ -307,7 +307,7 @@ def does_diff_include_js_files(files_to_lint):
 
     js_files_to_check = [
         filename for filename in files_to_lint if
-        filename.endswith('.js')]
+        filename.endswith('.ts')]
 
     return bool(js_files_to_check)
 
@@ -344,7 +344,7 @@ def main():
                     print 'Push failed, please correct the linting issues above'
                     sys.exit(1)
             frontend_status = 0
-            if does_diff_include_js_files(files_to_lint):
+            if does_diff_include_ts_files(files_to_lint):
                 frontend_status = _start_sh_script(FRONTEND_TEST_SCRIPT)
             if frontend_status != 0:
                 print 'Push aborted due to failing frontend tests.'
