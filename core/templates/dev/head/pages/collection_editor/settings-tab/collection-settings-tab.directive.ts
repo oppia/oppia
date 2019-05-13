9245bfa64464adf62e6f0a98e9c0b2554aab98be
amey-kudari <work.ameykudari@gmail.com>
2019-05-13 19:43:52 +0530

    rename all folders within collection_editor

--- core/templates/dev/head/pages/collection_editor/settings-tab/collection-settings-tab.directive.ts
+++ core/templates/dev/head/pages/collection_editor/settings-tab/collection-settings-tab.directive.ts
@@ -21,7 +21,7 @@ angular.module('collectionSettingsTabModule').directive('collectionSettingsTab',
     return {
       restrict: 'E',
       templateUrl: UrlInterpolationService.getDirectiveTemplateUrl(
-        '/pages/collection_editor/settings_tab/' +
+        '/pages/collection_editor/settings-tab/' +
         'collection-settings-tab.directive.html'),
       controller: [function() {}]
     };
