227e390c8b28656aef74dd1e8347702b0a92a3f4
Ankita Saxena <ankitasonu24@gmail.com>
2019-05-14 00:24:54 +0530

    Background banner

--- core/templates/dev/head/components/background/BackgroundBannerDirective.ts
+++ core/templates/dev/head/components/background/BackgroundBannerDirective.ts
@@ -21,16 +21,18 @@ oppia.directive('backgroundBanner', [
     return {
       restrict: 'E',
       scope: {},
+      bindToController: true,
       templateUrl: UrlInterpolationService.getDirectiveTemplateUrl(
         '/components/background/background_banner_directive.html'),
+      controllerAs: 'backgroundBannerCtrl',
       controller: [
-        '$scope',
-        function($scope) {
+        function() {
+          var ctrl = this;
           var possibleBannerFilenames = [
             'bannerA.svg', 'bannerB.svg', 'bannerC.svg', 'bannerD.svg'];
           var bannerImageFilename = possibleBannerFilenames[
             Math.floor(Math.random() * possibleBannerFilenames.length)];
-          $scope.bannerImageFileUrl = UrlInterpolationService.getStaticImageUrl(
+          ctrl.bannerImageFileUrl = UrlInterpolationService.getStaticImageUrl(
             '/background/' + bannerImageFilename);
         }
       ]
