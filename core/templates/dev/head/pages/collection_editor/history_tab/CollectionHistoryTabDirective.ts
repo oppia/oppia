e32808d44811d2c34b0d8633dc9e88de2f77f45c
Ankita Saxena <ankitasonu24@gmail.com>
2019-05-15 03:15:17 +0530

    Collection history tab

--- core/templates/dev/head/pages/collection_editor/history_tab/CollectionHistoryTabDirective.ts
+++ core/templates/dev/head/pages/collection_editor/history_tab/CollectionHistoryTabDirective.ts
@@ -22,9 +22,12 @@ oppia.directive('collectionHistoryTab', [
   'UrlInterpolationService', function(UrlInterpolationService) {
     return {
       restrict: 'E',
+      scope: {},
+      bindToController: true,
       templateUrl: UrlInterpolationService.getDirectiveTemplateUrl(
         '/pages/collection_editor/history_tab/' +
         'collection_history_tab_directive.html'),
+      controllerAs: 'collectionHistoryTabCtrl',
       controller: [function() {}]
     };
   }]);
