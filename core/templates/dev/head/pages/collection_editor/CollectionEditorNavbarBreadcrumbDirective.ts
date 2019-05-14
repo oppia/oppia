2e9e6bfab2636ccb5dbab7002339f837e5bed71c
Ankita Saxena <ankitasonu24@gmail.com>
2019-05-15 03:24:33 +0530

    Collection editor navbar breadcrumb

--- core/templates/dev/head/pages/collection_editor/CollectionEditorNavbarBreadcrumbDirective.ts
+++ core/templates/dev/head/pages/collection_editor/CollectionEditorNavbarBreadcrumbDirective.ts
@@ -27,15 +27,18 @@ oppia.directive('collectionEditorNavbarBreadcrumb', [
     return {
       restrict: 'E',
       scope: {},
+      bindToController: true,
       templateUrl: UrlInterpolationService.getDirectiveTemplateUrl(
         '/pages/collection_editor/' +
         'collection_editor_navbar_breadcrumb_directive.html'),
+      controllerAs: 'collectionEditorNavbarBreadcrumbCtrl',
       controller: [
-        '$scope', 'RouterService', 'CollectionEditorStateService',
+        'RouterService', 'CollectionEditorStateService',
         'FocusManagerService', 'COLLECTION_TITLE_INPUT_FOCUS_LABEL',
         function(
-            $scope, RouterService, CollectionEditorStateService,
+            RouterService, CollectionEditorStateService,
             FocusManagerService, COLLECTION_TITLE_INPUT_FOCUS_LABEL) {
+          var ctrl = this;
           var _TAB_NAMES_TO_HUMAN_READABLE_NAMES = {
             main: 'Edit',
             preview: 'Preview',
@@ -45,14 +48,14 @@ oppia.directive('collectionEditorNavbarBreadcrumb', [
             history: 'History',
           };
 
-          $scope.collection = CollectionEditorStateService.getCollection();
+          ctrl.collection = CollectionEditorStateService.getCollection();
 
-          $scope.getCurrentTabName = function() {
+          ctrl.getCurrentTabName = function() {
             return _TAB_NAMES_TO_HUMAN_READABLE_NAMES[
               RouterService.getActiveTabName()];
           };
 
-          $scope.editCollectionTitle = function() {
+          ctrl.editCollectionTitle = function() {
             RouterService.navigateToSettingsTab();
             FocusManagerService.setFocus(COLLECTION_TITLE_INPUT_FOCUS_LABEL);
           };
