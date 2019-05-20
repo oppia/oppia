96e89160057936345f5215037b7465fa0dcb9186
Ankita Saxena <ankitasonu24@gmail.com>
2019-05-21 03:11:59 +0530

    Convert directives in extensions to component directives (#6752)
    * Code repl
    
    * Continue
    
    * Drag and drop
    
    * End exploration
    
    * Fraction
    
    * Graph Input
    
    * Image click input
    
    * Interactive map
    
    * Item selection
    
    * Logic proof
    
    * Math
    
    * Multiple choice
    
    * Music notes
    
    * Number with units
    
    * Numeric input
    
    * Pencil code editor
    
    * Set Input
    
    * Text Input
    
    * Boolean editor
    
    * Code string editor
    
    * Coordtwodim editor
    
    * Drag and drop html string editor
    
    * Drag and drop int editor
    
    * Filepath editor
    
    * Fraction editor
    
    * Graph editor
    
    * Graph property editor
    
    * Html editor
    
    * Image with region editor
    
    * Int editor
    
    * List of set of html strings editor
    
    * List of tabs editor
    
    * List of unicode editor
    
    * Logic error category editor
    
    * Logic question editor
    
    * Math latex editor
    
    * Music phrase editor
    
    * Non negative int editor
    
    * Normalized string editor
    
    * Number with units editor
    
    * Parameter name editor
    
    * Sanitized url and real editor
    
    * Set of html string editor
    
    * Set of unicode string editor
    
    * Unicode string editor
    
    * Collapsible
    
    * Image
    
    * Link
    
    * Math
    
    * Tabs
    
    * Video
    
    * Visualizations
    
    * Fix test
    
    * Fix comment
    
    * Fix fraction input test
    
    * Fix logic question test
    
    * Fix graph input
    
    * Fix multiple choice input test
    
    * Fix number with units
    
    * Fix number with units editor

--- extensions/interactions/MultipleChoiceInput/directives/OppiaInteractiveMultipleChoiceInputDirective.ts
+++ extensions/interactions/MultipleChoiceInput/directives/OppiaInteractiveMultipleChoiceInputDirective.ts
@@ -28,17 +28,20 @@ oppia.directive('oppiaInteractiveMultipleChoiceInput', [
     return {
       restrict: 'E',
       scope: {},
+      bindToController: {},
       templateUrl: UrlInterpolationService.getExtensionResourceUrl(
         '/interactions/MultipleChoiceInput/directives/' +
         'multiple_choice_input_interaction_directive.html'),
+      controllerAs: '$ctrl',
       controller: [
-        '$scope', '$attrs', 'CurrentInteractionService',
-        function($scope, $attrs, CurrentInteractionService) {
-          $scope.choices = HtmlEscaperService.escapedJsonToObj(
+        '$attrs', 'CurrentInteractionService',
+        function($attrs, CurrentInteractionService) {
+          var ctrl = this;
+          ctrl.choices = HtmlEscaperService.escapedJsonToObj(
             $attrs.choicesWithValue);
-          $scope.answer = null;
+          ctrl.answer = null;
 
-          $scope.submitAnswer = function(answer) {
+          ctrl.submitAnswer = function(answer) {
             if (answer === null) {
               return;
             }
