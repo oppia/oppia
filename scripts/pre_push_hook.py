52c314676da9fb06fc676dbc86246ce6b92692c8
Sean Lip <sll@google.com>
2019-05-20 17:16:56 -0700

    Fix lint error.

--- scripts/pre_push_hook.py
+++ scripts/pre_push_hook.py
@@ -227,7 +227,7 @@ def _collect_files_being_pushed(ref_list, remote):
     collected_files = {}
     # Git allows that multiple branches get pushed simultaneously with the "all"
     # flag. Therefore we need to loop over the ref_list provided.
-    for branch, sha1 in zip(branches, hashes):
+    for branch, _ in zip(branches, hashes):
         # Get the difference to remote/develop.
         modified_files = _compare_to_remote(
             remote, branch, remote_branch='develop')
