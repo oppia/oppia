858ce62929b718a875f6897c7b1141b02a41ccd2
mzaman07 <9009585+mzaman07@users.noreply.github.com>
2019-05-21 23:42:35 -0600

    removed references to angular-recorder/angular-audio-recorder

--- core/templates/dev/head/AppInit.ts
+++ core/templates/dev/head/AppInit.ts
@@ -14,7 +14,7 @@
 
 var oppia = angular.module(
   'oppia', [
-    'angularAudioRecorder', 'dndLists', 'headroom', 'infinite-scroll',
+    'dndLists', 'headroom', 'infinite-scroll',
     'ngAnimate', 'ngAudio', 'ngCookies', 'ngImgCrop', 'ngJoyRide', 'ngMaterial',
     'ngResource', 'ngSanitize', 'ngTouch', 'pascalprecht.translate', 'toastr',
     'ui.bootstrap', 'ui.sortable', 'ui.tree', 'ui.validate'
