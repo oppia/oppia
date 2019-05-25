784980f7fb25febba2519f8ef822a601540ca96e
YashJipkate <yashjipkate@gmail.com>
2019-05-25 16:37:11 +0530

    Fix e2e

--- core/templates/dev/head/services/StateRulesStatsService.ts
+++ core/templates/dev/head/services/StateRulesStatsService.ts
@@ -19,10 +19,12 @@
 require(
   'pages/exploration-editor-page/exploration-editor-page-services/' +
   'angular-name/angular-name.service.ts');
-require('pages/exploration-player-page/exploration-player-page-services/answer-classification/answer-classification.service.ts');
+require(
+  'pages/exploration-player-page/exploration-player-page-services/' +
+  'answer-classification/answer-classification.service.ts');
 require('services/ContextService.ts');
 require('domain/objects/FractionObjectFactory.ts');
-require('domain/utilities/UrlInterpolationService.ts')
+require('domain/utilities/UrlInterpolationService.ts');
 
 oppia.factory('StateRulesStatsService', [
   '$http', '$injector', 'AngularNameService', 'AnswerClassificationService',
