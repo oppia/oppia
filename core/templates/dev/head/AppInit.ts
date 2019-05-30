1b269cd6cce4b96dfffc0b3134991a5228db0afa
mzaman07 <9009585+mzaman07@users.noreply.github.com>
2019-05-29 21:35:17 -0600

    changed AppInit.ts for improved readability.

--- core/templates/dev/head/AppInit.ts
+++ core/templates/dev/head/AppInit.ts
@@ -18,10 +18,10 @@
 
 var oppia = angular.module(
   'oppia', [
-    'dndLists', 'headroom', 'infinite-scroll',
-    'ngAnimate', 'ngAudio', 'ngCookies', 'ngImgCrop', 'ngJoyRide', 'ngMaterial',
-    'ngResource', 'ngSanitize', 'ngTouch', 'pascalprecht.translate', 'toastr',
-    'ui.bootstrap', 'ui.sortable', 'ui.tree', 'ui.validate'
+    'dndLists', 'headroom', 'infinite-scroll', 'ngAnimate',
+    'ngAudio', 'ngCookies', 'ngImgCrop', 'ngJoyRide', 'ngMaterial',
+    'ngResource', 'ngSanitize', 'ngTouch', 'pascalprecht.translate',
+    'toastr', 'ui.bootstrap', 'ui.sortable', 'ui.tree', 'ui.validate'
   ].concat(
   window.GLOBALS ? (window.GLOBALS.ADDITIONAL_ANGULAR_MODULES || []) : []));
 
