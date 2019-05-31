b187f7302548af1b36fb3d0d84fd952a77557d89
Sean Lip <sll@google.com>
2019-05-31 15:37:07 -0700

    Remove width parameter.

--- core/controllers/editor.py
+++ core/controllers/editor.py
@@ -422,13 +422,12 @@ class ExplorationFileDownloader(EditorHandler):
             raise self.PageNotFoundException
 
         version_str = self.request.get('v', default_value=exploration.version)
+        output_format = self.request.get('output_format', default_value='zip')
+
         try:
             version = int(version_str)
         except ValueError:
             version = exploration.version
-
-        output_format = self.request.get('output_format', default_value='zip')
-        width = int(self.request.get('width', default_value=80))
 
         # If the title of the exploration has changed, we use the new title.
         filename = 'oppia-%s-v%s.zip' % (
@@ -441,7 +440,7 @@ class ExplorationFileDownloader(EditorHandler):
                 filename, 'text/plain')
         elif output_format == feconf.OUTPUT_FORMAT_JSON:
             self.render_json(exp_services.export_states_to_yaml(
-                exploration_id, version=version, width=width))
+                exploration_id, version=version, width=80))
         else:
             raise self.InvalidInputException(
                 'Unrecognized output format %s' % output_format)
