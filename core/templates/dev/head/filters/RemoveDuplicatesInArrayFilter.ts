5aa2377b4f32e28bd0f5bb47a0b335080db8d241
Nithesh <nithusha21@yahoo.co.in>
2019-06-02 00:04:09 +0530

    fix lint

--- core/templates/dev/head/filters/RemoveDuplicatesInArrayFilter.ts
+++ core/templates/dev/head/filters/RemoveDuplicatesInArrayFilter.ts
@@ -19,7 +19,7 @@
 oppia.filter('removeDuplicatesInArray', [function() {
   return function(input) {
     if (!input.filter) {
-      throw Error('Bad input: ' + JSON.stringify(input))
+      throw Error('Bad input: ' + JSON.stringify(input));
     }
     return input.filter(function(val, pos) {
       return input.indexOf(val) === pos;
