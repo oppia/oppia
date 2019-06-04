65c22bcd2a98a396d555b980a819a7f0f9f03956
Anubhav Sinha <anubhavsinha98@gmail.com>
2019-06-04 12:11:52 +0530

    Fix lint issues

--- core/storage/statistics/gae_models_test.py
+++ core/storage/statistics/gae_models_test.py
@@ -284,8 +284,9 @@ class LearnerAnswerDetailsModelUnitTests(test_utils.GenericTestBase):
         id_parameters = {'exp_id': 'abc', 'state_name': 'init_state'}
         entity_type = feconf.ENTITY_TYPE_TOPIC
         with self.assertRaisesRegexp(Exception, 'Invalid entity type'):
-            entity_id = stat_models.LearnerAnswerDetailsModel.get_entity_id(
+            stat_models.LearnerAnswerDetailsModel.get_entity_id(
                 id_parameters, entity_type)
+
     def test_create_and_get_answer_details_model(self):
         id_parameters = {'exp_id': 'abc', 'state_name': 'init_state'}
         entity_id = stat_models.LearnerAnswerDetailsModel.get_entity_id(
