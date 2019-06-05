a431ad82220d0b80c82681c693623c946a04e866
James John James <jamesjay4199@gmail.com>
2019-06-05 15:17:28 +0100

    adress reviews

--- webpack-utils.ts
+++ webpack-utils.ts
@@ -20,31 +20,6 @@
 var PageMeta = function(name, description) {
   this.name = name;
   this.description = description;
-  this.referrer = 'no-referrer';
-  this.viewport = 'width=device-width, initial-scale=1.0, user-scalable=yes';
-
-  this['application-name'] = '<[ siteName ]>';
-  this['msapplication-square310x310logo'] = (
-    '<[ getAssetUrl(\'/assets/images/logo/msapplication-large.png\') ]>');
-  this['msapplication-wide310x150logo'] = (
-    '<[ getAssetUrl(\'/assets/images/logo/msapplication-wide.png\') ]>');
-  this['msapplication-square150x150logo'] = (
-    '<[ getAssetUrl(\'/assets/images/logo/msapplication-square.png\') ]>');
-  this['msapplication-square70x70logo'] = (
-    '<[ getAssetUrl(\'/assets/images/logo/msapplication-tiny.png\') ]>');
-
-  this['og:type'] = {
-    property: 'og:type',
-    content: 'article'
-  };
-  this['og:site_name'] = {
-    property: 'og:site_name',
-    content: 'Oppia'
-  };
-  this['og:url'] = {
-    property: 'og:url',
-    content: '<[pageUrl]>'
-  };
   this['og:title'] = {
     property: 'og:title',
     content: name
@@ -53,14 +28,10 @@ var PageMeta = function(name, description) {
     property: 'og:description',
     content: description
   };
-  this['og:image'] = {
-    property: 'og:image',
-    content: '<[ getAssetUrl(\'/assets/images/logo/288x288_logo_mint.png\') ]>'
-  };
 };
 
 PageMeta.prototype.toHTML = function() {
-  var html = '<meta charset="UTF-8">\n';
+  var html = '';
   for (var meta in this) {
     if (this.hasOwnProperty(meta)) {
       if (typeof this[meta] === 'object') {
