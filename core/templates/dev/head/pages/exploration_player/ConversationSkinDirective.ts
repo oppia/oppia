b2956976b15b18c71e2f8b1fc2ea5091bbc7ab83
vinitamurthi <murthi.vinita@gmail.com>
2019-06-05 08:59:53 -0700

    Remove new line in ConversationSkinDirective

--- core/templates/dev/head/pages/exploration_player/ConversationSkinDirective.ts
+++ core/templates/dev/head/pages/exploration_player/ConversationSkinDirective.ts
@@ -377,7 +377,6 @@ oppia.directive('conversationSkin', [
           var tutorCardIsDisplayedIfNarrow = true;
           $scope.explorationId = ExplorationEngineService.getExplorationId();
           var questionPlayerConfig = $scope.getQuestionPlayerConfig();
-
           $scope.isInPreviewMode = ExplorationEngineService.isInPreviewMode();
           $scope.isIframed = UrlService.isIframed();
           $rootScope.loadingMessage = 'Loading';
