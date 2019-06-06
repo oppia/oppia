ccb3b5cad457a4b2bef01051dcab060a4a25c760
Ankita Saxena <ankitasonu24@gmail.com>
2019-06-07 01:01:18 +0530

    Add collection commit commands

--- core/domain/commit_commands_domain.py
+++ core/domain/commit_commands_domain.py
@@ -16,16 +16,21 @@
 
 """Domain object for commit commands."""
 
-
-import utils
+from core.domain import collection_domain
 from core.domain import exp_domain
+import utils
 
 
-def validate_dict_keys(actual_keys, required_keys, optional_keys=[]):
+def validate_dict_keys(actual_keys, required_keys, optional_keys=None):
     """Validates that actual keys in a dict contain all the required keys
-    and some/all of the optional keys."""
+    and some/all of the optional keys.
+    """
 
     missing_keys = [key for key in required_keys if key not in actual_keys]
+
+    if not optional_keys:
+        optional_keys = []
+
     extra_keys = [key for key in actual_keys if key not in (
         required_keys + optional_keys)]
 
@@ -70,7 +75,7 @@ class ExplorationCommitCmd(object):
             Exception: The command is invalid.
         """
         if self.name == exp_domain.CMD_ADD_STATE or (
-            self.name == exp_domain.CMD_DELETE_STATE):
+                self.name == exp_domain.CMD_DELETE_STATE):
             required_keys = ['state_name']
             validate_dict_keys(self.parameters.keys(), required_keys)
 
@@ -93,7 +98,8 @@ class ExplorationCommitCmd(object):
                 self.parameters.keys(), required_keys,
                 optional_keys=optional_keys)
 
-        elif self.name == exp_domain.CMD_MIGRATE_STATES_SCHEMA_TO_LATEST_VERSION:
+        elif self.name == (
+                exp_domain.CMD_MIGRATE_STATES_SCHEMA_TO_LATEST_VERSION):
             required_keys = ['from_version', 'to_version']
             validate_dict_keys(self.parameters.keys(), required_keys)
 
@@ -108,6 +114,98 @@ class ExplorationRightsCommitCmd(object):
 
     def __init__(self, name, parameters):
         """Constructs a ExplorationRightsCommitCmd domain object.
+
+        Args:
+            name: str. The command name.
+            parameters: dict. The command parameters.
+        """
+        self.name = name
+        self.parameters = parameters
+
+    def validate(self):
+        """Checks that all parameters of a command are valid for the
+        given command name.
+
+        Raises:
+            Exception: The command is invalid.
+        """
+        return
+
+
+class CollectionCommitCmd(object):
+    """Domain object for commit commands of a collection.
+
+    Attributes:
+        name: str. The command name.
+        parameters: dict. The command parameters.
+    """
+
+    def __init__(self, name, parameters):
+        """Constructs a CollectionCommitCmd domain object.
+
+        Args:
+            name: str. The command name.
+            parameters: dict. The command parameters.
+        """
+        self.name = name
+        self.parameters = parameters
+
+    def validate(self):
+        """Checks that all parameters of a command are valid for the
+        given command name.
+
+        Raises:
+            Exception: The command is invalid.
+        """
+        if self.name == collection_domain.CMD_ADD_COLLECTION_NODE or (
+                self.name == collection_domain.CMD_DELETE_COLLECTION_NODE):
+            required_keys = ['exploration_id']
+            validate_dict_keys(self.parameters.keys(), required_keys)
+
+        elif self.name == collection_domain.CMD_SWAP_COLLECTION_NODES:
+            required_keys = ['first_index', 'second_index']
+            validate_dict_keys(self.parameters.keys(), required_keys)
+
+        elif self.name == collection_domain.CMD_EDIT_COLLECTION_PROPERTY:
+            required_keys = [
+                'property_name', 'new_value']
+            optional_keys = ['old_value']
+            validate_dict_keys(
+                self.parameters.keys(), required_keys,
+                optional_keys=optional_keys)
+
+        elif self.name == (
+                collection_domain.CMD_MIGRATE_SCHEMA_TO_LATEST_VERSION):
+            required_keys = ['from_version', 'to_version']
+            validate_dict_keys(self.parameters.keys(), required_keys)
+
+        elif self.name == collection_domain.CMD_ADD_COLLECTION_SKILL:
+            required_keys = ['name']
+            validate_dict_keys(self.parameters.keys(), required_keys)
+
+        elif self.name == collection_domain.CMD_DELETE_COLLECTION_SKILL:
+            required_keys = ['skill_id']
+            validate_dict_keys(self.parameters.keys(), required_keys)
+
+        elif self.name == collection_domain.CMD_ADD_QUESTION_ID_TO_SKILL:
+            required_keys = ['question_id', 'skill_id']
+            validate_dict_keys(self.parameters.keys(), required_keys)
+
+        elif self.name == collection_domain.CMD_REMOVE_QUESTION_ID_FROM_SKILL:
+            required_keys = ['question_id', 'skill_id']
+            validate_dict_keys(self.parameters.keys(), required_keys)
+
+
+class CollectionRightsCommitCmd(object):
+    """Domain object for commit commands of a collection rights.
+
+    Attributes:
+        name: str. The command name.
+        parameters: dict. The command parameters.
+    """
+
+    def __init__(self, name, parameters):
+        """Constructs a CollectionRightsCommitCmd domain object.
 
         Args:
             name: str. The command name.
