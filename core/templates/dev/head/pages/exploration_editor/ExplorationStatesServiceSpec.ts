2919b9d45eda8885497ad6d339950d62974310df
Brian Rodriguez <brian@brianrodri.com>
2019-06-06 23:27:17 -0400

    condense overview

--- core/templates/dev/head/pages/exploration_editor/ExplorationStatesServiceSpec.ts
+++ core/templates/dev/head/pages/exploration_editor/ExplorationStatesServiceSpec.ts
@@ -12,9 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * @fileoverview Tests for ExplorationStatesService.
- */
+/** @fileoverview Tests for ExplorationStatesService. */
 
 require('pages/exploration_editor/ExplorationStatesService.ts');
 
