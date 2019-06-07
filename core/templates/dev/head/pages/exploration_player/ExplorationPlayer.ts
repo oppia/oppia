bea7807b65f99d69ed551fa3436145cc424a423a
Yash Jipkate <34203227+YashJipkate@users.noreply.github.com>
2019-06-07 12:18:07 +0530

    Convert Learner Local Nav and Leaner Local Info in ExplorationPlayer to component directives. (#6856)
    * Learner Local Nav
    
    * Learner View Info
    
    * Learner View Info
    
    * Controller -> Directive in fileoverview

--- core/templates/dev/head/pages/exploration_player/ExplorationPlayer.ts
+++ core/templates/dev/head/pages/exploration_player/ExplorationPlayer.ts
@@ -60,8 +60,8 @@ require('components/attribution_guide/AttributionGuideDirective.ts');
 require('components/background/BackgroundBannerDirective.ts');
 require('pages/exploration_player/ConversationSkinDirective.ts');
 require('pages/exploration_player/ExplorationFooterDirective.ts');
-require('pages/exploration_player/LearnerLocalNav.ts');
-require('pages/exploration_player/LearnerViewInfo.ts');
+require('pages/exploration_player/LearnerLocalNavDirective.ts');
+require('pages/exploration_player/LearnerViewInfoDirective.ts');
 
 require('domain/exploration/ReadOnlyExplorationBackendApiService.ts');
 require('services/ContextService.ts');
