a582ed0b47f2477ccf96ce9f10596c5ab9588cb2
Brian Rodriguez <brian@brianrodri.com>
2019-06-07 09:23:03 -0400

    use consistent fileoverviwew

--- core/templates/dev/head/pages/exploration_editor/ExplorationStatesServiceSpec.ts
+++ core/templates/dev/head/pages/exploration_editor/ExplorationStatesServiceSpec.ts
@@ -12,7 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/** @fileoverview Tests for ExplorationStatesService. */
+/**
+ * @fileoverview Tests for ExplorationStatesService.
+ */
 
 require('pages/exploration_editor/ExplorationStatesService.ts');
 
