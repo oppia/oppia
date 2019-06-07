415b0fc71ea439fe5f9083682eb1606451ff6d7f
YashJipkate <yashjipkate@gmail.com>
2019-06-08 03:46:25 +0530

    Fix backend test

--- core/domain/rte_component_registry_test.py
+++ core/domain/rte_component_registry_test.py
@@ -204,7 +204,7 @@ class RteComponentUnitTests(test_utils.GenericTestBase):
 
         rtc_html_file = os.path.join(
             feconf.FRONTEND_TEMPLATES_DIR, 'components',
-            'rich_text_components.html')
+            'rich-text-components.template.html')
         with open(rtc_html_file, 'r') as f:
             rtc_html_file_contents = f.read()
 
