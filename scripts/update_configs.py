3853cf9864d7068078be845e0e6cc548aa3c27c5
Sean Lip <sean@seanlip.org>
2019-06-09 01:52:48 -0700

    Address review comments.

--- scripts/update_configs.py
+++ scripts/update_configs.py
@@ -1,4 +1,4 @@
-# Copyright 2017 The Oppia Authors. All Rights Reserved.
+# Copyright 2019 The Oppia Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,7 +18,7 @@ ONLY RELEASE COORDINATORS SHOULD USE THIS SCRIPT.
 
 Usage: Run this script from your oppia root folder:
 
-    python scripts/update_feconf.py
+    python scripts/update_configs.py
 """
 
 import os
