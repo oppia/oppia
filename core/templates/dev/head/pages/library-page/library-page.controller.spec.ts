763462a6aa3ec08256f73b18a63e978de2c8a44c
YashJipkate <yashjipkate@gmail.com>
2019-06-09 19:39:59 +0530

    Fix karma

--- core/templates/dev/head/pages/library-page/library-page.controller.spec.ts
+++ core/templates/dev/head/pages/library-page/library-page.controller.spec.ts
@@ -16,7 +16,7 @@
  * @fileoverview Unit tests for the controller of the library page.
  */
 
-require('pages/library/LibraryPageDirective.ts');
+require('pages/library-page/library-page.directive.ts');
 
 describe('Library controller', function() {
   beforeEach(angular.mock.module('oppia'));
