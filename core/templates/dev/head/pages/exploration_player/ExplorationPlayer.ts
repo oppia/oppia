0395e4aa711763b63e1a88b5b0a58da97eeeee5f
Kevin Lee <kevinlee963@gmail.com>
2019-06-09 18:57:54 -0700

    Employ webpack for extension objects for exploration player

--- core/templates/dev/head/pages/exploration_player/ExplorationPlayer.ts
+++ core/templates/dev/head/pages/exploration_player/ExplorationPlayer.ts
@@ -94,6 +94,11 @@ require('pages/exploration_player/LearnerLocalNavDirective.ts');
 require('pages/exploration_player/LearnerViewInfoDirective.ts');
 
 require('domain/exploration/ReadOnlyExplorationBackendApiService.ts');
+
+require('objects/templates/MathLatexStringEditorDirective.ts')
+require('objects/templates/SanitizedUrlEditorDirective.ts')
+require('objects/templates/ListOfTabsEditorDirective.ts')
+
 require('services/ContextService.ts');
 require('services/PageTitleService.ts');
 
