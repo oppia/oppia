3d3fce7af1f27b43efcbcbab255302a093b21508
Yash Jipkate <34203227+YashJipkate@users.noreply.github.com>
2019-06-10 14:04:04 +0530

    Fix e2e

--- core/templates/dev/head/App.ts
+++ core/templates/dev/head/App.ts
@@ -132,7 +132,7 @@ oppia.constant('SITE_NAME', 'Oppia.org');
 oppia.constant('DEFAULT_PROFILE_IMAGE_PATH', '/avatar/user_blue_72px.png');
 oppia.constant('FEEDBACK_POPOVER_PATH',
   '/pages/exploration-player-page/templates/' +
-  'feedback-popup-container.template.html.html');
+  'feedback-popup-container.template.html');
 
 oppia.constant('LOGOUT_URL', '/logout');
 
