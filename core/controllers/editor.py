69bc2c687f860e24efe150cd452910d0a58416fc
Vojtěch Jelínek <vojtin.j@gmail.com>
2019-06-12 07:10:52 +0200

    Fix part of #6732: Migrate visualizations to webpack (#6885)
    * migrate visualizations to webpack
    
    * fix linting
    
    * fix linting
    
    * fix linting
    
    * test coverage
    
    * fix tests

--- core/controllers/editor.py
+++ core/controllers/editor.py
@@ -38,7 +38,6 @@ from core.domain import state_domain
 from core.domain import stats_domain
 from core.domain import stats_services
 from core.domain import user_services
-from core.domain import visualization_registry
 from core.platform import models
 import feconf
 import utils
@@ -94,8 +93,6 @@ class ExplorationPage(EditorHandler):
         exploration_rights = rights_manager.get_exploration_rights(
             exploration_id)
 
-        visualizations_html = visualization_registry.Registry.get_full_html()
-
         interaction_ids = (
             interaction_registry.Registry.get_all_interaction_ids())
 
@@ -137,7 +134,6 @@ class ExplorationPage(EditorHandler):
             'interaction_templates': jinja2.utils.Markup(
                 interaction_templates),
             'meta_description': feconf.CREATE_PAGE_DESCRIPTION,
-            'visualizations_html': jinja2.utils.Markup(visualizations_html),
             'INVALID_PARAMETER_NAMES': feconf.INVALID_PARAMETER_NAMES,
             'SHOW_TRAINABLE_UNRESOLVED_ANSWERS': (
                 feconf.SHOW_TRAINABLE_UNRESOLVED_ANSWERS),
