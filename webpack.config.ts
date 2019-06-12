c3396f92da0113c0a7cc7777c199217bf18135b3
James John James <jamesjay4199@gmail.com>
2019-06-12 12:12:38 +0100

    serve strwards page statically

--- webpack.config.ts
+++ webpack.config.ts
@@ -347,8 +347,13 @@ module.exports = {
       inject: false
     }),
     new HtmlWebpackPlugin({
-      chunks: ['stewards'],
+      chunks: ['app', 'stewards'],
       filename: 'stewards-landing-page.mainpage.html',
+      meta: {
+        name: 'Personalized Online Learning from Oppia',
+        description: 'Oppia is a free, open-source learning platform. Join the ' +
+          'community to create or try an exploration today!'
+      },
       template:
         commonPrefix +
         '/pages/landing-pages/stewards-landing-page/' +
