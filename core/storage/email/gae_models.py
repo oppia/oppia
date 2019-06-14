aea517a356c7c9c9163c0c3328378bb74d5835b3
Ankita Saxena <ankitasonu24@gmail.com>
2019-06-14 17:13:01 +0530

    Update commit commands testing

--- core/storage/email/gae_models.py
+++ core/storage/email/gae_models.py
@@ -238,7 +238,6 @@ class BulkEmailModel(base_models.BaseModel):
     id/key of instances of this model is randomly generated string of
     length 12.
     """
-    MODEL_ID_LENGTH = 12
     # The user IDs of the email recipients.
     recipient_ids = ndb.JsonProperty(default=[], compressed=True)
     # The user ID of the email sender. For site-generated emails this is equal
