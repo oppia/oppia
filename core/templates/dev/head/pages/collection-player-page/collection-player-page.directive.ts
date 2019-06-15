f4368d6c0e3bce2d472cc712a60902ca63c44a96
Showtim3 <coolrishabhrawat@gmail.com>
2019-06-16 01:00:58 +0530

    fixed linting-2

--- core/templates/dev/head/pages/collection-player-page/collection-player-page.directive.ts
+++ core/templates/dev/head/pages/collection-player-page/collection-player-page.directive.ts
@@ -252,25 +252,25 @@ oppia.directive('collectionPlayerPage', ['UrlInterpolationService',
 
           ctrl.getExplorationTitlePosition = function(index) {
             if ( index % 2 === 0){
-              return {width:'160',
-                      position:'absolute',
-                      color:'#006553',
-                      'font-size':'20px',
-                      bottom:'70%'}
-            } else if ((index+1) % 2 === 0 && (index+1) % 4 !== 0) {
-              return {width:'160',
-                      position:'absolute',
-                      color:'#006553',
-                      bottom:'70%',
-                      'font-size':'20px',
-                       left:'20px'}
-            } else if ((index+1) % 4 === 0) {
-              return {width:'160px',
-                      position:'absolute',
-                      color:'#006553',
-                      'font-size':'20px',
-                      bottom:'70%',
-                      left:'-35px'}
+              return { width:'160',
+              position:'absolute',
+              color:'#006553',
+              'font-size':'20px',
+              bottom:'70%'}
+            } else if ((index + 1) % 2 === 0 && (index + 1) % 4 !== 0) {
+              return { width:'160',
+              position:'absolute',
+              color:'#006553',
+              bottom:'70%',
+              'font-size':'20px',
+               left:'20px'}
+            } else if ((index + 1) % 4 === 0) {
+              return { width:'160px',
+              position:'absolute',
+              color:'#006553',
+              'font-size':'20px',
+              bottom:'70%',
+              left:'-35px'}
             }
           };
 
