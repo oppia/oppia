3e5dd97d7cb6775057e79a1d51460006ae6c3335
sophiewu6 <wushiqi1998@gmail.com>
2019-06-16 18:34:18 -0700

    Fix typo

--- core/controllers/review_tests_test.py
+++ core/controllers/review_tests_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Tests for the reivew tests page."""
+"""Tests for the review tests page."""
 
 from constants import constants
 from core.domain import story_domain
