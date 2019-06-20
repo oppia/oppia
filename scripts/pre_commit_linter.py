502842b34df941127caa44a6b40ff19348c3d92b
NishealJ <nishealajohn@karunya.edu.in>
2019-06-20 11:17:08 +0300

    Merge conflicts-2

--- scripts/pre_commit_linter.py
+++ scripts/pre_commit_linter.py
@@ -392,7 +392,7 @@ _PATHS_TO_INSERT = [
     os.path.join(_PARENT_DIR, 'oppia_tools', 'browsermob-proxy-0.7.1'),
     os.path.join(_PARENT_DIR, 'oppia_tools', 'esprima-4.0.1'),
     os.path.join(_PARENT_DIR, 'oppia_tools', 'pycodestyle-2.5.0'),
-    os.path.join(_PARENT_DIR, 'oppia_tools', 'pylint-quotes-0.1.9'),
+    os.path.join(_PARENT_DIR, 'oppia_tools', 'pylint-quotes-0.2.1'),
     os.path.join(_PARENT_DIR, 'oppia_tools', 'selenium-2.53.2'),
     os.path.join(_PARENT_DIR, 'oppia_tools', 'PyGithub-1.43.5'),
     os.path.join(_PARENT_DIR, 'oppia_tools', 'Pillow-6.0.0'),
