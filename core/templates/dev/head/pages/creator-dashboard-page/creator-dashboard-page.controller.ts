bcc91e42d030e90977aeb30d260e04fb7dd3230f
YashJipkate <yashjipkate@gmail.com>
2019-06-27 13:13:21 +0530

    Remove unnecessary change

--- core/templates/dev/head/pages/creator-dashboard-page/creator-dashboard-page.controller.ts
+++ core/templates/dev/head/pages/creator-dashboard-page/creator-dashboard-page.controller.ts
@@ -249,14 +249,10 @@ oppia.directive('creatorDashboardPage', ['UrlInterpolationService', function(
             ctrl.lastWeekStats = responseData.last_week_stats;
             ctrl.myExplorationsView = responseData.display_preference;
             ctrl.topicSummaries = responseData.topic_summary_dicts;
-            if (responseData.threads_for_created_suggestions_list) {
-              var numberOfCreatedSuggestions = (
-                responseData.threads_for_created_suggestions_list.length);
-            }
-            if (responseData.threads_for_suggestions_to_review_list) {
-              var numberOfSuggestionsToReview = (
-                responseData.threads_for_suggestions_to_review_list.length);
-            }
+            var numberOfCreatedSuggestions = (
+              responseData.threads_for_created_suggestions_list.length);
+            var numberOfSuggestionsToReview = (
+              responseData.threads_for_suggestions_to_review_list.length);
             ctrl.mySuggestionsList = [];
             for (var i = 0; i < numberOfCreatedSuggestions; i++) {
               if (responseData.created_suggestions_list.length !==
