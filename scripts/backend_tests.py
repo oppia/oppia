38a5e322a6adc7f890307f7f8c6d85c0a73e4158
Ankita Saxena <ankitasonu24@gmail.com>
2019-07-01 01:32:00 +0530

    Remove print

--- scripts/backend_tests.py
+++ scripts/backend_tests.py
@@ -303,8 +303,6 @@ def main():
 
     import dev_appserver
     dev_appserver.fix_sys_path()
-    print sys.executable
-    print "\n".join(sys.path)
 
     parsed_args = _PARSER.parse_args()
     if parsed_args.test_target and parsed_args.test_path:
