0d25d509ee9bc745a9703a9fd1095a5e7eb4d8c0
Kevin Thomas <kevintab@tutanota.com>
2019-07-02 00:15:22 +0530

    Fixes #5795: Adds null check and changes z-index value for "Restart From Beginning" button (#7060)
    * Fixes #2197 and #2195
    
    * fix e2e error (library)
    
    * Use URL as constants
    
    * Constants in all caps
    
    * Fix linting issue
    
    * change z-index of oppia-preview-bottom-right-container so hints are clickable; add null check in isHintButtonVisible;
    
    * address review comment

--- core/templates/dev/head/components/button-directives/hint-and-solution-buttons.directive.ts
+++ core/templates/dev/head/components/button-directives/hint-and-solution-buttons.directive.ts
@@ -78,6 +78,7 @@ oppia.directive('hintAndSolutionButtons', [
           ctrl.isHintButtonVisible = function(index) {
             return (
               HintsAndSolutionManagerService.isHintViewable(index) &&
+              ctrl.displayedCard !== null &&
               ctrl.displayedCard.doesInteractionSupportHints());
           };
 
