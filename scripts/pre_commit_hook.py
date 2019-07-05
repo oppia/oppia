9410679c3d29b9a301bff273810ecf60ad026b4f
Sean Lip <sean@seanlip.org>
2019-07-05 04:03:34 -0700

    Remove unnecessary deletion.

--- scripts/pre_commit_hook.py
+++ scripts/pre_commit_hook.py
@@ -71,6 +71,7 @@ def _does_diff_include_package_lock_file_and_no_package_file():
     Raises:
         ValueError if git command fails.
     """
+
     git_cmd = ['git', 'diff', '--name-only', '--cached']
     out, err = _start_subprocess_for_result(git_cmd)
 
