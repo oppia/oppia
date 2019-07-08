f480794e97bfb232c90304015edf0a30f797c581
YashJipkate <yashjipkate@gmail.com>
2019-07-08 14:24:35 +0530

    Fix lint

--- core/templates/dev/head/services/ExplorationFeaturesService.ts
+++ core/templates/dev/head/services/ExplorationFeaturesService.ts
@@ -21,7 +21,7 @@ import { Injectable } from '@angular/core';
 import { downgradeInjectable } from '@angular/upgrade/static';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class ExplorationFeaturesService {
   static settings = {
