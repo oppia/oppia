22b532768303bd0e3971b558d5ef31b56bfe76d1
Pranav Siddharth S <pranavsid98@gmail.com>
2019-07-17 08:06:44 +0530

    Fix #5975: Fixed fraction display for Playthroughs (#7062)
    * First version
    
    * Working directives
    
    * Fixed all tests and directives
    
    * Removed erroneous comments
    
    * Removed erroneous statement
    
    * Fix lint errors
    
    * fix_e2e_tests
    
    * Addressed review comments
    
    * Addressed next round of review comments
    
    * Final changes

---
+++ core/templates/dev/head/pages/exploration-editor-page/statistics-tab/issues/answer-submit-action.directive.ts
@@ -0,0 +1,56 @@
+// Copyright 2019 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Directive for the Answer Submit Learner Action.
+ */
+
+require('domain/utilities/UrlInterpolationService.ts');
+require('services/ExplorationHtmlFormatterService.ts');
+require('services/HtmlEscaperService.ts');
+
+var oppia = require('AppInit.ts').module;
+
+oppia.directive('answerSubmitAction', [
+  'ExplorationHtmlFormatterService', 'HtmlEscaperService',
+  'UrlInterpolationService',
+  function(
+      ExplorationHtmlFormatterService, HtmlEscaperService,
+      UrlInterpolationService) {
+    return {
+      restrict: 'E',
+      scope: {},
+      bindToController: {},
+      templateUrl: UrlInterpolationService.getDirectiveTemplateUrl(
+        '/pages/exploration-editor-page/statistics-tab/issues/' +
+        'answer-submit-action.directive.html'),
+      controllerAs: '$ctrl',
+      controller: ['$attrs', function($attrs) {
+        var ctrl = this;
+        ctrl.currentStateName = $attrs.currentStateName;
+        ctrl.destStateName = $attrs.destStateName;
+        ctrl.actionIndex = $attrs.actionIndex;
+        ctrl.timeSpentInStateSecs = $attrs.timeSpentInStateSecs;
+
+        var _customizationArgs = HtmlEscaperService.escapedJsonToObj(
+          $attrs.interactionCustomizationArgs);
+        var _answer = HtmlEscaperService.escapedJsonToObj($attrs.answer);
+        ctrl.getShortAnswerHtml = function() {
+          return ExplorationHtmlFormatterService.getShortAnswerHtml(
+            _answer, $attrs.interactionId, _customizationArgs);
+        };
+      }]
+    };
+  }
+]);
