cbf6488e01b995d0333abaa1f4f33483c75c6a89
YashJipkate <yashjipkate@gmail.com>
2019-07-21 02:15:16 +0530

    Fix FE?

--- core/templates/dev/head/domain/learner_dashboard/LearnerDashboardIdsBackendApiServiceSpec.ts
+++ core/templates/dev/head/domain/learner_dashboard/LearnerDashboardIdsBackendApiServiceSpec.ts
@@ -16,6 +16,13 @@
  * @fileoverview Unit tests for LearnerDashboardIdsBackendApiService.
  */
 
+import 'zone.js/dist/long-stack-trace-zone';
+import 'zone.js/dist/proxy.js';
+import 'zone.js/dist/sync-test';
+import 'zone.js/dist/jasmine-patch';
+import 'zone.js/dist/async-test';
+import 'zone.js/dist/fake-async-test';
+
 import { BrowserDynamicTestingModule,
   platformBrowserDynamicTesting } from
   '@angular/platform-browser-dynamic/testing';
