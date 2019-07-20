1a8e07a97c5df98f91e815633eb852a106826a8f
YashJipkate <yashjipkate@gmail.com>
2019-07-20 13:15:39 +0530

    CollectionPlaythroughObjectFactory

--- core/templates/dev/head/domain/collection/CollectionPlaythroughObjectFactory.ts
+++ core/templates/dev/head/domain/collection/CollectionPlaythroughObjectFactory.ts
@@ -17,79 +17,78 @@
  * collection playthrough domain objects.
  */
 
-var oppia = require('AppInit.ts').module;
+import * as _ from 'lodash';
 
-oppia.factory('CollectionPlaythroughObjectFactory', [function() {
+import { downgradeInjectable } from '@angular/upgrade/static';
+import { Injectable } from '@angular/core';
+
+export class CollectionPlaythrough {
+  _nextExplorationId: any;
+  _completedExplorationIds: any;
+  // Stores information about a current playthrough of a collection for a
+  // user.
+  constructor(nextExplorationId: any, completedExplorationIds: any) {
+    this._nextExplorationId = nextExplorationId;
+    this._completedExplorationIds = completedExplorationIds;
+  }
+
+  // Returns the upcoming exploration ID. Changes to this are not
+  // reflected in the collection.
+  getNextExplorationId() {
+    return this._nextExplorationId;    
+  }
+
+  getNextRecommendedCollectionNodeCount() {
+    // As the collection is linear, only a single node would be available,
+    // after any node.
+    return 1;
+  }
+
+  // Returns a list of explorations completed that are related to this
+  // collection. Changes to this list are not reflected in this collection.
+  getCompletedExplorationIds() {
+    return _.cloneDeep(this._completedExplorationIds);
+  }
+
+  getCompletedExplorationNodeCount() {
+    return this._completedExplorationIds.length;
+  }
+
+  hasStartedCollection() {
+    return this._completedExplorationIds.length !== 0;
+  }
+}
+
+Injectable({
+  providedIn: 'root'
+})
+export class CollectionPlaythroughObjectFactory extends CollectionPlaythrough {
   // TODO(bhenning): Add setters for some of these properties. Setters allow
   // the collection editor to setup specifically configured playthrough
   // sessions of the collection player through this object (for example, the
   // editor would be able to fake which explorations were completed to see how
   // that particular configuration would look for a learner).
-
-  // Stores information about a current playthrough of a collection for a
-  // user.
-  var CollectionPlaythrough = function(
-      nextExplorationId, completedExplorationIds) {
-    this._nextExplorationId = nextExplorationId;
-    this._completedExplorationIds = completedExplorationIds;
-  };
-
-  // Returns the upcoming exploration ID. Changes to this are not
-  // reflected in the collection.
-  CollectionPlaythrough.prototype.getNextExplorationId = function() {
-    return this._nextExplorationId;
-  };
-
-  CollectionPlaythrough.prototype.getNextRecommendedCollectionNodeCount =
-    function() {
-      // As the collection is linear, only a single node would be available,
-      // after any node.
-      return 1;
-    };
-
-  // TODO (ankita240796) Remove the bracket notation once Angular2 gets in.
-  /* eslint-disable dot-notation */
-  CollectionPlaythrough['hasFinishedCollection'] = function() {
-  /* eslint-enable dot-notation */
+  hasFinishedCollection() {
     return this._nextExplorationId === null;
-  };
-
-  // Returns a list of explorations completed that are related to this
-  // collection. Changes to this list are not reflected in this collection.
-  CollectionPlaythrough.prototype.getCompletedExplorationIds = function() {
-    return angular.copy(this._completedExplorationIds);
-  };
-
-  CollectionPlaythrough.prototype.getCompletedExplorationNodeCount =
-    function() {
-      return this._completedExplorationIds.length;
-    };
-
-  CollectionPlaythrough.prototype.hasStartedCollection = function() {
-    return this._completedExplorationIds.length !== 0;
-  };
+  }
 
   // Static class methods. Note that "this" is not available in static
   // contexts. This function takes a JSON object which represents a backend
   // collection playthrough python dict.
-  // TODO (ankita240796) Remove the bracket notation once Angular2 gets in.
-  /* eslint-disable dot-notation */
-  CollectionPlaythrough['createFromBackendObject'] = function(
-  /* eslint-enable dot-notation */
-      collectionPlaythroughBackendObject) {
+  createFromBackendObject(collectionPlaythroughBackendObject) {
     return new CollectionPlaythrough(
       collectionPlaythroughBackendObject.next_exploration_id,
       collectionPlaythroughBackendObject.completed_exploration_ids);
-  };
+  }
 
-  // TODO (ankita240796) Remove the bracket notation once Angular2 gets in.
-  /* eslint-disable dot-notation */
-  CollectionPlaythrough['create'] = function(
-  /* eslint-enable dot-notation */
-      nextExplorationId, completedExplorationIds) {
+  create(nextExplorationId: any, completedExplorationIds: any) {
     return new CollectionPlaythrough(
-      nextExplorationId, angular.copy(completedExplorationIds));
-  };
+      nextExplorationId, _.cloneDeep(completedExplorationIds));
+  }
+}
 
-  return CollectionPlaythrough;
-}]);
+var oppia = require('AppInit.ts').module;
+
+oppia.factory(
+  'CollectionPlaythroughObjectFactory',
+  downgradeInjectable(CollectionPlaythroughObjectFactory));
