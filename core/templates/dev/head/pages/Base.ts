71fdef77b6702b4aff7dc716638df5ab81dd288b
James James <31052489+Jamesjay4199@users.noreply.github.com>
2019-07-22 08:54:57 +0100

    Serve some dynamic pages using transclusion components (#7195)
    * serve library page statically
    
    * serve creator dashboard page statically
    
    * serve exploration player statically
    
    * serve exploration_editor statically
    
    * serve profile page statically
    
    * serve collection editor page statically
    
    * serve collection player statically
    
    * serve collection_player statically
    
    * get exploration player meta via ajax
    
    * remove unnecessary console
    
    * modify test to add objective
    
    * work on failing backend tests
    
    * work on failing e2e tests
    
    * work on failing e2e test
    
    * fix issues arising from rootScope propagation
    
    * move dev mode test to e2e test
    
    * removed csrf token
    
    * work on failing accessibility test
    
    * fix lint issue
    
    * issues with rootscope
    
    * work on failing tests and address review comments
    
    * fix issue with collection player
    
    * removed exploration editor from this PR
    
    * work on failing test
    
    * still on backend test
    
    * remove unneeded file
    
    * remove remaining changes
    
    * minor changes
    
    * minor changes
    
    * address review comment
    
    * modified dev mode test
    
    * wrok on e2e tests
    
    * revert pre_commit_hook
    
    * address review comment
    
    * work on e2e test
    
    * work on failing test
    
    * fix e2e test
    
    * remove unnecessary files

--- core/templates/dev/head/pages/Base.ts
+++ core/templates/dev/head/pages/Base.ts
@@ -14,10 +14,8 @@
 
 require('domain/sidebar/SidebarStatusService.ts');
 require('domain/utilities/UrlInterpolationService.ts');
-require('services/AlertsService.ts');
 require('services/CsrfTokenService.ts');
 require('services/contextual/UrlService.ts');
-require('services/stateful/BackgroundMaskService.ts');
 
 require('app.constants.ts');
 
@@ -28,19 +26,15 @@ require('app.constants.ts');
 var oppia = require('AppInit.ts').module;
 
 oppia.controller('Base', [
-  '$document', '$http', '$rootScope', '$scope', 'AlertsService',
-  'BackgroundMaskService', 'CsrfTokenService', 'SidebarStatusService',
-  'UrlInterpolationService', 'UrlService', 'DEV_MODE',
-  'SITE_FEEDBACK_FORM_URL', 'SITE_NAME',
-  function($document, $http, $rootScope, $scope, AlertsService,
-      BackgroundMaskService, CsrfTokenService, SidebarStatusService,
-      UrlInterpolationService, UrlService, DEV_MODE,
-      SITE_FEEDBACK_FORM_URL, SITE_NAME) {
+  '$document', '$rootScope', '$scope', 'CsrfTokenService',
+  'SidebarStatusService', 'UrlInterpolationService', 'UrlService', 'DEV_MODE',
+  'SITE_NAME',
+  function(
+      $document, $rootScope, $scope, CsrfTokenService,
+      SidebarStatusService, UrlInterpolationService, UrlService, DEV_MODE,
+      SITE_NAME) {
     $scope.siteName = SITE_NAME;
-    $scope.AlertsService = AlertsService;
     $scope.currentLang = 'en';
-    $scope.iframed = UrlService.isIframed();
-    $scope.siteFeedbackFormUrl = SITE_FEEDBACK_FORM_URL;
     $scope.pageUrl = UrlService.getCurrentLocation().href;
     $scope.getAssetUrl = function(path) {
       return UrlInterpolationService.getFullStaticAssetUrl(path);
@@ -49,11 +43,6 @@ oppia.controller('Base', [
     $rootScope.DEV_MODE = DEV_MODE;
     // If this is nonempty, the whole page goes into 'Loading...' mode.
     $rootScope.loadingMessage = '';
-
-    $scope.isSidebarShown = SidebarStatusService.isSidebarShown;
-    $scope.closeSidebarOnSwipe = SidebarStatusService.closeSidebar;
-
-    $scope.isBackgroundMaskActive = BackgroundMaskService.isMaskActive;
 
     CsrfTokenService.initializeToken();
 
@@ -67,16 +56,5 @@ oppia.controller('Base', [
       SidebarStatusService.onDocumentClick();
       $scope.$apply();
     });
-
-    $scope.skipToMainContent = function() {
-      var mainContentElement = document.getElementById('oppia-main-content');
-
-      if (!mainContentElement) {
-        throw Error('Variable mainContentElement is undefined.');
-      }
-      mainContentElement.tabIndex = -1;
-      mainContentElement.scrollIntoView();
-      mainContentElement.focus();
-    };
   }
 ]);
