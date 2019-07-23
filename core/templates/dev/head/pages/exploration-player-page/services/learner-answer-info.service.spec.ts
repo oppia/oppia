7d70ef78a0c9db3d5a0ba90e003808e2fa8b5afe
Anubhav Sinha <anubhavsinha98@gmail.com>
2019-07-23 09:43:55 +0530

    Made some changes

--- core/templates/dev/head/pages/exploration-player-page/services/learner-answer-info.service.spec.ts
+++ core/templates/dev/head/pages/exploration-player-page/services/learner-answer-info.service.spec.ts
@@ -56,9 +56,299 @@ describe('Learner answer info service', function() {
       new WrittenTranslationObjectFactory());
   }));
 
+  var ExplorationEngineService = null;
   var LearnerAnswerInfoService = null;
-  beforeEach(angular.mock.inject(function($injector) {
-    LearnerAnswerInfoService = $injector.get('LearnerAnswerInfoService');
+
+  beforeEach(angular.mock.inject(function(
+    _LearnerAnswerInfoService_, _ExplorationEngineService_) {
+      ExplorationEngineService = _ExplorationEngineService_;
+      LearnerAnswerInfoService = _LearnerAnswerInfoService_;
+
+      var explorationDict = {
+        id: 1,
+        title: 'My Title',
+        category: 'Art',
+        objective: 'Your objective',
+        tags: [],
+        blurb: '',
+        author_notes: '',
+        states_schema_version: 15,
+        init_state_name: 'Introduction',
+        states: {
+          'State 1': {
+            param_changes: [],
+            content: {
+              html: '',
+              content_id: 'content'
+            },
+            recorded_voiceovers: {
+              voiceovers_mapping: {
+                content: {},
+                default_outcome: {}
+              }
+            },
+            interaction: {
+              id: 'Continue',
+              default_outcome: {
+                feedback: {
+                  content_id: 'default_outcome',
+                  html: ''
+                },
+                dest: 'State 3',
+                param_changes: []
+              },
+              confirmed_unclassified_answers: [],
+              customization_args: {
+                buttonText: {
+                  value: 'Continue'
+                }
+              },
+              solution: null,
+              answer_groups: [],
+              hints: []
+            },
+            solicit_answer_details: false,
+            written_translations: {
+              translations_mapping: {
+                content: {},
+                default_outcome: {}
+              }
+            },
+            classifier_model_id: null
+          },
+          'State 3': {
+            param_changes: [],
+            content: {
+              content_id: 'content',
+              html: 'Congratulations, you have finished!'
+            },
+            recorded_voiceovers: {
+              voiceovers_mapping: {
+                content: {}
+              }
+            },
+            interaction: {
+              id: 'EndExploration',
+              default_outcome: null,
+              confirmed_unclassified_answers: [],
+              customization_args: {
+                recommendedExplorationIds: {
+                  value: []
+                }
+              },
+              solution: null,
+              answer_groups: [],
+              hints: []
+            },
+            solicit_answer_details: false,
+            written_translations: {
+              translations_mapping: {
+                content: {}
+              }
+            },
+            classifier_model_id: null
+          },
+          Introduction: {
+            classifier_model_id: null,
+            param_changes: [],
+            content: {
+              content_id: 'content',
+              html: 'Multiple Choice'
+            },
+            recorded_voiceovers: {
+              voiceovers_mapping: {
+                content: {},
+                default_outcome: {},
+                feedback_1: {},
+                feedback_2: {}
+              }
+            },
+            interaction: {
+              id: 'MultipleChoiceInput',
+              default_outcome: {
+                dest: 'Introduction',
+                feedback: {
+                  content_id: 'default_outcome',
+                  html: 'Try Again!'
+                }
+              },
+              confirmed_unclassified_answers: [],
+              customization_args: {
+                choices: {
+                  value: [
+                    '<p> Go to ItemSelection <oppia-noninteractive-image' +
+                    ' filepath-with-value="&amp;quot;' +
+                    'sIMChoice1_height_32_width_42.png&amp;' +
+                    'quot;"></oppia-noninteractive-image></p>',
+                    '<p> Go to ImageAndRegion<oppia-noninteractive-image' +
+                    ' filepath-with-value="&amp;quot;' +
+                    'sIMChoice2_height_30_width_40.png&amp;' +
+                    'quot;"></oppia-noninteractive-image></p>'
+                  ]
+                }
+              },
+              answer_groups: [
+                {
+                  labelled_as_correct: false,
+                  outcome: {
+                    dest: 'State 6',
+                    feedback: {
+                      content_id: 'feedback_1',
+                      html: '<p>We are going to ItemSelection' +
+                            '<oppia-noninteractive-image filepath-with-value=' +
+                            '"&amp;quot;sIOFeedback_height_50_width_50.png' +
+                            '&amp;quot;"></oppia-noninteractive-image></p>'
+                    },
+                    param_changes: [],
+                    refresher_exploration_id: null,
+                    missing_prerequisite_skill_id: null
+                  },
+                  rule_specs: [
+                    {
+                      inputs: {
+                        x: 0
+                      },
+                      rule_type: 'Equals'
+                    }
+                  ]
+                },
+                {
+                  labelled_as_correct: false,
+                  outcome: {
+                    dest: 'State 1',
+                    feedback: {
+                      content_id: 'feedback_2',
+                      html: "Let's go to state 1 ImageAndRegion"
+                    },
+                    param_changes: [],
+                    refresher_exploration_id: null,
+                    missing_prerequisite_skill_id: null
+                  },
+                  rule_specs: [
+                    {
+                      inputs: {
+                        x: 1
+                      },
+                      rule_type: 'Equals'
+                    }
+                  ]
+                }
+              ],
+              hints: [],
+              solution: null
+            },
+            solicit_answer_details: false,
+            written_translations: {
+              translations_mapping: {
+                content: {},
+                default_outcome: {},
+                feedback_1: {},
+                feedback_2: {}
+              }
+            }
+          },
+          'State 6': {
+            param_changes: [],
+            content: {
+              content_id: 'content',
+              html: '<p>Text Input Content</p>'
+            },
+            recorded_voiceovers: {
+              voiceovers_mapping: {
+                content: {},
+                default_outcome: {},
+                feedback_1: {},
+                feedback_2: {},
+                hint_1: {}
+              }
+            },
+            interaction: {
+              id: 'TextInput',
+              default_outcome: {
+                dest: 'State 6',
+                feedback: {
+                  content_id: 'default_outcome',
+                  html: ''
+                },
+                labelled_as_correct: false,
+                param_changes: [],
+                refresher_exploration_id: null,
+                missing_prerequisite_skill_id: null
+              },
+              confirmed_unclassified_answers: [],
+              customization_args: {
+                rows: {
+                  value: 1
+                },
+                placeholder: {
+                  value: ''
+                }
+              },
+              answer_groups: [{
+                rule_specs: [{
+                  inputs: {
+                    x: '1'
+                  },
+                  rule_type: 'Contains'
+                }],
+                outcome: {
+                  dest: 'State 1',
+                  feedback: {
+                    content_id: 'feedback_1',
+                    html: "<p>Let's go to State 1</p>"
+                  },
+                  labelled_as_correct: false,
+                  param_changes: [],
+                  refresher_exploration_id: null,
+                  missing_prerequisite_skill_id: null
+                }
+              }, {
+                rule_specs: [{
+                  inputs: {
+                    x: '2'
+                  },
+                  rule_type: 'Contains'
+                }],
+                outcome: {
+                  dest: 'State 1',
+                  feedback: {
+                    content_id: 'feedback_2',
+                    html: "<p>Let's go to State 1</p>"
+                  },
+                  labelled_as_correct: false,
+                  param_changes: [],
+                  refresher_exploration_id: null,
+                  missing_prerequisite_skill_id: null
+                }
+              }],
+              hints: [{
+                hint_content: {
+                  content_id: 'hint_1',
+                  html: '<p><oppia-noninteractive-image filepath-with-value="' +
+                        '&amp;quot;s6Hint1_height_60_width_60.png&amp;quot;">' +
+                        '</oppia-noninteractive-image></p>'
+                }
+              }],
+              solution: null,
+            },
+            solicit_answer_details: false,
+            written_translations: {
+              translations_mapping: {
+                content: {},
+                default_outcome: {},
+                feedback_1: {},
+                feedback_2: {},
+                hint_1: {}
+              }
+            },
+            classifier_model_id: null
+          }
+        },
+        param_specs: {},
+        param_changes: [],
+        version: 1
+      };
+      ExplorationEngineService.init(explorationDict, '1', 'en', false);
   }));
 
   it('should increment number of attempts correctly', function() {
