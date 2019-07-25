88e062c88d24724070c4993459cce7b7034bdad3
Yash Jipkate <34203227+YashJipkate@users.noreply.github.com>
2019-07-25 21:36:03 +0530

    Upgrade more services to Angular 8 - III (#7214)
    * Untested work on PlaythroughIssueObjectFactory.ts
    
    * Revert "Untested work on PlaythroughIssueObjectFactory.ts"
    
    This reverts commit 13424bfcd086e90f6625608540220ebf747e3360.
    
    * TopicRightsObjectFactory
    
    * Lint fix
    
    * Removed unnecessary constructor statement
    
    * ClassifierObjectFactory
    
    * AnswerClassificationResultObjectFactory
    
    * PredictionResultObjectFactory
    
    * WrittenTranslationObjectFactory
    
    * RuleObjectFactory
    
    * AudioTranslationObjectFactory
    
    * ExplorationDraftObjectFactory
    
    * Fix failing FE in TopicRightsObjectFactory
    
    * Add force bootstrap to all pages
    
    * function() -> () =>
    
    * boilerplate
    
    * FeedbackMessageSummaryObjectFactory
    
    * FeedbackThreadObjectFactory
    
    * FeedbackThreadSummaryObjectFactory
    
    FeedbackThreadSummaryObjectFactory
    
    * Merge ser-up-more
    
    * SuggestionObjectFactory
    
    * AudioFileObjectFactory
    
    * AudioLanguageObjectFactory
    
    * ImageFileObjectFactory
    
    * FileDownloadRequestObjectFactory
    
    * AutogeneratedAudioLanguageObjectFactory
    
    * SkillSummaryObjectFactory
    
    * SkillRightsObjectFactory
    
    * MisconceptionObjectFactory
    
    * LearnerDashboardActivityIdsObjectFactory
    
    LearnerDashboardActivityIdsObjectFactory
    
    * UserInfoObjectFactory
    
    * PageTitleService
    
    * Merge proper
    
    * Merge proper
    
    * UtilsService
    
    * Revert "UtilsService"
    
    This reverts commit dc009f91de7a7fc091c82302252b524f646c08f9.
    
    * IdGenerationService Fail FE
    
    * Revert "IdGenerationService Fail FE"
    
    This reverts commit 2406979ac598bd925029e25a856e933514605f3e.
    
    * SchemaUndefinedLastElementService
    
    * AutoplayedVideosService
    
    * EditabilityService
    
    * CodeNormalizerService
    
    * SchemaDefaultValueService
    
    * ImprovementsService
    
    * SuggestionModalService
    
    * CurrentInteractionService
    
    * VoiceoverObjectFactory
    
    * AudioTranslationManagerService
    
    * Fix tests
    
    * Fix tests
    
    * PlayerCorrectnessFeedbackEnabledService
    
    * Update mocking method
    
    * Fix lint
    
    * Fix test
    
    * NumberAttemptsService
    
    * AngularNameService
    
    * SolutionValidityService
    
    * Fix lint
    
    Fix lint
    
    * AdminTaskManagerService
    
    * RatingComputationService
    
    * ContinueRulesService
    
    * AnswerStatsObjectFactory
    
    * More typing
    
    * Audit and newline
    
    * Merge
    
    * Stronger typing
    
    * Fix FE
    
    * Address comments
    
    * Remove newlines, TODO for imports, modify MockOutcome
    
    * Fix matcher case, modify TODO for imports
    
    * Fix FE
    
    * Fix FE
    
    * Stronger typing in RuleObjectFactory
    
    * Fix lint
    
    * Fix e2e
    
    * Fix AJAX errors in Karma
    
    * Fix FE
    
    * Add comment in csrftoken
    
    * Stronger typing
    
    Stronger typing
    
    * Fix FE?
    
    * Fix FE?
    
    * Stronger typing
    
    * Fix FE?
    
    * Fixed e2e but FE?
    
    * Fix FE?
    
    * Fix Karma for good
    
    * Wrap
    
    * Minor nit
    
    * Minor nit
    
    * Address review
    
    * Address review
    
    * Address review
    
    * Address review
    
    * Added comments and types
    
    * Added comments and types
    
    * Narrowed types
    
    * Lint
    
    * Address review
    
    * Address review
    
    * Address review

--- core/templates/dev/head/pages/exploration-editor-page/translation-tab/services/translation-status.service.spec.ts
+++ core/templates/dev/head/pages/exploration-editor-page/translation-tab/services/translation-status.service.spec.ts
@@ -18,6 +18,8 @@
 
 // TODO(YashJipkate): Remove the following block of unnnecessary imports once
 // translation-status.service.ts is upgraded to Angular 8.
+import { AngularNameService } from
+  'pages/exploration-editor-page/services/angular-name.service.ts';
 import { AnswerClassificationResultObjectFactory } from
   'domain/classifier/AnswerClassificationResultObjectFactory.ts';
 import { ClassifierObjectFactory } from
@@ -25,6 +27,12 @@ import { ClassifierObjectFactory } from
 import { ExplorationDraftObjectFactory } from
   'domain/exploration/ExplorationDraftObjectFactory.ts';
 import { RuleObjectFactory } from 'domain/exploration/RuleObjectFactory.ts';
+/* eslint-disable max-len */
+import { SolutionValidityService } from
+  'pages/exploration-editor-page/editor-tab/services/solution-validity.service.ts';
+/* eslint-enable max-len */
+import { VoiceoverObjectFactory } from
+  'domain/exploration/VoiceoverObjectFactory.ts';
 import { WrittenTranslationObjectFactory } from
   'domain/exploration/WrittenTranslationObjectFactory.ts';
 // ^^^ This block is to be removed.
@@ -50,6 +58,7 @@ describe('Translation status service', function() {
         return ['en', 'hi'];
       }
     });
+    $provide.value('AngularNameService', new AngularNameService());
     $provide.value(
       'AnswerClassificationResultObjectFactory',
       new AnswerClassificationResultObjectFactory());
@@ -57,6 +66,8 @@ describe('Translation status service', function() {
     $provide.value(
       'ExplorationDraftObjectFactory', new ExplorationDraftObjectFactory());
     $provide.value('RuleObjectFactory', new RuleObjectFactory());
+    $provide.value('SolutionValidityService', new SolutionValidityService());
+    $provide.value('VoiceoverObjectFactory', new VoiceoverObjectFactory());
     $provide.value(
       'WrittenTranslationObjectFactory',
       new WrittenTranslationObjectFactory());
