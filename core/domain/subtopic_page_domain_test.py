d3bc979fc5e2014cbb8fd7e89bd30b5edfbf3453
RafayGhafoor <rafayghafoor@pm.me>
2019-07-27 14:05:57 +0500

    linting

--- core/domain/subtopic_page_domain_test.py
+++ core/domain/subtopic_page_domain_test.py
@@ -154,7 +154,8 @@ class SubtopicPageDomainUnitTests(test_utils.GenericTestBase):
             'version': 0
         }
         self.subtopic_page.update_page_contents_audio(
-            state_domain.RecordedVoiceovers.from_dict(recorded_voiceovers_dict))
+            state_domain.RecordedVoiceovers.from_dict(
+                recorded_voiceovers_dict))
         self.assertEqual(self.subtopic_page.to_dict(),
                          expected_subtopic_page_dict)
 
@@ -187,7 +188,7 @@ class SubtopicPageDomainUnitTests(test_utils.GenericTestBase):
             state_domain.SubtitledHtml.from_dict({
                 'html': '<p>hello world</p>',
                 'content_id': 'content'
-        }))
+            }))
         self.assertEqual(self.subtopic_page.to_dict(),
                          expected_subtopic_page_dict)
 
