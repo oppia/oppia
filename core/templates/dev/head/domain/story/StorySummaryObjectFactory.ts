2df27da4f15325c37782b545a4cb0c7c9cf444e8
aks681 <akshayanand681@gmail.com>
2019-07-30 01:26:30 +0530

    lint

--- core/templates/dev/head/domain/story/StorySummaryObjectFactory.ts
+++ core/templates/dev/head/domain/story/StorySummaryObjectFactory.ts
@@ -20,44 +20,43 @@
 var oppia = require('AppInit.ts').module;
 
 oppia.factory('StorySummaryObjectFactory', [function() {
-    var StorySummary = function(id, title, nodeCount, storyIsPublished) {
-      this._id = id;
-      this._title = title;
-      this._nodeCount = nodeCount;
-      this._storyIsPublished = storyIsPublished;
-    };
+  var StorySummary = function(id, title, nodeCount, storyIsPublished) {
+    this._id = id;
+    this._title = title;
+    this._nodeCount = nodeCount;
+    this._storyIsPublished = storyIsPublished;
+  };
 
-    // Instance methods
+  // Instance methods
 
-    StorySummary.prototype.getId = function() {
-      return this._id;
-    };
+  StorySummary.prototype.getId = function() {
+    return this._id;
+  };
 
-    StorySummary.prototype.getTitle = function() {
-      return this._title;
-    };
+  StorySummary.prototype.getTitle = function() {
+    return this._title;
+  };
 
-    StorySummary.prototype.getNodeCount = function() {
-      return this._nodeCount;
-    };
+  StorySummary.prototype.getNodeCount = function() {
+    return this._nodeCount;
+  };
 
-    StorySummary.prototype.isStoryPublished = function() {
-      return this._storyIsPublished;
-    };
+  StorySummary.prototype.isStoryPublished = function() {
+    return this._storyIsPublished;
+  };
 
 
-    // TODO(ankita240796): Remove the bracket notation once Angular2 gets in.
-    /* eslint-disable dot-notation */
-    StorySummary['createFromBackendDict'] = function(storySummaryBackendDict) {
-    /* eslint-enable dot-notation */
-      return new StorySummary(
-        storySummaryBackendDict.id,
-        storySummaryBackendDict.title,
-        storySummaryBackendDict.node_count,
-        storySummaryBackendDict.story_is_published
-      );
-    };
+  // TODO(ankita240796): Remove the bracket notation once Angular2 gets in.
+  /* eslint-disable dot-notation */
+  StorySummary['createFromBackendDict'] = function(storySummaryBackendDict) {
+  /* eslint-enable dot-notation */
+    return new StorySummary(
+      storySummaryBackendDict.id,
+      storySummaryBackendDict.title,
+      storySummaryBackendDict.node_count,
+      storySummaryBackendDict.story_is_published
+    );
+  };
 
-    return StorySummary;
-  }
-]);
+  return StorySummary;
+}]);
