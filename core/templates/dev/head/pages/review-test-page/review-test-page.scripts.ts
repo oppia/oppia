97f451990ceae2c9959b9900d000df263734bfa2
YashJipkate <yashjipkate@gmail.com>
2019-08-01 14:27:26 +0530

    Review test page

--- core/templates/dev/head/pages/review-test-page/review-test-page.scripts.ts
+++ core/templates/dev/head/pages/review-test-page/review-test-page.scripts.ts
@@ -16,7 +16,7 @@
  * @fileoverview Directive scripts for the review tests.
  */
 
-require('AppInit.ts');
+require('pages/review-test-page/review-test-page.module.ts');
 require('App.ts');
 
 require('base_components/BaseContentDirective.ts');
