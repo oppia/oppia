b1b4a2e46e3e5ce571005a562a60bf2565689c9c
sophiewu6 <wushiqi1998@gmail.com>
2019-08-03 17:40:04 -0700

    lint

--- core/templates/dev/head/domain/skill/SkillMasteryBackendApiService.ts
+++ core/templates/dev/head/domain/skill/SkillMasteryBackendApiService.ts
@@ -16,14 +16,10 @@
  * @fileoverview Service to send changes to skill mastery to the backend.
  */
 
-require('domain/utilities/UrlInterpolationService.ts');
-
 angular.module('oppia').factory('SkillMasteryBackendApiService', [
-  '$http', '$q', 'UrlInterpolationService',
-  'SKILL_MASTERY_DATA_URL_TEMPLATE',
+  '$http', '$q', 'SKILL_MASTERY_DATA_URL_TEMPLATE',
   function(
-      $http, $q, UrlInterpolationService,
-      SKILL_MASTERY_DATA_URL_TEMPLATE) {
+      $http, $q, SKILL_MASTERY_DATA_URL_TEMPLATE) {
     var _fetchSkillMasteryDegrees = function(
         skillIds, successCallback, errorCallback) {
       $http.get(SKILL_MASTERY_DATA_URL_TEMPLATE, {
