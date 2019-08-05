206bd536e2d5ce29553c4365b0dc40d01753c831
Kevin Lee <kevinlee963@gmail.com>
2019-08-05 00:16:15 -0700

    Add additional line

--- schema_utils.py
+++ schema_utils.py
@@ -212,6 +212,7 @@ class Normalizers(object):
             raise Exception('Invalid normalizer id: %s' % normalizer_id)
         return getattr(cls, normalizer_id)
 
+
     @staticmethod
     def normalize_spaces(obj):
         """Collapses multiple spaces into single spaces.
