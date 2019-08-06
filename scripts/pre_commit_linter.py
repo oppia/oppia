35b7f71939b58ea3a29d3b2aaf14625645cf912b
Saurav Pratihar <pratihar.saurav@gmail.com>
2019-08-06 11:54:30 +0530

    change request implemented

--- scripts/pre_commit_linter.py
+++ scripts/pre_commit_linter.py
@@ -791,25 +791,19 @@ class CustomHTMLParser(HTMLParser.HTMLParser):
 
         # Check the indentation for content of style tag.
         if tag_line.startswith(opening_tag) and tag == 'style':
-            next_line_number = line_number + 1
-            next_line = self.file_lines[next_line_number - 1]
+            # Getting next line after style tag.
+            next_line = self.file_lines[line_number]
             next_line_expected_indentation = (
                 self.indentation_level + 1) * self.indentation_width
-            next_line_column_number = 0
-            # Check column number.
-            for i in next_line.split(' '):
-                if i == '':
-                    next_line_column_number = next_line_column_number + 1
-                else:
-                    break
-
+            next_line_column_number = len(next_line) - len(next_line.lstrip())
+            
             if next_line_column_number != next_line_expected_indentation:
                 print (
                     '%s --> Expected indentation '
                     'of %s, found indentation of %s '
                     'for content of %s tag on line %s ' % (
                         self.filepath, next_line_expected_indentation,
-                        expected_indentation, tag, next_line_number))
+                        expected_indentation, tag, line_number + 1))
                 print ''
                 self.failed = True
 
