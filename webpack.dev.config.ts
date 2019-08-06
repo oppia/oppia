a87821c67b43730b3fe86d444196ae894d05465c
Vojtěch Jelínek <vojtin.j@gmail.com>
2019-08-06 09:27:16 +0200

    Fix #6766: Fix webpack hanging (#7311)

--- webpack.dev.config.ts
+++ webpack.dev.config.ts
@@ -69,12 +69,7 @@ module.exports = {
       ],
       use: [
         'cache-loader',
-        {
-          loader: 'thread-loader',
-          options: {
-            poolTimeout: Infinity,
-          }
-        },
+        'thread-loader',
         {
           loader: 'ts-loader',
           options: {
