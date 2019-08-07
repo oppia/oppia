063658395232c2ab39ac49287e9963bdc7409faf
Vojtěch Jelínek <vojtin.j@gmail.com>
2019-08-07 15:26:08 +0200

    linting

--- scripts/build.py
+++ scripts/build.py
@@ -1345,7 +1345,7 @@ def build():
             generate_build_directory(hashes)
     else:
         save_hashes_to_file(dict())
-      
+
 
 # The 'no coverage' pragma is used as this line is un-testable. This is because
 # it will only be called when build.py is used as a script.
