47b23f010decf77c1a77596aac22034aea0e2ad6
Vojtěch Jelínek <vojtin.j@gmail.com>
2019-08-09 22:59:31 +0200

    Fix webpack configs

--- webpack.common.config.ts
+++ webpack.common.config.ts
@@ -19,7 +19,6 @@
 const CKEditorWebpackPlugin = require(
   '@ckeditor/ckeditor5-dev-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const { styles } = require('@ckeditor/ckeditor5-dev-utils/lib/');
@@ -541,8 +540,7 @@ module.exports = {
     }),
     new CleanWebpackPlugin({
       cleanAfterEveryBuildPatterns: ['**/*', '!*.html'],
-    }),
-    new ForkTsCheckerWebpackPlugin({ checkSyntacticErrors: true }),
+    })
   ],
   module: {
     rules: [{
