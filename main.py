5635ebc067e6dd8fe299523f42b4ce61d89de469
James James <31052489+Jamesjay4199@users.noreply.github.com>
2019-08-09 05:06:47 +0100

    Serve more static pages using app.yaml (#7268)
    * serve more static pages via app.yaml
    
    * move redirect test to e2e test
    
    * work on failing backend test
    
    * work on failing e2e test
    
    * work on e2e tests

--- main.py
+++ main.py
@@ -180,19 +180,13 @@ URLS = MAPREDUCE_HANDLERS + [
     get_redirect_route(r'/_ah/warmup', WarmupPage),
     get_redirect_route(r'/', HomePageRedirectPage),
 
-    get_redirect_route(r'/about', pages.AboutPage),
     get_redirect_route(r'/foundation', pages.FoundationRedirectPage),
     get_redirect_route(r'/credits', pages.AboutRedirectPage),
     get_redirect_route(r'/participate', pages.TeachRedirectPage),
     get_redirect_route(r'/site_guidelines', pages.TeachRedirectPage),
     get_redirect_route(r'/console_errors', pages.ConsoleErrorPage),
-    get_redirect_route(r'/contact', pages.ContactPage),
 
     get_redirect_route(r'/forum', pages.ForumRedirectPage),
-    get_redirect_route(r'/donate', pages.DonatePage),
-    get_redirect_route(r'/thanks', pages.ThanksPage),
-    get_redirect_route(r'/terms', pages.TermsPage),
-    get_redirect_route(r'/privacy', pages.PrivacyPage),
 
     get_redirect_route(r'%s' % feconf.ADMIN_URL, admin.AdminPage),
     get_redirect_route(r'/adminhandler', admin.AdminHandler),
