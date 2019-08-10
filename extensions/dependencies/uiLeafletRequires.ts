2f669a647132fd94b6c9699266f7cb9f84482ef3
James John James <jamesjay4199@gmail.com>
2019-08-10 13:34:00 +0100

    add file overview for new files

--- extensions/dependencies/uiLeafletRequires.ts
+++ extensions/dependencies/uiLeafletRequires.ts
@@ -1,3 +1,22 @@
+// Copyright 2019 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Loads scripts needed for ui-leaflet.
+ */
+
+// TODO: Uncomment this after #7286 goes in.
 // require('static/leaflet-1.4.0/leaflet.css');
 require('static/leaflet-1.4.0/leaflet.js');
 require('static/angular-ui-leaflet-1.0.3/ui-leaflet.min.no-header.js');
