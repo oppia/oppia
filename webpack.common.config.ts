5928adccc91639e01cc35a1c5735756528e57412
Vojtěch Jelínek <vojtin.j@gmail.com>
2019-08-12 21:46:58 +0200

    Fix webpack

--- webpack.common.config.ts
+++ webpack.common.config.ts
@@ -593,6 +593,13 @@ module.exports = {
     {
       test: /\.html$/,
       loader: 'underscore-template-loader'
+    },
+    {
+      test: /\.css$/,
+      include: [
+        path.resolve(__dirname, 'extensions'),
+      ],
+      use: ['style-loader', 'css-loader']
     }]
   },
   optimization: {
