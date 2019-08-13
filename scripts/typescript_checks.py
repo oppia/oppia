122d62732286ae47d6ccb075d005491a40c72a0e
Ankita Saxena <ankitasonu24@gmail.com>
2019-08-13 19:57:14 +0530

    Revert wrong commit

--- scripts/typescript_checks.py
+++ scripts/typescript_checks.py
@@ -12,87 +12,57 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Unit tests for scripts/typescript_checks.py."""
+"""File for compiling and checking typescript."""
 
 import json
 import os
+import shutil
 import subprocess
+import sys
 
-# pylint: disable=relative-import
-from core.tests import test_utils
-import typescript_checks
-
-# pylint: enable=relative-import
-
-TEST_SOURCE_DIR = os.path.join('core', 'tests', 'build_sources')
-MOCK_COMPILED_JS_DIR = os.path.join(TEST_SOURCE_DIR, 'compiled_js_dir', '')
+COMPILED_JS_DIR = os.path.join('local_compiled_js', '')
+TSCONFIG_FILEPATH = 'tsconfig-for-compile-check.json'
 
 
-class TypescriptChecksTests(test_utils.GenericTestBase):
-    """Test the typescript checks."""
-    def setUp(self):
-        super(TypescriptChecksTests, self).setUp()
-        process = subprocess.Popen(['test'], stdout=subprocess.PIPE)
-        # pylint: disable=unused-argument
-        def mock_popen(unused_cmd, stdout):
-            return process
-        # pylint: enable=unused-argument
+def validate_compiled_js_dir():
+    """Validates that compiled js dir matches out dir in tsconfig."""
+    with open(TSCONFIG_FILEPATH) as f:
+        config_data = json.load(f)
+        out_dir = os.path.join(config_data['compilerOptions']['outDir'], '')
+    if out_dir != COMPILED_JS_DIR:
+        raise Exception(
+            'COMPILED_JS_DIR: %s does not match the output directory '
+            'in %s: %s' % (COMPILED_JS_DIR, TSCONFIG_FILEPATH, out_dir))
 
-        self.popen_swap = self.swap(subprocess, 'Popen', mock_popen)
 
-    def test_compiled_js_dir_validation(self):
-        """Test that typescript_checks.COMPILED_JS_DIR is validated correctly
-        with outDir in typescript_checks.TSCONFIG_FILEPATH.
-        """
-        with self.popen_swap:
-            typescript_checks.compile_and_check_typescript()
-            out_dir = ''
-            with open(typescript_checks.TSCONFIG_FILEPATH) as f:
-                config_data = json.load(f)
-                out_dir = os.path.join(
-                    config_data['compilerOptions']['outDir'], '')
-            compiled_js_dir_swap = self.swap(
-                typescript_checks, 'COMPILED_JS_DIR', MOCK_COMPILED_JS_DIR)
-            with compiled_js_dir_swap, self.assertRaisesRegexp(
-                Exception,
-                'COMPILED_JS_DIR: %s does not match the output directory '
-                'in %s: %s' % (
-                    MOCK_COMPILED_JS_DIR, typescript_checks.TSCONFIG_FILEPATH,
-                    out_dir)):
-                typescript_checks.compile_and_check_typescript()
+def compile_and_check_typescript():
+    """Compiles typescript files and checks the compilation errors."""
+    node_path = os.path.join(os.pardir, 'oppia_tools/node-10.15.3')
+    os.environ['PATH'] = '%s/bin:' % node_path + os.environ['PATH']
 
-    def test_compiled_js_dir_is_deleted_before_compilation(self):
-        """Test that compiled_js_dir is deleted before a fresh compilation."""
-        def mock_validate_compiled_js_dir():
-            pass
+    validate_compiled_js_dir()
 
-        compiled_js_dir_swap = self.swap(
-            typescript_checks, 'COMPILED_JS_DIR', MOCK_COMPILED_JS_DIR)
-        validate_swap = self.swap(
-            typescript_checks, 'validate_compiled_js_dir',
-            mock_validate_compiled_js_dir)
-        with self.popen_swap, compiled_js_dir_swap, validate_swap:
-            if not os.path.exists(os.path.dirname(MOCK_COMPILED_JS_DIR)):
-                os.mkdir(os.path.dirname(MOCK_COMPILED_JS_DIR))
+    if os.path.exists(COMPILED_JS_DIR):
+        shutil.rmtree(COMPILED_JS_DIR)
 
-            typescript_checks.compile_and_check_typescript()
-            self.assertFalse(
-                os.path.exists(os.path.dirname(MOCK_COMPILED_JS_DIR)))
+    print 'Compiling and testing typescript...'
+    cmd = [
+        './node_modules/typescript/bin/tsc', '--project',
+        TSCONFIG_FILEPATH]
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    error_messages = []
+    for line in iter(process.stdout.readline, ''):
+        error_messages.append(line)
+    if error_messages:
+        print 'Errors found during compilation\n'
+        for message in error_messages:
+            print message
+        sys.exit(1)
+    else:
+        print 'Compilation successful!'
 
-    def test_no_error_is_produced_for_valid_compilation(self):
-        """Test that no error is produced if stdout is empty."""
-        with self.popen_swap:
-            typescript_checks.compile_and_check_typescript()
 
-    def test_error_is_produced_for_invalid_compilation(self):
-        """Test that error is produced if stdout is not empty."""
-        process = subprocess.Popen(['echo', 'test'], stdout=subprocess.PIPE)
-        # pylint: disable=unused-argument
-        def mock_popen_for_errors(unused_cmd, stdout):
-            return process
-        # pylint: enableg=unused-argument
-
-        with self.swap(
-            subprocess, 'Popen', mock_popen_for_errors), self.assertRaises(
-                SystemExit):
-            typescript_checks.compile_and_check_typescript()
+# The 'no coverage' pragma is used as this line is un-testable. This is because
+# it will only be called when build.py is used as a script.
+if __name__ == '__main__':  # pragma: no cover
+    compile_and_check_typescript()
