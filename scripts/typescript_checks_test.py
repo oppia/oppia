4033e490b1ac532c24fc19c4c416daed95ed186e
Ankita Saxena <ankitasonu24@gmail.com>
2019-08-14 01:18:38 +0530

    Make coverage 100%

--- scripts/typescript_checks_test.py
+++ scripts/typescript_checks_test.py
@@ -79,6 +79,31 @@ class TypescriptChecksTests(test_utils.GenericTestBase):
             self.assertFalse(
                 os.path.exists(os.path.dirname(MOCK_COMPILED_JS_DIR)))
 
+    def test_compiled_js_dir_is_deleted_after_compilation(self):
+        """Test that compiled_js_dir is deleted before a fresh compilation."""
+        def mock_validate_compiled_js_dir():
+            pass
+        process = subprocess.Popen(['test'], stdout=subprocess.PIPE)
+        # pylint: disable=unused-argument
+        def mock_popen_for_deletion(unused_cmd, stdout):
+            os.mkdir(os.path.dirname(MOCK_COMPILED_JS_DIR))
+            return process
+        # pylint: enable=unused-argument
+
+        compiled_js_dir_swap = self.swap(
+            typescript_checks, 'COMPILED_JS_DIR', MOCK_COMPILED_JS_DIR)
+        validate_swap = self.swap(
+            typescript_checks, 'validate_compiled_js_dir',
+            mock_validate_compiled_js_dir)
+        popen_swap = self.swap(
+            subprocess, 'Popen', mock_popen_for_deletion)
+        with popen_swap, compiled_js_dir_swap, validate_swap:
+            if not os.path.exists(os.path.dirname(MOCK_COMPILED_JS_DIR)):
+                os.mkdir(os.path.dirname(MOCK_COMPILED_JS_DIR))
+            typescript_checks.compile_and_check_typescript()
+            self.assertFalse(
+                os.path.exists(os.path.dirname(MOCK_COMPILED_JS_DIR)))
+
     def test_no_error_is_produced_for_valid_compilation(self):
         """Test that no error is produced if stdout is empty."""
         with self.popen_swap:
