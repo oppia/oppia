3742b3df9b621f41e17aa77945437b300f89af3b
Rafay Ghafoor <rafayghafoor@pm.me>
2019-08-15 12:02:06 +0500

    resolve case

--- extensions/classifiers/count-vectorizer.service.spec.ts
+++ extensions/classifiers/count-vectorizer.service.spec.ts
@@ -16,7 +16,7 @@
  * @fileoverview Unit tests for the count vectorizer service.
  */
 
-import { CountVectorizerService } from 'classifiers/CountVectorizerService.ts';
+import { CountVectorizerService } from 'classifiers/count-vectorizer.service.ts';
 
 describe('Count vectorizer service', function() {
   describe('Test count vectorizer service', function() {
