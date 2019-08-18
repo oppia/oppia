9839a3ff6ca20073b0173e79075e8e5a6084fc5f
James James <jamesjay4199@gmail.com>
2019-08-18 22:27:25 +0100

    address review comments

--- core/templates/dev/head/pages/interaction-specs.constants.ajs.ts
+++ core/templates/dev/head/pages/interaction-specs.constants.ajs.ts
@@ -16,7 +16,7 @@
  * @fileoverview Constant file for the INTERACTION_SPECS constant.
  */
 
-var interactionSpecs = require('interactions/interaction_specs.json');
+const interactionSpecs = require('interactions/interaction_specs.json');
 
 angular.module('oppia').constant(
   'INTERACTION_SPECS', interactionSpecs);
