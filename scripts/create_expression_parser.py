a5eebebee315054fabf84802cfd4c80318dffd38
Rishav Chakraborty <annonymousxyz@outlook.com>
2019-08-23 02:05:29 +0530

    convert

---
+++ scripts/create_expression_parser.py
@@ -0,0 +1,68 @@
+# Copyright 2019 The Oppia Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""INSTRUCTIONS:
+Run this script from the oppia root folder:
+  python -m scripts.create_expression_parser
+The root folder MUST be named 'oppia'.
+It produces the expression parser.
+"""
+
+import fileinput
+import os
+import re
+import subprocess
+
+from . import install_third_party_libs
+from . import setup
+
+
+def main():
+    """Produces the expression parser."""
+    setup.main()
+
+    expression_parser_definition = (
+        'core/templates/dev/head/expressions/parser.pegjs')
+    expression_parser_js = (
+        'core/templates/dev/head/expressions/ExpressionParserService.js')
+
+    # Install the basic environment, e.g. nodejs.
+    install_third_party_libs.main()
+
+    curr_dir = os.path.abspath(os.getcwd())
+    oppia_tools_dir = os.path.join(curr_dir, '..', 'oppia_tools')
+    node_path = os.path.join(oppia_tools_dir, 'node-10.15.3')
+
+    print 'Checking whether pegjs is installed in %s' % oppia_tools_dir
+    if not os.path.exists('node_modules/pegjs'):
+        print 'Installing pegjs'
+        subprocess.call(('%s/bin/npm install pegjs@0.8.0' % node_path).split())
+
+    subprocess.call((
+        'node_modules/pegjs/bin/pegjs %s %s'
+        % (expression_parser_definition, expression_parser_js)).split())
+
+    for line in fileinput.input(files=expression_parser_js, inplace=True):
+        print re.sub(
+            r'module\.exports.*$',
+            'angular.module(\'oppia\').factory('
+            '\'ExpressionParserService\', [\'$log\', function($log) {', line),
+
+        print re.sub(r'^})();\s*$', '}]);', line),
+
+    print 'Done!'
+
+
+if __name__ == '__main__':
+    main()
