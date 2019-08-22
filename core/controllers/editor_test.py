a6c42394404fe69a2cf5c285b55920b30e1fe8ec
Rishav Chakraborty <annonymousxyz@outlook.com>
2019-08-22 16:18:08 +0530

    fix

--- core/controllers/editor_test.py
+++ core/controllers/editor_test.py
@@ -525,7 +525,7 @@ written_translations:
         response = self.get_custom_response(download_url, 'text/plain')
 
         # Check downloaded zip file.
-        filename = 'oppia-b\'ThetitleforZIPdownloadhandlertest!\'-v2.zip'
+        filename = 'oppia-ThetitleforZIPdownloadhandlertest!-v2.zip'
         self.assertEqual(response.headers['Content-Disposition'],
                          'attachment; filename=%s' % filename)
         zf_saved = zipfile.ZipFile(
