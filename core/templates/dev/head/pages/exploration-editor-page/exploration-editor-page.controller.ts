1ad66a239927b45e1b1b0cdab12649306df09348
James James <jamesjay4199@gmail.com>
2019-08-23 21:47:58 +0100

    move some script imports from to webpack

--- core/templates/dev/head/pages/exploration-editor-page/exploration-editor-page.controller.ts
+++ core/templates/dev/head/pages/exploration-editor-page/exploration-editor-page.controller.ts
@@ -17,6 +17,7 @@
  *               help tab in the navbar.
  */
 
+require('d3');
 // TODO(vojtechjelinek): this block of requires should be removed after we
 // introduce webpack for /extensions
 require('components/ck-editor-helpers/ck-editor-4-rte.directive.ts');
