e1e6a3d1b3f78e9728a9a5e3c5e4db44ea4ea540
James James <jamesjay4199@gmail.com>
2019-08-24 16:06:00 +0100

    fix failing tests

--- core/templates/dev/head/pages/exploration-player-page/exploration-player-page.controller.ts
+++ core/templates/dev/head/pages/exploration-player-page/exploration-player-page.controller.ts
@@ -16,8 +16,6 @@
  * @fileoverview Dependencies for the explaration player page.
  */
 
- require('d3');
-
 // TODO(vojtechjelinek): this block of requires should be removed after we
 // introduce webpack for /extensions
 require('components/ck-editor-helpers/ck-editor-4-rte.directive.ts');
