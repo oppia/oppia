6fbeccab060cee017bd35966b938b1bb734e4677
Vojtěch Jelínek <vojtin.j@gmail.com>
2019-08-24 15:21:00 +0200

    Assert comments

--- scripts/build.py
+++ scripts/build.py
@@ -603,7 +603,8 @@ def build_third_party_libs(third_party_directory_path):
 def build_using_webpack():
     """Execute webpack build process. This takes all TypeScript files we have in
     /templates/dev/head and generates JS bundles according the require() imports
-    and also compiles HTML pages into the /templates/dev/head/dist folder.
+    and also compiles HTML pages into the /backend_prod_files/webpack_bundles
+    folder. The files are later copied into /build/webpack_bundles.
 
     The settings for this are specified in webpack.prod.config.ts.
     """
