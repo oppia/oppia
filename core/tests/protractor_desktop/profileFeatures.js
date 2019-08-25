5a8119629bdc8a1c1d9d791499b6273686271f08
cs <cs.temporary@icloud.com>
2019-08-25 18:24:02 +0000

    Protractor: Default Profile of Current User
    Create protractor end-to-end tests to check that when a logged-in
    user has not customized their profile, their profile page shows:
    
    * A profile photo
    * Placeholder bio text
    * Placeholder interests text

--- core/tests/protractor_desktop/profileFeatures.js
+++ core/tests/protractor_desktop/profileFeatures.js
@@ -15,3 +15,51 @@
 /**
  * @fileoverview End-to-end tests for user profile features.
  */
+
+var DEFAULT_BIO = 'This user has not supplied a bio yet.';
+var PLACEHOLDER_INTEREST_TEXT = 'none specified';
+
+var users = require('../protractor_utils/users.js');
+var general = require('../protractor_utils/general.js');
+var waitFor = require('../protractor_utils/waitFor.js');
+
+var ProfilePage = require('../protractor_utils/ProfilePage.js');
+
+describe('Un-customized profile page for current, logged-in user', function() {
+  var TEST_USERNAME = 'currentDefaultProfileFeatures';
+  var TEST_EMAIL = TEST_USERNAME + '@example.com';
+
+  var profilePage = null;
+
+  beforeAll(function() {
+    profilePage = new ProfilePage.ProfilePage();
+    users.createUser(TEST_EMAIL, TEST_USERNAME);
+  });
+
+  beforeEach(function() {
+    users.login(TEST_EMAIL);
+    profilePage.get(TEST_USERNAME);
+  });
+
+  it('displays profile photo', function() {
+    profilePage.expectCurrUserToHaveProfilePhoto();
+  });
+
+  it('displays placeholder bio text', function() {
+    profilePage.expectUserToHaveBio(DEFAULT_BIO);
+  });
+
+  it('displays no interests', function() {
+    profilePage.expectUserToHaveNoInterests();
+  });
+
+  it('displays placeholder interest text', function() {
+    profilePage.expectUserToHaveInterestPlaceholder(
+      PLACEHOLDER_INTEREST_TEXT);
+  });
+
+  afterEach(function() {
+    users.logout();
+    general.checkForConsoleErrors([]);
+  });
+});
