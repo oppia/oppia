42582829a41fceca3f797512d1141a82bfa7e1ea
Rishav Chakraborty <annonymousxyz@outlook.com>
2019-08-28 18:14:34 +0530

    fix

--- core/domain/stats_domain_test.py
+++ core/domain/stats_domain_test.py
@@ -1863,8 +1863,7 @@ class LearnerAnswerInfoTests(test_utils.GenericTestBase):
         learner_answer_info_id = (
             stats_domain.LearnerAnswerInfo.get_new_learner_answer_info_id())
         self.assertNotEqual(learner_answer_info_id, None)
-        self.assertTrue(
-            isinstance(learner_answer_info_id, python_utils.UNICODE))
+        self.assertTrue(isinstance(learner_answer_info_id, bytes))
 
     def test_id_must_be_string(self):
         self.learner_answer_info.id = 123
