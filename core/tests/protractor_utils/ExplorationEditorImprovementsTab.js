b16e3102f39b991a097732c891207242c5f2c5dc
Brian Rodriguez <brian@brianrodri.com>
2019-08-28 23:06:54 -0400

    fix merge conflicts

--- core/tests/protractor_utils/ExplorationEditorImprovementsTab.js
+++ core/tests/protractor_utils/ExplorationEditorImprovementsTab.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-// Copyright 2018 The Oppia Authors. All Rights Reserved.
-=======
 // Copyright 2019 The Oppia Authors. All Rights Reserved.
->>>>>>> upstream/develop
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,46 +13,6 @@
 // limitations under the License.
 
 /**
-<<<<<<< HEAD
- * @fileoverview Page object for the exploration editor's feedback tab, for
- * use in Protractor tests.
- */
-
-var ExplorationEditorImprovementsTab = function() {
-  var improvementsOpenCardsCount = element(
-    by.css('.protractor-test-improvements-open-cards-count'));
-
-  this.getNumberOfOpenCards = function() {
-    return parseInt(improvementsOpenCardsCount.getText());
-  };
-
-  /** Cards are returned in top-to-bottom order. */
-  this.getCardByIndex = function(cardIndex) {
-    return element(
-      by.css('.protractor-test-improvement-card' + cardIndex.toString()));
-  };
-
-  this.getCardTitle = function(cardElement) {
-    return cardElement.element(
-      by.css('.oppia-improvement-card-header-title')).getText();
-  };
-
-  this.getCardBody = function(cardElement) {
-    return cardElement.element(by.css('modal-body'));
-  };
-
-  this.getCardStatus = function(cardElement) {
-    return cardElement.element(
-      by.css('.oppia-improvement-card-header-status-pill')).getText();
-  };
-
-  /** Buttons are returned in left-to-right order. */
-  this.getActionButtonByIndex = function(cardElement, index) {
-    return cardElement.element(
-      by.css('.protractor-test-improvement-action-button' + index.toString()));
-  };
-};
-=======
  * @fileoverview Page object for the exploration editor's improvements tab, for
  *  use in Protractor tests.
  */
@@ -113,4 +69,3 @@ var ExplorationEditorImprovementsTab = function() {
 };
 
 exports.ExplorationEditorImprovementsTab = ExplorationEditorImprovementsTab;
->>>>>>> upstream/develop
