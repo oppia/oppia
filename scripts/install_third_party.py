5a8f6fc68886fe7269c37f7f40e24e26d3724a86
Luis Ulloa <ulloa@stanford.edu>
2019-09-02 01:42:47 -0400

    Remove Vagrant support from codebase (#7521)

--- scripts/install_third_party.py
+++ scripts/install_third_party.py
@@ -27,12 +27,6 @@ import python_utils
 
 from . import common
 
-# These two lines prevent a "IOError: [Errno socket error]
-# [Errno -2] Name or service not known" error
-# in urllib.urlretrieve, if the user is behind a proxy.
-if 'VAGRANT' in os.environ:
-    os.environ['http_proxy'] = ''
-
 TOOLS_DIR = os.path.join('..', 'oppia_tools')
 THIRD_PARTY_DIR = os.path.join('.', 'third_party')
 THIRD_PARTY_STATIC_DIR = os.path.join(THIRD_PARTY_DIR, 'static')
