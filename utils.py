9fb374a0a64d37d1775880ee8792e5c2d5012a96
Rishav Chakraborty <annonymousxyz@outlook.com>
2019-09-04 22:10:05 +0530

    Fix unicode errors on develop (#7545)
    * Fix unicode errors on develop
    
    * fix comment

--- utils.py
+++ utils.py
@@ -365,9 +365,11 @@ def convert_to_hash(input_string, max_length):
             (input_string, type(input_string)))
 
     # Encodes strings using the character set [A-Za-z0-9].
+    # Prefixing altchars with b' to ensure that all characters in encoded_string
+    # remain encoded (otherwise encoded_string would be of type unicode).
     encoded_string = base64.b64encode(
         hashlib.sha1(python_utils.convert_to_bytes(input_string)).digest(),
-        altchars='ab'
+        altchars=b'ab'
     ).replace('=', 'c')
 
     return encoded_string[:max_length]
