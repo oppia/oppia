a0f901a926e5d825c7930b437db685d8e81b0b42
Sean Lip <sean@seanlip.org>
2019-09-21 01:56:47 -0700

    exception.message is not compatible with python 3

--- scripts/run_backend_tests.py
+++ scripts/run_backend_tests.py
@@ -175,7 +175,7 @@ class TaskThread(threading.Thread):
         except Exception as e:
             self.exception = e
             if 'KeyboardInterrupt' not in python_utils.convert_to_bytes(
-                    self.exception.message):
+                    self.exception.args[0]):
                 log('ERROR %s: %.1f secs' %
                     (self.name, time.time() - self.start_time), show_time=True)
             self.finished = True
@@ -379,7 +379,7 @@ def main(args=None):
 
     for task in tasks:
         if task.exception:
-            log(python_utils.convert_to_bytes(task.exception.message))
+            log(python_utils.convert_to_bytes(task.exception.args[0]))
 
     python_utils.PRINT('')
     python_utils.PRINT('+------------------+')
@@ -399,18 +399,18 @@ def main(args=None):
             test_count = 0
         elif (task.exception and
               'No tests were run' in python_utils.convert_to_bytes(
-                task.exception.message)):
+                task.exception.args[0])):
             python_utils.PRINT(
                 'ERROR     %s: No tests found.' % spec.test_target)
             test_count = 0
         elif task.exception:
-            exc_str = python_utils.convert_to_bytes(task.exception.message)
+            exc_str = python_utils.convert_to_bytes(task.exception.args[0])
             python_utils.PRINT(exc_str[exc_str.find('='): exc_str.rfind('-')])
 
             tests_failed_regex_match = re.search(
                 r'Test suite failed: ([0-9]+) tests run, ([0-9]+) errors, '
                 '([0-9]+) failures',
-                python_utils.convert_to_bytes(task.exception.message))
+                python_utils.convert_to_bytes(task.exception.args[0]))
 
             try:
                 test_count = int(tests_failed_regex_match.group(1))
