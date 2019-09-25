23f47281153695c47ce2bc85e7b799260cc51987
Kevin Thomas <kevintab@tutanota.com>
2019-09-26 02:56:06 +0530

    minor change

--- core/templates/dev/head/components/state-editor/state-content-editor/state-content-editor.directive.ts
+++ core/templates/dev/head/components/state-editor/state-content-editor/state-content-editor.directive.ts
@@ -1,5 +1,3 @@
-import { StateEditorService } from "../state-editor-properties-services/state-editor.service";
-
 // Copyright 2017 The Oppia Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
