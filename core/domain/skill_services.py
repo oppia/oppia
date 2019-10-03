463b987a64d442cc90c8c172e525a15335a6be6b
harshita93 <harshukapadia93@gmail.com>
2019-10-03 13:45:55 -0400

    from core.domain import state_domain moved to the top due to import order checks

--- core/domain/skill_services.py
+++ core/domain/skill_services.py
@@ -22,9 +22,10 @@ import logging
 from core.domain import email_manager
 from core.domain import role_services
 from core.domain import skill_domain
+from core.domain import state_domain
 from core.domain import user_services
 from core.platform import models
-from core.domain import state_domain
+
 import feconf
 import python_utils
 
