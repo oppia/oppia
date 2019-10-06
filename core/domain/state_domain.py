3fa5498a7e65d1b3343ebab22f7ef38a681753d5
edeediong <edidiongmichael08@gmail.com>
2019-10-06 15:57:13 +0100

    updates

--- core/domain/state_domain.py
+++ core/domain/state_domain.py
@@ -798,9 +798,6 @@ class WrittenTranslations(python_utils.OBJECT):
 
     def __init__(self, translations_mapping):
         """Initializes a WrittenTranslations domain object.
-
-        Args:
-            translations_mapping: WrittenTranslations domain object.
         """
         self.translations_mapping = translations_mapping
 
@@ -968,7 +965,7 @@ class RecordedVoiceovers(python_utils.OBJECT):
         """Initializes a RecordedVoiceovers domain object.
 
         Args:
-            voiceovers_mapping: Recorded Voiceovers domain object.
+            voiceovers_mapping: Recorded Voiceovers. Recorded Voiceovers domain object.
         """
         self.voiceovers_mapping = voiceovers_mapping
 
