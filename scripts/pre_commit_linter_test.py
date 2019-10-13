b4bc445890fa24d300912a4448d5859c7eb24921
Luis Ulloa <ulloa@stanford.edu>
2019-10-13 04:51:32 -0700

    Added PythonLintTests and valid/invalid py files.

--- scripts/pre_commit_linter_test.py
+++ scripts/pre_commit_linter_test.py
@@ -27,8 +27,12 @@ import python_utils
 from . import pre_commit_linter
 
 LINTER_TESTS_DIR = os.path.join(os.getcwd(), 'core', 'tests', 'linter_tests')
+
 VALID_HTML_FILEPATH = os.path.join(LINTER_TESTS_DIR, 'valid.html')
 INVALID_HTML_FILEPATH = os.path.join(LINTER_TESTS_DIR, 'invalid.html')
+
+VALID_PYTHON_FILEPATH = os.path.join(LINTER_TESTS_DIR, 'valid.py')
+INVALID_PYTHON_FILEPATH = os.path.join(LINTER_TESTS_DIR, 'invalid.py')
 
 
 def mock_exit(unused_status):
@@ -57,6 +61,30 @@ def all_checks_passed(linter_stdout):
         bool. Whether all checks have passed or not.
     """
     return 'All Checks Passed.' in linter_stdout
+
+def appears_in_linter_stdout(phrases, linter_stdout):
+    """Checks to see if all of the phrases appear in at least one of the
+    linter_stdout outputs.
+
+    Args:
+        phrases: list(str). A list of phrases we are trying to find in
+        one of the linter_stdout outputs. For example, python linting
+        outputs a success string that includes data we don't have easy
+        access to, like how long the test took, so we may want to search
+        for a substring of that success string in linter_stdout.
+
+        linter_stdout: list(str). A list of the output results from the
+        linter's execution. Note that anything placed into the "result"
+        queue in pre_commit_linter will be in the same index.
+
+    Returns:
+        bool. True if and only if all of the phrases appear in at least
+        one of the results stored in linter_stdout.
+    """
+    for output in linter_stdout:
+        if all(phrase in output for phrase in phrases):
+            return True
+    return False
 
 
 class LintTests(test_utils.GenericTestBase):
@@ -107,3 +135,35 @@ class HTMLLintTests(LintTests):
             '[htmllint] found 1 errors out of 1 files\n' % INVALID_HTML_FILEPATH
             ) in self.linter_stdout)
         self.assertTrue('FAILED   HTML linting failed' in self.linter_stdout)
+
+
+class PythonLintTests(LintTests):
+    """Test the Python lint functions."""
+    def setUp(self):
+        super(PythonLintTests, self).setUp()
+        self.js_ts_lint_swap = self.swap(
+            pre_commit_linter.JsTsLintChecksManager, 'perform_all_lint_checks',
+            mock_perform_all_lint_checks)
+        self.check_codeowner_swap = self.swap(
+            pre_commit_linter, 'check_codeowner_file',
+            mock_check_codeowner_file)
+
+    def test_valid_python_file(self):
+        with self.print_swap, self.js_ts_lint_swap, self.check_codeowner_swap:
+            pre_commit_linter.main(args=['--path=%s' % VALID_PYTHON_FILEPATH])
+        self.assertTrue(all_checks_passed(self.linter_stdout))
+        self.assertTrue(
+            appears_in_linter_stdout(
+                ['SUCCESS   1 Python files linted'],
+                self.linter_stdout))
+
+    def test_invalid_python_file(self):
+        with self.print_swap, self.sys_swap:
+            with self.js_ts_lint_swap, self.check_codeowner_swap:
+                pre_commit_linter.main(
+                    args=['--path=%s' % INVALID_PYTHON_FILEPATH])
+        self.assertFalse(all_checks_passed(self.linter_stdout))
+        self.assertTrue(appears_in_linter_stdout(
+            ['C: 24, 0: Missing class docstring (missing-docstring)',
+            'FAILED    Python linting failed'],
+            self.linter_stdout))
