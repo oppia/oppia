bc9ca16d23b046c1c27c67df1408d587e9485fd0
Sagang Wee <9004726+sagangwee@users.noreply.github.com>
2019-10-18 18:02:56 -0700

    Fix part of #6776: Backend changes for listing skill opportunities on the Community Dashboard page. (#7550)
    * Initial changes without tests.
    
    * Added review comments.
    
    * Added var app into proper places.
    
    * Added test for the domain object.
    
    * Undpo unwanted comments.
    
    * Fixed and added new backend tests.
    
    * Fixed test and lint issues.
    
    * Undo package-lock.json changes.
    
    * Address review comments, adds model validation job and fixes minor lint issue.
    
    * Address review comments.
    
    * Fixed username issue.
    
    * Added test and addressed review comments.
    
    * Fix backend issue.
    
    * Add^Crod validator tests.
    
    * Fixes backend test issue.
    
    * Add more tests in opportunity_services.
    
    * Fixes backend test issues.
    
    * Added test for all missing places.
    
    * Fixes minor spell issues.
    
    * Address review comments.
    
    * Fixes backend test issues.
    
    * Fixes test coverage issue.
    
    * Fixes loading issue and address comments.
    
    * Removed MatJax changes.
    
    * Added review changes.
    
    * Fix backend tests.
    
    * Added test for topc and story title.
    
    * Added frontend review changes.
    
    * Addresed required changes.
    
    * Fix lint issues.
    
    * Fix lint issues.
    
    * Fix backend test issues.
    
    * Fix typscript issues.
    
    * Fix typescript issue.
    
    * Adds meta data for dashboard page.
    
    * Add todo message for static serving in future.
    
    * Added SkillOpportunity GAE model and domain classes.
    
    * SkillOpportunity Domain tests
    
    * from and to dict SkillOpportunity domain test
    
    * Set entity key id of SkillOpportunity to skill_id
    
    * Apply stashed changes and resolve merge conflicts
    
    * saving merge resolutions
    
    * Added CRUD logic for SkillOpportunities upon Topic, Skill, Question, QuestionSkillLink changes.
    
    * Removed package-lock.json.3538546349
    
    * Added prod_validation one off job logic
    
    * Refactored test setup duplication in opportunity_services_test.
    
    * Reset COMMUNITY_DASHBOARD_ENABLED flag
    
    * Removed core/templates/dev/head/dist files
    
    * Fixed Python linting errors
    
    * Fix python_utils.BASESTRING errors
    
    * Fixed Python linting issues
    
    * Fixed indentation in core/domain/opportunity_domain_test.py
    
    * Fixed linting issues in core/domain/opportunity_services_test.py
    
    * Addressed first iteration of reviewer comments
    
    * Removed topic information from SkillOpportunity
    
    * Associated SkillOpportunity GAE ID entity key with skill ID
    
    * Fixed import order in topic_services.py
    
    * Removed topic information from test_utils create_skill_opportunity
    
    * Use ndb get_by_id() instead of get() and add defensive checks
    
    * Refactor to operate on domain objects rather than model objects in updates
    
    * Fixed typo in Args description for create_skill_opportunity
    
    * Remove unused imports
    
    * Raise exception when creating skill opportunity for existing opportunity
    
    * Remove spaces for wrong hanging indentation lint
    
    * Remove 3 spaces for wrong hanging indentation lint
    
    * Move add_unpublished_skill test from topic_domain to topic_services.
    
    * Removed unused imports in topic_domain_test.py
    
    * Addressed reviewer comments
    
    * Save skills before creating questions in questions_services_test.py
    
    * Refactored getter methods in question_services to question_fetchers to resolve circular dependency
    
    * Added validation test for question_count
    
    * fixed import order in question_fetchers
    
    * Fixed python linting errors
    
    * Fixed blank line python lint
    
    * Address reviewer comments
    
    * Fix question_fetchers reference in admin_test
    
    * Addressed reviewer comments
    
    * Fixed linting issues
    
    * Addressed last reviewer comments
    
    * Removed unused variable cm
    
    * Fix Python linting errors
    
    * Updated try block fo domain validator check and corresponding domain validator schema test

--- core/domain/prod_validation_jobs_one_off_test.py
+++ core/domain/prod_validation_jobs_one_off_test.py
@@ -2421,6 +2421,134 @@ class ExplorationOpportunitySummaryModelValidatorTests(
         run_job_and_check_output(self, expected_output, sort=True)
 
 
+class SkillOpportunityModelValidatorTests(test_utils.GenericTestBase):
+
+    def setUp(self):
+        super(SkillOpportunityModelValidatorTests, self).setUp()
+
+        self.job_class = (
+            prod_validation_jobs_one_off
+            .SkillOpportunityModelAuditOneOffJob)
+
+        self.signup(self.ADMIN_EMAIL, self.ADMIN_USERNAME)
+        self.signup(self.OWNER_EMAIL, self.OWNER_USERNAME)
+
+        self.admin_id = self.get_user_id_from_email(self.ADMIN_EMAIL)
+        self.owner_id = self.get_user_id_from_email(self.OWNER_EMAIL)
+
+        self.set_admins([self.ADMIN_USERNAME])
+        self.admin = user_services.UserActionsInfo(self.admin_id)
+
+        for i in python_utils.RANGE(3):
+            skill_id = '%s' % i
+            self.save_new_skill(skill_id, self.admin_id, 'description %d' % i)
+            skill_services.publish_skill(skill_id, self.admin_id)
+
+        self.QUESTION_ID = question_services.get_new_question_id()
+        self.save_new_question(
+            self.QUESTION_ID, self.owner_id,
+            self._create_valid_question_data('ABC'), ['0'])
+        question_services.create_new_question_skill_link(
+            self.owner_id, self.QUESTION_ID, '0', 0.3)
+
+        self.model_instance_0 = (
+            opportunity_models.SkillOpportunityModel.get('0'))
+        self.model_instance_1 = (
+            opportunity_models.SkillOpportunityModel.get('1'))
+        self.model_instance_2 = (
+            opportunity_models.SkillOpportunityModel.get('2'))
+
+    def test_standard_operation(self):
+        expected_output = [
+            u'[u\'fully-validated SkillOpportunityModel\', 3]']
+        run_job_and_check_output(self, expected_output)
+
+    def test_model_with_last_updated_greater_than_current_time(self):
+        expected_output = [(
+            u'[u\'failed validation check for current time check of '
+            'SkillOpportunityModel\', '
+            '[u\'Entity id %s: The last_updated field has a '
+            'value %s which is greater than the time when the job was run\', '
+            'u\'Entity id %s: The last_updated field has a '
+            'value %s which is greater than the time when the job was run\', '
+            'u\'Entity id %s: The last_updated field has a '
+            'value %s which is greater than the time when the job was run\']]'
+        ) % (
+            self.model_instance_0.id, self.model_instance_0.last_updated,
+            self.model_instance_1.id, self.model_instance_1.last_updated,
+            self.model_instance_2.id, self.model_instance_2.last_updated)]
+
+        with self.swap(datetime, 'datetime', MockDatetime13Hours), self.swap(
+            db.DateTimeProperty, 'data_type', MockDatetime13Hours):
+            update_datastore_types_for_mock_datetime()
+            run_job_and_check_output(
+                self, expected_output, sort=True, literal_eval=True)
+
+    def test_missing_skill_model_failure(self):
+        skill_model = skill_models.SkillModel.get_by_id('0')
+        skill_model.delete(feconf.SYSTEM_COMMITTER_ID, '', [])
+
+        expected_output = [
+            (
+                u'[u\'failed validation check for skill_ids field '
+                'check of SkillOpportunityModel\', '
+                '[u"Entity id 0: based on field skill_ids having '
+                'value 0, expect model SkillModel with id 0 but it '
+                'doesn\'t exist"]]'
+            ),
+            u'[u\'fully-validated SkillOpportunityModel\', 2]']
+        run_job_and_check_output(self, expected_output, sort=True)
+
+    def test_model_with_invalid_skill_description(self):
+        self.model_instance_0.skill_description = 'invalid'
+        self.model_instance_0.put()
+
+        expected_output = [
+            (
+                u'[u\'failed validation check for skill_description field '
+                'check of SkillOpportunityModel\', '
+                '[u\'Entity id %s: skill_description field in entity: invalid '
+                'does not match corresponding skill description field: '
+                'description 0\']]'
+            ) % self.model_instance_0.id,
+            u'[u\'fully-validated SkillOpportunityModel\', 2]']
+        run_job_and_check_output(self, expected_output, sort=True)
+
+    def test_model_with_invalid_question_count(self):
+        self.model_instance_0.question_count = 10
+        self.model_instance_0.put()
+
+        expected_output = [
+            (
+                u'[u\'failed validation check for question_count check of '
+                'SkillOpportunityModel\', '
+                '[u\'Entity id %s: question_count: 10 does not match the '
+                'question_count of external skill model: 1\']]'
+            ) % self.model_instance_0.id,
+            u'[u\'fully-validated SkillOpportunityModel\', 2]']
+        run_job_and_check_output(self, expected_output, sort=True)
+
+    def test_model_with_invalid_question_count_schema(self):
+        self.model_instance_0.question_count = -1
+        self.model_instance_0.put()
+
+        expected_output = [
+            (
+                u'[u\'failed validation check for domain object check of '
+                'SkillOpportunityModel\', '
+                '[u\'Entity id 0: Entity fails domain validation with the '
+                'error Expected question_count to be a non-negative integer, '
+                'received -1\']]'
+            ),
+            (
+                u'[u\'failed validation check for question_count check of '
+                'SkillOpportunityModel\', '
+                '[u\'Entity id 0: question_count: -1 does not match the '
+                'question_count of external skill model: 1\']]'
+            ), u'[u\'fully-validated SkillOpportunityModel\', 2]']
+        run_job_and_check_output(self, expected_output, sort=True)
+
+
 class ConfigPropertyModelValidatorTests(test_utils.GenericTestBase):
 
     def setUp(self):
