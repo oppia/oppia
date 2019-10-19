6c1c1767b4f52b3437789e004e68cd025811ce92
Jethet <henriette.hettinga@gmail.com>
2019-10-19 12:51:21 +0200

    Changed 'required' line url-interpolation.service.ts

--- core/templates/dev/head/domain/utilities/url-interpolation.service.spec.ts
+++ core/templates/dev/head/domain/utilities/url-interpolation.service.spec.ts
@@ -21,7 +21,7 @@
 import { UpgradedServices } from 'services/UpgradedServices';
 // ^^^ This block is to be removed.
 
-require('domain/utilities/UrlInterpolationService.ts');
+require('domain/utilities/url-interpolation.service.ts');
 
 describe('URL Interpolation Service', function() {
   var hashes = require('hashes.json');
