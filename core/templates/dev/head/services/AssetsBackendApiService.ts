f27be4d4a437a438b4fe94266be3b472ca6dd90f
Kevin Thomas <kevintab@tutanota.com>
2019-10-20 11:31:13 +0530

    remove if block

--- core/templates/dev/head/services/AssetsBackendApiService.ts
+++ core/templates/dev/head/services/AssetsBackendApiService.ts
@@ -79,13 +79,7 @@ angular.module('oppia').factory('AssetsBackendApiService', [
       }).success(function(data) {
         var assetBlob = null;
         try {
-          if (assetType === ASSET_TYPE_AUDIO) {
-            // Add type for audio assets. Without this, translations can
-            // not be played on Safari.
-            assetBlob = new Blob([data], {type: 'audio/mpeg'});
-          } else {
-            assetBlob = new Blob([data], {type: data.type});
-          }
+          assetBlob = new Blob([data], {type: data.type});
         } catch (exception) {
           window.BlobBuilder = window.BlobBuilder ||
                          window.WebKitBlobBuilder ||
