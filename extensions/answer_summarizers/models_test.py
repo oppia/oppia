1a875849c65e713664cb8b519d068d578a0756f6
donosco98 <abyk476@gmail.com>
2019-10-27 17:29:16 +0530

    changed private method calls from test files to public method calls

--- extensions/answer_summarizers/models_test.py
+++ extensions/answer_summarizers/models_test.py
@@ -22,7 +22,6 @@ from core.domain import calculation_registry
 from core.domain import exp_domain
 from core.tests import test_utils
 from extensions.answer_summarizers import models as answer_models
-import utils
 
 
 class BaseCalculationUnitTests(test_utils.GenericTestBase):
@@ -34,9 +33,9 @@ class BaseCalculationUnitTests(test_utils.GenericTestBase):
                 state_answers_dict={})
 
     def test_equality_of_hashable_answers(self):
-        hashable_answer_1 = utils.get_hashable_value('answer_1')
-        hashable_answer_2 = utils.get_hashable_value('answer_2')
-        hashable_answer_3 = utils.get_hashable_value('answer_1')
+        hashable_answer_1 = answer_models.HashableAnswer('answer_1')
+        hashable_answer_2 = answer_models.HashableAnswer('answer_2')
+        hashable_answer_3 = answer_models.HashableAnswer('answer_1')
 
         self.assertFalse(hashable_answer_1 == hashable_answer_2)
         self.assertTrue(hashable_answer_1 == hashable_answer_3)
