2d3d3bf0ee705171fe485bf357a80b44b54aa510
NoumanMasood <noumanmasood64@gmail.com>
2019-10-28 23:07:29 +0500

    Remove custom type definitions for ckeditor

--- typings/custom-ckeditor-defs.d.ts
+++ typings/custom-ckeditor-defs.d.ts
@@ -2,6 +2,15 @@
 // https://github.com/ckeditor/ckeditor-dev/issues/2947 is resolved.
 declare namespace CKEDITOR {
     interface pluginDefinition {
+        hidpi?: boolean;
+        lang?: string | string[];
         icons?: string;
+        requires?: string | string[];
+
+        afterInit?(editor: editor): any;
+        beforeInit?(editor: editor): any;
+        init?(editor: editor): void;
+        isSupportedEnvironment?(editor: editor): Boolean;
+        onLoad?(): any;
     }
 }
