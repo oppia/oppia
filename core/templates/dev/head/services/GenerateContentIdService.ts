de15a6a9a6b99a613fd6b284b5f8cc68c4a6d1c2
Showtim3 <coolrishabhrawat@gmail.com>
2019-10-29 13:03:52 +0530

    removing empty line

--- core/templates/dev/head/services/GenerateContentIdService.ts
+++ core/templates/dev/head/services/GenerateContentIdService.ts
@@ -26,7 +26,6 @@ import { AppConstants } from 'app.constants';
   providedIn: 'root'
 })
 export class GenerateContentIdService {
-
   generateIdForComponent(existingComponentIds: Object,
       componentName: string): string {
     let contentIdList = JSON.parse(JSON.stringify(existingComponentIds));
