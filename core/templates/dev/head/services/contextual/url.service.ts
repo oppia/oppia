24bf0a26092094f72b4d8d2ba3e3a7524a4d6ac6
Showtim3 <coolrishabhrawat@gmail.com>
2019-11-04 18:57:10 +0530

    fixing imports

--- core/templates/dev/head/services/contextual/url.service.ts
+++ core/templates/dev/head/services/contextual/url.service.ts
@@ -20,7 +20,7 @@
 import { downgradeInjectable } from '@angular/upgrade/static';
 import { Injectable } from '@angular/core';
 
-import { WindowRef } from 'services/contextual/WindowRefService';
+import { WindowRef } from 'services/contextual/window-ref.service';
 
 @Injectable({
   providedIn: 'root'
