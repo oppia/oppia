f4d3565e0a6c2a5d0c4b44b31d9be5fdac6d834e
Anshul Hudda <anshulhudda.ssap@gmail.com>
2019-11-05 12:05:00 +0530

    Added lint check for newline above args

--- scripts/pylint_extensions.py
+++ scripts/pylint_extensions.py
@@ -639,7 +639,6 @@ class DocstringParameterChecker(checkers.BaseChecker):
         """Checks whether a class and corresponding  init() method are
         documented. If both of them are documented, it adds an error message.
 
-
         Args:
             class_doc: Docstring. Pylint docstring class instance representing
                 a class's docstring.
@@ -1028,7 +1027,7 @@ class SingleNewlineAboveArgsChecker(checkers.BaseChecker):
     priority = -1
     msgs = {
         'C0012': (
-            'Files must have a single newline above doc string.',
+            'Files must have a single newline above args in doc string.',
             'single-space-above-args',
             'Please enter a single newline above doc string.'
         )
@@ -1046,6 +1045,7 @@ class SingleNewlineAboveArgsChecker(checkers.BaseChecker):
         multi_line_indicator = b'"""'
         file_content = read_from_node(node)
         file_length = len(file_content)
+        blank_line_counter = 0
 
         for line_num in python_utils.RANGE(file_length):
             line = file_content[line_num].strip()
@@ -1067,8 +1067,15 @@ class SingleNewlineAboveArgsChecker(checkers.BaseChecker):
             if in_multi_line_comment:
                 continue
 
-            if line == b'Args:' or line == b'Returns:':
-                if file_content[line_num - 1] != b'\n':
+            if file_content[line_num] == b'\n':
+                blank_line_counter += 1
+            else:
+                blank_line_counter = 0
+
+            if (line_num + 1 < file_length and (
+                    blank_line_counter == 0 or blank_line_counter > 1)):
+                line = file_content[line_num + 1].strip()
+                if line == b'Args:' or line == b'Returns:':
                     self.add_message(
                         'single-space-above-args', line=line_num + 1)
 
