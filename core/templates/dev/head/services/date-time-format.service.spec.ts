e4c1a767249cf0fe067e335fab36f6af8f5088be
Rishabh Rawat <30312043+Showtim3@users.noreply.github.com>
2019-11-05 11:51:11 +0530

    Migrating dateTimeFormatService and format-timer.pipe (#7827)
    * migrating dateTimeFormatService and format-timer.pipe
    
    * migrating DateTimeSpec file
    
    * fixed styling
    
    * fixed order
    
    * merging develop
    
    * fixing transform
    
    * changing timer to time
    
    * moving filter
    
    * fixing types

--- core/templates/dev/head/services/date-time-format.service.spec.ts
+++ core/templates/dev/head/services/date-time-format.service.spec.ts
@@ -16,45 +16,31 @@
  * @fileoverview Unit test for DateTimeFormatService.
  */
 
-// TODO(#7222): Remove the following block of unnnecessary imports once
-// the code corresponding to the spec is upgraded to Angular 8.
-import { UpgradedServices } from 'services/UpgradedServices';
-// ^^^ This block is to be removed.
+import { DateTimeFormatService } from 'services/date-time-format.service';
+import { FormatTimePipe } from 'filters/format-timer.pipe';
 
-require('services/date-time-format.service.ts');
+describe('datetimeformatter', () => {
+  // This corresponds to Fri, 21 Nov 2014 09:45:00 GMT.
+  let NOW_MILLIS = 1416563100000;
+  let df: DateTimeFormatService;
+  let OldDate = Date;
 
-describe('Datetime Formatter', function() {
-  beforeEach(angular.mock.module('oppia'));
-  beforeEach(angular.mock.module('oppia', function($provide) {
-    var ugs = new UpgradedServices();
-    for (let [key, value] of Object.entries(ugs.upgradedServices)) {
-      $provide.value(key, value);
-    }
-  }));
+  beforeEach(() => {
+    df = new DateTimeFormatService(new FormatTimePipe());
 
-  describe('datetimeformatter', function() {
-    // This corresponds to Fri, 21 Nov 2014 09:45:00 GMT.
-    var NOW_MILLIS = 1416563100000;
-    var df = null;
-    var OldDate = Date;
-
-    beforeEach(angular.mock.inject(function($injector) {
-      df = $injector.get('DateTimeFormatService');
-
-      // Mock Date() to give a time of NOW_MILLIS in GMT. (Unfortunately, there
-      // doesn't seem to be a good way to set the timezone locale directly.)
-      spyOn(window, 'Date').and.callFake(function() {
-        return new OldDate(NOW_MILLIS);
-      });
-    }));
-
-    it('should correctly indicate recency', function() {
-      // 1 second ago is recent.
-      expect(df.isRecent(NOW_MILLIS - 1)).toBe(true);
-      // 72 hours ago is recent.
-      expect(df.isRecent(NOW_MILLIS - 72 * 60 * 60 * 1000)).toBe(true);
-      // 8 days ago is not recent.
-      expect(df.isRecent(NOW_MILLIS - 8 * 24 * 60 * 60 * 1000)).toBe(false);
+    // Mock Date() to give a time of NOW_MILLIS in GMT. (Unfortunately, there
+    // doesn't seem to be a good way to set the timezone locale directly.)
+    spyOn(window, 'Date').and.callFake(function() {
+      return new OldDate(NOW_MILLIS);
     });
+  });
+
+  it('should correctly indicate recency', () => {
+    // 1 second ago is recent.
+    expect(df.isRecent(NOW_MILLIS - 1)).toBe(true);
+    // 72 hours ago is recent.
+    expect(df.isRecent(NOW_MILLIS - 72 * 60 * 60 * 1000)).toBe(true);
+    // 8 days ago is not recent.
+    expect(df.isRecent(NOW_MILLIS - 8 * 24 * 60 * 60 * 1000)).toBe(false);
   });
 });
