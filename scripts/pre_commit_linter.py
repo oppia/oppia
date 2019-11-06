ba01b27abf981608bf70180bd3e1365984cce91c
Kevin Lee <kevinlee963@gmail.com>
2019-11-05 19:44:56 -0800

    Revert conversion

--- scripts/pre_commit_linter.py
+++ scripts/pre_commit_linter.py
@@ -886,7 +886,7 @@ def _is_path_ignored(path_to_check):
 
     # The "git check-ignore <path>" command returns 0 when the path is ignored
     # otherwise it returns 1. subprocess.call then returns this returncode.
-    if subprocess.check_call(command):
+    if subprocess.call(command):
         return False
     else:
         return True
