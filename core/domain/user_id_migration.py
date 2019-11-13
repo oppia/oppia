3bc365fcbcb6eae72b55a60822cb56e1a9c5dbf6
Vojtěch Jelínek <vojtin.j@gmail.com>
2019-11-13 14:09:34 +0100

    Add basic user id migration

---
+++ core/domain/user_id_migration.py
@@ -0,0 +1,192 @@
+# Copyright 2019 The Oppia Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Jobs for queries personalized to individual users."""
+from __future__ import absolute_import  # pylint: disable=import-only-modules
+from __future__ import unicode_literals  # pylint: disable=import-only-modules
+
+from core import jobs
+from core.platform import models
+
+(
+    audit_models, collection_models, config_models,
+    email_models, exp_models, feedback_models,
+    file_models, question_models, skill_models,
+    story_models, suggestion_models, topic_models,
+    user_models) = (
+        models.Registry.import_models([
+            models.NAMES.audit, models.NAMES.collection, models.NAMES.config,
+            models.NAMES.email, models.NAMES.exploration, models.NAMES.feedback,
+            models.NAMES.file, models.NAMES.question, models.NAMES.skill,
+            models.NAMES.story, models.NAMES.suggestion, models.NAMES.topic,
+            models.NAMES.user]))
+datastore_services = models.Registry.import_datastore_services()
+
+
+class UserIdJob(jobs.BaseMapReduceOneOffJobManager):
+    """One-off job for creating new user ids for all the users and re-adding
+    models that use the user id.
+    """
+
+    # Models that use user_id as id and thus need to be recreated.
+    MODEL_TYPES_TO_BE_COPIED = (
+        user_models.UserSettingsModel,
+        user_models.CompletedActivitiesModel,
+        user_models.IncompleteActivitiesModel,
+        user_models.LearnerPlaylistModel,
+        user_models.UserContributionsModel,
+        user_models.UserEmailPreferencesModel,
+        user_models.UserSubscriptionsModel,
+        user_models.UserSubscribersModel,
+        user_models.UserRecentChangesBatchModel,
+        user_models.UserStatsModel,
+        user_models.UserBulkEmailsModel,
+        user_models.UserRecentChangesBatchModel,
+        feedback_models.UnsentFeedbackEmailModel)
+
+    # Models that use user_id as part of an id and thus need to be recreated.
+    MODEL_TYPES_TO_BE_COPIED_PART_ID = (
+        user_models.ExpUserLastPlaythroughModel,
+        user_models.ExplorationUserDataModel,
+        user_models.CollectionProgressModel,
+        user_models.StoryProgressModel,
+        user_models.UserSkillMasteryModel)
+
+    # Models that have one field with user id.
+    MODEL_TYPES_ONE_USER_ID_FIELD = {
+        'author_id': (
+            feedback_models.GeneralFeedbackMessageModel,
+            suggestion_models.GeneralSuggestionModel,
+        ),
+        'committer_id': (
+            collection_models.CollectionSnapshotMetadataModel,
+            collection_models.CollectionRightsSnapshotMetadataModel,
+            config_models.ConfigPropertySnapshotMetadataModel,
+            exp_models.ExplorationSnapshotMetadataModel,
+            exp_models.ExplorationRightsSnapshotMetadataModel,
+            file_models.FileMetadataSnapshotMetadataModel,
+            file_models.FileSnapshotMetadataModel,
+            question_models.QuestionSnapshotMetadataModel,
+            question_models.QuestionRightsSnapshotMetadataModel,
+            skill_models.SkillSnapshotMetadataModel,
+            skill_models.SkillRightsSnapshotMetadataModel,
+            story_models.StorySnapshotMetadataModel,
+            topic_models.TopicSnapshotMetadataModel,
+            topic_models.SubtopicPageSnapshotMetadataModel,
+        ),
+        'creator_id': (
+            question_models.QuestionSummaryModel,
+            question_models.QuestionRightsModel,
+            skill_models.SkillRightsModel,
+        ),
+        'final_reviewer_id': (suggestion_models.GeneralSuggestionModel,),
+        'original_author_id': (feedback_models.GeneralFeedbackThreadModel,),
+        'recipient_id': (email_models.SentEmailModel,),
+        'sender_id': (email_models.SentEmailModel, email_models.BulkEmailModel),
+        'submitter_id': (user_models.UserQueryModel,),
+        'user_id': (
+            audit_models.RoleQueryAuditModel,
+            collection_models.CollectionCommitLogEntryModel,
+            email_models.GeneralFeedbackEmailReplyToIdModel,
+            exp_models.ExplorationCommitLogEntryModel,
+            feedback_models.GeneralFeedbackThreadUserModel,
+            question_models.QuestionCommitLogEntryModel,
+            skill_models.SkillCommitLogEntryModel,
+            story_models.StoryCommitLogEntryModel,
+            topic_models.TopicCommitLogEntryModel,
+            topic_models.SubtopicPageCommitLogEntryModel,
+        ),
+    }
+
+    @staticmethod
+    def _copy_model_with_new_id(model_class, old_user_id, new_user_id):
+        """Create new model with same values but new id."""
+        old_model = model_class.get_by_id(old_user_id)
+        if not old_model:
+            return
+        model_values = old_model.to_dict()
+        model_values['id'] = new_user_id
+        model_class(**model_values).put(update_last_updated_time=False)
+        old_model.delete()
+
+    @staticmethod
+    def _copy_model_with_new_id_and_user_id(
+            model_class, old_user_id, new_user_id):
+        """Create new model with same values but new id and user_id."""
+        old_models = model_class.query(
+            model_class.user_id == old_user_id).fetch()
+        for old_model in old_models:
+            model_values = old_model.to_dict()
+            new_id = '%s.%s' % (new_user_id, old_model.id.split('.')[1])
+            model_values['id'] = new_id
+            model_values['user_id'] = new_user_id
+            model_class(**model_values).put(update_last_updated_time=False)
+            old_model.delete()
+
+    @staticmethod
+    def _change_model_with_one_user_id_field(
+            model_class, old_user_id, new_user_id, field_name):
+        """Replace field in model with new user id."""
+        found_models = model_class.query(
+            getattr(model_class, field_name) == old_user_id).fetch()
+        for model in found_models:
+            model_values = model.to_dict()
+            model_values[field_name] = new_user_id
+            model.populate(**model_values)
+            model.put(update_last_updated_time=False)
+
+    @staticmethod
+    def _change_user_contribution_scoring_model(old_user_id, new_user_id):
+        """Create new model with same values but new id and user_id."""
+        old_models = user_models.UserContributionScoringModel.query(
+            user_models.UserContributionScoringModel.user_id == old_user_id
+        ).fetch()
+        for old_model in old_models:
+            model_values = old_model.to_dict()
+            new_id = '%s.%s' % (old_model.id.split('.')[0], new_user_id)
+            model_values['id'] = new_id
+            model_values['user_id'] = new_user_id
+            user_models.UserContributionScoringModel(
+                **model_values).put(update_last_updated_time=False)
+            old_model.delete()
+
+    @classmethod
+    def entity_classes_to_map_over(cls):
+        """Return a list of datastore class references to map over."""
+        return [user_models.UserSettingsModel]
+
+    @staticmethod
+    def map(user_model):
+        """Implements the map function for this job."""
+        old_user_id = user_model.id
+        new_user_id = user_models.UserSettingsModel.get_new_id('user')
+        for model_type in UserIdJob.MODEL_TYPES_TO_BE_COPIED:
+            UserIdJob._copy_model_with_new_id(
+                model_type, old_user_id, new_user_id)
+        for model_type in UserIdJob.MODEL_TYPES_TO_BE_COPIED_PART_ID:
+            UserIdJob._copy_model_with_new_id_and_user_id(
+                model_type, old_user_id, new_user_id)
+        for field_name, model_types in UserIdJob.MODEL_TYPES_ONE_USER_ID_FIELD:
+            for model_type in model_types:
+                UserIdJob._change_model_with_one_user_id_field(
+                    model_type, old_user_id, new_user_id, field_name)
+        UserIdJob._change_user_contribution_scoring_model(
+            old_user_id, new_user_id)
+
+        yield ('SUCCESS', new_user_id)
+
+    @staticmethod
+    def reduce(key, new_user_ids):
+        """Implements the reduce function for this job."""
+        yield (key, new_user_ids)
---
+++ core/domain/user_id_migration_test.py
@@ -0,0 +1,159 @@
+# coding: utf-8
+#
+# Copyright 2019 The Oppia Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for user-related one-off computations."""
+from __future__ import absolute_import  # pylint: disable=import-only-modules
+from __future__ import unicode_literals  # pylint: disable=import-only-modules
+
+import ast
+
+from core.domain import user_id_migration
+from core.platform import models
+from core.tests import test_utils
+
+(user_models, feedback_models, exp_models) = models.Registry.import_models(
+    [models.NAMES.user, models.NAMES.feedback, models.NAMES.exploration])
+taskqueue_services = models.Registry.import_taskqueue_services()
+search_services = models.Registry.import_search_services()
+
+
+class UserIdJobTests(test_utils.GenericTestBase):
+    """Tests for the one-off dashboard subscriptions job."""
+    EXP_ID_1 = 'exp_id_1'
+    EXP_ID_2 = 'exp_id_2'
+    USER_A_EMAIL = 'a@example.com'
+    USER_A_USERNAME = 'a'
+    USER_B_EMAIL = 'b@example.com'
+    USER_B_USERNAME = 'b'
+    USER_C_EMAIL = 'c@example.com'
+    USER_C_USERNAME = 'c'
+    USER_D_EMAIL = 'd@example.com'
+    USER_D_USERNAME = 'd'
+
+    def _run_one_off_job(self):
+        """Runs the one-off MapReduce job."""
+        job_id = user_id_migration.UserIdJob.create_new()
+        user_id_migration.UserIdJob.enqueue(job_id)
+        self.assertEqual(
+            self.count_jobs_in_taskqueue(
+                taskqueue_services.QUEUE_NAME_ONE_OFF_JOBS), 1)
+        self.process_and_flush_pending_tasks()
+        stringified_output = (
+            user_id_migration.UserIdJob.get_output(job_id))
+        eval_output = [ast.literal_eval(stringified_item)
+                       for stringified_item in stringified_output]
+        return eval_output
+
+    def setUp(self):
+        def empty(*_):
+            """Function that takes any number of arguments and does nothing."""
+            pass
+
+        # We don't want to signup the superadmin user.
+        with self.swap(test_utils.TestBase, 'signup_superadmin_user', empty):
+            super(UserIdJobTests, self).setUp()
+        self.signup(self.USER_A_EMAIL, self.USER_A_USERNAME)
+        self.user_a_id = self.get_user_id_from_email(self.USER_A_EMAIL)
+
+    def test_one_user_one_model_full_id(self):
+        original_model = user_models.CompletedActivitiesModel(
+            id=self.user_a_id,
+            exploration_ids=['1', '2'],
+            collection_ids=['1', '2'])
+        original_model.put()
+
+        output = self._run_one_off_job()
+
+        migrated_model = (
+            user_models.CompletedActivitiesModel.get_by_id(output[0][1][0]))
+        self.assertNotEqual(
+            original_model.id, migrated_model.id)
+        self.assertEqual(
+            original_model.exploration_ids, migrated_model.exploration_ids)
+        self.assertEqual(
+            original_model.collection_ids, migrated_model.collection_ids)
+        self.assertEqual(
+            original_model.created_on, migrated_model.created_on)
+        self.assertEqual(
+            original_model.last_updated, migrated_model.last_updated)
+
+        self.assertIsNone(
+            user_models.CompletedActivitiesModel.get_by_id(self.user_a_id))
+
+    def test_one_user_one_model_part_id(self):
+        original_model = user_models.ExpUserLastPlaythroughModel(
+            id='%s.%s' % (self.user_a_id, 'exp_id'),
+            user_id=self.user_a_id,
+            exploration_id='exp_id',
+            last_played_exp_version=2,
+            last_played_state_name='start')
+        original_model.put()
+
+        output = self._run_one_off_job()
+
+        migrated_model = (
+            user_models.ExpUserLastPlaythroughModel.get_by_id(
+                '%s.%s' % (output[0][1][0], 'exp_id')))
+        self.assertNotEqual(
+            original_model.id, migrated_model.id)
+        self.assertNotEqual(
+            original_model.user_id, migrated_model.user_id)
+        self.assertEqual(
+            original_model.exploration_id, migrated_model.exploration_id)
+        self.assertEqual(
+            original_model.last_played_exp_version,
+            migrated_model.last_played_exp_version)
+        self.assertEqual(
+            original_model.last_played_state_name,
+            migrated_model.last_played_state_name)
+        self.assertEqual(
+            original_model.created_on, migrated_model.created_on)
+        self.assertEqual(
+            original_model.last_updated, migrated_model.last_updated)
+
+        self.assertIsNone(
+            user_models.CompletedActivitiesModel.get_by_id(self.user_a_id))
+
+    def test_one_user_one_model_user_id_field(self):
+        original_model = exp_models.ExplorationSnapshotMetadataModel(
+            id='instance_id',
+            committer_id=self.user_a_id,
+            commit_type='create',
+            commit_message='commit message 2',
+            commit_cmds=[{'cmd': 'some_command'}])
+        original_model.put()
+
+        output = self._run_one_off_job()
+
+        migrated_model = (
+            exp_models.ExplorationSnapshotMetadataModel.query(
+                exp_models.ExplorationSnapshotMetadataModel.committer_id ==
+                output[0][1][0]
+            ).get())
+
+        self.assertNotEqual(
+            original_model.committer_id, migrated_model.committer_id)
+        self.assertEqual(original_model.id, migrated_model.id)
+        self.assertEqual(
+            original_model.commit_type, migrated_model.commit_type)
+        self.assertEqual(
+            original_model.commit_message, migrated_model.commit_message)
+        self.assertEqual(
+            original_model.commit_cmds, migrated_model.commit_cmds)
+        self.assertEqual(
+            original_model.created_on, migrated_model.created_on)
+        self.assertEqual(
+            original_model.last_updated, migrated_model.last_updated)
