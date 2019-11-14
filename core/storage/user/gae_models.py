a8369897edfcb1e709c02508b6e59e03ac62a938
Vojtěch Jelínek <vojtin.j@gmail.com>
2019-11-14 10:18:41 +0100

    Rename gae_user_id to gae_id (#7998)

--- core/storage/user/gae_models.py
+++ core/storage/user/gae_models.py
@@ -36,7 +36,7 @@ class UserSettingsModel(base_models.BaseModel):
     # User id used to identify user by GAE. Is not required for now because we
     # need to perform migration to fill this for existing users.
     # TODO(#7659): Set required to True.
-    gae_user_id = ndb.StringProperty(required=False, indexed=True)
+    gae_id = ndb.StringProperty(required=False, indexed=True)
     # Email address of the user.
     email = ndb.StringProperty(required=True, indexed=True)
     # User role. Required for authorization. User gets a default role of
