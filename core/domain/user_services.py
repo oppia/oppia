8527ae4623b2a74f9f73659d1bae7f17689e7201
Vojtěch Jelínek <vojtin.j@gmail.com>
2019-11-17 22:12:48 +0100

    Revert changes to gae_id to do them in separate PR

--- core/domain/user_services.py
+++ core/domain/user_services.py
@@ -399,7 +399,7 @@ def get_users_settings(user_ids):
         if user_ids[i] == feconf.SYSTEM_COMMITTER_ID:
             result.append(UserSettings(
                 user_id=feconf.SYSTEM_COMMITTER_ID,
-                gae_id=feconf.SYSTEM_COMMITTER_ID,
+                gae_id=None,
                 email=feconf.SYSTEM_EMAIL_ADDRESS,
                 role=feconf.ROLE_ID_ADMIN,
                 username='admin',
