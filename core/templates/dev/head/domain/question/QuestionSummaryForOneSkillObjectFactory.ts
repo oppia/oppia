964a9da3d7b08375cf9929dc3125e31a198a3a3a
Akshay Anand <akshayanand681@gmail.com>
2019-11-20 16:32:18 +0530

    modified comment

--- core/templates/dev/head/domain/question/QuestionSummaryForOneSkillObjectFactory.ts
+++ core/templates/dev/head/domain/question/QuestionSummaryForOneSkillObjectFactory.ts
@@ -14,7 +14,10 @@
 
 /**
  * @fileoverview Object factory for creating frontend instances of
- * question summary for one skill objects.
+ * question summary for one skill objects. This differs from
+ * QuestionSummaryObjectFactory in that, this contains QuestionSummary and the
+ * data for the relevant linked skill (description, difficulty and id), whereas
+ * QuestionSummary just contains the question data.
  */
 
 import { Injectable } from '@angular/core';
