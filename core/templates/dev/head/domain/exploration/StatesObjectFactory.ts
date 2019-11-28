b489f576cd77150d1dd8fce51d36a99f0df08b1d
Nitish Bansal <nitishbansal2297@gmail.com>
2019-11-28 23:09:00 +0530

    Upgrades StateObjectFactory and StatesObjectFactory to Angular (#8093)
    * Upgrades StateObjectFactory and StatesObjectFactory to Angular
    
    * Fixes constants

--- core/templates/dev/head/domain/exploration/StatesObjectFactory.ts
+++ core/templates/dev/head/domain/exploration/StatesObjectFactory.ts
@@ -16,135 +16,148 @@
  * @fileoverview Factory for creating new frontend instances of State
  * domain objects given a list of backend state dictionaries.
  */
+import { downgradeInjectable } from '@angular/upgrade/static';
+import { Injectable } from '@angular/core';
 
-require('domain/state/StateObjectFactory.ts');
+import { StateObjectFactory } from 'domain/state/StateObjectFactory';
 
-angular.module('oppia').factory('StatesObjectFactory', [
-  'StateObjectFactory', 'INTERACTION_SPECS',
-  function(StateObjectFactory, INTERACTION_SPECS) {
-    var States = function(states) {
-      this._states = states;
-    };
-    States.prototype.getState = function(stateName) {
-      return angular.copy(this._states[stateName]);
-    };
+const INTERACTION_SPECS = require('interactions/interaction_specs.json');
 
-    // TODO(tjiang11): Remove getStateObjects() and replace calls
-    // with an object to represent data to be manipulated inside
-    // ExplorationDiffService.
-
-    States.prototype.getStateObjects = function() {
-      return angular.copy(this._states);
-    };
-    States.prototype.addState = function(newStateName) {
-      this._states[newStateName] = StateObjectFactory.createDefaultState(
-        newStateName);
-    };
-    States.prototype.setState = function(stateName, stateData) {
-      this._states[stateName] = angular.copy(stateData);
-    };
-    States.prototype.hasState = function(stateName) {
-      return this._states.hasOwnProperty(stateName);
-    };
-    States.prototype.deleteState = function(deleteStateName) {
-      delete this._states[deleteStateName];
-      for (var otherStateName in this._states) {
-        var interaction = this._states[otherStateName].interaction;
-        var groups = interaction.answerGroups;
-        for (var i = 0; i < groups.length; i++) {
-          if (groups[i].outcome.dest === deleteStateName) {
-            groups[i].outcome.dest = otherStateName;
-          }
-        }
-        if (interaction.defaultOutcome) {
-          if (interaction.defaultOutcome.dest === deleteStateName) {
-            interaction.defaultOutcome.dest = otherStateName;
-          }
-        }
-      }
-    };
-    States.prototype.renameState = function(oldStateName, newStateName) {
-      this._states[newStateName] = angular.copy(this._states[oldStateName]);
-      this._states[newStateName].setName(newStateName);
-      delete this._states[oldStateName];
-
-      for (var otherStateName in this._states) {
-        var interaction = this._states[otherStateName].interaction;
-        var groups = interaction.answerGroups;
-        for (var i = 0; i < groups.length; i++) {
-          if (groups[i].outcome.dest === oldStateName) {
-            groups[i].outcome.dest = newStateName;
-          }
-        }
-        if (interaction.defaultOutcome) {
-          if (interaction.defaultOutcome.dest === oldStateName) {
-            interaction.defaultOutcome.dest = newStateName;
-          }
-        }
-      }
-    };
-    States.prototype.getStateNames = function() {
-      return Object.keys(this._states);
-    };
-    States.prototype.getFinalStateNames = function() {
-      var finalStateNames = [];
-      for (var stateName in this._states) {
-        var interaction = this._states[stateName].interaction;
-        if (interaction.id && INTERACTION_SPECS[interaction.id].is_terminal) {
-          finalStateNames.push(stateName);
-        }
-      }
-      return finalStateNames;
-    };
-
-    States.prototype.getAllVoiceoverLanguageCodes = function() {
-      var allAudioLanguageCodes = [];
-      for (var stateName in this._states) {
-        var state = this._states[stateName];
-        var contentIdsList = state.recordedVoiceovers.getAllContentId();
-        contentIdsList.forEach(function(contentId) {
-          var audioLanguageCodes = (
-            state.recordedVoiceovers.getVoiceoverLanguageCodes(contentId));
-          audioLanguageCodes.forEach(function(languageCode) {
-            if (allAudioLanguageCodes.indexOf(languageCode) === -1) {
-              allAudioLanguageCodes.push(languageCode);
-            }
-          });
-        });
-      }
-      return allAudioLanguageCodes;
-    };
-
-    States.prototype.getAllVoiceovers = function(languageCode) {
-      var allAudioTranslations = {};
-      for (var stateName in this._states) {
-        var state = this._states[stateName];
-        allAudioTranslations[stateName] = [];
-        var contentIdsList = state.recordedVoiceovers.getAllContentId();
-        contentIdsList.forEach(function(contentId) {
-          var audioTranslations = (
-            state.recordedVoiceovers.getBindableVoiceovers(contentId));
-          if (audioTranslations.hasOwnProperty(languageCode)) {
-            allAudioTranslations[stateName].push(
-              audioTranslations[languageCode]);
-          }
-        });
-      }
-      return allAudioTranslations;
-    };
-
-    // TODO(ankita240796): Remove the bracket notation once Angular2 gets in.
-    /* eslint-disable dot-notation */
-    States['createFromBackendDict'] = function(statesBackendDict) {
-    /* eslint-enable dot-notation */
-      var stateObjectsDict = {};
-      for (var stateName in statesBackendDict) {
-        stateObjectsDict[stateName] = StateObjectFactory.createFromBackendDict(
-          stateName, statesBackendDict[stateName]);
-      }
-      return new States(stateObjectsDict);
-    };
-
-    return States;
+export class States {
+  _stateObject;
+  _states;
+  constructor(stateObject: StateObjectFactory, states: any) {
+    this._stateObject = stateObject;
+    this._states = states;
   }
-]);
+
+  getState(stateName: string): any {
+    return this._states[stateName];
+  }
+
+  // TODO(tjiang11): Remove getStateObjects() and replace calls
+  // with an object to represent data to be manipulated inside
+  // ExplorationDiffService.
+
+  getStateObjects(): any {
+    return this._states;
+  }
+  addState(newStateName: string): void {
+    this._states[newStateName] = this._stateObject.createDefaultState(
+      newStateName);
+  }
+  setState(stateName: string, stateData: any): void {
+    this._states[stateName] = Object.assign({}, stateData);
+  }
+  hasState(stateName: string): boolean {
+    return this._states.hasOwnProperty(stateName);
+  }
+  deleteState(deleteStateName: string): void {
+    delete this._states[deleteStateName];
+    for (var otherStateName in this._states) {
+      var interaction = this._states[otherStateName].interaction;
+      var groups = interaction.answerGroups;
+      for (var i = 0; i < groups.length; i++) {
+        if (groups[i].outcome.dest === deleteStateName) {
+          groups[i].outcome.dest = otherStateName;
+        }
+      }
+      if (interaction.defaultOutcome) {
+        if (interaction.defaultOutcome.dest === deleteStateName) {
+          interaction.defaultOutcome.dest = otherStateName;
+        }
+      }
+    }
+  }
+  renameState(oldStateName: string, newStateName: string): void {
+    this._states[newStateName] = this._states[oldStateName];
+    this._states[newStateName].setName(newStateName);
+    delete this._states[oldStateName];
+
+    for (var otherStateName in this._states) {
+      var interaction = this._states[otherStateName].interaction;
+      var groups = interaction.answerGroups;
+      for (var i = 0; i < groups.length; i++) {
+        if (groups[i].outcome.dest === oldStateName) {
+          groups[i].outcome.dest = newStateName;
+        }
+      }
+      if (interaction.defaultOutcome) {
+        if (interaction.defaultOutcome.dest === oldStateName) {
+          interaction.defaultOutcome.dest = newStateName;
+        }
+      }
+    }
+  }
+  getStateNames(): Array<string> {
+    return Object.keys(this._states);
+  }
+  getFinalStateNames(): Array<string> {
+    var finalStateNames = [];
+    for (var stateName in this._states) {
+      var interaction = this._states[stateName].interaction;
+      if (interaction.id && INTERACTION_SPECS[interaction.id].is_terminal) {
+        finalStateNames.push(stateName);
+      }
+    }
+    return finalStateNames;
+  }
+
+  getAllVoiceoverLanguageCodes(): Array<string> {
+    var allAudioLanguageCodes = [];
+    for (var stateName in this._states) {
+      var state = this._states[stateName];
+      var contentIdsList = state.recordedVoiceovers.getAllContentId();
+      contentIdsList.forEach(function(contentId) {
+        var audioLanguageCodes = (
+          state.recordedVoiceovers.getVoiceoverLanguageCodes(contentId));
+        audioLanguageCodes.forEach(function(languageCode) {
+          if (allAudioLanguageCodes.indexOf(languageCode) === -1) {
+            allAudioLanguageCodes.push(languageCode);
+          }
+        });
+      });
+    }
+    return allAudioLanguageCodes;
+  }
+
+  getAllVoiceovers(languageCode: string): any {
+    var allAudioTranslations = {};
+    for (var stateName in this._states) {
+      var state = this._states[stateName];
+      allAudioTranslations[stateName] = [];
+      var contentIdsList = state.recordedVoiceovers.getAllContentId();
+      contentIdsList.forEach(function(contentId) {
+        var audioTranslations = (
+          state.recordedVoiceovers.getBindableVoiceovers(contentId));
+        if (audioTranslations.hasOwnProperty(languageCode)) {
+          allAudioTranslations[stateName].push(
+            audioTranslations[languageCode]);
+        }
+      });
+    }
+    return allAudioTranslations;
+  }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StatesObjectFactory {
+  constructor(private stateObject: StateObjectFactory) {}
+  // TODO(ankita240796): Remove the bracket notation once Angular2 gets in.
+  /* eslint-disable dot-notation */
+  createFromBackendDict(statesBackendDict: any): States {
+  /* eslint-enable dot-notation */
+    var stateObjectsDict = {};
+    for (var stateName in statesBackendDict) {
+      stateObjectsDict[stateName] = this.stateObject.createFromBackendDict(
+        stateName, statesBackendDict[stateName]);
+    }
+    return new States(this.stateObject, stateObjectsDict);
+  }
+}
+
+angular.module('oppia').factory(
+  'StatesObjectFactory',
+  downgradeInjectable(StatesObjectFactory));
