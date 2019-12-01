dcad3b03aa12e11816e1fb283e0fd21998678810
Brian Rodriguez <brian@brianrodri.com>
2019-12-01 03:47:45 -0500

    rename class and tests

--- core/domain/feedback_jobs_one_off_test.py
+++ core/domain/feedback_jobs_one_off_test.py
@@ -131,13 +131,14 @@ class GeneralFeedbackThreadUserOneOffJobTest(test_utils.GenericTestBase):
         self._check_model_validity(user_id2, thread_id2, user_feedback_model2)
 
 
-class GeneralFeedbackThreadOneOffJobTest(test_utils.GenericTestBase):
+class FeedbackThreadMessageCacheOneOffJobTest(test_utils.GenericTestBase):
     """Tests for GeneralFeedbackThreadUser migration."""
 
-    ONE_OFF_JOB_CLASS = feedback_jobs_one_off.GeneralFeedbackThreadOneOffJob
+    ONE_OFF_JOB_CLASS = (
+        feedback_jobs_one_off.FeedbackThreadMessageCacheOneOffJob)
 
     def setUp(self):
-        super(GeneralFeedbackThreadOneOffJobTest, self).setUp()
+        super(FeedbackThreadMessageCacheOneOffJobTest, self).setUp()
 
         self.signup(self.EDITOR_EMAIL, self.EDITOR_USERNAME)
         self.editor_id = self.get_user_id_from_email(self.EDITOR_EMAIL)
