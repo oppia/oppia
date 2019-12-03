f346ec088178d7a574174a41f82a5bcdd1b230c5
nadintamer <nadin.tamer@gmail.com>
2019-12-03 13:45:44 -0800

    Put imports in alphabetical order

--- core/domain/skill_services.py
+++ core/domain/skill_services.py
@@ -23,8 +23,8 @@ from core.domain import email_manager
 from core.domain import opportunity_services
 from core.domain import role_services
 from core.domain import skill_domain
-from core.domain import user_services
 from core.domain import state_domain
+from core.domain import user_services
 from core.platform import models
 import feconf
 import python_utils
