a049a1367398ebefa97c7e3d0fbc301e1352aee1
Nishant Mittal <mittalnishant14@outlook.com>
2019-12-04 01:12:24 +0530

    Fix lint tests

--- core/templates/dev/head/domain/subtopic_viewer/SubtopicDataObjectFactory.ts
+++ core/templates/dev/head/domain/subtopic_viewer/SubtopicDataObjectFactory.ts
@@ -17,26 +17,26 @@
  * subtopic data domain objects.
  */
 
-angular.module('oppia').factory('SubtopicDataObjectFactory', [function () {
-  var SubtopicData = function (
-    subtopic_title, page_contents) {
-    this._subtopic_title = subtopic_title;
-    this._page_contents = page_contents;
+angular.module('oppia').factory('SubtopicDataObjectFactory', [function() {
+  var SubtopicData = function(
+      subtopicTitle, pageContents) {
+    this._subtopic_title = subtopicTitle;
+    this._page_contents = pageContents;
   };
 
   // Instance methods
 
-  SubtopicData.prototype.getSubtopicTitle = function () {
+  SubtopicData.prototype.getSubtopicTitle = function() {
     return this._subtopic_title;
   };
 
-  SubtopicData.prototype.getPageContents = function () {
+  SubtopicData.prototype.getPageContents = function() {
     return this._page_contents;
   };
 
   // TODO(ankita240796): Remove the bracket notation once Angular2 gets in.
   /* eslint-disable dot-notation */
-  SubtopicData['createFromBackendDict'] = function (subtopicDataBackendDict) {
+  SubtopicData['createFromBackendDict'] = function(subtopicDataBackendDict) {
     /* eslint-enable dot-notation */
     return new SubtopicData(
       subtopicDataBackendDict.subtopic_title,
