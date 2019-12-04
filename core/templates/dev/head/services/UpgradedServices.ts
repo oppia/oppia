16882fba31f97f4f77c67de581d5566116601a20
Akshay Anand <akshayanand681@gmail.com>
2019-12-05 04:15:46 +0530

    fixed test

--- core/templates/dev/head/services/UpgradedServices.ts
+++ core/templates/dev/head/services/UpgradedServices.ts
@@ -48,6 +48,8 @@ import { DocumentAttributeCustomizationService } from
 import { EditabilityService } from 'services/editability.service';
 import { EditorFirstTimeEventsService } from
   'pages/exploration-editor-page/services/editor-first-time-events.service';
+import { EntityContextObjectFactory } from
+   'domain/utilities/EntityContextObjectFactory';
 import { ExplorationDiffService } from
   'pages/exploration-editor-page/services/exploration-diff.service';
 import { ExplorationDraftObjectFactory } from
@@ -172,6 +174,8 @@ export class UpgradedServices {
     upgradedServices['DateTimeFormatService'] = new DateTimeFormatService();
     upgradedServices['DebouncerService'] = new DebouncerService();
     upgradedServices['EditabilityService'] = new EditabilityService();
+    upgradedServices['EntityContextObjectFactory'] =
+      new EntityContextObjectFactory();
     upgradedServices['ExplorationDiffService'] = new ExplorationDiffService();
     upgradedServices['ExplorationDraftObjectFactory'] =
       new ExplorationDraftObjectFactory();
