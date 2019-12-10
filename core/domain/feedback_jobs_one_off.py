885d81c3e48e5f861d098219d2c7eff244da5d13
Brian Rodriguez <brian@brianrodri.com>
2019-12-09 21:28:50 -0500

    refer to models

--- core/domain/feedback_jobs_one_off.py
+++ core/domain/feedback_jobs_one_off.py
@@ -14,7 +14,7 @@
 
 """One-off jobs for feedback models."""
 from __future__ import absolute_import # pylint: disable=import-only-modules
-from __future__ import unicode_literals  # pylint: disable=import-only-modules
+from __future__ import unicode_literals # pylint: disable=import-only-modules
 
 from core import jobs
 from core.domain import feedback_services
@@ -29,21 +29,22 @@ class FeedbackThreadCacheOneOffJob(jobs.BaseMapReduceOneOffJobManager):
 
     @classmethod
     def entity_classes_to_map_over(cls):
-        """Return a list of datastore class references to map over."""
+        """The list of datastore classes to map over."""
         return [feedback_models.GeneralFeedbackThreadModel]
 
     @staticmethod
-    def map(thread):
+    def map(thread_model):
         """Implements the map function for this job."""
-        last_message = feedback_models.GeneralFeedbackMessageModel.get_by_id(
-            feedback_services.get_full_message_id(
-                thread.id, thread.message_count - 1))
+        last_message_model = (
+            feedback_models.GeneralFeedbackMessageModel.get_by_id(
+                feedback_services.get_full_message_id(
+                    thread_model.id, thread_model.message_count - 1)))
         cache_updated = any([
-            _cache_last_message_text(thread, last_message),
-            _cache_last_message_author(thread, last_message),
+            _cache_last_message_text(thread_model, last_message_model),
+            _cache_last_message_author(thread_model, last_message_model),
         ])
         if cache_updated:
-            thread.put()
+            thread_model.put()
             yield ('Updated', 1)
 
     @staticmethod
@@ -52,38 +53,42 @@ class FeedbackThreadCacheOneOffJob(jobs.BaseMapReduceOneOffJobManager):
         yield (key, sum(int(s) for s in value_strs))
 
 
-def _cache_last_message_text(thread, last_message):
+def _cache_last_message_text(thread_model, last_message_model):
     """Ensures the given thread's cache for the last message's text is correct.
 
     Args:
-        thread: feedback_models.GeneralFeedbackThreadModel.
-        last_message: feedback_models.GeneralFeedbackMessageModel.
+        thread_model: feedback_models.GeneralFeedbackThreadModel. Model of the
+            thread to have its cache updated.
+        last_message_model: feedback_models.GeneralFeedbackMessageModel. Model
+            of the message which represents the most recent message, or None.
 
     Returns:
         bool. Whether the cache was actually updated.
     """
-    last_message_text = last_message and last_message.text
-    if thread.last_message_text != last_message_text:
-        thread.last_message_text = last_message_text
+    last_message_text = last_message_model and last_message_model.text
+    if thread_model.last_message_text != last_message_text:
+        thread_model.last_message_text = last_message_text
         return True
     return False
 
 
-def _cache_last_message_author(thread, last_message):
+def _cache_last_message_author(thread_model, last_message_model):
     """Ensures the given thread's cache for the last message's author is
     correct.
 
     Args:
-        thread: feedback_models.GeneralFeedbackThreadModel.
-        last_message: feedback_models.GeneralFeedbackMessageModel.
+        thread_model: feedback_models.GeneralFeedbackThreadModel. Model of the
+            thread to have its cache updated.
+        last_message_model: feedback_models.GeneralFeedbackMessageModel. Model
+            of the message which represents the most recent message, or None.
 
     Returns:
         bool. Whether the cache was actually updated.
     """
     last_message_author = (
-        last_message and last_message.author_id and
-        user_services.get_username(last_message.author_id))
-    if thread.last_message_author != last_message_author:
-        thread.last_message_author = last_message_author
+        last_message_model and last_message_model.author_id and
+        user_services.get_username(last_message_model.author_id))
+    if thread_model.last_message_author != last_message_author:
+        thread_model.last_message_author = last_message_author
         return True
     return False
