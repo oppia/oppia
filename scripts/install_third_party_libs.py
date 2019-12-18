1419aff4410de1c1a9fc87395c8c969188531ae1
amey-kudari <work.ameykudari@gmail.com>
2019-12-18 12:46:43 +0530

    removed line that prints package install as its mentioned before

--- scripts/install_third_party_libs.py
+++ scripts/install_third_party_libs.py
@@ -143,8 +143,6 @@ def pip_install(package, version, install_path):
         python_utils.PRINT(stderr)
         python_utils.PRINT(
             'Refer to https://github.com/oppia/oppia/wiki/Troubleshooting')
-        python_utils.PRINT('Error installing package %s==%s'
-             %(package, version))
         raise Exception
 
 
