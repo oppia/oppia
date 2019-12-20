7e0ed8450ff27a855c82a904cb33bf898176c5fb
aaryan-kapur <ak6392@bennett.edu.in>
2019-12-20 15:01:04 +0530

    fix1

--- core/jobs_registry.py
+++ core/jobs_registry.py
@@ -229,15 +229,15 @@ class ContinuousComputationEventDispatcher(python_utils.OBJECT):
     """Dispatches events to the relevant ContinuousComputation classes."""
 
     @classmethod
-    def dispatch_event(cls, event_type, *args, **kwargs):
+    def dispatch_event(cls, event_type, data1, data2):
         """Dispatches an incoming event to the ContinuousComputation
         classes which listen to events of that type.
 
 
         Arg: event_type : type of event that is passed.
-            *args :data that is displayed, after event_type is listened to.
-            **kwargs :data that is displayed, after event_type is listened to.
+            data1 :data that is displayed, after event_type is listened to.
+            data2 :data that is displayed, after event_type is listened to.
         """
         for klass in ALL_CONTINUOUS_COMPUTATION_MANAGERS:
             if event_type in klass.get_event_types_listened_to():
-                klass.on_incoming_event(event_type, *args, **kwargs)
+                klass.on_incoming_event(event_type, data1, data2)
