4bbda8f7105aa965270320a0c07c105433f41705
Kevin Lee <kevinlee963@gmail.com>
2019-12-23 20:41:38 -0800

    Increase browser timeout

--- core/tests/karma.conf.ts
+++ core/tests/karma.conf.ts
@@ -98,7 +98,7 @@ module.exports = function(config) {
       format: '%b %T: %m',
       terminal: true
     },
-    browserNoActivityTimeout: 60000,
+    browserNoActivityTimeout: 100000,
     // Continue running in the background after running tests.
     singleRun: true,
     customLaunchers: {
