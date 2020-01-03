fa4f1a967db1d6de91c0be6b713b5c9e0bf826cb
ShivanshRakesh <49039969+ShivanshRakesh@users.noreply.github.com>
2020-01-04 01:09:25 +0530

    calling _save_exploration via update exploration

--- core/controllers/library_test.py
+++ core/controllers/library_test.py
@@ -145,8 +145,8 @@ class LibraryPageTests(test_utils.GenericTestBase):
         exploration = self.save_new_valid_exploration(
             'A', self.admin_id, title='Title A', category='Category A',
             objective='Objective A')
-        exp_services._save_exploration(  # pylint: disable=protected-access
-            self.admin_id, exploration, 'Exploration A', [])
+        exp_services.update_exploration(
+            self.admin_id, exploration.id, None, 'Exploration A')
 
         # Test that the private exploration isn't displayed.
         response_dict = self.get_json(feconf.LIBRARY_SEARCH_DATA_URL)
@@ -156,8 +156,8 @@ class LibraryPageTests(test_utils.GenericTestBase):
         exploration = self.save_new_valid_exploration(
             'B', self.admin_id, title='Title B', category='Category B',
             objective='Objective B')
-        exp_services._save_exploration(  # pylint: disable=protected-access
-            self.admin_id, exploration, 'Exploration B', [])
+        exp_services.update_exploration(
+            self.admin_id, exploration.id, None, 'Exploration B')
         rights_manager.publish_exploration(self.admin, 'B')
 
         # Publish exploration A.
