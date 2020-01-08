8c26a72384afe54ca1d0d9d560e7a42a6279b81b
Mariana Zangrossi <mari.zangue@protonmail.com>
2020-01-08 17:33:09 -0300

    Get audio-translation-language.service.ts up to 100% coverage

--- core/templates/dev/head/pages/exploration-player-page/services/audio-translation-language.service.spec.ts
+++ core/templates/dev/head/pages/exploration-player-page/services/audio-translation-language.service.spec.ts
@@ -22,9 +22,17 @@ import { AudioTranslationLanguageService } from
   'pages/exploration-player-page/services/audio-translation-language.service';
 
 describe('Audio translation language service', () => {
-  let atls: AudioTranslationLanguageService;
+  const atls: AudioTranslationLanguageService;
   beforeEach(() => {
     atls = TestBed.get(AudioTranslationLanguageService);
+
+    spyOn(window.speechSynthesis, 'getVoices').and.returnValue([{
+      'default': false,
+      lang: 'en-GB',
+      localService: false,
+      name: 'UK English',
+      voiceURI: 'UK English'
+    }]);
   });
 
   it('should properly initialize the current audio language when ' +
@@ -32,65 +40,191 @@ describe('Audio translation language service', () => {
     let allAudioLanguageCodesInExploration = ['hi-en', 'en'];
     let preferredLanguageCode = 'hi-en';
     let explorationLanguageCode = 'hi';
+    const automaticTextToSpeechEnabled = false;
+
     atls.init(allAudioLanguageCodesInExploration, preferredLanguageCode,
-      explorationLanguageCode, false);
+      explorationLanguageCode, automaticTextToSpeechEnabled);
+    expect(atls.isAutomaticTextToSpeechEnabled()).toBe(
+      automaticTextToSpeechEnabled);
     expect(atls.getCurrentAudioLanguageCode()).toEqual('hi-en');
+    expect(atls.getallAudioLanguageCodesInExploration()).toBe(
+      allAudioLanguageCodesInExploration);
     atls.clearCurrentAudioLanguageCode();
 
     allAudioLanguageCodesInExploration = ['hi-en', 'en'];
     preferredLanguageCode = 'en';
     explorationLanguageCode = 'hi';
+
     atls.init(allAudioLanguageCodesInExploration, preferredLanguageCode,
-      explorationLanguageCode, false);
+      explorationLanguageCode, automaticTextToSpeechEnabled);
     expect(atls.getCurrentAudioLanguageCode()).toEqual('en');
     atls.clearCurrentAudioLanguageCode();
 
     allAudioLanguageCodesInExploration = ['hi-en'];
     preferredLanguageCode = 'en';
     explorationLanguageCode = 'hi';
+
     atls.init(allAudioLanguageCodesInExploration, preferredLanguageCode,
       explorationLanguageCode, false);
     expect(atls.getCurrentAudioLanguageCode()).toEqual('hi-en');
+    expect(atls.getallAudioLanguageCodesInExploration()).toBe(
+      allAudioLanguageCodesInExploration);
   });
 
   it('should initialize the current audio language when ' +
      'no preferred language is set and the exploration contains an audio ' +
      'language that is related to the exploration language', () => {
-    let allAudioLanguageCodesInExploration = ['hi-en', 'en'];
-    let preferredLanguageCode = null;
-    let explorationLanguageCode = 'hi';
+    const allAudioLanguageCodesInExploration = ['hi-en', 'en'];
+    const preferredLanguageCode = null;
+    const explorationLanguageCode = 'hi';
     atls.init(allAudioLanguageCodesInExploration, preferredLanguageCode,
       explorationLanguageCode, false);
     expect(atls.getCurrentAudioLanguageCode()).toEqual('hi-en');
+    expect(atls.getallAudioLanguageCodesInExploration()).toBe(
+      allAudioLanguageCodesInExploration);
+  });
+
+  it('should initialize the current audio language when ' +
+    'no preferred language is set and the exploration contains an audio ' +
+    'language that is no related to the exploration language', () => {
+    const allAudioLanguageCodesInExploration = ['hi', 'en'];
+    const preferredLanguageCode = null;
+    const explorationLanguageCode = 'hi-en';
+    atls.init(allAudioLanguageCodesInExploration, preferredLanguageCode,
+      explorationLanguageCode, false);
+    expect(atls.getCurrentAudioLanguageCode()).toEqual('hi');
+    expect(atls.getallAudioLanguageCodesInExploration()).toBe(
+      allAudioLanguageCodesInExploration);
   });
 
   it('should initialize the current audio language to the most ' +
      'relevant language when multiple audio languages are related ' +
      'to the exploration language', () => {
-    let allAudioLanguageCodesInExploration = ['hi-en', 'en'];
-    let preferredLanguageCode = null;
-    let explorationLanguageCode = 'en';
+    const allAudioLanguageCodesInExploration = ['hi-en', 'en'];
+    const preferredLanguageCode = null;
+    const explorationLanguageCode = 'en';
+    const languageOptions = [{
+      value: 'hi-en',
+      displayed: 'Hinglish'
+    }, {
+      value: 'en',
+      displayed: 'English'
+    }];
     atls.init(allAudioLanguageCodesInExploration, preferredLanguageCode,
       explorationLanguageCode, false);
     expect(atls.getCurrentAudioLanguageCode()).toEqual('en');
+    expect(atls.getallAudioLanguageCodesInExploration()).toBe(
+      allAudioLanguageCodesInExploration);
+    expect(atls.getLanguageOptionsForDropdown()).toEqual(languageOptions);
   });
 
   it('should not have any audio language option when no audio is available ' +
      'and automatic Text-to-speech is disabled in an exploration', () => {
-    let allAudioLanguageCodesInExploration = [];
-    let explorationLanguageCode = 'en';
-    let automaticTextToSpeechEnabled = false;
+    const allAudioLanguageCodesInExploration = [];
+    const explorationLanguageCode = 'en';
+    const automaticTextToSpeechEnabled = false;
 
     // When preferredLanguageCode is set.
     let preferredLanguageCode = 'hi';
     atls.init(allAudioLanguageCodesInExploration, preferredLanguageCode,
       explorationLanguageCode, automaticTextToSpeechEnabled);
+    expect(atls.getCurrentAudioLanguageCode()).toEqual(null);
     expect(atls.getLanguageOptionsForDropdown()).toEqual([]);
+    atls.clearCurrentAudioLanguageCode();
 
     // When preferredLanguageCode is not set.
     preferredLanguageCode = null;
     atls.init(allAudioLanguageCodesInExploration, preferredLanguageCode,
       explorationLanguageCode, automaticTextToSpeechEnabled);
+    expect(atls.getCurrentAudioLanguageCode()).toEqual(null);
     expect(atls.getLanguageOptionsForDropdown()).toEqual([]);
+  });
+
+  it('should initialize the current audio language when audio is available ' +
+    'and automatic Text-to-speech is enabled in an exploration', () => {
+    const allAudioLanguageCodesInExploration = [];
+    const explorationLanguageCode = 'en';
+    const preferredLanguageCode = null;
+    const automaticTextToSpeechEnabled = true;
+    const languageOptions = [{
+      value: 'en-auto',
+      displayed: 'English (auto)',
+    }];
+    atls.init(allAudioLanguageCodesInExploration, preferredLanguageCode,
+      explorationLanguageCode, automaticTextToSpeechEnabled);
+    expect(atls.isAutogeneratedAudioAllowed()).toBe(true);
+    expect(atls.isAutogeneratedLanguageCodeSelected()).toBe(true);
+    expect(atls.getSpeechSynthesisLanguageCode()).toBe('en-GB');
+    expect(atls.getCurrentAudioLanguageCode()).toEqual('en-auto');
+    expect(atls.getLanguageOptionsForDropdown()).toEqual(languageOptions);
+  });
+
+  it('should initialize the current audio language when exploration has ' +
+    'audio language codes and audio is available and automatic ' + 
+    'Text-to-speech is enabled in it', () => {
+    const allAudioLanguageCodesInExploration = ['hi-en', 'en'];
+    const explorationLanguageCode = 'en';
+    const preferredLanguageCode = null;
+    const languageOptions = [{
+      value: 'hi-en',
+      displayed: 'Hinglish'
+    }, {
+      value: 'en',
+      displayed: 'English'
+    }, {
+      value: 'en-auto',
+      displayed: 'English (auto)',
+    }];
+    atls.init(allAudioLanguageCodesInExploration, preferredLanguageCode,
+      explorationLanguageCode, true);
+    expect(atls.getCurrentAudioLanguageCode()).toEqual('en');
+    expect(atls.getLanguageOptionsForDropdown()).toEqual(languageOptions);
+  });
+
+  it('should get speech synthesis language code when userAgent is mobile',
+    () => {
+      spyOnProperty(navigator, 'userAgent').and.returnValue('iPhone');
+      const allAudioLanguageCodesInExploration = [];
+      const explorationLanguageCode = 'en';
+      const preferredLanguageCode = null;
+      const automaticTextToSpeechEnabled = true;
+      atls.init(allAudioLanguageCodesInExploration, preferredLanguageCode,
+        explorationLanguageCode, automaticTextToSpeechEnabled);
+      expect(atls.isAutogeneratedAudioAllowed()).toBe(true);
+      expect(atls.isAutogeneratedLanguageCodeSelected()).toBe(true);
+      expect(atls.getSpeechSynthesisLanguageCode()).toBe('en_US');
+      expect(atls.getCurrentAudioLanguageCode()).toEqual('en-auto');
+      expect(atls.getLanguageOptionsForDropdown()).toEqual([{
+        value: 'en-auto',
+        displayed: 'English (auto)',
+      }])
+  });
+
+  it('should change current audio language after initialization', () => {
+    const allAudioLanguageCodesInExploration = ['hi-en', 'en'];
+    const preferredLanguageCode = 'hi-en';
+    const explorationLanguageCode = 'hi';
+    const automaticTextToSpeechEnabled = false;
+    atls.init(allAudioLanguageCodesInExploration, preferredLanguageCode,
+      explorationLanguageCode, automaticTextToSpeechEnabled);
+    expect(atls.getCurrentAudioLanguageCode()).toEqual('hi-en');
+    expect(atls.getallAudioLanguageCodesInExploration()).toBe(
+      allAudioLanguageCodesInExploration);
+    atls.setCurrentAudioLanguageCode('en');
+    expect(atls.getCurrentAudioLanguageCode()).toEqual('en');
+    expect(atls.getallAudioLanguageCodesInExploration()).toBe(
+      allAudioLanguageCodesInExploration);
+  });
+
+  it('should get the current language description correctly', () => {
+    const allAudioLanguageCodesInExploration = ['hi-en', 'en'];
+    const preferredLanguageCode = 'hi-en';
+    const explorationLanguageCode = 'hi';
+    atls.init(allAudioLanguageCodesInExploration, preferredLanguageCode,
+      explorationLanguageCode, false);
+    expect(atls.getCurrentAudioLanguageCode()).toEqual('hi-en');
+    expect(atls.getCurrentAudioLanguageDescription()).toBe('Hinglish');
+    atls.clearCurrentAudioLanguageCode();
+    expect(atls.getCurrentAudioLanguageDescription()).toBe(null);
   });
 });
