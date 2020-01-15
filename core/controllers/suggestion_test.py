ed4f3ba5a0714c5c09cc2cee4208ad6171edd0a5
Sagang Wee <9004726+sagangwee@users.noreply.github.com>
2020-01-15 13:37:43 -0800

    Fixed keyword arg lint

--- core/controllers/suggestion_test.py
+++ core/controllers/suggestion_test.py
@@ -1008,7 +1008,7 @@ class UserSubmittedSuggestionsHandlerTest(test_utils.GenericTestBase):
                 constants.SKILL_DIFFICULTIES[2], 'Explanation 3')]
         self.rubric_dicts = [rubric.to_dict() for rubric in self.rubrics]
         self.save_new_skill(
-            self.SKILL_ID, self.owner_id, self.SKILL_DESCRIPTION,
+            self.SKILL_ID, self.owner_id, description=self.SKILL_DESCRIPTION,
             rubrics=self.rubrics)
 
         self.owner_id = self.get_user_id_from_email(self.OWNER_EMAIL)
