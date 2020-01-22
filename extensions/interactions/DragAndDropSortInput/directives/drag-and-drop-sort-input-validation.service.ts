7137886b18eff092826b30f52516696ceb56f592
iamprayush <iamprayush@gmail.com>
2020-01-22 16:29:28 +0530

    Using JS min math functions instead of mathjs.

--- extensions/interactions/DragAndDropSortInput/directives/drag-and-drop-sort-input-validation.service.ts
+++ extensions/interactions/DragAndDropSortInput/directives/drag-and-drop-sort-input-validation.service.ts
@@ -16,8 +16,6 @@
  * @fileoverview Validator service for the drag and drop sorting interaction.
  */
 
-import { min, max } from 'mathjs';
-
 import { downgradeInjectable } from '@angular/upgrade/static';
 import { Injectable } from '@angular/core';
 
@@ -108,8 +106,8 @@ export class DragAndDropSortInputValidationService {
       var noOfMismatches = 0;
       var inputs = earlierRule.inputs.x;
       var answer = laterRule.inputs.x;
-      for (var i = 0; i < min(inputs.length, answer.length); i++) {
-        for (var j = 0; j < max(answer[i].length, inputs[i].length);
+      for (var i = 0; i < Math.min(inputs.length, answer.length); i++) {
+        for (var j = 0; j < Math.max(answer[i].length, inputs[i].length);
           j++) {
           if (inputs[i].length > answer[i].length) {
             if (answer[i].indexOf(inputs[i][j]) === -1) {
