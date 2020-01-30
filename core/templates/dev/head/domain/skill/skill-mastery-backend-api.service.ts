f94c91470514db5038c1206bf46cae49d13df350
Showtim3 <coolrishabhrawat@gmail.com>
2020-01-30 13:04:18 +0530

    Fixing resolve, reject types

--- core/templates/dev/head/domain/skill/skill-mastery-backend-api.service.ts
+++ core/templates/dev/head/domain/skill/skill-mastery-backend-api.service.ts
@@ -28,8 +28,9 @@ import { SkillDomainConstants } from 'domain/skill/skill-domain.constants';
 export class SkillMasteryBackendApiService {
   constructor(private httpClient: HttpClient) {}
 
-  _fetchSkillMasteryDegrees(skillIds: Array<string>, successCallback: Function,
-      errorCallback: Function): void {
+  _fetchSkillMasteryDegrees(skillIds: Array<string>,
+      successCallback: (value?: Object | PromiseLike<Object>) => void,
+      errorCallback: (reason?: any) => void): void {
     this.httpClient.get(SkillDomainConstants.SKILL_MASTERY_DATA_URL_TEMPLATE, {
       params: {
         comma_separated_skill_ids: skillIds.join(',')
@@ -46,7 +47,8 @@ export class SkillMasteryBackendApiService {
   }
 
   _updateSkillMasteryDegrees(masteryPerSkillMapping: {[key: string]: number},
-      successCallback: Function, errorCallback: Function): void {
+      successCallback: (value?: Object | PromiseLike<Object>) => void,
+      errorCallback: (reason?: any) => void): void {
     let putData = {
       mastery_change_per_skill: masteryPerSkillMapping
     };
@@ -62,14 +64,14 @@ export class SkillMasteryBackendApiService {
     });
   }
 
-  fetchSkillMasteryDegrees(skillIds: Array<string>): Promise<void> {
+  fetchSkillMasteryDegrees(skillIds: Array<string>): Promise<Object> {
     return new Promise((resolve, reject) => {
       this._fetchSkillMasteryDegrees(skillIds, resolve, reject);
     });
   }
 
   updateSkillMasteryDegrees(
-      masteryPerSkillMapping: {[key: string]: number}): Promise<void> {
+      masteryPerSkillMapping: {[key: string]: number}): Promise<Object> {
     return new Promise((resolve, reject) => {
       this._updateSkillMasteryDegrees(masteryPerSkillMapping, resolve, reject);
     });
