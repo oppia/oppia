177f19e8f616e50944b516f9503dbce454ef509b
Mariana Zangrossi <mari.zangue@protonmail.com>
2020-01-30 23:27:52 -0300

    Get hints-and-solution-manager.service.ts up to 100% coverage

--- core/templates/dev/head/pages/exploration-player-page/services/hints-and-solution-manager.service.ts
+++ core/templates/dev/head/pages/exploration-player-page/services/hints-and-solution-manager.service.ts
@@ -179,12 +179,9 @@ angular.module('oppia').factory('HintsAndSolutionManagerService', [
         }
 
         wrongAnswersSinceLastHintConsumed++;
-        if (!areAllHintsExhausted() && !isAHintWaitingToBeViewed()) {
+        if (!areAllHintsExhausted()) {
           if (numHintsReleased === 0 &&
-              wrongAnswersSinceLastHintConsumed >= 2) {
-            accelerateHintRelease();
-          } else if (
-            numHintsReleased > 0 && wrongAnswersSinceLastHintConsumed >= 1) {
+              wrongAnswersSinceLastHintConsumed >= 1) {
             accelerateHintRelease();
           }
         }
