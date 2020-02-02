d9e9a0d13bc40a17592b3b377b871aa654607516
Dushyant Yadav <dushyant9309@gmail.com>
2020-02-02 18:40:07 +0530

    Add test for translate-text service

---
+++ core/templates/dev/head/pages/community-dashboard-page/services/translate-text.service.spec.ts
@@ -0,0 +1,86 @@
+// Copyright 2018 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Tests that the translate-text service is working as
+ * expected.
+ */
+
+require(
+  'pages/community-dashboard-page/services/' +
+  'translate-text.service.ts');
+
+describe('TranslateTextService', function() {
+  var TranslateTextService, $httpBackend;
+  var mockTranslateBackendObject;
+  var callbackObject;
+  beforeEach(angular.mock.module('oppia'));
+  beforeEach(angular.mock.inject(function($injector) {
+    $httpBackend = $injector.get('$httpBackend');
+    TranslateTextService = $injector.get('TranslateTextService');
+    mockTranslateBackendObject = {
+      version: 1,
+      state_names_to_content_id_mapping: {
+        Intro: {
+          content: 'IntroContent'
+        },
+        END: {
+          content: '<p>Congratulations, you have finished!</p>'
+        },
+        'Second Question': {
+          content: 'testContent'
+        }
+      }
+    };
+  }));
+
+  afterEach(function() {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
+
+  describe('getTextToTranslate', function() {
+    it('should return text available for translation', function() {
+      $httpBackend.whenRoute('GET', '/gettranslatabletexthandler')
+        .respond(function(method, url, data, headers, params) {
+          return [200, mockTranslateBackendObject];
+        });
+      TranslateTextService.init(1, 'en', function() {
+        var translateData = TranslateTextService.getTextToTranslate();
+        expect(translateData.text).toEqual('IntroContent');
+        expect(translateData.more).toEqual(true);
+      });
+      $httpBackend.flush();
+    });
+  });
+
+  describe('suggestTranslatedText', function() {
+    it('should post new translated text data to the API', function() {
+      callbackObject = {
+        callback: function(data) {}
+      };
+      spyOn(callbackObject, 'callback');
+      $httpBackend.whenRoute('GET', '/gettranslatabletexthandler')
+        .respond(function(method, url, data, headers, params) {
+          return [200, mockTranslateBackendObject];
+        });
+      TranslateTextService.init(1, 'en', function() {
+        TranslateTextService.suggestTranslatedText(
+          '<p>test</p>', 'en', callbackObject.callback);
+        expect(callbackObject.callback).toHaveBeenCalled();
+      });
+      $httpBackend.flush();
+    });
+  });
+});
