fbbeac077a5946b4c2652596ca4a92d8aabdfb9b
darksun27 <sbsiddharth@gmail.com>
2020-02-07 23:59:20 +0530

    Serve Images as Webp

--- seeWhere.py
+++
@@ -1,9 +0,0 @@
-import os
-a = None
-with open("pngImages.txt","r") as f:
-    line = f.readline().strip()
-    while line:
-        l = line.split("/")[-1]
-        os.system("grep -r -n \"{}\"".format(l))
-        print("\n\n\n\n")
-        line = f.readline().strip()
