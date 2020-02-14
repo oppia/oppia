0158ce723f4ad02f706b2dce9afed24768fc44b4
Abhay Kumar Raghuwanshi <araghuwanshi@ee.iitr.ac.in>
2020-02-14 04:58:23 -0500

    removed exploration from frontend checks

--- scripts/check_frontend_coverage.py
+++ scripts/check_frontend_coverage.py
@@ -115,7 +115,6 @@ FULLY_COVERED_FILENAMES = [
     'exploration-correctness-feedback.service.ts',
     'exploration-editor-page.constants.ajs.ts',
     'exploration-editor-page.constants.ts',
-    'exploration-features.service.ts',
     'exploration-features-backend-api.service.ts',
     'exploration-html-formatter.service.ts',
     'exploration-init-state-name.service.ts',
