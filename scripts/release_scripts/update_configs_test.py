d5b04b7e3e1e667f201b6f1dd72ddaa8c15fef5d
Ankita Saxena <ankitasonu24@gmail.com>
2020-02-18 15:19:56 +0530

    Fix paths

--- scripts/release_scripts/update_configs_test.py
+++ scripts/release_scripts/update_configs_test.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Unit tests for scripts/update_configs.py."""
+"""Unit tests for scripts/release_scripts/update_configs.py."""
 
 from __future__ import absolute_import  # pylint: disable=import-only-modules
 from __future__ import unicode_literals  # pylint: disable=import-only-modules
