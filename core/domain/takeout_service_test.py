2cf4b44cabcf3510c7c44adf1b7da10b19aaa753
varun-tandon <vrntandon@gmail.com>
2020-02-20 22:26:08 -0800

    rename voiceover to voiceover_application

--- core/domain/takeout_service_test.py
+++ core/domain/takeout_service_test.py
@@ -440,7 +440,7 @@ class TakeoutServiceUnitTests(test_utils.GenericTestBase):
             'managed_topic_ids': []
         }
 
-        expected_voiceover_data = {}
+        expected_voiceover_application_data = {}
         expected_contrib_score_data = {}
 
         expected_export = {
@@ -465,7 +465,8 @@ class TakeoutServiceUnitTests(test_utils.GenericTestBase):
             'general_suggestion_data': general_suggestion_data,
             'exploration_rights_data': exploration_rights_data,
             'general_feedback_email_reply_to_id_data': reply_to_data,
-            'general_voiceover_application_data': expected_voiceover_data,
+            'general_voiceover_application_data': 
+                expected_voiceover_application_data,
             'user_contribution_scoring_data': expected_contrib_score_data
         }
 
