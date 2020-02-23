6867824167b9643c23b35e3bdde8d62882084158
RafayGhafoor <rafayghafoor@pm.me>
2020-02-23 19:46:59 +0500

    Fix #5676: Remove gulp from project (#8691)
    * Remove gulp from project
    
    * update yarn lock

--- scripts/start.py
+++ scripts/start.py
@@ -132,11 +132,6 @@ def main(args=None):
     # spam people accidentally.
     background_processes = []
     if not parsed_args.prod_env:
-        background_processes.append(subprocess.Popen([
-            common.NODE_BIN_PATH,
-            os.path.join(common.NODE_MODULES_PATH, 'gulp', 'bin', 'gulp.js'),
-            'watch']))
-
         # In prod mode webpack is launched through scripts/build.py
         python_utils.PRINT('Compiling webpack...')
         background_processes.append(subprocess.Popen([
