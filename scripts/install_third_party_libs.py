1920a6111d7b55bd538b7f0d6327de907125fd0b
amey-kudari <work.ameykudari@gmail.com>
2020-02-24 17:03:23 +0530

    convert lists to tuples

--- scripts/install_third_party_libs.py
+++ scripts/install_third_party_libs.py
@@ -26,8 +26,8 @@ import sys
 TOOLS_DIR = os.path.join(os.pardir, 'oppia_tools')
 
 PRE_IMPORT = [
-    ['pyyaml', '5.1.2', os.path.join(TOOLS_DIR, 'pyyaml-5.1.2')],
-    ['future', '0.17.1', os.path.join('third_party', 'future-0.17.1')]
+    ('pyyaml', '5.1.2', os.path.join(TOOLS_DIR, 'pyyaml-5.1.2')),
+    ('future', '0.17.1', os.path.join('third_party', 'future-0.17.1')),
 ]
 
 for PACKAGE, VERSION, INSTALL_PATH in PRE_IMPORT:
