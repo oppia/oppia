027fea6062062d6797d9b41e363e5e1f902eb471
Abhay raghuwanshi <45534131+abhayraghuwanshi@users.noreply.github.com>
2020-02-28 12:16:02 -0500

    updated the linter
    updated

--- scripts/pre_commit_linter.py
+++ scripts/pre_commit_linter.py
@@ -207,13 +207,13 @@ BAD_PATTERNS_JS_AND_TS_REGEXP = [
         'message': 'Please use \'toThrowError\' instead of '
                    '\'toThrow\'',
         'excluded_files': (),
-        'excluded_dirs': ()
+        'excluded_dirs': ('extensions/interactions/LogicProof/static/js')
     },
     {
         'regexp': re.compile(r'throw\s\b(\bError|\bTypeError|\bRangeError)\('),
         'message': 'Please use \'throw new\' instead of \'throw\'',
         'excluded_files': (),
-        'excluded_dirs': ()
+        'excluded_dirs': ('extensions/interactions/LogicProof/static/js')
     },
     {
         'regexp': re.compile(r'throw\s\b(\bSyntaxError|\bDimensionError)\('),
