8f0119a5c7b6ce5d5b59c001f6cf91826426ba68
Brian Rodriguez <brian@brianrodri.com>
2020-02-28 21:41:05 -0500

    fix typo

--- core/templates/pages/creator-dashboard-page/creator-dashboard-page.controller.ts
+++ core/templates/pages/creator-dashboard-page/creator-dashboard-page.controller.ts
@@ -181,7 +181,7 @@ angular.module('oppia').directive('creatorDashboardPage', [
 
           ctrl.setActiveThread = function(threadId) {
             if (!ctrl.threadsById.hasOwnProperty(threadId)) {
-              throw Error("Trying to set a nonexistant thread as active.");
+              throw Error('Trying to set a non-existant thread as active.');
             }
             ctrl.activeThread = ctrl.threadsById[threadId];
             ThreadDataService.fetchMessages(ctrl.activeThread);
