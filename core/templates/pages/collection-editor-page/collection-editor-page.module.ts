aeb8c2a8017dc2f3b57ec3dd72d4bbc22b22f39a
h-ARTS <hi5hanan@yahoo.com>
2020-03-01 15:35:21 +0100

    Merge CollectionRightsBackendApiService to Angular 8
    * Upgrade service and test file to Angular 8
    * $rootScope.$apply() added in the directive where method gets called
    * Request Interceptor added to the module
    * Test performed with success

--- core/templates/pages/collection-editor-page/collection-editor-page.module.ts
+++ core/templates/pages/collection-editor-page/collection-editor-page.module.ts
@@ -22,7 +22,8 @@ import 'zone.js';
 import { Component, NgModule, StaticProvider } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { downgradeComponent } from '@angular/upgrade/static';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RequestInterceptor } from 'services/request-interceptor.service';
 
 // This component is needed to force-bootstrap Angular at the beginning of the
 // app.
@@ -63,7 +64,12 @@ import { CollectionEditorPageConstants } from
     InteractionsExtensionsConstants,
     ObjectsDomainConstants,
     ServicesConstants,
-    CollectionEditorPageConstants
+    CollectionEditorPageConstants,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: RequestInterceptor,
+      multi: true
+    }
   ]
 })
 class CollectionEditorPageModule {
