f62d03ca0cb588cee3d5d33b909c6e3f2a96fcfa
Brian Rodriguez <brian@brianrodri.com>
2020-03-07 22:53:52 -0500

    add unit tests for 100% coverage

--- core/templates/services/UpgradedServicesSpec.ts
+++ core/templates/services/UpgradedServicesSpec.ts
@@ -22,29 +22,44 @@ import { UpgradedServices } from 'services/UpgradedServices';
 
 describe('UpgradedServices', () => {
   beforeEach(() => {
-    this.upgradedServices = TestBed.get(UpgradedServices);
-    // We need to access the private method "registerService" to test its edge
-    // cases. The method is private because calling it can cause result in very
-    // hard-to-find errors at run-time. As a compromise, we use this hack to
-    // test our expectations on the method.
+    this.upgradedServices = new UpgradedServices();
+    // We need to access the "registerService" method to test its edge cases,
+    // but we want to keep it private to discourage registering services in
+    // places outside of the UpgradedServices class. To compromise, we use this
+    // type-erasing hack to gain access to the method.
     this.registerService = (this.upgradedServices as any).registerService;
   });
 
-  it('catches out-of-order issues', () => {
+  it('should have a well-defined set of initial services', () => {
+    expect(() => this.upgradedServices.getUpgradedServices()).not.toThrow();
+  });
+
+  it('should detect service redefinitions', () => {
+    class MockService {}
+
+    this.registerService(MockService).withDependencies();
+    expect(() => this.registerService(MockService).withDependencies())
+      .toThrow(/Redefinition Error/);
+  });
+
+  it('should detect unregistered services', () => {
     class MockDependency {}
     class MockService {
       constructor(private mockDependency: MockDependency) {}
-    }
+    };
 
-    expect(() => this.registerService(MockService).withArgs(MockDependency))
-      .toThrowError(/Dependency Error/);
+    this.registerService(MockService).withDependencies(MockDependency);
+    expect(() => this.upgradedServices.getUpgradedServices())
+      .toThrow(/Registry Error/);
   });
 
-  it('catches redefinition errors', () => {
-    class MockService {}
+  it('should detect cyclic dependencies', () => {
+    class MockService1 { constructor(private mockService2: MockService2) {} }
+    class MockService2 { constructor(private mockService1: MockService1) {} }
 
-    this.registerService(MockService).withArgs();
-    expect(() => this.registerService(MockService).withArgs())
-      .toThrowError(/Redefinition Error/);
+    this.registerService(MockService1).withDependencies(MockService2);
+    this.registerService(MockService2).withDependencies(MockService1);
+    expect(() => this.upgradedServices.getUpgradedServices())
+      .toThrow(/Circular Dependency Error/);
   });
 });
