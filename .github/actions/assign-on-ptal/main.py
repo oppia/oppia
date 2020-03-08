e91201120b013bbec635a031ca3307f7df5c6464
shivansh <shivanshdhiman@gmail.com>
2020-03-08 15:33:57 +0530

    code refactor

--- .github/actions/assign-on-ptal/main.py
+++ .github/actions/assign-on-ptal/main.py
@@ -3,56 +3,54 @@ import requests
 import json
 import re
 
-def findAssignees (comment):
-    """
-    This function is used to extract the usernames present in the form 
+def find_assignees(comment):
+    """This function is used to extract the usernames present in the form 
     `@person1 @person2 ptal!` anywhere in a comment
 
     Args: 
-        comment - Input multiline string
+        comment: string. Comment to extract usernames from
 
     Returns:
-        result - Array of string (list of github usernames)
-
+        result: list(string). List of github usernames
     """
+
     result = []
 
     # regex1 extracts all the strings of the form `@person1 @person2 ptal`
-    #  from each line of the comment
+    # from each line of the comment
     # Refer: https://regex101.com/r/8nUKY0/2
-    regex1 = r"(@[a-zA-Z0-9\-]+[\s\W]+)+[p|P][t|T][a|A][l|L]\s*"
+    pattern_regex = r"(@[a-zA-Z0-9\-]+[\s\W]+)+[p|P][t|T][a|A][l|L]\s*"
 
     # regex2 extracts the github usernames from the strings resulted from 
-    #  above regex
+    # above regex
     # Refer: https://regex101.com/r/8nUKY0/4
-    regex2 = r"@([a-zA-Z\d](?:[a-zA-Z\d]|-(?=[a-zA-Z\d])){0,38})"
+    username_regex = r"@([a-zA-Z\d](?:[a-zA-Z\d]|-(?=[a-zA-Z\d])){0,38})"
 
     lines = comment.splitlines()
-    for str in lines:
+    for line in lines:
         # In each line of the comment, find the required pattern.
-        res1 = re.finditer(regex1, str)
-        for matchNum, match1 in enumerate(res1, start=1):
+        pattern_matches = re.finditer(pattern_regex, line)
+        for _, pattern in enumerate(pattern_matches, start=1):
             # For each string matching the pattern, find usernames in it
-            res2 = re.finditer(regex2, match1.group())
-            for matchNum, match2 in enumerate(res2, start=1):
+            username_matches = re.finditer(username_regex, pattern.group())
+            for _, username in enumerate(username_matches, start=1):
                 # Append all the usernames found in the string
-                print (match2.group(1))
-                result.append(match2.group(1))
+                print(username.group(1))
+                result.append(username.group(1))
 
     return result
 
 
-def buildPayload (event):
-    """
-    This function is used to build payload that is sent to Github API
+def build_payload(event):
+    """This function is used to build payload that is sent to Github API
 
     Args: 
-        event - Event payload that is sent BY github when an event occurs
+        event: dict. Event payload that is sent BY github when an event occurs
 
     Returns:
-        payload - Payload that is sent TO Github API
-
+        payload: dict. Payload that is sent TO Github API
     """
+
     if not event['repository']:
         raise Exception('Repository not given in event payload.')
 
@@ -65,57 +63,55 @@ def buildPayload (event):
     if not event['issue']['user']['login']:
         raise Exception('User payload is malformed.')
     
-    assigneeNames = findAssignees (event["comment"]["body"])
+    assignee_names = find_assignees(event["comment"]["body"])
     payload = {
-        "url": f"/repos/{event['repository']['owner']['login']}/{event['repository']['name']}/issues/{event['issue']['number']}/assignees",
+        "url": f"/repos/{event['repository']['owner']['login']}/" +
+                f"{event['repository']['name']}/issues/" +
+                f"{event['issue']['number']}/assignees",
         "body": {
-            "assignees": assigneeNames
+            "assignees": assignee_names
         }
     }
     return payload
 
 
 def main():
-    """
-    This is the main driver function. 
+    """This is the main driver function."""
 
-    Variables:
-        repoToken - github token used by github actions
-        eventpath - path to the file, where the event payload is stored
-        event - event payload after parsing from 'json' to 'dict'
-
-    """
     try:
-        repoToken = os.environ['INPUT_REPO-TOKEN']
+        # Github token used by github actions
+        repo_token = os.environ['INPUT_REPO-TOKEN']
     except Exception:
-        print ('Repo-token not provided')
+        raise Exception('Repo-token not provided')
         return
 
     try:
-        eventPath = os.environ['GITHUB_EVENT_PATH'];
+        # Path to the file, where the event payload is stored
+        event_path = os.environ['GITHUB_EVENT_PATH'];
     except Exception:
-        print ('Event path is not present.')
+        raise Exception('Event path is not present.')
         return
 
     try:
-        with open(eventPath, encoding='utf-8') as json_file:
+        with open(event_path, encoding='utf-8') as json_file:
+            # Event payload after parsing from 'json' to 'dict'
             event = json.load(json_file)
 
-        payload = buildPayload(event)
+        payload = build_payload(event)
         
         if not payload['body']['assignees']:
-            print ('No assignee found')
+            raise Exception('No assignee found')
             return
 
         headers = {
-            'Authorization': 'token ' + repoToken,
+            'Authorization': 'token ' + repo_token,
             'Accept': 'application/vnd.github.v3+json;'
         }
         r = requests.post('https://api.github.com'+payload['url'], 
             headers=headers, data=json.dumps(payload['body']))
 
     except Exception as e:
-        print (e)
+        raise Exception(e)
 
 
 if __name__ == "__main__":
