e38fde62793656d74bcee39f9a1fd4d20b0851c4
rohitkatlaa <rohitkatlaa@gmail.com>
2020-03-11 19:04:54 +0530

    adding comments

--- extensions/interactions/CodeRepl/protractor.js
+++ extensions/interactions/CodeRepl/protractor.js
@@ -30,6 +30,8 @@ var expectInteractionDetailsToMatch = function(elem, placeHolderText) {
     elem.element(by.css('.CodeMirror')).isPresent()
   ).toBe(true);
   // The /n must be included in the check because the editor inserts a newline.
+  // For testing purposes it is required that the order of
+  // the quotes is single-quotes within double-quotes
   /* eslint-disable quotes */
   var testValue = browser.executeScript(
     "var elem = $('.protractor-test-preview-tab .CodeMirror')[0].CodeMirror;" +
