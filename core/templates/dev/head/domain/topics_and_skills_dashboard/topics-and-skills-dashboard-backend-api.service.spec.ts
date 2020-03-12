d5545053431652155b3028d5cd0b24cd4f136461
Rajat012k <rajatdav2000@gmail.com>
2020-03-12 11:09:09 +0530

    migrated topicsandskillsdboard(service and spects)

--- core/templates/dev/head/domain/topics_and_skills_dashboard/topics-and-skills-dashboard-backend-api.service.spec.ts
+++ core/templates/dev/head/domain/topics_and_skills_dashboard/topics-and-skills-dashboard-backend-api.service.spec.ts
@@ -1,110 +1,94 @@
-// Copyright 2018 The Oppia Authors. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS-IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-/**
- * @fileoverview Unit tests for CreatorDashboardBackendApiService.
- */
-
-// TODO(#7222): Remove the following block of unnnecessary imports once
-// the code corresponding to the spec is upgraded to Angular 8.
-import { UpgradedServices } from 'services/UpgradedServices';
+import { UpgradedServices } from "services/UpgradedServices";
 // ^^^ This block is to be removed.
 
-import { TranslatorProviderForTests } from 'tests/test.extras';
+import { TranslatorProviderForTests } from "tests/test.extras";
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from "@angular/common/http/testing";
+import { TestBed, fakeAsync, flushMicrotasks } from "@angular/core/testing";
+import { TopicsAndSkillsDashboardBackendApiService } from "../topics_and_skills_dashboard/topics-and-skills-dashboard-backend-api.service";
+import { UrlInterpolationService } from "../utilities/url-interpolation.service";
 
-require(
-  'domain/topics_and_skills_dashboard/' +
-  'topics-and-skills-dashboard-backend-api.service.ts');
-require('domain/utilities/url-interpolation.service.ts');
+describe("Topics and Skills Dashboard backend API service", () => {
+  let TopicsAndSkillsDashboardBackendApiService: TopicsAndSkillsDashboardBackendApiService = null;
+  let httpTestingController: HttpTestingController;
 
-describe('Topics and Skills Dashboard backend API service', function() {
-  var TopicsAndSkillsDashboardBackendApiService = null;
-  var $httpBackend = null;
-  var UrlInterpolationService = null;
-  var SAMPLE_TOPIC_ID = 'hyuy4GUlvTqJ';
-
-  var sampleDataResults = {
-    topic_summary_dicts: [{
-      id: SAMPLE_TOPIC_ID,
-      name: 'Sample Name',
-      language_code: 'en',
-      version: 1,
-      canonical_story_count: 3,
-      additional_story_count: 0,
-      uncategorized_skill_count: 3,
-      subtopic_count: 3,
-      topic_model_created_on: 1466178691847.67,
-      topic_model_last_updated: 1466178759209.839
-    }],
-    skill_summary_dicts: []
-  };
+  var SAMPLE_TOPIC_ID = "hyuy4GUlvTqJ";
 
   var TOPICS_AND_SKILLS_DASHBOARD_DATA_URL =
-    '/topics_and_skills_dashboard/data';
-  var ERROR_STATUS_CODE = 500;
+    "/topics_and_skills_dashboard/data";
 
-  beforeEach(angular.mock.module('oppia'));
-  beforeEach(
-    angular.mock.module('oppia', TranslatorProviderForTests));
-  beforeEach(angular.mock.module('oppia', function($provide) {
-    var ugs = new UpgradedServices();
-    for (let [key, value] of Object.entries(ugs.getUpgradedServices())) {
-      $provide.value(key, value);
-    }
-  }));
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    httpTestingController = TestBed.get(HttpTestingController);
+    TopicsAndSkillsDashboardBackendApiService = TestBed.get(
+      TopicsAndSkillsDashboardBackendApiService
+    );
+    var sampleDataResults = {
+      topic_summary_dicts: [
+        {
+          id: SAMPLE_TOPIC_ID,
+          name: "Sample Name",
+          language_code: "en",
+          version: 1,
+          canonical_story_count: 3,
+          additional_story_count: 0,
+          uncategorized_skill_count: 3,
+          subtopic_count: 3,
+          topic_model_created_on: 1466178691847.67,
+          topic_model_last_updated: 1466178759209.839
+        }
+      ],
+      skill_summary_dicts: []
+    };
 
-  beforeEach(angular.mock.inject(function($injector) {
-    TopicsAndSkillsDashboardBackendApiService = $injector.get(
-      'TopicsAndSkillsDashboardBackendApiService');
-    UrlInterpolationService = $injector.get('UrlInterpolationService');
-    $httpBackend = $injector.get('$httpBackend');
-  }));
+    afterEach(() => {
+      httpTestingController.verify();
+    });
 
-  afterEach(function() {
-    $httpBackend.verifyNoOutstandingExpectation();
-    $httpBackend.verifyNoOutstandingRequest();
+    it("should use rejection handler if dashboard data backend request failed", fakeAsync(() => {
+      var successHandler = jasmine.createSpy("success");
+      var failHandler = jasmine.createSpy("fail");
+
+      TopicsAndSkillsDashboardBackendApiService.fetchDasboardData().subscribe(
+        successHandler,
+        failHandler
+      );
+      var req = httpTestingController.expectOne(
+        TOPICS_AND_SKILLS_DASHBOARD_DATA_URL,
+        "Error Loading dashboard data."
+      );
+      expect(req.request.method).toEqual("GET");
+
+      expect(successHandler).toHaveBeenCalled();
+      expect(failHandler).not.toHaveBeenCalled();
+    }));
+
+    it(
+      "should successfully fetch topics and skills dashboard data from the " +
+        "backend",
+      fakeAsync(() => {
+        var successHandler = jasmine.createSpy("success");
+        var failHandler = jasmine.createSpy("fail");
+
+        TopicsAndSkillsDashboardBackendApiService.fetchDasboardData().subscribe(
+          successHandler,
+          failHandler
+        );
+        var req = httpTestingController.expectOne(
+          TOPICS_AND_SKILLS_DASHBOARD_DATA_URL
+        );
+        expect(req.request.method).toEqual("GET");
+        req.flush(sampleDataResults);
+
+        flushMicrotasks();
+
+        expect(successHandler).toHaveBeenCalled();
+        expect(failHandler).not.toHaveBeenCalled();
+      })
+    );
   });
-
-  it('should successfully fetch topics and skills dashboard data from the ' +
-      'backend',
-  function() {
-    var successHandler = jasmine.createSpy('success');
-    var failHandler = jasmine.createSpy('fail');
-
-    $httpBackend.expect('GET', TOPICS_AND_SKILLS_DASHBOARD_DATA_URL).respond(
-      sampleDataResults);
-    TopicsAndSkillsDashboardBackendApiService.fetchDashboardData().then(
-      successHandler, failHandler);
-    $httpBackend.flush();
-
-    expect(successHandler).toHaveBeenCalled();
-    expect(failHandler).not.toHaveBeenCalled();
-  });
-
-  it('should use rejection handler if dashboard data backend request failed',
-    function() {
-      var successHandler = jasmine.createSpy('success');
-      var failHandler = jasmine.createSpy('fail');
-
-      $httpBackend.expect('GET', TOPICS_AND_SKILLS_DASHBOARD_DATA_URL).respond(
-        ERROR_STATUS_CODE, 'Error loading dashboard data.');
-      TopicsAndSkillsDashboardBackendApiService.fetchDashboardData().then(
-        successHandler, failHandler);
-      $httpBackend.flush();
-
-      expect(successHandler).not.toHaveBeenCalled();
-      expect(failHandler).toHaveBeenCalled();
-    }
-  );
 });
