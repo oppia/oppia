1d36a5bed04643e4cce24131134d3bbd7c54176d
sseth99 <sethsaloni01@gmail.com>
2020-03-14 17:29:24 +0530

    Changes made in top-navigation-bar.directive.ts

--- core/templates/components/common-layout-directives/navigation-bars/top-navigation-bar.directive.ts
+++ core/templates/components/common-layout-directives/navigation-bars/top-navigation-bar.directive.ts
@@ -40,12 +40,14 @@ angular.module('oppia').directive('topNavigationBar', [
         '$scope', '$http', '$window', '$timeout', '$translate',
         'SidebarStatusService', 'LABEL_FOR_CLEARING_FOCUS', 'UserService',
         'SiteAnalyticsService', 'NavigationService', 'WindowDimensionsService',
-        'DebouncerService', 'DeviceInfoService', 'LOGOUT_URL','SHOW_CLASSROOM_CALLOUT',
+        'DebouncerService', 'DeviceInfoService', 'LOGOUT_URL', 
+        'SHOW_CLASSROOM_CALLOUT', 
         function(
             $scope, $http, $window, $timeout, $translate,
             SidebarStatusService, LABEL_FOR_CLEARING_FOCUS, UserService,
             SiteAnalyticsService, NavigationService, WindowDimensionsService,
-            DebouncerService, DeviceInfoService, LOGOUT_URL, SHOW_CLASSROOM_CALLOUT) {
+            DebouncerService, DeviceInfoService, LOGOUT_URL,
+            SHOW_CLASSROOM_CALLOUT) {
           var ctrl = this;
           var NAV_MODE_SIGNUP = 'signup';
           var NAV_MODES_WITH_CUSTOM_LOCAL_NAV = [
