4e9cdf6fec0b45e005bfa1320475f7e5b25dca2f
Kevin Thomas <kevintab@tutanota.com>
2020-03-16 02:01:57 +0530

    Initialise Guppy symbols during exploration init

--- core/templates/pages/exploration-player-page/services/exploration-player-state.service.ts
+++ core/templates/pages/exploration-player-page/services/exploration-player-state.service.ts
@@ -21,6 +21,7 @@ require('domain/exploration/editable-exploration-backend-api.service.ts');
 require('domain/exploration/read-only-exploration-backend-api.service.ts');
 require('domain/question/pretest-question-backend-api.service.ts');
 require('domain/question/question-backend-api.service.ts');
+require('domain/utilities/url-interpolation.service.ts');
 require('pages/exploration-player-page/services/exploration-engine.service.ts');
 require('pages/exploration-player-page/services/number-attempts.service.ts');
 require('pages/exploration-player-page/services/player-position.service.ts');
@@ -49,7 +50,8 @@ angular.module('oppia').factory('ExplorationPlayerStateService', [
   'PretestQuestionBackendApiService',
   'QuestionBackendApiService', 'QuestionPlayerEngineService',
   'ReadOnlyExplorationBackendApiService', 'StateClassifierMappingService',
-  'StatsReportingService', 'UrlService', 'EXPLORATION_MODE',
+  'StatsReportingService', 'UrlInterpolationService', 'UrlService',
+  'EXPLORATION_MODE',
   function(
       $q, $rootScope, ContextService,
       EditableExplorationBackendApiService,
@@ -60,13 +62,16 @@ angular.module('oppia').factory('ExplorationPlayerStateService', [
       PretestQuestionBackendApiService,
       QuestionBackendApiService, QuestionPlayerEngineService,
       ReadOnlyExplorationBackendApiService, StateClassifierMappingService,
-      StatsReportingService, UrlService, EXPLORATION_MODE) {
+      StatsReportingService, UrlInterpolationService, UrlService,
+      EXPLORATION_MODE) {
     var currentEngineService = null;
     var explorationMode = EXPLORATION_MODE.OTHER;
     var editorPreviewMode = ContextService.isInExplorationEditorPage();
     var questionPlayerMode = ContextService.isInQuestionPlayerMode();
     var explorationId = ContextService.getExplorationId();
     var version = UrlService.getExplorationVersionFromUrl();
+    var oppiaSymbolsUrl = UrlInterpolationService.getStaticAssetUrl(
+      '/overrides/guppy/oppia_symbols.json');
     if (!questionPlayerMode) {
       ReadOnlyExplorationBackendApiService
         .loadExploration(explorationId, version)
@@ -137,6 +142,11 @@ angular.module('oppia').factory('ExplorationPlayerStateService', [
       ]).then(function(combinedData) {
         var explorationData = combinedData[0];
         var featuresData = combinedData[1];
+        if (explorationData.states['Math Expression Input']) {
+          Guppy.init({
+            symbols: ['/third_party/static/guppy-b5055b/sym/symbols.json',
+              oppiaSymbolsUrl]});
+        }
         ExplorationFeaturesService.init(explorationData, featuresData);
         ExplorationEngineService.init(
           explorationData, null, null, null, callback);
@@ -174,6 +184,11 @@ angular.module('oppia').factory('ExplorationPlayerStateService', [
         var explorationData = combinedData[0];
         var pretestQuestionsData = combinedData[1];
         var featuresData = combinedData[2];
+        if (explorationData.exploration.states['Math Expression Input']) {
+          Guppy.init({
+            symbols: ['/third_party/static/guppy-b5055b/sym/symbols.json',
+              oppiaSymbolsUrl]});
+        }
         ExplorationFeaturesService.init(explorationData, featuresData);
         if (pretestQuestionsData.length > 0) {
           setPretestMode();
