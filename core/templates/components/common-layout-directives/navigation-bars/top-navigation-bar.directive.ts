98ed4e743f49dfef116f0b3d61be8ccf8241285e
sseth99 <sethsaloni01@gmail.com>
2020-03-17 20:17:44 +0530

    Moving callout flag upwards

--- core/templates/components/common-layout-directives/navigation-bars/top-navigation-bar.directive.ts
+++ core/templates/components/common-layout-directives/navigation-bars/top-navigation-bar.directive.ts
@@ -48,7 +48,7 @@ angular.module('oppia').directive('topNavigationBar', [
             SiteAnalyticsService, NavigationService, WindowDimensionsService,
             DebouncerService, DeviceInfoService, LOGOUT_URL,
             SHOW_CLASSROOM_CALLOUT) {
-            $scope.SHOW_CLASSROOM_CALLOUT = SHOW_CLASSROOM_CALLOUT;
+        $scope.SHOW_CLASSROOM_CALLOUT = SHOW_CLASSROOM_CALLOUT;
           var ctrl = this;
           var NAV_MODE_SIGNUP = 'signup';
           var NAV_MODES_WITH_CUSTOM_LOCAL_NAV = [
