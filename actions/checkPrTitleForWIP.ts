f7e0b73719bd6779b7cfb78403a78d5b36b82b9f
amey-kudari <work.ameykudari@gmail.com>
2020-03-17 15:23:50 +0530

    switch to python

--- actions/checkPrTitleForWIP.ts
+++ actions/checkPrTitleForWIP.ts
@@ -1,23 +1,32 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { request } = require("@octokit/request");
-
 
 async function run() {
   try {
     console.log(github.context);
+    const octokit = new github.GitHub(process.env.GITHUB_TOKEN, {
+      log: console
+    });
+    //console.log(github);
     var pullRequest = github.context.payload.pull_request;
     var pullRequestNumber = pullRequest.number;
 
     if (pullRequest.title.includes('WIP')) {
-      
+      var repository = process.env.GITHUB_REPOSITORY;
+      const repo = repository.split('/');
+      //var repoName = github.context.payload.repository.name;
+      var issueNumber = github.context.payload.pull_request.number;
       var body = "comment hoja plz";
-      await request(
-        `POST /repos/:owner/:repo/issues/:issue_number/comments`
-        {
-          body: `Me too`
-        }
-      );
+      octokit.issues.createComment({
+        owner: repo[0],
+        repo: repo[1],
+        issue_number: issueNumber,
+        body: body
+      }).then(({ data, headers, status }) => {}).catch(err => {
+        console.log("ERROR!!\n");
+        console.log(err);
+      });
+      //console.log(userName);
     }
   } catch (error) {
     core.setFailed(error.message);
