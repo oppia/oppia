7a8a6f7063e3578bb5c45603f1cc5e7c6b91ed02
amey-kudari <work.ameykudari@gmail.com>
2020-03-17 13:23:09 +0530

    get repo owner name

--- actions/checkPrTitleForWIP.ts
+++ actions/checkPrTitleForWIP.ts
@@ -13,18 +13,19 @@ async function run() {
 
     if (pullRequest.title.includes('WIP')) {
       var repository = process.env.GITHUB_REPOSITORY;
-      console.log(repository);
+      const repo = repository.split('/');
       //var repoName = github.context.payload.repository.name;
-      //var issueNumber = github.context.payload.pull_request.number;
-      //var body = "comment hoja plz";
-      //octokit.issues.createComment({
-      //  owner,
-      //  repo: repoName,
-      //  issue_number: issueNumber,
-      //  body
-      //}).then(({ data, headers, status }) => {}).catch(err => {
-      //  console.log(err);
-      //});
+      var issueNumber = github.context.payload.pull_request.number;
+      var body = "comment hoja plz";
+      octokit.issues.createComment({
+        owner: repo[0],
+        repo: repo[1],
+        issue_number: issueNumber,
+        body: body
+      }).then(({ data, headers, status }) => {}).catch(err => {
+        console.log("ERROR!!\n");
+        console.log(err);
+      });
       //console.log(userName);
     }
   } catch (error) {
