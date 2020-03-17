5f6de753b2d073163796a176f2efc52041a1feff
amey-kudari <work.ameykudari@gmail.com>
2020-03-17 15:59:01 +0530

    use github to get context

--- actions/checkPrTitleForWIP.py
+++ actions/checkPrTitleForWIP.py
@@ -6,5 +6,9 @@ def main():
     print(gh.get_user())
     event = json.load(open(os.getenv('GITHUB_EVENT_PATH')))
     print(event)
+    if 'WIP' in event['pull_request']['title']:
+        print("FUCK YAEH, GOT WIP!!!!! fuck you node :D")
+
+
 if __name__ == '__main__':
     main()
