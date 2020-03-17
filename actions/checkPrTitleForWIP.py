720695fa3fb8bf068fd4415805d0f5d62461765d
amey-kudari <work.ameykudari@gmail.com>
2020-03-17 16:52:07 +0530

    get access token

--- actions/checkPrTitleForWIP.py
+++ actions/checkPrTitleForWIP.py
@@ -1,11 +1,11 @@
 import github, os, json
 
 def main():
-    print("WORKS!!")
     gh = github.Github(os.getenv('GITHUB_TOKEN'))
+    print(os.getenv('GITHUB_TOKEN'))
     print(gh.get_user())
     event = json.load(open(os.getenv('GITHUB_EVENT_PATH')))
-    print(event)
+    #print(event)
     if 'WIP' in event['pull_request']['title']:
         repo = gh.get_repo(event['repository']['full_name'])
         issue = repo.get_issue(event['pull_request']['number'])
