a53b45e157174e02b6787087546fb0cdc78653a7
amey-kudari <work.ameykudari@gmail.com>
2020-03-17 15:52:05 +0530

    use github to get context

--- actions/checkPrTitleForWIP.py
+++ actions/checkPrTitleForWIP.py
@@ -1,10 +1,10 @@
-import github, os
+import github, os, json
 
 def main():
     print("WORKS!!")
     gh = github.Github(os.getenv('GITHUB_TOKEN'))
     print(gh.get_user())
-    print(gh)
-
+    event = json.load(open(os.getenv('GITHUB_EVENT_PATH')))
+    print(event)
 if __name__ == '__main__':
     main()
