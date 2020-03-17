a0831f22958f0234afe1c9cd1bde41d87004cf4f
amey-kudari <work.ameykudari@gmail.com>
2020-03-17 17:33:33 +0530

    add comment message

--- actions/checkPrTitleForWIP.py
+++ actions/checkPrTitleForWIP.py
@@ -9,7 +9,14 @@ def main():
     if 'WIP' in event['pull_request']['title']:
         repo = gh.get_repo(event['repository']['full_name'])
         issue = repo.get_issue(event['pull_request']['number'])
-        issue.create_comment('I have arrived!!')
+        comment = ('Hi @' + str(a['pull_request']['user']['login']) + ' '
+                   'We typically do not want WIP PRs since each push will '
+                   'make the Travis queue unnecessarily long. If you need '
+                   'to run automated tests, please see our guides:'
+                   'please see our guides: '
+                   'https://github.com/oppia/oppia/wiki/Setup-your-own-CircleCI-instance'
+                   'https://github.com/oppia/oppia/wiki/Setup-your-own-Travis-instance')
+        issue.create_comment(comment)
 
 if __name__ == '__main__':
     main()
