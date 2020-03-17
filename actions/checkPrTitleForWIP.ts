fb1c590e571a42788bcb0339e2c8ccaff4d06640
amey-kudari <work.ameykudari@gmail.com>
2020-03-17 12:10:22 +0530

    start from 0

--- actions/checkPrTitleForWIP.ts
+++ actions/checkPrTitleForWIP.ts
@@ -3,26 +3,15 @@ const github = require('@actions/github');
 
 async function run() {
   try {
-    //console.log(github.context);
-    console.log(github);
-    var context = github.context;
-    var pullRequest = context.payload.pull_request;
+    console.log(github.context);
+    //console.log(github);
+    var pullRequest = github.context.payload.pull_request;
     var pullRequestNumber = pullRequest.number;
 
-    if (pullRequest.title.includes('WIP')) {
-      var userName = pullRequest.user.login;
-      var linkText = 'link';
-      var linkA = linkText.link(
-        'https://github.com/oppia/oppia/wiki/Setup-your-own-CircleCI-instance');
-      var linkB = linkText.link(
-        'https://github.com/oppia/oppia/wiki/Setup-your-own-Travis-instance');
-      console.log('Hi @' + userName + ' ' +
-            'We typically do not want WIP PRs since each ' +
-            'push will make the Travis queue unnecessarily ' +
-            'long. If you need to run automated tests, ' +
-            'please see our guides:' +
-            'Please follow this ' + linkA + ' and ' + linkB + ' ' +
-            'on how to set that up. Thanks!');
+    //if (pullRequest.title.includes('WIP')) {
+    //  //var userName = pullRequest.user.login;
+    //  console.log(github.context);
+    //}
   } catch (error) {
     core.setFailed(error.message);
   }
