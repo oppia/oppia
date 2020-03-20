66589e5220d6ca7156232df9a2598e8ff026e60e
rafalk342 <rafalk342@gmail.com>
2020-03-20 23:11:10 +0100

    Remove custom type definitions for ckeditor

--- typings/custom-ckeditor-defs.d.ts
+++
@@ -1,7 +0,0 @@
-// This definition can be removed once the issue here:
-// https://github.com/ckeditor/ckeditor-dev/issues/2947 is resolved.
-declare namespace CKEDITOR {
-    interface pluginDefinition {
-        icons?: string;
-    }
-}
