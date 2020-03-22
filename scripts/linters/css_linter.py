26f9aeb2479b6b7ea2fab408c83012e5d9c7db29
Anshul Hudda <anshulhudda.ssap@gmail.com>
2020-03-22 10:49:03 +0530

    Addressed review comments

--- scripts/linters/css_linter.py
+++ scripts/linters/css_linter.py
@@ -147,8 +147,8 @@ def get_linters(config_path, files_to_lint, verbose_mode_enabled=False):
         verbose_mode_enabled: bool. True if verbose mode is enabled.
 
     Returns:
-        linter: tuple(None, ThirdPartyCSSLintChecksManager). Returns a 2-tuple
-            of custom and third_party linter objects.
+        tuple(None, ThirdPartyCSSLintChecksManager). Returns a 2-tuple of custom
+            and third_party linter objects.
     """
     third_party_linter = ThirdPartyCSSLintChecksManager(
         config_path, files_to_lint, verbose_mode_enabled)
