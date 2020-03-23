b7b5e420b0b238e30621756e4539c4a24d94639c
Sagang Wee <9004726+sagangwee@users.noreply.github.com>
2020-03-23 11:30:56 -0700

    Reverted fdescribe

--- core/templates/domain/skill/skill-backend-api.service.spec.ts
+++ core/templates/domain/skill/skill-backend-api.service.spec.ts
@@ -25,7 +25,7 @@ import { UpgradedServices } from 'services/UpgradedServices';
 // ^^^ This block is to be removed.
 
 
-fdescribe('Skill backend API service', function() {
+describe('Skill backend API service', function() {
   var SkillBackendApiService = null;
   var UndoRedoService = null;
   var $httpBackend = null;
