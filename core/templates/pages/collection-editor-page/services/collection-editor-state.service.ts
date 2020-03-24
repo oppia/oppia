264bce26b3c314ac07a7f3bcb8b0de92c7daefd8
Kevin Thomas <kevintab@tutanota.com>
2020-03-25 03:51:52 +0530

    Add TODOs

--- core/templates/pages/collection-editor-page/services/collection-editor-state.service.ts
+++ core/templates/pages/collection-editor-page/services/collection-editor-state.service.ts
@@ -77,6 +77,8 @@ angular.module('oppia').factory('CollectionEditorStateService', [
           collectionId).then(
           function(newBackendCollectionObject) {
             _updateCollection(newBackendCollectionObject);
+            // TODO(#8521): Remove the use of $rootScope.$applyAsync()
+            // once the controller is migrated to angular.
             $rootScope.$applyAsync();
           },
           function(error) {
@@ -88,6 +90,8 @@ angular.module('oppia').factory('CollectionEditorStateService', [
           collectionId).then(function(newBackendCollectionRightsObject) {
           _updateCollectionRights(newBackendCollectionRightsObject);
           _collectionIsLoading = false;
+          // TODO(#8521): Remove the use of $rootScope.$applyAsync()
+          // once the controller is migrated to angular.
           $rootScope.$applyAsync();
         }, function(error) {
           AlertsService.addWarning(
@@ -189,6 +193,8 @@ angular.module('oppia').factory('CollectionEditorStateService', [
             if (successCallback) {
               successCallback();
             }
+            // TODO(#8521): Remove the use of $rootScope.$applyAsync()
+            // once the controller is migrated to angular.
             $rootScope.$applyAsync();
           }, function(error) {
             AlertsService.addWarning(
