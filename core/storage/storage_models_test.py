1fb2b76655e05ff877ea4e08865819c4d236b20c
varun-tandon <vrntandon@gmail.com>
2020-03-26 14:07:18 -0700

    update exploration models

--- core/storage/storage_models_test.py
+++ core/storage/storage_models_test.py
@@ -186,8 +186,6 @@ class StorageModelsTest(test_utils.GenericTestBase):
         # be implemented, and the TO_BE_IMPLEMENTED value will be removed.
         # Contact @varun-tandon for more information.
         expected_unimplemented = {
-            question_models.QuestionSnapshotContentModel,
-            question_models.QuestionSnapshotMetadataModel,
             exploration_models.ExplorationRightsSnapshotContentModel,
             exploration_models.ExplorationRightsSnapshotMetadataModel,
             exploration_models.ExplorationSnapshotContentModel,
