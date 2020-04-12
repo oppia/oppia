d255453a3b2fd14be18581d75ce258b685bd7f66
Mohammad Zaman <9009585+mzaman07@users.noreply.github.com>
2020-04-12 10:29:29 -0600

    Milestone 2 - Adding Audio Duration (#8751)
    * M 2.1 One-off job for duration_sec from audio files for existing explorations.
    
    * M 1.2 Add the frontend UI piece to show the time when the file is not downloaded yet.
    
    * Added the VoiceoverDurationSecondsOneOffJob test for increasing coverage. Changed the job handle exceptions related to audio files not existing.
    
    * M2.2: Added all of the test and finally got coverage in all of the scenarios.
    
    * Fixed the changes requested by PR reviewers.

--- core/templates/pages/exploration-editor-page/translation-tab/audio-translation-bar/audio-translation-bar.directive.ts
+++ core/templates/pages/exploration-editor-page/translation-tab/audio-translation-bar/audio-translation-bar.directive.ts
@@ -299,7 +299,7 @@ angular.module('oppia').directive('audioTranslationBar', [
               StateRecordedVoiceoversService.displayed.addVoiceover(
                 contentId, languageCode, filename, recordedAudioFile.size,
                 response.duration_secs);
-              $scope.durationSecs = Math.floor(response.duration_secs);
+              $scope.durationSecs = Math.round(response.duration_secs);
               saveRecordedVoiceoversChanges();
               AlertsService.addSuccessMessage(
                 'Succesfuly uploaded recorded audio.');
@@ -432,7 +432,7 @@ angular.module('oppia').directive('audioTranslationBar', [
               $scope.selectedRecording = false;
               $scope.audioNeedsUpdate = audioTranslationObject.needsUpdate;
               $scope.durationSecs =
-                Math.floor(audioTranslationObject.duration_secs);
+                Math.round(audioTranslationObject.duration_secs);
             } else {
               $scope.isAudioAvailable = false;
               $scope.audioBlob = null;
@@ -568,7 +568,7 @@ angular.module('oppia').directive('audioTranslationBar', [
               StateRecordedVoiceoversService.displayed.addVoiceover(
                 $scope.contentId, $scope.languageCode, result.filename,
                 result.fileSizeBytes, result.durationSecs);
-              $scope.durationSecs = Math.floor(result.durationSecs);
+              $scope.durationSecs = Math.round(result.durationSecs);
               saveRecordedVoiceoversChanges();
               $scope.initAudioBar();
             }, function() {
