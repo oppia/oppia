a10dec1b276ea8601a2aba56da73bda191ffb844
iamprayush <iamprayush@gmail.com>
2020-04-18 13:01:29 +0530

    Fixed topic landing frontend test.

--- core/templates/pages/landing-pages/topic-landing-page/topic-landing-page.controller.spec.ts
+++ core/templates/pages/landing-pages/topic-landing-page/topic-landing-page.controller.spec.ts
@@ -46,7 +46,7 @@ describe('Topic Landing Page', function() {
   it('should get information from topic identified at pathname', function() {
     spyOnProperty(windowRef, 'nativeWindow').and.returnValue({
       location: {
-        pathname: '/path/maths/ratios'
+        pathname: '/maths/ratios'
       }
     });
     ctrl.$onInit();
@@ -74,7 +74,7 @@ describe('Topic Landing Page', function() {
   it('should get video url', function() {
     spyOnProperty(windowRef, 'nativeWindow').and.returnValue({
       location: {
-        pathname: '/path/maths/ratios'
+        pathname: '/maths/ratios'
       }
     });
     ctrl.$onInit();
@@ -94,7 +94,7 @@ describe('Topic Landing Page', function() {
     var nativeWindowSpy = spyOnProperty(windowRef, 'nativeWindow');
     nativeWindowSpy.and.returnValue({
       location: {
-        pathname: '/path/maths/ratios'
+        pathname: '/maths/ratios'
       }
     });
     var analyticsSpy = spyOn(
