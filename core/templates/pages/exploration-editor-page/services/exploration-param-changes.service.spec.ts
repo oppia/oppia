b5dd512b709a86fd07788f15602351195b557b2a
safwan mansuri <safwanmansuri.ict17@gmail.com>
2020-04-19 20:32:57 +0530

    test-4

--- core/templates/pages/exploration-editor-page/services/exploration-param-changes.service.spec.ts
+++ core/templates/pages/exploration-editor-page/services/exploration-param-changes.service.spec.ts
@@ -22,7 +22,7 @@ import { UpgradedServices } from 'services/UpgradedServices';
 // ^^^ This block is to be removed.
 
 require('pages/exploration-editor-page/' +
-  'services/exploration-param-changes.service.spec.ts');
+  'services/exploration-param-changes.service.ts');
 
 describe('Exploration Param Changes Service', function() {
   var epcs = null;
