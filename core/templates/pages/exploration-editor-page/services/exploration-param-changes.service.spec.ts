e2c94e42af8edccca68dd9b618155c573d232d9a
safwan mansuri <safwanmansuri.ict17@gmail.com>
2020-04-19 19:38:39 +0530

    test-2

--- core/templates/pages/exploration-editor-page/services/exploration-param-changes.service.spec.ts
+++ core/templates/pages/exploration-editor-page/services/exploration-param-changes.service.spec.ts
@@ -42,6 +42,6 @@ fdescribe('Exploration Param Changes Service', function() {
   });
 
   it('should test the child object properties', function() {
-    expect(epcs.propertyName).toBe('param_changes')
+    expect(epcs.propertyName).toBe('param_changes');
   });
 });
