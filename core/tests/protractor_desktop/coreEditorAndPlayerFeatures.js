14efd8127dbb0e30da79ef7f4de5444cfd31f204
h-ARTS <hi5hanan@yahoo.com>
2020-04-24 20:06:12 +0200

    Revert "Fix #8758: add waitFor statement before accessing response tab element (#9081)"
    This reverts commit 3dfd27f093a12aff482d9117eda7ed1d69b2d0be.

--- core/tests/protractor_desktop/coreEditorAndPlayerFeatures.js
+++ core/tests/protractor_desktop/coreEditorAndPlayerFeatures.js
@@ -24,6 +24,7 @@ var general = require('../protractor_utils/general.js');
 var users = require('../protractor_utils/users.js');
 var workflow = require('../protractor_utils/workflow.js');
 
+
 var CreatorDashboardPage =
   require('../protractor_utils/CreatorDashboardPage.js');
 var ExplorationEditorPage =
