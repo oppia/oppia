fbc8ebb94331e822d0fb70292705ca64a8534401
Srijan Reddy <srijanreddy98@gmail.com>
2020-04-26 10:36:06 +0530

    Add loading-message.component.ts to whitelist

--- scripts/check_frontend_coverage.py
+++ scripts/check_frontend_coverage.py
@@ -215,6 +215,7 @@ FULLY_COVERED_FILENAMES = [
     'LearnerDashboardActivityIdsObjectFactory.ts',
     'library-page.constants.ajs.ts',
     'library-page.constants.ts',
+    'loading-message.component.ts',
     'local-storage.service.ts',
     'logger.service.ts',
     'logic-proof-rules.service.ts',
