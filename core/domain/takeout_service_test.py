79f1e0cb89724c1990a3f2df9638b37db641f783
Brian Rodriguez <brian@brianrodri.com>
2020-04-28 07:37:32 -0400

    fix import error

--- core/domain/takeout_service_test.py
+++ core/domain/takeout_service_test.py
@@ -33,14 +33,16 @@ import utils
 
 (
     base_models, collection_models, email_models,
-    exploration_models, feedback_models, skill_models,
-    topic_models, suggestion_models, user_models,
-    story_models, question_models, config_models
+    exploration_models, feedback_models, imps_models,
+    skill_models, topic_models, suggestion_models,
+    user_models, story_models, question_models,
+    config_models
 ) = models.Registry.import_models([
     models.NAMES.base_model, models.NAMES.collection, models.NAMES.email,
-    models.NAMES.exploration, models.NAMES.feedback, models.NAMES.skill,
-    models.NAMES.topic, models.NAMES.suggestion, models.NAMES.user,
-    models.NAMES.story, models.NAMES.question, models.NAMES.config])
+    models.NAMES.exploration, models.NAMES.feedback, models.NAMES.improvements,
+    models.NAMES.skill, models.NAMES.topic, models.NAMES.suggestion,
+    models.NAMES.user, models.NAMES.story, models.NAMES.question,
+    models.NAMES.config])
 
 
 class TakeoutServiceUnitTests(test_utils.GenericTestBase):
