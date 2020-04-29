1921a3b69f0d187ab35a42dedb47320bebb61f5a
Kevin Thomas <kevintab@tutanota.com>
2020-04-29 06:07:39 +0530

    remove unused import

--- core/controllers/topic_editor_test.py
+++ core/controllers/topic_editor_test.py
@@ -17,7 +17,6 @@
 from __future__ import absolute_import  # pylint: disable=import-only-modules
 from __future__ import unicode_literals  # pylint: disable=import-only-modules
 
-from constants import constants
 from core.domain import skill_services
 from core.domain import story_fetchers
 from core.domain import story_services
