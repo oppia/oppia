3f702d21ab0992006200b53b18872283c4316e44
Brian Rodriguez <brian@brianrodri.com>
2020-05-02 14:08:20 -0400

    empty out improvements tab logic

--- scripts/check_frontend_coverage.py
+++ scripts/check_frontend_coverage.py
@@ -183,7 +183,6 @@ FULLY_COVERED_FILENAMES = [
     'improvement-playthrough-modal.controller.ts',
     'improvement-suggestion-thread-modal.controller.ts',
     'ImprovementActionButtonObjectFactory.ts',
-    'improvements-display.service.ts',
     'improvements.service.ts',
     'interaction-details-cache.service.ts',
     'interaction-specs.constants.ajs.ts',
