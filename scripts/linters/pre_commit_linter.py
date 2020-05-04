caaca1f8814f2284af26aec4f9edff1dcf9cb3d2
Vojtěch Jelínek <vojtin.j@gmail.com>
2020-05-04 14:03:54 +0200

    Add support library for bleach

--- scripts/linters/pre_commit_linter.py
+++ scripts/linters/pre_commit_linter.py
@@ -132,6 +132,7 @@ _PATHS_TO_INSERT = [
     os.path.join('third_party', 'gae-cloud-storage-1.9.22.1'),
     os.path.join('third_party', 'gae-mapreduce-1.9.22.0'),
     os.path.join('third_party', 'gae-pipeline-1.9.22.1'),
+    os.path.join('third_party', 'packaging-20.3'),
     os.path.join('third_party', 'mutagen-1.43.0'),
     os.path.join('third_party', 'soupsieve-1.9.5'),
     os.path.join('third_party', 'six-1.12.0'),
