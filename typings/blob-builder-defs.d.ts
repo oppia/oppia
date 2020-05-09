965d9bc1afb2c114a366a0328b21de33b3cc3405
Nishant Mittal <mittalnishant14@outlook.com>
2020-05-09 19:38:46 +0530

    Removed BlobBuilder

--- typings/blob-builder-defs.d.ts
+++
@@ -1,9 +0,0 @@
-// Reference - https://developer.mozilla.org/en-US/docs/Web/API/BlobBuilder
-
-class BlobBuilder {
-  append: (data: ArrayBuffer) => void;
-  append: (data: Blob) => void;
-  append: (data: String, endings?: String) => void;
-  getBlob: (contentType?: String) => void;
-  getFile: (name: String, contentType?: String) => File;
-}
