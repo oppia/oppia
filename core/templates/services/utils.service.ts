8fb9ccd0c8e4c1278f2919a9b66e62420ce85d4f
Rizky Riyaldhi <rriyaldhi@gmail.com>
2020-05-11 05:28:20 +0200

    Fix part of #8668: Add documentation to utils.service.ts (#9197)
    * add documentation to utils.service.ts
    
    * add period
    
    * fix isEquivalent param documentation
    
    Co-authored-by: Rizky Riyaldhi <rizky.riyaldhi@traveloka.com>

--- core/templates/services/utils.service.ts
+++ core/templates/services/utils.service.ts
@@ -27,6 +27,10 @@ export class UtilsService {
   // The function here is to check whether the argument is empty or not. So, we
   // cannot have any specific type defined for the argument and the argument
   // is given a generic type of Object.
+  /**
+   * @param {Object} obj - the object to be checked.
+   * @return {boolean} - true if object is empty, false otherwise.
+   */
   isEmpty(obj: Object): boolean {
     for (var property in obj) {
       if (obj.hasOwnProperty(property)) {
@@ -39,6 +43,10 @@ export class UtilsService {
   // The function here is to check whether the argument is a string. So, we
   // cannot have any specific type defined for the argument and the argument
   // is given a generic type of Object.
+  /**
+   * @param {Object} input - the object to be checked.
+   * @return {boolean} - true if input is string, false otherwise.
+   */
   isString(input: Object): boolean {
     return (typeof input === 'string' || input instanceof String);
   }
@@ -46,6 +54,11 @@ export class UtilsService {
   // The function here is to check whether the two arguments are equivalent or
   // not empty or not. So, we cannot have any specific type defined for the
   // arguments and the arguments are given a generic type of Object.
+  /**
+   * @param {Object} a - the first object to be compared.
+   * @param {Object} b - the second object to be compared.
+   * @return {boolean} - true if a is equivalent to b, false otherwise.
+   */
   isEquivalent(a: Object, b: Object): boolean {
     if (a === null || b === null) {
       return a === b;
