5120d834bc294797e352d46211366f846f2d9c63
James James <jamesjay4199@gmail.com>
2020-05-11 09:43:13 +0100

    linting

--- scripts/linters/general_purpose_linter.py
+++ scripts/linters/general_purpose_linter.py
@@ -215,7 +215,7 @@ BAD_PATTERNS_JS_AND_TS_REGEXP = [
         'regexp': re.compile(r'require\(.*\.\..*\);'),
         'message': 'Please, don\'t use relative imports in require().',
         'excluded_files': (),
-        'excluded_dirs': ('core/tests/',)
+        'excluded_dirs': ('core/tests/', '.gitub/actions/oppiabot/')
     },
     {
         'regexp': re.compile(r'innerHTML'),
