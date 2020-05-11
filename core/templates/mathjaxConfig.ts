5e4bce6090239810ee386ff975cb8668297fa0c3
Nishant Mittal <mittalnishant14@outlook.com>
2020-05-12 01:28:36 +0530

    Comment

--- core/templates/mathjaxConfig.ts
+++ core/templates/mathjaxConfig.ts
@@ -1,4 +1,6 @@
 window.MathJax = {
+  // Need this because technically the type error thrown here is correct.
+  // This is not the type of MathJax but MathJaxConfig.
   // @ts-ignore
   skipStartupTypeset: true,
   messageStyle: 'none',
