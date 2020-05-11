25233e63a230c02f37261e301e0639466213dde4
Nishant Mittal <mittalnishant14@outlook.com>
2020-05-11 22:13:52 +0530

    Any

--- typings/custom-window-defs.d.ts
+++ typings/custom-window-defs.d.ts
@@ -1,20 +1,33 @@
 // Any property defined on window needs to be added here if is not
 // present on the type of window.
+
+namespace CodeMirror {
+    export class MergeView {
+      edit?: {
+        setValue?: (code: string) => void;
+      };
+      right?: {
+        orig?: {
+          setValue?: (code: string) => void;
+        }
+      };
+
+      constructor(node: Object, options: Object);
+    }
+}
+
 interface Window {
     BlobBuilder?: any;
-    CodeMirror?: any;
-    Date?: any;
-    GLOBALS?: any;
-    HTMLElement?: any;
+    CodeMirror?: typeof CodeMirror;
+    // Date?: any;
+    HTMLElement?: HTMLElement;
     MSBlobBuilder?: any;
-    Math?: any;
     MathJax?: any;
     MozBlobBuilder?: any;
     WebKitBlobBuilder?: any;
     __fixtures__?: any;
-    decodeURIComponent?: any;
-    encodeURIComponent?: any;
+    decodeURIComponent?: (encodedURIComponent: string) => string;
+    encodeURIComponent?: (decodedURIComponent: string) => string;
     opera?: any;
-    safari?: any;
-    ga?: any;
+    ga?: Function;
 }
