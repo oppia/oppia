1d1c9c9851cf44d1e7a219299db6f91c8f6041e7
iamprayush <iamprayush@gmail.com>
2020-05-14 19:19:42 +0530

    Minor change in constants file

--- assets/constants.ts
+++ assets/constants.ts
@@ -5285,8 +5285,8 @@ export = {
   // Used to hide the callout to classroom until it is ready.
   "SHOW_CLASSROOM_CALLOUT": false,
 
-  "DEV_MODE": true,
-
   // Maximum allowed length of a username.
-  "MAX_USERNAME_LENGTH": 30
+  "MAX_USERNAME_LENGTH": 30,
+
+  "DEV_MODE": true
 };
