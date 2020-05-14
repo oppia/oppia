8f1b4edcaec5ccec3c2537941deab76ad7b5ff8e
Nishant Mittal <mittalnishant14@outlook.com>
2020-05-14 15:35:33 +0530

    Comment

--- core/templates/services/date-time-format.service.spec.ts
+++ core/templates/services/date-time-format.service.spec.ts
@@ -46,6 +46,8 @@ describe('datetimeformatter', () => {
       } else {
         return new OldDate(millisSinceEpoch);
       }
+    // Need as any here because DateContructor has some properties on the
+    // function and this function doesn't really have those properties.
     } as any as MockDateContructorType;
 
     // Mock Date() to give a time of NOW_MILLIS in GMT. (Unfortunately, there
