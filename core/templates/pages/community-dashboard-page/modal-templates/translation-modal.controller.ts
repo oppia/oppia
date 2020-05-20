15b1a7a03c19f6b9aeca98c0fbc8d6a27305ed60
Mariana Zangrossi <mari.zangue@protonmail.com>
2020-05-20 19:35:32 -0300

    Create new file for inline modals in translation-opportunities.directive.ts

---
+++ core/templates/pages/community-dashboard-page/modal-templates/translation-modal.controller.ts
@@ -0,0 +1,102 @@
+// Copyright 2020 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Controller for the translation modal.
+ */
+
+require(
+  'components/common-layout-directives/common-elements/' +
+  'confirm-or-cancel-modal.controller.ts');
+
+require('pages/community-dashboard-page/services/translate-text.service.ts');
+require(
+  'pages/exploration-editor-page/translation-tab/services/' +
+  'translation-language.service.ts');
+require('services/alerts.service.ts');
+
+angular.module('oppia').controller('TranslationModalController', [
+  '$controller', '$scope', '$uibModalInstance', 'AlertsService',
+  'TranslationLanguageService', 'TranslateTextService', 'opportunity',
+  'userIsLoggedIn',
+  function(
+      $controller, $scope, $uibModalInstance, AlertsService,
+      TranslationLanguageService, TranslateTextService, opportunity,
+      userIsLoggedIn) {
+    $controller('ConfirmOrCancelModalController', {
+      $scope: $scope,
+      $uibModalInstance: $uibModalInstance
+    });
+
+    $scope.userIsLoggedIn = userIsLoggedIn;
+    $scope.uploadingTranslation = false;
+    $scope.activeWrittenTranslation = {};
+    $scope.activeWrittenTranslation.html = '';
+    $scope.HTML_SCHEMA = {
+      type: 'html',
+      ui_config: {
+        hide_complex_extensions: 'true'
+      }
+    };
+    $scope.subheading = opportunity.subheading;
+    $scope.heading = opportunity.heading;
+    $scope.loadingData = true;
+    $scope.moreAvailable = false;
+    $scope.textToTranslate = '';
+    $scope.languageDescription = (
+      TranslationLanguageService.getActiveLanguageDescription());
+    TranslateTextService.init(
+      opportunity.id,
+      TranslationLanguageService.getActiveLanguageCode(),
+      function() {
+        var textAndAvailability = (
+          TranslateTextService.getTextToTranslate());
+        $scope.textToTranslate = textAndAvailability.text;
+        $scope.moreAvailable = textAndAvailability.more;
+        $scope.loadingData = false;
+      });
+
+    $scope.skipActiveTranslation = function() {
+      var textAndAvailability = (
+        TranslateTextService.getTextToTranslate());
+      $scope.textToTranslate = textAndAvailability.text;
+      $scope.moreAvailable = textAndAvailability.more;
+      $scope.activeWrittenTranslation.html = '';
+    };
+
+    $scope.suggestTranslatedText = function() {
+      if (!$scope.uploadingTranslation && !$scope.loadingData) {
+        $scope.uploadingTranslation = true;
+        TranslateTextService.suggestTranslatedText(
+          $scope.activeWrittenTranslation.html,
+          TranslationLanguageService.getActiveLanguageCode(),
+          function() {
+            AlertsService.addSuccessMessage(
+              'Submitted translation for review.');
+            if ($scope.moreAvailable) {
+              var textAndAvailability = (
+                TranslateTextService.getTextToTranslate());
+              $scope.textToTranslate = textAndAvailability.text;
+              $scope.moreAvailable = textAndAvailability.more;
+            }
+            $scope.activeWrittenTranslation.html = '';
+            $scope.uploadingTranslation = false;
+          });
+      }
+      if (!$scope.moreAvailable) {
+        $scope.close();
+      }
+    };
+  }
+]);
