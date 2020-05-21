40c0a63affc6d20de3ea16e6f92a1d86de93d8be
Srijan Reddy <srijanreddy98@gmail.com>
2020-05-21 10:48:35 +0530

    Code review

--- core/templates/pages/skill-editor-page/services/skill-editor-state.service.ts
+++ core/templates/pages/skill-editor-page/services/skill-editor-state.service.ts
@@ -197,7 +197,7 @@ angular.module('oppia').factory('SkillEditorStateService', [
         return true;
       },
 
-      getskillChangedSubject: function() {
+      getSkillChangedSubject: function() {
         return skillChangedSubject;
       },
 
