ae41a82233221e0b46eb793ead16c53cfd4c79d7
Kevin Thomas <kevintab@tutanota.com>
2020-05-22 02:31:45 +0530

    fix wording

--- core/templates/App.ts
+++ core/templates/App.ts
@@ -276,8 +276,9 @@ angular.module('oppia').factory('$exceptionHandler', [
       }
       // If an unhandled rejection is encountered in the AngularJS promise
       // manager, it checks the data type of the rejection value. If it
-      // satifies the conditions below, it calls the exceptionHandler with an
-      // error message parameter.
+      // satifies the conditions below, it calls the exceptionHandler with the
+      // error object as the first argument and an message string as the
+      // second argument.
       // 1. It is an instance of Error type.
       // 2. It is an Error Object.
       // 3. It is Exception Object.
@@ -286,9 +287,10 @@ angular.module('oppia').factory('$exceptionHandler', [
       // see: https://github.com/angular/angular.js/blob/2dfb6b4af62d750032c91fd86dc1f8d684d179c6/src/Angular.js#L628
       // eslint-disable-next-line max-len
       // see: https://github.com/angular/angular.js/blob/2dfb6b4af62d750032c91fd86dc1f8d684d179c6/src/ng/q.js#L388
-      // Otherwise, it will call the exceptionHandler without a message
-      // parameter. In order to preserve the original stacktrace, we wrap such
-      // exceptions in an error object.
+      // Otherwise, it will call the exceptionHandler with only the error
+      // message. In order to log the error correctly on StackDriver and to
+      // preserve the original stacktrace, we wrap such exceptions in an error
+      // object.
       if (exception.message === undefined) {
         exception = new Error(exception);
       }
