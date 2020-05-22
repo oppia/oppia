bd133ab88bdd0e59028f46a8dc3da03db93d0965
Mariana Zangrossi <mari.zangue@protonmail.com>
2020-05-22 18:35:00 -0300

    Fixing errors

--- core/templates/components/state-editor/state-responses-editor/state-responses.directive.ts
+++ core/templates/components/state-editor/state-responses-editor/state-responses.directive.ts
@@ -32,6 +32,9 @@ require(
   'components/state-directives/outcome-editor/' +
   'outcome-feedback-editor.directive.ts');
 require('components/state-directives/rule-editor/rule-editor.directive.ts');
+require(
+  'pages/exploration-editor-page/editor-tab/templates/modal-templates/' +
+  'add-answer-group-modal.controller.ts');
 
 require('domain/exploration/AnswerGroupObjectFactory.ts');
 require('domain/exploration/HintObjectFactory.ts');
