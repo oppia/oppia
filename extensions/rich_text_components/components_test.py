80e361053614cc51eb4e62fdeaff0ce3a1f933f5
donosco98 <abyk476@gmail.com>
2020-05-27 01:18:39 +0530

    Fix the todo.

--- extensions/rich_text_components/components_test.py
+++ extensions/rich_text_components/components_test.py
@@ -117,10 +117,8 @@ class ComponentValidationUnitTests(test_utils.GenericTestBase):
         self.check_validation(
             components.Link, valid_items, invalid_items)
 
-    # TODO(donosco98): Add validations for svg_filename field and add proper
+    # TODO(#9379): Add validations for svg_filename field and add proper
     # values in the tests.
-    # see https://github.com/oppia/oppia/issues/9379
-
     def test_math_validation(self):
         """Tests collapsible component validation."""
         valid_items = [{
