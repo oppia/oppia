1326f9dd0336e698b5a1f8d1550fbe4767e5ac6a
Kevin Thomas <kevintab@tutanota.com>
2020-05-26 17:03:00 +0530

    test voiceoverUploadFeatures

--- core/tests/protractor_utils/ExplorationEditorTranslationTab.js
+++ core/tests/protractor_utils/ExplorationEditorTranslationTab.js
@@ -38,11 +38,10 @@ var ExplorationEditorTranslationTab = function() {
         dismissWelcomeModalButton,
         'Welcome modal is taking too long to appear');
       await dismissWelcomeModalButton.click();
+      await waitFor.invisibilityOf(
+        translationWelcomeModal,
+        'Translation welcome modal takes too long to disappear');
     }
-
-    await waitFor.invisibilityOf(
-      translationWelcomeModal,
-      'Translation welcome modal takes too long to disappear');
 
     // Otherwise, if the translation tutorial shows up, exit it.
     var buttons = element.all(by.css('.skipBtn'));
