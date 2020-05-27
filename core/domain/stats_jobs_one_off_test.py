ddd0616c2b8f2e3e6a1b55308210ed5e41ab1c8a
Brian Rodriguez <brian@brianrodri.com>
2020-05-27 03:06:19 -0400

    import division

--- core/domain/stats_jobs_one_off_test.py
+++ core/domain/stats_jobs_one_off_test.py
@@ -17,6 +17,7 @@
 """Tests for one off statistics jobs."""
 
 from __future__ import absolute_import  # pylint: disable=import-only-modules
+from __future__ import division  # pylint: disable=import-only-modules
 from __future__ import unicode_literals  # pylint: disable=import-only-modules
 
 import ast
