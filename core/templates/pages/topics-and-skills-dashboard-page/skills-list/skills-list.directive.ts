c142dd5c2d7b3107ac7d747eb2d12997c67126f3
donosco98 <abyk476@gmail.com>
2020-05-29 01:49:26 +0530

    Address review comments(maintain a uniform pattern).

--- core/templates/pages/topics-and-skills-dashboard-page/skills-list/skills-list.directive.ts
+++ core/templates/pages/topics-and-skills-dashboard-page/skills-list/skills-list.directive.ts
@@ -169,11 +169,14 @@ angular.module('oppia').directive('skillsList', [
 
                   $scope.skillSummaries = skillSummaries;
                   $scope.selectedSkillId = '';
-                  $scope.save = function() {
+                  $scope.confirm = function() {
                     $uibModalInstance.close(
                       {skill: skill,
                         supersedingSkillId: $scope.selectedSkillId
                       });
+                  };
+                  $scope.save = function() {
+                    $scope.confirm();
                   };
                 }
               ]
