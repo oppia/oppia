c62d630b09a5fba1b0f2a3cc8ab40f4621bbb8f3
shavavo <david.cheng@duke.edu>
2020-05-28 19:14:13 -0400

    added test for ReadOnlyStoryNodeObjectFactory.ts description

--- core/templates/domain/story_viewer/ReadOnlyStoryNodeObjectFactorySpec.ts
+++ core/templates/domain/story_viewer/ReadOnlyStoryNodeObjectFactorySpec.ts
@@ -52,6 +52,7 @@ describe('Read only story node object factory', () => {
   it('should correctly return all the values', function() {
     expect(_sampleStoryNode.getId()).toEqual('node_1');
     expect(_sampleStoryNode.getTitle()).toEqual('Title 1');
+    expect(_sampleStoryNode.getDescription()).toEqual('Description');
     expect(_sampleStoryNode.getExplorationId()).toEqual('exp_id');
     expect(_sampleStoryNode.isCompleted()).toEqual(true);
     expect(_sampleStoryNode.getExplorationSummaryObject()).toEqual({
