0a2c03cafe15d17f889851975a7734e5d8ce438f
Abhith Krishna <31371571+donosco98@users.noreply.github.com>
2020-05-29 21:29:11 +0530

    Fix #9340: Issue while linking another skill to a question (#9341)
    * Added a hook to save the skillLinkages before closing the modal.
    
    * Added frontent unit test.
    
    * Handle the saving of selected skill using the individual controllers rather than confirm-cancel controller.
    
    * Removed the preModalCloseHook function.
    
    * Address review comments(maintain a uniform pattern).

--- core/templates/pages/skill-editor-page/editor-tab/skill-prerequisite-skills-editor/skill-prerequisite-skills-editor.directive.ts
+++ core/templates/pages/skill-editor-page/editor-tab/skill-prerequisite-skills-editor/skill-prerequisite-skills-editor.directive.ts
@@ -77,6 +77,9 @@ angular.module('oppia').directive('skillPrerequisiteSkillsEditor', [
 
                   $scope.skillSummaries = sortedSkillSummaries;
                   $scope.selectedSkillId = null;
+                  $scope.save = function() {
+                    $scope.confirm($scope.selectedSkillId);
+                  };
                   $scope.countOfSkillsToPrioritize = skillsInSameTopicCount;
                 }
               ]
