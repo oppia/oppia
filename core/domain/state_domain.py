a15692468700f2047ff43d4c8e3c68b35c12d911
Anmol Mittal <anmol15554@gmail.com>
2020-06-06 01:02:14 +0530

    mend

--- core/domain/state_domain.py
+++ core/domain/state_domain.py
@@ -1927,8 +1927,9 @@ class State(python_utils.OBJECT):
 
         if solution is not None:
             if not isinstance(solution, Solution):
-                raise Exception('expected Solution object,recieved %s'
-                %solution)
+                raise Exception(
+                    'expected Solution object,recieved %s'
+                    % solution)
             self.interaction.solution = solution
             new_content_id_list.append(
                 self.interaction.solution.explanation.content_id)
