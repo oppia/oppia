1cb732a2f2b74f34cc72b98f660e107882f2a7c9
Chu Zhu <chuz@google.com>
2020-06-05 15:27:24 -0400

    Add SetInput validation

--- extensions/interactions/SetInput/directives/set-input-validation.service.spec.ts
+++ extensions/interactions/SetInput/directives/set-input-validation.service.spec.ts
@@ -24,13 +24,27 @@ import { SetInputValidationService } from
   'interactions/SetInput/directives/set-input-validation.service';
 import { Outcome, OutcomeObjectFactory } from
   'domain/exploration/OutcomeObjectFactory';
+import { Rule, RuleObjectFactory } from 'domain/exploration/RuleObjectFactory';
+
+import { WARNING_TYPES_CONSTANT } from 'app-type.constants';
+import { AppConstants } from 'app.constants';
 
 describe('SetInputValidationService', () => {
   let validatorService: SetInputValidationService;
+  let WARNING_TYPES: WARNING_TYPES_CONSTANT;
 
   let currentState: string;
   let goodAnswerGroups: AnswerGroup[], goodDefaultOutcome: Outcome;
-  let oof: OutcomeObjectFactory, agof: AnswerGroupObjectFactory;
+  let oof: OutcomeObjectFactory, agof: AnswerGroupObjectFactory,
+    rof: RuleObjectFactory;
+
+  let goodCustomizationArgs: object;
+
+  let equalsRule1: Rule, equalsRule2: Rule;
+  let subsetRuleSmall: Rule, subsetRuleLarge: Rule;
+  let omitElementRuleSmall: Rule, omitElementRuleLarge: Rule;
+
+  let createAnswerGroupByRules: (rules: Rule[]) => AnswerGroup;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -38,8 +52,13 @@ describe('SetInputValidationService', () => {
     });
 
     validatorService = TestBed.get(SetInputValidationService);
+
+    WARNING_TYPES = AppConstants.WARNING_TYPES;
     oof = TestBed.get(OutcomeObjectFactory);
     agof = TestBed.get(AnswerGroupObjectFactory);
+    rof = TestBed.get(RuleObjectFactory);
+
+    goodCustomizationArgs = { buttonText: { value: 'Add Item'} };
 
     currentState = 'First State';
 
@@ -56,11 +75,150 @@ describe('SetInputValidationService', () => {
     });
 
     goodAnswerGroups = [agof.createNew([], goodDefaultOutcome, false, null)];
+
+    subsetRuleSmall = rof.createFromBackendDict({
+      rule_type: 'IsSubsetOf',
+      inputs: {
+        x: ['1']
+      }
+    });
+    subsetRuleLarge = rof.createFromBackendDict({
+      rule_type: 'IsSubsetOf',
+      inputs: {
+        x: ['1', '2', '3']
+      }
+    });
+    omitElementRuleSmall = rof.createFromBackendDict({
+      rule_type: 'OmitsElementsIn',
+      inputs: {
+        x: ['1']
+      }
+    });
+    omitElementRuleLarge = rof.createFromBackendDict({
+      rule_type: 'OmitsElementsIn',
+      inputs: {
+        x: ['1', '2', '3']
+      }
+    });
+    equalsRule1 = rof.createFromBackendDict({
+      rule_type: 'Equals',
+      inputs: {
+        x: ['5', '6', '7']
+      }
+    });
+    equalsRule2 = rof.createFromBackendDict({
+      rule_type: 'Equals',
+      inputs: {
+        x: ['5', '6', '8']
+      }
+    });
+
+    createAnswerGroupByRules = (rules) => agof.createNew(
+      rules,
+      goodDefaultOutcome,
+      false,
+      null
+    );
   });
 
   it('should be able to perform basic validation', () => {
     var warnings = validatorService.getAllWarnings(
-      currentState, {}, goodAnswerGroups, goodDefaultOutcome);
+      currentState,
+      goodCustomizationArgs,
+      goodAnswerGroups,
+      goodDefaultOutcome);
     expect(warnings).toEqual([]);
+  });
+
+  it('should catch errors in customizationArgs', () => {
+    let badCustomizationArgs1 = {};
+    let badCustomizationArgs2 = { buttonText: { value: 1 } };
+    let badCustomizationArgs3 = { buttonText: { value: '' } };
+
+    expect(validatorService.getAllWarnings(
+      currentState, goodCustomizationArgs, goodAnswerGroups, goodDefaultOutcome
+    )).toEqual([]);
+
+    expect(validatorService.getAllWarnings(
+      currentState, badCustomizationArgs1, goodAnswerGroups, goodDefaultOutcome
+    )).toEqual([{
+      type: WARNING_TYPES.ERROR,
+      message: 'Button text must be a string.'
+    }]);
+
+    expect(validatorService.getAllWarnings(
+      currentState, badCustomizationArgs2, goodAnswerGroups, goodDefaultOutcome
+    )).toEqual([{
+      type: WARNING_TYPES.ERROR,
+      message: 'Button text must be a string.'
+    }]);
+
+    expect(validatorService.getAllWarnings(
+      currentState, badCustomizationArgs3, goodAnswerGroups, goodDefaultOutcome
+    )).toEqual([{
+      type: WARNING_TYPES.ERROR,
+      message: 'Label for this button should not be empty.'
+    }]);
+  });
+
+  it('should generate errors with identical rules', () => {
+    let answerGroup = createAnswerGroupByRules(
+      [subsetRuleSmall, subsetRuleSmall, subsetRuleLarge]
+    );
+
+    expect(validatorService.getAllWarnings(
+      currentState, goodCustomizationArgs, [answerGroup], goodDefaultOutcome
+    )).toEqual([{
+      type: WARNING_TYPES.ERROR,
+      message: 'Rule 2 from answer group ' +
+      '1 is the same as ' +
+      'rule 1 from ' +
+      'answer group 1'
+    }]);
+  });
+
+  it('should generate errors with redundant rules', () => {
+    let answerGroup1 = createAnswerGroupByRules(
+      [subsetRuleLarge, subsetRuleSmall]
+    );
+
+    let answerGroup2 = createAnswerGroupByRules(
+      [omitElementRuleSmall, omitElementRuleLarge]
+    );
+
+    expect(validatorService.getAllWarnings(
+      currentState, goodCustomizationArgs, [answerGroup1], goodDefaultOutcome
+    )).toEqual([{
+      type: WARNING_TYPES.ERROR,
+      message: 'Rule 2 from answer group ' +
+      '1 will never be matched because it ' +
+      'is made redundant by rule 1' +
+      ' from answer group 1.'
+    }]);
+
+    expect(validatorService.getAllWarnings(
+      currentState, goodCustomizationArgs, [answerGroup2], goodDefaultOutcome
+    )).toEqual([{
+      type: WARNING_TYPES.ERROR,
+      message: 'Rule 2 from answer group ' +
+      '1 will never be matched because it ' +
+      'is made redundant by rule 1' +
+      ' from answer group 1.'
+    }]);
+  });
+
+  it('should not generate errors with non-redundant rules', () => {
+    let answerGroup = createAnswerGroupByRules([
+      subsetRuleSmall,
+      subsetRuleLarge,
+      omitElementRuleLarge,
+      omitElementRuleSmall,
+      equalsRule1,
+      equalsRule2
+    ]);
+
+    expect(validatorService.getAllWarnings(
+      currentState, goodCustomizationArgs, [answerGroup], goodDefaultOutcome
+    )).toEqual([]);
   });
 });
--- extensions/interactions/SetInput/directives/set-input-validation.service.ts
+++ extensions/interactions/SetInput/directives/set-input-validation.service.ts
@@ -25,6 +25,9 @@ import { IWarning, baseInteractionValidationService } from
   'interactions/base-interaction-validation.service';
 import { Outcome } from
   'domain/exploration/OutcomeObjectFactory';
+import { Rule } from
+  'domain/exploration/RuleObjectFactory';
+import { AppConstants } from 'app.constants';
 
 @Injectable({
   providedIn: 'root'
@@ -34,13 +37,124 @@ export class SetInputValidationService {
     private baseInteractionValidationServiceInstance:
       baseInteractionValidationService) {}
 
+  private isSameSet(inputA: string[], inputB: string[]): boolean {
+    let setA = new Set(inputA);
+    let setB = new Set(inputB);
+    if (setA.size !== setB.size) {
+      return false;
+    }
+    for (let element of setA.values()) {
+      if (!setB.has(element)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private isSubset(inputA: string[], inputB: string[]): boolean {
+    // Checks if set inputA is a subset of inputB.
+    let setB = new Set(inputB);
+    return inputA.every(val => setB.has(val));
+  }
+
+  private isSameRule(ruleA: Rule, ruleB: Rule): boolean {
+    return ruleA.type === ruleB.type &&
+      this.isSameSet(<string[]>ruleA.inputs.x, <string[]>ruleB.inputs.x);
+  }
+
   // TODO(#7176): Replace 'any' with the exact type. This has been kept as
   // 'any' because 'customizationArgs' is a dict with possible underscore_cased
   // keys which give tslint errors against underscore_casing in favor of
   // camelCasing.
   getCustomizationArgsWarnings(customizationArgs: any): any[] {
-    // TODO(juansaba): Implement customization args validations.
-    return [];
+    let warningsList = [];
+
+    let buttonText = customizationArgs.buttonText &&
+      customizationArgs.buttonText.value;
+    if (!angular.isString(buttonText)) {
+      warningsList.push({
+        type: AppConstants.WARNING_TYPES.ERROR,
+        message: 'Button text must be a string.'
+      });
+    } else {
+      if (buttonText.length === 0) {
+        warningsList.push({
+          message: 'Label for this button should not be empty.',
+          type: AppConstants.WARNING_TYPES.ERROR
+        });
+      }
+    }
+
+    return warningsList;
+  }
+
+  getRuleAvailabilityWarnings(answerGroups: AnswerGroup[]): IWarning[] {
+    let warningsList: IWarning[] = [];
+
+    let previousRules: Array<{
+      answerGroupIndex: number,
+      ruleIndex: number,
+      rule: Rule
+    }> = [];
+
+    for (let [answerGroupIndex, answerGroup] of answerGroups.entries()) {
+      for (let [ruleIndex, rule] of answerGroup.rules.entries()) {
+        for (let prevRule of previousRules) {
+          if (this.isSameRule(prevRule.rule, rule)) {
+            warningsList.push({
+              type: AppConstants.WARNING_TYPES.ERROR,
+              message: `Rule ${ruleIndex + 1} from answer group ` +
+                `${answerGroupIndex + 1} is the same as ` +
+                `rule ${prevRule.ruleIndex + 1} from ` +
+                `answer group ${prevRule.answerGroupIndex + 1}`
+            });
+          } else if (prevRule.rule.type === rule.type) {
+            // Only consider rules of the same type as the previous rule
+            // of the same tyle might 'cover' the following one.
+            let isRuleCoveredByAnyPrevRule = false;
+            let ruleInput = <string[]>rule.inputs.x;
+            let prevRuleInput = <string[]>prevRule.rule.inputs.x;
+            switch (rule.type) {
+              case 'Equals':
+                break;
+              case 'IsSubsetOf':
+              case 'HasElementsIn':
+              case 'IsDisjointFrom':
+                isRuleCoveredByAnyPrevRule = this.isSubset(
+                  ruleInput, prevRuleInput
+                );
+                break;
+              case 'IsSupersetOf':
+              case 'HasElementsNotIn':
+              case 'OmitsElementsIn':
+                isRuleCoveredByAnyPrevRule = this.isSubset(
+                  prevRuleInput, ruleInput
+                );
+                break;
+              default:
+            }
+
+            if (isRuleCoveredByAnyPrevRule) {
+              warningsList.push({
+                type: AppConstants.WARNING_TYPES.ERROR,
+                message: `Rule ${ruleIndex + 1} from answer group ` +
+                  `${answerGroupIndex + 1} will never be matched because it ` +
+                  `is made redundant by rule ${prevRule.ruleIndex + 1}` +
+                  ` from answer group ${prevRule.answerGroupIndex + 1}.`
+              });
+            }
+          }
+        }
+
+        previousRules.push({
+          rule,
+          ruleIndex,
+          answerGroupIndex
+        });
+      }
+    }
+
+    return warningsList;
   }
 
   // TODO(#7176): Replace 'any' with the exact type. This has been kept as
@@ -50,9 +164,12 @@ export class SetInputValidationService {
   getAllWarnings(
       stateName: string, customizationArgs: any, answerGroups: AnswerGroup[],
       defaultOutcome: Outcome): IWarning[] {
-    return this.getCustomizationArgsWarnings(customizationArgs).concat(
-      this.baseInteractionValidationServiceInstance.getAllOutcomeWarnings(
-        answerGroups, defaultOutcome, stateName));
+    return [
+      ...this.getCustomizationArgsWarnings(customizationArgs),
+      ...this.getRuleAvailabilityWarnings(answerGroups),
+      ...this.baseInteractionValidationServiceInstance.getAllOutcomeWarnings(
+        answerGroups, defaultOutcome, stateName)
+    ];
   }
 }
 
