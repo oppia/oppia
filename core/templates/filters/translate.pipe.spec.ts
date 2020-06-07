4d9cb7850fd6be4e48a0128b22c5c430d4e6e8e8
Srijan Reddy <srijanreddy98@gmail.com>
2020-06-07 11:53:41 +0530

    Code review

--- core/templates/filters/translate.pipe.spec.ts
+++ core/templates/filters/translate.pipe.spec.ts
@@ -87,9 +87,9 @@ describe('TranslatePipe', () => {
     expect(pipe.transform('I18n_t_1')).toBe('Hello');
     expect(pipe.transform('I18n_t_2', {val: 'World'})).toBe('Hello World');
     expect(pipe.transform('I18n_t_2', {val: 'World'})).toBe('Hello World');
-    // Adding rogue params here, it should return the interpolated value with
-    // the script tags as santization happens when it is sent to the view using
-    // innerHTM"L"
+    // The pipe should return the interpolated value with the rogue params, as
+    // santization happens when the interpolated value is sent to the view
+    // using innerHTM"L".
     expect(pipe.transform('I18n_t_2', {val: '<script>World</script>'})).toBe(
       'Hello <script>World</script>');
     expect(pipe.transform('I18n_t_3')).toBe('I18n_t_3');
