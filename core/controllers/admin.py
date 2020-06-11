8bd97f307adc509f564e462f2dccf88b51690576
shavavo <david.cheng@duke.edu>
2020-06-11 11:56:32 -0400

    added docstring

--- core/controllers/admin.py
+++ core/controllers/admin.py
@@ -435,7 +435,13 @@ class AdminHandler(base.BaseHandler):
             }]
 
             def generate_dummy_story_nodes(node_id, exp_id, title, description):
-                """Generates and connects sequential story nodes."""
+                """Generates and connects sequential story nodes.
+                Args:
+                    node_id: int. The node id.
+                    exp_id: str. The exploration id.
+                    title: str. The title of the story node.
+                    description: str. The description of the story node.
+                """
 
                 story.add_node(
                     '%s%d' % (story_domain.NODE_ID_PREFIX, node_id),
