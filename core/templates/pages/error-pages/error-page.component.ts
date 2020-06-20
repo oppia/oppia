5771140933e8a0752bb0f2d093c2550440682cb0
darksun27 <sbsiddharth@gmail.com>
2020-06-21 00:57:10 +0530

    Fix Thanks Page Overlap

--- core/templates/pages/error-pages/error-page.component.ts
+++ core/templates/pages/error-pages/error-page.component.ts
@@ -32,6 +32,11 @@ angular.module('oppia').component('errorPage', {
       ctrl.getStatusCode = function() {
         return Number(ctrl.statusCode);
       };
+
+      ctrl.getStaticImageUrl = function(imagePath) {
+        return UrlInterpolationService.getStaticImageUrl(imagePath);
+      };
+
       ctrl.$onInit = function() {
         ctrl.oopsMintImgUrl = UrlInterpolationService.getStaticImageUrl(
           '/general/oops_mint.png');
