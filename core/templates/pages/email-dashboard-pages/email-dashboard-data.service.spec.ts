3317ab53340a3b3161cdb65ba255e891dcd3874b
Nishant Mittal <mittalnishant14@outlook.com>
2020-06-26 01:47:57 +0530

    Fix part of #8038: Remove http requests from the files that are not backend api services  (#9433)
    * Remove http requests from the files that are not backend api services
    
    * Add CODEOWNERS
    
    * Typo
    
    * FE tests coverage
    
    * Reviews
    
    * Reviews
    
    * Refactor Code
    
    * FE Tests
    
    * refactor code
    
    * fe tests
    
    * fe tests
    
    * reviews
    
    * Fix ts errors
    
    * JobSpec -> JobStatusSummary
    
    * Remove admin object factory
    
    * FE Tests
    
    * Fix all failing e2e tests
    
    * Refactor Code
    
    * Add a check and create backend api services for remaining files
    
    * Reviews
    
    * Reviews
    
    * Reviews
    
    * root component

--- core/templates/pages/email-dashboard-pages/email-dashboard-data.service.spec.ts
+++ core/templates/pages/email-dashboard-pages/email-dashboard-data.service.spec.ts
@@ -22,23 +22,28 @@ import { TestBed, fakeAsync, flushMicrotasks } from '@angular/core/testing';
 
 import { CsrfTokenService } from
   'services/csrf-token.service';
-import { EmailDashboardDataService, Query } from
+import { EmailDashboardDataService } from
   'pages/email-dashboard-pages/email-dashboard-data.service';
+import { EmailDashboardQueryObjectFactory } from
+  'domain/email-dashboard/email-dashboard-query-object.factory';
 
 describe('Email Dashboard Services', () => {
   describe('Email Dashboard Services', () => {
     let csrfService: CsrfTokenService = null;
     let emailDashboardDataService: EmailDashboardDataService = null;
     let httpTestingController: HttpTestingController;
+    let emailDashboardQueryObjectFactory: EmailDashboardQueryObjectFactory;
 
     beforeEach(() => {
       TestBed.configureTestingModule({
         imports: [HttpClientTestingModule],
-        providers: [EmailDashboardDataService]
+        providers: [EmailDashboardDataService, EmailDashboardQueryObjectFactory]
       });
       csrfService = TestBed.get(CsrfTokenService);
       emailDashboardDataService = TestBed.get(EmailDashboardDataService);
       httpTestingController = TestBed.get(HttpTestingController);
+      emailDashboardQueryObjectFactory = TestBed.get(
+        EmailDashboardQueryObjectFactory);
 
       spyOn(csrfService, 'getTokenAsync').and.callFake(() => {
         return new Promise((resolve) => {
@@ -49,14 +54,19 @@ describe('Email Dashboard Services', () => {
 
     it('should fetch correct data from backend',
       fakeAsync(() => {
-        var recentQueries = [{
-          id: 'q123',
-          status: 'processing'
-        },
-        {
-          id: 'q456',
-          status: 'processing'
-        }];
+        var recentQueries = [
+          {
+            query: {
+              id: 'q123',
+              status: 'processing'
+            }
+          },
+          {
+            query: {
+              id: 'q456',
+              status: 'processing'
+            }
+          }].map(emailDashboardQueryObjectFactory.createFromBackendDict);
 
         emailDashboardDataService.getNextQueries();
 
@@ -80,13 +90,12 @@ describe('Email Dashboard Services', () => {
     it('should post correct data to backend',
       fakeAsync(() => {
         var data = {
-          param1: 'value1',
-          param2: 'value2'
+          hasNotLoggedInForNDays: 'value1'
         };
-        var queryData = {
+        var queryData = emailDashboardQueryObjectFactory.createFromQueryDict({
           id: 'qnew',
           status: 'processing'
-        };
+        });
         var expectedQueries = [queryData];
 
         emailDashboardDataService.submitQuery(data);
@@ -113,7 +122,7 @@ describe('Email Dashboard Services', () => {
         {
           id: 'q456',
           status: 'processing'
-        }];
+        }].map(emailDashboardQueryObjectFactory.createFromQueryDict);
         var expectedQueries = [{
           id: 'q123',
           status: 'completed'
@@ -121,7 +130,7 @@ describe('Email Dashboard Services', () => {
         {
           id: 'q456',
           status: 'processing'
-        }];
+        }].map(emailDashboardQueryObjectFactory.createFromQueryDict);
 
         emailDashboardDataService.getNextQueries();
 
@@ -129,7 +138,14 @@ describe('Email Dashboard Services', () => {
           req => (/.*?emaildashboarddatahandler?.*/g).test(req.url));
         expect(req.request.method).toEqual('GET');
         req.flush({
-          recent_queries: recentQueries,
+          recent_queries: [{
+            id: 'q123',
+            status: 'processing'
+          },
+          {
+            id: 'q456',
+            status: 'processing'
+          }],
           cursor: null
         });
 
@@ -138,7 +154,7 @@ describe('Email Dashboard Services', () => {
         expect(emailDashboardDataService.getQueries().length).toEqual(2);
         expect(emailDashboardDataService.getQueries()).toEqual(recentQueries);
 
-        emailDashboardDataService.fetchQuery('q123').then((query: Query) => {
+        emailDashboardDataService.fetchQuery('q123').then((query) => {
           expect(query.id).toEqual('q123');
           expect(query.status).toEqual('completed');
         });
@@ -181,8 +197,7 @@ describe('Email Dashboard Services', () => {
         expect(emailDashboardDataService.getCurrentPageIndex()).toEqual(0);
 
         var data = {
-          param1: 'value1',
-          param2: 'value2'
+          hasNotLoggedInForNDays: 'value1'
         };
         // Maintain list of all submitted queries for cross checking.
         var totalQueries = [];
@@ -205,29 +220,33 @@ describe('Email Dashboard Services', () => {
 
           flushMicrotasks();
         }
+
+        let totalQueriesResponse = totalQueries.map(
+          emailDashboardQueryObjectFactory.createFromQueryDict);
         expect(emailDashboardDataService.getQueries().length).toEqual(25);
         expect(emailDashboardDataService.getCurrentPageIndex()).toEqual(0);
-        expect(emailDashboardDataService.getQueries()).toEqual(totalQueries);
+        expect(emailDashboardDataService.getQueries()).toEqual(
+          totalQueriesResponse);
 
         // Check that queries on page 1 are correct.
         emailDashboardDataService.getNextQueries().then(
-          (queries: Array<Query>) => {
+          (queries) => {
             expect(queries.length).toEqual(10);
-            expect(queries).toEqual(totalQueries.slice(10, 20));
+            expect(queries).toEqual(totalQueriesResponse.slice(10, 20));
           });
         expect(emailDashboardDataService.getCurrentPageIndex()).toEqual(1);
 
         // Check that queries on page 2 are correct.
         emailDashboardDataService.getNextQueries().then(
-          (queries: Array<Query>) => {
+          (queries) => {
             expect(queries.length).toEqual(5);
-            expect(queries).toEqual(totalQueries.slice(20, 25));
+            expect(queries).toEqual(totalQueriesResponse.slice(20, 25));
           });
         expect(emailDashboardDataService.getCurrentPageIndex()).toEqual(2);
 
         // Go back to page 1 and check again.
         expect(emailDashboardDataService.getPreviousQueries()).toEqual(
-          totalQueries.slice(10, 20));
+          totalQueriesResponse.slice(10, 20));
         expect(emailDashboardDataService.getCurrentPageIndex()).toEqual(1);
 
         // Submit a new query.
@@ -248,28 +267,34 @@ describe('Email Dashboard Services', () => {
         flushMicrotasks();
 
         totalQueries.unshift(queryData);
+        let queryDataResponse = (
+          emailDashboardQueryObjectFactory.createFromQueryDict(queryData));
+        totalQueriesResponse.unshift(queryDataResponse);
+
         expect(emailDashboardDataService.getQueries().length).toEqual(26);
-        expect(emailDashboardDataService.getQueries()).toEqual(totalQueries);
+        expect(emailDashboardDataService.getQueries()).toEqual(
+          totalQueriesResponse);
 
         // Check that new query is added on the top of fetched queries.
-        expect(emailDashboardDataService.getQueries()[0]).toEqual(queryData);
+        expect(emailDashboardDataService.getQueries()[0]).toEqual(
+          queryDataResponse);
 
         // Check queries on page 2.
         emailDashboardDataService.getNextQueries().then(
-          (queries: Array<Query>) => {
+          (queries) => {
             expect(queries.length).toEqual(6);
-            expect(queries).toEqual(totalQueries.slice(20, 26));
+            expect(queries).toEqual(totalQueriesResponse.slice(20, 26));
           });
         expect(emailDashboardDataService.getCurrentPageIndex()).toEqual(2);
 
         // Check queries on page 1.
         expect(emailDashboardDataService.getPreviousQueries()).toEqual(
-          totalQueries.slice(10, 20));
+          totalQueriesResponse.slice(10, 20));
         expect(emailDashboardDataService.getCurrentPageIndex()).toEqual(1);
 
         // Check queries on page 0.
         expect(emailDashboardDataService.getPreviousQueries()).toEqual(
-          totalQueries.slice(0, 10));
+          totalQueriesResponse.slice(0, 10));
         expect(emailDashboardDataService.getCurrentPageIndex()).toEqual(0);
       })
     );
