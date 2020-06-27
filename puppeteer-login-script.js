598a63b9037b92e27e8264ff2defbbf61df4e86f
jimbyo <jimz@uci.edu>
2020-06-27 08:22:50 -0700

    linting errors

--- puppeteer-login-script.js
+++ puppeteer-login-script.js
@@ -2,33 +2,31 @@
  * @param {puppeteer.Browser} browser
  * @param {{url: string, options: LHCI.CollectCommand.Options}} context
  */
-module.exports = async (browser, context) => {
-    const page = await browser.newPage();
-    await page.setDefaultNavigationTimeout(0);
-    await page.goto(context.url);
-    try {
-    await page.click('#admin');
-    await Promise.all([
-      page.waitForNavigation(), // The promise resolves after navigation has finished
-      page.click('#submit-login')
-    ]);
-  
-    await page.type('#username', 'username1');
-    await page.click('#terms-checkbox');
-    await page.waitFor(5000);
-    await page.evaluate(() => document.querySelector('#signup-submit').scrollIntoView());
-  
-    await Promise.all([
-      page.waitForNavigation(), // The promise resolves after navigation has finished
-      await page.click('#signup-submit')
-    ]);
-  
-    // await page.screenshot({path: 'example1.png'});
-    await page.screenshot({path: 'example.png'});
-  
-    await page.close();
-    }
-    catch(e) {
-  
-    }
-  };
+module.exports = async(browser, context) => {
+  const page = await browser.newPage();
+  await page.setDefaultNavigationTimeout(0);
+  await page.goto(context.url);
+  try 
+  {
+  //Sign in to Oppia 
+  await page.click('#admin');
+  await Promise.all([
+    page.waitForNavigation(),
+    page.click('#submit-login')
+  ]);
+
+  await page.type('#username', 'username1');
+  await page.click('#terms-checkbox');
+  await page.waitFor(5000);
+  await page.evaluate(() => document.querySelector('#signup-submit').scrollIntoView());
+
+  await Promise.all([
+    page.waitForNavigation(),
+    await page.click('#signup-submit')
+  ]);
+
+  await page.close();
+
+  }
+  catch(e) {}
+};
