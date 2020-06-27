bdb7fbff5ff1f514473bc42fa638f53af5ca8335
donosco98 <abyk476@gmail.com>
2020-06-27 20:00:10 +0530

    fix merge conflicts

--- extensions/objects/templates/math-expression-content-editor.directive.ts
+++ extensions/objects/templates/math-expression-content-editor.directive.ts
@@ -15,15 +15,19 @@
 /**
  * @fileoverview Directive for math expression content editor.
  */
-
+require('mathjaxConfig.ts');
 require('directives/mathjax-bind.directive.ts');
+require('services/image-upload-helper.service.ts');
+require('services/alerts.service.ts');
+
 
 // Every editor directive should implement an alwaysEditable option. There
 // may be additional customization options for the editor that should be passed
 // in via initArgs.
 
 angular.module('oppia').directive('mathExpressionContentEditor', [
-  function() {
+  'AlertsService', 'ImageUploadHelperService',
+  function(AlertsService, ImageUploadHelperService) {
     return {
       restrict: 'E',
       scope: {},
@@ -35,15 +39,58 @@ angular.module('oppia').directive('mathExpressionContentEditor', [
       controllerAs: '$ctrl',
       controller: ['$scope', function($scope) {
         var ctrl = this;
+        var convertLatexStringToSvg = function(inputLatexString) {
+          var emptyDiv = document.createElement('div');
+          var outputElement = angular.element(emptyDiv);
+          var $script = angular.element(
+            '<script type="math/tex">'
+          ).html(inputLatexString === undefined ? '' : inputLatexString);
+          outputElement.html('');
+          outputElement.append($script);
+          MathJax.Hub.Queue(['Typeset', MathJax.Hub, outputElement[0]]);
+          MathJax.Hub.Queue(function() {
+            ctrl.svgString = (
+              outputElement[0].getElementsByTagName('span')[0].outerHTML);
+          });
+        };
+
+        var processAndSaveSvg = function() {
+          var cleanedSvgString = (
+            ImageUploadHelperService.cleanMathExpressionSvgString(
+              ctrl.svgString));
+          var dimensions = (
+            ImageUploadHelperService.
+              extractDimensionsFromMathExpressionSvgString(cleanedSvgString));
+          var fileName = (
+            ImageUploadHelperService.generateMathExpressionImageFilename(
+              dimensions.height, dimensions.width, dimensions.verticalPadding));
+          var dataURI = 'data:image/svg+xml;base64,' + btoa(cleanedSvgString);
+          var invalidTagsAndAttributes = (
+            ImageUploadHelperService.getInvalidSvgTagsAndAttrs(
+              dataURI));
+          var tags = invalidTagsAndAttributes.tags;
+          var attrs = invalidTagsAndAttributes.attrs;
+          if (tags.length === 0 && attrs.length === 0) {
+            ctrl.value.svgFileDict.svgData = dataURI;
+            ctrl.value.svgFileDict.fileName = fileName;
+          } else {
+            ctrl.value.raw_latex = '';
+            ctrl.value.svg_filename = '';
+            AlertsService.addWarning('SVG failed validation.');
+          }
+        };
+
         ctrl.$onInit = function() {
           // Reset the component each time the value changes (e.g. if this is
           // part of an editable list).
+          ctrl.svgString = '';
           $scope.$watch('$ctrl.value', function() {
             ctrl.localValue = {
-              label: ctrl.value.raw_latex || ''
+              label: ctrl.value.raw_latex || '',
             };
           }, true);
           $scope.$on('externalSave', function() {
+            processAndSaveSvg();
             if (ctrl.active) {
               ctrl.replaceValue(ctrl.localValue.label);
               // The $scope.$apply() call is needed to propagate the replaced
@@ -53,10 +100,10 @@ angular.module('oppia').directive('mathExpressionContentEditor', [
           });
           ctrl.placeholderText = '\\frac{x}{y}';
           ctrl.alwaysEditable = ctrl.getAlwaysEditable();
-
           if (ctrl.alwaysEditable) {
             $scope.$watch('$ctrl.localValue.label', function(newValue) {
               ctrl.value.raw_latex = newValue;
+              convertLatexStringToSvg(ctrl.localValue.label);
             });
           } else {
             ctrl.openEditor = function() {
