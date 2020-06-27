3eeaeaf3b36d3a7b8c59e7d92fa1693f23743d13
jimbyo <jimz@uci.edu>
2020-06-27 08:42:36 -0700

    linting errors

--- puppeteer-login-script.js
+++ puppeteer-login-script.js
@@ -5,28 +5,28 @@
 module.exports = async(browser, context) => {
   const page = await browser.newPage();
   await page.setDefaultNavigationTimeout(0);
-  await page.goto(context.url);
-  try 
-  {
-  //Sign in to Oppia 
-  await page.click('#admin');
-  await Promise.all([
-    page.waitForNavigation(),
-    page.click('#submit-login')
-  ]);
+  await page.goTo(context.url);
+  try {
+    // Sign into Oppia
+    await page.click('#admin');
+    await Promise.all([
+      page.waitForNavigation(),
+      page.click('#submit-login')
+    ]);
 
-  await page.type('#username', 'username1');
-  await page.click('#terms-checkbox');
-  await page.waitFor(5000);
-  await page.evaluate(() => document.querySelector('#signup-submit').scrollIntoView());
+    await page.type('#username', 'username1');
+    await page.click('#terms-checkbox');
+    await page.waitFor(5000);
+    await page.evaluate(() => document.querySelector('#signup-submit').scrollIntoView());
 
-  await Promise.all([
-    page.waitForNavigation(),
-    await page.click('#signup-submit')
-  ]);
+    await Promise.all([
+      page.waitForNavigation(),
+      await page.click('#signup-submit')
+    ]);
 
-  await page.close();
-
+    await page.close();
+  } 
+  catch(e) {
+    // Logged in
   }
-  catch(e) {}
 };
