cbfa999732aa8a2fe9976ce2564b3d6b52bc94c1
Brian Rodriguez <brian@brianrodri.com>
2020-06-30 21:58:21 -0400

    fix lint

--- core/domain/exp_services_test.py
+++ core/domain/exp_services_test.py
@@ -17,8 +17,8 @@
 """Unit tests for core.domain.exp_services."""
 
 from __future__ import absolute_import  # pylint: disable=import-only-modules
-from __future__ import unicode_literals  # pylint: disable=import-only-modules
 from __future__ import division  # pylint: disable=import-only-modules
+from __future__ import unicode_literals  # pylint: disable=import-only-modules
 
 import datetime
 import logging
