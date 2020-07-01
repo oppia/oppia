14385995d0db70092295e16d5755f08c99dd27b6
Brian Rodriguez <brian@brianrodri.com>
2020-07-01 17:30:42 -0400

    remove console

--- extensions/visualizations/oppia-visualization-click-hexbins.directive.ts
+++ extensions/visualizations/oppia-visualization-click-hexbins.directive.ts
@@ -37,7 +37,6 @@ angular.module('oppia').directive('oppiaVisualizationClickHexbins', () => ({
     function(
         $scope, AssetsBackendApiService, ContextService,
         ImagePreloaderService) {
-      console.log('brianrodri: yo.');
       $scope.tooltipVisibility = 'hidden';
       $scope.tooltipLeft = 0;
       $scope.tooltipTop = 0;
@@ -70,7 +69,6 @@ angular.module('oppia').directive('oppiaVisualizationClickHexbins', () => ({
       };
 
       this.$onInit = () => {
-        console.log('brianrodri: ' + JSON.stringify($scope.interactionArgs));
         const imagePath = (
           $scope.interactionArgs.imageAndRegions.value.imagePath);
         const { height, width } = (
