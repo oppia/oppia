f0737df47e009a0da3cf255c54ed156dd7c12682
Kevin Thomas <kevintab@tutanota.com>
2020-07-03 19:23:28 +0530

    add waitFor feedback editor

--- core/tests/protractor_utils/ExplorationEditorMainTab.js
+++ core/tests/protractor_utils/ExplorationEditorMainTab.js
@@ -280,6 +280,8 @@ var ExplorationEditorMainTab = function() {
     // Open the feedback entry form if it is not already open.
     var isVisible = await feedbackEditor.isPresent();
     if (isVisible) {
+      await waitFor.elementToBeClickable(
+        feedbackEditor, 'Feedback editor takes too long to be clickable.');
       await feedbackEditor.click();
     }
 
