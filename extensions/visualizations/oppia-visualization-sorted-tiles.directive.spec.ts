226b9beaafb658b4db07f0455103d87c3d844d5d
Brian Rodriguez <brian@brianrodri.com>
2020-07-04 14:46:19 -0400

    Remove ugs from spec

--- extensions/visualizations/oppia-visualization-sorted-tiles.directive.spec.ts
+++ extensions/visualizations/oppia-visualization-sorted-tiles.directive.spec.ts
@@ -28,6 +28,7 @@ describe('Oppia sorted tiles visualization', function() {
   let $compile, $rootScope;
   let answerStatsObjectFactory: AnswerStatsObjectFactory;
 
+  beforeEach(angular.mock.module('oppia'));
   beforeEach(angular.mock.inject(function(_$compile_, _$rootScope_) {
     $compile = _$compile_;
     $rootScope = _$rootScope_;
