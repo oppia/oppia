0d8da1cf194307db3cd251773d09e9896a5b7cdc
Srijan Reddy <srijanreddy98@gmail.com>
2020-07-06 13:19:01 +0530

    Introduces UIB modal migration, Flex Layout Classes, and migrates sharing links directive - attempt 2 (#9784)
    * Uib Modal Migration
    
    * Add support for uib-modal
    
    * Changes
    
    * [skip ci] merge
    
    * Code review and massive unit test revamp
    
    * Code owners
    
    * Add flex
    
    * Add new line at eof
    
    * Add types
    
    * Changes
    
    * Changes
    
    * Changes
    
    * Downgrade tests to bareminimum
    
    * Code review
    
    * Fix Merge conflicts
    
    * Remove files
    
    * Code review

---
+++ core/templates/components/button-directives/exploration-embed-button-modal.component.spec.ts
@@ -0,0 +1,103 @@
+// Copyright 2020 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Unit tests for ExplorationEmbedButtonModalComponent.
+ */
+
+import { ComponentFixture, fakeAsync, TestBed, async, tick} from
+  '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+import { ExplorationEmbedButtonModalComponent } from
+  './exploration-embed-button-modal.component';
+import { SiteAnalyticsService } from 'services/site-analytics.service';
+
+class MockActiveModal {
+  dismiss(): void {
+    return;
+  }
+}
+
+class MockSiteAnalyticsService {
+  registerOpenEmbedInfoEvent(id: string): void {
+    return;
+  }
+}
+
+describe('ExplorationEmbedButtonModalComponent', () => {
+  let component: ExplorationEmbedButtonModalComponent;
+  let fixture: ComponentFixture<ExplorationEmbedButtonModalComponent>;
+  let siteAnalyticsService: SiteAnalyticsService;
+  let ngbActiveModal: NgbActiveModal;
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ExplorationEmbedButtonModalComponent],
+      providers: [
+        {
+          provide: NgbActiveModal,
+          useClass: MockActiveModal
+        },
+        {
+          provide: SiteAnalyticsService,
+          useClass: MockSiteAnalyticsService
+        }
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ExplorationEmbedButtonModalComponent);
+    component = fixture.componentInstance;
+    siteAnalyticsService = TestBed.get(SiteAnalyticsService);
+    ngbActiveModal = TestBed.get(NgbActiveModal);
+    component.explorationId = '3f6cD869';
+    component.serverName = 'https://oppia.org';
+    fixture.detectChanges();
+  });
+
+  it('should call the siteanalytics service when initialized', () => {
+    const registerOpenEmbedInfoEventSpy = spyOn(
+      siteAnalyticsService, 'registerOpenEmbedInfoEvent'
+    ).and.callThrough();
+    component.ngOnInit();
+    expect(registerOpenEmbedInfoEventSpy).toHaveBeenCalledWith('3f6cD869');
+  });
+
+  it('should dismiss the modal when clicked on cancel', fakeAsync(() => {
+    const dismissSpy = spyOn(ngbActiveModal, 'dismiss').and.callThrough();
+    let closeButtonDE = fixture.debugElement.query(
+      By.css('.modal-footer .btn.btn-secondary'));
+    closeButtonDE.nativeElement.click();
+    fixture.detectChanges();
+    expect(dismissSpy).toHaveBeenCalled();
+  }));
+
+  it('should select the embed url', fakeAsync(() => {
+    const removeAllRanges = jasmine.createSpy('removeAllRanges');
+    const addRange = jasmine.createSpy('addRange');
+    spyOn(window, 'getSelection').and.returnValue(<any>{
+      removeAllRanges: removeAllRanges,
+      addRange: addRange
+    });
+
+    let embedUrlDiv = fixture.debugElement.query(
+      By.css('.oppia-embed-modal-code'));
+    embedUrlDiv.nativeElement.click();
+    fixture.detectChanges();
+    expect(addRange).toHaveBeenCalled();
+    expect(removeAllRanges).toHaveBeenCalled();
+  }));
+});
--- core/templates/components/shared-component.module.ts
+++ core/templates/components/shared-component.module.ts
@@ -24,13 +24,25 @@ import { CommonModule } from '@angular/common';
 import { BackgroundBannerComponent } from
   './common-layout-directives/common-elements/background-banner.component';
 import { TranslatePipe } from 'filters/translate.pipe';
+import { SharingLinksComponent } from
+  './common-layout-directives/common-elements/sharing-links.component';
+import { ExplorationEmbedButtonModalComponent } from
+  './button-directives/exploration-embed-button-modal.component';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [BackgroundBannerComponent, TranslatePipe],
-  entryComponents: [BackgroundBannerComponent],
-  exports: [BackgroundBannerComponent, TranslatePipe]
+  imports: [CommonModule, NgbModalModule],
+  declarations: [BackgroundBannerComponent, TranslatePipe,
+    SharingLinksComponent, ExplorationEmbedButtonModalComponent],
+  entryComponents: [BackgroundBannerComponent, SharingLinksComponent,
+    // These elements will remain here even after migration.
+    ExplorationEmbedButtonModalComponent
+  ],
+  exports: [
+    BackgroundBannerComponent, TranslatePipe,
+    SharingLinksComponent,
+  ],
 })
 
 export class SharedComponentsModule { }
