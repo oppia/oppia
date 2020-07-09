7f30f2a56e876bf927246a55c09e94f8aa6533ad
Sagang Wee <9004726+sagangwee@users.noreply.github.com>
2020-07-09 12:22:16 -0700

    Fix #9634: Reject question suggestions upon corresponding skill deletion. (#9839)
    * Created skill_fetchers.py, reject suggestion after skill deletion
    
    * Migrate skill_fetcher tests and update references
    
    * fix linting issues
    
    * Fix missed backend tests
    
    * Removed extra parens

--- core/controllers/questions_list.py
+++ core/controllers/questions_list.py
@@ -22,7 +22,7 @@ from core.controllers import acl_decorators
 from core.controllers import base
 from core.domain import question_services
 from core.domain import skill_domain
-from core.domain import skill_services
+from core.domain import skill_fetchers
 import feconf
 
 
@@ -46,7 +46,7 @@ class QuestionsListHandler(base.BaseHandler):
                 raise self.PageNotFoundException(Exception('Invalid skill id'))
 
         try:
-            skill_services.get_multi_skills(skill_ids)
+            skill_fetchers.get_multi_skills(skill_ids)
         except Exception as e:
             raise self.PageNotFoundException(e)
 
