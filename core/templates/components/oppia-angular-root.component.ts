69e9754e58581f2f1f9a7e6c264817e334546442
Madhav Sainanee <msainanee@google.com>
2020-07-12 05:29:17 +0530

    Added trrof to oppia angular root comp

--- core/templates/components/oppia-angular-root.component.ts
+++ core/templates/components/oppia-angular-root.component.ts
@@ -596,8 +596,10 @@ import { ThreadStatusDisplayService } from
 import { TopicCreationBackendApiService } from
   'domain/topic/topic-creation-backend-api.service.ts';
 import { TopicObjectFactory } from 'domain/topic/TopicObjectFactory';
-import { TopicRightsBackendApiService }
-  from 'domain/topic/topic-rights-backend-api.service';
+import { TopicRightsBackendApiService } from
+  'domain/topic/topic-rights-backend-api.service';
+import { TopicRightsResponseObjectFactory } from
+  'domain/topic/TopicRightsResponseObjectFactory';
 import { TopicRightsObjectFactory } from
   'domain/topic/TopicRightsObjectFactory';
 import { TopicsAndSkillsDashboardBackendApiService } from
@@ -923,6 +925,7 @@ export class OppiaAngularRootComponent implements AfterViewInit {
   static topicCreationBackendApiService: TopicCreationBackendApiService;
   static topicObjectFactory: TopicObjectFactory;
   static topicRightsBackendApiService: TopicRightsBackendApiService;
+  static topicRightsResponseObjectFactory: TopicRightsResponseObjectFactory;
   static topicRightsObjectFactory: TopicRightsObjectFactory;
   static topicsAndSkillsDashboardBackendApiService: TopicsAndSkillsDashboardBackendApiService;
   static topicSummaryObjectFactory: TopicSummaryObjectFactory;
@@ -1222,6 +1225,7 @@ private threadStatusDisplayService: ThreadStatusDisplayService,
 private topicCreationBackendApiService: TopicCreationBackendApiService,
 private topicObjectFactory: TopicObjectFactory,
 private topicRightsBackendApiService: TopicRightsBackendApiService,
+private topicRightsResponseObjectFactory: TopicRightsResponseObjectFactory,
 private topicRightsObjectFactory: TopicRightsObjectFactory,
 private topicsAndSkillsDashboardBackendApiService: TopicsAndSkillsDashboardBackendApiService,
 private topicSummaryObjectFactory: TopicSummaryObjectFactory,
@@ -1522,6 +1526,7 @@ private writtenTranslationsObjectFactory: WrittenTranslationsObjectFactory
     OppiaAngularRootComponent.topicCreationBackendApiService = this.topicCreationBackendApiService;
     OppiaAngularRootComponent.topicObjectFactory = this.topicObjectFactory;
     OppiaAngularRootComponent.topicRightsBackendApiService = this.topicRightsBackendApiService;
+    OppiaAngularRootComponent.topicRightsResponseObjectFactory = this.topicRightsResponseObjectFactory;
     OppiaAngularRootComponent.topicRightsObjectFactory = this.topicRightsObjectFactory;
     OppiaAngularRootComponent.topicsAndSkillsDashboardBackendApiService = this.topicsAndSkillsDashboardBackendApiService;
     OppiaAngularRootComponent.topicSummaryObjectFactory = this.topicSummaryObjectFactory;
