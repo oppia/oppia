18082ebb0b8184eda517f050a8bcb96b7bc4e6cd
Madhav Sainanee <msainanee@google.com>
2020-07-13 19:17:32 +0530

    Fixed cov

--- scripts/check_frontend_coverage.py
+++ scripts/check_frontend_coverage.py
@@ -441,7 +441,7 @@ NOT_FULLY_COVERED_FILENAMES = [
     'topic-questions-tab.directive.ts',
     'topic-selector.directive.ts',
     'topic-summary-tile.component.ts',
-    'topic-viewer-navbar-breadcrumb.component.ts',
+    'topic-viewer-navbar-breadcrumb.directive.ts',
     'topic-viewer-stories-list.directive.ts',
     'TopicObjectFactory.ts',
     'topics-and-skills-dashboard-backend-api.service.ts',
