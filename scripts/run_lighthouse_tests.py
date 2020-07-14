0f2c4e05359f724bdeb9e8a1a429a4debbc84a7b
James John James <jamesjay4199@gmail.com>
2020-07-14 15:43:46 +0100

    setup github actions

--- scripts/run_lighthouse_tests.py
+++ scripts/run_lighthouse_tests.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """This script performs lighthouse checks and creates lighthouse reports."""
 
 from __future__ import absolute_import  # pylint: disable=import-only-modules
@@ -19,27 +18,17 @@ from __future__ import unicode_literals  # pylint: disable=import-only-modules
 
 import atexit
 import os
-import shutil
 import subprocess
 
 import python_utils
 from scripts import common
 
-from . import install_third_party_libs
-
-
 FECONF_FILE_PATH = os.path.join('feconf.py')
 CONSTANTS_FILE_PATH = os.path.join('assets/constants.ts')
 
 
-def setup_and_install_dependencies():
-    """Runs the setup and installation scripts."""
-    install_third_party_libs.main()
-
-
 def cleanup():
     """Deactivates webpages and deletes html lighthouse reports."""
-    shutil.rmtree('.lighthouseci')
 
     pattern = 'COMMUNITY_DASHBOARD_ENABLED = .*'
     replace = 'COMMUNITY_DASHBOARD_ENABLED = False'
@@ -52,18 +41,22 @@ def cleanup():
 
 def run_lighthouse_checks():
     """Runs the lighthhouse checks through the lighthouserc.json config."""
-    node_path = os.path.join(common.NODE_PATH, 'bin', 'node')
-    lhci_path = os.path.join(
-        'node_modules', '@lhci', 'cli', 'src', 'cli.js')
-    bash_command = [node_path, lhci_path, 'autorun']
-    process = subprocess.Popen(bash_command, stdout=subprocess.PIPE)
 
-    for line in iter(process.stdout.readline, ''):
-        python_utils.PRINT(line[:-1])
+    node_path = os.path.join(common.NODE_PATH, 'bin', 'node')
+    lhci_path = os.path.join('node_modules', '@lhci', 'cli', 'src', 'cli.js')
+    bash_command = [node_path, lhci_path, 'autorun']
+
+    try:
+        subprocess.check_call(bash_command)
+        python_utils.PRINT('Lighthouse checks completed successfully.')
+    except subprocess.CalledProcessError:
+        python_utils.PRINT(
+            'Lighthouse checks failed. More details can be found above.')
 
 
 def enable_webpages():
     """Enables deactivated webpages for testing."""
+
     pattern = 'COMMUNITY_DASHBOARD_ENABLED = .*'
     replace = 'COMMUNITY_DASHBOARD_ENABLED = True'
     common.inplace_replace_file(FECONF_FILE_PATH, pattern, replace)
@@ -75,7 +68,7 @@ def enable_webpages():
 
 def main():
     """Runs lighthouse checks and deletes reports."""
-    setup_and_install_dependencies()
+
     enable_webpages()
     atexit.register(cleanup)
     run_lighthouse_checks()
