d09e734e10926021adc4e4e97daec80a61815227
Madhav Sainanee <msainanee@google.com>
2020-07-15 02:17:55 +0530

    Removed unwanted changes

--- core/templates/pages/about-page/about-page.component.spec.ts
+++ core/templates/pages/about-page/about-page.component.spec.ts
@@ -28,7 +28,6 @@ import { WindowRef } from 'services/contextual/window-ref.service';
 import { I18nLanguageCodeService } from 'services/i18n-language-code.service';
 import { TranslateService } from 'services/translate.service';
 import { UtilsService } from 'services/utils.service';
-import { SiteAnalyticsService } from 'services/site-analytics.service';
 
 @Pipe({name: 'translate'})
 class MockTranslatePipe {
@@ -115,7 +114,6 @@ describe('About Page', function() {
     translate = TestBed.get(TranslateService);
     i18n = TestBed.get(I18nLanguageCodeService);
     fixture = TestBed.createComponent(AboutPageComponent);
-    sas = TestBed.get(SiteAnalyticsService);
     component = fixture.componentInstance;
   });
 
