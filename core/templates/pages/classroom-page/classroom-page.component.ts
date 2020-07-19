7ff7a9dbcf7c56c437b0b6564c273a77c8dad26a
Madhav Sainanee <30462390+orthodoxparadox@users.noreply.github.com>
2020-07-19 09:05:01 +0530

    Revert #9841: Migrates Topic Summary Tile to Angular (#9956)
    * Revert #9841
    
    * Added newline

--- core/templates/pages/classroom-page/classroom-page.component.ts
+++ core/templates/pages/classroom-page/classroom-page.component.ts
@@ -23,7 +23,7 @@ require('base-components/base-content.directive.ts');
 require(
   'components/common-layout-directives/common-elements/' +
   'background-banner.component.ts');
-require('components/summary-tile/topic-summary-tile.component.ts');
+require('components/summary-tile/topic-summary-tile.directive.ts');
 
 require('domain/classroom/classroom-backend-api.service.ts');
 require('domain/topic/TopicSummaryObjectFactory.ts');
