84a0529451a08ab4a618482a6f34fbfac34ddaf1
Claire McShane <cemcshane@wustl.edu>
2020-07-21 17:37:30 -0400

    include newly named files

---
+++ core/templates/services/user-backend-api.service.spec.ts
@@ -0,0 +1,294 @@
+// Copyright 2018 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Tests that the user service is working as expected.
+ */
+
+import { HttpClientTestingModule, HttpTestingController } from
+  '@angular/common/http/testing';
+import { TestBed, fakeAsync, flushMicrotasks } from '@angular/core/testing';
+import { WindowRef } from 'services/contextual/window-ref.service';
+import { CsrfTokenService } from 'services/csrf-token.service';
+
+import { UserBackendApiService } from 'services/user-backend-api.service';
+import { UrlInterpolationService } from
+  'domain/utilities/url-interpolation.service';
+import { UserInfoObjectFactory } from 'domain/user/UserInfoObjectFactory';
+import { UrlService } from './contextual/url.service';
+
+class MockWindowRef {
+  _window = {
+    location: {
+      pathname: 'home'
+    }
+  };
+  get nativeWindow() {
+    return this._window;
+  }
+}
+
+describe('User Backend Api Service', () => {
+  let userBackendApiService: UserBackendApiService = null;
+  let urlInterpolationService: UrlInterpolationService = null;
+  let userInfoObjectFactory: UserInfoObjectFactory = null;
+  let urlService: UrlService = null;
+  let httpTestingController: HttpTestingController = null;
+  let csrfService: CsrfTokenService = null;
+  let windowRef: MockWindowRef = null;
+
+  beforeEach(() => {
+    windowRef = new MockWindowRef();
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [{ provide: WindowRef, useValue: windowRef }]
+    });
+    httpTestingController = TestBed.get(HttpTestingController);
+    userBackendApiService = TestBed.get(UserBackendApiService);
+    urlInterpolationService = TestBed.get(UrlInterpolationService);
+    userInfoObjectFactory = TestBed.get(UserInfoObjectFactory);
+    urlService = TestBed.get(UrlService);
+    csrfService = TestBed.get(CsrfTokenService);
+
+    spyOn(csrfService, 'getTokenAsync').and.callFake(
+      () =>{
+        return Promise.resolve('sample-csrf-token');
+      });
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  it('should return userInfo data', fakeAsync(() => {
+    // Creating a test user for checking profile picture of user.
+    var sampleUserInfoBackendObject = {
+      is_moderator: false,
+      is_admin: false,
+      is_super_admin: false,
+      is_topic_manager: false,
+      can_create_collections: true,
+      preferred_site_language_code: null,
+      username: 'tester',
+      email: 'test@test.com',
+      user_is_logged_in: true
+    };
+    var sampleUserInfo = userInfoObjectFactory.createFromBackendDict(
+      sampleUserInfoBackendObject);
+
+    userBackendApiService.getUserInfoAsync().then((userInfo) => {
+      expect(userInfo.isAdmin()).toBe(sampleUserInfo.isAdmin());
+      expect(userInfo.isSuperAdmin()).toBe(sampleUserInfo.isSuperAdmin());
+      expect(userInfo.isModerator()).toBe(sampleUserInfo.isModerator());
+      expect(userInfo.isTopicManager()).toBe(sampleUserInfo.isTopicManager());
+      expect(userInfo.isLoggedIn()).toBe(
+        sampleUserInfo.isLoggedIn());
+      expect(userInfo.canCreateCollections()).toBe(
+        sampleUserInfo.canCreateCollections());
+      expect(userInfo.getUsername()).toBe(sampleUserInfo.getUsername());
+      expect(userInfo.getEmail()).toBe(sampleUserInfo.getEmail());
+      expect(userInfo.getPreferredSiteLanguageCode()).toBe(
+        sampleUserInfo.getPreferredSiteLanguageCode());
+    });
+
+    var req = httpTestingController.expectOne('/userinfohandler');
+    expect(req.request.method).toEqual('GET');
+    req.flush(sampleUserInfoBackendObject);
+
+    flushMicrotasks();
+  }));
+
+  it('should return new userInfo data when url path is signup',
+    fakeAsync(() => {
+      spyOn(urlService, 'getPathname').and.returnValue('/signup');
+      var sampleUserInfo = userInfoObjectFactory.createDefault();
+
+      userBackendApiService.getUserInfoAsync().then((userInfo) => {
+        expect(userInfo).toEqual(sampleUserInfo);
+      });
+    }));
+
+  it('should not fetch userInfo if it is was fetched before', fakeAsync(() => {
+    // Creating a test user for checking profile picture of user.
+    var sampleUserInfoBackendObject = {
+      is_moderator: false,
+      is_admin: false,
+      is_super_admin: false,
+      is_topic_manager: false,
+      can_create_collections: true,
+      preferred_site_language_code: null,
+      username: 'tester',
+      email: 'test@test.com',
+      user_is_logged_in: true
+    };
+    var sampleUserInfo = userInfoObjectFactory.createFromBackendDict(
+      sampleUserInfoBackendObject);
+
+    userBackendApiService.getUserInfoAsync().then((userInfo) => {
+      expect(userInfo).toEqual(sampleUserInfo);
+      // Fetch userInfo again.
+      userBackendApiService.getUserInfoAsync().then((sameUserInfo) => {
+        expect(sameUserInfo).toEqual(userInfo);
+      });
+    });
+    var req = httpTestingController.expectOne('/userinfohandler');
+    expect(req.request.method).toEqual('GET');
+    req.flush(sampleUserInfoBackendObject);
+
+    flushMicrotasks();
+  }));
+
+  it('should return new userInfo data if user is not logged', fakeAsync(() => {
+    // Creating a test user for checking profile picture of user.
+    var sampleUserInfoBackendObject = {
+      is_moderator: false,
+      is_admin: false,
+      is_super_admin: false,
+      is_topic_manager: false,
+      can_create_collections: true,
+      preferred_site_language_code: null,
+      username: 'tester',
+      email: 'test@test.com',
+      user_is_logged_in: false
+    };
+    var sampleUserInfo = userInfoObjectFactory.createDefault();
+
+    userBackendApiService.getUserInfoAsync().then((userInfo) => {
+      expect(userInfo).toEqual(sampleUserInfo);
+    });
+    var req = httpTestingController.expectOne('/userinfohandler');
+    expect(req.request.method).toEqual('GET');
+    req.flush(sampleUserInfoBackendObject);
+
+    flushMicrotasks();
+  }));
+
+  it('should return the default profile image path when user is not logged',
+    fakeAsync(() => {
+      // Creating a test user for checking profile picture of user.
+      var sampleUserInfoBackendObject = {
+        is_moderator: false,
+        is_admin: false,
+        is_super_admin: false,
+        is_topic_manager: false,
+        can_create_collections: true,
+        preferred_site_language_code: null,
+        username: 'tester',
+        email: 'test@test.com',
+        user_is_logged_in: false
+      };
+
+      userBackendApiService.getProfileImageDataUrlAsync().then((dataUrl) => {
+        expect(dataUrl).toBe(urlInterpolationService.getStaticImageUrl(
+          '/avatar/user_blue_72px.webp'));
+      });
+      var req = httpTestingController.expectOne('/userinfohandler');
+      expect(req.request.method).toEqual('GET');
+      req.flush(sampleUserInfoBackendObject);
+
+      flushMicrotasks();
+    }));
+
+  it('should return the login url', fakeAsync(() => {
+    var loginUrl = '/login';
+    var currentUrl = 'home';
+
+    userBackendApiService.getLoginUrlAsync().then((dataUrl) => {
+      expect(dataUrl).toBe(loginUrl);
+    });
+    var req = httpTestingController.expectOne(
+      '/url_handler?current_url=' + currentUrl);
+    expect(req.request.method).toEqual('GET');
+    req.flush({login_url: loginUrl});
+
+    flushMicrotasks();
+  }));
+
+  it('should set a profile image data url', fakeAsync(() => {
+    var newProfileImageDataurl = '/avatar/x.png';
+    userBackendApiService.setProfileImageDataUrlAsync(newProfileImageDataurl).then(
+      (response) => {
+        expect(response.profile_picture_data_url).toBe(
+          newProfileImageDataurl);
+      }
+    );
+    var req = httpTestingController.expectOne('/preferenceshandler/data');
+    expect(req.request.method).toEqual('PUT');
+    req.flush({profile_picture_data_url: newProfileImageDataurl});
+
+    flushMicrotasks();
+  }));
+
+  it('should handle when set profile image data url is reject',
+    fakeAsync(() => {
+      var newProfileImageDataurl = '/avatar/x.png';
+      var errorMessage = 'It\'s not possible to set a new profile image data';
+      userBackendApiService.setProfileImageDataUrlAsync(newProfileImageDataurl)
+        /* eslint-disable dot-notation */
+        .catch((error) => {
+        /* eslint-enable dot-notation */
+          expect(error.data).toEqual(errorMessage);
+        });
+      var req = httpTestingController.expectOne('/preferenceshandler/data');
+      expect(req.request.method).toEqual('PUT');
+      req.flush(errorMessage);
+
+      flushMicrotasks();
+    }));
+
+  it('should return user community rights data', fakeAsync(() => {
+    var sampleUserCommunityRightsDict = {
+      translation: ['hi'],
+      voiceover: [],
+      question: true
+    };
+
+    userBackendApiService.getUserCommunityRightsData().then(
+      (userCommunityRights) => {
+        expect(userCommunityRights).toEqual(sampleUserCommunityRightsDict);
+      });
+    var req = httpTestingController.expectOne(
+      '/usercommunityrightsdatahandler');
+    expect(req.request.method).toEqual('GET');
+    req.flush(sampleUserCommunityRightsDict);
+
+    flushMicrotasks();
+  }));
+
+  it('should not fetch userCommunityRights if it is was fetched before',
+    fakeAsync(() => {
+      var sampleUserCommunityRightsDict = {
+        translation: ['hi'],
+        voiceover: [],
+        question: true
+      };
+
+      userBackendApiService.getUserCommunityRightsData().then(
+        (userCommunityRights) => {
+          expect(userCommunityRights).toEqual(sampleUserCommunityRightsDict);
+          // Fetch userCommunityRightsInfo again.
+          userBackendApiService.getUserCommunityRightsData().then((
+              sameUserCommunityRights) => {
+            expect(sameUserCommunityRights).toEqual(
+              sampleUserCommunityRightsDict);
+          });
+        });
+      var req = httpTestingController.expectOne(
+        '/usercommunityrightsdatahandler');
+      expect(req.request.method).toEqual('GET');
+      req.flush(sampleUserCommunityRightsDict);
+
+      flushMicrotasks();
+    }));
+});
---
+++ core/templates/services/user-backend-api.service.ts
@@ -0,0 +1,157 @@
+// Copyright 2018 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Service for user data.
+ */
+
+import { downgradeInjectable } from '@angular/upgrade/static';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { WindowRef } from 'services/contextual/window-ref.service';
+
+import { UrlInterpolationService } from
+  'domain/utilities/url-interpolation.service';
+import { UrlService } from 'services/contextual/url.service.ts';
+import { UserInfoObjectFactory, UserInfo, IUserInfoBackendDict } from
+  'domain/user/UserInfoObjectFactory.ts';
+import { AppConstants } from 'app.constants';
+import { List } from 'lodash';
+
+interface subscriptionSummary {
+  'creator_picture_data_url': string;
+  'creator_username': string;
+  'creator_impact': number;
+}
+interface IPreferencesBackendDict {
+  'preferred_language_codes': string;
+  'preferred_site_language_code': string;
+  'preferred_audio_language_code': string;
+  'profile_picture_data_url': string;
+  'default_dashboard': string;
+  'user_bio': string;
+  'subject_interests': string;
+  'can_receive_email_updates': boolean;
+  'can_receive_editor_role_email': boolean;
+  'can_receive_feedback_message_email': boolean;
+  'can_receive_subscription_email': boolean;
+  'subscription_list': List<subscriptionSummary>;
+}
+interface IUrlBackendDict {
+  'login_url': string;
+}
+interface IUserCommunityRightsDataBackendDict {
+  'can_review_translation_for_language_codes': boolean;
+  'can_review_voiceover_for_language_codes': boolean;
+  'can_review_questions': boolean;
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserBackendApiService {
+  constructor(
+    private http: HttpClient,
+    private windowRef: WindowRef,
+    private urlInterpolationService: UrlInterpolationService,
+    private urlService: UrlService,
+    private userInfoObjectFactory: UserInfoObjectFactory) {}
+
+    private PREFERENCES_DATA_URL = '/preferenceshandler/data';
+    private USER_COMMUNITY_RIGHTS_DATA_URL = '/usercommunityrightsdatahandler';
+
+    private userInfo = null;
+    private userCommunityRightsInfo = null;
+
+    getUserInfoAsync(): Promise<UserInfo> {
+      if (this.urlService.getPathname() === '/signup') {
+        return Promise.resolve(this.userInfoObjectFactory.createDefault());
+      }
+      if (this.userInfo) {
+        return Promise.resolve(this.userInfo);
+      }
+      return this.http.get<IUserInfoBackendDict>(
+        '/userinfohandler').toPromise().then(
+        (backendDict) => {
+          if (backendDict.user_is_logged_in) {
+            this.userInfo =
+              this.userInfoObjectFactory.createFromBackendDict(
+                backendDict);
+            return Promise.resolve(this.userInfo);
+          } else {
+            return Promise.resolve(
+              this.userInfoObjectFactory.createDefault());
+          }
+        });
+    }
+    getProfileImageDataUrlAsync(): Promise<string> {
+      let profilePictureDataUrl = (
+        this.urlInterpolationService.getStaticImageUrl(
+          AppConstants.DEFAULT_PROFILE_IMAGE_PATH));
+      return this.getUserInfoAsync().then(
+        (userInfo) => {
+          if (userInfo.isLoggedIn()) {
+            return this.http.get<IPreferencesBackendDict>(
+              '/preferenceshandler/profile_picture'
+            ).toPromise().then(
+              (backendDict) => {
+                if (backendDict.profile_picture_data_url) {
+                  profilePictureDataUrl =
+                    backendDict.profile_picture_data_url;
+                }
+                return profilePictureDataUrl;
+              });
+          } else {
+            return Promise.resolve(profilePictureDataUrl);
+          }
+        });
+    }
+    setProfileImageDataUrlAsync(newProfileImageDataUrl: string):
+      Promise<IPreferencesBackendDict> {
+      let putData = {
+        update_type: 'profile_picture_data_url',
+        data: newProfileImageDataUrl
+      };
+      return this.http.put<IPreferencesBackendDict>(
+        this.PREFERENCES_DATA_URL, putData).toPromise();
+    }
+    getLoginUrlAsync(): Promise<string> {
+      let urlParameters = {
+        current_url: this.windowRef.nativeWindow.location.pathname
+      };
+      return this.http.get<IUrlBackendDict>('/url_handler',
+        { params: urlParameters }).toPromise().then(
+        (backendDict) => {
+          return backendDict.login_url;
+        });
+    }
+    getUserCommunityRightsData():
+      Promise<IUserCommunityRightsDataBackendDict> {
+      if (this.userCommunityRightsInfo) {
+        return Promise.resolve(this.userCommunityRightsInfo);
+      } else {
+        return this.http.get<IUserCommunityRightsDataBackendDict>(
+          this.USER_COMMUNITY_RIGHTS_DATA_URL).toPromise().then(
+          (backendDict) => {
+            this.userCommunityRightsInfo = backendDict;
+            return Promise.resolve(this.userCommunityRightsInfo);
+          });
+      }
+    }
+}
+
+angular.module('oppia').factory(
+  'UserBackendApiService',
+  downgradeInjectable(UserBackendApiService));
