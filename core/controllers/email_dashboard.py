dd47a6f4423f277344d772351d663fcb6aecee76
Kevin Zhang <kevinzhangucla@google.com>
2020-07-21 12:44:15 -0700

    Remove unused import

--- core/controllers/email_dashboard.py
+++ core/controllers/email_dashboard.py
@@ -17,8 +17,6 @@
 from __future__ import absolute_import  # pylint: disable=import-only-modules
 from __future__ import unicode_literals  # pylint: disable=import-only-modules
 
-import logging
-
 from core.controllers import acl_decorators
 from core.controllers import base
 from core.domain import email_manager
