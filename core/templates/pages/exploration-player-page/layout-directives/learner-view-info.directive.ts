abbd21d05710efed90bc97cc946d2bc98a0e7301
Madhav Sainanee <msainanee@google.com>
2020-07-24 22:58:26 +0530

    Changed refs

--- core/templates/pages/exploration-player-page/layout-directives/learner-view-info.directive.ts
+++ core/templates/pages/exploration-player-page/layout-directives/learner-view-info.directive.ts
@@ -17,7 +17,7 @@
  * footer.
  */
 
-require('components/profile-link-directives/profile-link-image.directive.ts');
+require('components/profile-link-directives/profile-link-image.component.ts');
 require(
   'components/common-layout-directives/common-elements/' +
   'sharing-links.component.ts');
