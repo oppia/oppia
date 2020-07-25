7e134a15bb123be1622ab4cef16292e35d5da28c
Nishant Mittal <mittalnishant14@outlook.com>
2020-07-25 15:26:34 +0530

    Fix #9348: Remove I from interfaces and types (#10017)
    * Remove I from interfaces and types
    
    * remove ts-ignore
    
    * Apply suggestions from code review
    
    Co-authored-by: Vojtěch Jelínek <vojtin.j@gmail.com>
    
    * reviews
    
    Co-authored-by: Vojtěch Jelínek <vojtin.j@gmail.com>

--- core/templates/domain/exploration/VoiceoverObjectFactory.ts
+++ core/templates/domain/exploration/VoiceoverObjectFactory.ts
@@ -16,7 +16,7 @@
  * @fileoverview Factory for creating new frontend instances of
  * Voiceover domain objects.
  */
-export interface IVoiceoverDict {
+export interface VoiceoverBackendDict {
   'duration_secs': number;
   'filename': string;
   'file_size_bytes': number;
@@ -53,7 +53,7 @@ export class Voiceover {
     return this.fileSizeBytes / NUM_BYTES_IN_MB;
   }
 
-  toBackendDict(): IVoiceoverDict {
+  toBackendDict(): VoiceoverBackendDict {
     return {
       filename: this.filename,
       file_size_bytes: this.fileSizeBytes,
@@ -73,7 +73,7 @@ export class VoiceoverObjectFactory {
   }
 
   createFromBackendDict(
-      translationBackendDict: IVoiceoverDict): Voiceover {
+      translationBackendDict: VoiceoverBackendDict): Voiceover {
     return new Voiceover(
       translationBackendDict.filename,
       translationBackendDict.file_size_bytes,
