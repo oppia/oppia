f98c5b2e9d9bbcc85a8b03ebd88abe34c86267cd
jimbyo <35385366+jimbyo@users.noreply.github.com>
2020-07-26 11:50:11 -0700

    Milestone 2.3: Achieve scores of 100% in lighthouse audits on the Notification_dashboard, Privacy-policy, and Practice_session pages (#9950)
    Achieve scores of 100% in lighthouse audits on the Notification_dashboard, Privacy-policy, and Practice_session pages

--- core/templates/components/question-directives/question-player/question-player.directive.ts
+++ core/templates/components/question-directives/question-player/question-player.directive.ts
@@ -194,8 +194,9 @@ angular.module('oppia').directive('questionPlayer', [
               srcset="${getStaticImageUrl('/icons/rocket@2x.webp')}">
               <source type="image/png" 
               srcset="${getStaticImageUrl('/icons/rocket@2x.png')}">
-              <img class="action-button-icon" src="
-              ${getStaticImageUrl('/icons/rocket@2x.png')}"/>
+              <img alt=""
+                   class="action-button-icon" 
+                   src="${getStaticImageUrl('/icons/rocket@2x.png')}"/>
               </picture>`;
             } else if (actionButtonType === 'RETRY_SESSION') {
               iconHtml = '<i class="material-icons md-36 ' +
