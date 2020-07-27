3fbcc37352602cfa8d0bdad09909150599e73758
shavavo <shavavo@google.com>
2020-07-27 14:18:59 -0400

    import

--- core/domain/question_domain.py
+++ core/domain/question_domain.py
@@ -19,6 +19,7 @@
 from __future__ import absolute_import  # pylint: disable=import-only-modules
 from __future__ import unicode_literals  # pylint: disable=import-only-modules
 
+import copy
 import datetime
 
 from constants import constants
