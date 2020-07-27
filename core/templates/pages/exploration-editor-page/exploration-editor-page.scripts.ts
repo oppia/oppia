8c955730e37278c6092566e52cb0b778793a959b
Mariana Zangrossi <mari.zangue@protonmail.com>
2020-07-27 10:07:54 -0300

    Fix changes reverted due to unit tests flakyness

--- core/templates/pages/exploration-editor-page/exploration-editor-page.scripts.ts
+++ core/templates/pages/exploration-editor-page/exploration-editor-page.scripts.ts
@@ -28,7 +28,7 @@ require(
   'editor-navbar-breadcrumb.directive.ts');
 require(
   'pages/exploration-editor-page/editor-navigation/' +
-  'editor-navigation.directive.ts');
+  'editor-navigation.component.ts');
 require(
   'pages/exploration-editor-page/exploration-save-and-publish-buttons/' +
   'exploration-save-and-publish-buttons.directive.ts');
