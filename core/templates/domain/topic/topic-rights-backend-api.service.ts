1f7c7651c9d571e64cf0879713a93992246c33f7
Madhav Sainanee <msainanee@google.com>
2020-07-28 04:50:22 +0530

    Fixed linting

--- core/templates/domain/topic/topic-rights-backend-api.service.ts
+++ core/templates/domain/topic/topic-rights-backend-api.service.ts
@@ -20,14 +20,12 @@ import { downgradeInjectable } from '@angular/upgrade/static';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { ITopicRightsBackendDict, TopicRightsObjectFactory } from
+import { TopicRightsBackendDict, TopicRightsObjectFactory } from
   'domain/topic/TopicRightsObjectFactory';
 import { TopicDomainConstants } from
   'domain/topic/topic-domain.constants';
 import { UrlInterpolationService } from
   'domain/utilities/url-interpolation.service';
-
-import cloneDeep from 'lodash/cloneDeep';
 
 @Injectable({
   providedIn: 'root'
@@ -47,7 +45,7 @@ export class TopicRightsBackendApiService {
       TopicDomainConstants.TOPIC_RIGHTS_URL_TEMPLATE, {
         topic_id: topicId
       });
-    this.http.get<ITopicRightsBackendDict>(topicRightsUrl).toPromise().then(
+    this.http.get<TopicRightsBackendDict>(topicRightsUrl).toPromise().then(
       (response) => {
         let res =
           this.topicRightsObjectFactory.createFromBackendDict(response);
