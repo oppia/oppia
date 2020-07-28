4710944db25beca0d33b86e55c76615a76992a09
jimbyo <jimz@uci.edu>
2020-07-28 03:12:34 -0700

    fix lints

--- core/tests/protractor_utils/waitFor.js
+++ core/tests/protractor_utils/waitFor.js
@@ -126,7 +126,7 @@ var newTabToBeCreated = async function(errorMessage, urlToMatch) {
 var urlRedirection = async function(url) {
   // Checks that the current URL matches the expected text.
   await browser.wait(until.urlIs(url), DEFAULT_WAIT_TIME_MSECS,
-  'URL redirection took too long');
+    'URL redirection took too long');
 };
 
 var visibilityOfInfoToast = async function(errorMessage) {
