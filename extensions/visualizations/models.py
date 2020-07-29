9cd6070b154bc7d6af81120f694c474051db6311
Brian Rodriguez <brian@brianrodri.com>
2020-07-29 18:10:06 -0400

    Deprecate BarChart visualization (#10095)
    * initial commit
    
    * hit 100% code coverage

--- extensions/visualizations/models.py
+++ extensions/visualizations/models.py
@@ -76,24 +76,6 @@ class BaseVisualization(python_utils.OBJECT):
                 (self.id, self.addressed_info_is_supported))
 
 
-class BarChart(BaseVisualization):
-    """A visualization representing a bar chart."""
-
-    _OPTIONS_SPECS = [{
-        'name': 'x_axis_label',
-        'description': 'The label for the x-axis.',
-        'schema': {
-            'type': 'unicode',
-        },
-    }, {
-        'name': 'y_axis_label',
-        'description': 'The label for the y-axis',
-        'schema': {
-            'type': 'unicode',
-        },
-    }]
-
-
 class ClickHexbins(BaseVisualization):
     """A visualization which overlays an image with a hexagonal grouping of
     clicks.
