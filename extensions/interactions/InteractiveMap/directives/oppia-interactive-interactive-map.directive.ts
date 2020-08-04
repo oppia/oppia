73d17c14507b6f92cfe9426e5fb8ce2663a092c4
Nishant Mittal <mittalnishant14@outlook.com>
2020-08-04 20:18:43 +0530

    Load leaflet using webpack

--- extensions/interactions/InteractiveMap/directives/oppia-interactive-interactive-map.directive.ts
+++ extensions/interactions/InteractiveMap/directives/oppia-interactive-interactive-map.directive.ts
@@ -20,7 +20,7 @@
  * followed by the name of the arg.
  */
 
-require('interactions/uiLeafletRequires.ts');
+require('third-party-imports/leaflet.import');
 
 require('domain/utilities/browser-checker.service.ts');
 require('domain/utilities/url-interpolation.service.ts');
