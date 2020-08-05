982872d1dbd67976ee918da92d2bc482b543aba1
shavavo <shavavo@google.com>
2020-08-05 11:34:17 -0400

    fixed backend test

--- assets/constants.ts
+++ assets/constants.ts
@@ -5269,7 +5269,6 @@ export = {
         "default_outcome": {}
       }
     },
-    "next_interaction_id_index": 0,
     "solicit_answer_details": false,
     "written_translations": {
       "translations_mapping": {
