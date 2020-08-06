f7ac214bc99c8187a2804dbbabc103c68de8190a
kaylahardie <kaylahardie7@gmail.com>
2020-08-06 03:04:55 -0700

    Fix #9636: Marks translation suggestions corresponding to a topic rejected whenever the topic is deleted. (#10022)
    * marks translation suggestions rejected when a topic is deleted.
    
    * fixed whitespace.
    
    * Improved documentation.
    
    * Made testing code more modular for future tests.
    
    * Minor word change.
    
    * Fixed it to make less storage calls.
    
    * Fixed whitespace.
    
    * Fixed one more whitepsace
    
    * Added check and test for the empty exp_id case.
    
    * Improved the get_translation_suggestions_with_exp_ids test and responded to reviewers comments.
    
    * Fixed whitespace.
    
    * Changed the return of get_content_html for MockExploration because it had unused arguments.
    
    * Added a comment to be more clear for get_content_html.
    
    * Removed corresponding-topic-id.
    
    * Split up tests in gae_models.py.
    
    * Aligned bracket.
    
    * Made testing messages more clear by adding a suggestion_id to the messages.
    
    * Fixed whitespace, added suggestion_id check.
    
    * Made changes and gae_models_test still pass.
    
    * feedback_services, suggestion_services tests pass.
    
    * Addressed reviewers comments.
    
    * Fixed lint errors.
    
    * Condensed brackets.
    
    * Fixed backend coverage.
    
    * Addressed reviewers comments.
    
    * Adjusted tests for gae_models.
    
    * Fixed whitespace.
    
    * Added return for create_messages.
    
    * Fixing an error.
    
    * Changed variable name.
    
    * Changed another variable name.
    
    * Addressed reviewers comments.
    
    * Changed docstring.
    
    * Changed docstring for one of the tests to be more concise.
    
    * Addressed final reviewers comments.

--- core/domain/story_services.py
+++ core/domain/story_services.py
@@ -32,6 +32,7 @@ from core.domain import opportunity_services
 from core.domain import rights_manager
 from core.domain import story_domain
 from core.domain import story_fetchers
+from core.domain import suggestion_services
 from core.domain import topic_fetchers
 from core.platform import models
 import feconf
@@ -521,9 +522,11 @@ def delete_story(committer_id, story_id, force_deletion=False):
     # force_deletion is True or not).
     delete_story_summary(story_id)
 
-    # Delete the opportunities available related to the exploration used in the
-    # story.
+    # Delete the opportunities available and reject the suggestions related to
+    # the exploration used in the story.
     opportunity_services.delete_exploration_opportunities(exp_ids)
+    suggestion_services.auto_reject_translation_suggestions_for_exp_ids(
+        exp_ids)
 
 
 def delete_story_summary(story_id):
