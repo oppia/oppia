fe8ab304525110436a227e4ac78ba8648a8f11b1
iamprayush <iamprayush@gmail.com>
2020-08-09 01:37:29 +0530

    Fixed nits.

--- core/domain/exp_jobs_one_off.py
+++ core/domain/exp_jobs_one_off.py
@@ -253,7 +253,7 @@ class ExplorationValidityJobManager(jobs.BaseMapReduceOneOffJobManager):
 
 
 class ExplorationMigrationAuditJob(jobs.BaseMapReduceOneOffJobManager):
-    """A reusbale one-off job for testing exploration migration from any
+    """A reusabale one-off job for testing exploration migration from any
     exploration schema version to the latest. This job runs the state
     migration, but does not commit the new exploration to the store.
     """
