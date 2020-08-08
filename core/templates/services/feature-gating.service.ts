48990bdebe8c76976cafa1ff8cb9c0eccd0e1b43
Chu Zhu <chuz@google.com>
2020-08-07 21:01:46 -0400

    Add guard for initialization, add docstring

--- core/templates/services/feature-gating.service.ts
+++ core/templates/services/feature-gating.service.ts
@@ -56,14 +56,93 @@ export class FeatureGatingService {
   // TODO(#9154): Remove static when migration is complete.
   static _initializedWithError = false;
 
+  // TODO(#9154): Remove static when migration is complete.
+  static initializationPromise: Promise<void> = null;
+
   constructor(
       private clientContextObjectFactory: ClientContextObjectFactory,
       private featureGatingBackendApiService: FeatureGatingBackendApiService,
       private featureFlagResultsObjectFactory: FeatureFlagResultsObjectFactory,
       private i18nLanguageCodeService: I18nLanguageCodeService,
-      private windowRef: WindowRef) {}
+      private windowRef: WindowRef) {
+    this.initialize();
+  }
 
+  /**
+   * Inializes the FeatureGatingService, it's guaranteed that it's only
+   * inialized once.
+   *
+   * @returns {Promise} - A promise that is resolved when the initialization
+   * is done.
+   */
   async initialize(): Promise<void> {
+    if (!FeatureGatingService.initializationPromise) {
+      FeatureGatingService.initializationPromise = this._initialize();
+    }
+    return FeatureGatingService.initializationPromise;
+  }
+
+  /**
+   * Gets the value of the feature flag.
+   *
+   * @param {FeatureNames} name - Name of the feature.
+   *
+   * @returns {boolean} - True if the feature is enabled.
+   * @throws {Error} - If this method is called before inialization.
+   */
+  isFeatureEnabled(name: FeatureNames): boolean {
+    if (FeatureGatingService.featureFlagResults) {
+      return FeatureGatingService.featureFlagResults.isFeatureEnabled(name);
+    } else if (FeatureGatingService._initializedWithError) {
+      return false;
+    } else {
+      throw new Error('The feature gating service has not been initialized.');
+    }
+  }
+
+  /**
+   * Checks if there's any error, e.g. request timeout, during initialization.
+   *
+   * @returns {boolean} - True if there is any error during initialization.
+   */
+  get initialzedWithError(): boolean {
+    return FeatureGatingService._initializedWithError;
+  }
+
+  /**
+   * Detects the type of browser from it's userAgent.
+   *
+   * @returns {string} - The name of the browser that is being used.
+   */
+  detectBrowserType(): string {
+    const ua = this.windowRef.nativeWindow.navigator.userAgent;
+
+    if (ua.includes('edg') || ua.includes('Edge')) {
+      return 'Edge';
+    }
+    if (ua.includes('Chrome')) {
+      return 'Chrome';
+    }
+    if (ua.includes('Firefox')) {
+      return 'Firefox';
+    }
+    if (ua.includes('Safari')) {
+      return 'Safari';
+    }
+
+    return 'Others';
+  }
+
+  /**
+   * Initializes the FeatureGatingService. It first checks if there is
+   * previously saved feature flag result in the sessionStorage, if there is
+   * and the result is still valid, it will be loaded. Otherwise it sends
+   * a request to the server to get the feature flag result.
+   *
+   * @returns {Promise} - A promise that is resolved when the initialization
+   * is done.
+   */
+  private async _initialize(): Promise<void> {
     try {
       const item = this.loadSavedResults();
       if (item && this.validateSavedResults(item)) {
@@ -85,44 +164,15 @@ export class FeatureGatingService {
     }
   }
 
-  isFeatureEnabled(name: FeatureNames): boolean {
-    if (FeatureGatingService.featureFlagResults) {
-      return FeatureGatingService.featureFlagResults.isFeatureEnabled(name);
-    } else if (FeatureGatingService._initializedWithError) {
-      return false;
-    } else {
-      throw new Error('The feature gating service has not been initialized.');
-    }
-  }
-
-  get initialzedWithError(): boolean {
-    return FeatureGatingService._initializedWithError;
-  }
-
-  detectBrowserType(): string {
-    const ua = this.windowRef.nativeWindow.navigator.userAgent;
-
-    if (ua.includes('edg') || ua.includes('Edge')) {
-      return 'Edge';
-    }
-    if (ua.includes('Chrome')) {
-      return 'Chrome';
-    }
-    if (ua.includes('Firefox')) {
-      return 'Firefox';
-    }
-    if (ua.includes('Safari')) {
-      return 'Safari';
-    }
-
-    return 'Others';
-  }
-
   private async loadFeatureFlagsFromServer(): Promise<FeatureFlagResults> {
     const context = this.generateClientContext();
     return this.featureGatingBackendApiService.fetchFeatureFlags(context);
   }
 
+  /**
+   * Saves the results in sessionStorage, along with current timestamp and
+   * the current session id.
+   */
   private saveResults(): void {
     const item = {
       timestamp: this.getCurrentTimestamp(),
@@ -134,11 +184,20 @@ export class FeatureGatingService {
       FeatureGatingService.SESSION_STORAGE_KEY, JSON.stringify(item));
   }
 
+  /**
+   * Clears results from the sessionStorage, if any.
+   */
   private clearSavedResults(): void {
     this.windowRef.nativeWindow.sessionStorage.removeItem(
       FeatureGatingService.SESSION_STORAGE_KEY);
   }
 
+  /**
+   * Reads and parse feature flag results from the sessionStorage.
+   *
+   * @returns {FeatureFlagsCacheItem|null} - Saved results along with timestamp
+   * and session id. Null if there isn't any saved result.
+   */
   private loadSavedResults(): FeatureFlagsCacheItem | null {
     const savedStr = this.windowRef.nativeWindow.sessionStorage.getItem(
       FeatureGatingService.SESSION_STORAGE_KEY);
@@ -155,6 +214,14 @@ export class FeatureGatingService {
     return null;
   }
 
+  /**
+   * Validates the result saved in sessionStorage.
+   *
+   * @param {FeatureFlagsCacheItem} item - The result item loaded from
+   * sessionStorage.
+   *
+   * @returns {boolean} - True if the result is valid and can be directly used.
+   */
   private validateSavedResults(item: FeatureFlagsCacheItem): boolean {
     if (this.getCurrentTimestamp() - item.timestamp >
         FeatureGatingService.SESSION_STORAGE_CACHE_TTL) {
@@ -166,6 +233,13 @@ export class FeatureGatingService {
     return true;
   }
 
+  /**
+   * Generates context containing the client side information required to
+   * request feature flag values.
+   *
+   * @returns {ClientContext} - The ClientContext instance containing required
+   * client information.
+   */
   private generateClientContext(): ClientContext {
     const clientType = 'Web';
     const appVersion = null;
@@ -177,6 +251,11 @@ export class FeatureGatingService {
       clientType, broswerType, appVersion, userLocale);
   }
 
+  /**
+   * Parse session id from cookies.
+   *
+   * @returns {string|null} - The value of the cookie representing session id.
+   */
   private getSessionIdFromCookie(): string | null {
     const cookieStrs = this.windowRef.nativeWindow.document.cookie.split('; ');
     const cookieMap = new Map(
@@ -192,6 +271,11 @@ export class FeatureGatingService {
     return null;
   }
 
+  /**
+   * Gets the current timestamp.
+   *
+   * @returns {number} - The current timestamp.
+   */
   private getCurrentTimestamp(): number {
     return Date.now();
   }
