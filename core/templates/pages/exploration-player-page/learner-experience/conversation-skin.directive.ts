2430c537df243616dcd27e69941187c6c51f45d4
Abhinav Gopal <abhinavgopal@google.com>
2020-08-15 04:07:02 +0000

    lint

--- core/templates/pages/exploration-player-page/learner-experience/conversation-skin.directive.ts
+++ core/templates/pages/exploration-player-page/learner-experience/conversation-skin.directive.ts
@@ -1193,7 +1193,7 @@ angular.module('oppia').directive('conversationSkin', [
           };
 
           $scope.clickContinueButtonFn = function(interactionId) {
-            // This is the processed interactionId from CommandExecutor
+            // This is the processed interactionId from CommandExecutor.
             if (interactionId === 'CONTINUE') {
               CurrentInteractionService.submitAnswer();
             } else {
