141bdfcca6b5bb38a02c5664dc3cef053359f8d6
Kevin Zhang <kevinzhangucla@google.com>
2020-08-16 15:12:25 -0700

    Fix lint

--- scripts/common_test.py
+++ scripts/common_test.py
@@ -808,7 +808,6 @@ class CommonTests(test_utils.GenericTestBase):
         with swap_os_check, windows_not_supported_exception:
             common.start_redis_server()
 
-
     def test_windows_os_throws_exception_when_stopping_redis_server(self):
         def mock_is_windows_os():
             return True
@@ -821,7 +820,6 @@ class CommonTests(test_utils.GenericTestBase):
 
         with swap_os_check, windows_not_supported_exception:
             common.stop_redis_server()
-
 
     def test_start_and_stop_server_calls_are_called(self):
         # Test that starting the server calls subprocess.call().
@@ -844,7 +842,7 @@ class CommonTests(test_utils.GenericTestBase):
                     return '', ''
             return Ret()
 
-        def mock_wait_for_port_to_be_open(*args):
+        def mock_wait_for_port_to_be_open(port): # pylint: disable=unused-argument
             return
 
         swap_call = self.swap(subprocess, 'call', mock_call)
