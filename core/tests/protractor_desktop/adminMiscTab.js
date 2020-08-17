58206d586750203ce1aebb031d3048ac5845d636
Krishna Rama Rao <krishnarao.2208@gmail.com>
2020-08-17 13:33:47 +0400

    editing ci check script file

--- core/tests/protractor_desktop/adminMiscTab.js
+++ core/tests/protractor_desktop/adminMiscTab.js
@@ -41,10 +41,7 @@ describe('Admin misc tab', function() {
     await users.createAndLoginAdminUser(
       'miscTabTester@miscTab.com', 'miscTabTester');
     await topicsAndSkillsDashboardPage.get();
-    await topicsAndSkillsDashboardPage.createTopic(topicName,
-      'admin-misc-tab-test', 'A topic to test the Admin Page\'s Misc Tab',
-      true);
-    await topicsAndSkillsDashboardPage.editTopic(topicName);
+    await topicsAndSkillsDashboardPage.createTopic(topicName, 'admin-misc-tab-test', 'A topic to test the Admin Page\'s Misc Tab', false);
     var url = await browser.getCurrentUrl();
     topicId = url.split('/')[4].substring(0, 12);
     await adminPage.get();
