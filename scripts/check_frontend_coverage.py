8122eb3762c824020f864297ba8ede313a3c5ad2
Kevin Thomas <kevintab@tutanota.com>
2020-08-20 11:40:15 +0530

    Add topic-info-tab.directive to coverage whitelist

--- scripts/check_frontend_coverage.py
+++ scripts/check_frontend_coverage.py
@@ -374,6 +374,7 @@ NOT_FULLY_COVERED_FILENAMES = [
     'topic-editor-page.component.ts',
     'topic-editor-state.service.ts',
     'topic-editor-stories-list.directive.ts',
+    'topic-info-tab.directive.ts',
     'topic-questions-tab.directive.ts',
     'topic-selector.directive.ts',
     'topic-summary-tile.directive.ts',
