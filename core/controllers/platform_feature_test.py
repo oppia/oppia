9e02cb69c2b81fe8a44d9729d7ae877e52262fa3
Zhu Chu <chuz@google.com>
2020-08-20 14:39:35 -0400

    Implement controllers for dynamic feature gating (#10021)
    * Add storage models
    
    * Rename models, add validation jobs and change domain object
    
    * Fix method naming
    
    * Fix import error
    
    * Fix test issues
    
    * Improve coverage, fix linting errors, add codeowner
    
    * Remove debug statements
    
    * Add code owner entries
    
    * Change validator id regex
    
    * Add domain objects
    
    * Add docstrings
    
    * Improve test coverage
    
    * Added requested changes
    
    * Fix test errors
    
    * Fix test errors
    
    * Add requested changes
    
    * Change code owners
    
    * Add requested changes
    
    * Add missing docstrings, fix test errors
    
    * Add service methods
    
    * Add docstrings
    
    * Improve test
    
    * Add schema_version, improve docstrings
    
    * Styling
    
    * Fix audit job tests
    
    * Add schema version
    
    * Add method, fix test errors
    
    * Add necessary imports
    
    * Styling, fix codeowners
    
    * Styling
    
    * Add controllers
    
    * Add method for feature, add dummy instance for demo
    
    * Add test
    
    * Fix linting errors on keyword args
    
    * Add requested changes
    
    * Add requested changes
    
    * Refactor filter to use (op, value) schema
    
    * Add requested change
    
    * Add browser type check
    
    * Change docstring
    
    * Add rquested changes
    
    * Change test class name
    
    * Move Registry
    
    * Add requested changes
    
    * Improve test case names
    
    * Fix docstring
    
    * Fix test error
    
    * Improve test naming, add requested changes
    
    * Rename helper method
    
    * Remove metadata object, add comment for tests
    
    * Add app_version_flavor filter
    
    * Change none value handling
    
    * Sync interface, refactor and improve test
    
    * Edit codeowners
    
    * Fix linter error
    
    * Sync interfaces, fix codeowner
    
    * Fix docstrings
    
    * Add requested changes
    
    * Add comparison support to flavor filters
    
    * Fix bug with None version
    
    * Change expected payload format
    
    * Improve test and docstrings
    
    * Fix import sorting
    
    * Fix flavor ordering, fix docstring
    
    * Add requested change
    
    * Sync interface
    
    * Rename files, improve comment & test
    
    * Add requested changes
    
    * Add interface description
    
    * Link todo with issue
    
    * Add requested changes
    
    * Improve interface
    
    * Rename files & fix typo
    
    * Rename files
    
    * Sync interface
    
    * Sync renaming
    
    * Fix naming
    
    * Improve error handling & add test for invalid rule update
    
    * Fix linting error
    
    * Add requested changes
    
    * Add requested changes
    
    * Fix typo
    
    * Fix errors after merging develop
    
    * Add requested changes
    
    * Add requested changes

---
+++ core/controllers/platform_feature_test.py
@@ -0,0 +1,106 @@
+# Copyright 2020 The Oppia Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for platform feature evaluation handler."""
+
+from __future__ import absolute_import  # pylint: disable=import-only-modules
+from __future__ import unicode_literals  # pylint: disable=import-only-modules
+
+from constants import constants
+from core.domain import caching_services
+from core.domain import platform_feature_services as feature_services
+from core.domain import platform_parameter_domain as param_domain
+from core.domain import platform_parameter_registry as registry
+from core.tests import test_utils
+
+
+class PlatformFeaturesEvaluationHandlerTest(test_utils.GenericTestBase):
+    """Tests for the PlatformFeaturesEvaluationHandler."""
+
+    def setUp(self):
+        super(PlatformFeaturesEvaluationHandlerTest, self).setUp()
+
+        self.signup(self.OWNER_EMAIL, self.OWNER_USERNAME)
+        self.user_id = self.get_user_id_from_email(self.OWNER_EMAIL)
+
+        self.original_registry = registry.Registry.parameter_registry
+        self.original_feature_list = feature_services.ALL_FEATURES_LIST
+        self.original_feature_name_set = feature_services.ALL_FEATURES_NAMES_SET
+
+        param_names = ['parameter_a', 'parameter_b']
+        caching_services.delete_multi(
+            caching_services.CACHE_NAMESPACE_PLATFORM_PARAMETER, None,
+            param_names)
+
+        registry.Registry.parameter_registry.clear()
+        self.dev_feature = registry.Registry.create_platform_parameter(
+            'parameter_a', 'parameter for test', 'bool',
+            is_feature=True, feature_stage=param_domain.FEATURE_STAGES.dev)
+        self.prod_feature = registry.Registry.create_platform_parameter(
+            'parameter_b', 'parameter for test', 'bool',
+            is_feature=True, feature_stage=param_domain.FEATURE_STAGES.prod)
+        registry.Registry.update_platform_parameter(
+            self.prod_feature.name, self.user_id, 'edit rules',
+            [
+                {
+                    'filters': [
+                        {
+                            'type': 'server_mode',
+                            'conditions': [['=', param_domain.SERVER_MODES.dev]]
+                        }
+                    ],
+                    'value_when_matched': True
+                }
+            ]
+        )
+
+        feature_services.ALL_FEATURES_LIST = param_names
+        feature_services.ALL_FEATURES_NAMES_SET = set(param_names)
+
+    def tearDown(self):
+        super(PlatformFeaturesEvaluationHandlerTest, self).tearDown()
+
+        feature_services.ALL_FEATURES_LIST = self.original_feature_list
+        feature_services.ALL_FEATURES_NAMES_SET = self.original_feature_name_set
+        registry.Registry.parameter_registry = self.original_registry
+
+    def test_get_dev_mode_android_client_returns_correct_flag_values(self):
+        with self.swap(constants, 'DEV_MODE', True):
+            result = self.get_json(
+                '/platform_features_evaluation_handler',
+                params={
+                    'client_type': 'Android',
+                    'app_version': '1.0.0',
+                    'user_locale': 'en',
+                }
+            )
+            self.assertEqual(
+                result,
+                {self.dev_feature.name: False, self.prod_feature.name: True})
+
+    def test_get_with_invalid_client_type_context_raises_400(self):
+        resp_dict = self.get_json(
+            '/platform_features_evaluation_handler',
+            params={
+                'client_type': 'Invalid',
+                'app_version': '1.0.0',
+                'user_locale': 'en',
+            },
+            expected_status_int=400
+        )
+        self.assertEqual(
+            resp_dict['error'],
+            'Invalid client type \'Invalid\', must be one of [u\'Web\', '
+            'u\'Android\'].'
+        )
