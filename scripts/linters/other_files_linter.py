de30051c8f44c092dc790a3aadcf5815796650eb
Anshul Hudda <anshulhudda.ssap@gmail.com>
2020-08-21 20:04:37 +0530

    Fixed docstring.

--- scripts/linters/other_files_linter.py
+++ scripts/linters/other_files_linter.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Lint checks for codeowner file."""
+"""Lint checks of other file types."""
 
 from __future__ import absolute_import  # pylint: disable=import-only-modules
 from __future__ import unicode_literals  # pylint: disable=import-only-modules
