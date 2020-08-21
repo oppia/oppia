7fa83ba5cf4decf9f95b54440a445c40e776fae2
Vojtech Jelinek <vojtin.j@gmail.com>
2020-08-21 15:18:32 +0200

    Upgrade lighthouse

--- scripts/run_lighthouse_tests.py
+++ scripts/run_lighthouse_tests.py
@@ -52,7 +52,7 @@ def cleanup():
 
     for p in processes_to_kill:
         common.kill_processes_based_on_regex(p)
-    build.set_constants_to_default()
+
     common.stop_redis_server()
 
 
@@ -109,8 +109,7 @@ def main():
     # We are using --source_maps here, so that we have at least one CI check
     # that builds using source maps in prod env. This is to ensure that
     # there are no issues while deploying oppia.
-    build.main(args=['--prod_env', '--source_maps'])
-    build.modify_constants(prod_env=True)
+    build.main(args=['--prod_env'])
     common.start_redis_server()
     start_google_app_engine_server()
     common.wait_for_port_to_be_open(GOOGLE_APP_ENGINE_PORT)
