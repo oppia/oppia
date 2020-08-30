7d06ad64bb3096f8e771bf4e851820abab604513
varun-tandon <vrntandon@gmail.com>
2020-08-30 13:13:08 -0700

    remove unused import

--- core/controllers/profile_test.py
+++ core/controllers/profile_test.py
@@ -18,7 +18,6 @@ from __future__ import absolute_import  # pylint: disable=import-only-modules
 from __future__ import unicode_literals  # pylint: disable=import-only-modules
 
 import datetime
-import os
 import re
 import zipfile
 
