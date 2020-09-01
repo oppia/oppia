f188b38ed7934608f4126ea07c43d4fa9ce4f43f
Kevin Thomas <kevintab@tutanota.com>
2020-09-01 16:25:32 +0530

    Introduce fields 'not_applicable_misconception_ids' for questions and 'misconception_ids' for question summaries (#10481)
    * introduce new fields for opt misconception validations
    
    * address comments
    
    * fix tests
    
    * fix backend tests
    
    * fix coverage issue
    
    * remove unused variable
    
    * address comment
    
    * address comment
    
    * address comments
    
    * fix lint failure
    
    * update doc string

--- core/templates/domain/question/QuestionObjectFactorySpec.ts
+++ core/templates/domain/question/QuestionObjectFactorySpec.ts
@@ -205,6 +205,7 @@ describe('Question object factory', function() {
         },
         solicit_answer_details: false
       },
+      inapplicable_misconception_ids: ['a-1', 'b-2'],
       language_code: 'en',
       version: 1
     };
@@ -221,6 +222,11 @@ describe('Question object factory', function() {
     sampleQuestion.setLinkedSkillIds(['skill_id1', 'skill_id2']);
     expect(sampleQuestion.getLinkedSkillIds()).toEqual(
       ['skill_id1', 'skill_id2']);
+    expect(sampleQuestion.getInApplicableMisconceptionIds()).toEqual(
+      ['a-1', 'b-2']);
+    sampleQuestion.setInApplicableMisconceptionIds(['abc-123']);
+    expect(sampleQuestion.getInApplicableMisconceptionIds()).toEqual(
+      ['abc-123']);
     var stateData = sampleQuestion.getStateData();
     expect(stateData.name).toEqual('question');
     expect(stateData.content.getHtml()).toEqual('Question 1');
@@ -240,6 +246,8 @@ describe('Question object factory', function() {
     var newQuestionBackendDict = sampleQuestion.toBackendDict(true);
     expect(newQuestionBackendDict.id).toEqual(null);
     expect(newQuestionBackendDict.linked_skill_ids).not.toBeDefined();
+    expect(newQuestionBackendDict.inapplicable_misconception_ids).toEqual(
+      ['a-1', 'b-2']);
     expect(newQuestionBackendDict.version).toEqual(0);
     expect(sampleQuestion.toBackendDict(false).id).toEqual('question_id');
   });
@@ -295,5 +303,7 @@ describe('Question object factory', function() {
     expect(sampleQuestion1.getStateData()).toEqual(state);
     expect(sampleQuestion1.getLinkedSkillIds()).toEqual(
       ['skill_id3', 'skill_id4']);
+    expect(sampleQuestion.getInApplicableMisconceptionIds()).toEqual(
+      ['a-1', 'b-2']);
   });
 });
