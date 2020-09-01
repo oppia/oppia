03a6fd8884d7fc04f9e83abed75f5beb1ad2112e
Kevin Zhang <kevinzhangucla@google.com>
2020-09-01 16:29:07 -0700

    Add mode

--- scripts/pre_push_hook.py
+++ scripts/pre_push_hook.py
@@ -444,7 +444,7 @@ def check_for_backend_python_library_inconsistencies():
             '   this option will run scripts.install_third_party to\n'
             '   regenerate the third_party/python_utils folder with\n'
             '   the correct dependencies.)\n\n'])
-        sys.stdin = python_utils.open_file('/dev/tty')
+        sys.stdin = python_utils.open_file('/dev/tty', 'r')
         while True:
             choice = python_utils.INPUT('Choose an option or Ctrl-C to exit: ')
             if choice == '2':
