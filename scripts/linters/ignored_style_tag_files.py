b06caa35357f85c92763edec29c96bdc61a0d227
Anshul Hudda <anshulhudda.ssap@gmail.com>
2020-09-04 00:56:12 +0530

    Addressed review comments

--- scripts/linters/ignored_style_tag_files.py
+++ scripts/linters/ignored_style_tag_files.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Excluded files from style check."""
+"""Excluded files from the inline style tag check."""
 
 from __future__ import absolute_import  # pylint: disable=import-only-modules
 from __future__ import unicode_literals  # pylint: disable=import-only-modules
