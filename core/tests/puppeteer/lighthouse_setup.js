b958077a4fc344976509595adddaf434e48ae303
jimbyo <jimz@uci.edu>
2020-09-04 15:32:00 -0700

    fix lints

--- core/tests/puppeteer/lighthouse_setup.js
+++ core/tests/puppeteer/lighthouse_setup.js
@@ -29,8 +29,9 @@ const networkIdle = 'networkidle0';
 var explorationEditorUrl = 'Exploration editor not loaded';
 var collectionEditorUrl = 'Collection editor not loaded';
 var topicEditorUrl = 'Topic editor not loaded';
-var _ = 'Story editor not loaded';
 var skillEditorUrl = 'Skill editor not loaded';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+var unusedStoryEditorURL = 'Story editor not loaded';
 
 var usernameInput = '.protractor-test-username-input';
 var agreeToTermsCheckBox = '.protractor-test-agree-to-terms-checkbox';
@@ -241,7 +242,7 @@ const unusedGetStoryEditorUrl = async function(browser, page) {
     await page.waitFor(5000);
     await page.click(confirmStoryCreationButton);
     await page.waitFor(15000);
-    _ = await page.url();
+    unusedGetStoryEditorUrl = await page.url();
   } catch (e) {
     // eslint-disable-next-line no-console
     console.log(e);
