39ddc63a4138e3a82f171b06ccd37e45c4f94224
Srijan Reddy <srijanreddy98@gmail.com>
2020-09-08 21:47:24 +0530

    Changes

--- core/templates/tests/unit-test-utils.ts
+++ core/templates/tests/unit-test-utils.ts
@@ -89,7 +89,7 @@ export const bootstrap = (
     // module. In order to closer emulate what happens in real application,
     // ensure AngularJS is bootstrapped inside the Angular zone.
     ngZone.run(() => upgrade.bootstrap(
-      // This throws "Type '($provide: any) => string' is not assignable to 
+      // This throws "Type '($provide: any) => string' is not assignable to
       // type 'string'."
       // @ts-ignore
       element, [failHardModule, ng1Module.name]));
