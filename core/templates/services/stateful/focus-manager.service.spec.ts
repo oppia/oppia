0fa47d426e1ab76a5de9d822e33b57af580ef9ee
Brian Rodriguez <brian@brianrodri.com>
2020-09-12 15:10:11 -0400

    use absolute import

--- core/templates/services/stateful/focus-manager.service.spec.ts
+++ core/templates/services/stateful/focus-manager.service.spec.ts
@@ -21,7 +21,7 @@ import { fakeAsync, flush, TestBed } from '@angular/core/testing';
 import { Subscription } from 'rxjs';
 
 import { AppConstants } from 'app.constants';
-import { FocusManagerService } from './focus-manager.service';
+import { FocusManagerService } from 'services/stateful/focus-manager.service';
 import { DeviceInfoService } from 'services/contextual/device-info.service';
 import { IdGenerationService } from 'services/id-generation.service';
 
