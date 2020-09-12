c39eb6a02f0352b3d6c9d6e253d617808944f344
Srijan Reddy <srijanreddy98@gmail.com>
2020-09-12 21:33:03 +0530

    Add test file helper

--- core/templates/domain/question/QuestionObjectFactory.ts
+++ core/templates/domain/question/QuestionObjectFactory.ts
@@ -19,16 +19,111 @@
 
 require('domain/state/StateObjectFactory.ts');
 
-import { StateBackendDict } from 'domain/state/StateObjectFactory';
+import { State } from 'domain/state/StateObjectFactory';
+import { InteractionSpecsConstants } from 'pages/interaction-specs.constants';
 
 export interface QuestionBackendDict {
   'id': string;
-  'question_state_data': StateBackendDict,
+  'question_state_data': State,
   'question_state_data_schema_version': number;
   'language_code': string;
   'version': number;
   'linked_skill_ids': string[];
   'inapplicable_skill_misconception_ids': string[];
+}
+
+export class Question {
+  private id: string;
+  private stateData: State;
+  private languageCode: string;
+  private version: number;
+  private linkedSkillIds: string[];
+  private inApplicableSkillMisconceptionIds: string[];
+
+  constructor(
+      id: string, stateData: State, languageCode: string,
+      version: number, linkedSkillIds: string[],
+      inApplicableSkillMisconceptionIds: string[]) {
+    this.id = id;
+    this.stateData = stateData;
+    this.languageCode = languageCode;
+    this.version = version;
+    this.linkedSkillIds = linkedSkillIds;
+    this.inApplicableSkillMisconceptionIds = inApplicableSkillMisconceptionIds;
+  }
+  static createDefaultQuestion(skillIds: string[]): Question {
+    /* eslint-enable dot-notation */
+    return new Question(
+      null, StateObjectFactory.createDefaultState(null),
+      DEFAULT_LANGUAGE_CODE, 1, skillIds, []);
+  };
+  getId(): string {
+    return this.id;
+  }
+
+  getStateData(): State {
+    return this.stateData;
+  }
+
+  setStateData(newStateData: State): void {
+    this.stateData = angular.copy(newStateData);
+  }
+
+  getLanguageCode(): string {
+    return this.languageCode;
+  }
+
+  setLanguageCode(languageCode: string): void {
+    this.languageCode = languageCode;
+  }
+
+  getVersion(): number {
+    return this.version;
+  }
+
+  getLinkedSkillIds(): string[] {
+    return this.linkedSkillIds;
+  }
+
+  setLinkedSkillIds(linkedSkillIds: string[]): void {
+    this.linkedSkillIds = linkedSkillIds;
+  }
+
+  getInApplicableSkillMisconceptionIds(): string[] {
+    return this.inApplicableSkillMisconceptionIds;
+  }
+
+  setInApplicableSkillMisconceptionIds(
+      inApplicableSkillMisconceptionIds: string[]): void {
+    this.inApplicableSkillMisconceptionIds = inApplicableSkillMisconceptionIds;
+  }
+
+  getValidationErrorMessage(): null | string {
+    var interaction = this.stateData.interaction;
+    if (interaction.id === null) {
+      return 'An interaction must be specified';
+    }
+    if (interaction.hints.length === 0) {
+      return 'At least 1 hint should be specified';
+    }
+    if (
+      !interaction.solution &&
+      InteractionSpecsConstants[interaction.id].can_have_solution) {
+      return 'A solution must be specified';
+    }
+    var answerGroups = this.stateData.interaction.answerGroups;
+    var atLeastOneAnswerCorrect = false;
+    for (var i = 0; i < answerGroups.length; i++) {
+      if (answerGroups[i].outcome.labelledAsCorrect) {
+        atLeastOneAnswerCorrect = true;
+        continue;
+      }
+    }
+    if (!atLeastOneAnswerCorrect) {
+      return 'At least one answer should be marked correct';
+    }
+    return null;
+  }
 }
 
 angular.module('oppia').factory('QuestionObjectFactory', [
@@ -47,48 +142,6 @@ angular.module('oppia').factory('QuestionObjectFactory', [
     };
 
     // ---- Instance methods ----
-
-    Question.prototype.getId = function() {
-      return this._id;
-    };
-
-    Question.prototype.getStateData = function() {
-      return this._stateData;
-    };
-
-    Question.prototype.setStateData = function(newStateData) {
-      this._stateData = angular.copy(newStateData);
-    };
-
-    Question.prototype.getLanguageCode = function() {
-      return this._languageCode;
-    };
-
-    Question.prototype.setLanguageCode = function(languageCode) {
-      this._languageCode = languageCode;
-    };
-
-    Question.prototype.getVersion = function() {
-      return this._version;
-    };
-
-    Question.prototype.getLinkedSkillIds = function() {
-      return this._linkedSkillIds;
-    };
-
-    Question.prototype.setLinkedSkillIds = function(linkedSkillIds) {
-      this._linkedSkillIds = linkedSkillIds;
-    };
-
-    Question.prototype.getInApplicableSkillMisconceptionIds = function() {
-      return this._inApplicableSkillMisconceptionIds;
-    };
-
-    Question.prototype.setInApplicableSkillMisconceptionIds = function(
-        inApplicableSkillMisconceptionIds) {
-      this._inApplicableSkillMisconceptionIds = (
-        inApplicableSkillMisconceptionIds);
-    };
 
     // TODO(ankita240796): Remove the bracket notation once Angular2 gets in.
     /* eslint-disable dot-notation */
