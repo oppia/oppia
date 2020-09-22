8b054373cf83baff346517bead0e48a35a6354f0
Vojtěch Jelínek <vojtin.j@gmail.com>
2020-09-22 10:59:31 +0200

    Address comment

--- core/storage/base_model/gae_models.py
+++ core/storage/base_model/gae_models.py
@@ -1230,7 +1230,7 @@ class BaseSnapshotContentModel(BaseModel):
         the content field of ExplorationRightsSnapshotContentModel.
 
         The pseudonymization of these models is handled in the wipeout service
-        (in the relevant pseudonymization function, e.g. in the
+        (in the relevant pseudonymization function, e.g. in
         _pseudonymize_activity_models_with_associated_rights_models() for
         CollectionRightsModel or ExplorationRightsModel), based on
         the content_user_ids field of the relevant metadata model.
