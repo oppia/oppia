ddd4b7c7cd502c97125c1f2be31e15743f8ff620
jimbyo <jimz@uci.edu>
2020-09-22 16:27:25 -0700

    refactor imports

--- core/domain/opportunity_services_test.py
+++ core/domain/opportunity_services_test.py
@@ -41,11 +41,10 @@ from core.platform import models
 from core.tests import test_utils
 import python_utils
 
-(suggestion_models, feedback_models) = models.Registry.import_models([
-    models.NAMES.suggestion, models.NAMES.feedback])
-
-(opportunity_models,) = models.Registry.import_models(
-    [models.NAMES.opportunity])
+(   
+    suggestion_models, feedback_models,
+    opportunity_models) = models.Registry.import_models([
+    models.NAMES.suggestion, models.NAMES.feedback, models.NAMES.opportunity])
 
 
 class OpportunityServicesIntegrationTest(test_utils.GenericTestBase):
