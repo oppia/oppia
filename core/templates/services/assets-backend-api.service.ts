66aaa4a29080c0598be52a770da7d59b4891470e
Brian Rodriguez <brian@brianrodri.com>
2020-09-23 16:42:47 -0400

    remove unused import

--- core/templates/services/assets-backend-api.service.ts
+++ core/templates/services/assets-backend-api.service.ts
@@ -1,4 +1,4 @@
-// Copyright 2017 The Oppia Authors. All Rights Reserved.
+// Copyright 2020 The Oppia Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
  * assets from Google Cloud Storage.
  */
 
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { downgradeInjectable } from '@angular/upgrade/static';
 
