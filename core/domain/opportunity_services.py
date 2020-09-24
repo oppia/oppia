e33c3236d38532d0fbcf906253cc66ff26e11bae
jimbyo <35385366+jimbyo@users.noreply.github.com>
2020-09-23 18:37:49 -0700

    Fixes #10625 and Fixes #10696 Adds check to make sure opportunities are not undefined (#10745)

--- core/domain/opportunity_services.py
+++ core/domain/opportunity_services.py
@@ -623,9 +623,10 @@ def get_skill_opportunities_by_ids(ids):
         opportunity_models.SkillOpportunityModel.get_multi(ids))
     opportunities = []
     for skill_opportunity_model in skill_opportunity_models:
-        skill_opportunity = (
-            get_skill_opportunity_from_model(skill_opportunity_model))
-        opportunities.append(skill_opportunity)
+        if skill_opportunity_model is not None:
+            skill_opportunity = (
+                get_skill_opportunity_from_model(skill_opportunity_model))
+            opportunities.append(skill_opportunity)
     return opportunities
 
 
