8010f5dba0318fc7df707f079e36308bf6608bbc
Brian Rodriguez <brian@brianrodri.com>
2020-09-27 19:55:31 -0400

    add raises docs

--- export/cloud_datastore_admin.py
+++ export/cloud_datastore_admin.py
@@ -50,6 +50,9 @@ class ExportToCloudDatastoreHandler(base.BaseHandler):
         Raises:
             UnauthorizedUserException. The user does not have credentials to
                 access the page.
+            PageNotFoundException. The request was not made by the production
+                environment.
+            InvalidInputException. The bucket parameter is malformed or missing.
         """
 
         if not (self.is_cron_job or self.is_super_admin):
