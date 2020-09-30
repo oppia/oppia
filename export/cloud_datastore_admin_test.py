53470b88233c6ce6fd7daa7c9dba3909e962afeb
Brian Rodriguez <brian@brianrodri.com>
2020-09-29 20:02:31 -0400

    use requests-mock

--- export/cloud_datastore_admin_test.py
+++ export/cloud_datastore_admin_test.py
@@ -19,6 +19,7 @@ from __future__ import unicode_literals  # pylint: disable=import-only-modules
 
 from core.tests import test_utils
 from export import cloud_datastore_admin
+import requests_mock
 
 from google.appengine.api import app_identity
 import requests
@@ -29,6 +30,9 @@ class ExportToCloudDatastoreHandlerTests(test_utils.GenericTestBase):
 
     VIEWER_EMAIL = 'viewer@example.com'
     VIEWER_USERNAME = 'viewer'
+
+    EXPORT_URL = 'https://datastore.googleapis.com/v1/projects/%s:export' % (
+        cloud_datastore_admin.APP_NAME_OPPIASERVER)
 
     def setUp(self):
         super(ExportToCloudDatastoreHandlerTests, self).setUp()
@@ -43,103 +47,78 @@ class ExportToCloudDatastoreHandlerTests(test_utils.GenericTestBase):
             app_identity, 'get_application_id',
             lambda: cloud_datastore_admin.APP_NAME_OPPIASERVER)
 
-    def mock_http_response(self, content, status_code=200):
-        """Return a context manager that mocks all HTTP responses by returning a
-        string.
-
-        Args:
-            content: str. The content of the mock HTTP response.
-            status_code: int. The status code of the mock HTTP response.
-
-        Returns:
-            *. A one-time use context manager.
-        """
-        response = requests.Response()
-        response._content = content # pylint: disable=protected-access
-        response.status_code = status_code
-        return self.swap(requests, 'post', lambda *args, **kwargs: response)
-
-    def mock_http_exception(self, error_message):
-        """Return a context manager that mocks out all HTTP responses by raising
-        an exception.
-
-        Args:
-            error_message: str. The content of the exception to raise.
-
-        Returns:
-            *. A one-time use context manager.
-        """
-        def raise_exception(*unused_args, **unused_kwargs):
-            """Always raises an exception."""
-
-            raise Exception(error_message)
-
-        return self.swap(requests, 'post', raise_exception)
-
     def test_export_as_admin_succeeds(self):
         admin_ctx = self.login_context(self.ADMIN_EMAIL, is_super_admin=True)
         prod_ctx = self.mock_production_environment()
-        http_mock = self.mock_http_response('success')
 
-        with admin_ctx, prod_ctx, http_mock:
-            response = (
-                self.testapp.get('/cloud_datastore_export?bucket=gs://abc'))
-        self.assertEqual(response.status_code, 200)
+        with admin_ctx, prod_ctx, requests_mock.Mocker() as requests_mocker:
+            requests_mocker.post(self.EXPORT_URL)
+
+            res = self.testapp.get('/cloud_datastore_export?bucket=gs://abc')
+
+        self.assertEqual(res.status_code, 200)
 
     def test_export_as_cron_job_succeeds(self):
         cron_job_headers = {b'X-AppEngine-Cron': b'true'}
         prod_ctx = self.mock_production_environment()
-        http_mock = self.mock_http_response('success')
 
-        with prod_ctx, http_mock:
-            response = self.testapp.get(
+        with prod_ctx, requests_mock.Mocker() as requests_mocker:
+            requests_mocker.post(self.EXPORT_URL)
+
+            res = self.testapp.get(
                 '/cloud_datastore_export?bucket=gs://abc',
                 headers=cron_job_headers)
-        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(res.status_code, 200)
 
     def test_export_when_http_request_raises_error_fails(self):
         admin_ctx = self.login_context(self.ADMIN_EMAIL, is_super_admin=True)
         prod_ctx = self.mock_production_environment()
-        http_mock = self.mock_http_exception('uh-oh!')
 
-        with admin_ctx, prod_ctx, http_mock:
-            response = self.testapp.get(
+        with admin_ctx, prod_ctx, requests_mock.Mocker() as requests_mocker:
+            requests_mocker.post(self.EXPORT_URL, exc=Exception('uh-oh!'))
+
+            res = self.testapp.get(
                 '/cloud_datastore_export?bucket=gs://abc',
                 expect_errors=True)
-        self.assertEqual(response.status_code, 500)
-        self.assertIn('uh-oh!', response.body)
+
+        self.assertEqual(res.status_code, 500)
+        self.assertIn('uh-oh!', res.body)
 
     def test_export_outside_of_prod_environment_fails(self):
         admin_ctx = self.login_context(self.ADMIN_EMAIL, is_super_admin=True)
 
         with admin_ctx:
-            response = self.testapp.get(
+            res = self.testapp.get(
                 '/cloud_datastore_export?bucket=gs://abc', expect_errors=True)
-        self.assertEqual(response.status_code, 500)
+
+        self.assertEqual(res.status_code, 500)
         self.assertIn(
             'Export service has been pinged from a non-production '
             'or non-Oppia environment, so the request has been ignored.',
-            response.body)
+            res.body)
 
     def test_export_with_no_bucket_fails(self):
         admin_ctx = self.login_context(self.ADMIN_EMAIL, is_super_admin=True)
         prod_ctx = self.mock_production_environment()
 
         with admin_ctx, prod_ctx:
-            response = self.testapp.get(
+            res = self.testapp.get(
                 '/cloud_datastore_export', expect_errors=True)
-        self.assertEqual(response.status_code, 400)
-        self.assertIn('bucket must begin with gs://', response.body)
+
+        self.assertEqual(res.status_code, 400)
+        self.assertIn('bucket must begin with gs://', res.body)
 
     def test_export_with_bucket_missing_prefix_fails(self):
         admin_ctx = self.login_context(self.ADMIN_EMAIL, is_super_admin=True)
         prod_ctx = self.mock_production_environment()
 
         with admin_ctx, prod_ctx:
-            response = self.testapp.get(
+            res = self.testapp.get(
                 '/cloud_datastore_export?bucket=abc', expect_errors=True)
-        self.assertEqual(response.status_code, 400)
-        self.assertIn('bucket must begin with gs://', response.body)
+
+        self.assertEqual(res.status_code, 400)
+        self.assertIn('bucket must begin with gs://', res.body)
 
     def test_viewer_can_not_initiate_export(self):
         non_admin_ctx = (
@@ -147,9 +126,9 @@ class ExportToCloudDatastoreHandlerTests(test_utils.GenericTestBase):
         prod_ctx = self.mock_production_environment()
 
         with non_admin_ctx, prod_ctx:
-            response = self.testapp.get(
+            res = self.testapp.get(
                 '/cloud_datastore_export?bucket=gs://abc', expect_errors=True)
-        self.assertEqual(response.status_code, 401)
+
+        self.assertEqual(res.status_code, 401)
         self.assertIn(
-            'You do not have the credentials to access this page',
-            response.body)
+            'You do not have the credentials to access this page', res.body)
