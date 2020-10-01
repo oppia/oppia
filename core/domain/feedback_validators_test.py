913286af3ed18cf53331bae174537c80237e30b5
Kiranmayee Dobbali <kiranmayee.dobbali@gmail.com>
2020-10-01 16:25:35 -0600

    added missing import statements(absolute,unicode) in feedback_validator_test.py

--- core/domain/feedback_validators_test.py
+++ core/domain/feedback_validators_test.py
@@ -16,6 +16,9 @@
 
 """Unit tests for core.domain.feedback_validators."""
 
+from __future__ import absolute_import  # pylint: disable=import-only-modules
+from __future__ import unicode_literals  # pylint: disable=import-only-modules
+
 import datetime
 
 from core.domain import exp_domain
