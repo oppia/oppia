5aaed15b1c6120cbaa583aa389f5d22d58cd5761
iamprayush <iamprayush@gmail.com>
2020-10-03 02:00:07 +0530

    Lint fix.

--- extensions/interactions/NumericExpressionInput/protractor.js
+++ extensions/interactions/NumericExpressionInput/protractor.js
@@ -28,8 +28,8 @@ var customizeInteraction = async function(elem, placeholderText) {
 };
 
 var expectInteractionDetailsToMatch = async function(elem, placeholderText) {
-  await waitFor.presenceOf(elem.element(by.tagName(
-    'oppia-interactive-numeric-expression-input')),
+  await waitFor.presenceOf(
+    elem.element(by.tagName('oppia-interactive-numeric-expression-input')),
     'The numeric expression input editor took too long to load.');
   if (placeholderText) {
     expect(
