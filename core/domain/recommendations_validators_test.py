59f67584d9dac169330f0d099011b914a9eb3a3f
sunny1401 <sankrantijoshi14@gmail.com>
2020-10-02 19:34:14 +0100

    pr comment - changing from Unit tests for core.domain.opportunity_validators. to Unit tests for core.domain.recommendations_validators.

--- core/domain/recommendations_validators_test.py
+++ core/domain/recommendations_validators_test.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Unit tests for core.domain.opportunity_validators."""
+"""Unit tests for core.domain.recommendations_validators."""
 
 from __future__ import absolute_import  # pylint: disable=import-only-modules
 from __future__ import unicode_literals  # pylint: disable=import-only-modules
