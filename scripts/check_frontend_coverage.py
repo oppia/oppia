7bc947e143a23a29866214b7e429eff6a4959a2d
darksun27 <sbsiddharth@gmail.com>
2020-10-04 23:30:59 +0530

    Fix coverage

--- scripts/check_frontend_coverage.py
+++ scripts/check_frontend_coverage.py
@@ -76,7 +76,7 @@ NOT_FULLY_COVERED_FILENAMES = [
     'collection-navbar.directive.ts',
     'collection-node-creator.directive.ts',
     'collection-node-editor.directive.ts',
-    'collection-node-object.factory.ts',
+    'collection-node.model.ts',
     'collection-player-page.directive.ts',
     'collection-summary-tile.directive.ts',
     'collection-update.service.ts',
