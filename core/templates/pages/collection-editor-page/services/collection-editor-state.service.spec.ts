d10a411d59e0779a9e4302166155db515ce8d6c7
arpit1912 <arpitbandejiya@gmail.com>
2020-10-11 01:32:13 +0530

    resolved the conflicts

--- core/templates/pages/collection-editor-page/services/collection-editor-state.service.spec.ts
+++ core/templates/pages/collection-editor-page/services/collection-editor-state.service.spec.ts
@@ -375,7 +375,7 @@ describe('Collection editor state service', function() {
       var actualCollectionRights = (
         CollectionEditorStateService.getCollectionRights());
 
-        expect(actualCollectionRights).toEqual(expectedCollectionRights);
+      expect(actualCollectionRights).toEqual(expectedCollectionRights);
       // Although the actual collection rights equals the expected collection
       // rights, they are different objects.
       expect(actualCollectionRights).not.toBe(expectedCollectionRights);
