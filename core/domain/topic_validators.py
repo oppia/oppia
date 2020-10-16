3fc940e42bb55c58504e240ee05ed2283a1b8dc0
Matt Higgins <matt@matthiggins.dev>
2020-10-16 16:13:16 -0400

    validators: Fix docstring naming and update copyright date

--- core/domain/topic_validators.py
+++ core/domain/topic_validators.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Validators for topic prod models."""
+"""Validators for topic models."""
 
 from __future__ import absolute_import  # pylint: disable=import-only-modules
 from __future__ import unicode_literals  # pylint: disable=import-only-modules
