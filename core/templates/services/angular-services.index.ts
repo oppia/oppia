fcbcf11676a16edb80340f9418e2a26b52e99c44
darksun27 <sbsiddharth@gmail.com>
2020-10-17 16:19:09 +0530

    Fix Minor Issue

--- core/templates/services/angular-services.index.ts
+++ core/templates/services/angular-services.index.ts
@@ -166,6 +166,7 @@ import { BrowserCheckerService } from 'domain/utilities/browser-checker.service'
 import { LanguageUtilService } from 'domain/utilities/language-util.service';
 import { UrlInterpolationService } from 'domain/utilities/url-interpolation.service';
 import { ExpressionEvaluatorService } from 'expressions/expression-evaluator.service';
+import { ExpressionInterpolationService } from 'expressions/expression-interpolation.service';
 import { ExpressionParserService } from 'expressions/expression-parser.service';
 import { ExpressionSyntaxTreeService } from 'expressions/expression-syntax-tree.service';
 import { FormatTimePipe } from 'filters/format-timer.pipe';
@@ -419,6 +420,7 @@ export const angularServices: [string, unknown][] = [
   ['ExplorationStatsService', ExplorationStatsService],
   ['ExplorationTaskObjectFactory', ExplorationTaskObjectFactory],
   ['ExpressionEvaluatorService', ExpressionEvaluatorService],
+  ['ExpressionInterpolationService', ExpressionInterpolationService],
   ['ExpressionParserService', ExpressionParserService],
   ['ExpressionSyntaxTreeService', ExpressionSyntaxTreeService],
   ['ExtensionTagAssemblerService', ExtensionTagAssemblerService],
