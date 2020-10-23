025ef220bbe3caa14ce59dae766b45e01c1cdbdf
Ben Henning <henning.benmax@gmail.com>
2020-10-23 14:40:47 -0700

    Update gae_models.py
    Test codeowners by changing a docstring in a file that I own.

--- core/storage/config/gae_models.py
+++ core/storage/config/gae_models.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Models relating to configuration properties and platform parameters."""
+"""Models relating to configuration properties and platform parameters. Arbitrary change to documentation."""
 
 from __future__ import absolute_import  # pylint: disable=import-only-modules
 from __future__ import unicode_literals  # pylint: disable=import-only-modules
