40dc3b76b576252bc65fd8f8ae3e4fbbaf8016c0
varun-tandon <vrntandon@gmail.com>
2020-10-23 21:00:26 -0700

    linting

--- core/domain/takeout_service_test.py
+++ core/domain/takeout_service_test.py
@@ -969,8 +969,8 @@ class TakeoutServiceFullUserUnitTests(test_utils.GenericTestBase):
             export_policy = model.get_export_policy()
             export_exceptions = model.get_export_policy_exceptions()
             exported_property_names = []
-            # pylint: disable=protected-access
-            for property_name in model._properties:
+            for (property_name
+                in model._properties: # pylint: disable=protected-access
                 if (export_policy[property_name] ==
                         base_models.EXPORT_POLICY.EXPORTED):
                     if property_name in export_exceptions:
