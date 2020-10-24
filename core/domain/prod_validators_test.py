81a4f441a421b5f580772d40353c03a4e6221408
kate-perkins <kperkins@hmc.edu>
2020-10-24 13:48:57 -0400

    Fix lint errors

--- core/domain/prod_validators_test.py
+++ core/domain/prod_validators_test.py
@@ -67,13 +67,13 @@ USER_NAME = 'username'
     classifier_models, collection_models, config_models,
     email_models, exp_models, feedback_models,
     job_models, opportunity_models, question_models,
-    skill_models, story_models, subtopic_models, 
+    skill_models, story_models, subtopic_models,
     suggestion_models, topic_models, user_models
 ) = models.Registry.import_models([
     models.NAMES.classifier, models.NAMES.collection, models.NAMES.config,
     models.NAMES.email, models.NAMES.exploration, models.NAMES.feedback,
     models.NAMES.job, models.NAMES.opportunity, models.NAMES.question,
-    models.NAMES.skill, models.NAMES.story, models.NAMES.subtopic, 
+    models.NAMES.skill, models.NAMES.story, models.NAMES.subtopic,
     models.NAMES.suggestion, models.NAMES.topic, models.NAMES.user
 ])
 
