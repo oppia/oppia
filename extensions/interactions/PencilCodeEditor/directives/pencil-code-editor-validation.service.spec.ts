76c78517810039f3c0e045421ea02b217386330c
safwan mansuri <safwanmansuri.ict17@gmail.com>
2020-10-28 08:56:20 +0530

    pencil-fourth-commit

--- extensions/interactions/PencilCodeEditor/directives/pencil-code-editor-validation.service.spec.ts
+++ extensions/interactions/PencilCodeEditor/directives/pencil-code-editor-validation.service.spec.ts
@@ -1,4 +1,4 @@
-// Copyright 2015 The Oppia Authors. All Rights Reserved.
+// Copyright 2020 The Oppia Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,22 +15,20 @@
 /**
  * @fileoverview Unit tests for Pencil Code Editor Validation Service.
  */
-import { TestBed } from '@angular/core/testing';
 
+import { AnswerGroupObjectFactory } from
+'domain/exploration/AnswerGroupObjectFactory';
+import { AppConstants } from 'app.constants';
 import { OutcomeObjectFactory } from
   'domain/exploration/OutcomeObjectFactory';
-
 import { PencilCodeEditorValidationService } from
   // eslint-disable-next-line max-len
   'interactions/PencilCodeEditor/directives/pencil-code-editor-validation.service';
 import { RuleObjectFactory, RuleInputs } from
   'domain/exploration/RuleObjectFactory';
-import { AnswerGroupObjectFactory } from
-  'domain/exploration/AnswerGroupObjectFactory';
-import { AppConstants } from 'app.constants';
+import { TestBed } from '@angular/core/testing';
 
-
-describe('Pencil Code Editor Validation Service', () => {
+fdescribe('Pencil Code Editor Validation Service', () => {
   let pcevs: PencilCodeEditorValidationService = null;
   let oof: OutcomeObjectFactory = null;
   let rof: RuleObjectFactory = null;
@@ -44,7 +42,7 @@ describe('Pencil Code Editor Validation Service', () => {
     agof = TestBed.get(AnswerGroupObjectFactory);
   });
 
-  it('should return empty list', () => {
+  fit('should return empty list when feeback is given', () => {
     var customizationArgs = {
       initialCode: {
         value: ' Add the initial code snippet here.↵code is here'
@@ -53,7 +51,7 @@ describe('Pencil Code Editor Validation Service', () => {
     expect(pcevs.getCustomizationArgsWarnings(customizationArgs)).toEqual([]);
   });
 
-  it('should return error or empty list if no error', () => {
+  fit('should return error when no feedback is given', () => {
     var statename = 'Introduction';
 
     var customizationArgs = {
@@ -75,16 +73,10 @@ describe('Pencil Code Editor Validation Service', () => {
         'rule.')
     });
 
-    spyOn(pcevs, 'getCustomizationArgsWarnings')
-      .withArgs(customizationArgs).and.returnValue([]);
-
     // It returns the error when feedback is not provided.
     expect(pcevs.getAllWarnings(
       statename, customizationArgs, answergroup1, testOutcome1)
     ).toEqual(partialWarningsList);
-
-    // It checks the getCustomizationArgsWarnings has been called or not.
-    expect(pcevs.getCustomizationArgsWarnings).toHaveBeenCalled();
 
     inputBackend = {
       x: [['<p>one</p>']]
@@ -102,14 +94,60 @@ describe('Pencil Code Editor Validation Service', () => {
       statename, customizationArgs,
       [answergroup2], testOutcome1)
     ).toEqual(partialWarningsList);
+  });
 
-    const testOutcome3 = oof.createNew(
+  fit('should not return error when feedback is given', () => {
+    var statename = 'Introduction';
+
+    var customizationArgs = {
+      initialCode: {
+        value: ' Add the initial code snippet here.↵code is here'
+      }
+    };
+
+    inputBackend = {
+      x: [['<p>one</p>']]
+    };
+
+    const testOutcome = oof.createNew(
+      'Introduction', 'feedback_0', '<p>YES</p>', []);
+
+    let rulesDict = rof.createNew('CodeString', inputBackend);
+
+    let answergroup2 = agof.createNew([rulesDict], testOutcome, [], null);
+
+    const testOutcome2 = oof.createNew(
       'Introduction', 'default_outcome',
       '<p>no</p>', []);
 
     // It returns the list when feedback is provided.
     expect(pcevs.getAllWarnings(
-      statename, customizationArgs, [answergroup2], testOutcome3)
+      statename, customizationArgs, [answergroup2], testOutcome2)
     ).toEqual([]);
+  });
+
+  fit('should call getCustomizationArgsWarnings', () => {
+    var statename = 'Introduction';
+
+    var customizationArgs = {
+      initialCode: {
+        value: ' Add the initial code snippet here.↵code is here'
+      }
+    };
+
+    const testOutcome1 = oof.createNew(
+      'Introduction', 'default_outcome', '', []);
+
+    var answergroup1 = [];
+
+    spyOn(pcevs, 'getCustomizationArgsWarnings')
+      .withArgs(customizationArgs).and.returnValue([]);
+
+    // It returns the error when feedback is not provided.
+    pcevs.getAllWarnings(
+      statename, customizationArgs, answergroup1, testOutcome1);
+
+    // It checks the getCustomizationArgsWarnings has been called or not.
+    expect(pcevs.getCustomizationArgsWarnings).toHaveBeenCalled();
   });
 });
