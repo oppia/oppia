d3460f2d9726be7a8a453ffac921f83eeafd722a
Brian Rodriguez <brian@brianrodri.com>
2020-10-31 11:22:39 -0400

    fix position of 'or None'

--- core/tests/test_utils.py
+++ core/tests/test_utils.py
@@ -205,7 +205,7 @@ class TaskqueueServicesStub(python_utils.OBJECT):
         headers = {
             'X-Appengine-QueueName': python_utils.convert_to_bytes(queue_name),
             'X-Appengine-TaskName': (
-                python_utils.convert_to_bytes(task_name) or None),
+                python_utils.convert_to_bytes(task_name or None)),
             'X-AppEngine-Fake-Is-Admin': python_utils.convert_to_bytes(1),
         }
         csrf_token = self._test_base.get_new_csrf_token()
