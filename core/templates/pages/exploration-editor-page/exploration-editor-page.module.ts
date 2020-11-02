4fa9611faf3096331201027c24878a016144f70f
Deepam101 <50206813+Deepam101@users.noreply.github.com>
2020-11-02 18:33:02 +0530

    Fixes part of #9749: Migrates state-param-changes-editor.component to Angular (#10962)
    * Migrated state-param-changes
    
    * Linting changes
    
    * Linting fix
    
    * Linting fix
    
    * Linting fix
    
    * Linting fix
    
    * Some fixes
    
    * Minor changes
    
    * Minor changes
    
    * Added wrapping class to paramChangesEditor Component
    
    * Resolved frontend errors
    
    * Linting changes
    
    * More linting changes
    
    * More linting changes
    
    * Testing file changed
    
    * Changes
    
    * Changes to module
    
    * Changes in testing file
    
    * Linting changes
    
    * Linting changes
    
    * Linting changes
    
    * More Linting changes
    
    Co-authored-by: deepam101 <deepambanerjee07@gmail.com>

--- core/templates/pages/exploration-editor-page/exploration-editor-page.module.ts
+++ core/templates/pages/exploration-editor-page/exploration-editor-page.module.ts
@@ -41,14 +41,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { downgradeComponent } from '@angular/upgrade/static';
 import { HttpClientModule } from '@angular/common/http';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { RequestInterceptor } from 'services/request-interceptor.service';
 import { SharedComponentsModule } from 'components/shared-component.module';
+import { CkEditorCopyToolbarComponent } from 'components/ck-editor-helpers/ck-editor-copy-toolbar/ck-editor-copy-toolbar.component';
 import { OppiaAngularRootComponent } from
   'components/oppia-angular-root.component';
 import { platformFeatureInitFactory, PlatformFeatureService } from
   'services/platform-feature.service';
-
-import { CkEditorCopyToolbarComponent } from 'components/ck-editor-helpers/ck-editor-copy-toolbar/ck-editor-copy-toolbar.component';
+import { RequestInterceptor } from 'services/request-interceptor.service';
+import { StateParamChangesEditorComponent } from './editor-tab/state-param-changes-editor/state-param-changes-editor.component';
+import { ParamChangesEditorDirective } from './param-changes-editor/param-changes-editor.component';
 
 @NgModule({
   imports: [
@@ -58,11 +59,14 @@ import { CkEditorCopyToolbarComponent } from 'components/ck-editor-helpers/ck-ed
   ],
   declarations: [
     CkEditorCopyToolbarComponent,
-    OppiaAngularRootComponent
+    OppiaAngularRootComponent,
+    ParamChangesEditorDirective,
+    StateParamChangesEditorComponent,
   ],
   entryComponents: [
     CkEditorCopyToolbarComponent,
-    OppiaAngularRootComponent
+    OppiaAngularRootComponent,
+    StateParamChangesEditorComponent
   ],
   providers: [
     {
