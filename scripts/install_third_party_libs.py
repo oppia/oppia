149102cd5cb68aa725d6a6c01f88ead0323a9839
Shubha Rajan <shubharajan94@gmail.com>
2020-11-05 17:53:52 -0800

    add debug logging

--- scripts/install_third_party_libs.py
+++ scripts/install_third_party_libs.py
@@ -240,6 +240,10 @@ def main():
     if common.is_windows_os():
         tweak_yarn_executable()
 
+    print("yarn path:" , common.YARN_PATH)
+    print("current dir from common.py:" , common.CURR_DIR)
+    print("current dir from common.py:" , os.path.abspath(os.getcwd()))
+
     # Install third-party node modules needed for the build process.
     subprocess.check_call([get_yarn_command(), 'install', '--pure-lockfile'])
 
