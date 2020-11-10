3536999854597928690c117493307b053052a18b
darksun27 <sbsiddharth@gmail.com>
2020-11-10 13:52:56 +0530

    Migrate Spec File

--- core/templates/services/image-upload-helper.service.spec.ts
+++ core/templates/services/image-upload-helper.service.spec.ts
@@ -1,4 +1,4 @@
-// Copyright 2014 The Oppia Authors. All Rights Reserved.
+// Copyright 2020 The Oppia Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,50 +16,43 @@
  * @fileoverview Unit test for ImageUploadHelperService.
  */
 
-import { UpgradedServices } from 'services/UpgradedServices';
+import { HttpClientTestingModule } from
+  '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { ImageUploadHelperService } from 'services/image-upload-helper.service';
 
-require('services/assets-backend-api.service.ts');
-require('services/image-upload-helper.service.ts');
+describe('ImageUploadHelperService', () => {
+  let imageUploadHelperService: ImageUploadHelperService = null;
 
-describe('ImageUploadHelperService', function() {
-  var ImageUploadHelperService = null;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    imageUploadHelperService =
+      TestBed.get(ImageUploadHelperService);
+  });
 
-  beforeEach(angular.mock.module('oppia'));
-  beforeEach(angular.mock.module('oppia', function($provide) {
-    $provide.value('AssetsBackendApiService', {});
-  }));
-  beforeEach(angular.mock.module('oppia', function($provide) {
-    var ugs = new UpgradedServices();
-    for (let [key, value] of Object.entries(ugs.getUpgradedServices())) {
-      $provide.value(key, value);
-    }
-  }));
-
-  beforeEach(angular.mock.inject(function($injector) {
-    ImageUploadHelperService = $injector.get('ImageUploadHelperService');
-  }));
-
-  it('should convert image data to image file', function() {
-    var imageFile = null;
+  it('should convert image data to image file', () => {
+    let imageFile = null;
     imageFile = (
-      ImageUploadHelperService.convertImageDataToImageFile(
+      imageUploadHelperService.convertImageDataToImageFile(
         'data:image/png;base64,xyz'));
     expect(imageFile instanceof Blob).toBe(true);
   });
 
-  it('should generate a filename for a math SVG', function() {
-    var height = '1d345';
-    var width = '2d455';
-    var verticalPadding = '0d123';
-    var generatedFilename = (
-      ImageUploadHelperService.generateMathExpressionImageFilename(
+  it('should generate a filename for a math SVG', () => {
+    let height = '1d345';
+    let width = '2d455';
+    let verticalPadding = '0d123';
+    let generatedFilename = (
+      imageUploadHelperService.generateMathExpressionImageFilename(
         height, width, verticalPadding));
     expect(generatedFilename.endsWith(
       '_height_1d345_width_2d455_vertical_0d123.svg')).toBe(true);
   });
 
-  it('should remove the role attribute from the Math SVG string', function() {
-    var svgString = (
+  it('should remove the role attribute from the Math SVG string', () => {
+    let svgString = (
       '<svg width="1.33ex" height="1.429ex" viewBox="0 -511.5 572.5 615.4" ' +
       'focusable="false" role= "img" style="vertical-align: -0.241ex;" xmln' +
       's="http://www.w3.org/2000/svg"><g stroke="currentColor" fill="curren' +
@@ -69,9 +62,9 @@ describe('ImageUploadHelperService', function() {
       '14 59T463 140Q466 150 469 151T485 153H489Q504 153 504 145284 52 289Z' +
       '"/></g></svg>'
     );
-    var cleanedSvgString = (
-      ImageUploadHelperService.cleanMathExpressionSvgString(svgString));
-    var expectedCleanSvgString = (
+    let cleanedSvgString = (
+      imageUploadHelperService.cleanMathExpressionSvgString(svgString));
+    let expectedCleanSvgString = (
       '<svg xmlns="http://www.w3.org/2000/svg" width="1.33ex" height="1.429e' +
       'x" viewBox="0 -511.5 572.5 615.4" focusable="false" style="vertical-a' +
       'lign: -0.241ex;"><g stroke="currentColor" fill="currentColor" stro' +
@@ -83,8 +76,8 @@ describe('ImageUploadHelperService', function() {
     expect(cleanedSvgString).toEqual(expectedCleanSvgString);
   });
 
-  it('should replace xmlns:xlink with xmlns in a Math SVG string', function() {
-    var svgString = (
+  it('should replace xmlns:xlink with xmlns in a Math SVG string', () => {
+    let svgString = (
       '<svg xmlns:xlink="http://www.w3.org/1999/xlink" width="1.33ex" height' +
       '="1.429ex" viewBox="0 -511.5 572.5 615.4" focusable="false" style="ve' +
       'rtical-align: -0.241ex;"><g stroke="currentColor" fill="currentColor"' +
@@ -94,9 +87,9 @@ describe('ImageUploadHelperService', function() {
       '140Q466 150 469 151T485 153H489Q504 153 504 145284 52 289Z"></path></' +
       'g></svg>'
     );
-    var cleanedSvgString = (
-      ImageUploadHelperService.cleanMathExpressionSvgString(svgString));
-    var expectedCleanSvgString = (
+    let cleanedSvgString = (
+      imageUploadHelperService.cleanMathExpressionSvgString(svgString));
+    let expectedCleanSvgString = (
       '<svg width="1.33ex" height="1.429ex" viewBox="0 -511.5 572.5 615.4" ' +
       'focusable="false" style="vertical-align: -0.241ex;" xmlns="http://ww' +
       'w.w3.org/2000/svg"><g stroke="currentColor" fill="currentColor" stro' +
@@ -108,8 +101,8 @@ describe('ImageUploadHelperService', function() {
     expect(cleanedSvgString).toEqual(expectedCleanSvgString);
   });
 
-  it('should extract dimensions from an math SVG string', function() {
-    var svgString = (
+  it('should extract dimensions from an math SVG string', () => {
+    let svgString = (
       '<svg width="1.33ex" height="1.429ex" viewBox="0 -511.5 572.5 615.4" ' +
       'focusable="false" style="vertical-align: -0.241ex;" xmlns="http://ww' +
       'w.w3.org/2000/svg"><g stroke="currentColor" fill="currentColor" stro' +
@@ -118,10 +111,10 @@ describe('ImageUploadHelperService', function() {
       '386 350 336Q290 115 290 78Q290 50 306 38T341 26Q378 26 414 59T463 14' +
       '0Q466 150 469 151T485 153H489Q504 153 504 145284 52 289Z"/></g></svg>'
     );
-    var dimensions = (
-      ImageUploadHelperService.extractDimensionsFromMathExpressionSvgString(
+    let dimensions = (
+      imageUploadHelperService.extractDimensionsFromMathExpressionSvgString(
         svgString));
-    var expectedDimension = {
+    let expectedDimension = {
       height: '1d429',
       width: '1d33',
       verticalPadding: '0d241'
@@ -129,12 +122,12 @@ describe('ImageUploadHelperService', function() {
     expect(dimensions).toEqual(expectedDimension);
   });
 
-  it('should generate a filename for a normal image', function() {
-    var height = 720;
-    var width = 180;
-    var format = 'png';
-    var generatedFilename = (
-      ImageUploadHelperService.generateImageFilename(height, width, format));
+  it('should generate a filename for a normal image', () => {
+    let height = 720;
+    let width = 180;
+    let format = 'png';
+    let generatedFilename = (
+      imageUploadHelperService.generateImageFilename(height, width, format));
     expect(generatedFilename.endsWith('_height_720_width_180.png')).toBe(true);
   });
 });
