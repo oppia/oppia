85ac0b315dba00ebdb633bdebabb444c6cacd3bb
Nikhil Nair <nairnikhil2001@gmail.com>
2020-11-14 11:39:01 +0530

    scroll to stateNameContainer before executing tests

--- core/tests/protractor_desktop/additionalEditorFeatures.js
+++ core/tests/protractor_desktop/additionalEditorFeatures.js
@@ -247,7 +247,6 @@ describe('Full exploration editor', function() {
     await users.login('user6@editorAndPlayer.com');
     await workflow.createExploration();
 
-    await waitFor.pageToFullyLoad();
     // Create an exploration with multiple groups.
     await explorationEditorMainTab.setStateName('first card');
     await explorationEditorMainTab.setContent(await forms.toRichText(
