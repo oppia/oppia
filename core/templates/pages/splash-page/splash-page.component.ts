7139b27a3d7e26445a9057927307f2b297a41817
Akshay Anand <akshayanand681@gmail.com>
2020-11-25 00:13:36 +0530

    Desktop page done

--- core/templates/pages/splash-page/splash-page.component.ts
+++ core/templates/pages/splash-page/splash-page.component.ts
@@ -56,6 +56,28 @@ angular.module('oppia').component('splashPage', {
         return false;
       };
 
+      ctrl.onClickLearnMoreStudentsButton = function() {
+        $timeout(function() {
+          WindowRef.nativeWindow.location = '/learn/math';
+        }, 150);
+        return false;
+      };
+
+      ctrl.onClickLearnMoreTeachersButton = function() {
+        $timeout(function() {
+          WindowRef.nativeWindow.location = '/teach';
+        }, 150);
+        return false;
+      };
+
+      ctrl.onClickStartContributingButton = function() {
+        $timeout(function() {
+          WindowRef.nativeWindow.location = (
+            'https://www.oppiafoundation.org/volunteer');
+        }, 150);
+        return false;
+      };
+
       ctrl.onClickCreateExplorationButton = function() {
         SiteAnalyticsService.registerClickCreateExplorationButtonEvent();
         $timeout(function() {
@@ -63,9 +85,27 @@ angular.module('oppia').component('splashPage', {
         }, 150);
         return false;
       };
+
+      ctrl.incrementTestimonialId = function() {
+        ctrl.testimonialId = (ctrl.testimonialId + 1) % 3;
+      };
+
+      ctrl.decrementTestimonialId = function() {
+        ctrl.testimonialId = (((ctrl.testimonialId - 1) % 3) + 3) % 3;
+      };
+
       ctrl.$onInit = function() {
         ctrl.userIsLoggedIn = null;
+        ctrl.testimonialId = 0;
         LoaderService.showLoadingScreen('Loading');
+        document.getElementsByClassName(
+          'oppia-splash-section-five')[0].style.backgroundImage = (
+            'url(' + ctrl.getStaticImageUrl(
+              '/splash/dsk_testimonial_background.png') + ')');
+        document.getElementsByClassName(
+          'oppia-splash-section-seven')[0].style.backgroundImage = (
+            'url(' + ctrl.getStaticImageUrl(
+              '/splash/dsk_community_background.png') + ')');
         UserService.getUserInfoAsync().then(function(userInfo) {
           ctrl.userIsLoggedIn = userInfo.isLoggedIn();
           LoaderService.hideLoadingScreen();
