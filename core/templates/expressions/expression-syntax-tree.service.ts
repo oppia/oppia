64e2786fa38fb938a85d3b4d0f681b798a1bc76e
Pankaj Dahiya <pankaj.id.008@gmail.com>
2020-11-25 14:17:01 +0530

    solving errors

--- core/templates/expressions/expression-syntax-tree.service.ts
+++ core/templates/expressions/expression-syntax-tree.service.ts
@@ -19,7 +19,6 @@
 import { downgradeInjectable } from '@angular/upgrade/static';
 import { Injectable } from '@angular/core';
 
-import { AppConstants } from 'app.constants';
 import { ExpressionParserService } from
   'expressions/expression-parser.service.ts';
 
