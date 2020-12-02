8a0bfcedff69d519f63ab69d59066b398564bae9
cowboydaniel <nskirkey@gmail.com>
2020-12-02 15:02:35 -0500

    Added job_models back to prod_validators.py

--- core/domain/prod_validators.py
+++ core/domain/prod_validators.py
@@ -49,15 +49,16 @@ import utils
 (
     base_models, collection_models,
     email_models, exp_models, feedback_models,
-    question_models, skill_models, story_models,
+    job_models, question_models, skill_models, story_models,
     subtopic_models, suggestion_models, topic_models,
     user_models
 ) = models.Registry.import_models([
     models.NAMES.base_model, models.NAMES.collection,
     models.NAMES.email, models.NAMES.exploration, models.NAMES.feedback,
-    models.NAMES.question, models.NAMES.skill,
+    models.NAMES.job, models.NAMES.question, models.NAMES.skill,
     models.NAMES.story, models.NAMES.subtopic,
     models.NAMES.suggestion, models.NAMES.topic, models.NAMES.user
+
 ])
 
 ALLOWED_AUDIO_EXTENSIONS = list(feconf.ACCEPTED_AUDIO_EXTENSIONS.keys())
