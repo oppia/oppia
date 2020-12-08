eb6738d67c3a11c7410ad4c42fc7423f652800ed
Vojtěch Jelínek <vojtin.j@gmail.com>
2020-12-08 10:13:39 +0100

    Fix #11157 and #10758: Adress comments from PRs (#11210)
    * [skip ci] Initial work
    
    * [skip ci] Address comments
    
    * [skip ci] Address comments
    
    * Slight change
    
    * Fix backend tests
    
    * Refactor contributors summary in exp summary
    
    * Add explanatory comment
    
    * Rename function

--- core/domain/prod_validators.py
+++ core/domain/prod_validators.py
@@ -3197,7 +3197,13 @@ class UserAuthDetailsModelValidator(
     def _get_external_id_relationships(cls, item):
         return [
             base_model_validators.ExternalModelFetcherDetails(
-                'user_settings_ids', user_models.UserSettingsModel, [item.id])]
+                'user_settings_ids', user_models.UserSettingsModel, [item.id]),
+            base_model_validators.ExternalModelFetcherDetails(
+                'user_identifiers_ids',
+                user_models.UserIdentifiersModel,
+                [item.gae_id]
+            )
+        ]
 
 
 class UserIdentifiersModelValidator(base_model_validators.BaseModelValidator):
@@ -3223,6 +3229,11 @@ class UserIdentifiersModelValidator(base_model_validators.BaseModelValidator):
             base_model_validators.ExternalModelFetcherDetails(
                 'user_settings_ids',
                 user_models.UserSettingsModel,
+                [item.user_id]
+            ),
+            base_model_validators.ExternalModelFetcherDetails(
+                'user_auth_details_ids',
+                user_models.UserAuthDetailsModel,
                 [item.user_id]
             )
         ]
