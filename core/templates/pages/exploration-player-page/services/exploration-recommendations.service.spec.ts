844b373318408fc83391209c768df361b2a7c6fe
Theo Lipeles <theolipeles@google.com>
2020-12-07 19:08:29 -0500

    add initialization context tests

--- core/templates/pages/exploration-player-page/services/exploration-recommendations.service.spec.ts
+++ core/templates/pages/exploration-player-page/services/exploration-recommendations.service.spec.ts
@@ -21,28 +21,89 @@ import { async, TestBed } from '@angular/core/testing';
 import { ExplorationRecommendationsService } from
   // eslint-disable-next-line max-len
   'pages/exploration-player-page/services/exploration-recommendations.service';
-describe('Exploration Recommendations Service', () => {
+import { ContextService } from 'services/context.service';
+import { ServicesConstants } from 'services/services.constants';
+import { UrlService } from 'services/contextual/url.service';
+import { ExplorationRecommendationsBackendApiService } from
+  'domain/recommendations/exploration-recommendations-backend-api.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+
+
+fdescribe('Exploration Recommendations Service', () => {
+  let expRecsService: ExplorationRecommendationsService;
+  let urlService: UrlService;
+  let contextService: ContextService;
+  let expRecsBackendApiService: ExplorationRecommendationsBackendApiService;
+  const STORY_ID = '1';
+  const NODE_ID = '3';
+  const EXPLORATION_ID = '4';
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ExplorationRecommendationsService]
-    })
-      .compileComponents();
+      imports: [
+        HttpClientTestingModule,
+      ],
+      providers: [
+        ExplorationRecommendationsService,
+      ]
+    });
   }));
 
   beforeEach(() => {
+    urlService = TestBed.get(UrlService);
+    contextService = TestBed.get(ContextService);
+    expRecsBackendApiService = TestBed.get(
+      ExplorationRecommendationsBackendApiService
+    );
 
+    spyOn(urlService, 'getCollectionIdFromExplorationUrl');
+    spyOn(urlService, 'getUrlParams').and.returnValue({
+      story_id: STORY_ID,
+      node_id: NODE_ID
+    });
+
+    spyOn(contextService, 'getExplorationId').and.returnValue(EXPLORATION_ID);
+
+    spyOn(expRecsBackendApiService, 'getRecommendedSummaryDicts');
   });
 
-  describe('when in editor page', () => {
+  describe('when context is in editor page', () => {
     beforeEach(() => {
+      spyOn(contextService, 'getPageContext').and.returnValue(
+        ServicesConstants.PAGE_CONTEXT.EXPLORATION_EDITOR
+      );
+    });
+
+    it('should initialize with correct context', () => {
+      expRecsService = TestBed.get(ExplorationRecommendationsService);
+      expect(expRecsService.isInEditorPage).toBeTrue();
+    });
+
+    describe('in preview mode', () => {
+      beforeEach(() => {
+        spyOn(contextService, 'getEditorTabContext').and.returnValue(
+          ServicesConstants.EXPLORATION_EDITOR_TAB_CONTEXT.PREVIEW
+        );
+      });
+      it('should initialize with correct context', () => {
+        expRecsService = TestBed.get(ExplorationRecommendationsService);
+        expect(expRecsService.isInEditorPage).toBeTrue();
+        expect(expRecsService.isInEditorPreviewMode).toBeTrue();
+      });
     });
   });
 
-  describe('when in editor preview mode', () => {
+  describe('when not in editor page', () => {
+    beforeEach(() => {
+      spyOn(contextService, 'getPageContext').and.returnValue(
+        ServicesConstants.PAGE_CONTEXT.OTHER
+      );
+    });
 
-  });
-
-  describe('when iframed', () => {
-
+    it('should initialize with correct context', () => {
+      expRecsService = TestBed.get(ExplorationRecommendationsService);
+      expect(expRecsService.isInEditorPage).toBeFalse();
+    });
   });
 });
