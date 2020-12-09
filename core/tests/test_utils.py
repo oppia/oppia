dfb8f14f1d665de5aa7d2a1551a3e49df31072ed
Varun Tandon <vrntandon@gmail.com>
2020-12-09 15:35:43 -0800

    Update core/tests/test_utils.py
    Co-authored-by: Vojtěch Jelínek <vojtin.j@gmail.com>

--- core/tests/test_utils.py
+++ core/tests/test_utils.py
@@ -91,9 +91,9 @@ platform_taskqueue_services = models.Registry.import_taskqueue_services()
 # We are using the b' prefix as all the stdouts are in bytes.
 LOG_LINE_PREFIX = b'LOG_INFO_TEST: '
 
-# List of model classes that don't have Takeout related class methods
-# defined because they're not used directly but only as a base classes for
-# the other models.
+# List of model classes that don't have Wipeout or Takeout, related class 
+# methods defined because they're not used directly but only as 
+# base classes for the other models.
 BASE_MODEL_CLASSES_WITHOUT_DATA_POLICIES = (
     'BaseCommitLogEntryModel',
     'BaseMapReduceBatchResultsModel',
