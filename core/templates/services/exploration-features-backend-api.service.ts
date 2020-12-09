94fff45b7b8aa1652ef61175cec2c5319e2a95bd
cernoit <mail@cernoit.co.uk>
2020-12-09 15:09:53 +0000

    added async to start and Async to end of function name. cannot see function keyword but it is then-able and it seems that a promise object is returned.

--- core/templates/services/exploration-features-backend-api.service.ts
+++ core/templates/services/exploration-features-backend-api.service.ts
@@ -43,7 +43,7 @@ export class ExplorationFeaturesBackendApiService {
       private http: HttpClient,
       private urlInterpolationService: UrlInterpolationService) {}
 
-  fetchExplorationFeatures(
+  asyncfetchExplorationFeaturesAsync(
       explorationId: string): Promise<ExplorationFeatures> {
     return this.http.get<ExplorationFeaturesBackendDict>(
       this.urlInterpolationService.interpolateUrl(
