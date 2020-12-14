9bfca5225fa01a0d79a3e6cb43d9e799906dc98c
Brian Rodriguez <brian@brianrodri.com>
2020-12-14 00:29:35 -0500

    ignore google-analytics.initializer.ts

--- core/templates/google-analytics.initializer.ts
+++ core/templates/google-analytics.initializer.ts
@@ -16,7 +16,7 @@
  * @fileoverview Initialization of Google Analytics..
  */
 
-import constants from 'assets/constants';
+const constants = require('constants.ts');
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
