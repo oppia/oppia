4f41bbd1f3947284fcb0bf83b5c35cd4ec47b6f7
Jacque Li <jacqueli@google.com>
2020-12-21 09:38:51 -0800

    Use AngularJS test to check component migration

--- core/templates/pages/exploration-editor-page/statistics-tab/issues/answer-submit-action.component.spec.ts
+++ core/templates/pages/exploration-editor-page/statistics-tab/issues/answer-submit-action.component.spec.ts
@@ -16,74 +16,76 @@
  * @fileoverview Unit tests for answerSubmitAction component.
  */
 
-import { waitForAsync, TestBed, ComponentFixture } from '@angular/core/testing';
-import { AnswerSubmitActionComponent } from './answer-submit-action.component';
-import { ElementRef, NO_ERRORS_SCHEMA } from '@angular/core';
-import { InteractionObjectFactory } from 'domain/exploration/InteractionObjectFactory';
-import { ExplorationHtmlFormatterService } from 'services/exploration-html-formatter.service';
+import { TestBed } from '@angular/core/testing';
 import { HtmlEscaperService } from 'services/html-escaper.service';
+import { ExplorationHtmlFormatterService } from
+  'services/exploration-html-formatter.service';
+import { InteractionObjectFactory } from
+  'domain/exploration/InteractionObjectFactory';
 
-class MockElementRef {
-  actionIndex = 2;
-  answer = '"This is an answer string."';
-  currentStateName = 'State name';
-  destStateName = 'Introduction';
-  interactionCustomizationArgs = `{
-    "choices": {
-      "value": [{
-        "content_id": "",
-        "html": "Value"
-      }]
-    },
-    "showChoicesInShuffledOrder": {"value": true}
-  }`;
-  interactionId = 'MultipleChoiceInput';
-  timeSpentInStateSecs: 2000
-}
+require(
+  'pages/exploration-editor-page/statistics-tab/issues/' +
+  'answer-submit-action.component.ts');
 
-class MockHtmlEscaperService {
+describe('Answer Submit Action directive', function() {
+  var ctrl = null;
+  var $scope = null;
+  var explorationHtmlFormatterService = null;
+  var htmlEscaperService = null;
+  var interactionObjectFactory = null;
 
-}
-
-let component: AnswerSubmitActionComponent;
-let fixture: ComponentFixture<AnswerSubmitActionComponent>;
-
-describe('Answer Submit Action directive', () => {
-  let elementRef: MockElementRef;
-  // let explorationHtmlFormatterService = null;
-  // let htmlEscaperService = null;
-  // let interactionObjectFactory = null;
-
-  beforeEach(waitForAsync(() => {
-    elementRef = new MockElementRef;
-    TestBed.configureTestingModule({
-      declarations: [AnswerSubmitActionComponent],
-      providers: [
-        { provide: ElementRef, useValue: elementRef },
-        ExplorationHtmlFormatterService,
-        HtmlEscaperService,
-        InteractionObjectFactory
-      ],
-      schemas: [NO_ERRORS_SCHEMA]
-    }).compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AnswerSubmitActionComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(function() {
+    explorationHtmlFormatterService = TestBed.get(
+      ExplorationHtmlFormatterService);
+    htmlEscaperService = TestBed.get(HtmlEscaperService);
+    interactionObjectFactory = TestBed.get(InteractionObjectFactory);
   });
 
+  beforeEach(angular.mock.module('oppia', function($provide) {
+    $provide.value('$attrs', {
+      actionIndex: 2,
+      answer: '"This is an answer string."',
+      currentStateName: 'State name',
+      destStateName: 'Introduction',
+      interactionCustomizationArgs:
+        `{
+          "choices": {
+            "value": [{
+              "content_id": "",
+              "html": "Value"
+            }]
+          },
+          "showChoicesInShuffledOrder": {"value": true}
+        }`,
+      interactionId: 'MultipleChoiceInput',
+      timeSpentInStateSecs: 2000
+    });
+  }));
+
+
+  beforeEach(angular.mock.inject(function($injector, $componentController) {
+    var $rootScope = $injector.get('$rootScope');
+
+    $scope = $rootScope.$new();
+    ctrl = $componentController('answerSubmitAction', {
+      $scope: $scope,
+      ExplorationHtmlFormatterService: explorationHtmlFormatterService,
+      HtmlEscaperService: htmlEscaperService,
+      InteractionObjectFactory: interactionObjectFactory
+    });
+    ctrl.$onInit();
+  }));
+
   it('should initialize controller properties after its initialization',
-    () => {
-      expect(component.currentStateName).toBe('State name');
-      expect(component.destStateName).toBe('Introduction');
-      expect(component.actionIndex).toBe(2);
-      expect(component.timeSpentInStateSecs).toBe(2000);
+    function() {
+      expect(ctrl.currentStateName).toBe('State name');
+      expect(ctrl.destStateName).toBe('Introduction');
+      expect(ctrl.actionIndex).toBe(2);
+      expect(ctrl.timeSpentInStateSecs).toBe(2000);
     });
 
-  it('should get short answer html', () => {
-    expect(component.getShortAnswerHtml()).toBe(
+  it('should get short answer html', function() {
+    expect(ctrl.getShortAnswerHtml()).toBe(
       '<oppia-short-response-multiple-choice-input answer="&amp;quot;This is' +
       ' an answer string.&amp;quot;" choices="[&amp;quot;Value&amp;quot;]"' +
       '></oppia-short-response-multiple-choice-input>');
