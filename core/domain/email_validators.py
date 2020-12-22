18a3ea9037149a1092e16e0c9d857b1189a2980f
varun-tandon <vrntandon@gmail.com>
2020-12-21 17:14:02 -0800

    add feconf import

--- core/domain/email_validators.py
+++ core/domain/email_validators.py
@@ -20,6 +20,7 @@ from __future__ import absolute_import  # pylint: disable=import-only-modules
 from __future__ import unicode_literals  # pylint: disable=import-only-modules
 
 import datetime
+import feconf
 
 from core.domain import base_model_validators
 from core.platform import models
