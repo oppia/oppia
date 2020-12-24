ea6bd841b5abbcd90a9713cf53d38d9c3201280e
Brian Rodriguez <brian@brianrodri.com>
2020-12-24 09:15:39 -0500

    s/switchMap/mergeMap

--- core/templates/services/auth-interceptor.service.ts
+++ core/templates/services/auth-interceptor.service.ts
@@ -19,7 +19,7 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { switchMap, take } from 'rxjs/operators';
+import { mergeMap, take } from 'rxjs/operators';
 import { AuthService } from 'services/auth.service';
 
 
@@ -31,7 +31,7 @@ export class AuthInterceptor implements HttpInterceptor {
 
   intercept<T>(
       request: HttpRequest<T>, next: HttpHandler): Observable<HttpEvent<T>> {
-    return this.authService.idToken$.pipe(take(1), switchMap(idToken => {
+    return this.authService.idToken$.pipe(take(1), mergeMap(idToken => {
       if (idToken !== null) {
         // Add the Authorization header to the request.
         request = request.clone({
