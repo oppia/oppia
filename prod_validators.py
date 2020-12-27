a873df45fc99564ad69b2eb1ee77d9ef38dd76f8
Rupeshkoushik <73252546+Rupeshkoushik@users.noreply.github.com>
2020-12-28 00:22:01 +0530

    Update prod_validators.py

--- prod_validators.py
+++ prod_validators.py
@@ -28,8 +28,8 @@ from core.domain import exp_fetchers
 from core.domain import exp_services
 from core.domain import rights_domain
 from core.domain import rights_manager
-from core.domain import subtopic_page_domain
-from core.domain import subtopic_page_services
+
+
 from core.domain import user_services
 from core.platform import models
 import feconf
