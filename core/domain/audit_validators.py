9da45345b0865430816ddabbcd8ccedbb757154e
rohitkatlaa <rohitkatlaa@gmail.com>
2020-12-28 08:58:43 +0530

    linting

--- core/domain/audit_validators.py
+++ core/domain/audit_validators.py
@@ -21,6 +21,7 @@ from __future__ import unicode_literals  # pylint: disable=import-only-modules
 
 from core.domain import base_model_validators
 
+
 class RoleQueryAuditModelValidator(base_model_validators.BaseModelValidator):
     """Class for validating RoleQueryAuditModels."""
 
