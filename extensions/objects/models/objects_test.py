92119b237e95a197f08a6eb50d8d13f42d938e1c
shavavo <shavavo@google.com>
2020-12-28 04:15:02 -0500

    comment

--- extensions/objects/models/objects_test.py
+++ extensions/objects/models/objects_test.py
@@ -906,11 +906,11 @@ class BaseTranslatableObjectTests(test_utils.GenericTestBase):
         for name, member in inspect.getmembers(objects):
             if not inspect.isclass(member):
                 continue
-            
+
             # Assert that BaseTranslatableObject subclasses start with
             # 'Translatable'. All objects that start with 'Translatable'
-            # subclassBaseTranslatableObject, with the exception of any object
-            # name that contains 'ContentId' (exm. TranslatableHtmlContentId).
+            # subclass BaseTranslatableObject, with the exception of any object
+            # name that contains 'ContentId' (e.g. TranslatableHtmlContentId).
             if isinstance(member(), objects.BaseTranslatableObject):
                 if name == 'BaseTranslatableObject':
                     continue
