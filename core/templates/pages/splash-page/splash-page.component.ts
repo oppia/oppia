68807a6c0e11e7627f72e8dfb2834bf201ebbd03
DubeySandeep <dubeysandeep.in@gmail.com>
2021-01-04 05:47:21 +0530

    Removes unwanted changes from PR #11517

--- core/templates/pages/splash-page/splash-page.component.ts
+++ core/templates/pages/splash-page/splash-page.component.ts
@@ -78,10 +78,6 @@ angular.module('oppia').component('splashPage', {
           // once the controller is migrated to angular.
           $rootScope.$applyAsync();
         });
-
-        $rootScope.$on('$translateChangeSuccess', function() {
-          ctrl.testimonials = ctrl.getTestimonials();
-        });
       };
     }
   ]
