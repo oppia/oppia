37262a430a56f14e9ff07514605a16452dd421b2
mridul-netizen <mridulsetia18@gmail.com>
2021-01-05 16:40:30 +0530

    updated

--- core/templates/pages/collection-player-page/collection-player-page.module.ts
+++ core/templates/pages/collection-player-page/collection-player-page.module.ts
@@ -27,9 +27,7 @@ import { OppiaAngularRootComponent } from
   'components/oppia-angular-root.component';
 import { CollectionFooterComponent } from
   'pages/collection-player-page/collection-footer/collection-footer.component';
-import { CollectionLocalNavComponent } from
-  // eslint-disable-next-line max-len
-  'pages/collection-player-page/collection-local-nav/collection-local-nav.component';
+import { CollectionLocalNavComponent } from 'pages/collection-player-page/collection-local-nav/collection-local-nav.component';
 import { CollectionNavbarComponent } from
   'pages/collection-player-page/collection-navbar/collection-navbar.component';
 import { CollectionNodeListComponent } from
