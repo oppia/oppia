158ccc958a9b39f25e35eded3e8ead42a7ebf59b
Radesh-kumar <imradesh@gmail.com>
2021-01-05 17:48:08 +0530

    final update

--- core/templates/domain/admin/admin-backend-api.service.spec.ts
+++ core/templates/domain/admin/admin-backend-api.service.spec.ts
@@ -319,7 +319,7 @@ describe('Admin backend api service', () => {
     let jobId = 'UserSettingsModelAuditOneOffJob-1609088541992-314';
     let adminJobOutputUrl = '/adminjoboutput?job_id=' +
       'UserSettingsModelAuditOneOffJob-1609088541992-314';
-    let jobOutput = {output: ["[u'fully-validated UserSettingsModel', 1]"]};
+    let jobOutput = {output: ['[u\'fully-validated UserSettingsModel\', 1]']};
     abas.fetchJobOutputAsync(jobId).then(successHandler, failHandler);
 
     let req = httpTestingController.expectOne(adminJobOutputUrl);
@@ -337,8 +337,8 @@ describe('Admin backend api service', () => {
     let jobId = 'UserSettingsModelAuditOneOffJob-1609088541992-314';
     let adminJobOutputUrl = '/adminjoboutput?job_id=' +
       'UserSettingsModelAuditOneOffJob-1609088541992-314';
-    let jobOutput = {output: ["[u'SUCCESS_KEPT', 1]",
-      "[u'SUCCESS_DELETED', 1]"]};
+    let jobOutput = {output: ['[u\'SUCCESS_KEPT\', 1]',
+      '[u\'SUCCESS_DELETED\', 1]']};
     abas.fetchJobOutputAsync(jobId).then(successHandler, failHandler);
 
     let req = httpTestingController.expectOne(adminJobOutputUrl);
