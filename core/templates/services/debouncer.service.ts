fa2586dee8bedede6d5afb239cbf3dbc6b634734
Aryaman Gupta <aryaman.gupta.met19@iitbhu.ac.in>
2021-01-05 20:58:59 +0530

    Fix part of #10474: Cover Few Services with strict checks (#11460)
    * covered three files in strict checks
    
    * adding punctuation in comment
    
    * adding punctuation in comment
    
    * made some changes in debouncer service
    
    * made some changes in debouncer service
    
    * removed unnecessary comments
    
    * Update core/templates/services/debouncer.service.ts
    
    Co-authored-by: Vojtěch Jelínek <vojtin.j@gmail.com>
    
    * Update core/templates/services/debouncer.service.ts
    
    Co-authored-by: Vojtěch Jelínek <vojtin.j@gmail.com>
    
    * changes in type of some variables
    
    * Resolved merge Conflicts
    
    * Resolved merge Conflicts
    
    * changed window to windowref in debouncer service
    
    * undoing the unnecessary changes
    
    Co-authored-by: Vojtěch Jelínek <vojtin.j@gmail.com>

--- core/templates/services/debouncer.service.ts
+++ core/templates/services/debouncer.service.ts
@@ -18,6 +18,7 @@
 
 import { downgradeInjectable } from '@angular/upgrade/static';
 import { Injectable } from '@angular/core';
+import { WindowRef } from './contextual/window-ref.service';
 
 @Injectable({
   providedIn: 'root'
@@ -27,32 +28,30 @@ export class DebouncerService {
   // be invoked. The function only gets executed after it stops being called
   // for `wait` milliseconds.
   debounce(func: () => void, millisecsToWait: number): () => void {
-    let timeout;
-    let context = this;
-    let args = arguments;
-    let timestamp;
-    let result;
+    let timeout: number;
+    let context: this | null = this;
+    let timestamp: number;
+    let result: void;
+    let windowRef: WindowRef = new WindowRef();
 
     const later = () => {
       let last = new Date().getTime() - timestamp;
       if (last < millisecsToWait) {
-        timeout = setTimeout(later, millisecsToWait - last);
+        timeout = windowRef.nativeWindow.setTimeout(
+          later, millisecsToWait - last);
       } else {
-        timeout = null;
-        result = func.apply(context, args);
+        timeout = 0;
+        result = func.apply(context);
         if (!timeout) {
           context = null;
-          args = null;
         }
       }
     };
 
     return function() {
-      context = this;
-      args = arguments;
       timestamp = new Date().getTime();
       if (!timeout) {
-        timeout = setTimeout(later, millisecsToWait);
+        timeout = window.setTimeout(later, millisecsToWait);
       }
       return result;
     };
