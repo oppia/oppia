c099ab454e8c259e67c017cba57163846be8a79d
varun-tandon <vrntandon@gmail.com>
2021-01-05 20:39:54 -0800

    linting

--- core/controllers/profile.py
+++ core/controllers/profile.py
@@ -17,7 +17,6 @@
 from __future__ import absolute_import  # pylint: disable=import-only-modules
 from __future__ import unicode_literals  # pylint: disable=import-only-modules
 
-import base64
 import json
 import re
 import zipfile
