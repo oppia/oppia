968fd5e20a4440bee6f82d90bdec5dca9afcf771
Brian Rodriguez <brian@brianrodri.com>
2021-01-09 18:24:24 -0500

    fix new test

--- core/domain/user_jobs_one_off_test.py
+++ core/domain/user_jobs_one_off_test.py
@@ -48,6 +48,7 @@ import utils
 (user_models, feedback_models, exp_models) = models.Registry.import_models(
     [models.NAMES.user, models.NAMES.feedback, models.NAMES.exploration])
 
+auth_services = models.Registry.import_auth_services()
 datastore_services = models.Registry.import_datastore_services()
 search_services = models.Registry.import_search_services()
 
@@ -1880,12 +1881,11 @@ class FixUserSettingsCreatedOnOneOffJobTests(test_utils.GenericTestBase):
             migrated_user_model.created_on, final_created_on_timestamp)
 
     def test_time_difference_less_than_time_delta_does_not_update(self):
-        user_auth_details_model = (
-            user_models.UserAuthDetailsModel(
-                id=self.USER_ID_1,
-                gae_id='gae_id'
-            )
-        )
+        user_auth_details = (
+            auth_services.create_user_auth_details(self.USER_ID_1, 'auth_id'))
+
+        user_auth_details_model = user_models.UserAuthDetailsModel(
+            id=self.USER_ID_1, **user_auth_details.to_dict_with_changes())
         user_auth_details_model.update_timestamps()
         user_auth_details_model.put()
 
