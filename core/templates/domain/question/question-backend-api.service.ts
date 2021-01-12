38717ee5365c742fbeb119a061c70025515acede
Rupesh <punnarupeshkoushik@gmail.com>
2021-01-12 12:41:49 +0530

    lint checks`

--- core/templates/domain/question/question-backend-api.service.ts
+++ core/templates/domain/question/question-backend-api.service.ts
@@ -191,7 +191,7 @@ export class QuestionBackendApiService {
     });
   }
 
-  async fetchTotalQuestionCountForSkillIdsAsync(skillIds: string[]): Promise<number> {
+  async fetchTotalQuestionCountForSkillIdsAsync (skillIds: string[]): Promise<number> {
     return new Promise((resolve, reject) => {
       this._fetchTotalQuestionCountForSkillIds(skillIds, resolve, reject);
     });
