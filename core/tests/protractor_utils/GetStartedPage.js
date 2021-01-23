19be4b45f3288a792b680ddb3bce7fff04900ad2
prayutsu <gargabhay2000@gmail.com>
2021-01-23 14:34:30 +0530

    Remove await keyword

--- core/tests/protractor_utils/GetStartedPage.js
+++ core/tests/protractor_utils/GetStartedPage.js
@@ -37,7 +37,7 @@ var GetStartedPage = function() {
       throw new Error('Unsupported tag type specified: ' + type);
     }
 
-    await waitFor.visibilityOf(tag, 'Tag is taking too long to appear');
+    waitFor.visibilityOf(tag, 'Tag is taking too long to appear');
     return tag.getAttribute('content');
   };
 };
