d8b5cf6ba6d2b5bc8101e3f77c71f323199799e9
Htesh Tomar <lkbhitesh07@gmail.com>
2021-01-25 18:53:54 +0530

    Replace run_in_transaction call with decorator

--- core/domain/user_jobs_continuous.py
+++ core/domain/user_jobs_continuous.py
@@ -380,6 +380,7 @@ class UserStatsAggregator(jobs.BaseContinuousComputationManager):
         """
         exp_id = args[0]
 
+        @transaction_services.run_in_transaction_wrapper
         def _refresh_average_ratings(user_id, rating, old_rating):
             """Refreshes the average ratings in the given realtime layer.
 
@@ -416,6 +417,7 @@ class UserStatsAggregator(jobs.BaseContinuousComputationManager):
                 model.update_timestamps()
                 model.put()
 
+        @transaction_services.run_in_transaction_wrapper
         def _increment_total_plays_count(user_id):
             """Increments the total plays count of the exploration in the
             realtime layer.
@@ -441,14 +443,12 @@ class UserStatsAggregator(jobs.BaseContinuousComputationManager):
         if exp_summary:
             for user_id in exp_summary.owner_ids:
                 if event_type == feconf.EVENT_TYPE_START_EXPLORATION:
-                    transaction_services.run_in_transaction(
-                        _increment_total_plays_count, user_id)
+                    _increment_total_plays_count(user_id)
 
                 elif event_type == feconf.EVENT_TYPE_RATE_EXPLORATION:
                     rating = args[2]
                     old_rating = args[3]
-                    transaction_services.run_in_transaction(
-                        _refresh_average_ratings, user_id, rating, old_rating)
+                    _refresh_average_ratings(user_id, rating, old_rating)
 
     # Public query method.
     @classmethod
