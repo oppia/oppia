962567b5a97aef9d36e2325fe80c57440f45d75a
Praneeth <praneethg2001@gmail.com>
2021-01-30 14:42:08 +0530

    Apply suggestion from code rev - 1.5

---
+++ core/templates/services/prevent-page-unload-event.service.spec.ts
@@ -0,0 +1,93 @@
+// Copyright 2020 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Unit tests for the preventPageUnloadEventService Service.
+ */
+
+import { TestBed } from '@angular/core/testing';
+import { PreventPageUnloadEventService } from 'services/prevent-page-unload-event.service';
+import { WindowRef } from 'services/contextual/window-ref.service';
+
+describe ('Prevent page unload event service', function() {
+  let preventPageUnloadEventService: PreventPageUnloadEventService = null;
+  let windowRef: WindowRef = null;
+
+  var reloadEvt = document.createEvent('Event');
+  reloadEvt.initEvent('mockbeforeunload', true, true);
+  reloadEvt.returnValue = null;
+  reloadEvt.preventDefault = () => {};
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [PreventPageUnloadEventService]
+    });
+    preventPageUnloadEventService = TestBed.get(PreventPageUnloadEventService);
+    windowRef = TestBed.get(WindowRef);
+  });
+
+  var mockWindow = {
+    addEventListener: function(eventname: string, callback: () => {}) {
+      document.addEventListener('mock' + eventname, callback);
+    },
+    location: {
+      reload: () => {
+        document.dispatchEvent(reloadEvt);
+      }
+    }
+  };
+
+  it('should adding listener', () => {
+    // Testing adding listener
+    expect(preventPageUnloadEventService.getListenerActive()).toBe(false);
+
+    preventPageUnloadEventService.addListener();
+
+    expect(preventPageUnloadEventService.getListenerActive()).toBe(true);
+  });
+
+  it('should removing listener', () => {
+    expect(preventPageUnloadEventService.getListenerActive()).toBe(false);
+    preventPageUnloadEventService.addListener();
+    preventPageUnloadEventService.removeListener();
+
+    expect(preventPageUnloadEventService.getListenerActive()).toBe(false);
+  });
+
+  it('should test if Alert is displayed', () => {
+    spyOnProperty(windowRef, 'nativeWindow', 'get').and.returnValue(mockWindow);
+    preventPageUnloadEventService.addListener();
+    spyOn(reloadEvt, 'preventDefault');
+
+    mockWindow.location.reload();
+
+    expect(reloadEvt.preventDefault).toHaveBeenCalled();
+    expect(preventPageUnloadEventService.getListenerActive()).toBe(true);
+  });
+
+  it('should prevent multiple listeners', () => {
+    // TODO: Does not test for mul listeners... FIX!
+    preventPageUnloadEventService.addListener();
+    expect(preventPageUnloadEventService.getListenerActive()).toBe(true);
+    expect(preventPageUnloadEventService.addListener()).toBeUndefined();
+    expect(preventPageUnloadEventService.getListenerActive()).toBe(true);
+  });
+
+  it('should remove listener on ngondestroy', () => {
+    spyOn(preventPageUnloadEventService, 'removeListener');
+    preventPageUnloadEventService.ngOnDestroy();
+
+    expect(preventPageUnloadEventService.removeListener).toHaveBeenCalled();
+  });
+});
---
+++ core/templates/services/prevent-page-unload-event.service.ts
@@ -0,0 +1,69 @@
+// Copyright 2021 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Service to handle prevent reload events.
+ */
+
+import { Injectable } from '@angular/core';
+import { downgradeInjectable } from '@angular/upgrade/static';
+import { WindowRef } from 'services/contextual/window-ref.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PreventPageUnloadEventService {
+  private listenerActive: boolean;
+  constructor(private windowRef: WindowRef) {
+    this.listenerActive = false;
+  }
+
+  addListener(): void {
+    if (this.listenerActive) {
+      return;
+    }
+    this.windowRef.nativeWindow.addEventListener(
+      'beforeunload', this._preventReloadEventHandler);
+    this.listenerActive = true;
+  }
+
+  removeListener(): void {
+    this.windowRef.nativeWindow.removeEventListener(
+      'beforeunload', this._preventReloadEventHandler);
+    this.listenerActive = false;
+  }
+
+  private _preventReloadEventHandler(
+      e: { preventDefault: () => void; returnValue: string; }
+  ): void {
+    // HTML specification states that authors should use the
+    // below function.
+    e.preventDefault();
+    // The exact value in returnValue is not relevant, but it needs to be set
+    // in order to trigger a confirmation before leaving.
+    e.returnValue = '';
+  }
+
+  getListenerActive(): boolean {
+    return this.listenerActive;
+  }
+
+  ngOnDestroy(): void {
+    this.removeListener();
+  }
+}
+
+angular.module('oppia').factory(
+  'PreventPageUnloadEventService',
+  downgradeInjectable(PreventPageUnloadEventService));
