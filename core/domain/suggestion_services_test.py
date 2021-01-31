bde7761dec9d81f7c628e25de07558a837f12b11
Hardik Katehara <54679643+hardikkat24@users.noreply.github.com>
2021-01-31 20:58:02 +0530

    Fix part of #7450: Modify private methods in SuggestionIntegrationTests (#11797)
    * Changed private method _save_exploration to update_exploration
    
    * Fixed lint errors

--- core/domain/suggestion_services_test.py
+++ core/domain/suggestion_services_test.py
@@ -1315,11 +1315,21 @@ class SuggestionIntegrationTests(test_utils.GenericTestBase):
         self.old_recorded_voiceovers = (
             state_domain.RecordedVoiceovers.from_dict(recorded_voiceovers_dict))
         # Create content in State A with a single audio subtitle.
-        exploration.states['State 1'].update_content(
-            state_domain.SubtitledHtml.from_dict(self.old_content))
-        exploration.states['State 1'].update_recorded_voiceovers(
-            self.old_recorded_voiceovers)
-        exp_services._save_exploration(self.editor_id, exploration, '', [])  # pylint: disable=protected-access
+        content_change = exp_domain.ExplorationChange({
+            'cmd': exp_domain.CMD_EDIT_STATE_PROPERTY,
+            'property_name': exp_domain.STATE_PROPERTY_CONTENT,
+            'state_name': 'State 1',
+            'new_value': self.old_content,
+        })
+        recorded_voiceovers_change = exp_domain.ExplorationChange({
+            'cmd': exp_domain.CMD_EDIT_STATE_PROPERTY,
+            'property_name': exp_domain.STATE_PROPERTY_RECORDED_VOICEOVERS,
+            'state_name': 'State 1',
+            'new_value': recorded_voiceovers_dict,
+        })
+        exp_services.update_exploration(
+            self.editor_id, exploration.id,
+            [content_change, recorded_voiceovers_change], '')
 
         rights_manager.publish_exploration(self.editor, self.EXP_ID)
         rights_manager.assign_role_for_exploration(
