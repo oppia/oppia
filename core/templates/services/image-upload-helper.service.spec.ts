23f5e1b6fee8d2ff49d204b0e153dadea30d51dc
Theo Lipeles <theolipeles@google.com>
2021-02-02 12:25:18 -0500

    migrate image upload helper and tests

--- core/templates/services/image-upload-helper.service.spec.ts
+++ core/templates/services/image-upload-helper.service.spec.ts
@@ -15,51 +15,49 @@
 /**
  * @fileoverview Unit test for ImageUploadHelperService.
  */
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
 
-import { UpgradedServices } from 'services/UpgradedServices';
+import { ImageUploadHelperService } from 'services/image-upload-helper.service';
 
-require('services/assets-backend-api.service.ts');
-require('services/image-upload-helper.service.ts');
 
-describe('ImageUploadHelperService', function() {
-  var ImageUploadHelperService = null;
+fdescribe('ImageUploadHelperService', () => {
+  let imageUploadHelperService: ImageUploadHelperService;
+  let httpTestingController: HttpTestingController;
 
-  beforeEach(angular.mock.module('oppia'));
-  beforeEach(angular.mock.module('oppia', function($provide) {
-    $provide.value('AssetsBackendApiService', {});
-  }));
-  beforeEach(angular.mock.module('oppia', function($provide) {
-    var ugs = new UpgradedServices();
-    for (let [key, value] of Object.entries(ugs.getUpgradedServices())) {
-      $provide.value(key, value);
-    }
-  }));
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+    httpTestingController = TestBed.inject(HttpTestingController);
+    imageUploadHelperService = TestBed.inject(ImageUploadHelperService);
+  });
 
-  beforeEach(angular.mock.inject(function($injector) {
-    ImageUploadHelperService = $injector.get('ImageUploadHelperService');
-  }));
+  afterEach(() => {
+    httpTestingController.verify();
+  });
 
-  it('should convert image data to image file', function() {
-    var imageFile = null;
+  it('should convert image data to image file', () => {
+    let imageFile = null;
     imageFile = (
-      ImageUploadHelperService.convertImageDataToImageFile(
+      imageUploadHelperService.convertImageDataToImageFile(
         'data:image/png;base64,xyz'));
     expect(imageFile instanceof Blob).toBe(true);
   });
 
-  it('should generate a filename for a math SVG', function() {
-    var height = '1d345';
-    var width = '2d455';
-    var verticalPadding = '0d123';
-    var generatedFilename = (
-      ImageUploadHelperService.generateMathExpressionImageFilename(
+  it('should generate a filename for a math SVG', () => {
+    const height = '1d345';
+    const width = '2d455';
+    const verticalPadding = '0d123';
+    const generatedFilename = (
+      imageUploadHelperService.generateMathExpressionImageFilename(
         height, width, verticalPadding));
     expect(generatedFilename.endsWith(
       '_height_1d345_width_2d455_vertical_0d123.svg')).toBe(true);
   });
 
-  it('should remove the role attribute from the Math SVG string', function() {
-    var svgString = (
+  it('should remove the role attribute from the Math SVG string', () => {
+    const svgString = (
       '<svg width="1.33ex" height="1.429ex" viewBox="0 -511.5 572.5 615.4" ' +
       'focusable="false" role= "img" style="vertical-align: -0.241ex;" xmln' +
       's="http://www.w3.org/2000/svg"><g stroke="currentColor" fill="curren' +
@@ -69,9 +67,9 @@ describe('ImageUploadHelperService', function() {
       '14 59T463 140Q466 150 469 151T485 153H489Q504 153 504 145284 52 289Z' +
       '"/></g></svg>'
     );
-    var cleanedSvgString = (
-      ImageUploadHelperService.cleanMathExpressionSvgString(svgString));
-    var expectedCleanSvgString = (
+    const cleanedSvgString = (
+      imageUploadHelperService.cleanMathExpressionSvgString(svgString));
+    const expectedCleanSvgString = (
       '<svg xmlns="http://www.w3.org/2000/svg" width="1.33ex" height="1.429e' +
       'x" viewBox="0 -511.5 572.5 615.4" focusable="false" style="vertical-a' +
       'lign: -0.241ex;"><g stroke="currentColor" fill="currentColor" stro' +
@@ -83,8 +81,8 @@ describe('ImageUploadHelperService', function() {
     expect(cleanedSvgString).toEqual(expectedCleanSvgString);
   });
 
-  it('should replace xmlns:xlink with xmlns in a Math SVG string', function() {
-    var svgString = (
+  it('should replace xmlns:xlink with xmlns in a Math SVG string', () => {
+    const svgString = (
       '<svg xmlns:xlink="http://www.w3.org/1999/xlink" width="1.33ex" height' +
       '="1.429ex" viewBox="0 -511.5 572.5 615.4" focusable="false" style="ve' +
       'rtical-align: -0.241ex;"><g stroke="currentColor" fill="currentColor"' +
@@ -94,9 +92,9 @@ describe('ImageUploadHelperService', function() {
       '140Q466 150 469 151T485 153H489Q504 153 504 145284 52 289Z"></path></' +
       'g></svg>'
     );
-    var cleanedSvgString = (
-      ImageUploadHelperService.cleanMathExpressionSvgString(svgString));
-    var expectedCleanSvgString = (
+    const cleanedSvgString = (
+      imageUploadHelperService.cleanMathExpressionSvgString(svgString));
+    const expectedCleanSvgString = (
       '<svg width="1.33ex" height="1.429ex" viewBox="0 -511.5 572.5 615.4" ' +
       'focusable="false" style="vertical-align: -0.241ex;" xmlns="http://ww' +
       'w.w3.org/2000/svg"><g stroke="currentColor" fill="currentColor" stro' +
@@ -108,8 +106,8 @@ describe('ImageUploadHelperService', function() {
     expect(cleanedSvgString).toEqual(expectedCleanSvgString);
   });
 
-  it('should extract dimensions from an math SVG string', function() {
-    var svgString = (
+  it('should extract dimensions from an math SVG string', () => {
+    const svgString = (
       '<svg width="1.33ex" height="1.429ex" viewBox="0 -511.5 572.5 615.4" ' +
       'focusable="false" style="vertical-align: -0.241ex;" xmlns="http://ww' +
       'w.w3.org/2000/svg"><g stroke="currentColor" fill="currentColor" stro' +
@@ -118,10 +116,10 @@ describe('ImageUploadHelperService', function() {
       '386 350 336Q290 115 290 78Q290 50 306 38T341 26Q378 26 414 59T463 14' +
       '0Q466 150 469 151T485 153H489Q504 153 504 145284 52 289Z"/></g></svg>'
     );
-    var dimensions = (
-      ImageUploadHelperService.extractDimensionsFromMathExpressionSvgString(
+    const dimensions = (
+      imageUploadHelperService.extractDimensionsFromMathExpressionSvgString(
         svgString));
-    var expectedDimension = {
+    const expectedDimension = {
       height: '1d429',
       width: '1d33',
       verticalPadding: '0d241'
@@ -129,12 +127,12 @@ describe('ImageUploadHelperService', function() {
     expect(dimensions).toEqual(expectedDimension);
   });
 
-  it('should generate a filename for a normal image', function() {
-    var height = 720;
-    var width = 180;
-    var format = 'png';
-    var generatedFilename = (
-      ImageUploadHelperService.generateImageFilename(height, width, format));
+  it('should generate a filename for a normal image', () => {
+    const height = 720;
+    const width = 180;
+    const format = 'png';
+    const generatedFilename = (
+      imageUploadHelperService.generateImageFilename(height, width, format));
     expect(generatedFilename.endsWith('_height_720_width_180.png')).toBe(true);
   });
 });
--- core/templates/services/image-upload-helper.service.ts
+++ core/templates/services/image-upload-helper.service.ts
@@ -16,174 +16,185 @@
  * @fileoverview Image upload helper service.
  */
 
-require('services/assets-backend-api.service.ts');
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { Injectable } from '@angular/core';
+import { downgradeInjectable } from '@angular/upgrade/static';
 
-// TODO(#9186): Change variable name to 'constants' once this file
-// is migrated to Angular.
-import Constants from 'assets/constants';
+import constants from 'assets/constants';
+import { AssetsBackendApiService } from './assets-backend-api.service';
 
-angular.module('oppia').factory('ImageUploadHelperService', [
-  '$sce', 'AssetsBackendApiService',
-  function($sce, AssetsBackendApiService) {
-    var _generateDateTimeStringForFilename = function() {
-      var date = new Date();
-      return date.getFullYear() +
-        ('0' + (date.getMonth() + 1)).slice(-2) +
-        ('0' + date.getDate()).slice(-2) + '_' +
-        ('0' + date.getHours()).slice(-2) +
-        ('0' + date.getMinutes()).slice(-2) +
-        ('0' + date.getSeconds()).slice(-2) + '_' +
-        Math.random().toString(36).substr(2, 10);
-    };
-    return {
-      convertImageDataToImageFile: function(dataURI) {
-        // Convert base64/URLEncoded data component to raw binary data
-        // held in a string.
-        var byteString = atob(dataURI.split(',')[1]);
+@Injectable({
+  providedIn: 'root'
+})
+export class ImageUploadHelperService {
+  constructor(
+    private _assetsBackendApiService: AssetsBackendApiService,
+    private _sanitizer: DomSanitizer) {}
 
-        // Separate out the mime component.
-        var mime = dataURI.split(',')[0].split(':')[1].split(';')[0];
+  private _generateDateTimeStringForFilename() {
+    const date = new Date();
+    return date.getFullYear() +
+      ('0' + (date.getMonth() + 1)).slice(-2) +
+      ('0' + date.getDate()).slice(-2) + '_' +
+      ('0' + date.getHours()).slice(-2) +
+      ('0' + date.getMinutes()).slice(-2) +
+      ('0' + date.getSeconds()).slice(-2) + '_' +
+      Math.random().toString(36).substr(2, 10);
+  }
 
-        // Write the bytes of the string to a typed array.
-        var ia = new Uint8Array(byteString.length);
-        for (var i = 0; i < byteString.length; i++) {
-          ia[i] = byteString.charCodeAt(i);
+  convertImageDataToImageFile(dataURI: string): Blob {
+    // Convert base64/URLEncoded data component to raw binary data
+    // held in a string.
+    const byteString = atob(dataURI.split(',')[1]);
+
+    // Separate out the mime component.
+    const mime = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+    // Write the bytes of the string to a typed array.
+    const ia = new Uint8Array(byteString.length);
+    for (let i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+
+    const blob = new Blob([ia], { type: mime });
+    if (blob.type.match('image') &&
+      blob.size > 0) {
+      return blob;
+    } else {
+      return null;
+    }
+  }
+
+  getInvalidSvgTagsAndAttrs(dataURI: string):
+    {tags: string[], attrs: string[]} {
+    // Convert base64/URLEncoded data component to raw binary data
+    // held in a string.
+    const svgString = atob(dataURI.split(',')[1]);
+    const domParser = new DOMParser();
+    const doc = domParser.parseFromString(svgString, 'image/svg+xml');
+    const invalidTags = [];
+    const invalidAttrs = [];
+    const allowedTags = Object.keys(constants.SVG_ATTRS_WHITELIST);
+    let nodeTagName = null;
+    doc.querySelectorAll('*').forEach((node) => {
+      nodeTagName = node.tagName.toLowerCase();
+      if (allowedTags.indexOf(nodeTagName) !== -1) {
+        for (let i = 0; i < node.attributes.length; i++) {
+          if (constants.SVG_ATTRS_WHITELIST[nodeTagName].indexOf(
+            node.attributes[i].name.toLowerCase()) === -1) {
+            invalidAttrs.push(
+              node.tagName + ':' + node.attributes[i].name);
+          }
         }
+      } else {
+        invalidTags.push(node.tagName);
+      }
+    });
+    return { tags: invalidTags, attrs: invalidAttrs };
+  }
 
-        var blob = new Blob([ia], { type: mime });
-        if (blob.type.match('image') &&
-          blob.size > 0) {
-          return blob;
-        } else {
-          return null;
-        }
-      },
+  getTrustedResourceUrlForThumbnailFilename(
+      imageFileName: string, entityType: string, entityId: string):
+        SafeResourceUrl {
+    const encodedFilepath = window.encodeURIComponent(imageFileName);
+    return this._sanitizer.bypassSecurityTrustResourceUrl(
+      this._assetsBackendApiService.getThumbnailUrlForPreview(
+        entityType, entityId, encodedFilepath));
+  }
 
-      getInvalidSvgTagsAndAttrs: function(dataURI) {
-        // Convert base64/URLEncoded data component to raw binary data
-        // held in a string.
-        var svgString = atob(dataURI.split(',')[1]);
-        var domParser = new DOMParser();
-        var doc = domParser.parseFromString(svgString, 'image/svg+xml');
-        var invalidTags = [];
-        var invalidAttrs = [];
-        var allowedTags = Object.keys(Constants.SVG_ATTRS_WHITELIST);
-        var nodeTagName = null;
-        doc.querySelectorAll('*').forEach((node) => {
-          nodeTagName = node.tagName.toLowerCase();
-          if (allowedTags.indexOf(nodeTagName) !== -1) {
-            for (var i = 0; i < node.attributes.length; i++) {
-              if (Constants.SVG_ATTRS_WHITELIST[nodeTagName].indexOf(
-                node.attributes[i].name.toLowerCase()) === -1) {
-                invalidAttrs.push(
-                  node.tagName + ':' + node.attributes[i].name);
-              }
-            }
-          } else {
-            invalidTags.push(node.tagName);
-          }
-        });
-        return { tags: invalidTags, attrs: invalidAttrs };
-      },
+  generateImageFilename(
+      height: number, width: number, extension: string): string {
+    return 'img_' +
+      this._generateDateTimeStringForFilename() +
+      '_height_' + height +
+      '_width_' + width +
+      '.' + extension;
+  }
 
-      getTrustedResourceUrlForThumbnailFilename: function(
-          imageFileName, entityType, entityId) {
-        var encodedFilepath = window.encodeURIComponent(imageFileName);
-        return $sce.trustAsResourceUrl(
-          AssetsBackendApiService.getThumbnailUrlForPreview(
-            entityType, entityId, encodedFilepath));
-      },
-
-      generateImageFilename: function(height, width, extension) {
-        return 'img_' +
-          _generateDateTimeStringForFilename() +
-          '_height_' + height +
-          '_width_' + width +
-          '.' + extension;
-      },
-
-      cleanMathExpressionSvgString: function(svgString) {
-        // We need to modify/remove unnecessary attributes added by mathjax
-        // from the svg tag.
-        var domParser = new DOMParser();
-        var doc = domParser.parseFromString(svgString, 'image/svg+xml');
-        doc.querySelectorAll('*').forEach((node) => {
-          if (node.tagName.toLowerCase() === 'svg') {
-            node.removeAttribute('xmlns:xlink');
-            node.removeAttribute('role');
-            // We are removing this attribute, because currently it is not in
-            // the white list of valid attributes.
-            node.removeAttribute('aria-hidden');
-            node.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-          }
-          // Remove the custom data attributes added by MathJax.
-          // These custom attributes don't affect the rendering of the SVGs,
-          // and they are not present in the white list for allowed attributes.
-          for (var i = 0; i < node.attributes.length; i++) {
-            if (node.attributes[i].name.toLowerCase().startsWith('data-')) {
-              node.removeAttribute(node.attributes[i].name.toLowerCase());
-            }
-          }
-        });
-        return doc.documentElement.outerHTML;
-      },
-
-      extractDimensionsFromMathExpressionSvgString: function(svgString) {
-        // The method below extracts the dimensions from the attributes of a
-        // math SVG string generated by mathJax.
-        var domParser = new DOMParser();
-        var dimensions = {
-          height: '',
-          width: '',
-          verticalPadding: ''
-        };
-        var doc = domParser.parseFromString(svgString, 'image/svg+xml');
-        doc.querySelectorAll('*').forEach((node) => {
-          // Mathjax SVGs have relative dimensions in the unit of 'ex' rather
-          // than 'px'(pixels). Hence the dimesions have decimal points in them,
-          // we need to replace these decimals with a letter so that it's easier
-          // to process and validate the filnames.
-          if (node.tagName.toLowerCase() === 'svg') {
-            dimensions.height = (
-              (node.getAttribute('height').match(/\d+\.*\d*/g)[0]).replace(
-                '.', 'd'));
-            dimensions.width = (
-              (node.getAttribute('width').match(/\d+\.*\d*/g)[0]).replace(
-                '.', 'd'));
-            // This attribute is useful for the vertical allignment of the
-            // Math SVG while displaying inline with other text.
-            // Math SVGs don't necessarily have a vertical allignment, in that
-            // case we assign it zero.
-            var styleValue = node.getAttribute('style').match(/\d+\.*\d*/g);
-            if (styleValue) {
-              dimensions.verticalPadding = styleValue[0].replace('.', 'd');
-            } else {
-              dimensions.verticalPadding = '0';
-            }
-          }
-        });
-        return dimensions;
-      },
-
-      generateMathExpressionImageFilename: function(
-          height, width, verticalPadding) {
-        var filename = (
-          'mathImg_' +
-            _generateDateTimeStringForFilename() +
-            '_height_' + height +
-            '_width_' + width +
-            '_vertical_' + verticalPadding +
-            '.' + 'svg'
-        );
-        var filenameRegexString = Constants.MATH_SVG_FILENAME_REGEX;
-        var filenameRegex = RegExp(filenameRegexString, 'g');
-        if (filenameRegex.exec(filename)) {
-          return filename;
-        } else {
-          throw new Error(
-            'The Math SVG filename format is invalid.');
+  cleanMathExpressionSvgString(svgString: string): string {
+    // We need to modify/remove unnecessary attributes added by mathjax
+    // from the svg tag.
+    const domParser = new DOMParser();
+    const doc = domParser.parseFromString(svgString, 'image/svg+xml');
+    doc.querySelectorAll('*').forEach((node) => {
+      if (node.tagName.toLowerCase() === 'svg') {
+        node.removeAttribute('xmlns:xlink');
+        node.removeAttribute('role');
+        // We are removing this attribute, because currently it is not in
+        // the white list of valid attributes.
+        node.removeAttribute('aria-hidden');
+        node.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+      }
+      // Remove the custom data attributes added by MathJax.
+      // These custom attributes don't affect the rendering of the SVGs,
+      // and they are not present in the white list for allowed attributes.
+      for (let i = 0; i < node.attributes.length; i++) {
+        if (node.attributes[i].name.toLowerCase().startsWith('data-')) {
+          node.removeAttribute(node.attributes[i].name.toLowerCase());
         }
       }
-    };
+    });
+    return doc.documentElement.outerHTML;
   }
-]);
+
+  extractDimensionsFromMathExpressionSvgString(svgString: string):
+    {height: string, width: string, verticalPadding: string} {
+    // The method below extracts the dimensions from the attributes of a
+    // math SVG string generated by mathJax.
+    const domParser = new DOMParser();
+    const dimensions = {
+      height: '',
+      width: '',
+      verticalPadding: ''
+    };
+    const doc = domParser.parseFromString(svgString, 'image/svg+xml');
+    doc.querySelectorAll('*').forEach((node) => {
+      // Mathjax SVGs have relative dimensions in the unit of 'ex' rather
+      // than 'px'(pixels). Hence the dimesions have decimal points in them,
+      // we need to replace these decimals with a letter so that it's easier
+      // to process and validate the filnames.
+      if (node.tagName.toLowerCase() === 'svg') {
+        dimensions.height = (
+          (node.getAttribute('height').match(/\d+\.*\d*/g)[0]).replace(
+            '.', 'd'));
+        dimensions.width = (
+          (node.getAttribute('width').match(/\d+\.*\d*/g)[0]).replace(
+            '.', 'd'));
+        // This attribute is useful for the vertical allignment of the
+        // Math SVG while displaying inline with other text.
+        // Math SVGs don't necessarily have a vertical allignment, in that
+        // case we assign it zero.
+        const styleValue = node.getAttribute('style').match(/\d+\.*\d*/g);
+        if (styleValue) {
+          dimensions.verticalPadding = styleValue[0].replace('.', 'd');
+        } else {
+          dimensions.verticalPadding = '0';
+        }
+      }
+    });
+    return dimensions;
+  }
+
+  generateMathExpressionImageFilename(
+      height: string, width: string, verticalPadding: string): string {
+    const filename = (
+      'mathImg_' +
+        this._generateDateTimeStringForFilename() +
+        '_height_' + height +
+        '_width_' + width +
+        '_vertical_' + verticalPadding +
+        '.' + 'svg'
+    );
+    const filenameRegexString = constants.MATH_SVG_FILENAME_REGEX;
+    const filenameRegex = RegExp(filenameRegexString, 'g');
+    if (filenameRegex.exec(filename)) {
+      return filename;
+    } else {
+      throw new Error(
+        'The Math SVG filename format is invalid.');
+    }
+  }
+}
+
+angular.module('oppia').factory(
+  'ImageUploadHelperService', downgradeInjectable(ImageUploadHelperService));
