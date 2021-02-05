540f6a447066f7fd6077317bd89cffe1cf09cf4e
krishita30j <jainkrishita15@gmail.com>
2021-02-05 10:51:30 +0530

    Modified file

--- assets/constants.ts
+++ assets/constants.ts
@@ -5612,5 +5612,5 @@ export default {
   // up to 4 bytes long.
   "MAX_COMMIT_MESSAGE_LENGTH": 375,
 
-  "DEV_MODE": true
+  "DEV_MODE": false
 } as const;
--- core/templates/pages/exploration-editor-page/history-tab/history-tab.component.ts
+++ core/templates/pages/exploration-editor-page/history-tab/history-tab.component.ts
@@ -121,8 +121,7 @@ angular.module('oppia').component('historyTab', {
               for (
                 var i = currentVersion - 1; i >= lowestVersionIndex; i--) {
                 var versionNumber = explorationSnapshots[i].version_number;
-                ctrl.explorationVersionMetadata[
-                  currentVersion - versionNumber] = {
+                ctrl.explorationVersionMetadata[versionNumber - 1] = {
                   committerId: explorationSnapshots[i].committer_id,
                   createdOnMsecsStr: (
                     DateTimeFormatService
