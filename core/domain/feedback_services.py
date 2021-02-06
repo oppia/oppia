7e494a21606de29aab536055ea43558d99a81234
Htesh Tomar <lkbhitesh07@gmail.com>
2021-02-06 20:14:49 +0530

    Added transactional in the end of function

--- core/domain/feedback_services.py
+++ core/domain/feedback_services.py
@@ -819,7 +819,8 @@ def enqueue_feedback_message_batch_email_task(user_id):
         feconf.DEFAULT_FEEDBACK_MESSAGE_EMAIL_COUNTDOWN_SECS)
 
 
-def enqueue_feedback_message_instant_email_task(user_id, reference):
+def enqueue_feedback_message_instant_email_task_transactional(
+        user_id, reference):
     """Adds a 'send feedback email' (instant) task into the task queue.
 
     Args:
