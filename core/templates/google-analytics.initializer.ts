11e491495fc43578719faac2922aed634f29c424
Sean Lip <sean@seanlip.org>
2021-02-09 02:47:01 -0800

    Upgrade GA version to use gtag. (#11882)

--- core/templates/google-analytics.initializer.ts
+++ core/templates/google-analytics.initializer.ts
@@ -13,24 +13,24 @@
 // limitations under the License.
 
 /**
- * @fileoverview Initialization of Google Analytics..
+ * @fileoverview Initialization of Google Analytics (gtag.js).
  */
 
 const constants = require('constants.ts');
 
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})
-(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
 (function() {
   if (constants.ANALYTICS_ID && constants.SITE_NAME_FOR_ANALYTICS) {
-    ga('create', constants.ANALYTICS_ID, 'auto', {allowLinker: true});
-    ga('set', 'anonymizeIp', true);
-    ga('set', 'forceSSL', true);
-    ga('require', 'linker');
-    ga('linker:autoLink', [constants.SITE_NAME_FOR_ANALYTICS]);
-    ga('send', 'pageview');
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('set', 'linker', {
+      'domains': [constants.SITE_NAME_FOR_ANALYTICS]
+    });
+    gtag('js', new Date());
+    gtag('config', constants.ANALYTICS_ID, {
+      'anonymize_ip': true,
+      'forceSSL': true,
+    });
   }
 })()
