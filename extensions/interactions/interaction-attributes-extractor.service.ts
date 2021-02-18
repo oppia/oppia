03f78fc5d3fd00ef0fadf5622b506b87701d7f2d
17sushmita <17sushmita@gmail.com>
2021-02-18 12:29:29 +0530

    Add absolute import for interactionID in interaction-attributes-extractor service

--- extensions/interactions/interaction-attributes-extractor.service.ts
+++ extensions/interactions/interaction-attributes-extractor.service.ts
@@ -25,7 +25,7 @@ import { InteractionCustomizationArgs } from
   'extensions/interactions/customization-args-defs';
 import { InteractionObjectFactory } from
   'domain/exploration/InteractionObjectFactory';
-import { InteractionId } from './rule-input-defs';
+import { InteractionId } from 'interactions/rule-input-defs';
 
 const INTERACTION_SPECS = require('interactions/interaction_specs.json');
 
