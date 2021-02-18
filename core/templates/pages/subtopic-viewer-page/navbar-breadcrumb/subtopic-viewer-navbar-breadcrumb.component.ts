b8aa1abd8a00857a4b23f6e4daf1c91a8bb77d77
mridul-netizen <mridulsetia18@gmail.com>
2021-02-18 23:16:14 +0530

    migrated subtopic-viewer

--- core/templates/pages/subtopic-viewer-page/navbar-breadcrumb/subtopic-viewer-navbar-breadcrumb.component.ts
+++ core/templates/pages/subtopic-viewer-page/navbar-breadcrumb/subtopic-viewer-navbar-breadcrumb.component.ts
@@ -20,7 +20,7 @@ import { Component, OnInit } from '@angular/core';
 import { downgradeComponent } from '@angular/upgrade/static';
 
 import { ClassroomDomainConstants } from
-  'domain/classroom/classroom-domain.constants.ts';
+  'domain/classroom/classroom-domain.constants';
 import { ReadOnlySubtopicPageData } from
   'domain/subtopic_viewer/ReadOnlySubtopicPageObjectFactory';
 import { SubtopicViewerBackendApiService } from
