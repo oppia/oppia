077b94e5eb3575af4acda315555ee124c1cbf0de
farees <fareezzhussain@gmail.com>
2021-02-22 09:27:08 +0530

    reverting browser.actions().sendKeys()

--- core/tests/protractor/accessibility.js
+++ core/tests/protractor/accessibility.js
@@ -59,11 +59,8 @@ describe('screenreader and keyboard user accessibility features', function() {
   var DONATE_URL = 'http://localhost:9001/donate';
 
   var holdCtrlAndPressKey = async function(key) {
-    await action.sendKeys(
-      'Browser actions',
-      browser.actions(),
-      protractor.Key.chord(protractor.Key.CONTROL, key)
-    ).perform();
+    await browser.actions().sendKeys(
+      protractor.Key.chord(protractor.Key.CONTROL, key)).perform();
   };
 
   beforeAll(async function() {
@@ -79,8 +76,7 @@ describe('screenreader and keyboard user accessibility features', function() {
   var checkActionShortcuts = async function(key, elementToFocus) {
     await waitFor.presenceOf(elementToFocus, 'Element took too long to load');
     // Should move the focus to the elementToFocus.
-    await action.sendKeys(
-      'Browser', browser, key).perform();
+    await browser.actions().sendKeys(key).perform();
     expect(
       await browser.driver.switchTo().activeElement()
         .getAttribute('class')).toEqual(
@@ -89,18 +85,15 @@ describe('screenreader and keyboard user accessibility features', function() {
     // Should move the focus away from the elementToFocus.
     // Tab must be pressed twice to move focus away from categoryBar.
     // The categoryBar shares the same class as the next element in DOM order.
-    await action.sendKeys(
-      'Browser', browser, protractor.Key.TAB).perform();
-    await action.sendKeys(
-      'Browser', browser, protractor.Key.TAB).perform();
+    await browser.actions().sendKeys(protractor.Key.TAB).perform();
+    await browser.actions().sendKeys(protractor.Key.TAB).perform();
     expect(
       await browser.driver.switchTo().activeElement()
         .getAttribute('class')).not.toEqual(
       await elementToFocus.getAttribute('class'));
 
     // Should move the focus back to the elementToFocus.
-    await action.sendKeys(
-      'Browser', browser, key).perform();
+    await browser.actions().sendKeys(key).perform();
     expect(
       await browser.driver.switchTo().activeElement()
         .getAttribute('class')).toEqual(
@@ -109,8 +102,7 @@ describe('screenreader and keyboard user accessibility features', function() {
 
   it('should skip to the main content element', async function() {
     await libraryPage.get();
-    await action.sendKeys(
-      'Browser', browser, protractor.Key.TAB).perform();
+    await browser.actions().sendKeys(protractor.Key.TAB).perform();
     await waitFor.elementToBeClickable(skipLink, 'Could not click skip link');
     await action.click('Skip link', skipLink);
     expect(await mainContent.getAttribute('class')).toEqual(
@@ -577,20 +569,17 @@ describe('screenreader and keyboard user accessibility features', function() {
       // Should press 'j' key to navigate to the next card.
       await waitFor.elementToBeClickable(continueButton);
       await checkActionShortcuts('j', continueButton);
-      await action.sendKeys(
-        'Browser', browser, protractor.Key.ENTER).perform();
+      await browser.actions().sendKeys(protractor.Key.ENTER).perform();
 
       // Should press 'k' key to navigate to the previous card.
       await waitFor.elementToBeClickable(backButton);
       await checkActionShortcuts('k', backButton);
-      await action.sendKeys(
-        'Browser', browser, protractor.Key.ENTER).perform();
+      await browser.actions().sendKeys(protractor.Key.ENTER).perform();
 
       // Should press 'j' key to navigate to the next card.
       await waitFor.elementToBeClickable(nextButton);
       await checkActionShortcuts('j', nextButton);
-      await action.sendKeys(
-        'Browser', browser, protractor.Key.ENTER).perform();
+      await browser.actions().sendKeys(protractor.Key.ENTER).perform();
 
       // Should safely exit out of the exploration.
       await action.click('Oppia Logo', oppiaLogo);
