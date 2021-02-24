4232c50deef3cd41964fcaa58bd61d753154096f
Hardik Katehara <hardikkatehara@gmail.com>
2021-02-24 17:11:49 +0530

    changes

--- core/domain/exp_jobs_one_off.py
+++ core/domain/exp_jobs_one_off.py
@@ -84,7 +84,8 @@ class RemoveDeprecatedExplorationModelFieldsOneOffJob(
                     del exp_model._values[deprecated_field]  # pylint: disable=protected-access
                 removed_deprecated_field = True
         if removed_deprecated_field:
-            exp_models.ExplorationModel.put_multi([exp_model])
+            exp_models.ExplorationModel.put_multi(
+                [exp_model], update_last_updated_time=False)
             yield ('SUCCESS_REMOVED - ExplorationModel', exp_model.id)
         else:
             yield ('SUCCESS_ALREADY_REMOVED - ExplorationModel', exp_model.id)
