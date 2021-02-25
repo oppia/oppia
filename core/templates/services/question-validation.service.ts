d660e451c13bf42bc4b560de102bea1cfde33184
ashutoshc8101 <ashutoshc8101@gmail.com>
2021-02-26 00:46:19 +0530

    Minor fixes

--- core/templates/services/question-validation.service.ts
+++ core/templates/services/question-validation.service.ts
@@ -12,16 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/**
+ * @fileoverview Service to validate a question.
+ *
+ */
+
 import { Injectable } from '@angular/core';
 import { downgradeInjectable } from '@angular/upgrade/static';
 import { StateEditorService } from 'components/state-editor/state-editor-properties-services/state-editor.service';
 import { Question } from 'domain/question/QuestionObjectFactory';
 import { Misconception } from 'domain/skill/MisconceptionObjectFactory';
-
-/**
- * @fileoverview Service to validate a question.
- *
- */
 
 @Injectable({
   providedIn: 'root'
