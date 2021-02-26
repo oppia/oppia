42c3e5c51fa612c2c69c3aea7b96a095a6c513ff
YBCS <budha627@gmail.com>
2021-02-26 15:04:04 +0530

    add async keyword to recordChapterCompletionAsync

--- core/templates/domain/story_viewer/story-viewer-backend-api.service.ts
+++ core/templates/domain/story_viewer/story-viewer-backend-api.service.ts
@@ -124,7 +124,7 @@ export class StoryViewerBackendApiService {
     });
   }
 
-  recordChapterCompletion(
+  async recordChapterCompletionAsync(
       topicUrlFragment: string,
       classroomUrlFragment: string,
       storyUrlFragment: string,
