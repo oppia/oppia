281ae8a49e0dadc4ccd54398a6d1170b12d69c87
Hardik Katehara <hardikkatehara@gmail.com>
2021-02-26 15:06:47 +0530

    update_timestamp changed

--- core/domain/exp_jobs_one_off.py
+++ core/domain/exp_jobs_one_off.py
@@ -84,7 +84,7 @@ class RemoveDeprecatedExplorationModelFieldsOneOffJob(
                     del exp_model._values[deprecated_field]  # pylint: disable=protected-access
                 removed_deprecated_field = True
         if removed_deprecated_field:
-            exp_model.update_timestamps()
+            exp_model.update_timestamps(update_last_updated_time=False)
             exp_models.ExplorationModel.put_multi([exp_model])
             yield ('SUCCESS_REMOVED - ExplorationModel', exp_model.id)
         else:
