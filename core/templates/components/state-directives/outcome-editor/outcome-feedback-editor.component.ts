56e0ea96f6ce9ce014b7746246adee3be0371c7f
Pankaj Dahiya <pankaj.id.008@gmail.com>
2021-02-28 11:57:55 +0530

    solving errors

--- core/templates/components/state-directives/outcome-editor/outcome-feedback-editor.component.ts
+++ core/templates/components/state-directives/outcome-editor/outcome-feedback-editor.component.ts
@@ -26,7 +26,7 @@ import { UrlInterpolationService } from
 import { ContextService } from 'services/context.service.ts';
 
 @Component({
-  selector: 'outcome-feedback-editor',
+  selector: 'oppia-outcome-feedback-editor',
   templateUrl: './outcome-feedback-editor.component.html',
   styleUrls: []
 })
