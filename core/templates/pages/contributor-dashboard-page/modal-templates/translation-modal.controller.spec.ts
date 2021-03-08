c91ec03299a1993ca00f65ea817173743cded3d3
jimbyo <35385366+jimbyo@users.noreply.github.com>
2021-03-07 18:07:19 -0800

    Fix #10721 Refactor translation-text-service (#11757)
    * refactor stateWiseContentIds data-structure
    
    * move translate-text-container class
    
    * fix lints
    
    * fix lints
    
    * replace any with unknown
    
    * at as string for text
    
    * fix lints
    
    * fix frontend tests
    
    * fix lints
    
    * fix manual bugs in the go back button
    
    * edit frontend_tests
    
    * resolving comments
    
    * fix lints
    
    * fix lints
    
    * resolve comments
    
    * fix lints
    
    * change structure of StateAndContent Class
    
    * fix comments
    
    * fix comments

--- core/templates/pages/contributor-dashboard-page/modal-templates/translation-modal.controller.spec.ts
+++ core/templates/pages/contributor-dashboard-page/modal-templates/translation-modal.controller.spec.ts
@@ -144,6 +144,12 @@ describe('Translation Modal Controller', function() {
     expect($scope.uploadingTranslation).toBe(false);
   });
 
+  it('should return null when clicking on a disabled back button',
+    function() {
+      $scope.returnToPreviousTranslation();
+      expect($scope.textToTranslate).toBe(null);
+    });
+
   it('should broadcast copy to ck editor when clicking on content',
     function() {
       spyOn(CkEditorCopyContentService, 'broadcastCopy').and
