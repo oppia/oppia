bdedf7698a6500ad4640c73e23770bf2b74ea573
Vojtěch Jelínek <vojtin.j@gmail.com>
2021-03-10 22:18:12 +0100

    Fix tests

--- core/domain/exp_jobs_one_off.py
+++ core/domain/exp_jobs_one_off.py
@@ -87,8 +87,7 @@ class RemoveDeprecatedExplorationModelFieldsOneOffJob(
                 removed_deprecated_field = True
 
         if removed_deprecated_field:
-            exp_model.update_timestamps(update_last_updated_time=False)
-            exp_models.ExplorationModel.put_multi([exp_model])
+            exp_models.ExplorationModel.put_multi_for_bot([exp_model])
             yield ('SUCCESS_REMOVED - ExplorationModel', exp_model.id)
         else:
             yield ('SUCCESS_ALREADY_REMOVED - ExplorationModel', exp_model.id)
@@ -142,56 +141,6 @@ class RemoveDeprecatedExplorationRightsModelFieldsOneOffJob(
     def reduce(key, values):
         """Implements the reduce function for this job."""
         yield (key, len(values))
-
-
-class RegenerateStringPropertyIndexOneOffJob(
-        jobs.BaseMapReduceOneOffJobManager):
-    """One-off job for regenerating the index of models changed to use an
-    indexed StringProperty.
-
-    Cloud NDB dropped support for StringProperty(indexed=False) and
-    TextProperty(indexed=True). Therefore, to prepare for the migration to Cloud
-    NDB, we need to regenerate the indexes for every model that has been changed
-    in this way.
-
-    https://cloud.google.com/appengine/docs/standard/python/datastore/indexes#unindexed-properties:
-    > changing a property from unindexed to indexed does not affect any existing
-    > entities that may have been created before the change. Queries filtering
-    > on the property will not return such existing entities, because the
-    > entities weren't written to the query's index when they were created. To
-    > make the entities accessible by future queries, you must rewrite them to
-    > Datastore so that they will be entered in the appropriate indexes. That
-    > is, you must do the following for each such existing entity:
-    > 1.  Retrieve (get) the entity from Datastore.
-    > 2.  Write (put) the entity back to Datastore.
-    """
-
-    @classmethod
-    def entity_classes_to_map_over(cls):
-        return [
-            exp_models.ExplorationModel,
-            feedback_models.GeneralFeedbackMessageModel,
-            improvements_models.TaskEntryModel,
-            skill_models.SkillModel,
-            stats_models.ExplorationAnnotationsModel,
-            story_models.StoryModel,
-            story_models.StorySummaryModel,
-        ]
-
-    @staticmethod
-    def map(model):
-        model_kind = type(model).__name__
-        if isinstance(model, base_models.VersionedModel):
-            # Change the method resolution order of model to use BaseModel's
-            # implementation of `put`.
-            model = super(base_models.VersionedModel, model)
-        model.update_timestamps(update_last_updated_time=False)
-        model.put()
-        yield (model_kind, 1)
-
-    @staticmethod
-    def reduce(key, counts):
-        yield (key, len(counts))
 
 
 class ExplorationFirstPublishedOneOffJob(jobs.BaseMapReduceOneOffJobManager):
