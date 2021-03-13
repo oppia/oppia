2418390c3cd62a74a368f8b3b544c967a203a1e7
Vojtěch Jelínek <vojtin.j@gmail.com>
2021-03-13 13:29:09 +0100

    Fix jobs

--- core/jobs_registry.py
+++ core/jobs_registry.py
@@ -56,6 +56,7 @@ ONE_OFF_JOB_MANAGERS = [
     activity_jobs_one_off.ValidateSnapshotMetadataModelsJob,
     activity_jobs_one_off.SnapshotMetadataCommitMsgAuditOneOffJob,
     activity_jobs_one_off.SnapshotMetadataCommitMsgShrinkOneOffJob,
+    activity_jobs_one_off.ExplorationModelsFillLastHumanUpdatedOneOffJob,
     auth_jobs_one_off.AuditFirebaseImportReadinessOneOffJob,
     auth_jobs_one_off.PopulateFirebaseAccountsOneOffJob,
     collection_jobs_one_off.CollectionMigrationOneOffJob,
