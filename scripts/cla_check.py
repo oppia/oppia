ae68b0a645bb898e8ebc6282a39c59cb49965cb1
Praneeth <praneethg2001@gmail.com>
2021-03-18 00:06:02 +0530

    CLA check Github action

---
+++ scripts/cla_check.py
@@ -0,0 +1,75 @@
+# Copyright 2021 The Oppia Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""This script performs cla check for PR authors.
+"""
+
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+import os.path
+import sys
+import json
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1naQC7iEfnro5iOjTFEn7iPCxNMPaPa4YnIddjT5CTM8'
+SAMPLE_RANGE_NAME = 'Usernames'
+TOKEN = os.environ['SHEETS_TOKEN']
+
+_PARSER = argparse.ArgumentParser(
+    description="""
+Run the script from the oppia root folder:
+    python -m scripts.cla_check
+Note that the root folder MUST be named 'oppia'.
+""")
+
+def getValues():
+    result = None
+    creds = None
+    try:
+        creds = Credentials.from_authorized_user_info(
+            json.loads(TOKEN), SCOPES)
+        service = build('sheets', 'v4', credentials=creds)
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                    range=SAMPLE_RANGE_NAME).execute()
+        result = result.get('values', [])
+    except:
+        print("API error:", sys.exc_info()[0])
+    finally:
+        return result
+
+
+def main():
+    prAuthor = [sys.argv[1]]
+    print('Checking if ', prAuthor, ' has signed the CLA')
+    values = getValues()
+    if not values:
+        print('No data found.')
+        exit(1)
+    if(prAuthor in values):
+        print(prAuthor, ' has signed the CLA')
+        exit(0)
+    else:
+        print(prAuthor, ' has not signed the CLA')
+        exit(1)
+
+
+if __name__ == '__main__':
+    main()
