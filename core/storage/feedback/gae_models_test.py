0d0625d6b150b899b6d543a547c4de579d5fbff8
Jacque Li <jacqueli@google.com>
2021-03-18 22:25:15 -0700

    Add test for model

--- core/storage/feedback/gae_models_test.py
+++ core/storage/feedback/gae_models_test.py
@@ -523,3 +523,103 @@ class UnsentFeedbackEmailModelTest(test_utils.GenericTestBase):
             retrieved_instance.feedback_message_references,
             [message_reference_dict])
         self.assertEqual(retrieved_instance.retries, 0)
+
+class AppFeedbackReportModelTests(test_utils.GenericTestBase):
+    """Tests for the AppFeedbackReport class."""
+
+    PLATFORM = 'android'
+    # Timestamp in ms since epoch for Mar 19 2021 04:21:55 UTC
+    REPORT_SUBMITTED_TIMESTAMP = 1616127715097
+    TICKET_ID = '%s.%s.%s' % (
+                'random_hash', self.TICKET_CREATION_TIMESTAMP,
+                '16CharString1234')
+    # Timestamp in ms since epoch for Mar 18 2021 20:40:00 UTC
+    TICKET_CREATION_TIMESTAMP = 1616100000000
+    USER_ID = 'user_1'
+    REPORT_TYPE_SUGGESTION='suggestion'
+    CATEGORY_OTHER='other'
+    PLATFORM_VERSION='0.1-alpha-abcdef1234'
+    COUNTRY_LOCALE_CODE_INDIA='in'
+    ANDROID_DEVICE_MODEL='Pixel 4a'
+    ANDROID_SDK_VERSION=22
+    ENTRY_POINT_NAVIGATION_DRAWER='navigation_drawer'
+    TEXT_LANGUAGE_CODE_ENGLISH='en'
+    AUDIO_LANGUAGE_ENGLISH='english'
+    ANDROID_REPORT_INFO={
+        'user_feedback_other_text_input':'add an admin',
+        'event_logs':['event1', 'event2'],
+        'logcat_logs':['logcat1', 'logcat2'],
+        'package_version_code':1
+        'language_locale':,
+        'entry_point_info': {
+            'entry_point_name':'crash',
+        },
+        'text_size':'MEDIUM_TEXT_SIZE',
+        'download_and_update_only_on_wifi':True,
+        'automatically_update_topics':False,
+        'is_admin':False
+    }
+    ANDROID_REPORT_INFO_SCHEMA_VERSION=1
+
+
+    def setUp(self):
+        """Set up  models in datastore for use in testing."""
+        super(AppFeedbackReportModelTest, self).setUp()
+        self.feedback_report_model = feedback_models.AppFeedbackReportModel(
+            id='%s.%s.%s' % (
+                self.PLATFORM, self.REPORT_SUBMITTED_TIMESTAMP,
+                'randomInteger123'),
+            platform=self.PLATFORM,
+            scrubbed_by=self.USER_ID
+            ticket_id='%s.%s.%s' % (
+                'random_hash', self.TICKET_CREATION_TIMESTAMP,
+                '16CharString1234'),
+            submitted_on=self.REPORT_SUBMITTED_TIMESTAMP,
+            report_type=self.REPORT_TYPE_Suggestion,
+            category=self.CATEGORY_OTHER,
+            platform_version=self.PLATFORM_VERSION,
+            country_locale_code=self.COUNTRY_LOCALE_CODE_INDIA,
+            android_device_model=self.ANDROID_DEVICE_MODEL,
+            android_sdk_version=self.ANDROID_SDK_VERSION,
+            entry_point=self.ENTRY_POINT_NAVIGATION_DRAWER,
+            text_language_code=self.TEXT_LANGUAGE_CODE_ENGLISH,
+            audio_language=self.AUDIO_LANGUAGE_ENGLISH,
+            android_report_info=self.ANDROID_REPORT_INFO,
+            android_report_info_schema_version=
+                self.ANDROID_REPORT_INFO_SCHEMA_VERSION
+        )
+        self.feedback_report_model.update_timestamps()
+        self.feedback_report_model.put()
+
+    def test_get_deletion_policy(self):
+        self.assertEqual(
+            feedback_models.AppFeedbackReportModel.get_deletion_policy(),
+            base_models.DELETION_POLICY.LOCALLY_PSEUDONYMIZE)
+
+    def test_export_data_nontrivial(self):
+        stored_data = (
+            feedback_models
+            .AppFeedbackReportModel.export_data(self.USER_ID))
+        report_id = '%s.%s.%s' % (
+                self.PLATFORM, self.REPORT_SUBMITTED_TIMESTAMP,
+                'randomInteger123'),
+        test_data = {
+            report_id: {
+                'platform': self.PLATFORM,
+                'ticket_id': self.TICKET_ID,
+                'submitted_on': self.REPORT_SUBMITTED_TIMESTAMP,
+                'report_type': self.REPORT_TYPE_SUGGESTION,
+                'category': self.CATEGORY_OTHER,
+                'platform_version': self.PLATFORM_VERSION,
+                'country_locale_code': self.COUNTRY_LOCALE_CODE_INDIA,
+                'android_device_model': self.ANDROID_DEVICE_MODEL,
+                'android_sdk_versoin': self.ANDROID_SDK_VERSION,
+                'entry_point': self.ENTRY_POINT_NAVIGATION_DRAWER,
+                'text_language_code': self.TEXT_LANGUAGE_CODE_ENGLISH,
+                'audio_language': self.AUDIO_LANGUAGE_ENGLISH,
+                'android_report_info': self.ANDROID_REPORT_INFO,
+                'android_report_info_schema_version': 
+                    self.ANDROID_REPORT_INFO_SCHEMA_VERSION
+            }
+        }
+        self.assertEqual(stored_data, test_data)
