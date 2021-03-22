39a50eec21a9b02c88a1dc3c5d22671b71121e84
Aasif Faizal <aasif.faizal@gmail.com>
2021-03-22 19:16:25 +0530

    fixed linting issue

--- scripts/linters/html_linter.py
+++ scripts/linters/html_linter.py
@@ -112,7 +112,7 @@ class CustomHTMLParser(html.parser.HTMLParser):
 
         # Check whether there is space around attributes.
         # An = is followed either by " or {.
-        tag_substrings = [substring for substring in starttag_text.split(" ")
+        tag_substrings = [substring for substring in starttag_text.split(' ')
                           if substring]
         required_attribute_suffix = ['"', '{']
         conditional_statement_chars = ['=', '>', '<', '!']
