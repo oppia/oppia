3a763147a526566a3e9273f85dc4c125f88f0f8e
Hasitha Kaushan <hkraclh@gmail.com>
2021-03-25 18:40:27 +0530

    coverage improved

--- core/templates/pages/contributor-dashboard-page/modal-templates/translation-modal.controller.spec.ts
+++ core/templates/pages/contributor-dashboard-page/modal-templates/translation-modal.controller.spec.ts
@@ -17,7 +17,7 @@
  */
 import { importAllAngularServices } from 'tests/unit-test-utils';
 
-fdescribe('Translation Modal Controller', function() {
+describe('Translation Modal Controller', function() {
   let $httpBackend = null;
   let $q = null;
   let $scope = null;
@@ -246,7 +246,7 @@ fdescribe('Translation Modal Controller', function() {
       '<p>ABC</p>';
 
     $scope.suggestTranslatedText();
-    
+
     expect($scope.incompleteTranslationError).toBe(true);
     expect(
       SiteAnalyticsService.registerContributorDashboardSubmitSuggestionEvent)
