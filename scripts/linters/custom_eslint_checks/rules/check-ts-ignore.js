42b58c485d93cc555f469d3e0138bb4dbc1280c6
nlok5923 <nlokhande5923@gmail.com>
2021-03-25 09:31:19 +0530

    fix lint

--- scripts/linters/custom_eslint_checks/rules/check-ts-ignore.js
+++ scripts/linters/custom_eslint_checks/rules/check-ts-ignore.js
@@ -16,61 +16,54 @@
  * @fileoverview Lint check to ensure that comments follow correct style.
  */
 
- 'use strict';
+'use strict';
 
- const fs = require('fs');
- const path  = require('path')
- const filePath = path.resolve('scripts/linters/ts_ignore_exceptions.json');
- var filedata = JSON.parse(fs.readFileSync(filePath));
- var tsignoreFilepath = Object.keys(filedata);
-
- module.exports = {
-   meta: {
-     type: 'layout',
-     docs: {
-       description: (
-         'To check whether we added proper comments before using ts-ignore or not'),
-       category: 'Stylistic Issues',
-       recommended: true
-     },
-     fixable: null,
-     schema: [],
-     messages: {
-       expectedNature: 'Expected comment above ts-ignore'
-     }
-   },
-   create :function(context) {
-       const sourceCode = context.getSourceCode();
-       const tsIgnorePattern = "@ts-ignore";
-       var code = sourceCode.text.split('\n');
-       function raiseWarning(node) {
-        context.report({
-          node: node,
-          messageId:'expectedNature'
-        });
-       }
-       return {
-           Program: function(node){
-             let currentComment = '';
-             let previousComment = '';
-             let i = 0;
-             code.forEach((code)=>{
-               i++;
-               if(i === 1) {
-                 previousComment = code;
-                 currentComment = code;
-               } else {
-                 currentComment = code;
-               }
-               if(currentComment.includes(tsIgnorePattern)) {
-                   if(!previousComment.includes('//')) {
-                     raiseWarning(node);
-                   }
-               }
-               previousComment = currentComment; 
-             })
-           }
-        };
-   }
+module.exports = {
+  meta: {
+    type: 'layout',
+    docs: {
+      description: (
+         'To check proper comments before using ts-ignore'),
+      category: 'Stylistic Issues',
+      recommended: true
+    },
+    fixable: null,
+    schema: [],
+    messages: {
+      expectedNature: 'Expected comment above ts-ignore'
+    }
+  },
+  create: function(context) {
+    const sourceCode = context.getSourceCode();
+    const tsIgnorePattern = "@ts-ignore";
+    var code = sourceCode.text.split('\n');
+    function raiseWarning(node) {
+      context.report({
+        node: node,
+        messageId:'expectedNature'
+      });
+    }
+    return {
+      Program: function(node){
+        let currentComment = '';
+        let previousComment = '';
+        let i = 0;
+        code.forEach((code)=>{
+          i++;
+          if (i === 1) {
+            previousComment = code;
+            currentComment = code;
+          } else {
+            currentComment = code;
+          }
+          if (currentComment.includes(tsIgnorePattern)) {
+            if (!previousComment.includes('//')) {
+              raiseWarning(node);
+            }
+          }
+          previousComment = currentComment; 
+        })
+      }
+    };
+  }
 };
-
