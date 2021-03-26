7b2423915263b9dbfaba92d860892adc62db4df4
jimbyo <jimz@uci.edu>
2021-03-26 13:03:17 -0700

    Add translation tips

--- core/templates/pages/contributor-dashboard-page/modal-templates/translation-modal.controller.ts
+++ core/templates/pages/contributor-dashboard-page/modal-templates/translation-modal.controller.ts
@@ -1,13 +1,13 @@
 // Copyright 2020 The Oppia Authors. All Rights Reserved.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the 'License');
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS-IS" BASIS,
+// distributed under the License is distributed on an 'AS-IS' BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -53,6 +53,8 @@ angular.module('oppia').controller('TranslationModalController', [
     $scope.uploadingTranslation = false;
     $scope.activeWrittenTranslation = {};
     $scope.activeWrittenTranslation.html = '';
+    $scope.activeLanguageCode =
+      TranslationLanguageService.getActiveLanguageCode();
     $scope.HTML_SCHEMA = {
       type: 'html',
       ui_config: {
@@ -68,6 +70,70 @@ angular.module('oppia').controller('TranslationModalController', [
     $scope.moreAvailable = false;
     $scope.previousTranslationAvailable = false;
     $scope.textToTranslate = '';
+    $scope.TRANSLATION_TIPS = {
+      zh:
+      [
+        'Write fractions or numbers as they are, unless they are written out' +
+        'in words. For instance, one-fifth would be (五分之一)',
+        'When referring to Mr. Baker' +
+        '(or, in general, Mr./Ms. followed by an occupation), ' +
+        'leave it as Baker先生, since in certain cases Baker is the last name.',
+        'Make sure to use the correct punctuation:',
+        'Period = 。',
+        'Comma for compound sentences or translation phrases = ，',
+        'Comma for list of numbers or objects = 、',
+        'Preserve bolding. If the original content has bold text,' +
+        'make sure it is bold in Chinese as well.',
+        'Make sure that you have selected the correct words' +
+        '(e.g. words such as 再 and 在 ).'
+      ],
+      hi:
+      [
+        'Prefer simple Hindi words that are used in daily communication.' +
+        ' Note that common English words (pen, paper, cake, etc.) can be' +
+        ' written as transliterations (पेन, पेपर, केक). For harder words,' +
+        ' include the English word in parentheses, e.g. अंश (Numerator),' +
+        ' हर (Denominator), भिन्न (Fraction).',
+        'Use respectful pronouns (like “आप” instead of “तुम/तू ”) and a' +
+        ' corresponding respectful tone like “करिये, करेंगे”.',
+        'Use the same voice (active or passive)' +
+        ' as in the original English text.',
+        'Preserve punctuation and bolding. If the original content has' +
+        ' bold text, make sure it is bold in Hindi as well. If there' +
+        ' are bullet points, double quotes, etc., make sure that the' +
+        ' translated content also has bullet points and double quotes.',
+        'If the original card has “components” (such as pictures,' +
+        ' links, and equations), these need to be added to the' +
+        ' translated content. You can use the “Copy tool” for this' +
+        ' -- click on the Copy tool and then click on the component you want' +
+        ' to carry over. Also, double-click on the image and translate the ' +
+        ' alt text (and caption, if any).'
+      ],
+      bn:
+      [
+        'Use simple Bangla words that are used in daily communication.' +
+        ' Note that common English words (pencil, etc.) can be written as' +
+        ' transliterations (e.g পেন্সিল ).',
+        'Use proper punctuation.',
+        'Full stop = |',
+        'Use the same voice (active or passive)' +
+        ' as in the original English text.',
+        'Preserve punctuation and bolding. If the original' +
+        ' content has bold text, make sure it is bold in' +
+        ' Bangla as well. If there are bullet points, double quotes,' +
+        ' etc., make sure that the translated content also has bullet' +
+        ' points and double quotes.'
+      ],
+      ar:
+      [
+        'استعمال الألقاب الدالة على الاحترام مثل: سيدي، سيدتي.',
+        'استعمال نفس الصيغة المستعملة في ' +
+        'النص الإنجليزية من حيث البناء للمعلوم أو البناء للمجهول.',
+        ' استعمال علامات الترقيم بشكل صحيح ومناسب. واتباع المنهج المتبع في' +
+        ' النص الإنجليزي من حيث التظليل، الاقتباس،' +
+        ' علامات التنصيص، نقاط التعداد، وغيرها.',
+      ]
+    };
     $scope.languageDescription = (
       TranslationLanguageService.getActiveLanguageDescription());
     TranslateTextService.init(
