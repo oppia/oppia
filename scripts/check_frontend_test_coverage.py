c8ef03a1e18e77a836fdf89e99edda1a4ebae716
Radesh-kumar <imradesh@gmail.com>
2021-03-26 21:33:45 +0530

    lints

--- scripts/check_frontend_test_coverage.py
+++ scripts/check_frontend_test_coverage.py
@@ -361,6 +361,7 @@ NOT_FULLY_COVERED_FILENAMES = [
     'voiceover-recording.service.ts',
     'warnings-and-alerts.directive.ts',
     'worked-example-editor.directive.ts',
+    'suggestion-modal-for-learner-dashboard.service'
 ]
 
 
