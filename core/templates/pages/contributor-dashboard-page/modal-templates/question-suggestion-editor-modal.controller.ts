fe44b42f016b28be4e00dec00ae19d669b477ffd
Rijuta Singh <68547101+Rijuta-s@users.noreply.github.com>
2021-03-29 02:25:23 +0530

    Fix part of #11464: Fixes task 8 by allowing user to edit skill difficulty . (#12308)
    * adding question editor modal
    
    * adding question difficulty modal
    
    * fixing lint issues
    
    * adding test for  question-suggestion-editor-modal to open question difficulty modal
    
    * minor fix
    
    * fixing lint issues
    
    * fixing frontend tests
    
    * fixing lint issue
    
    * Update question-suggestion-editor-modal.directive.html
    
    Fixing e2e test

--- core/templates/pages/contributor-dashboard-page/modal-templates/question-suggestion-editor-modal.controller.ts
+++ core/templates/pages/contributor-dashboard-page/modal-templates/question-suggestion-editor-modal.controller.ts
@@ -80,7 +80,32 @@ angular.module('oppia').controller('QuestionSuggestionEditorModalController', [
       return QuestionValidationService.isQuestionValid(
         $scope.question, $scope.misconceptionsBySkill);
     };
-
+    $scope.skillId = $scope.skill.getId();
+    $scope.onClickChangeDifficulty = function() {
+      $uibModal.open({
+        templateUrl: UrlInterpolationService.getDirectiveTemplateUrl(
+          '/pages/topic-editor-page/modal-templates/' +
+          'select-skill-and-difficulty-modal.template.html'),
+        backdrop: true,
+        resolve: {
+          skillId: () => $scope.skillId
+        },
+        controller: (
+          'QuestionsOpportunitiesSelectSkillAndDifficultyModalController')
+      }).result.then(function(result) {
+        if (AlertsService.warnings.length === 0) {
+          $scope.skillDifficulty = result.skillDifficulty;
+          skillDifficulty = result.skillDifficulty;
+          $scope.skillDifficultyString = Object.entries(
+            SKILL_DIFFICULTY_LABEL_TO_FLOAT).find(
+            entry => entry[1] === skillDifficulty)[0];
+        }
+      }, function() {
+        // Note to developers:
+        // This callback is triggered when the Cancel button is clicked.
+        // No further action is needed.
+      });
+    };
     $scope.cancel = function() {
       if (QuestionUndoRedoService.hasChanges()) {
         $uibModal.open({
