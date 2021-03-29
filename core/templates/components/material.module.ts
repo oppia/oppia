a0e692aa2edeedd99220d472086b66a447103e9f
Pankaj Dahiya <pankaj.id.008@gmail.com>
2021-03-29 18:00:39 +0530

    solving errors

--- core/templates/components/material.module.ts
+++ core/templates/components/material.module.ts
@@ -21,6 +21,7 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatListModule } from '@angular/material/list';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -31,6 +32,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatButtonModule,
     MatCardModule,
     MatCheckboxModule,
+    MatListModule,
     MatProgressSpinnerModule
   ],
   exports: [
@@ -38,6 +40,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatButtonModule,
     MatCardModule,
     MatCheckboxModule,
+    MatListModule,
     MatProgressSpinnerModule
   ]
 })
