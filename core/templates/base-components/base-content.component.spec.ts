47fa662b7564e367576b3600331c1017a6a3c439
ashutoshc8101 <ashutoshc8101@gmail.com>
2021-04-01 20:45:03 +0530

    test for base content

--- core/templates/base-components/base-content.component.spec.ts
+++ core/templates/base-components/base-content.component.spec.ts
@@ -16,7 +16,15 @@
  * @fileoverview Tests for the Base Transclusion Component.
  */
 
+import { DOCUMENT } from '@angular/common';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { AlertMessageComponent } from 'components/common-layout-directives/common-elements/alert-message.component';
+import { PromoBarComponent } from 'components/common-layout-directives/common-elements/promo-bar.component';
+import { SideNavigationBarComponent } from 'components/common-layout-directives/navigation-bars/side-navigation-bar.component';
+import { TopNavigationBarComponent } from 'components/common-layout-directives/navigation-bars/top-navigation-bar.directive';
+import { LimiToPipe } from 'filters/limit-to.pipe';
+import { TranslatePipe } from 'filters/translate.pipe';
+import { OppiaFooterDirective } from 'pages/OppiaFooterDirective';
 import { BottomNavbarStatusService } from 'services/bottom-navbar-status.service';
 import { UrlService } from 'services/contextual/url.service';
 import { WindowRef } from 'services/contextual/window-ref.service';
@@ -26,17 +34,24 @@ import { PageTitleService } from 'services/page-title.service';
 import { SidebarStatusService } from 'services/sidebar-status.service';
 import { BackgroundMaskService } from 'services/stateful/background-mask.service';
 import { BaseContentComponent } from './base-content.component';
+import { LoadingMessageComponent } from './loading-message.component';
+import { WarningsAndAlertsComponent } from './warnings-and-alerts.component';
 
 fdescribe('Base Content Component', () => {
   let fixture: ComponentFixture<BaseContentComponent>;
   let componentInstance: BaseContentComponent;
   let isIframed: boolean = false;
+  let hostname: string = '';
   let href: string = 'test_href';
   let pathname: string = 'test_pathname';
   let search: string = 'test_search';
   let hash: string = 'test_hash';
-  let isMaskActive: boolean;
-  let isBottomNavbarEnabled: boolean;
+  let backgroundMaskService: BackgroundMaskService;
+  let bottomNavbarStatusService: BottomNavbarStatusService;
+  let windowRef: WindowRef;
+  let loaderService: LoaderService;
+  let keyboardShortcutService: KeyboardShortcutService;
+  let sidebarStatusService: SidebarStatusService;
 
   class MockUrlService {
     isIframed(): boolean {
@@ -47,6 +62,7 @@ fdescribe('Base Content Component', () => {
   class MockWindowRef {
     nativeWindow = {
       location: {
+        hostname: hostname,
         href: href,
         pathname: pathname,
         search: search,
@@ -55,22 +71,29 @@ fdescribe('Base Content Component', () => {
     };
   }
 
-  class MockBackgroundMaskService {
-    isMaskActive(): boolean {
-      return isMaskActive;
+  class MockPageTitleService {
+    getPageTitleForMobileView(): string {
+      return 'Page Title';
     }
-  }
 
-  class MockBottomNavbarStatusService {
-    isBottomNavbarEnabled(): boolean {
-      return isBottomNavbarEnabled;
+    getPageSubtitleForMobileView(): string {
+      return 'Page Subtitle';
     }
   }
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [
-        BaseContentComponent
+        BaseContentComponent,
+        OppiaFooterDirective,
+        LoadingMessageComponent,
+        WarningsAndAlertsComponent,
+        PromoBarComponent,
+        TopNavigationBarComponent,
+        SideNavigationBarComponent,
+        TranslatePipe,
+        AlertMessageComponent,
+        LimiToPipe
       ],
       providers: [
         {
@@ -81,17 +104,14 @@ fdescribe('Base Content Component', () => {
           provide: UrlService,
           useClass: MockUrlService
         },
-        {
-          provide: BackgroundMaskService,
-          useClass: MockBackgroundMaskService
-        },
-        {
-          provide: BottomNavbarStatusService,
-          useClass: MockBottomNavbarStatusService
-        },
+        BackgroundMaskService,
+        BottomNavbarStatusService,
         KeyboardShortcutService,
         LoaderService,
-        PageTitleService,
+        {
+          provide: PageTitleService,
+          useClass: MockPageTitleService
+        },
         SidebarStatusService
       ]
     }).compileComponents();
@@ -100,9 +120,93 @@ fdescribe('Base Content Component', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BaseContentComponent);
     componentInstance = fixture.componentInstance;
+    loaderService = TestBed.inject(LoaderService);
+    loaderService = (loaderService as unknown) as jasmine.SpyObj<LoaderService>;
+    keyboardShortcutService = TestBed.inject(KeyboardShortcutService);
+    keyboardShortcutService = (keyboardShortcutService as unknown) as
+    jasmine.SpyObj<KeyboardShortcutService>;
+    windowRef = TestBed.inject(WindowRef);
+    windowRef = (windowRef as unknown) as jasmine.SpyObj<WindowRef>;
+    sidebarStatusService = TestBed.inject(SidebarStatusService);
+    sidebarStatusService = (sidebarStatusService as unknown) as
+    jasmine.SpyObj<SidebarStatusService>;
+    bottomNavbarStatusService = TestBed.inject(BottomNavbarStatusService);
+    bottomNavbarStatusService = (bottomNavbarStatusService as unknown) as
+    jasmine.SpyObj<BottomNavbarStatusService>;
+    backgroundMaskService = TestBed.inject(BackgroundMaskService);
+    backgroundMaskService = (backgroundMaskService as unknown) as
+    jasmine.SpyObj<BackgroundMaskService>;
   });
 
   it('should create', () => {
     expect(componentInstance).toBeDefined();
+  });
+
+  it('should initiaize', () => {
+    spyOn(loaderService.onLoadingMessageChange, 'subscribe').and.
+      callFake((calb: (message: string) => void) => {
+        calb('Test Message');
+      });
+    spyOn(keyboardShortcutService, 'bindNavigationShortcuts');
+    windowRef.nativeWindow.location.hostname = 'oppiaserver.appspot.com';
+    componentInstance.ngOnInit();
+    expect(componentInstance.loadingMessage).toEqual('Test Message');
+    expect(keyboardShortcutService.bindNavigationShortcuts).toHaveBeenCalled();
+    expect(componentInstance.iframed).toEqual(isIframed);
+    expect(componentInstance.getHeaderText()).toEqual('Page Title');
+    expect(componentInstance.getSubheaderText()).toEqual('Page Subtitle');
+    expect(componentInstance.getSubheaderText).toBeDefined();
+    expect(windowRef.nativeWindow.location.href).toEqual(
+      'https://oppiatestserver.appspot.com' +
+      pathname + search + hash
+    );
+    expect(loaderService.onLoadingMessageChange.subscribe).toHaveBeenCalled();
+  });
+
+  it('should toggle sidebar', () => {
+    spyOn(sidebarStatusService, 'toggleSidebar');
+    componentInstance.toggleSidebar();
+    expect(sidebarStatusService.toggleSidebar).toHaveBeenCalled();
+  });
+
+  it('should get sidebar status', () => {
+    spyOn(sidebarStatusService, 'isSidebarShown').and.returnValue(false);
+    expect(componentInstance.isSidebarShown()).toBeFalse();
+    expect(sidebarStatusService.isSidebarShown).toHaveBeenCalled();
+  });
+
+  it('should get bottom navbar status', () => {
+    spyOn(bottomNavbarStatusService, 'isBottomNavbarEnabled')
+      .and.returnValue(false);
+    expect(componentInstance.isBottomNavbarShown()).toBeFalse();
+    expect(bottomNavbarStatusService.isBottomNavbarEnabled).toHaveBeenCalled();
+  });
+
+  it('should close sidebar on swipe', () => {
+    spyOn(sidebarStatusService, 'closeSidebar');
+    componentInstance.closeSidebarOnSwipe();
+    expect(sidebarStatusService.closeSidebar).toHaveBeenCalled();
+  });
+
+  it('should toggle mobile nav options', () => {
+    componentInstance.mobileNavOptionsAreShown = false;
+    componentInstance.toggleMobileNavOptions();
+    expect(componentInstance.mobileNavOptionsAreShown).toBeTrue();
+  });
+
+  it('should get background mask status', () => {
+    spyOn(backgroundMaskService, 'isMaskActive').and.returnValue(false);
+    expect(componentInstance.isBackgroundMaskActive()).toBeFalse();
+    expect(backgroundMaskService.isMaskActive).toHaveBeenCalled();
+  });
+
+  it('should skip to main content', () => {
+    let document = TestBed.inject(DOCUMENT);
+    let dummyElement = document.createElement('div');
+    spyOn(document, 'getElementById').and.returnValue(dummyElement);
+    componentInstance.skipToMainContent();
+    expect(dummyElement.tabIndex).toEqual(-1);
+    document.getElementById.and.returnValue(null);
+    expect(componentInstance.skipToMainContent).toThrowError();
   });
 });
