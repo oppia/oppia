ffaf922796a6bd8fd894e7c4f7ea6d55ca363864
Praneeth <praneethg2001@gmail.com>
2021-04-02 12:27:49 +0530

    fixed backend test and added testcase

--- scripts/linters/pylint_extensions_test.py
+++ scripts/linters/pylint_extensions_test.py
@@ -3276,7 +3276,7 @@ class NonTestFilesFunctionNameCheckerTests(unittest.TestCase):
 
 class MetaclassCheckerTests(unittest.TestCase):
 
-    def test_finds_backslash_continuation(self):
+    def test_finds_metaclass_usage_add_msg(self):
         checker_test_object = testutils.CheckerTestCase()
         checker_test_object.CHECKER_CLASS = (
             pylint_extensions.MetaclassChecker)
@@ -3301,4 +3301,25 @@ class MetaclassCheckerTests(unittest.TestCase):
                 node=metaclass_node
             ),
         ):
+            checker_test_object.checker.visit_classdef(metaclass_node)
+
+    def test_finds_metaclass_usage_no_msg(self):
+        checker_test_object = testutils.CheckerTestCase()
+        checker_test_object.CHECKER_CLASS = (
+            pylint_extensions.MetaclassChecker)
+        checker_test_object.setup_method()
+
+        metaclass_node = astroid.extract_node(
+            """
+            class FakeClass(python_utils.OBJECT):
+                def __init__(self, fake_arg):
+                    self.fake_arg = fake_arg
+                def fake_method(self, name):
+                    yield (name, name)
+            class MyObject: #@
+                def __init__(self, fake_arg):
+                    self.fake_arg = fake_arg
+            """)
+
+        with checker_test_object.assertNoMessages():
             checker_test_object.checker.visit_classdef(metaclass_node)
