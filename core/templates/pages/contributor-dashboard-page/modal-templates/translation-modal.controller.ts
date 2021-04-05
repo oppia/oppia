aaf9688252fd90d36c37eb1faff3332cae07c20a
shubhamkorde <sskorde@iitk.ac.in>
2021-04-05 09:15:05 +0530

    fixed linting issues 7

--- core/templates/pages/contributor-dashboard-page/modal-templates/translation-modal.controller.ts
+++ core/templates/pages/contributor-dashboard-page/modal-templates/translation-modal.controller.ts
@@ -37,7 +37,7 @@ angular.module('oppia').controller('TranslationModalController', [
   '$controller', '$rootScope', '$scope', '$uibModalInstance', 'AlertsService',
   'CkEditorCopyContentService', 'ContextService', 'ImageLocalStorageService',
   'SiteAnalyticsService', 'TranslateTextService',
-  'TranslatedTextBackendApiService', 'TranslationLanguageService', 
+  'TranslatedTextBackendApiService', 'TranslationLanguageService',
   'opportunity', 'ENTITY_TYPE', 'TRANSLATION_TIPS',
   function(
       $controller, $rootScope, $scope, $uibModalInstance, AlertsService,
