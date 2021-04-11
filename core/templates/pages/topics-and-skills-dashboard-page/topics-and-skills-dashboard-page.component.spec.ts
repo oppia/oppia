cf6130a3d6e316c04fb2b9eb303457854d1adde3
ashutoshc8101 <ashutoshc8101@gmail.com>
2021-04-12 02:30:31 +0530

    done

--- core/templates/pages/topics-and-skills-dashboard-page/topics-and-skills-dashboard-page.component.spec.ts
+++ core/templates/pages/topics-and-skills-dashboard-page/topics-and-skills-dashboard-page.component.spec.ts
@@ -45,7 +45,7 @@ describe('Topics and Skills Dashboard Page', function() {
 
   var mocktasdReinitalizedEventEmitter = null;
 
-  describe('when backend dict contains topics', function() {
+  fdescribe('when backend dict contains topics', function() {
     var sampleDataResults = {
       topic_summary_dicts: [{
         id: SAMPLE_TOPIC_ID,
@@ -73,6 +73,7 @@ describe('Topics and Skills Dashboard Page', function() {
       can_delete_skill: true
     };
     var SkillCreationService;
+    let CreateNewSkillModalService;
 
     beforeEach(angular.mock.inject(function($injector, $componentController) {
       $rootScope = $injector.get('$rootScope');
@@ -117,6 +118,7 @@ describe('Topics and Skills Dashboard Page', function() {
         isWindowNarrow: () => false
       };
       SkillCreationService = $injector.get('SkillCreationService');
+      CreateNewSkillModalService = $injector.get('CreateNewSkillModalService');
 
       ctrl = $componentController('topicsAndSkillsDashboardPage', {
         $scope: $scope,
@@ -197,24 +199,9 @@ describe('Topics and Skills Dashboard Page', function() {
     });
 
     it('should open Create Skill Modal', function() {
-      var modalSpy = spyOn($uibModal, 'open').and.callThrough();
+      let modalSpy = spyOn(CreateNewSkillModalService, 'createNewSkill');
       ctrl.createSkill();
       expect(modalSpy).toHaveBeenCalled();
-    });
-
-    it('should call the skill creation service', function() {
-      spyOn($uibModal, 'open').and.returnValue({
-        result: $q.resolve({
-          description: 'Description',
-          rubrics: 'Easy',
-          explanation: 'Explanation'
-        })
-      });
-
-      var skillSpy = spyOn(SkillCreationService, 'createNewSkill');
-      ctrl.createSkill();
-      $rootScope.$apply();
-      expect(skillSpy).toHaveBeenCalled();
     });
 
     it('should navigate the page', function() {
