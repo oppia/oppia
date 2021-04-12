9aa6ae97f2b5c382848407557df0f40401d1b814
PANKAJ <52493334+pankajdahiyapd@users.noreply.github.com>
2021-04-12 21:45:03 +0530

    Fix part of #9749 Migrating outcome-feedback-editor.directive.ts to Angular (#12479)
    * initial commit
    
    * solving errors
    
    * solving errors
    
    * solving errors
    
    * solving errors
    
    * solving errors
    
    * solving errors
    
    * solving errors
    
    * solving errors
    
    * solving errors
    
    * solving errors
    
    * solving errors
    
    * solving errors
    
    * solving errors
    
    * solving errors
    
    * solving errors
    
    * solving errors
    
    * solving errors
    
    * solving errors
    
    * solving errors
    
    * solving errors
    
    * solving errors
    
    * solving errors
    
    * solving errors
    
    * solving errors
    
    * solving errors
    
    * solving errors
    
    * solving errors
    
    * Update core/templates/components/state-directives/outcome-editor/outcome-feedback-editor.component.ts
    
    Co-authored-by: Nithesh N. Hariharan <nithusha21@yahoo.co.in>
    
    * solving errors
    
    * solving errors
    
    * Update outcome-feedback-editor.component.html
    
    * solving errors
    
    * solving errors
    
    * solving errors
    
    * Update outcome-feedback-editor.component.ts
    
    Co-authored-by: Nithesh N. Hariharan <nithusha21@yahoo.co.in>

--- core/templates/pages/exploration-editor-page/editor-tab/training-panel/training-panel.component.ts
+++ core/templates/pages/exploration-editor-page/editor-tab/training-panel/training-panel.component.ts
@@ -17,7 +17,7 @@
  */
 require(
   'components/state-directives/outcome-editor/' +
-  'outcome-feedback-editor.directive.ts');
+  'outcome-feedback-editor.component.ts');
 require('directives/angular-html-bind.directive.ts');
 require(
   'components/state-editor/state-editor-properties-services/' +
