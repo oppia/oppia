64c4034559bf6f5b01c644ea04a7ea0538167773
Vojtěch Jelínek <vojtin.j@gmail.com>
2021-04-15 01:59:36 +0200

    Fix python utils

--- python_utils.py
+++ python_utils.py
@@ -302,19 +302,17 @@ def url_retrieve(source_url, filename=None):
     Returns:
         urlretrieve. The 'urlretrieve' object.
     """
-    context = ssl.create_default_context(cafile=certifi.where())
     try:
         import urllib.request as urlrequest
     except ImportError:
         import urllib as urlrequest
         # Change the User-Agent to prevent servers from blocking requests.
         # See https://support.cloudflare.com/hc/en-us/articles/360029779472-Troubleshooting-Cloudflare-1XXX-errors#error1010. # pylint: disable=line-too-long
-        urlrequest.URLopener.version = (
-            'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) '
-            'Gecko/20100101 Firefox/47.0'
-        )
-    return urlrequest.urlretrieve(
-        source_url, filename=filename, context=context)
+    urlrequest.URLopener.version = (
+        'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) '
+        'Gecko/20100101 Firefox/47.0'
+    )
+    return urlrequest.urlretrieve(source_url, filename=filename)
 
 
 def url_open(source_url):
