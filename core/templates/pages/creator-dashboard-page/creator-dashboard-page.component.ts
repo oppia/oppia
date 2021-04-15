8b5fc305796ae5ae930e4edf910eece40d32922f
Radesh-kumar <imradesh@gmail.com>
2021-04-15 12:30:35 +0530

    fixed e2e

--- core/templates/pages/creator-dashboard-page/creator-dashboard-page.component.ts
+++ core/templates/pages/creator-dashboard-page/creator-dashboard-page.component.ts
@@ -33,6 +33,7 @@ require('components/ratings/rating-computation/rating-computation.service.ts');
 require('domain/creator_dashboard/creator-dashboard-backend-api.service.ts');
 require('domain/suggestion/SuggestionThreadObjectFactory.ts');
 require('domain/utilities/url-interpolation.service.ts');
+require('filters/string-utility-filters/truncate.filter.ts');
 require(
   'pages/creator-dashboard-page/suggestion-modal-for-creator-view/' +
   'suggestion-modal-for-creator-view.service.ts');
