2f5af25c3cd0c30f9c2f9ee23ef1f4abf72c14c8
Hasitha Kaushan <hkraclh@gmail.com>
2021-04-16 22:22:13 +0530

    lint errors fixed

--- core/templates/pages/contributor-dashboard-page/modal-templates/translation-modal.component.ts
+++ core/templates/pages/contributor-dashboard-page/modal-templates/translation-modal.component.ts
@@ -16,43 +16,43 @@
  * @fileoverview Component for the translation modal.
  */
 
- import { ChangeDetectorRef, Component, Input } from '@angular/core';
- import { downgradeComponent } from '@angular/upgrade/static';
- import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
- 
- import { AlertsService } from 'services/alerts.service';
- import { CkEditorCopyContentService } from 'components/ck-editor-helpers/ck-editor-copy-content-service';
- import { ContextService } from 'services/context.service';
- import { ImageLocalStorageService } from 'services/image-local-storage.service';
- import { SiteAnalyticsService } from 'services/site-analytics.service';
- import { TranslateTextService } from 'pages/contributor-dashboard-page/services/translate-text.service';
- import { TranslationLanguageService } from 'pages/exploration-editor-page/translation-tab/services/translation-language.service';
- import { AppConstants } from 'app.constants';
- import constants from 'assets/constants';
- 
- class UiConfig {
+import { ChangeDetectorRef, Component, Input } from '@angular/core';
+import { downgradeComponent } from '@angular/upgrade/static';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+import { AlertsService } from 'services/alerts.service';
+import { CkEditorCopyContentService } from 'components/ck-editor-helpers/ck-editor-copy-content-service';
+import { ContextService } from 'services/context.service';
+import { ImageLocalStorageService } from 'services/image-local-storage.service';
+import { SiteAnalyticsService } from 'services/site-analytics.service';
+import { TranslateTextService } from 'pages/contributor-dashboard-page/services/translate-text.service';
+import { TranslationLanguageService } from 'pages/exploration-editor-page/translation-tab/services/translation-language.service';
+import { AppConstants } from 'app.constants';
+import constants from 'assets/constants';
+
+class UiConfig {
    'hide_complex_extensions': boolean;
    'startupFocusEnabled'?: boolean;
    'language'?: string;
    'languageDirection'?: string;
- }
- export interface TranslationOpportunity {
+}
+export interface TranslationOpportunity {
    id: string;
    heading: string;
    subheading: string;
    progressPercentage: string;
    actionButtonTitle: string;
  }
- export interface HTMLSchema {
+export interface HTMLSchema {
      'type': string;
      'ui_config': UiConfig;
  }
- 
+
  @Component({
    selector: 'oppia-translation-modal',
    templateUrl: './translation-modal.component.html'
  })
- export class TranslationModalComponent {
+export class TranslationModalComponent {
    @Input() opportunity: TranslationOpportunity;
    activeWrittenTranslation: {html: string} = {html: ''};
    uploadingTranslation = false;
@@ -69,7 +69,7 @@
    };
    TRANSLATION_TIPS = constants.TRANSLATION_TIPS;
    activeLanguageCode: string;
- 
+
    constructor(
      private readonly activeModal: NgbActiveModal,
      private readonly alertsService: AlertsService,
@@ -81,7 +81,7 @@
      private readonly translationLanguageService: TranslationLanguageService,
      private readonly changeDetectorRef: ChangeDetectorRef
    ) {}
- 
+
    ngOnInit(): void {
      this.activeLanguageCode =
      this.translationLanguageService.getActiveLanguageCode();
@@ -114,33 +114,33 @@
        }
      };
    }
- 
+
    close(): void {
      this.activeModal.close();
    }
- 
+
    getHtmlSchema(): HTMLSchema {
      return this.HTML_SCHEMA;
    }
- 
+
    onContentClick(event: MouseEvent): void {
      if (this.isCopyModeActive()) {
        event.stopPropagation();
      }
      this.ckEditorCopyContentService.broadcastCopy(event.target as HTMLElement);
    }
- 
+
    isCopyModeActive(): boolean {
      return this.ckEditorCopyContentService.copyModeActive;
    }
- 
+
    updateHtml($event: string): void {
      if ($event !== this.activeWrittenTranslation.html) {
        this.activeWrittenTranslation.html = $event;
        this.changeDetectorRef.detectChanges();
      }
    }
- 
+
    skipActiveTranslation(): void {
      const textAndAvailability = (
        this.translateTextService.getTextToTranslate());
@@ -148,14 +148,14 @@
      this.moreAvailable = textAndAvailability.more;
      this.activeWrittenTranslation.html = '';
    }
- 
+
    returnToPreviousTranslation(): void {
      const textAndAvailability = (
        this.translateTextService.getPreviousTextToTranslate());
      this.textToTranslate = textAndAvailability.text;
      this.previousTranslationAvailable = textAndAvailability.more;
    }
- 
+
    suggestTranslatedText(): void {
      if (!this.uploadingTranslation && !this.loadingData) {
        this.siteAnalyticsService
@@ -184,8 +184,8 @@
        this.close();
      }
    }
- }
- 
- angular.module('oppia').directive(
-   'oppiaTranslationModal', downgradeComponent(
-     {component: TranslationModalComponent}));
+}
+
+angular.module('oppia').directive(
+  'oppiaTranslationModal', downgradeComponent(
+    {component: TranslationModalComponent}));
