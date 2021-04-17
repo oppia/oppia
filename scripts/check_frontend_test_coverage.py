bbbb2e35ac099432f24a3ef46efbadb37100f02e
ashutoshc8101 <ashutoshc8101@gmail.com>
2021-04-17 21:54:42 +0530

    done

--- scripts/check_frontend_test_coverage.py
+++ scripts/check_frontend_test_coverage.py
@@ -285,7 +285,6 @@ NOT_FULLY_COVERED_FILENAMES = [
     'skill-selector-editor.directive.ts',
     'skill-selector.component.ts',
     'SkillObjectFactory.ts',
-    'skills-list.component.ts',
     'skills-mastery-list.directive.ts',
     'social-buttons.component.ts',
     'solution-editor.directive.ts',
