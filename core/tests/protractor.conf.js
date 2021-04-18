132aa6e3f035aa75f187e9fd00a0825360c83b6e
Krishna Rama Rao <krishnarao.2208@gmail.com>
2021-04-19 00:24:17 +0400

    widening video

--- core/tests/protractor.conf.js
+++ core/tests/protractor.conf.js
@@ -346,7 +346,7 @@ exports.config = {
       '-y',
       '-r', '30',
       '-f', 'x11grab',
-      '-s', '1024x768',
+      '-s', '1366x768',
       '-i', process.env.DISPLAY,
       '-g', '300',
       '-vcodec', 'qtrle',
