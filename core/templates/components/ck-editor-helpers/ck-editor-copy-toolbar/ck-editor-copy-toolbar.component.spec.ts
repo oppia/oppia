63491eaadc8070f7f8325d67c247523b437b9991
Arkadyuti Bandyopadhyay <bandyopadhyayarkadyuti@gmail.com>
2021-04-18 14:10:37 +0530

    Fixing more spacing issues

--- core/templates/components/ck-editor-helpers/ck-editor-copy-toolbar/ck-editor-copy-toolbar.component.spec.ts
+++ core/templates/components/ck-editor-helpers/ck-editor-copy-toolbar/ck-editor-copy-toolbar.component.spec.ts
@@ -84,7 +84,7 @@ describe('CkEditor copy toolbar', () => {
   it('should revert cursor to normal when modal is closed', () => {
     component.toggleToolActive();
     fixture.detectChanges();
- 
+
     expect(document.body.style.cursor).toBe('copy');
 
     component.ngOnDestroy();
