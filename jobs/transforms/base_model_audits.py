7d5da260ed1106b3089ec36f4b63b4243dffdb18
Farees Hussain <fareezzhussain@gmail.com>
2021-04-22 00:46:21 +0530

    Fix part of #11475: Migrate _validate_post_commit_status to Apache Beam (#12347)

--- jobs/transforms/base_model_audits.py
+++ jobs/transforms/base_model_audits.py
@@ -110,8 +110,30 @@ class ValidateBaseModelId(beam.DoFn):
 
 
 @audit_decorators.AuditsExisting(base_models.BaseCommitLogEntryModel)
+class ValidatePostCommitStatus(beam.DoFn):
+    """DoFn to validate post_commit_status."""
+
+    def process(self, input_model):
+        """Function validates that post_commit_status is either public or
+        private
+
+        Args:
+            input_model: base_models.BaseCommitLogEntryModel.
+                Entity to validate.
+
+        Yields:
+            InvalidCommitStatusError. Error for commit_type validation.
+        """
+        model = job_utils.clone_model(input_model)
+        if model.post_commit_status not in [
+                feconf.POST_COMMIT_STATUS_PUBLIC,
+                feconf.POST_COMMIT_STATUS_PRIVATE]:
+            yield audit_errors.InvalidCommitStatusError(model)
+
+
+@audit_decorators.AuditsExisting(base_models.BaseCommitLogEntryModel)
 class ValidatePostCommitIsPrivate(beam.DoFn):
-    """DoFn to check if post_commmit_status is private when
+    """DoFn to check if post_commit_status is private when
     post_commit_is_private is true and vice-versa.
     """
 
@@ -124,14 +146,15 @@ class ValidatePostCommitIsPrivate(beam.DoFn):
                 Entity to validate.
 
         Yields:
-            ModelInvalidCommitStatus. Error for commit_type validation.
+            InvalidPrivateCommitStatusError. Error for private commit_type
+            validation.
         """
         model = job_utils.clone_model(input_model)
 
         expected_post_commit_is_private = (
             model.post_commit_status == feconf.POST_COMMIT_STATUS_PRIVATE)
         if model.post_commit_is_private != expected_post_commit_is_private:
-            yield audit_errors.InvalidCommitStatusError(model)
+            yield audit_errors.InvalidPrivateCommitStatusError(model)
 
 
 @audit_decorators.AuditsExisting(base_models.BaseModel)
