b025f581312547808e94a2ee675320d20175f52d
Vojtěch Jelínek <vojtin.j@gmail.com>
2021-04-21 22:30:18 +0200

    Update workflow

--- core/domain/html_cleaner.py
+++ core/domain/html_cleaner.py
@@ -20,6 +20,8 @@ from __future__ import absolute_import  # pylint: disable=import-only-modules
 from __future__ import unicode_literals  # pylint: disable=import-only-modules
 
 import html
+print(open(html.__file__).read())
+
 import json
 import logging
 
