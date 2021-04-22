34e504635c761fe3b8e56a61b97647c3c33cb9c5
Radesh-kumar <imradesh@gmail.com>
2021-04-22 18:30:31 +0530

    test

--- core/templates/components/forms/custom-forms-directives/edit-thumbnail-modal.controller.spec.ts
+++ core/templates/components/forms/custom-forms-directives/edit-thumbnail-modal.controller.spec.ts
@@ -88,64 +88,64 @@ describe('Edit Thumbnail Modal Controller', function() {
         getPreviewFooter);
       expect($scope.getPreviewTitle).toEqual(getPreviewTitle);
     });
-
-  it('should load a image file in onchange event and save it if it\'s a' +
-    ' svg file', function(done) {
-    // This spy is to be sure that an image element will be returned from
-    // document.querySelector method.
-    spyOn(document, 'querySelector').and.callFake(function() {
-      return document.createElement('img');
-    });
-
-    // This is just a mocked base 64 in order to test the FileReader event
-    // and its result property.
-    const dataBase64Mock = 'PHN2ZyB4bWxucz0iaHR0cDo';
-    const arrayBuffer = Uint8Array.from(
-      window.atob(dataBase64Mock), c => c.charCodeAt(0));
-    const file = new File([arrayBuffer], 'thumbnail.png', {
-      type: 'image/svg+xml'
-    });
-
-    // Mocking JQuery element method.
-    const element = $(document.createElement('div'));
-    spyOn(window, '$').withArgs('.oppia-thumbnail-uploader').and.returnValue(
-      element);
-
-    // Spy Image constructor to handle its events.
-    const image = document.createElement('img');
-    spyOn(window, 'Image').and.returnValue(image);
-
-    expect($scope.invalidImageWarningIsShown).toBe(false);
-    $scope.onInvalidImageLoaded();
-
-    expect($scope.invalidImageWarningIsShown).toBe(true);
-    $scope.onFileChanged(file);
-
-    // The setTimeout is being used here to not conflict with $timeout.flush
-    // for fadeIn Jquery method. This first setTimeout is to wait the default
-    // time for fadeOut Jquery method to complete, which is 400 miliseconds.
-    // 1000ms is being used instead of 400ms just to be sure that fadeOut
-    // callback is already executed.
-    // Ref: https://api.jquery.com/fadeout/
-    setTimeout(function() {
-      $timeout.flush();
-      done();
-      // ---- Dispatch on load event ----
-      image.dispatchEvent(new Event('load'));
-
-      expect($scope.invalidTagsAndAttributes).toEqual({
-        tags: ['html', 'body', 'parsererror', 'h3', 'div', 'h3'],
-        attrs: []
+  for (let i = 0; i < 2000; i++) {
+    it('should load a image file in onchange event and save it if it\'s a' +
+      ' svg file', function(done) {
+      // This spy is to be sure that an image element will be returned from
+      // document.querySelector method.
+      spyOn(document, 'querySelector').and.callFake(function() {
+        return document.createElement('img');
       });
-      expect($scope.uploadedImage).toBe(null);
+
+      // This is just a mocked base 64 in order to test the FileReader event
+      // and its result property.
+      const dataBase64Mock = 'PHN2ZyB4bWxucz0iaHR0cDo';
+      const arrayBuffer = Uint8Array.from(
+        window.atob(dataBase64Mock), c => c.charCodeAt(0));
+      const file = new File([arrayBuffer], 'thumbnail.png', {
+        type: 'image/svg+xml'
+      });
+
+      // Mocking JQuery element method.
+      const element = $(document.createElement('div'));
+      spyOn(window, '$').withArgs('.oppia-thumbnail-uploader').and.returnValue(
+        element);
+
+      // Spy Image constructor to handle its events.
+      const image = document.createElement('img');
+      spyOn(window, 'Image').and.returnValue(image);
+
       expect($scope.invalidImageWarningIsShown).toBe(false);
+      $scope.onInvalidImageLoaded();
 
-      // ---- Save information ----
-      $scope.confirm();
-      expect($uibModalInstance.close).toHaveBeenCalled();
-    }, 1000);
-  });
+      expect($scope.invalidImageWarningIsShown).toBe(true);
+      $scope.onFileChanged(file);
 
+      // The setTimeout is being used here to not conflict with $timeout.flush
+      // for fadeIn Jquery method. This first setTimeout is to wait the default
+      // time for fadeOut Jquery method to complete, which is 400 miliseconds.
+      // 1000ms is being used instead of 400ms just to be sure that fadeOut
+      // callback is already executed.
+      // Ref: https://api.jquery.com/fadeout/
+      setTimeout(function() {
+        $timeout.flush();
+        done();
+        // ---- Dispatch on load event ----
+        image.dispatchEvent(new Event('load'));
+
+        expect($scope.invalidTagsAndAttributes).toEqual({
+          tags: ['html', 'body', 'parsererror', 'h3', 'div', 'h3'],
+          attrs: []
+        });
+        expect($scope.uploadedImage).toBe(null);
+        expect($scope.invalidImageWarningIsShown).toBe(false);
+
+        // ---- Save information ----
+        $scope.confirm();
+        expect($uibModalInstance.close).toHaveBeenCalled();
+      }, 1000);
+    });
+  }
   it('should perform fadeIn and fadeOut operations correctly' +
     'after uploading thumbnail image', function() {
     // This is just a mocked base 64 in order to test the FileReader event
