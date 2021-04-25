9bdf5080c09d4ee13f50a190b01eed269a9c3824
tejpals <tejpal021@gmail.com>
2021-04-25 19:58:02 -0400

    issue 11248

--- core/templates/components/forms/custom-forms-directives/image-uploader.component.ts
+++ core/templates/components/forms/custom-forms-directives/image-uploader.component.ts
@@ -18,7 +18,7 @@
 
 import { Component, ElementRef, Input, Output, EventEmitter, ViewChild } from '@angular/core';
 import { downgradeComponent } from '@angular/upgrade/static';
-import { PassThrough } from 'node:stream';
+//import { PassThrough } from 'node:stream';
 import { WindowRef } from 'services/contextual/window-ref.service';
 import { IdGenerationService } from 'services/id-generation.service';
 
@@ -97,8 +97,7 @@ export class ImageUploaderComponent {
       /(\\|\/)/g).pop();
     this.errorMessage = this.validateUploadedFile(file, filename);
     let fchng = this.fileChanged;
-    let newFile: File = new File(["asd".repeat(34134)], 'so.txt', 
-                        { type: 'text/plain' });
+    let newFile: File = new File(["asd"], 'so.txt', { type: 'text/plain' });
     if (!this.errorMessage) {
       // Only fir this event if validation pass.
       const HUNDRED_KB_IN_BYTES: number = 100 * 1024;
@@ -136,8 +135,7 @@ export class ImageUploaderComponent {
             });
           };
         };
-      }
-      else {
+      } else {
         this.fileChanged.emit(file);
       }
     }
