94a3ad4996fca7c5b62f6d77c52e71988780fd67
Srijan Reddy <srijan@srijanreddy.com>
2021-04-25 12:56:13 +0530

    Fix window interface

---
+++ typings/custom-window-defs.d.ts
@@ -0,0 +1,14 @@
+// Any property defined on window needs to be added here if is not
+// present on the type of window.
+
+interface Window {
+    HTMLElement: HTMLElement;
+    __fixtures__: KarmaFixtures;
+    decodeURIComponent: (encodedURIComponent: string) => string;
+    encodeURIComponent: (decodedURIComponent: string) => string;
+    gtag: Function;
+    $: JQueryStatic;
+    Base64Binary: Base64Binary;
+    jQuery: JQueryStatic;
+    dataLayer: object[];
+}
