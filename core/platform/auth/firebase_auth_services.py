a017567b2f2b9a33f8a2b290c6aa3915e0dcedcb
Brian Rodriguez <brian@brianrodri.com>
2021-04-27 00:20:20 -0400

    address comments

--- core/platform/auth/firebase_auth_services.py
+++ core/platform/auth/firebase_auth_services.py
@@ -123,7 +123,7 @@ def establish_auth_session(request, response):
         overwrite=True,
         # Only allow this cookie to persist on Oppia's requests and responses,
         # promoting it to a "first-party cookie" (as opposed to a "third-party
-        # cookie", which are untrusted by some browsers).
+        # cookie", which is untrusted by some browsers).
         samesite='strict',
         # Toggles https vs http. The production server uses https, but the local
         # developement server uses http.
