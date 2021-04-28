a2534d9c982a541b2f4d048d1f02eb378c78f17e
Shubham Singh <55784408+shubhamcodez@users.noreply.github.com>
2021-04-28 12:25:25 +0530

    Update priv.js

--- scripts/linters/custom_eslint_checks/rules/priv.js
+++ scripts/linters/custom_eslint_checks/rules/priv.js
@@ -1,4 +1,5 @@
 // Copyright 2020 The Oppia Authors. All Rights Reserved.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -20,52 +21,60 @@
 
 'use strict';
 
+const console = require("node:console");
+const { isVariableDeclaration } = require("typescript");
+
 // TODO(#10479): Implement this rule using the nodes instead of tokens.
+
 module.exports = {
-  meta: {
-    type: 'layout',
-    docs: {
-      description: (
-        'Add lint check to ensure that all private functions/' +
-        'variables names start with _, and that all functions/variables' +
-        'that start with _ are tagged as private'),
-      category: 'Issue',
-      recommended: true
+    meta: {
+        type: "suggestion",
+        docs: {
+          description: (
+            'Add lint check to ensure that all private functions/' +
+            'variables names start with _, and that all functions/variables' +
+            'that start with _ are tagged as private'),
+            category: "Possible Errors",
+            recommended: true,
+            url: "https://eslint.org/docs/rules/no-extra-semi"
+        },
+        fixable: "code",
+        schema: [], 
+        messages: {
+          expectedPriv: 'Functions/Variables starting with _ should be private',
+          expected_ : 'Private functions/variables should start with _'
+        }
     },
-    fixable: null,
-    schema: [],
-    messages: {
-      expectedAfter: 'all private functions /' +
-      'variables names should start with _'
-    }
-  },
-  create: function(context) {
-    const sourceCode = context.getSourceCode();
-    const lines = sourceCode.lines;
-
-    return {
-      Program: function(node) {
-        const tokens = sourceCode.tokensAndComments;
-        var priv = [];
-        // Iterates over all tokens and store all variables/
-        // functions starting with _ in a list.
-        tokens.forEach((token, i) => {
-          if (token.value === '_') {
-            priv.push(token);
-          }
-        });
-
-        priv.forEach((priv, i) => {
-          const line = lines[priv.loc.start.line - 1].trim();
-          if ('_' === line[0]) {
+    create: function(context) {
+        return {
+          // get variables from the object
+        Identifier(node)  // I don't know what this does,  
+        //I referenced Eslint Rule writing Docs for most of it.
+        // for your reference: https://eslint.org/docs/developer-guide/working-with-rules
+        /*
+        Content as Pseudocode 
+        { list[] = getDeclaredVariables(node)  
+         // I assumed that the above function returns me a list of variables 
+          if(list contains private variable && string doesn't contain _ after first whitespace (bool return is false))
+          {  
             context.report({
-              node,
-              loc: paren.loc,
-              messageId: 'expectedAfter'
+               node: node,
+              message: expectedPriv
+              suggest : [ fix: function (fixer) {
+                return insertTextAfterRange(range = length of scope type length, _)}]
             });
           }
-        });
-      }
-    };
-  }
+        }*/
+      };
+    },
 };
+
+function whitespaceafterscope(Variable) {
+  for(let i = 0; i < Variable.length(); i++)
+  {
+    if(Variable[i] == ' ' && Variable[i+1] == '_')
+    {return true;}
+    else
+    {return false;}
+  }
+}
