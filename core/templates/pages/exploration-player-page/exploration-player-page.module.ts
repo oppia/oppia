fe9bd77de0c08f5446e5318677b9ec0f8de795fe
ashutoshc8101 <ashutoshc8101@gmail.com>
2021-04-30 18:37:24 +0530

    nit

--- core/templates/pages/exploration-player-page/exploration-player-page.module.ts
+++ core/templates/pages/exploration-player-page/exploration-player-page.module.ts
@@ -35,6 +35,7 @@ import { SwitchContentLanguageRefreshRequiredModalComponent } from
   // eslint-disable-next-line max-len
   'pages/exploration-player-page/switch-content-language-refresh-required-modal.component';
 import { InteractionExtensionsModule } from 'interactions/interactions.module';
+import { CorrectnessFooterComponent } from './layout-directives/correctness-footer.component';
 
 @NgModule({
   imports: [
@@ -77,7 +78,6 @@ class ExplorationPlayerPageModule {
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { downgradeModule } from '@angular/upgrade/static';
-import { CorrectnessFooterComponent } from './layout-directives/correctness-footer.component';
 
 const bootstrapFn = (extraProviders: StaticProvider[]) => {
   const platformRef = platformBrowserDynamic(extraProviders);
