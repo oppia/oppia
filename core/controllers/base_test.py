849f2eac579c74996f1d49ad9791232e8d5db627
Brian Rodriguez <brian@brianrodri.com>
2021-04-30 20:22:47 -0400

    move TODOs into test implementation

--- core/controllers/base_test.py
+++ core/controllers/base_test.py
@@ -702,10 +702,10 @@ class MaintenanceModeTests(test_utils.GenericTestBase):
     def test_signup_succeeds_when_user_is_super_admin(self):
         self.signup(self.ADMIN_EMAIL, self.ADMIN_USERNAME, is_super_admin=True)
 
-    # TODO(#12692): Use stateful login sessions to assert the behavior of
-    # logging out, rather than asserting that destroy_auth_session() gets
-    # called.
     def test_admin_auth_session_is_preserved_when_in_maintenance_mode(self):
+        # TODO(#12692): Use stateful login sessions to assert the behavior of
+        # logging out, rather than asserting that destroy_auth_session() gets
+        # called.
         destroy_auth_session_call_counter = self.context_stack.enter_context(
             self.swap_with_call_counter(auth_services, 'destroy_auth_session'))
         self.context_stack.enter_context(self.super_admin_context())
@@ -719,10 +719,10 @@ class MaintenanceModeTests(test_utils.GenericTestBase):
 
         self.assertEqual(destroy_auth_session_call_counter.times_called, 0)
 
-    # TODO(#12692): Use stateful login sessions to assert the behavior of
-    # logging out, rather than asserting that destroy_auth_session() gets
-    # called.
     def test_non_admin_auth_session_is_destroyed_when_in_maintenance_mode(self):
+        # TODO(#12692): Use stateful login sessions to assert the behavior of
+        # logging out, rather than asserting that destroy_auth_session() gets
+        # called.
         destroy_auth_session_call_counter = self.context_stack.enter_context(
             self.swap_with_call_counter(auth_services, 'destroy_auth_session'))
 
