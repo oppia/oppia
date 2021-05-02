0bdd1e1e1d2616c76ffe2c9a4831e22a7c7108de
Vojtěch Jelínek <vojtin.j@gmail.com>
2021-05-02 23:15:25 +0200

    Fix backend tests

--- core/domain/exp_fetchers.py
+++ core/domain/exp_fetchers.py
@@ -302,7 +302,7 @@ def get_exploration_by_id(exploration_id, strict=True, version=None):
     Returns:
         Exploration. The domain object corresponding to the given exploration.
     """
-    sub_namespace = python_utils.convert_to_bytes(version) if version else None
+    sub_namespace = str(version) if version else None
     cached_exploration = caching_services.get_multi(
         caching_services.CACHE_NAMESPACE_EXPLORATION,
         sub_namespace,
