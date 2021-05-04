932d5ce33ade6fbf9b0afd81d501739b0230eb0c
Ashutosh Chauhan <ashutoshc8101@gmail.com>
2021-05-05 01:40:37 +0530

    Fix part of #9749: Migrate moderator page (#12602)
    * continue
    
    * done
    
    * asd
    
    * nit
    
    * Change detection issue fixed
    
    Co-authored-by: Radesh-kumar <imradesh@gmail.com>
    
    * changes
    
    * nits
    
    * commented
    
    * nits
    
    Co-authored-by: Radesh-kumar <imradesh@gmail.com>

--- core/templates/services/angular-services.index.ts
+++ core/templates/services/angular-services.index.ts
@@ -343,6 +343,7 @@ import { ExplorationCreationService } from 'components/entity-creation-services/
 import { ImageLocalStorageService } from './image-local-storage.service';
 import { TranslateTextService } from 'pages/contributor-dashboard-page/services/translate-text.service';
 import { TranslateTextBackendApiService } from 'pages/contributor-dashboard-page/services/translate-text-backend-api.service';
+import { ModeratorPageBackendApiService } from 'pages/moderator-page/services/moderator-page-backend-api.service';
 import { NotificationsDashboardPageBackendApiService } from 'pages/notifications-dashboard-page/notifications-dashboard-page-backend-api.service';
 import { ExplorationEngineService } from 'pages/exploration-player-page/services/exploration-engine.service';
 
@@ -525,6 +526,7 @@ export const angularServices: [string, Type<{}>][] = [
   ['MetaTagCustomizationService', MetaTagCustomizationService],
   ['MisconceptionObjectFactory', MisconceptionObjectFactory],
   ['MockCsrfTokenService', MockCsrfTokenService],
+  ['ModeratorPageBackendApiService', ModeratorPageBackendApiService],
   ['MultipleChoiceInputRulesService', MultipleChoiceInputRulesService],
   ['MultipleChoiceInputValidationService',
     MultipleChoiceInputValidationService],
