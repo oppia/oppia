fc62cba71cb8d7040df4aab884cfcebbcce2fe11
ashutoshc8101 <ashutoshc8101@gmail.com>
2021-05-04 14:52:52 +0530

    nit

--- scripts/check_frontend_test_coverage.py
+++ scripts/check_frontend_test_coverage.py
@@ -216,7 +216,6 @@ NOT_FULLY_COVERED_FILENAMES = [
     'parameterize-rule-description.filter.ts',
     'player-correctness-feedback-enabled.service.ts',
     'player-transcript.service.ts',
-    'pretest-question-backend-api.service.ts',
     'profile-link-image.component.ts',
     'profile-link-text.component.ts',
     'progress-nav.directive.ts',
