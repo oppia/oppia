ff591c47fe32ead09bb35f540f206fce8b3a2b4e
Mayank-gaur <56195849+Mayank-gaur@users.noreply.github.com>
2021-05-07 02:14:41 +0530

    Update email-dashboard-backend-api.service.ts

--- core/templates/domain/email-dashboard/email-dashboard-backend-api.service.ts
+++ core/templates/domain/email-dashboard/email-dashboard-backend-api.service.ts
@@ -31,72 +31,72 @@ import {
 
 export type QueryData = Record<string, boolean | number>;
 
- @Injectable({
-   providedIn: 'root'
- })
+@Injectable({
+  providedIn: 'root'
+})
 export class EmailDashboardBackendApiService {
-   QUERY_DATA_URL: string = '/emaildashboarddatahandler';
-   QUERY_STATUS_CHECK_URL: string = '/querystatuscheck';
+  QUERY_DATA_URL: string = '/emaildashboarddatahandler';
+  QUERY_STATUS_CHECK_URL: string = '/querystatuscheck';
 
-   constructor(
-     private http: HttpClient) {}
+  constructor(
+    private http: HttpClient) {}
 
-   async fetchQueriesPageAsync(
-       pageSize: number, cursor: string): Promise<EmailDashboardQueryResults> {
-     // Here 'cursor' property is optional because it is present only if this
-     // function is called with a non-null value to 'cursor' arg.
-     // If we send a null value of 'cursor' the request URL would have
-     // something like '?cursor=null' and the backend would start looking for
-     // cursor with value 'null' which is not correct.
-     let params: {'cursor'?: string; 'num_queries_to_fetch': string;} = {
-       num_queries_to_fetch: String(pageSize)
-     };
-     if (cursor) {
-       params.cursor = cursor;
-     }
+  async fetchQueriesPageAsync(
+      pageSize: number, cursor: string): Promise<EmailDashboardQueryResults> {
+    // Here 'cursor' property is optional because it is present only if this
+    // function is called with a non-null value to 'cursor' arg.
+    // If we send a null value of 'cursor' the request URL would have
+    // something like '?cursor=null' and the backend would start looking for
+    // cursor with value 'null' which is not correct.
+    let params: {'cursor'?: string; 'num_queries_to_fetch': string;} = {
+      num_queries_to_fetch: String(pageSize)
+    };
+    if (cursor) {
+      params.cursor = cursor;
+    }
 
-     return new Promise((resolve, reject) => {
-       this.http.get<EmailDashboardQueryResultsBackendDict>(
-         this.QUERY_DATA_URL, {
-           params: params
-         }).toPromise().then(data => {
-         let emailDashboardQueryResultsObject = (
-           EmailDashboardQueryResults.createFromBackendDict(data));
-         resolve(emailDashboardQueryResultsObject);
-       }, errorResponse => {
-         reject(errorResponse.error.error);
-       });
-     });
-   }
+    return new Promise((resolve, reject) => {
+      this.http.get<EmailDashboardQueryResultsBackendDict>(
+        this.QUERY_DATA_URL, {
+          params: params
+        }).toPromise().then(data => {
+        let emailDashboardQueryResultsObject = (
+          EmailDashboardQueryResults.createFromBackendDict(data));
+        resolve(emailDashboardQueryResultsObject);
+      }, errorResponse => {
+        reject(errorResponse.error.error);
+      });
+    });
+  }
 
-   async fetchQueryAsync(queryId: string): Promise<EmailDashboardQuery> {
-     return new Promise((resolve, reject) => {
-       this.http.get<EmailDashboardQueryBackendDict>(
-         this.QUERY_STATUS_CHECK_URL, {
-           params: {
-             query_id: queryId
-           }
-         }).toPromise().then(data => {
-         let queryObject = EmailDashboardQuery.createFromBackendDict(data);
-         resolve(queryObject);
-       }, errorResponse => {
-         reject(errorResponse.error.error);
-       });
-     });
-   }
+  async fetchQueryAsync(queryId: string): Promise<EmailDashboardQuery> {
+    return new Promise((resolve, reject) => {
+      this.http.get<EmailDashboardQueryBackendDict>(
+        this.QUERY_STATUS_CHECK_URL, {
+          params: {
+            query_id: queryId
+          }
+        }).toPromise().then(data => {
+        let queryObject = EmailDashboardQuery.createFromBackendDict(data);
+        resolve(queryObject);
+      }, errorResponse => {
+        reject(errorResponse.error.error);
+      });
+    });
+  }
 
-   async submitQueryAsync(data: QueryData): Promise<EmailDashboardQuery> {
-     return new Promise((resolve, reject) => {
-       this.http.post<EmailDashboardQueryBackendDict>(
-         this.QUERY_DATA_URL, {
-           data: data }).toPromise().then(data => {
-         let queryObject = EmailDashboardQuery.createFromBackendDict(data);
-         resolve(queryObject);
-       }, errorResponse => {
-         reject(errorResponse.error.error);
-       });
-     });
-   }
+  async submitQueryAsync(data: QueryData): Promise<EmailDashboardQuery> {
+    return new Promise((resolve, reject) => {
+      this.http.post<EmailDashboardQueryBackendDict>(
+        this.QUERY_DATA_URL, {
+          data: data }).toPromise().then(data => {
+        let queryObject = EmailDashboardQuery.createFromBackendDict(data);
+        resolve(queryObject);
+      }, errorResponse => {
+        reject(errorResponse.error.error);
+      });
+    });
+  }
 }
 
 angular.module('oppia').factory(
