2d11a85f0c902c4c036285b39801168e3fde2cf1
Ashutosh Chauhan <ashutoshc8101@gmail.com>
2021-05-07 19:53:05 +0530

    Fix part of #8472: Learner view rating Service Migrated (#12676)
    * done
    
    * done
    
    * stage 1
    
    * done
    
    * nits
    
    * done
    
    * nit
    
    * e2e fix

--- core/templates/pages/exploration-player-page/learner-experience/conversation-skin.directive.ts
+++ core/templates/pages/exploration-player-page/learner-experience/conversation-skin.directive.ts
@@ -1407,6 +1407,7 @@ angular.module('oppia').directive('conversationSkin', [
               LearnerViewRatingService.onRatingUpdated.subscribe(() => {
                 $scope.userRating = LearnerViewRatingService.getUserRating();
                 AlertsService.addSuccessMessage('Rating saved!', 1000);
+                $rootScope.$applyAsync();
               })
             );
             ctrl.directiveSubscriptions.add(
---
+++ core/templates/pages/exploration-player-page/services/learner-view-rating-backend-api.service.spec.ts
@@ -0,0 +1,70 @@
+// Copyright 2021 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+/**
+ * @fileoverview Unit tests for LearnerViewRatingBackendApiService.
+ */
+
+import { HttpClientTestingModule, HttpTestingController } from
+  '@angular/common/http/testing';
+import { fakeAsync, flushMicrotasks, TestBed } from '@angular/core/testing';
+import { LearnerViewRatingBackendApiService } from './learner-view-rating-backend-api.service';
+
+describe('Learner View Rating Backend Api Service', () => {
+  let lvrbas: LearnerViewRatingBackendApiService;
+  let httpTestingController: HttpTestingController;
+  let successHandler = jasmine.createSpy('success');
+  let failHandler = jasmine.createSpy('fail');
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [LearnerViewRatingBackendApiService]
+    });
+    httpTestingController = TestBed.inject(HttpTestingController);
+    lvrbas = TestBed.inject(LearnerViewRatingBackendApiService);
+  });
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  it('should fetch user rating', fakeAsync(() => {
+    let jobOutput = {
+      user_rating: 5
+    };
+    lvrbas.getUserRatingAsync().then(successHandler, failHandler);
+
+    let req = httpTestingController.expectOne(
+      lvrbas.ratingsUrl);
+    expect(req.request.method).toEqual('GET');
+    req.flush(jobOutput);
+
+    flushMicrotasks();
+
+    expect(successHandler).toHaveBeenCalled();
+    expect(failHandler).not.toHaveBeenCalled();
+  }));
+
+  it('should submit user rating', fakeAsync(() => {
+    lvrbas.submitUserRatingAsync(3).then(successHandler, failHandler);
+    let req = httpTestingController.expectOne(
+      lvrbas.ratingsUrl);
+    expect(req.request.method).toEqual('PUT');
+    req.flush({});
+
+    flushMicrotasks();
+
+    expect(successHandler).toHaveBeenCalled();
+    expect(failHandler).not.toHaveBeenCalled();
+  }));
+});
