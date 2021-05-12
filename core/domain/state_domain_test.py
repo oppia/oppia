b085adc61040336aa51a9213ca0ec9c353dc87df
Sagang Wee <9004726+sagangwee@users.noreply.github.com>
2021-05-12 18:20:21 -0400

    Add interactions to translatable content

--- core/domain/state_domain_test.py
+++ core/domain/state_domain_test.py
@@ -1366,7 +1366,8 @@ class StateDomainUnitTests(test_utils.GenericTestBase):
                 'hint_1': '<p>hint one</p>',
                 'solution': '<p>hello_world is a string</p>',
                 'feedback_1': '<p>Feedback</p>',
-                'default_outcome': '<p>The default outcome.</p>'
+                'default_outcome': '<p>The default outcome.</p>',
+                'content': '<p>This is content</p>'
             })
 
     def test_get_content_id_mapping_needing_translations_with_interaction_translations(self): # pylint: disable=line-too-long
@@ -1478,7 +1479,9 @@ class StateDomainUnitTests(test_utils.GenericTestBase):
                 'hint_1': '<p>hint one</p>',
                 'solution': '<p>hello_world is a string</p>',
                 'feedback_1': '<p>Feedback</p>',
-                'default_outcome': '<p>The default outcome.</p>'
+                'default_outcome': '<p>The default outcome.</p>',
+                'content': '<p>This is content</p>',
+                'ca_placeholder_0': 'Placeholder'
             })
 
     def test_add_translation_works_correctly(self):
