cad7e01f21b378aab43df5e0ff2b2ee392356f84
Hardik Katehara <54679643+hardikkat24@users.noreply.github.com>
2021-05-13 20:33:45 +0530

    Update core/controllers/questions_list.py
    Co-authored-by: Vojtěch Jelínek <vojtin.j@gmail.com>

--- core/controllers/questions_list.py
+++ core/controllers/questions_list.py
@@ -67,7 +67,8 @@ class QuestionsListHandler(base.BaseHandler):
 
         (
             question_summaries, merged_question_skill_links,
-            offset) = (
+            offset
+        ) = (
                 question_services.get_displayable_question_skill_link_details(
                     constants.NUM_QUESTIONS_PER_PAGE, skill_ids, offset=offset)
             )
