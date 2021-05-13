5521c5e5ca9b2132604e0ee9efa5f9fb9306bb01
ashutoshc8101 <ashutoshc8101@gmail.com>
2021-05-14 00:15:30 +0530

    nits

--- scripts/linters/general_purpose_linter.py
+++ scripts/linters/general_purpose_linter.py
@@ -274,6 +274,7 @@ BAD_PATTERNS_JS_AND_TS_REGEXP = [
             'api services.'),
         'excluded_files': (
             'backend-api.service.ts',
+            'core/templates/pages/translate-loader.factory.ts',
             'core/templates/services/auth-interceptor.service.spec.ts',
             'core/templates/services/request-interceptor.service.spec.ts',),
         'excluded_dirs': ()
