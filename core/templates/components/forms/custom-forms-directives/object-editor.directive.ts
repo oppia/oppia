f2af5623a39fda3bf6449ac36120b33245420cc4
Srijan Reddy <srijan@srijanreddy.com>
2021-05-17 19:33:12 +0530

    Fix e2e

--- core/templates/components/forms/custom-forms-directives/object-editor.directive.ts
+++ core/templates/components/forms/custom-forms-directives/object-editor.directive.ts
@@ -71,7 +71,7 @@ angular.module('oppia').directive('objectEditor', [
           'set-of-unicode-string',
           'music-phrase',
           'number-with-units',
-          'non-negative-int',
+          'nonnegative-int',
           'numeric-expression',
           'position-of-terms',
           'positive-int',
