22fd25ef8e043b789b711affe0acc91ac528026d
Ashutosh Chauhan <ashutoshc8101@gmail.com>
2021-05-20 01:02:21 +0530

    Fix part of #8472: Migrate Exploration player state service (#12679)
    * done
    
    * done
    
    * done
    
    * done
    
    * nits
    
    * nits
    
    * nits
    
    * nit
    
    * tests added
    
    * nits
    
    * continue
    
    * tests done
    
    * nit
    
    * nits
    
    * nit
    
    * nits
    
    * nits
    
    * ft fixed;
    
    * nits
    
    * nits
    
    * nits
    
    * requested changed

--- core/templates/domain/exploration/ExplorationObjectFactory.ts
+++ core/templates/domain/exploration/ExplorationObjectFactory.ts
@@ -52,6 +52,7 @@ import { ExplorationChange } from './exploration-draft.model';
 
 export interface ExplorationBackendDict {
   'auto_tts_enabled'?: boolean;
+  'correctness_feedback_enabled': boolean;
   'draft_changes': ExplorationChange[];
   'is_version_of_draft_valid': boolean;
   'init_state_name': string;
@@ -61,7 +62,7 @@ export interface ExplorationBackendDict {
   'title': string;
   'language_code': string;
   'draft_change_list_id'?: number;
-  'version'?: string;
+  'version'?: number;
 }
 
 export class Exploration {
