f90037fd1505f712b88faee4d3c34c9981c1933b
Kevin Thomas <kevintab@tutanota.com>
2021-05-22 06:48:05 +0530

    Force pasting plain text in RTE editor

--- core/templates/components/ck-editor-helpers/ck-editor-4-rte.directive.ts
+++ core/templates/components/ck-editor-helpers/ck-editor-4-rte.directive.ts
@@ -66,6 +66,8 @@ angular.module('oppia').directive('ckEditor4Rte', [
         title: false,
         floatSpaceDockedOffsetY: 15,
         extraAllowedContent: extraAllowedContentRules,
+        forcePasteAsPlainText: true,
+
         sharedSpaces: sharedSpaces,
         skin: (
           'bootstrapck,' +
