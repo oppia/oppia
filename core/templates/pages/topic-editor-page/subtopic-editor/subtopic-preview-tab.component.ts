3f4bec4e0831d4cb406d7a0fbba2e7c37f47dad9
ashutoshc8101 <ashutoshc8101@gmail.com>
2021-05-23 00:09:51 +0530

    start

--- core/templates/pages/topic-editor-page/subtopic-editor/subtopic-preview-tab.component.ts
+++ core/templates/pages/topic-editor-page/subtopic-editor/subtopic-preview-tab.component.ts
@@ -13,96 +13,104 @@
 // limitations under the License.
 
 /**
- * @fileoverview Controller for the subtopic preview tab directive.
+ * @fileoverview Component for the subtopic preview tab directive.
  */
-require(
-  'components/forms/custom-forms-directives/thumbnail-uploader.directive.ts');
 
-require('domain/topic/SubtopicPageObjectFactory.ts');
-require('pages/topic-editor-page/services/topic-editor-state.service.ts');
-require('pages/topic-editor-page/services/topic-editor-routing.service.ts');
-require('pages/topic-viewer-page/subtopics-list/subtopics-list.component.ts');
-require('services/contextual/window-dimensions.service.ts');
+import { Component } from '@angular/core';
+import { downgradeComponent } from '@angular/upgrade/static';
 
-import { Subscription } from 'rxjs';
+@Component({
+  selector: 'oppia-subtopic-preview-tab',
+  templateUrl: './subtopic-preview-tab.component.html'
+})
+export class SubtopicPreviewTab {
+  constructor(
+    private topicEditorRoutingService: TopicEditorRoutingService
+  ) {}
+}
 
-angular.module('oppia').component('subtopicPreviewTab', {
-  template: require('./subtopic-preview-tab.component.html'),
-  controller: [
-    '$scope', 'TopicEditorRoutingService', 'TopicEditorStateService',
-    'WindowDimensionsService',
-    function(
-        $scope, TopicEditorRoutingService, TopicEditorStateService,
-        WindowDimensionsService) {
-      var ctrl = this;
-      ctrl.directiveSubscriptions = new Subscription();
-      var _initEditor = function() {
-        $scope.topic = TopicEditorStateService.getTopic();
-        $scope.subtopicId = (
-          TopicEditorRoutingService.getSubtopicIdFromUrl());
-        $scope.subtopic = (
-          $scope.topic.getSubtopicById(parseInt($scope.subtopicId)));
+angular.module('oppia').directive('oppiaSubtopicPreviewTab',
+  downgradeComponent({
+    component: SubtopicPreviewTab
+  }) as angular.IDirectiveFactory);
 
-        if ($scope.topic.getId() && $scope.subtopic) {
-          TopicEditorStateService.loadSubtopicPage(
-            $scope.topic.getId(), $scope.subtopicId);
-          $scope.editableTitle = $scope.subtopic.getTitle();
-          $scope.editableThumbnailFilename = (
-            $scope.subtopic.getThumbnailFilename());
-          $scope.editableThumbnailBgColor = (
-            $scope.subtopic.getThumbnailBgColor());
-          $scope.subtopicPage = (
-            TopicEditorStateService.getSubtopicPage());
-          $scope.pageContents = $scope.subtopicPage.getPageContents();
-          if ($scope.pageContents) {
-            $scope.htmlData = $scope.pageContents.getHtml();
-          }
-        }
-      };
+// angular.module('oppia').component('subtopicPreviewTab', {
+//   template: require('./subtopic-preview-tab.component.html'),
+//   controller: [
+//     '$scope', 'TopicEditorRoutingService', 'TopicEditorStateService',
+//     'WindowDimensionsService',
+//     function(
+//         $scope, TopicEditorRoutingService, TopicEditorStateService,
+//         WindowDimensionsService) {
+//       var ctrl = this;
+//       ctrl.directiveSubscriptions = new Subscription();
+//       var _initEditor = function() {
+//         $scope.topic = TopicEditorStateService.getTopic();
+//         $scope.subtopicId = (
+//           TopicEditorRoutingService.getSubtopicIdFromUrl());
+//         $scope.subtopic = (
+//           $scope.topic.getSubtopicById(parseInt($scope.subtopicId)));
 
-      $scope.navigateToSubtopic = function() {
-        TopicEditorRoutingService.navigateToSubtopicEditorWithId(
-          $scope.subtopicId);
-      };
+//         if ($scope.topic.getId() && $scope.subtopic) {
+//           TopicEditorStateService.loadSubtopicPage(
+//             $scope.topic.getId(), $scope.subtopicId);
+//           $scope.editableTitle = $scope.subtopic.getTitle();
+//           $scope.editableThumbnailFilename = (
+//             $scope.subtopic.getThumbnailFilename());
+//           $scope.editableThumbnailBgColor = (
+//             $scope.subtopic.getThumbnailBgColor());
+//           $scope.subtopicPage = (
+//             TopicEditorStateService.getSubtopicPage());
+//           $scope.pageContents = $scope.subtopicPage.getPageContents();
+//           if ($scope.pageContents) {
+//             $scope.htmlData = $scope.pageContents.getHtml();
+//           }
+//         }
+//       };
 
-      ctrl.directiveSubscriptions.add(
-        TopicEditorStateService.onSubtopicPageLoaded.subscribe(
-          () => {
-            $scope.subtopicPage = TopicEditorStateService.getSubtopicPage();
-            $scope.pageContents = $scope.subtopicPage.getPageContents();
-            $scope.htmlData = $scope.pageContents.getHtml();
-          }
-        )
-      );
+//       $scope.navigateToSubtopic = function() {
+//         TopicEditorRoutingService.navigateToSubtopicEditorWithId(
+//           $scope.subtopicId);
+//       };
 
-      $scope.changeContent = function(itemToDisplay) {
-        if (itemToDisplay === $scope.THUMBNAIL) {
-          $scope.thumbnailIsShown = true;
-          return;
-        }
-        $scope.thumbnailIsShown = false;
-      };
+//       ctrl.directiveSubscriptions.add(
+//         TopicEditorStateService.onSubtopicPageLoaded.subscribe(
+//           () => {
+//             $scope.subtopicPage = TopicEditorStateService.getSubtopicPage();
+//             $scope.pageContents = $scope.subtopicPage.getPageContents();
+//             $scope.htmlData = $scope.pageContents.getHtml();
+//           }
+//         )
+//       );
 
-      ctrl.directiveSubscriptions.add(
-        TopicEditorStateService.onTopicInitialized.subscribe(
-          () => _initEditor()
-        ));
-      ctrl.directiveSubscriptions.add(
-        TopicEditorStateService.onTopicReinitialized.subscribe(
-          () => _initEditor()
-        ));
+//       $scope.changeContent = function(itemToDisplay) {
+//         if (itemToDisplay === $scope.THUMBNAIL) {
+//           $scope.thumbnailIsShown = true;
+//           return;
+//         }
+//         $scope.thumbnailIsShown = false;
+//       };
 
-      ctrl.$onInit = function() {
-        $scope.THUMBNAIL = 'thumbnail';
-        $scope.CONTENT = 'content';
-        $scope.thumbnailIsShown = (
-          !WindowDimensionsService.isWindowNarrow());
-        _initEditor();
-      };
+//       ctrl.directiveSubscriptions.add(
+//         TopicEditorStateService.onTopicInitialized.subscribe(
+//           () => _initEditor()
+//         ));
+//       ctrl.directiveSubscriptions.add(
+//         TopicEditorStateService.onTopicReinitialized.subscribe(
+//           () => _initEditor()
+//         ));
 
-      ctrl.$onDestroy = function() {
-        ctrl.directiveSubscriptions.unsubscribe();
-      };
-    }
-  ]
-});
+//       ctrl.$onInit = function() {
+//         $scope.THUMBNAIL = 'thumbnail';
+//         $scope.CONTENT = 'content';
+//         $scope.thumbnailIsShown = (
+//           !WindowDimensionsService.isWindowNarrow());
+//         _initEditor();
+//       };
+
+//       ctrl.$onDestroy = function() {
+//         ctrl.directiveSubscriptions.unsubscribe();
+//       };
+//     }
+//   ]
+// });
