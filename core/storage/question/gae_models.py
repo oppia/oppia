4b87c9b00d8c6adb11c79ec8e50c5497636e1755
Hardik Katehara <hardikkatehara@gmail.com>
2021-05-25 03:20:02 +0530

    cls.key removed from order

--- core/storage/question/gae_models.py
+++ core/storage/question/gae_models.py
@@ -368,7 +368,7 @@ class QuestionSkillLinkModel(base_models.BaseModel):
             # Order by cls.key is needed alongside cls.last_updated so as to
             # resolve conflicts, if any.
             # Reference SO link: https://stackoverflow.com/q/12449197
-        ).order(-cls.last_updated, cls.key).fetch(
+        ).order(-cls.last_updated).fetch(
             question_skill_count, offset=offset)
 
         return (
