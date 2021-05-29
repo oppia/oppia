2b10c574381f1cd571c6c2574dd60b275df4d272
ashutoshc8101 <ashutoshc8101@gmail.com>
2021-05-29 23:50:10 +0530

    done

--- core/templates/pages/translate-custom-parser.ts
+++ core/templates/pages/translate-custom-parser.ts
@@ -17,15 +17,13 @@
  */
 
 import { TranslateDefaultParser, TranslateParser } from '@ngx-translate/core';
-import constants from 'assets/constants';
+import { AppConstants } from 'app.constants';
 import MessageFormat from 'messageformat';
-import { I18nLanguageCodeService } from 'services/i18n-language-code.service';
 
 export class TranslateCustomParser extends TranslateParser {
   private _messageFormat: MessageFormat;
   constructor(
-    private translateDefaultParser: TranslateDefaultParser,
-    private i18nLanguageCodeService: I18nLanguageCodeService
+    private translateDefaultParser: TranslateDefaultParser
   ) {
     super();
     /**
@@ -34,7 +32,7 @@ export class TranslateCustomParser extends TranslateParser {
      * So, here templateMatcher is modified to look for <[ ]> instead.
      */
     this.translateDefaultParser.templateMatcher = /<\[\s?([^{}\s]*)\s?\]>/g;
-    this._messageFormat = new MessageFormat();
+    this._messageFormat = new MessageFormat(AppConstants.DEFAULT_LANGUAGE_CODE);
   }
 
   interpolate(
@@ -43,8 +41,7 @@ export class TranslateCustomParser extends TranslateParser {
     let interpolatedValue = this.translateDefaultParser.interpolate(
       expr, params);
 
-    if (!(params &&
-         interpolatedValue !== undefined)) {
+    if (!(params && interpolatedValue !== undefined)) {
       return interpolatedValue;
     }
 
@@ -56,23 +53,7 @@ export class TranslateCustomParser extends TranslateParser {
       return interpolatedValue;
     }
 
-    /**
-     * The interpolated value by the default parser and the language
-     * code passed to messageFormat.compile should be compatible.
-     * But there is no way to tell whether the default parser used the primary
-     * language or the default language.
-     * So, here we first try with the primary language, if messageformat throws
-     * an error, we try with the default language.
-     */
-    try {
-      interpolatedValue = this._messageFormat.compile(
-        interpolatedValue, this.i18nLanguageCodeService
-          .getCurrentI18nLanguageCode())(params);
-    } catch (e) {
-      interpolatedValue = this._messageFormat.compile(
-        interpolatedValue, constants.DEFAULT_LANGUAGE_CODE)(params);
-    }
-    return interpolatedValue;
+    return this._messageFormat.compile(interpolatedValue)(params);
   }
 
   getValue(target: Object, key: string): string {
