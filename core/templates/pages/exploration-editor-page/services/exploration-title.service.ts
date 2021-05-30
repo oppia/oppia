f7921b5f15bc02bf5cd5e6b5c4631dbf63a6099a
Jayasanka Madhawa <78210362+Jayasanka-madhawa@users.noreply.github.com>
2021-05-31 02:56:47 +0530

    Fix part of  #8668: Document exploration-title-service  file with jsDoc

--- core/templates/pages/exploration-editor-page/services/exploration-title.service.ts
+++ core/templates/pages/exploration-editor-page/services/exploration-title.service.ts
@@ -32,6 +32,11 @@ angular.module('oppia').factory('ExplorationTitleService', [
     var child = Object.create(ExplorationPropertyService);
     child.propertyName = 'title';
     child._normalize = $filter('normalizeWhitespace');
+     /**
+     * 
+     * @param {string} value - tag array to check whether valid entity name or not
+     * @returns {boolean} - True if value is a valid entity name, false otherwise
+     */
     child._isValid = function(value) {
       return ValidatorsService.isValidEntityName(
         value, true, ExplorationRightsService.isPrivate());
