4b4aaef334305f6be2079031db6ed985e13579ce
Brian Rodriguez <brian@brianrodri.com>
2021-06-01 13:19:51 -0400

    Update file overviews

--- core/domain/beam_job_services.py
+++ core/domain/beam_job_services.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Services for managing user authentication."""
+"""Services for managing Apache Beam jobs."""
 
 from __future__ import absolute_import  # pylint: disable=import-only-modules
 from __future__ import unicode_literals  # pylint: disable=import-only-modules
--- core/domain/beam_job_services_test.py
+++ core/domain/beam_job_services_test.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Services for managing user authentication."""
+"""Unit tests for core.domain.beam_job_services."""
 
 from __future__ import absolute_import  # pylint: disable=import-only-modules
 from __future__ import unicode_literals  # pylint: disable=import-only-modules
