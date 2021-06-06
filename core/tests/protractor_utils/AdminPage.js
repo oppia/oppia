f07a4eeb6d3fb2ac70cc7c55adce42445b55ba2f
Krishna Rama Rao <krishnarao.2208@gmail.com>
2021-06-06 18:22:14 +0400

    lint

--- core/tests/protractor_utils/AdminPage.js
+++ core/tests/protractor_utils/AdminPage.js
@@ -321,7 +321,7 @@ var AdminPage = function() {
   this.expectNumberOfRunningOneOffJobs = async function(count) {
     await waitFor.visibilityOf(
       element(by.css('.protractor-test-unfinished-one-off-jobs-id')),
-    'Unfinished jobs taking too long to appear.');
+      'Unfinished jobs taking too long to appear.');
     var len = await element.all(by.css(
       '.protractor-test-unfinished-one-off-jobs-id')).count();
     expect(len).toEqual(count);
