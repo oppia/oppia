138d5df8d305845a7fbca70d30c32468c91b2d07
Aishwary Saxena <aishwary.saxena.min19@itbhu.ac.in>
2021-06-08 04:39:06 +0530

    Added tests for social-buttons.component

---
+++ core/templates/components/button-directives/social-buttons.component.spec.ts
@@ -0,0 +1,59 @@
+// Copyright 2021 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { UrlInterpolationService } from 'domain/utilities/url-interpolation.service';
+import { MockTranslatePipe } from 'tests/unit-test-utils';
+import { SocialButtonsComponent } from './social-buttons.component';
+
+/**
+ * @fileoverview Unit tests for SocialButtonsComponent
+ */
+
+fdescribe('SocialButtonsComponent', () => {
+  let component: SocialButtonsComponent;
+  let fixture: ComponentFixture<SocialButtonsComponent>;
+  let urlInterpolationService: UrlInterpolationService;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        SocialButtonsComponent,
+        MockTranslatePipe
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SocialButtonsComponent);
+    component = fixture.componentInstance;
+    urlInterpolationService = TestBed.inject(UrlInterpolationService);
+  });
+
+  it('should successfully instantiate the component from beforeEach block',
+    () => {
+      expect(component).toBeDefined();
+    });
+
+  it('should get static image url', () => {
+    spyOn(urlInterpolationService, 'getStaticImageUrl').and.returnValue(
+      'image-url'
+    );
+
+    expect(component.getStaticImageUrl('image-url')).toBe('image-url');
+    expect(urlInterpolationService.getStaticImageUrl).toHaveBeenCalledWith(
+      'image-url'
+    );
+  });
+});
