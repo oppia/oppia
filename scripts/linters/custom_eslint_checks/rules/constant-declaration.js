92384ff196974de3ec1328aaacb9b402d6edcdfd
AdityaDubey0 <74500dubey@gmail.com>
2021-06-07 21:52:37 +0530

    lint check for constany-declaration

---
+++ scripts/linters/custom_eslint_checks/rules/constant-declaration.js
@@ -0,0 +1,69 @@
+// Copyright 2021 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Lint check to ensure that constants are not declared in files
+ * other than *.constants.ajs.ts and that the constants are declared
+ * only single time.
+ */
+
+'use strict';
+
+module.exports = {
+  meta: {
+    type: 'problem',
+    docs: {
+      description: (
+        'Lint check to ensure that constants are not declared in files other' +
+        ' than *.constants.ajs.ts and that the constants are declared' +
+        ' only single time.'),
+      category: 'Possible Errors',
+      recommended: true,
+    },
+    fixable: null,
+    schema: [],
+    messages: {
+      nonConstantFile: 'constant is used in non constant file.',
+      multipleConstant: 'There are two constants in this file.'
+    },
+  },
+
+  create: function(context) {
+    var constantsDeclarations = [];
+    var args;
+    var fileName = context.getFilename();
+    var selector = (
+      'CallExpression[callee.property.name=constant][callee.object.callee.object.name=angular]');
+    return {
+      [selector]: function(node) {
+        if (!fileName.endsWith('.constants.ajs.ts')) {
+          context.report ({
+            node: node,
+            messageId: 'nonConstantFile'
+          });
+        }
+        args = node.arguments[0].value;
+        if (!constantsDeclarations.includes(args)) {
+          constantsDeclarations.push(args);
+        }
+        else {
+          context.report ({
+            node: node.arguments[0],
+            messageId: 'multipleConstant'
+          });
+        }
+      }
+    };
+  }
+};
---
+++ scripts/linters/custom_eslint_checks/rules/constant-declaration.spec.js
@@ -0,0 +1,59 @@
+// Copyright 2021 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Tests for the constant-declaration.js file.
+ */
+
+'use strict';
+
+var rule = require('./constant-declaration');
+var RuleTester = require('eslint').RuleTester;
+
+var ruleTester = new RuleTester();
+ruleTester.run('constant-declaration', rule, {
+  valid: [{
+    code:
+    `angular.module('oppia').constant(
+    'DEFAULT_SKILL', QuestionsListConstants.MODE_SELECT_SKILL);
+    angular.module('oppia').constant(
+    'MODE_SELECT_DIFFICULTY', QuestionsListConstants.MODE_SELECT_SKILL);`
+  }
+  ],
+
+  invalid: [
+    // {
+    //   code:
+    //   `angular.module('oppia').constant(
+    //   'DEFAULT_SKILL_DIFFICULTY', QuestionsListConstants.DEFAULT_SKILL_DIFFIC);
+    // angular.module('oppia').constant(
+    //   'MODE_SELECT_DIFFICULTY', QuestionsListConstants.MODE_SELECT_DIFFICULTY);
+    // angular.module('oppia').constant(
+    //   'DEFAULT_SKILL', QuestionsListConstants.MODE_SELECT_SKILL);`,
+    //   errors: [{
+    //     message: 'constant is used in non constant file.',
+    //   }],
+    // },
+    {
+      code:
+      `angular.module('oppia').constant(
+      'MODE_SELECT_DIFFICULTY', QuestionsListConstants.MODE_SELECT_DIFFICULT);
+      angular.module('oppia').constant(
+      'MODE_SELECT_DIFFICULTY', QuestionsListConstants.MODE_SELECT_DIFFICULT);`,
+      errors: [{
+        message: 'There are two constants in this file.',
+      }],
+    },
+  ]
+});
