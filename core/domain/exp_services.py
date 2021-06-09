4e8c5ef43f21405c9084aaaa1769c48ce86d8850
Aryaman Gupta <aryaman.gupta.met19@itbhu.ac.in>
2021-06-09 14:13:18 +0530

    added composite change list function

--- core/domain/exp_services.py
+++ core/domain/exp_services.py
@@ -1696,6 +1696,32 @@ def is_voiceover_change_list(change_list):
     return True
 
 
+def get_composite_change_list(exp_id, old_version, new_version):
+    """Returns a list of ExplorationChange domain objects consisting of
+    changes from old_version to new_version.
+
+    Args:
+        exp_id: str. The id of the exploration.
+        old_version: int. The version of the exploration on which the user is working.
+        new_version: int. The latest version of the exploration from the backend.
+
+    Returns:
+        list(ExplorationChange). List of ExplorationChange domain objects
+        consisting of changes from old_version to new_version.
+    """
+    snapshots_metadata = get_exploration_snapshots_metadata(exp_id)
+
+    composite_change_list_dict = []
+    for i in range(old_version, new_version):
+        composite_change_list_dict += snapshots[i]['commit_cmds']
+
+    composite_change_list = [
+                exp_domain.ExplorationChange(change)
+                for change in change_list]
+
+    return composite_change_list
+
+
 def is_version_of_draft_valid(exp_id, version):
     """Checks if the draft version is the same as the latest version of the
     exploration.
@@ -1708,10 +1734,8 @@ def is_version_of_draft_valid(exp_id, version):
         bool. Whether the given version number is the same as the current
         version number of the exploration in the datastore.
     """
-    latest_version = exp_fetchers.get_exploration_by_id(exp_id).version
-    if latest_version != version:
-        print(get_composite_change_list(exp_id, version, latest_version))
-    return latest_version == version
+
+    return exp_fetchers.get_exploration_by_id(exp_id).version == version
 
 
 def get_user_exploration_data(
@@ -2126,31 +2150,3 @@ def regenerate_missing_stats_for_exploration(exp_id):
         missing_exp_stats, missing_state_stats,
         num_valid_exp_stats, num_valid_state_stats
     )
-
-def get_composite_change_list(exp_id, old_version, new_version):
-    """Returns a list of ExplorationChange domain objects consisting of
-    changes from old_version to new_version.
-
-    Args:
-        exp_id: str. The id of the exploration.
-        old_version: int. The version of the exploration on which the user is working.
-        new_version: int. The latest version of the exploration from the backend.
-
-    Returns:
-        list(ExplorationChange). List of ExplorationChange domain objects
-        consisting of changes from old_version to new_version.
-    """
-    snapshots = get_exploration_snapshots_metadata(
-        exp_id)
-
-    change_list = []
-    for i in range(old_version, new_version):
-        change_list += snapshots[i].commit_cmds
-
-    print(change_list)
-
-    complete_change_list = [
-                exp_domain.ExplorationChange(change)
-                for change in change_list]
-
-    return complete_change_list
