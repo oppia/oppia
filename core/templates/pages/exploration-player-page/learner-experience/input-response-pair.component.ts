oppia/oppia

commit 091abbe1b741ecfdd2aaa71767c64ba8e1c60fcd

Fix part of #9749: Migrate input response pair. (#12853)
* nit

* asdf

* nits

* nit

* nits

* nit

core/templates/pages/exploration-player-page/learner-experience/input-response-pair.component.html
@@ -1,0 +1,98 @@
+<ng-template #popContent>
+  <div class="oppia-answer-popup-container">
+    <div class="oppia-answer-popup">
+      <angular-html-bind-wrapper [htmlData]="getAnswerHtml()">
+      </angular-html-bind-wrapper>
+    </div>
+  </div>
+</ng-template>
+
+<div class="conversation-skin-learner-answer-container">
+  <img class="conversation-skin-user-avatar rounded-circle" [src]="profilePicture" alt="">
+  <div *ngIf="!data.isHint">
+    <div *ngIf="getShortAnswerHtml()"
+         [ngbPopover]="popContent" triggers="manual" (click)="togglePopover()"
+         class="oppia-popover" #popover="ngbPopover" [autoClose]="false">
+      <angular-html-bind-wrapper classStr="oppia-rte-viewer conversation-skin-learner-answer-content"
+                                 [htmlData]="getShortAnswerHtml()">
+      </angular-html-bind-wrapper>
+    </div>
+    <div *ngIf="!getShortAnswerHtml()">
+      <angular-html-bind-wrapper classStr="oppia-rte-viewer conversation-skin-learner-answer-content"
+                                 [htmlData]="getAnswerHtml()">
+      </angular-html-bind-wrapper>
+    </div>
+  </div>
+  <div *ngIf="data.isHint">
+    <div class="oppia-rte-viewer conversation-skin-learner-answer-content">
+      {{ data.learnerInput | translate }}
+    </div>
+  </div>
+</div>
+
+<div class="conversation-skin-oppia-feedback-container" *ngIf="data.oppiaResponse !== ''">
+  <a [ngClass]="{'oppia-disabled-image-link': (OPPIA_AVATAR_LINK_URL === null)}"
+     [href]="OPPIA_AVATAR_LINK_URL"
+     target="_blank"
+     rel="noopener">
+    <img class="conversation-skin-oppia-avatar rounded-circle"
+         [src]="oppiaAvatarImageUrl" alt="">
+  </a>
+  <div *ngIf="!data.oppiaResponse && isCurrentCardAtEndOfTranscript()"
+       class="protractor-test-input-response-loading-dots">
+    <div class="conversation-skin-feedback-dot-one"></div>
+    <div class="conversation-skin-feedback-dot-two"></div>
+    <div class="conversation-skin-feedback-dot-three"></div>
+  </div>
+  <span id="getInputResponsePairId()"></span>
+  <div>
+    <angular-html-bind-wrapper *ngIf="data.oppiaResponse !== null"
+                               [htmlData]="data.oppiaResponse"
+                               classStr="oppia-rte-viewer conversation-skin-oppia-feedback-content protractor-test-conversation-feedback"
+                               [ngStyle]="{'min-width': (isVideoRteElementPresentInResponse() ? '75%': 'inherit')}"
+                               [ngClass]="getFeedbackAudioHighlightClass()">
+    </angular-html-bind-wrapper>
+  </div>
+</div>
+
+<style>
+  .conversation-skin-learner-answer-container,
+  .conversation-skin-oppia-feedback-container {
+    padding: 0 20px;
+    position: relative;
+  }
+
+  .conversation-skin-learner-answer-container {
+    text-align: right;
+  }
+
+  .conversation-skin-learner-answer-container .conversation-skin-user-avatar {
+    border: 2px solid #094142;
+    height: 100px;
+    right: -98px;
+    top: -33px;
+    width: 100px;
+  }
+
+  .conversation-skin-learner-answer-container:after {
+    border: 7px solid;
+    border-color: rgb(236, 239, 241) transparent transparent rgb(236, 239, 241);
+    content: ' ';
+    height: 0;
+    position: absolute;
+    top: 11%;
+    width: 0;
+  }
+
+  .conversation-skin-tutor-card-middle-section .conversation-skin-learner-answer-container:after {
+    border-color: rgba(236, 239, 241, 0.4) transparent transparent rgba(236, 239, 241, 0.4);
+  }
+
+  .oppia-popover {
+    cursor: pointer;
+  }
+  .oppia-answer-popup-container .oppia-answer-popup {
+    max-height: 300px;
+    overflow-y: scroll;
+  }
+</style>

core/templates/pages/exploration-player-page/learner-experience/input-response-pair.component.ts
@@ -1,0 +1,125 @@
+// Copyright 2021 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Component for an input/response pair in the learner view.
+ */
+
+import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { downgradeComponent } from '@angular/upgrade/static';
+import { NgbPopover } from '@ng-bootstrap/ng-bootstrap';
+import { AppConstants } from 'app.constants';
+import { InputResponsePair } from 'domain/state_card/StateCardObjectFactory';
+import { InteractionSpecsConstants } from 'pages/interaction-specs.constants';
+import { AudioPlayerService } from 'services/audio-player.service';
+import { AutogeneratedAudioPlayerService } from 'services/autogenerated-audio-player.service';
+import { ExplorationHtmlFormatterService } from 'services/exploration-html-formatter.service';
+import { ExplorationPlayerConstants } from '../exploration-player-page.constants';
+import { AudioTranslationManagerService } from '../services/audio-translation-manager.service';
+import { PlayerPositionService } from '../services/player-position.service';
+import { PlayerTranscriptService } from '../services/player-transcript.service';
+
+@Component({
+  selector: 'oppia-input-response-pair',
+  templateUrl: './input-response-pair.component.html'
+})
+export class InputResponsePairComponent {
+  @Input() data: InputResponsePair;
+  @Output() dataChange: EventEmitter<InputResponsePair> = new EventEmitter();
+  @Input() oppiaAvatarImageUrl: string;
+  @Input() profilePicture: string;
+  @Input() inputResponsePairId: string;
+  @Input() isLastPair: boolean;
+  OPPIA_AVATAR_LINK_URL: string;
+  @ViewChild('popover') popover: NgbPopover;
+
+  constructor(
+    private audioPlayerService: AudioPlayerService,
+    private audioTranslationManagerService: AudioTranslationManagerService,
+    private autogeneratedAudioPlayerService: AutogeneratedAudioPlayerService,
+    private explorationHtmlFormatterService: ExplorationHtmlFormatterService,
+    private playerPositionService: PlayerPositionService,
+    private playerTranscriptService: PlayerTranscriptService,
+  ) {}
+
+  ngOnInit(): void {
+    this.profilePicture = decodeURIComponent(this.profilePicture);
+  }
+
+  isVideoRteElementPresentInResponse(): boolean {
+    return this.data.oppiaResponse.includes(
+      'oppia-noninteractive-video');
+  }
+
+  isCurrentCardAtEndOfTranscript(): boolean {
+    return this.playerTranscriptService.isLastCard(
+      this.playerPositionService.getDisplayedCardIndex());
+  }
+
+  getAnswerHtml(): string {
+    let displayedCard = this.playerTranscriptService.getCard(
+      this.playerPositionService.getDisplayedCardIndex());
+    let interaction = displayedCard.getInteraction();
+    if (this.data) {
+      return this.explorationHtmlFormatterService.getAnswerHtml(
+        this.data.learnerInput, interaction.id,
+        interaction.customizationArgs);
+    }
+  }
+
+  // Returns a HTML string representing a short summary of the answer
+  // , or null if the answer does not have to be summarized.
+  getShortAnswerHtml(): string {
+    let displayedCard = this.playerTranscriptService.getCard(
+      this.playerPositionService.getDisplayedCardIndex());
+    let interaction = displayedCard.getInteraction();
+    let shortAnswerHtml = '';
+    if (this.data.learnerInput.hasOwnProperty('answerDetails')) {
+      shortAnswerHtml = (
+        this.data.learnerInput as unknown as { answerDetails: string })
+        .answerDetails;
+    } else if (
+      this.data && interaction.id &&
+        InteractionSpecsConstants.INTERACTION_SPECS[interaction.id]
+          .needs_summary) {
+      shortAnswerHtml = (
+        this.explorationHtmlFormatterService.getShortAnswerHtml(
+          this.data.learnerInput, interaction.id,
+          interaction.customizationArgs));
+    }
+    return shortAnswerHtml;
+  }
+
+  getFeedbackAudioHighlightClass(): string {
+    if (!this.isLastPair) {
+      return '';
+    }
+    if (this.audioTranslationManagerService
+      .getCurrentComponentName() ===
+      AppConstants.COMPONENT_NAME_FEEDBACK &&
+      (this.audioPlayerService.isPlaying() ||
+      this.autogeneratedAudioPlayerService.isPlaying())) {
+      return ExplorationPlayerConstants.AUDIO_HIGHLIGHT_CSS_CLASS;
+    }
+  }
+
+  togglePopover(): void {
+    this.popover.toggle();
+  }
+}
+
+angular.module('oppia').directive('oppiaInputResponsePair',
+  downgradeComponent({
+    component: InputResponsePairComponent
+  }) as angular.IDirectiveFactory);

core/templates/components/shared-component.module.ts
@@ -21,7 +21,7 @@ import 'zone.js';
 // Modules.
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { NgbModalModule, NgbNavModule, NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModalModule, NgbPopoverModule, NgbNavModule, NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuth, AngularFireAuthModule, USE_EMULATOR } from '@angular/fire/auth';
 import { FormsModule } from '@angular/forms';
@@ -73,6 +73,7 @@ import { UploadActivityModalComponent } from 'pages/creator-dashboard-page/modal
 import { CorrectnessFooterComponent } from 'pages/exploration-player-page/layout-directives/correctness-footer.component';
 import { ContinueButtonComponent } from 'pages/exploration-player-page/learner-experience/continue-button.component';
 import { PreviewThumbnailComponent } from 'pages/topic-editor-page/modal-templates/preview-thumbnail.component';
+import { InputResponsePairComponent } from 'pages/exploration-player-page/learner-experience/input-response-pair.component';
 
 
 // Directives.
@@ -123,6 +124,7 @@ const toastrConfig = {
     NgbTooltipModule,
     NgbNavModule,
     NgbModalModule,
+    NgbPopoverModule,
     FormsModule,
     RichTextComponentsModule,
     ToastrModule.forRoot(toastrConfig),
@@ -152,6 +154,7 @@ const toastrConfig = {
     CollectionSummaryTileComponent,
     ExplorationEmbedButtonModalComponent,
     FilterForMatchingSubstringPipe,
+    InputResponsePairComponent,
     KeyboardShortcutHelpModalComponent,
     LazyLoadingComponent,
     LimitToPipe,
@@ -212,6 +215,7 @@ const toastrConfig = {
     StateSkillEditorComponent,
     ExplorationEmbedButtonModalComponent,
     OutcomeFeedbackEditorComponent,
+    InputResponsePairComponent,
     KeyboardShortcutHelpModalComponent,
     PreviewThumbnailComponent,
     PromoBarComponent,
@@ -253,6 +257,7 @@ const toastrConfig = {
     CreateActivityModalComponent,
     ExplorationSummaryTileComponent,
     CollectionSummaryTileComponent,
+    InputResponsePairComponent,
     LazyLoadingComponent,
     LoadingMessageComponent,
     FilterForMatchingSubstringPipe,

core/templates/pages/exploration-player-page/learner-experience/input-response-pair.directive.html
@@ -1,81 +1,0 @@
-<div class="conversation-skin-learner-answer-container">
-  <img class="conversation-skin-user-avatar rounded-circle" ng-src="<[profilePicture()]>" alt="">
-  <div ng-if="!data.isHint">
-    <div ng-if="getShortAnswerHtml()"
-         popover-placement="bottom" uib-popover-template="'<[getAnswerPopoverUrl()]>'"
-         popover-trigger="click" class="oppia-popover">
-      <angular-html-bind class="oppia-rte-viewer conversation-skin-learner-answer-content" html-data="getShortAnswerHtml()">
-      </angular-html-bind>
-    </div>
-    <div ng-if="!getShortAnswerHtml()">
-      <angular-html-bind class="oppia-rte-viewer conversation-skin-learner-answer-content" html-data="getAnswerHtml()">
-      </angular-html-bind>
-    </div>
-  </div>
-  <div ng-if="data.isHint">
-    <div class="oppia-rte-viewer conversation-skin-learner-answer-content">
-      <[data.learnerInput | translate]>
-    </div>
-  </div>
-</div>
-
-<div class="conversation-skin-oppia-feedback-container" ng-if="data.oppiaResponse !== ''">
-  <a ng-class="{'oppia-disabled-image-link': (OPPIA_AVATAR_LINK_URL === null)}"
-     ng-href="<[OPPIA_AVATAR_LINK_URL]>"
-     target="_blank"
-     rel="noopener">
-    <img class="conversation-skin-oppia-avatar rounded-circle" ng-src="<[oppiaAvatarImageUrl()]>" alt="">
-  </a>
-  <div ng-if="!data.oppiaResponse && isCurrentCardAtEndOfTranscript()" class="protractor-test-input-response-loading-dots">
-    <div class="conversation-skin-feedback-dot-one"></div>
-    <div class="conversation-skin-feedback-dot-two"></div>
-    <div class="conversation-skin-feedback-dot-three"></div>
-  </div>
-  <span id="<[getInputResponsePairId()]>"></span>
-  <div>
-    <angular-html-bind ng-if="data.oppiaResponse !== null"
-                       html-data="data.oppiaResponse"
-                       class="oppia-rte-viewer conversation-skin-oppia-feedback-content protractor-test-conversation-feedback"
-                       ng-style="{'min-width': (isVideoRteElementPresentInResponse() ? '75%': 'inherit')}"
-                       ng-class="getFeedbackAudioHighlightClass()">
-    </angular-html-bind>
-  </div>
-</div>
-
-<style>
-  .conversation-skin-learner-answer-container,
-  .conversation-skin-oppia-feedback-container {
-    padding: 0 20px;
-    position: relative;
-  }
-
-  .conversation-skin-learner-answer-container {
-    text-align: right;
-  }
-
-  .conversation-skin-learner-answer-container .conversation-skin-user-avatar {
-    border: 2px solid #094142;
-    height: 100px;
-    right: -98px;
-    top: -33px;
-    width: 100px;
-  }
-
-  .conversation-skin-learner-answer-container:after {
-    border: 7px solid;
-    border-color: rgb(236, 239, 241) transparent transparent rgb(236, 239, 241);
-    content: ' ';
-    height: 0;
-    position: absolute;
-    top: 11%;
-    width: 0;
-  }
-
-  .conversation-skin-tutor-card-middle-section .conversation-skin-learner-answer-container:after {
-    border-color: rgba(236, 239, 241, 0.4) transparent transparent rgba(236, 239, 241, 0.4);
-  }
-
-  input-response-pair .oppia-popover {
-    cursor: pointer;
-  }
-</style>

core/templates/pages/exploration-player-page/learner-experience/input-response-pair.directive.ts
@@ -1,121 +1,0 @@
-// Copyright 2017 The Oppia Authors. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS-IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-/**
- * @fileoverview Directive for an input/response pair in the learner view.
- */
-
-require('directives/angular-html-bind.directive.ts');
-
-require(
-  'pages/exploration-player-page/services/' +
-  'audio-translation-manager.service.ts');
-require(
-  'pages/exploration-player-page/services/exploration-player-state.service.ts');
-require('pages/exploration-player-page/services/player-position.service.ts');
-require('pages/exploration-player-page/services/player-transcript.service.ts');
-require('services/audio-player.service.ts');
-require('services/autogenerated-audio-player.service.ts');
-require('services/exploration-html-formatter.service.ts');
-
-require('pages/interaction-specs.constants.ajs.ts');
-
-angular.module('oppia').directive('inputResponsePair', [
-  'UrlInterpolationService', function(UrlInterpolationService) {
-    return {
-      restrict: 'E',
-      scope: {
-        data: '=',
-        oppiaAvatarImageUrl: '&',
-        profilePicture: '&',
-        getInputResponsePairId: '&inputResponsePairId',
-        isLastPair: '&',
-      },
-      template: require('./input-response-pair.directive.html'),
-      controller: [
-        '$scope', 'AudioPlayerService', 'AudioTranslationManagerService',
-        'AutogeneratedAudioPlayerService', 'ExplorationHtmlFormatterService',
-        'PlayerPositionService', 'PlayerTranscriptService',
-        'AUDIO_HIGHLIGHT_CSS_CLASS', 'COMPONENT_NAME_FEEDBACK',
-        'INTERACTION_SPECS', 'OPPIA_AVATAR_LINK_URL',
-        function(
-            $scope, AudioPlayerService, AudioTranslationManagerService,
-            AutogeneratedAudioPlayerService, ExplorationHtmlFormatterService,
-            PlayerPositionService, PlayerTranscriptService,
-            AUDIO_HIGHLIGHT_CSS_CLASS, COMPONENT_NAME_FEEDBACK,
-            INTERACTION_SPECS, OPPIA_AVATAR_LINK_URL) {
-          $scope.OPPIA_AVATAR_LINK_URL = OPPIA_AVATAR_LINK_URL;
-          $scope.getAnswerPopoverUrl = function() {
-            return UrlInterpolationService.getDirectiveTemplateUrl(
-              '/pages/exploration-player-page/templates/' +
-              'answer-popup-container.template.html');
-          };
-
-          $scope.isVideoRteElementPresentInResponse = function() {
-            return $scope.data.oppiaResponse.includes(
-              'oppia-noninteractive-video');
-          };
-
-          $scope.isCurrentCardAtEndOfTranscript = function() {
-            return PlayerTranscriptService.isLastCard(
-              PlayerPositionService.getDisplayedCardIndex());
-          };
-
-          $scope.getAnswerHtml = function() {
-            var displayedCard = PlayerTranscriptService.getCard(
-              PlayerPositionService.getDisplayedCardIndex());
-            var interaction = displayedCard.getInteraction();
-            if ($scope.data) {
-              return ExplorationHtmlFormatterService.getAnswerHtml(
-                $scope.data.learnerInput, interaction.id,
-                interaction.customizationArgs);
-            }
-          };
-
-          // Returns a HTML string representing a short summary of the answer
-          // , or null if the answer does not have to be summarized.
-          $scope.getShortAnswerHtml = function() {
-            var displayedCard = PlayerTranscriptService.getCard(
-              PlayerPositionService.getDisplayedCardIndex());
-            var interaction = displayedCard.getInteraction();
-            var shortAnswerHtml = '';
-            if ($scope.data.learnerInput.hasOwnProperty('answerDetails')) {
-              shortAnswerHtml = $scope.data.learnerInput.answerDetails;
-            } else if (
-              $scope.data && interaction.id &&
-                INTERACTION_SPECS[interaction.id].needs_summary) {
-              shortAnswerHtml = (
-                ExplorationHtmlFormatterService.getShortAnswerHtml(
-                  $scope.data.learnerInput, interaction.id,
-                  interaction.customizationArgs));
-            }
-            return shortAnswerHtml;
-          };
-
-          $scope.getFeedbackAudioHighlightClass = function() {
-            if (!$scope.isLastPair()) {
-              return '';
-            }
-            if (AudioTranslationManagerService
-              .getCurrentComponentName() ===
-              COMPONENT_NAME_FEEDBACK &&
-              (AudioPlayerService.isPlaying() ||
-              AutogeneratedAudioPlayerService.isPlaying())) {
-              return AUDIO_HIGHLIGHT_CSS_CLASS;
-            }
-          };
-        }
-      ]
-    };
-  }]);

core/templates/pages/exploration-player-page/learner-experience/tutor-card.directive.html
@@ -46,24 +46,24 @@
          class="conversation-skin-tutor-card-middle-section conversation-skin-responses-animate-slide">
       <div ng-repeat="responsePair in getDisplayedCard().getInputResponsePairs() track by $index">
         <div ng-if="(!$last || !isInteractionInline())">
-          <input-response-pair data="responsePair"
-                               profile-picture="profilePicture"
-                               oppia-avatar-image-url="OPPIA_AVATAR_IMAGE_URL"
-                               input-response-pair-id="getInputResponsePairId($index)"
-                               is-last-pair="false">
-          </input-response-pair>
+          <oppia-input-response-pair [data]="responsePair"
+                                     [profile-picture]="profilePicture"
+                                     [oppia-avatar-image-url]="OPPIA_AVATAR_IMAGE_URL"
+                                     [input-response-pair-id]="getInputResponsePairId($index)"
+                                     [is-last-pair]="false">
+          </oppia-input-response-pair>
         </div>
       </div>
     </div>
 
     <!-- If the interaction is inline, always show the most recent response pair, if there is one. -->
     <div class="conversation-skin-tutor-card-bottom-section" ng-if="isInteractionInline() && getDisplayedCard().getInputResponsePairs().length > 0">
-      <input-response-pair data="getDisplayedCard().getInputResponsePairs()[getDisplayedCard().getInputResponsePairs().length - 1]"
-                           profile-picture="profilePicture"
-                           oppia-avatar-image-url="OPPIA_AVATAR_IMAGE_URL"
-                           input-response-pair-id="getInputResponsePairId(getDisplayedCard().getInputResponsePairs().length - 1)"
-                           is-last-pair="true">
-      </input-response-pair>
+      <oppia-input-response-pair [data]="getDisplayedCard().getInputResponsePairs()[getDisplayedCard().getInputResponsePairs().length - 1]"
+                                 [profile-picture]="profilePicture"
+                                 [oppia-avatar-image-url]="OPPIA_AVATAR_IMAGE_URL"
+                                 [input-response-pair-id]="getInputResponsePairId(getDisplayedCard().getInputResponsePairs().length - 1)"
+                                 [is-last-pair]="true">
+      </oppia-input-response-pair>
     </div>
 
     <!--

core/templates/pages/exploration-player-page/learner-experience/tutor-card.directive.ts
@@ -21,7 +21,7 @@ require(
   'pages/exploration-player-page/layout-directives/audio-bar.directive.ts');
 require(
   'pages/exploration-player-page/learner-experience/' +
-  'input-response-pair.directive.ts');
+  'input-response-pair.component.ts');
 
 require('domain/utilities/url-interpolation.service.ts');
 require('pages/exploration-player-page/services/audio-preloader.service.ts');

core/templates/pages/exploration-player-page/templates/answer-popup-container.template.html
@@ -1,12 +1,0 @@
-<div class="oppia-answer-popup-container">
-  <div class="oppia-answer-popup">
-    <angular-html-bind html-data="getAnswerHtml()">
-    </angular-html-bind>
-  </div>
-</div>
-<style>
-  .oppia-answer-popup-container .oppia-answer-popup {
-    max-height: 300px;
-    overflow-y: scroll;
-  }
-</style>

commit 783656435c1a870487b8ff44057d2cf52180fe5c

Fix for 'Failed to load resource' flake introduced in #12853. (#13032)

core/templates/pages/exploration-player-page/learner-experience/input-response-pair.component.html
@@ -8,7 +8,7 @@
 </ng-template>
 
 <div class="conversation-skin-learner-answer-container">
-  <img class="conversation-skin-user-avatar rounded-circle" [src]="profilePicture" alt="">
+  <img class="conversation-skin-user-avatar rounded-circle" [src]="decodedProfilePicture ? decodedProfilePicture : ''" alt="">
   <div *ngIf="!data.isHint">
     <div *ngIf="getShortAnswerHtml()"
          [ngbPopover]="popContent" triggers="manual" (click)="togglePopover()"

core/templates/pages/exploration-player-page/learner-experience/input-response-pair.component.ts
@@ -42,6 +42,7 @@ export class InputResponsePairComponent {
   @Input() inputResponsePairId: string;
   @Input() isLastPair: boolean;
   OPPIA_AVATAR_LINK_URL: string;
+  decodedProfilePicture: string;
   @ViewChild('popover') popover: NgbPopover;
 
   constructor(
@@ -54,7 +55,7 @@ export class InputResponsePairComponent {
   ) {}
 
   ngOnInit(): void {
-    this.profilePicture = decodeURIComponent(this.profilePicture);
+    this.decodedProfilePicture = decodeURIComponent(this.profilePicture);
   }
 
   isVideoRteElementPresentInResponse(): boolean {