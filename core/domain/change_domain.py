52ca1bc8f8ae73dc12dc62c6e1a1ae8cb8a407bc
rohitkatlaa <rohitkatlaa@gmail.com>
2021-06-12 16:28:53 +0530

    reverting the if condition

--- core/domain/change_domain.py
+++ core/domain/change_domain.py
@@ -75,12 +75,13 @@ def validate_cmd(cmd_name, valid_cmd_attribute_specs, actual_cmd_attributes):
         raise utils.ValidationError((', ').join(error_msg_list))
 
     deprecated_values = valid_cmd_attribute_specs.get('deprecated_values')
-    for attribute_name, attribute_values in deprecated_values.items():
-        actual_value = actual_cmd_attributes[attribute_name]
-        if actual_value in attribute_values:
-            raise utils.DeprecatedError(
-                'Value for %s in cmd %s: %s is deprecated' % (
-                    attribute_name, cmd_name, actual_value))
+    if deprecated_values:
+        for attribute_name, attribute_values in deprecated_values.items():
+            actual_value = actual_cmd_attributes[attribute_name]
+            if actual_value in attribute_values:
+                raise utils.DeprecatedError(
+                    'Value for %s in cmd %s: %s is deprecated' % (
+                        attribute_name, cmd_name, actual_value))
 
     allowed_values = valid_cmd_attribute_specs.get('allowed_values')
     if not allowed_values:
