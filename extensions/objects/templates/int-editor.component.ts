8395b67172fa2c7443d4d8096e9a6c97a93c2409
EeshaArif <43031234+EeshaArif@users.noreply.github.com>
2021-06-14 08:49:50 +0500

    Update int-editor.component.ts

--- extensions/objects/templates/int-editor.component.ts
+++ extensions/objects/templates/int-editor.component.ts
@@ -36,7 +36,7 @@ interface IntValidatorSchema {
 export class IntEditorComponent implements OnInit {
   // These properties are initialized using Angular lifecycle hooks 
   // and we need to do non-null assertion, for more information see
-  // https://github.com/oppia/oppia/wiki/Guide-on-defining-types#ts-7-1
+  // https://github.com/oppia/oppia/wiki/Guide-on-defining-types#ts-7-1.
   @Input() modalId!: symbol;
   @Input() value!: number;
   @Input() valueChanged: EventEmitter<number> = new EventEmitter<number>();
