64066a0cf8bea5f16630f9dbe0855e542f952ff0
mridul-netizen <mridulsetia18@gmail.com>
2021-06-14 11:42:41 +0530

    nit

--- core/templates/components/code-mirror/codemirror-mergeview.component.ts
+++ core/templates/components/code-mirror/codemirror-mergeview.component.ts
@@ -28,11 +28,12 @@ import { WindowRef } from 'services/contextual/window-ref.service';
 export class CodemirrorMergeviewComponent implements
   AfterViewInit, OnInit, OnChanges {
   @Input() options = {};
-  @Input() leftValue: string = ' ';
-  @Input() rightValue: string = ' ';
-  // This property is initialized using Angular lifecycle hook
-  // and we need to do non-null assertion, for more information see
-  // https://github.com/oppia/oppia/wiki/Guide-on-defining-types#ts-7-1
+  // These properties are initialized using Angular lifecycle hooks
+  // and component interactions, therefore we need to do non-null assertion,
+  // for more information see https://github.com/oppia/oppia/wiki/Guide-on-
+  // defining-types#ts-7-1
+  @Input() leftValue!: string;
+  @Input() rightValue!: string;
   codeMirrorInstance!: CodeMirror.MergeView.MergeViewEditor;
 
   constructor(
