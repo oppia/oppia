c43126ccd51024c278a8514148e350cc9ee2deae
EeshaArif <43031234+EeshaArif@users.noreply.github.com>
2021-06-14 08:47:41 +0500

    Update list-of-tabs-editor.component.ts

--- extensions/objects/templates/list-of-tabs-editor.component.ts
+++ extensions/objects/templates/list-of-tabs-editor.component.ts
@@ -28,8 +28,9 @@ import { downgradeComponent } from '@angular/upgrade/static';
   styleUrls: []
 })
 export class ListOfTabsEditorComponent implements OnInit {
-  // These properties are initialized using angular lifecycle hooks, see
-  // https://github.com/oppia/oppia/wiki/Guide-on-defining-types#ts-7-1.
+  // These properties are initialized using Angular lifecycle hooks 
+  // and we need to do non-null assertion, for more information see
+  // https://github.com/oppia/oppia/wiki/Guide-on-defining-types#ts-7-1
   @Input() modalId!: symbol;
   // TODO(#13015): Remove use of unknown as a type.
   // The property 'value' is dependent on another property, 'localValue', from
