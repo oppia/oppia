c0f37b55a60f9b3d88ff534c0e2882eb2fc26b49
EeshaArif <43031234+EeshaArif@users.noreply.github.com>
2021-06-14 09:09:34 +0500

    Update profile-link-text.component.ts

--- core/templates/components/profile-link-directives/profile-link-text.component.ts
+++ core/templates/components/profile-link-directives/profile-link-text.component.ts
@@ -25,7 +25,8 @@ import { downgradeComponent } from '@angular/upgrade/static';
   styleUrls: []
 })
 export class ProfileLinkTextComponent {
-  // This property is initialized using angular lifecycle hooks, see
+  // These properties are initialized using Angular lifecycle hooks 
+  // and we need to do non-null assertion, for more information see
   // https://github.com/oppia/oppia/wiki/Guide-on-defining-types#ts-7-1.
   @Input() username!: string;
   constructor() {}
