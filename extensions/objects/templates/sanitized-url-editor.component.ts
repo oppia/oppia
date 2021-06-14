c77ff8e978611fc6ce70d63404c2109efd20b5a5
EeshaArif <43031234+EeshaArif@users.noreply.github.com>
2021-06-14 08:54:21 +0500

    Update sanitized-url-editor.component.ts

--- extensions/objects/templates/sanitized-url-editor.component.ts
+++ extensions/objects/templates/sanitized-url-editor.component.ts
@@ -34,7 +34,8 @@ interface SanitizedUrlSchema {
   styleUrls: []
 })
 export class SanitizedUrlEditorComponent {
-  // These properties are initialized using angular lifecycle hooks, see
+  // These properties are initialized using Angular lifecycle hooks 
+  // and we need to do non-null assertion, for more information see
   // https://github.com/oppia/oppia/wiki/Guide-on-defining-types#ts-7-1.
   @Input() modalId!: symbol;
   // TODO(#13015): Remove use of unknown as a type.
