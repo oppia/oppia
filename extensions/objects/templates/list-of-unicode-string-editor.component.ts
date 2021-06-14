40e664469fa1ba6cea26ba479eb52d31d6de044a
EeshaArif <43031234+EeshaArif@users.noreply.github.com>
2021-06-14 08:50:33 +0500

    Update list-of-unicode-string-editor.component.ts

--- extensions/objects/templates/list-of-unicode-string-editor.component.ts
+++ extensions/objects/templates/list-of-unicode-string-editor.component.ts
@@ -29,7 +29,8 @@ import { downgradeComponent } from '@angular/upgrade/static';
   styleUrls: []
 })
 export class ListOfUnicodeStringEditorComponent implements OnInit {
-  // These properties are initialized using angular lifecycle hooks, see
+  // These properties are initialized using Angular lifecycle hooks 
+  // and we need to do non-null assertion, for more information see
   // https://github.com/oppia/oppia/wiki/Guide-on-defining-types#ts-7-1.
   @Input() modalId!: symbol;
   // TODO(#13015): Remove use of unknown as a type.
