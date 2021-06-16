21e577aaf37d9804f06bf4bdcc82a8fd0367bf53
EeshaArif <eeshaarif@gmail.com>
2021-06-16 09:43:02 +0500

    fix: fix lint errors

--- core/templates/components/forms/custom-forms-directives/audio-file-uploader.component.ts
+++ core/templates/components/forms/custom-forms-directives/audio-file-uploader.component.ts
@@ -24,9 +24,9 @@ import { downgradeComponent } from '@angular/upgrade/static';
   templateUrl: './audio-file-uploader.component.html'
 })
 export class AudioFileUploaderComponent {
-  // These properties are initialized using Angular lifecycle hooks 
+  // These properties are initialized using Angular lifecycle hooks
   // and we need to do non-null assertion, for more information see
-  // https://github.com/oppia/oppia/wiki/Guide-on-defining-types#ts-7-1.
+  // https://github.com/oppia/oppia/wiki/Guide-on-defining-types#ts-7-1
   @Input() droppedFile!: FileList;
   @Output() fileChange: EventEmitter<File> = new EventEmitter<File>();
   @Output() fileClear: EventEmitter<void> = new EventEmitter<void>();
