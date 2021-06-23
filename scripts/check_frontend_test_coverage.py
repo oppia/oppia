939ad2ad66101dab2b845d5f54c411d83f60799b
Ashutosh Chauhan <ashutoshc8101@gmail.com>
2021-06-24 01:25:04 +0530

    Fix part of #9749: Migrate Admin page. (#13125)
    * continue
    
    * progress
    
    * done
    
    * continue;
    
    * continue
    
    * nit
    
    * nit
    
    * nit
    
    * nit
    
    * nit
    
    * nits
    
    * nits
    
    * nit
    
    * nit
    
    * not
    
    * lint
    
    * nit
    
    * nit
    
    * feature removed
    
    * nit
    
    * nit
    
    * backend tests fixed
    
    * nit
    
    * nit
    
    * test-coverage
    
    * nit
    
    * nit
    
    * lighthouse_script fixed

--- scripts/check_frontend_test_coverage.py
+++ scripts/check_frontend_test_coverage.py
@@ -38,10 +38,10 @@ EXCLUDED_DIRECTORIES = [
 # NOTE TO DEVELOPERS: do not add any new files to this list without asking
 # @nithusha21 first.
 NOT_FULLY_COVERED_FILENAMES = [
-    'admin-config-tab.directive.ts',
-    'admin-misc-tab.directive.ts',
-    'admin-page.directive.ts',
-    'admin-roles-tab.directive.ts',
+    'admin-config-tab.component.ts',
+    'admin-misc-tab.component.ts',
+    'admin-page.component.ts',
+    'admin-roles-tab.component.ts',
     'angular-html-bind.directive.ts',
     'answer-classification.service.ts',
     'answer-group-editor.directive.ts',
