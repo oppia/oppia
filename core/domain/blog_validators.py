fb64a72b7e0ce242b72d88becd5c57be5c139b8c
Rijuta Singh <68547101+Rijuta-s@users.noreply.github.com>
2021-06-23 12:04:46 +0530

    Milestone 1.2 : Adds Blog Domain, Services and Validators. (#13118)
    * adding blog models
    
    * adding to platform models.py
    
    * minor changes
    
    * Adding wipeout test
    
    * fixing wipeout file
    
    * fixing lint issues
    
    * lint check
    
    * minor fix
    
    * adding takeout  functions
    
    * adding takeout service
    
    * fixes
    
    * MINOR FIX
    
    * fix
    
    * fixing takeout service
    
    * excluding models from audit jobs
    
    * Applying suggestions
    
    * Fixing doc strings
    
    * Fixing coverage
    
    * minor fix
    
    * minor fixes
    
    * doc string fixes
    
    * Applying suggestions
    
    * applying suggestions.
    
    * adding new files
    
    * adding validations and applying suggestions
    
    * applying suggestions
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * fix lint issue
    
    * adding wipeout for blogPostRights model.
    
    * Merge branch 'blog_storage_models' into blog_domain_layer
    
    * adding function in storage
    
    * Adding removing user from editor ids function
    
    * minor fix
    
    * Applying suggestions
    
    * minor fix
    
    * Adding domain,services and validators
    
    * Merge remote-tracking branch 'upstream/develop' into blog_storage_models
    
    * Merge remote-tracking branch 'upstream/develop' into blog_storage_models
    
    * lint fixes
    
    * minor fix
    
    * Fixing doc string.
    
    * fixing coverage
    
    * fixing lint issues
    
    * applying suggestions
    
    * fixing coverage
    
    * Minor fix
    
    * applying suggestions
    
    * Removing extra line after function definitions.
    
    * Applying suggestions.
    
    * lint fix
    
    * lint fix
    
    * fixing coverage
    
    * fixing tag expression
    
    * applying suggestions
    
    * minor fix
    
    * minor lint fix

--- core/domain/blog_validators.py
+++ core/domain/blog_validators.py
@@ -20,17 +20,31 @@ from __future__ import absolute_import  # pylint: disable=import-only-modules
 from __future__ import unicode_literals  # pylint: disable=import-only-modules
 
 from core.domain import base_model_validators
+from core.domain import blog_services
 from core.platform import models
 
 datastore_services = models.Registry.import_datastore_services()
 
 (base_models, blog_models) = models.Registry.import_models([
-    models.NAMES.base_model, models.NAMES.blog
-    ])
+    models.NAMES.base_model, models.NAMES.blog])
 
 
 class BlogPostModelValidator(base_model_validators.BaseModelValidator):
     """Class for validating BlogPostModel."""
+
+    @classmethod
+    def _get_model_domain_object_instance(cls, item):
+        return blog_services.get_blog_post_from_model(item)
+
+    @classmethod
+    def _get_domain_object_validation_type(cls, item):
+        blog_post_rights = blog_services.get_blog_post_rights(
+            item.id, strict=False)
+
+        if blog_post_rights.blog_post_is_published:
+            return base_model_validators.VALIDATION_MODE_STRICT
+
+        return base_model_validators.VALIDATION_MODE_NON_STRICT
 
     @classmethod
     def _get_model_id_regex(cls, unused_item):
@@ -53,20 +67,102 @@ class BlogPostModelValidator(base_model_validators.BaseModelValidator):
         ]
         field_name_to_external_model_references.append(
             base_model_validators.UserSettingsModelFetcherDetails(
-                'author_ids', [item.author_id],
+                'author_id', [item.author_id],
                 may_contain_system_ids=True,
                 may_contain_pseudonymous_ids=True
             )
         )
         return field_name_to_external_model_references
 
+    @classmethod
+    def _validate_title_is_unique(cls, item):
+        """Validate that title of the model unique.
+
+        Args:
+            item: datastore_services.Model. BlogPostModel to validate.
+        """
+        if item.title != '':
+            blog_post_models = blog_models.BlogPostModel.query().filter(
+                blog_models.BlogPostModel.title == (
+                    item.title)).filter(
+                        blog_models.BlogPostModel.deleted == False).fetch() # pylint: disable=singleton-comparison
+            blog_model_ids = [
+                model.id for model in blog_post_models if model.id != item.id]
+            if blog_model_ids:
+                cls._add_error(
+                    'unique title for blog post',
+                    'Entity id %s: title %s matches with title '
+                    'blog post models with ids %s' % (
+                        item.id, item.title, blog_model_ids))
+
+    @classmethod
+    def _validate_url_fragment_is_unique(cls, item):
+        """Validates that the url fragment of the model is unique.
+
+        Args:
+            item: datastore_services.Model. BlogPostModel to validate.
+        """
+        if item.url_fragment != '':
+            blog_post_models = blog_models.BlogPostModel.query().filter(
+                blog_models.BlogPostModel.url_fragment == (
+                    item.url_fragment)).filter(
+                        blog_models.BlogPostModel.deleted == False).fetch() # pylint: disable=singleton-comparison
+            blog_model_ids = [
+                model.id for model in blog_post_models if model.id != item.id]
+            if blog_model_ids:
+                cls._add_error(
+                    'unique url fragment for blog post',
+                    'Entity id %s: url fragment %s matches with url fragment '
+                    'of blog post models with ids %s' % (
+                        item.id, item.url_fragment, blog_model_ids))
+
+    @classmethod
+    def _validate_title_matches_summary_model_title(cls, item):
+        """Validates that the title of blog post model matches corresponding
+        summary model title.
+
+        Args:
+            item: datastore_services.Model. BlogPostModel to validate.
+        """
+        if item.title != '':
+            blog_post_summary_title = (
+                blog_models.BlogPostSummaryModel.get_by_id(item.id).title)
+            if blog_post_summary_title != item.title:
+                cls._add_error(
+                    'Same Title for blog post and blog post summary',
+                    'Title for blog post with Entity id'
+                    ' %s does not match with title of corresponding'
+                    ' blog post summary model' % (item.id))
+
+    @classmethod
+    def _get_custom_validation_functions(cls):
+        return [
+            cls._validate_title_is_unique,
+            cls._validate_url_fragment_is_unique,
+            cls._validate_title_matches_summary_model_title
+        ]
+
 
 class BlogPostSummaryModelValidator(base_model_validators.BaseModelValidator):
     """Class for validating BlogPostSummaryModel."""
 
     @classmethod
+    def _get_model_domain_object_instance(cls, item):
+        return blog_services.get_blog_post_summary_from_model(item)
+
+    @classmethod
+    def _get_domain_object_validation_type(cls, item):
+        blog_post_rights = blog_services.get_blog_post_rights(
+            item.id, strict=False)
+
+        if blog_post_rights.blog_post_is_published:
+            return base_model_validators.VALIDATION_MODE_STRICT
+
+        return base_model_validators.VALIDATION_MODE_NON_STRICT
+
+    @classmethod
     def _get_model_id_regex(cls, unused_item):
-        # Valid id: random_hash of 12 chars.
+        # Valid id: random_hash of constants.BASE_MODELS_ID_LENGTH chars.
         regex_string = '[A-Za-z0-9-_]{1,%s}$' % (base_models.ID_LENGTH)
         return regex_string
 
@@ -86,12 +182,67 @@ class BlogPostSummaryModelValidator(base_model_validators.BaseModelValidator):
         ]
         field_name_to_external_model_references.append(
             base_model_validators.UserSettingsModelFetcherDetails(
-                'author_ids', [item.author_id],
+                'author_id', [item.author_id],
                 may_contain_system_ids=True,
                 may_contain_pseudonymous_ids=True
             )
         )
         return field_name_to_external_model_references
+
+    @classmethod
+    def _validate_title_is_unique(cls, item):
+        """Validate that title of the model unique.
+
+        Args:
+            item: datastore_services.Model. BlogPostSummaryModel to validate.
+        """
+        if item.title != '':
+            blog_post_summary_models = (
+                blog_models.BlogPostSummaryModel.query().filter(
+                    blog_models.BlogPostSummaryModel.title == (item.title)
+                    ).filter(
+                        blog_models.BlogPostSummaryModel.deleted == False  # pylint: disable=singleton-comparison
+                    ).fetch()
+                )
+            blog_model_ids = [
+                model.id
+                for model in blog_post_summary_models if model.id != item.id]
+            if blog_model_ids:
+                cls._add_error(
+                    'unique title for blog post',
+                    'Entity id %s: title %s matches with title '
+                    'blog post summary models with ids %s' % (
+                        item.id, item.title, blog_model_ids))
+
+    @classmethod
+    def _validate_url_fragment_is_unique(cls, item):
+        """Validates that the url fragment of the model is unique.
+
+        Args:
+            item: datastore_services.Model. BlogPostModel to validate.
+        """
+        if item.url_fragment != '':
+            summary_models = (
+                blog_models.BlogPostSummaryModel.query().filter(
+                    blog_models.BlogPostSummaryModel.url_fragment == (
+                        item.url_fragment)).filter(
+                            blog_models.BlogPostSummaryModel.deleted == False # pylint: disable=singleton-comparison
+                            ).fetch())
+            blog_model_ids = [
+                model.id for model in summary_models if model.id != item.id]
+            if blog_model_ids:
+                cls._add_error(
+                    'unique url fragment for blog post',
+                    'Entity id %s: url fragment %s matches with url fragment '
+                    'of blog post summary models with ids %s' % (
+                        item.id, item.url_fragment, blog_model_ids))
+
+    @classmethod
+    def _get_custom_validation_functions(cls):
+        return [
+            cls._validate_title_is_unique,
+            cls._validate_url_fragment_is_unique,
+        ]
 
 
 class BlogPostRightsModelValidator(base_model_validators.BaseModelValidator):
@@ -99,7 +250,7 @@ class BlogPostRightsModelValidator(base_model_validators.BaseModelValidator):
 
     @classmethod
     def _get_model_id_regex(cls, unused_item):
-        # Valid id: random_hash of 12 chars.
+        # Valid id: random_hash of constants.BASE_MODELS_ID_LENGTH chars.
         regex_string = '[A-Za-z0-9-_]{1,%s}$' % (base_models.ID_LENGTH)
         return regex_string
 
@@ -117,4 +268,11 @@ class BlogPostRightsModelValidator(base_model_validators.BaseModelValidator):
                 [item.id]
             )
         ]
+        field_name_to_external_model_references.append(
+            base_model_validators.UserSettingsModelFetcherDetails(
+                'editor_ids', item.editor_ids,
+                may_contain_system_ids=True,
+                may_contain_pseudonymous_ids=False
+            )
+        )
         return field_name_to_external_model_references
