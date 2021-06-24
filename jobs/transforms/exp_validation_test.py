ca7d50866f9caba0c1b1ae133d230d292ad0eaf3
YBCS <budha627@gmail.com>
2021-06-24 22:35:12 +0530

    [ci skip] IntegerProperty error

--- jobs/transforms/exp_validation_test.py
+++ jobs/transforms/exp_validation_test.py
@@ -20,7 +20,9 @@ from __future__ import absolute_import  # pylint: disable=import-only-modules
 from __future__ import unicode_literals  # pylint: disable=import-only-modules
 
 from core.platform import models
+from core.tests import test_utils
 from jobs import job_test_utils
+from jobs.decorators import validation_decorators
 from jobs.transforms import exp_validation
 from jobs.types import base_validation_errors
 
@@ -249,3 +251,12 @@ class ValidateExplorationCommitCmdsSchemaTests(
                 'Value for property_name in cmd edit_state_property: '
                 'invalid is not allowed')
         ])
+
+
+class RelationshipsOfTests(test_utils.TestBase):
+
+    def test_exploration_model_relationships(self):
+        self.assertItemsEqual(
+            validation_decorators.RelationshipsOf.get_model_kind_references(
+                'ExplorationModel', 'exploration_model_ids'), # to change
+            ['ExplorationCommitLogEntryModel'])
