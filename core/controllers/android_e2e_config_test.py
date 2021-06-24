93e3bced252942017f24a89321b235bc4a0d3f50
farees <fareezzhussain@gmail.com>
2021-06-24 09:07:24 +0530

    remove csrf token

--- core/controllers/android_e2e_config_test.py
+++ core/controllers/android_e2e_config_test.py
@@ -46,13 +46,12 @@ class AndroidConfigTest(test_utils.GenericTestBase):
     """Server integration tests for operations on the admin page."""
 
     def test_cannot_load_structures_in_production_mode(self):
-        csrf_token = self.get_new_csrf_token()
         prod_mode_swap = self.swap(constants, 'DEV_MODE', False)
         assert_raises_regexp_context_manager = self.assertRaisesRegexp(
             Exception, 'Cannot load new structures data in production.')
         with assert_raises_regexp_context_manager, prod_mode_swap:
-            self.post_json(
-                '/initialize_android_test_data', {}, csrf_token=csrf_token)
+            self.post_req(
+                '/initialize_android_test_data')
 
     def test_check_if_topic_is_published(self):
         self.post_req(
@@ -63,17 +62,15 @@ class AndroidConfigTest(test_utils.GenericTestBase):
         self.assertTrue(topic_rights.topic_is_published)
 
     def test_check_if_topic_is_already_published(self):
-        csrf_token = self.get_new_csrf_token()
         self.post_req(
             '/initialize_android_test_data')
         assert_raises_regexp_context_manager = self.assertRaisesRegexp(
             Exception, 'The topic is already published.')
         with assert_raises_regexp_context_manager:
-            self.post_json(
-                '/initialize_android_test_data', {}, csrf_token=csrf_token)
+            self.post_req(
+                '/initialize_android_test_data')
 
     def test_check_if_topic_exists_and_not_published(self):
-        csrf_token = self.get_new_csrf_token()
         self.post_req(
             '/initialize_android_test_data')
         topic = topic_fetchers.get_topic_by_name('Android test')
@@ -82,5 +79,5 @@ class AndroidConfigTest(test_utils.GenericTestBase):
         assert_raises_regexp_context_manager = self.assertRaisesRegexp(
             Exception, 'The topic exists but not published.')
         with assert_raises_regexp_context_manager:
-            self.post_json(
-                '/initialize_android_test_data', {}, csrf_token=csrf_token)
+            self.post_req(
+                '/initialize_android_test_data')
