3e05d43ad22d2f4535e5b1886fd816eb8dbdd1a3
Aishwary Saxena <aishwary.saxena.min19@itbhu.ac.in>
2021-06-25 21:51:42 +0530

    removed fdescribe

--- core/templates/components/common-layout-directives/navigation-bars/top-navigation-bar.component.spec.ts
+++ core/templates/components/common-layout-directives/navigation-bars/top-navigation-bar.component.spec.ts
@@ -63,7 +63,7 @@ class MockWindowRef {
   }
 }
 
-fdescribe('TopNavigationBarComponent', () => {
+describe('TopNavigationBarComponent', () => {
   let fixture: ComponentFixture<TopNavigationBarComponent>;
   let component: TopNavigationBarComponent;
   let mockWindowRef: MockWindowRef;
