936a5eed4ad69ecdbb03ab74fa002d02668c0156
Radesh-kumar <imradesh@gmail.com>
2021-06-25 23:00:11 +0530

    revert back

--- core/templates/components/common-layout-directives/navigation-bars/top-navigation-bar.component.spec.ts
+++ core/templates/components/common-layout-directives/navigation-bars/top-navigation-bar.component.spec.ts
@@ -61,7 +61,7 @@ class MockWindowRef {
   }
 }
 
-fdescribe('TopNavigationBarComponent', () => {
+describe('TopNavigationBarComponent', () => {
   let fixture: ComponentFixture<TopNavigationBarComponent>;
   let component: TopNavigationBarComponent;
   let mockWindowRef: MockWindowRef;
