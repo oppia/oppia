2d61af33cb357fb38461853dbe307cc87222641f
Praneeth <praneethg2001@gmail.com>
2021-07-02 23:19:12 +0530

    Frontend tests for image-with-regions-editor.component (#13258)
    * initial commit
    
    * addressed comments
    
    * addressed comments

--- scripts/check_frontend_test_coverage.py
+++ scripts/check_frontend_test_coverage.py
@@ -102,7 +102,6 @@ NOT_FULLY_COVERED_FILENAMES = [
     'html-editor.component.ts',
     'html-select.directive.ts',
     'i18n-language-selector.component.ts',
-    'image-with-regions-editor.component.ts',
     'input-response-pair.component.ts',
     'int-editor.component.ts',
     'item-selection-input-validation.service.ts',
