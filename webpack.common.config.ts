34b03c48601617e6cf105c6e24e5cb0f0ccadf64
ashutoshc8101 <ashutoshc8101@gmail.com>
2021-07-06 12:53:26 +0530

    nit

--- webpack.common.config.ts
+++ webpack.common.config.ts
@@ -476,8 +476,7 @@ module.exports = {
       filename: 'oppia-root.mainpage.html',
       meta: defaultMeta,
       template:
-          commonPrefix + '/pages/oppia-root/' +
-          'index.html',
+          commonPrefix + '/pages/oppia-root/index.html',
       minify: htmlMinifyConfig,
       inject: false
     }),
