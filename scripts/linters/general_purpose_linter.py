2614315f67b9b1af41042fd48a9d78fdd41547c7
ashutoshc8101 <ashutoshc8101@gmail.com>
2021-07-08 01:09:57 +0530

    nits

--- scripts/linters/general_purpose_linter.py
+++ scripts/linters/general_purpose_linter.py
@@ -162,7 +162,7 @@ BAD_PATTERNS_JS_AND_TS_REGEXP = [
             'api services.'),
         'excluded_files': (
             'backend-api.service.ts',
-            'core/templates/pages/translate-loader.factory.ts',
+            'core/templates/i18n/translate-loader.factory.ts',
             'translate-loader.factory.spec.ts',
             'core/templates/services/auth-interceptor.service.spec.ts',
             'core/templates/services/request-interceptor.service.spec.ts',),
