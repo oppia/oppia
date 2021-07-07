be5358cc896c20ad98b25ed739ad57d406c15265
Hardik Katehara <hardikkatehara@gmail.com>
2021-07-07 12:34:42 +0530

    remove mypy_req folder

--- mypy_requirements/mypy/typeshed/stdlib/tkinter/colorchooser.pyi
+++
@@ -1,9 +0,0 @@
-from tkinter.commondialog import Dialog
-from typing import Any, ClassVar, Optional, Tuple, Union
-
-class Chooser(Dialog):
-    command: ClassVar[str]
-
-def askcolor(
-    color: Optional[Union[str, bytes]] = ..., **options: Any
-) -> Union[Tuple[None, None], Tuple[Tuple[float, float, float], str]]: ...
