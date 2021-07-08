2112606d88402796402d1c6e5d57be659f168022
Aishwary Saxena <aishwary.saxena.min19@itbhu.ac.in>
2021-07-08 09:34:40 +0530

    Removed file name from NOT_FULLY_COVERED_FILENAMES

--- scripts/check_frontend_test_coverage.py
+++ scripts/check_frontend_test_coverage.py
@@ -40,7 +40,6 @@ EXCLUDED_DIRECTORIES = [
 NOT_FULLY_COVERED_FILENAMES = [
     'angular-html-bind.directive.ts',
     'answer-classification.service.ts',
-    'answer-group-editor.component.ts',
     'App.ts',
     'audio-player.service.ts',
     'audio-preloader.service.ts',
