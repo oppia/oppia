110b489aff212b1f01ecaaf7575e254e0e8cdf18
Sagang Wee <9004726+sagangwee@users.noreply.github.com>
2021-07-10 02:25:48 -0400

    Test coverage

--- core/controllers/contributor_dashboard_admin_test.py
+++ core/controllers/contributor_dashboard_admin_test.py
@@ -21,6 +21,8 @@ import datetime
 
 from constants import constants
 from core.domain import user_services
+from core.domain import topic_domain
+from core.domain import topic_services
 from core.platform import models
 from core.tests import test_utils
 import feconf
@@ -533,14 +535,31 @@ class TranslationContributionStatsHandlerTest(test_utils.GenericTestBase):
     def setUp(self):
         super(TranslationContributionStatsHandlerTest, self).setUp()
         self.signup(self.ADMIN_EMAIL, self.ADMIN_USERNAME)
-        self.login(self.ADMIN_EMAIL, is_super_admin=True)
         self.signup(self.CONTRIBUTOR_EMAIL, self.CONTRIBUTOR_USERNAME)
+        self.admin_id = self.get_user_id_from_email(self.ADMIN_EMAIL)
+        self.contributor_id = self.get_user_id_from_email(
+            self.CONTRIBUTOR_EMAIL)
+        self.login(self.ADMIN_EMAIL, is_super_admin=True)
+        self.set_admins([self.ADMIN_USERNAME])
         user_services.update_user_role(
-            self.get_user_id_from_email(self.ADMIN_EMAIL),
-            feconf.ROLE_ID_TRANSLATION_ADMIN)
+            self.contributor_id, feconf.ROLE_ID_TRANSLATION_ADMIN)
+
+    def _publish_topic(self, topic_id, topic_name):
+        topic = topic_domain.Topic.create_default_topic(
+            topic_id, topic_name, 'abbrev', 'description')
+        topic.thumbnail_filename = 'thumbnail.svg'
+        topic.thumbnail_bg_color = '#C6DCDA'
+        topic.subtopics = [
+            topic_domain.Subtopic(
+                1, 'Title', ['skill_id_3'], 'image.svg',
+                constants.ALLOWED_THUMBNAIL_BG_COLORS['subtopic'][0], 21131,
+                'dummy-subtopic-three')]
+        topic.next_subtopic_id = 2
+        topic_services.save_new_topic(self.admin_id, topic)
+        topic_services.publish_topic(topic_id, self.admin_id)
 
     def test_get_stats_without_username_raises_error(self):
-        self.login(self.ADMIN_EMAIL, is_super_admin=True)
+        self.login(self.CONTRIBUTOR_EMAIL)
 
         response = self.get_json(
             '/translationcontributionstatshandler', params={},
@@ -551,7 +570,7 @@ class TranslationContributionStatsHandlerTest(test_utils.GenericTestBase):
             'Missing key in handler args: username.')
 
     def test_get_stats_with_invalid_username_raises_error(self):
-        self.login(self.ADMIN_EMAIL, is_super_admin=True)
+        self.login(self.CONTRIBUTOR_EMAIL)
 
         response = self.get_json(
             '/translationcontributionstatshandler', params={
@@ -562,7 +581,12 @@ class TranslationContributionStatsHandlerTest(test_utils.GenericTestBase):
             response['error'], 'Invalid username: invalid')
 
     def test_get_stats_returns_transformed_stats(self):
-        self.login(self.ADMIN_EMAIL, is_super_admin=True)
+        self.login(self.CONTRIBUTOR_EMAIL)
+        # Create and publish a topic.
+        published_topic_id = 'published_topic_id'
+        published_topic_name = 'published_topic_name'
+        self._publish_topic(published_topic_id, published_topic_name)
+
         submitted_translations_count = 2
         submitted_translation_word_count = 100
         accepted_translations_count = 1
@@ -570,10 +594,29 @@ class TranslationContributionStatsHandlerTest(test_utils.GenericTestBase):
         accepted_translation_word_count = 50
         rejected_translations_count = 0
         rejected_translation_word_count = 0
+        # Create two translation contribution stats models, one with a linked
+        # topic, one without.
         suggestion_models.TranslationContributionStatsModel.create(
             language_code='es',
-            contributor_user_id=self.get_user_id_from_email(
-                self.CONTRIBUTOR_EMAIL),
+            contributor_user_id=self.contributor_id,
+            topic_id=published_topic_id,
+            submitted_translations_count=submitted_translations_count,
+            submitted_translation_word_count=submitted_translation_word_count,
+            accepted_translations_count=accepted_translations_count,
+            accepted_translations_without_reviewer_edits_count=(
+                accepted_translations_without_reviewer_edits_count),
+            accepted_translation_word_count=accepted_translation_word_count,
+            rejected_translations_count=rejected_translations_count,
+            rejected_translation_word_count=rejected_translation_word_count,
+            contribution_dates=[
+                # Timestamp dates in sec since epoch for Mar 19 2021 UTC.
+                datetime.date.fromtimestamp(1616173836),
+                datetime.date.fromtimestamp(1616173837)
+            ]
+        )
+        suggestion_models.TranslationContributionStatsModel.create(
+            language_code='es',
+            contributor_user_id=self.contributor_id,
             topic_id='topic_id',
             submitted_translations_count=submitted_translations_count,
             submitted_translation_word_count=submitted_translation_word_count,
@@ -595,22 +638,43 @@ class TranslationContributionStatsHandlerTest(test_utils.GenericTestBase):
                 'username': self.CONTRIBUTOR_USERNAME,
             })
 
+        # There should be two results, one for each topic.
         expected_response = {
-            'translation_contribution_stats': [{
-                'language': 'Spanish',
-                'topic_name': 'UNKNOWN',
-                'submitted_translations_count': submitted_translations_count,
-                'submitted_translation_word_count': (
-                    submitted_translation_word_count),
-                'accepted_translations_count': accepted_translations_count,
-                'accepted_translations_without_reviewer_edits_count': (
-                    accepted_translations_without_reviewer_edits_count),
-                'accepted_translation_word_count': (
-                    accepted_translation_word_count),
-                'rejected_translations_count': rejected_translations_count,
-                'rejected_translation_word_count': (
-                    rejected_translation_word_count),
-                'contribution_months': ['Mar 2021']
-            }]
+            'translation_contribution_stats': [
+                {
+                    'language': 'Spanish',
+                    'topic_name': published_topic_name,
+                    'submitted_translations_count': (
+                        submitted_translations_count),
+                    'submitted_translation_word_count': (
+                        submitted_translation_word_count),
+                    'accepted_translations_count': accepted_translations_count,
+                    'accepted_translations_without_reviewer_edits_count': (
+                        accepted_translations_without_reviewer_edits_count),
+                    'accepted_translation_word_count': (
+                        accepted_translation_word_count),
+                    'rejected_translations_count': rejected_translations_count,
+                    'rejected_translation_word_count': (
+                        rejected_translation_word_count),
+                    'contribution_months': ['Mar 2021']
+                },
+                {
+                    'language': 'Spanish',
+                    'topic_name': 'UNKNOWN',
+                    'submitted_translations_count': (
+                        submitted_translations_count),
+                    'submitted_translation_word_count': (
+                        submitted_translation_word_count),
+                    'accepted_translations_count': accepted_translations_count,
+                    'accepted_translations_without_reviewer_edits_count': (
+                        accepted_translations_without_reviewer_edits_count),
+                    'accepted_translation_word_count': (
+                        accepted_translation_word_count),
+                    'rejected_translations_count': rejected_translations_count,
+                    'rejected_translation_word_count': (
+                        rejected_translation_word_count),
+                    'contribution_months': ['Mar 2021']
+                }
+            ]
         }
         self.assertEqual(response, expected_response)
