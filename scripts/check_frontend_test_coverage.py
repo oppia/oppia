a9b618487faf34d1a898e2e5d941a78ac8d6be82
Aishwary Saxena <aishwary.saxena.min19@itbhu.ac.in>
2021-07-13 20:38:03 +0530

    Added tests

--- scripts/check_frontend_test_coverage.py
+++ scripts/check_frontend_test_coverage.py
@@ -236,7 +236,6 @@ NOT_FULLY_COVERED_FILENAMES = [
     'unit-test-utils.ajs.ts', # Please don't try to cover this file.
     'url-interpolation.service.ts',
     'utils.service.ts',
-    'version-diff-visualization.component.ts',
     'voiceover-recording.service.ts',
     'worked-example-editor.directive.ts',
 ]
