f1abaabcb0c1e1d6e88518233b04e0320abe18dd
anandwana001 <akshaynandwana001@gmail.com>
2021-07-15 16:45:07 +0530

    added exp proto

--- core/domain/exp_domain.py
+++ core/domain/exp_domain.py
@@ -650,11 +650,10 @@ class Exploration(python_utils.OBJECT):
 
         # Calculate proto_size_in_bytes.
         proto_size_in_bytes = 0
-        Parse(json.dumps({
-            'name': 'Akshya',
-            'id': 1,
-            'email': 'gmail',
-        }), Person.Person())
+        exploration_yaml = exploration.to_dict()
+        exploration_json = json.dumps(exploration_yaml).encode('utf-8')
+        proto_message = Parse(json.dumps(exploration_json), Person.Exploration())
+        print('Size of proto title % is %' % exploration.title % proto_message.ByteSize())
         exploration.update_proto_size_in_bytes(proto_size_in_bytes)
         return exploration
 
