7e8a1f64e36497eb7bfec984b3033de7cc907341
Radesh-kumar <imradesh@gmail.com>
2021-07-16 23:50:14 +0530

    nits

--- scripts/check_frontend_test_coverage.py
+++ scripts/check_frontend_test_coverage.py
@@ -61,7 +61,6 @@ NOT_FULLY_COVERED_FILENAMES = [
     'collection-player-page.directive.ts',
     'collection.model.ts',
     'concept-card.directive.ts',
-    'ConceptCardObjectFactory.ts',
     'contribution-and-review.service.ts',
     'contributions-and-review.component.ts',
     'contributor-dashboard-admin-page.component.ts',
