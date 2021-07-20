dfe10b2d585f55b523c4704acf05985382ca04da
Md Shahbaz Alam <shahbazalam75508@gmail.com>
2021-07-20 18:02:08 +0530

    Fix

--- core/tests/protractor_desktop/embedding.js
+++ core/tests/protractor_desktop/embedding.js
@@ -27,6 +27,16 @@ var ExplorationPlayerPage =
 var workflow = require('../protractor_utils/workflow.js');
 
 describe('Embedding', function() {
+  // These errors are to be ignored as 'idToBeReplaced' is not a valid
+  // exploration id. It appears just after the page loads.
+  var firstErrorToIgnore = 'http:\/\/localhost:9001\/assets\/' +
+    'scripts\/embedding_tests_dev_i18n_0.0.1.html - Refused to display ' +
+    '\'http:\/\/localhost:9001\/explore\/idToBeReplaced\\?iframed=true&' +
+    'locale=en#version=0.0.1&secret=';
+  var secondErrorToIgnore = 'http:\/\/localhost:9001\/assets\/' +
+    'scripts\/embedding_tests_dev_i18n_0.0.2.html - Refused to display ' +
+    '\'http:\/\/localhost:9001\/explore\/idToBeReplaced\\?iframed=true&' +
+    'locale=en#version=0.0.2&secret=';
   var explorationEditorPage = null;
   var explorationEditorMainTab = null;
   var explorationEditorSettingsTab = null;
@@ -240,19 +250,8 @@ describe('Embedding', function() {
 
     await users.logout();
     
-    // This error is to be ignored as 'idToBeReplaced' is not a valid
-      // exploration id. It appears just after the page loads.
-    var errorToIgnore = [
-      'http:\/\/localhost:9001\/assets\/' +
-      'scripts\/embedding_tests_dev_i18n_0.0.1.html - Refused to display ' +
-      '\'http:\/\/localhost:9001\/explore\/idToBeReplaced\\?iframed=true&' +
-      'locale=en#version=0.0.1&secret=',
-      'http:\/\/localhost:9001\/assets\/' +
-      'scripts\/embedding_tests_dev_i18n_0.0.2.html - Refused to display ' +
-      '\'http:\/\/localhost:9001\/explore\/idToBeReplaced\\?iframed=true&' +
-      'locale=en#version=0.0.1&secret='
-    ];
-    await general.checkForConsoleErrors([errorToIgnore]);
+    await general.checkForConsoleErrors(
+      [firstErrorToIgnore, secondErrorToIgnore]);
   });
 
   it('should use the exploration language as site language.',
@@ -345,18 +344,7 @@ describe('Embedding', function() {
 
       await users.logout();
 
-      // This error is to be ignored as 'idToBeReplaced' is not a valid
-      // exploration id. It appears just after the page loads.
-      var errorToIgnore = [
-        'http:\/\/localhost:9001\/assets\/' +
-        'scripts\/embedding_tests_dev_i18n_0.0.1.html - Refused to display ' +
-        '\'http:\/\/localhost:9001\/explore\/idToBeReplaced\\?iframed=true&' +
-        'locale=en#version=0.0.1&secret=',
-        'http:\/\/localhost:9001\/assets\/' +
-        'scripts\/embedding_tests_dev_i18n_0.0.2.html - Refused to display ' +
-        '\'http:\/\/localhost:9001\/explore\/idToBeReplaced\\?iframed=true&' +
-        'locale=en#version=0.0.1&secret='
-      ];
-      await general.checkForConsoleErrors([errorToIgnore]);
+      await general.checkForConsoleErrors(
+        [firstErrorToIgnore,secondErrorToIgnore]);
     });
 });
