e8399cf298345387e16713e7cb44d7f2a672e530
ashutoshc8101 <ashutoshc8101@gmail.com>
2021-07-21 13:07:14 +0530

    nit

--- core/templates/pages/landing-pages/stewards-landing-page/stewards-landing-page.module.ts
+++ core/templates/pages/landing-pages/stewards-landing-page/stewards-landing-page.module.ts
@@ -22,8 +22,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RequestInterceptor } from 'services/request-interceptor.service';
 import { SharedComponentsModule } from 'components/shared-component.module';
-import { OppiaAngularRootComponent } from
-  'components/oppia-angular-root.component';
 import { platformFeatureInitFactory, PlatformFeatureService } from
   'services/platform-feature.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
