897607153e2f89776e65bdc34affa97e38843e13
ashutoshc8101 <ashutoshc8101@gmail.com>
2021-07-21 19:01:58 +0530

    nit

--- core/controllers/base.py
+++ core/controllers/base.py
@@ -573,8 +573,8 @@ class BaseHandler(webapp2.RequestHandler):
                 self.render_template('maintenance-page.mainpage.html')
             elif values['status_code'] == 404:
                 # Only 404 routes can be handled with angular router as it only
-                # has access to the path not to the status code. As a result,
-                # only 404 error page can be migrated to angular router.
+                # has access to the path, not to the status code.
+                # That's why 404 status code is treated differently.
                 self.render_template('oppia-root.mainpage.html')
             else:
                 self.render_template(
