598b0cd6b7d480d15b244478f6c6e3927a945d0d
Kevin Thomas <kevintab@tutanota.com>
2021-07-23 06:43:05 +0530

    remove svgdiagram from the rte toolbar

--- assets/rich_text_components_definitions.ts
+++ assets/rich_text_components_definitions.ts
@@ -165,39 +165,6 @@ export default {
       "default_value": ""
     }]
   },
-  "Svgdiagram": {
-    "backend_id": "svgdiagram",
-    "category": "Basic Input",
-    "description": "A custom SVG diagram.",
-    "frontend_id": "svgdiagram",
-    "tooltip": "Insert diagram",
-    "icon_data_url": "/rich_text_components/Svgdiagram/Svgdiagram.png",
-    "is_complex": false,
-    "requires_fs": true,
-    "is_block_element": true,
-    "customization_arg_specs": [{
-      "name": "svg_filename",
-      "description": "The SVG Diagram Editor",
-      "schema": {
-        "type": "custom",
-        "obj_type": "SvgFilename"
-      },
-      "default_value": ""
-    }, {
-      "name": "alt",
-      "description": "Briefly explain this diagram to a visually impaired learner",
-      "schema": {
-        "type": "unicode",
-        "validators": [{
-          "id": "is_nonempty"
-        }],
-        "ui_config": {
-          "placeholder": "Description of the diagram"
-        }
-      },
-      "default_value": ""
-    }]
-  },
   "Tabs": {
     "backend_id": "Tabs",
     "category": "Basic Input",
