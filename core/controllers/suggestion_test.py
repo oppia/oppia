2393d8c24941a4e2084f2ea43ec196311fc1c1c8
Hasitha Kaushan <hkraclh@gmail.com>
2021-08-02 17:24:32 +0530

    Fix lint

--- core/controllers/suggestion_test.py
+++ core/controllers/suggestion_test.py
@@ -1023,9 +1023,9 @@ class SuggestionUnitTests(test_utils.GenericTestBase):
             suggestion.suggestion_id), {
                 'question_state_data': question_state_data,
                 'skill_difficulty': 0.6
-            }, csrf_token=csrf_token, upload_files=((
-                'file.svg', 'file.svg', large_image),),
-                expected_status_int=400)
+            }, csrf_token=csrf_token, upload_files=(
+                ('file.svg', 'file.svg', large_image),),
+            expected_status_int=400)
 
         self.assertIn(
             'Image exceeds file size limit of 100 KB.',
