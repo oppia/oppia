efefe34b1548c1c3e9e9395aa28902ec3b14b895
Nik-09 <nikhil.agarwal.2019@gmail.com>
2021-08-03 12:15:44 +0530

    updates schema for exp_id

--- core/controllers/improvements.py
+++ core/controllers/improvements.py
@@ -117,7 +117,7 @@ class ExplorationImprovementsHistoryHandler(base.BaseHandler):
                 'type': 'basestring',
                 'validators': [{
                     'id': 'is_regex_matched',
-                    'regex_pattern': r'^[a-zA-Z0-9+/]*$'
+                    'regex_pattern': r'^[a-zA-Z0-9+/]+$'
                 }]
             }
         }
@@ -159,7 +159,7 @@ class ExplorationImprovementsConfigHandler(base.BaseHandler):
                 'type': 'basestring',
                 'validators': [{
                     'id': 'is_regex_matched',
-                    'regex_pattern': r'^[a-zA-Z0-9+/]*$'
+                    'regex_pattern': r'^[a-zA-Z0-9+/]+$'
                 }]
             }
         }
