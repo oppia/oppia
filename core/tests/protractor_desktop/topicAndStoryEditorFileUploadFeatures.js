d710588a489297e901a566dd58394a2ded74a2eb
Krishna Rama Rao <krishnarao.2208@gmail.com>
2021-08-03 20:16:43 +0400

    removing require waitFor

--- core/tests/protractor_desktop/topicAndStoryEditorFileUploadFeatures.js
+++ core/tests/protractor_desktop/topicAndStoryEditorFileUploadFeatures.js
@@ -20,7 +20,6 @@
 var forms = require('../protractor_utils/forms.js');
 var general = require('../protractor_utils/general.js');
 var users = require('../protractor_utils/users.js');
-var waitFor = require('../protractor_utils/waitFor.js');
 var workflow = require('../protractor_utils/workflow.js');
 
 var TopicsAndSkillsDashboardPage =
