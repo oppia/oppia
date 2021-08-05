bddbb8c7df962eaa98c62fd5938e6434f2937130
Eric Lou <ericzlou@gmail.com>
2021-08-04 21:23:26 -0400

    importAllAngularServices

--- core/templates/components/forms/validators/has-length-at-least.filter.spec.ts
+++ core/templates/components/forms/validators/has-length-at-least.filter.spec.ts
@@ -17,10 +17,7 @@
  * least args.
  */
 
-// TODO(#7222): Remove the following block of unnnecessary imports once
-// the code corresponding to the spec is upgraded to Angular 8.
-import { UpgradedServices } from 'services/UpgradedServices';
-// ^^^ This block is to be removed.
+import { importAllAngularServices } from 'tests/unit-test-utils.ajs';
 
 require('components/forms/validators/has-length-at-least.filter.ts');
 
@@ -28,13 +25,7 @@ describe('Normalizer tests', function() {
   var filterName = 'hasLengthAtLeast';
 
   beforeEach(angular.mock.module('oppia'));
-
-  beforeEach(angular.mock.module('oppia', function($provide) {
-    var ugs = new UpgradedServices();
-    for (let [key, value] of Object.entries(ugs.getUpgradedServices())) {
-      $provide.value(key, value);
-    }
-  }));
+  importAllAngularServices();
 
   it('should have the relevant filters', angular.mock.inject(function($filter) {
     expect($filter(filterName)).not.toEqual(null);
