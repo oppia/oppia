c553c37b6314ad273367b9f1636a5eaa29a032bb
Kevin Thomas <kevintab@tutanota.com>
2021-08-05 21:00:52 +0530

    fix lint errors

--- core/templates/services/rte-helper-modal.controller.spec.ts
+++ core/templates/services/rte-helper-modal.controller.spec.ts
@@ -340,51 +340,51 @@ describe('Rte Helper Modal Controller', function() {
       });
     });
 
-    describe('when cancel is clicked with default customization args', () => {
-      var customizationArgSpecs = [{
-        name: 'filepath',
-        default_value: ''
-      }, {
-        name: 'caption',
-        default_value: ''
-      }, {
-        name: 'alt',
-        default_value: ''
-      }];
+  describe('when cancel is clicked with default customization args', () => {
+    var customizationArgSpecs = [{
+      name: 'filepath',
+      default_value: ''
+    }, {
+      name: 'caption',
+      default_value: ''
+    }, {
+      name: 'alt',
+      default_value: ''
+    }];
 
-      beforeEach(angular.mock.module('oppia'));
+    beforeEach(angular.mock.module('oppia'));
 
-      beforeEach(angular.mock.module('oppia', function($provide) {
-        mockExternalRteSaveEventEmitter = new EventEmitter();
-        $provide.value('ExternalRteSaveService', {
-          onExternalRteSave: mockExternalRteSaveEventEmitter
-        });
-      }));
-
-      beforeEach(angular.mock.inject(function($injector, $controller) {
-        $timeout = $injector.get('$timeout');
-        var $rootScope = $injector.get('$rootScope');
-
-        $uibModalInstance = jasmine.createSpyObj(
-          '$uibModalInstance', ['close', 'dismiss']);
-
-        $scope = $rootScope.$new();
-        $controller(
-          'RteHelperModalController', {
-            $scope: $scope,
-            $uibModalInstance: $uibModalInstance,
-            attrsCustomizationArgsDict: {
-              alt: '',
-              caption: '',
-              filepath: ''
-            },
-            customizationArgSpecs: customizationArgSpecs,
-          });
-      }));
-
-      it('should close modal and remove the tag', function() {
-        $scope.cancel();
-        expect($uibModalInstance.dismiss).toHaveBeenCalledWith(true);
+    beforeEach(angular.mock.module('oppia', function($provide) {
+      mockExternalRteSaveEventEmitter = new EventEmitter();
+      $provide.value('ExternalRteSaveService', {
+        onExternalRteSave: mockExternalRteSaveEventEmitter
       });
+    }));
+
+    beforeEach(angular.mock.inject(function($injector, $controller) {
+      $timeout = $injector.get('$timeout');
+      var $rootScope = $injector.get('$rootScope');
+
+      $uibModalInstance = jasmine.createSpyObj(
+        '$uibModalInstance', ['close', 'dismiss']);
+
+      $scope = $rootScope.$new();
+      $controller(
+        'RteHelperModalController', {
+          $scope: $scope,
+          $uibModalInstance: $uibModalInstance,
+          attrsCustomizationArgsDict: {
+            alt: '',
+            caption: '',
+            filepath: ''
+          },
+          customizationArgSpecs: customizationArgSpecs,
+        });
+    }));
+
+    it('should close modal and remove the tag', function() {
+      $scope.cancel();
+      expect($uibModalInstance.dismiss).toHaveBeenCalledWith(true);
     });
+  });
 });
