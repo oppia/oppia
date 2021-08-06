4b9c8acb7803df7a6decd1decff6313ffcccced0
Praneeth <praneethg2001@gmail.com>
2021-08-07 03:37:00 +0530

    Frontend Unit Tests(8): Add frontend tests for multiple files (#13535)
    * added tests
    
    * minor fix
    
    * change back to directive
    
    * minot nit

--- core/templates/pages/topic-editor-page/services/topic-editor-state.service.spec.ts
+++ core/templates/pages/topic-editor-page/services/topic-editor-state.service.spec.ts
@@ -24,7 +24,7 @@ import { StorySummaryBackendDict } from 'domain/story/story-summary.model';
 import { EditableTopicBackendApiService, FetchTopicResponse, UpdateTopicResponse } from 'domain/topic/editable-topic-backend-api.service';
 import { SubtopicPage, SubtopicPageBackendDict } from 'domain/topic/subtopic-page.model';
 import { TopicRightsBackendApiService } from 'domain/topic/topic-rights-backend-api.service';
-import { TopicRightsBackendDict } from 'domain/topic/topic-rights.model';
+import { TopicRights, TopicRightsBackendDict } from 'domain/topic/topic-rights.model';
 import { TopicBackendDict, TopicObjectFactory } from 'domain/topic/TopicObjectFactory';
 import { AlertsService } from 'services/alerts.service';
 import { TopicEditorStateService } from './topic-editor-state.service';
@@ -33,67 +33,37 @@ describe('Topic editor state service', () => {
   let topicEditorStateService: TopicEditorStateService;
   let mockEditableTopicBackendApiService: MockEditableTopicBackendApiService;
   let alertsService: AlertsService;
+  let undoRedoService: UndoRedoService;
   let topicObjectFactory: TopicObjectFactory;
+  let editableStoryBackendApiService: EditableStoryBackendApiService;
 
   let skillCreationIsAllowed: boolean = true;
   let skillQuestionCountDict = {};
   let groupedSkillSummaries = {
-    topic1: {
-      id: 'topic_id',
-      description: 'desc',
+    topic_name: [{
+      id: 'skillId1',
+      description: 'description1',
       language_code: 'en',
-      version: 2,
-      misconception_count: 0,
-      worked_examples_count: 0,
-      skill_model_created_on: 123,
-      skill_model_last_updated: 213
-    }
+      version: 1,
+      misconception_count: 3,
+      worked_examples_count: 3,
+      skill_model_created_on: 1593138898626.193,
+      skill_model_last_updated: 1593138898626.193
+    }],
+    others: [{
+      id: 'skillId2',
+      description: 'description2',
+      language_code: 'en',
+      version: 1,
+      misconception_count: 3,
+      worked_examples_count: 3,
+      skill_model_created_on: 1593138898626.193,
+      skill_model_last_updated: 1593138898626.193
+    }]
   };
-  let topicDict: TopicBackendDict = {
-    id: 'topic_id',
-    name: 'topic_name',
-    abbreviated_name: 'topic',
-    description: 'topic description',
-    language_code: 'en',
-    uncategorized_skill_ids: [],
-    next_subtopic_id: 2,
-    version: 1,
-    thumbnail_filename: '',
-    thumbnail_bg_color: '',
-    subtopics: [],
-    canonical_story_references: [],
-    additional_story_references: [],
-    url_fragment: 'fragment',
-    practice_tab_is_displayed: true,
-    meta_tag_content: 'content',
-    page_title_fragment_for_web: 'title_fragment'
-  };
-  let storySummaryBackendDict: StorySummaryBackendDict = {
-    id: 'id',
-    title: 'story summary title',
-    node_titles: [],
-    thumbnail_filename: 'filename',
-    thumbnail_bg_color: 'bg_color',
-    description: 'desc',
-    story_is_published: true,
-    completed_node_titles: [],
-    url_fragment: 'story_fragment',
-    all_node_dicts: []
-  };
-  let subtopicPage: SubtopicPageBackendDict = {
-    id: 'subtopic_id',
-    topic_id: 'topic_id',
-    page_contents: {
-      subtitled_html: {
-        content_id: 'content_id',
-        html: 'html'
-      },
-      recorded_voiceovers: {
-        voiceovers_mapping: {}
-      }
-    },
-    language_code: 'en'
-  };
+  let topicDict: TopicBackendDict;
+  let storySummaryBackendDict: StorySummaryBackendDict;
+  let subtopicPage: SubtopicPageBackendDict;
 
   class MockEditableTopicBackendApiService {
     fetchTopicAsync(topicId: string): Promise<FetchTopicResponse> {
@@ -125,8 +95,33 @@ describe('Topic editor state service', () => {
         Promise<UpdateTopicResponse> {
       return Promise.resolve({
         topicDict: topicDict,
-        skillIdToRubricsDict: {},
-        skillIdToDescriptionDict: {}
+        skillIdToRubricsDict: {
+          skill_id_1: [
+            {
+              difficulty: 'Easy',
+              explanations: [
+                'Explanation 1'
+              ]
+            },
+            {
+              difficulty: 'Medium',
+              explanations: [
+                'Explanation 2'
+              ]
+            },
+            {
+              difficulty: 'Hard',
+              explanations: [
+                'Explanation 3'
+              ]
+            }
+          ],
+          skill_id_2: null
+        },
+        skillIdToDescriptionDict: {
+          skill_id_1: 'Description 1',
+          skill_id_2: ''
+        }
       });
     }
 
@@ -178,13 +173,62 @@ describe('Topic editor state service', () => {
   }));
 
   beforeEach(() => {
+    undoRedoService = TestBed.inject(UndoRedoService);
     topicEditorStateService = TestBed.inject(TopicEditorStateService);
     mockEditableTopicBackendApiService = (TestBed.inject(
       EditableTopicBackendApiService) as unknown) as
       jasmine.SpyObj<MockEditableTopicBackendApiService>;
+    editableStoryBackendApiService =
+      TestBed.inject(EditableStoryBackendApiService);
     alertsService = (TestBed.inject(AlertsService) as unknown) as
       jasmine.SpyObj<AlertsService>;
     topicObjectFactory = TestBed.inject(TopicObjectFactory);
+
+    topicDict = {
+      id: 'topic_id',
+      name: 'topic_name',
+      abbreviated_name: 'topic',
+      description: 'topic description',
+      language_code: 'en',
+      uncategorized_skill_ids: [],
+      next_subtopic_id: 2,
+      version: 1,
+      thumbnail_filename: '',
+      thumbnail_bg_color: '',
+      subtopics: [],
+      canonical_story_references: [],
+      additional_story_references: [],
+      url_fragment: 'fragment',
+      practice_tab_is_displayed: true,
+      meta_tag_content: 'content',
+      page_title_fragment_for_web: 'title_fragment'
+    };
+    storySummaryBackendDict = {
+      id: 'id',
+      title: 'story summary title',
+      node_titles: [],
+      thumbnail_filename: 'filename',
+      thumbnail_bg_color: 'bg_color',
+      description: 'desc',
+      story_is_published: true,
+      completed_node_titles: [],
+      url_fragment: 'story_fragment',
+      all_node_dicts: []
+    };
+    subtopicPage = {
+      id: 'subtopic_id',
+      topic_id: 'topic_id',
+      page_contents: {
+        subtitled_html: {
+          content_id: 'content_id',
+          html: 'html'
+        },
+        recorded_voiceovers: {
+          voiceovers_mapping: {}
+        }
+      },
+      language_code: 'en'
+    };
   });
 
   it('should create', () => {
@@ -247,13 +291,24 @@ describe('Topic editor state service', () => {
       'There was an error when loading the topic.');
   }));
 
+  it('should show error when invalid topic id is provided', fakeAsync(() => {
+    spyOn(mockEditableTopicBackendApiService, 'fetchSubtopicPageAsync')
+      .and.returnValue(Promise.reject());
+    spyOn(alertsService, 'addWarning');
+    topicEditorStateService.loadSubtopicPage(null, 2);
+    tick();
+    expect(alertsService.addWarning).toHaveBeenCalledWith(
+      'There was an error when loading the topic.');
+  }));
+
   it('should set subtopic page', fakeAsync(() => {
+    subtopicPage.id = 'topic_id1234-0';
     topicEditorStateService.setSubtopicPage(
       SubtopicPage.createFromBackendDict(subtopicPage));
     expect(topicEditorStateService.getSubtopicPage()).toEqual(
       SubtopicPage.createFromBackendDict(subtopicPage));
 
-    topicEditorStateService.loadSubtopicPage('1', 2);
+    topicEditorStateService.loadSubtopicPage('topic_id1234', 0);
     tick();
     topicEditorStateService.setSubtopicPage(
       SubtopicPage.createFromBackendDict(subtopicPage));
@@ -265,6 +320,151 @@ describe('Topic editor state service', () => {
     let topic = topicObjectFactory.create(topicDict, {});
     topicEditorStateService.setTopic(topic);
     expect(topicEditorStateService.getTopic()).toEqual(topic);
+  });
+
+  it('should delete subtopic page when user deletes subtopic', fakeAsync(() => {
+    topicDict.id = 'topic_id1234';
+    let topic = topicObjectFactory.create(topicDict, {});
+    topicEditorStateService.setTopic(topic);
+    subtopicPage.id = 'topic_id1234-0';
+    topicEditorStateService.setSubtopicPage(
+      SubtopicPage.createFromBackendDict(subtopicPage));
+    tick();
+    subtopicPage.id = 'topic_id1234-1';
+    topicEditorStateService.setSubtopicPage(
+      SubtopicPage.createFromBackendDict(subtopicPage));
+    tick();
+    subtopicPage.id = 'topic_id1234-2';
+    topicEditorStateService.setSubtopicPage(
+      SubtopicPage.createFromBackendDict(subtopicPage));
+    tick();
+
+    topicEditorStateService.deleteSubtopicPage('topic_id1234', 1);
+
+    subtopicPage.id = 'topic_id1234-0';
+    let subtopic0 = SubtopicPage.createFromBackendDict(subtopicPage);
+    subtopicPage.id = 'topic_id1234-1';
+    let subtopic1 = SubtopicPage.createFromBackendDict(subtopicPage);
+    expect(topicEditorStateService.getCachedSubtopicPages()).toEqual([
+      subtopic0, subtopic1
+    ]);
+  }));
+
+  it('should save topic when user saves a topic', fakeAsync(() => {
+    spyOn(undoRedoService, 'hasChanges').and.returnValue(true);
+    spyOn(undoRedoService, 'clearChanges');
+    spyOn(undoRedoService, 'getCommittableChangeList').and.returnValue([{
+      cmd: 'delete_canonical_story',
+      story_id: 'story_id'
+    }]);
+    spyOn(mockEditableTopicBackendApiService, 'updateTopicAsync').and
+      .callThrough();
+    spyOn(editableStoryBackendApiService, 'deleteStoryAsync').and
+      .callThrough();
+    var successCallback = jasmine.createSpy('successCallback');
+    let topic = topicObjectFactory.create(topicDict, {});
+    topicEditorStateService.setTopic(topic);
+
+    topicEditorStateService.saveTopic('Commit Message', successCallback);
+    tick();
+
+    expect(mockEditableTopicBackendApiService.updateTopicAsync)
+      .toHaveBeenCalledWith(
+        'topic_id', 1, 'Commit Message', [
+          {
+            cmd: 'delete_canonical_story',
+            story_id: 'story_id'
+          }
+        ]
+      );
+    expect(successCallback).toHaveBeenCalled();
+    expect(undoRedoService.getCommittableChangeList).toHaveBeenCalled();
+    expect(editableStoryBackendApiService.deleteStoryAsync)
+      .toHaveBeenCalled();
+    expect(undoRedoService.clearChanges).toHaveBeenCalled();
+  }));
+
+  it('should warn user when there is an error saving the topic',
+    fakeAsync(() => {
+      spyOn(undoRedoService, 'hasChanges').and.returnValue(true);
+      spyOn(undoRedoService, 'getCommittableChangeList').and.returnValue([{
+        cmd: 'delete_canonical_story',
+        story_id: 'story_id'
+      }]);
+      spyOn(mockEditableTopicBackendApiService, 'updateTopicAsync').and
+        .returnValue(Promise.reject());
+      spyOn(alertsService, 'addWarning');
+      var successCallback = jasmine.createSpy('successCallback');
+      let topic = topicObjectFactory.create(topicDict, {});
+      topicEditorStateService.setTopic(topic);
+
+      topicEditorStateService.saveTopic('Commit Message', successCallback);
+      tick();
+
+      expect(mockEditableTopicBackendApiService.updateTopicAsync)
+        .toHaveBeenCalledWith(
+          'topic_id', 1, 'Commit Message', [
+            {
+              cmd: 'delete_canonical_story',
+              story_id: 'story_id'
+            }
+          ]
+        );
+      expect(successCallback).not.toHaveBeenCalled();
+      expect(alertsService.addWarning)
+        .toHaveBeenCalledWith('There was an error when saving the topic.');
+    }));
+
+  it('should not save topic when there are no changes', fakeAsync(() => {
+    spyOn(undoRedoService, 'hasChanges').and.returnValue(false);
+    spyOn(mockEditableTopicBackendApiService, 'updateTopicAsync');
+
+    let topic = topicObjectFactory.create(topicDict, {});
+    topicEditorStateService.setTopic(topic);
+
+    topicEditorStateService.saveTopic('Commit Message', () => {});
+    tick();
+
+    expect(mockEditableTopicBackendApiService.updateTopicAsync).not
+      .toHaveBeenCalled();
+  }));
+
+  it('should not save topic when topicis not initialised', fakeAsync(() => {
+    spyOn(alertsService, 'fatalWarning');
+    spyOn(mockEditableTopicBackendApiService, 'updateTopicAsync');
+
+    topicEditorStateService.saveTopic('Commit Message', () => {});
+    tick();
+
+    expect(alertsService.fatalWarning).toHaveBeenCalledWith(
+      'Cannot save a topic before one is loaded.'
+    );
+    expect(mockEditableTopicBackendApiService.updateTopicAsync).not
+      .toHaveBeenCalled();
+  }));
+
+  it('should set topic rights when called', () => {
+    let topic = topicObjectFactory.create(topicDict, {});
+    topicEditorStateService.setTopic(topic);
+    expect(topicEditorStateService.getTopicRights()).toEqual(
+      TopicRights.createFromBackendDict({
+        published: false,
+        can_publish_topic: false,
+        can_edit_topic: false
+      }));
+
+    topicEditorStateService.setTopicRights(TopicRights.createFromBackendDict({
+      published: true,
+      can_publish_topic: true,
+      can_edit_topic: true
+    }));
+
+    expect(topicEditorStateService.getTopicRights()).toEqual(
+      TopicRights.createFromBackendDict({
+        published: true,
+        can_publish_topic: true,
+        can_edit_topic: true
+      }));
   });
 
   it('should delete subtopic page', fakeAsync(() => {
