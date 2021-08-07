01c22bce95ecfeda325cc433d51b5314ecc71986
ashutoshc8101 <ashutoshc8101@gmail.com>
2021-08-08 03:04:52 +0530

    nit

--- core/controllers/acl_decorators.py
+++ core/controllers/acl_decorators.py
@@ -160,8 +160,9 @@ def does_classroom_exist(handler):
             else:
                 # As this decorator is not expected to be used with other
                 # handler types, raising an error here.
-                raise Exception('does_classroom_exist decorator is expected to '
-                                'be only used with json return type handlers')
+                raise Exception(
+                    'does_classroom_exist decorator is expected to '
+                    'be only used with json return type handlers.')
 
         return handler(self, classroom_url_fragment, **kwargs)
     test_does_classroom_exist.__wrapped__ = True
