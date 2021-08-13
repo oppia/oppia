68878ba001483d7e39965ac32de87b83559fb502
Radesh-kumar <imradesh@gmail.com>
2021-08-13 10:04:56 +0530

    more tests

--- core/templates/pages/skill-editor-page/editor-tab/skill-concept-card-editor/worked-example-editor.directive.spec.ts
+++ core/templates/pages/skill-editor-page/editor-tab/skill-concept-card-editor/worked-example-editor.directive.spec.ts
@@ -25,16 +25,16 @@ import { importAllAngularServices } from 'tests/unit-test-utils.ajs';
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { WorkedExample, WorkedExampleBackendDict, WorkedExampleObjectFactory } from 'domain/skill/WorkedExampleObjectFactory';
+import { SkillUpdateService } from 'domain/skill/skill-update.service';
 
-fdescribe('Worked example editor directive', function() {
+describe('Worked example editor directive', function() {
   let $scope = null;
   let ctrl = null;
   let $rootScope = null;
-  let $timeout = null;
   let directive = null;
   let UndoRedoService = null;
-  let $uibModal = null;
   let workedExampleObjectFactory: WorkedExampleObjectFactory = null;
+  let skillUpdateService: SkillUpdateService = null;
 
   let example1: WorkedExampleBackendDict = null;
   let workedExample1: WorkedExample = null;
@@ -50,17 +50,11 @@ fdescribe('Worked example editor directive', function() {
 
   beforeEach(angular.mock.inject(function($injector) {
     $rootScope = $injector.get('$rootScope');
-    $timeout = $injector.get('$timeout');
     $scope = $rootScope.$new();
-    $uibModal = $injector.get('$uibModal');
     UndoRedoService = $injector.get('UndoRedoService');
     directive = $injector.get('workedExampleEditorDirective')[0];
-
     workedExampleObjectFactory = $injector.get('WorkedExampleObjectFactory');
-    UndoRedoService = $injector.get('UndoRedoService');
-    UndoRedoService = $injector.get('UndoRedoService');
-    UndoRedoService = $injector.get('UndoRedoService');
-    UndoRedoService = $injector.get('UndoRedoService');
+    skillUpdateService = $injector.get('SkillUpdateService');
 
     example1 = {
       question: {
@@ -79,10 +73,86 @@ fdescribe('Worked example editor directive', function() {
       $rootScope: $scope,
       $scope: $scope
     });
+    $scope.isEditable = function() {
+      return true;
+    };
+    $scope.getIndex = function() {
+      return 2;
+    };
+    $scope.workedExample = workedExample1;
+    ctrl.$onInit();
   }));
 
   it('should set properties when initialized', function() {
-    $scope.workedExample = workedExample1;
-    ctrl.$onInit();
+    expect($scope.questionEditorIsOpen).toBe(false);
+    expect($scope.explanationEditorIsOpen).toBe(false);
+    expect($scope.WORKED_EXAMPLE_FORM_SCHEMA).toEqual({
+      type: 'html',
+      ui_config: {}
+    });
+  });
+
+  it('should open question editor when calling ' +
+    '\'openQuestionEditor\'', function() {
+    expect($scope.questionEditorIsOpen).toBe(false);
+
+    $scope.openQuestionEditor();
+
+    expect($scope.questionEditorIsOpen).toBe(true);
+  });
+
+  it('should open explanation editor when calling ' +
+    '\'openExplanationEditor\'', function() {
+    expect($scope.explanationEditorIsOpen).toBe(false);
+
+    $scope.openExplanationEditor();
+
+    expect($scope.explanationEditorIsOpen).toBe(true);
+  });
+
+  it('should close question editor when calling ' +
+    '\'cancelEditQuestion\'', function() {
+    expect($scope.questionEditorIsOpen).toBe(false);
+
+    $scope.openQuestionEditor();
+
+    expect($scope.questionEditorIsOpen).toBe(true);
+
+    $scope.cancelEditQuestion();
+
+    expect($scope.questionEditorIsOpen).toBe(false);
+  });
+
+  it('should close explanation editor when calling ' +
+    '\'cancelEditExplanation\'', function() {
+    expect($scope.explanationEditorIsOpen).toBe(false);
+
+    $scope.openExplanationEditor();
+
+    expect($scope.explanationEditorIsOpen).toBe(true);
+
+    $scope.cancelEditExplanation();
+
+    expect($scope.explanationEditorIsOpen).toBe(false);
+  });
+
+  it('should save worked example when calling ' +
+    '\'saveWorkedExample\' and in question editor', function() {
+    let skillUpdateSpy = spyOn(skillUpdateService, 'updateWorkedExample')
+      .and.returnValue(null);
+
+    $scope.saveWorkedExample(true);
+
+    expect(skillUpdateSpy).toHaveBeenCalled();
+  });
+
+  it('should save worked example when calling ' +
+    '\'saveWorkedExample\' and not in question editor', function() {
+    let skillUpdateSpy = spyOn(skillUpdateService, 'updateWorkedExample')
+      .and.returnValue(null);
+
+    $scope.saveWorkedExample(false);
+
+    expect(skillUpdateSpy).toHaveBeenCalled();
   });
 });
