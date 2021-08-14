8bdef4bdd664cbe6b750fb0dfd4de782baf5762e
EeshaArif <eeshaarif@gmail.com>
2021-08-14 06:55:30 +0500

    update comment

--- core/templates/services/user-backend-api.service.ts
+++ core/templates/services/user-backend-api.service.ts
@@ -48,9 +48,9 @@ interface NonEmailPreferencesBackendDict {
 // The following type is an intersection of EmailPreferencesBackendDict
 // and NonEmailPreferencesbackendDict and hence will have all the properties
 // of both the interfaces.
-// Note: Intersection in typescript is used differently compared to set theory,
+// Note: Intersection in TypeScript is used differently compared to set theory,
 // the latter implies that the type will include only the properties that are
-// are same in both the interfaces whereas the former includes all the
+// are the same in both the interfaces whereas the former includes all the
 // properties from both the interfaces. For more information, check
 // https://www.typescriptlang.org/docs/handbook/unions-and-intersections.html#intersection-types
 export type PreferencesBackendDict = (
