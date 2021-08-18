8b1daef640a0c18f270f14a12ed2cb82c9537573
Ashutosh Chauhan <ashutoshc8101@gmail.com>
2021-08-18 17:25:36 +0530

    Fix #13619: Remove Google Analytics require from mock-ajs.ts and introduce a common import file. (#13640)
    * j9t
    
    * introduce common imports
    
    * nit
    
    * nit

--- core/templates/pages/story-viewer-page/story-viewer-page.import.ts
+++ core/templates/pages/story-viewer-page/story-viewer-page.import.ts
@@ -16,12 +16,7 @@
  * @fileoverview Directive scripts for the story viewer page.
  */
 
-import 'core-js/es7/reflect';
-import 'zone.js';
-
-// TODO(#13080): Remove the mock-ajs.ts file after the migration is complete.
-import 'pages/mock-ajs';
-import 'Polyfills.ts';
+import 'pages/common-imports';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { StoryViewerPageModule } from './story-viewer-page.module';
 import { AppConstants } from 'app.constants';
