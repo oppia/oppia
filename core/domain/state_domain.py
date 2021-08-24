b2fc513ff9b4bc42deebff71cefa15a84f3d614d
Kevin Thomas <kevintab@tutanota.com>
2021-08-25 04:25:34 +0530

    address comment

--- core/domain/state_domain.py
+++ core/domain/state_domain.py
@@ -1618,8 +1618,10 @@ class WrittenTranslations(python_utils.OBJECT):
         """
         correctly_translated_content_ids = []
         for content_id, translations in self.translations_mapping.items():
-            if language_code in translations and not (
-                    translations[language_code].needs_update):
+            if (
+            	language_code in translations and
+            	not translations[language_code].needs_update
+            ):
                 correctly_translated_content_ids.append(content_id)
 
         return correctly_translated_content_ids
