oppia/oppia

commit 38e4b552bb6f19809a579d3280291550baf6fbc8

Added unit tests for the following files (#13645)
* added a spec

* added tests

* more tests

* more tests

* nits

* nits

* nits

* more tests

* nits

* nits

* lints

* added spec

* more test

* nits

* added spec

* more tests

* added sped

* added spec

* added spec

* added spec

* more tests

* lints

* added a spec

* more tests

* lints

* more tests

* nits

* nits

* nits

* nits

* nits

* nits

* nits

* nits

* nits

* nits

* nits

* nits

* nits

core/templates/pages/exploration-editor-page/services/change-list.service.ts
@@ -92,7 +92,13 @@ export class ChangeListService implements OnInit {
     private loaderService: LoaderService,
     private loggerService: LoggerService,
     private internetConnectivityService: InternetConnectivityService,
-  ) {
+  ) {}
+
+  ngOnInit(): void {
+    this.loaderService.onLoadingMessageChange.subscribe(
+      (message: string) => this.loadingMessage = message
+    );
+
     this.internetConnectivityService.onInternetStateChange.subscribe(
       internetAccessible => {
         if (internetAccessible && this.temporaryListOfChanges.length > 0) {
@@ -102,12 +108,6 @@ export class ChangeListService implements OnInit {
           this.temporaryListOfChanges = [];
         }
       });
-  }
-
-  ngOnInit(): void {
-    this.loaderService.onLoadingMessageChange.subscribe(
-      (message: string) => this.loadingMessage = message
-    );
   }
 
   private autosaveChangeListOnChange(explorationChangeList) {

core/templates/pages/exploration-editor-page/services/change-list.service.spec.ts
@@ -1,0 +1,380 @@
+// Copyright 2021 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Tests for Change List Service.
+ */
+
+import { async, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ChangeListService } from './change-list.service';
+import { LoaderService } from 'services/loader.service';
+import { EventEmitter } from '@angular/core';
+import { InternetConnectivityService } from 'services/internet-connectivity.service';
+import { ExplorationDataService } from './exploration-data.service';
+import { AutosaveInfoModalsService } from './autosave-info-modals.service';
+import { AlertsService } from 'services/alerts.service';
+import { WindowRef } from 'services/contextual/window-ref.service';
+
+class MockWindowRef {
+  _window = {
+    location: {
+      _pathname: '/learn/math',
+      _href: '',
+      get href() {
+        return this._href;
+      },
+      set href(val) {
+        this._href = val;
+      },
+      get pathname() {
+        return this._pathname;
+      },
+      set pathname(val) {
+        this._pathname = val;
+      },
+      reload: () => {}
+    },
+    localStorage: {
+      last_uploaded_audio_lang: 'en',
+      removeItem: (name: string) => {}
+    },
+    navigator: {
+      // Internet Connection.
+      onLine: true
+    }
+  };
+  get nativeWindow() {
+    return this._window;
+  }
+}
+
+class MockExplorationDataService1 {
+  explorationId: 0;
+  autosaveChangeListAsync(changeList, successCb, errorCb) {
+    successCb({
+      changes_are_mergeable: true,
+      is_version_of_draft_valid: false,
+    });
+  }
+  discardDraftAsync() {
+    return;
+  }
+}
+
+class MockExplorationDataService2 {
+  explorationId: 0;
+  autosaveChangeListAsync(changeList, successCb, errorCb) {
+    successCb({
+      changes_are_mergeable: false,
+      is_version_of_draft_valid: false,
+    });
+  }
+  discardDraftAsync() {
+    return;
+  }
+}
+
+class MockExplorationDataService3 {
+  explorationId: 0;
+  autosaveChangeListAsync(changeList, successCb, errorCb) {
+    errorCb();
+  }
+  discardDraftAsync() {
+    return;
+  }
+}
+
+class MockAutosaveInfoModalsService {
+  isModalOpen() {
+    return false;
+  }
+
+  showNonStrictValidationFailModal() {
+    return;
+  }
+}
+
+describe('Change List Service when changes are mergable', () => {
+  let changeListService: ChangeListService;
+  let loaderService: LoaderService;
+  let alertsService: AlertsService;
+  let mockWindowRef: MockWindowRef;
+  let internetConnectivityService: InternetConnectivityService;
+  let autosaveInfoModalsService: AutosaveInfoModalsService = null;
+
+  let alertsSpy = null;
+  let mockExplorationDataService = null;
+
+  beforeEach(async(() => {
+    mockWindowRef = new MockWindowRef();
+    mockExplorationDataService = new MockExplorationDataService1();
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        {
+          provide: ExplorationDataService,
+          useValue: mockExplorationDataService
+        },
+        {
+          provide: WindowRef,
+          useValue: mockWindowRef
+        }
+      ]
+    });
+  }));
+
+  beforeEach(() => {
+    changeListService = TestBed.inject(ChangeListService);
+    loaderService = TestBed.inject(LoaderService);
+    internetConnectivityService = TestBed.inject(InternetConnectivityService);
+    autosaveInfoModalsService = TestBed.inject(AutosaveInfoModalsService);
+    alertsService = TestBed.inject(AlertsService);
+
+    spyOn(autosaveInfoModalsService, 'showVersionMismatchModal')
+      .and.returnValue(null);
+    spyOn(autosaveInfoModalsService, 'showNonStrictValidationFailModal')
+      .and.returnValue(null);
+    alertsSpy = spyOn(alertsService, 'addWarning')
+      .and.returnValue(null);
+  });
+
+  it('should set loading message when initialized', () => {
+    let mockEventEmitter = new EventEmitter();
+    spyOnProperty(loaderService, 'onLoadingMessageChange')
+      .and.returnValue(mockEventEmitter);
+
+    changeListService.ngOnInit();
+    mockEventEmitter.emit('loadingMessage');
+
+    expect(changeListService.loadingMessage).toBe('loadingMessage');
+  });
+
+  it('should save changes after deleting a state ' +
+    'when calling \'deleteState\'', () => {
+    changeListService.changeListAddedTimeoutId = 10;
+    changeListService.explorationChangeList.length = 0;
+    let saveSpy = spyOn(
+      changeListService.autosaveInProgressEventEmitter, 'emit')
+      .and.callThrough();
+
+    changeListService.deleteState('state');
+
+    expect(saveSpy).toHaveBeenCalled();
+  });
+
+  it('should not save changes after deleting a state ' +
+    'if loading message is being shown', () => {
+    changeListService.explorationChangeList.length = 0;
+    let saveSpy = spyOn(
+      changeListService.autosaveInProgressEventEmitter, 'emit')
+      .and.callThrough();
+
+    // Setting loading message.
+    changeListService.loadingMessage = 'loadingMessage';
+    changeListService.deleteState('state');
+
+    expect(saveSpy).not.toHaveBeenCalled();
+  });
+
+  it('should not save changes after deleting a state ' +
+    'if internet is offline', () => {
+    changeListService.explorationChangeList.length = 0;
+    let saveSpy = spyOn(
+      changeListService.autosaveInProgressEventEmitter, 'emit')
+      .and.callThrough();
+    // Setting internet to offline.
+    spyOn(internetConnectivityService, 'isOnline')
+      .and.returnValue(false);
+
+    changeListService.deleteState('state');
+
+    expect(saveSpy).not.toHaveBeenCalled();
+  });
+
+  it('should discard all changes ' +
+    'when calling \'discardAllChanges\'', () => {
+    let discardSpy = spyOn(mockExplorationDataService, 'discardDraftAsync')
+      .and.callThrough();
+
+    changeListService.discardAllChanges();
+
+    expect(discardSpy).toHaveBeenCalled();
+  });
+
+  it('should show alert message if we try to edit ' +
+    'an exploration with invalid property', () => {
+    changeListService.editExplorationProperty(
+      'prop1', 'oldValue', 'newValue');
+
+    expect(alertsSpy).toHaveBeenCalledWith(
+      'Invalid exploration property: prop1');
+  });
+
+  it('should show alert message if we try to edit ' +
+    'an state with invalid property', () => {
+    changeListService.editStateProperty(
+      'stateName', 'prop1', 'oldValue', 'newValue');
+
+    expect(alertsSpy).toHaveBeenCalledWith(
+      'Invalid state property: prop1');
+  });
+
+  it('should check whether exploration locked for editing ' +
+    'when calling \'isExplorationLockedForEditing\'', () => {
+    changeListService.explorationChangeList.length = 2;
+    expect(changeListService.isExplorationLockedForEditing())
+      .toBe(true);
+
+    changeListService.explorationChangeList.length = 0;
+    expect(changeListService.isExplorationLockedForEditing())
+      .toBe(false);
+  });
+});
+
+describe('Change List Service when changes are not mergable', () => {
+  let changeListService: ChangeListService;
+  let alertsService: AlertsService;
+  let mockWindowRef: MockWindowRef;
+  let autosaveInfoModalsService: AutosaveInfoModalsService = null;
+
+  let alertsSpy = null;
+  let mockExplorationDataService = null;
+
+  beforeEach(async(() => {
+    mockWindowRef = new MockWindowRef();
+    mockExplorationDataService = new MockExplorationDataService2();
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        {
+          provide: ExplorationDataService,
+          useValue: mockExplorationDataService
+        },
+        {
+          provide: WindowRef,
+          useValue: mockWindowRef
+        }
+      ]
+    });
+  }));
+
+  beforeEach(() => {
+    changeListService = TestBed.inject(ChangeListService);
+    autosaveInfoModalsService = TestBed.inject(AutosaveInfoModalsService);
+    alertsService = TestBed.inject(AlertsService);
+
+    spyOn(autosaveInfoModalsService, 'showVersionMismatchModal')
+      .and.returnValue(null);
+    spyOn(autosaveInfoModalsService, 'showNonStrictValidationFailModal')
+      .and.returnValue(null);
+    alertsSpy = spyOn(alertsService, 'addWarning')
+      .and.returnValue(null);
+  });
+
+  it('should undo and save changes when calling \'undoLastChange\'', () => {
+    let saveSpy = spyOn(
+      changeListService.autosaveInProgressEventEmitter, 'emit')
+      .and.returnValue(null);
+    changeListService.explorationChangeList.length = 2;
+
+    changeListService.undoLastChange();
+
+    expect(saveSpy).toHaveBeenCalled();
+  });
+
+  it('should not undo changes when there are no changes', () => {
+    changeListService.explorationChangeList.length = 0;
+
+    changeListService.undoLastChange();
+
+    expect(alertsSpy).toHaveBeenCalledWith(
+      'There are no changes to undo.');
+  });
+});
+
+describe('Change List Service when internet is available', () => {
+  let changeListService: ChangeListService;
+  let alertsService: AlertsService;
+  let mockWindowRef: MockWindowRef;
+  let internetConnectivityService: InternetConnectivityService;
+  let onInternetStateChangeEventEmitter = new EventEmitter();
+
+  let alertsSpy = null;
+  let mockExplorationDataService = null;
+  let mockAutosaveInfoModalsService = null;
+
+  beforeEach(async(() => {
+    mockWindowRef = new MockWindowRef();
+    mockExplorationDataService = new MockExplorationDataService3();
+    mockAutosaveInfoModalsService = new MockAutosaveInfoModalsService();
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        {
+          provide: ExplorationDataService,
+          useValue: mockExplorationDataService
+        },
+        {
+          provide: WindowRef,
+          useValue: mockWindowRef
+        },
+        {
+          provide: AutosaveInfoModalsService,
+          useValue: mockAutosaveInfoModalsService
+        }
+      ]
+    });
+  }));
+
+  beforeEach(() => {
+    changeListService = TestBed.inject(ChangeListService);
+    internetConnectivityService = TestBed.inject(InternetConnectivityService);
+    alertsService = TestBed.inject(AlertsService);
+
+    spyOnProperty(internetConnectivityService, 'onInternetStateChange')
+      .and.returnValue(onInternetStateChangeEventEmitter);
+    alertsSpy = spyOn(alertsService, 'addWarning')
+      .and.returnValue(null);
+    changeListService.ngOnInit();
+  });
+
+  it('should undo and save changes when calling \'undoLastChange\'', () => {
+    let saveSpy = spyOn(
+      changeListService.autosaveInProgressEventEmitter, 'emit')
+      .and.returnValue(null);
+    changeListService.temporaryListOfChanges = [{
+      cmd: 'add_state',
+      state_name: 'stateName'
+    }];
+    changeListService.explorationChangeList.length = 2;
+
+    onInternetStateChangeEventEmitter.emit(true);
+    changeListService.undoLastChange();
+
+    expect(saveSpy).toHaveBeenCalled();
+  });
+
+  it('should not undo changes when there are no changes', () => {
+    changeListService.temporaryListOfChanges = [{
+      cmd: 'add_state',
+      state_name: 'stateName'
+    }];
+
+    changeListService.undoLastChange();
+
+    expect(alertsSpy).toHaveBeenCalledWith(
+      'There are no changes to undo.');
+  });
+});

core/templates/directives/focus-on.directive.spec.ts
@@ -1,0 +1,83 @@
+// Copyright 2021 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Unit tests for Focus on directive.
+ */
+
+import { Component, EventEmitter } from '@angular/core';
+import { ComponentFixture, fakeAsync, TestBed, tick, waitForAsync } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { FocusManagerService } from 'services/stateful/focus-manager.service';
+import { FocusOnDirective } from './focus-on.directive';
+
+@Component({
+  selector: 'mock-comp-a',
+  template: '<div [oppiaFocusOn]="label" class="focus-label"></div>'
+})
+class MockCompA {
+  label: 'label';
+}
+
+describe('Focus on component', () => {
+  let fixture: ComponentFixture<MockCompA>;
+  let directiveInstance: FocusOnDirective;
+  let mockEventEmitter = new EventEmitter();
+  let focusManagerService: FocusManagerService = null;
+  let focusSpy = null;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [MockCompA, FocusOnDirective]
+    }).compileComponents();
+    focusManagerService = TestBed.inject(FocusManagerService);
+    focusSpy = spyOnProperty(focusManagerService, 'onFocus')
+      .and.returnValue(mockEventEmitter);
+
+    fixture = TestBed.createComponent(MockCompA);
+    fixture.detectChanges();
+
+    const directiveEl = fixture.debugElement.query(
+      By.directive(FocusOnDirective));
+    expect(directiveEl).not.toBeNull();
+    directiveInstance = directiveEl.injector.get(FocusOnDirective);
+  }));
+
+  afterEach(() => {
+    directiveInstance.ngOnDestroy();
+  });
+
+  it('should successfully compile the given component', fakeAsync(
+    () => {
+      let el = fixture.debugElement.nativeElement
+        .getElementsByClassName('focus-label')[0].innerHtml = (
+          '<div class="focus-label"></div>');
+
+      directiveInstance.focusOn = 'label';
+      mockEventEmitter.emit('labelForClearingFocus');
+      tick();
+
+      expect(el).toEqual('<div class="focus-label"></div>');
+    }));
+
+  it('should focus on the given label', fakeAsync(
+    () => {
+      directiveInstance.focusOn = 'label';
+
+      mockEventEmitter.emit('label');
+      tick();
+
+      expect(focusSpy).toHaveBeenCalled();
+    }));
+});

core/templates/domain/question/question-update.service.spec.ts
@@ -255,4 +255,21 @@ describe('Question update service', function() {
     QuestionUndoRedoService.undoChange(sampleQuestion);
     expect(sampleQuestion.getStateData()).toEqual(oldStateData);
   });
+
+  it('should set question inapplicable skills misconception ids when ' +
+    'calling \'setQuestionInapplicableSkillMisconceptionIds\'', function() {
+    expect(sampleQuestion.getInapplicableSkillMisconceptionIds())
+      .toBe(undefined);
+
+    QuestionUpdateService.setQuestionInapplicableSkillMisconceptionIds(
+      sampleQuestion, ['id1']);
+
+    expect(sampleQuestion.getInapplicableSkillMisconceptionIds())
+      .toEqual(['id1']);
+
+    QuestionUndoRedoService.undoChange(sampleQuestion);
+
+    expect(sampleQuestion.getInapplicableSkillMisconceptionIds())
+      .toBe(undefined);
+  });
 });

core/templates/domain/state_card/state-card.model.spec.ts
@@ -32,6 +32,7 @@ import { WrittenTranslationsObjectFactory } from
   'domain/exploration/WrittenTranslationsObjectFactory';
 import { RecordedVoiceovers } from 'domain/exploration/recorded-voiceovers.model';
 import { Voiceover } from 'domain/exploration/voiceover.model';
+import { InteractionCustomizationArgs } from 'interactions/customization-args-defs';
 
 
 describe('State card object factory', () => {
@@ -200,5 +201,39 @@ describe('State card object factory', () => {
 
     _sampleCard.setLastOppiaResponse('response_3');
     expect(_sampleCard.getLastOppiaResponse()).toEqual('response_3');
+  });
+
+  it('should return null when calling \'getLastAnswer\'' +
+    'if there is no last answer', () => {
+    expect(_sampleCard.getLastAnswer()).toEqual(null);
+  });
+
+  it('should return voiceovers when calling ' +
+    '\'getVoiceovers\'', () => {
+    const expectedResults = {
+      en: new Voiceover('filename1.mp3', 100000, false, 10),
+      hi: new Voiceover('filename2.mp3', 11000, false, 0.11)
+    };
+    expect(_sampleCard.getVoiceovers()).toEqual(expectedResults);
+  });
+
+  it('should return current interaction id when calling ' +
+    '\'getInteractionId\'', () => {
+    expect(_sampleCard.getInteractionId()).toEqual('TextInput');
+  });
+
+  it('should return interaction customization arguments when calling ' +
+    '\'getInteractionCustomizationArgs\'', () => {
+    const expectedResults = {
+      rows: {
+        value: 1
+      },
+      placeholder: {
+        value: new SubtitledUnicode(
+          'Type your answer here.', '')
+      }
+    } as InteractionCustomizationArgs;
+    expect(_sampleCard.getInteractionCustomizationArgs())
+      .toEqual(expectedResults);
   });
 });

commit 37e9d26a9a268990e49dc34b99f1eaaa452ca468

Fixes regression caused by #13645 (#13721)
* fix

* nits

* nits

* nits

* nits

* nits

core/templates/pages/exploration-editor-page/services/change-list.service.ts
@@ -18,7 +18,7 @@
  */
 
 import { downgradeInjectable } from '@angular/upgrade/static';
-import { EventEmitter, OnInit, Output } from '@angular/core';
+import { EventEmitter, Output } from '@angular/core';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -34,7 +34,7 @@ import { InternetConnectivityService } from 'services/internet-connectivity.serv
 @Injectable({
   providedIn: 'root'
 })
-export class ChangeListService implements OnInit {
+export class ChangeListService {
   // Temporary buffer for changes made to the exploration.
   explorationChangeList: ExplorationChange[] = [];
   undoneChangeStack: ExplorationChange[] = [];
@@ -92,13 +92,13 @@ export class ChangeListService implements OnInit {
     private loaderService: LoaderService,
     private loggerService: LoggerService,
     private internetConnectivityService: InternetConnectivityService,
-  ) {}
-
-  ngOnInit(): void {
+  ) {
+    // We have added subscriptions in the constructor.
+    // Since, ngOnInit does not work in angular services.
+    // Ref: https://github.com/angular/angular/issues/23235.
     this.loaderService.onLoadingMessageChange.subscribe(
       (message: string) => this.loadingMessage = message
     );
-
     this.internetConnectivityService.onInternetStateChange.subscribe(
       internetAccessible => {
         if (internetAccessible && this.temporaryListOfChanges.length > 0) {

core/templates/pages/exploration-editor-page/services/change-list.service.spec.ts
@@ -108,7 +108,6 @@ class MockAutosaveInfoModalsService {
 
 describe('Change List Service when changes are mergable', () => {
   let changeListService: ChangeListService;
-  let loaderService: LoaderService;
   let alertsService: AlertsService;
   let mockWindowRef: MockWindowRef;
   let internetConnectivityService: InternetConnectivityService;
@@ -116,6 +115,7 @@ describe('Change List Service when changes are mergable', () => {
 
   let alertsSpy = null;
   let mockExplorationDataService = null;
+  let mockEventEmitter = new EventEmitter();
 
   beforeEach(async(() => {
     mockWindowRef = new MockWindowRef();
@@ -130,6 +130,12 @@ describe('Change List Service when changes are mergable', () => {
         {
           provide: WindowRef,
           useValue: mockWindowRef
+        },
+        {
+          provide: LoaderService,
+          useValue: {
+            onLoadingMessageChange: mockEventEmitter
+          }
         }
       ]
     });
@@ -137,7 +143,6 @@ describe('Change List Service when changes are mergable', () => {
 
   beforeEach(() => {
     changeListService = TestBed.inject(ChangeListService);
-    loaderService = TestBed.inject(LoaderService);
     internetConnectivityService = TestBed.inject(InternetConnectivityService);
     autosaveInfoModalsService = TestBed.inject(AutosaveInfoModalsService);
     alertsService = TestBed.inject(AlertsService);
@@ -151,11 +156,6 @@ describe('Change List Service when changes are mergable', () => {
   });
 
   it('should set loading message when initialized', () => {
-    let mockEventEmitter = new EventEmitter();
-    spyOnProperty(loaderService, 'onLoadingMessageChange')
-      .and.returnValue(mockEventEmitter);
-
-    changeListService.ngOnInit();
     mockEventEmitter.emit('loadingMessage');
 
     expect(changeListService.loadingMessage).toBe('loadingMessage');
@@ -308,7 +308,6 @@ describe('Change List Service when internet is available', () => {
   let changeListService: ChangeListService;
   let alertsService: AlertsService;
   let mockWindowRef: MockWindowRef;
-  let internetConnectivityService: InternetConnectivityService;
   let onInternetStateChangeEventEmitter = new EventEmitter();
 
   let alertsSpy = null;
@@ -333,6 +332,15 @@ describe('Change List Service when internet is available', () => {
         {
           provide: AutosaveInfoModalsService,
           useValue: mockAutosaveInfoModalsService
+        },
+        {
+          provide: InternetConnectivityService,
+          useValue: {
+            onInternetStateChange: onInternetStateChangeEventEmitter,
+            isOnline() {
+              return true;
+            }
+          }
         }
       ]
     });
@@ -340,14 +348,9 @@ describe('Change List Service when internet is available', () => {
 
   beforeEach(() => {
     changeListService = TestBed.inject(ChangeListService);
-    internetConnectivityService = TestBed.inject(InternetConnectivityService);
     alertsService = TestBed.inject(AlertsService);
-
-    spyOnProperty(internetConnectivityService, 'onInternetStateChange')
-      .and.returnValue(onInternetStateChangeEventEmitter);
     alertsSpy = spyOn(alertsService, 'addWarning')
       .and.returnValue(null);
-    changeListService.ngOnInit();
   });
 
   it('should undo and save changes when calling \'undoLastChange\'', () => {