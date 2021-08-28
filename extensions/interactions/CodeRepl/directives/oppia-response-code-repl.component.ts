e1692150174d1359571a29096e07f22343a02649
PranshuSrivastava <iampranshu24@gmail.com>
2021-08-28 17:29:44 +0530

    typescript checks

--- extensions/interactions/CodeRepl/directives/oppia-response-code-repl.component.ts
+++ extensions/interactions/CodeRepl/directives/oppia-response-code-repl.component.ts
@@ -31,9 +31,7 @@ import { FocusManagerService } from 'services/stateful/focus-manager.service';
 })
 export class ResponseCodeReplComponent implements OnInit {
   @Input('answer') answerWithValue!: string; 
-
-  
-  constructor(
+    constructor(
     private htmlEscaperService: HtmlEscaperService,
     private focusManagerService: FocusManagerService,
     private errorFocusLabel: string,
