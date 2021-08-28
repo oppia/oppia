oppia/oppia

commit 63d453f703406e32f47b3706ae900bfc346a67ae

Fix part of #8423 and Change Python2 styled type annotations to Python3 style in backend (#13681)
* fix linter

* initial fix, no mypy errors

* fix lint issues

* lint fixes

* minor change

* fix more files

* fix lint and backend tests

* minor lint fix

* minor fix

* fix text usages and other issues

* remove file from typecheck

* lint fix

* fix-backend-tests

* fix type annotation

* annotation formatting

* lint fix

* fix typo

* fix linting

* fix failing backend tests

* fix jobs/blog_validation_jobs annotations

* add lint check for Text

* lint fix

* remove unwanted pylint disables and Text

* fix linting

* fix lint

* fix schema_utils

* fix formatting

* remove unwanted pylint: disable=unused-import

* fix lint

* correct lint check for # type: ignore

* lint fixes for new lint check

* more lint fixes

* fix lint and type check

* fix type check

* fix lint and remove unwanted script change

* fix lint

* fix linting

* improve annotations part 1

* lint fixes

* improve annotations part 2

* improve annotations part 3

* lint fix

* formatting changes

* fix usage of Any

* lint fix

* address comments

* lint fix

* lint fix

* fix lint and add Any explanation

* fix linting

* add Any explanation

* fix linting

* fix mypy checks

* new files lint fix

* address comments-fix typo,add explanation

* improve comment

* change annotation

* fix isort ignore and annotations

* fix linting

* fix linting

* remove other isort ignores

* fix lint

core/storage/auth/gae_models.py
@@ -22,7 +22,7 @@ from __future__ import unicode_literals
 from core.platform import models
 import feconf
 
-from typing import Dict, List, Optional, Text, cast # isort:skip # pylint: disable=unused-import
+from typing import Dict, List, Optional, cast
 
 MYPY = False
 if MYPY: # pragma: no cover
@@ -56,16 +56,15 @@ class UserAuthDetailsModel(base_models.BaseModel):
         datastore_services.StringProperty(indexed=True, default=None))
 
     @staticmethod
-    def get_deletion_policy():
-        # type: () -> base_models.DELETION_POLICY
+    def get_deletion_policy() -> base_models.DELETION_POLICY:
         """Model contains data to delete corresponding to a user: id, gae_id,
         firebase_auth_id, and parent_user_id fields.
         """
         return base_models.DELETION_POLICY.DELETE_AT_END
 
     @staticmethod
-    def get_model_association_to_user():
-        # type: () -> base_models.MODEL_ASSOCIATION_TO_USER
+    def get_model_association_to_user(
+    ) -> base_models.MODEL_ASSOCIATION_TO_USER:
         """Currently, the model holds authentication details relevant only for
         backend. Currently the only relevant user data is the username of the
         parent.
@@ -73,8 +72,7 @@ class UserAuthDetailsModel(base_models.BaseModel):
         return base_models.MODEL_ASSOCIATION_TO_USER.ONE_INSTANCE_PER_USER
 
     @staticmethod
-    def get_field_names_for_takeout():
-        # type: () -> Dict[Text, Text]
+    def get_field_names_for_takeout() -> Dict[str, str]:
         """We do not want to export the internal user id for the parent, so we
         export the username instead.
         """
@@ -83,8 +81,7 @@ class UserAuthDetailsModel(base_models.BaseModel):
         }
 
     @classmethod
-    def get_export_policy(cls):
-        # type: () -> Dict[Text, base_models.EXPORT_POLICY]
+    def get_export_policy(cls) -> Dict[str, base_models.EXPORT_POLICY]:
         """Model doesn't contain any data directly corresponding to a user.
         Currently, the model holds authentication details relevant only for
         backend, and no exportable user data. It may contain user data in the
@@ -97,8 +94,7 @@ class UserAuthDetailsModel(base_models.BaseModel):
         })
 
     @classmethod
-    def export_data(cls, user_id):
-        # type: (Text) -> Dict[Text, Text]
+    def export_data(cls, user_id: str) -> Dict[str, str]:
         """Exports the username of the parent."""
         user_auth_model = cls.get(user_id, strict=False)
         if user_auth_model and user_auth_model.parent_user_id:
@@ -112,8 +108,7 @@ class UserAuthDetailsModel(base_models.BaseModel):
             return {}
 
     @classmethod
-    def apply_deletion_policy(cls, user_id):
-        # type: (Text) -> None
+    def apply_deletion_policy(cls, user_id: str) -> None:
         """Delete instances of UserAuthDetailsModel for the user.
 
         Args:
@@ -122,8 +117,7 @@ class UserAuthDetailsModel(base_models.BaseModel):
         cls.delete_by_id(user_id)
 
     @classmethod
-    def has_reference_to_user_id(cls, user_id):
-        # type: (Text) -> bool
+    def has_reference_to_user_id(cls, user_id: str) -> bool:
         """Check whether UserAuthDetailsModel exists for the given user.
 
         Args:
@@ -135,8 +129,11 @@ class UserAuthDetailsModel(base_models.BaseModel):
         return cls.get_by_id(user_id) is not None
 
     @classmethod
-    def get_by_auth_id(cls, provider_id, auth_id):
-        # type: (Text, Text) -> Optional[UserAuthDetailsModel]
+    def get_by_auth_id(
+            cls,
+            provider_id: str,
+            auth_id: str
+    ) -> Optional['UserAuthDetailsModel']:
         """Fetch a user entry by auth_id of a particular auth service.
 
         Args:
@@ -169,24 +166,22 @@ class UserIdentifiersModel(base_models.BaseModel):
     user_id = datastore_services.StringProperty(required=True, indexed=True)
 
     @staticmethod
-    def get_deletion_policy():
-        # type: () -> base_models.DELETION_POLICY
+    def get_deletion_policy() -> base_models.DELETION_POLICY:
         """Model contains data to delete corresponding to a user: id, and
         user_id fields.
         """
         return base_models.DELETION_POLICY.DELETE_AT_END
 
     @staticmethod
-    def get_model_association_to_user():
-        # type: () -> base_models.MODEL_ASSOCIATION_TO_USER
+    def get_model_association_to_user(
+    ) -> base_models.MODEL_ASSOCIATION_TO_USER:
         """Currently, the model holds identifiers relevant only for backend that
         should not be exported.
         """
         return base_models.MODEL_ASSOCIATION_TO_USER.NOT_CORRESPONDING_TO_USER
 
     @classmethod
-    def get_export_policy(cls):
-        # type: () -> Dict[Text, base_models.EXPORT_POLICY]
+    def get_export_policy(cls) -> Dict[str, base_models.EXPORT_POLICY]:
         """Model doesn't contain any data directly corresponding to a user.
         Currently, the model holds authentication details relevant only for
         backend, and no exportable user data. It may contain user data in the
@@ -197,8 +192,7 @@ class UserIdentifiersModel(base_models.BaseModel):
         })
 
     @classmethod
-    def apply_deletion_policy(cls, user_id):
-        # type: (Text) -> None
+    def apply_deletion_policy(cls, user_id: str) -> None:
         """Delete instances of UserIdentifiersModel for the user.
 
         Args:
@@ -208,8 +202,7 @@ class UserIdentifiersModel(base_models.BaseModel):
         datastore_services.delete_multi(keys)
 
     @classmethod
-    def has_reference_to_user_id(cls, user_id):
-        # type: (Text) -> bool
+    def has_reference_to_user_id(cls, user_id: str) -> bool:
         """Check whether UserIdentifiersModel exists for the given user.
 
         Args:
@@ -221,8 +214,7 @@ class UserIdentifiersModel(base_models.BaseModel):
         return cls.query(cls.user_id == user_id).get(keys_only=True) is not None
 
     @classmethod
-    def get_by_gae_id(cls, gae_id):
-        # type: (Text) -> Optional[UserIdentifiersModel]
+    def get_by_gae_id(cls, gae_id: str) -> Optional['UserIdentifiersModel']:
         """Fetch an entry by GAE ID.
 
         Args:
@@ -235,8 +227,7 @@ class UserIdentifiersModel(base_models.BaseModel):
         return cls.get_by_id(gae_id)
 
     @classmethod
-    def get_by_user_id(cls, user_id):
-        # type: (Text) -> Optional[UserIdentifiersModel]
+    def get_by_user_id(cls, user_id: str) -> Optional['UserIdentifiersModel']:
         """Fetch an entry by user ID.
 
         Args:
@@ -259,22 +250,20 @@ class UserIdByFirebaseAuthIdModel(base_models.BaseModel):
     user_id = datastore_services.StringProperty(required=True, indexed=True)
 
     @staticmethod
-    def get_deletion_policy():
-        # type: () -> base_models.DELETION_POLICY
+    def get_deletion_policy() -> base_models.DELETION_POLICY:
         """Model has data to delete corresponding to users: id and user_id."""
         return base_models.DELETION_POLICY.DELETE_AT_END
 
     @staticmethod
-    def get_model_association_to_user():
-        # type: () -> base_models.MODEL_ASSOCIATION_TO_USER
+    def get_model_association_to_user(
+    ) -> base_models.MODEL_ASSOCIATION_TO_USER:
         """Currently, the model holds IDs relevant only for backend that should
         not be exported.
         """
         return base_models.MODEL_ASSOCIATION_TO_USER.NOT_CORRESPONDING_TO_USER
 
     @classmethod
-    def get_export_policy(cls):
-        # type: () -> Dict[Text, base_models.EXPORT_POLICY]
+    def get_export_policy(cls) -> Dict[str, base_models.EXPORT_POLICY]:
         """Model doesn't contain any data directly corresponding to a user.
         Currently, the model holds authentication details relevant only for
         backend, and no exportable user data. It may contain user data in the
@@ -285,8 +274,7 @@ class UserIdByFirebaseAuthIdModel(base_models.BaseModel):
             **{'user_id': base_models.EXPORT_POLICY.NOT_APPLICABLE})
 
     @classmethod
-    def apply_deletion_policy(cls, user_id):
-        # type: (Text) -> None
+    def apply_deletion_policy(cls, user_id: str) -> None:
         """Delete instances of UserIdByFirebaseAuthIdModel for the user.
 
         Args:
@@ -296,8 +284,7 @@ class UserIdByFirebaseAuthIdModel(base_models.BaseModel):
         datastore_services.delete_multi(keys)
 
     @classmethod
-    def has_reference_to_user_id(cls, user_id):
-        # type: (Text) -> bool
+    def has_reference_to_user_id(cls, user_id: str) -> bool:
         """Check whether UserIdByFirebaseAuthIdModel exists for given user.
 
         Args:
@@ -310,8 +297,9 @@ class UserIdByFirebaseAuthIdModel(base_models.BaseModel):
         return cls.query(cls.user_id == user_id).get(keys_only=True) is not None
 
     @classmethod
-    def get_by_user_id(cls, user_id):
-        # type: (Text) -> Optional[UserIdByFirebaseAuthIdModel]
+    def get_by_user_id(
+            cls, user_id: str
+    ) -> Optional['UserIdByFirebaseAuthIdModel']:
         """Fetch an entry by user ID.
 
         Args:
@@ -329,19 +317,17 @@ class FirebaseSeedModel(base_models.BaseModel):
     """Dummy model used to kick-off the DestroyFirebaseAccountsOneOffJob."""
 
     @staticmethod
-    def get_deletion_policy():
-        # type: () -> base_models.DELETION_POLICY
+    def get_deletion_policy() -> base_models.DELETION_POLICY:
         """Model should never be erased."""
         return base_models.DELETION_POLICY.KEEP
 
     @staticmethod
-    def get_model_association_to_user():
-        # type: () -> base_models.MODEL_ASSOCIATION_TO_USER
+    def get_model_association_to_user(
+    ) -> base_models.MODEL_ASSOCIATION_TO_USER:
         """Model does not correspond to any users."""
         return base_models.MODEL_ASSOCIATION_TO_USER.NOT_CORRESPONDING_TO_USER
 
     @classmethod
-    def has_reference_to_user_id(cls, unused_user_id):
-        # type: (Text) -> bool
+    def has_reference_to_user_id(cls, unused_user_id: str) -> bool:
         """Model does not correspond to any users."""
         return False

core/storage/activity/gae_models.py
@@ -23,7 +23,7 @@ from core.platform import models
 import core.storage.base_model.gae_models as base_models
 import feconf
 
-from typing import Dict, Text # isort:skip # pylint: disable=unused-import
+from typing import Dict
 
 MYPY = False
 if MYPY: # pragma: no cover
@@ -44,28 +44,25 @@ class ActivityReferencesModel(base_models.BaseModel):
     activity_references = datastore_services.JsonProperty(repeated=True)
 
     @staticmethod
-    def get_deletion_policy():
-        # type: () -> base_models.DELETION_POLICY
+    def get_deletion_policy() -> base_models.DELETION_POLICY:
         """Model doesn't contain any data directly corresponding to a user."""
         return base_models.DELETION_POLICY.NOT_APPLICABLE
 
     @staticmethod
-    def get_model_association_to_user():
-        # type: () -> base_models.MODEL_ASSOCIATION_TO_USER
+    def get_model_association_to_user(
+    ) -> base_models.MODEL_ASSOCIATION_TO_USER:
         """Model does not contain user data."""
         return base_models.MODEL_ASSOCIATION_TO_USER.NOT_CORRESPONDING_TO_USER
 
     @classmethod
-    def get_export_policy(cls):
-        # type: () -> Dict[Text, base_models.EXPORT_POLICY]
+    def get_export_policy(cls) -> Dict[str, base_models.EXPORT_POLICY]:
         """Model doesn't contain any data directly corresponding to a user."""
         return dict(super(cls, cls).get_export_policy(), **{
             'activity_references': base_models.EXPORT_POLICY.NOT_APPLICABLE
         })
 
     @classmethod
-    def get_or_create(cls, list_name):
-         # type: (Text) -> ActivityReferencesModel
+    def get_or_create(cls, list_name: str) -> 'ActivityReferencesModel':
         """This creates the relevant model instance, if it does not already
         exist.
         """

core/storage/app_feedback_report/gae_models.py
@@ -24,7 +24,7 @@ import feconf
 import python_utils
 import utils
 
-from typing import Any, Dict, List, Optional, Text, cast # isort:skip # pylint: disable=unused-import
+from typing import Any, Dict, List, Optional, cast
 
 MYPY = False
 if MYPY: # pragma: no cover
@@ -144,25 +144,24 @@ class AppFeedbackReportModel(base_models.BaseModel):
     @classmethod
     def create(
             cls,
-            platform, # type: Text
-            submitted_on, # type: datetime.datetime
-            report_type, # type: Text
-            category, # type: Text
-            platform_version, # type: Text
-            android_device_country_locale_code, # type: Optional[Text]
-            android_sdk_version, # type: Optional[int]
-            android_device_model, # type: Optional[Text]
-            entry_point, # type: Text
-            entry_point_topic_id, # type: Optional[Text]
-            entry_point_story_id, # type: Optional[Text]
-            entry_point_exploration_id, # type: Optional[Text]
-            entry_point_subtopic_id, # type: Optional[Text]
-            text_language_code, # type: Text
-            audio_language_code, # type: Text
-            android_report_info, # type: Optional[Dict[Text, Any]]
-            web_report_info # type: Optional[Dict[Text, Any]]
-    ):
-        # type: (...) -> Text
+            platform: str,
+            submitted_on: datetime.datetime,
+            report_type: str,
+            category: str,
+            platform_version: str,
+            android_device_country_locale_code: Optional[str],
+            android_sdk_version: Optional[int],
+            android_device_model: Optional[str],
+            entry_point: str,
+            entry_point_topic_id: Optional[str],
+            entry_point_story_id: Optional[str],
+            entry_point_exploration_id: Optional[str],
+            entry_point_subtopic_id: Optional[str],
+            text_language_code: str,
+            audio_language_code: str,
+            android_report_info: Optional[Dict[str, Any]],
+            web_report_info: Optional[Dict[str, Any]]
+    ) -> str:
         """Creates a new AppFeedbackReportModel instance and returns its ID.
 
         Args:
@@ -234,8 +233,11 @@ class AppFeedbackReportModel(base_models.BaseModel):
         return entity_id
 
     @classmethod
-    def _generate_id(cls, platform, submitted_on_datetime):
-        # type: (Text, datetime.datetime) -> Text
+    def _generate_id(
+            cls,
+            platform: str,
+            submitted_on_datetime: datetime.datetime
+    ) -> str:
         """Generates key for the instance of AppFeedbackReportModel class in the
         required format with the arguments provided.
 
@@ -266,8 +268,7 @@ class AppFeedbackReportModel(base_models.BaseModel):
             'many collisions.')
 
     @staticmethod
-    def get_deletion_policy():
-        # type: () -> base_models.DELETION_POLICY
+    def get_deletion_policy() -> base_models.DELETION_POLICY:
         """Model stores the user ID of who has scrubbed this report for auditing
         purposes but otherwise does not contain data directly corresponding to
         the user themselves.
@@ -275,8 +276,7 @@ class AppFeedbackReportModel(base_models.BaseModel):
         return base_models.DELETION_POLICY.LOCALLY_PSEUDONYMIZE
 
     @classmethod
-    def get_export_policy(cls):
-        # type: () -> Dict[Text, base_models.EXPORT_POLICY]
+    def get_export_policy(cls) -> Dict[str, base_models.EXPORT_POLICY]:
         """Model contains data referencing user and will be exported."""
         return dict(super(cls, cls).get_export_policy(), **{
             'platform': base_models.EXPORT_POLICY.EXPORTED,
@@ -307,8 +307,7 @@ class AppFeedbackReportModel(base_models.BaseModel):
         })
 
     @classmethod
-    def export_data(cls, user_id):
-        # type: (Text) -> Dict[Text, Dict[Text, Text]]
+    def export_data(cls, user_id: str) -> Dict[str, Dict[str, str]]:
         """Exports the data from AppFeedbackReportModel into dict format for
         Takeout.
 
@@ -340,22 +339,20 @@ class AppFeedbackReportModel(base_models.BaseModel):
         return user_data
 
     @staticmethod
-    def get_model_association_to_user():
-        # type: () -> base_models.MODEL_ASSOCIATION_TO_USER
+    def get_model_association_to_user(
+    ) -> base_models.MODEL_ASSOCIATION_TO_USER:
         """Model is exported as multiple instances per user since there
         are multiple reports relevant to a user.
         """
         return base_models.MODEL_ASSOCIATION_TO_USER.MULTIPLE_INSTANCES_PER_USER
 
     @staticmethod
-    def get_lowest_supported_role():
-        # type: () -> Text
+    def get_lowest_supported_role() -> str:
         """The lowest supported role for feedback reports will be moderator."""
         return feconf.ROLE_ID_MODERATOR
 
     @classmethod
-    def has_reference_to_user_id(cls, user_id):
-        # type: (Text) -> bool
+    def has_reference_to_user_id(cls, user_id: str) -> bool:
         """Check whether AppFeedbackReportModel exists for user.
 
         Args:
@@ -399,13 +396,12 @@ class AppFeedbackReportTicketModel(base_models.BaseModel):
     @classmethod
     def create(
             cls,
-            ticket_name, # type: Text
-            github_issue_repo_name, # type: Optional[Text]
-            github_issue_number, # type: Optional[int]
-            newest_report_timestamp, # type: datetime.datetime
-            report_ids # type: List[Text]
-    ):
-        # type: (...) -> Text
+            ticket_name: str,
+            github_issue_repo_name: Optional[str],
+            github_issue_number: Optional[int],
+            newest_report_timestamp: datetime.datetime,
+            report_ids: List[str]
+    ) -> str:
         """Creates a new AppFeedbackReportTicketModel instance and returns its
         ID.
 
@@ -438,8 +434,7 @@ class AppFeedbackReportTicketModel(base_models.BaseModel):
         return ticket_id
 
     @classmethod
-    def _generate_id(cls, ticket_name):
-        # type: (Text) -> Text
+    def _generate_id(cls, ticket_name: str) -> str:
         """Generates key for the instance of AppFeedbackReportTicketModel
         class in the required format with the arguments provided.
 
@@ -470,16 +465,14 @@ class AppFeedbackReportTicketModel(base_models.BaseModel):
             'many collisions.')
 
     @staticmethod
-    def get_deletion_policy():
-        # type: () -> base_models.DELETION_POLICY
+    def get_deletion_policy() -> base_models.DELETION_POLICY:
         """Model doesn't contain any information directly corresponding to a
         user.
         """
         return base_models.DELETION_POLICY.NOT_APPLICABLE
 
     @classmethod
-    def get_export_policy(cls):
-        # type: () -> Dict[Text, base_models.EXPORT_POLICY]
+    def get_export_policy(cls) -> Dict[str, base_models.EXPORT_POLICY]:
         """Model doesn't contain any data directly corresponding to a user."""
         return dict(super(cls, cls).get_export_policy(), **{
             'ticket_name': base_models.EXPORT_POLICY.NOT_APPLICABLE,
@@ -491,14 +484,13 @@ class AppFeedbackReportTicketModel(base_models.BaseModel):
         })
 
     @staticmethod
-    def get_model_association_to_user():
-        # type: () -> base_models.MODEL_ASSOCIATION_TO_USER
+    def get_model_association_to_user(
+    ) -> base_models.MODEL_ASSOCIATION_TO_USER:
         """Model doesn't contain any data directly corresponding to a user."""
         return base_models.MODEL_ASSOCIATION_TO_USER.NOT_CORRESPONDING_TO_USER
 
     @staticmethod
-    def get_lowest_supported_role():
-        # type: () -> Text
+    def get_lowest_supported_role() -> str:
         """The lowest supported role for feedback report tickets will be
         moderator.
         """
@@ -548,13 +540,12 @@ class AppFeedbackReportStatsModel(base_models.BaseModel):
     @classmethod
     def create(
             cls,
-            platform, # type: Text
-            ticket_id, # type: Text
-            stats_tracking_date, # type: datetime.date
-            total_reports_submitted, # type: int
-            daily_param_stats # type: Dict[Text, Dict[Text, int]]
-    ):
-        # type: (...) -> Text
+            platform: str,
+            ticket_id: str,
+            stats_tracking_date: datetime.date,
+            total_reports_submitted: int,
+            daily_param_stats: Dict[str, Dict[str, int]]
+    ) -> str:
         """Creates a new AppFeedbackReportStatsModel instance and returns its
         ID.
 
@@ -586,8 +577,12 @@ class AppFeedbackReportStatsModel(base_models.BaseModel):
         return entity_id
 
     @classmethod
-    def _generate_id(cls, platform, ticket_id, stats_tracking_date):
-        # type: (Text, Text, datetime.date) -> Text
+    def _generate_id(
+            cls,
+            platform: str,
+            ticket_id: str,
+            stats_tracking_date: datetime.date
+    ) -> str:
         """Generates key for the instance of AppFeedbackReportStatsModel
         class in the required format with the arguments provided.
 
@@ -611,8 +606,9 @@ class AppFeedbackReportStatsModel(base_models.BaseModel):
             'many collisions.')
 
     @classmethod
-    def get_stats_for_ticket(cls, ticket_id):
-        # type: (Text) -> List[AppFeedbackReportStatsModel]
+    def get_stats_for_ticket(
+            cls, ticket_id: str
+    ) -> List['AppFeedbackReportStatsModel']:
         """Fetches the stats for a single ticket.
 
         Args:
@@ -627,16 +623,14 @@ class AppFeedbackReportStatsModel(base_models.BaseModel):
         return cast(List[AppFeedbackReportStatsModel], ticket_models)
 
     @staticmethod
-    def get_deletion_policy():
-        # type: () -> base_models.DELETION_POLICY
+    def get_deletion_policy() -> base_models.DELETION_POLICY:
         """Model doesn't contain any information directly corresponding to a
         user.
         """
         return base_models.DELETION_POLICY.NOT_APPLICABLE
 
     @classmethod
-    def get_export_policy(cls):
-        # type: () -> Dict[Text, base_models.EXPORT_POLICY]
+    def get_export_policy(cls) -> Dict[str, base_models.EXPORT_POLICY]:
         """Model doesn't contain any data directly corresponding to a user."""
         return dict(super(cls, cls).get_export_policy(), **{
             'ticket_id': base_models.EXPORT_POLICY.NOT_APPLICABLE,
@@ -649,14 +643,13 @@ class AppFeedbackReportStatsModel(base_models.BaseModel):
         })
 
     @staticmethod
-    def get_model_association_to_user():
-        # type: () -> base_models.MODEL_ASSOCIATION_TO_USER
+    def get_model_association_to_user(
+    ) -> base_models.MODEL_ASSOCIATION_TO_USER:
         """Model doesn't contain any data directly corresponding to a user."""
         return base_models.MODEL_ASSOCIATION_TO_USER.NOT_CORRESPONDING_TO_USER
 
     @staticmethod
-    def get_lowest_supported_role():
-        # type: () -> Text
+    def get_lowest_supported_role() -> str:
         """The lowest supported role for feedback reports stats will be
         moderator.
         """

core/storage/audit/gae_models.py
@@ -22,7 +22,7 @@ from __future__ import unicode_literals
 from core.platform import models
 import feconf
 
-from typing import Dict, Text # isort:skip # pylint: disable=unused-import
+from typing import Dict
 
 MYPY = False
 if MYPY: # pragma: no cover
@@ -57,16 +57,15 @@ class RoleQueryAuditModel(base_models.BaseModel):
     username = datastore_services.StringProperty(default=None, indexed=True)
 
     @staticmethod
-    def get_deletion_policy():
-        # type: () -> base_models.DELETION_POLICY
+    def get_deletion_policy() -> base_models.DELETION_POLICY:
         """Model contains data corresponding to a user: user_id and username
         fields, but it isn't deleted because it is needed for auditing purposes.
         """
         return base_models.DELETION_POLICY.KEEP
 
     @staticmethod
-    def get_model_association_to_user():
-        # type: () -> base_models.MODEL_ASSOCIATION_TO_USER
+    def get_model_association_to_user(
+    ) -> base_models.MODEL_ASSOCIATION_TO_USER:
         """Model contains data corresponding to a user: user_id and username
         fields, but it isn't exported because it is only used for auditing
         purposes.
@@ -74,8 +73,7 @@ class RoleQueryAuditModel(base_models.BaseModel):
         return base_models.MODEL_ASSOCIATION_TO_USER.NOT_CORRESPONDING_TO_USER
 
     @classmethod
-    def get_export_policy(cls):
-        # type: () -> Dict[Text, base_models.EXPORT_POLICY]
+    def get_export_policy(cls) -> Dict[str, base_models.EXPORT_POLICY]:
         """Model contains data corresponding to a user: user_id and username
         fields, but it isn't exported because it is only used for auditing
         purposes.
@@ -88,8 +86,7 @@ class RoleQueryAuditModel(base_models.BaseModel):
         })
 
     @classmethod
-    def has_reference_to_user_id(cls, user_id):
-        # type: (Text) -> bool
+    def has_reference_to_user_id(cls, user_id: str) -> bool:
         """Check whether RoleQueryAuditModel exists for the given user.
 
         Args:
@@ -121,8 +118,7 @@ class UsernameChangeAuditModel(base_models.BaseModel):
         datastore_services.StringProperty(required=True, indexed=True))
 
     @staticmethod
-    def get_deletion_policy():
-        # type: () -> base_models.DELETION_POLICY
+    def get_deletion_policy() -> base_models.DELETION_POLICY:
         """Model contains data corresponding to a user: committer_id,
         old_username, and new_username fields but it isn't deleted because it is
         needed for auditing purposes.
@@ -130,14 +126,13 @@ class UsernameChangeAuditModel(base_models.BaseModel):
         return base_models.DELETION_POLICY.KEEP
 
     @staticmethod
-    def get_model_association_to_user():
-        # type: () -> base_models.MODEL_ASSOCIATION_TO_USER
+    def get_model_association_to_user(
+    ) -> base_models.MODEL_ASSOCIATION_TO_USER:
         """Model does not contain user data."""
         return base_models.MODEL_ASSOCIATION_TO_USER.NOT_CORRESPONDING_TO_USER
 
     @classmethod
-    def get_export_policy(cls):
-        # type: () -> Dict[Text, base_models.EXPORT_POLICY]
+    def get_export_policy(cls) -> Dict[str, base_models.EXPORT_POLICY]:
         """Model contains data corresponding to a user: committer_id,
         old_username, new_username, but this isn't exported because this model
         is only used temporarily for username changes.
@@ -149,8 +144,7 @@ class UsernameChangeAuditModel(base_models.BaseModel):
         })
 
     @classmethod
-    def has_reference_to_user_id(cls, user_id):
-        # type: (Text) -> bool
+    def has_reference_to_user_id(cls, user_id: str) -> bool:
         """Check whether UsernameChangeAuditModel exists for the given user.
 
         Args:

commit 829b2bcebee16861a30a68ca953b3b64ab1c1472

Fix failing lint check introduced by #13681: Remove unwanted List import (#13759)

core/storage/auth/gae_models.py
@@ -22,7 +22,7 @@ from __future__ import unicode_literals
 from core.platform import models
 import feconf
 
-from typing import Dict, List, Optional, cast
+from typing import Dict, Optional, cast
 
 MYPY = False
 if MYPY: # pragma: no cover