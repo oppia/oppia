1ea005a6c4bf292f530961f5940368b338ad3a7f
Md Shahbaz Alam <shahbazalam75508@gmail.com>
2021-08-30 17:39:18 +0530

    Address

--- core/tests/protractor_utils/waitFor.js
+++ core/tests/protractor_utils/waitFor.js
@@ -207,15 +207,6 @@ var clientSideRedirection = async function(
   await browser.waitForAngularEnabled(true);
 };
 
-/**
- * Wait for count of element.all Element Locater to be greater than 0
- */
-var countToBeGreaterThanZero = async function(element, errorMessage) {
-  await browser.wait(async function() {
-    return await element.count() > 0;
-  }, DEFAULT_WAIT_TIME_MSECS, errorMessage);
-};
-
 exports.DEFAULT_WAIT_TIME_MSECS = DEFAULT_WAIT_TIME_MSECS;
 exports.alertToBePresent = alertToBePresent;
 exports.elementToBeClickable = elementToBeClickable;
@@ -235,4 +226,3 @@ exports.fadeInToComplete = fadeInToComplete;
 exports.modalPopupToAppear = modalPopupToAppear;
 exports.fileToBeDownloaded = fileToBeDownloaded;
 exports.clientSideRedirection = clientSideRedirection;
-exports.countToBeGreaterThanZero = countToBeGreaterThanZero;
