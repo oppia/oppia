fe230cb41a851ed090882acfd74b2e8f6b657db9
NischayaSharma <nischaya1703@gmail.com>
2021-08-31 23:56:39 +0530

    Minor Fixes

--- core/templates/components/forms/validators/has-length-at-most.filter.ts
+++ core/templates/components/forms/validators/has-length-at-most.filter.ts
@@ -17,9 +17,8 @@
  * least some value.
  */
 
-angular.module('oppia').filter('hasLengthAtMost', [function () {
-  return function (input, args) {
-    console.log("LEN AT MOST", input, args);
+angular.module('oppia').filter('hasLengthAtMost', [function() {
+  return function(input, args) {
     return (input.length <= args.maxValue);
   };
 }]);
