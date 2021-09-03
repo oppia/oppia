cdf3e64080d8b1cce0db9ae001c73d8e3cc3328f
Vojtěch Jelínek <vojtin.j@gmail.com>
2021-09-03 14:52:23 +0200

    Implement job indexing explorations in search (#13740)
    Co-authored-by: Brian Rodriguez <brian@brianrodri.com>
    Co-authored-by: Vojtěch Jelínek <vojtech.jelinek@hey.com>

--- jobs/registry.py
+++ jobs/registry.py
@@ -45,6 +45,7 @@ from typing import List
 # registry depends on JobMetaclass to handle the responsibility of keeping track
 # of every job.
 from jobs.batch_jobs import blog_validation_jobs # pylint: disable=unused-import  # isort: skip
+from jobs.batch_jobs import cron_jobs  # pylint: disable=unused-import  # isort: skip
 from jobs.batch_jobs import validation_jobs      # pylint: disable=unused-import  # isort: skip
 
 
