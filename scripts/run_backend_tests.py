fee81e8a3bed91b864dda95e14cff77f24e2c1d0
cs <cs.temporary@icloud.com>
2021-09-09 00:20:56 +0000

    Fix extraction of coverage value from report

--- scripts/run_backend_tests.py
+++ scripts/run_backend_tests.py
@@ -506,7 +506,7 @@ def main(args=None):
         python_utils.PRINT(report_stdout)
 
         coverage_result = re.search(
-            rb'TOTAL\s+(\d+)\s+(\d+)\s+(?P<total>\d+)%\s+', report_stdout)
+            r'TOTAL\s+(\d+)\s+(\d+)\s+(?P<total>\d+)%\s+', report_stdout)
         if (coverage_result.group('total') != '100'
                 and not parsed_args.ignore_coverage):
             raise Exception('Backend test coverage is not 100%')
