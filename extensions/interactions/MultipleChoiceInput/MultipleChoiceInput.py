94c3f04318c3fc1b756fc92ce60ac1b7bc9af7dd
Hitesh Tomar <lkbhitesh07@gmail.com>
2021-09-11 01:15:25 +0530

    updated code for backend checks to pass for MultipleChoice Validation

--- extensions/interactions/MultipleChoiceInput/MultipleChoiceInput.py
+++ extensions/interactions/MultipleChoiceInput/MultipleChoiceInput.py
@@ -66,6 +66,15 @@ class MultipleChoiceInput(base.BaseInteraction):
         'default_value': [{
             'content_id': None,
             'html': ''
+        },{
+            'content_id': None,
+            'html': ''
+        },{
+            'content_id': None,
+            'html': ''
+        },{
+            'content_id': None,
+            'html': ''
         }],
     }, {
         'name': 'showChoicesInShuffledOrder',
