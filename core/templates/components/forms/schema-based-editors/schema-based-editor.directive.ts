da771951d102b3cf68ada548a73b86557a1b28c1
Nischaya Sharma <45327263+NischayaSharma@users.noreply.github.com>
2021-09-11 01:13:43 +0530

    Fix part of #13449: Image Caption and Skill Review Text Validation Checks (#13785)
    * Added Validators for Image Caption and Skill Review text
    
    * Fixed indentation in the new filters
    
    * Minor Fixes
    
    * Minor Fix
    
    * Minor Fix: Added the python-version file
    
    * Removed the python-version file
    
    * Minor Fix: Removed from Test
    
    * Fixed Spec File
    
    * Fixed Spec File
    
    * Update assets/rich_text_components_definitions.ts
    
    Co-authored-by: Eric Lou <ericzlou@gmail.com>
    
    * Update core/templates/components/forms/validators/has-length-at-most.filter.spec.ts
    
    Co-authored-by: Eric Lou <ericzlou@gmail.com>
    
    * Update core/templates/components/forms/validators/has-length-at-most.filter.ts
    
    Co-authored-by: Eric Lou <ericzlou@gmail.com>
    
    * Update rte-helper.service.spec.ts
    
    Co-authored-by: Eric Lou <ericzlou@gmail.com>

--- core/templates/components/forms/schema-based-editors/schema-based-editor.directive.ts
+++ core/templates/components/forms/schema-based-editors/schema-based-editor.directive.ts
@@ -45,6 +45,7 @@ require(
   'components/forms/schema-based-editors/' +
   'schema-based-unicode-editor.directive.ts');
 require('components/forms/validators/has-length-at-least.filter.ts');
+require('components/forms/validators/has-length-at-most.filter.ts');
 require('components/forms/validators/is-at-least.filter.ts');
 require('components/forms/validators/is-at-most.filter.ts');
 require('components/forms/validators/is-float.filter.ts');
