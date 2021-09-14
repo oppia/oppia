a1f751d682d3ee97175e45732ee2576bf4b4d56d
Aasif Faizal <aasif.faizal@gmail.com>
2021-09-14 10:31:14 -0300

    Fixed linting issues

--- jobs/transforms/validation/opportunity_validation_test.py
+++ jobs/transforms/validation/opportunity_validation_test.py
@@ -21,6 +21,7 @@ from __future__ import unicode_literals  # pylint: disable=import-only-modules
 
 from core.tests import test_utils
 from jobs.decorators import validation_decorators
+from jobs.transforms.validation import opportunity_validation # isort:skip # pylint: disable=unused-import
 
 
 class RelationshipsOfTests(test_utils.TestBase):
