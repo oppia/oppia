444af88d4259266caf85ccb54d71ef9801ec61f2
Vojtěch Jelínek <vojtin.j@gmail.com>
2021-09-17 20:12:13 +0200

    Fix tests

--- scripts/common.py
+++ scripts/common.py
@@ -196,6 +196,10 @@ DIRS_TO_ADD_TO_SYS_PATH = [
     os.path.join(OPPIA_TOOLS_DIR, 'grpcio-%s' % GRPCIO_VERSION),
     os.path.join(OPPIA_TOOLS_DIR, 'setuptools-%s' % '36.6.0'),
     os.path.join(OPPIA_TOOLS_DIR, 'PyGithub-%s' % PYGITHUB_VERSION),
+    os.path.join(
+        OPPIA_TOOLS_DIR,
+        'typing_extensions-%s' % TYPING_EXTENSIONS_VERSION
+    ),
     CURR_DIR,
     THIRD_PARTY_PYTHON_LIBS_DIR,
 ]
