c7a4c625d13c007e6fab1da4fe1d79734c077797
Chirag Baid <chiragbaid@pop-os.localdomain>
2021-10-02 03:08:26 +0530

    Remove trailing whitespace

--- core/controllers/subscriptions.py
+++ core/controllers/subscriptions.py
@@ -17,7 +17,7 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-from core import feconf 
+from core import feconf
 from core.controllers import acl_decorators
 from core.controllers import base
 from core.domain import subscription_services
