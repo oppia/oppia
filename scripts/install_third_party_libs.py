b0a9925be2fb2d3ab299d131a4fc4154ecb04b74
ProttoyChakraborty <pchakraborty2002@gmail.com>
2021-10-03 12:55:31 +0530

    fixed module issues

--- scripts/install_third_party_libs.py
+++ scripts/install_third_party_libs.py
@@ -24,7 +24,7 @@ import shutil
 import subprocess
 import sys
 import zipfile
-import urllib
+import urllib.request as urlrequest
 
 TOOLS_DIR = os.path.join(os.pardir, 'oppia_tools')
 
@@ -129,9 +129,9 @@ def install_buf_and_protoc():
 
     common.ensure_directory_exists(BUF_DIR)
     for bin_file in buf_files:
-        urllib.request.urlretrieve('%s/%s' % (
+        urlrequest.urlretrieve('%s/%s' % (
             BUF_BASE_URL, bin_file), filename=os.path.join(BUF_DIR, bin_file))
-    urllib.request.urlretrieve('%s/%s' % (
+    urlrequest.urlretrieve('%s/%s' % (
         PROTOC_URL, protoc_file), filename=os.path.join(BUF_DIR, protoc_file))
     try:
         with zipfile.ZipFile(os.path.join(BUF_DIR, protoc_file), 'r') as zfile:
