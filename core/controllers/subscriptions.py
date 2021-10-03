e1792b86694cffc73449dfd467afffdb6dacb548
Chirag Baid <chiragbaid@pop-os.localdomain>
2021-10-03 16:37:12 +0530

    Add feconf to handle error_type -feconf stores configuratoins and constants for Oppia

--- core/controllers/subscriptions.py
+++ core/controllers/subscriptions.py
@@ -17,6 +17,7 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
+from core import feconf
 from core.controllers import acl_decorators
 from core.controllers import base
 from core.domain import subscription_services
@@ -26,6 +27,7 @@ from core.domain import user_services
 class SubscribeHandler(base.BaseHandler):
     """Handles operations relating to new subscriptions."""
 
+    POST_HANDLER_ERROR_RETURN_TYPE = feconf.HANDLER_TYPE_JSON
     @acl_decorators.can_subscribe_to_users
     def post(self):
         creator_id = user_services.get_user_id_from_username(
@@ -37,6 +39,7 @@ class SubscribeHandler(base.BaseHandler):
 class UnsubscribeHandler(base.BaseHandler):
     """Handles operations related to unsubscriptions."""
 
+    POST_HANDLER_ERROR_RETURN_TYPE = feconf.HANDLER_TYPE_JSON
     @acl_decorators.can_subscribe_to_users
     def post(self):
         creator_id = user_services.get_user_id_from_username(
