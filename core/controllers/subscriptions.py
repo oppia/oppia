62e52bdb874bdb3109021cbf41222ce9a31bfaac
Chirag Baid <chiragbaid@pop-os.localdomain>
2021-10-03 17:29:39 +0530

    Add schema subscribe handler * schema validaton for subscribe handler

--- core/controllers/subscriptions.py
+++ core/controllers/subscriptions.py
@@ -28,10 +28,24 @@ class SubscribeHandler(base.BaseHandler):
     """Handles operations relating to new subscriptions."""
 
     POST_HANDLER_ERROR_RETURN_TYPE = feconf.HANDLER_TYPE_JSON
+    URL_PATH_ARGS_SCHEMAS = {}
+    HANDLER_ARGS_SCHEMAS = {
+        'POST': {
+            'creator_username': {
+                'schema': {
+                    'type': 'basestring',
+                    'validators': [{
+                        'id': 'is_valid_username_string'
+                    }]
+                }
+            }
+        }
+    }
+
     @acl_decorators.can_subscribe_to_users
     def post(self):
         creator_id = user_services.get_user_id_from_username(
-            self.payload.get('creator_username'))
+            self.normalized_payload.get('creator_username'))
         subscription_services.subscribe_to_creator(self.user_id, creator_id)
         self.render_json(self.values)
 
