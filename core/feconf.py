43557546da4d12e58c627fb5956d0b924a1b00d1
Ruinan Liu <ruinanliu@google.com>
2021-10-13 03:07:07 +0000

    fixing linting issue

--- core/feconf.py
+++ core/feconf.py
@@ -1487,6 +1487,6 @@ ACCESS_VALIDATION_HANDLER_PREFIX = '/access_validation_handler'
 
 # Regex to check for valid format of language code.
 # Currently, we use language codes that consist of these
-# types 'en', 'kab', and 'hi-en'. We are also keeping our 
+# types 'en', 'kab', and 'hi-en'. We are also keeping our
 # language codes ISO 639 compatible.
 LANGUAGE_CODE_REGEX = '^[a-z]{2,4}([-]([a-z]{2,}))?$'
