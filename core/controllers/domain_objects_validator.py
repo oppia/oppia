be889c19f9e2c7fcf7714a6dd6acd804cc782c13
Chirag Baid <chiragbaid@pop-os.localdomain>
2021-10-14 23:54:32 +0530

    Add validate_change method

--- core/controllers/domain_objects_validator.py
+++ core/controllers/domain_objects_validator.py
@@ -22,6 +22,7 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 from core import python_utils
+from core import feconf 
 from core.constants import constants
 from core.controllers import base
 from core.domain import blog_domain
@@ -29,10 +30,24 @@ from core.domain import collection_domain
 from core.domain import config_domain
 from core.domain import exp_domain
 from core.domain import state_domain
-
+from core.domain import question_domain
 from typing import Dict, Optional, Union
 
 
+def validate_change(obj):
+    """Validates Exploration and Question domain change.
+    
+    Args:
+        obj: dict. Data that needs to be validated.
+    """
+    if(obj['cmd'] in feconf.LIST_CMD_EXPLORATION_CHANGE):
+        exp_domain.ExplorationChange(obj)
+    elif(obj['cmd'] in feconf.LIST_CMD_QUESTION_CHANGE):
+        question_domain.QuestionChange(obj)
+    else:
+        raise base.BaseHandler.InvalidInputException('%s cmd is'
+        'not allowed.'% obj['cmd'])
+ 
 def validate_exploration_change(obj):
     """Validates exploration change.
 
