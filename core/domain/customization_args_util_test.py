fc66f3ad475f4e695d7287f6f862aa38f7646f7a
PranshuSrivastava <iampranshu24@gmail.com>
2021-10-17 16:48:03 +0530

    updated the comments

--- core/domain/customization_args_util_test.py
+++ core/domain/customization_args_util_test.py
@@ -456,8 +456,8 @@ class CustomizationArgsUtilUnitTests(test_utils.GenericTestBase):
         for line in lines:
             # Checks that the customization args interfaces are being used
             # to typecast the customization args. Matches patterns
-            # 'as XCustomizationArgs' or 'as XCustomizationArgsBackendDict' where
-            # X is an interaction id.
+            # 'as XCustomizationArgs' or 'as XCustomizationArgsBackendDict'
+            # where X is an interaction id.
             # Group 1: Matches the string 'as'
             # Group 2: Matches an interaction id.
             # Group 3: Matches the string 'CustomizationArgs'.
