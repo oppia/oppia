b67b85e47f1c63d265ccf598ea22bc4c36744462
Joel Lau <29514264+JoelLau@users.noreply.github.com>
2021-10-18 20:57:24 +0800

    changed '' no null

--- core/templates/domain/topic/subtopic.model.ts
+++ core/templates/domain/topic/subtopic.model.ts
@@ -25,9 +25,9 @@ export interface SubtopicBackendDict {
   'id': number;
   'title': string;
   'skill_ids': string[];
-  'thumbnail_filename': string;
-  'thumbnail_bg_color': string;
-  'url_fragment': string;
+  'thumbnail_filename': string | null;
+  'thumbnail_bg_color': string | null;
+  'url_fragment': string | null;
 }
 
 export interface SkillIdToDescriptionMap {
@@ -39,14 +39,15 @@ export class Subtopic {
   _title: string;
   _skillSummaries: ShortSkillSummary[];
   _skillIds: string[];
-  _thumbnailFilename: string;
-  _thumbnailBgColor: string;
-  _urlFragment: string;
+  _thumbnailFilename: string | null;
+  _thumbnailBgColor: string | null;
+  _urlFragment: string | null;
   constructor(
       subtopicId: number, title: string, skillIds: string[],
       skillIdToDescriptionMap: SkillIdToDescriptionMap,
-      thumbnailFilename: string, thumbnailBgColor: string,
-      urlFragment: string) {
+      thumbnailFilename: string | null,
+      thumbnailBgColor: string | null,
+      urlFragment: string | null) {
     this._id = subtopicId;
     this._title = title;
     this._skillIds = skillIds;
@@ -81,7 +82,7 @@ export class Subtopic {
     this._title = title;
   }
 
-  getUrlFragment(): string {
+  getUrlFragment(): string | null {
     return this._urlFragment;
   }
 
@@ -163,7 +164,7 @@ export class Subtopic {
     this._thumbnailFilename = thumbnailFilename;
   }
 
-  getThumbnailFilename(): string {
+  getThumbnailFilename(): string | null {
     return this._thumbnailFilename;
   }
 
@@ -171,7 +172,7 @@ export class Subtopic {
     this._thumbnailBgColor = thumbnailBgColor;
   }
 
-  getThumbnailBgColor(): string {
+  getThumbnailBgColor(): string | null {
     return this._thumbnailBgColor;
   }
 
@@ -191,9 +192,9 @@ export class Subtopic {
       id: subtopicId,
       title: title,
       skill_ids: [],
-      thumbnail_filename: '',
-      thumbnail_bg_color: '',
-      url_fragment: ''
+      thumbnail_filename: null,
+      thumbnail_bg_color: null,
+      url_fragment: null
     }, {});
   }
 }
