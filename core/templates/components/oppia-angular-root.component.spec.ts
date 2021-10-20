fdc6eb4d0d97853d7f10bf79ff3251309fb11992
ashutoshc8101 <ashutoshc8101@gmail.com>
2021-10-20 20:22:51 +0530

    fe added

--- core/templates/components/oppia-angular-root.component.spec.ts
+++ core/templates/components/oppia-angular-root.component.spec.ts
@@ -16,13 +16,11 @@
  * @fileoverview Unit tests for the OppiaAngularRootComponent.
  */
 
-import { TranslateService } from '@ngx-translate/core';
-import { TranslateCacheService, TranslateCacheSettings } from 'ngx-translate-cache';
 import { ComponentFixture, TestBed, async } from
   '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { CookieModule, CookieService } from 'ngx-cookie';
+import { CookieModule } from 'ngx-cookie';
 import { OppiaAngularRootComponent } from './oppia-angular-root.component';
 import { I18nLanguageCodeService } from 'services/i18n-language-code.service';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
@@ -34,7 +32,6 @@ import { WindowRef } from 'services/contextual/window-ref.service';
 
 let component: OppiaAngularRootComponent;
 let fixture: ComponentFixture<OppiaAngularRootComponent>;
-const CACHE_KEY_FOR_TESTS: string = 'CACHED_LANG_KEY';
 
 class MockWindowRef {
   nativeWindow = {
@@ -52,8 +49,6 @@ class MockWindowRef {
 
 describe('OppiaAngularRootComponent', function() {
   let emitSpy: jasmine.Spy;
-  let windowRef: WindowRef;
-  let cookieService: CookieService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -65,30 +60,6 @@ describe('OppiaAngularRootComponent', function() {
         {
           provide: AngularFireAuth,
           useValue: null
-        },
-        {
-          provide: TranslateCacheService,
-          useValue: {
-            init: () => {},
-            getCachedLanguage: () => {
-              return 'en';
-            }
-          }
-        },
-        {
-          provide: TranslateService,
-          useValue: {
-            use: () => {}
-          }
-        },
-        {
-          provide: TranslateCacheSettings,
-          useValue: {
-            // We directly return name of cache in which language key code
-            // will be stored, mocking TranslateCacheSettings where a cache name
-            // is generated using injection token which comes from angular.
-            cacheName: CACHE_KEY_FOR_TESTS
-          }
         },
         {
           provide: WindowRef,
@@ -107,8 +78,6 @@ describe('OppiaAngularRootComponent', function() {
 
     fixture = TestBed.createComponent(OppiaAngularRootComponent);
     component = fixture.componentInstance;
-    windowRef = TestBed.inject(WindowRef);
-    cookieService = TestBed.inject(CookieService);
     let metaTagCustomizationService = (
       TestBed.inject(MetaTagCustomizationService));
     emitSpy = spyOn(component.initialized, 'emit');
@@ -129,49 +98,5 @@ describe('OppiaAngularRootComponent', function() {
     TestBed.inject(I18nLanguageCodeService).setI18nLanguageCode('en');
 
     expect(emitSpy).toHaveBeenCalled();
-  });
-
-  it('should set cache language according to URL lang param', () => {
-    // Setting 'en' as cache language code.
-    cookieService.put(CACHE_KEY_FOR_TESTS, 'en');
-    // This sets the url to 'http://localhost:8181/?lang=es'
-    // when initialized.
-    spyOn(windowRef.nativeWindow.location, 'toString')
-      .and.returnValue('http://localhost:8181/?lang=es');
-    expect(cookieService.get(CACHE_KEY_FOR_TESTS)).toBe('en');
-
-    component.ngAfterViewInit();
-
-    expect(cookieService.get(CACHE_KEY_FOR_TESTS)).toBe('es');
-  });
-
-  it('should remove language param from URL if it is invalid', () => {
-    cookieService.put(CACHE_KEY_FOR_TESTS, 'en');
-    spyOn(cookieService, 'put');
-    // This sets the url to 'http://localhost:8181/?lang=invalid'
-    // when initialized.
-    spyOn(windowRef.nativeWindow.location, 'toString')
-      .and.returnValue('http://localhost:8181/?lang=invalid');
-
-    component.ngAfterViewInit();
-
-    // Translation cache should not be updated as the language param
-    // is invalid.
-    expect(cookieService.put).not.toHaveBeenCalledWith();
-    expect(cookieService.get(CACHE_KEY_FOR_TESTS)).toBe('en');
-  });
-
-  it('should not update translation cache if no language param is present in' +
-  ' URL', () => {
-    cookieService.put(CACHE_KEY_FOR_TESTS, 'en');
-    // This sets the url to 'http://localhost:8181/' when initialized.
-    spyOn(windowRef.nativeWindow.location, 'toString')
-      .and.returnValue('http://localhost:8181');
-    spyOn(cookieService, 'put');
-
-    component.ngAfterViewInit();
-
-    expect(cookieService.put).not.toHaveBeenCalledWith();
-    expect(cookieService.get(CACHE_KEY_FOR_TESTS)).toBe('en');
   });
 });
