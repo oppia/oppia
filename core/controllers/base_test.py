8e39dab462a5d5674ce025a329f724a085a8063c
Pranshu Srivastava <iampranshu24@gmail.com>
2021-10-28 00:57:16 +0530

    fixed errors

--- core/controllers/base_test.py
+++ core/controllers/base_test.py
@@ -36,7 +36,7 @@ from core import utils
 from core.constants import constants
 from core.controllers import acl_decorators
 from core.controllers import base
-from core.controllers import noschema
+from core.controllers import handler_schema_constants
 from core.controllers import payload_validator
 from core.domain import auth_domain
 from core.domain import classifier_domain
@@ -1316,7 +1316,7 @@ class SchemaValidationIntegrationTests(test_utils.GenericTestBase):
     architecture.
     """
     handler_class_names_with_no_schema = (
-        noschema.HANDLER_CLASS_NAMES_WITH_NO_SCHEMA)
+        handler_schema_constants.HANDLER_CLASS_NAMES_WITH_NO_SCHEMA)
     wiki_page_link = (
         'https://github.com/oppia/oppia/wiki/Writing-schema-for-handler-args')
 
@@ -1508,7 +1508,7 @@ class SchemaValidationIntegrationTests(test_utils.GenericTestBase):
 
         list_of_handlers_to_be_removed = []
         handler_names_which_require_schemas = (
-            noschema.HANDLER_CLASS_NAMES_WHICH_STILL_NEED_SCHEMAS)
+        handler_schema_constants.HANDLER_CLASS_NAMES_WHICH_STILL_NEED_SCHEMAS)
         list_of_routes_which_need_schemas = (
             self._get_list_of_routes_which_need_schemas())
 
