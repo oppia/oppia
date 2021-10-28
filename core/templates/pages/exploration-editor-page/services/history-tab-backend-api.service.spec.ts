2e58522a497b06150d08a76e5ebaf1428c73ecb2
heyimShivam <shivamthalwal@gmail.com>
2021-10-28 21:02:51 +0530

    changes suggest

--- core/templates/pages/exploration-editor-page/services/history-tab-backend-api.service.spec.ts
+++ core/templates/pages/exploration-editor-page/services/history-tab-backend-api.service.spec.ts
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 /**
- * @fileoverview Unit tests for LearnerViewInfoBackendApiService.
+ * @fileoverview Unit tests for HistoryTabBackendApiService.
  */
 
 import { HttpClientTestingModule, HttpTestingController } from
--- core/templates/pages/exploration-editor-page/services/history-tab-backend-api.service.ts
+++ core/templates/pages/exploration-editor-page/services/history-tab-backend-api.service.ts
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 /**
- * @fileoverview Backend api service for fetching the learner data;
+ * @fileoverview Backend api service for history tab component;
  */
 
 import { downgradeInjectable } from '@angular/upgrade/static';
