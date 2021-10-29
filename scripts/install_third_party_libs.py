028823abb758b6497475a1dfcd1db5f632cadd54
anandwana001 <akshaynandwana001@gmail.com>
2021-10-29 08:01:44 +0530

    nit fix

--- scripts/install_third_party_libs.py
+++ scripts/install_third_party_libs.py
@@ -90,7 +90,8 @@ PROTOC_DIR = os.path.join(BUF_DIR, 'protoc')
 # Path of files which needs to be compiled by protobuf.
 PROTO_FILES_PATHS = [
     os.path.join(common.THIRD_PARTY_DIR, 'oppia-ml-proto-0.0.0'),
-    os.path.join(common.THIRD_PARTY_DIR, 'oppia_android_proto')]
+    os.path.join(
+        common.THIRD_PARTY_DIR, 'oppia-proto-api-introduce-proto-api-v1')]
 # Path to typescript plugin required to compile ts compatible files from proto.
 PROTOC_GEN_TS_PATH = os.path.join(common.NODE_MODULES_PATH, 'protoc-gen-ts')
 
