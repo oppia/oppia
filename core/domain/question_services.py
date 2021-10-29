b225bd9b1ebb4cbc45945e4d0ad3814fb995e11a
anandwana001 <akshaynandwana001@gmail.com>
2021-10-29 08:54:03 +0530

    nit fix

--- core/domain/question_services.py
+++ core/domain/question_services.py
@@ -561,8 +561,9 @@ def apply_change_list(question_id, change_list):
                         change.new_value)
 
         question_android_proto = question.to_proto(
-            question_id, question.question_state_data, 
-            question.question_state_data_schema_version, question.linked_skill_ids)
+            question_id, question.question_state_data,
+            question.question_state_data_schema_version,
+            question.linked_skill_ids)
         question_android_proto_size = int(question_android_proto.ByteSize())
         question.update_proto_size_in_bytes(question_android_proto_size)
 
