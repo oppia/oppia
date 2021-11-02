7f4616b11b71eefb3a2690103cc26f2b7c27ed13
Chirag Baid <chiragbaid@pop-os.localdomain>
2021-11-02 12:52:01 +0530

    Correctly sort imports

--- core/tests/test_utils.py
+++ core/tests/test_utils.py
@@ -17,8 +17,8 @@
 """Common utilities for test classes."""
 
 from __future__ import absolute_import
-from __future__ import unicode_literals
 from __future__ import annotations
+from __future__ import unicode_literals
 
 import collections
 import contextlib
