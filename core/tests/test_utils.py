e011b81835fc030237c47718fd2881a379ca7de3
Chirag Baid <chiragbaid@pop-os.localdomain>
2021-11-02 12:35:45 +0530

    Fix Line too long lint check

--- core/tests/test_utils.py
+++ core/tests/test_utils.py
@@ -2327,8 +2327,9 @@ title: Title
             if 'files' in data:
                 # Bytes type data cannot be used as the json value
                 # json cannot serialize object of type bytes.
-                data['files'] = {k: v.decode('ISO-8859-1') if isinstance(v,bytes)
-                                else v for k, v in data['files'].items()}
+                data['files'] = {k: v.decode('ISO-8859-1')
+                                if isinstance(v, bytes) else v
+                                for k, v in data['files'].items()}
             data = {'payload': json.dumps(data)}
         if csrf_token:
             data['csrf_token'] = csrf_token
