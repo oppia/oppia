b089ba8841a528474abe5ea7810c93dcc5b95304
Chirag Baid <chiragbaid@pop-os.localdomain>
2021-11-02 11:06:03 +0530

    Pass payload object to save images

--- core/controllers/suggestion.py
+++ core/controllers/suggestion.py
@@ -127,7 +127,7 @@ class SuggestionHandler(base.BaseHandler):
             return
 
         _upload_suggestion_images(
-            self.request,
+            self.normalized_payload,
             suggestion,
             suggestion.get_new_image_filenames_added_in_suggestion())
 
@@ -456,7 +456,7 @@ class UpdateQuestionSuggestionHandler(base.BaseHandler):
             )
         )
         _upload_suggestion_images(
-            self.request, updated_suggestion, new_image_filenames)
+            self.payload, updated_suggestion, new_image_filenames)
 
         self.render_json(self.values)
 
@@ -541,7 +541,7 @@ def _upload_suggestion_images(request, suggestion, filenames):
     """Saves a suggestion's images to storage.
 
     Args:
-        request: webapp2.Request. Request object containing a mapping of image
+        request: payload. payload object containing a mapping of image
             filename to image blob.
         suggestion: BaseSuggestion. The suggestion for which images are being
             uploaded.
@@ -552,7 +552,7 @@ def _upload_suggestion_images(request, suggestion, filenames):
     # created.
     images = request.get('files')
     for filename in filenames:
-        image = images.get(filename) if images else None
+        image = images.get(filename).encode('ISO-8859-1') if images else None
         if not image:
             logging.exception(
                 'Image not provided for file with name %s when the '
