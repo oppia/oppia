38c8371f0a4112ee94ada15eb7bd5a68354414cc
loic <loic.mortier@imt-atlantique.net>
2021-11-16 10:11:08 +0100

    Modif fonction

--- core/templates/pages/topic-editor-page/editor-tab/topic-editor-tab.directive.ts
+++ core/templates/pages/topic-editor-page/editor-tab/topic-editor-tab.directive.ts
@@ -323,12 +323,16 @@ angular.module('oppia').directive('topicEditorTab', [
           };
 
           $scope.updatePracticeTabIsDisplayed = function(
-              newPracticeTabIsDisplayed) {
-            if (
-              newPracticeTabIsDisplayed !==
-              $scope.topic.getPracticeTabIsDisplayed()) {
-              TopicUpdateService.setPracticeTabIsDisplayed(
-                $scope.topic, newPracticeTabIsDisplayed);
+            newPracticeTabIsDisplayed) {
+            var countQuestions = 0;
+            $scope.topic.getSkillIds().forEach(item => {
+              countQuestions += $scope.skillQuestionCountDict[item];
+            })
+          if (
+            newPracticeTabIsDisplayed !==
+            $scope.topic.getPracticeTabIsDisplayed() && countQuestions >= 10) {
+            TopicUpdateService.setPracticeTabIsDisplayed(
+              $scope.topic, newPracticeTabIsDisplayed);
             }
           };
 
