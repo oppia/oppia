2b52808431a4465a2167dbadda6a419b50f3cb37
ClementBLV <88928058+ClementBLV@users.noreply.github.com>
2021-11-24 17:09:00 +0100

    Update core/domain/platform_parameter_domain.py
    Co-authored-by: Vojtěch Jelínek <vojtech.jelinek@hey.com>

--- core/domain/platform_parameter_domain.py
+++ core/domain/platform_parameter_domain.py
@@ -603,8 +603,7 @@ class PlatformParameter:
     # Module in python.utils has no attribute BASESTRING
     DATA_TYPE_PREDICATES_DICT = {
         DATA_TYPES.bool.value: lambda x: isinstance(x, bool),
-        DATA_TYPES.string.value: (
-            lambda x: isinstance(x, python_utils.BASESTRING)), # type: ignore[attr-defined]
+        DATA_TYPES.string.value: lambda x: isinstance(x, str),
         DATA_TYPES.number.value: lambda x: isinstance(x, (float, int)),
     }
 
