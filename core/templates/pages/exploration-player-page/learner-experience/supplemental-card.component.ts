dd972716e8d8b36e7327724433525051d6fb9530
Kevin Thomas <kevintab@tutanota.com>
2021-11-23 17:24:51 -0800

    Fix #14228 and #14253: Set classroom filter choices in skills tab and fix change detection/propagation issue in the supplemental card component (#14270)
    * fix classroom filter in skills tab
    
    * fix change detection issue in supplemental card directive
    
    * add test
    
    * update test message
    
    * fix lint error

--- core/templates/pages/exploration-player-page/learner-experience/supplemental-card.component.ts
+++ core/templates/pages/exploration-player-page/learner-experience/supplemental-card.component.ts
@@ -16,7 +16,7 @@
  * @fileoverview Component for the supplemental card.
  */
 
-import { Component, Output, EventEmitter, Input, OnInit, OnDestroy, ElementRef, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { Component, Output, EventEmitter, Input, OnInit, OnDestroy, ElementRef, ViewChild, ChangeDetectorRef, SimpleChanges } from '@angular/core';
 import { downgradeComponent } from '@angular/upgrade/static';
 import { AppConstants } from 'app.constants';
 import { StateCard } from 'domain/state_card/state-card.model';
@@ -97,6 +97,13 @@ export class SupplementalCardComponent implements OnInit, OnDestroy {
       )
     );
     this.updateDisplayedCard();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.displayedCard) {
+      this.displayedCard = changes.displayedCard.currentValue;
+      this.updateDisplayedCard();
+    }
   }
 
   ngOnDestroy(): void {
