7604187e478a8ed2f21a9214e9d307d6ae8feb55
taylor <taylor@thunkable.com>
2021-11-24 16:29:08 -0500

    Fix part of #13935: Handle zip_longest without python_utils

--- core/python_utils.py
+++ core/python_utils.py
@@ -432,23 +432,3 @@ def create_enum(*sequential):
             }
             _enums[name] = type('Enum', (), _value)
         return type('Enum', (), _enums)
-
-
-def zip_longest(*args, **kwargs):
-    """Creates an iterator that aggregates elements from each of the iterables.
-    If the iterables are of uneven length, missing values are
-    filled-in with fillvalue.
-
-    Args:
-        *args: list(*). Iterables that needs to be aggregated into an iterable.
-        **kwargs: dict. It contains fillvalue.
-
-    Returns:
-        iterable(iterable). A sequence of aggregates elements
-        from each of the iterables.
-    """
-    fillvalue = kwargs.get('fillvalue')
-    try:
-        return itertools.zip_longest(*args, fillvalue=fillvalue)
-    except AttributeError:
-        return itertools.izip_longest(*args, fillvalue=fillvalue)
