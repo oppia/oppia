9509afaf94c98d90cfaad7bf297b2423cc6491e3
Nik-09 <nikhil.agarwal.2019@gmail.com>
2021-12-06 20:45:43 +0530

    Addresses review comments

--- core/domain/platform_parameter_registry.py
+++ core/domain/platform_parameter_registry.py
@@ -150,7 +150,8 @@ class Registry:
 
     @classmethod
     def update_platform_parameter(
-        cls, name, committer_id, commit_message, new_rules):
+        cls, name, committer_id, commit_message, new_rules
+    ):
         """Updates the platform parameter with new rules.
 
         Args:
