adedbd93eb07e55de0a4202a7e86e191acabd027
ShivamJhaa <20bcs206@iiitdmj.ac.in>
2021-12-09 00:06:05 +0530

    checks

--- core/templates/pages/topic-editor-page/modal-templates/create-new-story-modal.component.spec.ts
+++ core/templates/pages/topic-editor-page/modal-templates/create-new-story-modal.component.spec.ts
@@ -27,7 +27,7 @@ import { FormsModule } from '@angular/forms';
 import { TopicEditorStateService } from '../services/topic-editor-state.service';
 import { AppConstants } from 'app.constants';
 
-fdescribe('Create New Story Modal Component', function() {
+describe('Create New Story Modal Component', function() {
   let componentInstance: CreateNewStoryModalComponent;
   let fixture: ComponentFixture<CreateNewStoryModalComponent>;
   let ngbActiveModal: NgbActiveModal;
