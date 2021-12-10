e96cb3218de4030b5a47567a160a3cbc672eb929
Harshvardhan Singh <73544247+Lawful2002@users.noreply.github.com>
2021-12-10 10:49:27 -0800

    Implemented the new UI for the Learn (formerly Classroom) Tab of the navbar and fixes #13667 (#14294)
    * initial ui changes
    
    * changed classroom to learn (en)
    
    * some more changes
    
    * added multi-language support for learn tab
    
    * made navbar contents dynamic
    
    * added test for classroom data
    
    * minor changes
    
    * added webp image and minor changes
    
    * changes
    
    * corrected variable names and some ui changes
    
    * [skip ci] made dropdown smaller to fix issue with certain languages
    
    * [skip ci] made dropdown smaller to fix issue with certain languages
    
    * added dynamic shifting for nav dropdown
    
    * [skip ci] added a frontend test for getDropdownOffset
    
    * fixed some console errors
    
    * [skip ci] corrected alphabetical order
    
    * corrected call to classroomBackendAPI
    
    * fixed ui bugs
    
    * moved fetch classroom data function from ngOnInit to ngAfterViewChecked
    
    * added description for the new function
    
    * removed unnecessary api calls
    
    * added keyboard navigation

--- core/templates/components/common-layout-directives/navigation-bars/top-navigation-bar.component.spec.ts
+++ core/templates/components/common-layout-directives/navigation-bars/top-navigation-bar.component.spec.ts
@@ -35,6 +35,9 @@ import { ClassroomBackendApiService } from 'domain/classroom/classroom-backend-a
 import { I18nLanguageCodeService } from 'services/i18n-language-code.service';
 import { I18nService } from 'i18n/i18n.service';
 import { CookieService } from 'ngx-cookie';
+import { CreatorTopicSummary } from 'domain/topic/creator-topic-summary.model';
+import { ClassroomData } from 'domain/classroom/classroom-data.model';
+import { AccessValidationBackendApiService } from 'pages/oppia-root/routing/access-validation-backend-api.service';
 
 class MockWindowRef {
   nativeWindow = {
@@ -58,6 +61,7 @@ class MockWindowRef {
 }
 
 describe('TopNavigationBarComponent', () => {
+  let accessValidationBackendApiService: AccessValidationBackendApiService;
   let fixture: ComponentFixture<TopNavigationBarComponent>;
   let component: TopNavigationBarComponent;
   let mockWindowRef: MockWindowRef;
@@ -125,6 +129,8 @@ describe('TopNavigationBarComponent', () => {
     i18nService = TestBed.inject(I18nService);
     classroomBackendApiService = TestBed.inject(ClassroomBackendApiService);
     i18nLanguageCodeService = TestBed.inject(I18nLanguageCodeService);
+    accessValidationBackendApiService = TestBed
+      .inject(AccessValidationBackendApiService);
 
     spyOn(searchService, 'onSearchBarLoaded')
       .and.returnValue(new EventEmitter<string>());
@@ -231,10 +237,10 @@ describe('TopNavigationBarComponent', () => {
     spyOn(navigationService, 'openSubmenu');
     spyOn(deviceInfoService, 'isMobileDevice').and.returnValue(false);
 
-    component.openSubmenu(mouseoverEvent, 'classroomMenu');
+    component.openSubmenu(mouseoverEvent, 'learnMenu');
 
     expect(navigationService.openSubmenu).toHaveBeenCalledWith(
-      mouseoverEvent, 'classroomMenu');
+      mouseoverEvent, 'learnMenu');
   });
 
   it('should close submenu when user moves the mouse away' +
@@ -372,7 +378,7 @@ describe('TopNavigationBarComponent', () => {
 
     component.navElementsVisibilityStatus = {
       I18N_TOPNAV_DONATE: true,
-      I18N_TOPNAV_CLASSROOM: true,
+      I18N_TOPNAV_LEARN: true,
       I18N_TOPNAV_ABOUT: true,
       I18N_CREATE_EXPLORATION_CREATE: true,
       I18N_TOPNAV_LIBRARY: true
@@ -384,7 +390,7 @@ describe('TopNavigationBarComponent', () => {
     fixture.whenStable().then(() => {
       expect(component.navElementsVisibilityStatus).toEqual({
         I18N_TOPNAV_DONATE: false,
-        I18N_TOPNAV_CLASSROOM: true,
+        I18N_TOPNAV_LEARN: true,
         I18N_TOPNAV_ABOUT: true,
         I18N_CREATE_EXPLORATION_CREATE: true,
         I18N_TOPNAV_LIBRARY: true
@@ -469,5 +475,58 @@ describe('TopNavigationBarComponent', () => {
     expect(component.userIsLoggedIn).toBe(true);
     expect(component.username).toBe('username1');
     expect(component.profilePageUrl).toBe('/profile/username1');
+  }));
+
+  it('should return proper offset for learn dropdown', ()=>{
+    var dummyElement = document.createElement('div');
+    spyOn(document, 'querySelector').and.returnValue(dummyElement);
+
+    spyOn(Element.prototype, 'getBoundingClientRect').and.callFake(
+      jasmine.createSpy('getBoundingClientRect').and
+        .returnValue({ top: 1, height: 100, left: 0, width: 200, right: 202 })
+    );
+
+    expect(component.getDropdownOffset('.dummy', 0)).toBe(0);
+  });
+
+  it('should return proper offset for learn dropdown when element is undefined',
+    ()=>{
+      spyOn(document, 'querySelector').and.returnValue(null);
+
+      expect(component.getDropdownOffset('.dummy', 0)).toBe(0);
+    });
+
+  it('should check if navbar dropdown offsets are updated', fakeAsync (()=>{
+    spyOn(component, 'truncateNavbar').and.stub();
+    spyOn(component, 'getDropdownOffset')
+      .withArgs('.learn-tab', 688).and.returnValue(-10)
+      .withArgs('.learn-tab', 300).and.returnValue(-10);
+
+    expect(component.learnDropdownOffset).toBe(0);
+
+    component.ngAfterViewChecked();
+    tick();
+
+    expect(component.learnDropdownOffset).toBe(-10);
+  }));
+
+  it('should check if classroom data is fetched', fakeAsync(() => {
+    spyOn(component, 'truncateNavbar').and.stub();
+    component.CLASSROOM_PROMOS_ARE_ENABLED = true;
+    spyOn(
+      classroomBackendApiService, 'fetchClassroomPromosAreEnabledStatusAsync').
+      and.resolveTo(true);
+    spyOn(accessValidationBackendApiService, 'validateAccessToClassroomPage')
+      .and.returnValue(Promise.resolve());
+    let array: CreatorTopicSummary[] = [];
+    let classroomData = new ClassroomData('test', array, 'dummy', 'dummy');
+    spyOn(
+      classroomBackendApiService, 'fetchClassroomDataAsync')
+      .and.resolveTo(classroomData);
+
+    component.ngOnInit();
+    tick();
+
+    expect(component.classroomData).toEqual(array);
   }));
 });
