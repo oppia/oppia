462161ec9544db20a7f69890fe67c24e85b8e570
JG-11 <80846488+JeeveshGarg@users.noreply.github.com>
2021-12-11 22:31:09 +0530

    lint

--- core/templates/pages/exploration-editor-page/modal-templates/help-modal.component.ts
+++ core/templates/pages/exploration-editor-page/modal-templates/help-modal.component.ts
@@ -1,4 +1,4 @@
-// Copyright 2020 The Oppia Authors. All Rights Reserved.
+// Copyright 2021 The Oppia Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 /**
- * @fileoverview Controller for help modal.
+ * @fileoverview Component for help modal.
  */
 
 import { Component, OnInit } from '@angular/core';
