f9af2e10e1b1173afdd4bb113d0b6c6d15fdf5b5
JG-11 <80846488+JeeveshGarg@users.noreply.github.com>
2021-12-14 17:59:21 +0530

    Fix part of 9749: delete-answer-group-modal.template

--- core/templates/pages/exploration-editor-page/exploration-editor-page.module.ts
+++ core/templates/pages/exploration-editor-page/exploration-editor-page.module.ts
@@ -29,6 +29,7 @@ import { platformFeatureInitFactory, PlatformFeatureService } from
   'services/platform-feature.service';
 import { RequestInterceptor } from 'services/request-interceptor.service';
 import { StateParamChangesEditorComponent } from './editor-tab/state-param-changes-editor/state-param-changes-editor.component';
+import { DeleteAnswerGroupModalComponent } from './editor-tab/templates/modal-templates/delete-answer-group-modal.component';
 import { DeleteStateSkillModalComponent } from './editor-tab/templates/modal-templates/delete-state-skill-modal.component';
 import { ParamChangesEditorDirective } from './param-changes-editor/param-changes-editor.component';
 import { SwitchContentLanguageRefreshRequiredModalComponent } from 'pages/exploration-player-page/switch-content-language-refresh-required-modal.component';
@@ -62,6 +63,7 @@ import { TransferExplorationOwnershipModalComponent } from './settings-tab/templ
   ],
   declarations: [
     CkEditorCopyToolbarComponent,
+    DeleteAnswerGroupModalComponent,
     DeleteStateSkillModalComponent,
     ParamChangesEditorDirective,
     StateParamChangesEditorComponent,
@@ -85,6 +87,7 @@ import { TransferExplorationOwnershipModalComponent } from './settings-tab/templ
   ],
   entryComponents: [
     CkEditorCopyToolbarComponent,
+    DeleteAnswerGroupModalComponent,
     DeleteStateSkillModalComponent,
     StateParamChangesEditorComponent,
     SwitchContentLanguageRefreshRequiredModalComponent,
