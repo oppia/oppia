92a79b28a1cc4896f58a99f24c29dc4b3910229e
Anurag verma <78868769+anurag629@users.noreply.github.com>
2021-12-18 11:07:23 +0530

    fix

--- core/domain/platform_parameter_domain.py
+++ core/domain/platform_parameter_domain.py
@@ -23,7 +23,6 @@ import json
 import re
 
 from core import feconf
-from core import python_utils
 from core import utils
 from core.constants import constants
 from core.domain import change_domain
