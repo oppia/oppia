8fa87d08bd0afea76247052939050f157845c7db
YBCS <budha627@gmail.com>
2021-12-19 10:30:23 +0530

    draft for collectionJob

--- core/controllers/collection_editor.py
+++ core/controllers/collection_editor.py
@@ -30,6 +30,7 @@ from core.domain import rights_manager
 from core.domain import search_services
 from core.domain import summary_services
 
+# can disble from here 
 
 def _require_valid_version(version_from_payload, collection_version):
     """Check that the payload version matches the given collection version."""
@@ -196,6 +197,7 @@ class CollectionRightsHandler(CollectionEditorHandler):
 
 
 class CollectionPublishHandler(base.BaseHandler):
+    # this is the one -> return an error is user tries to publish or make change
     """Handles the publication of the given collection."""
 
     URL_PATH_ARGS_SCHEMAS = {
---
+++ core/jobs/batch_jobs/collection_info_jobs.py
@@ -0,0 +1,64 @@
+# coding: utf-8
+#
+# Copyright 2021 The Oppia Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Jobs that extract Collection models information."""
+
+from __future__ import annotations
+
+from core.domain import user_services
+from core.jobs import base_jobs
+from core.jobs.io import ndb_io
+from core.jobs.transforms import job_result_transforms
+from core.jobs.types import job_run_result
+from core.platform import models
+
+import apache_beam as beam
+
+MYPY = False
+if MYPY: # pragma: no cover
+    from mypy_imports import datastore_services
+    from mypy_imports import collection_models
+
+collection_models = models.Registry.import_models([models.NAMES.collection])
+datastore_services = models.Registry.import_datastore_services()
+
+class CountCollectionModel(base_jobs.JobBase):
+    """Job that generate emails of CollectionModels users."""
+
+    def run(self) -> beam.PCollection[job_run_result.JobRunResult]:
+        """Returns a PCollection of 'SUCCESS' or 'FAILURE' results from
+        generating email of CollectionModel user.
+
+        Returns:
+            PCollection. A PCollection of 'SUCCESS' or 'FAILURE' results from
+            generating email of CollectionModel user.
+        """
+        extract_collection_model = (
+            self.pipeline
+            | 'Get all collection models' >> ndb_io.GetModels(
+                collection_models.CollectionModel.get_all(
+                    include_deleted=False))
+            | 'Extract user IDs' >> beam.Map(
+                lambda collection: collection.id)
+            | 'Extract emails' >> beam.Map(
+                user_services.get_email_from_user_id)
+        )
+
+        return (
+            extract_collection_model
+            | 'Count the output' >> (
+                job_result_transforms.ResultsToJobRunResults())
+        )
