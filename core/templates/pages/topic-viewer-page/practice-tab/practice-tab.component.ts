838c3ed07901016603b95316d09c86b8fff688fe
Jaideep Sharma <jaideep.sharma.cse20@itbhu.ac.in>
2022-01-17 09:48:33 +0530

    Added comments for the changes

--- core/templates/pages/topic-viewer-page/practice-tab/practice-tab.component.ts
+++ core/templates/pages/topic-viewer-page/practice-tab/practice-tab.component.ts
@@ -35,9 +35,12 @@ import { WindowRef } from 'services/contextual/window-ref.service';
   styleUrls: []
 })
 export class PracticeTabComponent implements OnInit {
+  // These properties are initialized using Angular lifecycle hooks
+  // and we need to do non-null assertion, for more information see
+  // https://github.com/oppia/oppia/wiki/Guide-on-defining-types#ts-7-1
   @Input() topicName!: string;
-  @Input() startButtonIsDisabled: boolean = false;
   @Input() subtopicsList!: Subtopic[];
+  @Input() startButtonIsDisabled: boolean = false;
   @Input() displayArea: string = 'topicViewer';
   @Input() topicUrlFragment: string = '';
   @Input() classroomUrlFragment: string = '';
