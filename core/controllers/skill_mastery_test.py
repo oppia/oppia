d37a0fa1814cc40ca6aff82b1573a723449049a8
Your Name <you@example.com>
2022-01-18 21:39:18 +0530

    changed // to / in skill mastry test.py

--- core/controllers/skill_mastery_test.py
+++ core/controllers/skill_mastery_test.py
@@ -571,11 +571,11 @@ class SubtopicMasteryDataHandlerTest(test_utils.GenericTestBase):
                 'comma_separated_topic_ids': ','.join([topic_id_1, topic_id_2])
             })
         degrees_of_mastery_1 = {
-            u'1': ((self.degree_of_mastery_1 + self.degree_of_mastery_2) // 2),
+            u'1': ((self.degree_of_mastery_1 + self.degree_of_mastery_2) / 2),
             u'2': self.degree_of_mastery_3
         }
         degrees_of_mastery_2 = {
-            u'1': ((self.degree_of_mastery_3 + self.degree_of_mastery_4) // 2),
+            u'1': ((self.degree_of_mastery_3 + self.degree_of_mastery_4) / 2),
             u'2': self.degree_of_mastery_5
         }
         self.assertEqual(
