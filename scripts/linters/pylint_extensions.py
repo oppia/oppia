2b5825d8d6756bc31333822f142dd378ed05f8ee
Your Name <you@example.com>
2022-01-18 08:51:38 +0530

    removed linter register cecker

--- scripts/linters/pylint_extensions.py
+++ scripts/linters/pylint_extensions.py
@@ -2320,7 +2320,6 @@ def register(linter):
     linter.register_checker(FunctionArgsOrderChecker(linter))
     linter.register_checker(RestrictedImportChecker(linter))
     linter.register_checker(SingleCharAndNewlineAtEOFChecker(linter))
-    linter.register_checker(DivisionOperatorChecker(linter))
     linter.register_checker(SingleLineCommentChecker(linter))
     linter.register_checker(BlankLineBelowFileOverviewChecker(linter))
     linter.register_checker(SingleLinePragmaChecker(linter))
