cca8060730973ff455d72aa0f84501317902e14f
Prayush Dawda <iamprayush@gmail.com>
2022-01-19 11:22:26 +0530

    Testing (temp commit).

--- extensions/interactions/AlgebraicExpressionInput/protractor.js
+++ extensions/interactions/AlgebraicExpressionInput/protractor.js
@@ -56,7 +56,31 @@ var submitAnswer = async function(elem, answer) {
 
 var answerObjectType = 'AlgebraicExpression';
 
-var testSuite = [];
+var testSuite = [{
+  interactionArguments: [['a', 'b']],
+  ruleArguments: ['MatchesExactlyWith', '((a+b))^(2)'],
+  expectedInteractionDetails: [],
+  wrongAnswers: ['(a-b)^2', '(a-b)^3', 'a^2+2*a*b+b^2'],
+  correctAnswers: ['(a+b)^2', '(b+a)^2', '(a+b)*(a+b)']
+}, {
+  interactionArguments: [['x', 'z']],
+  ruleArguments: ['MatchesExactlyWith', '((x^2)-x)/z'],
+  expectedInteractionDetails: [],
+  wrongAnswers: ['((x^3)-x)/z', 'x(x-1)/z', '((x^2)/z)-x/z'],
+  correctAnswers: ['((x^2)-x)/z', '((x*x)-x)*z^(-1)']
+}, {
+  interactionArguments: [['π', 'r']],
+  ruleArguments: ['IsEquivalentTo', 'pi*r^2'],
+  expectedInteractionDetails: [],
+  wrongAnswers: ['pi*r', 'pi*r*2', 'pi', 'pi/r^2'],
+  correctAnswers: ['pi*r^2', 'pi*r*r', '(pi*r^3)/(2*r-r)']
+}, {
+  interactionArguments: [['x']],
+  ruleArguments: ['IsEquivalentTo', '(9*x^2)-6*x+1'],
+  expectedInteractionDetails: [],
+  wrongAnswers: ['sqrt((3x-1)^(2))', '9*(x)^(2)-6*x-1', '((3*x-1))^(4)'],
+  correctAnswers: ['(9*x^2)-6*x+1', '(1-3x)^(2)']
+}];
 
 exports.customizeInteraction = customizeInteraction;
 exports.expectInteractionDetailsToMatch = expectInteractionDetailsToMatch;
