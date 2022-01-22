deccbd01f754e73ab668530fafe99108d2d9bddf
Lawful2002 <sharshvardhan101@gmail.com>
2022-01-23 01:18:51 +0530

    [skip ci] skip ci

--- core/templates/components/forms/schema-based-editors/schema-based-float-editor.directive.ts
+++ core/templates/components/forms/schema-based-editors/schema-based-float-editor.directive.ts
@@ -127,7 +127,6 @@ angular.module('oppia').directive('schemaBasedFloatEditor', [
                   .convertToEnglishDecimal(ctrl.localStringValue);
               }
             }
-
             // Generate errors (if any).
             ctrl.generateErrors();
           };
