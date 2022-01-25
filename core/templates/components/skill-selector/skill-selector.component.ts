9d83842484490a56ced8342b3e7c1e85691d2d67
Manan Rathi <76519771+Manan-Rathi@users.noreply.github.com>
2022-01-25 13:18:21 +0530

    Fix part of #10474 : Make typescript checks strict for `core/templates/components/skill-selector/merge-skill-modal.component.ts` (#14644)
    * Type merge-skill-modal
    
    * Make changes
    
    * fix lint
    
    * Changes
    
    * Make changes
    
    * Changes-1
    
    * Fix tsconfig
    
    * Fix lint issues
    
    * Fix
    
    * Typing changes
    
    * Zero changes made to tsconfig
    
    * Refactor code
    
    * Added CanCreateSkill
    
    * Both properties made true
    
    Co-authored-by: Manan Rathi <rathimanan27@gmail.com>

--- core/templates/components/skill-selector/skill-selector.component.ts
+++ core/templates/components/skill-selector/skill-selector.component.ts
@@ -37,15 +37,19 @@ export class SkillSelectorComponent implements OnInit {
   // If countOfSkillsToPrioritize > 0, then sortedSkillSummaries should
   // have the initial 'countOfSkillsToPrioritize' entries of skills with
   // the same priority.
-  @Input() sortedSkillSummaries: GroupedSkillSummaries;
-  @Input() selectedSkillId: string;
-  @Input() countOfSkillsToPrioritize: number;
-  @Input() categorizedSkills: CategorizedSkills;
-  @Input() untriagedSkillSummaries: SkillSummary[];
-  @Input() allowSkillsFromOtherTopics: boolean;
+
+  // Some properties below are initialized using Angular lifecycle hooks
+  // where we need to do non-null assertion. For more information see
+  // https://github.com/oppia/oppia/wiki/Guide-on-defining-types#ts-7-1
+  @Input() sortedSkillSummaries!: GroupedSkillSummaries;
+  @Input() selectedSkillId!: string;
+  @Input() countOfSkillsToPrioritize!: number;
+  @Input() categorizedSkills!: CategorizedSkills;
+  @Input() untriagedSkillSummaries!: SkillSummary[];
+  @Input() allowSkillsFromOtherTopics!: boolean;
   @Output() selectedSkillIdChange: EventEmitter<string> = new EventEmitter();
-  currCategorizedSkills: CategorizedSkills = null;
-  selectedSkill: string = null;
+  currCategorizedSkills!: CategorizedSkills;
+  selectedSkill!: string;
   skillFilterText: string = '';
   topicFilterList: { topicName: string ; checked: boolean }[] = [];
   subTopicFilterDict: SubTopicFilterDict = {};
