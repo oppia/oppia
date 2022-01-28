8359d5a0ca0bc1ce2c5b8298fdcd53b12f06d077
soumyo123-prog <deysoumyajyoti2017@gmail.com>
2022-01-28 13:47:22 +0530

    completed expression-evaluator-service

--- scripts/check_frontend_test_coverage.py
+++ scripts/check_frontend_test_coverage.py
@@ -48,7 +48,6 @@ NOT_FULLY_COVERED_FILENAMES = [
     'collection-player-page.component.ts',
     'contribution-and-review.service.ts',
     'exploration-states.service.ts',
-    'expression-evaluator.service.ts',
     'expression-interpolation.service.ts',
     'fatigue-detection.service.ts',
     'google-analytics.initializer.ts',
