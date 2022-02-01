f2839de83e6e36f9a6233f5dc555b3ae15a1b231
Aayush Kumar Singh <malemeaayushkumarsingh@gmail.com>
2022-02-02 00:54:10 +0530

    Fix Part of #13162: Adds schema for handler FetchSkillsHandler class (#14749)
    * Added schema for FetchSkillsHandler module
    
    * Added schema for fetchskillshandler addressing the review comments
    
    Co-authored-by: Vojtěch Jelínek <vojtin.j@gmail.com>

--- core/handler_schema_constants.py
+++ core/handler_schema_constants.py
@@ -40,7 +40,6 @@ HANDLER_CLASS_NAMES_WHICH_STILL_NEED_SCHEMAS = [
     'EditableStoryDataHandler',
     'ExportAccountHandler',
     'FeedbackThreadStatusChangeEmailHandler',
-    'FetchSkillsHandler',
     'FlagExplorationEmailHandler',
     'IncomingReplyEmailHandler',
     'InstantFeedbackMessageEmailHandler',
