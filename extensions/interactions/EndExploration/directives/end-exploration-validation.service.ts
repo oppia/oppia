7b616f8bf7ad4afa6b05cd820b619abda21f02fd
Pankaj Prajapati <paprajapati9@gmail.com>
2022-02-03 20:48:36 +0530

    minor fix

--- extensions/interactions/EndExploration/directives/end-exploration-validation.service.ts
+++ extensions/interactions/EndExploration/directives/end-exploration-validation.service.ts
@@ -63,14 +63,17 @@ export class EndExplorationValidationService {
     }
     // Validate invalid exploration ids given in author recommended
     // explorations ids.
-    let warningMessage = document.querySelector(
+    let warningMessageElement = document.querySelector(
       'oppia-interactive-end-exploration .oppia-error-message-text span'
-    ).textContent;
-    warningMessage &&
-    warningsList.push({
-      type: AppConstants.WARNING_TYPES.ERROR,
-      message: warningMessage
-    });
+    );
+    if (warningMessageElement) {
+      let warningMessage = warningMessageElement.textContent;
+      warningMessage &&
+      warningsList.push({
+        type: AppConstants.WARNING_TYPES.ERROR,
+        message: warningMessage
+      });
+    }
     return warningsList;
   }
 
