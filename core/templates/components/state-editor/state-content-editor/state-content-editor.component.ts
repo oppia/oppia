06b16c8c543840d6941252d055e0628bb856affa
JG-11 <80846488+JeeveshGarg@users.noreply.github.com>
2022-02-05 14:34:41 +0530

    may solve

--- core/templates/components/state-editor/state-content-editor/state-content-editor.component.ts
+++ core/templates/components/state-editor/state-content-editor/state-content-editor.component.ts
@@ -19,7 +19,6 @@
 import { Component, OnInit, ChangeDetectorRef, Input, Output, EventEmitter } from '@angular/core';
 import { downgradeComponent } from '@angular/upgrade/static';
 import { ContextService } from 'services/context.service';
-import { EditabilityService } from 'services/editability.service';
 import { StateContentService } from 'components/state-editor/state-editor-properties-services/state-content.service';
 import { StateEditorService } from 'components/state-editor/state-editor-properties-services/state-editor.service';
 import { EditorFirstTimeEventsService } from 'pages/exploration-editor-page/services/editor-first-time-events.service';
@@ -54,7 +53,6 @@ export class StateContentEditorComponent implements OnInit {
   constructor(
     private changeDetectorRef: ChangeDetectorRef,
     private contextService: ContextService,
-    private editabilityService: EditabilityService,
     private stateContentService: StateContentService,
     private stateEditorService: StateEditorService,
     private editorFirstTimeEventsService: EditorFirstTimeEventsService,
@@ -75,7 +73,6 @@ export class StateContentEditorComponent implements OnInit {
     }
 
     this.contentEditorIsOpen = false;
-    this.isEditable = this.editabilityService.isEditable();
     this.cardHeightLimitWarningIsShown = true;
     this.directiveSubscriptions.add(
       this.externalSaveService.onExternalSave.subscribe(
@@ -116,10 +113,8 @@ export class StateContentEditorComponent implements OnInit {
   }
 
   openStateContentEditor(): void {
-    if (this.isEditable) {
-      this.editorFirstTimeEventsService.registerFirstOpenContentBoxEvent();
-      this.contentEditorIsOpen = true;
-    }
+    this.editorFirstTimeEventsService.registerFirstOpenContentBoxEvent();
+    this.contentEditorIsOpen = true;
   }
 
   onSaveContentButtonClicked(): void {
