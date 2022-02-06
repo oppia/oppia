97554db8b22952c65dc6cca37c0bb51cb36ca143
Shivam Chaudhary <shivamthalwal@gmail.com>
2022-02-07 00:23:36 +0530

    first commit

--- core/templates/components/shared-component.module.ts
+++ core/templates/components/shared-component.module.ts
@@ -98,9 +98,11 @@ import { TutorCardComponent } from 'pages/exploration-player-page/learner-experi
 import { ContentLanguageSelectorComponent } from 'pages/exploration-player-page/layout-directives/content-language-selector.component';
 import { RatingDisplayComponent } from './ratings/rating-display/rating-display.component';
 import { SupplementalCardComponent } from 'pages/exploration-player-page/learner-experience/supplemental-card.component';
+import { AddOrUpdateSolutionModalComponent } from 'pages/exploration-editor-page/editor-tab/templates/modal-templates/add-or-update-solution-modal.component';
 import { SavePendingChangesModalComponent } from './save-pending-changes/save-pending-changes-modal.component';
 import { AddHintModalComponent } from 'pages/exploration-editor-page/editor-tab/templates/modal-templates/add-hint-modal.component';
 import { SmoothHeightAnimatorComponent } from './smooth-height/smooth-height-animator.component';
+import { QuestionMisconceptionSelectorComponent } from './question-directives/question-misconception-selector/question-misconception-selector.component';
 import { ConversationSkinComponent } from 'pages/exploration-player-page/learner-experience/conversation-skin.component';
 import { RatingsAndRecommendationsComponent } from 'pages/exploration-player-page/learner-experience/ratings-and-recommendations.component';
 import { LearnerAnswerInfoCard } from 'pages/exploration-player-page/learner-experience/learner-answer-info-card.component';
@@ -114,8 +116,14 @@ import { HintEditorComponent } from 'components/state-directives/hint-editor/hin
 import { ResponseHeaderComponent } from './state-directives/response-header/response-header.component';
 import { StateHintsEditorComponent } from 'components/state-editor/state-hints-editor/state-hints-editor.component';
 import { ReviewMaterialEditorComponent } from './review-material-editor/review-material-editor.component';
+import { ConfirmLeaveModalComponent } from 'pages/exploration-editor-page/modal-templates/confirm-leave-modal.component';
+import { CustomizeInteractionModalComponent } from 'pages/exploration-editor-page/editor-tab/templates/modal-templates/customize-interaction-modal.component';
+import { TagMisconceptionModalComponent } from './question-directives/question-misconception-editor/tag-misconception-modal-component';
+import { QuestionMisconceptionEditorComponent } from './question-directives/question-misconception-editor/question-misconception-editor.component';
 import { SolutionExplanationEditor } from './state-directives/solution-editor/solution-explanation-editor.component';
 import { SolutionEditor } from './state-directives/solution-editor/solution-editor.component';
+import { StateSolutionEditorComponent } from './state-editor/state-solution-editor/state-solution-editor.component';
+import { StateInteractionEditorComponent } from './state-editor/state-interaction-editor/state-interaction-editor.component';
 
 // Pipes.
 import { TruncatePipe } from 'filters/string-utility-filters/truncate.pipe';
@@ -171,6 +179,7 @@ import { HybridRouterModuleProvider } from 'hybrid-router-module-provider';
     BackgroundBannerComponent,
     CompletionGraphComponent,
     CorrectnessFooterComponent,
+    ConfirmLeaveModalComponent,
     ConfirmQuestionExitModalComponent,
     ContinueButtonComponent,
     ContentLanguageSelectorComponent,
@@ -178,6 +187,7 @@ import { HybridRouterModuleProvider } from 'hybrid-router-module-provider';
     ConversationSkinEmbedComponent,
     CreateNewSkillModalComponent,
     CreateActivityModalComponent,
+    CustomizeInteractionModalComponent,
     DeleteAnswerGroupModalComponent,
     DeleteHintModalComponent,
     DeleteInteractionModalComponent,
@@ -241,6 +251,9 @@ import { HybridRouterModuleProvider } from 'hybrid-router-module-provider';
     SortByPipe,
     LearnerDashboardIconsComponent,
     PreviewThumbnailComponent,
+    AddOrUpdateSolutionModalComponent,
+    AddHintModalComponent,
+    QuestionMisconceptionSelectorComponent,
     QuestionsOpportunitiesSelectDifficultyModalComponent,
     QuestionsListSelectSkillAndDifficultyModalComponent,
     DeleteInteractionModalComponent,
@@ -250,8 +263,12 @@ import { HybridRouterModuleProvider } from 'hybrid-router-module-provider';
     SavePendingChangesModalComponent,
     AddHintModalComponent,
     ReviewMaterialEditorComponent,
+    TagMisconceptionModalComponent,
+    QuestionMisconceptionEditorComponent,
     SolutionEditor,
     SolutionExplanationEditor,
+    StateSolutionEditorComponent,
+    StateInteractionEditorComponent
   ],
 
   entryComponents: [
@@ -260,6 +277,7 @@ import { HybridRouterModuleProvider } from 'hybrid-router-module-provider';
     BackgroundBannerComponent,
     CompletionGraphComponent,
     CorrectnessFooterComponent,
+    ConfirmLeaveModalComponent,
     ConfirmQuestionExitModalComponent,
     ContinueButtonComponent,
     ConceptCardComponent,
@@ -268,6 +286,7 @@ import { HybridRouterModuleProvider } from 'hybrid-router-module-provider';
     ConversationSkinEmbedComponent,
     CreateNewSkillModalComponent,
     CreateActivityModalComponent,
+    CustomizeInteractionModalComponent,
     DeleteHintModalComponent,
     DeleteInteractionModalComponent,
     DeleteLastHintModalComponent,
@@ -326,6 +345,9 @@ import { HybridRouterModuleProvider } from 'hybrid-router-module-provider';
     TopicsAndSkillsDashboardNavbarBreadcrumbComponent,
     LearnerDashboardIconsComponent,
     PreviewThumbnailComponent,
+    AddOrUpdateSolutionModalComponent,
+    AddHintModalComponent,
+    QuestionMisconceptionSelectorComponent,
     QuestionsOpportunitiesSelectDifficultyModalComponent,
     QuestionsListSelectSkillAndDifficultyModalComponent,
     DeleteInteractionModalComponent,
@@ -335,8 +357,12 @@ import { HybridRouterModuleProvider } from 'hybrid-router-module-provider';
     SavePendingChangesModalComponent,
     AddHintModalComponent,
     ReviewMaterialEditorComponent,
+    TagMisconceptionModalComponent,
+    QuestionMisconceptionEditorComponent,
     SolutionEditor,
-    SolutionExplanationEditor
+    SolutionExplanationEditor,
+    StateSolutionEditorComponent,
+    StateInteractionEditorComponent
   ],
 
   exports: [
@@ -360,6 +386,7 @@ import { HybridRouterModuleProvider } from 'hybrid-router-module-provider';
     BackgroundBannerComponent,
     CompletionGraphComponent,
     CorrectnessFooterComponent,
+    ConfirmLeaveModalComponent,
     ConfirmQuestionExitModalComponent,
     ContinueButtonComponent,
     ContentLanguageSelectorComponent,
@@ -367,6 +394,7 @@ import { HybridRouterModuleProvider } from 'hybrid-router-module-provider';
     ConversationSkinEmbedComponent,
     CreateNewSkillModalComponent,
     CreateActivityModalComponent,
+    CustomizeInteractionModalComponent,
     DeleteAnswerGroupModalComponent,
     DeleteHintModalComponent,
     DeleteInteractionModalComponent,
@@ -428,6 +456,8 @@ import { HybridRouterModuleProvider } from 'hybrid-router-module-provider';
     SortByPipe,
     SavePendingChangesModalComponent,
     LearnerDashboardIconsComponent,
+    AddOrUpdateSolutionModalComponent,
+    QuestionMisconceptionSelectorComponent,
     QuestionsOpportunitiesSelectDifficultyModalComponent,
     QuestionsListSelectSkillAndDifficultyModalComponent,
     DeleteInteractionModalComponent,
@@ -435,8 +465,12 @@ import { HybridRouterModuleProvider } from 'hybrid-router-module-provider';
     DeleteLastHintModalComponent,
     DeleteSolutionModalComponent,
     ReviewMaterialEditorComponent,
+    TagMisconceptionModalComponent,
+    QuestionMisconceptionEditorComponent,
     SolutionEditor,
-    SolutionExplanationEditor
+    SolutionExplanationEditor,
+    StateSolutionEditorComponent,
+    StateInteractionEditorComponent,
   ],
 })
 
---
+++ core/templates/components/state-editor/state-interaction-editor/state-interaction-editor.component.spec.ts
@@ -0,0 +1,361 @@
+// Copyright 2022 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the 'License');
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS-IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+/**
+ * @fileoverview Unit tests for 'State Interaction Editor Component'.
+ */
+
+import { EventEmitter, NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, fakeAsync, TestBed, tick, waitForAsync } from '@angular/core/testing';
+import { StateEditorService } from '../state-editor-properties-services/state-editor.service';
+import { StateInteractionEditorComponent } from './state-interaction-editor.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { State } from 'domain/state/StateObjectFactory';
+import { Interaction } from 'domain/exploration/InteractionObjectFactory';
+import { ResponsesService } from 'pages/exploration-editor-page/editor-tab/services/responses.service';
+import { UrlInterpolationService } from 'domain/utilities/url-interpolation.service';
+import { EditabilityService } from 'services/editability.service';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { CustomizeInteractionModalComponent } from 'pages/exploration-editor-page/editor-tab/templates/modal-templates/customize-interaction-modal.component';
+import { StateNextContentIdIndexService } from '../state-editor-properties-services/state-next-content-id-index.service';
+import { StateInteractionIdService } from '../state-editor-properties-services/state-interaction-id.service';
+import { StateContentService } from '../state-editor-properties-services/state-content.service';
+import { SubtitledHtml } from 'domain/exploration/subtitled-html.model';
+import { ContextService } from 'services/context.service';
+import { StateCustomizationArgsService } from '../state-editor-properties-services/state-customization-args.service';
+import { StateSolutionService } from '../state-editor-properties-services/state-solution.service';
+import { ExplorationHtmlFormatterService } from 'services/exploration-html-formatter.service';
+import { InteractionDetailsCacheService } from 'pages/exploration-editor-page/editor-tab/services/interaction-details-cache.service';
+
+class MockNgbModal {
+  modal: string;
+  success: boolean = true;
+  open(content, options) {
+    if (this.modal === 'add_interaction') {
+      return {
+        result: {
+          componentInstance: {
+            showMarkAllAudioAsNeedingUpdateModalIfRequired: EventEmitter
+          },
+          then: (
+              successCallback: (result) => void,
+              cancelCallback: () => void
+          ) => {
+            if (this.success) {
+              successCallback({});
+            } else {
+              cancelCallback();
+            }
+          }
+        }
+      };
+    } else if (this.modal === 'delete_interaction') {
+      return {
+        result: {
+          then: (
+              successCallback: () => void,
+              errorCallback: () => void
+          ) => {
+            if (this.success) {
+              successCallback();
+            } else {
+              errorCallback();
+            }
+            return {
+              then: (callback: () => void) => {
+                callback();
+              }
+            };
+          }
+        }
+      };
+    }
+  }
+}
+
+// eslint-disable-next-line oppia/no-test-blockers
+fdescribe('State Interaction component', () => {
+  let component: StateInteractionEditorComponent;
+  let fixture: ComponentFixture<StateInteractionEditorComponent>;
+  let stateEditorService: StateEditorService;
+  let responsesService: ResponsesService;
+  let urlInterpolationService: UrlInterpolationService;
+  let editabilityService: EditabilityService;
+  let mockNgbModal: MockNgbModal;
+  let stateNextContentIdIndexService: StateNextContentIdIndexService;
+  let stateInteractionIdService: StateInteractionIdService;
+  let stateContentService: StateContentService;
+  let contextService: ContextService;
+  let stateCustomizationArgsService: StateCustomizationArgsService;
+  let stateSolutionService: StateSolutionService;
+  let explorationHtmlFormatterService: ExplorationHtmlFormatterService;
+  let interactionDetailsCacheService: InteractionDetailsCacheService;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+      ],
+      declarations: [
+        StateInteractionEditorComponent,
+      ],
+      providers: [
+        StateEditorService,
+        ResponsesService,
+        {
+          provide: NgbModal,
+          useClass: MockNgbModal
+        },
+        InteractionDetailsCacheService,
+        CustomizeInteractionModalComponent,
+        UrlInterpolationService,
+        EditabilityService,
+        StateNextContentIdIndexService,
+        StateInteractionIdService,
+        StateSolutionService,
+        StateContentService,
+        ExplorationHtmlFormatterService,
+        ContextService,
+        StateCustomizationArgsService
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StateInteractionEditorComponent);
+    component = fixture.componentInstance;
+
+    stateEditorService = TestBed.inject(StateEditorService);
+    responsesService = TestBed.inject(ResponsesService);
+    urlInterpolationService = TestBed.inject(UrlInterpolationService);
+    editabilityService = TestBed.inject(EditabilityService);
+    stateNextContentIdIndexService =
+      TestBed.inject(StateNextContentIdIndexService);
+    stateInteractionIdService = TestBed.inject(StateInteractionIdService);
+    stateContentService = TestBed.inject(StateContentService);
+    mockNgbModal = (TestBed.inject(NgbModal) as unknown) as MockNgbModal;
+    contextService = TestBed.inject(ContextService);
+    stateCustomizationArgsService =
+      TestBed.inject(StateCustomizationArgsService);
+    interactionDetailsCacheService =
+      TestBed.inject(InteractionDetailsCacheService);
+    stateSolutionService = TestBed.inject(StateSolutionService);
+    explorationHtmlFormatterService =
+      TestBed.inject(ExplorationHtmlFormatterService);
+
+    fixture.detectChanges();
+  });
+
+  it('should keep non-empty content when setting a interaction',
+    fakeAsync(() => {
+      spyOn(component, 'throwError').and.stub();
+      spyOn(stateEditorService, 'updateStateInteractionEditorInitialised')
+        .and.stub();
+
+      component.ngOnInit();
+      tick();
+      stateEditorService.onStateEditorInitialized.emit(undefined);
+      tick();
+
+      expect(component.interactionIsDisabled).toBe(false);
+      expect(stateEditorService.updateStateInteractionEditorInitialised)
+        .toHaveBeenCalled();
+    }));
+
+  it('should keep non-empty content when setting a interaction', () => {
+    spyOn(responsesService.onInitializeAnswerGroups, 'emit').and.stub();
+
+    const state = new State(
+      'shivam', 'id', 'some', null,
+      new Interaction([], [], null, null, [], 'id', null),
+      null, null, true, true, null, 7);
+
+    component.ngOnInit();
+    stateEditorService.onStateEditorInitialized.emit(state);
+
+    expect(component.interactionIsDisabled).toBe(false);
+    expect(component.hasLoaded).toBe(true);
+    expect(responsesService.onInitializeAnswerGroups.emit)
+      .toHaveBeenCalled();
+  });
+
+  it('should 2', () => {
+    const interactionCustomizationArgsValue = {
+      placeholder: {
+        value: null
+      },
+      rows: {
+        value: 0
+      }
+    };
+    component.interactionEditorIsShown = true;
+    spyOn(urlInterpolationService, 'getStaticImageUrl').and.returnValue(
+      'image'
+    );
+    spyOn(explorationHtmlFormatterService, 'getInteractionHtml')
+      .and.returnValue('htmlValue');
+    stateInteractionIdService.savedMemento = 'interactionID';
+
+    component.toggleInteractionEditor();
+
+    expect(component.getStaticImageUrl('image')).toEqual('image');
+    expect(component.interactionEditorIsShown).toBe(false);
+    expect(component._getInteractionPreviewTag(
+      interactionCustomizationArgsValue)).toBe('htmlValue');
+  });
+
+  it('should delete interaction when user click on cross btn', fakeAsync(() => {
+    mockNgbModal.modal = 'delete_interaction';
+
+    spyOn(stateSolutionService, 'saveDisplayedValue').and.stub();
+    spyOn(mockNgbModal, 'open').and.callFake((dlg, opt) => {
+      return (
+        {
+          result: Promise.resolve('success')
+        }
+      ) as NgbModalRef;
+    });
+
+    component.deleteInteraction();
+    tick();
+
+    expect(stateSolutionService.saveDisplayedValue)
+      .toHaveBeenCalled();
+  }));
+
+  it('should not delete interaction when user click on cross btn',
+    fakeAsync(() => {
+      mockNgbModal.modal = 'delete_interaction';
+
+      spyOn(stateSolutionService, 'saveDisplayedValue').and.stub();
+      spyOn(mockNgbModal, 'open').and.callFake((dlg, opt) => {
+        return (
+          {
+            result: Promise.reject('success')
+          }
+        ) as NgbModalRef;
+      });
+
+      component.deleteInteraction();
+      tick();
+
+      expect(stateSolutionService.saveDisplayedValue)
+        .not.toHaveBeenCalled();
+    }));
+
+  it('should close modal when user click cancel', fakeAsync(() => {
+    const mockEventEmitter = new EventEmitter();
+    mockNgbModal.modal = 'add_interaction';
+    stateContentService.savedMemento = new SubtitledHtml('html', 'contentID');
+    stateCustomizationArgsService.savedMemento = {
+      useFractionForDivision: false,
+      customOskLetters: {
+        value: ['wrok', 'done']
+      }
+    };
+    component.interactionId = 'EndExploration';
+    component.interactionIsDisabled = false;
+    component.updateDefaultTerminalStateContentIfEmpty();
+
+    spyOn(mockNgbModal, 'open').and.callFake((dlg, opt) => {
+      return (
+        { componentInstance: {
+          showMarkAllAudioAsNeedingUpdateModalIfRequired: mockEventEmitter
+        },
+        result: Promise.reject('reject')
+        }
+      ) as NgbModalRef;
+    });
+    spyOn(stateNextContentIdIndexService, 'restoreFromMemento').and.stub();
+    spyOn(editabilityService, 'isEditable').and.returnValue(true);
+    spyOn(contextService, 'isExplorationLinkedToStory').and.returnValue(true);
+    spyOn(stateEditorService.onHandleCustomArgsUpdate, 'emit').and.stub();
+
+    component.openInteractionCustomizerModal();
+    tick();
+    mockEventEmitter.emit();
+
+    expect(stateNextContentIdIndexService.restoreFromMemento)
+      .toHaveBeenCalled();
+
+    component.interactionIsDisabled = true;
+    tick();
+    component.openInteractionCustomizerModal();
+
+    expect(stateNextContentIdIndexService.restoreFromMemento)
+      .toHaveBeenCalledTimes(1);
+  }));
+
+  it('should save interaction when user click okay', fakeAsync(() => {
+    stateNextContentIdIndexService.displayed = 2;
+    stateNextContentIdIndexService.savedMemento = 1;
+    stateInteractionIdService.displayed = 'EndExploration';
+    stateInteractionIdService.savedMemento = 'InteractiveMap';
+    component.DEFAULT_TERMINAL_STATE_CONTENT = 'HTML Content';
+    stateContentService.savedMemento = SubtitledHtml.createDefault(
+      '', 'contentID');
+    stateContentService.displayed = SubtitledHtml.createDefault(
+      '', 'contentID2');
+    stateCustomizationArgsService.savedMemento = {
+      latitude: {
+        value: 35
+      },
+      longitude: {
+        value: 20
+      },
+      zoom: {
+        value: 8
+      }
+    };
+    stateCustomizationArgsService.displayed = {
+      recommendedExplorationIds: {
+        value: ['null']
+      }
+    };
+
+    const mockEventEmitter = new EventEmitter();
+    mockNgbModal.modal = 'add_interaction';
+    spyOn(mockNgbModal, 'open').and.callFake((dlg, opt) => {
+      return (
+        { componentInstance: {
+          showMarkAllAudioAsNeedingUpdateModalIfRequired: mockEventEmitter
+        },
+        result: Promise.resolve('success')
+        }
+      ) as NgbModalRef;
+    });
+
+    spyOn(interactionDetailsCacheService, 'set').and.stub();
+    spyOn(stateNextContentIdIndexService, 'saveDisplayedValue').and.stub();
+    spyOn(stateContentService, 'saveDisplayedValue').and.stub();
+    spyOn(stateNextContentIdIndexService, 'restoreFromMemento').and.stub();
+    spyOn(editabilityService, 'isEditable').and.returnValue(true);
+    spyOn(contextService, 'isExplorationLinkedToStory').and.returnValue(true);
+    spyOn(stateEditorService.onHandleCustomArgsUpdate, 'emit').and.stub();
+
+    component.openInteractionCustomizerModal();
+    tick();
+
+    expect(stateEditorService.onHandleCustomArgsUpdate.emit).toHaveBeenCalled();
+    expect(stateContentService.saveDisplayedValue).toHaveBeenCalled();
+  }));
+
+  it('should through error', () => {
+    expect(() => {
+      component.throwError(undefined);
+    }).toThrowError('Expected stateData to be defined but received undefined');
+  });
+});
