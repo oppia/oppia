4c3115fa6a5a12698de066ec4d9976172828d4f8
Aman Jolly <aman.amanjolly@gmail.com>
2022-02-11 13:36:53 +0530

    Debug

--- core/schema_utils.py
+++ core/schema_utils.py
@@ -212,7 +212,7 @@ def normalize_against_schema(
     elif schema[SCHEMA_KEY_TYPE] == SCHEMA_TYPE_UNICODE:
         # TODO(#14028): Use just one type.
         assert isinstance(obj, (str, bytes)), (
-            'Expected unicode string, received %s' % obj)
+            'Expected unicode string, received %s, schema %s' % (obj,sch))
         if isinstance(obj, bytes):
             obj = obj.decode('utf-8')
         else:
