10f86fe4198ad2f8fb1b77c4ba12dc7f31f3a0e6
ashish-patwal <ashishpatwal147@gmail.com>
2022-02-11 10:50:47 +0530

    removed user_query_domain*.py from excluded

--- scripts/run_mypy_checks.py
+++ scripts/run_mypy_checks.py
@@ -189,8 +189,6 @@ NOT_FULLY_COVERED_FILES = [
     'core/domain/translation_services_test.py',
     'core/domain/user_domain.py',
     'core/domain/user_domain_test.py',
-    'core/domain/user_query_domain.py',
-    'core/domain/user_query_domain_test.py',
     'core/domain/user_query_services.py',
     'core/domain/user_query_services_test.py',
     'core/domain/user_services.py',
