602c71bbd68046049c82d25f8f4a5d5afa7415c5
Soumyajyoti Dey <deysoumyajyoti2017@gmail.com>
2022-02-11 15:08:09 +0530

    Fix part of #14187: Add remaining frontend tests for some files for 100% coverage (#14796)

---
+++ core/templates/pages/exploration-player-page/services/refresher-exploration-confirmation-modal.service.spec.ts
@@ -0,0 +1,56 @@
+// Copyright 2022 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Unit tests for refresher exploration confirmation modal service
+ */
+
+import { EventEmitter } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { RefresherExplorationConfirmationModalService }
+  from './refresher-exploration-confirmation-modal.service';
+
+describe('Refresher exploration confirmation modal service', () => {
+  let recms: RefresherExplorationConfirmationModalService;
+  let ngbModal: NgbModal;
+
+  beforeEach(() => {
+    recms = TestBed.inject(RefresherExplorationConfirmationModalService);
+    ngbModal = TestBed.inject(NgbModal);
+  });
+
+  it('should display redirect confirmation modal', () => {
+    const redirectConfirmationCallback = jasmine.createSpy(
+      'redirectConfirmationCallback', () => {}
+    );
+    const mockComponentInstance = {
+      confirmRedirectEventEmitter: new EventEmitter(),
+      refresherExplorationId: ''
+    };
+    const modalSpy = spyOn(ngbModal, 'open').and.callFake(() => {
+      return {
+        componentInstance: mockComponentInstance,
+        result: Promise.resolve()
+      } as NgbModalRef;
+    });
+
+    recms.displayRedirectConfirmationModal('abc', redirectConfirmationCallback);
+    expect(modalSpy).toHaveBeenCalled();
+
+    mockComponentInstance.confirmRedirectEventEmitter.emit();
+    expect(redirectConfirmationCallback).toHaveBeenCalled();
+    expect(mockComponentInstance.refresherExplorationId).toEqual('abc');
+  });
+});
