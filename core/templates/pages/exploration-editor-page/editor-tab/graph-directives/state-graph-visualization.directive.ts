66fdb2c4326824c55f03712da9c3b399c45ec92e
SanjaySajuJacob <sanjaysajujacob2@gmail.com>
2022-02-12 22:22:41 +0530

    append to error message

--- core/templates/pages/exploration-editor-page/editor-tab/graph-directives/state-graph-visualization.directive.ts
+++ core/templates/pages/exploration-editor-page/editor-tab/graph-directives/state-graph-visualization.directive.ts
@@ -181,13 +181,9 @@ angular.module('oppia').directive('stateGraphVisualization', [
                   origTranslations[0] = (
                     dimensions.w / 2 - nodeData[$scope.currentStateId()].x0 -
                     nodeData[$scope.currentStateId()].width / 2);
-                } catch (e) {
-                  console.error(
-                    e +
-                    '\ncurrentStateId(): ' + $scope.currentStateId(
-                    ) +
-                    '\nnodeData:' + nodeData + '\n\n\n'
-                  );
+                } catch (e : TypeError) {
+                  error.message += `\ncurrentStateId(): ${ $scope.currentStateId() }\nnodeData: ${ nodeData }`;
+                  throw error;
                 }
 
                 origTranslations[1] = (
