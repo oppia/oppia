9874803714be8a38195f0999a383b4721ffea03e
Pankaj Prajapati <42764756+paprajapati9@users.noreply.github.com>
2022-02-14 19:59:16 +0530

    Learner dahsboard fixes based on learner dashboard doc (#14898)
    * minor fixes on learner dashboard
    
    * added 3 dots design
    
    * lint fixes
    
    * increased frontend test coverage
    
    * css fixes
    
    * fixed rtl test

--- core/templates/pages/learner-dashboard-page/goals-tab.component.spec.ts
+++ core/templates/pages/learner-dashboard-page/goals-tab.component.spec.ts
@@ -339,4 +339,32 @@ describe('Goals tab Component', () => {
 
     expect(urlSpy).toHaveBeenCalled();
   });
+
+  it('should correctly show and hide the dropdown', () => {
+    expect(component.showThreeDotsDropdown).toBe(false);
+
+    component.toggleThreeDotsDropdown();
+    expect(component.showThreeDotsDropdown).toBe(true);
+
+    component.toggleThreeDotsDropdown();
+    expect(component.showThreeDotsDropdown).toBe(false);
+
+    component.toggleThreeDotsDropdown();
+    expect(component.showThreeDotsDropdown).toBe(true);
+
+    let fakeClickAwayEvent = new MouseEvent('click');
+    Object.defineProperty(
+      fakeClickAwayEvent,
+      'target',
+      {value: document.createElement('div')});
+    component.onDocumentClick(fakeClickAwayEvent);
+    fixture.detectChanges();
+    expect(component.showThreeDotsDropdown).toBe(false);
+
+    // Three dots are not shown when no goals are present.
+    component.dropdownRef = null;
+    component.onDocumentClick(fakeClickAwayEvent);
+    fixture.detectChanges();
+    expect(component.showThreeDotsDropdown).toBe(false);
+  });
 });
