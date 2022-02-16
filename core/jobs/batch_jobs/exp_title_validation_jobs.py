88cf31f615705e6ee72f26fce604240f6f631a67
Hitesh Tomar <lkbhitesh07@gmail.com>
2022-02-16 07:22:01 +0530

    created issue for todo and added

--- core/jobs/batch_jobs/exp_title_validation_jobs.py
+++ core/jobs/batch_jobs/exp_title_validation_jobs.py
@@ -31,7 +31,7 @@ import apache_beam as beam
 (exp_models, ) = models.Registry.import_models([models.NAMES.exploration])
 
 
-# TODO(#13822): Remove this job after the fix.
+# TODO(#14943): Remove this job after the fix.
 class GetNumberOfExpExceedsMaxTitleLengthJob(base_jobs.JobBase):
     """Job that returns exploration having title length more than 36."""
 
