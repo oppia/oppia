d1a990d5a30b82e9ae592bfc6767f430d4d1d041
Rijuta-s <rijuta_s@me.iitr.ac.in>
2022-02-20 16:58:29 +0530

    Fixing typescript issue

--- core/templates/services/search.service.ts
+++ core/templates/services/search.service.ts
@@ -53,7 +53,7 @@ export class SearchService {
   // and we need to do non-null assertion, for more information see
   // https://github.com/oppia/oppia/wiki/Guide-on-defining-types#ts-7-1
   private _lastQuery!: string;
-  private _searchOffset!: number;
+  private _searchOffset!: number | null;
   private _lastSelectedCategories: SelectionList = {};
   private _lastSelectedLanguageCodes: SelectionList = {};
   private _isCurrentlyFetchingResults = false;
