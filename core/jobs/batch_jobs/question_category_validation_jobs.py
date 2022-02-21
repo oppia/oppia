eeace0538db80b0ca795a30a7f251d1543a46407
Lawful2002 <sharshvardhan101@gmail.com>
2022-02-21 10:23:39 -0800

    added beam job

---
+++ core/jobs/batch_jobs/question_category_validation_jobs.py
@@ -0,0 +1,90 @@
+# coding: utf-8
+#
+# Copyright 2022 The Oppia Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Validation Jobs for exploration category"""
+
+from __future__ import annotations
+
+from core.domain import exp_fetchers
+from core.jobs import base_jobs
+from core.jobs.io import ndb_io
+from core.jobs.transforms import job_result_transforms
+from core.jobs.types import job_run_result
+from core.platform import models
+from core.constants import constants
+
+import apache_beam as beam
+
+(exp_models, ) = models.Registry.import_models([models.NAMES.exploration])
+
+
+# TODO(#14943): Remove this job after we fix the exploration titles length.
+class GetNumberOfExpWithInvalidCategoryJob(base_jobs.JobBase):
+    """Job that returns exploration ids having categories which aren't included in constants.ts"""
+
+    def run(self) -> beam.PCollection[job_run_result.JobRunResult]:
+        """Returns PCollection of invalid explorations with their id and
+        category.
+
+        Returns:
+            PCollection. Returns PCollection of invalid explorations with
+            their id and category.
+        """
+        total_explorations = (
+            self.pipeline
+            | 'Get all Explorations' >> ndb_io.GetModels(
+                exp_models.ExplorationModel.get_all(include_deleted=False))
+            | 'Get exploration from model' >> beam.Map(
+                exp_fetchers.get_exploration_from_model)
+        )
+
+        exp_ids_with_category_not_in_contants = (
+            total_explorations
+            | 'Combine exploration id and categoty' >> beam.Map(
+                lambda exp: (exp.id, exp.category))
+            | 'Filter exploraton with category not in constants.ts' >>
+                beam.Filter(lambda exp: not (exp[1] in constants.ALL_CATEGORIES))
+        )
+
+        report_number_of_exps_queried = (
+            total_explorations
+            | 'Report count of exp models' >> (
+                job_result_transforms.CountObjectsToJobRunResult('EXPS'))
+        )
+
+        report_number_of_invalid_exps = (
+            exp_ids_with_category_not_in_contants
+            | 'Report count of invalid exp models' >> (
+                job_result_transforms.CountObjectsToJobRunResult('INVALID'))
+        )
+
+        report_invalid_ids_and_their_actual_len = (
+            exp_ids_with_category_not_in_contants
+            | 'Save info on invalid exps' >> beam.Map(
+                lambda objects: job_run_result.JobRunResult.as_stderr(
+                    'The id of exp is %s and its category is %s'
+                    % (objects[0], objects[1])
+                ))
+        )
+
+        return (
+            (
+                report_number_of_exps_queried,
+                report_number_of_invalid_exps,
+                report_invalid_ids_and_their_actual_len
+            )
+            | 'Combine results' >> beam.Flatten()
+        )
