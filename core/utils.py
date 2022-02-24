246bc8570bf4a77255665ed69530197c3eb147b2
Darshan Abhaykumar <darshanabhay@Hotmail.com>
2022-02-24 23:16:06 +0530

    Undo 1 fix

--- core/utils.py
+++ core/utils.py
@@ -288,7 +288,7 @@ def get_random_int(upper_bound: int) -> int:
     Returns:
         int. Randomly generated integer less than the upper_bound.
     """
-    assert upper_bound >= 0 and isinstance(upper_bound, int), 'Only positive integers allowed'
+    assert upper_bound >= 0 and isinstance(upper_bound, int), 'Only positive integers allowed' # pylint: disable=line-too-long
 
     generator = random.SystemRandom()
     return generator.randrange(0, stop=upper_bound)
@@ -303,7 +303,7 @@ def get_random_choice(alist: List[T]) -> T:
     Returns:
         *. Random element choosen from the passed input list.
     """
-    assert isinstance(alist, list) and len(alist) > 0, 'Only non-empty lists allowed'
+    assert isinstance(alist, list) and len(alist) > 0, 'Only non-empty lists allowed' # pylint: disable=line-too-long
 
     index = get_random_int(len(alist))
     return alist[index]
