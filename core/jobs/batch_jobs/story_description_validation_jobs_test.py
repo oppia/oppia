dcc40238e1965430f5e14450d00f4bda08376320
soumyo123-prog <deysoumyajyoti2017@gmail.com>
2022-02-28 15:57:30 +0530

    completed the job

---
+++ core/jobs/batch_jobs/story_description_validation_jobs_test.py
@@ -0,0 +1,120 @@
+# coding: utf-8
+#
+# Copyright 2022 The Oppia Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Unit tests for jobs.batch_jobs.story_description_validation_jobs."""
+
+from __future__ import annotations
+from core import feconf
+
+from core.jobs import job_test_utils
+from core.jobs.batch_jobs import story_description_validation_jobs
+from core.jobs.types import job_run_result
+from core.platform import models
+
+MYPY = False
+if MYPY: # pragma: no cover
+    from mypy_imports import story_models
+
+(story_models, ) = models.Registry.import_models([models.NAMES.story])
+
+
+class GetNumberOfStoriesExceedsMaxDescriptionLengthJobTests(
+    job_test_utils.JobTestBase):
+
+    JOB_CLASS = (
+        story_description_validation_jobs.
+            GetNumberOfStoriesExceedsMaxDescriptionLengthJob)
+
+    STORY_ID_1 = '1'
+    STORY_ID_2 = '2'
+    STORY_ID_3 = '3'
+
+    VALID_DESCRIPTION = 'description'
+    INVALID_DESCRIPTION = 'description' * 100
+
+    def setUp(self):
+        super().setUp()
+
+        # This is an invalid model with description length greater than 1000.
+        self.story_1 = self.create_model(
+            story_models.StoryModel,
+            id=self.STORY_ID_1,
+            title='story 1',
+            description=self.INVALID_DESCRIPTION,
+            language_code='en',
+            story_contents_schema_version=(
+                feconf.CURRENT_STORY_CONTENTS_SCHEMA_VERSION),
+            corresponding_topic_id='1',
+            url_fragment='story_1',
+        )
+
+        # This is an invalid model with description length greater than 1000.
+        self.story_2 = self.create_model(
+            story_models.StoryModel,
+            id=self.STORY_ID_2,
+            title='story 2',
+            description=self.INVALID_DESCRIPTION,
+            language_code='en',
+            story_contents_schema_version=(
+                feconf.CURRENT_STORY_CONTENTS_SCHEMA_VERSION),
+            corresponding_topic_id='1',
+            url_fragment='story_2',
+        )
+
+        # This is a valid model with description length less than 1000.
+        self.story_3 = self.create_model(
+            story_models.StoryModel,
+            id=self.STORY_ID_3,
+            title='story 3',
+            description=self.VALID_DESCRIPTION,
+            language_code='en',
+            story_contents_schema_version=(
+                feconf.CURRENT_STORY_CONTENTS_SCHEMA_VERSION),
+            corresponding_topic_id='1',
+            url_fragment='story_3',
+        )
+
+    def test_run_with_no_models(self) -> None:
+        self.assert_job_output_is([])
+
+    def test_run_with_single_valid_model(self) -> None:
+        self.put_multi([self.story_3])
+        self.assert_job_output_is([
+            job_run_result.JobRunResult.as_stdout('STORIES SUCCESS: 1')
+        ])
+
+    def test_run_with_single_invalid_model(self) -> None:
+        self.put_multi([self.story_1])
+        self.assert_job_output_is([
+            job_run_result.JobRunResult.as_stdout('STORIES SUCCESS: 1'),
+            job_run_result.JobRunResult.as_stdout('INVALID SUCCESS: 1'),
+            job_run_result.JobRunResult.as_stderr(
+                'The id of story is %s and its description length is %s'
+                % (self.STORY_ID_1, len(self.INVALID_DESCRIPTION))),
+        ])
+
+    def test_run_with_mixed_models(self) -> None:
+        self.put_multi([self.story_1, self.story_2, self.story_3])
+        self.assert_job_output_is([
+            job_run_result.JobRunResult.as_stdout('STORIES SUCCESS: 3'),
+            job_run_result.JobRunResult.as_stdout('INVALID SUCCESS: 2'),
+            job_run_result.JobRunResult.as_stderr(
+                'The id of story is %s and its description length is %s'
+                % (self.STORY_ID_1, len(self.INVALID_DESCRIPTION))),
+            job_run_result.JobRunResult.as_stderr(
+                'The id of story is %s and its description length is %s'
+                % (self.STORY_ID_2, len(self.INVALID_DESCRIPTION))),
+        ])
