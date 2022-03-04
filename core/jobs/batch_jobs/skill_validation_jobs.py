3268b896cdf008d3ae3b7451690f59b2238c6dbb
Lawful2002 <sharshvardhan101@gmail.com>
2022-03-04 02:15:30 -0800

    fixed typo

--- core/jobs/batch_jobs/skill_validation_jobs.py
+++ core/jobs/batch_jobs/skill_validation_jobs.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Validation Jobs for skills"""
+"""Validation jobs for skill models"""
 
 from __future__ import annotations
 
