4f97bcc3180d9c5bf21867fb8dfb37d9d78152e6
JG-11 <80846488+JeeveshGarg@users.noreply.github.com>
2022-03-07 14:03:35 +0530

    nit

--- core/templates/pages/delete-account-page/templates/delete-account-modal.component.ts
+++ core/templates/pages/delete-account-page/templates/delete-account-modal.component.ts
@@ -26,6 +26,9 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
   templateUrl: './delete-account-modal.component.html'
 })
 export class DeleteAccountModalComponent implements OnInit {
+  // These properties are initialized using Angular lifecycle hooks
+  // and we need to do non-null assertion, for more information see
+  // https://github.com/oppia/oppia/wiki/Guide-on-defining-types#ts-7-1
   expectedUsername!: string;
   username!: string;
   constructor(
