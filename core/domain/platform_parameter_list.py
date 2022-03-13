f7faa90441e4645e9e52ca084a9552bb7beef1eb
ashira friedman <ashiramagid@gmail.com>
2022-03-13 22:27:28 +0200

    af358 changed format for PARAM NAMES

--- core/domain/platform_parameter_list.py
+++ core/domain/platform_parameter_list.py
@@ -25,28 +25,23 @@ from core.domain import platform_parameter_registry as registry
 
 Registry = registry.Registry
 
-
-# TODO(#14419): Change naming style of Enum class from SCREAMING_SNAKE_CASE
-# to PascalCase and its values to UPPER_CASE. Because we want to be consistent
-# throughout the codebase according to the coding style guide.
-# https://github.com/oppia/oppia/wiki/Coding-style-guide
-class PARAM_NAMES(enum.Enum): # pylint: disable=invalid-name
+class ParamNames(enum.Enum): # pylint: disable=invalid-name
     """Enum for parameter names."""
 
-    dummy_feature = 'dummy_feature' # pylint: disable=invalid-name
-    dummy_parameter = 'dummy_parameter' # pylint: disable=invalid-name
+    DUMMY_FEATURE = 'DUMMY_FEATURE' # pylint: disable=invalid-name
+    DUMMY_PARAMETER = 'DUMMY_PARAMETER' # pylint: disable=invalid-name
 
 
 # Platform parameters should all be defined below.
 
 Registry.create_feature_flag(
-    PARAM_NAMES.dummy_feature,
+    ParamNames.DUMMY_FEATURE,
     'This is a dummy feature flag.',
     platform_parameter_domain.FeatureStages.DEV,
 )
 
 Registry.create_platform_parameter(
-    PARAM_NAMES.dummy_parameter,
+    ParamNames.DUMMY_PARAMETER,
     'This is a dummy platform parameter.',
     platform_parameter_domain.DataTypes.STRING
 )
