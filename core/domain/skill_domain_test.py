4296e0d1daa4ba106226f29ce00c5690baad3f31
Lawful2002 <sharshvardhan101@gmail.com>
2022-03-14 23:30:19 -0700

    fixed typo

--- core/domain/skill_domain_test.py
+++ core/domain/skill_domain_test.py
@@ -95,7 +95,7 @@ class SkillDomainUnitTests(test_utils.GenericTestBase):
     def test_valid_misconception_id_greater_than_zero(self):
         self.skill.next_misconception_id = -12
         self._assert_validation_error(
-            'Expected misconception ID to be greater than 0')
+            'Expected misconception ID to be >= 0')
 
     def test_get_all_html_content_strings(self):
         html_strings = self.skill.get_all_html_content_strings()
