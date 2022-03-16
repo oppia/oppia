307f2179de09d6d63238fd7023a30ecb53b58993
JG-11 <80846488+JeeveshGarg@users.noreply.github.com>
2022-03-16 20:41:34 +0530

    nit

--- core/templates/components/state-directives/rule-editor/rule-editor.component.ts
+++ core/templates/components/state-directives/rule-editor/rule-editor.component.ts
@@ -318,3 +318,325 @@ angular.module('oppia').component('ruleEditor', {
     }
   ]
 });
+
+// Copyright 2014 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Directive for the rule editor.
+ */
+
+ import { EventBusGroup } from 'app-events/event-bus.service';
+ import { ObjectFormValidityChangeEvent } from 'app-events/app-events';
+ 
+ require('components/forms/custom-forms-directives/html-select.component.ts');
+ require('components/forms/custom-forms-directives/object-editor.directive.ts');
+ require(
+   'components/state-directives/rule-editor/rule-type-selector.directive.ts');
+ require(
+   'pages/exploration-editor-page/services/populate-rule-content-ids.service');
+ require(
+   'components/state-editor/state-editor-properties-services/' +
+   'state-editor.service.ts');
+ require('filters/string-utility-filters/convert-to-plain-text.filter.ts');
+ require('filters/string-utility-filters/truncate.filter.ts');
+ 
+ require('domain/utilities/url-interpolation.service.ts');
+ require(
+   'pages/exploration-editor-page/editor-tab/services/responses.service.ts');
+ require(
+   'components/state-editor/state-editor-properties-services/' +
+   'state-property.service.ts');
+ require(
+   'components/state-editor/state-editor-properties-services/' +
+   'state-interaction-id.service');
+ 
+ var DEFAULT_OBJECT_VALUES = require('objects/object_defaults.json');
+ // This directive controls an editor for selecting the type and input parameters
+ // to a rule. It also includes 'Cancel' and 'Save Answer' buttons which call
+ // This directive controls an editor for selecting the type and input parameters
+ // to a rule. It also includes 'Cancel' and 'Save Answer' buttons which call
+ // respective 'onCancelRuleEdit' and 'onSaveRule' callbacks when called. These
+ // buttons only show up if 'isEditingRuleInline' is true.
+ // respective 'onCancelRuleEdit' and 'onSaveRule' callbacks when called. These
+ // buttons only show up if 'isEditingRuleInline' is true.
+ angular.module('oppia').component('ruleEditor', {
+   bindings: {
+     isEditable: '=',
+     isEditingRuleInline: '&',
+     onCancelRuleEdit: '&',
+     onSaveRule: '&',
+     rule: '=',
+     modalId: '<'
+   },
+   template: require('./rule-editor.component.html'),
+   controllerAs: '$ctrl',
+   controller: [
+     '$scope', '$timeout', 'EventBusService',
+     'PopulateRuleContentIdsService', 'ResponsesService',
+     'StateEditorService', 'StateInteractionIdService',
+     'INTERACTION_SPECS',
+     function(
+         $scope, $timeout, EventBusService,
+         PopulateRuleContentIdsService, ResponsesService,
+         StateEditorService, StateInteractionIdService,
+         INTERACTION_SPECS) {
+       var ctrl = this;
+       // This returns the rule description string.
+       var computeRuleDescriptionFragments = function() {
+         if (!ctrl.rule.type) {
+           ctrl.ruleDescriptionFragments = [];
+           return '';
+         }
+ 
+         var ruleDescription = (
+           INTERACTION_SPECS[ctrl.currentInteractionId].rule_descriptions[
+             ctrl.rule.type]);
+ 
+         var PATTERN = /\{\{\s*(\w+)\s*\|\s*(\w+)\s*\}\}/;
+         var finalInputArray = ruleDescription.split(PATTERN);
+ 
+         var result = [];
+         for (var i = 0; i < finalInputArray.length; i += 3) {
+           result.push({
+             // Omit the leading noneditable string.
+             text: i !== 0 ? finalInputArray[i] : '',
+             type: 'noneditable'
+           });
+           if (i === finalInputArray.length - 1) {
+             break;
+           }
+ 
+           var answerChoices = ResponsesService.getAnswerChoices();
+ 
+           if (answerChoices) {
+             // This rule is for a multiple-choice, image-click, or item
+             // selection interaction.
+             // TODO(sll): Remove the need for this special case.
+             // This directive controls an editor for selecting the type and input parameters
+             // to a rule. It also includes 'Cancel' and 'Save Answer' buttons which call
+             // respective 'onCancelRuleEdit' and 'onSaveRule' callbacks when called. These
+             // buttons only show up if 'isEditingRuleInline' is true.
+             if (answerChoices.length > 0) {
+               if (
+                 finalInputArray[2] === 'SetOfTranslatableHtmlContentIds'
+               ) {
+                 ctrl.ruleDescriptionChoices = answerChoices.map(
+                   choice => ({
+                     id: choice.label,
+                     val: choice.val
+                   })
+                 );
+                 result.push({
+                   type: 'checkboxes',
+                   varName: finalInputArray[i + 1]
+                 });
+               } else if (
+                 finalInputArray[2] ===
+                 'ListOfSetsOfTranslatableHtmlContentIds'
+               ) {
+                 ctrl.ruleDescriptionChoices = answerChoices.map(
+                   choice => ({
+                     id: choice.label,
+                     val: choice.val
+                   })
+                 );
+                 result.push({
+                   type: 'dropdown',
+                   varName: finalInputArray[i + 1]
+                 });
+               } else if (
+                 finalInputArray[i + 2] === 'TranslatableHtmlContentId') {
+                 ctrl.ruleDescriptionChoices = answerChoices.map(
+                   function(choice) {
+                     return {
+                       id: choice.label,
+                       val: choice.val
+                     };
+                   }
+                 );
+                 result.push({
+                   type: 'dragAndDropHtmlStringSelect',
+                   varName: finalInputArray[i + 1]
+                 });
+               } else if (
+                 finalInputArray[i + 2] === 'DragAndDropPositiveInt') {
+                 ctrl.ruleDescriptionChoices = answerChoices.map(
+                   function(choice) {
+                     return {
+                       id: choice.label,
+                       val: choice.val
+                     };
+                   }
+                 );
+                 result.push({
+                   type: 'dragAndDropPositiveIntSelect',
+                   varName: finalInputArray[i + 1]
+                 });
+               } else {
+                 ctrl.ruleDescriptionChoices = answerChoices.map(
+                   function(choice) {
+                     return {
+                       id: choice.val,
+                       val: choice.label
+                     };
+                   }
+                 );
+                 result.push({
+                   type: 'select',
+                   varName: finalInputArray[i + 1]
+                 });
+                 if (!ctrl.rule.inputs[finalInputArray[i + 1]]) {
+                   ctrl.rule.inputs[finalInputArray[i + 1]] = (
+                     ctrl.ruleDescriptionChoices[0].id);
+                 }
+               }
+             } else {
+               ctrl.ruleDescriptionChoices = [];
+               result.push({
+                 text: ' [Error: No choices available] ',
+                 type: 'noneditable'
+               });
+             }
+           } else {
+             result.push({
+               type: finalInputArray[i + 2],
+               varName: finalInputArray[i + 1]
+             });
+           }
+         }
+ 
+         // The following is necessary in order to ensure that the
+         // object-editor HTML tags load correctly when the rule type is
+         // changed. This is an issue for, e.g., the MusicNotesInput
+         // interaction, where the rule inputs can sometimes be integers and
+         // sometimes be lists of music notes.
+         ctrl.ruleDescriptionFragments = [];
+         $timeout(function() {
+           ctrl.ruleDescriptionFragments = result;
+         }, 10);
+ 
+         return ruleDescription;
+       };
+ 
+       ctrl.onSelectNewRuleType = function(newRuleType) {
+         var oldRuleInputs = angular.copy(ctrl.rule.inputs) || {};
+         var oldRuleInputTypes = angular.copy(ctrl.rule.inputTypes) || {};
+ 
+         ctrl.rule.type = newRuleType;
+         ctrl.rule.inputs = {};
+         ctrl.rule.inputTypes = {};
+ 
+         var tmpRuleDescription = computeRuleDescriptionFragments();
+         // This provides the list of choices for the multiple-choice and
+         // image-click interactions.
+         var answerChoices = ResponsesService.getAnswerChoices();
+ 
+         // Finds the parameters and sets them in ctrl.rule.inputs.
+         var PATTERN = /\{\{\s*(\w+)\s*(\|\s*\w+\s*)?\}\}/;
+         while (true) {
+           if (!tmpRuleDescription.match(PATTERN)) {
+             break;
+           }
+           var varName = tmpRuleDescription.match(PATTERN)[1];
+           var varType = null;
+           if (tmpRuleDescription.match(PATTERN)[2]) {
+             varType = tmpRuleDescription.match(PATTERN)[2].substring(1);
+           }
+           ctrl.rule.inputTypes[varName] = varType;
+ 
+           // TODO(sll): Find a more robust way of doing this. For example,
+           // we could associate a particular varName with answerChoices
+           // depending on the interaction. This varName would take its
+           // default value from answerChoices, but other variables would
+           // take their default values from the DEFAULT_OBJECT_VALUES dict.
+           if (angular.equals(DEFAULT_OBJECT_VALUES[varType], [])) {
+             ctrl.rule.inputs[varName] = [];
+           } else if (answerChoices && answerChoices.length > 0) {
+             ctrl.rule.inputs[varName] = angular.copy(
+               answerChoices[0].val);
+           } else {
+             ctrl.rule.inputs[varName] = angular.copy(
+               DEFAULT_OBJECT_VALUES[varType]);
+           }
+ 
+           tmpRuleDescription = tmpRuleDescription.replace(PATTERN, ' ');
+         }
+ 
+         for (var key in ctrl.rule.inputs) {
+           if (oldRuleInputs.hasOwnProperty(key) &&
+             oldRuleInputTypes[key] === ctrl.rule.inputTypes[key]) {
+             ctrl.rule.inputs[key] = oldRuleInputs[key];
+           }
+         }
+       };
+ 
+       ctrl.cancelThisEdit = function() {
+         ctrl.onCancelRuleEdit();
+       };
+ 
+       ctrl.saveThisRule = function() {
+         PopulateRuleContentIdsService.populateNullRuleContentIds(ctrl.rule);
+         ctrl.onSaveRule();
+       };
+ 
+       ctrl.$onInit = function() {
+         ctrl.isInvalid = false;
+         /**
+           * Rule editors are usually used in two ways. Inline or in a modal.
+           * When in a modal, the save button is in the modal html and when
+           * inline it is in the rule editors template. When listening to the
+           * object validity change event, we need to know which button to
+           * disable. If we are inline, we disable the button in the
+           * rule-editor template. Which is why we using the if condition
+           * below.
+           */
+         if (ctrl.isEditingRuleInline()) {
+           const eventBusGroup: EventBusGroup = new EventBusGroup(
+             EventBusService);
+           ctrl.eventBusGroup = eventBusGroup;
+           ctrl.modalId = Symbol();
+           eventBusGroup.on(
+             ObjectFormValidityChangeEvent,
+             event => {
+               if (event.message.modalId === ctrl.modalId) {
+                 ctrl.isInvalid = event.message.value;
+                 $scope.$applyAsync();
+               }
+             });
+         }
+         ctrl.currentInteractionId = StateInteractionIdService.savedMemento;
+         ctrl.editRuleForm = {};
+         // Select a default rule type, if one isn't already selected.
+         if (ctrl.rule.type === null) {
+           ctrl.onSelectNewRuleType(ctrl.rule.type);
+         }
+         computeRuleDescriptionFragments();
+ 
+         $scope.$watch(
+           '$ctrl.ruleEditForm.form.$invalid', function(newValue) {
+             StateEditorService.updateCurrentRuleInputIsValid(!newValue);
+           }
+         );
+       };
+ 
+       ctrl.$onDestroy = function() {
+         if (ctrl.eventBusGroup) {
+           ctrl.eventBusGroup.unsubscribe();
+         }
+       };
+     }
+   ]
+ });
+ 
