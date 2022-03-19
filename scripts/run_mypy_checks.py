8fdfd12dd8609cd939aece8d05cb0124c0c8c20f
Darshan Abhaykumar <darshanabhay@Hotmail.com>
2022-03-19 22:51:02 +0530

    Adding file name

--- scripts/run_mypy_checks.py
+++ scripts/run_mypy_checks.py
@@ -117,6 +117,7 @@ NOT_FULLY_COVERED_FILES = [
     'core/domain/platform_parameter_domain_test.py',
     'core/domain/question_domain.py',
     'core/domain/question_domain_test.py',
+    'core/domain/question_fetchers.py',
     'core/domain/question_fetchers_test.py',
     'core/domain/question_services.py',
     'core/domain/question_services_test.py',
