91ad7a5a865feb44b41d1703c57e93e33b97ec7d
KIMBOH LOVETTE <kimbohlovette@gmail.com>
2022-03-21 06:55:17 +0100

    separating = from expressions

--- core/templates/components/skill-selector/select-skill-modal.component.ts
+++ core/templates/components/skill-selector/select-skill-modal.component.ts
@@ -42,7 +42,7 @@ export class SelectSkillModalComponent extends ConfirmOrCancelModal {
 
   confirm(): void {
     let totalSkills:
-    (ShortSkillSummary | SkillSummaryBackendDict | SkillSummary)[]= [];
+    (ShortSkillSummary | SkillSummaryBackendDict | SkillSummary)[] = [];
     if (this.skillSummaries) {
       totalSkills = [...this.skillSummaries];
     }
