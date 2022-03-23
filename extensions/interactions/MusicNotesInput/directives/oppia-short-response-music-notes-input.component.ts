7c0e956882b1cfb5a4a21f7a6253410cdebcdb51
mridul-netizen <mridulsetia18@gmail.com>
2022-03-24 00:26:46 +0530

    Fix Part of #9749: Migrates Music-Notes Input Interaction to Angular (#14841)
    * updated
    
    * updated
    
    * updated
    
    * indentation
    
    * updated
    
    * fixes
    
    * updated
    
    * updated
    
    * short-summary-fixed
    
    * resolve lints
    
    * e2e fixed
    
    * Revert "Adds Autofocus on exploration interactions (#12538)"
    
    This reverts commit 2d35d7c4353b742d4ce87a5d3b55c15de2846bc4.
    
    save-merge
    
    * updated
    
    * Revert "Revert "Adds Autofocus on exploration interactions (#12538)""
    
    This reverts commit 03e76a58723477393cbf1cf607f0afd80138888d.
    
    save-merge
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * nits
    
    * nit
    
    * nit
    
    * nits
    
    * nits
    
    * nit
    
    * lints
    
    * renamed files
    
    * updated
    
    * working
    
    * major nits
    
    * typings
    
    * updated
    
    * nits
    
    * nits
    
    * nits

---
+++ extensions/interactions/MusicNotesInput/directives/oppia-short-response-music-notes-input.component.ts
@@ -0,0 +1,59 @@
+// Copyright 2022 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Component for the music notes input short response.
+ *
+ * IMPORTANT NOTE: The naming convention for customization args that are passed
+ * into the directive is: the name of the parameter, followed by 'With',
+ * followed by the name of the arg.
+ */
+
+import { Component, Input, OnInit } from '@angular/core';
+import { downgradeComponent } from '@angular/upgrade/static';
+import { HtmlEscaperService } from 'services/html-escaper.service';
+
+@Component({
+  selector: 'oppia-short-response-music-notes-input',
+  templateUrl: './music-notes-input-short-response.component.html'
+})
+export class ShortResponseMusicNotesInput implements OnInit {
+  @Input() answer;
+  displayedAnswer: string;
+
+  constructor(
+    private htmlEscaperService: HtmlEscaperService
+  ) {}
+
+  ngOnInit(): void {
+    let _answer = this.htmlEscaperService.escapedJsonToObj(this.answer);
+    let _notes = [];
+    for (let i = 0; i < Object.keys(_answer).length; i++) {
+      if (_answer[i].readableNoteName) {
+        _notes.push(_answer[i].readableNoteName);
+      }
+    }
+
+    if (_notes.length > 0) {
+      this.displayedAnswer = _notes.join(', ');
+    } else {
+      this.displayedAnswer = 'No answer given.';
+    }
+  }
+}
+
+angular.module('oppia').directive(
+  'oppiaShortResponseMusicNotesInput', downgradeComponent(
+    {component: ShortResponseMusicNotesInput}
+  ) as angular.IDirectiveFactory);
