97b21d4dc820a8b8f497b5b3aa188641a4836cac
KIMBOH LOVETTE <kimbohlovette@gmail.com>
2022-03-26 02:23:49 +0100

    Refined the code and filtered redudant types

--- core/templates/components/skill-selector/select-skill-modal.component.ts
+++ core/templates/components/skill-selector/select-skill-modal.component.ts
@@ -19,7 +19,7 @@ import { Component } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { ConfirmOrCancelModal } from 'components/common-layout-directives/common-elements/confirm-or-cancel-modal.component';
 import { ShortSkillSummary } from 'domain/skill/short-skill-summary.model';
-import { SkillSummary, SkillSummaryBackendDict } from 'domain/skill/skill-summary.model';
+import { SkillSummaryBackendDict } from 'domain/skill/skill-summary.model';
 import { SkillsCategorizedByTopics } from 'pages/topics-and-skills-dashboard-page/skills-list/skills-list.component';
 
 @Component({
