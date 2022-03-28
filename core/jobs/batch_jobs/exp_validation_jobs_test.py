9fbe81cbccdef3bb189ad4067fd0dd8d48a72b17
sahil jhangar <souravjangar91@gmail.com>
2022-03-28 20:29:53 +0530

    altered comments

--- core/jobs/batch_jobs/exp_validation_jobs_test.py
+++ core/jobs/batch_jobs/exp_validation_jobs_test.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Unit tests for jobs.batch_jobs.exp_tags_validation_jobs."""
+"""Unit tests for jobs.batch_jobs.exp_validation_jobs."""
 
 from __future__ import annotations
 
