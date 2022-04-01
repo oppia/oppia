455fbf364bb1975d91171752dfb4b3342490b2ed
Kevin Thomas <kevintab@tutanota.com>
2022-04-01 14:32:50 -0700

    Revert "Fix part of #10474: Make typescript checks strict for some files. (#15049)"
    This reverts commit a826dfbb30e73bd0734309523b7732466e9c0fa0.

--- core/templates/services/generate-content-id.service.ts
+++ core/templates/services/generate-content-id.service.ts
@@ -70,9 +70,7 @@ export class GenerateContentIdService {
     // This function is used to generate content_ids for content that live in
     // the State domain. This includes hints, feedback, and customization args.
     const contentIdIndex = this.stateNextContentIdIndexService.displayed;
-    if (this.stateNextContentIdIndexService.displayed !== null) {
-      this.stateNextContentIdIndexService.displayed += 1;
-    }
+    this.stateNextContentIdIndexService.displayed += 1;
     return `${prefix}_${contentIdIndex}`;
   }
 
