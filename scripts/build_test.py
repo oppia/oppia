476f45a7f085d566ec74670ec5cc3fa7c6df66e1
SD-13 <sujaydey641@gmail.com>
2022-04-05 13:32:25 +0530

    update function names in other places

--- scripts/build_test.py
+++ scripts/build_test.py
@@ -204,7 +204,7 @@ class BuildTests(test_utils.GenericTestBase):
 
         # Generate a random hash dict for base.html.
         file_hashes = {base_filename: (
-            test_utils.generate_random_str_from_regex())}  # type: ignore[no-untyped-call]
+            test_utils.generate_random_hexa_str())}  # type: ignore[no-untyped-call]
         with self.assertRaisesRegex(  # type: ignore[no-untyped-call]
             ValueError, '%s is expected to contain MD5 hash' % base_filename):
             build._verify_filepath_hash(base_filename, file_hashes)  # pylint: disable=protected-access
@@ -219,7 +219,7 @@ class BuildTests(test_utils.GenericTestBase):
             build._verify_filepath_hash(bad_filepath, file_hashes)  # pylint: disable=protected-access
 
         hashed_base_filename = build._insert_hash(  # pylint: disable=protected-access
-            base_filename, test_utils.generate_random_str_from_regex())  # type: ignore[no-untyped-call]
+            base_filename, test_utils.generate_random_hexa_str())  # type: ignore[no-untyped-call]
         with self.assertRaisesRegex(  # type: ignore[no-untyped-call]
             KeyError,
             'Hash from file named %s does not match hash dict values' %
