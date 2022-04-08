f39023e2c9b2cbdd5f1931114d0c7dfbae0e060d
Ankita Saxena <ankitasonu24@gmail.com>
2022-04-08 13:04:33 +0100

    Update branch protection rule prompt

--- scripts/release_scripts/cut_release_or_hotfix_branch.py
+++ scripts/release_scripts/cut_release_or_hotfix_branch.py
@@ -318,13 +318,8 @@ def execute_branch_cut(target_version, hotfix_number):
             'branch protection rule and doing all the cherrypicks.')
 
     common.ask_user_to_confirm(
-        'Ask Sean (or Ben, if Sean isn\'t available) to create '
-        'a new branch protection rule by:\n'
-        '1. Going to this page: https://github.com/oppia/oppia/'
-        'settings/branch_protection_rules/new.\n'
-        '2. Typing in the full branch name %s.\n'
-        '3. Checking the box: Restrict who can push to matching '
-        'branches (then add the oppia/release-coordinators team)\n' % (
+        'Confirm with Sean (or Ben, if Sean isn\'t available) that '
+        'a new branch protection rule is created for %s' % (
             new_branch_name))
 
     print('')
