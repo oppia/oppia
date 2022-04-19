4ee0da88ab4215cded0d7b8a86bead23a43bb285
SD-13 <sujaydey641@gmail.com>
2022-04-19 22:05:15 +0530

    replace 'if' with assert

--- scripts/typescript_checks.py
+++ scripts/typescript_checks.py
@@ -77,8 +77,11 @@ def compile_and_check_typescript(config_path: str) -> None:
     if os.path.exists(COMPILED_JS_DIR):
         shutil.rmtree(COMPILED_JS_DIR)
 
-    if process.stdout is not None:
-        error_messages = list(iter(process.stdout.readline, ''))
+    # The value of process.stdout can be None but according to the
+    # arguments passed here, it should not be None.
+    # Hence add an assert.
+    assert process.stdout is not None
+    error_messages = list(iter(process.stdout.readline, ''))
 
     if error_messages:
         print('Errors found during compilation\n')
