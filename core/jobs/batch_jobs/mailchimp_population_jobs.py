d632d1ff1c46e5ee7196a350cc7aaaf60d4b0096
Akshay Anand <akshayanand681@gmail.com>
2022-04-21 20:24:57 +0530

    merged updated changes

--- core/jobs/batch_jobs/mailchimp_population_jobs.py
+++ core/jobs/batch_jobs/mailchimp_population_jobs.py
@@ -30,7 +30,7 @@ import apache_beam as beam
 import mailchimp3
 from mailchimp3 import mailchimpclient
 import result
-from typing import List
+from typing import Iterable, List
 
 MYPY = False
 if MYPY: # pragma: no cover
@@ -41,11 +41,37 @@ if MYPY: # pragma: no cover
     [models.NAMES.config, models.NAMES.user])
 
 
-@staticmethod
+class CombineItems(beam.CombineFn):  # type: ignore[misc]
+    """CombineFn for combining all user email, status tuples."""
+
+    def create_accumulator(self) -> List[str]:
+        """Base accumulator where the tuples are added."""
+        return []
+
+    def add_input(self, accumulator: List[str], email: str) -> List[str]:
+        """Append each tuple to the accumulator list."""
+        accumulator.append(email)
+        return accumulator
+
+    def merge_accumulators(
+        self, accumulators: Iterable[List[str]]
+    ) -> List[str]:
+        """Merging accumulators is just combining both of them into a single
+        list.
+        """
+        output_accumulator = []
+        for accumulator in accumulators:
+            output_accumulator.extend(accumulator)
+        return output_accumulator
+
+    def extract_output(self, accumulator: List[str]) -> List[str]:
+        """Output is the accumulator itself."""
+        return accumulator
+
+
 def _get_mailchimp_class() -> mailchimp3.MailChimp:
     """Returns the mailchimp api class. This is separated into a separate
     function to facilitate testing.
-
     NOTE: No other functionalities should be added to this function.
 
     Returns:
@@ -83,19 +109,20 @@ class SendBatchMailchimpRequest(beam.DoFn):
             JobRunResult. Job run result which is either 'Ok' or an error with
             corresponding error message.
         """
-        emails = sorted(emails)
-        emails = emails[
+        sorted_emails = sorted(emails)
+        selected_emails = sorted_emails[
             batch_index_dict * 500: (batch_index_dict + 1) * 500]
 
         if test_run:
             # There is a max limit of 1500 bytes for job output. Hence, only
             # returning first and last 5 emails in batch for testing.
-            yield result.Ok(','.join(emails[: 5] + emails[-5:]))
+            yield result.Ok(
+                ','.join(selected_emails[: 5] + selected_emails[-5:]))
             return
         mailchimp_data = []
 
         client = _get_mailchimp_class()
-        for email in emails:
+        for email in selected_emails:
             mailchimp_data.append({
                 'email_address': email,
                 'status': 'subscribed'
@@ -112,7 +139,7 @@ class SendBatchMailchimpRequest(beam.DoFn):
 
         response_emails_count = (
             len(response['new_members']) + len(response['updated_members']))
-        source_emails_count = len(emails)
+        source_emails_count = len(selected_emails)
         if response_emails_count == source_emails_count:
             yield result.Ok('Request successful')
         else:
@@ -146,7 +173,7 @@ class MailchimpPopulateJob(base_jobs.JobBase):
             | 'Get all UserEmailPreferencesModel' >> ndb_io.GetModels(
                 user_models.UserEmailPreferencesModel.get_all().filter(
                     user_models.UserEmailPreferencesModel.site_updates == True # pylint: disable=singleton-comparison
-                ).order('created_on'))
+                ))
             | 'Extract user ID' >> beam.Map(
                 lambda preferences_model: preferences_model.id)
         )
@@ -157,8 +184,7 @@ class MailchimpPopulateJob(base_jobs.JobBase):
         relevant_user_emails = (
             self.pipeline
             | 'Get all user settings models' >> ndb_io.GetModels(
-                user_models.UserSettingsModel.get_all().order(
-                    'created_on'))
+                user_models.UserSettingsModel.get_all())
             | 'Filter user models' >> (
                 beam.Filter(
                     lambda model, ids: model.id in ids, ids=valid_user_ids))
@@ -169,8 +195,7 @@ class MailchimpPopulateJob(base_jobs.JobBase):
             relevant_user_emails
             # A large batch size is given so that all emails are included in a
             # single list.
-            | 'Combine into a list' >> beam.transforms.util.BatchElements(
-                min_batch_size=50000, max_batch_size=50000)
+            | 'Combine into a list' >> beam.CombineGlobally(CombineItems())
             | 'Send mailchimp request for current batch' >> beam.ParDo(
                 SendBatchMailchimpRequest(), batch_index_dict=batch_index_dict,
                 test_run=False)
@@ -228,8 +253,7 @@ class MockMailchimpPopulateJob(base_jobs.JobBase):
             relevant_user_emails
             # A large batch size is given so that all emails are included in a
             # single list.
-            | 'Combine into a list' >> beam.transforms.util.BatchElements(
-                min_batch_size=50000, max_batch_size=50000)
+            | 'Combine into a list' >> beam.CombineGlobally(CombineItems())
             | 'Send mailchimp request for current batch' >> beam.ParDo(
                 SendBatchMailchimpRequest(), batch_index_dict=batch_index_dict,
                 test_run=True)
