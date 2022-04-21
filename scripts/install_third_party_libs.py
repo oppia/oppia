6c75432b50618a1f5e4babd49dee43e0f5eb14c7
YBCS <budha627@gmail.com>
2022-04-22 01:51:40 +0530

    fix lint

--- scripts/install_third_party_libs.py
+++ scripts/install_third_party_libs.py
@@ -37,7 +37,7 @@ PREREQUISITES = (
     ('six', '1.16.0', os.path.join('third_party', 'python_libs')),
     ('certifi', '2021.10.8', os.path.join(TOOLS_DIR, 'certifi-2021.10.8')),
     # This library is used very frequently so we install it to default location.
-    # If installed in third_party, for it to work correctly, we may have to add 
+    # If installed in third_party, for it to work correctly, we may have to add
     # it to path in many different places.
     ('typing-extensions', '4.0.1', '')
 )
