e4f9adcc0be633e9e09019948c5cb03955a6199f
Nik-09 <nikhil.agarwal.2019@gmail.com>
2022-04-23 21:37:45 +0530

    Adds base structure for topic jobs unit test.

--- core/jobs/batch_jobs/topic_migration_jobs_test.py
+++ core/jobs/batch_jobs/topic_migration_jobs_test.py
@@ -18,16 +18,47 @@
 
 from __future__ import annotations
 
+import copy
+import datetime
+
 from core import feconf
 from core.domain import caching_services
-from core.domain import topic_domain
-from core.domain import topic_fetchers
-from core.jobs import base_jobs
-from core.jobs.io import ndb_io
-from core.jobs.transforms import job_result_transforms
+from core.domain topic_domain
+from core.jobs import job_test_utils
+from core.jobs.batch_jobs import topic_migration_jobs
 from core.jobs.types import job_run_result
 from core.platform import models
 
-import apache_beam as beam
-import result
+MYPY = False
+if MYPY:
+    from mypy_imports import datastore_services
+    from mypy_imports import topic_models
 
+(topic_model, topic_summary_model) = models.Registry.import_models([
+    models.NAMES.Topic, models.NAMES.TopicSummaryModel])
+
+
+class MigrateTopicJobTests(job_test_utils.JobTestBase):
+    """TODO
+    """
+
+    def setUp(self) -> None:
+        # TODO.
+
+    def test_empty_storage(self) -> None:
+        # TODO.
+
+    def test_unmigrated_topic_with_unmigrated_prop_is_migrated(self) -> None:
+        # TODO.
+
+    def test_topic_summary_of_unmigrated_topic_is_updated(self) -> None:
+        # TODO.
+
+    def test_broken_cache_is_reported(self) -> None:
+        # TODO.
+
+    def test_broken_topic_is_not_migrated(self) -> None:
+        # TODO.
+
+    def test_migrated_topic_is_not_migrated(self) -> None:
+        # TODO.
