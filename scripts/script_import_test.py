ce30c61ed540bc830843de6c1f68ba94932b809d
YBCS <budha627@gmail.com>
2022-04-26 23:50:02 +0530

    remove whitespace

--- scripts/script_import_test.py
+++ scripts/script_import_test.py
@@ -79,5 +79,5 @@ class InstallThirdPartyLibsImportTests(test_utils.GenericTestBase):
                 '--user', '--prefix=', '--system',
                 sys.executable, '-m', 'pip', 'install',
                 'typing-extensions==4.0.1', '--target',
-                '', '--user', '--prefix=', '--system',                
+                '', '--user', '--prefix=', '--system',
             ])
