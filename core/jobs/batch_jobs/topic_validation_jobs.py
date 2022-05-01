e5c976e94c850b40c05b86ea01ec49baaea77f54
Gopi Vaibhav <gopivaibhav0@gmail.com>
2022-05-01 22:07:00 +0530

    Added .

--- core/jobs/batch_jobs/topic_validation_jobs.py
+++ core/jobs/batch_jobs/topic_validation_jobs.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Validation jobs for topic models"""
+"""Validation jobs for topic models."""
 
 from __future__ import annotations
 
