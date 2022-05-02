d7df0490f5bee54feadcb7a44a932498ab2ebd59
sahil jhangar <souravjangar91@gmail.com>
2022-05-02 15:11:39 +0530

    added ratings

--- scripts/run_mypy_checks.py
+++ scripts/run_mypy_checks.py
@@ -107,8 +107,6 @@ NOT_FULLY_COVERED_FILES = [
     'core/domain/question_fetchers_test.py',
     'core/domain/question_services.py',
     'core/domain/question_services_test.py',
-    'core/domain/rating_services.py',
-    'core/domain/rating_services_test.py',
     'core/domain/recommendations_services.py',
     'core/domain/recommendations_services_test.py',
     'core/domain/rights_manager.py',
