ed4daad661ad771878d5208d328dd42a04c460ae
Lawful2002 <sharshvardhan101@gmail.com>
2022-05-03 14:30:26 +0530

    fixed tests

--- core/domain/topic_services_test.py
+++ core/domain/topic_services_test.py
@@ -176,7 +176,8 @@ class TopicServicesUnitTests(test_utils.GenericTestBase):
             language_code='en',
             subtopics=[subtopic_dict],
             subtopic_schema_version=0,
-            story_reference_schema_version=0
+            story_reference_schema_version=0,
+            page_title_fragment_for_web='fragm'
         )
         commit_cmd_dicts = [commit_cmd.to_dict()]
         model.commit(
