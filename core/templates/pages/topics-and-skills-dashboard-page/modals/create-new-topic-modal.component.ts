208ba5fee6f28877b5dab28a8c5dc199eaf7e4cd
Harshvardhan Singh <73544247+Lawful2002@users.noreply.github.com>
2022-05-09 22:47:11 +0530

    Update core/templates/pages/topics-and-skills-dashboard-page/modals/create-new-topic-modal.component.ts
    Co-authored-by: Vojtěch Jelínek <vojtech.jelinek@hey.com>

--- core/templates/pages/topics-and-skills-dashboard-page/modals/create-new-topic-modal.component.ts
+++ core/templates/pages/topics-and-skills-dashboard-page/modals/create-new-topic-modal.component.ts
@@ -45,8 +45,8 @@ export class CreateNewTopicModalComponent extends ConfirmOrCancelModal {
 
   topicUrlFragmentExists: boolean = false;
   topicNameExists: boolean = false;
-  max_web_title_frag = constants.MAX_CHARS_IN_PAGE_TITLE_FRAGMENT_FOR_WEB;
-  min_web_title_frag = constants.MIN_CHARS_IN_PAGE_TITLE_FRAGMENT_FOR_WEB;
+  max_web_title_frag = AppConstants.MAX_CHARS_IN_PAGE_TITLE_FRAGMENT_FOR_WEB;
+  min_web_title_frag = AppConstants.MIN_CHARS_IN_PAGE_TITLE_FRAGMENT_FOR_WEB;
 
   constructor(
     private contextService: ContextService,
