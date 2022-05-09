bebd7af8b4ae503cf8e4ec37a189d5e0d3871552
JayVivarekar <jayviva@gmail.com>
2022-05-09 21:52:31 +0530

    Fix lint errors

--- scripts/install_third_party_libs.py
+++ scripts/install_third_party_libs.py
@@ -67,6 +67,7 @@ def install_prerequisite(package: Tuple[str]) -> None:
             raise Exception(
                 'Error installing prerequisite %s' % package_name) from e
 
+
 # Add third_party to path. Some scripts access the scripts layer even before
 # python_libs is added to path.
 _THIRD_PARTY_PATH = os.path.join(os.getcwd(), 'third_party', 'python_libs')
