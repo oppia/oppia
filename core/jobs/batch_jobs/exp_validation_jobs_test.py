19c624560ae4ec5f68495f012de791fafe75a07a
Soumyajyoti Dey <deysoumyajyoti2017@gmail.com>
2022-05-13 17:50:20 +0530

    added tests for individual invalidity types

--- core/jobs/batch_jobs/exp_validation_jobs_test.py
+++ core/jobs/batch_jobs/exp_validation_jobs_test.py
@@ -47,6 +47,10 @@ class GetNumberOfInvalidExplorationsJobTests(job_test_utils.JobTestBase):
     EXPLORATION_ID_3 = '3'
     EXPLORATION_ID_4 = '4'
     EXPLORATION_ID_5 = '5'
+    EXPLORATION_ID_6 = '6'
+    EXPLORATION_ID_7 = '7'
+    EXPLORATION_ID_8 = '8'
+    EXPLORATION_ID_9 = '9'
 
     STATE_1 = state_domain.State.create_default_state(
         feconf.DEFAULT_INIT_STATE_NAME, is_initial_state=True).to_dict()
@@ -286,6 +290,99 @@ class GetNumberOfInvalidExplorationsJobTests(job_test_utils.JobTestBase):
             }
         )
 
+        # Exploration model with invalid state classifier model id.
+        self.exp_6 = self.create_model(
+            exp_models.ExplorationModel,
+            id=self.EXPLORATION_ID_6,
+            title='title 6',
+            init_state_name=feconf.DEFAULT_INIT_STATE_NAME,
+            category=feconf.DEFAULT_EXPLORATION_CATEGORY,
+            objective=feconf.DEFAULT_EXPLORATION_OBJECTIVE,
+            language_code=constants.DEFAULT_LANGUAGE_CODE,
+            tags=['Topic'],
+            blurb='blurb',
+            author_notes='author notes',
+            states_schema_version=feconf.CURRENT_STATE_SCHEMA_VERSION,
+            param_specs={},
+            param_changes=[],
+            auto_tts_enabled=feconf.DEFAULT_AUTO_TTS_ENABLED,
+            correctness_feedback_enabled=False,
+            states={
+                feconf.DEFAULT_INIT_STATE_NAME: self.STATE_2,
+                'second state': self.STATE_3
+            }
+        )
+
+        # Exploration model with invalid param changes.
+        self.exp_7 = self.create_model(
+            exp_models.ExplorationModel,
+            id=self.EXPLORATION_ID_7,
+            title='title 7',
+            init_state_name=feconf.DEFAULT_INIT_STATE_NAME,
+            category=feconf.DEFAULT_EXPLORATION_CATEGORY,
+            objective=feconf.DEFAULT_EXPLORATION_OBJECTIVE,
+            language_code=constants.DEFAULT_LANGUAGE_CODE,
+            tags=['Topic'],
+            blurb='blurb',
+            author_notes='author notes',
+            states_schema_version=feconf.CURRENT_STATE_SCHEMA_VERSION,
+            param_specs={},
+            param_changes=self.PARAM_CHANGES_1,
+            auto_tts_enabled=feconf.DEFAULT_AUTO_TTS_ENABLED,
+            correctness_feedback_enabled=False,
+            states={
+                feconf.DEFAULT_INIT_STATE_NAME: self.STATE_1,
+            }
+        )
+
+        # Exploration model with invalid param specs.
+        self.exp_8 = self.create_model(
+            exp_models.ExplorationModel,
+            id=self.EXPLORATION_ID_8,
+            title='title 8',
+            init_state_name=feconf.DEFAULT_INIT_STATE_NAME,
+            category=feconf.DEFAULT_EXPLORATION_CATEGORY,
+            objective=feconf.DEFAULT_EXPLORATION_OBJECTIVE,
+            language_code=constants.DEFAULT_LANGUAGE_CODE,
+            tags=['Topic'],
+            blurb='blurb',
+            author_notes='author notes',
+            states_schema_version=feconf.CURRENT_STATE_SCHEMA_VERSION,
+            param_specs={
+                'ExampleParamOne': self.PARAM_SPECS,
+                'ExampleParamTwo': self.PARAM_SPECS
+            },
+            param_changes=[],
+            auto_tts_enabled=feconf.DEFAULT_AUTO_TTS_ENABLED,
+            correctness_feedback_enabled=False,
+            states={
+                feconf.DEFAULT_INIT_STATE_NAME: self.STATE_1
+            }
+        )
+
+        # Exploration model with invalid training data.
+        self.exp_9 = self.create_model(
+            exp_models.ExplorationModel,
+            id=self.EXPLORATION_ID_9,
+            title='title 9',
+            init_state_name=feconf.DEFAULT_INIT_STATE_NAME,
+            category=feconf.DEFAULT_EXPLORATION_CATEGORY,
+            objective=feconf.DEFAULT_EXPLORATION_OBJECTIVE,
+            language_code=constants.DEFAULT_LANGUAGE_CODE,
+            tags=['Topic'],
+            blurb='blurb',
+            author_notes='author notes',
+            states_schema_version=feconf.CURRENT_STATE_SCHEMA_VERSION,
+            param_specs={},
+            param_changes=[],
+            auto_tts_enabled=feconf.DEFAULT_AUTO_TTS_ENABLED,
+            correctness_feedback_enabled=False,
+            states={
+                'state 1': self.INVALID_STATE_1.to_dict(),
+                'state 2': self.INVALID_STATE_2.to_dict(),
+            }
+        )
+
         # Valid opportunity model.
         self.opportunity_1 = self.create_model(
             opportunity_models.ExplorationOpportunitySummaryModel,
@@ -325,6 +422,70 @@ class GetNumberOfInvalidExplorationsJobTests(job_test_utils.JobTestBase):
             topic_id='topic_id3',
             topic_name='a_topic name',
             story_id='story_id3',
+            story_title='A story title',
+            chapter_title='A chapter title',
+            content_count=20,
+            incomplete_translation_language_codes=['hi', 'ar'],
+            translation_counts={},
+            language_codes_needing_voice_artists=['en'],
+            language_codes_with_assigned_voice_artists=[]
+        )
+
+        # Valid opportunity model.
+        self.opportunity_4 = self.create_model(
+            opportunity_models.ExplorationOpportunitySummaryModel,
+            id=self.EXPLORATION_ID_6,
+            topic_id='topic_id6',
+            topic_name='a_topic name',
+            story_id='story_id6',
+            story_title='A story title',
+            chapter_title='A chapter title',
+            content_count=20,
+            incomplete_translation_language_codes=['hi', 'ar'],
+            translation_counts={},
+            language_codes_needing_voice_artists=['en'],
+            language_codes_with_assigned_voice_artists=[]
+        )
+
+        # Valid opportunity model.
+        self.opportunity_5 = self.create_model(
+            opportunity_models.ExplorationOpportunitySummaryModel,
+            id=self.EXPLORATION_ID_7,
+            topic_id='topic_id7',
+            topic_name='a_topic name',
+            story_id='story_id7',
+            story_title='A story title',
+            chapter_title='A chapter title',
+            content_count=20,
+            incomplete_translation_language_codes=['hi', 'ar'],
+            translation_counts={},
+            language_codes_needing_voice_artists=['en'],
+            language_codes_with_assigned_voice_artists=[]
+        )
+
+        # Valid opportunity model.
+        self.opportunity_6 = self.create_model(
+            opportunity_models.ExplorationOpportunitySummaryModel,
+            id=self.EXPLORATION_ID_8,
+            topic_id='topic_id8',
+            topic_name='a_topic name',
+            story_id='story_id8',
+            story_title='A story title',
+            chapter_title='A chapter title',
+            content_count=20,
+            incomplete_translation_language_codes=['hi', 'ar'],
+            translation_counts={},
+            language_codes_needing_voice_artists=['en'],
+            language_codes_with_assigned_voice_artists=[]
+        )
+
+        # Valid opportunity model.
+        self.opportunity_7 = self.create_model(
+            opportunity_models.ExplorationOpportunitySummaryModel,
+            id=self.EXPLORATION_ID_9,
+            topic_id='topic_id9',
+            topic_name='a_topic name',
+            story_id='story_id9',
             story_title='A story title',
             chapter_title='A chapter title',
             content_count=20,
@@ -378,6 +539,65 @@ class GetNumberOfInvalidExplorationsJobTests(job_test_utils.JobTestBase):
                 'with non-empty training data are %s' % (
                     self.EXPLORATION_ID_2,
                     ['state 1']
+                )
+            )
+        ])
+
+    def test_run_with_invalid_state_classifier_id(self) -> None:
+        self.put_multi([self.exp_6, self.opportunity_4])
+        self.assert_job_output_is([
+            job_run_result.JobRunResult.as_stdout('EXPS SUCCESS: 1'),
+            job_run_result.JobRunResult.as_stdout('CURATED EXPS SUCCESS: 1'),
+            job_run_result.JobRunResult.as_stdout(
+                'INVALID STATE CLASSIFIER SUCCESS: 1'),
+            job_run_result.JobRunResult.as_stderr(
+                'The id of exp is %s and the states having not None '
+                'classifier model id are %s' % (
+                    self.EXPLORATION_ID_6,
+                    [feconf.DEFAULT_INIT_STATE_NAME, 'second state'])
+            )
+        ])
+
+    def test_run_with_invalid_param_changes(self) -> None:
+        self.put_multi([self.exp_7, self.opportunity_5])
+        self.assert_job_output_is([
+            job_run_result.JobRunResult.as_stdout('EXPS SUCCESS: 1'),
+            job_run_result.JobRunResult.as_stdout('CURATED EXPS SUCCESS: 1'),
+            job_run_result.JobRunResult.as_stdout(
+                'INVALID PARAM CHANGES SUCCESS: 1'),
+            job_run_result.JobRunResult.as_stderr(
+                'The id of exp is %s and the length of '
+                'its param changes is %s' % (
+                    self.EXPLORATION_ID_7, len(self.PARAM_CHANGES_1)
+                )
+            )
+        ])
+
+    def test_run_with_invalid_param_specs(self) -> None:
+        self.put_multi([self.exp_8, self.opportunity_6])
+        self.assert_job_output_is([
+            job_run_result.JobRunResult.as_stdout('EXPS SUCCESS: 1'),
+            job_run_result.JobRunResult.as_stdout('CURATED EXPS SUCCESS: 1'),
+            job_run_result.JobRunResult.as_stdout(
+                'INVALID PARAM SPECS SUCCESS: 1'),
+            job_run_result.JobRunResult.as_stderr(
+                'The id of exp is %s and the length of '
+                'its param specs is %s' % (self.EXPLORATION_ID_8, 2)
+            )
+        ])
+
+    def test_run_with_invalid_training_data(self) -> None:
+        self.put_multi([self.exp_9, self.opportunity_7])
+        self.assert_job_output_is([
+            job_run_result.JobRunResult.as_stdout('EXPS SUCCESS: 1'),
+            job_run_result.JobRunResult.as_stdout('CURATED EXPS SUCCESS: 1'),
+            job_run_result.JobRunResult.as_stdout(
+                'INVALID TRAINING DATA SUCCESS: 1'),
+            job_run_result.JobRunResult.as_stderr(
+                'The id of exp is %s and the states having interaction '
+                'with non-empty training data are %s' % (
+                    self.EXPLORATION_ID_9,
+                    ['state 1', 'state 2']
                 )
             )
         ])
@@ -446,7 +666,7 @@ class GetNumberOfInvalidExplorationsJobTests(job_test_utils.JobTestBase):
             )
         ])
 
-    def test_with_non_curated_explorations(self) -> None:
+    def test_run_with_non_curated_explorations(self) -> None:
         self.put_multi([self.exp_4, self.exp_5])
         self.assert_job_output_is([
             job_run_result.JobRunResult.as_stdout('EXPS SUCCESS: 2')
