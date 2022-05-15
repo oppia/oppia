0b66dd06faa7e8b5c63670b25877731dac6457bd
paprajapati9 <paprajapati9@gmail.com>
2022-05-15 16:15:13 +0530

    minor change

--- core/templates/pages/exploration-player-page/layout-directives/exploration-footer.component.ts
+++ core/templates/pages/exploration-player-page/layout-directives/exploration-footer.component.ts
@@ -140,7 +140,7 @@ export class ExplorationFooterComponent {
           this.hintsAndSolutionsAreSupported = !resultsLoaded;
         });
       this.footerIsInQuestionPlayerMode = true;
-    } else {
+    } else if(this.explorationId) {
       // Fetching the number of checkpoints.
       this.getCheckpointCount();
       this.setLearnerHasViewedLessonInfoTooltip();
