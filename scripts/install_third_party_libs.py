88281c1e8551372a0953b9097a358f689b64af04
JayVivarekar <jayviva@gmail.com>
2022-05-17 20:20:04 +0530

    Thrd party libs test

--- scripts/install_third_party_libs.py
+++ scripts/install_third_party_libs.py
@@ -29,6 +29,11 @@ from typing import Tuple
 
 TOOLS_DIR = os.path.join(os.pardir, 'oppia_tools')
 
+# Add third_party to path. Some scripts access the scripts layer even before
+# python_libs is added to path.
+_THIRD_PARTY_PATH = os.path.join(os.getcwd(), 'third_party', 'python_libs')
+sys.path.insert(0, _THIRD_PARTY_PATH)
+
 # These libraries need to be installed before running or importing any script.
 
 PREREQUISITES = (
