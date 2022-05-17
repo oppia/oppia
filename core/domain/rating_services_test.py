ea66574ee3b0573a825e0314d9ed92f0689b112c
sahil jhangar <souravjangar91@gmail.com>
2022-05-17 19:00:17 +0530

    added todo

--- core/domain/rating_services_test.py
+++ core/domain/rating_services_test.py
@@ -174,20 +174,18 @@ class RatingServicesTests(test_utils.GenericTestBase):
         with self.assertRaisesRegex(  # type: ignore[no-untyped-call]
             ValueError, 'Expected the rating to be an integer, received 2'
             ):
-            # The argument new_rating of assign_rating_to_exploration() can
-            # only accept integer values, but for testing purposes here we are
-            # providing string value. Thus to silent MyPy error, we added an
-            # ignore statement here.
+            # TODO(#13059): After we fully type the codebase we plan to get
+            # rid of the tests that intentionally test wrong inputs that we
+            # can normally catch by typing.
             rating_services.assign_rating_to_exploration(
                 self.USER_ID_1, self.EXP_ID, '2')  # type: ignore[arg-type]
 
         with self.assertRaisesRegex(  # type: ignore[no-untyped-call]
             ValueError, 'Expected the rating to be an integer, received aaa'
             ):
-            # The argument new_rating of assign_rating_to_exploration() can
-            # only accept integer values, but for testing purposes here we are
-            # providing string value. Thus to silent MyPy error, we added an
-            # ignore statement here.
+            # TODO(#13059): After we fully type the codebase we plan to get
+            # rid of the tests that intentionally test wrong inputs that we
+            # can normally catch by typing.
             rating_services.assign_rating_to_exploration(
                 self.USER_ID_1, self.EXP_ID, 'aaa')  # type: ignore[arg-type]
 
