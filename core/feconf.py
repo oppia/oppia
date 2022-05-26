fe2a9331299d715f065299ab574e5a4fe74d7952
JayVivarekar <jayviva@gmail.com>
2022-05-26 21:24:33 +0530

    Fix CI tests

--- core/feconf.py
+++ core/feconf.py
@@ -25,6 +25,8 @@ import os
 
 from core.constants import constants
 
+from scripts import common # pylint: disable=unused-import
+
 from typing import Dict, List, Union
 
 from typing_extensions import TypedDict
