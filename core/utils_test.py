880a4c02e1bef6d3c6a5771d4801a002682e9904
Darshan Abhaykumar <darshanabhay@Hotmail.com>
2022-05-26 23:12:10 +0530

    Updating file

--- core/utils_test.py
+++ core/utils_test.py
@@ -901,8 +901,8 @@ class UtilsTests(test_utils.GenericTestBase):
     def test_get_exploration_components_from_dir_with_yaml_content(self) -> None: # pylint: disable=line-too-long
         img1_path = 'images/sample_Img.svg'
         img2_path = 'images/sample_Img2.svg'
-        img1_file_content = b'<svg width="100" height="100"><circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" /></svg>'  # pylint: disable=line-too-long
-        img2_file_content = b'<svg width="400" height="110"><rect width="300" height="100" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" /></svg>'  # pylint: disable=line-too-long
+        img1_file_content = b'<svg width="100" height="100"><circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" /></svg>\n'  # pylint: disable=line-too-long
+        img2_file_content = b'<svg width="400" height="110"><rect width="300" height="100" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" /></svg>\n'  # pylint: disable=line-too-long
         yaml_content = 'name: John Smith\ncontact:\n    home:   1012355532\n    office:  5002586256\naddress:\n  street: |\n            123 Tornado Alley\n            Suite 16            \n    city:   East Centerville\n    state:  KS'   # pylint: disable=line-too-long
         result = utils.get_exploration_components_from_dir(
             'core/tests/data/dummy_assets_yaml')
