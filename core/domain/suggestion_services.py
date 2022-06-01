591b696a1204ef28eac021d2b81a50f20084d63d
Sagang Wee <9004726+sagangwee@users.noreply.github.com>
2022-06-01 14:35:13 -0400

    Fix tests and linter

--- core/domain/suggestion_services.py
+++ core/domain/suggestion_services.py
@@ -784,9 +784,9 @@ def get_translation_suggestions_in_review_by_exp_ids(exp_ids, language_code):
 def get_suggestions_with_translatable_explorations(suggestions):
     """Filters the supplied suggestions for those suggestions that have
     translatable exploration content. That is, the following are true:
-        - The suggestion's change content corresponds to an existing exploration
-        content card.
-        - The suggestion's corresponding exploration allows edits.
+    - The suggestion's change content corresponds to an existing exploration
+    content card.
+    - The suggestion's corresponding exploration allows edits.
 
     Args:
         suggestions: list(Suggestion). List of translation suggestions to
@@ -809,7 +809,7 @@ def get_suggestions_with_translatable_explorations(suggestions):
 
         Returns:
             bool. Whether the supplied suggestion corresponds to a translatable
-                exploration content card.
+            exploration content card.
         """
         exploration = suggestion_exp_id_to_exp[suggestion.target_id]
         content_id_exists = False
@@ -821,17 +821,16 @@ def get_suggestions_with_translatable_explorations(suggestions):
             content_id_exists = exploration.states[
                 suggestion.change.state_name].has_content_id(
                     suggestion.change.content_id)
-
         return content_id_exists and exploration.edits_allowed
 
     suggestion_exp_ids = {
         suggestion.target_id for suggestion in suggestions}
     suggestion_exp_id_to_exp = exp_fetchers.get_multiple_explorations_by_id(
         list(suggestion_exp_ids))
-    return filter(
+    return list(filter(
         lambda suggestion: _has_translatable_exploration(
             suggestion, suggestion_exp_id_to_exp),
-        suggestions)
+        suggestions))
 
 
 def _get_plain_text_from_html_content_string(html_content_string):
