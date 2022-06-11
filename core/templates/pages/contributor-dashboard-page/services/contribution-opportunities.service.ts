1974abc80b3d4a0a24d46c6552add98d3282ed10
Sagang Wee <9004726+sagangwee@users.noreply.github.com>
2022-06-11 00:02:07 -0400

    Fix part of #15182: Organize translations in translation review page by lesson (#15295)

--- core/templates/pages/contributor-dashboard-page/services/contribution-opportunities.service.ts
+++ core/templates/pages/contributor-dashboard-page/services/contribution-opportunities.service.ts
@@ -119,6 +119,18 @@ export class ContributionOpportunitiesService {
     }
   }
 
+  async getReviewableTranslationOpportunitiesAsync(topicName: string):
+  Promise<ExplorationOpportunitiesDict> {
+    return this.contributionOpportunitiesBackendApiService
+      .fetchReviewableTranslationOpportunitiesAsync(topicName)
+      .then(({ opportunities }) => {
+        return {
+          opportunities: opportunities,
+          more: false
+        };
+      });
+  }
+
   async getTranslatableTopicNamesAsync(): Promise<string[]> {
     return this._getTranslatableTopicNamesAsync();
   }
