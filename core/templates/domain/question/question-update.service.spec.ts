598a731c6eb541f579104d192df4d199d3b470e5
shivam <shivamthalwal@gmail.com>
2022-06-13 22:17:27 +0530

    testing done for question update file

--- core/templates/domain/question/question-update.service.spec.ts
+++ core/templates/domain/question/question-update.service.spec.ts
@@ -16,86 +16,41 @@
  * @fileoverview Unit tests for question update service.
  */
 
-// TODO(#7222): Remove the following block of unnnecessary imports once
-// question-update.service.ts is upgraded to Angular 8.
-import { AnswerGroupObjectFactory } from
-  'domain/exploration/AnswerGroupObjectFactory';
-import { HintObjectFactory } from 'domain/exploration/HintObjectFactory';
-import { OutcomeObjectFactory } from
-  'domain/exploration/OutcomeObjectFactory';
-import { ParamChangeObjectFactory } from
-  'domain/exploration/ParamChangeObjectFactory';
-import { ParamChangesObjectFactory } from
-  'domain/exploration/ParamChangesObjectFactory';
-import { RuleObjectFactory } from 'domain/exploration/RuleObjectFactory';
-import { UnitsObjectFactory } from 'domain/objects/UnitsObjectFactory';
-import { WrittenTranslationObjectFactory } from
-  'domain/exploration/WrittenTranslationObjectFactory';
-import { WrittenTranslationsObjectFactory } from
-  'domain/exploration/WrittenTranslationsObjectFactory';
-import { UpgradedServices } from 'services/UpgradedServices';
-// ^^^ This block is to be removed.
-// TODO(#7222): Remove usage of importAllAngularServices once upgraded to
-// Angular 8.
-import { importAllAngularServices } from 'tests/unit-test-utils.ajs';
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { SubtitledHtml } from 'domain/exploration/subtitled-html.model';
+import { QuestionObjectFactory } from 'domain/question/QuestionObjectFactory';
+import { QuestionUndoRedoService } from 'domain/editor/undo_redo/question-undo-redo.service';
+import { QuestionUpdateService} from 'domain/question/question-update.service';
+import { StateObjectFactory } from 'domain/state/StateObjectFactory';
+import cloneDeep from 'lodash/cloneDeep';
 
-require('App.ts');
-require('domain/editor/undo_redo/question-undo-redo.service.ts');
-require('domain/question/QuestionObjectFactory.ts');
-require('domain/question/question-update.service.ts');
-require('domain/state/StateObjectFactory.ts');
-require(
-  'components/question-directives/question-editor/' +
-  'question-editor.component.ts');
+describe('Question update service', () => {
+  let questionUpdateService = null;
+  let questionObjectFactory = null;
+  let questionUndoRedoService = null;
+  let stateObjectFactory = null;
+  let sampleQuestion = null;
+  let sampleStateDict = null;
+  let expectedOutputStateDict = null;
+  let expectedOutputState = null;
+  let sampleQuestionBackendObject = null;
 
-describe('Question update service', function() {
-  var QuestionUpdateService = null;
-  var QuestionObjectFactory = null;
-  var QuestionUndoRedoService = null;
-  var StateObjectFactory = null;
-  var sampleQuestion = null;
-  var sampleStateDict = null;
-  var expectedOutputStateDict = null;
-  var expectedOutputState = null;
-  var sampleQuestionBackendObject = null;
-  importAllAngularServices();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        QuestionUpdateService,
+        QuestionObjectFactory,
+        QuestionUndoRedoService,
+        StateObjectFactory
+      ]
+    });
 
-  beforeEach(angular.mock.module('oppia'));
-  beforeEach(angular.mock.module('oppia', function($provide) {
-    $provide.value(
-      'AnswerGroupObjectFactory', new AnswerGroupObjectFactory(
-        new OutcomeObjectFactory(),
-        new RuleObjectFactory()));
-    $provide.value(
-      'HintObjectFactory', new HintObjectFactory());
-    $provide.value(
-      'OutcomeObjectFactory', new OutcomeObjectFactory());
-    $provide.value('ParamChangeObjectFactory', new ParamChangeObjectFactory());
-    $provide.value(
-      'ParamChangesObjectFactory', new ParamChangesObjectFactory(
-        new ParamChangeObjectFactory()));
-    $provide.value('RuleObjectFactory', new RuleObjectFactory());
-    $provide.value('UnitsObjectFactory', new UnitsObjectFactory());
-    $provide.value(
-      'WrittenTranslationObjectFactory',
-      new WrittenTranslationObjectFactory());
-    $provide.value(
-      'WrittenTranslationsObjectFactory',
-      new WrittenTranslationsObjectFactory(
-        new WrittenTranslationObjectFactory()));
-  }));
-  beforeEach(angular.mock.module('oppia', function($provide) {
-    var ugs = new UpgradedServices();
-    for (let [key, value] of Object.entries(ugs.getUpgradedServices())) {
-      $provide.value(key, value);
-    }
-  }));
-  beforeEach(angular.mock.inject(function($injector) {
-    QuestionUpdateService = $injector.get('QuestionUpdateService');
-    QuestionObjectFactory = $injector.get('QuestionObjectFactory');
-    QuestionUndoRedoService = $injector.get('QuestionUndoRedoService');
-    StateObjectFactory = $injector.get('StateObjectFactory');
+    questionUpdateService = TestBed.inject(QuestionUpdateService);
+    questionObjectFactory = TestBed.inject(QuestionObjectFactory);
+    questionUndoRedoService = TestBed.inject(QuestionUndoRedoService);
+    stateObjectFactory = TestBed.inject(StateObjectFactory);
 
     sampleStateDict = {
       name: 'question',
@@ -221,7 +176,7 @@ describe('Question update service', function() {
       }
     };
 
-    expectedOutputState = StateObjectFactory.createFromBackendDict(
+    expectedOutputState = stateObjectFactory.createFromBackendDict(
       'question', expectedOutputStateDict);
 
     sampleQuestionBackendObject = {
@@ -230,45 +185,47 @@ describe('Question update service', function() {
       language_code: 'en',
       version: 1
     };
-    sampleQuestion = QuestionObjectFactory.createFromBackendDict(
-      sampleQuestionBackendObject);
-  }));
 
-  it('should update the language code of the question', function() {
+    sampleQuestion = questionObjectFactory.createFromBackendDict(
+      sampleQuestionBackendObject);
+  });
+  it('should update the language code of the question', () => {
     expect(sampleQuestion.getLanguageCode()).toEqual('en');
-    QuestionUpdateService.setQuestionLanguageCode(sampleQuestion, 'zh');
+
+    questionUpdateService.setQuestionLanguageCode(sampleQuestion, 'zh');
     expect(sampleQuestion.getLanguageCode()).toEqual('zh');
-    QuestionUndoRedoService.undoChange(sampleQuestion);
+
+    questionUndoRedoService.undoChange(sampleQuestion);
     expect(sampleQuestion.getLanguageCode()).toEqual('en');
   });
 
-  it('should update the state data of the question', function() {
-    var oldStateData = angular.copy(sampleQuestion.getStateData());
-    var updateFunction = function() {
-      var stateData = sampleQuestion.getStateData();
+  it('should update the state data of the question', () => {
+    let oldStateData = cloneDeep(sampleQuestion.getStateData());
+    let updateFunction = () => {
+      let stateData = sampleQuestion.getStateData();
       stateData.content = SubtitledHtml.createDefault(
         'test content', 'content');
     };
-    QuestionUpdateService.setQuestionStateData(
+
+    questionUpdateService.setQuestionStateData(
       sampleQuestion, updateFunction);
     expect(sampleQuestion.getStateData()).toEqual(expectedOutputState);
-    QuestionUndoRedoService.undoChange(sampleQuestion);
+
+    questionUndoRedoService.undoChange(sampleQuestion);
     expect(sampleQuestion.getStateData()).toEqual(oldStateData);
   });
 
   it('should set question inapplicable skills misconception ids when ' +
-    'calling \'setQuestionInapplicableSkillMisconceptionIds\'', function() {
+    'calling \'setQuestionInapplicableSkillMisconceptionIds\'', () => {
     expect(sampleQuestion.getInapplicableSkillMisconceptionIds())
       .toBe(undefined);
 
-    QuestionUpdateService.setQuestionInapplicableSkillMisconceptionIds(
+    questionUpdateService.setQuestionInapplicableSkillMisconceptionIds(
       sampleQuestion, ['id1']);
-
     expect(sampleQuestion.getInapplicableSkillMisconceptionIds())
       .toEqual(['id1']);
 
-    QuestionUndoRedoService.undoChange(sampleQuestion);
-
+    questionUndoRedoService.undoChange(sampleQuestion);
     expect(sampleQuestion.getInapplicableSkillMisconceptionIds())
       .toBe(undefined);
   });
