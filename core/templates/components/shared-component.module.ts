823964e994e5d8783ef90dc981a4a68335c5ae59
shivam <shivamthalwal@gmail.com>
2022-06-14 04:54:55 +0530

    removed from shared folder

--- core/templates/components/shared-component.module.ts
+++ core/templates/components/shared-component.module.ts
@@ -124,10 +124,7 @@ import { SolutionEditor } from './state-directives/solution-editor/solution-edit
 import { OutcomeDestinationEditorComponent } from './state-directives/outcome-editor/outcome-destination-editor.component';
 import { StateSolutionEditorComponent } from './state-editor/state-solution-editor/state-solution-editor.component';
 import { StateInteractionEditorComponent } from './state-editor/state-interaction-editor/state-interaction-editor.component';
-import { TrainingPanelComponent } from 'pages/exploration-editor-page/editor-tab/training-panel/training-panel.component';
-import { TrainingModalComponent } from 'pages/exploration-editor-page/editor-tab/training-panel/training-modal.component';
-import { TrainingDataEditorPanelServiceModalComponent } from 'pages/exploration-editor-page/editor-tab/training-panel/training-data-editor-panel-modal.component';
-import { TestInteractionPanel } from 'pages/exploration-editor-page/editor-tab/test-interaction-panel/test-interaction-panel.component';
+
 // Pipes.
 import { StringUtilityPipesModule } from 'filters/string-utility-filters/string-utility-pipes.module';
 import { SummarizeNonnegativeNumberPipe } from 'filters/summarize-nonnegative-number.pipe';
@@ -268,10 +265,6 @@ import { UnsavedChangesStatusInfoModalComponent } from './unsaved-changes-status
     StateInteractionEditorComponent,
     StaleTabInfoModalComponent,
     UnsavedChangesStatusInfoModalComponent,
-    TrainingPanelComponent,
-    TrainingModalComponent,
-    TrainingDataEditorPanelServiceModalComponent,
-    TestInteractionPanel
   ],
 
   entryComponents: [
@@ -366,10 +359,6 @@ import { UnsavedChangesStatusInfoModalComponent } from './unsaved-changes-status
     StateInteractionEditorComponent,
     StaleTabInfoModalComponent,
     UnsavedChangesStatusInfoModalComponent,
-    TrainingPanelComponent,
-    TrainingModalComponent,
-    TrainingDataEditorPanelServiceModalComponent,
-    TestInteractionPanel
   ],
 
   exports: [
@@ -473,10 +462,6 @@ import { UnsavedChangesStatusInfoModalComponent } from './unsaved-changes-status
     SolutionExplanationEditor,
     StateSolutionEditorComponent,
     StateInteractionEditorComponent,
-    TrainingPanelComponent,
-    TrainingModalComponent,
-    TrainingDataEditorPanelServiceModalComponent,
-    TestInteractionPanel
   ],
 })
 
