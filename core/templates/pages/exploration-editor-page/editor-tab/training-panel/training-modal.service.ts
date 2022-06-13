01defdfaa4ebf6b2fb4e5741e204696657c0c3e2
shivam <shivamthalwal@gmail.com>
2022-06-14 04:48:14 +0530

    small nit

--- core/templates/pages/exploration-editor-page/editor-tab/training-panel/training-modal.service.ts
+++ core/templates/pages/exploration-editor-page/editor-tab/training-panel/training-modal.service.ts
@@ -17,57 +17,6 @@
  * the training modal used for unresolved answers.
  */
 
-
-require(
-  'pages/exploration-editor-page/editor-tab/training-panel/' +
-  'training-panel.component.ts');
-
-require('domain/exploration/AnswerGroupObjectFactory.ts');
-require('domain/utilities/url-interpolation.service.ts');
-require('pages/exploration-editor-page/services/angular-name.service.ts');
-require('pages/exploration-editor-page/services/exploration-states.service.ts');
-require(
-  'pages/exploration-editor-page/services/exploration-warnings.service.ts');
-require('pages/exploration-editor-page/services/graph-data.service.ts');
-require(
-  'pages/exploration-editor-page/editor-tab/services/responses.service.ts');
-require(
-  'pages/exploration-editor-page/editor-tab/training-panel/' +
-  'training-data.service.ts');
-require(
-  'pages/exploration-player-page/services/answer-classification.service.ts');
-require(
-  'components/state-editor/state-editor-properties-services/' +
-  'state-editor.service.ts');
-require(
-  'components/state-editor/state-editor-properties-services/' +
-  'state-property.service.ts');
-require('services/alerts.service.ts');
-require('services/context.service.ts');
-require(
-  'pages/exploration-editor-page/editor-tab/training-panel/' +
-  'training-modal.controller');
-require('services/external-save.service.ts');
-
-// Copyright 2018 The Oppia Authors. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS-IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-/**
- * @fileoverview Service which handles opening and closing
- * the training modal used for unresolved answers.
- */
-
 import { downgradeInjectable } from '@angular/upgrade/static';
 import { Injectable } from '@angular/core';
 import { AlertsService } from 'services/alerts.service';
