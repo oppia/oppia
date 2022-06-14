34c7fd3618e8cf46c4639eee1f8eca80bd214bf8
shivam <shivamthalwal@gmail.com>
2022-06-15 02:20:30 +0530

    ts

--- core/templates/pages/exploration-editor-page/editor-tab/training-panel/training-modal.component.ts
+++ core/templates/pages/exploration-editor-page/editor-tab/training-panel/training-modal.component.ts
@@ -15,151 +15,157 @@
 /**
  * @fileoverview Controller for TrainingModal.
  */
-require(
-  'pages/exploration-editor-page/editor-tab/training-panel/' +
-  'training-panel.component.ts');
 
-require('domain/exploration/AnswerGroupObjectFactory.ts');
-require('domain/utilities/url-interpolation.service.ts');
-require('pages/exploration-editor-page/services/angular-name.service.ts');
-require('pages/exploration-editor-page/services/exploration-states.service.ts');
-require(
-  'pages/exploration-editor-page/services/exploration-warnings.service.ts');
-require('pages/exploration-editor-page/services/graph-data.service.ts');
-require(
-  'pages/exploration-editor-page/editor-tab/services/responses.service.ts');
-require(
-  'pages/exploration-editor-page/editor-tab/training-panel/' +
-  'training-data.service.ts');
-require(
-  'pages/exploration-player-page/services/answer-classification.service.ts');
-require(
-  'components/state-editor/state-editor-properties-services/' +
-  'state-editor.service.ts');
-require(
-  'components/state-editor/state-editor-properties-services/' +
-  'state-interaction-id.service');
-require(
-  'components/state-editor/state-editor-properties-services/' +
-  'state-property.service.ts');
-require('services/alerts.service.ts');
-require('services/context.service.ts');
-require('expressions/expression-interpolation.service.ts');
-require(
-  'pages/exploration-editor-page/services/' +
-  'exploration-param-changes.service.ts');
-require('pages/exploration-editor-page/services/exploration-states.service.ts');
-require('pages/exploration-editor-page/services/graph-data.service.ts');
-require('pages/exploration-editor-page/services/parameter-metadata.service.ts');
-require(
-  'pages/exploration-editor-page/editor-tab/services/' +
-  'solution-validity.service.ts');
-require('services/improvements.service.ts');
-require('services/state-top-answers-stats.service.ts');
 
-require(
-  'pages/exploration-editor-page/exploration-editor-page.constants.ajs.ts');
+import { Component, Injector, Input, OnInit } from '@angular/core';
+import { downgradeComponent } from '@angular/upgrade/static';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ConfirmOrCancelModal } from 'components/common-layout-directives/common-elements/confirm-or-cancel-modal.component';
+import { ResponsesService } from '../services/responses.service';
+import { StateEditorService } from 'components/state-editor/state-editor-properties-services/state-editor.service';
+import { StateInteractionIdService } from 'components/state-editor/state-editor-properties-services/state-interaction-id.service';
+import { AnswerGroup, AnswerGroupObjectFactory } from 'domain/exploration/AnswerGroupObjectFactory';
+import { AngularNameService } from 'pages/exploration-editor-page/services/angular-name.service';
+import { ExplorationStatesService } from 'pages/exploration-editor-page/services/exploration-states.service';
+import { ExplorationWarningsService } from 'pages/exploration-editor-page/services/exploration-warnings.service';
+import { GraphDataService } from 'pages/exploration-editor-page/services/graph-data.service';
+import { AnswerClassificationService } from 'pages/exploration-player-page/services/answer-classification.service';
+import { TrainingDataService } from './training-data.service';
+import cloneDeep from 'lodash/cloneDeep';
+import { RULES_SERVICE_MAPPING } from './training-data-editor-panel-modal.component';
+import { InteractionAnswer } from 'interactions/answer-defs';
+import { Outcome } from 'domain/exploration/OutcomeObjectFactory';
 
-angular.module('oppia').controller('TrainingModalController', [
-  '$injector', '$scope', '$uibModalInstance', 'AngularNameService',
-  'AnswerClassificationService', 'AnswerGroupObjectFactory',
-  'ExplorationStatesService', 'ExplorationWarningsService', 'GraphDataService',
-  'ResponsesService', 'StateEditorService', 'StateInteractionIdService',
-  'TrainingDataService', 'finishTrainingCallback', 'unhandledAnswer',
-  function(
-      $injector, $scope, $uibModalInstance, AngularNameService,
-      AnswerClassificationService, AnswerGroupObjectFactory,
-      ExplorationStatesService, ExplorationWarningsService, GraphDataService,
-      ResponsesService, StateEditorService, StateInteractionIdService,
-      TrainingDataService, finishTrainingCallback, unhandledAnswer) {
-    $scope.trainingDataAnswer = '';
+interface classification {
+  answerGroupIndex: number;
+  newOutcome: Outcome;
+}
 
-    // See the training panel directive in ExplorationEditorTab for an
-    // explanation on the structure of this object.
-    $scope.classification = {
+@Component({
+  selector: 'oppia-training-modal',
+  templateUrl: './training-modal.component.html'
+})
+export class TrainingModalComponent
+  extends ConfirmOrCancelModal implements OnInit {
+  @Input() unhandledAnswer: InteractionAnswer;
+  @Input() finishTrainingCallback: Function;
+
+  trainingDataAnswer: InteractionAnswer | string = '';
+
+  // See the training panel directive in ExplorationEditorTab for an
+  // explanation on the structure of this object.
+  classification: classification;
+  addingNewResponse: boolean = false;
+
+  constructor(
+    private injector: Injector,
+    private ngbActiveModal: NgbActiveModal,
+    private responsesService: ResponsesService,
+    private explorationStatesService: ExplorationStatesService,
+    private stateEditorService: StateEditorService,
+    private graphDataService: GraphDataService,
+    private explorationWarningsService: ExplorationWarningsService,
+    private answerGroupObjectFactory: AnswerGroupObjectFactory,
+    private trainingDataService: TrainingDataService,
+    private angularNameService: AngularNameService,
+    private answerClassificationService: AnswerClassificationService,
+    private stateInteractionIdService: StateInteractionIdService,
+  ) {
+    super(ngbActiveModal);
+  }
+
+  ngOnInit(): void {
+    this.classification = {
       answerGroupIndex: 0,
       newOutcome: null
     };
-    $scope.addingNewResponse = false;
+    this.addingNewResponse = false;
 
-    var _saveNewAnswerGroup = function(newAnswerGroup) {
-      var answerGroups = ResponsesService.getAnswerGroups();
-      answerGroups.push(newAnswerGroup);
-      ResponsesService.save(
-        answerGroups, ResponsesService.getDefaultOutcome(),
-        function(newAnswerGroups, newDefaultOutcome) {
-          ExplorationStatesService.saveInteractionAnswerGroups(
-            StateEditorService.getActiveStateName(),
-            angular.copy(newAnswerGroups));
-
-          ExplorationStatesService.saveInteractionDefaultOutcome(
-            StateEditorService.getActiveStateName(),
-            angular.copy(newDefaultOutcome));
-
-          GraphDataService.recompute();
-          ExplorationWarningsService.updateWarnings();
-        });
-    };
-
-    $scope.exitTrainer = function() {
-      $uibModalInstance.close();
-    };
-
-    $scope.onConfirm = function() {
-      var index = $scope.classification.answerGroupIndex;
-      if (index > ResponsesService.getAnswerGroupCount()) {
-        var newOutcome = $scope.classification.newOutcome;
-        var newAnswerGroup = AnswerGroupObjectFactory.createNew(
-          [], angular.copy(newOutcome), [unhandledAnswer], null);
-        _saveNewAnswerGroup(newAnswerGroup);
-        TrainingDataService.associateWithAnswerGroup(
-          ResponsesService.getAnswerGroupCount() - 1,
-          unhandledAnswer);
-      } else if (index === ResponsesService.getAnswerGroupCount()) {
-        TrainingDataService.associateWithDefaultResponse(
-          unhandledAnswer);
-      } else {
-        TrainingDataService.associateWithAnswerGroup(
-          index, unhandledAnswer);
-      }
-      finishTrainingCallback();
-      $uibModalInstance.close();
-    };
-
-    $scope.init = function() {
-      var currentStateName =
-        StateEditorService.getActiveStateName();
-      var state = ExplorationStatesService.getState(currentStateName);
-
-      // Retrieve the interaction ID.
-      var interactionId = StateInteractionIdService.savedMemento;
-
-      var rulesServiceName =
-        AngularNameService.getNameOfInteractionRulesService(
-          interactionId);
-
-      // Inject RulesService dynamically.
-      var rulesService = $injector.get(rulesServiceName);
-
-      var classificationResult = (
-        AnswerClassificationService.getMatchingClassificationResult(
-          currentStateName, state.interaction, unhandledAnswer,
-          rulesService));
-
-      // $scope.trainingDataAnswer, $scope.trainingDataFeedback
-      // $scope.trainingDataOutcomeDest are intended to be local
-      // to this modal and should not be used to populate any
-      // information in the active exploration (including the
-      // feedback). The feedback here refers to a representation
-      // of the outcome of an answer group, rather than the
-      // specific feedback of the outcome (for instance, it
-      // includes the destination state within the feedback).
-      $scope.trainingDataAnswer = unhandledAnswer;
-      $scope.classification.answerGroupIndex = (
-        classificationResult.answerGroupIndex);
-      $scope.classification.newOutcome = classificationResult.outcome;
-    };
-
-    $scope.init();
+    this.init();
   }
-]);
+
+  _saveNewAnswerGroup(newAnswerGroup: AnswerGroup): void {
+    let answerGroups = this.responsesService.getAnswerGroups();
+    answerGroups.push(newAnswerGroup);
+    this.responsesService.save(
+      answerGroups, this.responsesService.getDefaultOutcome(),
+      (newAnswerGroups, newDefaultOutcome) => {
+        this.explorationStatesService.saveInteractionAnswerGroups(
+          this.stateEditorService.getActiveStateName(),
+          cloneDeep(newAnswerGroups));
+
+        this.explorationStatesService.saveInteractionDefaultOutcome(
+          this.stateEditorService.getActiveStateName(),
+          cloneDeep(newDefaultOutcome));
+
+        this.graphDataService.recompute();
+        this.explorationWarningsService.updateWarnings();
+      });
+  }
+
+  exitTrainer(): void {
+    this.ngbActiveModal.close();
+  }
+
+  onConfirm(): void {
+    let index = this.classification.answerGroupIndex;
+    if (index > this.responsesService.getAnswerGroupCount()) {
+      let newOutcome = this.classification.newOutcome;
+      let newAnswerGroup = this.answerGroupObjectFactory.createNew(
+        [], cloneDeep(newOutcome), [this.unhandledAnswer], null);
+      this._saveNewAnswerGroup(newAnswerGroup);
+      this.trainingDataService.associateWithAnswerGroup(
+        this.responsesService.getAnswerGroupCount() - 1,
+        this.unhandledAnswer);
+    } else if (index === this.responsesService.getAnswerGroupCount()) {
+      this.trainingDataService.associateWithDefaultResponse(
+        this.unhandledAnswer);
+    } else {
+      this.trainingDataService.associateWithAnswerGroup(
+        index, this.unhandledAnswer);
+    }
+
+    this.finishTrainingCallback();
+    this.ngbActiveModal.close();
+  }
+
+  init(): void {
+    let currentStateName =
+      this.stateEditorService.getActiveStateName();
+    let state = this.explorationStatesService.getState(currentStateName);
+
+    // Retrieve the interaction ID.
+    let interactionId = this.stateInteractionIdService.savedMemento;
+
+    let rulesServiceName =
+      this.angularNameService.getNameOfInteractionRulesService(
+        interactionId);
+
+    // Inject RulesService dynamically.
+    let rulesService = (
+      this.injector.get(RULES_SERVICE_MAPPING[rulesServiceName]));
+
+    let classificationResult = (
+      this.answerClassificationService.getMatchingClassificationResult(
+        currentStateName, state.interaction, this.unhandledAnswer,
+        rulesService));
+
+    // This.trainingDataAnswer, this.trainingDataFeedback
+    // this.trainingDataOutcomeDest are intended to be local
+    // to this modal and should not be used to populate any
+    // information in the active exploration (including the
+    // feedback). The feedback here refers to a representation
+    // of the outcome of an answer group, rather than the
+    // specific feedback of the outcome (for instance, it
+    // includes the destination state within the feedback).
+    this.trainingDataAnswer = this.unhandledAnswer;
+    this.classification.answerGroupIndex = (
+      classificationResult.answerGroupIndex);
+    this.classification.newOutcome = classificationResult.outcome;
+  }
+}
+
+angular.module('oppia').factory('oppiaTrainingModal',
+  downgradeComponent({
+    component: TrainingModalComponent
+  }) as angular.IDirectiveFactory);
