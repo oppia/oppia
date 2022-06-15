1b6614104ad890ba80c689709ffd039d83d19ba3
Nithin <78612244+nithinrdy@users.noreply.github.com>
2022-06-15 12:19:08 +0530

    revert test

--- core/templates/pages/exploration-player-page/learner-experience/tutor-card.component.spec.ts
+++ core/templates/pages/exploration-player-page/learner-experience/tutor-card.component.spec.ts
@@ -216,7 +216,7 @@ describe('Tutor card component', () => {
       spyOn(componentInstance, 'isOnTerminalCard').and.returnValue(true);
       spyOn(componentInstance, 'triggerCelebratoryAnimation');
       componentInstance.animationHasPlayedOnce = false;
-      componentInstance.inStoryMode = false;
+      componentInstance.inStoryMode = true;
       const changes: SimpleChanges = {
         displayedCard: {
           previousValue: false,
@@ -236,7 +236,7 @@ describe('Tutor card component', () => {
       spyOn(componentInstance, 'isOnTerminalCard').and.returnValue(true);
       spyOn(componentInstance, 'triggerCelebratoryAnimation');
       componentInstance.animationHasPlayedOnce = false;
-      componentInstance.inStoryMode = true;
+      componentInstance.inStoryMode = false;
       const changes: SimpleChanges = {
         displayedCard: {
           previousValue: false,
