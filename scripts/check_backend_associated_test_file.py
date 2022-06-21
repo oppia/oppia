acfac1e378ec46903c55c5a2c1c63b51211e678c
IamEzio <anshumanmaurya111@gmail.com>
2022-06-22 00:55:33 +0530

    lint fix

--- scripts/check_backend_associated_test_file.py
+++ scripts/check_backend_associated_test_file.py
@@ -20,57 +20,59 @@ import logging
 import os
 import sys
 
-TOPMOST_LEVEL_PATH = "./"
+TOPMOST_LEVEL_PATH = './'
 
 # These files are data files for testing and have no logic to be tested.
 FILES_WITHOUT_ASSOCIATED_TEST_FILES = [
-    "scripts/linters/test_files/invalid_annotations.py",
-    "scripts/linters/test_files/invalid_author.py",
-    "scripts/linters/test_files/invalid_copyright.py",
-    "scripts/linters/test_files/invalid_docstring.py",
-    "scripts/linters/test_files/invalid_import_order.py",
-    "scripts/linters/test_files/invalid_merge_conflict.py",
-    "scripts/linters/test_files/invalid_ndb.py",
-    "scripts/linters/test_files/invalid_no_newline.py",
-    "scripts/linters/test_files/invalid_pycodestyle_error.py",
-    "scripts/linters/test_files/invalid_pylint_id.py",
-    "scripts/linters/test_files/invalid_python_three.py",
-    "scripts/linters/test_files/invalid_request.py",
-    "scripts/linters/test_files/invalid_tabs.py",
-    "scripts/linters/test_files/invalid_todo.py",
-    "scripts/linters/test_files/invalid_urlopen.py",
-    "scripts/linters/test_files/valid.py",
-    "scripts/linters/test_files/valid_py_ignore_pragma.py",
-    "core/tests/build_sources/extensions/CodeRepl.py",
-    "core/tests/build_sources/extensions/DragAndDropSortInput.py",
-    "core/tests/data/failing_tests.py",
-    "core/tests/data/image_constants.py",
-    "core/tests/data/unicode_and_str_handler.py",
+    'scripts/linters/test_files/invalid_annotations.py',
+    'scripts/linters/test_files/invalid_author.py',
+    'scripts/linters/test_files/invalid_copyright.py',
+    'scripts/linters/test_files/invalid_docstring.py',
+    'scripts/linters/test_files/invalid_import_order.py',
+    'scripts/linters/test_files/invalid_merge_conflict.py',
+    'scripts/linters/test_files/invalid_ndb.py',
+    'scripts/linters/test_files/invalid_no_newline.py',
+    'scripts/linters/test_files/invalid_pycodestyle_error.py',
+    'scripts/linters/test_files/invalid_pylint_id.py',
+    'scripts/linters/test_files/invalid_python_three.py',
+    'scripts/linters/test_files/invalid_request.py',
+    'scripts/linters/test_files/invalid_tabs.py',
+    'scripts/linters/test_files/invalid_todo.py',
+    'scripts/linters/test_files/invalid_urlopen.py',
+    'scripts/linters/test_files/valid.py',
+    'scripts/linters/test_files/valid_py_ignore_pragma.py',
+    'core/tests/build_sources/extensions/CodeRepl.py',
+    'core/tests/build_sources/extensions/DragAndDropSortInput.py',
+    'core/tests/data/failing_tests.py',
+    'core/tests/data/image_constants.py',
+    'core/tests/data/unicode_and_str_handler.py',
 ]
 
+
 def main() -> None:
+    """Finds the non-empty backend files that lack an associated test file."""
     all_backend_files = []
     for (root, _, files) in os.walk(TOPMOST_LEVEL_PATH):
         for file in files:
             full_path = os.path.join(root, file)
             _, file_extension = os.path.splitext(full_path)
-            if(file_extension == '.py') and (
-                "third_party" not in full_path) and (
-                    "node_modules" not in full_path):
+            if (file_extension == '.py') and (
+                'third_party' not in full_path) and (
+                    'node_modules' not in full_path):
                 all_backend_files.append(full_path)
 
     all_backend_files.sort()
     files_without_test = []
     for file in all_backend_files:
-        if "_test.py" not in file:
-            test_file = file.replace(".py", "_test.py")
-            if(test_file not in all_backend_files):
+        if '_test.py' not in file:
+            test_file = file.replace('.py', '_test.py')
+            if test_file not in all_backend_files:
                 files_without_test.append(file)
 
     non_empty_files = []
     for file in files_without_test:
         line_count = 0
-        with open(file,'r') as f:
+        with open(file, 'r', encoding='utf8') as f:
             line_count = len(f.readlines())
         if line_count > 0:
             non_empty_files.append(file[2:])
@@ -82,7 +84,7 @@ def main() -> None:
                 '\033[1m{}\033[0m does not have an associated test file.'
                 ' Make sure it\'s fully tested by it\'s associated test'
                 ' file.\n'.format(file))
-    
+
     if errors:
         print('---------------------------------------------')
         print('Some backend files lack associated test file.')
@@ -93,6 +95,7 @@ def main() -> None:
         print('-----------------------------------------------')
         print('All backend files have an associated test file.')
         print('-----------------------------------------------')
+
 
 if __name__ == '__main__':
     main()
