704e1d81610bb77c5b0f8bd58ecf018d71dda19f
Vojtěch Jelínek <vojtin.j@gmail.com>
2022-06-21 02:28:38 +0200

    Improve coverage

--- core/templates/hybrid-router-module-provider.spec.ts
+++ core/templates/hybrid-router-module-provider.spec.ts
@@ -24,6 +24,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 
 import { SmartRouterLink } from 'hybrid-router-module-provider';
 import { WindowRef } from 'services/contextual/window-ref.service';
+import {UserService} from "services/user.service";
 
 @Component({
   selector: 'mock-comp-a',
@@ -43,6 +44,18 @@ class MockCompB {}
 })
 class MockCompC {}
 
+@Component({
+  selector: 'mock-comp-d',
+  template: '<a href="/" [smartRouterLink]="\'/\'"></a><router-outlet>'
+})
+class MockCompD {}
+
+@Component({
+  selector: 'mock-comp-e',
+  template: '<a href="/" [smartRouterLink]="\'/\'"></a><router-outlet custom="light">'
+})
+class MockCompE {}
+
 class MockWindowRef {
   nativeWindow = {
     location: {
@@ -55,9 +68,13 @@ describe('Smart router link directive', () => {
   let mockCompAFixture: ComponentFixture<MockCompA>;
   let mockCompBFixture: ComponentFixture<MockCompB>;
   let mockCompCFixture: ComponentFixture<MockCompC>;
+  let mockCompDFixture: ComponentFixture<MockCompD>;
+  let mockCompEFixture: ComponentFixture<MockCompE>;
   let mockCompALink;
   let mockCompBLink;
   let mockCompCLink;
+  let mockCompDLink;
+  let mockCompELink;
   let mockWindowRef: MockWindowRef;
 
   beforeEach(waitForAsync(() => {
@@ -67,13 +84,17 @@ describe('Smart router link directive', () => {
         RouterTestingModule.withRoutes([
           { path: 'contact', component: MockCompA },
           { path: 'contact', component: MockCompB },
-          { path: 'contact', component: MockCompC }
+          { path: 'contact', component: MockCompC },
+          { path: '', component: MockCompD },
+          { path: '', component: MockCompE },
         ]),
       ],
       declarations: [
         MockCompA,
         MockCompB,
         MockCompC,
+        MockCompD,
+        MockCompE,
         SmartRouterLink,
       ],
       providers: [
@@ -89,12 +110,18 @@ describe('Smart router link directive', () => {
     mockCompAFixture = TestBed.createComponent(MockCompA);
     mockCompBFixture = TestBed.createComponent(MockCompB);
     mockCompCFixture = TestBed.createComponent(MockCompC);
+    mockCompDFixture = TestBed.createComponent(MockCompD);
+    mockCompEFixture = TestBed.createComponent(MockCompE);
     mockCompALink = (
       mockCompAFixture.debugElement.nativeElement.querySelector('a'));
     mockCompBLink = (
       mockCompBFixture.debugElement.nativeElement.querySelector('a'));
     mockCompCLink = (
       mockCompCFixture.debugElement.nativeElement.querySelector('a'));
+    mockCompDLink = (
+      mockCompDFixture.debugElement.nativeElement.querySelector('a'));
+    mockCompELink = (
+      mockCompEFixture.debugElement.nativeElement.querySelector('a'));
   }));
 
   it('should navigate by refreshing from non-router page', fakeAsync(() => {
@@ -136,6 +163,37 @@ describe('Smart router link directive', () => {
       tick();
 
       expect(mockWindowRef.nativeWindow.location.href).toBe('/contact');
+    })
+  );
+
+  it(
+    'should navigate by refreshing from normal router to lightweight router',
+    fakeAsync(() => {
+      mockCompDFixture.detectChanges();
+      tick();
+      expect(mockWindowRef.nativeWindow.location.href).toBe('');
+
+      mockCompDLink.click();
+      mockCompDFixture.detectChanges();
+      tick();
+
+      expect(mockWindowRef.nativeWindow.location.href).toBe('');
+    })
+  );
+
+
+  it(
+    'should navigate without refreshing inside lightweight router',
+    fakeAsync(() => {
+      mockCompEFixture.detectChanges();
+      tick();
+      expect(mockWindowRef.nativeWindow.location.href).toBe('');
+
+      mockCompELink.click();
+      mockCompEFixture.detectChanges();
+      tick();
+
+      expect(mockWindowRef.nativeWindow.location.href).toBe('');
     })
   );
 });
