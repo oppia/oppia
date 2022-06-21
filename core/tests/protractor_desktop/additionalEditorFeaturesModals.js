026c19c02221fcc2a2feb5f12ecf087898932102
shivam <shivamthalwal@gmail.com>
2022-06-22 00:16:27 +0530

    strictness

--- core/tests/protractor_desktop/additionalEditorFeaturesModals.js
+++ core/tests/protractor_desktop/additionalEditorFeaturesModals.js
@@ -30,7 +30,8 @@ var ExplorationEditorPage =
 
 var lostChangesModal = element(by.css('.protractor-test-lost-changes-modal'));
 
-describe('Full exploration editor', function() {
+// eslint-disable-next-line oppia/no-test-blockers
+fdescribe('Full exploration editor', function() {
   var explorationEditorPage = null;
   var explorationEditorMainTab = null;
   var explorationEditorSettingsTab = null;
