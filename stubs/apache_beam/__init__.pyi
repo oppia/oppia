25bfbf03b5b3ccdaaae063f60ca74a6183092581
sahil jhangar <souravjangar91@gmail.com>
2022-06-21 18:43:55 +0530

    cleaned dirty stubs

--- stubs/apache_beam/__init__.pyi
+++
@@ -1,2 +0,0 @@
-from .core import DoFn as DoFn, Create as Create, Map as Map, Filter as Filter
-from .ptransform import PTransform as PTransform
--- stubs/apache_beam/ptransform.pyi
+++
@@ -1,5 +0,0 @@
-from typing import Optional
-
-
-class PTransform():
-    def __init__(self, label: Optional[str] = None) -> None: ...
