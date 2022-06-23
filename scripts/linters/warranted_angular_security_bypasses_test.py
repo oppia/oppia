56c289ffb9fd1c42c1f8bba322d08c7d3ae8aabe
IamEzio <anshumanmaurya111@gmail.com>
2022-06-23 23:35:29 +0530

    added some more tests

--- scripts/linters/warranted_angular_security_bypasses_test.py
+++ scripts/linters/warranted_angular_security_bypasses_test.py
@@ -19,11 +19,17 @@
 from __future__ import annotations
 
 from core.tests import test_utils
-from . import (                            # pylint: disable=unused-import
-    warranted_angular_security_bypasses) 
+from . import warranted_angular_security_bypasses
 
 
 class WarrantedAngularSecurityBypassesTests(test_utils.GenericTestBase):
 
-    def test_trivial(self) -> None:
-        pass
+    def test_svg_sanitizer_service_is_present_in_excluded_files(self) -> None:
+        excluded_files = (
+            warranted_angular_security_bypasses
+            .EXCLUDED_BYPASS_SECURITY_TRUST_FILES)
+        self.assertIn(
+            'core/templates/services/svg-sanitizer.service.spec.ts',
+            excluded_files)
+        self.assertIn(
+            'core/templates/services/svg-sanitizer.service.ts', excluded_files)
