c36a1a69ebaa45cab5654a942abe6e8e5119bb50
JG-11 <80846488+JeeveshGarg@users.noreply.github.com>
2022-06-23 21:24:21 +0530

    check

--- extensions/interactions/EndExploration/directives/oppia-interactive-end-exploration.component.ts
+++ extensions/interactions/EndExploration/directives/oppia-interactive-end-exploration.component.ts
@@ -40,7 +40,7 @@ export class InteractiveEndExplorationComponent implements OnInit {
   // and we need to do non-null assertion. For more information, see
   // https://github.com/oppia/oppia/wiki/Guide-on-defining-types#ts-7-1
   @Input() recommendedExplorationIdsWithValue!: string;
-  errorMessage!: string;
+  errorMessage: string;
   isInEditorPage: boolean = false;
   isInEditorPreviewMode: boolean = false;
   isInEditorMainTab: boolean = false;
