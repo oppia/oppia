40b7c57deba07dc331157c7802d0764a6ee0163e
shivam <shivamthalwal@gmail.com>
2022-06-25 03:15:01 +0530

    safe

--- core/templates/components/state-directives/rule-editor/rule-type-selector.component.ts
+++ core/templates/components/state-directives/rule-editor/rule-type-selector.component.ts
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 /**
- * @fileoverview Directive for the rule type selector.
+ * @fileoverview Component for the rule type selector.
  */
 
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
@@ -37,11 +37,7 @@ export class RuleTypeSelector implements OnInit {
   ) {}
 
   selectedRule($event: Event): void {
-    console.error('choices');
-    console.error(this.choices);
-
-    console.error('localValue');
-    console.error(this.localValue);
+    this.onSelectionChange.emit(this.localValue);
   }
 
   ngOnInit(): void {
@@ -69,7 +65,9 @@ export class RuleTypeSelector implements OnInit {
       [this.choices[0], this.choices[equalToIndex]] = [
         this.choices[equalToIndex], this.choices[0]];
     }
+
     this.localValue = this.choices[0].id;
+    this.onSelectionChange.emit(this.localValue);
   }
 }
 
@@ -78,80 +76,3 @@ angular.module('oppia').directive('oppiaRuleTypeSelector',
   downgradeComponent({
     component: RuleTypeSelector
   }) as angular.IDirectiveFactory);
-
-// Angular.module('oppia').directive('ruleTypeSelector', [function() {
-//   return {
-//     restrict: 'E',
-//     scope: {},
-//     bindToController: {
-//       localValue: '@',
-//       onSelectionChange: '&'
-//     },
-//     template: '<select></select>',
-//     controllerAs: '$ctrl',
-//     controller: [
-//       '$element', '$filter', '$scope',
-//       'StateInteractionIdService', 'INTERACTION_SPECS',
-//       function(
-//           $element, $filter, $scope,
-//           StateInteractionIdService, INTERACTION_SPECS) {
-//         let ctrl = this;
-//         ctrl.$onInit = function() {
-//           let choices = [];
-
-//           let ruleTypesToDescriptions = INTERACTION_SPECS[
-//             StateInteractionIdService.savedMemento].rule_descriptions;
-//           let equalToIndex = null;
-//           let idx = 0;
-//           for (let ruleType in ruleTypesToDescriptions) {
-//             if (ruleType.includes('Equal') || ruleType.includes('Exactly')) {
-//               equalToIndex = idx;
-//             }
-//             choices.push({
-//               id: ruleType,
-//               text: $filter('replaceInputsWithEllipses')(
-//                 ruleTypesToDescriptions[ruleType])
-//             });
-//             idx++;
-//           }
-//           // If the 'Equals' rule is not the first one, swap it with the first
-//           // rule, so the equals rule is always the default one selected in the
-//           // editor.
-//           if (equalToIndex) {
-//             [choices[0], choices[equalToIndex]] = [
-//               choices[equalToIndex], choices[0]];
-//           }
-//           let select2Node = $element[0].firstChild;
-//           $(select2Node).select2({
-//             allowClear: false,
-//             data: choices,
-//             // Suppress the search box.
-//             minimumResultsForSearch: -1,
-//             width: '100%',
-//             dropdownParent: $(select2Node).parent(),
-//             templateSelection: function(object) {
-//               return $filter('truncateAtFirstEllipsis')(object.text);
-//             }
-//           });
-
-//           // Select the first choice by default.
-//           if (!ctrl.localValue) {
-//             ctrl.localValue = choices[0].id;
-//             ctrl.onSelectionChange()(ctrl.localValue);
-//           }
-
-//           // Initialize the dropdown.
-//           $(select2Node).val(ctrl.localValue).trigger('change');
-
-//           $(select2Node).on('change', function(e) {
-//             ctrl.onSelectionChange()($(select2Node).val());
-//             // This is needed to propagate the change and display input fields
-//             // for parameterizing the rule. Otherwise, the input fields do not
-//             // get updated when the rule type is changed.
-//             $scope.$apply();
-//           });
-//         };
-//       }
-//     ]
-//   };
-// }]);
