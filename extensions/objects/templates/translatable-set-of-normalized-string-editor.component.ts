3fa68f634622b2f87affe82641e0be77ea015e8d
shivam <shivamthalwal@gmail.com>
2022-06-25 16:18:28 +0530

    testing passed

--- extensions/objects/templates/translatable-set-of-normalized-string-editor.component.ts
+++ extensions/objects/templates/translatable-set-of-normalized-string-editor.component.ts
@@ -28,7 +28,8 @@ export interface TranslatableSetOfStringSchema {
   selector: 'translatable-set-of-normalized-string-editor',
   templateUrl: './translatable-set-of-normalized-string-editor.component.html'
 })
-export class TranslatableSetOfNormalizedStringEditorComponent implements OnInit {
+export class TranslatableSetOfNormalizedStringEditorComponent
+  implements OnInit {
   // This property is initialized using Angular lifecycle hooks
   // and we need to do non-null assertion. For more information, see
   // https://github.com/oppia/oppia/wiki/Guide-on-defining-types#ts-7-1
