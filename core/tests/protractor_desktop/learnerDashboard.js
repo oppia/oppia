e912cb358e9a557698a129528afec45eea111280
Manan Rathi <rathimanan27@gmail.com>
2022-06-26 14:20:40 +0530

    Trying

--- core/tests/protractor_desktop/learnerDashboard.js
+++ core/tests/protractor_desktop/learnerDashboard.js
@@ -356,7 +356,7 @@ describe('Learner dashboard functionality', function() {
       await explorationEditorMainTab.setContent(
         await forms.toRichText('Question 1'));
       await explorationEditorMainTab.setInteraction(
-        'TextInput', 'Placeholder', 5);
+        'TextInput', 'Placeholder', 5, false);
       await explorationEditorMainTab.addResponse(
         'TextInput', await forms.toRichText('Correct Answer'), null, false,
         'FuzzyEquals', ['correct']);
