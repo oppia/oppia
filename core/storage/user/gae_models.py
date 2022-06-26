ce25d73163d4a084bce5232fe35bb258ea8daba5
Ayush Jain <lcs2020014@iiitl.ac.in>
2022-06-27 00:52:46 +0530

    Fix #15385: Added functionality to persist translator language on [CD - Translate Text]. (#15460)
    * added functionality to persist language on CD - Translate Text
    
    * removed redundant code
    
    * pip version error
    
    * requested changes
    
    * fixed few errors
    
    * implemented requested changes and fixed backend tests
    
    * fixed lint errors and python type check errors
    
    * fixed backend tests
    
    * fixed takeout_service_test.py error
    
    * fixed front-end tests and added new tests
    
    * fixed lint errors
    
    * fixed e2e coverage and typescipt tests failing.
    
    * fixed known bugs and applied requested changes
    
    * removed irrelevant comments
    
    * fixed lint and typescript errors
    
    * fixed e2e tests errors and added a new e2e test
    
    * fixed lint errors
    
    * fixed typescript check error
    
    * Update core/domain/user_services.py
    
    Co-authored-by: Vojtěch Jelínek <vojtech.jelinek@hey.com>
    
    * Update core/templates/pages/contributor-dashboard-page/services/contribution-opportunities-backend-api.service.ts
    
    Co-authored-by: Vojtěch Jelínek <vojtech.jelinek@hey.com>
    
    * Update core/templates/pages/contributor-dashboard-page/services/contribution-opportunities-backend-api.service.ts
    
    Co-authored-by: Vojtěch Jelínek <vojtech.jelinek@hey.com>
    
    * Update core/controllers/contributor_dashboard.py
    
    Co-authored-by: Sagang Wee <9004726+sagangwee@users.noreply.github.com>
    
    * accomodated suggested changes
    
    * fixed e2e error for logged-in and non-logged-in users by adding UserService
    
    * removed typescript errors
    
    * removed typescript errors
    
    * fixed typescript errors
    
    * fixed backend test errors
    
    * merged commits and changed preferred_translation_language_code description
    
    * fixed lint check errors and front-end test errors
    
    * fixed trailing spaces lint check error
    
    * incorporated requested suggestions
    
    * replaced empty string with null
    
    * retrigger checks - 2
    
    * removed ?
    
    Co-authored-by: Vojtěch Jelínek <vojtech.jelinek@hey.com>
    Co-authored-by: Sagang Wee <9004726+sagangwee@users.noreply.github.com>

--- core/storage/user/gae_models.py
+++ core/storage/user/gae_models.py
@@ -91,6 +91,10 @@ class UserSettingsModel(base_models.BaseModel):
     preferred_audio_language_code = datastore_services.StringProperty(
         default=None, choices=[
             language['id'] for language in constants.SUPPORTED_AUDIO_LANGUAGES])
+    # Language preference when submitting text translations in the
+    # contributor dashboard.
+    preferred_translation_language_code = datastore_services.StringProperty(
+        default=None)
 
     # Attributes used for full users only.
 
@@ -185,6 +189,8 @@ class UserSettingsModel(base_models.BaseModel):
             'preferred_site_language_code':
                 base_models.EXPORT_POLICY.EXPORTED,
             'preferred_audio_language_code':
+                base_models.EXPORT_POLICY.EXPORTED,
+            'preferred_translation_language_code':
                 base_models.EXPORT_POLICY.EXPORTED,
             'username': base_models.EXPORT_POLICY.EXPORTED,
             'normalized_username': base_models.EXPORT_POLICY.EXPORTED,
@@ -293,6 +299,8 @@ class UserSettingsModel(base_models.BaseModel):
             'preferred_language_codes': user.preferred_language_codes,
             'preferred_site_language_code': user.preferred_site_language_code,
             'preferred_audio_language_code': user.preferred_audio_language_code,
+            'preferred_translation_language_code': (
+                user.preferred_translation_language_code),
             'display_alias': user.display_alias,
             'has_viewed_lesson_info_modal_once': (
                 user.has_viewed_lesson_info_modal_once)
