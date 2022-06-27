092e51f6b3f90f5da1621851c0a0670b304bfff9
Nithin <78612244+nithinrdy@users.noreply.github.com>
2022-06-28 01:50:10 +0530

    add recommendations component

--- core/templates/components/shared-component.module.ts
+++ core/templates/components/shared-component.module.ts
@@ -105,6 +105,7 @@ import { QuestionMisconceptionSelectorComponent } from './question-directives/qu
 import { ConversationSkinComponent } from 'pages/exploration-player-page/learner-experience/conversation-skin.component';
 import { EndChapterCheckMarkComponent } from 'pages/exploration-player-page/learner-experience/end-chapter-check-mark.component';
 import { EndChapterConfettiComponent } from 'pages/exploration-player-page/learner-experience/end-chapter-confetti.component';
+import { PostChapterRecommendationsComponent } from 'pages/exploration-player-page/learner-experience/post-chapter-recommendations.component';
 import { RatingsAndRecommendationsComponent } from 'pages/exploration-player-page/learner-experience/ratings-and-recommendations.component';
 import { LearnerAnswerInfoCard } from 'pages/exploration-player-page/learner-experience/learner-answer-info-card.component';
 import { FeedbackPopupComponent } from 'pages/exploration-player-page/layout-directives/feedback-popup.component';
@@ -189,6 +190,7 @@ import { UnsavedChangesStatusInfoModalComponent } from './unsaved-changes-status
     ConversationSkinComponent,
     EndChapterCheckMarkComponent,
     EndChapterConfettiComponent,
+    PostChapterRecommendationsComponent,
     ConversationSkinEmbedComponent,
     CreateNewSkillModalComponent,
     CreateActivityModalComponent,
@@ -286,6 +288,7 @@ import { UnsavedChangesStatusInfoModalComponent } from './unsaved-changes-status
     ConversationSkinComponent,
     EndChapterCheckMarkComponent,
     EndChapterConfettiComponent,
+    PostChapterRecommendationsComponent,
     ConversationSkinEmbedComponent,
     CreateNewSkillModalComponent,
     CreateActivityModalComponent,
@@ -400,6 +403,7 @@ import { UnsavedChangesStatusInfoModalComponent } from './unsaved-changes-status
     ConversationSkinComponent,
     EndChapterCheckMarkComponent,
     EndChapterConfettiComponent,
+    PostChapterRecommendationsComponent,
     ConversationSkinEmbedComponent,
     CreateNewSkillModalComponent,
     CreateActivityModalComponent,
