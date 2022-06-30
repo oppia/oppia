11e6b3f4968ada936ee5ec049cf8b1e905069280
Vojtěch Jelínek <vojtin.j@gmail.com>
2022-07-01 00:14:52 +0200

    Fix #15638: Fix broken page and broken FA icons (#15641)

--- core/templates/pages/review-test-page/review-test-page.module.ts
+++ core/templates/pages/review-test-page/review-test-page.module.ts
@@ -21,6 +21,9 @@ import { BrowserModule, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser'
 import { downgradeComponent } from '@angular/upgrade/static';
 import { HttpClientModule } from '@angular/common/http';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { InteractionExtensionsModule } from 'interactions/interactions.module';
 import { RequestInterceptor } from 'services/request-interceptor.service';
 import { SharedComponentsModule } from 'components/shared-component.module';
@@ -28,8 +31,8 @@ import { OppiaAngularRootComponent } from
   'components/oppia-angular-root.component';
 import { platformFeatureInitFactory, PlatformFeatureService } from
   'services/platform-feature.service';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MyHammerConfig, toastrConfig } from 'pages/oppia-root/app.module';
+import { SmartRouterModule } from 'hybrid-router-module-provider';
 
 @NgModule({
   imports: [
@@ -38,6 +41,10 @@ import { MyHammerConfig, toastrConfig } from 'pages/oppia-root/app.module';
     HttpClientModule,
     InteractionExtensionsModule,
     SharedComponentsModule,
+    // TODO(#13443): Remove smart router module provider once all pages are
+    // migrated to angular router.
+    SmartRouterModule,
+    RouterModule.forRoot([]),
     ToastrModule.forRoot(toastrConfig)
   ],
   providers: [
