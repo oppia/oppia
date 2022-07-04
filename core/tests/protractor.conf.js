f10fcace8ba5a1cbeae02be5f2e6dc17968eeb60
ShivamJhaa <20bcs206@iiitdmj.ac.in>
2022-07-04 13:29:33 +0530

    Fixed failing tests

--- core/tests/protractor.conf.js
+++ core/tests/protractor.conf.js
@@ -100,10 +100,6 @@ var suites = {
       'protractor_desktop/fileUploadExtensions.js'
     ],
 
-    learnerDashboard: [
-      'protractor_desktop/learnerDashboard.js'
-    ],
-
     library: [
       'protractor/libraryFlow.js'
     ],
