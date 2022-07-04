e462a0df286bcdc93bc183f1314ea69697cbdfa5
IamEzio <anshumanmaurya111@gmail.com>
2022-07-04 12:46:50 +0530

    nit

--- core/domain/app_feedback_report_constants_test.py
+++ core/domain/app_feedback_report_constants_test.py
@@ -18,8 +18,7 @@
 
 from __future__ import annotations
 
-from core.domain import (   # pylint: disable=unused-import
-    app_feedback_report_constants)
+from core.domain import app_feedback_report_constants   # pylint: disable=unused-import # isort: skip
 from core.tests import test_utils
 
 
