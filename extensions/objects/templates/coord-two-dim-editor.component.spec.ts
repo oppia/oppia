2a6618dc89f9d35fd5efad1ce7c73d225a65f4c8
shivam <shivamthalwal@gmail.com>
2022-07-10 16:59:54 +0530

    upstream/merge

--- extensions/objects/templates/coord-two-dim-editor.component.spec.ts
+++ extensions/objects/templates/coord-two-dim-editor.component.spec.ts
@@ -1,3 +1,4 @@
+
 // Copyright 2021 The Oppia Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -42,7 +43,6 @@ describe('CoordTwoDimEditorComponent', () => {
   it('should initialize value [0, 0]', () => {
     expect(component).toBeDefined();
 
-    component.value = undefined;
     component.ngOnInit();
 
     expect(component.value).toEqual([0, 0]);
