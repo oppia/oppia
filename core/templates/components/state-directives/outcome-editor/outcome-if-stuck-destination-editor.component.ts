5c5042d46e0ebe8b8d37d0cdf71fc5cc2ddd1d52
Manan Rathi <rathimanan27@gmail.com>
2022-07-15 17:34:26 +0530

    Remove has-feedback field

--- core/templates/components/state-directives/outcome-editor/outcome-if-stuck-destination-editor.component.ts
+++ core/templates/components/state-directives/outcome-editor/outcome-if-stuck-destination-editor.component.ts
@@ -43,7 +43,6 @@ export class OutcomeIfStuckDestinationEditorComponent implements OnInit {
   // and we need to do non-null assertion. For more information, see
   // https://github.com/oppia/oppia/wiki/Guide-on-defining-types#ts-7-1
   @Input() outcome!: Outcome;
-  @Input() outcomeHasFeedback!: boolean;
   explorationAndSkillIdPattern!: RegExp;
   newStateNamePattern!: RegExp;
   destinationChoices: DestinationChoice[] = [];
