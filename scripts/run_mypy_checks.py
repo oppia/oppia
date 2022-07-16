700646dd08d1e6e7961cdd67aa85b3186134e889
sahil jhangar <souravjangar91@gmail.com>
2022-07-16 16:27:29 +0530

    added html_validation

--- scripts/run_mypy_checks.py
+++ scripts/run_mypy_checks.py
@@ -51,8 +51,6 @@ NOT_FULLY_COVERED_FILES = [
     'core/domain/expression_parser_test.py',
     'core/domain/feedback_services.py',
     'core/domain/feedback_services_test.py',
-    'core/domain/html_validation_service.py',
-    'core/domain/html_validation_service_test.py',
     'core/domain/learner_progress_services.py',
     'core/domain/learner_progress_services_test.py',
     'core/domain/object_registry.py',
