d1aec51e52fee854c901fb040646c2c9bedbbf2f
Praneeth <praneethg2001@gmail.com>
2022-07-16 01:40:08 -0700

    Fix

--- core/domain/subtopic_page_services.py
+++ core/domain/subtopic_page_services.py
@@ -329,7 +329,8 @@ def delete_subtopic_page(
 def get_topic_ids_from_subtopic_page_ids(
     subtopic_page_ids: List[str]
 ) -> List[str]:
-    """Returns the topic ids corresponding to the given set of subtopic page ids.
+    """Returns the topic ids corresponding to the given set of subtopic page
+    ids.
 
     Args:
         subtopic_page_ids: list(str). The ids of the subtopic pages.
