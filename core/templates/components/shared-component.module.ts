d2944c628d1624782c5176d27fe33e6f2a4efc25
Nithin <78612244+nithinrdy@users.noreply.github.com>
2022-07-18 20:13:56 +0530

    register post chapter recommendations component in a dedicated module

--- core/templates/components/shared-component.module.ts
+++ core/templates/components/shared-component.module.ts
@@ -29,6 +29,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from 'modules/material.module';
 import { ObjectComponentsModule } from 'objects/object-components.module';
 import { SharedFormsModule } from './forms/shared-forms.module';
+import { RecommendationsModule } from './recommendations/recommendations.module';
 import { CommonElementsModule } from './common-layout-directives/common-elements/common-elements.module';
 import { RichTextComponentsModule } from 'rich_text_components/rich-text-components.module';
 import { CodeMirrorModule } from './code-mirror/codemirror.module';
@@ -105,7 +106,6 @@ import { QuestionMisconceptionSelectorComponent } from './question-directives/qu
 import { ConversationSkinComponent } from 'pages/exploration-player-page/learner-experience/conversation-skin.component';
 import { EndChapterCheckMarkComponent } from 'pages/exploration-player-page/learner-experience/end-chapter-check-mark.component';
 import { EndChapterConfettiComponent } from 'pages/exploration-player-page/learner-experience/end-chapter-confetti.component';
-import { PostChapterRecommendationsComponent } from 'pages/exploration-player-page/learner-experience/post-chapter-recommendations.component';
 import { RatingsAndRecommendationsComponent } from 'pages/exploration-player-page/learner-experience/ratings-and-recommendations.component';
 import { LearnerAnswerInfoCard } from 'pages/exploration-player-page/learner-experience/learner-answer-info-card.component';
 import { FeedbackPopupComponent } from 'pages/exploration-player-page/layout-directives/feedback-popup.component';
@@ -176,6 +176,7 @@ import { UnsavedChangesStatusInfoModalComponent } from './unsaved-changes-status
     RichTextComponentsModule,
     SearchBarModule,
     SharedFormsModule,
+    RecommendationsModule,
     StringUtilityPipesModule,
     AngularFireModule.initializeApp(AuthService.firebaseConfig),
     AngularFireAuthModule,
@@ -202,7 +203,6 @@ import { UnsavedChangesStatusInfoModalComponent } from './unsaved-changes-status
     ConversationSkinComponent,
     EndChapterCheckMarkComponent,
     EndChapterConfettiComponent,
-    PostChapterRecommendationsComponent,
     ConversationSkinEmbedComponent,
     CreateNewSkillModalComponent,
     CreateActivityModalComponent,
@@ -311,7 +311,6 @@ import { UnsavedChangesStatusInfoModalComponent } from './unsaved-changes-status
     ConversationSkinComponent,
     EndChapterCheckMarkComponent,
     EndChapterConfettiComponent,
-    PostChapterRecommendationsComponent,
     ConversationSkinEmbedComponent,
     CreateNewSkillModalComponent,
     CreateActivityModalComponent,
@@ -437,7 +436,6 @@ import { UnsavedChangesStatusInfoModalComponent } from './unsaved-changes-status
     ConversationSkinComponent,
     EndChapterCheckMarkComponent,
     EndChapterConfettiComponent,
-    PostChapterRecommendationsComponent,
     ConversationSkinEmbedComponent,
     CreateNewSkillModalComponent,
     CreateActivityModalComponent,
