733413de9e3a629c60d6424dd159002af8a8d053
shivam <shivamthalwal@gmail.com>
2022-07-21 20:36:36 +0530

    shivam PTal removed

--- core/templates/components/question-directives/question-editor/question-editor.component.ts
+++ core/templates/components/question-directives/question-editor/question-editor.component.ts
@@ -199,8 +199,6 @@ export class QuestionEditorComponent implements OnInit, OnDestroy {
       stateData.content = cloneDeep(displayedValue);
       this.interactionIsShown = true;
     });
-
-    // Shivam PTAL.
   }
 
   _init(): void {
