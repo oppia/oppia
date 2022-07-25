3d73732191c41e43fcbee3bc3ce6c32122c0ab95
ShivamJhaa <20bcs206@iiitdmj.ac.in>
2022-07-25 22:36:25 +0530

    Added debugging

--- core/tests/wdio.conf.js
+++ core/tests/wdio.conf.js
@@ -6,6 +6,7 @@ var childProcess = require('child_process');
 var Constants = require('./webdriverio_utils/WebdriverioConstants');
 var DOWNLOAD_PATH = path.resolve(__dirname, Constants.DOWNLOAD_PATH);
 var args = process.argv;
+require('longjohn');
 
 // When tests is running in debug mode, the chrome version number
 // is passed as 7th argument else it is passed as 6th argument.
