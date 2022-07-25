c897e7233980dc53299d06516f0c8ce0588640f2
ashutoshc8101 <ashutoshc8101@gmail.com>
2022-07-26 01:42:23 +0530

    nit

--- core/templates/pages/oppia-root/app-error-handler.ts
+++ core/templates/pages/oppia-root/app-error-handler.ts
@@ -86,8 +86,8 @@ export class AppErrorHandler extends ErrorHandler {
       !(errorType instanceof Error)
     ) {
       // The error passed to this handler in some cases does not provide
-      // a meaningful stack trace of the exception. Different browsers set 
-      // this value at different times. To ensure that the stack property is 
+      // a meaningful stack trace of the exception. Different browsers set
+      // this value at different times. To ensure that the stack property is
       // populated we use try/catch.
       // see: https://web.archive.org/web/20140210004225/http://msdn.microsoft.com/en-us/library/windows/apps/hh699850.aspx
       try {
