2d284de4c8f8ddd38d113deb0159fb3ce15ea245
ShivamJhaa <20bcs206@iiitdmj.ac.in>
2022-07-26 20:07:35 +0530

    testing

--- core/tests/webdriverio_utils/waitFor.js
+++ core/tests/webdriverio_utils/waitFor.js
@@ -177,22 +177,22 @@ var urlRedirection = async function(url) {
 };
 
 var visibilityOfInfoToast = async function(errorMessage) {
-  var toastInfoElement = $('.toast-info');
+  var toastInfoElement = await $('.toast-info');
   await visibilityOf(toastInfoElement, errorMessage);
 };
 
 var invisibilityOfInfoToast = async function(errorMessage) {
-  var toastInfoElement = $('.toast-info');
+  var toastInfoElement = await $('.toast-info');
   await invisibilityOf(toastInfoElement, errorMessage);
 };
 
 var invisibilityOfLoadingMessage = async function(errorMessage) {
-  var loadingMessage = $('.e2e-test-loading-message');
+  var loadingMessage = await $('.e2e-test-loading-message');
   await invisibilityOf(loadingMessage, errorMessage);
 };
 
 var visibilityOfSuccessToast = async function(errorMessage) {
-  var toastSuccessElement = $('.toast-success');
+  var toastSuccessElement = await $('.toast-success');
   await visibilityOf(toastSuccessElement, errorMessage);
 };
 
