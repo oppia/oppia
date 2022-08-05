82262c65b09bb88f0947b60c3eb13648b8e9f444
ShivamJhaa <20bcs206@iiitdmj.ac.in>
2022-08-06 01:50:25 +0530

    testing devtools service

--- core/tests/wdio.conf.js
+++ core/tests/wdio.conf.js
@@ -150,10 +150,7 @@ exports.config = {
   // They enhance your test setup with almost no effort. Unlike plugins,
   // they don't add newcommands. Instead, they hook themselves up into
   // the test process.
-  services: [
-    ['chromedriver', {
-      chromedriverCustomPath: chromedriverPath
-    }]],
+  services: ['devtools'],
 
   // Framework you want to run your specs with.
   // The following are supported: Mocha, Jasmine, and Cucumber
