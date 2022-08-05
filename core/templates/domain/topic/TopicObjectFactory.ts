84e06a48b56c9b1a68b7e24512753744e41d8a27
Nik-09 <nikhil.agarwal.2019@gmail.com>
2022-08-05 11:33:32 +0530

    Linting issue

--- core/templates/domain/topic/TopicObjectFactory.ts
+++ core/templates/domain/topic/TopicObjectFactory.ts
@@ -576,7 +576,7 @@ export class Topic {
     let diagnosticTestSkillSummaries = (
       this.getSkillSummariesForDiagnosticTest());
 
-    const skillIdToDiagnosticTestMap: {[id:string]: boolean} = {};
+    const skillIdToDiagnosticTestMap: {[id: string]: boolean} = {};
     for (let skillSummary of diagnosticTestSkillSummaries) {
       skillIdToDiagnosticTestMap[skillSummary.getId()] = true;
     }
