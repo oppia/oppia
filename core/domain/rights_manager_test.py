7149e70555ad78f0182af8ccf5a1678066ae7f90
sahil jhangar <souravjangar91@gmail.com>
2022-08-06 23:23:48 +0530

    nits

--- core/domain/rights_manager_test.py
+++ core/domain/rights_manager_test.py
@@ -26,8 +26,8 @@ from core.domain import rights_manager
 from core.domain import role_services
 from core.domain import user_domain
 from core.domain import user_services
-from core.tests import test_utils
 from core.platform import models
+from core.tests import test_utils
 
 from typing_extensions import Final
 
