690ae36a7c2b015ec33b46708d400c96d0690f4a
U8N WXD <U8NWXD@users.noreply.github.com>
2022-08-16 18:05:16 -0700

    Clarify backend test shard error messages (#15908)

--- scripts/run_backend_tests.py
+++ scripts/run_backend_tests.py
@@ -295,12 +295,16 @@ def _check_shards_match_tests(include_load_tests=True):
     shard_modules_set = set(shard_modules)
     shard_extra = shard_modules_set - test_modules_set
     if shard_extra:
-        return 'Modules {} in shards not found. See {}.'.format(
-            shard_extra, SHARDS_WIKI_LINK)
+        return (
+            'Modules {} are in the backend test shards but missing from the '
+            'filesystem. See {}.'
+        ).format(shard_extra, SHARDS_WIKI_LINK)
     test_extra = test_modules_set - shard_modules_set
     assert test_extra
-    return 'Modules {} not in shards. See {}.'.format(
-        test_extra, SHARDS_WIKI_LINK)
+    return (
+        'Modules {} are present on the filesystem but are not listed in the '
+        'backend test shards. See {}.'
+    ).format(test_extra, SHARDS_WIKI_LINK)
 
 
 def _load_coverage_exclusion_list(path):
