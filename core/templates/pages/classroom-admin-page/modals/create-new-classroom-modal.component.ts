52b0c39ae191a343e5f344976959a33c76ee4e48
Nik-09 <nikhil.agarwal.2019@gmail.com>
2022-08-27 10:44:10 +0530

    Adds copyright comments in the starting of newly created files.

--- core/templates/pages/classroom-admin-page/modals/create-new-classroom-modal.component.ts
+++ core/templates/pages/classroom-admin-page/modals/create-new-classroom-modal.component.ts
@@ -1,7 +1,27 @@
+
+// Copyright 2022 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Create new classroom modal.
+ */
+
 import { Component } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { ConfirmOrCancelModal } from 'components/common-layout-directives/common-elements/confirm-or-cancel-modal.component';
 import { ClassroomBackendApiService } from '../../../domain/classroom/classroom-backend-api.service';
+
 
 @Component({
   selector: 'oppia-create-new-classroom-modal',
@@ -34,8 +54,6 @@ export class CreateNewClassroomModalComponent
   }
 
   createClassroom(): void {
-    console.log(this.newClassroomName);
-    console.log(this.existingClassroomNames.indexOf(this.newClassroomName) !== -1);
     if (this.existingClassroomNames.indexOf(this.newClassroomName) !== -1) {
         this.classroomNameAlreadyExist = true;
         return;
