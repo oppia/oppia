c0c9f1bb2caebf440d794ff02ac36e6823405117
Pankaj Prajapati <42764756+paprajapati9@users.noreply.github.com>
2022-08-28 01:54:02 +0530

    Milestone 1.5 (Learner group MVP): Add facilitator views of the learner group (#15840)
    * added learner group model
    
    * added takeout and wipeout policy
    
    * added learner group model test file
    
    * added tests for learner group model
    
    * added learner group user model
    
    * added tests for learner group user model
    
    * added learner group user domain and tests
    
    * lint fixes
    
    * mypy fixes
    
    * added learner group domain
    
    * mypy test fixes
    
    * multiple fixes
    
    * model changes
    
    * backend test fixes
    
    * mypy fixes
    
    * mypy fixes
    
    * removed unwanted code
    
    * extended backend test coverage
    
    * fix backend tests
    
    * added controllers
    
    * review changes
    
    * minor change
    
    * minor change
    
    * name changes
    
    * review changes
    
    * lint fixes
    
    * minor changes
    
    * minor changes
    
    * review changes
    
    * review changes
    
    * minor change
    
    * minor change
    
    * multiple changes
    
    * review changes
    
    * lint fixes
    
    * review changes
    
    * review changes
    
    * lint fixes
    
    * minor change
    
    * review changes
    
    * test coverage fix
    
    * multiple changes
    
    * review changes
    
    * multiple changes
    
    * multiple lint fixes
    
    * multiple changes
    
    * lint fixes
    
    * multiple changes
    
    * added tests
    
    * added tests
    
    * added multiple tests
    
    * added multiple tests
    
    * added tests
    
    * added multiple tests
    
    * multiple changes
    
    * added multiple models and api services
    
    * lint fixes
    
    * fix backend test
    
    * added 2 test files
    
    * fix backend test
    
    * fix backend test
    
    * review changes
    
    * added multiple tests files
    
    * multiple review changes
    
    * minor changes
    
    * added teacher dashboard page
    
    * review changes
    
    * lint fixes
    
    * added creator page
    
    * review changes
    
    * review changes
    
    * added create group page
    
    * mypy test fix
    
    * added add syllabus tab
    
    * review changes
    
    * lint fixes
    
    * minor change
    
    * fix backend test
    
    * fix search of syllabus
    
    * minor changes
    
    * review changes
    
    * search-query-fix
    
    * minor change
    
    * modified syllabus tab
    
    * added invite students tab
    
    * minor fix
    
    * name changes
    
    * review changes
    
    * review changes
    
    * multiple changes
    
    * minor merge fix
    
    * review changes
    
    * multiple changes to creation journey and updated facilitator dashboard
    
    * review changes
    
    * lint fixes
    
    * multiple lint fixes
    
    * multiple changes
    
    * review changes
    
    * minor changes
    
    * minor review changes
    
    * multiple changes on the creatoin joourney and teacher dashboard
    
    * review changes
    
    * minor change
    
    * lint fixes
    
    * minor change
    
    * added multiple test files
    
    * lint fixes
    
    * added new tests
    
    * Added multiple tests
    
    * multiple changes
    
    * test fixes
    
    * type fixes
    
    * multiple fixes
    
    * minor frontend test fix
    
    * added rtl support
    
    * extended frontend coverage
    
    * fix backend tests
    
    * fix backend tests
    
    * fix backend tests
    
    * added edit learner group page
    
    * multiple docstring changes
    
    * added multiple views
    
    * multiple view changes
    
    * review changes
    
    * review changes
    
    * Added multiple views for edit learner group
    
    * multiple changes
    
    * lint fixes
    
    * multiple changes
    
    * lint checks
    
    * added multiple backend tests
    
    * finalized learner group view
    
    * lint fixes
    
    * multiple changes
    
    * type check fixes
    
    * added frontend tests
    
    * lint fixes
    
    * added multiple test cases
    
    * karma test fixes
    
    * added multiple keys to qqq.json
    
    * review changes
    
    * review changes
    
    * extended frontend coverage
    
    * minor change
    
    * test fixes
    
    * review changes
    
    * minor change
    
    * review changes
    
    * minor change
    
    * minor change
    
    * minor change
    
    * changing students name to learners
    
    * multiple changes
    
    * minor change
    
    * multiple changes
    
    * ui fixes
    
    * minor change
    
    * ui fixes
    
    * test fix
    
    * review changes
    
    * minor change
    
    * changing student name to learner
    
    * minor change
    
    * multiple changes
    
    * minor changes
    
    * review ui fixes
    
    * review fixes

---
+++ core/templates/pages/learner-group-pages/edit-group/learner-group-preferences.component.ts
@@ -0,0 +1,230 @@
+// Copyright 2022 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Component for the learner group preferences.
+ */
+
+import { Component, Input, OnInit } from '@angular/core';
+import { downgradeComponent } from '@angular/upgrade/static';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { LearnerGroupBackendApiService } from 'domain/learner_group/learner-group-backend-api.service';
+import { LearnerGroupUserInfo } from 'domain/learner_group/learner-group-user-info.model';
+import { LearnerGroupData } from 'domain/learner_group/learner-group.model';
+import { LearnerGroupPagesConstants } from '../learner-group-pages.constants';
+import { InviteLearnersModalComponent } from '../templates/invite-learners-modal.component';
+import { InviteSuccessfulModalComponent } from '../templates/invite-successful-modal.component';
+import { RemoveItemModalComponent } from
+  '../templates/remove-item-modal.component';
+
+import './learner-group-preferences.component.css';
+
+
+@Component({
+  selector: 'oppia-learner-group-preferences',
+  templateUrl: './learner-group-preferences.component.html'
+})
+export class LearnerGroupPreferencesComponent implements OnInit {
+  @Input() learnerGroup!: LearnerGroupData;
+  activeTab!: string;
+  newLearnerGroupTitle!: string;
+  newLearnerGroupDescription!: string;
+  readOnlyMode = true;
+  invitedLearnersInfo!: LearnerGroupUserInfo[];
+  currentLearnersInfo!: LearnerGroupUserInfo[];
+  invitedLearners: string[] = [];
+  EDIT_PREFERENCES_SECTIONS_I18N_IDS = (
+    LearnerGroupPagesConstants.EDIT_LEARNER_GROUP_PREFERENCES_SECTIONS);
+
+  constructor(
+    private ngbModal: NgbModal,
+    private learnerGroupBackendApiService:
+      LearnerGroupBackendApiService
+  ) {}
+
+  ngOnInit(): void {
+    this.activeTab = this.EDIT_PREFERENCES_SECTIONS_I18N_IDS.GROUP_DETAILS;
+    if (this.learnerGroup) {
+      this.learnerGroupBackendApiService.fetchLearnersInfoAsync(
+        this.learnerGroup.id).then((learnersInfo) => {
+        this.currentLearnersInfo = learnersInfo.learnersInfo;
+        this.invitedLearnersInfo = learnersInfo.invitedLearnersInfo;
+      });
+    }
+  }
+
+  isTabActive(tabName: string): boolean {
+    return this.activeTab === tabName;
+  }
+
+  setActiveTab(tabName: string): void {
+    this.activeTab = tabName;
+  }
+
+  updateLearnerGroupTitle(title: string): void {
+    this.newLearnerGroupTitle = title;
+  }
+
+  updateLearnerGroupDesc(description: string): void {
+    this.newLearnerGroupDescription = description;
+  }
+
+  toggleReadOnlyMode(): void {
+    this.readOnlyMode = !this.readOnlyMode;
+  }
+
+  isReadOnlyModeActive(): boolean {
+    return this.readOnlyMode;
+  }
+
+  saveLearnerGroupInfo(): void {
+    if (this.newLearnerGroupTitle || this.newLearnerGroupDescription) {
+      this.learnerGroup.title = (
+        this.newLearnerGroupTitle ?
+        this.newLearnerGroupTitle : this.learnerGroup.title
+      );
+      this.learnerGroup.description = (
+        this.newLearnerGroupDescription ?
+        this.newLearnerGroupDescription : this.learnerGroup.description
+      );
+      this.learnerGroupBackendApiService.updateLearnerGroupAsync(
+        this.learnerGroup).then((learnerGroup) => {
+        this.learnerGroup = learnerGroup;
+      });
+    }
+    this.toggleReadOnlyMode();
+  }
+
+  openInviteLearnersModal(): void {
+    let modalRef = this.ngbModal.open(
+      InviteLearnersModalComponent,
+      {
+        backdrop: 'static',
+        windowClass: 'invite-learners-modal'
+      }
+    );
+    modalRef.componentInstance.learnerGroupId = this.learnerGroup.id;
+
+    modalRef.result.then((data) => {
+      this.invitedLearners = data.invitedLearners;
+      this.learnerGroup.inviteLearners(this.invitedLearners);
+      this.learnerGroupBackendApiService.updateLearnerGroupAsync(
+        this.learnerGroup).then((learnerGroup) => {
+        this.learnerGroup = learnerGroup;
+        this.invitedLearnersInfo.push(...data.invitedLearnersInfo);
+      });
+      let successModalRef = this.ngbModal.open(
+        InviteSuccessfulModalComponent,
+        {
+          backdrop: 'static',
+          windowClass: 'invite-successful-modal'
+        }
+      );
+      successModalRef.componentInstance.successMessage = (
+        'An invitation has been sent to ');
+      successModalRef.componentInstance.invitedUsernames = (
+        this.invitedLearners);
+
+      successModalRef.result.then(() => {
+        // Note to developers:
+        // This callback is triggered when the Confirm button is clicked.
+        // No further action is needed.
+      }, () => {
+        // Note to developers:
+        // This callback is triggered when the Cancel button is clicked.
+        // No further action is needed.
+      });
+    }, () => {
+      // Note to developers:
+      // This callback is triggered when the Cancel button is clicked.
+      // No further action is needed.
+    });
+  }
+
+  openRemoveLearnerFromGroupModal(learner: LearnerGroupUserInfo): void {
+    let modalRef = this.ngbModal.open(
+      RemoveItemModalComponent,
+      {
+        backdrop: 'static',
+        windowClass: 'remove-learner-modal'
+      }
+    );
+    modalRef.componentInstance.confirmationTitle = 'Remove Learner';
+    modalRef.componentInstance.confirmationMessage = (
+      'Are you sure you want to remove this learner from the group?'
+    );
+
+    modalRef.result.then(() => {
+      this.learnerGroup.removeLearner(learner.username);
+      this.learnerGroupBackendApiService.updateLearnerGroupAsync(
+        this.learnerGroup).then((learnerGroup) => {
+        this.learnerGroup = learnerGroup;
+        this.currentLearnersInfo = this.currentLearnersInfo.filter(
+          (currentLearner) => currentLearner.username !== learner.username
+        );
+      });
+    });
+  }
+
+  openWithdrawLearnerInvitationModal(learner: LearnerGroupUserInfo): void {
+    let modalRef = this.ngbModal.open(
+      RemoveItemModalComponent,
+      {
+        backdrop: 'static',
+        windowClass: 'withdraw-learner-invitation-modal'
+      }
+    );
+    modalRef.componentInstance.confirmationTitle = 'Withdraw Invitation';
+    modalRef.componentInstance.confirmationMessage = (
+      'Are you sure you want to withdraw this invitation?'
+    );
+
+    modalRef.result.then(() => {
+      this.learnerGroup.revokeInvitation(learner.username);
+      this.invitedLearnersInfo = this.invitedLearnersInfo.filter(
+        (invitedLearner) => invitedLearner.username !== learner.username
+      );
+      this.learnerGroupBackendApiService.updateLearnerGroupAsync(
+        this.learnerGroup).then((learnerGroup) => {
+        this.learnerGroup = learnerGroup;
+      });
+    });
+  }
+
+  updateInvitedLearners(invitedLearners: string[]): void {
+    this.invitedLearners = invitedLearners;
+  }
+
+  getProfileImageDataUrl(dataUrl: string): string {
+    return decodeURIComponent(dataUrl);
+  }
+
+  addLearnerToLearnerGroup(learner: LearnerGroupUserInfo): void {
+    this.learnerGroup.addLearner(learner.username);
+    this.learnerGroup.revokeInvitation(learner.username);
+    this.invitedLearnersInfo = this.invitedLearnersInfo.filter(
+      (invitedLearner) => invitedLearner.username !== learner.username
+    );
+    this.learnerGroupBackendApiService.updateLearnerGroupInviteAsync(
+      this.learnerGroup.id, learner.username, true, true
+    ).then((learnerGroup) => {
+      this.learnerGroup = learnerGroup;
+      this.currentLearnersInfo.push(learner);
+    });
+  }
+}
+
+angular.module('oppia').directive(
+  'oppiaLearnerGroupPreferences',
+  downgradeComponent({component: LearnerGroupPreferencesComponent}));
