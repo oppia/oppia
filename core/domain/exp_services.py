2cb7aacac13e0bddf8d5a596503d10927e714abb
Soumyajyoti Dey <deysoumyajyoti2017@gmail.com>
2022-08-29 16:58:56 +0530

    fix lint errors

--- core/domain/exp_services.py
+++ core/domain/exp_services.py
@@ -842,8 +842,12 @@ def update_states_version_history(
     exp_versions_diff = exp_domain.ExplorationVersionsDiff(change_list)
     prev_version = current_version - 1
 
-    logging.info('Deleted state names: %s' % (exp_versions_diff.deleted_state_names))
-    logging.info('Renamed states: %s' % (exp_versions_diff.old_to_new_state_names))
+    logging.info(
+        'Deleted state names: %s' % (exp_versions_diff.deleted_state_names)
+    )
+    logging.info(
+        'Renamed states: %s' % (exp_versions_diff.old_to_new_state_names)
+    )
 
     # Firstly, delete the states from the state version history which were
     # deleted during this commit.
