a3cf9ec54344236f5e3f8d2fb9db5e4b55a43334
Manan Rathi <rathimanan27@gmail.com>
2022-09-01 00:34:07 +0530

    Backend fix

--- core/domain/exp_domain_test.py
+++ core/domain/exp_domain_test.py
@@ -5790,8 +5790,14 @@ class StateOperationsUnitTests(test_utils.GenericTestBase):
             exploration.delete_state(exploration.init_state_name)
 
         exploration.add_states(['second state'])
-        exploration.delete_state('second state')
 
+        interaction = exploration.states['first state'].interaction
+        default_outcome_for_first_state = interaction.default_outcome
+        default_outcome_for_first_state.dest_if_really_stuck = 'second state'
+
+        exploration.delete_state('second state')
+        self.assertEqual(
+            default_outcome_for_first_state.dest_if_really_stuck, 'first state')
         with self.assertRaisesRegex(ValueError, 'fake state does not exist'):
             exploration.delete_state('fake state')
 
