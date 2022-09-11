5340c2ae70c2bcd3306fe1739758d5c0a86351ce
Shivkant Chauhan <shivkantchauhan2002@gmail.com>
2022-09-11 23:27:24 +0530

    button condition for end exploration

--- core/templates/pages/exploration-editor-page/editor-tab/templates/modal-templates/customize-interaction-modal.component.ts
+++ core/templates/pages/exploration-editor-page/editor-tab/templates/modal-templates/customize-interaction-modal.component.ts
@@ -256,7 +256,7 @@ export class CustomizeInteractionModalComponent
   isSaveInteractionButtonEnabled(): boolean {
     return !!(
       this.getTitle
-        (this.stateInteractionIdService.displayed) === 'End Exploration' ||
+      (this.stateInteractionIdService.displayed) === 'End Exploration' ||
       (
         this.hasCustomizationArgs &&
         this.stateInteractionIdService.displayed &&
