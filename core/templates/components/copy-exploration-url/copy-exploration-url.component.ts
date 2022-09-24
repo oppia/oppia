c46d53fd6276d17f891260ba305bd3f395924c22
Shivkant Chauhan <shivkantchauhan2002@gmail.com>
2022-09-24 13:45:18 +0530

    new component

---
+++ core/templates/components/copy-exploration-url/copy-exploration-url.component.ts
@@ -0,0 +1,33 @@
+// import { Clipboard } from '@angular/cdk/clipboard';
+import { Component } from '@angular/core';
+// import { ExplorationPlayerStateService } from 'pages/exploration-player-page/services/exploration-player-state.service';
+
+@Component({
+  selector: 'copy-exploration-url',
+  templateUrl: './copy-exploration-url.component.html'
+})
+
+export class ComponentOverviewComponent {
+  // showTooltip: boolean = false;
+  // loggedOutProgressUniqueUrlId: string;
+  // loggedOutProgressUniqueUrl: string;
+
+  // constructor(
+  //   private clipboard: Clipboard,
+  //   private explorationPlayerStateService: ExplorationPlayerStateService
+  // ) {}
+  
+  // ngOnInit(): void {
+  //   this.loggedOutProgressUniqueUrlId = (
+  //     this.explorationPlayerStateService.getUniqueProgressUrlId());
+  // }
+
+  // copyProgressUrl(): void {
+  //   this.clipboard.copy(this.loggedOutProgressUniqueUrl);
+  //   this.showTooltip = true;
+  //   setTimeout(() => {
+  //     this.showTooltip = false;
+  //   }, 1000);
+  // }
+
+}
