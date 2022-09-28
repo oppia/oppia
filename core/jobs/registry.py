11837e381102e106d087208515a075bf9a8517ca
Srikanth Kadaba Bhogananda [I] <skadababhogananda@tesla.com>
2022-09-27 21:21:53 -0400

    add jobs for proto size

--- core/jobs/registry.py
+++ core/jobs/registry.py
@@ -49,6 +49,7 @@ from core.jobs.batch_jobs import email_deletion_jobs                 # pylint: d
 from core.jobs.batch_jobs import exp_migration_jobs                  # pylint: disable=unused-import  # isort: skip
 from core.jobs.batch_jobs import (                                   # pylint: disable=unused-import  # isort: skip
     exp_recommendation_computation_jobs)
+from core.jobs.batch_jobs import exp_proto_size_calculation_job      # pylint: disable=unused-import  # isort: skip
 from core.jobs.batch_jobs import exp_search_indexing_jobs            # pylint: disable=unused-import  # isort: skip
 from core.jobs.batch_jobs import model_validation_jobs               # pylint: disable=unused-import  # isort: skip
 from core.jobs.batch_jobs import opportunity_management_jobs         # pylint: disable=unused-import  # isort: skip
