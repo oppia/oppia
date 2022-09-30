6e4ebf521892eefb450e702397067e2d98fd39ff
Lawful2002 <sharshvardhan101@gmail.com>
2022-09-30 18:15:14 +0530

    fixed backend tests

--- core/domain/exp_fetchers_test.py
+++ core/domain/exp_fetchers_test.py
@@ -166,7 +166,7 @@ class ExplorationRetrievalTests(test_utils.GenericTestBase):
                 'does not match the latest schema version %s' % (
                     self.EXP_1_ID,
                     '1',
-                    '52',
+                    '53',
                     '60'
                 )
         )
