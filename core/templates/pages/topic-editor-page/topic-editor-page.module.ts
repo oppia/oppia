1cf3a8f80a464da514efd315813b7df97dcccde6
mridul-netizen <mridulsetia18@gmail.com>
2022-10-03 15:47:17 +0530

    Fix Part Of #9749: Migrates Topic Editor Navbar to Angular (#16107)
    * updated
    
    * updated
    
    * updated
    
    * indentation
    
    * updated
    
    * fixes
    
    * updated
    
    * updated
    
    * short-summary-fixed
    
    * resolve lints
    
    * e2e fixed
    
    * Revert "Adds Autofocus on exploration interactions (#12538)"
    
    This reverts commit 2d35d7c4353b742d4ce87a5d3b55c15de2846bc4.
    
    save-merge
    
    * updated
    
    * Revert "Revert "Adds Autofocus on exploration interactions (#12538)""
    
    This reverts commit 03e76a58723477393cbf1cf607f0afd80138888d.
    
    save-merge
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * nits
    
    * nit
    
    * nit
    
    * nits
    
    * nits
    
    * nit
    
    * lints
    
    * topic-editor-navbar migrated
    
    * lints
    
    * unit tests coverage
    "
    
    * nits
    
    * topic-editor-routing fix
    
    * oppia- prefix css

--- core/templates/pages/topic-editor-page/topic-editor-page.module.ts
+++ core/templates/pages/topic-editor-page/topic-editor-page.module.ts
@@ -47,6 +47,7 @@ import { TopicEditorSendMailComponent } from './modal-templates/topic-editor-sen
 import { TopicEditorSaveModalComponent } from './modal-templates/topic-editor-save-modal.component';
 import { SmartRouterModule } from 'hybrid-router-module-provider';
 import { AppErrorHandlerProvider } from 'pages/oppia-root/app-error-handler';
+import { TopicEditorNavbarComponent } from './navbar/topic-editor-navbar.component';
 import { TopicQuestionsTabComponent } from './questions-tab/topic-questions-tab.component';
 
 @NgModule({
@@ -73,6 +74,7 @@ import { TopicQuestionsTabComponent } from './questions-tab/topic-questions-tab.
     DeleteStoryModalComponent,
     TopicEditorSendMailComponent,
     TopicEditorSaveModalComponent,
+    TopicEditorNavbarComponent,
     TopicQuestionsTabComponent
   ],
   entryComponents: [
@@ -86,6 +88,7 @@ import { TopicQuestionsTabComponent } from './questions-tab/topic-questions-tab.
     DeleteStoryModalComponent,
     TopicEditorSendMailComponent,
     TopicEditorSaveModalComponent,
+    TopicEditorNavbarComponent,
     TopicQuestionsTabComponent
   ],
   providers: [
