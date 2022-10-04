b030d84ddd7763ec145785eda52a1a5fb9226c2b
Srikanth Kadaba Bhogananda [I] <skadababhogananda@tesla.com>
2022-10-03 23:17:58 -0400

    add migration jobs for oppia

--- core/jobs/batch_jobs/exp_proto_size_calculation_job.py
+++ core/jobs/batch_jobs/exp_proto_size_calculation_job.py
@@ -22,13 +22,11 @@ import logging
 
 from core import feconf
 from core import utils
-from core.constants import constants
 from core.domain import caching_services
 from core.domain import exp_domain
 from core.domain import exp_fetchers
 from core.domain import exp_services
 from core.domain import opportunity_services
-from core.domain import rights_manager
 from core.jobs import base_jobs
 from core.jobs.io import ndb_io
 from core.jobs.transforms import job_result_transforms
@@ -54,7 +52,8 @@ datastore_services = models.Registry.import_datastore_services()
 
 
 class PopulateExplorationProtoSizeInBytesJob(base_jobs.JobBase):
-    """Job that migrates Exploration models to include new attribute."""
+    """Job that populates android_proto_size_in_bytes attribute
+    for Exploration models."""
 
     @staticmethod
     def _migrate_exploration(
@@ -90,7 +89,6 @@ class PopulateExplorationProtoSizeInBytesJob(base_jobs.JobBase):
         except Exception as e:
             logging.exception(e)
             return result.Err((exp_id, e))
-        print('***********')
         return result.Ok((exp_id, exploration))
 
     @staticmethod
@@ -103,18 +101,16 @@ class PopulateExplorationProtoSizeInBytesJob(base_jobs.JobBase):
         than the latest schema version.
 
         Args:
-            exp_id: str. The ID of the exploration.
-            exp_model: ExplorationModel. The exploration for which to generate
-                the change objects.
+            exp_model: ExplorationModel. The exploration model for which to
+                generate the change objects.
+            exploration: Exploration. The exploration domain object for which
+                to generate the change objects.
 
         Yields:
             (str, ExplorationChange). Tuple containing exploration ID and
             ExplorationChange object.
         """
         exp_version = exp_model.version
-        print('***********')
-        print(exp_model.android_proto_size_in_bytes)
-        print(exploration.android_proto_size_in_bytes)
         if exp_version < exp_domain.Exploration.CURRENT_EXP_SCHEMA_VERSION:
             exp_change = exp_domain.ExplorationChange({
                 'cmd': (
@@ -145,7 +141,6 @@ class PopulateExplorationProtoSizeInBytesJob(base_jobs.JobBase):
             return result.Ok(exploration.id)
         except Exception as e:
             return result.Err(e)
-
 
     @staticmethod
     def _update_exploration(
@@ -316,7 +311,8 @@ class PopulateExplorationProtoSizeInBytesJob(base_jobs.JobBase):
         exp_rights_models = (
                 self.pipeline
                 | 'Get all non-deleted exploration rights models' >> (
-                    ndb_io.GetModels(exp_models.ExplorationRightsModel.get_all()))
+                    ndb_io.GetModels(
+                        exp_models.ExplorationRightsModel.get_all()))
                 | 'Add exploration rights ID' >> beam.WithKeys(  # pylint: disable=no-value-for-parameter
             lambda exp_rights_model: exp_rights_model.id)
         )
@@ -382,21 +378,6 @@ class PopulateExplorationProtoSizeInBytesJob(base_jobs.JobBase):
             })
         )
 
-        # transformed_exp_objects_list = (
-        #     exp_objects_list
-        #     | 'Remove unmigrated explorations' >> beam.Filter(
-        #         lambda x: (
-        #                 len(x['exp_changes']) > 0 and
-        #                 len(x['exploration']) > 0
-        #         ))
-        #     | 'Reorganize the transformed exploration objects' >> beam.Map(lambda objects: {
-        #         'exp_model': objects['exp_model'][0],
-        #         'exploration': objects['exploration'][0],
-        #         'exp_rights_model': objects['exp_rights_model'][0],
-        #         'exp_changes': objects['exp_changes']
-        #     })
-        # )
-
         exp_objects_list_job_run_results = (
             exp_objects_list
             | 'Transform exploration objects into job run results' >> (
@@ -415,6 +396,16 @@ class PopulateExplorationProtoSizeInBytesJob(base_jobs.JobBase):
                 ))
         )
 
+        cache_deletion_job_run_results = (
+                exp_objects_list
+                | 'Delete exploration from cache' >> beam.Map(
+            lambda exp_object: self._delete_exploration_from_cache(
+                exp_object['exploration']))
+                | 'Generate results for cache deletion' >> (
+                    job_result_transforms.ResultsToJobRunResults(
+                        'CACHE DELETION'))
+        )
+
         unused_put_results = (
             exp_models_to_put
             # | 'Merge models' >> beam.Flatten()
@@ -425,174 +416,9 @@ class PopulateExplorationProtoSizeInBytesJob(base_jobs.JobBase):
 
         return (
             (
-                # cache_deletion_job_run_results,
+                cache_deletion_job_run_results,
                 migrated_exploration_job_run_results,
                 exp_objects_list_job_run_results,
-                # already_migrated_job_run_results
             )
             | beam.Flatten()
         )
-
-#
-# # TODO(#15927): This job needs to be kept in sync with MigrateExplorationJob and
-# # later we will unify these jobs together.
-# class AuditExplorationMigrationJob(base_jobs.JobBase):
-#     """Job that migrates Exploration models."""
-#
-#     @staticmethod
-#     def _migrate_exploration(
-#         exp_id: str,
-#         exp_model: exp_models.ExplorationModel
-#     ) -> result.Result[
-#         Tuple[str, exp_domain.Exploration],
-#         Tuple[str, Exception]
-#     ]:
-#         """Migrates exploration and transform exploration model into
-#         exploration object.
-#
-#         Args:
-#             exp_id: str. The ID of the exploration.
-#             exp_model: ExplorationModel. The exploration model to migrate.
-#
-#         Returns:
-#             Result((str, Exploration), (str, Exception)). Result containing
-#             tuple that consists of exploration ID and either Exploration object
-#             or Exception. Exploration object is returned when the migration was
-#             successful and Exception is returned otherwise.
-#         """
-#         try:
-#             exploration = exp_fetchers.get_exploration_from_model(exp_model)
-#             exploration.validate()
-#
-#             with datastore_services.get_ndb_context():
-#                 if exp_services.get_story_id_linked_to_exploration(
-#                     exp_id
-#                 ) is not None:
-#                     exp_services.validate_exploration_for_story(
-#                         exploration, True)
-#
-#         except Exception as e:
-#             logging.exception(e)
-#             return result.Err((exp_id, e))
-#
-#         return result.Ok((exp_id, exploration))
-#
-#     @staticmethod
-#     def _generate_exploration_changes(
-#         exp_id: str, exp_model: exp_models.ExplorationModel
-#     ) -> Iterable[Tuple[str, exp_domain.ExplorationChange]]:
-#         """Generates exploration change objects. The ExplorationChange object
-#         is only generated when the exploration's states schema version is lower
-#         than the latest schema version.
-#
-#         Args:
-#             exp_id: str. The ID of the exploration.
-#             exp_model: ExplorationModel. The exploration for which to generate
-#                 the change objects.
-#
-#         Yields:
-#             (str, ExplorationChange). Tuple containing exploration ID and
-#             ExplorationChange object.
-#         """
-#         exp_states_version = exp_model.states_schema_version
-#         if exp_states_version < feconf.CURRENT_STATE_SCHEMA_VERSION:
-#             exp_change = exp_domain.ExplorationChange({
-#                 'cmd': (
-#                     exp_domain.CMD_MIGRATE_STATES_SCHEMA_TO_LATEST_VERSION),
-#                 'from_version': exp_states_version,
-#                 'to_version': feconf.CURRENT_STATE_SCHEMA_VERSION
-#             })
-#             yield (exp_id, exp_change)
-#
-#     def run(self) -> beam.PCollection[job_run_result.JobRunResult]:
-#         """Returns a PCollection of results from the audit of exploration
-#         migration.
-#
-#         Returns:
-#             PCollection. A PCollection of results from the exploration
-#             migration.
-#         """
-#
-#         unmigrated_exploration_models = (
-#             self.pipeline
-#             | 'Get all non-deleted exploration models' >> (
-#                 ndb_io.GetModels(
-#                     exp_models.ExplorationModel.get_all(
-#                         include_deleted=False)))
-#             # Pylint disable is needed because pylint is not able to correctly
-#             # detect that the value is passed through the pipe.
-#             | 'Add exploration keys' >> beam.WithKeys(  # pylint: disable=no-value-for-parameter
-#                 lambda exp_model: exp_model.id)
-#             # TODO(#15871): This filter should be removed after the explorations
-#             # are fixed and it is possible to migrate them.
-#             | 'Remove broken exploration' >> beam.Filter(
-#                 lambda id_and_exp: id_and_exp[0] not in (
-#                     'umPkwp0L1M0-', '670bU6d9JGBh'))
-#         )
-#
-#         migrated_exp_results = (
-#             unmigrated_exploration_models
-#             | 'Transform and migrate model' >> beam.MapTuple( # pylint: disable=no-value-for-parameter
-#                 self._migrate_exploration)
-#         )
-#
-#         migrated_exp = (
-#             migrated_exp_results
-#             | 'Filter oks' >> beam.Filter(
-#                 lambda result_item: result_item.is_ok())
-#             | 'Unwrap ok' >> beam.Map(
-#                 lambda result_item: result_item.unwrap())
-#         )
-#
-#         migrated_exp_job_run_results = (
-#             migrated_exp_results
-#             | 'Generate results for migration' >> (
-#                 job_result_transforms.ResultsToJobRunResults('EXP PROCESSED'))
-#         )
-#
-#         exp_changes = (
-#             unmigrated_exploration_models
-#             | 'Generate exploration changes' >> beam.FlatMapTuple(
-#                 self._generate_exploration_changes)
-#         )
-#
-#         exp_objects_list = (
-#             {
-#                 'exp_model': unmigrated_exploration_models,
-#                 'exploration': migrated_exp,
-#                 'exp_changes': exp_changes
-#             }
-#             | 'Merge objects' >> beam.CoGroupByKey()
-#             | 'Get rid of ID' >> beam.Values() # pylint: disable=no-value-for-parameter
-#         )
-#
-#         exp_objects_list_job_run_results = (
-#             exp_objects_list
-#             | 'Remove unmigrated explorations' >> beam.Filter(
-#                 lambda x: (
-#                     len(x['exp_changes']) > 0 and len(x['exploration']) > 0
-#                 ))
-#             | 'Transform exp objects into job run results' >> (
-#                 job_result_transforms.CountObjectsToJobRunResult(
-#                     'EXP MIGRATED'))
-#         )
-#
-#         already_migrated_job_run_results = (
-#             exp_objects_list
-#             | 'Remove migrated explorations' >> beam.Filter(
-#                 lambda x: (
-#                     len(x['exp_changes']) == 0 and len(x['exploration']) > 0
-#                 ))
-#             | 'Transform previously migrated exps into job run results' >> (
-#                 job_result_transforms.CountObjectsToJobRunResult(
-#                     'EXP PREVIOUSLY MIGRATED'))
-#         )
-#
-#         return (
-#             (
-#                 migrated_exp_job_run_results,
-#                 exp_objects_list_job_run_results,
-#                 already_migrated_job_run_results
-#             )
-#             | beam.Flatten()
-#         )
