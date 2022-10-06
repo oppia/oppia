635fb9d6a68d91f6c8cadca31b4669b60c0363f2
mridul-netizen <mridulsetia18@gmail.com>
2022-10-06 13:32:45 +0530

    nit

--- core/tests/webdriverio_utils/TopicEditorPage.js
+++ core/tests/webdriverio_utils/TopicEditorPage.js
@@ -196,8 +196,6 @@ var TopicEditorPage = function() {
   };
 
   this.expectSubtopicPageContentsToMatch = async function(contents) {
-    await action.click(
-      'Subtopic Page Content Button', subtopicPageContentButton);
     var text = await action.getText(
       'Subtopic Content Text', subtopicContentText);
     expect(text).toMatch(contents);
