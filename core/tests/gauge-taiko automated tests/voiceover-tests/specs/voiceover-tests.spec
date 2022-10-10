ad479f193b53812832f112bf7de6c5e5cb45f72b
Shivkant Chauhan <shivkantchauhan2002@gmail.com>
2022-10-10 12:52:37 +0530

    voiceover CUJ automated testing

---
+++ core/tests/gauge-taiko automated tests/voiceover-tests/specs/voiceover-tests.spec
@@ -0,0 +1,16 @@
+# Getting Started with Gauge
+
+This is an example markdown specification file.
+Every heading in this file is a scenario.
+Every bulleted point is a step.
+
+To execute this specification, use
+	npm test
+
+This is a context step that runs before every scenario
+* Sign-in to the dev server
+
+## Display number of items
+* Create a new Exploration
+* Go to the Translations tab
+* Record new audio
---
+++ core/tests/puppeteer-automated tests/voiceover-tests/upload-recording.js
@@ -0,0 +1,48 @@
+const puppeteer = require("puppeteer");
+
+//adding headless flag to false and maximizing browser height-width
+puppeteer
+  .launch({
+    headless: false,
+    args: ["--start-fullscreen", "--use-fake-ui-for-media-stream"],
+  })
+  .then(async (browser) => {
+    //browser new page
+    const page = await browser.newPage();
+    await page.setViewport({ width: 0, height: 0 }); // for maximizing page height-width
+
+    // logging into dev server!
+    await page.goto("http://localhost:8181/", {waitUntil: "networkidle0"});
+    await page.waitForSelector("button.e2e-mobile-test-login");
+    await page.click("button.e2e-mobile-test-login");
+    let selector = "input.e2e-test-sign-in-email-input";
+    await page.waitForSelector(selector);
+    await page.type(selector, "testadmin@example.com");
+    selector = "button.e2e-test-sign-in-button";
+    // does puppeteer waits until the typing is completed?
+    await page.evaluate(() => {
+      document.querySelector('.e2e-test-sign-in-button').click();
+    });
+    selector = ".oppia-learner-dashboard-main-content";
+    await page.waitForSelector(selector);
+
+    // creating a new exploration
+    await page.goto("http://localhost:8181/creator-dashboard", {waitUntil: "networkidle0"});
+    selector = "button.e2e-test-create-new-exploration-button";
+    await page.waitForSelector(selector);
+    await page.click(selector);
+
+    // going into the Translations Tab
+    selector = "li#tutorialTranslationTab";
+    await page.waitForSelector(selector);
+    await page.click(selector);
+
+    // uploading the audio
+    selector = 'button.e2e-test-accessibility-translation-upload-audio';
+    await page.waitForSelector(selector);
+    await page.click(selector);
+
+
+    console.log("Successfully tested uploading audio!");
+    // await browser.close();
+  });
