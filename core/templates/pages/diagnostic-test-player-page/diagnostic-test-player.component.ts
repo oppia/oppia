d0accc0161cb5d8298da5e5c742409c810319d00
Nik-09 <nikhil.agarwal.2019@gmail.com>
2022-10-11 14:39:16 +0530

    Adds html text

--- core/templates/pages/diagnostic-test-player-page/diagnostic-test-player.component.ts
+++ core/templates/pages/diagnostic-test-player-page/diagnostic-test-player.component.ts
@@ -1,13 +1,40 @@
+// Copyright 2019 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Diagnostic test player component.
+ */
+
 import { Component, OnInit } from "@angular/core";
 import { downgradeComponent } from '@angular/upgrade/static';
+import { UrlInterpolationService } from 'domain/utilities/url-interpolation.service';
 
 @Component({
   selector: 'oppia-diagnostic-test-player',
   templateUrl: './diagnostic-test-player.component.html'
 })
 export class DiagnosticTestPlayerComponent implements OnInit{
+  OPPIA_AVATAR_IMAGE_URL!: string;
+
+  constructor(
+    private urlInterpolationService: UrlInterpolationService
+  ) {}
+
   ngOnInit(): void {
-    console.log('Nikhil');
+    this.OPPIA_AVATAR_IMAGE_URL = (
+      this.urlInterpolationService.getStaticImageUrl(
+        '/avatar/oppia_avatar_100px.svg'));
   }
 }
 
