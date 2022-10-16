8343dacf81d40a6979fdd45445a0f113b09ea783
Praneeth <praneethg2001@gmail.com>
2022-10-16 11:26:46 -0700

    Edit firebase json to allow authentication

--- start_docker.sh
+++ start_docker.sh
@@ -7,4 +7,4 @@ oppia_loc=${args[0]}
 
 docker build -t oppia -f ubuntu_dockerfile .
 
-docker run -p 8181:8181 -v ${oppia_loc}:/home/opensource/oppia -it oppia /bin/bash
+docker run -p 8181:8181 -p 9099:9099 -p 4000:4000 -v ${oppia_loc}:/home/opensource/oppia -it oppia /bin/bash
