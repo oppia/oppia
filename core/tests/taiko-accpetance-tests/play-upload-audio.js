37ad11340b1b9372f752bf17c0482e212b73dd99
Shivkant Chauhan <shivkantchauhan2002@gmail.com>
2022-10-17 16:32:50 +0530

    added taiko tests

--- core/tests/taiko-accpetance-tests/play-upload-audio.js
+++ core/tests/taiko-accpetance-tests/play-upload-audio.js
@@ -11,30 +11,30 @@ const {
   setConfig,
   press,
   closeBrowser,
-} = require("taiko");
+} = require('taiko');
 
 (async () => {
   try {
     await openBrowser({args: [
-      "--start-fullscreen",
-      "--use-fake-ui-for-media-stream",
-    ]})
-    await goto("localhost:8181");
-    await click("SIGN IN");
-    await write("testadmin@example.com", into(textBox("enter an email")));
-    await click(button("Sign In", below("Emulator Mode Sign In")));
-    await goto("http://localhost:8181/creator-dashboard");
-  
-    await click(button({class: "e2e-test-create-new-exploration-button" }));
-    await click(listItem({ class: "e2e-test-translation-tab" }));
+      '--start-fullscreen',
+      '--use-fake-ui-for-media-stream',
+    ]});
+    await goto('localhost:8181');
+    await click('SIGN IN');
+    await write('testadmin@example.com', into(textBox('enter an email')));
+    await click(button('Sign In', below('Emulator Mode Sign In')));
+    await goto('http://localhost:8181/creator-dashboard');
+
+    await click(button({class: 'e2e-test-create-new-exploration-button' }));
+    await click(listItem({ class: 'e2e-test-translation-tab' }));
     await click(
-      button({ class: "e2e-test-accessibility-translation-upload-audio" })
+      button({ class: 'e2e-test-accessibility-translation-upload-audio' })
     );
-    
-    await attach("./A4.mp3", 
+
+    await attach('./A4.mp3', 
         fileField(above(button('Cancel'))));
     await click(
-      button({ class: "e2e-test-save-uploaded-audio-button" })
+      button({ class: 'e2e-test-save-uploaded-audio-button' })
     );
 
     await click(button({class: 'e2e-test-play-pause-audio-button'}));
--- core/tests/taiko-accpetance-tests/record-audio.js
+++ core/tests/taiko-accpetance-tests/record-audio.js
@@ -11,28 +11,28 @@ const {
   setConfig,
   press,
   closeBrowser,
-} = require("taiko");
+} = require('taiko');
 
 (async () => {
   try {
     await openBrowser({args: [
-      "--start-fullscreen",
-      "--use-fake-ui-for-media-stream",
-    ]})
-    await goto("localhost:8181");
-    await click("SIGN IN");
-    await write("testadmin@example.com", into(textBox("enter an email")));
-    await click(button("Sign In", below("Emulator Mode Sign In")));
-    await goto("http://localhost:8181/creator-dashboard");
-    // await click("CREATE EXPLORATION");
-    await click(button({class: "e2e-test-create-new-exploration-button" }));
-    await click(listItem({ class: "e2e-test-translation-tab" }));
+      '--start-fullscreen',
+      '--use-fake-ui-for-media-stream',
+    ]});
+    await goto('localhost:8181');
+    await click('SIGN IN');
+    await write('testadmin@example.com', into(textBox('enter an email')));
+    await click(button('Sign In', below('Emulator Mode Sign In')));
+    await goto('http://localhost:8181/creator-dashboard');
+    // await click('CREATE EXPLORATION');
+    await click(button({class: 'e2e-test-create-new-exploration-button' }));
+    await click(listItem({ class: 'e2e-test-translation-tab' }));
     await click(
-      button({ class: "e2e-test-accessibility-translation-start-record" })
+      button({ class: 'e2e-test-accessibility-translation-start-record' })
     );
     await setConfig({ observeTime: 3000 });
-    await press("R");
-    await click(button({ class: "e2e-test-confirm-record" }));
+    await press('R');
+    await click(button({ class: 'e2e-test-confirm-record' }));
   } catch (error) {
     console.error(error);
   } finally {
