f1b784391cd3155317a7f22155c55c85ebf381ef
Pankaj Prajapati <paprajapati9@gmail.com>
2022-10-19 22:04:47 +0530

    review changes

--- core/templates/pages/learner-group-pages/templates/delete-learner-group-modal.component.ts
+++ core/templates/pages/learner-group-pages/templates/delete-learner-group-modal.component.ts
@@ -1,4 +1,4 @@
-// Copyright 2021 The Oppia Authors. All Rights Reserved.
+// Copyright 2022 The Oppia Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 /**
- * @fileoverview Component for Remove Item modal.
+ * @fileoverview Component for Delete Learner Group modal.
  */
 
 import { Component } from '@angular/core';
