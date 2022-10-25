c2bea2dabcfd9e7fbfd252576907192f21041308
aaronlwan <aaronwan@stanford.edu>
2022-10-25 11:24:09 -0700

    remove suggestionlisthandler from handler_schema_constants.py

--- core/handler_schema_constants.py
+++ core/handler_schema_constants.py
@@ -64,7 +64,6 @@ HANDLER_CLASS_NAMES_WHICH_STILL_NEED_SCHEMAS = [
     'StartedTranslationTutorialEventHandler',
     'StateHitEventHandler',
     'SubtopicPageDataHandler',
-    'SuggestionListHandler',
     'SuggestionToExplorationActionHandler',
     'SuggestionToSkillActionHandler',
     'SuggestionsProviderHandler',
