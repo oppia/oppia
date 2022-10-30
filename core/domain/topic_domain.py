a4d912aa6e3133b2735f3fe6cb52df3f64839318
JayVivarekar <jayviva@gmail.com>
2022-10-30 08:51:20 +0530

    Add review comments

--- core/domain/topic_domain.py
+++ core/domain/topic_domain.py
@@ -90,7 +90,8 @@ CMD_UPDATE_TOPIC_PROPERTY = 'update_topic_property'
 CMD_UPDATE_SUBTOPIC_PROPERTY = 'update_subtopic_property'
 
 CMD_MIGRATE_SUBTOPIC_SCHEMA_TO_LATEST_VERSION = 'migrate_subtopic_schema_to_latest_version' # pylint: disable=line-too-long
-CMD_MIGRATE_STORY_REFERENCE_SCHEMA_TO_LATEST_VERSION = 'migrate_story_reference_schema_to_latest_version' # pylint: disable=line-too-long
+CMD_MIGRATE_STORY_REFERENCE_SCHEMA_TO_LATEST_VERSION = (
+    'migrate_story_reference_schema_to_latest_version')
 
 
 class TopicChange(change_domain.BaseChange):
