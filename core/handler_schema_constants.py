d90e0e98117d5b69493a466e4f7d1c561dbd6b1a
Mattias Erlingsson <mater915@student.liu.se>
2022-11-02 12:19:06 +0100

    Remove handler from schema constants

--- core/handler_schema_constants.py
+++ core/handler_schema_constants.py
@@ -39,7 +39,6 @@ HANDLER_CLASS_NAMES_WHICH_STILL_NEED_SCHEMAS = [
     'FeedbackThreadStatusChangeEmailHandler',
     'FlagExplorationEmailHandler',
     'IncomingReplyEmailHandler',
-    'InstantFeedbackMessageEmailHandler',
     'LearnerAnswerDetailsSubmissionHandler',
     'LearnerGoalsHandler',
     'MemoryCacheAdminHandler',
