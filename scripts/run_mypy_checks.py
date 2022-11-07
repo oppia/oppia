2e6531f0fa2551a7ec60db858eba15b4df182354
sahil jhangar <souravjangar91@gmail.com>
2022-11-07 16:19:11 +0530

    added changes

--- scripts/run_mypy_checks.py
+++ scripts/run_mypy_checks.py
@@ -99,8 +99,6 @@ NOT_FULLY_COVERED_FILES: Final = [
     'core/controllers/oppia_root_test.py',
     'core/controllers/pages.py',
     'core/controllers/pages_test.py',
-    'core/controllers/review_tests.py',
-    'core/controllers/review_tests_test.py',
     'core/controllers/skill_editor.py',
     'core/controllers/skill_editor_test.py',
     'core/controllers/skill_mastery.py',
