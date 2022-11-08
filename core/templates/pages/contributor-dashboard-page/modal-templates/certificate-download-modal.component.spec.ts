9b30d4080ca7a24c8768290a1c5ca40aa236aeaa
chris7716 <hkraclh@gmail.com>
2022-11-09 02:52:55 +0530

    Add new component

---
+++ core/templates/pages/contributor-dashboard-page/modal-templates/certificate-download-modal.component.spec.ts
@@ -0,0 +1,140 @@
+// Copyright 2022 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Unit tests for CertificateDownloadModalComponent.
+*/
+
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { ChangeDetectorRef, NO_ERRORS_SCHEMA } from '@angular/core';
+
+import { ComponentFixture, fakeAsync, TestBed, tick, waitForAsync } from '@angular/core/testing';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { OppiaAngularRootComponent } from 'components/oppia-angular-root.component';
+import { ContextService } from 'services/context.service';
+import { WrapTextWithEllipsisPipe } from 'filters/string-utility-filters/wrap-text-with-ellipsis.pipe';
+import { CertificateDownloadModalComponent } from './certificate-download-modal.component';
+import { ContributionAndReviewService } from '../services/contribution-and-review.service';
+
+class MockChangeDetectorRef {
+  detectChanges(): void {}
+}
+
+describe('Contributor Certificate Download Modal Component', () => {
+  let activeModal: NgbActiveModal;
+  let httpTestingController: HttpTestingController;
+  let fixture: ComponentFixture<CertificateDownloadModalComponent>;
+  let component: CertificateDownloadModalComponent;
+  let changeDetectorRef: MockChangeDetectorRef = new MockChangeDetectorRef();
+  let contributionAndReviewService: ContributionAndReviewService;
+  const fileResponse: Blob = {
+    size: 100,
+    type: 'image/png',
+    arrayBuffer: null,
+    stream: null,
+    slice: null,
+    text: null
+  };
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      declarations: [
+        CertificateDownloadModalComponent,
+        WrapTextWithEllipsisPipe
+      ],
+      providers: [
+        NgbActiveModal,
+        {
+          provide: ChangeDetectorRef,
+          useValue: changeDetectorRef
+        }
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
+    OppiaAngularRootComponent.contextService = TestBed.inject(ContextService);
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CertificateDownloadModalComponent);
+    component = fixture.componentInstance;
+    component.suggestionType = 'translate_content';
+    component.username = 'test_user';
+    component.languageCode = 'hi';
+    component.fromDate = '2022/01/01';
+    component.toDate = '2022/12/31';
+    component = fixture.componentInstance;
+    httpTestingController = TestBed.inject(HttpTestingController);
+    activeModal = TestBed.inject(NgbActiveModal);
+    contributionAndReviewService = TestBed.inject(ContributionAndReviewService);
+    fixture.detectChanges();
+  });
+
+  it('should download the certificate when available', () => {
+    component.fromDate = '2022/01/01';
+    component.toDate = '2022/12/31';
+    spyOn(
+      contributionAndReviewService,
+      'downloadContributorCertificateAsync')
+      .and.returnValue(Promise.resolve(fileResponse));
+
+    component.downloadCertificate();
+
+    expect(component.errorsFound).toBeFalse();
+    expect(
+      contributionAndReviewService.downloadContributorCertificateAsync
+    ).toHaveBeenCalled();
+  });
+
+  it('should show errors when invalid', fakeAsync(() => {
+    component.fromDate = '2022/01/01';
+    component.toDate = '2022/12/31';
+    spyOn(
+      contributionAndReviewService,
+      'downloadContributorCertificateAsync')
+      .and.returnValue(Promise.reject());
+
+    component.downloadCertificate();
+    tick();
+
+    expect(
+      contributionAndReviewService.downloadContributorCertificateAsync
+    ).toHaveBeenCalled();
+    expect(component.errorsFound).toBeTrue();
+    expect(component.errorMessage).toEqual(
+      'Contributions not found for the given inputs.');
+  }));
+
+  it('should show invalid time ranges', () => {
+    component.fromDate = '2023/01/01';
+    component.toDate = '2022/12/31';
+
+    component.downloadCertificate();
+
+    expect(component.errorsFound).toBeTrue();
+    expect(component.errorMessage).toEqual('Invalid date range.');
+  });
+
+  it('should close', () => {
+    spyOn(activeModal, 'close');
+    component.close();
+    expect(activeModal.close).toHaveBeenCalled();
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+});
---
+++ core/templates/pages/contributor-dashboard-page/modal-templates/certificate-download-modal.component.ts
@@ -0,0 +1,93 @@
+// Copyright 2022 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Component for the certificate download modal.
+ */
+
+import { Component, Input } from '@angular/core';
+import { downgradeComponent } from '@angular/upgrade/static';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ContributionAndReviewService } from '../services/contribution-and-review.service';
+
+@Component({
+  selector: 'certificate-download-modal',
+  templateUrl: './certificate-download-modal.component.html'
+})
+export class CertificateDownloadModalComponent {
+  @Input() suggestionType: string;
+  @Input() username: string;
+  @Input() languageCode?: string;
+  fromDate: string;
+  toDate: string;
+  errorMessage: string;
+  errorsFound = false;
+  certificateDownloadiing = false;
+  constructor(
+    private readonly activeModal: NgbActiveModal,
+    private api: ContributionAndReviewService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  close(): void {
+    this.activeModal.close();
+  }
+
+  downloadCertificate(): void {
+    this.errorsFound = false;
+    this.errorMessage = '';
+    if (
+      new Date(this.fromDate) >= new Date(this.toDate) ||
+      !this.fromDate ||
+      !this.toDate
+    ) {
+      this.errorsFound = true;
+      this.errorMessage = 'Invalid date range.';
+      return;
+    }
+    this.certificateDownloadiing = true;
+    if (!this.errorsFound) {
+      this.api.downloadContributorCertificateAsync(
+        this.username,
+        this.suggestionType,
+        this.languageCode,
+        this.fromDate,
+        this.toDate).then((response: Blob) => {
+        let dataType = response.type;
+        let binaryData = [];
+        binaryData.push(response);
+
+        let downloadLink = document.createElement('a');
+        downloadLink.href = window.URL.createObjectURL(
+          new Blob(binaryData, {type: dataType}));
+        downloadLink.setAttribute('download', 'certificate');
+
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        this.certificateDownloadiing = false;
+      }).catch(() => {
+        this.errorsFound = true;
+        this.certificateDownloadiing = false;
+        this.errorMessage = (
+          'Accepted contributions not found for the given inputs.');
+      });
+    }
+  }
+}
+
+angular.module('oppia').directive(
+  'certificateDownloadModal', downgradeComponent(
+    {component: CertificateDownloadModalComponent}));
