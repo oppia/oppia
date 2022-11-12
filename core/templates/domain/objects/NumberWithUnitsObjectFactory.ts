6d212bd066ac509b6813221a0694b382b132e42b
Winnie Chen <winnie@DNa808276.SUNet>
2022-11-12 13:03:49 -0800

    codeql fixes

--- core/templates/domain/objects/NumberWithUnitsObjectFactory.ts
+++ core/templates/domain/objects/NumberWithUnitsObjectFactory.ts
@@ -76,7 +76,9 @@ export class NumberWithUnits {
     let unitsString = (new UnitsObjectFactory()).fromList(
       this.units).toString();
     if (unitsString.includes('$')) {
-      unitsString = unitsString.replace('$', '');
+      while (unitsString.includes('$')) {
+        unitsString = unitsString.replace('$', '');
+      }
       numberWithUnitsString += '$' + ' ';
     }
     if (unitsString.includes('Rs')) {
