f84ab837113f358e28262dd50991e0d31b02ea9b
Hitesh Tomar <lkbhitesh07@gmail.com>
2022-11-14 18:03:11 +0530

    audit job to fetch user images

--- core/jobs/batch_jobs/store_profile_images_to_gcs_jobs.py
+++ core/jobs/batch_jobs/store_profile_images_to_gcs_jobs.py
@@ -29,7 +29,6 @@ from core.jobs.types import job_run_result
 from core.platform import models
 
 import apache_beam as beam
-from typing import Optional
 
 MYPY = False
 if MYPY:  # pragma: no cover
@@ -40,21 +39,37 @@ if MYPY:  # pragma: no cover
 
 class StoreProfilePictureToGCSJob(base_jobs.JobBase):
     """Store profile picture to GCS job."""
-    def _filenames_png(self, user_model):
-        """"""
+    def _filenames_png(
+        self, user_model: user_models.UserSettingsModel
+    ) -> gcs_io.FileObjectDict:
+        """Returns file object for png images to write to the GCS.
+
+        Args:
+            user_model: UserSettingsModel. The user settings model.
+
+        Returns: file_dict. The FileObjectDict.
+        """
         file_dict = {}
         username = user_model.username
         filename = f'user/{username}/profile_picture.png'
         profile_picture_binary = utils.convert_png_data_url_to_binary(
             user_model.profile_picture_data_url)
         file_dict = {
-            'file': filename,
+            'filepath': filename,
             'data': profile_picture_binary
         }
         return file_dict
 
-    def _filenames_webp(self, user_model):
-        """"""
+    def _filenames_webp(
+        self, user_model: user_models.UserSettingsModel
+    ) -> gcs_io.FileObjectDict:
+        """Returns file object for webp images to write to the GCS.
+
+        Args:
+            user_model: UserSettingsModel. The user settings model.
+
+        Returns: file_dict. The FileObjectDict.
+        """
         file_dict = {}
         username = user_model.username
         filename = f'user/{username}/profile_picture.webp'
@@ -64,7 +79,7 @@ class StoreProfilePictureToGCSJob(base_jobs.JobBase):
             option="-q 80",logging="-v")
         webp_binary = utils.convert_webp_data_url_to_binary(webp_base64)
         file_dict = {
-            'file': filename,
+            'filepath': filename,
             'data': webp_binary
         }
         return file_dict
@@ -129,8 +144,60 @@ class AuditProfilePictureFromGCSJob(base_jobs.JobBase):
 
         audit_png_profile_pictures = (
             users_with_valid_username
-            | 'Map with username' >> beam.Map(lambda model: model.username)
-            | 'Map with filename' >> beam.Map(
+            | 'Map with username for png' >> beam.Map(
+                lambda model: model.username)
+            | 'Map with filename for png' >> beam.Map(
                 lambda username: f'user/{username}/profile_picture.png')
             | 'Read png files from GCS' >> gcs_io.ReadFile(mode='rb')
+        )
+
+        png_values = (
+            audit_png_profile_pictures
+            | 'Report the png data' >> beam.Map(lambda data: (
+                job_run_result.JobRunResult.as_stdout(
+                    f'The png image value is {data}'
+                )
+            ))
+        )
+
+        total_png_images = (
+            audit_png_profile_pictures
+            | 'Total number of png images' >> (
+                job_result_transforms.CountObjectsToJobRunResult(
+                    'TOTAL PNG IMAGES'))
+        )
+
+        audit_webp_profile_pictures = (
+            users_with_valid_username
+            | 'Map with username for webp' >> beam.Map(
+                lambda model: model.username)
+            | 'Map with filename for webp' >> beam.Map(
+                lambda username: f'user/{username}/profile_picture.webp')
+            | 'Read webp files from GCS' >> gcs_io.ReadFile(mode='rb')
+        )
+
+        webp_values = (
+            audit_webp_profile_pictures
+            | 'Report the webp data' >> beam.Map(lambda data: (
+                job_run_result.JobRunResult.as_stdout(
+                    f'The webp image value is {data}'
+                )
+            ))
+        )
+
+        total_webp_images = (
+            audit_webp_profile_pictures
+            | 'Total number of webp images' >> (
+                job_result_transforms.CountObjectsToJobRunResult(
+                    'TOTAL WEBP IMAGES'))
+        )
+
+        return (
+            (
+                png_values,
+                total_png_images,
+                webp_values,
+                total_webp_images
+            )
+            | 'Combine results' >> beam.Flatten()
         )
