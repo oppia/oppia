e27f39fc364378c931cdea47ab9e91e0eb93ec24
Gustav Stappe Renner <gusre394@student.liu.se>
2022-11-15 11:28:58 +0100

    added space and punctuation

--- core/domain/change_domain_test.py
+++ core/domain/change_domain_test.py
@@ -109,7 +109,7 @@ class ChangeDomainTests(test_utils.GenericTestBase):
             valid_cmd_dict, actual_cmd_attributes)
 
     def test_that_thing(self) -> None:
-        #does not seem to work
+        # does not seem to work.
         change_domain.BaseChange.ALLOWED_COMMANDS = [{
         'name': 'CMD_CHANGE_PROPERTY_VALUE',
         'required_attribute_names': ['new_value'],
