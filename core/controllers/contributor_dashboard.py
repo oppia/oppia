11f20fab65452600d56b62799a886c231c6bf591
Jairo Honorio <registrofx@gmail.com>
2022-11-14 23:53:16 -0800

    Fix Mypy issues

--- core/controllers/contributor_dashboard.py
+++ core/controllers/contributor_dashboard.py
@@ -283,7 +283,7 @@ class ReviewableOpportunitiesHandlerNormalizedRequestDict(TypedDict):
     """
 
     topic_name: Optional[str]
-    language: Optional[str]
+    language: str
 
 
 class ReviewableOpportunitiesHandler(
@@ -333,7 +333,7 @@ class ReviewableOpportunitiesHandler(
         self.render_json(self.values)
 
     def _get_reviewable_exploration_opportunity_summaries(
-        self, user_id: str, topic_name: Optional[str], language: Optional[str]
+        self, user_id: str, topic_name: Optional[str], language: str
     ) -> List[Optional[opportunity_domain.ExplorationOpportunitySummary]]:
         """Returns exploration opportunity summaries that have translation
         suggestions that are reviewable by the supplied user. The result is
