d442f5c3393c438fcdf7688c408fa7d03d34eb2f
chris7716 <hkraclh@gmail.com>
2022-11-18 11:56:51 +0530

    Fix checks

--- core/templates/pages/contributor-dashboard-page/contributor-stats/contributor-stats.component.ts
+++ core/templates/pages/contributor-dashboard-page/contributor-stats/contributor-stats.component.ts
@@ -247,11 +247,11 @@ export class ContributorStatsComponent {
             [this.createTranslationContributionStat(stat)]);
         } else {
           if (
-            typeof this.statsData?.translationReview[
+            typeof this.statsData?.translationContribution[
               language]?.data === 'undefined') {
             throw new Error('Data does not exist');
           }
-          this.statsData?.translationContribution[language].data?.push(
+          this.statsData?.translationContribution[language]?.data?.push(
             this.createTranslationContributionStat(stat));
         }
       });
@@ -275,7 +275,7 @@ export class ContributorStatsComponent {
               language]?.data === 'undefined') {
             throw new Error('Data does not exist');
           }
-          this.statsData?.translationReview[language].data?.push(
+          this.statsData?.translationReview[language]?.data?.push(
             this.createTranslationReviewStat(stat));
         }
       });
