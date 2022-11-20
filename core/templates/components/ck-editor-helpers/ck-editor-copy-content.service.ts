09fa549a4f5c86922b07b5b85bd6992abf9240f4
Winnie Chen <winnie@DNa8091ad.SUNet>
2022-11-19 21:19:16 -0800

    fix tests

--- core/templates/components/ck-editor-helpers/ck-editor-copy-content.service.ts
+++ core/templates/components/ck-editor-helpers/ck-editor-copy-content.service.ts
@@ -127,10 +127,7 @@ export class CkEditorCopyContentService {
     let elementTagName = (
       containedWidgetTagName || element.tagName.toLowerCase());
     let html = element.outerHTML;
-    while (html.includes('<!--')) {
-      html = html.replace(/<!--[^>]*-->/g, '').trim();
-      html = html.replace(/<!--/g, '').trim();
-    }
+    html = html.replace(/<!--[^>]*-->/g, '').trim();
     if (!containedWidgetTagName) {
       editor.insertHtml(html);
     } else {
