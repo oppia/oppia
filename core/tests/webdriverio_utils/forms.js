683e588397ae61d1a443d689e4dd5dc3275203f6
Winnie Chen <winnie@DNa8091ad.SUNet>
2022-12-09 15:11:43 -0800

    revert webdriverio_utils

--- core/tests/webdriverio_utils/forms.js
+++ core/tests/webdriverio_utils/forms.js
@@ -552,11 +552,11 @@ var RichTextChecker = async function(arrayOfElems, arrayOfTexts, fullText) {
       await arrayOfElems[arrayPointer].getTagName()
     ).toBe(tagName);
     // Remove comments introduced by angular for bindings using replace.
-    let res = await arrayOfElems[arrayPointer].getHTML(false);
-    while (res.includes('<!--')) {
-      res = res.replace(/<!--[^>]*-*-*>*/gm, '').trim();
-    }
-    expect(res).toBe(text);
+    expect(
+      (
+        await arrayOfElems[arrayPointer].getHTML(false)
+      ).replace(/<!--[^>]*-->/g, '').trim()
+    ).toBe(text);
     expect(arrayOfTexts[arrayPointer]).toEqual(text);
     arrayPointer = arrayPointer + 1;
     textPointer = textPointer + text.length;
