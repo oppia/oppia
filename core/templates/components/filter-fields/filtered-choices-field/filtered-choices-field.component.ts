b1f104c033f0563ba36051eb9d8cd2f8a1699046
kshitij01042002 <kshitijpatil1098@gmail.com>
2022-12-27 22:46:16 +0530

    modified

--- core/templates/components/filter-fields/filtered-choices-field/filtered-choices-field.component.ts
+++ core/templates/components/filter-fields/filtered-choices-field/filtered-choices-field.component.ts
@@ -30,7 +30,7 @@ export class FilteredChoicesFieldComponent {
   @Input() selection!: string;
   @Input() placeholder!: string;
   @Input() searchLabel: string = 'search';
-  @Input() isSearchable: boolean = true;
+  @Input() isSearchable?: boolean = true;
   @Input() noEntriesFoundLabel: string = 'No matches found';
   @Output() selectionChange: EventEmitter<string> = (
     new EventEmitter());
