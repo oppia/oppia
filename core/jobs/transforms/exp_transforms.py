36ef382c48019d6c05de023afa4e2e703f563aa7
JayVivarekar <jayviva@gmail.com>
2023-01-07 14:30:42 +0530

    Create a transform for exp migration

---
+++ core/jobs/transforms/exp_transforms.py
@@ -0,0 +1,259 @@
+# coding: utf-8
+#
+# Copyright 2023 The Oppia Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Provides a transform to migrate exploration models."""
+
+from __future__ import annotations
+
+import logging
+
+from core import feconf
+from core.constants import constants
+from core.jobs.io import ndb_io
+from core.domain import exp_domain
+from core.domain import exp_fetchers
+from core.domain import exp_services
+from core.jobs.transforms import job_result_transforms
+from core.jobs.transforms import results_transforms
+from core.platform import models
+
+import apache_beam as beam
+import result
+from typing import Any, Iterable, Sequence, Tuple
+
+MYPY = False
+if MYPY: # pragma: no cover
+    from mypy_imports import base_models
+    from mypy_imports import datastore_services
+    from mypy_imports import exp_models
+
+(base_models, exp_models) = (
+    models.Registry.import_models(
+        [models.Names.BASE_MODEL, models.Names.EXPLORATION]))
+datastore_services = models.Registry.import_datastore_services()
+
+
+# TODO(#15613): Here we use MyPy ignore because the incomplete typing of
+# apache_beam library and absences of stubs in Typeshed, forces MyPy to
+# assume that PTransform class is of type Any. Thus to avoid MyPy's error
+# (Class cannot subclass 'PTransform' (has type 'Any')), we added an
+# ignore here.
+class MigrateExplorationModels(beam.PTransform):  # type: ignore[misc]
+    """Transform that gets all Exploration models, performs migration
+      and filters any error results.
+    """
+
+    @staticmethod
+    def _migrate_exploration(
+        exp_model: exp_models.ExplorationModel,
+        exp_is_published: bool
+    ) -> result.Result[
+        Tuple[str, exp_domain.Exploration],
+        Tuple[str, Exception]
+    ]:
+        """Migrates exploration and transform exploration model into
+        exploration object.
+
+        Args:
+            exp_model: ExplorationModel. The exploration model to migrate.
+            exp_is_published: bool. Whether the exploration is published or not.
+
+        Returns:
+            Result((str, Exploration), (str, Exception)). Result containing
+            tuple that consists of exploration ID and either Exploration object
+            or Exception. Exploration object is returned when the migration was
+            successful and Exception is returned otherwise.
+        """
+        try:
+            exploration = exp_fetchers.get_exploration_from_model(exp_model)
+            exploration.validate(strict=exp_is_published)
+
+            with datastore_services.get_ndb_context():
+                if exp_services.get_story_id_linked_to_exploration(
+                        exp_model.id) is not None:
+                    exp_services.validate_exploration_for_story(
+                        exploration, True)
+
+        except Exception as e:
+            logging.exception(e)
+            return result.Err((exp_model.id, e))
+
+        return result.Ok((exp_model.id, exploration))
+
+    @staticmethod
+    def _generate_exploration_changes(
+        exp_id: str, exp_model: exp_models.ExplorationModel
+    ) -> Iterable[Tuple[str, exp_domain.ExplorationChange]]:
+        """Generates exploration change objects. The ExplorationChange object
+        is only generated when the exploration's states schema version is lower
+        than the latest schema version.
+
+        Args:
+            exp_id: str. The ID of the exploration.
+            exp_model: ExplorationModel. The exploration for which to generate
+                the change objects.
+
+        Yields:
+            (str, ExplorationChange). Tuple containing exploration ID and
+            ExplorationChange object.
+        """
+        exp_states_version = exp_model.states_schema_version
+        if exp_states_version < feconf.CURRENT_STATE_SCHEMA_VERSION:
+            exp_change = exp_domain.ExplorationChange({
+                'cmd': (
+                    exp_domain.CMD_MIGRATE_STATES_SCHEMA_TO_LATEST_VERSION),
+                'from_version': str(exp_states_version),
+                'to_version': str(feconf.CURRENT_STATE_SCHEMA_VERSION)
+            })
+            yield (exp_id, exp_change)
+
+    # Here we use type Any because this method can accept any kind of
+    # PCollection object to return the filtered migration results.
+    def expand(
+        self, objects: Sequence[base_models.BaseModel]
+    ) -> Tuple[Sequence[base_models.BaseModel],
+        beam.PCollection[result.Result[Tuple[str, Any], None]]]:
+        """Migrate exploration objects and flush the input
+            in case of errors.
+
+        Args:
+            objects: PCollection. Sequence of models.
+
+        Returns:
+            (Sequence(BaseModel), PCollection). Tuple containing
+            sequence of models which should be put into the datastore and
+            a PCollection of results from the exploration  migration.
+        """
+        unmigrated_exploration_models = (
+            objects
+            | 'Get all non-deleted exploration models' >> (
+                ndb_io.GetModels(
+                    exp_models.ExplorationModel.get_all(
+                        include_deleted=False)))
+            # Pylint disable is needed because pylint is not able to correctly
+            # detect that the value is passed through the pipe.
+            | 'Add exploration keys' >> beam.WithKeys(  # pylint: disable=no-value-for-parameter
+                lambda exp_model: exp_model.id)
+            # TODO(#15871): This filter should be removed after the explorations
+            # are fixed and it is possible to migrate them.
+            | 'Remove broken exploration' >> beam.Filter(
+                lambda id_and_exp: id_and_exp[0] not in (
+                    'umPkwp0L1M0-', '670bU6d9JGBh'))
+        )
+
+        exp_publication_status = (
+            objects
+            | 'Get all non-deleted exploration rights models' >> (
+                ndb_io.GetModels(exp_models.ExplorationRightsModel.get_all()))
+            | 'Extract publication status' >> beam.Map(
+                lambda exp_rights: (
+                    exp_rights.id,
+                    exp_rights.status == constants.ACTIVITY_STATUS_PUBLIC
+                )
+            )
+            # TODO(#15871): This filter should be removed after the explorations
+            # are fixed and it is possible to migrate them.
+            | 'Remove exp rights for broken exps' >> beam.Filter(
+                lambda exp_rights: exp_rights[0] not in (
+                    'umPkwp0L1M0-', '670bU6d9JGBh'))
+        )
+
+        all_migrated_exp_results = (
+            (
+                unmigrated_exploration_models,
+                exp_publication_status
+            )
+            | 'Merge model and staus' >> beam.CoGroupByKey()
+            | 'Get rid of exp ID' >> beam.Values() # pylint: disable=no-value-for-parameter
+            | 'Transform and migrate model' >> beam.MapTuple( # pylint: disable=no-value-for-parameter
+                lambda exploration_models, status: self._migrate_exploration(
+                    exploration_models[0], status[0]))
+        )
+
+        migrated_exp_job_run_results = (
+            all_migrated_exp_results
+            | 'Generate results for migration' >> (
+                job_result_transforms.ResultsToJobRunResults('EXP PROCESSED'))
+        )
+
+        filtered_migrated_exp = (
+            all_migrated_exp_results
+            | 'Filter migration results' >> (
+                results_transforms.DrainResultsOnError())
+        )
+        migrated_exp = (
+            filtered_migrated_exp
+            | 'Unwrap ok' >> beam.Map(
+                lambda result_item: result_item.unwrap())
+        )
+
+        exp_changes = (
+            unmigrated_exploration_models
+            | 'Generate exploration changes' >> beam.FlatMapTuple(
+                self._generate_exploration_changes)
+        )
+
+        exp_objects_list = (
+            {
+                'exp_model': unmigrated_exploration_models,
+                'exploration': migrated_exp,
+                'exp_changes': exp_changes
+            }
+            | 'Merge objects' >> beam.CoGroupByKey()
+            | 'Get rid of ID' >> beam.Values() # pylint: disable=no-value-for-parameter
+        )
+
+        transformed_exp_objects_list = (
+            exp_objects_list
+            | 'Remove unmigrated explorations' >> beam.Filter(
+                lambda x: (
+                    len(x['exp_changes']) > 0 and
+                    len(x['exploration']) > 0
+                ))
+            | 'Reorganize the exploration objects' >> beam.Map(lambda objects: {
+                'exp_model': objects['exp_model'][0],
+                'exploration': objects['exploration'][0],
+                'exp_changes': objects['exp_changes']
+            })
+        )
+
+        exp_objects_list_job_run_results = (
+            transformed_exp_objects_list
+            | 'Transform exp objects into job run results' >> (
+                job_result_transforms.CountObjectsToJobRunResult(
+                    'EXP MIGRATED'))
+        )
+
+        already_migrated_job_run_results = (
+            exp_objects_list
+            | 'Remove migrated explorations' >> beam.Filter(
+                lambda x: (
+                    len(x['exp_changes']) == 0 and len(x['exploration']) > 0
+                ))
+            | 'Transform previously migrated exps into job run results' >> (
+                job_result_transforms.CountObjectsToJobRunResult(
+                    'EXP PREVIOUSLY MIGRATED'))
+        )
+
+        return (
+            transformed_exp_objects_list,
+            (
+                migrated_exp_job_run_results,
+                exp_objects_list_job_run_results,
+                already_migrated_job_run_results
+            )
+            | beam.Flatten()
+        )
---
+++ core/jobs/transforms/exp_transforms_test.py
@@ -0,0 +1,260 @@
+# coding: utf-8
+#
+# Copyright 2023 The Oppia Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Unit tests for core.jobs.transforms.exp_transforms."""
+
+from __future__ import annotations
+
+from core import feconf
+from core.constants import constants
+from core.domain import exp_domain
+from core.domain import exp_services
+from core.domain import opportunity_services
+from core.domain import rights_domain
+from core.domain import story_domain
+from core.domain import story_services
+from core.domain import topic_domain
+from core.domain import topic_services
+from core.domain import user_services
+from core.jobs.types import job_run_result
+from core.jobs import job_test_utils
+from core.jobs.transforms import exp_transforms
+from core.jobs.transforms import results_transforms
+from core.platform import models
+from core.tests import test_utils
+
+import apache_beam as beam
+
+MYPY = False
+if MYPY: # pragma: no cover
+    from mypy_imports import exp_models, opportunity_models
+
+(
+    exp_models,
+    opportunity_models,
+) = models.Registry.import_models([
+    models.Names.EXPLORATION,
+    models.Names.OPPORTUNITY
+])
+
+class MigrateExplorationModelsTests(
+    job_test_utils.PipelinedTestBase, test_utils.GenericTestBase):
+
+    NEW_EXP_ID = 'exp_1'
+    EXP_TITLE = 'title'
+    EXP_ID_ONE = 'exp_one'
+    EXP_ID_TWO = 'exp_two'
+
+    def create_story_linked_to_exploration(self) -> None:
+        """Creates a new story linked to the test exploration."""
+        topic_id = 'topic_id_1'
+        story_id = 'story_id_1'
+
+        topic = topic_domain.Topic.create_default_topic(
+            topic_id, 'topic', 'abbrev', 'description', 'fragment')
+        topic.thumbnail_filename = 'thumbnail.svg'
+        topic.thumbnail_bg_color = '#C6DCDA'
+        topic.subtopics = [
+            topic_domain.Subtopic(
+                1, 'Title', ['skill_id_1'], 'image.svg',
+                constants.ALLOWED_THUMBNAIL_BG_COLORS['subtopic'][0], 21131,
+                'dummy-subtopic-url')]
+        topic.next_subtopic_id = 2
+        topic.skill_ids_for_diagnostic_test = ['skill_id_1']
+        topic_services.save_new_topic(feconf.SYSTEM_COMMITTER_ID, topic)
+        topic_services.publish_topic(topic_id, feconf.SYSTEM_COMMITTER_ID)
+
+        story = story_domain.Story.create_default_story(
+            story_id, 'A story title', 'description', topic_id,
+            'story-one')
+        story_services.save_new_story(feconf.SYSTEM_COMMITTER_ID, story)
+        topic_services.add_canonical_story(
+            feconf.SYSTEM_COMMITTER_ID, topic_id, story_id)
+        topic_services.publish_story(
+            topic_id, story_id, feconf.SYSTEM_COMMITTER_ID)
+        change_list = [
+            story_domain.StoryChange({
+                'cmd': story_domain.CMD_ADD_STORY_NODE,
+                'node_id': story_domain.NODE_ID_PREFIX + '1',
+                'title': 'Title 1'
+            }),
+            story_domain.StoryChange({
+                'cmd': story_domain.CMD_UPDATE_STORY_NODE_PROPERTY,
+                'property_name': (
+                    story_domain.STORY_NODE_PROPERTY_EXPLORATION_ID),
+                'node_id': story_domain.NODE_ID_PREFIX + '1',
+                'old_value': None,
+                'new_value': self.NEW_EXP_ID
+            })
+        ]
+        story_services.update_story(
+            feconf.SYSTEM_COMMITTER_ID, story_id, change_list,
+            'Added node.')
+
+    def test_unmigrated_valid_published_exp_migrates(self) -> None:
+        swap_states_schema_48 = self.swap(
+            feconf, 'CURRENT_STATE_SCHEMA_VERSION', 48)
+        swap_exp_schema_53 = self.swap(
+            exp_domain.Exploration, 'CURRENT_EXP_SCHEMA_VERSION', 53)
+
+        with swap_states_schema_48, swap_exp_schema_53:
+            exploration = (
+                self.save_new_linear_exp_with_state_names_and_interactions(
+                    self.NEW_EXP_ID, feconf.SYSTEM_COMMITTER_ID,
+                    ['Introduction'], ['EndExploration'],
+                    title=self.EXP_TITLE, category='Algorithms',
+                    correctness_feedback_enabled=True
+            ))
+            owner_action = user_services.get_user_actions_info(
+                feconf.SYSTEM_COMMITTER_ID)
+            exp_services.publish_exploration_and_update_user_profiles(
+                owner_action, self.NEW_EXP_ID)
+            opportunity_model = (
+                opportunity_models.ExplorationOpportunitySummaryModel(
+                    id=self.NEW_EXP_ID,
+                    topic_id='topic_id1',
+                    topic_name='topic',
+                    story_id='story_id_1',
+                    story_title='A story title',
+                    chapter_title='Title 1',
+                    content_count=20,
+                    incomplete_translation_language_codes=['hi', 'ar'],
+                    translation_counts={'hi': 1, 'ar': 2},
+                    language_codes_needing_voice_artists=['en'],
+                    language_codes_with_assigned_voice_artists=[]))
+            opportunity_model.put()
+
+            self.create_story_linked_to_exploration()
+
+            self.assertEqual(exploration.states_schema_version, 48)
+
+        unused_migrated_exps, job_run_results = (
+            self.pipeline
+            | exp_transforms.MigrateExplorationModels()
+        )
+        self.assert_pcoll_equal(
+            job_run_results,
+            [job_run_result.JobRunResult(stdout='EXP PROCESSED SUCCESS: 1'),
+            job_run_result.JobRunResult(
+                stdout='EXP MIGRATED SUCCESS: 1', stderr='')
+        ])
+
+        updated_opp_model = (
+            opportunity_models.ExplorationOpportunitySummaryModel.get(
+                self.NEW_EXP_ID))
+        updated_opp_summary = (
+                opportunity_services
+                    .get_exploration_opportunity_summary_from_model(
+                        updated_opp_model))
+
+        expected_opp_summary_dict = {
+            'id': 'exp_1',
+            'topic_name': 'topic',
+            'chapter_title': 'Title 1',
+            'story_title': 'A story title',
+            'content_count': 1,
+            'translation_counts': {},
+            'translation_in_review_counts': {}}
+
+        self.assertEqual(
+            updated_opp_summary.to_dict(), expected_opp_summary_dict)
+
+    def test_broken_exp_is_not_migrated(self) -> None:
+        exploration_rights = rights_domain.ActivityRights(
+            self.EXP_ID_ONE, [feconf.SYSTEM_COMMITTER_ID],
+            [], [], [])
+        commit_cmds = [{'cmd': rights_domain.CMD_CREATE_NEW}]
+
+        exp_models.ExplorationRightsModel(
+            id=exploration_rights.id,
+            owner_ids=exploration_rights.owner_ids,
+            editor_ids=exploration_rights.editor_ids,
+            voice_artist_ids=exploration_rights.voice_artist_ids,
+            viewer_ids=exploration_rights.viewer_ids,
+            community_owned=exploration_rights.community_owned,
+            status=exploration_rights.status,
+            viewable_if_private=exploration_rights.viewable_if_private,
+            first_published_msec=exploration_rights.first_published_msec,
+        ).commit(
+            feconf.SYSTEM_COMMITTER_ID, 'Created new exploration', commit_cmds)
+        exp_model = self.create_model(
+            exp_models.ExplorationModel,
+            id=self.EXP_ID_ONE,
+            title='title',
+            category=' category',
+            init_state_name='Introduction',
+            states_schema_version=49)
+        exp_model.update_timestamps()
+        exp_model.commit(
+            feconf.SYSTEM_COMMITTER_ID, 'Create exploration', [{
+                'cmd': exp_domain.CMD_CREATE_NEW
+        }])
+        # Save a valid unmigrated exploration.
+        swap_states_schema_48 = self.swap(
+            feconf, 'CURRENT_STATE_SCHEMA_VERSION', 48)
+        with swap_states_schema_48:
+            exploration = exp_domain.Exploration.create_default_exploration(
+                self.EXP_ID_TWO, title=self.EXP_TITLE, category='category')
+            exp_services.save_new_exploration(
+                feconf.SYSTEM_COMMITTER_ID, exploration)
+        
+        unused_migrated_exps, job_run_results = (
+            self.pipeline
+            | exp_transforms.MigrateExplorationModels()
+        )
+
+        self.assert_pcoll_equal(
+            job_run_results,
+            [job_run_result.JobRunResult(
+                stderr=(
+                    'EXP PROCESSED ERROR: "(\'exp_one\', ''ValidationError('
+                    '\'Names should not start or end with whitespace.\'))": 1'
+                )
+            ),
+            job_run_result.JobRunResult(stdout='EXP PROCESSED SUCCESS: 1')
+        ])
+
+    def test_migrated_exp_is_not_migrated(self) -> None:
+        exploration = exp_domain.Exploration.create_default_exploration(
+            self.NEW_EXP_ID, title=self.EXP_TITLE, category='category')
+        exp_services.save_new_exploration(
+            feconf.SYSTEM_COMMITTER_ID, exploration)
+
+        self.assertEqual(
+            exploration.states_schema_version,
+            feconf.CURRENT_STATE_SCHEMA_VERSION)
+
+        unused_migrated_exps, job_run_results = (
+            self.pipeline
+            | exp_transforms.MigrateExplorationModels()
+        )
+
+        self.assert_pcoll_equal(
+            job_run_results, 
+            [job_run_result.JobRunResult(
+                stdout='EXP PREVIOUSLY MIGRATED SUCCESS: 1'),
+            job_run_result.JobRunResult(
+                stdout='EXP PROCESSED SUCCESS: 1')
+        ])
+
+    def test_zero_objects_correctly_outputs(self) -> None:
+        transform_result = (
+            self.pipeline
+            | beam.Create([])
+            | results_transforms.DrainResultsOnError()
+        )
+
+        self.assert_pcoll_empty(transform_result)
