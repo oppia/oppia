dd25bfe4089f34500638c44e02b0a432be982002
Kevin Thomas <kevintab@tutanota.com>
2023-01-15 19:52:30 -0800

    address comment and fix tests

--- core/templates/pages/android-page/android-page.component.ts
+++ core/templates/pages/android-page/android-page.component.ts
@@ -105,7 +105,7 @@ export class AndroidPageComponent implements OnInit, OnDestroy {
         this.setPageTitle();
       })
     );
-    this.featuresShown = 1;
+    this.featuresShown = 0;
   }
 
   ngAfterViewInit(): void {
