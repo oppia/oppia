52a679d19aec0e1b8f3aa22ed29d9e6c55524815
kashish bhandula <kashishbhandula112@gmail.com>
2023-01-21 22:16:18 +0530

    fixed coverage of rules_registry

--- core/domain/rules_registry_test.py
+++ core/domain/rules_registry_test.py
@@ -43,6 +43,7 @@ class RulesRegistryUnitTests(test_utils.GenericTestBase):
         self.assertDictEqual(
             html_field_types_to_rule_specs,
             specs_from_json)
+        
 
     def test_get_html_field_types_to_rule_specs_for_previous_state_schema_version(  # pylint: disable=line-too-long
         self
@@ -73,3 +74,32 @@ class RulesRegistryUnitTests(test_utils.GenericTestBase):
                 rules_registry.Registry
                 .get_html_field_types_to_rule_specs(state_schema_version=10)
             )
+   
+    def test_get_html_field_types_to_rule_specs_for_previous_andcurrent_state_schema_version(  # pylint: disable=line-too-long
+        self
+    ) -> None:
+        html_field_types_to_rule_specs_v41 = (
+            rules_registry.Registry.get_html_field_types_to_rule_specs(
+                state_schema_version=41))
+
+        spec_file_v41 = os.path.join(
+            'extensions', 'interactions',
+            'legacy_html_field_types_to_rule_specs_by_state_version',
+            'html_field_types_to_rule_specs_state_v41.json')
+        with utils.open_file(spec_file_v41, 'r') as f:
+            specs_from_json_v41 = json.loads(f.read())
+
+        self.assertDictEqual(
+            html_field_types_to_rule_specs_v41,
+            specs_from_json_v41)
+        html_field_types_to_rule_specs = (
+            rules_registry.Registry.get_html_field_types_to_rule_specs())
+
+        spec_file = os.path.join(
+            'extensions', 'interactions', 'html_field_types_to_rule_specs.json')
+        with utils.open_file(spec_file, 'r') as f:
+            specs_from_json = json.loads(f.read())
+
+        self.assertDictEqual(
+            html_field_types_to_rule_specs,
+            specs_from_json)
