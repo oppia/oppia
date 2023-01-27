72e2abb75a4bc08f7cdfce7d0c3ca97646966937
Shivkant Chauhan <shivkantchauhan2002@gmail.com>
2023-01-28 00:33:45 +0530

    small changes

--- core/tests/puppeteer-acceptance-tests/puppeteer-testing-utilities/puppeteer-utils.js
+++ core/tests/puppeteer-acceptance-tests/puppeteer-testing-utilities/puppeteer-utils.js
@@ -109,7 +109,7 @@ module.exports = class puppeteerUtilities {
       const [button] = await this.page.$x(
         /** Disabling the line because we need to use backticks for
          * string interpolation. */
-        // eslint-disable-next-line no-singlequote
+        // eslint-disable-next-line quotes
         `//` + `*[contains(text(), '${selector}')]`);
       await button.click();
     } catch {
