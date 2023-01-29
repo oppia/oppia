bb2866849c7f4224ae81d67e5102433fb2fb9688
adityanaraynm095 <adityanaraynm095@gmail.com>
2023-01-29 10:57:18 +0530

    Changed timeout values in karma.conf.ts

--- core/tests/karma.conf.ts
+++ core/tests/karma.conf.ts
@@ -106,9 +106,9 @@ module.exports = function(config) {
     autoWatch: true,
     browsers: ['CI_Chrome'],
     // Kill the browser if it does not capture in the given timeout [ms].
-    captureTimeout: 60000,
-    browserNoActivityTimeout: 120000,
-    browserDisconnectTimeout: 60000,
+    captureTimeout: 120000,
+    browserNoActivityTimeout: 60000,
+    browserDisconnectTimeout: 120000,
     browserDisconnectTolerance: 3,
     browserConsoleLogOptions: {
       level: 'log',
