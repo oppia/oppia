4d7ef3d4f90f0c374cc75a36a0670a2eec3f63f9
yqh09hd <31307813+yqh09hd@users.noreply.github.com>
2023-01-31 08:43:48 -0600

    Set destination as Null in the frontend

--- core/domain/question_domain.py
+++ core/domain/question_domain.py
@@ -1949,7 +1949,7 @@ class Question(translation_domain.BaseTranslatableObject):
         if interaction.default_outcome.labelled_as_correct:
             at_least_one_correct_answer = True
 
-        if interaction.default_outcome.dest:
+        if interaction.default_outcome.dest is not None:
             dest_is_specified = True
 
         if interaction.default_outcome.dest_if_really_stuck is not None:
