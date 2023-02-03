ab8ae37d1dd5f28348505aff5cf22e4933656123
kashish bhandula <kashishbhandula112@gmail.com>
2023-02-03 10:35:12 +0530

    fixed few nits

--- core/domain/image_validation_services_test.py
+++ core/domain/image_validation_services_test.py
@@ -137,9 +137,9 @@ class ImageValidationServiceTests(test_utils.GenericTestBase):
             'Image not recognized'
         )
         xmlns_attribute_svg = '<svg xmlns="http://www.w3.org/2000/svg" ></svg>'
-        xmlns_attribute_svg = xmlns_attribute_svg.encode('utf-8')
+        base64_encoded_string = xmlns_attribute_svg.encode('utf-8')
         validated_image = image_validation_services.validate_image_and_filename(
-            xmlns_attribute_svg,
+            base64_encoded_string,
             'image.svg',
             feconf.ENTITY_TYPE_EXPLORATION
         )
