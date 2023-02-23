c305dd55d7f5b23c3e0e53bad998bb4baf89d2e9
adityanaraynm095 <adityanaraynm095@gmail.com>
2023-02-23 17:21:33 +0530

    Removed indentations

--- extensions/interactions/AlgebraicExpressionInput/directives/oppia-interactive-algebraic-expression-input.component.spec.ts
+++ extensions/interactions/AlgebraicExpressionInput/directives/oppia-interactive-algebraic-expression-input.component.spec.ts
@@ -138,7 +138,8 @@ describe('AlgebraicExpressionInputInteractive', () => {
     component.submitAnswer();
     expect(mockCurrentInteractionService.onSubmit).not.toHaveBeenCalled();
     expect(component.warningText).toBe(
-      'Your answer seems to be missing a variable/number after the ÷ operator.');
+      'Your answer seems to be missing a variable/number after' +
+      ' the ÷ operator.');
   });
 
   it('should correctly validate current answer', () => {
