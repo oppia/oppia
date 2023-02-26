ff5b17d5f66fe241202a3e8d90410e722647db2b
Shivkant Chauhan <shivkantchauhan2002@gmail.com>
2023-02-26 18:59:05 +0530

    fixing flakes of blog editor tests

--- core/tests/puppeteer-acceptance-tests/user-utilities/blog-post-admin-utils.js
+++ core/tests/puppeteer-acceptance-tests/user-utilities/blog-post-admin-utils.js
@@ -22,6 +22,7 @@ const testConstants = require(
   '../puppeteer-testing-utilities/test-constants.js');
 const { showMessage } = require(
   '../puppeteer-testing-utilities/show-message-utils.js');
+const path = require('path');
 
 const blogTitleInput = 'input.e2e-test-blog-post-title-field';
 const blogBodyInput = 'div.e2e-test-rte';
@@ -36,6 +37,8 @@ const blogDashboardUrl = testConstants.URLs.BlogDashboard;
 const blogAdminUrl = testConstants.URLs.BlogAdmin;
 const publishBlogPostButton = 'button.e2e-test-publish-blog-post-button';
 const addThumbnailImageButton = 'button.e2e-test-photo-upload-submit';
+const blogPostThumnailImagePath = path.resolve(
+  __dirname, '../images/blog-post-thumbnail.svg');
 
 const LABEL_FOR_NEW_BLOG_POST_CREATE_BUTTON = 'CREATE NEW BLOG POST';
 const LABEL_FOR_SAVE_BUTTON = 'Save';
@@ -43,7 +46,6 @@ const LABEL_FOR_DONE_BUTTON = 'DONE';
 const LABEL_FOR_SAVE_DRAFT_BUTTON = 'SAVE AS DRAFT';
 const LABEL_FOR_DELETE_BUTTON = 'Delete';
 const LABEL_FOR_CONFIRM_BUTTON = 'Confirm';
-const LABEL_FOR_ADD_THUMBNAIL_BUTTON = 'Add Thumbnail Image';
 const LABEL_FOR_ADD_ELEMENT_BUTTON = 'Add element';
 
 module.exports = class e2eBlogPostAdmin extends baseUser {
@@ -161,10 +163,10 @@ module.exports = class e2eBlogPostAdmin extends baseUser {
     await this.clickOn('button.mat-button-toggle-button');
     await this.expectPublishButtonToBeDisabled();
     await this.clickOn(thumbnailPhotoBox);
-    await this.uploadFile('../images/blog-post-thumbnail.svg');
+    await this.uploadFile(blogPostThumnailImagePath);
     await this.page.waitForSelector(
       `${addThumbnailImageButton}:not([disabled])`);
-    await this.clickOn(LABEL_FOR_ADD_THUMBNAIL_BUTTON);
+    await this.clickOn(addThumbnailImageButton);
     await this.page.waitForSelector('body.modal-open', {hidden: true});
     await this.expectPublishButtonToBeDisabled();
 
@@ -189,10 +191,10 @@ module.exports = class e2eBlogPostAdmin extends baseUser {
     await this.clickOn('NEW POST');
     await this.clickOn('button.mat-button-toggle-button');
     await this.clickOn(thumbnailPhotoBox);
-    await this.uploadFile('../images/blog-post-thumbnail.svg');
+    await this.uploadFile(blogPostThumnailImagePath);
     await this.page.waitForSelector(
       `${addThumbnailImageButton}:not([disabled])`);
-    await this.clickOn(LABEL_FOR_ADD_THUMBNAIL_BUTTON);
+    await this.clickOn(addThumbnailImageButton);
     await this.page.waitForSelector('body.modal-open', {hidden: true});
 
     await this.type(blogTitleInput, newBlogPostTitle);
