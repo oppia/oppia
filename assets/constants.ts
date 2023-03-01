6f5ed188ea4a6c9228a74331a36fec1c516b2280
Kashish Bhandula <56387644+kashishbhandula@users.noreply.github.com>
2023-03-01 13:01:17 +0530

    Fixes  #12941: [LaCE image editor] [Contributor Dashboard] Issue when there is a "X" sign in text in RTL languages (#17438)
    small changes

--- assets/constants.ts
+++ assets/constants.ts
@@ -5168,7 +5168,7 @@ export default {
     "code": "prs",
     "description": "دری (Dari)",
     "direction": "rtl",
-    "decimal_separator": ","
+    "decimal_separator": "."
   }, {
     "code": "pt",
     "description": "português (Portuguese)",
