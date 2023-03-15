c55a4dc187166b433015fae1fde260a06e8e67b4
Lukas Blouin <lukas.blouin@etu.u-bordeaux.fr>
2023-03-15 15:56:00 +0100

    Issue #17065 :
    * Add to some component tabindex="0" and (keyboard.enter)="something()" in order to make them keyboard focusable and interagisable
    
    * Add a condition in core/templates/components/state-editor/state-responses-editor/state-responses.component.ts to check if the event is a click event before stopPropagation.

--- core/templates/components/state-editor/state-responses-editor/state-responses.component.ts
+++ core/templates/components/state-editor/state-responses-editor/state-responses.component.ts
@@ -349,7 +349,8 @@ export class StateResponsesComponent implements OnInit, OnDestroy {
   deleteAnswerGroup(evt: Event, index: number): void {
     // Prevent clicking on the delete button from also toggling the
     // display state of the answer group.
-    evt.stopPropagation();
+    if(evt.type == "click")
+      evt.stopPropagation();
 
     this.alertsService.clearWarnings();
     this.ngbModal.open(DeleteAnswerGroupModalComponent, {
