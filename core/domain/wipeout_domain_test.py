4bcea812f4c147cc5cf84c6677698c29fd93e0d3
Sellars <leew@dickinson.edu>
2023-03-22 15:37:00 -0400

    created test_validate_succeeds_for_empty_pseudonymizable_entity_mappings

--- core/domain/wipeout_domain_test.py
+++ core/domain/wipeout_domain_test.py
@@ -18,7 +18,7 @@
 
 from __future__ import annotations
 
-from core import utils
+from core import feconf, utils
 from core.domain import wipeout_domain
 from core.tests import test_utils
 
@@ -58,3 +58,15 @@ class PendingDeletionRequestUnitTests(test_utils.GenericTestBase):
             'pseudonymizable_entity_mappings contain wrong key'
         ):
             pending_deletion_request.validate()
+
+    def test_validate_succeeds_for_empty_pseudonymizable_entity_mappings(
+            self
+        ) -> None:
+        """Tests the validate() function when pseudonymizable_entity_mappings
+        is empty."""
+        pending_deletion_request = (
+            wipeout_domain.PendingDeletionRequest.create_default(
+                self.user_id_a, 'username', 'a@example.com'))
+        # No exception should be raised as the pseudonymizable_entity_mappings
+        # is empty.
+        pending_deletion_request.validate()
