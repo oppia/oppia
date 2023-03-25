771f6824c4817d100ac3eeb0aebdd4012d4f5280
hemantmm <hemant29mehta@gmail.com>
2023-03-25 14:33:12 +0530

    Keyboard focus is now going to Lesson, Practice and Revision tabs with enter #17214

--- core/templates/pages/topic-viewer-page/topic-viewer-page.component.ts
+++ core/templates/pages/topic-viewer-page/topic-viewer-page.component.ts
@@ -80,6 +80,23 @@ export class TopicViewerPageComponent implements OnInit, OnDestroy {
     private translateService: TranslateService
   ) {}
 
+  ACTION_OPEN!: string;
+  ACTION_CLOSE!: string;
+  KEYBOARD_EVENT_TO_KEY_CODES!: {
+    enter: {
+        shiftKeyIsPressed: boolean;
+        keyCode: number;
+        };
+    tab: {
+        shiftKeyIsPressed: boolean;
+        keyCode: number;
+        };
+    shiftTab: {
+      shiftKeyIsPressed: boolean;
+      keyCode: number;
+      };
+    };
+
   ngOnInit(): void {
     if (this.urlService.getPathname().endsWith('revision')) {
       this.activeTab = 'subtopics';
