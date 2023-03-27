ebf3009ae25e1fe638340f3fa0ab377aebd47b91
Kej-r03 <99071926+Kej-r03@users.noreply.github.com>
2023-03-27 22:37:16 +0530

    Fix question editor image interaction console errors (#17670)
    * Fix question editor image interaction console errors
    
    * send image array while saving question
    
    * Rectify backend errors
    
    * Fix backend coverage
    
    ---------
    
    Co-authored-by: Priyansh Mehta <96513964+Priyansh61@users.noreply.github.com>

--- core/controllers/question_editor_test.py
+++ core/controllers/question_editor_test.py
@@ -15,7 +15,7 @@
 """Tests for the Question Editor controller."""
 
 from __future__ import annotations
-
+import json
 import os
 
 from core import feconf
@@ -380,7 +380,8 @@ class QuestionCreationHandlerTest(BaseQuestionEditorControllerTests):
         post_data = {
             'question_dict': question_dict,
             'skill_ids': [self.skill_id],
-            'skill_difficulties': [0.6]
+            'skill_difficulties': [0.6],
+            'filenames': json.dumps(['img.png'])
         }
 
         with utils.open_file(
@@ -391,7 +392,7 @@ class QuestionCreationHandlerTest(BaseQuestionEditorControllerTests):
         self.post_json(
             feconf.NEW_QUESTION_URL, post_data,
             csrf_token=csrf_token,
-            upload_files=[(filename, filename, raw_image)]
+            upload_files=[('image0', filename, raw_image)]
         )
         all_models = question_models.QuestionModel.get_all()
         questions = [
@@ -421,7 +422,8 @@ class QuestionCreationHandlerTest(BaseQuestionEditorControllerTests):
         post_data = {
             'question_dict': question_dict,
             'skill_ids': [self.skill_id],
-            'skill_difficulties': [0.6]
+            'skill_difficulties': [0.6],
+            'filenames': json.dumps(['img.svg'])
         }
 
         response_dict = self.post_json(
@@ -438,7 +440,7 @@ class QuestionCreationHandlerTest(BaseQuestionEditorControllerTests):
             feconf.NEW_QUESTION_URL, post_data,
             csrf_token=csrf_token,
             upload_files=[
-                ('img.svg', 'img.svg', large_image)
+                ('image0', 'img.svg', large_image)
             ], expected_status_int=400)
         self.assertIn(
             'Image exceeds file size limit of 100 KB.',
