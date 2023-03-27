1b816998f7afd270d2f33941a90d3e959c597bfa
liyong7679 <462702985@qq.com>
2023-03-27 14:38:45 -0400

    lint

--- core/tests/test_utils.py
+++ core/tests/test_utils.py
@@ -1852,7 +1852,7 @@ class AppEngineTestBase(TestBase):
         datastore_services.delete_multi(
             list(datastore_services.query_everything().iter(keys_only=True)))
         storage_services.CLIENT.reset()
-        if self.contextManager != None:
+        if self.contextManager is not None:
             self.contextManager.__exit__(None, None, None)
         super().tearDown()
 
