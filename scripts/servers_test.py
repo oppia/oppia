0a4ed8b5551660c72d83dcca20d9bd9526dfa628
ashish-patwal <ashishpatwal147@gmail.com>
2023-03-28 16:22:51 +0530

    servers_test covered

--- scripts/servers_test.py
+++ scripts/servers_test.py
@@ -1039,8 +1039,14 @@ class ManagedProcessTests(test_utils.TestBase):
     def test_managed_acceptance_test_server_with_explicit_args(self) -> None:
         popen_calls = self.exit_stack.enter_context(self.swap_popen())
 
+        puppeteer_acceptance_tests_dir_path = os.path.join(
+            common.CURR_DIR, 'core', 'tests', 'puppeteer-acceptance-tests')
+        spec_dir_path = os.path.join(
+            puppeteer_acceptance_tests_dir_path, 'spec')
+        test_file_path = os.path.join(spec_dir_path, 'test')
+
         self.exit_stack.enter_context(servers.managed_acceptance_tests_server(
-            suite_name='blog-admin-tests/assign-roles-to-users-and-change-tag-properties.spec.js',  # pylint: disable=line-too-long
+            suite_name='test',  # pylint: disable=line-too-long
             stdout=subprocess.PIPE))
         self.exit_stack.close()
 
@@ -1049,4 +1055,4 @@ class ManagedProcessTests(test_utils.TestBase):
             popen_calls[0].kwargs, {'shell': True, 'stdout': subprocess.PIPE})
         program_args = popen_calls[0].program_args
         # From suite.
-        self.assertIn('--suite=blog-admin-tests/assign-roles-to-users-and-change-tag-properties.spec.js', program_args)
+        self.assertIn(test_file_path, program_args)
