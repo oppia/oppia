556edf2dda33a4ff16c1be630a4f42dc612324a1
Jairo Honorio <registrofx@gmail.com>
2023-04-11 01:11:17 -0700

    Fix backend bug

--- core/controllers/contributor_dashboard.py
+++ core/controllers/contributor_dashboard.py
@@ -362,7 +362,7 @@ class ReviewableOpportunitiesHandler(
         # 3. Get the reviewable translation suggestion target IDs for the user.
         # 4. Get story exploration nodes in order, filtering for explorations
         # that have in review translation suggestions.
-        if topic_name is None or topic == 'All':
+        if topic_name is None or topic_name == 'All':
             topics = topic_fetchers.get_all_topics()
         else:
             topic = topic_fetchers.get_topic_by_name(topic_name)
