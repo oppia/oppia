db400c34b0b93db51e258277f6f3838faba4dbe2
Ash-2k3 <ash.me.cse@gmail.com>
2023-04-14 02:43:09 +0530

    Optimize

--- extensions/objects/templates/math-expression-content-editor.component.ts
+++ extensions/objects/templates/math-expression-content-editor.component.ts
@@ -226,8 +226,6 @@ export class MathExpressionContentEditorComponent implements OnInit {
       this.localValue = {
         label: this.value.raw_latex || '',
       };
-    }
-    if (this.value.raw_latex) {
       this.convertLatexStringToSvg(this.localValue.label);
     }
   }
