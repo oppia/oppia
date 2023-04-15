4d30c182fcfff5e1b3d14b162e2700c6889c9035
uglycrown <gourav.singh9756@gmail.com>
2023-04-15 19:57:27 +0530

    fix-moreoption-button

--- core/templates/pages/exploration-editor-page/history-tab/history-tab.component.ts
+++ core/templates/pages/exploration-editor-page/history-tab/history-tab.component.ts
@@ -465,13 +465,14 @@ export class HistoryTabComponent
 
     this.versionChoices = [10, 15, 20];
   }
-  
   isDropdownVisible = false;
   moreActionButton(event: KeyboardEvent, dropdownButton: HTMLElement) {
-    if (event.key === 'Enter') { // enter key
+    if (event.key === 'Enter') {
       const dropdownButtonId = dropdownButton.id;
-      const index = dropdownButtonId.substring(dropdownButtonId.lastIndexOf("-") + 1);
-      const dropdownMenuButton = document.getElementById(`dropdownMenuButton-${index}`);
+      const index = dropdownButtonId.substring(
+        dropdownButtonId.lastIndexOf('-') + 1);
+      const dropdownMenuButton = document.getElementById(
+        `dropdownMenuButton-${index}`);
       dropdownMenuButton?.click();
     }
   }
