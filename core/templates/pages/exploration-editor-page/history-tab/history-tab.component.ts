fce279aa443d25db1dcb1b44ac969fccce1912fb
uglycrown <gourav.singh9756@gmail.com>
2023-04-15 22:58:57 +0530

    more-option-button

--- core/templates/pages/exploration-editor-page/history-tab/history-tab.component.ts
+++ core/templates/pages/exploration-editor-page/history-tab/history-tab.component.ts
@@ -471,14 +471,14 @@ export class HistoryTabComponent
 
   moreActionButton(event: KeyboardEvent, dropdownButton: HTMLElement): void {
     if (event.key === 'Enter') {
-
       const dropdownButtonId = dropdownButton.id;
+
       const index = dropdownButtonId.substring(
         dropdownButtonId.lastIndexOf('-') + 1);
 
       const dropdownMenuButton = document.getElementById(
         `dropdownMenuButton-${index}`);
- 
+
       dropdownMenuButton?.click();
     }
   }
