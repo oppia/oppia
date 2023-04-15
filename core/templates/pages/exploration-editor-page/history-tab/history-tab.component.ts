c4dd4b10333b380bdd05c30f9c36645c4abd0c94
uglycrown <gourav.singh9756@gmail.com>
2023-04-15 22:53:41 +0530

    more-option-button

--- core/templates/pages/exploration-editor-page/history-tab/history-tab.component.ts
+++ core/templates/pages/exploration-editor-page/history-tab/history-tab.component.ts
@@ -465,8 +465,9 @@ export class HistoryTabComponent
 
     this.versionChoices = [10, 15, 20];
   }
-  isDropdownVisible = 
-  false;
+  isDropdownVisible =
+    
+    false;
 
   moreActionButton(event: KeyboardEvent, dropdownButton: HTMLElement): void {
     if (event.key === 'Enter') {
