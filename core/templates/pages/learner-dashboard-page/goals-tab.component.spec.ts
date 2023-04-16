e38e5b0170ff56e344305c69a968b17e6023490b
Aditya Rai <118007662+AdityaRaimec22@users.noreply.github.com>
2023-04-16 18:16:37 +0530

    last commit

--- core/templates/pages/learner-dashboard-page/goals-tab.component.spec.ts
+++ core/templates/pages/learner-dashboard-page/goals-tab.component.spec.ts
@@ -341,14 +341,14 @@ describe('Goals tab Component', () => {
 
   it('should correctly show and hide the dropdown', () => {
     for (let i = 0; i < component.currentGoals.length; i++) {
-      component.toggleThreeDotsDropdown(i);
-      expect(component.showThreeDotsDropdown[i]).toBe(true);
+      component.toggleThreeDotsDropdown(0);
+      expect(component.showThreeDotsDropdown[0]).toBe(true);
 
-      component.toggleThreeDotsDropdown(i);
-      expect(component.showThreeDotsDropdown[i]).toBe(false);
+      component.toggleThreeDotsDropdown(0);
+      expect(component.showThreeDotsDropdown[0]).toBe(false);
 
-      component.toggleThreeDotsDropdown(i);
-      expect(component.showThreeDotsDropdown[i]).toBe(true);
+      component.toggleThreeDotsDropdown(0);
+      expect(component.showThreeDotsDropdown[0]).toBe(true);
 
       let fakeClickAwayEvent = new MouseEvent('click');
       Object.defineProperty(
@@ -357,13 +357,13 @@ describe('Goals tab Component', () => {
         {value: document.createElement('div')});
       component.onDocumentClick(fakeClickAwayEvent);
       fixture.detectChanges();
-      expect(component.showThreeDotsDropdown[i]).toBe(false);
+      expect(component.showThreeDotsDropdown[0]).toBe(false);
 
       // Three dots are not shown when no goals are present.
       component.dropdownRef = undefined;
       component.onDocumentClick(fakeClickAwayEvent);
       fixture.detectChanges();
-      expect(component.showThreeDotsDropdown[i]).toBe(false);
+      expect(component.showThreeDotsDropdown[0]).toBe(false);
     }
   });
 });
