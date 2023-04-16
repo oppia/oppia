c1316870ed4a0216630bc6ace48dc213a5e66019
Aditya Rai <118007662+AdityaRaimec22@users.noreply.github.com>
2023-04-16 18:18:25 +0530

    commit

--- core/templates/pages/learner-dashboard-page/goals-tab.component.ts
+++ core/templates/pages/learner-dashboard-page/goals-tab.component.ts
@@ -200,16 +200,14 @@ export class GoalsTabComponent implements OnInit {
   @HostListener('document:click', ['$event'])
   onDocumentClick(event: MouseEvent): void {
     const targetElement = event.target as HTMLElement;
-    for (let i = 0; i < this.currentGoals.length; i++) {
-      if (!this.dropdownRef) {
-        return;
-      }
-      if (
-        targetElement &&
-        !this.dropdownRef.nativeElement.contains(targetElement)
-      ) {
-        this.showThreeDotsDropdown[i] = false;
-      }
+    if (!this.dropdownRef) {
+      return;
+    }
+    if (
+      targetElement &&
+      !this.dropdownRef.nativeElement.contains(targetElement)
+    ) {
+      this.showThreeDotsDropdown[0] = false;
     }
   }
 
