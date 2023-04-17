0d15765d0594918b4ab938eaf579aa0ab3e838ec
jakarpen <jakarpen@umich.edu>
2023-04-17 18:14:29 -0400

    Increased coverage for core.controllers.subtopic_viewer_test.py to 98%

--- core/controllers/subtopic_viewer_test.py
+++ core/controllers/subtopic_viewer_test.py
@@ -1,14 +1,30 @@
-from __future__ import annotations
-from core import feconf
+# Copyright 2019 The Oppia Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+"""Tests for subtopic viewer page"""
+
+from __future__ import annotations
+
+from core import feconf
+from core.domain import state_domain
 from core.domain import subtopic_page_domain
 from core.domain import subtopic_page_services
 from core.domain import topic_domain
-from core.tests import test_utils
-from core.domain import user_services
 from core.domain import topic_services
 from core.domain import translation_domain
-from core.domain import state_domain
+from core.domain import user_services
+from core.tests import test_utils
 
 
 class BaseSubtopicViewerControllerTests(test_utils.GenericTestBase):
@@ -266,6 +282,7 @@ class SubtopicPageDataHandlerTests(BaseSubtopicViewerControllerTests):
 
 
 class SubtopicViewerPageTests2(test_utils.GenericTestBase):
+    """Test for SubtopicViewerPage when len(topic.subtopic) = 1."""
     def setUp(self):
         super().setUp()
         self.signup(self.CURRICULUM_ADMIN_EMAIL, self.CURRICULUM_ADMIN_USERNAME)
