91d2a3355a3393371423025aabc3e156f3f24d81
Sagang Wee <9004726+sagangwee@users.noreply.github.com>
2023-04-17 17:44:01 -0400

    return type

--- core/templates/domain/question/question-update.service.ts
+++ core/templates/domain/question/question-update.service.ts
@@ -73,7 +73,7 @@ export class QuestionUpdateService {
 
   _getParameterFromChangeDict(
       changeDict: BackendChangeObject, paramName: string
-  ): string {
+  ): string | string[] {
     return changeDict[paramName];
   }
 
