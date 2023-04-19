9870cbaaf5565040572862b0d642425a667dee3b
liyong7679 <462702985@qq.com>
2023-04-19 19:13:06 +0800

    test6

--- scripts/create_topological_sort_of_all_services.py
+++ scripts/create_topological_sort_of_all_services.py
@@ -129,7 +129,7 @@ def make_graph() -> Tuple[Dict[str, List[str]], List[str]]:
 
 def main() -> None:
     """Prints the topological order of the services based on the
-    dependencies. Test4.
+    dependencies. Test5.
     """
     adj_list, nodes_list = make_graph()
     visit_stack: List[str] = []
