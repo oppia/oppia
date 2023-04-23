2e6874431dbdd3e9d6c15863ef070c7ffaa31dfc
uglycrown <gourav.singh9756@gmail.com>
2023-04-23 12:05:41 +0530

    progressbar-read-fix

--- core/templates/pages/exploration-player-page/templates/lesson-information-card-modal.component.spec.ts
+++ core/templates/pages/exploration-player-page/templates/lesson-information-card-modal.component.spec.ts
@@ -436,26 +436,4 @@ describe('Lesson Information card modal component', () => {
 
     expect(componentInstance.saveProgressMenuIsShown).toBeFalse();
   });
-
-  describe('ProgressBar', () => {
-    let progressBar;
-
-    beforeEach(() => {
-      progressBar = new progressBar(5);
-    });
-
-    it('should return "0" when no checkpoints have been completed', () => {
-      expect(progressBar.getProgressPercentage()).toEqual('0');
-    });
-
-    it('should return "100" when all checkpoints have been completed', () => {
-      progressBar.completedCheckpointsCount = 5;
-      expect(progressBar.getProgressPercentage()).toEqual('100');
-    });
-
-    it('should return the correct percentage when have been completed', () => {
-      progressBar.completedCheckpointsCount = 2;
-      expect(progressBar.getProgressPercentage()).toEqual('40');
-    });
-  });
 });
