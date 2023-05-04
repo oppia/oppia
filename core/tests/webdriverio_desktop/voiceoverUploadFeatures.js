52f3f3f7ddd30015c7ab599e66feda6f92456b2e
liyong7679 <462702985@qq.com>
2023-05-05 01:32:55 +0800

    5

--- core/tests/webdriverio_desktop/voiceoverUploadFeatures.js
+++ core/tests/webdriverio_desktop/voiceoverUploadFeatures.js
@@ -114,7 +114,7 @@ describe('Voiceover upload features', function() {
       await explorationEditorTranslationTab.stopAudioRecord();
       await explorationEditorTranslationTab.confirmAudioRecord();
       await explorationEditorTranslationTab.playAudioRecord();
-      // Refresh create flake3.
+      // Refresh create flake4.
       await browser.refresh();
       await explorationEditorTranslationTab.playAudioRecord();
 
