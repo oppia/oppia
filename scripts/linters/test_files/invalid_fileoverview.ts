5af19af6f20cbbcd6e6b756e0bb9947683dc0802
Shivkant Chauhan <shivkantchauhan2002@gmail.com>
2023-05-19 02:12:01 +0530

    migrated dependencies to package.json

--- scripts/linters/test_files/invalid_fileoverview.ts
+++ scripts/linters/test_files/invalid_fileoverview.ts
@@ -18,8 +18,8 @@ Object.defineProperty(window, 'CodeMirror', {
   writable: false
 });
 
-require('static/code-mirror-5.17.0/mode/javascript/javascript.js');
-require('static/code-mirror-5.17.0/mode/python/python.js');
-require('static/code-mirror-5.17.0/mode/yaml/yaml.js');
+require('/node_modules/code-mirror/mode/javascript/javascript.js');
+require('/node_modules/code-mirror/mode/python/python.js');
+require('/node_modules/code-mirror/mode/yaml/yaml.js');
 require('static/ui-codemirror-5d04fa/src/ui-codemirror.js');
 require('static/diff-match-patch-1.0.0/diff_match_patch.js');
