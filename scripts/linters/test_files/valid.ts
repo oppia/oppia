615bdcf166993b377e0c61bf4a5ccda463d3a0a2
Shivkant Chauhan <shivkantchauhan2002@gmail.com>
2023-05-21 11:56:44 +0530

    migrated code-mirror

--- scripts/linters/test_files/valid.ts
+++ scripts/linters/test_files/valid.ts
@@ -17,14 +17,14 @@
  * js_ts_linter_test.py. This file contain valid ts file content.
  */
 
-const CodeMirror = require('static/code-mirror-5.17.0/lib/codemirror.js');
+const CodeMirror = require('/node_modules/code-mirror/lib/codemirror.js');
 Object.defineProperty(window, 'CodeMirror', {
   value: CodeMirror,
   writable: false
 });
 
-require('static/code-mirror-5.17.0/mode/javascript/javascript.js');
-require('static/code-mirror-5.17.0/mode/python/python.js');
-require('static/code-mirror-5.17.0/mode/yaml/yaml.js');
+require('/node_modules/code-mirror/mode/javascript/javascript.js');
+require('/node_modules/code-mirror/mode/python/python.js');
+require('/node_modules/code-mirror/mode/yaml/yaml.js');
 require('static/ui-codemirror-5d04fa/src/ui-codemirror.js');
 require('static/diff-match-patch-1.0.0/diff_match_patch.js');
