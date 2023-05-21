89e8c4eb1b2144ea0c3bcf586563b20116b94265
Praneeth <praneethg2001@gmail.com>
2023-05-21 17:50:33 +0530

    Clean up

--- core/tests/karma.conf.ts
+++ core/tests/karma.conf.ts
@@ -11,7 +11,7 @@ if (argv.prodEnv) {
 
 // Generate a random number between 0 and 999 to use as the seed for the
 // frontend test execution order.
-let jasmineSeed = 163;
+let jasmineSeed = Math.floor(Math.random() * 1000);
 // eslint-disable-next-line no-console
 console.log(`Seed for Frontend Test Execution Order ${jasmineSeed}`);
 
