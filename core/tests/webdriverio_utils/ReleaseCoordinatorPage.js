976bc9af73dd0b8ccdfd2272d9d834b9ef03160b
Hitesh Tomar <lkbhitesh07@gmail.com>
2023-05-31 17:03:15 +0530

    intial changes for e2e test file for release-coordinator

---
+++ core/tests/webdriverio_utils/ReleaseCoordinatorPage.js
@@ -0,0 +1,165 @@
+// Copyright 2023 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Page object for the release-coordinator page, for use in
+ * WebdriverIO tests.
+ */
+
+var action = require('./action.js');
+var forms = require('./forms.js');
+var general = require('./general.js');
+var waitFor = require('./waitFor.js');
+
+var ReleaseCoordinatorPage = function() {
+    var RELEASE_COORDINATOR_URL_SUFFIX = '/release-coordinator';
+    var addFeatureRuleButtonLocator = '.e2e-test-feature-add-rule-button';
+    var featureFlagElementsSelector = function() {
+        return $$('.e2e-test-feature-flag');
+    };
+    var featureFlagElement = $('.e2e-test-feature-flag');
+    var featureNameLocator = '.e2e-test-feature-name';
+    var featuresTab = $('.e2e-test-features-tab');
+    var removeRuleButtonLocator = '.e2e-test-remove-rule-button';
+
+    this.get = async function() {
+        await browser.url(RELEASE_COORDINATOR_URL_SUFFIX);
+        await waitFor.pageToFullyLoad();
+    };
+
+    this.getFeaturesTab = async function() {
+        await this.get();
+        await action.click('Admin features tab', featuresTab);
+        await waitFor.visibilityOf(
+          featureFlagElement, 'Feature flags not showing up');
+    };
+
+    this.getDummyFeatureElement = async function() {
+        var featureFlagElements = await featureFlagElementsSelector();
+        var count = featureFlagElements.length;
+        for (let i = 0; i < count; i++) {
+          var elem = featureFlagElements[i];
+          if ((await elem.$(featureNameLocator).getText()) ===
+              'dummy_feature') {
+            return elem;
+          }
+        }
+    
+        return null;
+    };
+
+    this.removeAllRulesOfFeature = async function(featureElement) {
+        while (!await featureElement.$(noRuleIndicatorLocator).isExisting()) {
+          await action.click(
+            'Remove feature rule button',
+            featureElement
+              .$(removeRuleButtonLocator)
+          );
+        }
+    };
+
+  // Remove this method after the end_chapter_celebration feature flag
+  // is deprecated.
+  this.getEndChapterCelebrationFeatureElement = async function() {
+    var featureFlagElements = await featureFlagElementsSelector();
+    var count = featureFlagElements.length;
+    for (let i = 0; i < count; i++) {
+      var elem = featureFlagElements[i];
+      if ((await elem.$(featureNameLocator).getText()) ===
+          'end_chapter_celebration') {
+        return elem;
+      }
+    }
+
+    return null;
+  };
+
+  // Remove this method after the checkpoint_celebration feature flag
+  // is deprecated.
+  this.getCheckpointCelebrationFeatureElement = async function() {
+    var featureFlagElements = await featureFlagElementsSelector();
+    var count = featureFlagElements.length;
+    for (let i = 0; i < count; i++) {
+      var elem = featureFlagElements[i];
+      if ((await elem.$(featureNameLocator).getText()) ===
+          'checkpoint_celebration') {
+        return elem;
+      }
+    }
+
+    return null;
+  };
+
+  // This function is meant to be used to enable a feature gated behind
+  // a feature flag in prod mode, which is the server environment the E2E
+  // tests are run in.
+  this.enableFeatureForProd = async function(featureElement) {
+    await this.removeAllRulesOfFeature(featureElement);
+
+    await action.click(
+      'Add feature rule button',
+      featureElement
+        .$(addFeatureRuleButtonLocator)
+    );
+
+    await waitFor.visibilityOf(
+      featureElement.$(valueSelectorLocator),
+      'Value Selector takes too long to appear'
+    );
+    await (featureElement.$(valueSelectorLocator)).selectByVisibleText(
+      'Enabled');
+
+    await action.click(
+      'Add condition button',
+      featureElement
+        .$(addConditionButtonLocator)
+    );
+
+    await waitFor.visibilityOf(
+      featureElement.$(serverModeSelectorLocator),
+      'Value Selector takes too long to appear'
+    );
+    await (featureElement.$(serverModeSelectorLocator)).selectByVisibleText(
+      'prod');
+
+    await this.saveChangeOfFeature(featureElement);
+  };
+
+  this.enableFeatureForDev = async function(featureElement) {
+    await this.removeAllRulesOfFeature(featureElement);
+
+    await action.click(
+      'Add feature rule button',
+      featureElement
+        .$(addFeatureRuleButtonLocator)
+    );
+
+    await waitFor.visibilityOf(
+      featureElement.$(valueSelectorLocator),
+      'Value Selector takes too long to appear'
+    );
+    await (featureElement.$(valueSelectorLocator)).selectByVisibleText(
+      'Enabled');
+
+    await action.click(
+      'Add condition button',
+      featureElement
+        .$(addConditionButtonLocator)
+    );
+
+    await this.saveChangeOfFeature(featureElement);
+  };
+}
+
+exports.ReleaseCoordinatorPage = ReleaseCoordinatorPage;
