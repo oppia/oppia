b9f3cd6c1e4d96d5d64df9105ed2c9a42b497f16
Ash-2k3 <ash.me.cse@gmail.com>
2023-06-01 13:10:54 +0530

    Elvis operatoee for undefined type

--- extensions/objects/templates/skill-selector-editor.component.ts
+++ extensions/objects/templates/skill-selector-editor.component.ts
@@ -36,8 +36,8 @@ export class SkillSelectorEditorComponent implements OnInit, OnDestroy {
   @Input() modalId!: symbol;
   @Input() value!: string;
   @Output() valueChanged = new EventEmitter();
-  private _initialEntityId?;
-  private _initialEntityType?;
+  private _initialEntityId ?: string ;
+  private _initialEntityType ?: string ;
   skills: SkillBackendDict[] = [];
   showLoading = false;
   skillsToShow: SkillBackendDict[] = [];
