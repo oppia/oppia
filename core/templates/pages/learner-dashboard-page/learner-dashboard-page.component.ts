94a92405c0ac804b542c832bdfa3b1204a5a0b88
Patel Muhammad <96219910+Patel-Muhammad@users.noreply.github.com>
2023-06-08 16:04:05 +0530

    fixed import

--- core/templates/pages/learner-dashboard-page/learner-dashboard-page.component.ts
+++ core/templates/pages/learner-dashboard-page/learner-dashboard-page.component.ts
@@ -17,7 +17,7 @@
  * @fileoverview Component for the learner dashboard.
  */
 
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ElementRef, ViewChild  } from '@angular/core';
 import { downgradeComponent } from '@angular/upgrade/static';
 import { trigger, state, style, transition,
   animate, group } from '@angular/animations';
