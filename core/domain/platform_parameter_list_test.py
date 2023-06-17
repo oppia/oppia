93f84c73ee25fa6a17fda129421abb95d703c770
JSRKJSRK <SubhashThenua>
2023-06-17 19:52:21 +0530

    fix linter

--- core/domain/platform_parameter_list_test.py
+++ core/domain/platform_parameter_list_test.py
@@ -41,7 +41,6 @@ class ExistingPlatformParameterValidityTests(test_utils.GenericTestBase):
                             'promo_bar_enabled',
                             'promo_bar_message']
 
-
     def test_all_defined_parameters_are_valid(self) -> None:
         all_names = params.Registry.get_all_platform_parameter_names()
         for name in all_names:
