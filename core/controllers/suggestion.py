954e171fc8aa3b0007eebea6327115ab81d8fead
Ash-2k3 <ash.me.cse@gmail.com>
2023-06-19 19:11:43 +0530

    Fix mypy checks

--- core/controllers/suggestion.py
+++ core/controllers/suggestion.py
@@ -771,10 +771,11 @@ class ReviewableSuggestionsHandler(
                 .get_suggestions_with_editable_explorations(
                     reviewable_suggestions))
         elif suggestion_type == feconf.SUGGESTION_TYPE_ADD_QUESTION:
-            suggestions, next_offset = (
-                suggestion_services
-                .get_reviewable_question_suggestions_by_offset(
-                    self.user_id, limit, offset, sort_key))
+            if limit:
+                suggestions, next_offset = (
+                    suggestion_services
+                    .get_reviewable_question_suggestions_by_offset(
+                        self.user_id, limit, offset, sort_key))
         self._render_suggestions(target_type, suggestions, next_offset)
 
 
