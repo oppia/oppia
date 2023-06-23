3d236c1cc1f14981dc7ffd06b55f4c766efc6392
Ashwath Kannan <98253080+Ash-2k3@users.noreply.github.com>
2023-06-24 02:29:06 +0530

    Fix frontend tests

--- core/templates/pages/contributor-dashboard-page/opportunities-list-item/opportunities-list-item.component.spec.ts
+++ core/templates/pages/contributor-dashboard-page/opportunities-list-item/opportunities-list-item.component.spec.ts
@@ -244,7 +244,7 @@ describe('Opportunities List Item Component', () => {
       const translationLengthLabel = bannerElement.querySelector(
         '.oppia-translation-length-label');
 
-      expect(translationLengthLabel).toBeFalsy();
+      expect(translationLengthLabel?.textContent).toContain('');
     });
   });
 });
