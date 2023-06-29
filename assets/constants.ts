41e50c8c26580fc3d6dbd4795cf4adf087531bec
Ykumar1415 <yogeshsaini1213@gmail.com>
2023-06-29 19:21:32 +0530

    navbar and learn-math page  problem solved

--- assets/constants.ts
+++ assets/constants.ts
@@ -7057,6 +7057,6 @@ export default {
   ],
   "CONTRIBUTOR_CERTIFICATE_WIDTH": 1493,
   "CONTRIBUTOR_CERTIFICATE_HEIGHT": 1313,
-  "BRANCH_NAME": "",
-  "SHORT_COMMIT_HASH": ""
+  "BRANCH_NAME": "responsive-langbtn",
+  "SHORT_COMMIT_HASH": "b550fab54"
 } as const;
