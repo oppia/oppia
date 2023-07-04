ae2d8f1ff8b072946c9ec84606598df1a4dec4fc
Yogesh Saini <97088265+Ykumar1415@users.noreply.github.com>
2023-07-04 12:17:10 +0530

    Update constants.ts

--- assets/constants.ts
+++ assets/constants.ts
@@ -7057,6 +7057,7 @@ export default {
   ],
   "CONTRIBUTOR_CERTIFICATE_WIDTH": 1493,
   "CONTRIBUTOR_CERTIFICATE_HEIGHT": 1313,
-  "BRANCH_NAME": "log-data",
+  "BRANCH_NAME": "",
+  "SHORT_COMMIT_HASH": "",
   "SHORT_COMMIT_HASH": "d902f4a02"
 } as const;
