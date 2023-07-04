5a7d11a1f84acadaa36fc1e641b8d9483a6af0f3
Shivkant Chauhan <shivkantchauhan2002@gmail.com>
2023-07-05 02:01:28 +0530

    added separate script for dependencies.json package + multi-stage dockerfile

--- scripts/install_dependencies_json_packages.py
+++ scripts/install_dependencies_json_packages.py
@@ -16,12 +16,10 @@
 
 from __future__ import annotations
 
-import argparse
 import contextlib
 import io
 import json
 import os
-import subprocess
 import sys
 import tarfile
 import urllib
@@ -31,7 +29,6 @@ from core import utils
 from typing import Dict, Final, List, Literal, Optional, TypedDict, cast
 
 from . import common
-from . import install_python_prod_dependencies
 
 TOOLS_DIR: Final = os.path.join('..', 'oppia_tools')
 THIRD_PARTY_DIR: Final = os.path.join('.', 'third_party')
@@ -86,11 +83,6 @@ DOWNLOAD_FORMATS_TO_DEPENDENCIES_KEYS: Dict[
         'optional_key_pairs': []
     }
 }
-
-_PARSER = argparse.ArgumentParser(
-    description="""
-Installation script for Oppia third-party libraries.
-""")
 
 
 # Here we use total=False since some fields in this dict
@@ -403,125 +395,10 @@ def download_all_dependencies(filepath: str) -> None:
                     dependency_tar_root_name, dependency_target_root_name)
 
 
-def install_elasticsearch_dev_server() -> None:
-    """This installs a local ElasticSearch server to the oppia_tools
-    directory to be used by development servers and backend tests.
-    """
-    try:
-        subprocess.call(
-            ['%s/bin/elasticsearch' % common.ES_PATH, '--version'],
-            stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            # Set the minimum heap size to 100 MB and maximum to 500 MB.
-            env={'ES_JAVA_OPTS': '-Xms100m -Xmx500m'}
-        )
-        print('ElasticSearch is already installed.')
-        return
-    except OSError:
-        print('Installing ElasticSearch...')
-
-    if common.is_mac_os() or common.is_linux_os():
-        file_ext = 'tar.gz'
-        def download_and_extract(*args: str) -> None:
-            """This downloads and extracts the elasticsearch files."""
-            download_and_untar_files(*args)
-    elif common.is_windows_os():
-        file_ext = 'zip'
-        def download_and_extract(*args: str) -> None:
-            """This downloads and extracts the elasticsearch files."""
-            download_and_unzip_files(*args)
-    else:
-        raise Exception('Unrecognized or unsupported operating system.')
-
-    download_and_extract(
-        'https://artifacts.elastic.co/downloads/elasticsearch/' +
-        'elasticsearch-%s-%s-x86_64.%s' % (
-            common.ELASTICSEARCH_VERSION,
-            common.OS_NAME.lower(),
-            file_ext
-        ),
-        TARGET_DOWNLOAD_DIRS['oppiaTools'],
-        'elasticsearch-%s' % common.ELASTICSEARCH_VERSION,
-        'elasticsearch-%s' % common.ELASTICSEARCH_VERSION
-    )
-    print('ElasticSearch installed successfully.')
-
-
-def install_redis_cli() -> None:
-    """This installs the redis-cli to the local oppia third_party directory so
-    that development servers and backend tests can make use of a local redis
-    cache. Redis-cli installed here (redis-cli-6.0.6) is different from the
-    redis package installed in dependencies.json (redis-3.5.3). The redis-3.5.3
-    package detailed in dependencies.json is the Python library that allows
-    users to communicate with any Redis cache using Python. The redis-cli-6.0.6
-    package installed in this function contains C++ scripts for the redis-cli
-    and redis-server programs detailed below.
-
-    The redis-cli program is the command line interface that serves up an
-    interpreter that allows users to connect to a redis database cache and
-    query the cache using the Redis CLI API. It also contains functionality to
-    shutdown the redis server. We need to install redis-cli separately from the
-    default installation of backend libraries since it is a system program and
-    we need to build the program files after the library is untarred.
-
-    The redis-server starts a Redis database on the local machine that can be
-    queried using either the Python redis library or the redis-cli interpreter.
-    """
-    try:
-        subprocess.call(
-            [common.REDIS_SERVER_PATH, '--version'],
-            stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE)
-        print('Redis-cli is already installed.')
-    except OSError:
-        # The redis-cli is not installed, run the script to install it.
-        # NOTE: We do the installation here since we need to use make.
-        print('Installing redis-cli...')
-
-        download_and_untar_files(
-            ('https://download.redis.io/releases/redis-%s.tar.gz') %
-            common.REDIS_CLI_VERSION,
-            TARGET_DOWNLOAD_DIRS['oppiaTools'],
-            'redis-%s' % common.REDIS_CLI_VERSION,
-            'redis-cli-%s' % common.REDIS_CLI_VERSION)
-
-        # Temporarily change the working directory to redis-cli-6.0.6 so we can
-        # build the source code.
-        with common.CD(
-            os.path.join(
-                TARGET_DOWNLOAD_DIRS['oppiaTools'],
-                'redis-cli-%s' % common.REDIS_CLI_VERSION)):
-            # Build the scripts necessary to start the redis server.
-            # The make command only builds the C++ files in the src/ folder
-            # without modifying anything outside of the oppia root directory.
-            # It will build the redis-cli and redis-server files so that we can
-            # run the server from inside the oppia folder by executing the
-            # script src/redis-cli and src/redis-server.
-            subprocess.call(['make'])
-
-        # Make the scripts executable.
-        subprocess.call([
-            'chmod', '+x', common.REDIS_SERVER_PATH])
-        subprocess.call([
-            'chmod', '+x', common.REDIS_CLI_PATH])
-
-        print('Redis-cli installed successfully.')
-
-
 def main(args: Optional[List[str]] = None) -> None:
-    """Installs all the third party libraries."""
-    if common.is_windows_os():
-        # The redis cli is not compatible with Windows machines.
-        raise Exception(
-            'The redis command line interface will not be installed because '
-            'your machine is on the Windows operating system.')
-    unused_parsed_args = _PARSER.parse_args(args=args)
-    install_python_prod_dependencies.main()
+    """Installs all the packages from the dependencies.json file."""
+
     download_all_dependencies(DEPENDENCIES_FILE_PATH)
-    install_redis_cli()
-    install_elasticsearch_dev_server()
 
 
 # The 'no coverage' pragma is used as this line is un-testable. This is because
