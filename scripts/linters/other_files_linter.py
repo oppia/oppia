15e2978780f8fd8761c537beea9052c723cdccb4
Shivkant Chauhan <shivkantchauhan2002@gmail.com>
2023-07-05 21:25:29 +0530

    added regex for git commit hash packages

--- scripts/linters/other_files_linter.py
+++ scripts/linters/other_files_linter.py
@@ -194,7 +194,7 @@ class CustomLintChecksManager(linter_utils.BaseLinter):
                 # In cases where the version is in the form of git commit hashes
                 # such as 'git+https://github.com/username/repo#commit-hash',
                 # we extract the commit hash and use it as the version.
-                elif lib_version[:23] == 'git+https://github.com/':
+                elif lib_version.startswith('git+https://github.com/'):
                     match = re.search(GIT_COMMIT_HASH_REGEX, lib_version)
                     if match:
                         lib_version = match.group(1)
