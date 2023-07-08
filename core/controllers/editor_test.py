b3db90707a56154aa88413f8e48f41f0ad067582
lkbhitesh07 <lkbhiteh07@gmail.com>
2023-07-08 17:13:30 +0530

    fix lint

--- core/controllers/editor_test.py
+++ core/controllers/editor_test.py
@@ -28,7 +28,6 @@ from core import feconf
 from core import utils
 from core.constants import constants
 from core.controllers import creator_dashboard
-from core.domain import config_services
 from core.domain import exp_domain
 from core.domain import exp_fetchers
 from core.domain import exp_services
