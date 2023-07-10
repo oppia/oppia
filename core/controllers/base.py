fb81fe01b7fc862919e1bf6b64856b7a6c4e4a5e
lkbhitesh07 <lkbhiteh07@gmail.com>
2023-07-10 15:18:11 +0530

    fixed lint

--- core/controllers/base.py
+++ core/controllers/base.py
@@ -36,7 +36,6 @@ from core.controllers import payload_validator
 from core.domain import auth_domain
 from core.domain import auth_services
 from core.domain import classifier_domain
-from core.domain import config_services
 from core.domain import platform_feature_services
 from core.domain import platform_parameter_domain
 from core.domain import platform_parameter_list
