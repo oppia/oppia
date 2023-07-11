d5ced9acad882d84c2020d8886d8f100f8d5f6cd
Pirajesh <pirajeshmr@gmail.com>
2023-07-11 23:09:03 +0530

    updates

--- assets/constants.ts
+++ assets/constants.ts
@@ -7057,6 +7057,6 @@ export default {
   ],
   "CONTRIBUTOR_CERTIFICATE_WIDTH": 1493,
   "CONTRIBUTOR_CERTIFICATE_HEIGHT": 1313,
-  "BRANCH_NAME": "",
-  "SHORT_COMMIT_HASH": ""
+  "BRANCH_NAME": "tab-alignment",
+  "SHORT_COMMIT_HASH": "76fd182e1a"
 } as const;
