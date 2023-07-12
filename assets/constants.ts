fec991f686878b03d07e44641e453ac9c3ed2d3f
SUBHASH THENUA <100484672+SubhashThenua@users.noreply.github.com>
2023-07-13 00:30:26 +0530

    Update constants.ts

--- assets/constants.ts
+++ assets/constants.ts
@@ -7077,6 +7077,6 @@ export default {
   ],
   "CONTRIBUTOR_CERTIFICATE_WIDTH": 1493,
   "CONTRIBUTOR_CERTIFICATE_HEIGHT": 1313,
-  "BRANCH_NAME": "Redesign-home-tab-GSoC-M1.3",
-  "SHORT_COMMIT_HASH": "ab21608c78"
+  "BRANCH_NAME": "",
+  "SHORT_COMMIT_HASH": ""
 } as const;
