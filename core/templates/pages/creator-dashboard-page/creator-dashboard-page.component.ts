657381daa5556bd4dd31392773c49a47fc2e8297
Patel-Muhammad <patelmuh2003@gmail.com>
2023-07-15 08:00:31 +0530

    fixed screenreader issues

--- core/templates/pages/creator-dashboard-page/creator-dashboard-page.component.ts
+++ core/templates/pages/creator-dashboard-page/creator-dashboard-page.component.ts
@@ -59,6 +59,7 @@ export class CreatorDashboardPageComponent {
   // 'lastWeekStats' is null for a new creator.
   lastWeekStats!: CreatorDashboardStats | null;
   dashboardStats!: CreatorDashboardStats;
+  dashbaordNumratings!: number | null;
   relativeChangeInTotalPlays!: number;
   getLocaleAbbreviatedDatetimeString!: (millisSinceEpoch: number) => string;
   getHumanReadableStatus!: (status: string) => string;
