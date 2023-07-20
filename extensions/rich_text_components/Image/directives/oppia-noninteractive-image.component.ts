4b30b8ca4ced2d3200ca22119dd3a876e0930eb2
Patel-Muhammad <patelmuh2003@gmail.com>
2023-07-20 12:42:21 +0530

    lint

--- extensions/rich_text_components/Image/directives/oppia-noninteractive-image.component.ts
+++ extensions/rich_text_components/Image/directives/oppia-noninteractive-image.component.ts
@@ -169,7 +169,7 @@ export class NoninteractiveImage implements OnInit, OnChanges {
           }
         }
         throw e;
-      }
+     }
     }
      // We set the alt text and caption for the image, regardless of when
     // user is in the exploration player or not in the exploration player.
