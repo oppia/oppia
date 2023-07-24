d116eeb8bc25451da7815d2323981406dba12269
lkbhitesh07 <lkbhiteh07@gmail.com>
2023-07-24 19:36:03 +0530

    nit

--- core/domain/platform_parameter_registry.py
+++ core/domain/platform_parameter_registry.py
@@ -78,9 +78,10 @@ class Registry:
             default: Optional[Union[bool, int, str, float]]. The default value
                 for the platform parameter.
             is_feature: bool. True if the platform parameter is a feature flag.
-            feature_stage: Enum(FeatureStages)|None. The stage of the feature,
-                required if 'is_feature' is True. The value for the platform
-                parameters should be None.
+            feature_stage: Enum(FeatureStages)|None. For feature flags
+                (i.e., where 'is_feature' is True), this specifies the feature
+                stage for that feature. For platform parameters, this value
+                should be None.
 
         Returns:
             PlatformParameter. The created platform parameter.
