b7fe3f3eff3a3459203605e50ffe93adbb20f531
PRAFUL BANDRE <109658545+prafulbbandre@users.noreply.github.com>
2023-07-29 15:08:02 +0530

    Update constants.ts

--- assets/constants.ts
+++ assets/constants.ts
@@ -7095,6 +7095,6 @@ export default {
   ],
   "CONTRIBUTOR_CERTIFICATE_WIDTH": 1493,
   "CONTRIBUTOR_CERTIFICATE_HEIGHT": 1313,
-  "BRANCH_NAME": "concept_card",
-  "SHORT_COMMIT_HASH": "441a01437e"
+  "BRANCH_NAME": "",
+  "SHORT_COMMIT_HASH": ""
 } as const;
