23eaac531e145b06a9d6872e27c125a29a0718de
Praful <prafulbbandre2001@gmail.com>
2023-08-02 00:18:13 +0530

    backend test resolved

--- assets/constants.ts
+++ assets/constants.ts
@@ -7092,6 +7092,6 @@ export default {
   ],
   "CONTRIBUTOR_CERTIFICATE_WIDTH": 1493,
   "CONTRIBUTOR_CERTIFICATE_HEIGHT": 1313,
-  "BRANCH_NAME": "contact_us-page",
-  "SHORT_COMMIT_HASH": "5819c854ef"
+  "BRANCH_NAME": "",
+  "SHORT_COMMIT_HASH": ""
 } as const;
