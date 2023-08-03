7f4e4b9107f444614be40317902b150272b70f41
Cary Xiao <caryxiao@stanford.edu>
2023-08-03 14:32:30 -0700

    fix lhci backend tests 5

--- scripts/run_lighthouse_tests_test.py
+++ scripts/run_lighthouse_tests_test.py
@@ -137,7 +137,6 @@ class RunLighthouseTestsTests(test_utils.GenericTestBase):
             'Puppeteer script completed successfully.', self.print_arr)
         self.assertIn(
             'Starting LHCI Puppeteer script with recording.', self.print_arr)
-        self.assertIn('Resulting puppeteer video saved at', self.print_arr)
 
     def test_run_lighthouse_puppeteer_script_failed(self) -> None:
         class MockTask:
@@ -171,7 +170,6 @@ class RunLighthouseTestsTests(test_utils.GenericTestBase):
             self.print_arr)
         self.assertIn(
             'Starting LHCI Puppeteer script with recording.', self.print_arr)
-        self.assertIn('Resulting puppeteer video saved at', self.print_arr)
 
     def test_run_webpack_compilation_successfully(self) -> None:
         swap_isdir = self.swap_with_checks(
