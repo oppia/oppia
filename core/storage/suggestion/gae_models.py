9b5e21d654b291694cc5bbdc1f30c186d8c72349
Ash-2k3 <ash.me.cse@gmail.com>
2023-08-06 19:51:44 +0530

    Add todo's issue number

--- core/storage/suggestion/gae_models.py
+++ core/storage/suggestion/gae_models.py
@@ -709,7 +709,7 @@ class GeneralSuggestionModel(base_models.BaseModel):
             next_offset
         )
 
-    # TODO(sll): Transition the callsite to use the new method
+    # TODO(#18745): Transition the callsite to use the new method
     # get_reviewable_translation_suggestions_for_single_exploration instead
     # for the case of a single exploration without a limit. Deprecate the
     # no-limit behavior of this method to avoid future issues.
