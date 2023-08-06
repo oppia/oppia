834089fe9a73728c3b9b16d65a32eb8460abcf27
Rishi <rishikejriwal2003@gmail.com>
2023-08-06 23:39:15 +0530

    Change navbar buttons spacing

--- core/templates/pages/story-editor-page/navbar/story-editor-navbar.component.spec.ts
+++ core/templates/pages/story-editor-page/navbar/story-editor-navbar.component.spec.ts
@@ -736,10 +736,10 @@ describe('Story editor navbar component', () => {
       storyEditorStateService, 'loadStory').and.returnValue();
     let alertServiceSpy = spyOn(
       alertsService, 'addInfoMessage').and.callThrough();
-    let success: boolean = false;
+    let successCallbackFunctionIsCalled: boolean = false;
     let saveStorySpy = spyOn(storyEditorStateService, 'saveStory')
       .and.callFake((commitMessage, successCallback, errorCallback) => {
-        if (success) {
+        if (successCallbackFunctionIsCalled) {
           successCallback();
         } else {
           errorCallback('Error');
@@ -760,7 +760,7 @@ describe('Story editor navbar component', () => {
       setStatus('Ready To Publish');
     component.story.getStoryContents().getNodes()[0].
       setFirstPublicationDateMsecs(null);
-    success = true;
+    successCallbackFunctionIsCalled = true;
 
     component.changeChapterStatus('Published');
     expect(storyNodeStatusSpy).toHaveBeenCalledTimes(3);
@@ -791,11 +791,11 @@ describe('Story editor navbar component', () => {
       storyEditorStateService, 'loadStory').and.returnValue();
     let alertServiceSpy = spyOn(
       alertsService, 'addInfoMessage').and.callThrough();
-    let success: boolean = false;
+    let successCallbackFunctionIsCalled: boolean = false;
     const saveStorySpy = spyOn(
       storyEditorStateService, 'saveStory')
       .and.callFake((commitMessage, successCallback, errorCallback) => {
-        if (success) {
+        if (successCallbackFunctionIsCalled) {
           successCallback();
         } else {
           errorCallback('Error');
@@ -826,7 +826,7 @@ describe('Story editor navbar component', () => {
     component.story.getStoryContents().getNodes()[1].setStatus('Published');
     component.story.getStoryContents().getNodes()[1].
       setPlannedPublicationDateMsecs(null);
-    success = true;
+    successCallbackFunctionIsCalled = true;
 
     component.changeChapterStatus('Published');
     tick();
