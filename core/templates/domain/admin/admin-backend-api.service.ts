4a704eecf6cd21dbd578f8e672e411ade0fbe42d
lkbhitesh07 <lkbhiteh07@gmail.com>
2023-09-11 11:24:53 +0530

    nit

--- core/templates/domain/admin/admin-backend-api.service.ts
+++ core/templates/domain/admin/admin-backend-api.service.ts
@@ -91,13 +91,6 @@ export interface ConfigProperty {
 
 export interface ConfigPropertyValues {
   'classroom_pages_data': ClassroomPageData;
-  'contributor_dashboard_reviewer_emails_is_enabled': boolean;
-  'enable_admin_notifications_for_reviewer_shortage': boolean;
-  'email_footer': string;
-  'email_sender_name': string;
-  'max_number_of_suggestions_per_reviewer': number;
-  'notification_user_ids_for_failed_tasks': string[];
-  'notify_admins_suggestions_waiting_too_long_is_enabled': boolean;
   'record_playthrough_probability': number;
 }
 
