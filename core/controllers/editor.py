cfb0b59996a561b08d2037aeed1bb5a83a4ab916
Adarsh Gupta <109406757+Adarsh7825@users.noreply.github.com>
2023-09-24 11:36:37 +0530

    Update editor.py

--- core/controllers/editor.py
+++ core/controllers/editor.py
@@ -198,8 +198,7 @@ class ExplorationHandler(
                 has_seen_editor_tutorial = True
             if user_settings.last_started_state_translation_tutorial:
                 has_seen_translation_tutorial = True
-
-        exploration_data = {}
+                
         try:
             exploration_data = exp_services.get_user_exploration_data(
                 self.user_id, exploration_id, apply_draft=apply_draft,
