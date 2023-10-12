dd4add43998153bc5bde662b2bbd415a34e93d41
hiramatsuyuusuke <140389350+hiramatsuyuusuke@users.noreply.github.com>
2023-10-12 17:48:08 +0900

    move updateOutline() into onSaveButtonClicked(), 8th.

--- core/templates/pages/story-editor-page/editor-tab/story-node-editor.component.ts
+++ core/templates/pages/story-editor-page/editor-tab/story-node-editor.component.ts
@@ -451,7 +451,7 @@ export class StoryNodeEditorComponent implements OnInit, OnDestroy {
       this.storyUpdateService.setStoryNodeOutline(
         this.story, this.nodeId, newOutline);
       this.oldOutline = newOutline;
-      this.outlineEditViewIsShown = this.oldOutline;
+      this.outlineEditViewIsShown = newOutline;
     }
   }
 
