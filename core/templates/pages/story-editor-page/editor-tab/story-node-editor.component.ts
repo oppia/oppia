ff2ed4cbbe58254b5e0923d9c39ff91cab836fa5
hiramatsuyuusuke <140389350+hiramatsuyuusuke@users.noreply.github.com>
2023-10-15 15:08:08 +0900

    remove ` $().text();`.

--- core/templates/pages/story-editor-page/editor-tab/story-node-editor.component.ts
+++ core/templates/pages/story-editor-page/editor-tab/story-node-editor.component.ts
@@ -496,7 +496,7 @@ export class StoryNodeEditorComponent implements OnInit, OnDestroy {
     this.storyUpdateService.setStoryNodeOutline(
       this.story, this.nodeId, newOutline);
     this.oldOutline = newOutline;
-    this.outlineEditViewIsShown = $(this.oldOutline).text();
+    this.outlineEditViewIsShown = this.oldOutline;
     this.chapterOutlineIsShown = false;
   }
 
