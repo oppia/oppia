e44814a3ce38e25758af59db249e1b06b500f85f
ShriyaKT <shriyaktarcar@gmail.com>
2023-10-30 20:51:01 +0530

    Updating by docstrings

--- core/controllers/questions_list.py
+++ core/controllers/questions_list.py
@@ -68,94 +68,72 @@ class QuestionsListHandler(
 
     @acl_decorators.open_access
     def get(self, comma_separated_skill_ids: str) -> None:
-        """Handles GET requests."""
-        assert self.normalized_request is not None
-        offset = self.normalized_request['offset']
+        """Handles GET requests.
 
-        skill_ids = list(set(comma_separated_skill_ids.split(',')))
+    Args:
+        comma_separated_skill_ids: A string containing comma-separated skill IDs.
 
-        try:
-            skill_fetchers.get_multi_skills(skill_ids)
-        except Exception as e:
-            raise self.PageNotFoundException(e)
+    Returns:
+        A JSON response containing the following fields:
+            * `question_summary_dicts`: A list of dictionaries, each containing the following fields:
+                * `summary`: A dictionary containing the question summary data.
+                * `skill_id`: The ID of the skill that the question is assigned to.
+                * `skill_description`: The description of the skill that the question is assigned to.
+                * `skill_difficulty`: The difficulty of the skill that the question is assigned to.
+            * `more`: A boolean value indicating whether there are more questions available to be loaded.
 
-        question_summaries, merged_question_skill_links = (
-            question_services.get_displayable_question_skill_link_details(
-                constants.NUM_QUESTIONS_PER_PAGE + 1, skill_ids, offset)
-        )
-
-        if len(question_summaries) <= constants.NUM_QUESTIONS_PER_PAGE:
-            more = False
-        else:
-            more = True
-            question_summaries.pop()
-            merged_question_skill_links.pop()
-
-        return_dicts = []
-        for index, summary in enumerate(question_summaries):
-            if summary is not None:
-                if len(skill_ids) == 1:
-                    return_dicts.append({
-                        'summary': summary.to_dict(),
-                        'skill_id': merged_question_skill_links[
-                            index].skill_ids[0],
-                        'skill_description': (
-                            merged_question_skill_links[
-                                index].skill_descriptions[0]),
-                        'skill_difficulty': (
-                            merged_question_skill_links[
-                                index].skill_difficulties[0])
-                    })
-                else:
-                    return_dicts.append({
-                        'summary': summary.to_dict(),
-                        'skill_ids': merged_question_skill_links[
-                            index].skill_ids,
-                        'skill_descriptions': (
-                            merged_question_skill_links[
-                                index].skill_descriptions),
-                        'skill_difficulties': (
-                            merged_question_skill_links[
-                                index].skill_difficulties)
-                    })
-
-        self.values.update({
-            'question_summary_dicts': return_dicts,
-            'more': more
-        })
-        self.render_json(self.values)
-
-
-class QuestionCountDataHandler(
-    base.BaseHandler[Dict[str, str], Dict[str, str]]
-):
-    """Provides data regarding the number of questions assigned to the given
-    skill ids.
+    Raises:
+        PageNotFoundException: If the specified skill IDs do not exist.
     """
 
-    GET_HANDLER_ERROR_RETURN_TYPE = feconf.HANDLER_TYPE_JSON
-    # TODO(#16538): Change the type of `comma_separated_skill_ids` url_path
-    # argument to `JsonEncodedInString`.
-    URL_PATH_ARGS_SCHEMAS = {
-        'comma_separated_skill_ids': {
-            'schema': {
-                'type': 'object_dict',
-                'validation_method': domain_objects_validator.validate_skill_ids
-            }
-        }
-    }
-    HANDLER_ARGS_SCHEMAS: Dict[str, Dict[str, str]] = {'GET': {}}
+    assert self.normalized_request is not None
+    offset = self.normalized_request['offset']
 
-    @acl_decorators.open_access
-    def get(self, comma_separated_skill_ids: str) -> None:
-        """Handles GET requests."""
-        skill_ids = list(set(comma_separated_skill_ids.split(',')))
+    skill_ids = list(set(comma_separated_skill_ids.split(',')))
 
-        total_question_count = (
-            question_services.get_total_question_count_for_skill_ids(skill_ids))
+    try:
+        skill_fetchers.get_multi_skills(skill_ids)
+    except Exception as e:
+        raise self.PageNotFoundException(e)
 
-        self.values.update({
-            'total_question_count': total_question_count
-        })
+    question_summaries, merged_question_skill_links = (
+        question_services.get_displayable_question_skill_link_details(
+            constants.NUM_QUESTIONS_PER_PAGE + 1, skill_ids, offset)
+    )
 
-        self.render_json(self.values)
+    if len(question_summaries) <= constants.NUM_QUESTIONS_PER_PAGE:
+        more = False
+    else:
+        more = True
+        question_summaries.pop()
+        merged_question_skill_links.pop()
+
+    return_dicts = []
+    for index, summary in enumerate(question_summaries):
+        if summary is not None:
+            if len(skill_ids) == 1:
+                return_dicts.append({
+                    'summary': summary.to_dict(),
+                    'skill_id': merged_question_skill_links[index].skill_ids[0],
+                    'skill_description': (
+                        merged_question_skill_links[index].skill_descriptions[0]),
+                    'skill_difficulty': (
+                        merged_question_skill_links[index].skill_difficulties[0])
+                })
+            else:
+                return_dicts.append({
+                    'summary': summary.to_dict(),
+                    'skill_ids': merged_question_skill_links[index].skill_ids,
+                    'skill_descriptions': (
+                        merged_question_skill_links[index].skill_descriptions),
+                    'skill_difficulties': (
+                        merged_question_skill_links[index].skill_difficulties)
+                })
+
+    self.values.update({
+        'question_summary_dicts': return_dicts,
+        'more': more
+    })
+
+    self.render_json(self.values)
+
