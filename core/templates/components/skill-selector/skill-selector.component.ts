abac824c4fbf0cb3b3c43510f3672f61cf6d3f80
Olivestam <anton.olivestam@gmail.com>
2023-10-31 21:10:35 +0100

    Added check so addedSkillIds array is not undefined to pass test

--- core/templates/components/skill-selector/skill-selector.component.ts
+++ core/templates/components/skill-selector/skill-selector.component.ts
@@ -196,7 +196,7 @@ export class SkillSelectorComponent implements OnInit {
 
   searchInUntriagedSkillSummaries(searchText: string): SkillSummary[] {
     let skills: string[] = this.untriagedSkillSummaries
-      .filter(val => !this.addedSkillsIds.includes(val.id))
+      .filter(val => !this.addedSkillsIds ? true : !this.addedSkillsIds.includes(val.id))
       .map(val => {
         return val.description;
       });
