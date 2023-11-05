f18d4785d19e7e68ec2c9e9db675f7a20daae12a
hiramatsuyuusuke <140389350+hiramatsuyuusuke@users.noreply.github.com>
2023-11-05 18:54:47 +0900

    Avoid hardcoding the `expect` block

--- core/tests/webdriverio_utils/StoryEditorPage.js
+++ core/tests/webdriverio_utils/StoryEditorPage.js
@@ -473,7 +473,7 @@ var StoryEditorPage = function() {
       await nodeOutlineEditorRteContentSelector());
     var outlineEditorRteContentText = (
       await nodeOutlineEditorRteContent[0].getText());
-    expect('<p>' + outlineEditorRteContentText + '</p>').toEqual(nodeOutline);
+    expect(outlineEditorRteContentText).toEqual(nodeOutline);
   };
 
   this.expectExplorationIdAlreadyExistWarning = async function() {
