62faf0ef7b8fe48583eacd35d784b784cc4afe87
Kapil Agarwal <kapilag96@gmail.com>
2023-11-17 12:52:09 +0530

    Fix backend tests and lint checks Fix backend tests and lint failures

--- core/domain/story_services_test.py
+++ core/domain/story_services_test.py
@@ -357,13 +357,16 @@ class StoryServicesUnitTests(test_utils.GenericTestBase):
             constants.STORY_NODE_STATUS_PUBLISHED)
         self.assertEqual(
             story.story_contents.nodes[1].
-            planned_publication_date, datetime.datetime(2023, 1, 2, 0, 0))
+            planned_publication_date,
+            datetime.datetime(2023, 1, 2, 0, 0, tzinfo=datetime.timezone.utc))
         self.assertEqual(
             story.story_contents.nodes[1].
-            first_publication_date, datetime.datetime(2023, 1, 1, 0, 0))
+            first_publication_date,
+            datetime.datetime(2023, 1, 1, 0, 0, tzinfo=datetime.timezone.utc))
         self.assertEqual(
             story.story_contents.nodes[1].
-            last_modified, datetime.datetime(2023, 1, 1, 0, 0))
+            last_modified,
+            datetime.datetime(2023, 1, 1, 0, 0, tzinfo=datetime.timezone.utc))
 
         story_summary = story_fetchers.get_story_summary_by_id(self.STORY_ID)
         self.assertEqual(story_summary.node_titles, ['Title 1', 'Title 2'])
