358618721677a677426ebb6c0403479262823460
PRAFUL BANDRE <109658545+prafulbbandre@users.noreply.github.com>
2023-11-18 22:58:30 +0530

    Update topic_services.py

--- core/domain/topic_services.py
+++ core/domain/topic_services.py
@@ -1736,9 +1736,9 @@ def get_chapter_counts_in_topic_summaries(
                 stories.append(story_id_mapping[story_reference.story_id])
             else:
                 logging.error(
-            'Topic %s has canonical story references' 
-            '%s but story reference %s is not a valid story.' 
-            % topic.id, topic.canonical_story_references, story_reference.story_id
+            'Topic %s has canonical story references ' 
+            '%s but story reference %s is not a valid story.' % (topic.id
+            ,topic.canonical_story_references, story_reference.story_id)
         )
         for story in stories:
             nodes = story.story_contents.nodes
