9581abeff91969b6ef8482705dd36681df0ff6e6
Olivestam <anton.olivestam@gmail.com>
2023-11-21 22:28:38 +0100

    Fixed import specification and removed not null check for skillIdsToExclude

--- core/templates/components/skill-selector/skill-selector.component.ts
+++ core/templates/components/skill-selector/skill-selector.component.ts
@@ -48,7 +48,7 @@ export class SkillSelectorComponent implements OnInit {
   @Input() categorizedSkills!: CategorizedSkills;
   @Input() untriagedSkillSummaries!: SkillSummary[];
   @Input() allowSkillsFromOtherTopics!: boolean;
-  @Input() skillIdsToExclude!: string[];
+  @Input() skillIdsToExclude!: { [key: string]: boolean };
   @Output() selectedSkillIdChange: EventEmitter<string> = new EventEmitter();
   currCategorizedSkills!: CategorizedSkills;
   selectedSkill!: string;
@@ -196,10 +196,7 @@ export class SkillSelectorComponent implements OnInit {
 
   searchInUntriagedSkillSummaries(searchText: string): SkillSummary[] {
     let skills: string[] = this.untriagedSkillSummaries
-      .filter(
-        val => !this.skillIdsToExclude ? true :
-        !this.skillIdsToExclude.hasOwnProperty(val.id)
-      )
+      .filter(val => !this.skillIdsToExclude.hasOwnProperty(val.id))
       .map(val => {
         return val.description;
       });
