oppia/oppia

commit 723bc3693551d975e8020b4f0bcd65df094ecb51

fix part of 16458: support topic filtering for questions on backend (#19146)
* fix part of 16458: support topic filter for questions on backend

* polish code

* rework the test

* increase test coverage

* None indicates no filtering needed

* fix indentation

* add a test

* fix linter error

* fix a test

* fix linter errors

* apply review suggestion

Co-authored-by: Vojtěch Jelínek <vojtech.jelinek@hey.com>

* apply review suggestion

Co-authored-by: Hasitha Kaushan <41409951+chris7716@users.noreply.github.com>

---------

Co-authored-by: Vojtěch Jelínek <vojtech.jelinek@hey.com>
Co-authored-by: Hasitha Kaushan <41409951+chris7716@users.noreply.github.com>
Co-authored-by: Sean Lip <sean@seanlip.org>

core/storage/suggestion/gae_models.py
@@ -870,7 +870,8 @@ class GeneralSuggestionModel(base_models.BaseModel):
         limit: int,
         offset: int,
         user_id: str,
-        sort_key: Optional[str]
+        sort_key: Optional[str],
+        skill_ids: Optional[List[str]],
     ) -> Tuple[Sequence[GeneralSuggestionModel], int]:
         """Fetches question suggestions that are in-review and not authored by
         the supplied user.
@@ -883,6 +884,8 @@ class GeneralSuggestionModel(base_models.BaseModel):
                 user cannot review their own suggestions, suggestions authored
                 by the user will be excluded.
             sort_key: str|None. The key to sort the suggestions by.
+            skill_ids: List[str] | None. The skills for which to return
+                question suggestions. None for returning all suggestions.
 
         Returns:
             Tuple of (results, next_offset). Where:
@@ -891,17 +894,29 @@ class GeneralSuggestionModel(base_models.BaseModel):
                     one of the supplied language codes.
                 next_offset: int. The input offset + the number of results
                     returned by the current query.
+
+        Raises:
+            RuntimeError. If skill_ids is empty.
         """
+
+        filters = [
+            cls.status == STATUS_IN_REVIEW,
+            cls.suggestion_type == feconf.SUGGESTION_TYPE_ADD_QUESTION,
+        ]
+
+        if skill_ids is not None:
+            # If this is not filtered here, gae throws BadQueryError.
+            if len(skill_ids) == 0:
+                raise RuntimeError('skill_ids list can\'t be empty')
+
+            filters.append(cls.target_id.IN(skill_ids))
 
         if sort_key == constants.SUGGESTIONS_SORT_KEY_DATE:
             # The first sort property must be the same as the property to which
             # an inequality filter is applied. Thus, the inequality filter on
             # author_id can not be used here.
             suggestion_query = cls.get_all().filter(
-                datastore_services.all_of(
-                    cls.status == STATUS_IN_REVIEW,
-                    cls.suggestion_type == feconf.SUGGESTION_TYPE_ADD_QUESTION,
-            )).order(-cls.created_on)
+                datastore_services.all_of(*filters)).order(-cls.created_on)
 
             sorted_results: List[GeneralSuggestionModel] = []
             num_suggestions_per_fetch = 1000
@@ -924,11 +939,9 @@ class GeneralSuggestionModel(base_models.BaseModel):
                 offset
             )
 
-        suggestion_query = cls.get_all().filter(datastore_services.all_of(
-            cls.status == STATUS_IN_REVIEW,
-            cls.suggestion_type == feconf.SUGGESTION_TYPE_ADD_QUESTION,
-            cls.author_id != user_id
-        ))
+        filters.append(cls.author_id != user_id)
+        suggestion_query = cls.get_all().filter(
+            datastore_services.all_of(*filters))
 
         results: Sequence[GeneralSuggestionModel] = (
             suggestion_query.fetch(limit, offset=offset)

core/storage/suggestion/gae_models_test.py
@@ -885,96 +885,143 @@ class SuggestionModelUnitTests(test_utils.GenericTestBase):
         suggestion_models.GeneralSuggestionModel.create(
             feconf.SUGGESTION_TYPE_ADD_QUESTION,
             feconf.ENTITY_TYPE_SKILL,
-            'skill_1', self.target_version_at_submission,
+            'skill_2', self.target_version_at_submission,
             suggestion_models.STATUS_IN_REVIEW, 'author_4',
             'reviewer_2', self.change_cmd, 'category1',
             suggestion_2_id, self.question_language_code)
         suggestion_models.GeneralSuggestionModel.create(
             feconf.SUGGESTION_TYPE_ADD_QUESTION,
             feconf.ENTITY_TYPE_SKILL,
-            'skill_1', self.target_version_at_submission,
+            'skill_3', self.target_version_at_submission,
             suggestion_models.STATUS_IN_REVIEW, 'author1',
             'reviewer_2', self.change_cmd, 'category1',
             suggestion_3_id, self.question_language_code)
 
-        results, offset_1 = (
+        results, offset = (
             suggestion_models.GeneralSuggestionModel
             .get_in_review_question_suggestions_by_offset(
                 limit=limit,
                 offset=0,
                 user_id=user_id,
-                sort_key=None))
+                sort_key=None,
+                skill_ids=None))
         # Ruling out the possibility of None for mypy type checking.
         assert results is not None
         self.assertEqual(len(results), limit)
         self.assertEqual(results[0].id, suggestion_1_id)
-        self.assertEqual(offset_1, 1)
+        self.assertEqual(offset, 1)
+        prev_offset = offset
 
-        results, offset_2 = (
+        results, offset = (
             suggestion_models.GeneralSuggestionModel
             .get_in_review_question_suggestions_by_offset(
                 limit=limit,
-                offset=offset_1,
+                offset=prev_offset,
                 user_id=user_id,
-                sort_key=None))
+                sort_key=None,
+                skill_ids=None))
         # Ruling out the possibility of None for mypy type checking.
         assert results is not None
         self.assertEqual(len(results), limit)
         self.assertEqual(results[0].id, suggestion_2_id)
-        self.assertEqual(offset_2, 2)
+        self.assertEqual(offset, 2)
+        prev_offset = offset
 
-        results, offset_3 = (
+        results, offset = (
             suggestion_models.GeneralSuggestionModel
             .get_in_review_question_suggestions_by_offset(
                 limit=limit,
-                offset=offset_2,
+                offset=prev_offset,
                 user_id=user_id,
-                sort_key=None))
+                sort_key=None,
+                skill_ids=None))
         # Ruling out the possibility of None for mypy type checking.
         assert results is not None
         self.assertEqual(len(results), 0)
-        self.assertEqual(offset_3, 2)
+        self.assertEqual(offset, 2)
 
-        sorted_results, offset_4 = (
+        results, offset = (
+            suggestion_models.GeneralSuggestionModel
+            .get_in_review_question_suggestions_by_offset(
+                limit=limit,
+                offset=0,
+                user_id=user_id,
+                sort_key=None,
+                skill_ids=['skill_2', 'skill_3']))
+        # Ruling out the possibility of None for mypy type checking.
+        assert results is not None
+        self.assertEqual(len(results), 1)
+        self.assertEqual(results[0].id, suggestion_2_id)
+
+        sorted_results, offset = (
             suggestion_models.GeneralSuggestionModel
             .get_in_review_question_suggestions_by_offset(
                 limit=1,
                 offset=0,
                 user_id=user_id,
-                sort_key=constants.SUGGESTIONS_SORT_KEY_DATE))
+                sort_key=constants.SUGGESTIONS_SORT_KEY_DATE,
+                skill_ids=None))
         # Ruling out the possibility of None for mypy type checking.
         assert sorted_results is not None
         self.assertEqual(len(sorted_results), 1)
         self.assertEqual(sorted_results[0].id, suggestion_2_id)
-        self.assertEqual(offset_4, 2)
+        self.assertEqual(offset, 2)
 
-        sorted_results, offset_5 = (
+        sorted_results, offset = (
             suggestion_models.GeneralSuggestionModel
             .get_in_review_question_suggestions_by_offset(
                 limit=2,
                 offset=0,
                 user_id=user_id,
-                sort_key=constants.SUGGESTIONS_SORT_KEY_DATE))
+                sort_key=constants.SUGGESTIONS_SORT_KEY_DATE,
+                skill_ids=None))
         # Ruling out the possibility of None for mypy type checking.
         assert sorted_results is not None
         self.assertEqual(len(sorted_results), 2)
         self.assertEqual(sorted_results[0].id, suggestion_2_id)
         self.assertEqual(sorted_results[1].id, suggestion_1_id)
-        self.assertEqual(offset_5, 3)
+        self.assertEqual(offset, 3)
 
-        sorted_results, offset_6 = (
+        sorted_results, offset = (
             suggestion_models.GeneralSuggestionModel
             .get_in_review_question_suggestions_by_offset(
                 limit=10,
                 offset=0,
                 user_id=user_id,
-                sort_key=constants.SUGGESTIONS_SORT_KEY_DATE))
+                sort_key=constants.SUGGESTIONS_SORT_KEY_DATE,
+                skill_ids=None))
         # Ruling out the possibility of None for mypy type checking.
         assert sorted_results is not None
         self.assertEqual(len(sorted_results), 2)
         self.assertEqual(sorted_results[0].id, suggestion_2_id)
         self.assertEqual(sorted_results[1].id, suggestion_1_id)
-        self.assertEqual(offset_6, 3)
+        self.assertEqual(offset, 3)
+
+        sorted_results, offset = (
+            suggestion_models.GeneralSuggestionModel
+            .get_in_review_question_suggestions_by_offset(
+                limit=10,
+                offset=0,
+                user_id=user_id,
+                sort_key=constants.SUGGESTIONS_SORT_KEY_DATE,
+                skill_ids=['skill_1', 'skill_3']))
+        # Ruling out the possibility of None for mypy type checking.
+        assert sorted_results is not None
+        self.assertEqual(len(sorted_results), 1)
+        self.assertEqual(sorted_results[0].id, suggestion_1_id)
+        self.assertEqual(offset, 2)
+
+        with self.assertRaisesRegex(
+            RuntimeError,
+            'skill_ids list can\'t be empty'):
+            (
+                suggestion_models.GeneralSuggestionModel
+                .get_in_review_question_suggestions_by_offset(
+                    limit=10,
+                    offset=0,
+                    user_id=user_id,
+                    sort_key=constants.SUGGESTIONS_SORT_KEY_DATE,
+                    skill_ids=[]))
 
     def test_user_created_suggestions_by_offset(self) -> None:
         authored_translation_suggestion_id = 'exploration.exp1.thread_6'

core/controllers/suggestion.py
@@ -37,6 +37,7 @@ from core.domain import skill_fetchers
 from core.domain import state_domain
 from core.domain import suggestion_registry
 from core.domain import suggestion_services
+from core.domain import topic_fetchers
 from core.domain import translation_domain
 from core.domain import user_services
 
@@ -690,6 +691,7 @@ class ReviewableSuggestionsHandlerNormalizedRequestDict(TypedDict):
     offset: int
     sort_key: str
     exploration_id: Optional[str]
+    topic_name: Optional[str]
 
 
 class ReviewableSuggestionsHandler(
@@ -747,9 +749,33 @@ class ReviewableSuggestionsHandler(
                     'type': 'basestring'
                 },
                 'default_value': None
-            }
+            },
+            'topic_name': {
+                'schema': {
+                    'type': 'basestring'
+                },
+                'default_value': None
+            },
         }
     }
+
+    def _get_skill_ids_for_topic(
+            self, topic_name: Optional[str]
+    ) -> Optional[List[str]]:
+        """Gets all skill ids for the provided topic.
+
+        Returns None to indicate that no filtering is needed.
+        """
+        if (
+            topic_name is None or
+            topic_name == constants.TOPIC_SENTINEL_NAME_ALL
+        ):
+            return None
+        topic = topic_fetchers.get_topic_by_name(topic_name)
+        if topic is None:
+            raise self.InvalidInputException(
+                f'The topic \'{topic_name}\' is not valid')
+        return topic.get_all_skill_ids()
 
     @acl_decorators.can_view_reviewable_suggestions
     def get(self, target_type: str, suggestion_type: str) -> None:
@@ -804,11 +830,13 @@ class ReviewableSuggestionsHandler(
             if limit is None:
                 raise ValueError(
                     'Limit must be provided for question suggestions.')
+            topic_name = self.normalized_request.get('topic_name')
+            skill_ids = self._get_skill_ids_for_topic(topic_name)
 
             suggestions, next_offset = (
                 suggestion_services
                 .get_reviewable_question_suggestions_by_offset(
-                    self.user_id, limit, offset, sort_key))
+                    self.user_id, limit, offset, sort_key, skill_ids))
         self._render_suggestions(target_type, suggestions, next_offset)
 
 

core/controllers/suggestion_test.py
@@ -2948,11 +2948,11 @@ class ReviewableSuggestionsHandlerTest(test_utils.GenericTestBase):
         topic.thumbnail_bg_color = '#C6DCDA'
         topic.subtopics = [
             topic_domain.Subtopic(
-                1, 'Title', ['skill_id_333'], 'image.svg',
+                1, 'Title', [self.SKILL_ID], 'image.svg',
                 constants.ALLOWED_THUMBNAIL_BG_COLORS['subtopic'][0], 21131,
                 'dummy-subtopic-three')]
         topic.next_subtopic_id = 2
-        topic.skill_ids_for_diagnostic_test = ['skill_id_333']
+        topic.skill_ids_for_diagnostic_test = [self.SKILL_ID]
         topic_services.save_new_topic(self.owner_id, topic)
         topic_services.publish_topic(self.TOPIC_ID, self.admin_id)
 
@@ -3046,10 +3046,20 @@ class ReviewableSuggestionsHandlerTest(test_utils.GenericTestBase):
                 'suggestion_type': (
                     feconf.SUGGESTION_TYPE_ADD_QUESTION),
                 'target_type': feconf.ENTITY_TYPE_SKILL,
+                'target_id': 'skill_without_topic',
+                'target_version_at_submission': 1,
+                'change': self.translate_question_change,
+                'description': 'Add question for a skill without topic'
+            }, csrf_token=csrf_token)
+        self.post_json(
+            '%s/' % feconf.SUGGESTION_URL_PREFIX, {
+                'suggestion_type': (
+                    feconf.SUGGESTION_TYPE_ADD_QUESTION),
+                'target_type': feconf.ENTITY_TYPE_SKILL,
                 'target_id': self.SKILL_ID,
                 'target_version_at_submission': 1,
                 'change': self.translate_question_change,
-                'description': 'Add new question to skill'
+                'description': 'Add new question for a skill with topic'
             }, csrf_token=csrf_token)
 
         self.logout()
@@ -3124,9 +3134,9 @@ class ReviewableSuggestionsHandlerTest(test_utils.GenericTestBase):
             '/getreviewablesuggestions/skill/add_question', {
                 'limit': constants.OPPORTUNITIES_PAGE_SIZE,
                 'offset': 0,
-                'sort_key': constants.SUGGESTIONS_SORT_KEY_DATE
+                'sort_key': constants.SUGGESTIONS_SORT_KEY_DATE,
             })
-        self.assertEqual(len(response['suggestions']), 1)
+        self.assertEqual(len(response['suggestions']), 2)
         suggestion = response['suggestions'][0]
         self.assertDictEqual(
             suggestion['change'], self.translate_question_change)
@@ -3139,27 +3149,47 @@ class ReviewableSuggestionsHandlerTest(test_utils.GenericTestBase):
         self.assertEqual(suggestion['author_name'], 'author')
         self.assertEqual(suggestion['status'], 'review')
         self.assertDictEqual(
-            response['target_id_to_opportunity_dict'],
+            response['target_id_to_opportunity_dict'][self.SKILL_ID],
             {
-                'skill1234567': {
-                    'id': 'skill1234567',
-                    'question_count': 0,
-                    'skill_description': 'skill to link question to',
-                    'skill_rubrics': [
-                        {
-                            'difficulty': 'Easy',
-                            'explanations': ['Explanation 1']
-                        }, {
-                            'difficulty': 'Medium',
-                            'explanations': ['Explanation 2']
-                        }, {
-                            'difficulty': 'Hard',
-                            'explanations': ['Explanation 3']
-                        }
-                    ]
-                }
+                'id': self.SKILL_ID,
+                'question_count': 0,
+                'skill_description': 'skill to link question to',
+                'skill_rubrics': [
+                    {
+                        'difficulty': 'Easy',
+                        'explanations': ['Explanation 1']
+                    }, {
+                        'difficulty': 'Medium',
+                        'explanations': ['Explanation 2']
+                    }, {
+                        'difficulty': 'Hard',
+                        'explanations': ['Explanation 3']
+                    }
+                ]
             }
         )
+
+    def test_skill_handler_with_topic_filter_returns_one_question(self) -> None:
+        response = self.get_json(
+            '/getreviewablesuggestions/skill/add_question', {
+                'limit': constants.OPPORTUNITIES_PAGE_SIZE,
+                'offset': 0,
+                'sort_key': constants.SUGGESTIONS_SORT_KEY_DATE,
+                'topic_name': self.TOPIC_ID,
+            })
+        self.assertEqual(len(response['suggestions']), 1)
+
+    def test_skill_handler_with_all_topics_filter_returns_one_question(
+            self
+        ) -> None:
+        response = self.get_json(
+            '/getreviewablesuggestions/skill/add_question', {
+                'limit': constants.OPPORTUNITIES_PAGE_SIZE,
+                'offset': 0,
+                'sort_key': constants.SUGGESTIONS_SORT_KEY_DATE,
+                'topic_name': 'All',
+            })
+        self.assertEqual(len(response['suggestions']), 2)
 
     def test_topic_question_handler_returns_no_data(self) -> None:
         response = self.get_json(
@@ -3169,6 +3199,17 @@ class ReviewableSuggestionsHandlerTest(test_utils.GenericTestBase):
                 'sort_key': constants.SUGGESTIONS_SORT_KEY_DATE
             })
         self.assertEqual(response, {})
+
+    def test_handler_with_non_existent_topic_raise_error(self) -> None:
+        self.get_json(
+            '/getreviewablesuggestions/topic/add_question', {
+                'limit': constants.OPPORTUNITIES_PAGE_SIZE,
+                'offset': 0,
+                'sort_key': constants.SUGGESTIONS_SORT_KEY_DATE,
+                'topic_name': 'non_existent_topic'
+            },
+            expected_status_int=400
+        )
 
     def test_handler_with_invalid_suggestion_type_raise_error(self) -> None:
         self.get_json(

core/domain/suggestion_services.py
@@ -1157,7 +1157,8 @@ def get_reviewable_question_suggestions_by_offset(
     user_id: str,
     limit: int,
     offset: int,
-    sort_key: Optional[str]
+    sort_key: Optional[str],
+    skill_ids: Optional[List[str]],
 ) -> Tuple[List[suggestion_registry.SuggestionAddQuestion], int]:
     """Returns a list of question suggestions which the user
        can review.
@@ -1168,6 +1169,8 @@ def get_reviewable_question_suggestions_by_offset(
         offset: int. The number of results to skip from the beginning of all
             results matching the query.
         sort_key: str|None. The key to sort the suggestions by.
+        skill_ids: List[str]|None. The skills for which to return question
+            suggestions. None for returning all suggestions.
 
     Returns:
         Tuple of (results, next_offset). Where:
@@ -1179,7 +1182,7 @@ def get_reviewable_question_suggestions_by_offset(
     suggestions, next_offset = (
         suggestion_models.GeneralSuggestionModel
         .get_in_review_question_suggestions_by_offset(
-            limit, offset, user_id, sort_key))
+            limit, offset, user_id, sort_key, skill_ids))
 
     question_suggestions = []
     for suggestion_model in suggestions:

core/domain/suggestion_services_test.py
@@ -1860,6 +1860,7 @@ class SuggestionGetServicesUnitTests(test_utils.GenericTestBase):
         # Add a few question suggestions.
         self._create_question_suggestion_with_skill_id('skill1')
         self._create_question_suggestion_with_skill_id('skill2')
+        self._create_question_suggestion_with_skill_id('skill3')
         # Provide the user permission to review suggestions in particular
         # languages.
         user_services.allow_user_to_review_translation_in_language(
@@ -1875,7 +1876,8 @@ class SuggestionGetServicesUnitTests(test_utils.GenericTestBase):
                 self.reviewer_id_1,
                 limit=constants.OPPORTUNITIES_PAGE_SIZE,
                 offset=0,
-                sort_key=constants.SUGGESTIONS_SORT_KEY_DATE))
+                sort_key=constants.SUGGESTIONS_SORT_KEY_DATE,
+                skill_ids=['skill1', 'skill2']))
 
         # Expect that the results correspond to question suggestions.
         self.assertEqual(len(suggestions), 2)

commit 557e42c4efde00a71d8f1f3fa1125f6aeb123a52

fix part of 16458: fix a gae problem introduced in #19146

core/storage/suggestion/gae_models.py
@@ -909,8 +909,6 @@ class GeneralSuggestionModel(base_models.BaseModel):
             if len(skill_ids) == 0:
                 raise RuntimeError('skill_ids list can\'t be empty')
 
-            filters.append(cls.target_id.IN(skill_ids))
-
         if sort_key == constants.SUGGESTIONS_SORT_KEY_DATE:
             # The first sort property must be the same as the property to which
             # an inequality filter is applied. Thus, the inequality filter on
@@ -929,10 +927,14 @@ class GeneralSuggestionModel(base_models.BaseModel):
                     break
                 for suggestion_model in suggestion_models:
                     offset += 1
-                    if suggestion_model.author_id != user_id:
-                        sorted_results.append(suggestion_model)
-                        if len(sorted_results) == limit:
-                            break
+                    if suggestion_model.author_id == user_id:
+                        continue
+                    if (skill_ids is not None and
+                        suggestion_model.target_id not in skill_ids):
+                        continue
+                    sorted_results.append(suggestion_model)
+                    if len(sorted_results) == limit:
+                        break
 
             return (
                 sorted_results,

core/storage/suggestion/gae_models_test.py
@@ -940,19 +940,6 @@ class SuggestionModelUnitTests(test_utils.GenericTestBase):
         self.assertEqual(len(results), 0)
         self.assertEqual(offset, 2)
 
-        results, offset = (
-            suggestion_models.GeneralSuggestionModel
-            .get_in_review_question_suggestions_by_offset(
-                limit=limit,
-                offset=0,
-                user_id=user_id,
-                sort_key=None,
-                skill_ids=['skill_2', 'skill_3']))
-        # Ruling out the possibility of None for mypy type checking.
-        assert results is not None
-        self.assertEqual(len(results), 1)
-        self.assertEqual(results[0].id, suggestion_2_id)
-
         sorted_results, offset = (
             suggestion_models.GeneralSuggestionModel
             .get_in_review_question_suggestions_by_offset(
@@ -1009,7 +996,7 @@ class SuggestionModelUnitTests(test_utils.GenericTestBase):
         assert sorted_results is not None
         self.assertEqual(len(sorted_results), 1)
         self.assertEqual(sorted_results[0].id, suggestion_1_id)
-        self.assertEqual(offset, 2)
+        self.assertEqual(offset, 3)
 
         with self.assertRaisesRegex(
             RuntimeError,