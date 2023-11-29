4b3db96e2f3e90a14c8402acb0bad5d9b12e48ab
Adarsh Gupta <csedsadarsh@gmail.com>
2023-11-29 22:26:00 +0530

    resolve issue in develop branch

--- core/templates/pages/topic-viewer-page/topic-viewer-page.component.ts
+++ core/templates/pages/topic-viewer-page/topic-viewer-page.component.ts
@@ -129,7 +129,7 @@ export class TopicViewerPageComponent implements OnInit, OnDestroy {
       },
       errorResponse => {
         let errorCodes = AppConstants.FATAL_ERROR_CODES;
-        if (errorResponse && errorCodes.indexOf(errorResponse.status) !== -1) {
+        if (errorCodes.indexOf(errorResponse.status) !== -1) {
           this.alertsService.addWarning('Failed to get dashboard data');
         }
       }
