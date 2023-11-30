bdd8a66a5a7753f153707b5b08ab19de4ecc11ec
Kapil Agarwal <kapilag96@gmail.com>
2023-11-30 10:28:57 +0800

    Fix donate page tests

--- core/templates/pages/donate-page/donate-page.component.spec.ts
+++ core/templates/pages/donate-page/donate-page.component.spec.ts
@@ -16,22 +16,16 @@
  * @fileoverview Unit tests for donate page.
  */
 
-import { TestBed, fakeAsync, tick, flushMicrotasks } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { DonatePageComponent } from './donate-page.component';
-import { SiteAnalyticsService } from 'services/site-analytics.service';
-import { UrlInterpolationService } from
-  'domain/utilities/url-interpolation.service';
-import { WindowDimensionsService } from
-  'services/contextual/window-dimensions.service';
+import { UrlInterpolationService } from 'domain/utilities/url-interpolation.service';
 import { WindowRef } from 'services/contextual/window-ref.service';
 import { MockTranslatePipe } from 'tests/unit-test-utils';
-import { MailingListBackendApiService } from 'domain/mailing-list/mailing-list-backend-api.service';
-import { AlertsService } from 'services/alerts.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { FormsModule } from '@angular/forms';
+import { DonationBoxModalComponent } from './donation-box/donation-box-modal.component';
+import { ThanksForDonatingModalComponent } from './thanks-for-donating-modal.component';
 
 class MockWindowRef {
   _window = {
@@ -56,129 +50,38 @@ class MockWindowRef {
 }
 
 describe('Donate page', () => {
-  const siteAnalyticsServiceStub = new SiteAnalyticsService(
-    new WindowRef());
+  let component: DonatePageComponent;
   let windowRef: MockWindowRef;
-  let mailingListBackendApiService: MailingListBackendApiService;
-  let alertsService: AlertsService;
   let ngbModal: NgbModal;
 
-  beforeEach(async() => {
+  beforeEach(() => {
     windowRef = new MockWindowRef();
     TestBed.configureTestingModule({
-      imports: [
-        FormsModule,
-        HttpClientTestingModule
-      ],
       declarations: [
         DonatePageComponent,
         MockTranslatePipe
       ],
       providers: [
-        {provide: SiteAnalyticsService, useValue: siteAnalyticsServiceStub},
         UrlInterpolationService,
-        {
-          provide: WindowDimensionsService,
-          useValue: {
-            isWindowNarrow: () => true
-          }
-        },
         { provide: WindowRef, useValue: windowRef },
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
   });
 
-  let component: DonatePageComponent;
-
   beforeEach(() => {
     const donatePageComponent = TestBed.createComponent(DonatePageComponent);
     component = donatePageComponent.componentInstance;
-    alertsService = TestBed.inject(AlertsService);
-    mailingListBackendApiService = TestBed.inject(
-      MailingListBackendApiService);
     ngbModal = TestBed.inject(NgbModal);
     spyOn(ngbModal, 'open');
   });
 
-  it('should set component properties when ngOnInit() is called', () => {
-    component.ngOnInit();
-
-    expect(component.windowIsNarrow).toBe(true);
-    expect(component.donateImgUrl).toBe(
-      '/assets/images/general/opp_donate_text.svg');
-  });
-
-  it('should validate email address correctly', () => {
-    component.emailAddress = 'invalidEmail';
-    expect(component.validateEmailAddress()).toBeFalse();
-
-    component.emailAddress = 'validEmail@example.com';
-    expect(component.validateEmailAddress()).toBeTrue();
-  });
-
-  it('should add user to mailing list and return status',
-    fakeAsync(() => {
-      spyOn(alertsService, 'addInfoMessage');
-      component.ngOnInit();
-      tick();
-      component.emailAddress = 'validEmail@example.com';
-      component.name = 'validName';
-      spyOn(mailingListBackendApiService, 'subscribeUserToMailingList')
-        .and.returnValue(Promise.resolve(true));
-
-      component.subscribeToMailingList();
-
-      flushMicrotasks();
-
-      expect(alertsService.addInfoMessage).toHaveBeenCalledWith(
-        'Done!', 1000);
-    }));
-
-  it('should fail to add user to mailing list and return status',
-    fakeAsync(() => {
-      spyOn(alertsService, 'addInfoMessage');
-      component.ngOnInit();
-      tick();
-      component.emailAddress = 'validEmail@example.com';
-      component.name = 'validName';
-      spyOn(mailingListBackendApiService, 'subscribeUserToMailingList')
-        .and.returnValue(Promise.resolve(false));
-
-      component.subscribeToMailingList();
-
-      flushMicrotasks();
-
-      expect(alertsService.addInfoMessage).toHaveBeenCalledWith(
-        'Sorry, an unexpected error occurred. Please email admin@oppia.org ' +
-        'to be added to the mailing list.', 10000);
-    }));
-
-  it('should reject request to the mailing list correctly',
-    fakeAsync(() => {
-      spyOn(alertsService, 'addInfoMessage');
-      component.ngOnInit();
-      tick();
-      component.emailAddress = 'validEmail@example.com';
-      component.name = 'validName';
-      spyOn(mailingListBackendApiService, 'subscribeUserToMailingList')
-        .and.returnValue(Promise.reject(false));
-
-      component.subscribeToMailingList();
-
-      flushMicrotasks();
-
-      expect(alertsService.addInfoMessage).toHaveBeenCalledWith(
-        'Sorry, an unexpected error occurred. Please email admin@oppia.org ' +
-        'to be added to the mailing list.', 10000);
-    }));
-
-  it('should get image set', () => {
+  it('should get image path', () => {
     spyOn(component, 'getStaticImageUrl');
 
-    component.getImageSet('abc', 'png');
+    component.getStaticImageUrl('abc.webp');
 
-    expect(component.getStaticImageUrl).toHaveBeenCalled();
+    expect(component.getStaticImageUrl).toHaveBeenCalledWith('abc.webp');
   });
 
   it('should show thank you modal on query parameters change', () => {
@@ -192,6 +95,24 @@ describe('Donate page', () => {
 
     windowRef.nativeWindow.location.search = '?thanks';
     component.ngOnInit();
-    expect(ngbModal.open).toHaveBeenCalled();
+    expect(ngbModal.open).toHaveBeenCalledWith(
+      ThanksForDonatingModalComponent,
+      {
+        backdrop: 'static',
+        size: 'xl',
+      }
+    );
+  });
+
+  it('should open donation box modal', () => {
+    component.openDonationBoxModal();
+
+    expect(ngbModal.open).toHaveBeenCalledWith(
+      DonationBoxModalComponent, {
+        backdrop: 'static',
+        size: 'xl',
+        windowClass: 'donation-box-modal',
+      }
+    );
   });
 });
