f30dee28c094393b3d980e461ae0f30b2c462fad
David Envall <daven009@student.liu.se>
2023-12-14 21:37:18 +0100

    more some()

--- core/templates/services/rte-helper-modal.controller.ts
+++ core/templates/services/rte-helper-modal.controller.ts
@@ -293,20 +293,19 @@ export class RteHelperModalComponent {
       // First check if the `text` looks like a URL.
       const suffixes = ['.com', '.org', '.edu', '.gov'];
       let textLooksLikeUrl = suffixes.some(suffix => text.endsWith(suffix));
-
       if (!textLooksLikeUrl) {
         this.saveButtonIsDisabled = false;
       } else {
       // If the text looks like a URL, strip the leading 'http://' or
       // 'https://' or 'www.'.
         const prefixes = ['https://', 'http://', 'www.'];
-        for (const prefix of prefixes) {
-          if (url.startsWith(prefix)) {
-            url = url.substring(prefix.length);
-          }
-          if (text.startsWith(prefix)) {
-            text = text.substring(prefix.length);
-          }
+        if (prefixes.some(prefix => url.startsWith(prefix))) {
+          const urlPrefix = prefixes.find(prefix => url.startsWith(prefix));
+          url = url.substring(urlPrefix.length);
+        }
+        if (prefixes.some(prefix => text.startsWith(prefix))) {
+          const textPrefix = prefixes.find(prefix => text.startsWith(prefix));
+          text = text.substring(textPrefix.length);
         }
         // After the cleanup, if the strings are not equal, then we do not
         // allow the lesson creator to save it.
