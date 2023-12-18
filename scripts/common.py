2bcfe502851b40000f02a4a4bb4d27746deee377
Lawful2002 <sharshvardhan101@gmail.com>
2023-12-18 21:32:11 +0530

    upgraded protobuf

--- scripts/common.py
+++ scripts/common.py
@@ -60,7 +60,7 @@ YARN_VERSION = '1.22.15'
 BUF_VERSION = '0.29.0'
 
 # Must match the version of protobuf in requirements_dev.in.
-PROTOC_VERSION = '3.13.0'
+PROTOC_VERSION = '3.18.3'
 
 # IMPORTANT STEPS FOR DEVELOPERS TO UPGRADE REDIS:
 # 1. Download the new version of the redis cli.
