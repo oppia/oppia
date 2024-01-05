d903881d1485165794a731e35bd296541a97c67b
Nik-09 <nikhil.agarwal.2019@gmail.com>
2024-01-05 13:35:53 +0530

    Creates voiceover model

---
+++ core/storage/voiceover/gae_models_test.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+#
+# Copyright 2021 The Oppia Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for EntityVoiceoversModel models."""
+
+from __future__ import annotations
+
+from core import feconf
+from core.platform import models
+from core.tests import test_utils
+
+MYPY = False
+if MYPY: # pragma: no cover
+    from mypy_imports import base_models
+    from mypy_imports import voiceover_models
+
+(base_models, voiceover_models) = models.Registry.import_models([
+    models.Names.BASE_MODEL, models.Names.TRANSLATION
+])
+
+
+class EntityVoiceoversModelTest(test_utils.GenericEmailTestBase):
+    """Unit tests for EntityVoiceoversModel class."""
