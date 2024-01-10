40e065e14ab27b437fd857bcfb486f27ebc155b2
Ash-2k3 <ash.me.cse@gmail.com>
2024-01-10 19:43:49 +0530

    Fix mypy checks

--- core/controllers/cron.py
+++ core/controllers/cron.py
@@ -251,7 +251,8 @@ class CronMailReviewerNewSuggestionsHandler(
             reviewer_ids = [
                 user_services.get_user_id_from_username(
                     username) for username in reviewer_usernames]
-            filtered_reviewer_ids = [id for id in reviewer_ids if id is not None]
+            filtered_reviewer_ids = [
+                id for id in reviewer_ids if id is not None]
             reviewer_ids_by_language[language_property].extend(
                 filtered_reviewer_ids)
 
