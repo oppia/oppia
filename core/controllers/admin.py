41b70d7ac9e327c39b9128d88e757924f44a0c49
sambhav kaushik <masterboy376@gmail.com>
2024-01-14 14:25:05 +0530

    opened the svg file using with open() block

--- core/controllers/admin.py
+++ core/controllers/admin.py
@@ -672,11 +672,12 @@ class AdminHandler(
                 'fragm')
 
             topic_1.update_meta_tag_content('dummy-meta')
-            svg_file = open(
-                'core/tests/data/thumbnail.svg', 'rt', encoding='utf8')
-            svg_file_content = svg_file.read()
-            svg_file.close()
-            raw_image = svg_file_content.encode('ascii')
+            raw_image = ''
+            with open(
+                'core/tests/data/thumbnail.svg', 'rt',
+                encoding='utf8') as svg_file:
+                    svg_file_content = svg_file.read()
+                    raw_image = svg_file_content.encode('ascii')
             fs_services.save_original_and_compressed_versions_of_image(
                 'thumbnail.svg', feconf.ENTITY_TYPE_TOPIC, topic_id_1,
                 raw_image, 'thumbnail', False)
