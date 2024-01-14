da42e54bb8307c9417d5398b978ed82a61c46ddb
jayam04 <hello@crimsontwilight.maskmy.id>
2024-01-14 11:42:43 +0530

    removed files due to previous bad commit

--- check_images.sh
+++
@@ -1,3 +0,0 @@
-if docker ps -a --format={{.Image}} | grep -q "oppia-dev-server" || docker ps -a --format={{.Image}} | grep -q "oppia-angular-build"; then
-    echo "Stopping Oppia containers..."
-fi
--- docker/dev-server.healthcheck.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if curl -f http://localhost:8181/; then  
-  # Healthy - update label
-  docker container update --label-add HEALTHY=true oppia-dev-server
-  exit 0
-else
-  exit 1
-fi
