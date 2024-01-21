3b4edb226232cca6d3e51b0fd1b55255dacc700c
Afzal Khan <tricktomade@gmail.com>
2024-01-21 15:16:10 +0530

    fix test

--- core/templates/components/state-editor/state-responses-editor/state-responses.component.ts
+++ core/templates/components/state-editor/state-responses-editor/state-responses.component.ts
@@ -151,15 +151,11 @@ export class StateResponsesComponent implements OnInit, OnDestroy {
   }
 
   isSelfLoopThatIsMarkedCorrect(outcome: Outcome): boolean {
-    if (!outcome) {
-      return false;
-    }
-
     let currentStateName = this.stateName;
 
     return (
-      (outcome.dest === currentStateName) &&
-      outcome.labelledAsCorrect);
+      (outcome?.dest === currentStateName) &&
+      outcome?.labelledAsCorrect);
   }
 
   changeActiveAnswerGroupIndex(newIndex: number): void {
