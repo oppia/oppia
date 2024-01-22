388032f12a45b96e816a232177e7932df7a340bd
Afzal Khan <tricktomade@gmail.com>
2024-01-22 13:11:55 +0530

    fix test

--- core/templates/components/state-editor/state-responses-editor/state-responses.component.ts
+++ core/templates/components/state-editor/state-responses-editor/state-responses.component.ts
@@ -150,7 +150,7 @@ export class StateResponsesComponent implements OnInit, OnDestroy {
     }
   }
 
-  isSelfLoopThatIsMarkedCorrect(outcome: Outcome | null): boolean {
+  isSelfLoopThatIsMarkedCorrect(outcome: Outcome): boolean {
     if (!outcome) {
       return false;
     }
