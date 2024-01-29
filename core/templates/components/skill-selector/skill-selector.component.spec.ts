c3b6fce6cf34f1455a2b24c026677323c32be3f2
Jaimin791 <jaimin.purohit21@gmail.com>
2024-01-29 16:50:47 +0530

    fixed padding issues

--- core/templates/components/skill-selector/skill-selector.component.spec.ts
+++ core/templates/components/skill-selector/skill-selector.component.spec.ts
@@ -20,6 +20,8 @@ import { SkillSummary } from 'domain/skill/skill-summary.model';
 import { UserService } from 'services/user.service';
 import { SkillSelectorComponent } from './skill-selector.component';
 import { SkillEditorStateService } from 'pages/skill-editor-page/services/skill-editor-state.service';
+import { Skill } from 'core/templates/domain/skill/SkillObjectFactory';
+
 
 
 /**
