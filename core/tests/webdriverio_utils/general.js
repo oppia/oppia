f0fc530d65890dbfe7a3d0bf1a1f1301b2727e1d
Justin Nguyen <jn-nguyen@outlook.com>
2024-02-14 09:01:00 -0800

    Updates

--- core/tests/webdriverio_utils/general.js
+++ core/tests/webdriverio_utils/general.js
@@ -80,10 +80,10 @@ var CONSOLE_ERRORS_TO_FIX = [
   _.escapeRegExp('ERROR CONTEXT'),
   // TODO(#19746): Development console error "Uncaught in Promise" on signup.
   _.escapeRegExp(
-    'Uncaught (in promise): HttpErrorResponse: {"headers":{"normalizedNames"' +
-    ':{},"lazyUpdate":null},"status":200,"statusText":"OK","url":"' +
-    'http://localhost:8181/logout?redirect_url=/learner_groups_feature_' +
-    'status_handler","ok":false,"name":"HttpErrorResponse"'
+    'Uncaught (in promise): S: {\"headers\":{\"normalizedNames\":{},' +
+    '\"lazyUpdate\":null},\"status\":200,\"statusText\":\"OK\",\"url\":' +
+    '\"http://localhost:8181/logout?redirect_url=/learner_groups_feature_' +
+    'status_handler\",\"ok\":false,\"name\":\"HttpErrorResponse\"'
   ),
   // TODO(#19687): 404 (Not Found) for favicon on home page.
   _.escapeRegExp(
