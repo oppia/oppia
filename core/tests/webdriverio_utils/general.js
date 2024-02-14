609b8749c6a195c16efc8a16baeb449fb0e27bf7
Justin Nguyen <jn-nguyen@outlook.com>
2024-02-13 22:03:28 -0800

    Updates

--- core/tests/webdriverio_utils/general.js
+++ core/tests/webdriverio_utils/general.js
@@ -58,8 +58,6 @@ var CONSOLE_ERRORS_TO_IGNORE = [
   _.escapeRegExp(
     'https://pencilcode.net/lib/pencilcodeembed.js - Failed to ' +
     'load resource: net::ERR_CERT_DATE_INVALID'),
-  // Webpack uncaught promise errors during e2e test runs.
-  _.escapeRegExp('Uncaught (in promise)'),
   // These errors are related to the gtag script that is used to track events.
   // They are of the form "Failed to load resource: the server responded
   // with a status of 405", this happens when the HTTP method used for a
@@ -80,6 +78,13 @@ var CONSOLE_ERRORS_TO_FIX = [
   // Several issues involving these angular errors.
   _.escapeRegExp('ExpressionChangedAfterItHasBeenCheckedError'),
   _.escapeRegExp('ERROR CONTEXT'),
+  // TODO(#19746): Development console error "Uncaught in Promise" on signup.
+  _.escapeRegExp(
+    'Uncaught (in promise): HttpErrorResponse: {"headers":{"normalizedNames"' +
+    ':{},"lazyUpdate":null},"status":200,"statusText":"OK","url":"' +
+    'http://localhost:8181/logout?redirect_url=/learner_groups_feature_' +
+    'status_handler","ok":false,"name":"HttpErrorResponse"'
+  ),
   // TODO(#19687): 404 (Not Found) for favicon on home page.
   _.escapeRegExp(
     'http://localhost:8181/dist/oppia-angular/favicon.ico - Failed to ' +
