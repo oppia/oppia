4308e651971f7287c0e87632540619c44c71e496
sambhav kaushik <masterboy376@gmail.com>
2024-02-15 23:12:37 +0530

    created a new getter function for active state name and removed outdated state name variable.

--- core/templates/components/state-editor/state-responses-editor/state-responses.component.ts
+++ core/templates/components/state-editor/state-responses-editor/state-responses.component.ts
@@ -63,7 +63,6 @@ export class StateResponsesComponent implements OnInit, OnDestroy {
   activeAnswerGroupIndex!: number;
   // State name is null if their is no state selected or have no active state.
   // This is the case when the user is creating a new state.
-  stateName!: string | null;
   @Output() onResponsesInitialized = new EventEmitter<void>();
   @Output() onSaveInteractionAnswerGroups = (
     new EventEmitter<AnswerGroup[] | AnswerGroup>());
@@ -143,10 +142,15 @@ export class StateResponsesComponent implements OnInit, OnDestroy {
     this.stateSolicitAnswerDetailsService.saveDisplayedValue();
   }
 
+  getActiveStateName(): string {
+    return this.stateEditorService.getActiveStateName();
+  }
+
   isSelfLoopWithNoFeedback(outcome: Outcome): boolean | void {
-    if (outcome && typeof outcome === 'object' && this.stateName &&
+    let currentStateName = this.getActiveStateName();
+    if (outcome && typeof outcome === 'object' && currentStateName &&
       outcome.constructor.name === 'Outcome') {
-      return outcome.isConfusing(this.stateName);
+      return outcome.isConfusing(currentStateName);
     }
   }
 
@@ -154,9 +158,7 @@ export class StateResponsesComponent implements OnInit, OnDestroy {
     if (!outcome) {
       return false;
     } else {
-      this.stateName = this.stateEditorService.getActiveStateName()
-      let currentStateName = this.stateName;
-      console.log("current: ", currentStateName, ", destination: ", outcome.dest)
+      let currentStateName = this.getActiveStateName();
       return (
         (outcome.dest === currentStateName) &&
         outcome.labelledAsCorrect);
@@ -479,7 +481,7 @@ export class StateResponsesComponent implements OnInit, OnDestroy {
   }
 
   isOutcomeLooping(outcome: Outcome): boolean {
-    let activeStateName = this.stateName;
+    let activeStateName = this.getActiveStateName();
     return outcome && (outcome.dest === activeStateName);
   }
 
@@ -573,7 +575,6 @@ export class StateResponsesComponent implements OnInit, OnDestroy {
     this.SHOW_TRAINABLE_UNRESOLVED_ANSWERS = (
       AppConstants.SHOW_TRAINABLE_UNRESOLVED_ANSWERS);
     this.responseCardIsShown = true;
-    this.stateName = this.stateEditorService.getActiveStateName();
     this.enableSolicitAnswerDetailsFeature = (
       AppConstants.ENABLE_SOLICIT_ANSWER_DETAILS_FEATURE);
     this.misconceptionsBySkill = {};
