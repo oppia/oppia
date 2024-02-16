fc843b1c91aa0dd5d088735dd2fe6487e8fcee1e
jayam04 <jayampatel04@gmail.com>
2024-02-16 18:15:22 +0530

    handling docker containers

--- scripts/pre_push_hook.sh
+++ scripts/pre_push_hook.sh
@@ -23,7 +23,8 @@
 # To install hook (which runs inside docker container), run this script
 # command from oppia root directory with --install argument
 
-DOCKER_EXEC_COMMAND="docker compose exec -T dev-server "
+DEV_CONTAINER="dev-server"
+DOCKER_EXEC_COMMAND="docker compose exec -T $DEV_CONTAINER "
 
 PYTHON_PREPUSH_HOOK_PATH=".git/hooks/pre-push-python"
 PREPUSH_HOOK_PATH=".git/hooks/pre-push"
@@ -38,45 +39,58 @@ HOOKS_DIR="$OPPIA_DIR/.git/hooks"
 PRE_PUSH_FILE="$HOOKS_DIR/pre-push"
 PYTHON_PRE_PUSH_FILE="$HOOKS_DIR/pre-push-python"
 
+# Install pre-push hook
+install_hook() {
+    # Create symlinks for pre-push hook files
+    for file in "$PRE_PUSH_FILE" "$PYTHON_PRE_PUSH_FILE"; do
+        # Check if pre-push file is already a symlink
+        if [ -h "$file" ] && [ -e "$file" ]; then
+            echo "Symlink already exists (for $file)"
+
+        else
+            # If broken symlink, remove it
+            if [ -h "$file" ] && [ ! -e "$file" ]; then
+                rm "$file"
+                echo "Removed broken symlink (for $file)"
+            fi
+
+            # Try creating a symlink
+            ln -s "$(pwd)/$(basename "$0")" "$file" &&
+                echo "Created symlink in .git/hooks directory" ||
+                {
+                    # Fallback to copy on windows
+                    cp "$(pwd)/$(basename "$0")" "$file"
+                    echo "Copied file to .git/hooks directory"
+                }
+
+            # Make the hook file executable
+            chmod +x "$file"
+
+            if [ $? -eq 0 ]; then
+                echo "pre-push hook file is now executable!"
+            else
+                echo >&2 "Failed to make pre-push executable"
+                exit 1
+            fi
+        fi
+    done
+    exit 0
+}
+
+
 # Check for --install in args and install pre-push hook if it's found
 for arg in "$@"; do
     if [ "$arg" == "--install" ]; then
-        # Create symlinks for pre-push hook files
-        for file in "$PRE_PUSH_FILE" "$PYTHON_PRE_PUSH_FILE"; do
-            # Check if pre-push file is already a symlink
-            if [ -h "$file" ] && [ -e "$file" ]; then
-                echo "Symlink already exists (for $file)"
-
-            else
-                # If broken symlink, remove it
-                if [ -h "$file" ] && [ ! -e "$file" ]; then
-                    rm "$file"
-                    echo "Removed broken symlink (for $file)"
-                fi
-
-                # Try creating a symlink
-                ln -s "$(pwd)/$(basename "$0")" "$file" &&
-                    echo "Created symlink in .git/hooks directory" ||
-                    {
-                        # Fallback to copy on windows
-                        cp "$(pwd)/$(basename "$0")" "$file"
-                        echo "Copied file to .git/hooks directory"
-                    }
-
-                # Make the hook file executable
-                chmod +x "$file"
-
-                if [ $? -eq 0 ]; then
-                    echo "pre-push hook file is now executable!"
-                else
-                    echo >&2 "Failed to make pre-push executable"
-                    exit 1
-                fi
-            fi
-        done
-        exit 0
+        install_hook
     fi
 done
+
+
+# Check if containers are running, so we can shut them down at end if not
+WAS_RUNNING=$(docker inspect -f '{{.State.Running}}' "$DEV_CONTAINER")
+
+# Start containers and run pre-push hook
+make run-offline
 
 CMD="$DOCKER_EXEC_COMMAND python3 $PYTHON_PREPUSH_HOOK_PATH $@"
 echo "Running $CMD"
@@ -86,4 +100,9 @@ $CMD
 # Exit with the exit code from the docker command
 exitcode=$?
 echo "Python script exited with code $exitcode"
+
+# Shut down containers if they were not running before
+if [ "$WAS_RUNNING" != "false" ]; then
+    make stop
+
 exit $exitcode
