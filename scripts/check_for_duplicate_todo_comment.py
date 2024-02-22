b1705f0a99448138438d2563a45bda5f08742847
Justin Nguyen <jn-nguyen@outlook.com>
2024-02-22 13:07:56 -0800

    Updates

--- scripts/check_for_duplicate_todo_comment.py
+++ scripts/check_for_duplicate_todo_comment.py
@@ -52,28 +52,32 @@ def main(args: Optional[List[str]] = None) -> None:
 
     repository_path = f'{parsed_args.repository_path}/'
     github_perma_link_url = 'https://github.com/oppia/oppia/blob/'
+    # Only start comparing after the commit SHA hash as the commit SHA hash 
+    # might change depending on the workflow.
     compare_start_index = len(github_perma_link_url) + COMMIT_SHA_HASH_LENGTH
 
     latest_comment_lines: List[str] = []
-    new_comment_lines: List[str] = []
     with open(
         repository_path + parsed_args.latest_comment_file, 'r',
         encoding='utf-8'
     ) as latest_comment_file:
         latest_comment_lines = latest_comment_file.read().splitlines()
 
+    new_comment_lines: List[str] = []
     with open(
         repository_path + parsed_args.new_comment_file, 'r',
         encoding='utf-8'
     ) as new_comment_file:
         new_comment_lines = new_comment_file.read().splitlines()
 
+    # Automatically fail if the first line or number of lines differs.
     if (
         len(latest_comment_lines) != len(new_comment_lines) or
         latest_comment_lines[0] != new_comment_lines[0]
     ):
         raise Exception(NO_DUPLICATE_TODO_COMMENT_INDICATOR)
 
+    # Loop through each line and check if the lines differ.
     for index in range(1, len(latest_comment_lines)):
         latest_comment_line_content = (
             latest_comment_lines[index][compare_start_index:])
