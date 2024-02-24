04a83df266c2519c8c196b63bdeb730166579730
imchristie <im.christie.ho@gmail.com>
2024-02-23 22:25:49 -0800

    lint fixed

--- core/tests/puppeteer-acceptance-tests/user-utilities/logged-in-users-utils.js
+++ core/tests/puppeteer-acceptance-tests/user-utilities/logged-in-users-utils.js
@@ -368,8 +368,6 @@ module.exports = class LoggedInUsers extends baseUser {
     } else {
       showMessage(
         'The Learn More About Oppia button opens the About page.');
-      // const newPage = await newTab.page();
-      // await newPage.close();
     }
   }
 
