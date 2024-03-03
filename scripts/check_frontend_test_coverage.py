49081ec85845bca4eda85e2b6f12ad970be05ec8
Nikita Vorobev <vorobyev.nikita@gmail.com>
2024-03-03 22:56:24 +0000

    fix coverage exclusion

--- scripts/check_frontend_test_coverage.py
+++ scripts/check_frontend_test_coverage.py
@@ -64,12 +64,11 @@ NOT_FULLY_COVERED_FILENAMES = [
     'rte-helper-modal.controller.ts',
     'rule-type-selector.directive.ts',
     'translation-file-hash-loader-backend-api.service.ts',
+    # TODO(#19499): add missing tests
+    'translation-suggestion-review-modal.component.ts',
     # Please don't try to cover `unit-test-utils.ajs.ts` file.
     'unit-test-utils.ajs.ts',
     'voiceover-recording.service.ts',
-    # TODO(#19499): add missing tests
-    'translation-suggestion-review-modal.component.ts',
-    'image-local-storage.service.ts',
 ]
 
 
