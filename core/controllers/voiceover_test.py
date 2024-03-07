b129ca0ac5a3e164956fa9dc85df2521cddf7759
Nik-09 <nikhil.agarwal.2019@gmail.com>
2024-03-07 20:41:42 +0530

    Removes unnecessary print logs

--- core/controllers/voiceover_test.py
+++ core/controllers/voiceover_test.py
@@ -31,25 +31,6 @@ if MYPY: # pragma: no cover
 class VoiceoverAdminPageTests(test_utils.GenericTestBase):
     """Checks the voiceover admin page functionality and its rendering."""
 
-    def test_voiceover_admin_page_access_without_logging_in(self) -> None:
-        """Tests access to the Voiceover Admin page."""
-        self.get_html_response('/voiceover-admin', expected_status_int=302)
-
-    def test_voiceover_admin_page_access_without_being_curriculum_admin(
-        self
-    ) -> None:
-        self.signup(self.VIEWER_EMAIL, self.VIEWER_USERNAME)
-        self.login(self.VIEWER_EMAIL)
-        self.get_html_response('/voiceover-admin', expected_status_int=401)
-        self.logout()
-
-    def test_voiceover_admin_page_access_as_curriculum_admin(self) -> None:
-        self.signup(self.CURRICULUM_ADMIN_EMAIL, self.CURRICULUM_ADMIN_USERNAME)
-        self.set_curriculum_admins([self.CURRICULUM_ADMIN_USERNAME])
-        self.login(self.CURRICULUM_ADMIN_EMAIL, is_super_admin=True)
-        self.get_html_response('/voiceover-admin')
-        self.logout()
-
     def test_get_voiceover_admin_data(self) -> None:
         self.signup(self.CURRICULUM_ADMIN_EMAIL, self.CURRICULUM_ADMIN_USERNAME)
         self.set_curriculum_admins([self.CURRICULUM_ADMIN_USERNAME])
