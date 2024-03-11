757ff1d958ae6795c641908ba253be42af118e16
rahat2134 <arnavkhan4343@gmail.com>
2024-03-11 13:26:22 +0530

    correct comma in suiteName

--- scripts/common.py
+++ scripts/common.py
@@ -214,7 +214,7 @@ ACCEPTANCE_TESTS_SUITE_NAMES = [
     'logged-in-user-tests/click-all-buttons-in-thanks-for-donating-page',
     'practice-question-admin-tests/add-and-remove-contribution-rights',
     'translation-admin-tests/add-translation-rights',
-    'translation-admin-tests/remove-translation-rights'
+    'translation-admin-tests/remove-translation-rights',
     'exploration-creator-tests/create-exploration-and-change-basic-settings',
     'exploration-creator-tests/savedraft-publish-and-discard-the-changes'
 
