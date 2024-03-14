735de7c747d53aa842f3ad44fc65f71cf956b612
Akhilesh-max <2827daya@gmail.com>
2024-03-15 04:42:10 +0530

    changes

--- core/tests/puppeteer-acceptance-tests/puppeteer-testing-utilities/puppeteer-utils.ts
+++ core/tests/puppeteer-acceptance-tests/puppeteer-testing-utilities/puppeteer-utils.ts
@@ -47,7 +47,7 @@ export class BaseUser {
       '--use-fake-ui-for-media-stream',
     ];
 
-    const headless = process.env.HEADLESS === 'false';
+    const headless = process.env.HEADLESS === 'true';
     /**
      * Here we are disabling the site isolation trials because it is causing
      * tests to fail while running in non headless mode (see
