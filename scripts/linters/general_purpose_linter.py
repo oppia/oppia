4bc3ab918b1c073cee37c9e49f00b3014bc4dadf
gp201 <praneethg2001@gmail.com>
2024-03-15 22:27:35 -0700

    Miscellaneous Fixes

--- scripts/linters/general_purpose_linter.py
+++ scripts/linters/general_purpose_linter.py
@@ -84,7 +84,7 @@ EXCLUDED_PATHS: Final = (
     'core/templates/css/oppia-material.css',
     'core/templates/google-analytics.initializer.ts',
     'extensions/classifiers/proto/*',
-    'core/tests/puppeteer-acceptance-tests/build/*',
+    'core/tests/puppeteer-acceptance-tests/build/*', '.mypy_cache/*',
     '%s/*' % js_ts_linter.COMPILED_TYPESCRIPT_TMP_PATH)
 
 GENERATED_FILE_PATHS: Final = (
--- scripts/pre_commit_hook.py
+++ scripts/pre_commit_hook.py
@@ -42,12 +42,16 @@ from typing import Final, List, Optional, Tuple
 # we migrate to Python 3.8.
 sys.path.append(os.getcwd())
 from scripts import common  # isort:skip # pylint: disable=wrong-import-position
+from core import feconf  # isort:skip # pylint: disable=wrong-import-position
 from core import utils  # isort:skip # pylint: disable=wrong-import-position
 
 FECONF_FILEPATH: Final = os.path.join('core', 'feconf.py')
 CONSTANTS_FILEPATH: Final = os.path.join('.', 'assets', 'constants.ts')
 RELEASE_CONSTANTS_FILEPATH: Final = os.path.join(
-    '.', 'assets', 'release_constants.json')
+    '.', 'assets', 'release_constants_docker.json'
+) if feconf.OPPIA_IS_DOCKERIZED else os.path.join(
+    '.', 'assets', 'release_constants.json'
+)
 KEYS_UPDATED_IN_FECONF: Final = [
     b'ADMIN_EMAIL_ADDRESS',
     b'SYSTEM_EMAIL_ADDRESS', b'NOREPLY_EMAIL_ADDRESS', b'CAN_SEND_EMAILS',
