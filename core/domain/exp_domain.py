2dffeb4fa097738dbe18261e3a857f674bdc81e5
Afzal Khan <tricktomade@gmail.com>
2024-03-17 01:04:58 +0530

    add break

--- core/domain/exp_domain.py
+++ core/domain/exp_domain.py
@@ -5842,8 +5842,8 @@ class UserExplorationData(translation_domain.BaseTranslatableObject):
             rights: rights_domain.ActivityRightsDict. Dictionary
                 representation of activity rights.
             exploration_email_preferences: (
-                Optional[user_domain.UserExplorationPrefsDict]).
-                Dictionary representing feedback and suggestion email settings.
+                Optional[user_domain.UserExplorationPrefsDict]). Dictionary
+                representing feedback and suggestion email settings.
             draft_change_list_id: int. The id of draf change list.
             is_valid_draft_version: Optional[bool]. Whether the given draft
                 version is valid or not.
