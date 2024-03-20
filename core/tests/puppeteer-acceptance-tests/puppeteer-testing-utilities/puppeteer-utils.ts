bf08e3e8f8ae71ae43bb0a9b49323eeaa4f76f31
Justin Nguyen <jn-nguyen@outlook.com>
2024-03-20 08:32:34 -0700

    Updates

--- core/tests/puppeteer-acceptance-tests/puppeteer-testing-utilities/puppeteer-utils.ts
+++ core/tests/puppeteer-acceptance-tests/puppeteer-testing-utilities/puppeteer-utils.ts
@@ -153,10 +153,10 @@ export class BaseUser {
     selector: string | ElementHandle<Element>
   ): Promise<void> {
     try {
-      const element =
+      const element = (
         typeof selector === 'string'
           ? await this.page.waitForSelector(selector)
-          : selector;
+          : selector);
       await this.page.waitForFunction(isElementClickable, {}, element);
     } catch (error) {
       throw new Error(`Element ${selector} took too long to be clickable.`);
