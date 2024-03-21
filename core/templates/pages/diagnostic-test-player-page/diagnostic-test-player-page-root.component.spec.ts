125dd64dfaa8e890ab570ee7d66daf38dbc137a3
Sagar Subedi <sagarxic@gmail.com>
2024-03-21 12:20:51 +0545

    modified unit tests to match the code changes in component

--- core/templates/pages/diagnostic-test-player-page/diagnostic-test-player-page-root.component.spec.ts
+++ core/templates/pages/diagnostic-test-player-page/diagnostic-test-player-page-root.component.spec.ts
@@ -16,35 +16,56 @@
  * @fileoverview Unit tests for the Diagnostic Test Player page root component.
  */
 
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { ComponentFixture, TestBed, waitForAsync, tick, fakeAsync } from '@angular/core/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {NO_ERRORS_SCHEMA} from '@angular/core';
+import {
+  ComponentFixture,
+  TestBed,
+  waitForAsync,
+  tick,
+  fakeAsync,
+} from '@angular/core/testing';
 
-import { PageHeadService } from 'services/page-head.service';
+import {PageHeadService} from 'services/page-head.service';
+import {AppConstants} from 'app.constants';
 
-import { MockTranslatePipe } from 'tests/unit-test-utils';
-import { DiagnosticTestPlayerPageRootComponent } from './diagnostic-test-player-page-root.component';
-import { AccessValidationBackendApiService } from 'pages/oppia-root/routing/access-validation-backend-api.service';
+import {MockTranslatePipe} from 'tests/unit-test-utils';
+import {DiagnosticTestPlayerPageRootComponent} from './diagnostic-test-player-page-root.component';
+import {AccessValidationBackendApiService} from 'pages/oppia-root/routing/access-validation-backend-api.service';
+import {Router} from '@angular/router';
+
+class MockAccessValidationBackendApiService {
+  validateAccessToDiagnosticTestPlayerPage() {
+    return Promise.resolve();
+  }
+}
+
+class MockRouter {
+  navigate(commands: string[]): Promise<boolean> {
+    return Promise.resolve(true);
+  }
+}
 
 describe('Diagnostic Test Player Root Page', () => {
   let fixture: ComponentFixture<DiagnosticTestPlayerPageRootComponent>;
   let component: DiagnosticTestPlayerPageRootComponent;
   let pageHeadService: PageHeadService;
   let accessValidationBackendApiService: AccessValidationBackendApiService;
+  let router: Router;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [
-        HttpClientTestingModule
-      ],
-      declarations: [
-        DiagnosticTestPlayerPageRootComponent,
-        MockTranslatePipe
-      ],
+      imports: [HttpClientTestingModule],
+      declarations: [DiagnosticTestPlayerPageRootComponent, MockTranslatePipe],
       providers: [
-        PageHeadService
+        PageHeadService,
+        {
+          provide: AccessValidationBackendApiService,
+          useClass: MockAccessValidationBackendApiService,
+        },
+        {provide: Router, useClass: MockRouter},
       ],
-      schemas: [NO_ERRORS_SCHEMA]
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
   }));
 
@@ -53,49 +74,54 @@ describe('Diagnostic Test Player Root Page', () => {
     component = fixture.componentInstance;
     pageHeadService = TestBed.inject(PageHeadService);
     accessValidationBackendApiService = TestBed.inject(
-      AccessValidationBackendApiService);
+      AccessValidationBackendApiService
+    );
+    accessValidationBackendApiService = TestBed.inject(
+      AccessValidationBackendApiService
+    );
+    router = TestBed.inject(Router);
   });
 
-  it('should successfully instantiate the component', (
-  ) => {
+  it('should successfully instantiate the component', () => {
     spyOn(
       accessValidationBackendApiService,
-      'validateAccessToDiagnosticTestPlayerPage')
-      .and.returnValue(Promise.resolve());
+      'validateAccessToDiagnosticTestPlayerPage'
+    ).and.returnValue(Promise.resolve());
+
     expect(component).toBeDefined();
   });
 
-  it('should initialize', (
-  ) => {
+  it('should initialize', () => {
     spyOn(pageHeadService, 'updateTitleAndMetaTags');
     spyOn(
       accessValidationBackendApiService,
-      'validateAccessToDiagnosticTestPlayerPage')
-      .and.returnValue(Promise.resolve());
+      'validateAccessToDiagnosticTestPlayerPage'
+    ).and.returnValue(Promise.resolve());
     component.ngOnInit();
 
     expect(
-      accessValidationBackendApiService
-        .validateAccessToDiagnosticTestPlayerPage)
-      .toHaveBeenCalledWith();
+      accessValidationBackendApiService.validateAccessToDiagnosticTestPlayerPage
+    ).toHaveBeenCalledWith();
   });
 
-  it('should show error when Diagnostic Test Player does not exist',
-    fakeAsync((
-    ) => {
-      spyOn(pageHeadService, 'updateTitleAndMetaTags');
-      spyOn(
-        accessValidationBackendApiService,
-        'validateAccessToDiagnosticTestPlayerPage')
-        .and.returnValue(Promise.reject());
+  it('should show error when Diagnostic Test Player does not exist', fakeAsync(() => {
+    spyOn(pageHeadService, 'updateTitleAndMetaTags');
+    spyOn(
+      accessValidationBackendApiService,
+      'validateAccessToDiagnosticTestPlayerPage'
+    ).and.returnValue(Promise.reject());
+    const navigateSpy = spyOn(router, 'navigate').and.returnValue(
+      Promise.resolve(true)
+    );
 
-      expect(component.errorPageIsShown).toBeFalse();
-      expect(component.pageIsShown).toBeFalse();
+    expect(component.pageIsShown).toBeFalse();
 
-      component.ngOnInit();
-      tick();
+    component.ngOnInit();
+    tick();
 
-      expect(component.pageIsShown).toBeFalse();
-      expect(component.errorPageIsShown).toBeTrue();
-    }));
+    expect(component.pageIsShown).toBeFalse();
+    expect(navigateSpy).toHaveBeenCalledWith([
+      `${AppConstants.PAGES_REGISTERED_WITH_FRONTEND.ERROR.ROUTE}/404`,
+    ]);
+  }));
 });
