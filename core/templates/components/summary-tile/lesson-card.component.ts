a470bee9c5719091159f65930a3798dc8cb2e0e7
Amy Yeung <amyyeung17@gmail.com>
2024-03-25 14:18:27 -0400

    Updated i18n, styling, and card for collections

--- core/templates/components/summary-tile/lesson-card.component.ts
+++ core/templates/components/summary-tile/lesson-card.component.ts
@@ -1,20 +1,18 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { downgradeComponent } from '@angular/upgrade/static';
-import { AppConstants } from 'app.constants';
-import { AssetsBackendApiService } from 'services/assets-backend-api.service';
-import { UrlInterpolationService } from 'domain/utilities/url-interpolation.service';
-import { CollectionSummary } from 'domain/collection/collection-summary.model';
-import { LearnerExplorationSummary } from 'domain/summary/learner-exploration-summary.model';
-import { StorySummary } from 'domain/story/story-summary.model';
-import { UrlService } from 'services/contextual/url.service';
+import {Component, Input, OnInit} from '@angular/core';
+import {downgradeComponent} from '@angular/upgrade/static';
+import {AppConstants} from 'app.constants';
+import {AssetsBackendApiService} from 'services/assets-backend-api.service';
+import {UrlInterpolationService} from 'domain/utilities/url-interpolation.service';
+import {CollectionSummary} from 'domain/collection/collection-summary.model';
+import {LearnerExplorationSummary} from 'domain/summary/learner-exploration-summary.model';
+import {StorySummary} from 'domain/story/story-summary.model';
+import {UrlService} from 'services/contextual/url.service';
 
 @Component({
   selector: 'lesson-card',
-  templateUrl: './lesson-card.component.html'
+  templateUrl: './lesson-card.component.html',
 })
-
-
-export class LessonCardComponent implements OnInit{
+export class LessonCardComponent implements OnInit {
   @Input() story: StorySummary | LearnerExplorationSummary | CollectionSummary;
   @Input() topic: string;
 
@@ -24,18 +22,19 @@ export class LessonCardComponent implements OnInit{
   lessonUrl!: string;
   progress!: number;
   title!: string;
+  lessonTopic!: string;
   /**
    * if lessonType === classes
    * Move functions from
    * - exploration-summary-tile.component
-   *  - getThumbnailIconUrl - imgUrl 
+   *  - getThumbnailIconUrl - imgUrl
    *  - getExplorationLink (?)
    *  - Translation Key
    */
   constructor(
     private urlInterpolationService: UrlInterpolationService,
     private assetsBackendApiService: AssetsBackendApiService,
-    private urlService: UrlService,
+    private urlService: UrlService
   ) {}
 
   ngOnInit(): void {
@@ -44,10 +43,13 @@ export class LessonCardComponent implements OnInit{
 
       this.desc = this.story.getTitle();
       this.imgColor = this.story.getThumbnailBgColor();
-      //check if undefined needs to be tested 
+      //check if undefined needs to be tested
       if (this.story.getThumbnailFilename()) {
         this.imgUrl = this.assetsBackendApiService.getThumbnailUrlForPreview(
-          AppConstants.ENTITY_TYPE.STORY, this.story.getId(), this.story.getThumbnailFilename());
+          AppConstants.ENTITY_TYPE.STORY,
+          this.story.getId(),
+          this.story.getThumbnailFilename()
+        );
       }
 
       //last completed story index works because if 1 is completed, 1 index is 2nd item
@@ -55,45 +57,67 @@ export class LessonCardComponent implements OnInit{
       let classFragment = this.story.getClassroomUrlFragment();
       let topicFragment = this.story.getTopicUrlFragment();
 
-      //check if undefined needs to be tested 
-      let temp = `/explore/${currentStory.getExplorationId()}`
-      this.lessonUrl = ((classFragment === undefined || topicFragment === undefined ) ?
-        '#'
-      :
-        `/explore/${currentStory.getExplorationId()}?` +  Object.entries({
-          topic_url_fragment: topicFragment,
-          classroom_url_fragment: classFragment,
-          story_url_fragment: this.story.getUrlFragment(),
-          node_id: currentStory.getId()
-        }).map(([key, value]) => `${key}=${value}`).join('&')
+      //check if undefined needs to be tested
+      this.lessonUrl =
+        classFragment === undefined || topicFragment === undefined
+          ? '#'
+          : `/explore/${currentStory.getExplorationId()}?` +
+            Object.entries({
+              topic_url_fragment: topicFragment,
+              classroom_url_fragment: classFragment,
+              story_url_fragment: this.story.getUrlFragment(),
+              node_id: currentStory.getId(),
+            })
+              .map(([key, value]) => `${key}=${value}`)
+              .join('&');
+
+      this.title = `Chapter ${completedStories + 1}: ${this.story.getNodeTitles()[completedStories]}`;
+      this.progress = Math.floor(
+        (completedStories / this.story.getNodeTitles().length) * 100
       );
-        console.log(this.lessonUrl)
-      //Must be a better way to do this (remove getNodeTitles())
-      this.title =`Chapter ${completedStories + 1}: ${this.story.getNodeTitles()[completedStories]}`;
-      this.progress = Math.floor(completedStories / this.story.getNodeTitles().length * 100);
-
-    } else if (this.story instanceof CollectionSummary) {
-
+      this.lessonTopic = this.topic;
     } else {
+      /*  Implementation for explorations & collections */
       this.desc = this.story.objective;
       this.imgColor = this.story.thumbnailBgColor;
-      this.imgUrl = this.urlInterpolationService.getStaticImageUrl(this.story.thumbnailIconUrl);
-      //temp
-      this.lessonUrl = `/explore/${this.story.id}`;
-      //temp
+      this.imgUrl = this.urlInterpolationService.getStaticImageUrl(
+        this.story.thumbnailIconUrl
+      );
       this.progress = 0;
       this.title = this.story.title;
+
+      if (this.story instanceof CollectionSummary) {
+        this.lessonUrl = '';
+        this.lessonTopic = 'Collections';
+      } else {
+        this.lessonUrl = `/explore/${this.story.id}`;
+        this.lessonTopic = 'Community Lessons';
+      }
     }
   }
 
-  //go back 1 - add test case 
-  getProgress(arg: number) : string {
-    return 'linear-gradient(' + (50 >= arg? (180 + (arg / 100 * 180)) : 270) + 'deg, #00645c ' + (50 >= arg ? arg : 50) + '%, transparent ' + (50 >= arg ? arg : 50) +'%),' +
-    'linear-gradient(0deg, #00645c ' + (arg > 50 ? arg - 50 : 0) + '%, lightgray ' + (arg> 50 ? arg - 50 : 0) + '%)'
+  //go back 1 - add test case
+  getProgress(arg: number): string {
+    return (
+      'linear-gradient(' +
+      (50 >= arg ? 180 + (arg / 100) * 180 : 270) +
+      'deg, #00645c ' +
+      (50 >= arg ? arg : 50) +
+      '%, transparent ' +
+      (50 >= arg ? arg : 50) +
+      '%),' +
+      'linear-gradient(0deg, #00645c ' +
+      (arg > 50 ? arg - 50 : 0) +
+      '%, lightgray ' +
+      (arg > 50 ? arg - 50 : 0) +
+      '%)'
+    );
   }
-
 }
 
-angular.module('oppia').directive(
-  'lessonCardComponent', downgradeComponent(
-    {component: LessonCardComponent}));
+angular
+  .module('oppia')
+  .directive(
+    'lessonCardComponent',
+    downgradeComponent({component: LessonCardComponent})
+  );
