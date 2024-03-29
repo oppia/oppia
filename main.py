a6f3a6244b2316d97aa93e2be1ee7f29d87fe0f1
Ahmed Bhouri <49922043+Ahmed-Bhouri@users.noreply.github.com>
2024-03-29 15:44:32 +0000

    BackEnd changes: Add access validator

--- main.py
+++ main.py
@@ -413,6 +413,10 @@ URLS = [
         r'/<story_url_fragment>' % feconf.REVIEW_TEST_DATA_URL_PREFIX,
         review_tests.ReviewTestsPageDataHandler),
     get_redirect_route(
+         r'%s/can_access_review_tests_page/<classroom_url_fragment>/<topic_url_fragment>'
+         r'/<story_url_fragment>' % feconf.ACCESS_VALIDATION_HANDLER_PREFIX,
+         access_validators.ReviewTestsPageAccessValidationHandler),
+    get_redirect_route(
         r'%s/<classroom_url_fragment>/<topic_url_fragment>'
         r'/<story_url_fragment>' % feconf.STORY_DATA_HANDLER,
         story_viewer.StoryPageDataHandler),
