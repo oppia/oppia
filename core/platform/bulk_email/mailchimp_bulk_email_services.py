b93ef474295a2c74a14d1068cdb0e019ec14eda9
cs <cs.temporary@icloud.com>
2024-03-31 21:26:09 +0000

    Let users register if MailChimp signup fails
    If registering the user for bulk emails via MailChimp fails, show an
    error message directing the user to sign up for bulk emails manually.
    Then, the user can proceed with registration by de-selecting the option
    to sign up for emails on the registration form.

--- core/platform/bulk_email/mailchimp_bulk_email_services.py
+++ core/platform/bulk_email/mailchimp_bulk_email_services.py
@@ -195,13 +195,9 @@ def add_or_update_user_status(
         tag: str. Tag to add to user in mailchimp.
 
     Returns:
-        bool. Whether the user was successfully added to the db. (This will be
-        False if the user was permanently deleted earlier and therefore cannot
-        be added back.)
-
-    Raises:
-        Exception. Any error (other than the case where the user was permanently
-            deleted earlier) raised by the mailchimp API.
+        bool. Whether the user was successfully added to the db. This will be
+        False if and only if MailChimp throws an error or getting the MailChimp
+        client fails.
     """
     client = _get_mailchimp_class()
     if not client:
@@ -301,5 +297,8 @@ def add_or_update_user_status(
                 if not user_creation_successful:
                     return False
         else:
-            raise Exception(error_message['detail']) from error
+            logging.error(
+                'Mailchimp error prevented email signup: %s',
+                error_message['detail'])
+            return False
     return True
