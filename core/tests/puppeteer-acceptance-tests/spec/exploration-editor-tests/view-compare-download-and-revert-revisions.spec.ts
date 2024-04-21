d627d7897374ec6f20529652572bd7fc1df7f3a7
Akhilesh-max <2827daya@gmail.com>
2024-04-22 00:57:01 +0530

    updates

---
+++ core/tests/puppeteer-acceptance-tests/spec/exploration-editor-tests/view-compare-download-and-revert-revisions.spec.ts
@@ -0,0 +1,107 @@
+// Copyright 2023 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the 'License');
+// you may not use explorationEditor file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS-IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Acceptance Test for history tab in exploration editor.
+ */
+
+import {UserFactory} from '../../puppeteer-testing-utilities/user-factory';
+import testConstants from '../../puppeteer-testing-utilities/test-constants';
+import {ExplorationEditor} from '../../user-utilities/exploration-editor-utils';
+
+const DEFAULT_SPEC_TIMEOUT_MSECS: number =
+  testConstants.DEFAULT_SPEC_TIMEOUT_MSECS;
+const DATE: string = 'Date';
+enum INTERACTION_TYPES {
+  END_EXPLORATION = 'End Exploration',
+}
+
+describe('Exploration Editor', function () {
+  let explorationEditor: ExplorationEditor;
+
+  beforeAll(async function () {
+    explorationEditor = await UserFactory.createNewUser(
+      'explorationEditor',
+      'exploration_editor@example.com'
+    );
+    await explorationEditor.navigateToCreatorDashboardPage();
+    await explorationEditor.navigateToExplorationEditorPage();
+    await explorationEditor.dismissWelcomeModal();
+    await explorationEditor.createExplorationWithContentAndInteraction(
+      'Test-revision 1',
+      INTERACTION_TYPES.END_EXPLORATION
+    );
+
+    // Create multiple revisions
+    await explorationEditor.updateCardContent('changed');
+    await explorationEditor.saveExplorationDraft();
+    await explorationEditor.updateCardContent('changed again');
+    await explorationEditor.saveExplorationDraft();
+
+    // Create a new revision with metadata changes
+    await explorationEditor.navigateToSettingsTab();
+    await explorationEditor.updateExplorationTitle('Test-Revision 2');
+    await explorationEditor.navigateToMainTab();
+    await explorationEditor.saveExplorationDraft();
+  }, DEFAULT_SPEC_TIMEOUT_MSECS);
+
+  it(
+    'should be able to view, compare, download, and revert exploration revisions',
+    async function () {
+      await explorationEditor.navigateToHistoryTab();
+
+      // Check properties of revisions
+      await explorationEditor.expectRevisionToHave(1, [
+        'Version No.',
+        'Notes',
+        'User',
+        'Date',
+      ]);
+      await explorationEditor.expectRevisionToHave(2, [
+        'Version No.',
+        'Notes',
+        'User',
+        'Date',
+      ]);
+
+      // Check order of revisions and filter by user
+      await explorationEditor.expectRevisionsToBeOrderedBy(DATE);
+      await explorationEditor.filterRevisionsByUser('explorationEditor');
+      await explorationEditor.expectNumberOfRevisions(5);
+
+      // Adjust paginator and select revisions for comparison
+      await explorationEditor.adjustPaginatorToShowRevisionsPerPage(15);
+      await explorationEditor.expectNextPageOfRevisionsButtonToBe('disabled');
+
+      // Select two revisions for comparison and verify changes in metadata and state
+      await explorationEditor.selectTwoRevisionsForComparison(1, 5);
+      await explorationEditor.expectMetadataChangesInProperties([
+        'title',
+        'goal',
+      ]);
+      await explorationEditor.expectStateChangesInProperties(['content']);
+      await explorationEditor.resetComparisonResults();
+
+      // Download and revert revisions
+      await explorationEditor.downloadRevisionAtIndex(1);
+      await explorationEditor.revertToRevisionAtIndex(2);
+      await explorationEditor.expectReversionOfRevisionAtIndex(5);
+    },
+    DEFAULT_SPEC_TIMEOUT_MSECS
+  );
+
+  afterAll(async function () {
+    await UserFactory.closeAllBrowsers();
+  });
+});
--- core/tests/puppeteer-acceptance-tests/user-utilities/exploration-editor-utils.ts
+++ core/tests/puppeteer-acceptance-tests/user-utilities/exploration-editor-utils.ts
@@ -19,8 +19,17 @@
 import {BaseUser} from '../puppeteer-testing-utilities/puppeteer-utils';
 import testConstants from '../puppeteer-testing-utilities/test-constants';
 import {showMessage} from '../puppeteer-testing-utilities/show-message-utils';
+import {ElementHandle} from 'puppeteer';
 
 const creatorDashboardPage = testConstants.URLs.CreatorDashboard;
+const previewTabButton = '.e2e-test-preview-tab';
+const SettingsTabButton = '.e2e-test-settings-tab';
+const MainTabButton = '.e2e-test-main-tab';
+const HistoryTabButton = '.e2e-test-history-tab';
+const mobileSettingsTabButton = '.e2e-test-mobile-settings-tab';
+const mobilePreviewTabButton = '.e2e-test-mobile-preview-tab';
+const mobileHistoryTabButton = '.e2e-test-mobile-history-tab';
+const mobileMainTabButton = '.e2e-test-mobile-main-tab';
 
 const createExplorationButton = 'button.e2e-test-create-new-exploration-button';
 const dismissWelcomeModalSelector = 'button.e2e-test-dismiss-welcome-modal';
@@ -45,8 +54,6 @@ const mobileSaveChangesButton =
   'button.e2e-test-save-changes-for-small-screens';
 const mobilePublishButton = 'button.e2e-test-mobile-publish-button';
 
-const previewTabButton = '.e2e-test-preview-tab';
-const mobilePreviewTabButton = '.e2e-test-mobile-preview-button';
 const stateEditSelector = '.e2e-test-state-edit-content';
 const stateContentInputField = '.oppia-rte';
 const correctAnswerInTheGroupSelector = '.e2e-test-editor-correctness-toggle';
@@ -75,6 +82,27 @@ const previewRestartButton = '.e2e-test-preview-restart-button';
 const stateConversationContent = '.e2e-test-conversation-content';
 const explorationCompletionToastMessage = '.e2e-test-lesson-completion-message';
 
+// Settings tab elements.
+const explorationTitleInputField = '.e2e-test-exploration-title-input';
+
+// History tab elements.
+const userNameEdit = 'input.e2e-test-history-filter-input';
+const historyListItem = 'div.e2e-test-history-list-item';
+const firstRevisionDropdown = '.e2e-test-history-version-dropdown-first';
+const secondRevisionDropdown = '.e2e-test-history-version-dropdown-second';
+const revertVersionButton = '.e2e-test-revert-version';
+const downloadVersionButton = '.e2e-test-download-version';
+const resetGraphButton = '.e2e-test-reset-graph';
+const confirmRevertVersionButton = '.e2e-test-confirm-revert';
+const paginatorToggler = '.mat-paginator-page-size-select';
+const viewMetadataChangesButton = '.e2e-test-view-metadata-history';
+const testNodeBackground = '.e2e-test-node';
+const closeMetadataModal = '.e2e-test-close-history-metadata-modal';
+const closeStateModal = '.e2e-test-close-history-state-modal';
+const revisionVersionNoSelector = '.e2e-test-history-table-index';
+const revisionNoteSelector = '.e2e-test-history-table-message';
+const revisionUsernameSelector = '.e2e-test-history-table-profile';
+const revisionDateSelector = '.e2e-test-history-tab-commit-date';
 export class ExplorationEditor extends BaseUser {
   /**
    * Function to navigate to creator dashboard page
@@ -425,6 +453,335 @@ export class ExplorationEditor extends BaseUser {
       }
     }
     await this.clickOn(previewRestartButton);
+  }
+
+  async navigateToSettingsTab(): Promise<void> {
+    if (this.isViewportAtMobileWidth()) {
+      await this.clickOn(mobileNavbarDropdown);
+      await this.page.waitForSelector(mobileNavbarPane);
+      await this.clickOn(mobileSettingsTabButton);
+    } else {
+      await this.clickOn(SettingsTabButton);
+    }
+    await this.page.waitForNavigation();
+  }
+
+  async updateExplorationTitle(title: string): Promise<void> {
+    await this.type(explorationTitleInputField, title);
+  }
+
+  async navigateToMainTab(): Promise<void> {
+    if (this.isViewportAtMobileWidth()) {
+      await this.clickOn(mobileNavbarDropdown);
+      await this.page.waitForSelector(mobileNavbarPane);
+      await this.clickOn(mobileMainTabButton);
+    } else {
+      await this.clickOn(MainTabButton);
+    }
+    await this.page.waitForNavigation();
+  }
+
+  async navigateToHistoryTab(): Promise<void> {
+    if (this.isViewportAtMobileWidth()) {
+      await this.clickOn(mobileNavbarDropdown);
+      await this.page.waitForSelector(mobileNavbarPane);
+      await this.clickOn(mobileHistoryTabButton);
+    } else {
+      await this.clickOn(HistoryTabButton);
+    }
+    await this.page.waitForNavigation();
+  }
+
+  /**
+   * This function checks if a specific revision has certain properties.
+   * @param {number} revisionNumber - The number of the revision to check.
+   * @param {string[]} properties - The properties to check for in the revision.
+   */
+  async expectRevisionToHave(
+    revisionNumber: number,
+    properties: string[]
+  ): Promise<void> {
+    let elements = await this.page.$$(historyListItem);
+    if (elements.length < revisionNumber) {
+      throw new Error(
+        `There are not enough revisions. Requested: ${revisionNumber}, available: ${elements.length}`
+      );
+    }
+
+    let element = elements[revisionNumber - 1];
+
+    const propertyToSelector = {
+      'Version No.': revisionVersionNoSelector,
+      Notes: revisionNoteSelector,
+      User: revisionUsernameSelector,
+      Date: revisionDateSelector,
+    };
+
+    for (let property of properties) {
+      let selector = propertyToSelector[property];
+      if (!selector) {
+        throw new Error(`Invalid property: ${property}`);
+      }
+
+      let value = await element.$eval(selector, async el => el.textContent);
+      if (property !== 'Notes' && (!value || value.trim() === '')) {
+        throw new Error(
+          `Revision ${revisionNumber} is missing or has an empty ${property}`
+        );
+      }
+
+      showMessage(`Revision ${revisionNumber} has the property ${property}`);
+    }
+  }
+
+  /**
+   * This function checks if the revisions are ordered by a specific property.
+   * @param {string} property - The property to check the order by.
+   */
+  async expectRevisionsToBeOrderedBy(property: string): Promise<void> {
+    let revisions = await this.page.$$(historyListItem);
+    for (let i = 0; i < revisions.length - 1; i++) {
+      let value1 = revisions[i][property];
+      let value2 = revisions[i + 1][property];
+
+      if (typeof value1 === 'string') {
+        value1 = new Date(value1);
+        value2 = new Date(value2);
+      }
+
+      if (value1 < value2) {
+        throw new Error(`Revisions are not sorted by ${property}`);
+      }
+    }
+
+    showMessage(`Revisions are sorted by ${property}`);
+  }
+
+  /**
+   * This function filters the revisions by a specific user.
+   * @param {string} userName - The name of the user to filter the revisions by.
+   */
+  async filterRevisionsByUser(userName: string): Promise<void> {
+    await this.type(userNameEdit, userName);
+    // The unicode '\u000d' is used to simulate the Enter key press.
+    await this.type(userNameEdit, '\u000d');
+  }
+
+  /**
+   * This function checks if the number of revisions matches the expected number.
+   * @param {number} expectedNumber - The expected number of revisions.
+   */
+  async expectNumberOfRevisions(expectedNumber: number): Promise<void> {
+    let revisions = await this.page.$$(historyListItem);
+    if (revisions.length !== expectedNumber) {
+      throw new Error(
+        `Expected ${expectedNumber} revisions, but found ${revisions.length}`
+      );
+    } else {
+      showMessage(`Found ${expectedNumber} revisions as expected`);
+    }
+  }
+
+  /**
+   * This function adjusts the paginator to show a specific number of revisions per page.
+   *
+   * @param {number} revisionsPerPage - The number of revisions to show per page. Can be 10, 15, or 20.
+   */
+  async adjustPaginatorToShowRevisionsPerPage(
+    revisionsPerPage: number
+  ): Promise<void> {
+    const revisionsToIndex = {
+      10: 1,
+      15: 2,
+      20: 3,
+    };
+
+    const index = revisionsToIndex[revisionsPerPage];
+    if (index === undefined) {
+      throw new Error(
+        `Invalid number of revisions per page: ${revisionsPerPage}`
+      );
+    }
+
+    await this.page.waitForSelector(paginatorToggler, {visible: true});
+    await this.page.click(paginatorToggler);
+    const PaginatorOptionsSelector = '.mat-select-panel';
+    await this.clickOn(
+      `${PaginatorOptionsSelector} mat-option:nth-child(${index})`
+    );
+  }
+
+  /**
+   * Function to check if the next page of revisions button is in a certain status.
+   * @param {string} status - The status to check for.
+   */
+  async expectNextPageOfRevisionsButtonToBe(status: string): Promise<void> {
+    const nextPageButton = await this.page.$('.mat-paginator-navigation-next');
+    if (!nextPageButton) {
+      throw new Error('Next page button not found');
+    }
+    const isDisabled = await nextPageButton.evaluate(
+      button => button.getAttribute('disabled') === 'true'
+    );
+    if (
+      (status === 'disabled' && isDisabled) ||
+      (status === 'enabled' && !isDisabled)
+    ) {
+      showMessage(`Next page button is ${status}`);
+      if (status === 'enabled') {
+        await nextPageButton.click();
+      }
+    } else {
+      throw new Error(
+        `Expected next page button to be ${status}, but it was ${isDisabled ? 'disabled' : 'enabled'}`
+      );
+    }
+  }
+
+  /**
+   * Function to select two revisions of an exploration for comparison.
+   * @param {number} revisionIndex1 - The index of the first revision to select.
+   * @param {number} revisionIndex2 - The index of the second revision to select.
+   */
+  async selectTwoRevisionsForComparison(
+    revisionIndex1: number,
+    revisionIndex2: number
+  ): Promise<void> {
+    await this.page.waitForSelector(firstRevisionDropdown);
+    await this.clickOn(firstRevisionDropdown);
+    const panel1 = '.mat-select-panel';
+    await this.clickOn(
+      `${panel1} mat-option:nth-last-child(${revisionIndex1})`
+    );
+    await this.page.waitForSelector(secondRevisionDropdown);
+    await this.clickOn(secondRevisionDropdown);
+    const panel2 = '#mat-select-2-panel';
+    await this.clickOn(
+      `${panel2} mat-option:nth-last-child(${revisionIndex2})`
+    );
+  }
+
+  /**
+   * Function to verify if changes are reflected for each property.
+   * @param {string[]} properties - The properties to check for.
+   * @param {any[]} codeBlocks - The code blocks to check.
+   */
+  async expectChangesInProperties(
+    properties: string[],
+    codeBlocks: ElementHandle<Element>[]
+  ): Promise<void> {
+    // Function to extract property values from a block.
+    const extractPropertyValues = async block => {
+      const values = {};
+      for (let property of properties) {
+        const element = await block.$x(
+          `.//span[contains(text(), '${property}')]`
+        );
+        if (element[0]) {
+          const line = await element[0].$x('..');
+          const valueElement = await line[0].$('span:nth-child(3)');
+          values[property] = await valueElement?.evaluate(
+            node => node.textContent
+          );
+        }
+      }
+      return values;
+    };
+
+    // Extract property values from both blocks.
+    const propertyValues = await Promise.all(
+      codeBlocks.map(extractPropertyValues)
+    );
+
+    // Check if the property values are the same in both blocks.
+    properties.forEach(property => {
+      if (propertyValues[0][property] !== propertyValues[1][property]) {
+        throw new Error(`Changes are not reflected in ${property}`);
+      } else {
+        showMessage(`Changes are reflected in ${property}.`);
+      }
+    });
+  }
+
+  /**
+   * Function to verify if metadata changes are reflected for each property.
+   * @param {string[]} properties - The properties to check for.
+   */
+  async expectMetadataChangesInProperties(properties: string[]): Promise<void> {
+    await this.page.waitForSelector(viewMetadataChangesButton, {visible: true});
+    await this.clickOn(viewMetadataChangesButton);
+    const codeBlocks = await this.page.$$('.CodeMirror-code');
+
+    await this.expectChangesInProperties(properties, codeBlocks);
+
+    await this.clickOn(closeMetadataModal);
+  }
+
+  /**
+   * Function to verify if exploration state changes are reflected for each property.
+   * @param {string[]} properties - The properties to check for.
+   */
+  async expectStateChangesInProperties(properties: string[]): Promise<void> {
+    await this.clickOn(testNodeBackground);
+    const codeBlocks = await this.page.$$('.CodeMirror-code');
+
+    await this.expectChangesInProperties(properties, codeBlocks);
+
+    await this.clickOn(closeStateModal);
+  }
+
+  /**
+   * Function to reset the comparison results.
+   */
+  async resetComparisonResults(): Promise<void> {
+    await this.page.waitForSelector(resetGraphButton);
+    await this.clickOn(resetGraphButton);
+  }
+
+  /**
+   * Function to download a specific revision.
+   * @param {number} index - The index of the revision to be downloaded.
+   */
+  async downloadRevisionAtIndex(index: number): Promise<void> {
+    const buttons = await this.page.$$('.history-table-option');
+    await buttons[index].click();
+    await this.page.waitForSelector(downloadVersionButton);
+    await this.clickOn(downloadVersionButton);
+  }
+
+  /**
+   * Function to revert to a specific revision.
+   * @param {number} index - The index of the revision to revert to.
+   */
+  async revertToRevisionAtIndex(index: number): Promise<void> {
+    const buttons = await this.page.$$('.history-table-option');
+    await buttons[index].click();
+    await this.clickOn(revertVersionButton);
+    await this.clickOn(confirmRevertVersionButton);
+  }
+
+  /**
+   * Function to verify if a revision is reverting and downloading or not.
+   * @param {number} index - The index of the revision to check.
+   */
+  async expectReversionOfRevisionAtIndex(index: number): Promise<void> {
+    await this.page.waitForSelector(historyListItem);
+    let element = await this.page.$(historyListItem);
+    if (element === null) {
+      throw new Error('No Revisions found');
+    }
+    let notes = await element.$eval(
+      revisionNoteSelector,
+      async el => el.textContent
+    );
+    if (notes === null) {
+      throw new Error('Revision does not contain a Note');
+    }
+    if (notes === ` Reverted exploration to version ${index + 1} `) {
+      showMessage('Revision is reverting successfully');
+    } else {
+      throw new Error('Revision is not reverting');
+    }
   }
 }
 
