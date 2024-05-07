a90c72cdaec2b770fee5f75deb297d144af61b2c
Akhilesh-max <2827daya@gmail.com>
2024-05-07 18:05:44 +0530

    updates

--- core/tests/puppeteer-acceptance-tests/puppeteer-testing-utilities/puppeteer-utils.ts
+++ core/tests/puppeteer-acceptance-tests/puppeteer-testing-utilities/puppeteer-utils.ts
@@ -421,7 +421,7 @@ export class BaseUser {
   async waitForText(text: string, timeout: number = 30000): Promise<void> {
     try {
       await this.page.waitForFunction(
-        textToFind => {
+        (textToFind: string) => {
           const bodyText = document.querySelector('body')?.innerText || '';
           return bodyText.includes(textToFind);
         },
