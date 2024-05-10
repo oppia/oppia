37469c663bcc00b29ba5f081f57ee77f41471ae3
Justin Nguyen <70992422+jnvtnguyen@users.noreply.github.com>
2024-05-10 08:41:50 -0700

    Fix part of #20286: Lighthouse flake minScore for deprecations on donate page. (#20287)
    * Updates
    
    * Updates
    
    * Update .lighthouserc-1.js

--- .lighthouserc-1.js
+++ .lighthouserc-1.js
@@ -88,7 +88,9 @@ module.exports = {
             // The YouTube embed on donate page uses passive listeners.
             'uses-passive-event-listeners': ['error', {minScore: 0}],
             'uses-rel-preload': ['error', {minScore: 1}],
-            deprecations: ['error', {minScore: 1}],
+            // TODO(#20286): There is a deprecated API on the /donate page due to the
+            // donorbox script, change the minScore to 1 once it is fixed.
+            deprecations: ['error', {minScore: 0}],
             redirects: ['error', {minScore: 1}],
             'uses-responsive-images': ['error', {minScore: 0.8}],
           },
