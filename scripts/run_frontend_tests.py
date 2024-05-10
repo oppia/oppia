78279e0026508699eee23b3e27fc5be0390d1d4f
Gabriel Fuentes <12247834+Helper2020@users.noreply.github.com>
2024-05-10 15:42:48 -0400

    Removed string concatenation run_frontend_tests.py

--- scripts/run_frontend_tests.py
+++ scripts/run_frontend_tests.py
@@ -176,7 +176,7 @@ def main(args: Optional[Sequence[str]] = None) -> None:
                 parsed_args.download_combined_frontend_spec_file):
                 download_task = subprocess.Popen(
                     ['wget',
-                    'http://localhost:9876/base/core/templates/' +
+                    'http://localhost:9876/base/core/templates/'
                     'combined-tests.spec.js',
                     '-P',
                     os.path.join('../karma_coverage_reports')])
