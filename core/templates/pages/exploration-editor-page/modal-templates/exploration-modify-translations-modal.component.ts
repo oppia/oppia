30cb5562e18ec71faa9c0ffd031d05927f04b034
Vir Kothari <virkothari8@gmail.com>
2024-06-07 19:40:02 +0530

    Create frontend modal with list of translations populated

---
+++ core/templates/pages/exploration-editor-page/modal-templates/exploration-modify-translations-modal.component.ts
@@ -0,0 +1,65 @@
+// Copyright 2024 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Component for updating translations of edited content.
+ */
+
+import {Component, Input} from '@angular/core';
+import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import {ConfirmOrCancelModal} from 'components/common-layout-directives/common-elements/confirm-or-cancel-modal.component';
+import {ContextService} from 'services/context.service';
+import {EntityBulkTranslationsBackendApiService} from '../services/entity-bulk-translations-backend-api.service';
+import {LanguageCodeToEntityTranslations} from 'services/entity-translations.services';
+
+@Component({
+  selector: 'oppia-exploration-modify-translations-modal',
+  templateUrl: './exploration-modify-translations-modal.component.html',
+})
+export class ModifyTranslationsModalComponent extends ConfirmOrCancelModal {
+  @Input() contentId!: string;
+  explorationId!: string;
+  explorationVersion!: number;
+  contentTranslations: LanguageCodeToEntityTranslations = {};
+
+  constructor(
+    private ngbActiveModal: NgbActiveModal,
+    private contextService: ContextService,
+    private entityBulkTranslationsBackendApiService: EntityBulkTranslationsBackendApiService
+  ) {
+    super(ngbActiveModal);
+  }
+
+  ngOnInit(): void {
+    this.explorationId = this.contextService.getExplorationId();
+    this.explorationVersion = this.contextService.getExplorationVersion();
+
+    this.entityBulkTranslationsBackendApiService
+      .fetchEntityBulkTranslationsAsync(
+        this.explorationId,
+        'exploration',
+        this.explorationVersion
+      )
+      .then(response => {
+        for (let language in response) {
+          let language_translations = response[language]['translations'];
+
+          if (language_translations[this.contentId]) {
+            this.contentTranslations[language] =
+              language_translations[this.contentId]['content_value'];
+          }
+        }
+      });
+  }
+}
