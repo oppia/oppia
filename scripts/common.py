209b8ac11bfa369222a83135df0b39a33ea270bf
rahat2134 <arnavkhan4343@gmail.com>
2024-06-10 23:28:31 +0530

    blog-editor-create-delete-drafted-post

--- scripts/common.py
+++ scripts/common.py
@@ -213,6 +213,7 @@ CHROME_PATHS = [
 
 ACCEPTANCE_TESTS_SUITE_NAMES = [
     'blog-admin/assign-roles-to-users-and-change-tag-properties',
+    'blog-editor/create-and-delete-draft-blog-post',
     'blog-editor/try-to-publish-a-duplicate-blog-post-and-get-blocked',
     'curriculum-admin/create-and-publish-topics-and-stories',
     'exploration-editor/create-exploration-and-change-basic-settings',
