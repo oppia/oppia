e47bfb8ebdc599619790c39e8fd9337ada2ac9d2
chris7716 <hkraclh@gmail.com>
2024-06-11 15:47:40 +0530

    Dummy change

--- core/templates/pages/contributor-dashboard-page/modal-templates/translation-suggestion-review-modal.component.ts
+++ core/templates/pages/contributor-dashboard-page/modal-templates/translation-suggestion-review-modal.component.ts
@@ -113,6 +113,7 @@ export class TranslationSuggestionReviewModalComponent implements OnInit {
   // These properties are initialized using Angular lifecycle hooks
   // and we need to do non-null assertion. For more information, see
   // https://github.com/oppia/oppia/wiki/Guide-on-defining-types#ts-7-1
+  // This is to undo feature.
   activeContribution!: ActiveContributionDict;
   authorName!: string;
   activeSuggestion!: ActiveSuggestionDict;
