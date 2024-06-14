32879815de89bf436e1a1ebc8e69c5206c15fe9a
Akhilesh-max <2827daya@gmail.com>
2024-06-14 23:54:31 +0530

    updates

--- scripts/common.py
+++ scripts/common.py
@@ -238,7 +238,7 @@ ACCEPTANCE_TESTS_SUITE_NAMES = [
     'translation-admin/add-and-remove-translation-rights',
     'voiceover-admin/add-voiceover-artist-to-an-exploration',
     'super-admin/edit-user-roles',
-    'super-admin/load-dummy-data-in-dev-mode',,
+    'super-admin/load-dummy-data-in-dev-mode',
     'moderator/view-recent-commits-and-feedback-messages',
     'moderator/edit-featured-activities-list'
 ]
