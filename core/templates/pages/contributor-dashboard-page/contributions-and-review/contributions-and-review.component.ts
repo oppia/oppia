bf38a6ae9c11486da4d63860958992d9bed211a5
chris7716 <hkraclh@gmail.com>
2024-06-15 23:48:28 +0530

    Add dummy comment

--- core/templates/pages/contributor-dashboard-page/contributions-and-review/contributions-and-review.component.ts
+++ core/templates/pages/contributor-dashboard-page/contributions-and-review/contributions-and-review.component.ts
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+// This is to test undo feature.
 
 /**
  * @fileoverview Component for showing and reviewing contributions.
