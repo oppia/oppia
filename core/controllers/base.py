21990963a227c55b3d5195d032dbbea4fdf3b114
Mohit <mohitruwatia2312@gmail.com>
2024-06-22 19:14:40 +0530

    Fixed linting errors

--- core/controllers/base.py
+++ core/controllers/base.py
@@ -850,8 +850,8 @@ class BaseHandler(
         if isinstance(exception, TypeError):
             self.error(405)
             values = {
-                'error': 'Invalid method %s for %s or Extra Parameters passed.' % (
-                    request_method, handler_class_name ),
+                'error': 'Invalid method %s for %s/ Extra Parameters passed.' % (
+                    request_method, handler_class_name),
                 'status_code': 405
             }
             self._render_exception(values)
