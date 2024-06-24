a296c6035d1ddafd41059c1d6c56673563e173ee
Akhilesh-max <2827daya@gmail.com>
2024-06-24 23:45:06 +0530

    updates

---
+++ core/tests/puppeteer-acceptance-tests/specs/topic-manager/edit-and-preview-a-subtopic.spec.ts
@@ -0,0 +1,74 @@
+// Copyright 2024 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Acceptance Test for the journey of a topic manager. The journey includes adding a sub-topic to a topic, assigning skills to a sub-topic, changing the assignments and re-publishing the topic, opening an existing sub-topic, modifying its data and publishing it again, and previewing the sub-topic in the preview tab.
+ */
+
+import {UserFactory} from '../../utilities/common/user-factory';
+import testConstants from '../../utilities/common/test-constants';
+import {TopicManager} from '../../utilities/user/topic-manager';
+import {CurriculumAdmin} from '../../utilities/user/curriculum-admin';
+
+const DEFAULT_SPEC_TIMEOUT_MSECS = testConstants.DEFAULT_SPEC_TIMEOUT_MSECS;
+const ROLES = testConstants.Roles;
+
+describe('Topic Manager User Journey', function () {
+  let topicManager: TopicManager;
+  let curriculumAdmin: CurriculumAdmin;
+
+  beforeAll(async function () {
+    topicManager = await UserFactory.createNewUser(
+      'topicManager',
+      'topic_manager@example.com',
+      [ROLES.TOPIC_MANAGER]
+    );
+
+    curriculumAdmin = await UserFactory.createNewUser(
+      'curriculumAdmin',
+      'curriculum_Admin@example.com',
+      [ROLES.CURRICULUM_ADMIN]
+    );
+
+    curriculumAdmin.createSkill('Test Skill 1', 'Mathematics');
+    curriculumAdmin.createSkill('Test Skill 2', 'Mathematics');
+  }, DEFAULT_SPEC_TIMEOUT_MSECS);
+
+  it(
+    'should add a sub-topic to a topic, assign skills to the sub-topic, change the assignments and re-publish the topic, open an existing sub-topic, modify its data and publish it again, and preview the sub-topic in the preview tab.',
+    async function () {
+      await topicManager.navigateToTopicDashboardPage();
+      await topicManager.addSubTopic('Sub-Topic 1');
+      await topicManager.assignSkillsToSubTopic('Sub-Topic 1', [
+        'Test Skill 1',
+        'Test Skill 2',
+      ]);
+      await topicManager.changeAssignmentsAndPublishTopic();
+
+      await topicManager.openExistingSubTopic('Sub-Topic 1');
+      await topicManager.modifySubTopicData(
+        'Sub-Topic 1',
+        'Updated Sub-Topic 1'
+      );
+      await topicManager.publishSubTopic();
+
+      await topicManager.previewSubTopicInPreviewTab('Sub-Topic 1');
+    },
+    DEFAULT_SPEC_TIMEOUT_MSECS
+  );
+
+  afterAll(async function () {
+    await UserFactory.closeAllBrowsers();
+  });
+});
---
+++ core/tests/puppeteer-acceptance-tests/specs/topic-manager/edit-and-preview-a-topic.spec.ts
@@ -0,0 +1,60 @@
+// Copyright 2024 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Acceptance Test for the journey of a topic manager to edit topic details, manage practice tab visibility, and preview lesson, practice, and revision.
+ */
+
+import {UserFactory} from '../../utilities/common/user-factory';
+import testConstants from '../../utilities/common/test-constants';
+import {TopicManager} from '../../utilities/user/topic-manager';
+
+const DEFAULT_SPEC_TIMEOUT_MSECS = testConstants.DEFAULT_SPEC_TIMEOUT_MSECS;
+const ROLES = testConstants.Roles;
+
+describe('Topic Manager User Journey', function () {
+  let topicManager: TopicManager;
+
+  beforeAll(async function () {
+    topicManager = await UserFactory.createNewUser(
+      'topicManager',
+      'topic_manager@example.com',
+      [ROLES.TOPIC_MANAGER]
+    );
+  }, DEFAULT_SPEC_TIMEOUT_MSECS);
+
+  it(
+    'should be able to edit topic name, thumbnail, description, page title fragment for web, and meta tags, enable or disable the practice tab to learners, and preview lesson, practice, and revision in the topic preview.',
+    async function () {
+      await topicManager.navigateToTopicDashboardPage();
+      await topicManager.openTopicEditor('Topic 1');
+      await topicManager.editTopicDetails('Topic 1', {
+        name: 'Updated Topic 1',
+        thumbnail: 'updated_thumbnail.png',
+        description: 'Updated description',
+        pageTitleFragment: 'Updated Page Title Fragment',
+        metaTags: 'updated, meta, tags',
+      });
+
+      await topicManager.previewLessonInTopicPreview('Topic 1');
+      await topicManager.previewPracticeInTopicPreview('Topic 1');
+      await topicManager.previewRevisionInTopicPreview('Topic 1');
+    },
+    DEFAULT_SPEC_TIMEOUT_MSECS
+  );
+
+  afterAll(async function () {
+    await UserFactory.closeAllBrowsers();
+  });
+});
