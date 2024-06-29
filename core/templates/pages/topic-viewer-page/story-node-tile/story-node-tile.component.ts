e82edc4d8ce3da004a44314aad5518f2226e4ce5
pritam2317 <pritsingh2317@gmail.com>
2024-06-29 13:39:26 +0530

    fixing typescript issue

--- core/templates/pages/topic-viewer-page/story-node-tile/story-node-tile.component.ts
+++ core/templates/pages/topic-viewer-page/story-node-tile/story-node-tile.component.ts
@@ -58,16 +58,15 @@ export class StoryNodeTileComponent implements OnInit {
   @Input() storyUrlFragment!: string;
   thumbnailFilename!: string | null;
   thumbnailBgColor!: string | null;
-  thumbnailUrl: string;
+  thumbnailUrl: string = '';
   nodeId!: string;
   pathIconParameters: IconParametersArray[] = [];
   EXPLORE_PAGE_PREFIX = '/explore/';
-  explorationId!: string | null;
+  explorationId!: string;
   expStates!: StateObjectsBackendDict;
   checkpointCount!: number;
   mostRecentlyReachedCheckpoint!: string;
   prevSessionStatesProgress: string[] = [];
-  pinIdUrl: string;
   completedcheckpoints: number = 0;
   nodeDataLoaded = false;
   totalCheckpointLoaded = false;
