fec9c29a5fc3d5c714bc22ef552cc269e6221237
Akhilesh-max <2827daya@gmail.com>
2024-07-02 20:08:40 +0530

    updates

--- core/feature_flag_list.py
+++ core/feature_flag_list.py
@@ -85,7 +85,6 @@ DEV_FEATURES_LIST = [
     FeatureNames.SHOW_TRANSLATION_SIZE,
     FeatureNames.NEW_LESSON_PLAYER,
     FeatureNames.EXPLORATION_EDITOR_CAN_TAG_MISCONCEPTIONS,
-    FeatureNames.ENABLE_MULTIPLE_CLASSROOMS,
     FeatureNames.REDESIGNED_TOPIC_VIEWER_PAGE
 ]
 
@@ -255,7 +254,7 @@ FEATURE_FLAG_NAME_TO_DESCRIPTION_AND_FEATURE_STAGE = {
         (
             'The flag enables flow for multiple classrooms '
             'and makes the classrooms page available to learners.',
-            feature_flag_domain.ServerMode.DEV
+            feature_flag_domain.ServerMode.TEST
         )
     ),
     FeatureNames.REDESIGNED_TOPIC_VIEWER_PAGE.value: (
