d443ca51d8a13094d045204382c0d3832eba456d
Joao Manita <joao.c.manita@tecnico.ulisboa.pt>
2024-07-04 20:28:19 +0100

    add html and css files sor subject selectors

--- core/templates/pages/library-page/search-selectors/subject-selector-modal.component.spec.ts
+++ core/templates/pages/library-page/search-selectors/subject-selector-modal.component.spec.ts
@@ -23,21 +23,40 @@ import {I18nLanguageCodeService} from 'services/i18n-language-code.service';
 import {SubjectSelectorModalComponent} from 'pages/library-page/search-selectors/subject-selector-modal.component';
 import {FormsModule} from '@angular/forms';
 import {MockTranslatePipe} from 'tests/unit-test-utils';
+import {TranslateService} from '@ngx-translate/core';
 import {MaterialModule} from 'modules/material.module';
 import {SearchService} from 'services/search.service';
+import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+
+class MockTranslateService {
+  onLangChange: EventEmitter<string> = new EventEmitter();
+
+  instant(key: string, interpolateParams?: Object): string {
+    return key;
+  }
+}
 
 describe('subject-selector-modal component', () => {
   let i18nLanguageCodeService: I18nLanguageCodeService;
+  let translateService: TranslateService;
   let component: SubjectSelectorModalComponent;
   let searchService: SearchService;
   let fixture: ComponentFixture<SubjectSelectorModalComponent>;
   let preferredLanguageCodesLoadedEmitter = new EventEmitter();
   let selectionDetailsStub: SelectionDetails;
+  let ngbActiveModal: NgbActiveModal;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule, FormsModule, MaterialModule],
       declarations: [SubjectSelectorModalComponent, MockTranslatePipe],
+      providers: [
+        {
+          provide: TranslateService,
+          useClass: MockTranslateService,
+        },
+        NgbActiveModal,
+      ],
     }).compileComponents();
   }));
 
@@ -94,13 +113,74 @@ describe('subject-selector-modal component', () => {
       i18nLanguageCodeService,
       'onPreferredLanguageCodesLoaded'
     ).and.returnValue(preferredLanguageCodesLoadedEmitter);
+    translateService = TestBed.inject(TranslateService);
     searchService = TestBed.inject(SearchService);
+    ngbActiveModal = TestBed.inject(NgbActiveModal);
     component.ngOnInit();
     fixture.detectChanges();
     searchService.selectionDetails = selectionDetailsStub;
   });
 
-  it('should be able to initialize component', () => {
+  it('should update selection details if there are no selections', () => {
+    spyOn(translateService, 'instant').and.returnValue('key');
+    component.updateSelectionDetails('categories');
+    let selectionDetails = component.selectionDetails;
+    expect(selectionDetails.categories.numSelections).toEqual(0);
+  });
+
+  it('should update selection details', () => {
+    component.tempSelectionDetails = selectionDetailsStub;
+    component.updateSelectionDetails('categories');
+    expect(component.selectionDetails).toEqual(selectionDetailsStub);
+  });
+
+  it('should initialize', () => {
     component.ngOnInit();
+    expect(component.tempSelectionDetails).toEqual(selectionDetailsStub);
+  });
+
+  it('should detect selections', () => {
+    spyOn(component, 'updateSelectionDetails');
+
+    component.toggleSelection('categories', 'Algorithms');
+    component.updateSelectionDetails('categories');
+    expect(
+      component.tempSelectionDetails.categories.selections.Algorithms
+    ).toEqual(true);
+    component.toggleSelection('categories', 'Algorithms');
+    expect(
+      component.tempSelectionDetails.categories.selections.Algorithms
+    ).toEqual(false);
+  });
+
+  it('should contain "Algorithms" in categories masterList', () => {
+    const containsAlgorithms =
+      component.selectionDetails.categories.masterList.some(
+        category => category.id === 'Algorithms'
+      );
+    expect(containsAlgorithms).toBe(true);
+  });
+
+  it('should clear all selections', () => {
+    component.toggleSelection('categories', 'Algorithms');
+    component.clearAll();
+    expect(component.tempSelectionDetails.categories.selections).toEqual({});
+  });
+
+  it('should apply selections', () => {
+    component.toggleSelection('categories', 'Algorithms');
+    component.applySelections();
+    expect(
+      searchService.selectionDetails.categories.selections.Algorithms
+    ).toBe(true);
+  });
+
+  it('should apply filters on close', () => {
+    const dismissSpy = spyOn(ngbActiveModal, 'dismiss').and.callThrough();
+    spyOn(searchService, 'triggerSearch');
+    component.toggleSelection('categories', 'id');
+    component.applySelections();
+    expect(searchService.triggerSearch).toHaveBeenCalled();
+    expect(dismissSpy).toHaveBeenCalled();
   });
 });
--- core/templates/pages/library-page/search-selectors/subject-selector-modal.component.ts
+++ core/templates/pages/library-page/search-selectors/subject-selector-modal.component.ts
@@ -16,17 +16,126 @@
  * @fileoverview Component for the SubjectSelectorModal.
  */
 
+import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 import {Component, OnInit} from '@angular/core';
 import {downgradeComponent} from '@angular/upgrade/static';
+import {SearchService, SelectionDetails} from 'services/search.service';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'oppia-subject-selector-modal',
   templateUrl: './subject-selector-modal.component.html',
 })
 export class SubjectSelectorModalComponent implements OnInit {
-  constructor() {}
+  translationData: Record<string, number> = {};
+  tempSelectionDetails: SelectionDetails = {
+    categories: {
+      description: '',
+      itemsName: 'categories',
+      masterList: [], // Should be initialized by the component.
+      selections: {},
+      numSelections: 0,
+      summary: '',
+    },
+    languageCodes: {
+      description: '',
+      itemsName: 'languages',
+      masterList: [], // Should be initialized by the component.
+      selections: {},
+      numSelections: 0,
+      summary: '',
+    },
+  };
 
-  ngOnInit(): void {}
+  constructor(
+    public activeModal: NgbActiveModal,
+    private searchService: SearchService,
+    private translateService: TranslateService
+  ) {}
+
+  closeModal(): void {
+    this.clearAll();
+    this.activeModal.dismiss();
+  }
+
+  applySelections(): void {
+    this.searchService.selectionDetails = JSON.parse(
+      JSON.stringify(this.tempSelectionDetails)
+    );
+    this.searchService.triggerSearch();
+    this.closeModal();
+  }
+
+  clearAll(): void {
+    this.tempSelectionDetails.categories.selections = {};
+    this.tempSelectionDetails.categories.numSelections = 0;
+  }
+
+  // Update the description, numSelections and summary fields of the
+  // relevant entry of selectionDetails.
+  updateSelectionDetails(itemsType: string): void {
+    let selectionDetails = this.tempSelectionDetails;
+    let itemsName = selectionDetails[itemsType].itemsName;
+    let masterList = selectionDetails[itemsType].masterList;
+
+    let selectedItems = [];
+    for (let i = 0; i < masterList.length; i++) {
+      if (selectionDetails[itemsType].selections[masterList[i].id]) {
+        selectedItems.push(masterList[i].text);
+      }
+    }
+
+    let totalCount = selectedItems.length;
+    selectionDetails[itemsType].numSelections = totalCount;
+
+    selectionDetails[itemsType].summary =
+      totalCount === 0
+        ? 'I18N_LIBRARY_ALL_' + itemsName.toUpperCase()
+        : totalCount === 1
+          ? selectedItems[0]
+          : 'I18N_LIBRARY_N_' + itemsName.toUpperCase();
+    this.translationData[itemsName + 'Count'] = totalCount;
+
+    if (selectedItems.length > 0) {
+      let translatedItems = [];
+      for (let i = 0; i < selectedItems.length; i++) {
+        translatedItems.push(this.translateService.instant(selectedItems[i]));
+      }
+      selectionDetails[itemsType].description = translatedItems.join(', ');
+    } else {
+      selectionDetails[itemsType].description =
+        'I18N_LIBRARY_ALL_' + itemsName.toUpperCase() + '_SELECTED';
+    }
+  }
+
+  get selectionDetails(): SelectionDetails {
+    return this.searchService.selectionDetails;
+  }
+
+  toggleSelection(itemsType: string, optionName: string): void {
+    let selectionDetails = this.tempSelectionDetails;
+    let selections = selectionDetails[itemsType].selections;
+
+    if (!selections.hasOwnProperty(optionName)) {
+      // Initialize the selection as false if it doesn't exist.
+      selections[optionName] = false;
+    }
+
+    // Toggle the selection state.
+    selections[optionName] = !selections[optionName];
+
+    this.updateSelectionDetails(itemsType);
+  }
+
+  ngOnInit(): void {
+    let selectionDetails = this.selectionDetails;
+
+    // Non-translatable parts of the html strings, like numbers or user
+    // names.
+    this.translationData = {};
+
+    this.tempSelectionDetails = JSON.parse(JSON.stringify(selectionDetails));
+  }
 }
 
 angular.module('oppia').directive(
