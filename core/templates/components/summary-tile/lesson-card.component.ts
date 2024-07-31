f2c81fc0d4819f4d6f0a6f773e6ae644d8377ca7
Amy Yeung <amyyeung17@gmail.com>
2024-07-31 18:15:32 -0400

    Migrate classes to learner dashboard css and removed shared file

--- core/templates/components/summary-tile/lesson-card.component.ts
+++ core/templates/components/summary-tile/lesson-card.component.ts
@@ -30,9 +30,6 @@ import {StoryNode} from 'domain/story/story-node.model';
 @Component({
   selector: 'lesson-card',
   templateUrl: './lesson-card.component.html',
-  styleUrls: [
-    '../../pages/learner-dashboard-page/learner-dashboard-shared.css',
-  ],
 })
 export class LessonCardComponent implements OnInit {
   @Input() story!: StorySummary | LearnerExplorationSummary | CollectionSummary;
--- core/templates/pages/learner-dashboard-page/home-tab.component.ts
+++ core/templates/pages/learner-dashboard-page/home-tab.component.ts
@@ -32,7 +32,7 @@ import './home-tab.component.css';
 @Component({
   selector: 'oppia-home-tab',
   templateUrl: './home-tab.component.html',
-  styleUrls: ['./home-tab.component.css', './learner-dashboard-shared.css'],
+  styleUrls: ['./home-tab.component.css'],
 })
 export class HomeTabComponent {
   @Output() setActiveSection: EventEmitter<string> = new EventEmitter();
