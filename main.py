0bf2f3f1d2ff759d89958cf5d40cf0c4180cac4c
Mohit Ruwatia <120438312+mon4our@users.noreply.github.com>
2024-08-06 21:55:29 +0530

    Fixed 404 error

--- main.py
+++ main.py
@@ -1221,6 +1221,10 @@ URLS.extend((
     get_redirect_route(
         r'%s/<author_username>' % feconf.BLOG_AUTHOR_PROFILE_PAGE_URL_PREFIX,
         oppia_root.OppiaRootPage
+    ),
+    get_redirect_route(
+        r'%s/<skill_id>' % feconf.SKILL_EDITOR_URL_PREFIX,
+        oppia_root.OppiaRootPage
     )
 ))
 
