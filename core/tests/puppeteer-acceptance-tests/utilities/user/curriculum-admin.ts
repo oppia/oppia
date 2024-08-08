2a78819e224ec2706676df559bbf038f5febef28
Akhilesh Kr. <2827daya@gmail.com>
2024-08-08 17:03:01 +0530

    Fixes #20725, #20750, #20687, #20643 and #20253 acceptance test flakes.  (#20785)
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates

--- core/tests/puppeteer-acceptance-tests/utilities/user/curriculum-admin.ts
+++ core/tests/puppeteer-acceptance-tests/utilities/user/curriculum-admin.ts
@@ -224,7 +224,7 @@ export class CurriculumAdmin extends BaseUser {
    */
   async navigateToTopicAndSkillsDashboardPage(): Promise<void> {
     await this.page.bringToFront();
-    await this.page.waitForNetworkIdle();
+    await this.waitForNetworkIdle();
     await this.goto(topicAndSkillsDashboardUrl);
   }
 
@@ -936,7 +936,7 @@ export class CurriculumAdmin extends BaseUser {
     const pathSegments = url.pathname.split('/');
     const storyId = pathSegments[pathSegments.length - 1];
     showMessage(`Story ${storyTitle} is created.`);
-    await this.page.waitForNetworkIdle();
+    await this.waitForNetworkIdle();
 
     return storyId;
   }
@@ -1298,7 +1298,7 @@ export class CurriculumAdmin extends BaseUser {
    */
   async navigateToClassroomAdminPage(): Promise<void> {
     await this.page.bringToFront();
-    await this.page.waitForNetworkIdle();
+    await this.waitForNetworkIdle();
     await this.goto(classroomAdminUrl);
   }
 
