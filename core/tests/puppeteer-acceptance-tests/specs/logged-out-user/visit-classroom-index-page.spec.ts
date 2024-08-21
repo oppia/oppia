765e0332692b93454c737347cb58f20a9a9c34fb
Afzal Khan <tricktomade@gmail.com>
2024-08-21 13:05:33 +0000

    add await

--- core/tests/puppeteer-acceptance-tests/specs/logged-out-user/visit-classroom-index-page.spec.ts
+++ core/tests/puppeteer-acceptance-tests/specs/logged-out-user/visit-classroom-index-page.spec.ts
@@ -49,7 +49,7 @@ describe('Logged-out User', function () {
       [ROLES.RELEASE_COORDINATOR]
     );
 
-    releaseCoordinator.enableFeatureFlag('enable_multiple_classrooms');
+    await releaseCoordinator.enableFeatureFlag('enable_multiple_classrooms');
   }, DEFAULT_SPEC_TIMEOUT_MSECS);
 
   it(
