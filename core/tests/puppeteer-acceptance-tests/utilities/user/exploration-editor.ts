d262dae41656a842cd3d3d919e30b559ae97c7b0
Vir Kothari <virkothari8@gmail.com>
2024-08-23 21:59:42 +0530

    Fix

--- core/tests/puppeteer-acceptance-tests/utilities/user/exploration-editor.ts
+++ core/tests/puppeteer-acceptance-tests/utilities/user/exploration-editor.ts
@@ -1075,11 +1075,14 @@ export class ExplorationEditor extends BaseUser {
       await this.page.waitForSelector(addNewResponseButton, {
         visible: true,
       });
-      await this.page.waitForTimeout(1000);
       await this.clickOn(addNewResponseButton);
-      await this.page.waitForSelector(responseModalHeaderSelector, {
-        hidden: true,
-      });
+      await this.page
+        .waitForSelector(responseModalHeaderSelector, {
+          hidden: true,
+        })
+        .catch(async () => {
+          await this.clickOn(addNewResponseButton);
+        });
     } else {
       await this.clickOn(addAnotherResponseButton);
     }
