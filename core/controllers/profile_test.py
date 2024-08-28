f1e8409cd0b4fca5bb7a9e30f4e6383180357a82
Mohit Ruwatia <mohitruwatia2312@gmail.com>
2024-08-28 18:51:10 +0530

    Fixed failing linter tests

--- core/controllers/profile_test.py
+++ core/controllers/profile_test.py
@@ -1157,8 +1157,8 @@ class MailingListSubscriptionHandlerTests(test_utils.GenericTestBase):
         # mock_add_user_to_raise_exception() will be unused as they are
         # just to prevent a TypeError from being thrown, which in turn
         # will give a 405 status code.
-        def mock_add_user_to_raise_exception( unused_email: Any,
-         unused_tag: Any, name=None) -> None:
+        def mock_add_user_to_raise_exception(
+                unused_email: Any, unused_tag: Any, name=None) -> None:
             raise Exception('Backend error')
         swap_add_fn = self.swap(
             user_services, 'add_user_to_mailing_list',
