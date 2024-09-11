3054d129b134790ce0292b763a66fae05fe79be1
rishabh.sharma <rishabh.sharma@createbytes.com>
2024-09-11 21:57:40 +0530

    fix: add character limits to chapter editor for title and description

--- core/templates/pages/story-editor-page/editor-tab/story-node-editor.component.ts
+++ core/templates/pages/story-editor-page/editor-tab/story-node-editor.component.ts
@@ -57,6 +57,8 @@ export class StoryNodeEditorComponent implements OnInit, OnDestroy {
   @Input() acquiredSkillIds: string[];
   @Input() plannedPublicationDateMsecs: number;
 
+  MAX_CHARS_IN_EXPLORATION_TITLE = 36; 
+  MAX_CHARS_IN_CHAPTER_DESCRIPTION = 156; 
   chapterPreviewCardIsShown = false;
   mainChapterCardIsShown = true;
   explorationInputButtonsAreShown = false;
