510e435412adcef07401b159ea4fa17ca7ca133d
Priyanshu Verma <priyanshuverma@outlook.in>
2024-09-17 16:04:26 +0530

    Fix part of #19435: Review test page migration (#20790)
    * review tests page migration
    
    * *lint fix
    
    * lint fix
    
    * Reverting import changes
    
    ---------
    
    Co-authored-by: Akhilesh Kr. <2827daya@gmail.com>
    Co-authored-by: Hardik Goyal <135348086+HardikGoyal2003@users.noreply.github.com>

--- assets/constants.ts
+++ assets/constants.ts
@@ -6647,6 +6647,24 @@ export default {
         }
       ]
     },
+    "REVIEW_TEST": {
+      "ROUTE": "learn/:classroom_url_fragment/:topic_url_fragment/review-test/:story_url_fragment",
+      "TITLE": "Oppia",
+      "META": [
+        {
+          "PROPERTY_TYPE": "itemprop",
+          "PROPERTY_VALUE": "description",
+          // eslint-disable-next-line max-len
+          "CONTENT": "With Oppia, you can access free lessons on math, physics, statistics, chemistry, music, history and more from anywhere in the world. Oppia is a nonprofit with the mission of providing high-quality education to those who lack access to it."
+        },
+        {
+          "PROPERTY_TYPE": "itemprop",
+          "PROPERTY_VALUE": "og:description",
+          // eslint-disable-next-line max-len
+          "CONTENT": "With Oppia, you can access free lessons on math, physics, statistics, chemistry, music, history and more from anywhere in the world. Oppia is a nonprofit with the mission of providing high-quality education to those who lack access to it."
+        }
+      ]
+    },
     "SUBTOPIC_VIEWER": {
       "ROUTE": "learn/:classroom_url_fragment/:topic_url_fragment/revision/:subtopic_url_fragment",
       "TITLE": "Oppia",
