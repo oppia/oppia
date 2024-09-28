547889fd683dac9d3552123a43089d675752a114
Rishabh Sharma <127947846+TARishabh@users.noreply.github.com>
2024-09-28 22:00:47 +0530

    Fix #20923: Display Character Limits in Chapter Editor (#20999)
    * fix: add character limits to chapter editor for title and description
    
    * Fix: Use constants from constants.ts for chapter editor character limits
    
    * fix: linting issues
    
    * fix: renamed branch to follow naming conventions
    
    * fix: renamed branch to follow naming conventions
    
    * fix: conflicts resolved
    
    * fix: adding comment to fix prettier issues
    
    * fix: prettier on whole codebase
    
    ---------
    
    Co-authored-by: rishabh.sharma <rishabh.sharma@createbytes.com>

--- core/templates/pages/story-editor-page/editor-tab/story-node-editor.component.ts
+++ core/templates/pages/story-editor-page/editor-tab/story-node-editor.component.ts
@@ -57,6 +57,9 @@ export class StoryNodeEditorComponent implements OnInit, OnDestroy {
   @Input() acquiredSkillIds: string[];
   @Input() plannedPublicationDateMsecs: number;
 
+  MAX_CHARS_IN_EXPLORATION_TITLE = AppConstants.MAX_CHARS_IN_EXPLORATION_TITLE;
+  MAX_CHARS_IN_CHAPTER_DESCRIPTION =
+    AppConstants.MAX_CHARS_IN_CHAPTER_DESCRIPTION;
   chapterPreviewCardIsShown = false;
   mainChapterCardIsShown = true;
   explorationInputButtonsAreShown = false;
