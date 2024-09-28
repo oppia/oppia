0e36dc53d0233f2d69a9494bdaa8cc5566581f99
Hardik Goyal <135348086+HardikGoyal2003@users.noreply.github.com>
2024-09-28 22:01:04 +0530

    Fix part of #19435: Removed the Email Dashboard Result Page (#20968)
    * Removed the email dashboard result page
    
    * Removed the webpack
    
    * Removed module name and removed backend tests
    
    * lint fix

--- core/tests/test-dependencies/route-to-module-mapping-generator.ts
+++ core/tests/test-dependencies/route-to-module-mapping-generator.ts
@@ -66,12 +66,6 @@ const MANUAL_ROUTE_TO_MODULE_MAPPING: Map<Route, string> = new Map([
   ],
   [
     {
-      path: 'emaildashboardresult/:query_id',
-    },
-    'core/templates/pages/email-dashboard-pages/email-dashboard-result.import.ts',
-  ],
-  [
-    {
       path: 'learn/:classroom_url_fragment/:topic_url_fragment/practice/session',
     },
     'core/templates/pages/practice-session-page/practice-session-page.import.ts',
