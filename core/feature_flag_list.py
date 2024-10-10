ac64afe8dc6531d02019e97e344e144a03d10ad5
Nikhil <nikhil.agarwal.2019@gmail.com>
2024-10-10 17:17:41 +0530

    Hardcoded math classroom for diagnostic test. (#21092)
    * Hardcoded math classroom for diagnostic test.
    
    * Fixes frontend test

--- core/feature_flag_list.py
+++ core/feature_flag_list.py
@@ -92,7 +92,6 @@ DEV_FEATURES_LIST = [
 TEST_FEATURES_LIST: List[FeatureNames] = [
     FeatureNames.CD_ADMIN_DASHBOARD_NEW_UI,
     FeatureNames.SERIAL_CHAPTER_LAUNCH_CURRICULUM_ADMIN_VIEW,
-    FeatureNames.DIAGNOSTIC_TEST,
     FeatureNames.SERIAL_CHAPTER_LAUNCH_LEARNER_VIEW,
     FeatureNames.CD_ALLOW_UNDOING_TRANSLATION_REVIEW,
     FeatureNames.EXPLORATION_EDITOR_CAN_MODIFY_TRANSLATIONS,
@@ -111,6 +110,7 @@ PROD_FEATURES_LIST: List[FeatureNames] = [
     FeatureNames.ENABLE_VOICEOVER_CONTRIBUTION,
     FeatureNames.AUTO_UPDATE_EXP_VOICE_ARTIST_LINK,
     FeatureNames.ADD_VOICEOVER_WITH_ACCENT,
+    FeatureNames.DIAGNOSTIC_TEST,
 ]
 
 # Names of features that should not be used anymore, e.g. features that are
@@ -144,7 +144,7 @@ FEATURE_FLAG_NAME_TO_DESCRIPTION_AND_FEATURE_STAGE = {
     FeatureNames.DIAGNOSTIC_TEST.value: (
         (
             'This flag is for the diagnostic test functionality.',
-            feature_flag_domain.ServerMode.TEST
+            feature_flag_domain.ServerMode.PROD
         )
     ),
     FeatureNames.SERIAL_CHAPTER_LAUNCH_CURRICULUM_ADMIN_VIEW.value: (
