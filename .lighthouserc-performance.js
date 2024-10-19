c7f8a4e7882e96c7c869e0663f395fe0058dcdbe
Mohit Ruwatia <mohitruwatia2312@gmail.com>
2024-10-19 18:01:58 +0530

    Ignored error in console due to third party lib

--- .lighthouserc-performance.js
+++ .lighthouserc-performance.js
@@ -165,7 +165,15 @@ module.exports = {
         {
           matchingUrlPattern: 'http://[^/]+/create/.*$',
           assertions: {
-            'errors-in-console': ['error', {minScore: 1}],
+            'errors-in-console': [
+              'error',
+              {
+                minScore: 1,
+                ignorePatterns: [
+                  'TypeError: angular.module\\(\\.\\.\\.\\)\\.directive\\(\\.\\.\\.\\)\\.animation is not a function',
+                ],
+              },
+            ],
             // TODO(#13465): Change this maxLength to 0 once images are migrated.
             'modern-image-formats': [
               'error',
