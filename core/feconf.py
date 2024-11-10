3864522287641fee0a360a7d8d0dd77238890d88
Jay Vivarekar <jayviva@gmail.com>
2024-11-10 15:33:53 +0530

    Update versions to 3.4.3 (#21240)
    * Update core/feconf.py
    
    * Update package.json
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions@noreply.com>

--- core/feconf.py
+++ core/feconf.py
@@ -585,7 +585,7 @@ ENV_IS_OPPIA_ORG_PRODUCTION_SERVER = bool(OPPIA_PROJECT_ID == 'oppiaserver')
 DATAFLOW_TEMP_LOCATION = 'gs://todo/todo'
 DATAFLOW_STAGING_LOCATION = 'gs://todo/todo'
 
-OPPIA_VERSION = '3.4.2'
+OPPIA_VERSION = '3.4.3'
 OPPIA_PYTHON_PACKAGE_PATH = './build/oppia-beam-job-%s.tar.gz' % OPPIA_VERSION
 
 # Committer id for system actions. The username for the system committer
