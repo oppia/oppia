e6b29be247d8bfed829d18f80d285f6f1917c414
Himkar <himkarsingh00@gmail.com>
2025-01-23 23:18:41 +0530

    added validation for duplicated unit

--- core/templates/domain/objects/UnitsObjectFactorySpec.ts
+++ core/templates/domain/objects/UnitsObjectFactorySpec.ts
@@ -307,4 +307,12 @@ describe('UnitsObjectFactory', () => {
       units.fromRawInputString('cent %mol$');
     }).toThrowError('Unit "dollarcent" not found.');
   });
+  it('should return the dupplicated unit in a input string or an empty string', () => {
+    expect(units.getDuplicatedUnit('2 km km')).toEqual('km');
+    expect(units.getDuplicatedUnit('2 kg/kg^4*K*mol')).toEqual('kg');
+    expect(units.getDuplicatedUnit('2 kg/km^4*K*mol')).toEqual('');
+  });
+  it('should return slash if count more than one or an empty string', () => {
+    expect(units.getMultipleSlash('2 km/s/kg')).toEqual('/');
+  });
 });
