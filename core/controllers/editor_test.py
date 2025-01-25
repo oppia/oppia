dd949b05dec632944f98a1589399151cfe51753d
TARishabh <rishabh.sharma110e@gmail.com>
2025-01-25 19:15:49 +0530

    fix: linting tests

--- core/controllers/editor_test.py
+++ core/controllers/editor_test.py
@@ -3822,7 +3822,8 @@ class ImageUploadHandlerTests(BaseEditorControllerTests):
 
     def test_filename_regex_matches_accepted_extensions(self) -> None:
         """Test that filename regex pattern matches all
-        accepted image extensions."""
+        accepted image extensions.
+        """
 
         self.login(self.EDITOR_EMAIL)
         csrf_token = self.get_new_csrf_token()
