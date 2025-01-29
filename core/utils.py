5127357a41f954f19e6b0ad6ff2eb137f73c8f0e
TARishabh <rishabh.sharma110e@gmail.com>
2025-01-30 00:23:42 +0530

    fix: lint checks

--- core/utils.py
+++ core/utils.py
@@ -1442,7 +1442,7 @@ def unescape_html(escaped_html_data: str) -> str:
 
 
 def get_image_filename_regex_pattern() -> str:
-    """ Returns regex pattern for valid image
+    """Returns regex pattern for valid image
     filenames based on accepted extensions.
     """
     extensions = []
