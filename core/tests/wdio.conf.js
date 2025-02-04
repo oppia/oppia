43a4348cb95e962030f17901bd586b2422d2287c
Jayam Patel <jayampatel04@gmail.com>
2025-02-04 09:12:45 +0530

    Added more useful and informative comments

--- core/tests/wdio.conf.js
+++ core/tests/wdio.conf.js
@@ -329,6 +329,7 @@ exports.config = {
         '-i', process.env.DISPLAY,
         '-g', '300',
         '-loglevel', '16',
+        // Use ultrafast preset to reduce overhead, curcial when testing in CI.
         '-preset', 'ultrafast',
       ];
       const uniqueString = Math.random().toString(36).substring(2, 8);
