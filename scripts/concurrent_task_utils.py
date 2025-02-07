5708ca0a7e96e9483f90bf90a2d9ebb7d154565d
Mayank Mohapatra <125661248+Mayank77maruti@users.noreply.github.com>
2025-02-07 11:16:14 +0530

    Update concurrent_task_utils.py

--- scripts/concurrent_task_utils.py
+++ scripts/concurrent_task_utils.py
@@ -225,6 +225,7 @@ def create_task(
             can run at any time.
         name: str|None. Name of the task that is going to be created.
         report_enabled: bool. Decide whether task result will print or not.
+        errors_to_retry_on: list of error messages
 
     Returns:
         task: TaskThread object. Created task.
