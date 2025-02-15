d2e4c3b916ca07e8a202aecb49187594cd9cf4a1
TARishabh <rishabh.sharma110e@gmail.com>
2025-02-15 13:33:34 +0530

    fix: lint checks

--- core/templates/pages/contributor-dashboard-page/modal-templates/translation-suggestion-review-modal.component.spec.ts
+++ core/templates/pages/contributor-dashboard-page/modal-templates/translation-suggestion-review-modal.component.spec.ts
@@ -2711,7 +2711,7 @@ describe('Translation Suggestion Review Modal Component', function () {
     expect(result).toEqual(expectedHtmlString);
   });
 
-  fdescribe('Translation Suggestion Review Modal - Component Validation', () => {
+  describe('Translation Suggestion Review Modal - Component Validation', () => {
     const htmlWithComponents = `
     <p>Content with components</p>
     <oppia-noninteractive-image
