fb6729b6bb6480d45644503e05a00cc6e9369ff5
Harish Machha <harishmachha1603@gmail.com>
2025-02-16 22:41:33 +0530

    Update gif-frames.import.ts to use a default export and modify tests to spy on gifFrames directly, fixing the missing default() method error.

--- extensions/objects/templates/image-editor.component.spec.ts
+++ extensions/objects/templates/image-editor.component.spec.ts
@@ -40,7 +40,7 @@ import {SimpleChanges} from '@angular/core';
 import {SvgSanitizerService} from 'services/svg-sanitizer.service';
 import {MockTranslatePipe} from 'tests/unit-test-utils';
 let gifshot = require('gifshot');
-(window as Window).gifFrames = require('gif-Frames');
+(window as Window).gifFrames = require('gif-frames');
 // Declare global {
 //   interface Window {
 //     GifFrames: Function;
