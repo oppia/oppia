537b7b6e347a0ae1a3f34f023d407bc5b41369ec
HardikGoyal2003 <hardikgoyal2003@gmail.com>
2025-02-18 19:09:01 +0530

    lint fix

--- core/controllers/access_validators_test.py
+++ core/controllers/access_validators_test.py
@@ -62,7 +62,9 @@ class ClassroomPageAccessValidationHandlerTests(test_utils.GenericTestBase):
         self.signup(
             self.CURRICULUM_ADMIN_EMAIL, self.CURRICULUM_ADMIN_USERNAME)
         self.set_curriculum_admins([self.CURRICULUM_ADMIN_USERNAME])
-        self.save_new_valid_classroom()
+        self.save_new_valid_classroom(
+            is_diagnostic_test_enabled=False
+        )
         self.save_new_valid_classroom(
             'history', 'history', 'history',
             is_published=False,
