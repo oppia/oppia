cdbfde0a4095b8f610d396dc40274bb4fb73e801
Mohit Ruwatia <mohitruwatia2312@gmail.com>
2025-02-22 11:12:54 +0530

    Removed unnecessary imports

--- core/templates/pages/blog-dashboard-page/blog-dashboard-page.module.ts
+++ core/templates/pages/blog-dashboard-page/blog-dashboard-page.module.ts
@@ -30,33 +30,9 @@ import {toastrConfig} from 'pages/oppia-root/app.module';
 import {BlogDashboardPageRootComponent} from './blog-dashboard-page-root.component';
 import {BlogAuthorDetailsEditorComponent} from './modal-templates/author-detail-editor-modal.component';
 import {BlogDashboardPageAuthGuard} from './blog-dashboard-page-auth.guard';
-import {AlertsService} from 'services/alerts.service';
-import {AssetsBackendApiService} from 'services/assets-backend-api.service';
-import {ContextService} from 'services/context.service';
-import {CsrfTokenService} from 'services/csrf-token.service';
-import {ImageLocalStorageService} from 'services/image-local-storage.service';
-import {ImagePreloaderService} from 'pages/exploration-player-page/services/image-preloader.service';
-import {ImageUploadHelperService} from 'services/image-upload-helper.service';
-import {RteHelperService} from 'services/rte-helper.service';
-import {SvgSanitizerService} from 'services/svg-sanitizer.service';
-import {UrlInterpolationService} from 'domain/utilities/url-interpolation.service';
-import {BlogDashboardPageService} from './services/blog-dashboard-page.service';
-import {BlogPostEditorBackendApiService} from 'domain/blog/blog-post-editor-backend-api.service';
-import {BlogPostUpdateService} from 'domain/blog/blog-post-update.service';
-import {LoaderService} from 'services/loader.service';
-import {WindowDimensionsService} from 'services/contextual/window-dimensions.service';
-import {PreventPageUnloadEventService} from 'services/prevent-page-unload-event.service';
-import {UserService} from 'services/user.service';
-import {UtilsService} from 'services/utils.service';
-import {UrlService} from 'services/contextual/url.service';
-import {BlogPostPageService} from 'pages/blog-post-page/services/blog-post-page.service';
-import {ComputeGraphService} from 'services/compute-graph.service';
-import {ExtractImageFilenamesFromModelService} from 'pages/exploration-player-page/services/extract-image-filenames-from-model.service';
-import {UserBackendApiService} from 'services/user-backend-api.service';
-import {RequestInterceptor} from 'services/request-interceptor.service';
-import {ExternalRteSaveService} from 'services/external-rte-save.service';
-import {FocusManagerService} from 'services/stateful/focus-manager.service';
 import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {RequestInterceptor} from 'services/request-interceptor.service';
+import {RteHelperService} from 'services/rte-helper.service';
 
 @NgModule({
   imports: [
