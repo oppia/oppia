9c2b33950c9ddce2823921be3b0c1f8c3436497f
Shriyam-Avasthi <avasthishriyam@gmail.com>
2025-02-23 22:43:56 +0530

    Fully cover rte helper modal with unit tests

--- core/templates/services/rte-helper-modal.component.spec.ts
+++ core/templates/services/rte-helper-modal.component.spec.ts
@@ -23,6 +23,7 @@ import {
   fakeAsync,
   flush,
 } from '@angular/core/testing';
+import {AppConstants} from 'app.constants';
 import {RteHelperModalComponent} from './rte-helper-modal.component';
 import {ExternalRteSaveService} from './external-rte-save.service';
 import {AlertsService} from './alerts.service';
@@ -47,6 +48,9 @@ describe('RteHelperModalComponent', () => {
   let component: RteHelperModalComponent;
   let fixture: ComponentFixture<RteHelperModalComponent>;
   let contextService: ContextService;
+  let assetsBackendApiService: AssetsBackendApiService;
+  let imageUploadHelperService: ImageUploadHelperService;
+  let alertsService: AlertsService;
   let mockExternalRteSaveEventEmitter = new EventEmitter();
   let activeModal: NgbActiveModal;
 
@@ -90,6 +94,9 @@ describe('RteHelperModalComponent', () => {
     fixture = TestBed.createComponent(RteHelperModalComponent);
     component = fixture.componentInstance;
     contextService = TestBed.inject(ContextService);
+    assetsBackendApiService = TestBed.inject(AssetsBackendApiService);
+    imageUploadHelperService = TestBed.inject(ImageUploadHelperService);
+    alertsService = TestBed.inject(AlertsService);
     activeModal = TestBed.inject(NgbActiveModal);
   });
 
@@ -178,8 +185,477 @@ describe('RteHelperModalComponent', () => {
     it('should disable save button', fakeAsync(() => {
       component.ngOnInit();
       flush();
-      fixture.detectChanges();
+    }));
+  });
+
+  describe('when the editor is Math expression editor', function () {
+    var customizationArgSpecs = [
+      {
+        name: 'math_content',
+        default_value: {
+          raw_latex: '',
+          svg_filename: '',
+        },
+      },
+    ];
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(RteHelperModalComponent);
+      component = fixture.componentInstance;
+      component.componentId = 'math';
+      component.attrsCustomizationArgsDict = {
+        math_content: {
+          raw_latex: '',
+          svg_filename: '',
+        },
+      };
+      component.customizationArgSpecs = customizationArgSpecs;
+    });
+
+    it('should load modal correctly', fakeAsync(() => {
+      expect(component.customizationArgSpecs).toEqual(customizationArgSpecs);
+      expect(component.componentId).toBe('math');
+      expect(component.modalIsLoading).toBe(true);
+      component.ngOnInit();
       flush();
+      expect(component.modalIsLoading).toBe(false);
+    }));
+
+    it('should close modal when clicking on cancel button', fakeAsync(() => {
+      component.ngOnInit();
+      flush();
+      component.cancel();
+      expect(activeModal.dismiss).toHaveBeenCalledWith(false);
+    }));
+
+    it('should close modal when clicking on cancel button when it is newly created', fakeAsync(() => {
+      component.ngOnInit();
+      component.componentIsNewlyCreated = true;
+      flush();
+      component.cancel();
+      expect(activeModal.dismiss).toHaveBeenCalledWith(true);
+    }));
+
+    it('should save modal customization args when closing it', fakeAsync(() => {
+      spyOn(mockExternalRteSaveEventEmitter, 'emit').and.callThrough();
+      spyOn(contextService, 'getImageSaveDestination').and.returnValue(
+        AppConstants.IMAGE_SAVE_DESTINATION_SERVER
+      );
+      spyOn(contextService, 'getEntityType').and.returnValue('exploration');
+      component.ngOnInit();
+      flush();
+
+      component.customizationArgsForm.value[0] = {
+        raw_latex: 'x^2',
+        svgFile: 'Svg Data',
+        svg_filename: 'mathImage.svg',
+        mathExpressionSvgIsBeingProcessed: true,
+      };
+      component.onCustomizationArgsFormChange(
+        component.customizationArgsForm.value
+      );
+      expect(component.isErrorMessageNonempty()).toBe(true);
+      component.customizationArgsForm.value[0].mathExpressionSvgIsBeingProcessed =
+        false;
+      component.onCustomizationArgsFormChange(
+        component.customizationArgsForm.value
+      );
+      expect(component.isErrorMessageNonempty()).toBe(false);
+      var response = {
+        filename: 'mathImage.svg',
+      };
+      var imageFile = new Blob();
+      spyOn(assetsBackendApiService, 'saveMathExpressionImage').and.returnValue(
+        Promise.resolve(response)
+      );
+      spyOn(
+        imageUploadHelperService,
+        'convertImageDataToImageFile'
+      ).and.returnValue(imageFile);
+      component.save();
+      flush();
+
+      expect(mockExternalRteSaveEventEmitter.emit).toHaveBeenCalled();
+      expect(activeModal.close).toHaveBeenCalledWith({
+        math_content: {
+          raw_latex: 'x^2',
+          svg_filename: 'mathImage.svg',
+        },
+      });
+    }));
+
+    it('should handle being unable to communicate to server and show error while saving', fakeAsync(() => {
+      spyOn(alertsService, 'addWarning');
+      spyOn(mockExternalRteSaveEventEmitter, 'emit').and.callThrough();
+      spyOn(contextService, 'getImageSaveDestination').and.returnValue(
+        AppConstants.IMAGE_SAVE_DESTINATION_SERVER
+      );
+      spyOn(contextService, 'getEntityType').and.returnValue('exploration');
+      component.ngOnInit();
+      flush();
+
+      component.customizationArgsForm.value[0] = {
+        raw_latex: 'x^2',
+        svgFile: 'Svg Data',
+        svg_filename: 'mathImage.svg',
+        mathExpressionSvgIsBeingProcessed: false,
+      };
+
+      var response = {
+        error: 'Error communicating with server.',
+      };
+      var imageFile = new Blob();
+      spyOn(assetsBackendApiService, 'saveMathExpressionImage').and.returnValue(
+        Promise.reject(response)
+      );
+      spyOn(
+        imageUploadHelperService,
+        'convertImageDataToImageFile'
+      ).and.returnValue(imageFile);
+      component.save();
+      flush();
+
+      expect(mockExternalRteSaveEventEmitter.emit).toHaveBeenCalled();
+      expect(alertsService.addWarning).toHaveBeenCalledWith(
+        'Error communicating with server.'
+      );
+      expect(activeModal.dismiss).toHaveBeenCalledWith('cancel');
+    }));
+
+    it('should cancel the modal when math SVG exceeds 100 KB', fakeAsync(() => {
+      spyOn(mockExternalRteSaveEventEmitter, 'emit').and.callThrough();
+      spyOn(contextService, 'getEntityType').and.returnValue('exploration');
+      component.ngOnInit();
+      flush();
+      component.customizationArgsForm.value[0] = {
+        raw_latex: 'x^2 + y^2 + x^2 + y^2 + x^2 + y^2 + x^2 + y^2 + x^2',
+        svgFile: 'Svg Data',
+        svg_filename: 'mathImage.svg',
+      };
+      component.onCustomizationArgsFormChange(
+        component.customizationArgsForm.value
+      );
+      var imageFile = new Blob([new ArrayBuffer(102 * 1024)], {
+        type: 'application/octet-stream',
+      });
+      spyOn(
+        imageUploadHelperService,
+        'convertImageDataToImageFile'
+      ).and.returnValue(imageFile);
+      component.save();
+      flush();
+
+      expect(mockExternalRteSaveEventEmitter.emit).toHaveBeenCalled();
+      expect(activeModal.dismiss).toHaveBeenCalledWith('cancel');
+    }));
+
+    it('should cancel the modal when SVG exceeds 1 MB for blog post', fakeAsync(() => {
+      spyOn(mockExternalRteSaveEventEmitter, 'emit').and.callThrough();
+      spyOn(contextService, 'getEntityType').and.returnValue(
+        AppConstants.ENTITY_TYPE.BLOG_POST
+      );
+      component.ngOnInit();
+      flush();
+      component.customizationArgsForm.value[0] = {
+        raw_latex: 'x^2 + y^2 + x^2 + y^2 + x^2 + y^2 + x^2 + y^2 + x^2',
+        svgFile: 'Svg Data',
+        svg_filename: 'mathImage.svg',
+      };
+      component.onCustomizationArgsFormChange(
+        component.customizationArgsForm.value
+      );
+      var imageFile = new Blob([new ArrayBuffer(102 * 1024 * 1024)], {
+        type: 'application/octet-stream',
+      });
+
+      spyOn(
+        imageUploadHelperService,
+        'convertImageDataToImageFile'
+      ).and.returnValue(imageFile);
+      component.save();
+      flush();
+
+      expect(mockExternalRteSaveEventEmitter.emit).toHaveBeenCalled();
+      expect(activeModal.dismiss).toHaveBeenCalledWith('cancel');
+    }));
+
+    it(
+      'should cancel the modal when if the rawLatex or filename field is' +
+        'empty for a math expression',
+      fakeAsync(() => {
+        spyOn(mockExternalRteSaveEventEmitter, 'emit').and.callThrough();
+        spyOn(contextService, 'getEntityType').and.returnValue('exploration');
+        component.ngOnInit();
+        flush();
+        component.customizationArgsForm.value[0] = {
+          raw_latex: '',
+          svgFile: null,
+          svg_filename: '',
+        };
+        component.onCustomizationArgsFormChange(
+          component.customizationArgsForm.value
+        );
+        component.save();
+        flush();
+
+        expect(mockExternalRteSaveEventEmitter.emit).toHaveBeenCalled();
+        expect(activeModal.dismiss).toHaveBeenCalledWith('cancel');
+      })
+    );
+
+    it('should save modal customization args while in local storage', fakeAsync(() => {
+      spyOn(mockExternalRteSaveEventEmitter, 'emit').and.callThrough();
+      spyOn(contextService, 'getEntityType').and.returnValue('exploration');
+      component.ngOnInit();
+      flush();
+      component.customizationArgsForm.value[0] = {
+        raw_latex: 'x^2',
+        svgFile: 'Svg Data',
+        svg_filename: 'mathImage.svg',
+        mathExpressionSvgIsBeingProcessed: true,
+      };
+      component.onCustomizationArgsFormChange(
+        component.customizationArgsForm.value
+      );
+
+      var imageFile = new Blob();
+      spyOn(contextService, 'getImageSaveDestination').and.returnValue(
+        AppConstants.IMAGE_SAVE_DESTINATION_LOCAL_STORAGE
+      );
+      spyOn(
+        imageUploadHelperService,
+        'convertImageDataToImageFile'
+      ).and.returnValue(imageFile);
+
+      component.save();
+      flush();
+
+      expect(mockExternalRteSaveEventEmitter.emit).toHaveBeenCalled();
+      expect(activeModal.close).toHaveBeenCalledWith({
+        math_content: {
+          raw_latex: 'x^2',
+          svg_filename: 'mathImage.svg',
+        },
+      });
+    }));
+  });
+
+  describe('when the editor is Link editor', function () {
+    const customizationArgSpecs = [
+      {
+        name: 'url',
+        default_value: 'google.com',
+      },
+      {
+        name: 'text',
+        default_value: '',
+      },
+    ];
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(RteHelperModalComponent);
+      component = fixture.componentInstance;
+      component.componentId = 'link';
+      component.attrsCustomizationArgsDict = {
+        url: 'google.com',
+        text: 'google.com',
+      };
+      component.customizationArgSpecs = customizationArgSpecs;
+    });
+
+    it('should load modal correctly', fakeAsync(() => {
+      expect(component.customizationArgSpecs).toEqual(customizationArgSpecs);
+      expect(component.componentId).toBe('link');
+    }));
+
+    it('should disable save button when text may be misleading', fakeAsync(() => {
+      const URLs = [
+        'malicious.com',
+        'www.malicious.com',
+        'https://malicious.com',
+        'https://www.malicious.com',
+      ];
+      const texts = ['google.com', 'friendly.org', 'https://www.happy.gov'];
+
+      for (const URL of URLs) {
+        for (const text of texts) {
+          component.ngOnInit();
+          flush();
+          component.customizationArgsForm.value[0] = URL;
+          component.customizationArgsForm.value[1] = text;
+
+          component.onCustomizationArgsFormChange(
+            component.customizationArgsForm.value
+          );
+          expect(component.isErrorMessageNonempty()).toBe(true);
+        }
+      }
+    }));
+
+    it('should enable save button when text matches url', fakeAsync(() => {
+      component.ngOnInit();
+      flush();
+      component.customizationArgsForm.value[0] = 'www.google.com';
+      component.customizationArgsForm.value[1] = 'https://google.com';
+
+      expect(component.isErrorMessageNonempty()).toBe(false);
+    }));
+
+    it('should enable save butoon when text is not a URL', fakeAsync(() => {
+      component.ngOnInit();
+      flush();
+      component.customizationArgsForm.value[0] = 'www.google.com';
+      component.customizationArgsForm.value[1] = 'click here';
+      component.onCustomizationArgsFormChange(
+        component.customizationArgsForm.value
+      );
+      expect(component.isErrorMessageNonempty()).toBe(false);
+    }));
+
+    it('should save modal customization args when closing it', fakeAsync(() => {
+      spyOn(mockExternalRteSaveEventEmitter, 'emit').and.callThrough();
+      spyOn(contextService, 'getEntityType').and.returnValue('exploration');
+      component.ngOnInit();
+      flush();
+      expect(component.isErrorMessageNonempty()).toBe(false);
+
+      component.save();
+
+      expect(mockExternalRteSaveEventEmitter.emit).toHaveBeenCalled();
+      expect(activeModal.close).toHaveBeenCalledWith({
+        url: 'google.com',
+        text: 'google.com',
+      });
+    }));
+  });
+
+  describe("when customization args doesn't have a valid youtube video", function () {
+    var customizationArgSpecs = [
+      {
+        name: 'video_id',
+        default_value: 'https://www.youtube.com/watch?v=Ntcw0H0hwPU',
+      },
+      {
+        name: 'start',
+        default_value: 0,
+      },
+      {
+        name: 'end',
+        default_value: 10,
+      },
+      {
+        name: 'autoplay',
+        default_value: false,
+      },
+    ];
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(RteHelperModalComponent);
+      component = fixture.componentInstance;
+      (component.componentId = 'video'),
+        (component.attrsCustomizationArgsDict = {
+          video_id: 'Ntcw0H0hwPU',
+          start: 0,
+          end: 10,
+          autoplay: false,
+        });
+      component.customizationArgSpecs = customizationArgSpecs;
+    });
+    it('should disable save button and display error message', fakeAsync(() => {
+      component.ngOnInit();
+      flush();
+
+      component.customizationArgsForm.value[0] = '';
+      component.onCustomizationArgsFormChange(
+        component.customizationArgsForm.value
+      );
+      expect(component.isErrorMessageNonempty()).toBe(true);
+      expect(component.errorMessage).toBe(
+        'Please ensure that the Youtube URL or id is valid.'
+      );
+      flush();
+    }));
+  });
+
+  describe('when the editor is updated', function () {
+    var customizationArgSpecs = [
+      {
+        name: 'filepath',
+        default_value: '',
+      },
+      {
+        name: 'caption',
+        default_value: '',
+      },
+      {
+        name: 'alt',
+        default_value: '',
+      },
+    ];
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(RteHelperModalComponent);
+      component = fixture.componentInstance;
+      component.attrsCustomizationArgsDict = {
+        alt: '',
+        caption: '',
+        filepath: '',
+      };
+      component.customizationArgSpecs = customizationArgSpecs;
+    });
+
+    it('should call onCustomizationArgsFormChange when customizationArgsForm value changes', fakeAsync(() => {
+      component.ngOnInit();
+      flush();
+      spyOn(component, 'onCustomizationArgsFormChange').and.callThrough();
+
+      component.customizationArgsForm.setValue({
+        0: 'alt',
+        1: 'caption',
+        2: 'filepath',
+      });
+
+      expect(component.onCustomizationArgsFormChange).toHaveBeenCalledWith({
+        0: 'alt',
+        1: 'caption',
+        2: 'filepath',
+      });
+    }));
+  });
+
+  describe('when delete is clicked', function () {
+    var customizationArgSpecs = [
+      {
+        name: 'filepath',
+        default_value: '',
+      },
+      {
+        name: 'caption',
+        default_value: '',
+      },
+      {
+        name: 'alt',
+        default_value: '',
+      },
+    ];
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(RteHelperModalComponent);
+      component = fixture.componentInstance;
+      component.componentId = 'link';
+      component.attrsCustomizationArgsDict = {
+        alt: '',
+        caption: '',
+        filepath: '',
+      };
+      component.customizationArgSpecs = customizationArgSpecs;
+    });
+
+    it('should delete the RTE component', fakeAsync(() => {
+      component.ngOnInit();
+      flush();
+      component.delete();
+      expect(activeModal.dismiss).toHaveBeenCalledWith(true);
     }));
   });
 
