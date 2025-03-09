0a5befc34cc03beb7ccacae2752d3aad596151d2
Shriyam Avasthi <avasthishriyam@gmail.com>
2025-03-09 06:07:41 +0530

    Update check_frontend_test_coverage.py to address merge conflicts properly

--- scripts/check_frontend_test_coverage.py
+++ scripts/check_frontend_test_coverage.py
@@ -69,9 +69,6 @@ NOT_FULLY_COVERED_FILENAMES = [
         'services/voiceover-recording.service.ts',
     'core/templates/pages/exploration-player-page/services/'
         'learner-answer-info.service.ts',
-    'core/templates/services/'
-        'translation-file-hash-loader-backend-api.service.ts',
-    # Please don't try to cover unit-test-utils.ajs.ts file.
     'core/templates/tests/unit-test-utils.ajs.ts',
     # Please don't try to cover unit-test-utils.ajs.ts file.
     'extensions/interactions/MusicNotesInput/directives/'
