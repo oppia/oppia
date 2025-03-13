03d7a094ff616858c50204b61f1f4d1cc5cf602a
assou <asma.gandour@mail.mcgill.ca>
2025-03-13 15:29:17 -0400

    Added MyPy ignore statement

--- core/domain/recommendations_domain_test.py
+++ core/domain/recommendations_domain_test.py
@@ -37,15 +37,10 @@ class ExplorationRecommendationsUnitTests(test_utils.GenericTestBase):
     def test_validate_invalid_recommended_ids_value_type_raises_exception(
         self
     ) -> None:
-        recommended_exploration_ids = 123
-        exploration_recommendations = (
-            recommendations_domain.ExplorationRecommendations(
-            recommended_exploration_ids))
-        with self.assertRaisesRegex(
-            utils.ValidationError,
-            'Expected recommended_exploration_ids to be a list'):
-            exploration_recommendations.validate()
-        recommended_exploration_ids = True
+        # TODO(#13059): Here we use MyPy ignore because after we fully type the
+        # codebase we plan to get rid of the tests that intentionally test wrong
+        # inputs that we can normally catch by typing.
+        recommended_exploration_ids = 123 # type: ignore[assignment]
         exploration_recommendations = (
             recommendations_domain.ExplorationRecommendations(
             recommended_exploration_ids))
@@ -54,10 +49,25 @@ class ExplorationRecommendationsUnitTests(test_utils.GenericTestBase):
             'Expected recommended_exploration_ids to be a list'):
             exploration_recommendations.validate()
 
+        # TODO(#13059): Here we use MyPy ignore because after we fully type the
+        # codebase we plan to get rid of the tests that intentionally test wrong
+        # inputs that we can normally catch by typing.
+        recommended_exploration_ids = True # type: ignore[assignment]
+        exploration_recommendations = (
+            recommendations_domain.ExplorationRecommendations(
+            recommended_exploration_ids))
+        with self.assertRaisesRegex(
+            utils.ValidationError,
+            'Expected recommended_exploration_ids to be a list'):
+            exploration_recommendations.validate()
+
+    # TODO(#13059): Here we use MyPy ignore because after we fully type the
+    # codebase we plan to get rid of the tests that intentionally test wrong
+    # inputs that we can normally catch by typing.
     def test_validate_non_str_recommended_id_raises_exception(
         self
     ) -> None:
-        recommended_exploration_ids = [0]
+        recommended_exploration_ids = [0] # type: ignore[list-item]
         exploration_recommendations = (
             recommendations_domain.ExplorationRecommendations(
             recommended_exploration_ids))
