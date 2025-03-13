4ea05d53dd7d1cfcd0976586b3fe449e32ef8f5e
Ashu463 <ashukasaudhan971@gmail.com>
2025-03-14 01:30:06 +0530

    fix: revoked testing credentials

--- scripts/inactive_issue_checker.py
+++ scripts/inactive_issue_checker.py
@@ -27,10 +27,10 @@ from scripts import install_third_party_libs
 import requests
 from typing import Dict, List, Optional, Set, TypedDict
 
-INACTIVE_DAYS_THRESHOLD = 0.000000000007
-UNASSIGN_DAYS_THRESHOLD = 0.00000000000010
-REPO_OWNER = 'Ashu463'
-REPO_NAME = 'grid6.0'
+INACTIVE_DAYS_THRESHOLD = 7
+UNASSIGN_DAYS_THRESHOLD = 10
+REPO_OWNER = 'oppia'
+REPO_NAME = 'oppia'
 
 
 class IssueDict(TypedDict, total=False):
