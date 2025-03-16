6fe7fbb794aff9daec5395f0fb5e6048dd5d4a10
assou <asma.gandour@mail.mcgill.ca>
2025-03-16 15:44:18 -0400

    Fixed syntax of returns

--- core/domain/recommendations_services.py
+++ core/domain/recommendations_services.py
@@ -386,7 +386,7 @@ def get_exploration_recommendations(
             recommendations.
 
     Returns:
-        ExplorationRecommendations object containing the recommended
+        ExplorationRecommendations. The object containing the recommended
         explorations IDs for the given exploration.
     """
 
