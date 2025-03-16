fdc3e26e5bd18f2e006c3d2b89f38b689a16e991
assou <asma.gandour@mail.mcgill.ca>
2025-03-16 14:12:08 -0400

    Added tests for exp_id validation and fixed ident

--- core/domain/recommendations_domain_test.py
+++ core/domain/recommendations_domain_test.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 #
-# Copyright 2024 The Oppia Authors. All Rights Reserved.
+# Copyright 2025 The Oppia Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -27,23 +27,70 @@ class ExplorationRecommendationsUnitTests(test_utils.GenericTestBase):
     """Test for ExplorationRecommendations domain class."""
 
     def test_creation_of_object(self) -> None:
+        exp_id = '123'
         recommended_exploration_ids = ['1']
         exploration_recommendations = (
             recommendations_domain.ExplorationRecommendations(
-            recommended_exploration_ids))
+                exp_id,
+                recommended_exploration_ids))
         self.assertEqual(
             exploration_recommendations.recommended_exploration_ids[0], '1')
+
+    def test_validate_invalid_exp_id_value_type_raises_exception(self) -> None:
+        # TODO(#13059): Here we use MyPy ignore because after we fully type the
+        # codebase we plan to get rid of the tests that intentionally test wrong
+        # inputs that we can normally catch by typing.
+        exp_id = 123
+        recommended_exploration_ids = ['1']
+        exploration_recommendations = (
+            recommendations_domain.ExplorationRecommendations(
+                exp_id,  # type: ignore[arg-type]
+                recommended_exploration_ids
+        ))
+        with self.assertRaisesRegex(
+            utils.ValidationError,
+            'Expected exp_id to be a string'):
+            exploration_recommendations.validate()
+
+        # TODO(#13059): Here we use MyPy ignore because after we fully type the
+        # codebase we plan to get rid of the tests that intentionally test wrong
+        # inputs that we can normally catch by typing.
+        exp_id = False
+        exploration_recommendations = (
+            recommendations_domain.ExplorationRecommendations(
+                exp_id,  # type: ignore[arg-type]
+                recommended_exploration_ids
+        ))
+        with self.assertRaisesRegex(
+            utils.ValidationError,
+            'Expected exp_id to be a string'):
+            exploration_recommendations.validate()
+
+    def test_validate_empty_str_exp_id_raises_exception(self) -> None:
+        exp_id = ''
+        recommended_exploration_ids = ['1']
+        exploration_recommendations = (
+            recommendations_domain.ExplorationRecommendations(
+                exp_id,
+                recommended_exploration_ids))
+        with self.assertRaisesRegex(
+            utils.ValidationError,
+            'Expected exp_id to be non-empty'
+        ):
+            exploration_recommendations.validate()
 
     def test_validate_invalid_recommended_ids_value_type_raises_exception(
         self
     ) -> None:
+        exp_id = '123'
         # TODO(#13059): Here we use MyPy ignore because after we fully type the
         # codebase we plan to get rid of the tests that intentionally test wrong
         # inputs that we can normally catch by typing.
         recommended_exploration_ids = 123
         exploration_recommendations = (
             recommendations_domain.ExplorationRecommendations(
-            recommended_exploration_ids  # type: ignore[arg-type]
+                exp_id,
+                recommended_exploration_ids  # type: ignore[arg-type]
         ))
         with self.assertRaisesRegex(
             utils.ValidationError,
@@ -56,23 +103,24 @@ class ExplorationRecommendationsUnitTests(test_utils.GenericTestBase):
         recommended_exploration_ids = True
         exploration_recommendations = (
             recommendations_domain.ExplorationRecommendations(
-            recommended_exploration_ids  # type: ignore[arg-type]
+                exp_id,
+                recommended_exploration_ids  # type: ignore[arg-type]
         ))
         with self.assertRaisesRegex(
             utils.ValidationError,
             'Expected recommended_exploration_ids to be a list'):
             exploration_recommendations.validate()
 
-    # TODO(#13059): Here we use MyPy ignore because after we fully type the
-    # codebase we plan to get rid of the tests that intentionally test wrong
-    # inputs that we can normally catch by typing.
-    def test_validate_non_str_recommended_id_raises_exception(
-        self
-    ) -> None:
+    def test_validate_non_str_recommended_id_raises_exception(self) -> None:
+        exp_id = '123'
+        # TODO(#13059): Here we use MyPy ignore because after we fully type the
+        # codebase we plan to get rid of the tests that intentionally test wrong
+        # inputs that we can normally catch by typing.
         recommended_exploration_ids = [0]
         exploration_recommendations = (
             recommendations_domain.ExplorationRecommendations(
-            recommended_exploration_ids  # type: ignore[arg-type]
+                exp_id,
+                recommended_exploration_ids  # type: ignore[arg-type]
         ))
         with self.assertRaisesRegex(
             utils.ValidationError,
@@ -80,13 +128,13 @@ class ExplorationRecommendationsUnitTests(test_utils.GenericTestBase):
         ):
             exploration_recommendations.validate()
 
-    def test_validate_empty_str_recommended_id_raises_exception(
-        self
-    ) -> None:
+    def test_validate_empty_str_recommended_id_raises_exception(self) -> None:
+        exp_id = '123'
         recommended_exploration_ids = ['']
         exploration_recommendations = (
             recommendations_domain.ExplorationRecommendations(
-            recommended_exploration_ids))
+                exp_id,
+                recommended_exploration_ids))
         with self.assertRaisesRegex(
             utils.ValidationError,
             'Expected recommended_exploration_id to be non-empty'
@@ -96,22 +144,24 @@ class ExplorationRecommendationsUnitTests(test_utils.GenericTestBase):
     def test_validate_recommended_ids_with_duplicate_value_raise_exception(
         self
     ) -> None:
+        exp_id = '123'
         recommended_exploration_ids = ['1', '2', '1']
         exploration_recommendations = (
             recommendations_domain.ExplorationRecommendations(
-            recommended_exploration_ids))
+                exp_id,
+                recommended_exploration_ids))
 
         with self.assertRaisesRegex(
             utils.ValidationError,
             'recommended_exploration_ids contains duplicate values:'):
             exploration_recommendations.validate()
 
-    def test_validation_passes(
-        self
-    ) -> None:
+    def test_validation_passes(self) -> None:
+        exp_id = '123'
         recommended_exploration_ids = ['1', '2']
         exploration_recommendations = (
             recommendations_domain.ExplorationRecommendations(
-            recommended_exploration_ids))
+                exp_id,
+                recommended_exploration_ids))
 
         exploration_recommendations.validate()
