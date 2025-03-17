268d4266f7e18c3b499f27cf4ceb1813df296b08
Nik-09 <nikhil.agarwal.2019@gmail.com>
2025-03-17 13:28:58 +0530

    Removes queue yaml from the codebase

--- core/domain/taskqueue_services.py
+++ core/domain/taskqueue_services.py
@@ -33,7 +33,6 @@ if MYPY: # pragma: no cover
 platform_taskqueue_services = models.Registry.import_taskqueue_services()
 
 
-# NOTE: The following constants should match the queue names in queue.yaml.
 # Taskqueue for backing up state.
 QUEUE_NAME_BACKUPS: Final = 'backups'
 # Default queue for processing tasks (including MapReduce ones).
