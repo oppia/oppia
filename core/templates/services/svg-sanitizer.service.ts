58a5a35f8529932820f82f4b6c004ff971b5903f
mishrayush15 <ayushmishra780193@gmail.com>
2025-03-17 12:31:25 +0530

    Fix: Added comments explaing the requirement of reverse iteration

--- core/templates/services/svg-sanitizer.service.ts
+++ core/templates/services/svg-sanitizer.service.ts
@@ -198,6 +198,9 @@ export class SvgSanitizerService {
       if (tagsToBeRemoved.indexOf(nodeTagName) !== -1) {
         node.remove();
       } else {
+        // Reverse iteration ensures no attributes are skipped when removing them.
+        // In a forward loop, removing an attribute shifts the next one, causing misses.
+        // This approach makes sure that all unwanted attributes are properly checked and removed.
         for (let i = node.attributes.length - 1; i >= 0; i--) {
           const nodeAttrName: string = node.attributes[i].name;
           // Check if the tag name and attribute combination matches any value
