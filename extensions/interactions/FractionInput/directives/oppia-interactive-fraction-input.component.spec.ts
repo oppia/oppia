441337f6c931be6ea309ee40ed53ce1b6491c183
Mayank Mohapatra <125661248+Mayank77maruti@users.noreply.github.com>
2025-03-19 23:13:18 +0530

    Update oppia-interactive-fraction-input.component.spec.ts

--- extensions/interactions/FractionInput/directives/oppia-interactive-fraction-input.component.spec.ts
+++ extensions/interactions/FractionInput/directives/oppia-interactive-fraction-input.component.spec.ts
@@ -251,16 +251,13 @@ describe('InteractiveFractionInputComponent', () => {
     }
   );
 
-  it(
-    'should reset validity status when user modifies answer input',
-    fakeAsync(() => {
-      component.answer = 'invalid';
-      component.answerValueChanged();
-      tick(150);
+  it('should reset validity status when user modifies answer input', fakeAsync(() => {
+    component.answer = 'invalid';
+    component.answerValueChanged();
+    tick(150);
 
-      expect(component.isValid).toBeTrue();
-    })
-  );
+    expect(component.isValid).toBeTrue();
+  }));
 
   it(
     'should display proper fraction error' +
@@ -292,19 +289,16 @@ describe('InteractiveFractionInputComponent', () => {
     }
   );
 
-  it(
-    'should display invalid length error when number exceeds 7 digits',
-    () => {
-      component.answer = '12345678/1';
+  it('should display invalid length error when number exceeds 7 digits', () => {
+    component.answer = '12345678/1';
 
-      component.submitAnswer();
+    component.submitAnswer();
 
-      expect(component.errorMessageI18nKey).toBe(
-        ObjectsDomainConstants.FRACTION_PARSING_ERROR_I18N_KEYS
-          .INVALID_CHARS_LENGTH
-      );
-    }
-  );
+    expect(component.errorMessageI18nKey).toBe(
+      ObjectsDomainConstants.FRACTION_PARSING_ERROR_I18N_KEYS
+        .INVALID_CHARS_LENGTH
+    );
+  });
 
   it(
     'should display simplest form error message when input' +
