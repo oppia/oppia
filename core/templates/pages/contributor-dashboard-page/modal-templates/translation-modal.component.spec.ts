1700231c151a614cb8ec206f30de5d494a8903c8
TARishabh <rishabh.sharma110e@gmail.com>
2025-03-20 00:17:25 +0530

    fix: lint checks

--- core/templates/pages/contributor-dashboard-page/modal-templates/translation-modal.component.spec.ts
+++ core/templates/pages/contributor-dashboard-page/modal-templates/translation-modal.component.spec.ts
@@ -41,7 +41,6 @@ import {
 import {TranslationLanguageService} from 'pages/exploration-editor-page/translation-tab/services/translation-language.service';
 import {ContextService} from 'services/context.service';
 import {WindowDimensionsService} from 'services/contextual/window-dimensions.service';
-import {WindowRef} from 'services/contextual/window-ref.service';
 import {
   ImageLocalStorageService,
   ImagesData,
