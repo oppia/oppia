1ead6ce026a850ed5bdd8bb6f1252d4c0645c1bf
Ojasvi <ojasvidoye@gmail.com>
2025-03-23 11:50:22 +0530

    skip feedback test in mobile viewport until issue 19443 is resolved

--- core/tests/puppeteer-acceptance-tests/specs/exploration-editor/submit-review-and-respond-to-feedback.spec.ts
+++ core/tests/puppeteer-acceptance-tests/specs/exploration-editor/submit-review-and-respond-to-feedback.spec.ts
@@ -223,7 +223,6 @@ describe('Exploration Editor', function () {
 
       // Return to the feedback tab and verify statuses are displayed correctly.
       await explorationEditor.pressFeedbackThreadBackButton();
-      await explorationEditor.page.reload();
       await explorationEditor.expectFeedbackStatusInList(1, 'Open');
       await explorationEditor.expectFeedbackStatusInList(2, 'Ignored');
       await explorationEditor.expectFeedbackStatusInList(3, 'Fixed');
