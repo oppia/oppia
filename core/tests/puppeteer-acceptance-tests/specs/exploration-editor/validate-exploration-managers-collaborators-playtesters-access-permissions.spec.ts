6fcca7c83f66b017e9caf198c50b2fbf5af18e4a
himkar-cmd <himkarsingh312@gmail.com>
2025-03-25 09:49:24 +0530

    Fix unused variable

--- core/tests/puppeteer-acceptance-tests/specs/exploration-editor/validate-exploration-managers-collaborators-playtesters-access-permissions.spec.ts
+++ core/tests/puppeteer-acceptance-tests/specs/exploration-editor/validate-exploration-managers-collaborators-playtesters-access-permissions.spec.ts
@@ -32,14 +32,14 @@ ConsoleReporter.setConsoleErrorsToIgnore([/.404.*Not Found./]);
 describe('Exploration User Roles', function () {
   let manager: ExplorationEditor;
   let collaborator: ExplorationEditor;
-  let newCollaborator: ExplorationEditor;
+  let _newCollaborator: ExplorationEditor;
   let playtester: ExplorationEditor;
   let explorationCreator: ExplorationEditor;
   let explorationId: string | null;
 
   beforeAll(async function () {
     // Create all users.
-    newCollaborator = await UserFactory.createNewUser(
+    _newCollaborator = await UserFactory.createNewUser(
       'newCollaborator',
       'newCollaborator@example.com'
     );
