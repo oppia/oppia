0fdc13ff7f293f3c0f06e143c500125832014524
TARishabh <rishabh.sharma110e@gmail.com>
2025-03-27 01:45:30 +0530

    fix: lint errors

--- core/domain/suggestion_services.py
+++ core/domain/suggestion_services.py
@@ -2160,15 +2160,16 @@ def _strip_prefix(component_name: str) -> str:
 
 
 def highlight_differences(
-    original: str, updated: str,
-    truncate_threshold: int = MAX_CONTENT_LENGTH_WITHOUT_TRUNCATION # pylint: disable=W9020
+        original: str,
+        updated: str,
+        truncate_threshold: int = MAX_CONTENT_LENGTH_WITHOUT_TRUNCATION
 ) -> Tuple[str, str]:
     """Finds the first difference between two strings and truncates accordingly.
 
     Args:
         original: str. The original string.
         updated: str. The updated string.
-        truncate_threshold : int. Maximum length of the returned snippets.
+        truncate_threshold: int. Maximum length of the returned snippets.
 
     Returns:
         tuple. A pair of truncated strings highlighting where they differ.
