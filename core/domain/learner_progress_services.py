7b6eef371b6f1cd0c205d867083e65200b1e0115
Om Gupta <omgupta23@iitk.ac.in>
2025-04-07 04:42:06 +0530

    Issue Fixes

--- core/domain/learner_progress_services.py
+++ core/domain/learner_progress_services.py
@@ -27,6 +27,7 @@ from core.domain import collection_domain
 from core.domain import collection_services
 from core.domain import exp_domain
 from core.domain import exp_fetchers
+from core.domain import exp_services
 from core.domain import learner_goals_services
 from core.domain import learner_playlist_services
 from core.domain import learner_progress_domain
@@ -257,6 +258,38 @@ def _save_last_playthrough_information(
     last_playthrough_information_model.put()
 
 
+def _check_if_terminal_exp(
+        user_id: str, story_id: str, exp_id: str
+    ) -> bool:
+    """Records the node completion for terminal nodes
+
+    Args:
+        user_id: str. The id of the user.
+        story_id: str. The id of the story.
+        exp_id: str. The id of the exploration.
+
+    Returns:
+        bool. Whether the story is learnt by the user.
+    """
+    story = story_fetchers.get_story_by_id(story_id)
+    completed_nodes = story_fetchers.get_completed_nodes_in_story(
+            user_id, story_id)
+    completed_exp_ids = [
+        completed_node.exploration_id for completed_node in completed_nodes]
+    ordered_nodes = story.story_contents.get_ordered_nodes()
+    next_exp_id = None
+    for node in ordered_nodes:
+        if (
+            node.exploration_id not in completed_exp_ids and
+            node.exploration_id != exp_id
+        ):
+            next_exp_id = node.exploration_id
+            break
+    if next_exp_id is None:
+        return True
+    return False
+
+
 def mark_exploration_as_completed(user_id: str, exp_id: str) -> None:
     """Adds the exploration id to the completed list of the user unless the
     exploration has already been completed or has been created/edited by the
@@ -292,6 +325,36 @@ def mark_exploration_as_completed(user_id: str, exp_id: str) -> None:
         learner_playlist_services.remove_exploration_from_learner_playlist(
             user_id, exp_id)
         activities_completed.add_exploration_id(exp_id)
+        story_id = exp_services.get_story_id_linked_to_exploration(
+        exp_id)
+        if story_id is not None:
+            is_terminal = _check_if_terminal_exp(
+                user_id, story_id, exp_id)
+            if is_terminal:
+                story = story_fetchers.get_story_by_id(story_id)
+                topic = topic_fetchers.get_topic_by_id(
+                    story.corresponding_topic_id)
+
+                if story_id not in activities_completed.story_ids:
+                    remove_story_from_incomplete_list(user_id, story_id)
+                    activities_completed.add_story_id(story_id)
+
+                if (
+                    topic.id is not None and
+                    topic.id not in activities_completed.learnt_topic_ids
+                ):
+                    topic_ids_to_learn = (
+                        learner_goals_services.get_all_topic_ids_to_learn(
+                            user_id)
+                    )
+                    remove_topic_from_partially_learnt_list(
+                        user_id, topic.id)
+                    if topic.id in topic_ids_to_learn:
+                        learner_goals_services.remove_topics_from_learn_goal(
+                            user_id,
+                            [topic.id]
+                        )
+                    activities_completed.add_learnt_topic_id(topic.id)
         _save_completed_activities(activities_completed)
 
 
--- core/domain/learner_progress_services_test.py
+++ core/domain/learner_progress_services_test.py
@@ -83,6 +83,13 @@ class LearnerProgressTests(test_utils.GenericTestBase):
     TOPIC_ID_3: Final = 'topic_3'
     USER_EMAIL: Final = 'user@example.com'
     USER_USERNAME: Final = 'user'
+    NEW_TOPIC_ID: Final = 'new_topic_id'
+    NEW_STORY_ID: Final = 'new_story_id'
+    STORY_URL_FRAGMENT_TWO = 'story-two'
+    NODE_ID_1: Final = 'node_1'
+    NODE_ID_2: Final = 'node_2'
+    STORY_ID: Final = 'story_id'
+    TOPIC_ID: Final = 'topic_id'
 
     def setUp(self) -> None:
         super().setUp()
@@ -312,6 +319,30 @@ class LearnerProgressTests(test_utils.GenericTestBase):
         topic_services.publish_story(
             self.TOPIC_ID_3, self.STORY_ID_3, self.admin_id)
         topic_services.publish_topic(self.TOPIC_ID_3, self.admin_id)
+        self.node_1: story_domain.StoryNodeDict = {
+            'id': self.NODE_ID_1,
+            'title': 'Title 1',
+            'description': 'Description 1',
+            'thumbnail_filename': 'image_1.svg',
+            'thumbnail_bg_color': constants.ALLOWED_THUMBNAIL_BG_COLORS[
+                'chapter'][0],
+            'thumbnail_size_in_bytes': 21131,
+            'destination_node_ids': [],
+            'acquired_skill_ids': [],
+            'prerequisite_skill_ids': [],
+            'outline': '',
+            'outline_is_finalized': False,
+            'exploration_id': self.EXP_ID_4,
+            'status': 'Draft',
+            'planned_publication_date_msecs': 100.0,
+            'last_modified_msecs': 100.0,
+            'first_publication_date_msecs': None,
+            'unpublishing_reason': None
+        }
+        self.nodes: list[story_domain.StoryNode] = []
+        self.signup(self.VIEWER_EMAIL, self.VIEWER_USERNAME)
+        self.viewer_id = self.get_user_id_from_email(self.VIEWER_EMAIL)
+        self.login(self.VIEWER_EMAIL)
 
     def _get_all_completed_exp_ids(self, user_id: str) -> List[str]:
         """Gets the ids of all the explorations completed by the learner
@@ -554,6 +585,52 @@ class LearnerProgressTests(test_utils.GenericTestBase):
             self.user_id, self.EXP_ID_2)
         self.assertEqual(self._get_all_completed_exp_ids(
             self.user_id), [self.EXP_ID_0, self.EXP_ID_1, self.EXP_ID_3])
+
+        # Test that the story is marked as completed, and the topic is marked
+        # as learnt, whenever the terminal exploration is completed.
+        self.save_new_valid_exploration(
+            self.EXP_ID_4, self.admin_id, title='Title 3', end_state_name='End')
+        self.publish_exploration(self.admin_id, self.EXP_ID_4)
+
+        story = story_domain.Story.create_default_story(
+            self.NEW_STORY_ID, 'Title', 'Description', self.TOPIC_ID_0,
+            self.STORY_URL_FRAGMENT_TWO)
+        story.meta_tag_content = 'story meta content'
+
+        story.story_contents.nodes = [
+            story_domain.StoryNode.from_dict(self.node_1)
+        ]
+        self.nodes = story.story_contents.nodes
+        story.story_contents.initial_node_id = 'node_1'
+        story.story_contents.next_node_id = 'node_2'
+        story_services.save_new_story(self.admin_id, story)
+        topic_services.add_canonical_story(
+            self.admin_id, self.TOPIC_ID_0, self.NEW_STORY_ID)
+        topic_services.publish_story(
+            self.TOPIC_ID_0, self.NEW_STORY_ID, self.admin_id)
+
+        learner_progress_services.validate_and_add_topic_to_learn_goal(
+            self.viewer_id, self.TOPIC_ID_0)
+
+        self.assertEqual(len(
+            learner_goals_services.get_all_topic_ids_to_learn(
+                self.viewer_id)), 1)
+
+        story_services.record_completed_node_in_story_context(
+            self.viewer_id, self.STORY_ID, self.NODE_ID_2)
+
+        learner_progress_services.mark_exploration_as_completed(
+            self.viewer_id, self.EXP_ID_4)
+
+        self.assertEqual(len(
+            learner_progress_services.get_all_completed_story_ids(
+                self.viewer_id)), 1)
+        self.assertEqual(len(
+            learner_progress_services.get_all_learnt_topic_ids(
+                self.viewer_id)), 1)
+        self.assertEqual(len(
+            learner_goals_services.get_all_topic_ids_to_learn(
+                self.viewer_id)), 0)
 
     def test_mark_collection_as_completed(self) -> None:
         self.assertEqual(
