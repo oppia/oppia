8f9d7a3b03580c3038bbccf549761b3cddaaf9b6
Kartik Suryavanshi <158498247+KartikSuryavanshi@users.noreply.github.com>
2025-04-09 15:27:03 +0530

    Update puppeteer-utils.ts

--- core/tests/puppeteer-acceptance-tests/utilities/common/puppeteer-utils.ts
+++ core/tests/puppeteer-acceptance-tests/utilities/common/puppeteer-utils.ts
@@ -206,7 +206,6 @@ export class BaseUser {
           downloadPath: downloadDir,
         });
 
-
         this.page.on('dialog', async dialog => {
           const alertText = dialog.message();
           if (acceptedBrowserAlerts.includes(alertText)) {
