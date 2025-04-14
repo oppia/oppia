9fb21409170bb4a66f2b96fe9807dd5a746c3fbb
charliemax5 <maxwellc@dickinson.edu>
2025-04-13 20:35:28 -0400

    Edited moderator.py to retrieve lists of invalid activities and customize an error message when there is a POST request

--- core/controllers/moderator.py
+++ core/controllers/moderator.py
@@ -78,15 +78,60 @@ class FeaturedActivitiesHandler(
             'featured_activity_reference_dicts']
 
         try:
-            summary_services.require_activities_to_be_public(
+            # Retrieve the list for each type of inavlid ID.
+            dne_exp, dne_col, priv_exp, priv_col = (
+                summary_services.check_activity_id_validity(
                 featured_activity_references)
+            )
+
+            # If all of the lists are empty, there are no invalid IDs.
+            if ((not dne_exp) & (not dne_col) &
+                (not priv_exp) & (not priv_col)):
+                activity_services.update_featured_activity_references(
+                featured_activity_references)
+                self.render_json({})
+            else:
+
+                error_message = ''
+
+                # If there are IDs for non-existent Explorations.
+                if dne_exp:
+                    error = (
+                        f'These Exploration IDs do not exist: '
+                        f'{", ".join(str(id) for id in dne_exp)}. '
+                    )
+                    error_message = error_message + error
+
+                # If there are IDs for non-existent Collections.
+                if dne_col:
+                    error = (
+                        f'These Collection IDs do not exist: '
+                        f'{", ".join(str(id) for id in dne_col)}. '
+                    )
+                    error_message = error_message + error
+
+                # If there are IDs for private Explorations.
+                if priv_exp:
+                    error = (
+                        f'These Exploration IDs are private: '
+                        f'{", ".join(str(id) for id in priv_exp)}. '
+                    )
+                    error_message = error_message + error
+
+                # If there are IDs for private Collections.
+                if priv_col:
+                    error = (
+                        f'These Collection IDs are private: '
+                        f'{", ".join(str(id) for id in priv_col)}. '
+                    )
+                    error_message = error_message + error
+
+                error_message = f'{error_message}Please enter a different ID.'
+
+                raise self.InvalidInputException(error_message)
+
         except Exception as e:
             raise self.InvalidInputException(e)
-
-        activity_services.update_featured_activity_references(
-            featured_activity_references)
-
-        self.render_json({})
 
 
 class EmailDraftHandler(base.BaseHandler[Dict[str, str], Dict[str, str]]):
