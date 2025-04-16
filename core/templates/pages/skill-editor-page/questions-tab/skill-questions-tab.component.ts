4bd23788d9eb35e255a23afa20a1bb046b0c8805
Simon Sherbet <sas9889@nyu.edu>
2025-04-16 16:04:15 -0400

    merge ready

--- core/templates/pages/skill-editor-page/questions-tab/skill-questions-tab.component.ts
+++ core/templates/pages/skill-editor-page/questions-tab/skill-questions-tab.component.ts
@@ -16,7 +16,7 @@
  * @fileoverview Component for the questions tab.
  */
 
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit, Input} from '@angular/core';
 import {Rubric} from 'domain/skill/rubric.model';
 import {Skill} from 'domain/skill/SkillObjectFactory';
 import {Subscription} from 'rxjs';
@@ -43,9 +43,6 @@ export class SkillQuestionsTabComponent implements OnInit, OnDestroy {
   directiveSubscriptions = new Subscription();
   _init(): void {
     this.skill = this.skillEditorStateService.getSkill();
-    this.difficultyCount = this.skill
-      .getRubrics()
-      .filter(r => r._explanations?.length > 0).length;
     this.groupedSkillSummaries =
       this.skillEditorStateService.getGroupedSkillSummaries();
     this.skillIdToRubricsObject = {};
@@ -56,6 +53,9 @@ export class SkillQuestionsTabComponent implements OnInit, OnDestroy {
     if (this.skillEditorStateService.getSkill() !== undefined) {
       this._init();
     }
+    this.difficultyCount = this.skill
+      .getRubrics()
+      .filter(r => r._explanations?.length > 0).length;
     this.directiveSubscriptions.add(
       this.skillEditorStateService.onSkillChange.subscribe(() => this._init())
     );
