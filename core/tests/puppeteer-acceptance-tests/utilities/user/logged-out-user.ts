7a288e2d7806742a5611da0ba4fc3888caf84e95
Jayam Patel <jayampatel04@gmail.com>
2025-04-24 11:27:53 +0530

    Fixed selector took too long

--- core/tests/puppeteer-acceptance-tests/utilities/user/logged-out-user.ts
+++ core/tests/puppeteer-acceptance-tests/utilities/user/logged-out-user.ts
@@ -2677,8 +2677,11 @@ export class LoggedOutUser extends BaseUser {
    */
   async continueToNextCard(): Promise<void> {
     try {
-      await this.page.waitForSelector(nextCardButton, {timeout: 7000});
+      showMessage('Hello 1');
+      await this.page.waitForSelector(nextCardButton, {timeout: 10000});
+      showMessage('Hello 2');
       await this.clickOn(nextCardButton);
+      showMessage('Hello 3');
     } catch (error) {
       if (error instanceof puppeteer.errors.TimeoutError) {
         await this.clickOn(nextCardArrowButton);
