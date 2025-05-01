3f338e7a0864a5a9bdd1b081e9d428024eebc83c
EliasBerhe <berhee@dickinson.edu>
2025-04-30 22:39:44 -0400

    Fix again liniting issues in numeric input test file

--- extensions/interactions/NumericInput/directives/numeric-input-validation.service.spec.ts
+++ extensions/interactions/NumericInput/directives/numeric-input-validation.service.spec.ts
@@ -296,8 +296,7 @@ describe('NumericInputValidationService', () => {
       {
         type: WARNING_TYPES.ERROR,
         message:
-          'Learner answer 1 input should be greater than ' +
-          'or equal to zero.',
+          'Learner answer 1 input should be greater than ' + 'or equal to zero',
       },
     ]);
   });
