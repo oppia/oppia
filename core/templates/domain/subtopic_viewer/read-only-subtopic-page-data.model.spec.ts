ba29a8d735ec2ca5d3c05ebd32295c2bc70e319f
Mohit Ruwatia <mohitruwatia2312@gmail.com>
2025-06-23 21:48:27 +0530

    Frontent coverage

--- core/templates/domain/subtopic_viewer/read-only-subtopic-page-data.model.spec.ts
+++ core/templates/domain/subtopic_viewer/read-only-subtopic-page-data.model.spec.ts
@@ -20,7 +20,7 @@ import {SubtopicPageContents} from 'domain/topic/subtopic-page-contents.model';
 
 import {ReadOnlySubtopicPageData} from 'domain/subtopic_viewer/read-only-subtopic-page-data.model';
 
-fdescribe('Subtopic data object factory', () => {
+describe('Subtopic data object factory', () => {
   describe('subtopic data object factory', () => {
     var _sampleSubtopicDataWithPageContents: ReadOnlySubtopicPageData;
 
