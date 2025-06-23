5143f0ad335b07ca11582d04e0dfda146dd17d86
Mohit Ruwatia <mohitruwatia2312@gmail.com>
2025-06-23 23:08:04 +0530

    Taking screenshots

--- core/tests/puppeteer-acceptance-tests/utilities/common/puppeteer-utils.ts
+++ core/tests/puppeteer-acceptance-tests/utilities/common/puppeteer-utils.ts
@@ -844,27 +844,53 @@ export class BaseUser {
     }
 
     try {
-      expect(await currentPage.screenshot()).toMatchImageSnapshot({
-        failureThreshold: failureTrigger,
-        failureThresholdType: 'percent',
-        customSnapshotIdentifier: imageName,
-        customSnapshotsDir: path.join(testPath, dirName),
-        /*
-         * The following checks if the tests are running on CI. If it is, the folder diff-snapshots will be uploaded as
-         * artifacts in the github workflow.
-         */
-        customDiffDir: __dirname.startsWith('/home/runner')
-          ? path.join(
-              '/home/runner/work/oppia/oppia/core/tests/puppeteer-acceptance-tests/specs/logged-in-user/prod-desktop-screenshots',
-              path.basename(dirName)
-            )
-          : path.join(testPath, dirName, 'prod-desktop-screenshots'),
-      });
-      if (typeof newPage !== 'undefined') {
-        await newPage.close();
-      }
       if (dirName === '/prod-desktop-screenshots') {
-        throw new Error('To download ss');
+        expect(await currentPage.screenshot()).toMatchImageSnapshot({
+          failureThreshold: failureTrigger,
+          failureThresholdType: 'percent',
+          customSnapshotIdentifier: imageName,
+          customSnapshotsDir: path.join(testPath, dirName),
+          /*
+           * The following checks if the tests are running on CI. If it is, the folder diff-snapshots will be uploaded as
+           * artifacts in the github workflow.
+           */
+          customDiffDir: __dirname.startsWith('/home/runner')
+            ? path.join(
+                '/home/runner/work/oppia/oppia/core/tests/puppeteer-acceptance-tests/specs/logged-in-user/prod-desktop-screenshots',
+                path.basename(dirName)
+              )
+            : path.join(testPath, dirName, 'prod-desktop-screenshots'),
+        });
+        if (typeof newPage !== 'undefined') {
+          await newPage.close();
+        }
+        if (dirName === '/prod-desktop-screenshots') {
+          throw new Error('To download ss');
+        }
+      }
+      if (dirName === '/prod-mobile-screenshots') {
+        expect(await currentPage.screenshot()).toMatchImageSnapshot({
+          failureThreshold: failureTrigger,
+          failureThresholdType: 'percent',
+          customSnapshotIdentifier: imageName,
+          customSnapshotsDir: path.join(testPath, dirName),
+          /*
+           * The following checks if the tests are running on CI. If it is, the folder diff-snapshots will be uploaded as
+           * artifacts in the github workflow.
+           */
+          customDiffDir: __dirname.startsWith('/home/runner')
+            ? path.join(
+                '/home/runner/work/oppia/oppia/core/tests/puppeteer-acceptance-tests/specs/logged-in-user/prod-mobile-screenshots',
+                path.basename(dirName)
+              )
+            : path.join(testPath, dirName, 'prod-mobile-screenshots'),
+        });
+        if (typeof newPage !== 'undefined') {
+          await newPage.close();
+        }
+        if (dirName === '/prod-mobile-screenshots') {
+          throw new Error('To download ss');
+        }
       }
     } catch (error) {
       if (__dirname.startsWith('/home/runner')) {
