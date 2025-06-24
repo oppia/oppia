5c1f36ffb8967f54dcbf021983ef7611c8065de6
Mohit Ruwatia <mohitruwatia2312@gmail.com>
2025-06-25 04:33:27 +0530

    Updated android controller

--- assets/constants.ts
+++ assets/constants.ts
@@ -84,6 +84,8 @@ export default {
   "ACTIVITY_TYPE_STORY": "story",
   "ACTIVITY_TYPE_SKILL": "skill",
   "ACTIVITY_TYPE_SUBTOPIC": "subtopic",
+  "ACTIVITY_TYPE_SUBTOPIC_WITH_STUDY_GUIDE_OLD": "subtopic_with_study_guide_old",
+  "ACTIVITY_TYPE_SUBTOPIC_WITH_STUDY_GUIDE_NEW": "subtopic_with_study_guide_new",
   "ACTIVITY_TYPE_LEARN_TOPIC": "learntopic",
   "ACTIVITY_TYPE_CLASSROOM": "classroom",
   "DISABLED_EXPLORATION_IDS": ["5"],
