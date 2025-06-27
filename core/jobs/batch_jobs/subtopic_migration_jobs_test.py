122862acf63bdd0e25b2e5735917e6f6190be62d
Mohit Ruwatia <mohitruwatia2312@gmail.com>
2025-06-27 19:23:28 +0530

    Modified tests

--- core/jobs/batch_jobs/subtopic_migration_jobs_test.py
+++ core/jobs/batch_jobs/subtopic_migration_jobs_test.py
@@ -19,6 +19,7 @@
 from __future__ import annotations
 
 from core import feconf
+from core.domain import study_guide_domain
 from core.domain import subtopic_page_domain
 from core.domain import topic_domain
 from core.jobs import job_test_utils
@@ -90,6 +91,244 @@ class MigrateSubtopicPageJobTests(job_test_utils.JobTestBase):
 
     def test_broken_subtopic_leads_to_no_migration(self) -> None:
         first_unmigrated_subtopic_model = self.create_model(
+            subtopic_models.SubtopicPageModel,
+            id=self.SUBTOPIC_1_ID,
+            topic_id=self.TOPIC_1_ID,
+            page_contents=self.subtopic_page.page_contents.to_dict(),
+            page_contents_schema_version=4,
+            language_code='abc',
+        )
+        first_unmigrated_subtopic_model.update_timestamps()
+        first_unmigrated_subtopic_model.commit(
+            feconf.SYSTEM_COMMITTER_ID,
+            'Create subtopic',
+            [{'cmd': topic_domain.CMD_CREATE_NEW}]
+        )
+
+        second_unmigrated_subtopic_model = self.create_model(
+            subtopic_models.SubtopicPageModel,
+            id=self.SUBTOPIC_2_ID,
+            topic_id=self.TOPIC_1_ID,
+            page_contents=self.subtopic_page.page_contents.to_dict(),
+            page_contents_schema_version=3,
+            language_code='en',
+        )
+        second_unmigrated_subtopic_model.update_timestamps()
+        second_unmigrated_subtopic_model.commit(
+            feconf.SYSTEM_COMMITTER_ID,
+            'Create subtopic',
+            [{'cmd': topic_domain.CMD_CREATE_NEW}]
+        )
+        self.assert_job_output_is([
+            job_run_result.JobRunResult(
+                stderr=(
+                    'SUBTOPIC PROCESSED ERROR: "(\'subtopic_1_id\', '
+                    'ValidationError(\'Invalid language code: abc\''
+                    '))": 1'
+                )
+            ),
+            job_run_result.JobRunResult(
+                stdout='SUBTOPIC PROCESSED SUCCESS: 1'
+            )
+        ])
+        first_migrated_model = subtopic_models.SubtopicPageModel.get(
+            self.SUBTOPIC_1_ID)
+        self.assertEqual(first_migrated_model.version, 1)
+
+        second_migrated_model = subtopic_models.SubtopicPageModel.get(
+            self.SUBTOPIC_2_ID)
+        self.assertEqual(second_migrated_model.version, 1)
+
+    def test_migrated_subtopic_is_not_migrated(self) -> None:
+        unmigrated_subtopic_model = self.create_model(
+            subtopic_models.SubtopicPageModel,
+            id=self.SUBTOPIC_1_ID,
+            topic_id=self.TOPIC_1_ID,
+            page_contents=self.subtopic_page.page_contents.to_dict(),
+            page_contents_schema_version=(
+                feconf.CURRENT_SUBTOPIC_PAGE_CONTENTS_SCHEMA_VERSION
+            ),
+            language_code='en',
+        )
+        unmigrated_subtopic_model.update_timestamps()
+        unmigrated_subtopic_model.commit(
+            feconf.SYSTEM_COMMITTER_ID,
+            'Create subtopic',
+            [{'cmd': topic_domain.CMD_CREATE_NEW}]
+        )
+
+        self.assert_job_output_is([
+            job_run_result.JobRunResult(
+                stdout='SUBTOPIC PROCESSED SUCCESS: 1'
+            ),
+            job_run_result.JobRunResult(
+                stdout='SUBTOPIC PREVIOUSLY MIGRATED SUCCESS: 1'
+            ),
+        ])
+
+        migrated_subtopic_model = subtopic_models.SubtopicPageModel.get(
+            self.SUBTOPIC_1_ID)
+        self.assertEqual(migrated_subtopic_model.version, 1)
+
+
+class AuditSubtopicMigrationJobTests(job_test_utils.JobTestBase):
+
+    JOB_CLASS: Type[
+        subtopic_migration_jobs.AuditSubtopicMigrationJob
+    ] = subtopic_migration_jobs.AuditSubtopicMigrationJob
+
+    TOPIC_1_ID: Final = 'topic_1_id'
+    SUBTOPIC_1_ID: Final = 'subtopic_1_id'
+    SUBTOPIC_2_ID: Final = 'subtopic_2_id'
+    subtopic_id = 1
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.subtopic_page = (
+            subtopic_page_domain.SubtopicPage.create_default_subtopic_page(
+                self.subtopic_id, self.TOPIC_1_ID))
+
+    def test_empty_storage(self) -> None:
+        self.assert_job_output_is_empty()
+
+    def test_broken_subtopic_leads_to_no_migration(self) -> None:
+        first_unmigrated_subtopic_model = self.create_model(
+            subtopic_models.SubtopicPageModel,
+            id=self.SUBTOPIC_1_ID,
+            topic_id=self.TOPIC_1_ID,
+            page_contents=self.subtopic_page.page_contents.to_dict(),
+            page_contents_schema_version=4,
+            language_code='abc',
+        )
+        first_unmigrated_subtopic_model.update_timestamps()
+        first_unmigrated_subtopic_model.commit(
+            feconf.SYSTEM_COMMITTER_ID,
+            'Create subtopic',
+            [{'cmd': topic_domain.CMD_CREATE_NEW}]
+        )
+
+        second_unmigrated_subtopic_model = self.create_model(
+            subtopic_models.SubtopicPageModel,
+            id=self.SUBTOPIC_2_ID,
+            topic_id=self.TOPIC_1_ID,
+            page_contents=self.subtopic_page.page_contents.to_dict(),
+            page_contents_schema_version=3,
+            language_code='en',
+        )
+        second_unmigrated_subtopic_model.update_timestamps()
+        second_unmigrated_subtopic_model.commit(
+            feconf.SYSTEM_COMMITTER_ID,
+            'Create subtopic',
+            [{'cmd': topic_domain.CMD_CREATE_NEW}]
+        )
+        self.assert_job_output_is([
+            job_run_result.JobRunResult(
+                stderr=(
+                    'SUBTOPIC PROCESSED ERROR: "(\'subtopic_1_id\', '
+                    'ValidationError(\'Invalid language code: abc\''
+                    '))": 1'
+                )
+            ),
+            job_run_result.JobRunResult(
+                stdout='SUBTOPIC PROCESSED SUCCESS: 1'
+            )
+        ])
+        first_migrated_model = subtopic_models.SubtopicPageModel.get(
+            self.SUBTOPIC_1_ID)
+        self.assertEqual(first_migrated_model.version, 1)
+
+        second_migrated_model = subtopic_models.SubtopicPageModel.get(
+            self.SUBTOPIC_2_ID)
+        self.assertEqual(second_migrated_model.version, 1)
+
+    def test_migrated_subtopic_is_not_migrated(self) -> None:
+        unmigrated_subtopic_model = self.create_model(
+            subtopic_models.SubtopicPageModel,
+            id=self.SUBTOPIC_1_ID,
+            topic_id=self.TOPIC_1_ID,
+            page_contents=self.subtopic_page.page_contents.to_dict(),
+            page_contents_schema_version=(
+                feconf.CURRENT_SUBTOPIC_PAGE_CONTENTS_SCHEMA_VERSION
+            ),
+            language_code='en',
+        )
+        unmigrated_subtopic_model.update_timestamps()
+        unmigrated_subtopic_model.commit(
+            feconf.SYSTEM_COMMITTER_ID,
+            'Create subtopic',
+            [{'cmd': topic_domain.CMD_CREATE_NEW}]
+        )
+
+        self.assert_job_output_is([
+            job_run_result.JobRunResult(
+                stdout='SUBTOPIC PROCESSED SUCCESS: 1'
+            ),
+            job_run_result.JobRunResult(
+                stdout='SUBTOPIC PREVIOUSLY MIGRATED SUCCESS: 1'
+            ),
+        ])
+
+        migrated_subtopic_model = subtopic_models.SubtopicPageModel.get(
+            self.SUBTOPIC_1_ID)
+        self.assertEqual(migrated_subtopic_model.version, 1)
+
+
+class MigrateStudyGuideJobTests(job_test_utils.JobTestBase):
+
+    JOB_CLASS: Type[
+        subtopic_migration_jobs.MigrateStudyGuideJob
+    ] = subtopic_migration_jobs.MigrateStudyGuideJob
+
+    TOPIC_1_ID: Final = 'topic_1_id'
+    SUBTOPIC_1_ID: Final = 'subtopic_1_id'
+    SUBTOPIC_2_ID: Final = 'subtopic_2_id'
+    subtopic_id = 1
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.study_guide = (
+            study_guide_domain.StudyGuide.create_study_guide(
+                self.subtopic_id, self.TOPIC_1_ID,
+                'Heading 1', '<p>Content 1</p>'))
+
+    def test_empty_storage(self) -> None:
+        self.assert_job_output_is_empty()
+
+    def test_unmigrated_study_guide_with_unmigrated_prop_is_migrated(
+        self
+    ) -> None:
+        unmigrated_study_guide_model = self.create_model(
+            subtopic_models.StudyGuideModel,
+            id=self.SUBTOPIC_1_ID,
+            topic_id=self.TOPIC_1_ID,
+            sections=self.study_guide.sections[0].to_dict(),
+            sections_schema_version=0,
+            language_code='cs',
+            version=1
+        )
+        unmigrated_study_guide_model.update_timestamps()
+        unmigrated_study_guide_model.commit(
+            feconf.SYSTEM_COMMITTER_ID,
+            'Create study guide',
+            [{'cmd': topic_domain.CMD_CREATE_NEW}]
+        )
+
+        self.assert_job_output_is([
+            job_run_result.JobRunResult(
+                stdout='STUDY GUIDE PROCESSED SUCCESS: 1'),
+            job_run_result.JobRunResult(
+                stdout='STUDY GUIDE MIGRATED SUCCESS: 1')
+        ])
+
+        migrated_study_guide_model = subtopic_models.StudyGuideModel.get(
+            self.SUBTOPIC_1_ID
+        )
+        self.assertEqual(
+            migrated_study_guide_model.sections_schema_version,
+            feconf.CURRENT_STUDY_GUIDE_SECTIONS_SCHEMA_VERSION)
+
+    def test_broken_study_guide_leads_to_no_migration(self) -> None:
+        first_unmigrated_study_guide_model = self.create_model(
             subtopic_models.SubtopicPageModel,
             id=self.SUBTOPIC_1_ID,
             topic_id=self.TOPIC_1_ID,
