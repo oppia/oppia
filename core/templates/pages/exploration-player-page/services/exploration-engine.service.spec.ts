a1b4008c6df519542da02827a0d2cdd89e9e5b89
Kartik <158498247+KartikSuryavanshi@users.noreply.github.com>
2025-07-08 12:34:16 +0530

    made changes

--- core/templates/pages/exploration-player-page/services/exploration-engine.service.spec.ts
+++ core/templates/pages/exploration-player-page/services/exploration-engine.service.spec.ts
@@ -67,7 +67,6 @@ describe('Exploration engine service ', () => {
   let readOnlyExplorationBackendApiService: ReadOnlyExplorationBackendApiService;
   let statsReportingService: StatsReportingService;
   let urlService: UrlService;
-  let paramChangeObjectFactory: ParamChangeObjectFactory;
   let textInputService: TextInputRulesService;
   let translateService: TranslateService;
   let explorationDict: ExplorationBackendDict;
@@ -386,7 +385,6 @@ describe('Exploration engine service ', () => {
     statsReportingService = TestBed.inject(StatsReportingService);
     urlService = TestBed.inject(UrlService);
     explorationEngineService = TestBed.inject(ExplorationEngineService);
-    paramChange = TestBed.inject(ParamChange);
     textInputService = TestBed.inject(TextInputRulesService);
     translateService = TestBed.inject(TranslateService);
   });
