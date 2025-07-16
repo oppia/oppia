d1f588ecbe592407b7a7dadb51791f20c44e1514
Kartik <158498247+KartikSuryavanshi@users.noreply.github.com>
2025-07-16 21:13:50 +0530

    Made changes requested

--- core/templates/domain/exploration/written-translation.model.spec.ts
+++ core/templates/domain/exploration/written-translation.model.spec.ts
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 /**
- * @fileoverview Unit tests for the WrittenTranslation object factory.
+ * @fileoverview Unit tests for the WrittenTranslation.
  */
 
 import {
