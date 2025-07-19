1c89ccc48f701d0403e710852db0945daf6e49c7
Kartik <158498247+KartikSuryavanshi@users.noreply.github.com>
2025-07-19 19:45:07 +0530

    changes made

--- core/templates/domain/exploration/lost-change.model.ts
+++ core/templates/domain/exploration/lost-change.model.ts
@@ -245,6 +245,14 @@ export class LostChange {
     utilsService: UtilsService,
     lostChangeDict: ExplorationChange | LostChangeBackendDict
   ): LostChange {
+    if (
+      !lostChangeDict ||
+      typeof lostChangeDict !== 'object' ||
+      !('cmd' in lostChangeDict)
+    ) {
+      throw new Error('Invalid lostChangeDict passed to LostChange.createNew');
+    }
+
     const dict = lostChangeDict as LostChangeBackendDict;
     return new LostChange(
       utilsService,
