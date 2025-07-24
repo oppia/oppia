ba2ab2b974a46e1d99693f4e072a99b75798c3f6
Kartik <158498247+KartikSuryavanshi@users.noreply.github.com>
2025-07-24 21:16:51 +0530

    Made changes

--- core/templates/domain/state/StateObjectFactory.ts
+++ core/templates/domain/state/StateObjectFactory.ts
@@ -20,7 +20,7 @@ import {Injectable} from '@angular/core';
 
 import {
   InteractionBackendDict,
-  Interaction
+  Interaction,
 } from 'domain/exploration/interaction.model';
 import {
   ParamChangeBackendDict,
