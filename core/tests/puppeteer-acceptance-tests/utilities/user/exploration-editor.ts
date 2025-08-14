6a8ebdbf04f657b33f4284f1c3c347bf0ce2e6a8
Jayam Patel <jayampatel04@gmail.com>
2025-08-14 16:58:46 +0530

    removed unused function

--- core/tests/puppeteer-acceptance-tests/utilities/user/exploration-editor.ts
+++ core/tests/puppeteer-acceptance-tests/utilities/user/exploration-editor.ts
@@ -6589,17 +6589,6 @@ export class ExplorationEditor extends BaseUser {
   ): Promise<void> {
     await this.expectElementToBeVisible(nodeWarningSignSelector, visible);
   }
-
-  // async republishTheExploration(descriptionOfChanges: string): Promise<void> {
-  //   // Click on save changes button.
-  //   await this.expectElementToBeVisible(saveChangesButton);
-  //   await this.clickOn(saveChangesButton);
-
-  //   // Add description of changes.
-  //   await this.expectElementToBeVisible(saveDraftTitleSelector);
-  //   await this.type('textarea', descriptionOfChanges);
-
-  // }
 }
 
 export let ExplorationEditorFactory = (): ExplorationEditor =>
