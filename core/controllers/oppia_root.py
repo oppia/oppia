39dbe83d90171460b85d245afa81aa4f01d3f054
mosin74 <work.mosin74@gmail.com>
2025-08-20 18:24:45 +0000

    Lint check

--- core/controllers/oppia_root.py
+++ core/controllers/oppia_root.py
@@ -81,3 +81,4 @@ class OppiaLightweightRootPage(
             self.render_template('lightweight-oppia-root.mainpage.html')
             return
         self.render_template('index.html', template_is_aot_compiled=True)
+        
