6e488df1abd5439eed4c2083e9aa61fbb97dd887
mosin74 <work.mosin74@gmail.com>
2025-08-22 15:07:09 +0000

    translator-overview.component test issue

--- core/templates/pages/exploration-editor-page/translation-tab/translator-overview/translator-overview.component.spec.ts
+++ core/templates/pages/exploration-editor-page/translation-tab/translator-overview/translator-overview.component.spec.ts
@@ -45,12 +45,12 @@ import {ChangeListService} from '../../services/change-list.service';
 import {EntityTranslation} from 'domain/translation/EntityTranslationObjectFactory';
 import {TranslatedContent} from 'domain/exploration/TranslatedContentObjectFactory';
 import {EntityVoiceoversService} from 'services/entity-voiceovers.services';
+import {PlatformFeatureService} from 'services/platform-feature.service';
 import {EntityVoiceovers} from '../../../../domain/voiceover/entity-voiceovers.model';
 import {Voiceover} from '../../../../domain/exploration/voiceover.model';
 import {LocalStorageService} from 'services/local-storage.service';
 import {VoiceoverPlayerService} from '../../../exploration-player-page/services/voiceover-player.service';
 import {VoiceoverLanguageManagementService} from 'services/voiceover-language-management-service';
-import {PlatformFeatureService} from 'services/platform-feature.service';
 class MockNgbModal {
   open() {
     return {
