b141c31e7e5b17d3f5075d64a9f1071a289bc750
Jayam Patel <jayampatel04@gmail.com>
2025-09-13 10:16:41 +0530

    Updates Puppeteer not to follow new tab

--- core/tests/puppeteer-acceptance-tests/utilities/common/puppeteer-utils.ts
+++ core/tests/puppeteer-acceptance-tests/utilities/common/puppeteer-utils.ts
@@ -165,7 +165,7 @@ export class BaseUser {
           }
 
           const config = {
-            followNewTab: true,
+            followNewTab: false,
             fps: 25,
             ffmpeg_Path: null,
             // Below dimensions are of recorded video.
