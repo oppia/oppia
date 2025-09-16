84e983f01dd9cc1163d0d5bc75a25d88fd4edbe3
FaihAbiola <abiolafaith92@gmail.com>
2025-09-16 10:39:47 +0100

    Fix linting errors

--- core/platform/storage/cloud_storage_emulator.py
+++ core/platform/storage/cloud_storage_emulator.py
@@ -22,8 +22,9 @@ import mimetypes
 
 from core import feconf
 
-import redis
 from typing import Dict, List, Mapping, Optional, Union
+import redis
+
 # Here we use MyPy ignore because the redis library's type stubs
 # do not fully support subscript notation for StrictRedis, and MyPy
 # requires explicit type parameters.
