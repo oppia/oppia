1ffa7a473bc409dff8afb14bf5fdc943581532ac
Ankita Saxena <ankitasonu24@gmail.com>
2025-09-16 23:23:43 +0100

    Fix lint

--- core/platform/cache/redis_cache_services.py
+++ core/platform/cache/redis_cache_services.py
@@ -19,8 +19,7 @@
 from __future__ import annotations
 
 from core import feconf
-from core.domain import caching_domain
-from core.domain import redis_services
+from core.domain import caching_domain, redis_services
 from core.platform import models
 
 import redis
