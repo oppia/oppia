76e03f92f214468f2fd0418fc6ed6a4ddc66a1aa
Ankita Saxena <ankitasonu24@gmail.com>
2025-09-16 23:22:57 +0100

    Fix lint

--- core/storage/redis/gae_models_test.py
+++ core/storage/redis/gae_models_test.py
@@ -21,8 +21,7 @@ from core.tests import test_utils
 
 MYPY = False
 if MYPY: # pragma: no cover
-    from mypy_imports import base_models
-    from mypy_imports import redis_client_models
+    from mypy_imports import base_models, redis_client_models
 
 (base_models, redis_client_models) = models.Registry.import_models([
     models.Names.BASE_MODEL, models.Names.REDIS_CLIENT
