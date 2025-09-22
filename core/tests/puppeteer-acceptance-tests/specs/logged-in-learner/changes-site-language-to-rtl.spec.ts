59d6877ec791ee0a3e4c86e7aed867e4d8e5f00f
hardikgoyal2003 <hardikgoyal2003@gmail.com>
2025-09-23 03:28:36 +0530

    Fix acceptance tests

--- core/tests/puppeteer-acceptance-tests/specs/logged-in-learner/changes-site-language-to-rtl.spec.ts
+++ core/tests/puppeteer-acceptance-tests/specs/logged-in-learner/changes-site-language-to-rtl.spec.ts
@@ -133,7 +133,10 @@ describe('Logged-In Learner', function () {
 
     await loggedInUser1.navigateToHome(false);
     await loggedInUser1.verifyPageIsRTL();
-    await loggedInUser1.expectScreenshotToMatch('RTLArabicHomePage', __dirname);
+    await loggedInUser1.expectScreenshotToMatch(
+      'RTLArabicLearnerDashboard',
+      __dirname
+    );
   });
 
   it('should be able to visit about page', async function () {
