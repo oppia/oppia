ba992474508106a14560e01817902c064bd5663a
hardikgoyal2003 <hardikgoyal2003@gmail.com>
2025-09-22 07:09:31 +0530

    Fix some more tests

--- scripts/check_frontend_test_coverage.py
+++ scripts/check_frontend_test_coverage.py
@@ -63,17 +63,17 @@ NOT_FULLY_COVERED_FILENAMES = [
     'core/templates/domain/question/question.model.ts',
     'core/templates/expressions/expression-interpolation.service.ts',
     'core/templates/google-analytics.initializer.ts',
+    'core/templates/pages/blog-post-page/blog-post-page.component.ts',
+    'core/templates/pages/contributor-dashboard-page/modal-templates/translation-suggestion-review-modal.component.ts',
     'core/templates/pages/exploration-editor-page/services/'
         'exploration-states.service.ts',
     'core/templates/pages/exploration-player-page/services/'
         'learner-answer-info.service.ts',
+    'core/templates/services/suggestion-modal.service.ts',
     'extensions/interactions/MusicNotesInput/directives/'
         'oppia-interactive-music-notes-input.component.ts',
     'extensions/interactions/PencilCodeEditor/directives/'
-        'oppia-interactive-pencil-code-editor.component.ts',
-    'core/templates/services/suggestion-modal.service.ts',
-    'core/templates/pages/blog-post-page/blog-post-page.component.ts',
-    'core/templates/pages/contributor-dashboard-page/modal-templates/translation-suggestion-review-modal.component.ts'
+        'oppia-interactive-pencil-code-editor.component.ts'
 ]
 
 
