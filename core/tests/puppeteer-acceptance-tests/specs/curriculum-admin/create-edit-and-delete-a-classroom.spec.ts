9f056d74dbc50bd0d5684a6434ff7ad6e3d3ffa0
Jayam Patel <jayampatel04@gmail.com>
2025-09-27 02:37:34 +0530

    Refactor clickOn to use clickOnElementWithText method

--- core/tests/puppeteer-acceptance-tests/specs/curriculum-admin/create-edit-and-delete-a-classroom.spec.ts
+++ core/tests/puppeteer-acceptance-tests/specs/curriculum-admin/create-edit-and-delete-a-classroom.spec.ts
@@ -144,9 +144,9 @@ describe('Curriculum Admin', function () {
       'science',
       'Test Topic 1'
     );
-    await curriculumAdmin.clickOn('Change Order');
+    await curriculumAdmin.clickOnElementWithText('Change Order');
     await curriculumAdmin.moveClassroomInOrder(['Science', 'Math']);
-    await curriculumAdmin.clickOn('Save');
+    await curriculumAdmin.clickOnElementWithText('Save');
     await curriculumAdmin.expectClassroomsInOrder(['Science', 'Math']);
   });
 
