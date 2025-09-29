0efba38fce810dea4ba3ac9cb002f467ad9871cd
Kevin Thomas <kevintab@mbp.local>
2025-09-29 13:49:22 -0700

    Use NDB context when executing exploration validation

--- core/jobs/batch_jobs/exp_migration_jobs.py
+++ core/jobs/batch_jobs/exp_migration_jobs.py
@@ -76,9 +76,9 @@ class MigrateExplorationModels(beam.PTransform):  # type: ignore[misc]
         """
         try:
             exploration = exp_fetchers.get_exploration_from_model(exp_model)
-            exploration.validate(strict=exp_is_published)
 
             with datastore_services.get_ndb_context():
+                exploration.validate(strict=exp_is_published)
                 if exp_services.get_story_id_linked_to_exploration(
                         exp_model.id) is not None:
                     exp_services.validate_exploration_for_story(
