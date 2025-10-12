13f0262a5179395e5e00fae2f02182d35607dca5
DevalPrime <lodhwaldevis@gmail.com>
2025-10-12 17:28:40 +0000

    another fix

--- core/templates/components/question-directives/questions-list/questions-list.component.ts
+++ core/templates/components/question-directives/questions-list/questions-list.component.ts
@@ -201,7 +201,7 @@ export class QuestionsListComponent implements OnInit, OnDestroy {
     this.linkedSkillsWithDifficulty.forEach(linkedSkillWithDifficulty => {
       this.skillLinkageModificationsArray.push({
         id: linkedSkillWithDifficulty.getId(),
-        task: null,
+        task: '',
         difficulty: linkedSkillWithDifficulty.getDifficulty(),
       });
     });
