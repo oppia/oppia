53e74a7edf1fc715ab57690bb43e924dec48c710
hardikgoyal2003 <hardikgoyal2003@gmail.com>
2025-10-19 22:51:18 +0530

    Removed dtslint

--- scripts/run_frontend_tests.py
+++ scripts/run_frontend_tests.py
@@ -27,10 +27,6 @@ from typing import Optional, Sequence, Set
 
 from . import build, check_frontend_test_coverage, install_third_party_libs
 
-# These is a relative path from the oppia/ folder. They are relative because the
-# dtslint command prepends the current working directory to the path, even if
-# the given path is absolute.
-DTSLINT_TYPE_TESTS_DIR_RELATIVE_PATH = os.path.join('typings', 'tests')
 TYPESCRIPT_DIR_RELATIVE_PATH = os.path.join('node_modules', 'typescript', 'lib')
 MAX_ATTEMPTS = 2
 
@@ -44,11 +40,6 @@ a single test or test suite.
 """
 )
 
-_PARSER.add_argument(
-    '--dtslint_only',
-    help='optional; if specified, only runs dtslint type tests.',
-    action='store_true',
-)
 _PARSER.add_argument(
     '--skip_install',
     help='optional; if specified, skips installing dependencies',
@@ -95,37 +86,6 @@ _PARSER.add_argument(
 )
 
 
-def run_dtslint_type_tests() -> None:
-    """Runs the dtslint type tests in typings/tests."""
-    print('Running dtslint type tests.')
-
-    # Pass the local version of typescript. Otherwise, dtslint will download and
-    # install all versions of typescript.
-    cmd = [
-        './node_modules/dtslint/bin/index.js',
-        DTSLINT_TYPE_TESTS_DIR_RELATIVE_PATH,
-        '--localTs',
-        TYPESCRIPT_DIR_RELATIVE_PATH,
-    ]
-    task = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-    output_lines = []
-    # The value of `process.stdout` should not be None since we passed
-    # the `stdout=subprocess.PIPE` argument to `Popen`.
-    assert task.stdout is not None
-    # Reads and prints realtime output from the subprocess until it terminates.
-    while True:
-        line = task.stdout.readline()
-        # No more output from the subprocess, and the subprocess has ended.
-        if len(line) == 0 and task.poll() is not None:
-            break
-        if line:
-            print(line, end='')
-            output_lines.append(line)
-    print('Done!')
-    if task.returncode:
-        sys.exit('The dtslint (type tests) failed.')
-
-
 def get_file_spec(file_path: str) -> str | None:
     """Returns the spec file path for a given file path.
 
@@ -151,10 +111,6 @@ def get_file_spec(file_path: str) -> str | None:
 def main(args: Optional[Sequence[str]] = None) -> None:
     """Runs the frontend tests."""
     parsed_args = _PARSER.parse_args(args=args)
-
-    run_dtslint_type_tests()
-    if parsed_args.dtslint_only:
-        return
 
     if not parsed_args.skip_install:
         install_third_party_libs.main()
--- scripts/run_frontend_tests_test.py
+++ scripts/run_frontend_tests_test.py
@@ -179,42 +179,6 @@ class RunFrontendTestsTests(test_utils.GenericTestBase):
             check_frontend_test_coverage, 'main', mock_check_frontend_coverage
         )
 
-    def test_run_dtslint_type_tests_passed(self) -> None:
-        with self.swap_success_Popen, self.print_swap:
-            run_frontend_tests.run_dtslint_type_tests()
-        cmd = [
-            './node_modules/dtslint/bin/index.js',
-            run_frontend_tests.DTSLINT_TYPE_TESTS_DIR_RELATIVE_PATH,
-            '--localTs',
-            run_frontend_tests.TYPESCRIPT_DIR_RELATIVE_PATH,
-        ]
-        self.assertIn(cmd, self.cmd_token_list)
-        self.assertIn('Running dtslint type tests.', self.print_arr)
-        self.assertNotIn(
-            'The dtslint (type tests) failed.', self.sys_exit_message
-        )
-
-    def test_run_dtslint_type_tests_failed(self) -> None:
-        with self.swap_failed_Popen, self.print_swap:
-            with self.swap_sys_exit:
-                run_frontend_tests.run_dtslint_type_tests()
-        cmd = [
-            './node_modules/dtslint/bin/index.js',
-            run_frontend_tests.DTSLINT_TYPE_TESTS_DIR_RELATIVE_PATH,
-            '--localTs',
-            run_frontend_tests.TYPESCRIPT_DIR_RELATIVE_PATH,
-        ]
-        self.assertIn(cmd, self.cmd_token_list)
-        self.assertIn('Running dtslint type tests.', self.print_arr)
-        self.assertIn('The dtslint (type tests) failed.', self.sys_exit_message)
-
-    def test_no_tests_are_run_when_dtslint_flag_passed(self) -> None:
-        with self.swap_success_Popen, self.print_swap:
-            run_frontend_tests.main(args=['--dtslint_only'])
-        self.assertIn('Running dtslint type tests.', self.print_arr)
-        self.assertIn('Done!', self.print_arr)
-        self.assertEqual(len(self.cmd_token_list), 1)
-
     def test_frontend_tests_with_specs_to_run(self) -> None:
         original_os_path_exists = os.path.exists
 
@@ -238,8 +202,9 @@ class RunFrontendTestsTests(test_utils.GenericTestBase):
                             '--specs_to_run='
                             'home-page.component.spec.ts,'
                             'about-page.component.ts,'
-                            'test-module.js'
-                        ])
+                            'test-module.js',
+                        ]
+                    )
         cmd = [
             common.NODE_BIN_PATH,
             '--max-old-space-size=4096',
