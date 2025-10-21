1e47e767a3d93cfc2022abc6fe4b38b5b20aa4f5
NITISH084 <imnitishkumar04@gmail.com>
2025-10-21 21:27:57 +0530

    resolved ACCEPTANCE issues

--- core/templates/pages/learner-dashboard-page/home-tab.component.spec.ts
+++ core/templates/pages/learner-dashboard-page/home-tab.component.spec.ts
@@ -31,6 +31,7 @@ import {I18nLanguageCodeService} from 'services/i18n-language-code.service';
 import {SiteAnalyticsService} from 'services/site-analytics.service';
 import {CollectionSummary} from 'domain/collection/collection-summary.model';
 import {LearnerExplorationSummary} from 'domain/summary/learner-exploration-summary.model';
+import {PlatformFeatureService} from 'services/platform-feature.service';
 
 describe('Home tab Component', () => {
   let component: HomeTabComponent;
@@ -40,6 +41,14 @@ describe('Home tab Component', () => {
   let i18nLanguageCodeService: I18nLanguageCodeService;
   let mockResizeEmitter: EventEmitter<void>;
   let siteAnalyticsService: SiteAnalyticsService;
+  class MockPlatformFeatureService {
+    status = {
+      SerialChapterLaunchLearnerView: {
+        isEnabled: false,
+      },
+    };
+  }
+  let mockPlatformFeatureService = new MockPlatformFeatureService();
 
   beforeEach(async(() => {
     mockResizeEmitter = new EventEmitter();
@@ -55,6 +64,7 @@ describe('Home tab Component', () => {
             getResizeEvent: () => mockResizeEmitter,
           },
         },
+        {provide: PlatformFeatureService, useValue: mockPlatformFeatureService},
       ],
       schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
