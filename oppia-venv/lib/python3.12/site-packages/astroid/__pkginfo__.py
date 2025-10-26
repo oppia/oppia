078305137bc4730cfbc45cf27802b06b34035bd9
Meeth <gangwarmeeth@gmail.com>
2025-10-26 13:07:24 +0000

    Lint and elastic_search test file fix -4

--- oppia-venv/lib/python3.12/site-packages/astroid/__pkginfo__.py
+++
@@ -1,6 +0,0 @@
-# Licensed under the LGPL: https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html
-# For details: https://github.com/pylint-dev/astroid/blob/main/LICENSE
-# Copyright (c) https://github.com/pylint-dev/astroid/blob/main/CONTRIBUTORS.txt
-
-__version__ = "4.0.1"
-version = __version__
