e197a8b140085ce86a3ec4765826ffc52aaeb439
Preet <preetjain2200@gmail.com>
2025-11-05 14:17:17 +0000

    Fix jumpy scroll in editor and translation tours

--- core/templates/pages/exploration-editor-page/editor-tab/exploration-editor-tab.component.ts
+++ core/templates/pages/exploration-editor-page/editor-tab/exploration-editor-tab.component.ts
@@ -156,6 +156,14 @@ export class ExplorationEditorTabComponent implements OnInit, OnDestroy {
             .querySelector<HTMLElement>('.e2e-test-joyride-title')
             .focus();
 
+          if (value.number === 1) {
+            this.smoothScrollTo(0, 1000);
+          }
+
+          if (value.number === 5) {
+            this.smoothScrollTo(0, 1000);
+          }
+
           if (value.number === 2) {
             this.smoothScrollTo(0, 1000);
 
