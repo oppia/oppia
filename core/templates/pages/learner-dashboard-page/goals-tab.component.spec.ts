3cc1cd41ebb18c94fd3f139e4654ef79c61e0ebf
Meeth <gangwarmeeth@gmail.com>
2025-11-23 07:33:02 +0000

    Test add

--- core/templates/pages/learner-dashboard-page/goals-tab.component.spec.ts
+++ core/templates/pages/learner-dashboard-page/goals-tab.component.spec.ts
@@ -363,6 +363,22 @@ describe('Goals tab Component', () => {
     expect(component.currentGoalsStoryIsShown[0]).toEqual(false);
   });
 
+    it('should call preventDefault when checkbox is clicked', () => {
+    component.editGoalsTopicClassification = [0]
+    fixture.detectChanges()
+
+      const checkbox = fixture.nativeElement.querySelector(
+    '.e2e-test-remove-topic-from-current-goals-button'
+    );
+
+    const eventClick = new Event('click');
+    const preventDefaultSpy = spyOn(eventClick, 'preventDefault');
+
+    checkbox.dispatchEvent(eventClick);
+
+    expect(preventDefaultSpy).toHaveBeenCalled();
+  })
+
   it('should add topic to learner goals if not already present', () => {
     component.topicIdsInCurrentGoals.length = 0;
     component.topicIdsInCompletedGoals = ['1', '2'];
