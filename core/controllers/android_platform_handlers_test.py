b79adef719d6eed8bccc5a903a91776fee01fdba
yash bahuguna <yashbahuguna918@gmail.com>
2025-11-26 22:31:09 +0530

    chore: fix import order and update copyright header in android_platform_handlers_test.py

--- core/controllers/android_platform_handlers_test.py
+++ core/controllers/android_platform_handlers_test.py
@@ -1,22 +1,17 @@
 # coding: utf-8
-# Copyright 2025 The Oppia Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS-IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Copyright 2021 The Oppia Authors.
 
 """Tests for Android platform parameters and feature flags handlers."""
 
 from __future__ import annotations
 
+import logging
+from typing import Dict, List, Optional, Sequence, TypedDict, Union
+
+# Then all Oppia imports:
+from core import feconf
+from core.constants import constants
+from core.controllers import acl_decorators, base
 from core.tests import test_utils
 
 
