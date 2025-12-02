ed5c82797711b9fd5bdfe07cca1df93446504d90
mosin74 <mosin74@gmail.com>
2025-12-02 05:37:56 +0530

    Lint checks

--- core/jobs/batch_jobs/remove_hanging_story_references_job.py
+++ core/jobs/batch_jobs/remove_hanging_story_references_job.py
@@ -23,7 +23,7 @@ from core.jobs.types import job_run_result
 from core.platform import models
 
 import apache_beam as beam
-from typing import Iterable, List, Tuple, Optional
+from typing import Iterable, List, Optional, Tuple
 
 MYPY = False
 if MYPY:  # pragma: no cover
