bbf62190ed0b5b0fbb33de3b6a8e61b0461d3f5a
yash bahuguna <yashbahuguna918@gmail.com>
2025-12-05 19:39:39 +0530

    Fix: Add proper <br> tag support in RTE output display
    This fix adds template support for <br> tags in the RTE
    renderer so that line breaks created by CKEditor (via
    Shift+Enter) are properly rendered.
    
    Changes:
    - Added @ViewChild('br') brTagPortal in the component
    - Added <ng-template #br><br></ng-template> in the HTML
    
    This fixes the issue where text like 'time.<br><br>Incidentally'
    was being rendered as 'time.Incidentally' because <br> tags
    had no template to render them.
    
    The voiceover highlighting functionality remains intact as
    the sentence-wrapping logic already handled <br> preservation.
    
    Fixes #21226

--- extensions/rich_text_components/rte-output-display.component.ts
+++ extensions/rich_text_components/rte-output-display.component.ts
@@ -67,6 +67,7 @@ export class RteOutputDisplayComponent implements OnInit, AfterViewInit {
   @ViewChild('blockquote') blockquoteTagPortal: TemplateRef<unknown>;
   @ViewChild('em') emTagPortal: TemplateRef<unknown>;
   @ViewChild('text') textTagPortal: TemplateRef<unknown>;
+  @ViewChild('br') brTagPortal: TemplateRef<unknown>;
   // Oppia Non interactive.
   @ViewChild('collapsible') collapsibleTagPortal: TemplateRef<unknown>;
   @ViewChild('image') imageTagPortal: TemplateRef<unknown>;
