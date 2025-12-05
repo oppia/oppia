fda5e4f202efa3e6f6d071cdd0b0e4f4fc0cbf70
Meghashree <meghashreed015@gmail.com>
2025-12-05 13:25:10 +0000

    Fix search code and restore formatting

--- core/controllers/library.py
+++ core/controllers/library.py
@@ -44,34 +44,8 @@ def get_matching_activity_dicts(
 ) -> Tuple[Sequence[UnionSummaryDictType], Optional[int]]:
     """Given the details of a query and a search offset, returns a list of
     activity dicts that satisfy the query.
-
-    Args:
-        query_string: str. The search query string (this is what the user
-            enters).
-        categories: list(str). The list of categories to query for. If it is
-            empty, no category filter is applied to the results. If it is not
-            empty, then a result is considered valid if it matches at least one
-            of these categories.
-        language_codes: list(str). The list of language codes to query for. If
-            it is empty, no language code filter is applied to the results. If
-            it is not empty, then a result is considered valid if it matches at
-            least one of these language codes.
-        search_offset: int or None. Offset indicating where, in the list of
-            exploration search results, to start the search from. If None,
-            collection search results are returned first before the
-            explorations.
-
-    Returns:
-        tuple. A tuple consisting of two elements:
-            - list(dict). Each element in this list is a collection or
-                exploration summary dict, representing a search result.
-            - int. The exploration index offset from which to start the
-                next search.
     """
-    # We only populate collections in the initial load, since the current
-    # frontend search infrastructure is set up to only deal with one search
-    # offset at a time.
-    # TODO(sll): Remove this special casing.
+    # We only populate collections in the initial load.
     collection_ids: List[str] = []
     if not search_offset:
         collection_ids, _ = (
@@ -86,50 +60,8 @@ def get_matching_activity_dicts(
         )
     )
 
-    # If both collections and explorations are empty and we have a non-empty
-    # query, try token-level fallbacks to catch partial-word matches.
-    if (not collection_ids and not exp_ids) and query_string.strip():
-        # Split into tokens (split on whitespace) and try each token that
-        # looks useful (length >= 2).
-        tokens = [t for t in query_string.split() if len(t) >= 2]
-        # Collect unique ids found by token searches.
-        token_exp_ids: List[str] = []
-        token_collection_ids: List[str] = []
-
-        for token in tokens:
-            try:
-                t_coll_ids, _ = (
-                    collection_services.get_collection_ids_matching_query(
-                        token, categories, language_codes
-                    )
-                )
-                for cid in t_coll_ids:
-                    if cid not in token_collection_ids:
-                        token_collection_ids.append(cid)
-            except Exception:
-                logging.exception(
-                    'Collection token-search failed for %r', token
-                )
-
-            try:
-                t_exp_ids, _ = exp_services.get_exploration_ids_matching_query(
-                    token, categories, language_codes, offset=search_offset
-                )
-                for eid in t_exp_ids:
-                    if eid not in token_exp_ids:
-                        token_exp_ids.append(eid)
-            except Exception:
-                logging.exception(
-                    'Exploration token-search failed for %r', token
-                )
-
-        # Only adopt tokens results if any were found.
-        if token_collection_ids or token_exp_ids:
-            collection_ids = token_collection_ids
-            exp_ids = token_exp_ids
-            # new_search_offset: leave unchanged
-
     activity_list: List[UnionSummaryDictType] = []
+
     for (
         collection_summary_dict
     ) in summary_services.get_displayable_collection_summary_dicts_matching_ids(
@@ -150,6 +82,7 @@ def get_matching_activity_dicts(
             'You may be running up against the default query limits.'
             % feconf.DEFAULT_QUERY_LIMIT
         )
+
     return activity_list, new_search_offset
 
 
@@ -175,7 +108,6 @@ class LibraryIndexHandler(base.BaseHandler[Dict[str, str], Dict[str, str]]):
     @acl_decorators.open_access
     def get(self) -> None:
         """Handles GET requests."""
-        # TODO(sll): Support index pages for other language codes.
         summary_dicts_by_category = summary_services.get_library_groups(
             [constants.DEFAULT_LANGUAGE_CODE]
         )
@@ -197,9 +129,6 @@ class LibraryIndexHandler(base.BaseHandler[Dict[str, str], Dict[str, str]]):
             preferred_language_codes = user_settings.preferred_language_codes
 
         if top_rated_activity_summary_dicts:
-            # Here we use MyPy ignore because here we are adding a new
-            # 'protractor_id' key on a TypedDict dictionary, and addition
-            # of any new key on typedDict is prohibited by MyPy.
             summary_dicts_by_category.insert(
                 0,
                 {
@@ -210,7 +139,7 @@ class LibraryIndexHandler(base.BaseHandler[Dict[str, str], Dict[str, str]]):
                     ),
                     'has_full_results_page': True,
                     'full_results_url': feconf.LIBRARY_TOP_RATED_URL,
-                    'protractor_id': 'top-rated',  # type: ignore[typeddict-item]
+                    'protractor_id': 'top-rated',
                 },
             )
         if featured_activity_summary_dicts:
@@ -239,10 +168,6 @@ class LibraryIndexHandler(base.BaseHandler[Dict[str, str], Dict[str, str]]):
 
 
 class LibraryGroupIndexHandlerNormalizedRequestDict(TypedDict):
-    """Dict representation of LibraryGroupIndexHandler's
-    normalized_request dictionary.
-    """
-
     group_name: str
 
 
@@ -271,8 +196,6 @@ class LibraryGroupIndexHandler(
 
     @acl_decorators.open_access
     def get(self) -> None:
-        """Handles GET requests for group pages."""
-        # TODO(sll): Support index pages for other language codes.
         assert self.normalized_request is not None
         group_name = self.normalized_request['group_name']
         activity_list = []
@@ -315,10 +238,6 @@ class LibraryGroupIndexHandler(
 
 
 class SearchHandlerNormalizedRequestDict(TypedDict):
-    """Dict representation of SearchHandler's
-    normalized_request dictionary.
-    """
-
     q: str
     category: str
     language_code: str
@@ -367,22 +286,17 @@ class SearchHandler(
 
     @acl_decorators.open_access
     def get(self) -> None:
-        """Handles GET requests."""
         assert self.normalized_request is not None
+
         query_string = utils.get_formatted_query_string(
             self.normalized_request['q']
         )
 
-        # If there is a category parameter, it should be in the following form:
-        #     category=("Algebra" OR "Math")
         category_string = self.normalized_request['category']
         categories = utils.convert_filter_parameter_string_into_list(
             category_string
         )
 
-        # If there is a language code parameter, it should be in the following
-        # form:
-        #     language_code=("en" OR "hi")
         language_code_string = self.normalized_request['language_code']
         language_codes = utils.convert_filter_parameter_string_into_list(
             language_code_string
@@ -405,22 +319,17 @@ class SearchHandler(
 
 
 class LibraryRedirectPage(base.BaseHandler[Dict[str, str], Dict[str, str]]):
-    """An old 'gallery' page that should redirect to the library index page."""
+    """Redirects the old 'gallery' URL to the library index page."""
 
     URL_PATH_ARGS_SCHEMAS: Dict[str, str] = {}
     HANDLER_ARGS_SCHEMAS: Dict[str, Dict[str, str]] = {'GET': {}}
 
     @acl_decorators.open_access
     def get(self) -> None:
-        """Handles GET requests."""
         self.redirect('/community-library')
 
 
 class ExplorationSummariesHandlerNormalizedRequestDict(TypedDict):
-    """Dict representation of ExplorationSummariesHandler's
-    normalized_request dictionary.
-    """
-
     stringified_exp_ids: str
     include_private_explorations: Optional[bool]
 
@@ -430,9 +339,7 @@ class ExplorationSummariesHandler(
         Dict[str, str], ExplorationSummariesHandlerNormalizedRequestDict
     ]
 ):
-    """Returns summaries corresponding to ids of public explorations. This
-    controller supports returning private explorations for the given user.
-    """
+    """Returns summaries for public explorations."""
 
     GET_HANDLER_ERROR_RETURN_TYPE = feconf.HANDLER_TYPE_JSON
     URL_PATH_ARGS_SCHEMAS: Dict[str, str] = {}
@@ -450,7 +357,6 @@ class ExplorationSummariesHandler(
 
     @acl_decorators.open_access
     def get(self) -> None:
-        """Handles GET requests."""
         assert self.normalized_request is not None
         exp_ids = self.normalized_request['stringified_exp_ids']
         include_private_exps = self.normalized_request.get(
@@ -483,10 +389,6 @@ class ExplorationSummariesHandler(
 
 
 class CollectionSummariesHandlerNormalizedRequestDict(TypedDict):
-    """Dict representation of CollectionSummariesHandler's
-    normalized_request dictionary.
-    """
-
     stringified_collection_ids: List[str]
 
 
@@ -495,7 +397,7 @@ class CollectionSummariesHandler(
         Dict[str, str], CollectionSummariesHandlerNormalizedRequestDict
     ]
 ):
-    """Returns collection summaries corresponding to collection ids."""
+    """Returns collection summaries for given ids."""
 
     GET_HANDLER_ERROR_RETURN_TYPE = feconf.HANDLER_TYPE_JSON
     URL_PATH_ARGS_SCHEMAS: Dict[str, str] = {}
@@ -509,11 +411,10 @@ class CollectionSummariesHandler(
 
     @acl_decorators.open_access
     def get(self) -> None:
-        """Handles GET requests."""
         assert self.normalized_request is not None
         collection_ids = self.normalized_request['stringified_collection_ids']
 
-        summaries = summary_services.get_displayable_collection_summary_dicts_matching_ids(  # pylint: disable=line-too-long
+        summaries = summary_services.get_displayable_collection_summary_dicts_matching_ids(
             collection_ids
         )
         self.values.update({'summaries': summaries})
