fad792694b5b17f4b596cb721ac453911b4c520f
NITISH084 <imnitishkumar04@gmail.com>
2025-12-06 14:31:43 +0530

    Added code for community lessons

--- core/tests/puppeteer-acceptance-tests/specs/logged-in-user/manage-classroom-progress-in-home-learner-dashboard.spec.ts
+++ core/tests/puppeteer-acceptance-tests/specs/logged-in-user/manage-classroom-progress-in-home-learner-dashboard.spec.ts
@@ -34,7 +34,7 @@ describe('Logged-in User', function () {
   let loggedInUser: LoggedInUser & LoggedOutUser;
   let curriculumAdmin: CurriculumAdmin & TopicManager & ExplorationEditor;
   let releaseCoordinator: ReleaseCoordinator;
-  // const chapterIds: string[] = [];
+  const chapterIds: string[] = [];
 
   beforeAll(async function () {
     curriculumAdmin = await UserFactory.createNewUser(
@@ -53,36 +53,36 @@ describe('Logged-in User', function () {
       'show_redesigned_learner_dashboard'
     );
 
-    // await curriculumAdmin.createNewClassroom('Math', 'math');
-    // await curriculumAdmin.updateClassroom(
-    //   'Math',
-    //   'Welcome to Math classroom!',
-    //   'This course covers basic operations.',
-    //   'In this course, you will learn the following topics: Place Values.'
-    // );
+    await curriculumAdmin.createNewClassroom('Math', 'math');
+    await curriculumAdmin.updateClassroom(
+      'Math',
+      'Welcome to Math classroom!',
+      'This course covers basic operations.',
+      'In this course, you will learn the following topics: Place Values.'
+    );
 
-    // await curriculumAdmin.createAndPublishTopic(
-    //   'Division',
-    //   'Division subtopics',
-    //   'Division skills'
-    // );
-    // await curriculumAdmin.createAndPublishTopic(
-    //   'Place Values',
-    //   'Place Values subtopics',
-    //   'Place Values skills'
-    // );
-    // await curriculumAdmin.addTopicToClassroom('Math', 'Place Values');
-    // await curriculumAdmin.addTopicToClassroom('Math', 'Division');
-    // await curriculumAdmin.publishClassroom('Math');
+    await curriculumAdmin.createAndPublishTopic(
+      'Division',
+      'Division subtopics',
+      'Division skills'
+    );
+    await curriculumAdmin.createAndPublishTopic(
+      'Place Values',
+      'Place Values subtopics',
+      'Place Values skills'
+    );
+    await curriculumAdmin.addTopicToClassroom('Math', 'Place Values');
+    await curriculumAdmin.addTopicToClassroom('Math', 'Division');
+    await curriculumAdmin.publishClassroom('Math');
 
-    // const placeValueChapters = [
-    //   'What are the Place Values',
-    //   'Find the Value of a Number',
-    //   'Comparing Numbers',
-    //   'Rounding Numbers part 1',
-    //   'Rounding Numbers part 2',
-    //   'Extra chapter',
-    // ];
+    const placeValueChapters = [
+      'What are the Place Values',
+      'Find the Value of a Number',
+      'Comparing Numbers',
+      'Rounding Numbers part 1',
+      'Rounding Numbers part 2',
+      'Extra chapter',
+    ];
 
     for (let i = 0; i < 3; i++) {
       const expId = await curriculumAdmin.createAndPublishExplorationWithCards(
@@ -92,27 +92,27 @@ describe('Logged-in User', function () {
       );
     }
 
-    // for (const chapter of placeValueChapters) {
-    //   const expId = await curriculumAdmin.createAndPublishExplorationWithCards(
-    //     chapter,
-    //     'Algebra',
-    //     3
-    //   );
-    //   chapterIds.push(expId ?? '');
-    // }
+    for (const chapter of placeValueChapters) {
+      const expId = await curriculumAdmin.createAndPublishExplorationWithCards(
+        chapter,
+        'Algebra',
+        3
+      );
+      chapterIds.push(expId ?? '');
+    }
 
-    // await curriculumAdmin.addStoryToTopic(
-    //   "Jamie's Adventures in the Arcade",
-    //   'story',
-    //   'Place Values'
-    // );
+    await curriculumAdmin.addStoryToTopic(
+      "Jamie's Adventures in the Arcade",
+      'story',
+      'Place Values'
+    );
 
-    // for (const [index, id] of chapterIds.entries()) {
-    //   await curriculumAdmin.addChapter(placeValueChapters[index], id as string);
-    // }
+    for (const [index, id] of chapterIds.entries()) {
+      await curriculumAdmin.addChapter(placeValueChapters[index], id as string);
+    }
 
-    // await curriculumAdmin.saveStoryDraft();
-    // await curriculumAdmin.publishStoryDraft();
+    await curriculumAdmin.saveStoryDraft();
+    await curriculumAdmin.publishStoryDraft();
 
     loggedInUser = await UserFactory.createNewUser(
       'loggedInUser1',
@@ -120,196 +120,196 @@ describe('Logged-in User', function () {
     );
   });
 
-  // it('should have the correct tab title, available sections on landing and Sidebar should contain these items in this order from top to bottom: Profile picture, "Home" button, "Goals" button, "Progress" button', async function () {
-  //   await loggedInUser.navigateToLearnerDashboard();
-  //   await loggedInUser.expectSidebarTabToBeActive('Home');
+  it('should have the correct tab title, available sections on landing and Sidebar should contain these items in this order from top to bottom: Profile picture, "Home" button, "Goals" button, "Progress" button', async function () {
+    await loggedInUser.navigateToLearnerDashboard();
+    await loggedInUser.expectSidebarTabToBeActive('Home');
 
-  //   await loggedInUser.expectLearnerGreetingsToBe('Welcome, loggedInUser1!');
+    await loggedInUser.expectLearnerGreetingsToBe('Welcome, loggedInUser1!');
 
-  //   await loggedInUser.expectElementsToBePresent(
-  //     ['Learn Something New'],
-  //     'tabSection'
-  //   );
-  //   await loggedInUser.expectElementsToBePresent(
-  //     ["Topics available in Oppia's Classroom"],
-  //     'cardDisplay'
-  //   );
-  //   await loggedInUser.expectClassroomButtonOnRedesignedLearnerDashboardToBePresent(
-  //     true
-  //   );
+    await loggedInUser.expectElementsToBePresent(
+      ['Learn Something New'],
+      'tabSection'
+    );
+    await loggedInUser.expectElementsToBePresent(
+      ["Topics available in Oppia's Classroom"],
+      'cardDisplay'
+    );
+    await loggedInUser.expectClassroomButtonOnRedesignedLearnerDashboardToBePresent(
+      true
+    );
 
-  //   await loggedInUser.expectNumberOfElementsToBe(
-  //     '.e2e-test-learer-topic-summary-tile',
-  //     2
-  //   );
-  // });
+    await loggedInUser.expectNumberOfElementsToBe(
+      '.e2e-test-learer-topic-summary-tile',
+      2
+    );
+  });
 
-  // it('should navigate directly to math classroom', async function () {
-  //   await loggedInUser.navigateToClassroomFromLearnerDashboard('math');
-  //   await loggedInUser.expectToBeOnPage('learn/math');
-  //   showMessage('Navigated to math classroom from learner dashboard.');
-  // });
+  it('should navigate directly to math classroom', async function () {
+    await loggedInUser.navigateToClassroomFromLearnerDashboard('math');
+    await loggedInUser.expectToBeOnPage('learn/math');
+    showMessage('Navigated to math classroom from learner dashboard.');
+  });
 
-  // it('should navigate directly to the Place Values topic in the math classroom', async function () {
-  //   await loggedInUser.navigateToLearnerDashboard();
-  //   await loggedInUser.navigateToTopicPageByCard('Place Values');
-  //   await loggedInUser.expectToBeOnPage('learn/math/place-values');
-  //   showMessage('Navigated to Place Values topic from learner dashboard.');
-  // });
+  it('should navigate directly to the Place Values topic in the math classroom', async function () {
+    await loggedInUser.navigateToLearnerDashboard();
+    await loggedInUser.navigateToTopicPageByCard('Place Values');
+    await loggedInUser.expectToBeOnPage('learn/math/place-values');
+    showMessage('Navigated to Place Values topic from learner dashboard.');
+  });
 
-  // it('should display in-progress and recommended lessons after starting a lesson', async function () {
-  //   await loggedInUser.navigateToLearnerDashboard();
-  //   await loggedInUser.navigateToTopicPageByCard('Place Values');
-  //   await loggedInUser.expectToBeOnPage('learn/math/place-values');
-  //   await loggedInUser.selectChapterWithinStoryToLearn(
-  //     "Jamie's Adventures in the Arcade",
-  //     'What are the Place Values'
-  //   );
-  //   await loggedInUser.continueToNextCard();
-  //   await loggedInUser.navigateToLearnerDashboard();
-  //   // Did not finish the chapter,So still in  In-progress section.
-  //   await loggedInUser.expectElementsToBePresent(
-  //     ['Continue where you left off', 'Learn Something New'],
-  //     'tabSection'
-  //   );
+  it('should display in-progress and recommended lessons after starting a lesson', async function () {
+    await loggedInUser.navigateToLearnerDashboard();
+    await loggedInUser.navigateToTopicPageByCard('Place Values');
+    await loggedInUser.expectToBeOnPage('learn/math/place-values');
+    await loggedInUser.selectChapterWithinStoryToLearn(
+      "Jamie's Adventures in the Arcade",
+      'What are the Place Values'
+    );
+    await loggedInUser.continueToNextCard();
+    await loggedInUser.navigateToLearnerDashboard();
+    // Did not finish the chapter,So still in  In-progress section.
+    await loggedInUser.expectElementsToBePresent(
+      ['Continue where you left off', 'Learn Something New'],
+      'tabSection'
+    );
 
-  //   await loggedInUser.expectLessonCardProgressToBe(
-  //     'Lessons in progress',
-  //     ['Chapter 1: What are the Place Values'],
-  //     0
-  //   );
+    await loggedInUser.expectLessonCardProgressToBe(
+      'Lessons in progress',
+      ['Chapter 1: What are the Place Values'],
+      0
+    );
 
-  //   await loggedInUser.expectLessonCardProgressToBe(
-  //     'Recommended for you',
-  //     ['Chapter 2: Find the Value of a Number'],
-  //     0
-  //   );
+    await loggedInUser.expectLessonCardProgressToBe(
+      'Recommended for you',
+      ['Chapter 2: Find the Value of a Number'],
+      0
+    );
 
-  //   await loggedInUser.navigateToLessonByCard(
-  //     'Lessons in progress',
-  //     'Chapter 1: What are the Place Values'
-  //   );
+    await loggedInUser.navigateToLessonByCard(
+      'Lessons in progress',
+      'Chapter 1: What are the Place Values'
+    );
 
-  //   await loggedInUser.expectToBeOnLessonPage(
-  //     'Chapter 1: What are the Place Values',
-  //     chapterIds[0]
-  //   );
-  // });
+    await loggedInUser.expectToBeOnLessonPage(
+      'Chapter 1: What are the Place Values',
+      chapterIds[0]
+    );
+  });
 
-  // it('should not recommend any lessons if currently on last lesson', async function () {
-  //   await loggedInUser.navigateToLearnerDashboard();
-  //   await loggedInUser.navigateToLessonByCard(
-  //     'Lessons in progress',
-  //     'Chapter 1: What are the Place Values'
-  //   );
-  //   await loggedInUser.continueToNextCard();
-  //   await loggedInUser.continueToNextCard();
-  //   await loggedInUser.expectExplorationCompletionToastMessage(
-  //     'Congratulations for completing this lesson!'
-  //   );
+  it('should not recommend any lessons if currently on last lesson', async function () {
+    await loggedInUser.navigateToLearnerDashboard();
+    await loggedInUser.navigateToLessonByCard(
+      'Lessons in progress',
+      'Chapter 1: What are the Place Values'
+    );
+    await loggedInUser.continueToNextCard();
+    await loggedInUser.continueToNextCard();
+    await loggedInUser.expectExplorationCompletionToastMessage(
+      'Congratulations for completing this lesson!'
+    );
 
-  //   await loggedInUser.navigateToLearnerDashboard();
+    await loggedInUser.navigateToLearnerDashboard();
 
-  //   await loggedInUser.expectLessonCardProgressToBe(
-  //     'Lessons in progress',
-  //     ['Chapter 2: Find the Value of a Number'],
-  //     0
-  //   );
-  //   await loggedInUser.expectLessonCardProgressToBe(
-  //     'Recommended for you',
-  //     ['Chapter 3: Comparing Numbers'],
-  //     0
-  //   );
+    await loggedInUser.expectLessonCardProgressToBe(
+      'Lessons in progress',
+      ['Chapter 2: Find the Value of a Number'],
+      0
+    );
+    await loggedInUser.expectLessonCardProgressToBe(
+      'Recommended for you',
+      ['Chapter 3: Comparing Numbers'],
+      0
+    );
 
-  //   await loggedInUser.navigateToLessonByCard(
-  //     'Lessons in progress',
-  //     'Chapter 2: Find the Value of a Number'
-  //   );
-  //   await loggedInUser.continueToNextCard();
-  //   await loggedInUser.continueToNextCard();
-  //   await loggedInUser.expectExplorationCompletionToastMessage(
-  //     'Congratulations for completing this lesson!'
-  //   );
+    await loggedInUser.navigateToLessonByCard(
+      'Lessons in progress',
+      'Chapter 2: Find the Value of a Number'
+    );
+    await loggedInUser.continueToNextCard();
+    await loggedInUser.continueToNextCard();
+    await loggedInUser.expectExplorationCompletionToastMessage(
+      'Congratulations for completing this lesson!'
+    );
 
-  //   await loggedInUser.navigateToLearnerDashboard();
+    await loggedInUser.navigateToLearnerDashboard();
 
-  //   await loggedInUser.expectLessonCardProgressToBe(
-  //     'Lessons in progress',
-  //     ['Chapter 3: Comparing Numbers'],
-  //     0
-  //   );
-  //   await loggedInUser.expectLessonCardProgressToBe(
-  //     'Recommended for you',
-  //     ['Chapter 4: Rounding Numbers part 1'],
-  //     0
-  //   );
+    await loggedInUser.expectLessonCardProgressToBe(
+      'Lessons in progress',
+      ['Chapter 3: Comparing Numbers'],
+      0
+    );
+    await loggedInUser.expectLessonCardProgressToBe(
+      'Recommended for you',
+      ['Chapter 4: Rounding Numbers part 1'],
+      0
+    );
 
-  //   await loggedInUser.navigateToLessonByCard(
-  //     'Lessons in progress',
-  //     'Chapter 3: Comparing Numbers'
-  //   );
-  //   await loggedInUser.continueToNextCard();
-  //   await loggedInUser.continueToNextCard();
-  //   await loggedInUser.expectExplorationCompletionToastMessage(
-  //     'Congratulations for completing this lesson!'
-  //   );
+    await loggedInUser.navigateToLessonByCard(
+      'Lessons in progress',
+      'Chapter 3: Comparing Numbers'
+    );
+    await loggedInUser.continueToNextCard();
+    await loggedInUser.continueToNextCard();
+    await loggedInUser.expectExplorationCompletionToastMessage(
+      'Congratulations for completing this lesson!'
+    );
 
-  //   await loggedInUser.navigateToLearnerDashboard();
+    await loggedInUser.navigateToLearnerDashboard();
 
-  //   await loggedInUser.expectLessonCardProgressToBe(
-  //     'Lessons in progress',
-  //     ['Chapter 4: Rounding Numbers part 1'],
-  //     0
-  //   );
-  //   await loggedInUser.expectLessonCardProgressToBe(
-  //     'Recommended for you',
-  //     ['Chapter 5: Rounding Numbers part 2'],
-  //     0
-  //   );
+    await loggedInUser.expectLessonCardProgressToBe(
+      'Lessons in progress',
+      ['Chapter 4: Rounding Numbers part 1'],
+      0
+    );
+    await loggedInUser.expectLessonCardProgressToBe(
+      'Recommended for you',
+      ['Chapter 5: Rounding Numbers part 2'],
+      0
+    );
 
-  //   await loggedInUser.navigateToLearnerDashboard();
+    await loggedInUser.navigateToLearnerDashboard();
 
-  //   await loggedInUser.navigateToLessonByCard(
-  //     'Lessons in progress',
-  //     'Chapter 4: Rounding Numbers part 1'
-  //   );
-  //   await loggedInUser.continueToNextCard();
-  //   await loggedInUser.continueToNextCard();
-  //   await loggedInUser.expectExplorationCompletionToastMessage(
-  //     'Congratulations for completing this lesson!'
-  //   );
+    await loggedInUser.navigateToLessonByCard(
+      'Lessons in progress',
+      'Chapter 4: Rounding Numbers part 1'
+    );
+    await loggedInUser.continueToNextCard();
+    await loggedInUser.continueToNextCard();
+    await loggedInUser.expectExplorationCompletionToastMessage(
+      'Congratulations for completing this lesson!'
+    );
 
-  //   await loggedInUser.navigateToLearnerDashboard();
+    await loggedInUser.navigateToLearnerDashboard();
 
-  //   await loggedInUser.expectLessonCardProgressToBe(
-  //     'Lessons in progress',
-  //     ['Chapter 5: Rounding Numbers part 2'],
-  //     0
-  //   );
-  //   await loggedInUser.expectLessonCardProgressToBe(
-  //     'Recommended for you',
-  //     ['Chapter 6: Extra chapter'],
-  //     0
-  //   );
+    await loggedInUser.expectLessonCardProgressToBe(
+      'Lessons in progress',
+      ['Chapter 5: Rounding Numbers part 2'],
+      0
+    );
+    await loggedInUser.expectLessonCardProgressToBe(
+      'Recommended for you',
+      ['Chapter 6: Extra chapter'],
+      0
+    );
 
-  //   await loggedInUser.navigateToLessonByCard(
-  //     'Lessons in progress',
-  //     'Chapter 5: Rounding Numbers part 2'
-  //   );
-  //   await loggedInUser.continueToNextCard();
-  //   await loggedInUser.continueToNextCard();
-  //   await loggedInUser.expectExplorationCompletionToastMessage(
-  //     'Congratulations for completing this lesson!'
-  //   );
+    await loggedInUser.navigateToLessonByCard(
+      'Lessons in progress',
+      'Chapter 5: Rounding Numbers part 2'
+    );
+    await loggedInUser.continueToNextCard();
+    await loggedInUser.continueToNextCard();
+    await loggedInUser.expectExplorationCompletionToastMessage(
+      'Congratulations for completing this lesson!'
+    );
 
-  //   await loggedInUser.navigateToLearnerDashboard();
+    await loggedInUser.navigateToLearnerDashboard();
 
-  //   await loggedInUser.expectLessonCardProgressToBe(
-  //     'Lessons in progress',
-  //     ['Chapter 6: Extra chapter'],
-  //     0
-  //   );
-  // });
+    await loggedInUser.expectLessonCardProgressToBe(
+      'Lessons in progress',
+      ['Chapter 6: Extra chapter'],
+      0
+    );
+  });
 
   it('should be able to see community lessons in In Progress section if not completed fully', async function () {
     await loggedInUser.navigateToLearnerDashboard();
