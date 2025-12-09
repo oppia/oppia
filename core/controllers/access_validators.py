fbc59fb56c52a97830ebec1d13f7984d78dace64
Gabriel Fuentes <12247834+Helper2020@users.noreply.github.com>
2025-12-09 13:08:27 -0500

    testing github actions with wrong formatting

--- core/controllers/access_validators.py
+++ core/controllers/access_validators.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
+
 """Controllers for validating access."""
 
 from __future__ import annotations
