4e0a85257c0ade9c1d60a7e8599982a7f0939d2d
Meeth <gangwarmeeth@gmail.com>
2025-12-10 18:39:04 +0000

    The files being reverted back to orignal

--- core/templates/services/rte-helper-modal.component.ts
+++ core/templates/services/rte-helper-modal.component.ts
@@ -79,8 +79,9 @@ type CustomizationArgsNameAndValueArray = {
   [K in keyof ComponentSpecsType]: {
     // Extract the 'name' property from the customization_arg_specs array.
     name: ComponentSpecsType[K]['customization_arg_specs'][number]['name'];
-    value: ComponentSpecsType[K]['customization_arg_specs'][number]['name'] extends 'math_content'
-    // Conditional type. Check if the 'name' property is equal to 'math_content' using a.
+    value: // Check if the 'name' property is equal to 'math_content' using a
+    // conditional type.
+    ComponentSpecsType[K]['customization_arg_specs'][number]['name'] extends 'math_content'
       ? ConvertStringLiteralsToString<
           ComponentSpecsType[K]['customization_arg_specs'][number]['default_value']
         > & {
