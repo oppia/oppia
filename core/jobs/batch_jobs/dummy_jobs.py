49e427ce7e18b7b817e61afccb0f5c638b5b83c8
Jayam Patel <jayampatel04@gmail.com>
2025-12-15 14:28:22 +0530

    Lint Fix

--- core/jobs/batch_jobs/dummy_jobs.py
+++ core/jobs/batch_jobs/dummy_jobs.py
@@ -47,10 +47,10 @@ class DummyFailJob(base_jobs.JobBase):
     """Dummy job that does nothing and outputs a fail status."""
 
     def run(self) -> beam.PCollection[job_run_result.JobRunResult]:
-        """Returns a PCollection with a single failure result.
+        """Fails the job.
 
         Raises:
-            Exception. Always.
+            Exception. Intentionally raised to fail the job.
         """
         # An exception is required to fail the job. Writing "Fail" to the
         # output will not fail the job; it only changes the output.
