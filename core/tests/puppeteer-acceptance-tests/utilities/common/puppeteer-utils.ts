4f8ab6cd23ae1855344695fc2a5938cc66927381
imchristie <im.christie.ho@gmail.com>
2025-12-26 17:47:10 -0800

    fix comments to match the new names of the folders

--- core/tests/puppeteer-acceptance-tests/utilities/common/puppeteer-utils.ts
+++ core/tests/puppeteer-acceptance-tests/utilities/common/puppeteer-utils.ts
@@ -1037,6 +1037,8 @@ export class BaseUser {
       }
     }
 
+    const runningInCI = __dirname.startsWith('/home/runner');
+
     try {
       const screenshot = await currentPage.screenshot(screenshotOptions);
       expect(screenshot).toMatchImageSnapshot({
@@ -1048,7 +1050,7 @@ export class BaseUser {
          * The following checks if the tests are running on CI. If it is, the folder diff-snapshots will be uploaded as
          * artifacts in the github workflow.
          */
-        customDiffDir: __dirname.startsWith('/home/runner')
+        customDiffDir: runningInCI
           ? path.join(
               testConstants.TEST_SNAPSHOTS_DIR,
               specName,
@@ -1057,7 +1059,7 @@ export class BaseUser {
             )
           : path.join(testPath, dirName, 'diff-snapshots'),
         storeReceivedOnFailure: true, // Store the new screenshots seperately from the composed diff screenshots on failure.
-        customReceivedDir: __dirname.startsWith('/home/runner')
+        customReceivedDir: runningInCI
           ? path.join(
               testConstants.TEST_SNAPSHOTS_DIR,
               specName,
@@ -1068,18 +1070,18 @@ export class BaseUser {
       });
     } catch (error) {
       var errorMessage = error.message;
-      if (__dirname.startsWith('/home/runner')) {
+      if (runningInCI) {
         errorMessage +=
           '\r\nDownload the artifact folder diff-snapshots from the github workflow to check the difference between the old screenshot(s)' +
           ' and the new one(s). To download the folder, go to "Summary" of the CI Job of the PR and find the "Artifacts" section. The artifact' +
-          ' folder name should be something like tests/puppeteer-acceptance-tests/(suite-name)/prod-desktop-screenshots/diff-snapshots';
+          ' folder name should be something like diff-snapshots_(suite-name)_desktop_original';
       }
       errorMessage +=
         '\r\nPlease update the screenshots if the UI changed. If screenshot comparisons consistently show the same difference percentage across ' +
         'multiple test runs, the baseline screenshot(s) should be updated.\r\nTo update the screenshots(s), you should ' +
         'run the tests in CI, download the artifact folder new-snapshots from the github workflow and use the screenshots in that folder to ' +
         'replace the old one(s).\r\nTo download the folder, go to "Summary" of the CI Job of the PR and find the "Artifacts" section. The artifact' +
-        ' folder name should be something like tests/puppeteer-acceptance-tests/(suite-name)/prod-desktop-screenshots/new-snapshots.' +
+        ' folder name should be something like new-snapshots_(suite-name)_desktop_original.' +
         ' The new screenshot(s) should not end with "-diff".';
       throw new Error(errorMessage);
     }
