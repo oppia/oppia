03c93d306b55ab4dfd6f8ec66e385bf047fc2c13
Kartik Suryavanshi <158498247+KartikSuryavanshi@users.noreply.github.com>
2026-01-06 08:29:15 +0530

    fix: update expected dictionary order in test_identify_job_multiple_duplicates

--- core/jobs/batch_jobs/delete_duplicate_content_ids_jobs_test.py
+++ core/jobs/batch_jobs/delete_duplicate_content_ids_jobs_test.py
@@ -135,7 +135,7 @@ class IdentifyExplorationsWithDuplicateContentIdsJobTests(
             [
                 job_run_result.JobRunResult.as_stdout(
                     'Exploration exp_multi_dup (version 1) has duplicate content IDs: '
-                    f'{{\'{dup_id1}\': [\'Introduction\', \'State2\'], \'{dup_id2}\': [\'Introduction\', \'State3\']}}'
+                    f'{{\'{dup_id2}\': [\'Introduction\', \'State3\'], \'{dup_id1}\': [\'Introduction\', \'State2\']}}'
                 )
             ]
         )
