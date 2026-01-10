7be6906b30ce953f2910ea9c15d4511e5fb66d81
mosin74 <mosin74@gmail.com>
2026-01-10 14:23:16 +0530

    Mypy type

--- core/controllers/story_viewer.py
+++ core/controllers/story_viewer.py
@@ -80,8 +80,8 @@ class StoryPageDataHandler(base.BaseHandler[Dict[str, str], Dict[str, str]]):
         # are adding new keys that are not defined on the 'StoryNodeDict'.
         ordered_node_dicts: List[FrontendStoryNodeDict] = [
             node.to_dict()
-            for node in story.story_contents.get_ordered_nodes()  # type: ignore[misc]
-            if node.status != constants.STORY_NODE_STATUS_DRAFT
+            for node in story.story_contents.get_ordered_nodes()
+            if node.status != constants.STORY_NODE_STATUS_DRAFT  # type: ignore[misc]
         ]
         for node in ordered_node_dicts:
             node['completed'] = False
