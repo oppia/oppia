e9b91f665d18686e4207fa8e78a9d612c82b88b4
mosin74 <mosin74@gmail.com>
2026-01-10 13:49:01 +0530

    Inital logic

--- core/controllers/story_viewer.py
+++ core/controllers/story_viewer.py
@@ -79,7 +79,9 @@ class StoryPageDataHandler(base.BaseHandler[Dict[str, str], Dict[str, str]]):
         # 'FrontendStoryNodeDict', and this is done because below we
         # are adding new keys that are not defined on the 'StoryNodeDict'.
         ordered_node_dicts: List[FrontendStoryNodeDict] = [
-            node.to_dict() for node in story.story_contents.get_ordered_nodes()  # type: ignore[misc]
+            node.to_dict()
+            for node in story.story_contents.get_ordered_nodes()  # type: ignore[misc]
+            if node.status != constants.STORY_NODE_STATUS_DRAFT
         ]
         for node in ordered_node_dicts:
             node['completed'] = False
