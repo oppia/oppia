78764fc7016279e2be2083b3544c0b9e0cde3e55
Deepali123223 <singhdeepali2828@gmail.com>
2026-01-14 09:28:21 +0530

    Revert "Merge branch 'develop' into navbar"
    This reverts commit 2f539f34cdcb3200a932f0f736e66cf184cb32b5, reversing
    changes made to e79760d3adeede867ce06ba34b29ec1f8db61711.

--- core/jobs/batch_jobs/dummy_jobs_test.py
+++ core/jobs/batch_jobs/dummy_jobs_test.py
@@ -46,7 +46,10 @@ class DummyFailJobTests(job_test_utils.JobTestBase):
 
     def test_dummy_fail_job_outputs_failure(self) -> None:
         """Test that DummyFailJob outputs a failure result."""
-        with self.assertRaisesRegex(
-            Exception, 'DummyFailJob intentionally failed.'
-        ):
-            self.run_job()
+        self.assert_job_output_is(
+            [
+                job_run_result.JobRunResult.as_stderr(
+                    'ERROR: Dummy job failed as expected'
+                )
+            ]
+        )
