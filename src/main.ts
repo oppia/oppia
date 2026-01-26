08fc82bb1c8f0e447cc4dfbb7505d26e2b66ab0c
yash bahuguna <yashbahuguna918@gmail.com>
2026-01-26 16:38:03 +0530

    Move Bootstrap & Font Awesome CSS into angular.json styles and add FA webfonts to assets; centralize bootstrap JS import in main.ts

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,7 @@
  */
 
 import 'pages/common-imports';
+import 'bootstrap';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {enableProdMode} from '@angular/core';
 import {LighweightAppModule} from 'pages/lightweight-oppia-root/app.module';
