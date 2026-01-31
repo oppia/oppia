a973d8e085f2beafde4ec4a37c8e38b9e82e9a89
yash bahuguna <yashbahuguna918@gmail.com>
2026-01-31 21:48:53 +0530

    done

--- assets/constants.ts
+++ assets/constants.ts
@@ -7757,7 +7757,7 @@ export default {
     }
   },
 
-  "DEV_MODE": true,
+  "DEV_MODE": false,
   "EMULATOR_MODE": true,
   "ASSET_TYPE_AUDIO": "audio",
   "ASSET_TYPE_IMAGE": "image",
@@ -7776,8 +7776,8 @@ export default {
   ],
   "CONTRIBUTOR_CERTIFICATE_WIDTH": 1493,
   "CONTRIBUTOR_CERTIFICATE_HEIGHT": 1313,
-  "BRANCH_NAME": "",
-  "SHORT_COMMIT_HASH": "",
+  "BRANCH_NAME": "fivepackage",
+  "SHORT_COMMIT_HASH": "061570733d",
   // Please consult the translation team before adding any entries here.
   // These words improve the quality of automatic voiceovers.
   "LANGUAGE_CODE_TO_MATH_SYMBOL_PRONUNCIATIONS": {
