abd98fbaf93439319952342514b9da21be762f8f
NITISH084 <imnitishkumar04@gmail.com>
2026-01-31 23:41:59 +0530

    added files

--- scripts/linters/test_files/valid_job_imports.py
+++ scripts/linters/test_files/valid_job_imports.py
@@ -75,7 +75,7 @@ from core.jobs.batch_jobs import (  # pylint: disable=unused-import  # isort: sk
     missing_translation_images_repair_jobs,
 )
 from core.jobs.batch_jobs import (  # pylint: disable=unused-import  # isort: skip
-    audit_non_existent_suggestion_jobs,
+    audit_threads_with_missing_suggestions_jobs,
 )
 
 
