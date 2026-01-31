4e26d44293849fe40e8dbd51b4c71f622da09beb
mosin74 <mosin74@gmail.com>
2026-01-31 22:09:13 +0530

    Fixing Lint checks

--- core/tests/puppeteer-acceptance-tests/specs/topic-manager/track-upcoming-delayed-publish-chapter.spec.ts
+++ core/tests/puppeteer-acceptance-tests/specs/topic-manager/track-upcoming-delayed-publish-chapter.spec.ts
@@ -1,3 +1,24 @@
+// Copyright 2025 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Acceptance test from CUJv3 Doc
+ * https://docs.google.com/document/d/1D7kkFTzg3rxUe3QJ_iPlnxUzBFNElmRkmAWss00nFno/
+ *
+ * Has to set this after Adding the CUJs in v3 docs
+ */
+
 import {UserFactory} from '../../utilities/common/user-factory';
 import testConstants from '../../utilities/common/test-constants';
 import {CurriculumAdmin} from '../../utilities/user/curriculum-admin';
@@ -144,7 +165,7 @@ describe('Logged-In Learner', function () {
       );
 
       const futureDate = new Date();
-      futureDate.setMonth(futureDate.getMonth() + 1); //Flaky nature
+      futureDate.setMonth(futureDate.getMonth() + 1);
 
       const dateString = futureDate.toLocaleDateString('en-US');
 
