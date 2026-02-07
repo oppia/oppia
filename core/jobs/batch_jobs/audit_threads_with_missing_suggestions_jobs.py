42e9eaa20d9f40b9f2142c689374339343488a3a
NITISH084 <imnitishkumar04@gmail.com>
2026-02-07 17:07:19 +0530

    updated implementation approach

--- core/jobs/batch_jobs/audit_threads_with_missing_suggestions_jobs.py
+++ core/jobs/batch_jobs/audit_threads_with_missing_suggestions_jobs.py
@@ -39,98 +39,72 @@ if MYPY:  # pragma: no cover
 )
 
 
-class BaseThreadsWithMissingSuggestionsJob(base_jobs.JobBase):
-    """Base class for threads with has_suggestion=True but no suggestion."""
+class FixThreadsWithMissingSuggestionsJob(base_jobs.JobBase):
+    """Fixes feedback threads where has_suggestion=True but no suggestion exists.
 
-    DATASTORE_UPDATES_ALLOWED = False
+    When DATASTORE_UPDATES_ALLOWED is False, this job behaves as an audit job
+    and only reports invalid models without mutating the datastore.
+    """
 
-    # TODO(#15613): Here we use MyPy ignore because the incomplete typing of
-    # apache_beam library and absences of stubs in Typeshed, forces MyPy to
-    # assume that PTransform class is of type Any. Thus to avoid MyPy's error
-    # (Class cannot subclass 'PTransform' (has type 'Any')), we added an
-    # ignore here.
-    class _GetSuggestionThreadIds(beam.PTransform):  # type: ignore[misc]
-        """Returns thread_ids that have a GeneralSuggestionModel."""
-
-        def expand(self, pbegin: beam.pvalue.PBegin) -> beam.PCollection[str]:
-            return (
-                pbegin
-                | 'Get GeneralSuggestionModels'
-                >> ndb_io.GetModels(
-                    suggestion_models.GeneralSuggestionModel.get_all(
-                        include_deleted=False
-                    )
-                )
-                | 'Extract suggestion thread ids'
-                >> beam.Map(lambda model: model.id)
-            )
-
-    # TODO(#15613): Here we use MyPy ignore because the incomplete typing of
-    # apache_beam library and absences of stubs in Typeshed, forces MyPy to
-    # assume that PTransform class is of type Any. Thus to avoid MyPy's error
-    # (Class cannot subclass 'PTransform' (has type 'Any')), we added an
-    # ignore here.
-    class _GetInvalidFeedbackThreads(beam.PTransform):  # type: ignore[misc]
-        """Returns feedback threads marked as having suggestions but without one."""
-
-        def expand(
-            self,
-            valid_suggestion_thread_ids: beam.PCollection[str],
-        ) -> beam.PCollection[feedback_models.GeneralFeedbackThreadModel]:
-            feedback_threads = (
-                valid_suggestion_thread_ids.pipeline
-                | 'Get GeneralFeedbackThreadModels'
-                >> ndb_io.GetModels(
-                    feedback_models.GeneralFeedbackThreadModel.get_all(
-                        include_deleted=False
-                    )
-                )
-                | 'Keep threads which has_suggestion=True'
-                >> beam.Filter(lambda thread: thread.has_suggestion)
-            )
-
-            feedback_id_to_model = (
-                feedback_threads
-                | 'Map feedback threads to (id,model)'
-                >> beam.Map(lambda thread: (thread.id, thread))
-            )
-
-            suggestion_id_to_none = (
-                valid_suggestion_thread_ids
-                | 'Map suggestion ids to None'
-                >> beam.Map(lambda id: (id, None))
-            )
-
-            return (
-                {
-                    'feedback': feedback_id_to_model,
-                    'suggestions': suggestion_id_to_none,
-                }
-                | 'CoGroup feedback threads with suggestion ids'
-                >> beam.CoGroupByKey()
-                | 'Select threads with missing suggestions'
-                >> beam.FlatMap(
-                    lambda group: (
-                        group[1]['feedback']
-                        if not group[1]['suggestions']
-                        else []
-                    )
-                )
-            )
-
-
-class AuditThreadsWithMissingSuggestionsJob(
-    BaseThreadsWithMissingSuggestionsJob
-):
-    """Audit job reporting invalid feedback threads."""
-
-    DATASTORE_UPDATES_ALLOWED = False
+    DATASTORE_UPDATES_ALLOWED = True
 
     def run(self) -> beam.PCollection[job_run_result.JobRunResult]:
-        invalid_threads = (
+        """Runs the job.
+
+        Returns:
+            PCollection[JobRunResult]. Audit results when datastore updates
+            are disabled, otherwise fix results.
+        """
+
+        suggestion_thread_ids = (
             self.pipeline
-            | BaseThreadsWithMissingSuggestionsJob._GetSuggestionThreadIds()
-            | BaseThreadsWithMissingSuggestionsJob._GetInvalidFeedbackThreads()
+            | 'Get GeneralSuggestionModels'
+            >> ndb_io.GetModels(
+                suggestion_models.GeneralSuggestionModel.get_all(
+                    include_deleted=False
+                )
+            )
+            | 'Extract suggestion thread ids'
+            >> beam.Map(lambda model: model.id)
+        )
+
+        feedback_threads = (
+            self.pipeline
+            | 'Get GeneralFeedbackThreadModels'
+            >> ndb_io.GetModels(
+                feedback_models.GeneralFeedbackThreadModel.get_all(
+                    include_deleted=False
+                )
+            )
+            | 'Keep threads with has_suggestion=True'
+            >> beam.Filter(lambda thread: thread.has_suggestion)
+        )
+
+        feedback_id_to_model = (
+            feedback_threads
+            | 'Map feedback threads to (id, model)'
+            >> beam.Map(lambda thread: (thread.id, thread))
+        )
+
+        suggestion_id_to_none = (
+            suggestion_thread_ids
+            | 'Map suggestion ids to None'
+            >> beam.Map(lambda thread_id: (thread_id, None))
+        )
+
+        invalid_threads = (
+            {
+                'feedback': feedback_id_to_model,
+                'suggestions': suggestion_id_to_none,
+            }
+            | 'CoGroup feedback threads with suggestion ids'
+            >> beam.CoGroupByKey()
+            | 'Select threads with missing suggestions'
+            >> beam.FlatMap(
+                lambda group: (
+                    group[1]['feedback'] if not group[1]['suggestions'] else []
+                )
+            )
         )
 
         invalid_thread_logs = invalid_threads | 'Log invalid threads' >> beam.Map(
@@ -155,57 +129,57 @@ class AuditThreadsWithMissingSuggestionsJob(
             )
         )
 
-        return (
-            invalid_thread_logs,
-            invalid_thread_count,
-        ) | 'Flatten audit outputs' >> beam.Flatten()
+        outputs = []
 
+        if self.DATASTORE_UPDATES_ALLOWED:
+            unused_fixed_threads_put_results = (
+                invalid_threads
+                | 'Unset has_suggestion flag'
+                >> beam.Map(self._unset_has_suggestion)
+                | 'Put updated threads' >> ndb_io.PutModels()
+            )
 
-class FixThreadsWithMissingSuggestionsJob(BaseThreadsWithMissingSuggestionsJob):
-    """Beam job that fixes threads by setting has_suggestion=False."""
-
-    DATASTORE_UPDATES_ALLOWED = True
-
-    def run(self) -> beam.PCollection[job_run_result.JobRunResult]:
-        invalid_threads = (
-            self.pipeline
-            | BaseThreadsWithMissingSuggestionsJob._GetSuggestionThreadIds()
-            | BaseThreadsWithMissingSuggestionsJob._GetInvalidFeedbackThreads()
-        )
-
-        updated_threads = (
-            invalid_threads
-            | 'Unset has_suggestion flag'
-            >> beam.Map(self._unset_has_suggestion)
-            | 'Put updated threads' >> ndb_io.PutModels()
-        )
-
-        updated_thread_logs = invalid_threads | 'Log fixed threads' >> beam.Map(
-            lambda model: job_run_result.JobRunResult.as_stdout(
-                (
-                    'Fixed GeneralFeedbackThreadModel by setting '
-                    f'has_suggestion=False: id={model.id}'
+            updated_thread_logs = (
+                invalid_threads
+                | 'Log fixed threads'
+                >> beam.Map(
+                    lambda model: job_run_result.JobRunResult.as_stdout(
+                        (
+                            'Fixed GeneralFeedbackThreadModel by setting '
+                            f'has_suggestion=False: id={model.id}'
+                        )
+                    )
                 )
             )
-        )
 
-        updated_thread_count = (
-            invalid_threads
-            | 'Count fixed threads'
-            >> beam.combiners.Count.Globally().with_defaults(0)
-            | 'Report fixed thread count'
-            >> beam.Map(
-                lambda count: job_run_result.JobRunResult.as_stdout(
-                    f'fixed_feedback_thread_models_count: {count}'
+            updated_thread_count = (
+                invalid_threads
+                | 'Count fixed threads'
+                >> beam.combiners.Count.Globally().with_defaults(0)
+                | 'Report fixed thread count'
+                >> beam.Map(
+                    lambda count: job_run_result.JobRunResult.as_stdout(
+                        f'fixed_feedback_thread_models_count: {count}'
+                    )
                 )
             )
-        )
 
-        return (
-            updated_threads,
-            updated_thread_logs,
-            updated_thread_count,
-        ) | 'Flatten fix outputs' >> beam.Flatten()
+            outputs.extend(
+                [
+                    updated_thread_logs,
+                    updated_thread_count,
+                ]
+            )
+
+        else:
+            outputs.extend(
+                [
+                    invalid_thread_logs,
+                    invalid_thread_count,
+                ]
+            )
+
+        return outputs | 'Flatten outputs' >> beam.Flatten()
 
     @staticmethod
     def _unset_has_suggestion(
@@ -214,3 +188,11 @@ class FixThreadsWithMissingSuggestionsJob(BaseThreadsWithMissingSuggestionsJob):
         """Unsets has_suggestion flag."""
         thread.has_suggestion = False
         return thread
+
+
+class AuditThreadsWithMissingSuggestionsJob(
+    FixThreadsWithMissingSuggestionsJob
+):
+    """Audit job reporting feedback threads with missing suggestions."""
+
+    DATASTORE_UPDATES_ALLOWED = False
