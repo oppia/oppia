e76c20dfad525d0acf21f53dc88e7ba1d1007ea4
tanay235 <tanay235mittal@gmail.com>
2026-02-14 11:22:55 +0000

    added comments and removed extra condition

--- core/templates/components/skill-selector/skill-selector.component.ts
+++ core/templates/components/skill-selector/skill-selector.component.ts
@@ -90,12 +90,18 @@ export class SkillSelectorComponent implements OnInit {
   }
 
   checkIfEmpty(skills: ShortSkillSummary[]): boolean {
+    // This method now depends on the search query (skillFilterText).
+    // If a skill matches the search query, it will be displayed.
+    // Otherwise, it will be hidden.
     return (
       this.searchInSubtopicSkills(skills, this.skillFilterText).length === 0
     );
   }
 
   checkTopicIsNotEmpty(topicName: string): boolean {
+    // This method now depends on the search query (skillFilterText).
+    // If a topic has at least one skill that matches the search query,
+    // it will be displayed. Otherwise, it will be hidden.
     for (let subtopicName in this.currCategorizedSkills[topicName]) {
       let skills = this.currCategorizedSkills[topicName][subtopicName];
       if (
