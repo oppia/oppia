45cd91ccd2dd08310b36ad223aba738f744564af
keerthana <keerthanahg28@gmail.com>
2026-02-14 18:54:56 +0530

    flakes prevention

--- core/tests/puppeteer-acceptance-tests/specs/collection-creator/create-a-collection.spec.ts
+++ core/tests/puppeteer-acceptance-tests/specs/collection-creator/create-a-collection.spec.ts
@@ -1,20 +1,20 @@
 import testConstants from '../../utilities/common/test-constants';
 import { UserFactory } from '../../utilities/common/user-factory';
 import { ExplorationEditor } from '../../utilities/user/exploration-editor';
+import { ExplorationLearner } from '../../utilities/user/exploration-learner';
 import { LoggedInUser } from '../../utilities/user/logged-in-user';
 
 const DEFAULT_TIMEOUT = testConstants.DEFAULT_SPEC_TIMEOUT_MSECS;
-
 enum INTERACTION_TYPES {
   END_EXPLORATION = 'End Exploration',
 }
 
 describe('Exploration Learner Flow', function () {
   let explorationEditor: ExplorationEditor & LoggedInUser;
-  let learner: LoggedInUser;
+  let learner: ExplorationLearner & LoggedInUser;
   let explorationId1: string | null;
   let explorationId2: string | null;
-
+  
   
 
   beforeAll(async function () {
@@ -45,7 +45,7 @@ describe('Exploration Learner Flow', function () {
         'This is the goal of exploration.',
         'Math'
       );
-  
+      await explorationEditor.waitForPageToFullyLoad();
       await explorationEditor.navigateToCreatorDashboardUsingProfileDropdown();
 
       // second exploration
@@ -64,7 +64,7 @@ describe('Exploration Learner Flow', function () {
         'This is the goal of exploration.',
         'Math'
       );
-  
+      await explorationEditor.waitForPageToFullyLoad();
       await explorationEditor.navigateToCreatorDashboardUsingProfileDropdown();
 
     }
@@ -74,23 +74,34 @@ describe('Exploration Learner Flow', function () {
       async function () {
           await createAndPublishMinimalExploration();
       },
-      DEFAULT_TIMEOUT
-    );
+      DEFAULT_TIMEOUT);
 
   it('should allow learner to play, rate, and subscribe', async function () {
+    expect(explorationId1).not.toBeNull();
+    expect(explorationId2).not.toBeNull();
     await learner.navigateToCommunityLibrary();
+    await learner.waitForPageToFullyLoad();
     await learner.playExploration(explorationId2);
     await learner.starRateExploration(5);
     await learner.giveFeedback('Super ,fantastic,explorations!!! I loves them',false);
-    await learner.submitFeedback();
+    //await learner.submitFeedback();
     await learner.navigateToCommunityLibrary();
+    await learner.waitForPageToFullyLoad();
     await learner.playExploration(explorationId1);
     await learner.starRateExploration(3);
     await learner.subscribeToCreator('explorationEditor');
     await explorationEditor.reloadPage();
     await explorationEditor.waitForPageToFullyLoad();
+    const viewport = explorationEditor.page.viewport();
+
+    if (
+      viewport &&
+      viewport.width >= testConstants.ViewportWidthBreakpoints.MOBILE_PX
+    ) {
     await explorationEditor.switchToListView();
     await explorationEditor.waitForPageToFullyLoad();
+   }
+
   }, DEFAULT_TIMEOUT);
 
   afterAll(async function () {
