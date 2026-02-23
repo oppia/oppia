03eeeded28a9ef61848058f7e2c4f74e3d0bbc5d
yash bahuguna <yashbahuguna918@gmail.com>
2026-02-23 23:51:56 +0530

    done added

--- scripts/linters/other_files_linter.py
+++ scripts/linters/other_files_linter.py
@@ -122,7 +122,9 @@ class CustomLintChecksManager(linter_utils.BaseLinter):
         error_messages: List[str] = []
         skip_files_section_found = False
 
-        for line_num, line in enumerate(self.file_cache.readlines(APP_YAML_FILEPATH)):
+        for line_num, line in enumerate(
+            self.file_cache.readlines(APP_YAML_FILEPATH)
+        ):
             stripped_line = line.strip()
 
             if '# Third party files:' in stripped_line:
@@ -155,7 +157,9 @@ class CustomLintChecksManager(linter_utils.BaseLinter):
                 failed = True
                 break
 
-        return concurrent_task_utils.TaskResult(name, failed, error_messages, error_messages)
+        return concurrent_task_utils.TaskResult(
+            name, failed, error_messages, error_messages
+        )
 
     def check_third_party_libs_type_defs(
         self,
