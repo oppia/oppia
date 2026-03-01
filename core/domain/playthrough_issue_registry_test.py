131eb2c3ff893d93143b5e095ef5098dc839b579
haani-maybe <haaniahismail12@gmail.com>
2026-03-01 14:29:49 +0500

    reverting unnecesarry change

--- core/domain/playthrough_issue_registry_test.py
+++ core/domain/playthrough_issue_registry_test.py
@@ -4,6 +4,7 @@
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
 #      http://www.apache.org/licenses/LICENSE-2.0
 #
@@ -18,7 +19,6 @@
 from __future__ import annotations
 
 import importlib
-from types import ModuleType
 
 from core.domain import playthrough_issue_registry
 from core.tests import test_utils
@@ -27,6 +27,8 @@ from extensions.issues.EarlyQuit import EarlyQuit
 from extensions.issues.MultipleIncorrectSubmissions import (
     MultipleIncorrectSubmissions,
 )
+
+from typing import Any
 
 
 class IssueRegistryUnitTests(test_utils.GenericTestBase):
@@ -52,11 +54,15 @@ class IssueRegistryUnitTests(test_utils.GenericTestBase):
         super().tearDown()
 
     def test_issue_registry(self) -> None:
+        """Do some sanity checks on the issue registry."""
         self.assertEqual(
             len(playthrough_issue_registry.Registry.get_all_issues()), 3
         )
 
     def test_correct_issue_registry_types(self) -> None:
+        """Tests issue registry for fetching of issue instances of correct
+        issue types.
+        """
         for issue_type, _class in self.issues_dict.items():
             self.assertIsInstance(
                 playthrough_issue_registry.Registry.get_issue_by_type(
@@ -66,6 +72,9 @@ class IssueRegistryUnitTests(test_utils.GenericTestBase):
             )
 
     def test_incorrect_issue_registry_types(self) -> None:
+        """Tests that an error is raised when fetching an incorrect issue
+        type.
+        """
         with self.assertRaisesRegex(KeyError, self.invalid_issue_type):
             playthrough_issue_registry.Registry.get_issue_by_type(
                 self.invalid_issue_type
@@ -79,13 +88,18 @@ class IssueRegistryUnitTests(test_utils.GenericTestBase):
         """
 
         class NotAnIssue:
-            """Dummy class."""
+            """A dummy class that does not inherit from
+            BaseExplorationIssueSpec.
+            """
 
             pass
 
         original_import = importlib.import_module
 
-        def mock_import_module(name: str) -> ModuleType:
+        # Here we use type Any because the mock_import_module function
+        # needs to return different module types depending on the name,
+        # so a specific return type cannot be used.
+        def mock_import_module(name: str) -> Any:
             module = original_import(name)
             if name.endswith('.EarlyQuit.EarlyQuit'):
                 setattr(module, 'EarlyQuit', NotAnIssue)
@@ -108,7 +122,12 @@ class IssueRegistryUnitTests(test_utils.GenericTestBase):
     def test_get_all_issues_returns_cached_when_already_populated(
         self,
     ) -> None:
+        """Test that get_all_issues skips _refresh when _issues is already
+        populated.
+        """
+        # First call populates _issues.
         first_result = playthrough_issue_registry.Registry.get_all_issues()
         self.assertTrue(len(first_result) > 0)
+        # Second call should return from cache without calling _refresh.
         second_result = playthrough_issue_registry.Registry.get_all_issues()
         self.assertEqual(len(first_result), len(second_result))
