df7cd57528feddf62d63f655c25d0ec4ff3eb71f
Nik-09 <nikhil.agarwal.2019@gmail.com>
2026-03-04 12:21:18 +0530

    Adds backend test for voiceover_cloud_task_services.py

--- core/domain/voiceover_cloud_task_services_test.py
+++ core/domain/voiceover_cloud_task_services_test.py
@@ -309,3 +309,336 @@ class CloudTaskServicesTests(test_utils.GenericTestBase):
         self.assertEqual(
             voiceover_cloud_task_run_mapping.task_run_id, cloud_task_id
         )
+
+    def test_should_create_voiceover_regeneration_task_batch_model(
+        self,
+    ) -> None:
+        parent_task_run_id = 'parent_task_run_id'
+        child_task_run_id = 'child_task_run_id'
+        exploration_id = 'exploration_id'
+        exploration_version = 1
+        language_accent_code = 'en-US'
+        content_ids_to_contents_map = {
+            'content_0': 'Hello world!',
+            'content_1': 'First card.',
+        }
+
+        voiceover_regeneration_task_batch = (
+            cloud_task_domain.VoiceoverRegenerationTaskBatch(
+                parent_task_run_id,
+                child_task_run_id,
+                exploration_id,
+                exploration_version,
+                language_accent_code,
+                content_ids_to_contents_map,
+            )
+        )
+
+        model_instance = voiceover_cloud_task_services.create_voiceover_regeneration_task_batch_model(
+            voiceover_regeneration_task_batch
+        )
+
+        self.assertEqual(
+            model_instance.parent_cloud_task_run_id, parent_task_run_id
+        )
+        self.assertEqual(
+            model_instance.child_cloud_task_run_id, child_task_run_id
+        )
+        self.assertEqual(model_instance.exploration_id, exploration_id)
+        self.assertEqual(
+            model_instance.exploration_version, exploration_version
+        )
+        self.assertEqual(
+            model_instance.language_accent_code, language_accent_code
+        )
+        self.assertEqual(
+            model_instance.content_ids_to_contents_map,
+            content_ids_to_contents_map,
+        )
+
+    def test_should_create_voiceover_regeneration_task_batch_models(
+        self,
+    ) -> None:
+        parent_task_run_id = 'parent_task_run_id'
+        exploration_id = 'exploration_id'
+        exploration_version = 1
+
+        batch_instances = [
+            cloud_task_domain.VoiceoverRegenerationTaskBatch(
+                parent_task_run_id,
+                'child_task_run_id_1',
+                exploration_id,
+                exploration_version,
+                'en-US',
+                {'content_0': 'Hello world!', 'content_1': 'First card.'},
+            ),
+            cloud_task_domain.VoiceoverRegenerationTaskBatch(
+                parent_task_run_id,
+                'child_task_run_id_2',
+                exploration_id,
+                exploration_version,
+                'hi-IN',
+                {'content_2': 'नमस्ते दुनिया!', 'content_3': 'पहला कार्ड.'},
+            ),
+        ]
+
+        voiceover_cloud_task_services.create_voiceover_regeneration_task_batch_models(
+            batch_instances
+        )
+
+        retrieved_batch_1 = voiceover_cloud_task_services.get_voiceover_regeneration_task_batch_model_by_id(
+            parent_task_run_id, 'child_task_run_id_1'
+        )
+        self.assertIsNotNone(retrieved_batch_1)
+        self.assertEqual(retrieved_batch_1.language_accent_code, 'en-US')
+
+        retrieved_batch_2 = voiceover_cloud_task_services.get_voiceover_regeneration_task_batch_model_by_id(
+            parent_task_run_id, 'child_task_run_id_2'
+        )
+        self.assertIsNotNone(retrieved_batch_2)
+        self.assertEqual(retrieved_batch_2.language_accent_code, 'hi-IN')
+
+    def test_should_get_voiceover_regeneration_batch_instances_by_parent_task_run_id(
+        self,
+    ) -> None:
+        parent_task_run_id = 'parent_task_run_id'
+        exploration_id = 'exploration_id'
+        exploration_version = 1
+
+        batch_instances = [
+            cloud_task_domain.VoiceoverRegenerationTaskBatch(
+                parent_task_run_id,
+                'child_task_run_id_1',
+                exploration_id,
+                exploration_version,
+                'en-US',
+                {'content_0': 'Hello world!'},
+            ),
+            cloud_task_domain.VoiceoverRegenerationTaskBatch(
+                parent_task_run_id,
+                'child_task_run_id_2',
+                exploration_id,
+                exploration_version,
+                'hi-IN',
+                {'content_1': 'नमस्ते दुनिया!'},
+            ),
+            cloud_task_domain.VoiceoverRegenerationTaskBatch(
+                parent_task_run_id,
+                'child_task_run_id_3',
+                exploration_id,
+                exploration_version,
+                'es-ES',
+                {'content_2': '¡Hola mundo!'},
+            ),
+        ]
+
+        voiceover_cloud_task_services.create_voiceover_regeneration_task_batch_models(
+            batch_instances
+        )
+
+        retrieved_instances = voiceover_cloud_task_services.get_voiceover_regeneration_batch_instances_by_parent_task_run_id(
+            parent_task_run_id
+        )
+
+        self.assertEqual(len(retrieved_instances), 3)
+        self.assertEqual(retrieved_instances[0].language_accent_code, 'en-US')
+        self.assertEqual(retrieved_instances[1].language_accent_code, 'hi-IN')
+        self.assertEqual(retrieved_instances[2].language_accent_code, 'es-ES')
+
+    def test_should_get_voiceover_regeneration_task_batch_model_by_id(
+        self,
+    ) -> None:
+        parent_task_run_id = 'parent_task_run_id'
+        child_task_run_id = 'child_task_run_id'
+        exploration_id = 'exploration_id'
+        exploration_version = 2
+        language_accent_code = 'en-US'
+        content_ids_to_contents_map = {
+            'content_0': 'Hello world!',
+            'content_1': 'First card.',
+        }
+
+        voiceover_regeneration_task_batch = (
+            cloud_task_domain.VoiceoverRegenerationTaskBatch(
+                parent_task_run_id,
+                child_task_run_id,
+                exploration_id,
+                exploration_version,
+                language_accent_code,
+                content_ids_to_contents_map,
+            )
+        )
+
+        voiceover_cloud_task_services.create_voiceover_regeneration_task_batch_model(
+            voiceover_regeneration_task_batch
+        )
+
+        retrieved_batch = voiceover_cloud_task_services.get_voiceover_regeneration_task_batch_model_by_id(
+            parent_task_run_id, child_task_run_id
+        )
+
+        self.assertIsNotNone(retrieved_batch)
+        self.assertEqual(
+            retrieved_batch.parent_cloud_task_run_id, parent_task_run_id
+        )
+        self.assertEqual(
+            retrieved_batch.child_cloud_task_run_id, child_task_run_id
+        )
+        self.assertEqual(retrieved_batch.exploration_id, exploration_id)
+        self.assertEqual(
+            retrieved_batch.exploration_version, exploration_version
+        )
+        self.assertEqual(
+            retrieved_batch.language_accent_code, language_accent_code
+        )
+        self.assertEqual(
+            retrieved_batch.content_ids_to_contents_map,
+            content_ids_to_contents_map,
+        )
+
+    def test_should_return_none_for_non_existent_batch_model(self) -> None:
+        retrieved_batch = voiceover_cloud_task_services.get_voiceover_regeneration_task_batch_model_by_id(
+            'non_existent_parent', 'non_existent_child'
+        )
+
+        self.assertIsNone(retrieved_batch)
+
+    def test_should_convert_voiceover_regeneration_task_batch_model_to_domain_instance(
+        self,
+    ) -> None:
+        parent_task_run_id = 'parent_task_run_id'
+        child_task_run_id = 'child_task_run_id'
+        exploration_id = 'exploration_id'
+        exploration_version = 3
+        language_accent_code = 'en-IN'
+        content_ids_to_contents_map = {
+            'content_0': 'Test content 0',
+            'content_1': 'Test content 1',
+        }
+
+        model_instance = (
+            cloud_task_models.VoiceoverRegenerationBatchExecutionModel(
+                parent_cloud_task_run_id=parent_task_run_id,
+                child_cloud_task_run_id=child_task_run_id,
+                exploration_id=exploration_id,
+                exploration_version=exploration_version,
+                language_accent_code=language_accent_code,
+                content_ids_to_contents_map=content_ids_to_contents_map,
+            )
+        )
+
+        domain_instance = voiceover_cloud_task_services.convert_voiceover_regeneration_task_batch_model_to_domain_instance(
+            model_instance
+        )
+
+        self.assertEqual(
+            domain_instance.parent_cloud_task_run_id, parent_task_run_id
+        )
+        self.assertEqual(
+            domain_instance.child_cloud_task_run_id, child_task_run_id
+        )
+        self.assertEqual(domain_instance.exploration_id, exploration_id)
+        self.assertEqual(
+            domain_instance.exploration_version, exploration_version
+        )
+        self.assertEqual(
+            domain_instance.language_accent_code, language_accent_code
+        )
+        self.assertEqual(
+            domain_instance.content_ids_to_contents_map,
+            content_ids_to_contents_map,
+        )
+
+    def test_should_create_voiceover_regeneration_task_batch_model_if_not_exists(
+        self,
+    ) -> None:
+        parent_task_run_id = 'parent_task_run_id'
+        child_task_run_id = 'child_task_run_id'
+        exploration_id = 'exploration_id'
+        exploration_version = 1
+        language_accent_code = 'en-US'
+        content_ids_to_contents_map = {
+            'content_0': 'Hello world!',
+        }
+
+        domain_instance = cloud_task_domain.VoiceoverRegenerationTaskBatch(
+            parent_task_run_id,
+            child_task_run_id,
+            exploration_id,
+            exploration_version,
+            language_accent_code,
+            content_ids_to_contents_map,
+        )
+
+        voiceover_cloud_task_services.create_or_update_voiceover_regeneration_task_batch_model(
+            domain_instance
+        )
+
+        retrieved_batch = voiceover_cloud_task_services.get_voiceover_regeneration_task_batch_model_by_id(
+            parent_task_run_id, child_task_run_id
+        )
+
+        self.assertIsNotNone(retrieved_batch)
+        self.assertEqual(
+            retrieved_batch.language_accent_code, language_accent_code
+        )
+        self.assertEqual(
+            retrieved_batch.content_ids_to_contents_map,
+            content_ids_to_contents_map,
+        )
+
+    def test_should_update_existing_voiceover_regeneration_task_batch_model(
+        self,
+    ) -> None:
+        parent_task_run_id = 'parent_task_run_id'
+        child_task_run_id = 'child_task_run_id'
+        exploration_id = 'exploration_id'
+        exploration_version = 1
+        language_accent_code = 'en-US'
+        initial_content_map = {'content_0': 'Hello world!'}
+
+        domain_instance = cloud_task_domain.VoiceoverRegenerationTaskBatch(
+            parent_task_run_id,
+            child_task_run_id,
+            exploration_id,
+            exploration_version,
+            language_accent_code,
+            initial_content_map,
+        )
+
+        voiceover_cloud_task_services.create_or_update_voiceover_regeneration_task_batch_model(
+            domain_instance
+        )
+
+        updated_content_map = {
+            'content_0': 'Hello world!',
+            'content_1': 'First card.',
+        }
+        updated_exploration_version = 2
+
+        updated_domain_instance = (
+            cloud_task_domain.VoiceoverRegenerationTaskBatch(
+                parent_task_run_id,
+                child_task_run_id,
+                exploration_id,
+                updated_exploration_version,
+                language_accent_code,
+                updated_content_map,
+            )
+        )
+
+        voiceover_cloud_task_services.create_or_update_voiceover_regeneration_task_batch_model(
+            updated_domain_instance
+        )
+
+        retrieved_batch = voiceover_cloud_task_services.get_voiceover_regeneration_task_batch_model_by_id(
+            parent_task_run_id, child_task_run_id
+        )
+
+        self.assertIsNotNone(retrieved_batch)
+        self.assertEqual(
+            retrieved_batch.exploration_version, updated_exploration_version
+        )
+        self.assertEqual(
+            retrieved_batch.content_ids_to_contents_map, updated_content_map
+        )
