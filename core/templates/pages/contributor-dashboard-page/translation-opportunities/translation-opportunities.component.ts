dae09b1671317570cbafb1abc80adceaab39ebf7
kishansinghifs1 <kishansingh956196@gmail.com>
2026-03-08 20:47:26 +0530

    feat: Enhance user info handling in TranslationOpportunitiesComponent
    (cherry picked from commit 2d46c127411e332bcc396587029f87af7d95e1f5)

--- core/templates/pages/contributor-dashboard-page/translation-opportunities/translation-opportunities.component.ts
+++ core/templates/pages/contributor-dashboard-page/translation-opportunities/translation-opportunities.component.ts
@@ -46,6 +46,7 @@ export class TranslationOpportunitiesComponent {
 
   allOpportunities: {[id: string]: TranslationOpportunity} = {};
   userIsLoggedIn = false;
+  private userInfoPromise!: Promise<void>;
   opportunityType = 'translation';
   languageSelected = false;
   constructor(
@@ -106,7 +107,8 @@ export class TranslationOpportunitiesComponent {
     return {opportunitiesDicts, more};
   }
 
-  onClickButton(expId: string): void {
+  async onClickButton(expId: string): Promise<void> {
+    await this.userInfoPromise;
     if (!this.userIsLoggedIn) {
       this.contributionOpportunitiesService.showRequiresLoginModal();
       return;
@@ -130,7 +132,7 @@ export class TranslationOpportunitiesComponent {
   }
 
   ngOnInit(): void {
-    this.userService.getUserInfoAsync().then(userInfo => {
+    this.userInfoPromise = this.userService.getUserInfoAsync().then(userInfo => {
       this.userIsLoggedIn = userInfo.isLoggedIn();
     });
     this.translationLanguageService.onActiveLanguageChanged.subscribe(
