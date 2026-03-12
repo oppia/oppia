fe4c5570681f81a6710d0ed50855cb4a8dc7a5a5
Kishan_Singh <147229665+kishansinghifs1@users.noreply.github.com>
2026-03-12 19:02:27 +0530

    Fix #24388 : TimeoutError: waiting for selector .e2e-test-new-goals-list failed: timeout 30000ms exceeded // addGoalInRedesignedLearnerDashboard (#25010)
    * Fix: Update visibility condition for new goals list in learner dashboard
    
    * Remove unnecessary wait for network idle in LoggedInUser class goal addition flow

--- core/tests/puppeteer-acceptance-tests/utilities/user/logged-in-user.ts
+++ core/tests/puppeteer-acceptance-tests/utilities/user/logged-in-user.ts
@@ -2880,9 +2880,8 @@ export class LoggedInUser extends BaseUser {
     );
     await this.waitForElementToBeClickable(addNewGoalButtonSelector);
     await this.page.click(addNewGoalButtonSelector);
-
     await this.page.waitForSelector(newGoalsListInRedesignedLearnerDashboard, {
-      visible: false,
+      hidden: true,
     });
   }
 
