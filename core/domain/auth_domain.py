b1ae2dc436bf24cb7f1944751ec6f158b87d4658
anujatappeta <anujatappeta@gmail.com>
2026-03-22 10:12:13 +0530

    Remove CsrfSecret changes (seperate PR)

--- core/domain/auth_domain.py
+++ core/domain/auth_domain.py
@@ -269,22 +269,3 @@ class UserAuthDetails:
             'parent_user_id': self.parent_user_id,
             'deleted': self.deleted,
         }
-
-
-class CsrfSecret:
-    """Domain object for CsrfSecretModel."""
-
-    def __init__(self, oppia_csrf_secret: str) -> None:
-        """Initializes CsrfSecret domain object.
-
-        Args:
-            oppia_csrf_secret: str. The CSRF secret value.
-        """
-        self.oppia_csrf_secret = oppia_csrf_secret
-
-    def validate(self) -> None:
-        """Validates the CSRF secret value."""
-        if not isinstance(self.oppia_csrf_secret, str):
-            raise utils.ValidationError('oppia_csrf_secret must be a string')
-        if not self.oppia_csrf_secret:
-            raise utils.ValidationError('oppia_csrf_secret cannot be empty')
