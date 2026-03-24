369580f73920eeb9662fff9bb46ea2297b0c5198
Simran Kaur <simrankaur47937@gmail.com>
2026-03-24 13:07:44 +0000

    Fix part of #23496: Enforce strict typescript checks for group 32 files

--- extensions/objects/templates/math-expression-content-editor.component.spec.ts
+++ extensions/objects/templates/math-expression-content-editor.component.spec.ts
@@ -194,7 +194,6 @@ describe('MathExpressionContentEditorComponent', () => {
       // 'HTMLCollectionOf<Element>': item, namedItem."
       // We need to suppress this error because we need to return a mock element
       // for testing purposes.
-      // @ts-expect-error
       .and.returnValue([svgElement]);
     component.alwaysEditable = true;
   });
@@ -215,7 +214,7 @@ describe('MathExpressionContentEditorComponent', () => {
 
       // eslint-disable-next-line dot-notation
       spyOn(component['insertScriptService'], 'loadScript').and.callFake(
-        (script, onLoadCb) => {
+        (script: KNOWN_SCRIPTS, onLoadCb?: () => void) => {
           if (onLoadCb) {
             onLoadCb();
           }
@@ -260,9 +259,10 @@ describe('MathExpressionContentEditorComponent', () => {
   it('should close editor if the expression is not editable on initialisation', fakeAsync(() => {
     spyOn(component, 'closeEditor');
     const loadScriptSpy = spyOn(
-      component.insertScriptService,
+      (component as unknown as {insertScriptService: {loadScript: Function}})
+        .insertScriptService,
       'loadScript'
-    ).and.callFake((script, callback) => {
+    ).and.callFake((script: KNOWN_SCRIPTS, callback: () => void) => {
       if (script === KNOWN_SCRIPTS.MATHJAX) {
         callback();
       }
