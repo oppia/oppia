7776d0a9916aa0df929d0ec49a4c877361e97e8b
Aditya22-smart <adityalakhotia21@gmail.com>
2026-03-24 15:06:29 +0000

    Fix #20338: Resolve race condition in exploration metadata model save flow

--- core/templates/pages/exploration-editor-page/modal-templates/exploration-metadata-modal.component.ts
+++ core/templates/pages/exploration-editor-page/modal-templates/exploration-metadata-modal.component.ts
@@ -30,6 +30,8 @@ import {ExplorationTitleService} from '../services/exploration-title.service';
 import {AlertsService} from 'services/alerts.service';
 import {ExplorationStatesService} from '../services/exploration-states.service';
 import {ParamChange} from 'domain/exploration/param-change.model';
+import {ChangeListService} from '../services/change-list.service';
+import {filter, take} from 'rxjs/operators';
 
 interface CategoryChoices {
   id: string;
@@ -71,6 +73,7 @@ export class ExplorationMetadataModalComponent
     private explorationStatesService: ExplorationStatesService,
     private explorationTagsService: ExplorationTagsService,
     private explorationTitleService: ExplorationTitleService,
+    private changeListService: ChangeListService,
     private ngbActiveModal: NgbActiveModal
   ) {
     super(ngbActiveModal);
@@ -147,6 +150,9 @@ export class ExplorationMetadataModalComponent
       return;
     }
 
+    const initialChangeListLength =
+      this.changeListService.getChangeList().length;
+
     // Record any fields that have changed.
     let metadataList: string[] = [];
     if (this.explorationTitleService.hasChanged()) {
@@ -172,15 +178,21 @@ export class ExplorationMetadataModalComponent
     this.explorationLanguageCodeService.saveDisplayedValue();
     this.explorationTagsService.saveDisplayedValue();
 
-    // TODO(#20338): Get rid of the $timeout here.
-    // It's currently used because there is a race condition: the
-    // saveDisplayedValue() calls above result in autosave calls.
-    // These race with the discardDraft() call that
-    // will be called when the draft changes entered here
-    // are properly saved to the backend.
-    setTimeout(() => {
+    if (
+      this.changeListService.getChangeList().length === initialChangeListLength
+    ) {
       this.ngbActiveModal.close(metadataList);
-    }, 500);
+      return;
+    }
+
+    this.changeListService.autosaveIsInProgress$
+      .pipe(
+        filter(inProgress => !inProgress),
+        take(1)
+      )
+      .subscribe(() => {
+        this.ngbActiveModal.close(metadataList);
+      });
   }
 
   areRequiredFieldsFilled(): boolean {
