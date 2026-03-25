f4ff4c9484856fcc812784c9d173cd32f9554335
Rohan-Unbeg <rohanunbeg0918@gmail.com>
2026-03-25 20:18:01 +0530

    Fix: Add coverage test for < 1 minute contribution as requested

--- core/templates/pages/contributor-dashboard-page/services/contribution-and-review.service.spec.ts
+++ core/templates/pages/contributor-dashboard-page/services/contribution-and-review.service.spec.ts
@@ -323,6 +323,46 @@ describe('Contribution and review service', () => {
       expect(downloadContributorCertificateAsyncSpy).toHaveBeenCalled();
     });
 
+    it('should download the contributor certificate when contribution time is less than 1 minute', async () => {
+      downloadContributorCertificateAsyncSpy.and.returnValue(
+        Promise.resolve({
+          certificate_data: {
+            from_date: '1 Nov 2022',
+            to_date: '1 Dec 2022',
+            contribution_hours: 0.01,
+            contribution_word_count: 3,
+            team_lead: 'Test User',
+            language: 'Hindi',
+          },
+        })
+      );
+
+      await cars
+        .downloadContributorCertificateAsync(
+          'user',
+          'translate_content',
+          'hi',
+          '2022-01-01',
+          '2022-01-02'
+        )
+        .then(response => {
+          const data = response.certificate_data;
+
+          expect(data).not.toBeNull();
+
+          // Type assertion needed to satisfy type checker.
+          const definedData = data as ContributorCertificateInfo;
+          expect(definedData.from_date).toEqual('1 Nov 2022');
+          expect(definedData.to_date).toEqual('1 Dec 2022');
+          expect(definedData.contribution_hours).toEqual(0.01);
+          expect(definedData.contribution_word_count).toEqual(3);
+          expect(definedData.team_lead).toEqual('Test User');
+          expect(definedData.language).toEqual('Hindi');
+        });
+
+      expect(downloadContributorCertificateAsyncSpy).toHaveBeenCalled();
+    });
+
     it('should return null when contributor certificate data is unavailable', async () => {
       downloadContributorCertificateAsyncSpy.and.returnValue(
         Promise.resolve({
