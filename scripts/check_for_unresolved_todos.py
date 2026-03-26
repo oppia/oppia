ce38c3bf530725411d2128d89ea4c27836864c34
tanay235 <tanay235mittal@gmail.com>
2026-03-26 09:36:47 +0000

    fix newest linter

--- scripts/check_for_unresolved_todos.py
+++ scripts/check_for_unresolved_todos.py
@@ -15,13 +15,15 @@
 
 from __future__ import annotations
 
-# TODO(#1): Temporary workflow test.
-
 import argparse
 import os
-from typing import List, Optional
 
 from scripts import github_api, todo_finder
+
+from typing import List, Optional
+
+# TODO(#1): Temporary workflow test.
+
 
 UNRESOLVED_TODOS_PRESENT_INDICATOR = (
     'THERE ARE TODOS ASSOCIATED WITH THE PROVIDED ISSUES.'
