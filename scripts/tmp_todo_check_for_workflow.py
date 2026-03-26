2c18587de8b56ed055bdf5de83c0dc2468bc20ed
tanay235 <tanay235mittal@gmail.com>
2026-03-26 06:03:07 +0000

    Fix lint issues for workflow proof

--- scripts/tmp_todo_check_for_workflow.py
+++ scripts/tmp_todo_check_for_workflow.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Temporary TODO used for workflow verification."""
+"""Temporary file used for workflow verification."""
 
 from __future__ import annotations
 
