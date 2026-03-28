4490ad7bb1ad3b4226bcdbb869a8511185ae4b10
Rohan Malhotra <139499925+rohanmalhotracodes@users.noreply.github.com>
2026-03-28 11:34:21 +0530

    Revert "Stabilize InsertScriptService script-loading tests"
    This reverts commit 47673d0b0ca6f32058fff42b36773b844c583cba.

--- core/templates/services/insert-script.service.spec.ts
+++ core/templates/services/insert-script.service.spec.ts
@@ -40,19 +40,6 @@ class MockRendererFactory {
 describe('InsertScriptService', () => {
   let insertScriptService: InsertScriptService;
   let rendererFactory: RendererFactory2;
-
-  const mockScriptLoadSuccess = (): void => {
-    spyOn(document.body, 'appendChild').and.callFake((node: Node): Node => {
-      const scriptElement = node as HTMLScriptElement;
-      setTimeout(() => {
-        if (typeof scriptElement.onload === 'function') {
-          scriptElement.onload.call(scriptElement, new Event('load'));
-        }
-      }, 0);
-      return node;
-    });
-  };
-
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
@@ -65,8 +52,6 @@ describe('InsertScriptService', () => {
   });
 
   it('should not reload script if already loaded', (done: jasmine.DoneFn) => {
-    mockScriptLoadSuccess();
-
     insertScriptService.loadScript(KNOWN_SCRIPTS.DONORBOX, () => {
       const result = insertScriptService.loadScript(
         KNOWN_SCRIPTS.DONORBOX,
@@ -79,8 +64,6 @@ describe('InsertScriptService', () => {
   });
 
   it('should not create new script element if script is still loading', (done: jasmine.DoneFn) => {
-    mockScriptLoadSuccess();
-
     spyOn(
       rendererFactory.createRenderer(null, null),
       'createElement'
@@ -168,8 +151,6 @@ describe('InsertScriptService', () => {
   });
 
   it('should insert script into html', (done: jasmine.DoneFn) => {
-    mockScriptLoadSuccess();
-
     const result = insertScriptService.loadScript(KNOWN_SCRIPTS.DONORBOX);
     expect(result).toBe(true);
     insertScriptService.loadScript(KNOWN_SCRIPTS.DONORBOX, () => {
