6a181ac2cc7b5cf4390807232cacaba5a1ad5ac6
tanay235 <tanay235mittal@gmail.com>
2026-03-30 15:54:09 +0000

    added gaps to todo comment

--- scripts/check_for_unresolved_todos_test.py
+++ scripts/check_for_unresolved_todos_test.py
@@ -142,10 +142,10 @@ class CheckForUnresolvedTodosTests(test_utils.GenericTestBase):
         github_perma_link_url = 'https://github.com/oppia/oppia/blob/abcdefg'
         expected_unresolved_todo_list_lines = [
             'The following TODOs are unresolved for this issue #4151:',
-            f'{github_perma_link_url}/file1.txt#L4',
-            f'{github_perma_link_url}/file1.txt#L11',
-            f'{github_perma_link_url}/file1.txt#L13',
-            f'{github_perma_link_url}/file2.txt#L3',
+            f'`{github_perma_link_url}/file1.txt#L4`',
+            f'`{github_perma_link_url}/file1.txt#L11`',
+            f'`{github_perma_link_url}/file1.txt#L13`',
+            f'`{github_perma_link_url}/file2.txt#L3`',
         ]
         with open(
             'dummy_directory/unresolved_todo_list.txt', 'r', encoding='utf-8'
@@ -235,14 +235,14 @@ class CheckForUnresolvedTodosTests(test_utils.GenericTestBase):
         github_perma_link_url = 'https://github.com/oppia/oppia/blob/abcdefg'
         expected_unresolved_todo_list_lines = [
             'The following TODOs are unresolved for this issue #4151:',
-            f'{github_perma_link_url}/file1.txt#L4',
-            f'{github_perma_link_url}/file1.txt#L11',
-            f'{github_perma_link_url}/file1.txt#L13',
-            f'{github_perma_link_url}/file2.txt#L3',
+            f'`{github_perma_link_url}/file1.txt#L4`',
+            f'`{github_perma_link_url}/file1.txt#L11`',
+            f'`{github_perma_link_url}/file1.txt#L13`',
+            f'`{github_perma_link_url}/file2.txt#L3`',
             'The following TODOs are unresolved for this issue #4156:',
-            f'{github_perma_link_url}/file1.txt#L6',
+            f'`{github_perma_link_url}/file1.txt#L6`',
             'The following TODOs are unresolved for this issue #4153:',
-            f'{github_perma_link_url}/file1.txt#L7',
+            f'`{github_perma_link_url}/file1.txt#L7`',
         ]
         with open(
             'dummy_directory/unresolved_todo_list.txt', 'r', encoding='utf-8'
