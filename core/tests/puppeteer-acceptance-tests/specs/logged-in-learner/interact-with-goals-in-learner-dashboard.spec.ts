8e071fa0b3a1aacf6832d4e9f1956e9b0303fe22
Mohak <143473709+Mohak51234@users.noreply.github.com>
2026-04-06 15:54:30 +0530

    Fixed interact-with-goals-in-learner-dashboard flake

--- core/tests/puppeteer-acceptance-tests/specs/logged-in-learner/interact-with-goals-in-learner-dashboard.spec.ts
+++ core/tests/puppeteer-acceptance-tests/specs/logged-in-learner/interact-with-goals-in-learner-dashboard.spec.ts
@@ -224,6 +224,7 @@ describe('Logged-In Learner', function () {
 
     await loggedInUser.expectMobileLayoutToBeCorrect();
 
+    await loggedInUser.disableLearnerDashboardNavTransitions();
     await loggedInUser.expectScreenshotToMatch('goalsTabMobileView', __dirname);
   });
 
