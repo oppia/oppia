7c227ca6454010c60ff5de0d140e0313d33be653
tanay235 <tanay235mittal@gmail.com>
2026-04-07 08:58:30 +0000

    added disable-bad-pattern-check

--- scripts/linters/pylint_extensions.py
+++ scripts/linters/pylint_extensions.py
@@ -66,6 +66,7 @@ ALLOWED_PRAGMAS_FOR_INLINE_COMMENTS: Final = [
     'type: ignore',
     'pragma:',
     'https:',
+    'disable-bad-pattern-check',
 ]
 
 ALLOWED_LINES_OF_GAP_IN_COMMENT: Final = 15
