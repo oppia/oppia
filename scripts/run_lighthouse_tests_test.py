6ef56499d434623c6045eb23b156a5ee958859c0
Meeth <gangwarmeeth@gmail.com>
2026-05-07 20:46:53 +0000

    Fixing  tests

--- scripts/run_lighthouse_tests_test.py
+++ scripts/run_lighthouse_tests_test.py
@@ -330,7 +330,7 @@ class RunLighthouseTestsTests(test_utils.GenericTestBase):
             os.path, 'isdir', lambda _: True, expected_kwargs=[]
         )
 
-        with self.print_swap, swap_ng_compiler, swap_isdir:
+        with self.print_swap, self.swap_ng_build, swap_isdir:
             servers.run_ng_compilation()
 
         self.assertNotIn(
@@ -342,7 +342,7 @@ class RunLighthouseTestsTests(test_utils.GenericTestBase):
             os.path, 'isdir', lambda _: False, expected_kwargs=[]
         )
 
-        with self.print_swap, swap_ng_compiler, swap_isdir:
+        with self.print_swap, self.swap_ng_build, swap_isdir:
             with self.swap_sys_exit:
                 servers.run_ng_compilation()
 
