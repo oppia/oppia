47e6f06d93b36c98876ec0f5643720cadee056e0
Mohak <143473709+Mohak51234@users.noreply.github.com>
2026-05-11 12:29:41 +0530

    Final change for second test migration

--- core/tests/playwright-acceptance-tests/utilities/user/blog-admin.ts
+++ core/tests/playwright-acceptance-tests/utilities/user/blog-admin.ts
@@ -47,12 +47,7 @@ export class BlogAdmin extends BaseUser {
     await this.page
       .locator('select#label-target-update-form-role-select')
       .selectOption(role);
-    const usernameLocator = this.page.locator(roleUpdateUsernameInput);
-    await expect(usernameLocator).toBeVisible();
-    await expect(usernameLocator).toBeEnabled();
-    await usernameLocator.click();
-    await usernameLocator.fill(username);
-    await expect(usernameLocator).toHaveValue(username);
+    await this.page.locator(roleUpdateUsernameInput).fill(username);
     await this.page.locator(updateRoleButtonSelector).click();
 
     await expect(this.page.locator(updateRoleButtonSelector)).not.toBeEnabled();
