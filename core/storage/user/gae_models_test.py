96cf2230271e517513303574000fb00a24bd58fe
Rohan-Unbeg <rohanunbeg0918@gmail.com>
2026-05-25 20:09:16 +0530

    Fix PR 1.2: Use feconf constant for entity_type in tests

--- core/storage/user/gae_models_test.py
+++ core/storage/user/gae_models_test.py
@@ -3970,7 +3970,7 @@ class PinnedOpportunityModelTest(test_utils.GenericTestBase):
             language_code=self.language_code,
             topic_id=self.topic_id,
             opportunity_id=self.opportunity_id_1,
-            entity_type='exploration',
+            entity_type=feconf.ENTITY_TYPE_EXPLORATION,
         )
 
     def test_create_and_fetch_model(self) -> None:
@@ -4006,7 +4006,7 @@ class PinnedOpportunityModelTest(test_utils.GenericTestBase):
                 language_code=self.language_code,
                 topic_id=self.topic_id,
                 opportunity_id=self.opportunity_id_1,
-                entity_type='exploration',
+                entity_type=feconf.ENTITY_TYPE_EXPLORATION,
             )
 
     def test_pre_put_hook_raises_exception_for_invalid_entity_type(
