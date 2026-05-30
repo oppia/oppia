ab1ccf8d2562673f390ab00d7fb07583631c512b
Viren Passi <virenpassi79@gmail.com>
2026-05-30 12:47:38 +0000

    Fix #26148: Resolve Arabic cursive text rendering in MathJax equations

--- core/templates/mathjaxConfig.ts
+++ core/templates/mathjaxConfig.ts
@@ -14,6 +14,7 @@ window.MathJax = {
   showProcessingMessages: false,
   SVG: {
     useGlobalCache: false,
+    mtextFontInherit: true,
     linebreaks: {
       automatic: true,
       width: '500px',
