c65a7296f196c266a1d3d07b40fca0b3860d24d2
tanmaygoyal2007 <tanmaygoyal0712@gmail.com>
2026-06-02 00:04:27 +0530

    addition in the logic

--- core/tests/puppeteer-acceptance-tests/utilities/user/exploration-editor.ts
+++ core/tests/puppeteer-acceptance-tests/utilities/user/exploration-editor.ts
@@ -3728,7 +3728,11 @@ export class ExplorationEditor extends BaseUser {
     });
     await this.clickOnElementWithSelector(openOutcomeDestButton);
     await this.waitForElementToBeClickable(destinationCardSelector);
+    // The '/' value is used to select the 'a new card called' option in the dropdown.
     await this.select(destinationCardSelector, '/');
+    await this.page.waitForSelector(addStateInput, {
+      visible: true,
+    });
     await this.typeInInputField(addStateInput, cardName);
     await this.clickOnElementWithSelector(saveOutcomeDestButton);
     await this.page.waitForSelector(saveOutcomeDestButton, {
