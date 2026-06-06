21f808ffb86f502e2caf38916fd87ab4c6761107
Kartik Suryavanshi <158498247+KartikSuryavanshi@users.noreply.github.com>
2026-06-06 15:10:49 +0530

    fixed type checks and lint checks

--- core/templates/pages/topic-viewer-page/topic-viewer-page.component.spec.ts
+++ core/templates/pages/topic-viewer-page/topic-viewer-page.component.spec.ts
@@ -33,6 +33,7 @@ import {PageTitleService} from 'services/page-title.service';
 import {MockTranslatePipe} from 'tests/unit-test-utils';
 import {I18nLanguageCodeService} from 'services/i18n-language-code.service';
 import {PlatformFeatureService} from 'services/platform-feature.service';
+import {StorySummary} from 'domain/story/story-summary.model';
 
 class MockPlatformFeatureService {
   status = {
@@ -444,6 +445,16 @@ describe('Topic viewer page', () => {
   }));
 
   it('should track story data by story id', () => {
+    const storySummarySpy = jasmine.createSpyObj<StorySummary>('StorySummary', [
+      'getId',
+      'getTitle',
+      'getDescription',
+      'getNodeTitles',
+      'getAllNodes',
+      'getUrlFragment',
+    ]);
+    storySummarySpy.getId.and.returnValue('story_1');
+
     expect(
       topicViewerPageComponent.trackStoryDataById(0, {
         storyId: 'story_1',
@@ -451,6 +462,10 @@ describe('Topic viewer page', () => {
         storyDescription: 'Description',
         lessonCount: 3,
         practiceCount: 2,
+        storySummary: storySummarySpy,
+        practiceSubtopicIds: [],
+        classroomUrlFragment: '',
+        topicUrlFragment: '',
       })
     ).toBe('story_1');
   });
