f7c5f2dfeb0e19c0eb29fc3c281320df0e0f64a7
tanmaygoyal2007 <tanmaygoyal0712@gmail.com>
2026-06-22 01:46:57 +0530

    fix type check

--- extensions/interactions/FractionInput/directives/oppia-interactive-fraction-input.component.spec.ts
+++ extensions/interactions/FractionInput/directives/oppia-interactive-fraction-input.component.spec.ts
@@ -22,7 +22,6 @@ import {
   fakeAsync,
   TestBed,
   tick,
-  waitForAsync,
 } from '@angular/core/testing';
 import {InteractiveFractionInputComponent} from './oppia-interactive-fraction-input.component';
 import {InteractionAttributesExtractorService} from 'interactions/interaction-attributes-extractor.service';
@@ -200,7 +199,7 @@ describe('InteractiveFractionInputComponent', () => {
       expect(component.errorMessageI18nKey).toBe(
         ObjectsDomainConstants.FRACTION_PARSING_ERROR_I18N_KEYS.INVALID_CHARS
       );
-      expect(component.isValid).toBeFalse();
+      expect(component.isValid).toBe(false);
     }
   );
 
@@ -216,7 +215,7 @@ describe('InteractiveFractionInputComponent', () => {
       expect(component.errorMessageI18nKey).toBe(
         ObjectsDomainConstants.FRACTION_PARSING_ERROR_I18N_KEYS.INVALID_FORMAT
       );
-      expect(component.isValid).toBeFalse();
+      expect(component.isValid).toBe(false);
     }
   );
 
@@ -256,7 +255,7 @@ describe('InteractiveFractionInputComponent', () => {
     component.answerValueChanged();
     tick(150);
 
-    expect(component.isValid).toBeTrue();
+    expect(component.isValid).toBe(true);
   }));
 
   it(
