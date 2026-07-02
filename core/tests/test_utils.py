287efa5b7aae87e813c06b92afb1d745b338105c
Alok <227512169+alok844937-design@users.noreply.github.com>
2026-07-02 13:55:15 +0530

    Fix part of #12912: Add arguments-differ disable for TestBase.assertRaisesRegex override

--- core/tests/test_utils.py
+++ core/tests/test_utils.py
@@ -1967,7 +1967,7 @@ class TestBase(unittest.TestCase):
 
     # Here we use MyPy ignore because the signature of this method
     # doesn't match with TestCase's assertRaisesRegex().
-    def assertRaisesRegex(  # type: ignore[override]
+    def assertRaisesRegex(  # type: ignore[override]  # pylint: disable=arguments-differ
         self,
         expected_exception: Union[
             Type[BaseException], Tuple[Type[BaseException], ...]
