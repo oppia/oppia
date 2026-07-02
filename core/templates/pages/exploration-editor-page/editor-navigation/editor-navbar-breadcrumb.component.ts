d91f84f0b7bf703145aaa6b2ba110512f900c60f
sheronjay <192441684+sheronjay@users.noreply.github.com>
2026-07-02 18:13:59 +0530

    remove outdated non null assertion explanation for navbarTitle

--- core/templates/pages/exploration-editor-page/editor-navigation/editor-navbar-breadcrumb.component.ts
+++ core/templates/pages/exploration-editor-page/editor-navigation/editor-navbar-breadcrumb.component.ts
@@ -30,9 +30,6 @@ import {RouterService} from '../services/router.service';
 })
 export class EditorNavbarBreadcrumbComponent implements OnInit, OnDestroy {
   directiveSubscriptions = new Subscription();
-  // This property is initialized using Angular lifecycle hooks
-  // and we need to do non-null assertion. For more information, see
-  // https://github.com/oppia/oppia/wiki/Guide-on-defining-types#ts-7-1
   navbarTitle: string | null = null;
 
   constructor(
