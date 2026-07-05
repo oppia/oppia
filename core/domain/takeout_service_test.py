b65ec54707dd79f2a60e424e0ea90be7cf2e2ad1
NITISH084 <imnitishkumar04@gmail.com>
2026-07-05 19:05:58 +0530

    added wipeout test

--- core/domain/takeout_service_test.py
+++ core/domain/takeout_service_test.py
@@ -1680,6 +1680,7 @@ class TakeoutServiceFullUserUnitTests(test_utils.GenericTestBase):
             author_id=self.USER_ID_1,
             feedback_text=self.LESSON_FEEDBACK_TEXT_1,
             status=feedback_models.STATUS_CHOICES_OPEN,
+            exploration_id='exp_id_1',
             lesson_metadata_schema_version=(
                 feconf.CURRENT_LESSON_METADATA_SCHEMA_VERSION
             ),
@@ -2074,6 +2075,7 @@ class TakeoutServiceFullUserUnitTests(test_utils.GenericTestBase):
             self.LESSON_FEEDBACK_ID_1: {
                 'feedback_text': self.LESSON_FEEDBACK_TEXT_1,
                 'status': feedback_models.STATUS_CHOICES_OPEN,
+                'exploration_id': 'exp_id_1',
                 'lesson_metadata_json': self.LESSON_METADATA_JSON,
                 'parent_feedback_id': self.PARENT_FEEDBACK_ID_1,
                 'response_list': self.LESSON_FEEDBACK_RESPONSE_LIST,
