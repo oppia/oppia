2d9f2ada3c5eaf94789187cfdcc623cd6ba5e22a
Kartik Suryavanshi <158498247+KartikSuryavanshi@users.noreply.github.com>
2026-07-06 22:10:49 +0530

    added more tests

--- core/controllers/practice_sessions_test.py
+++ core/controllers/practice_sessions_test.py
@@ -427,7 +427,9 @@ class PracticeSessionsPageDataHandlerTests(BasePracticeSessionsControllerTests):
         self,
     ) -> None:
         story_id = 'story_id_2'
+        deleted_story_id = 'del_story'
         exp_id = 'exp_2'
+        exp_id_2 = 'exp_3'
         self.save_new_valid_exploration(exp_id, self.admin_id)
         self.publish_exploration(self.admin_id, exp_id)
         self.save_new_story(story_id, self.admin_id, self.topic_id)
@@ -476,9 +478,17 @@ class PracticeSessionsPageDataHandlerTests(BasePracticeSessionsControllerTests):
             ],
             'Added arc.',
         )
+
+        self.save_new_valid_exploration(exp_id_2, self.admin_id)
+        self.publish_exploration(self.admin_id, exp_id_2)
+        self.save_new_story(deleted_story_id, self.admin_id, self.topic_id)
         topic_services.add_canonical_story(
-            self.admin_id, self.topic_id, 'nonexistent'
+            self.admin_id, self.topic_id, deleted_story_id
         )
+        topic_services.publish_story(
+            self.topic_id, deleted_story_id, self.admin_id
+        )
+        story_services.delete_story(self.admin_id, deleted_story_id)
 
         json_response = self.get_json(
             '%s/staging/%s/arc/nonexistent_arc'
