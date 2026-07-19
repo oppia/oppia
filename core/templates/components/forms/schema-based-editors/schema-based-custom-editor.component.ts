259b609c24fa1fed0f9048fd7e2bb6764aa53bb1
Aditya22-smart <adityalakhotia21@gmail.com>
2026-07-19 18:24:58 +0530

    Fixed extra line and import statement

--- core/templates/components/forms/schema-based-editors/schema-based-custom-editor.component.ts
+++ core/templates/components/forms/schema-based-editors/schema-based-custom-editor.component.ts
@@ -1,4 +1,3 @@
-import './schema-based-custom-editor.component.css';
 // Copyright 2022 The Oppia Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -39,6 +38,7 @@ import {
   CustomSchema,
   SchemaDefaultValue,
 } from 'services/schema-default-value.service';
+import './schema-based-custom-editor.component.css';
 
 @Component({
   selector: 'schema-based-custom-editor',
