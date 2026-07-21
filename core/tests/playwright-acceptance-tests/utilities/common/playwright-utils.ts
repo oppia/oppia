a03bd6223766be20ff5295471502285a59615c8e
Haaniah Ismail <haaniahismail12@gmail.com>
2026-07-21 23:23:25 +0500

    getting rid of dupe

--- core/tests/playwright-acceptance-tests/utilities/common/playwright-utils.ts
+++ core/tests/playwright-acceptance-tests/utilities/common/playwright-utils.ts
@@ -761,19 +761,6 @@ export class BaseUser {
   }
 
   /**
-   * Returns text in nested element
-   * @param {string} selector - The selector of the element to get text from.
-   */
-  async getTextContent(selector: string): Promise<string> {
-    const element = await this.page.$(selector);
-    const text = await this.page.evaluate(
-      (el: Element) => el.textContent,
-      element as ElementHandle<Element>
-    );
-    return text?.trim() ?? '';
-  }
-
-  /**
    * Close a single user's browser context.
    */
   async closeBrowser(): Promise<void> {
