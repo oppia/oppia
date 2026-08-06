04be939ef04bf9564a93fd1c187209bd484e1214
Alok <227512169+alok844937-design@users.noreply.github.com>
2026-08-06 19:04:08 +0530

    Assert user is not already in the group before adding in casing test

--- core/domain/user_services_test.py
+++ core/domain/user_services_test.py
@@ -2178,6 +2178,8 @@ class UserServicesUnitTests(test_utils.GenericTestBase):
             user_groups_data[0].user_group_id, 'USERGROUP1', ['testuser1']
         )
 
+        self.assertNotIn('TestUser1', user_groups_data[0].member_usernames)
+
         updated_user_groups_data = user_services.get_all_user_groups()
         self.assertIn('TestUser1', updated_user_groups_data[0].member_usernames)
 
