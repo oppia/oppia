187d0c0886f80c8814c42ed05a4c098f7ee53cdf
Haaniah Ismail <haaniahismail12@gmail.com>
2026-08-08 17:28:43 +0500

    removing storage clear

--- core/tests/puppeteer-acceptance-tests/specs/logged-out-learner/view-a-collection.spec.ts
+++ core/tests/puppeteer-acceptance-tests/specs/logged-out-learner/view-a-collection.spec.ts
@@ -40,8 +40,6 @@ describe('Logged-Out Learner', function () {
 
   beforeAll(async function () {
     loggedOutLearner = await UserFactory.createLoggedOutUser();
-    await loggedOutLearner.page.evaluate(() => localStorage.clear());
-    await loggedOutLearner.page.reload({waitUntil: 'networkidle0'});
     lessonCreator = (await UserFactory.createNewUser(
       'lessonCreator',
       'lessoncreator@example.com',
