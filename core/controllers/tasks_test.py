ca875ef435c8dc43b48f52dde7f67b3f718a138d
nik-09 <nikhil.agarwal.2019@gmail.com>
2026-08-08 12:02:56 +0000

    Fixes backend tests

--- core/controllers/tasks_test.py
+++ core/controllers/tasks_test.py
@@ -743,7 +743,11 @@ class TasksTests(test_utils.EmailTestBase):
 
     def test_should_handle_voiceover_deferred_tasks_successfully(self) -> None:
         exploration_id = 'exploration_id'
-        self.save_new_valid_exploration(exploration_id, self.owner_id)
+        self.save_new_valid_exploration(
+            exploration_id,
+            self.owner_id,
+            content_html='Hello world',
+        )
         rights_manager.publish_exploration(self.owner, exploration_id)
 
         url = feconf.TASK_URL_DEFERRED
@@ -790,7 +794,7 @@ class TasksTests(test_utils.EmailTestBase):
             taskqueue_services.get_cloud_task_run_by_model_id(new_model_id)
         )
         assert cloud_task_run_model_obj is not None
-        self.assertEqual(cloud_task_run_model_obj.latest_job_state, 'SUCCEEDED')
+        self.assertEqual(cloud_task_run_model_obj.latest_job_state, 'RUNNING')
 
     def test_should_handle_failure_case_for_voiceover_deferred_tasks(
         self,
