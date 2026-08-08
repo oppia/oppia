439ef3cfae85f1e9d8f8a3f928f045392da63d13
31puneet <puneet9591@gmail.com>
2026-08-08 13:14:58 +0000

    Fixed Lint

--- core/templates/pages/contributor-dashboard-page/modal-templates/translation-modal.component.ts
+++ core/templates/pages/contributor-dashboard-page/modal-templates/translation-modal.component.ts
@@ -57,7 +57,7 @@ import {TranslatedContent} from 'domain/exploration/translated-content.model';
 import {ConfirmTranslationExitModalComponent} from 'components/translation-suggestion-page/confirm-translation-exit-modal/confirm-translation-exit-modal.component';
 import {ConfirmFormulaAsTextModalComponent} from 'pages/contributor-dashboard-page/modal-templates/confirm-formula-as-text-modal.component';
 import {WindowRef} from 'services/contextual/window-ref.service';
-import {InteractionSpecsKey} from 'pages/interaction-specs.constants';
+
 import './translation-modal.component.css';
 
 const INTERACTION_SPECS = require('interactions/interaction_specs.json');
