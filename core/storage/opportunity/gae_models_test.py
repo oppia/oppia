d3bc09edcf616edb09ccc86d5b1180b3fffc30f8
Rohan-Unbeg <rohanunbeg0918@gmail.com>
2026-08-21 16:13:44 +0530

    Clean PR diff: remove unrelated service and storage test files

--- core/storage/opportunity/gae_models_test.py
+++ core/storage/opportunity/gae_models_test.py
@@ -25,7 +25,7 @@ MYPY = False
 if MYPY:  # pragma: no cover
     from mypy_imports import base_models, opportunity_models
 
-base_models, opportunity_models = models.Registry.import_models(
+(base_models, opportunity_models) = models.Registry.import_models(
     [models.Names.BASE_MODEL, models.Names.OPPORTUNITY]
 )
 
