1b642d93c5c6c0711aa64ff786b8df9228a97632
31puneet <puneet9591@gmail.com>
2026-08-25 13:06:07 +0000

    Added test file for migration job

---
+++ core/jobs/batch_jobs/translation_opportunity_migration_jobs_test.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+#
+# Copyright 2026 The Oppia Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Unit tests for translation_opportunity_migration_jobs."""
+
+from __future__ import annotations
+
+from core.jobs import job_test_utils
+from core.jobs.batch_jobs import translation_opportunity_migration_jobs
+from core.jobs.types import job_run_result
+
+
+class BackfillTranslationMissingReasonsJobTests(job_test_utils.JobTestBase):
+    """Tests for BackfillTranslationMissingReasonsJob."""
+
+    JOB_CLASS = (
+        translation_opportunity_migration_jobs.BackfillTranslationMissingReasonsJob
+    )
+
+    def test_empty_storage(self) -> None:
+        self.assert_job_output_is([])
