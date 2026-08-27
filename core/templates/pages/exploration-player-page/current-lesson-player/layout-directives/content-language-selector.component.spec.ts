5bcb08124d5bd226e9c70b8d4514916c622630ba
Rohan-Unbeg <rohanunbeg0918@gmail.com>
2026-08-27 14:51:03 +0530

    Fix part of #24933: Apply the initial content language translations on lesson load

--- core/templates/pages/exploration-player-page/current-lesson-player/layout-directives/content-language-selector.component.spec.ts
+++ core/templates/pages/exploration-player-page/current-lesson-player/layout-directives/content-language-selector.component.spec.ts
@@ -58,6 +58,7 @@ import {PageContextService} from '../../../../services/page-context.service';
 import {StateEditorService} from 'components/state-editor/state-editor-properties-services/state-editor.service';
 import {LoggerService} from '../../../../services/contextual/logger.service';
 import {UrlInterpolationService} from '../../../../domain/utilities/url-interpolation.service';
+import {ContentTranslationManagerService} from '../../services/content-translation-manager.service';
 
 class MockContentTranslationLanguageService {
   currentLanguageCode!: string;
@@ -110,6 +111,7 @@ describe('Content language selector component', () => {
   let stateEditorService: StateEditorService;
   let loggerService: LoggerService;
   let urlInterpolationService: UrlInterpolationService;
+  let contentTranslationManagerService: ContentTranslationManagerService;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -158,6 +160,9 @@ describe('Content language selector component', () => {
     pageContextService = TestBed.inject(PageContextService);
     loggerService = TestBed.inject(LoggerService);
     urlInterpolationService = TestBed.inject(UrlInterpolationService);
+    contentTranslationManagerService = TestBed.inject(
+      ContentTranslationManagerService
+    );
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
@@ -172,6 +177,34 @@ describe('Content language selector component', () => {
         {value: 'zh', displayed: '中文 (Chinese)'},
         {value: 'en', displayed: 'English'},
       ]);
+    }
+  );
+
+  it('should display translations for the initial content language', () => {
+    const displayTranslationsSpy = spyOn(
+      contentTranslationManagerService,
+      'displayTranslations'
+    );
+
+    component.ngOnInit();
+
+    expect(displayTranslationsSpy).toHaveBeenCalledWith('fr');
+  });
+
+  it(
+    'should not display translations when the initial content language is ' +
+      'not available in the exploration',
+    () => {
+      const displayTranslationsSpy = spyOn(
+        contentTranslationManagerService,
+        'displayTranslations'
+      );
+      windowRef.nativeWindow.location.href =
+        'http://localhost:8181/explore/wZiXFx1iV5bz?initialContentLanguageCode=hi';
+
+      component.ngOnInit();
+
+      expect(displayTranslationsSpy).not.toHaveBeenCalled();
     }
   );
 
