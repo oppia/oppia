828b42e24780a34dc76793437a4568e9761fd32d
Rohan-Unbeg <rohanunbeg0918@gmail.com>
2026-09-09 22:25:08 +0530

    Fix part of #24933: Update translation-modal.component.spec.ts for Category translation removal

--- core/templates/pages/contributor-dashboard-page/modal-templates/translation-modal.component.spec.ts
+++ core/templates/pages/contributor-dashboard-page/modal-templates/translation-modal.component.spec.ts
@@ -1688,13 +1688,6 @@ describe('Translation Modal Component', () => {
         )
       ).toBe('objective');
       expect(
-        component.getFormattedContentType(
-          'metadata',
-          null,
-          'exploration_category'
-        )
-      ).toBe('category');
-      expect(
         component.getFormattedContentType('metadata', null, 'exploration_tag_0')
       ).toBe('tag');
       expect(component.getFormattedContentType('metadata', null, 'other')).toBe(
