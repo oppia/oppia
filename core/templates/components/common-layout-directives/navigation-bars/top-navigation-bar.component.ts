ccbbe15e62fe7d6933941fde10a31fcd924a9a01
hardikgoyal2003 <hardikgoyal2003@gmail.com>
2026-09-10 14:10:19 +0530

    Remove dead feedback updates fetch call

--- core/templates/components/common-layout-directives/navigation-bars/top-navigation-bar.component.ts
+++ core/templates/components/common-layout-directives/navigation-bars/top-navigation-bar.component.ts
@@ -34,7 +34,6 @@ import {SiteAnalyticsService} from 'services/site-analytics.service';
 import {UserService} from 'services/user.service';
 import {DeviceInfoService} from 'services/contextual/device-info.service';
 import debounce from 'lodash/debounce';
-import {AlertsService} from 'services/alerts.service';
 import {WindowDimensionsService} from 'services/contextual/window-dimensions.service';
 import {SearchService} from 'services/search.service';
 import {EventToCodes, NavigationService} from 'services/navigation.service';
@@ -48,8 +47,7 @@ import {CreatorTopicSummary} from 'domain/topic/creator-topic-summary.model';
 import {UrlService} from 'services/contextual/url.service';
 import {PlatformFeatureService} from 'services/platform-feature.service';
 import {LearnerGroupBackendApiService} from 'domain/learner_group/learner-group-backend-api.service';
-import {FeedbackUpdatesBackendApiService} from 'domain/feedback_updates/feedback-updates-backend-api.service';
-import {FeedbackThreadSummaryBackendDict} from 'domain/feedback_thread/feedback-thread-summary.model';
+
 import {LanguageBannerService} from 'components/language-banner/language-banner.service';
 import {SignInEventService} from 'services/sign-in-event.service';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
@@ -150,8 +148,6 @@ export class TopNavigationBarComponent implements OnInit, OnDestroy {
   windowIsNarrow: boolean = false;
   profilePicturePngDataUrl!: string;
   profilePictureWebpDataUrl!: string;
-  unreadThreadsCount: number = 0;
-  paginatedThreadsList: FeedbackThreadSummaryBackendDict[][] = [];
   isWebFeedbackModalEnabled: boolean = false;
 
   // The 'username', 'profilePageUrl' properties
@@ -207,8 +203,6 @@ export class TopNavigationBarComponent implements OnInit, OnDestroy {
     private pageContextService: PageContextService,
     private i18nLanguageCodeService: I18nLanguageCodeService,
     private i18nService: I18nService,
-    private alertsService: AlertsService,
-    private feedbackUpdatesBackendApiService: FeedbackUpdatesBackendApiService,
     private sidebarStatusService: SidebarStatusService,
     private urlInterpolationService: UrlInterpolationService,
     private navigationService: NavigationService,
@@ -308,27 +302,6 @@ export class TopNavigationBarComponent implements OnInit, OnDestroy {
         this.isBlogPostEditor = userInfo.isBlogPostEditor();
         this.userIsLoggedIn = userInfo.isLoggedIn();
         let usernameFromUserInfo = userInfo.getUsername();
-        if (this.userIsLoggedIn) {
-          let feedbackUpdatesDataPromise =
-            this.feedbackUpdatesBackendApiService.fetchFeedbackUpdatesDataAsync(
-              this.paginatedThreadsList
-            );
-          feedbackUpdatesDataPromise.then(
-            responseData => {
-              this.unreadThreadsCount = responseData.numberOfUnreadThreads;
-            },
-            errorResponseStatus => {
-              if (
-                AppConstants.FATAL_ERROR_CODES.indexOf(errorResponseStatus) !==
-                -1
-              ) {
-                this.alertsService.addWarning(
-                  'Failed to get number of unread thread of feedback updates'
-                );
-              }
-            }
-          );
-        }
         if (usernameFromUserInfo) {
           this.username = usernameFromUserInfo;
           this.profilePageUrl = this.urlInterpolationService.interpolateUrl(
