13832f9bcec49aa063223d02843a3ea241b42031
iHamza14 <anwarhamza1408@gmail.com>
2026-09-10 16:23:03 +0530

    Address review comments: isolate TODO stripping logic and relocate valid fixture

--- scripts/linters/test_files/invalid_todo.py
+++ scripts/linters/test_files/invalid_todo.py
@@ -20,9 +20,6 @@ python_linter_test. This file has invalid syntax of todo comment.
 
 from __future__ import annotations
 
-# This string should not trigger the linter
-const_name = "TODO #123"
-
 
 class FakeClass:
     """This is a fake docstring for invalid syntax purposes."""
