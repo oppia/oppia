bc42ff8d74e80604deb0d0ec381bf917464fc96b
NITISH KUMAR <imnitishkumar04@gmail.com>
2026-09-12 19:29:50 +0530

    [GSoC 2026] M2.10 - Fix part of #24716: Adds MigrateLegacyFeedbackJob Beam job to migrate existing GeneralFeedbackThreadModel and GeneralFeedbackMessageModel entries into LessonFeedbackModel (#27112)
    * added files and removed unused temporary functions
    
    * added migration logic
    
    * added unit tests for the migration job
    
    * fixed failing tests
    
    * fixed failing tests
    
    * removed trailing comma
    
    * skipping empty threads, i.e threads with no messages, nothing needs to be migrated in that case.
    
    ---------
    
    Co-authored-by: Mohit Ruwatia <120438312+mon4our@users.noreply.github.com>

--- scripts/linters/test_files/valid_job_imports.py
+++ scripts/linters/test_files/valid_job_imports.py
@@ -35,6 +35,7 @@ from core.jobs.batch_jobs import (  # pylint: disable=unused-import  # isort: sk
     exp_end_state_normalization_jobs,
     exploration_voiceover_sync_jobs,
     firebase_server_sync_jobs,
+    legacy_feedback_migration_jobs,
     math_interactions_audit_jobs,
     model_validation_jobs,
     number_with_units_audit_jobs,
